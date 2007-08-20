b4c8f27cd7c610539094ef56f37d91b43a526b68
Wulfsskin <Wulfsskin@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-08-20 16:22:32 +0000

    Setter Methode eingefügt
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@80 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/router/RouterData.java
+++ src/jd/router/RouterData.java
@@ -31,42 +31,52 @@ public class RouterData implements Serializable{
     private String connectionConnect;
     private String connectionDisconnect;
     private String connectionLogoff;
-    private StatusPageIPAdress statusIPAddress;
+   // private StatusPageIPAdress statusIPAddress;    
     
-    public String getConnectionConnect()           { return connectionConnect;    }
-    public String getConnectionDisconnect()        { return connectionDisconnect; }
-    public String getConnectionLogoff()            { return connectionLogoff;     }
-    public int getHttpPort()                       { return httpPort;             }
-    public String getLoginString()                 { return loginString;          }
-    public int getLoginType()                      { return loginType;            }
-    public String getRouterName()                  { return routerName;           }
-    public StatusPageIPAdress getStatusIPAddress() { return statusIPAddress;      }
+    public void setConnectionConnect(String connectionConnect)          {   this.connectionConnect = connectionConnect;         }
+    public String getConnectionConnect()                                {   return connectionConnect;                           }
+    public void setConnectionDisconnect(String connectionDisconnect)    {   this.connectionDisconnect = connectionDisconnect;   }
+    public String getConnectionDisconnect()                             {   return connectionDisconnect;                        }
+    public void setConnectionLogoff(String connectionLogoff)            {   this.connectionLogoff = connectionLogoff;           }
+    public String getConnectionLogoff()                                 {   return connectionLogoff;                            }
+    public void setHttpPort(int httpPort)                               {   this.httpPort = httpPort;                           }
+    public int getHttpPort()                                            {   return httpPort;                                    }
+    public void setLoginString(String loginString)                      {   this.loginString = loginString;                     }
+    public String getLoginString()                                      {   return loginString;                                 }
+    public void setLoginType(int loginType)                             {   this.loginType = loginType;                         }
+    public int getLoginType()                                           {   return loginType;                                   }
+    public void setRouterName(String routerName)                        {   this.routerName = routerName;                       }
+    public String getRouterName()                                       {   return routerName;                                  }
+    //public StatusPageIPAdress getStatusIPAddress() { return statusIPAddress;      }
     
-    public String getIPAdress(String data){
-        String ipAddress = null;
-        if(data.contains(statusIPAddress.offline)){
-            logger.fine("offline");
-            return null;
-        }
-        int index1 = data.indexOf(statusIPAddress.ipAddressPre)+statusIPAddress.ipAddressPre.length();
-        int index2 = data.indexOf(statusIPAddress.ipAddressPost);
-        if(index1!=-1 && index2 != -1 && index2-index1>0){
-            ipAddress = data.substring(index1, index2);
-        }
-        
-        return ipAddress;
-    }
+//    public String getIPAdress(String data){
+//        String ipAddress = null;
+//        if(data.contains(statusIPAddress.offline)){
+//            logger.fine("offline");
+//            return null;
+//        }
+//        int index1 = data.indexOf(statusIPAddress.ipAddressPre)+statusIPAddress.ipAddressPre.length();
+//        int index2 = data.indexOf(statusIPAddress.ipAddressPost);
+//        if(index1!=-1 && index2 != -1 && index2-index1>0){
+//            ipAddress = data.substring(index1, index2);
+//        }
+//        
+//        return ipAddress;
+//    }
     public String toString(){
         return "Router:"+routerName;
     }
-    public class StatusPageIPAdress{
-        private String website;
-        private String offline;
-        private String ipAddressPre;
-        private String ipAddressPost;
-        public String getWebsite() {
-            return website;
-        }
+//    public class StatusPageIPAdress{
+//        private String website;
+//        private String offline;
+//        private String ipAddressPre;
+//        private String ipAddressPost;
+//        public String getWebsite() {
+//            return website;
+//        }
+    
         
-    }
+   // }
+    
+    
 }
