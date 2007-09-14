81e33be66aa5598f74e231bd799c5f9ab04ce81f
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-09-14 22:36:19 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@189 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDUtilities.java
+++ src/jd/JDUtilities.java
@@ -866,22 +866,18 @@ if(!f.exists())return null;
     public static String htmlDecode(String str) {
       // http://rs218.rapidshare.com/files/&#0052;&#x0037;&#0052;&#x0034;&#0049;&#x0032;&#0057;&#x0031;/STE_S04E04.Borderland.German.dTV.XviD-2Br0th3rs.part1.rar
         String pattern = "\\&\\#x(.*?)\\;";
-        for (Matcher r = Pattern.compile(pattern, Pattern.DOTALL).matcher(str); r.find();) {
-            for (int x = 1; x <= r.groupCount(); x++) {
-                if (r.group(0).length() > 0) {
-                    char c = (char) Integer.parseInt(r.group(0).substring(3, 5), 16);
+        for (Matcher r = Pattern.compile(pattern, Pattern.DOTALL).matcher(str); r.find();) {          
+                if (r.group(1).length() > 0) {
+                    char c = (char) Integer.parseInt(r.group(1), 16);
                     str = str.replaceFirst("\\&\\#x(.*?)\\;", c + "");
-                }
-            }
+                }       
         }
         pattern = "\\&\\#(.*?)\\;";
         for (Matcher r = Pattern.compile(pattern, Pattern.DOTALL).matcher(str); r.find();) {
-            for (int x = 1; x <= r.groupCount(); x++) {
-                if (r.group(0).length() > 0) {
-                    char c = (char) Integer.parseInt(r.group(0).substring(3, 5), 10);
+                if (r.group(1).length() > 0) {
+                    char c = (char) Integer.parseInt(r.group(1), 10);
                     str = str.replaceFirst("\\&\\#(.*?)\\;", c + "");
                 }
-            }
         }
         return str;
     }
