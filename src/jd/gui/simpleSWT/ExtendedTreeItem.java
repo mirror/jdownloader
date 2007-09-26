3799882208269405d31e8c3754702ba04ecf55b1
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-09-26 04:46:19 +0000

    ExtendedTree sollte den Sourcecode wesentlich leichter machen
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@255 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/gui/simpleSWT/ExtendedTreeItem.java
@@ -0,0 +1,504 @@
+package jd.gui.simpleSWT;
+
+import java.util.HashMap;
+
+import jd.plugins.DownloadLink;
+import jd.utils.JDSWTUtilities;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TreeEditor;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+public class ExtendedTreeItem {
+    /**
+     * Hier werden TreeItemImages reingeladen fals sie benoetigt werden
+     */
+    private static HashMap<String, ImageMap> programmImages = new HashMap<String, ImageMap>();
+    /**
+     * Achtung wenn ein neuer TYPE gesetzt wird muss dafuer in der
+     * jd.JDSWTUtilities.java die Methode getTreeImage gegebenenfalls angepasst
+     * werden und neue Bilder in der MainGui.java Methode loadImages() geladen
+     * werden
+     */
+    public final static int STATUS_NOTHING = 1;
+    public final static int STATUS_ERROR = 2;
+    public final static int STATUS_QUEUE = 3;
+    public final static int STATUS_DOWNLOADING = 4;
+    public final static int TYPE_FILE = 1;
+    public final static int TYPE_FOLDER = 2;
+    public final static int TYPE_CONTAINER = 3;
+    public final static int TYPE_LOCKEDCONTAINER = 4;
+    public final static int TYPE_DEFAULT = 5;
+    public boolean drawProgrammImage = true;
+    public TreeItem item;
+    private int status = STATUS_NOTHING;
+    private int type = TYPE_FILE;
+    private boolean isLocked = false;
+    private DownloadLink downloadLink = null;
+    private TreeEditor editor = null;
+    private ProgressBar progressBar = null;
+
+    public ExtendedTreeItem(ExtendedTree parent) {
+        item = new TreeItem(parent.tree, SWT.NONE);
+        item.setData(this);
+    }
+
+    public ExtendedTreeItem(ExtendedTreeItem parent) {
+        item = new TreeItem(parent.item, SWT.NONE);
+        item.setData(this);
+    }
+
+    public ExtendedTreeItem(ExtendedTree parent, int index) {
+        item = new TreeItem(parent.tree, SWT.NONE, index);
+        item.setData(this);
+    }
+
+    public ExtendedTreeItem(ExtendedTreeItem parent, int index) {
+        item = new TreeItem(parent.item, SWT.NONE, index);
+        item.setData(this);
+    }
+
+    public ExtendedTreeItem getParentItem() {
+        return ((item.getParentItem()!=null)?((ExtendedTreeItem) item.getParentItem().getData()):null);
+    }
+
+    public ExtendedTreeItem getItem(int index) {
+        return (ExtendedTreeItem) item.getItem(index).getData();
+    }
+
+    public ExtendedTreeItem[] getItems() {
+        ExtendedTreeItem[] returnExtendedTreeItems = new ExtendedTreeItem[item.getItemCount()];
+        for (int i = 0; i < item.getItemCount(); i++) {
+            returnExtendedTreeItems[i] = (ExtendedTreeItem) item.getData();
+        }
+        return returnExtendedTreeItems;
+    }
+
+    public void dispose() {
+        for (int i = 0; i < getItemCount(); i++) {
+            getItem(i).dispose();
+        }
+        disposeProgressBar();
+        item.dispose();
+    }
+
+    public void disposeProgrammImage() {
+        String extension = null;
+        if (getType() == ExtendedTreeItem.TYPE_FILE && (getText(0) != null)) {
+            int dot = getText(0).lastIndexOf('.');
+            if (dot != -1)
+                extension = getText(0).substring(dot + 1);
+        }
+        if (extension != null) {
+            if (programmImages.containsKey(extension)) {
+                ImageMap im = programmImages.get(extension);
+                if (im.usageCount <= 1) {
+                    im.image.dispose();
+                    programmImages.remove(extension);
+                } else {
+                    im.usageCount--;
+                    programmImages.put(extension, im);
+                }
+            }
+        }
+    }
+
+    public String getText(int i) {
+        return item.getText(i);
+    }
+
+    public void setText(int column, String text) {
+
+        if (column == 0) {
+            if (downloadLink != null)
+                downloadLink.setName(text);
+            if (drawProgrammImage)
+                redrawProgrammImage(text);
+        }
+        item.setText(column, text);
+        setData(this);
+    }
+    public void setText(String text) {
+        if (drawProgrammImage)
+            redrawProgrammImage(text);
+        item.setText(text);
+        if (downloadLink != null)
+            downloadLink.setName(text);
+        setData(this);
+    }
+    public void setText(String[] text) {
+        if (drawProgrammImage)
+            redrawProgrammImage(text[0]);
+        item.setText(text);
+        if (downloadLink != null)
+            downloadLink.setName(text[0]);
+        setData(this);
+
+    }
+
+    /**
+     * Diese Methode gibt das Bild fuer den Entsprechenden Typ aus
+     * 
+     * @param display
+     * @param type
+     * @return
+     */
+    private Image ProgrammImage(String filename) {
+        Image img;
+        String imageString = null;
+        int type = getType();
+        if (type != TYPE_FILE) {
+
+            switch (type) {
+                case TYPE_FOLDER :
+                    imageString = "folder";
+                    break;
+                case TYPE_CONTAINER :
+                    imageString = "container";
+                    break;
+                case TYPE_LOCKEDCONTAINER :
+                    imageString = "container";
+                    break;
+                case TYPE_DEFAULT :
+                    imageString = "default";
+                    break;
+                default :
+                    imageString = "default";
+                    break;
+            }
+            return JDSWTUtilities.getImageSwt(imageString);
+        } else {
+            String extension = null;
+            if (type == ExtendedTreeItem.TYPE_FILE && (filename != null)) {
+                int dot = filename.lastIndexOf('.');
+                if (dot != -1)
+                    extension = filename.substring(dot + 1);
+            }
+            if (filename != null && extension != null) {
+                if (programmImages.containsKey(extension)) {
+                    ImageMap im = programmImages.get(extension);
+                    im.usageCount++;
+                    programmImages.put(extension, im);
+                    return im.image;
+                } else {
+                    Program program = Program.findProgram(extension);
+                    ImageData imageData = (program == null ? null : program.getImageData());
+
+                    if (imageData != null) {
+                        imageData = imageData.scaledTo(24, 24);
+                        img = new Image(item.getDisplay(), imageData);
+                        programmImages.put(extension, new ImageMap(img));
+                        return img;
+                    }
+                }
+            }
+
+        }
+        return JDSWTUtilities.getImageSwt("default");
+
+    }
+
+    private void redrawProgrammImage(String newFilename) {
+
+        String oldFilename = getText(0);
+        if (item.getImage()==null) {
+            item.setImage(ProgrammImage(newFilename));
+            return;
+        }
+        if (getType() != ExtendedTreeItem.TYPE_FILE)
+            return;
+        String oldExtension = null;
+        if (oldFilename != null) {
+            int dot = oldFilename.lastIndexOf('.');
+            if (dot != -1)
+                oldExtension = oldFilename.substring(dot + 1);
+        }
+        String newExtension = null;
+        if (newFilename != null) {
+            int dot = newFilename.lastIndexOf('.');
+            if (dot != -1)
+                newExtension = newFilename.substring(dot + 1);
+        }
+        if (oldExtension == newExtension)
+            return;
+        else {
+            item.setImage(ProgrammImage(newFilename));
+            if (oldExtension != null) {
+                if (programmImages.containsKey(oldExtension)) {
+                    ImageMap im = programmImages.get(oldExtension);
+                    if (im.usageCount <= 1) {
+                        im.image.dispose();
+                        programmImages.remove(oldExtension);
+                    } else {
+                        im.usageCount--;
+                        programmImages.put(oldExtension, im);
+                    }
+                }
+            }
+
+        }
+    }
+    public String[] getTextArray() {
+        String[] returnStrings = new String[item.getParent().getColumnCount()];
+        for (int i = 0; i < returnStrings.length; i++) {
+            returnStrings[i] = item.getText(i);
+        }
+        return returnStrings;
+    }
+    public Image[] getImages() {
+        Image[] returnImages = new Image[item.getParent().getColumnCount()];
+        for (int i = 0; i < returnImages.length; i++) {
+            returnImages[i] = item.getImage(i);
+        }
+        return returnImages;
+    }
+
+    public ProgressBar getProgressBar() {
+        return getdata().progressBar;
+    }
+    public void setProgressBar(final int index) {
+        final Tree tree = item.getParent();
+        final TreeItem treeItem = item;
+        progressBar = new ProgressBar(tree, SWT.SMOOTH);
+        final Listener listener = new Listener() {
+
+            public void handleEvent(Event e) {
+                if (e.stateMask == SWT.CTRL && tree.getSelectionCount() > 0) {
+                    Event event = new Event();
+                    event.widget = tree;
+                    int x = 0;
+                    int[] c = tree.getColumnOrder();
+                    for (int i = 0; i < c.length; i++) {
+                        if (c[i] == index)
+                            break;
+                        x += tree.getColumn(c[i]).getWidth();
+
+                    }
+
+                    event.x = x + e.x;
+                    event.y = treeItem.getBounds().y + 5;
+                    event.button = 1;
+                    TreeItem[] items = tree.getSelection();
+                    TreeItem[] items2;
+                    boolean isSelected = false;
+                    for (int i = 0; i < items.length; i++) {
+                        if (items[i] == treeItem) {
+                            isSelected = true;
+                            break;
+                        }
+                    }
+
+                    if (isSelected) {
+                        if (e.button != 1 && tree.getSelectionCount() > 1)
+                            return;
+                        items2 = new TreeItem[items.length - 1];
+                        int d = 0;
+                        for (int i = 0; i < items.length; i++) {
+                            if (items[i] != treeItem)
+                                items2[d++] = items[i];
+                        }
+                    } else {
+                        items2 = new TreeItem[items.length + 1];
+                        items2[0] = treeItem;
+                        for (int i = 1; i < items2.length; i++) {
+                            items2[i] = items[i - 1];
+                        }
+                    }
+                    // tree.setSelection(items);
+                    tree.setSelection(items2);
+                    tree.notifyListeners(SWT.MouseDown, event);
+                    tree.notifyListeners(SWT.Selection, event);
+                } else {
+                    if (e.button != 1 && tree.getSelectionCount() > 1) {
+                        TreeItem[] items = tree.getSelection();
+                        boolean isSelected = false;
+                        for (int i = 0; i < items.length; i++) {
+                            if (items[i] == treeItem) {
+                                isSelected = true;
+                                break;
+                            }
+                        }
+                        if (isSelected)
+                            return;
+                    }
+                    Event event = new Event();
+                    event.widget = tree;
+                    int x = 0;
+                    int[] c = tree.getColumnOrder();
+                    for (int i = 0; i < c.length; i++) {
+                        if (c[i] == index)
+                            break;
+                        x += tree.getColumn(c[i]).getWidth();
+
+                    }
+                    event.x = x + e.x;
+                    event.y = treeItem.getBounds().y + 5;
+                    event.button = 1;
+                    tree.notifyListeners(SWT.MouseDown, event);
+                    tree.setSelection(treeItem);
+                    tree.notifyListeners(SWT.Selection, event);
+                }
+
+            }
+
+        };
+
+        progressBar.addListener(SWT.MouseDown, listener);
+
+        progressBar.setMenu(tree.getMenu());
+        editor = new TreeEditor(tree);
+        editor.grabHorizontal = editor.grabVertical = true;
+        editor.setEditor(progressBar, treeItem, index);
+        setData(this);
+    }
+
+    public void disposeProgressBar() {
+        if (editor != null)
+            editor.dispose();
+        if (progressBar != null)
+            progressBar.dispose();
+        progressBar = null;
+        editor = null;
+        setData(this);
+    }
+    /**
+     * Dies ist die Methode um den Itemstatus auf Downloading zu stellen es wird
+     * eine Progressbar fuer dieses Item erzeugt.
+     */
+    public void setTreeItemDownloading() {
+        setProgressBar(2);
+        setStatus(ExtendedTreeItem.STATUS_DOWNLOADING);
+        lock();
+    }
+    
+    public ExtendedTreeItem getdata()
+    {
+        return (ExtendedTreeItem) item.getData();
+        
+    }
+    public int getStatus() {
+        return getdata().status;
+    }
+    public int getType() {
+        return getdata().type;
+    }
+
+    public DownloadLink getDownloadLink() {
+        return getdata().downloadLink;
+    }
+    public void setStatus(int status) {
+        this.status = status;
+        setData(this);
+    }
+    
+    public void setType(int type) {
+        this.type = type;
+        setData(this);
+    }
+    
+    public void setDownloadLink(DownloadLink downloadLink) {
+        this.downloadLink = downloadLink;
+        setData(this);
+    }
+    private ExtendedTreeItem setex(ExtendedTreeItem ex)
+    {
+        for (int i = 0; i < item.getItemCount(); i++) {
+            System.out.println(i);
+            getItem(i).setex(new ExtendedTreeItem(ex));
+        }
+
+        ex.setStatus(getStatus());
+        ex.setType(getType());
+        ex.setDownloadLink(getDownloadLink());
+        ex.setText(getTextArray());
+        disposeProgrammImage();
+        return ex;
+    }
+    
+    public void setPosition(ExtendedTreeItem parent) {
+        ExtendedTreeItem ex = new ExtendedTreeItem(parent);
+        ex=setex(ex);
+        item.dispose();
+        item=ex.item;
+
+    }
+    public void setPosition(ExtendedTree parent) {
+        ExtendedTreeItem ex = new ExtendedTreeItem(parent);
+        ex=setex(ex);
+        item.dispose();
+        item=ex.item;
+    }
+    public void setPosition(ExtendedTreeItem parent, int index) {
+        ExtendedTreeItem ex = new ExtendedTreeItem(parent, index);
+        ex=setex(ex);
+        item.dispose();
+        item=ex.item;
+    }
+    public void setPosition(ExtendedTree parent, int index) {
+        ExtendedTreeItem ex = new ExtendedTreeItem(parent, index);
+        ex=setex(ex);
+        item.dispose();
+        item=ex.item;
+    }
+    private int getItemCount() {
+        return item.getItemCount();
+    }
+
+    private void setData(Object itemData) {
+        item.setData(itemData);
+    }
+
+    public void lock() {
+        isLocked = true;
+        item.setData(this);
+    }
+    public boolean isLocked() {
+        return getdata().isLocked;
+
+    }
+    /**
+     * Checkt ob ein ElternItem selektiert ist
+     * 
+     * @param item
+     * @return
+     */
+    public boolean isParentSelected() {
+        TreeItem[] selection = item.getParent().getSelection();
+        TreeItem parent = item.getParentItem();
+        while (parent != null) {
+            for (int i = 0; i < selection.length; i++) {
+                if (parent == selection[i])
+                    return true;
+            }
+            parent = parent.getParentItem();
+        }
+        return false;
+    }
+
+    private class ImageMap {
+
+        private int usageCount;
+        private Image image;
+        private ImageMap(Image image) {
+            this.image = image;
+            usageCount = 1;
+        }
+    }
+
+    public String getText() {
+        return item.getText();
+    }
+
+    public Rectangle getBounds() {
+        return item.getBounds();
+    }
+
+}
