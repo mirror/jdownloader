17031b4a4ebc92aed9a48369f44ae52b638619ab
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-10-22 08:21:32 +0000

    CAPTCHA fix
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@345 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/Uploadedto.java
+++ src/jd/plugins/host/Uploadedto.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 import jd.plugins.DownloadLink;
@@ -17,18 +18,23 @@ public class Uploadedto extends PluginForHost {
     static private final Pattern PAT_SUPPORTED  = getSupportPattern("http://[*]uploaded.to/\\?id\\=[+]");
     static private final String  HOST           = "uploaded.to";
     static private final String  PLUGIN_NAME    = HOST;
-    static private final String  PLUGIN_VERSION = "0";
+    static private final String  PLUGIN_VERSION = "0.1";
     static private final String  PLUGIN_ID      = PLUGIN_NAME + "-" + PLUGIN_VERSION;
-    static private final String  CODER          = "coalado";
+    static private final String  CODER          = "coalado/DwD CAPTCHA fix";
     // /Simplepattern
     static private final String  DOWNLOAD_URL   = "<form name=\"download_form\" method=\"post\" action=\"°\">";
     private static final String  FILE_INFO      = "Dateiname:°</td><td><b>°</b></td></tr>°<tr><td style=\"padding-left:4px;\">Dateityp:°</td><td>°</td></tr>°<tr><td style=\"padding-left:4px;\">Dateig°</td><td>°</td>";
     static private final String	 FILE_NOT_FOUND = "Datei existiert nicht";
-    
+    private static final Pattern  CAPTCHA_FLE = Pattern.compile("<img name=\"img_captcha\" src=\"(.*?)\" border=0 />");
+    private static final Pattern  CAPTCHA_TEXTFLD = Pattern.compile("<input type=\"text\" id=\".*?\" name=\"(.*?)\" onkeyup=\"cap\\(\\)\\;\" size=3 />");
+    private HashMap<String, String>        postParameter                    = new HashMap<String, String>();
+    private String              captchaAddress;
+    private String              postTarget;
     private String               finalURL;
     
     public Uploadedto() {
         steps.add(new PluginStep(PluginStep.STEP_WAIT_TIME, null));
+        steps.add(new PluginStep(PluginStep.STEP_GET_CAPTCHA_FILE, null));
         steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
     }
     @Override
@@ -77,7 +83,9 @@ public class Uploadedto extends PluginForHost {
                         step.setStatus(PluginStep.STATUS_ERROR);
                         return step;
                     }
-                    
+                    this.captchaAddress= "http://" + requestInfo.getConnection().getRequestProperty("host") + "/" + getFirstMatch(requestInfo.getHtmlCode(), CAPTCHA_FLE, 1);
+
+                    this.postTarget = getFirstMatch(requestInfo.getHtmlCode(),CAPTCHA_TEXTFLD, 1);
                     String url = getSimpleMatch(requestInfo.getHtmlCode(), DOWNLOAD_URL, 0);
                     logger.info(url);
                     requestInfo = postRequest(new URL(url), "lang=de", null, null, null, false);
@@ -94,8 +102,24 @@ public class Uploadedto extends PluginForHost {
                     step.setStatus(PluginStep.STATUS_ERROR);
                     downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
                     return step;
+                case PluginStep.STEP_GET_CAPTCHA_FILE:
+                    File file = this.getLocalCaptchaFile(this);
+                    if (!JDUtilities.download(file, captchaAddress) || !file.exists()) {
+                        logger.severe("Captcha Download fehlgeschlagen: " + captchaAddress);
+                        step.setParameter(null);
+                        step.setStatus(PluginStep.STATUS_ERROR);
+                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_IMAGEERROR);
+                        return step;
+                    }
+                    else {
+                        step.setParameter(file);
+                        step.setStatus(PluginStep.STATUS_USER_INPUT);
+                    }
+                    break;
                 case PluginStep.STEP_DOWNLOAD:
+                    this.finalURL=finalURL+(String) steps.get(1).getParameter();
                     logger.info("dl " + finalURL);
+                    postParameter.put(postTarget, (String) steps.get(1).getParameter());
                     requestInfo = getRequestWithoutHtmlCode(new URL(finalURL), "lang=de", null, false);
                     if (requestInfo.getConnection().getHeaderField("Location") != null && requestInfo.getConnection().getHeaderField("Location").indexOf("error") > 0) {
                         step.setStatus(PluginStep.STATUS_ERROR);
