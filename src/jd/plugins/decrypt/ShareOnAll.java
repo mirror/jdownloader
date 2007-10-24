56673286866b5a305abdb7c008472b83635db819
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-10-24 07:14:52 +0000

    www.shareonall.com decryptor
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@352 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/ShareOnAll.java
@@ -0,0 +1,111 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jd.config.ConfigContainer;
+import jd.config.ConfigEntry;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+import jd.plugins.event.PluginEvent;
+
+public class ShareOnAll extends PluginForDecrypt {
+
+    final static String host = "shareonall.com";
+    private String version = "1.0.0.0";
+    private Pattern patternSupported = getSupportPattern("http://[*]shareonall.com/[+]");
+    private String[][] conf = new String[][]{{"USE_RAPIDSHARE", "Rapidshare.com"}, {"USE_FILEFACTORY", "Filefactory.com"}, {"USE_MEGAUPLOAD", "Megaupload.com"}, {"USE_DEPOSITFILES", "DepositFiles.com"}, {"USE_DIVSHARE", "DivShare.com"}, {"USE_ZSHARE", "ZShare.net"}};
+    public ShareOnAll() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+        this.setConfigEelements();
+    }
+
+    @Override
+    public String getCoder() {
+        return "DwD";
+    }
+    @Override
+    public String getHost() {
+        return host;
+    }
+    @Override
+    public String getPluginID() {
+        return "ShareOnAll.com-1.0.0.";
+    }
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+    @Override
+    public String getVersion() {
+        return version;
+    }
+
+    private boolean checkLink(String link) {
+
+        for (int i = 0; i < conf.length; i++) {
+            if ((Boolean) this.getProperties().getProperty(conf[i][0], true)) {
+                Pattern pattern = Pattern.compile(".*" + conf[i][1] + ".*", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(link);
+                if(matcher.find())return true;
+            }
+
+        }
+        return false;
+    }
+    @Override
+    public PluginStep doStep(PluginStep step, String parameter) {
+        if (step.getStep() == PluginStep.STEP_DECRYPT) {
+            Vector<String> decryptedLinks = new Vector<String>();
+            try {
+                parameter = parameter.replace("\\?.*", "");
+                URL url = new URL("http://www.shareonall.com/showlinks.php?f=" + getFirstMatch(parameter, Pattern.compile("http://.*?shareonall.com/(.*)", Pattern.CASE_INSENSITIVE), 1));
+                RequestInfo reqinfo = getRequest(url);
+
+                // Links herausfiltern
+                Vector<Vector<String>> links = getAllSimpleMatches(reqinfo.getHtmlCode(), "<a href=\'°\' target='_blank'>");
+                firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_MAX, links.size()));
+                for (int i = 0; i < links.size(); i++) {
+                    if (checkLink(links.get(i).get(0)))
+                        decryptedLinks.add(links.get(i).get(0));
+                    firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_INCREASE, null));
+                }
+
+                // Decrypt abschliessen
+                firePluginEvent(new PluginEvent(this, PluginEvent.PLUGIN_PROGRESS_FINISH, null));
+                step.setParameter(decryptedLinks);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    private void setConfigEelements() {
+        ConfigEntry cfg;
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_LABEL, "Hoster Auswahl"));
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_SEPERATOR));
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_RAPIDSHARE", "Rapidshare.com"));
+        cfg.setDefaultValue(true);
+        for (int i = 1; i < conf.length; i++) {
+            config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), conf[i][0], conf[i][1]));
+            cfg.setDefaultValue(false);
+        }
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+}
