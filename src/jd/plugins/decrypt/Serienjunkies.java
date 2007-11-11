a142949109e5211ff691f13b31b16c036c6eb4e1
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-11-11 16:57:37 +0000

    patternSupported serienjunki.es jetzt auch möglich
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@387 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/Serienjunkies.java
+++ src/jd/plugins/decrypt/Serienjunkies.java
@@ -23,7 +23,8 @@ public class Serienjunkies extends PluginForDecrypt {
     private static final String DEFAULT_PASSWORD = "serienjunkies.dl.am";
     private String              version          = "2.0.0.0";
     // http://85.17.177.195/sjsafe/f-e657c0c256dd9e58/rc_h324.html
-    private Pattern             patternSupported = getSupportPattern("http://85.17.177.195/[+]");
+    //http://serienjunki.es/sjsafe/f-3221260b2fc369e2/rc_scrubs610.html
+    private Pattern             patternSupported = getSupportPattern("http://(85.17.177.195|serienjunki\\.es)/s[+]");
 //    private Pattern             patternCaptcha   = Pattern.compile("e/secure/");
     private Pattern             patternCaptcha   = null;
 
