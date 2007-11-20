a98d6db1d624cbcc1f473981cff714a56ce9d1b6
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-11-20 13:23:50 +0000

    Änderung am Konfigurationsmenu und Festgelegt Hosts hinzugefügt
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@445 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/MultiDecrypt.java
+++ src/jd/plugins/decrypt/MultiDecrypt.java
@@ -16,7 +16,7 @@ public class MultiDecrypt extends PluginForDecrypt {
 
     static private String host = "MultiDecrypt";
     private String version = "0.1";
-
+    static private String[] SUPPORTEDHOSTS = new String[]{"stacheldraht.be/show.php", "netfolder.in/folder.php"};
     public MultiDecrypt() {
         super();
         steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
@@ -42,9 +42,16 @@ public class MultiDecrypt extends PluginForDecrypt {
     }
     @Override
     public Pattern getSupportedLinks() {
-        String strSupported = this.getProperties().getStringProperty("SUPPORTED", "stacheldraht.be/show.php;netfolder.in/folder.php");
-        String[] Supported = strSupported.split(";");
-;
+        String strSupported = this.getProperties().getStringProperty("SUPPORTED", "");
+        String[] Supp = strSupported.split(System.getProperty("line.separator"));
+        String[] Supported = new String[Supp.length + SUPPORTEDHOSTS.length];
+        for (int i = 0; i < Supp.length; i++) {
+            Supported[i] = Supp[i];
+        }
+        for (int i = 0; i < SUPPORTEDHOSTS.length; i++) {
+            Supported[Supp.length + i] = SUPPORTEDHOSTS[i];
+        }
+
         String patternStr = "http://[\\w\\.]*?(";
         if (Supported.length > 0) {
             Supported[0] = Supported[0].replaceFirst("http://", "").trim();
@@ -59,7 +66,7 @@ public class MultiDecrypt extends PluginForDecrypt {
             }
         }
         patternStr += ").*";
-        return  Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);
+        return Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);
     }
     @Override
     public String getVersion() {
@@ -77,16 +84,16 @@ public class MultiDecrypt extends PluginForDecrypt {
             if (inst != 1)
                 filesearch.getProperties().setProperty(FileSearch.PARAM_INST, 1);
             String lnk = parameter.replaceFirst("http://", "").trim();
-            
+
             if (lnk.matches("[^\\/]+?\\.[^\\/]+?\\."))
                 lnk = lnk.replaceFirst(".*?\\.", "").replaceFirst("\\/.*", "");
             else
                 lnk = lnk.replaceFirst("\\/.*", "");
-            lnk="http://[\\w\\.]*?"+lnk+".*";
+            lnk = "http://[\\w\\.]*?" + lnk + ".*";
             decryptedLinks.addAll((Collection<? extends String[]>) ((PluginStep) filesearch.doStep(new PluginStep(PluginStep.STEP_SEARCH, null), parameter)).getParameter());
             for (int i = decryptedLinks.size() - 1; i >= 0 && decryptedLinks.size() > i; i--) {
                 String[] link = decryptedLinks.elementAt(i);
-                if(link[0]==null || link[0].isEmpty() || link[0].matches(lnk))
+                if (link[0] == null || link[0].isEmpty() || link[0].matches(lnk))
                     decryptedLinks.remove(i);
                 try {
                     Thread.sleep(20);
@@ -104,9 +111,21 @@ public class MultiDecrypt extends PluginForDecrypt {
     }
 
     private void setConfigElements() {
-        config.addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "Hier kann man URLS/Pattern hinzufügen die nach Links durchsucht werden sollen! (durch Semikolon getrennt)"));
-        ConfigEntry cfgTextField = new ConfigEntry(ConfigContainer.TYPE_TEXTFIELD, getProperties(), "SUPPORTED", "URLS: ");
-        cfgTextField.setDefaultValue("stacheldraht.be/show.php;netfolder.in/folder.php");
+        String[] hosts = new String[SUPPORTEDHOSTS.length];
+            for (int i = 0; i < SUPPORTEDHOSTS.length; i++) {
+               String lnk = SUPPORTEDHOSTS[i].replaceFirst("http://", "").trim();
+
+                if (lnk.matches("[^\\/]+?\\.[^\\/]+?\\."))
+                    lnk = lnk.replaceFirst(".*?\\.", "").replaceFirst("\\/.*", "");
+                else
+                    lnk = lnk.replaceFirst("\\/.*", "");
+                hosts[i]=lnk;
+            }
+        if(hosts.length>0)
+        config.addEntry(new ConfigEntry(ConfigContainer.TYPE_COMBOBOX, null, null, hosts, "Folgende Seiten sind hier fest eingetragen: "));
+        config.addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "Hier kann man URLS/Pattern hinzufügen die nach Links durchsucht werden sollen!"));
+        ConfigEntry cfgTextField = new ConfigEntry(ConfigContainer.TYPE_TEXTAREA, getProperties(), "SUPPORTED", "URLS: ");
+        cfgTextField.setDefaultValue("");
         config.addEntry(cfgTextField);
     }
 
