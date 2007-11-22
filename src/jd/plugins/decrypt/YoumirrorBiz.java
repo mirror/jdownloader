d03364598ad48464240588c48b0c1981e7c81a37
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-11-22 23:20:31 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@473 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/YoumirrorBiz.java
+++ src/jd/plugins/decrypt/YoumirrorBiz.java
@@ -17,19 +17,22 @@ public class YoumirrorBiz extends PluginForDecrypt {
 
 	final static String host = "youmirror.biz";
 	private String version = "1.0.0.0";
-	private Pattern patternSupported = getSupportPattern("http://[*]youmirror.biz/adfree/f[+]");
-	private Pattern patternRapidshare = Pattern.compile("1\" target=\"_blank\">rapidshare.com");
+	private Pattern patternSupported = getSupportPattern("http://[*]youmirror.biz[*]/f[+]");
+	private Pattern patternRapidshare = Pattern.compile("1\" target=\"_blank\">rapidshare.com");//+
 	private Pattern patternNetload = Pattern.compile("2\" target=\"_blank\">netload.in");
-	private Pattern patternUploaded = Pattern.compile("4\" target=\"_blank\">uploaded.to");
-	private Pattern patternGulli = Pattern.compile("5\" target=\"_blank\">gulli.com");
-	private Pattern patternShareonline = Pattern.compile("6\" target=\"_blank\">share-online.biz");
-	private Pattern patternLoad = Pattern.compile("7\" target=\"_blank\">load.to");
-	private Pattern patternSimpleupload = Pattern.compile("8\" target=\"_blank\">simpleupload.net");
-	private Pattern patternCocoshare = Pattern.compile("9\" target=\"_blank\">cocoshare.cc");
+	private Pattern patternUploaded = Pattern.compile("4\" target=\"_blank\">uploaded.to");//+
+	private Pattern patternGulli = Pattern.compile("5\" target=\"_blank\">gulli.com");//+
+	private Pattern patternShareonline = Pattern.compile("6\" target=\"_blank\">share-online.biz");//+
+	private Pattern patternLoad = Pattern.compile("7\" target=\"_blank\">load.to");//+
+	private Pattern patternSimpleupload = Pattern.compile("8\" target=\"_blank\">simpleupload.net");//+
+	private Pattern patternCocoshare = Pattern.compile("9\" target=\"_blank\">cocoshare.cc");//+
 	private Pattern patternFilehoster = Pattern.compile("10\" target=\"_blank\">filehoster.mobi");
-	private Pattern patternMegaupload = Pattern.compile("11\" target=\"_blank\">megaupload.com");
-	private Pattern patternSpeedyshare = Pattern.compile("12\" target=\"_blank\">speedyshare.com");
-	private Pattern patternArchiv = Pattern.compile("13\" target=\"_blank\">archiv.to");
+	private Pattern patternMegaupload = Pattern.compile("11\" target=\"_blank\">megaupload.com");//+
+	private Pattern patternSpeedyshare = Pattern.compile("12\" target=\"_blank\">speedyshare.com");//+
+	private Pattern patternArchiv = Pattern.compile("13\" target=\"_blank\">archiv.to");//+
+	private Pattern patternDatenklo = Pattern.compile("/14\" target=\"_blank\">datenklo.net");//+
+	private Pattern patternBluehost= Pattern.compile("/15\" target=\"_blank\">bluehost.to");//+
+	private Pattern patternSharebase = Pattern.compile("/16\" target=\"_blank\">sharebase.de");//+
 	/*
 	 * 1	Rapidshare.com
 	 * 2	Netload.in
@@ -44,6 +47,9 @@ public class YoumirrorBiz extends PluginForDecrypt {
 	 * 11	Megaupload.com
 	 * 12	Speedyshare
 	 * 13	archiv.to
+	 * 14	Datenklo
+	 * 15	Bluehost.to
+	 * 16	Sharebase.de
 	 */
 	
     public YoumirrorBiz() {
@@ -104,12 +110,22 @@ public class YoumirrorBiz extends PluginForDecrypt {
     			if((Boolean) this.getProperties().getProperty("USE_ARCHIV", true) && countOccurences(reqinfo.getHtmlCode(), patternArchiv) > 0) {
     				count++;
     			}
+    			if((Boolean) this.getProperties().getProperty("USE_BLUEHOST", true) && countOccurences(reqinfo.getHtmlCode(), patternBluehost) > 0) {
+    				count++;
+    			}
+    			if((Boolean) this.getProperties().getProperty("USE_DATENKLO", true) && countOccurences(reqinfo.getHtmlCode(), patternDatenklo) > 0) {
+    				count++;
+    			}
+    			if((Boolean) this.getProperties().getProperty("USE_SHAREBASE", true) && countOccurences(reqinfo.getHtmlCode(), patternSharebase) > 0) {
+    				count++;
+    			}
     			
     			firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_MAX, count));
 
     			RequestInfo reqhelp;
     			
     			parameter = parameter.replaceAll("/file/", "/out/");
+    			parameter = parameter.replaceAll("/adfree", "");
     			
     			if((Boolean) this.getProperties().getProperty("USE_RAPIDSHARE", true) && countOccurences(reqinfo.getHtmlCode(), patternRapidshare) > 0) {
     				reqhelp = getRequest(new URL(parameter + "/1"));
@@ -171,7 +187,22 @@ public class YoumirrorBiz extends PluginForDecrypt {
     				decryptedLinks.add(getBetween(reqhelp.getHtmlCode(), "src=\"", "\""));
     				firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_INCREASE, null));
     			}
-    			
+    			if((Boolean) this.getProperties().getProperty("USE_DATENKLO", true) && countOccurences(reqinfo.getHtmlCode(), patternDatenklo) > 0) {
+    				reqhelp = getRequest(new URL(parameter + "/14"));
+    				decryptedLinks.add(getBetween(reqhelp.getHtmlCode(), "src=\"", "\""));
+    				firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_INCREASE, null));
+    			}
+    			if((Boolean) this.getProperties().getProperty("USE_BLUEHOST", true) && countOccurences(reqinfo.getHtmlCode(), patternBluehost) > 0) {
+    				reqhelp = getRequest(new URL(parameter + "/15"));
+    				decryptedLinks.add(getBetween(reqhelp.getHtmlCode(), "src=\"", "\""));
+    				firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_INCREASE, null));
+    			}
+    			if((Boolean) this.getProperties().getProperty("USE_SHAREBASE", true) && countOccurences(reqinfo.getHtmlCode(), patternSharebase) > 0) {
+    				reqhelp = getRequest(new URL(parameter + "/16"));
+    				decryptedLinks.add(getBetween(reqhelp.getHtmlCode(), "src=\"", "\""));
+    				firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_INCREASE, null));
+    			}
+    			System.out.println(decryptedLinks.toString());
     			//Decrypt abschliessen
     			firePluginEvent(new PluginEvent(this,PluginEvent.PLUGIN_PROGRESS_FINISH, null));
     			step.setParameter(decryptedLinks);
@@ -210,6 +241,12 @@ public class YoumirrorBiz extends PluginForDecrypt {
         config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_SPEEDYSHARE", "Speedyshare.com"));
         cfg.setDefaultValue(false);
         config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_ARCHIV", "Archiv.to"));
+        cfg.setDefaultValue(false);
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_DATENKLO", "Datenklo.net"));
+        cfg.setDefaultValue(false);
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_BLUEHOST", "Bluehost.to"));
+        cfg.setDefaultValue(false);
+        config.addEntry(cfg = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getProperties(), "USE_SHAREBASE", "Sharebase.de"));
         cfg.setDefaultValue(false);
     }
     
