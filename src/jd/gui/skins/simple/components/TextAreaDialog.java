63e150f7078bcad54229c711c26afe84c345930b
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-11-28 13:43:24 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@490 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/gui/skins/simple/components/TextAreaDialog.java
@@ -0,0 +1,96 @@
+package jd.gui.skins.simple.components;
+
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import jd.utils.JDUtilities;
+
+/**
+ * Diese Klasse ist wie die Optionspane mit textfeld nur mit textarea
+ * 
+ * @author coalado
+ */
+public class TextAreaDialog extends JDialog implements ActionListener {
+    /**
+     * 
+     */
+
+    protected Insets    insets = new Insets(0, 0, 0, 0);
+
+    protected Logger    logger = JDUtilities.getLogger();
+
+    private JButton     btnOk;
+
+    private JButton     btnCancel;
+
+    private JTextArea   textArea;
+
+    private JLabel      lblText;
+
+    private JScrollPane scrollPane;
+
+    private String text=null;
+
+    private TextAreaDialog(JFrame frame, String title, String question, String def) {
+        super(frame);
+        setModal(true);
+        setLayout(new BorderLayout());
+
+        btnCancel = new JButton("Cancel");
+        btnCancel.addActionListener(this);
+        btnOk = new JButton("OK");
+        btnOk.addActionListener(this);
+        setTitle(title);
+        textArea = new JTextArea(10, 60);
+        scrollPane = new JScrollPane(textArea);
+        textArea.setEditable(true);
+        textArea.requestFocusInWindow();
+        if (question != null) {
+            this.add(new JLabel(question),BorderLayout.NORTH);
+        }
+        if (def != null) {
+            textArea.setText(def);
+        }
+        this.add(scrollPane,BorderLayout.CENTER);
+        JPanel p= new JPanel();
+        p.add(btnOk);
+        p.add(btnCancel);
+        this.add(p,BorderLayout.SOUTH);
+       pack();
+        this.setVisible(true);
+        
+        setLocation(JDUtilities.getCenterOfComponent(null, this));
+        getRootPane().setDefaultButton(btnOk);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+     
+
+    }
+    public static String showDialog(JFrame frame, String title, String question, String def){
+        TextAreaDialog  tda= new TextAreaDialog( frame,  title,  question,  def);
+        return tda.getText();
+        
+    }
+private String getText(){
+    return text;
+}
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnOk) {
+            this.text=textArea.getText();
+            dispose();
+        }else{
+            dispose();
+        }
+    }
+
+}
