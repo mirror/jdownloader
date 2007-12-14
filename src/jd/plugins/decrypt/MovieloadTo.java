4ec4701c1ccb5731f112bba1532c867f3940519c
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-12-14 20:47:30 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@539 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/MovieloadTo.java
@@ -0,0 +1,85 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+
+public class MovieloadTo extends PluginForDecrypt {
+
+    final static String host             = "movieload.to";
+
+    private String      version          = "1.0.0.0";
+
+    private Pattern     patternSupported = getSupportPattern("http://[*]movieload.to/v2/index.php\\?do=protect\\&i=[+]");
+
+    public MovieloadTo() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+    }
+
+    @Override
+    public String getCoder() {
+        return "Botzi";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return "Movieload.to-1.0.0.";
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+
+    @Override public PluginStep doStep(PluginStep step, String parameter) {
+    	if(step.getStep() == PluginStep.STEP_DECRYPT) {
+            Vector<String> decryptedLinks = new Vector<String>();
+    		try {
+    			URL url = new URL(parameter);
+    			RequestInfo reqinfo = getRequest(url);
+    			
+    			Vector<Vector<String>> links = getAllSimpleMatches(reqinfo.getHtmlCode(), "; popup_dl(°)\" ");
+    			progress.setRange(links.size());
+    			for(int i=0; i<links.size(); i++) {
+    				progress.increase(1);
+    				reqinfo = getRequest(new URL("http://movieload.to/v2/protector/futsch.php?i=" + links.get(i).get(0)));
+    				decryptedLinks.add(reqinfo.getLocation());
+    			}
+    			
+    			//Decrypt abschliessen    			
+    			step.setParameter(decryptedLinks);
+    		}
+    		catch(IOException e) {
+    			 e.printStackTrace();
+    		}
+    	}
+    	return null;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+}
