0b862898fd78d8ee8e548e62e128a0f2f8414c86
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-12-20 20:50:23 +0000

    Safelink über serienjunkies reaktiviert
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@569 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/Safelink.java
+++ src/jd/plugins/decrypt/Safelink.java
@@ -1,9 +1,6 @@
 package jd.plugins.decrypt;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Vector;
 import java.util.regex.Pattern;
 
@@ -11,13 +8,13 @@ import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginStep;
 
 public class Safelink extends PluginForDecrypt {
-    static private final String host             = "safelink.in";
+    static private final String host = "safelink.in";
 
-    private String              version          = "1.0.0.0";
-
-    private Pattern             patternSupported = Pattern.compile("http://(safelink.in|85.17.177.195)/rc-[/|.|a-zA-Z0-9|_|-]*");
-
-    private Pattern             Formp            = Pattern.compile(".FORM ACTION..http:..rapidshare.com.files.(.*)..METHOD..post. ID..postit", Pattern.CASE_INSENSITIVE);
+    private String version = "2.0.0.0";
+    // rs-M2MjVTNyIjN/lvs0123.part1.rar.html
+    // http://www.rapidsafe.net/rs-M2MjVTNyIjN/lvs0123.part1.rar.html
+    //   http://www.safelink.in/rc-UjZ4MWOwAjN/DG2.part02.rar.html
+    private Pattern patternSupported = Pattern.compile("http://.*?(safelink.in|85.17.177.195)/r[cs]\\-[a-zA-Z0-9]{11}/.*", Pattern.CASE_INSENSITIVE);
 
     public Safelink() {
         super();
@@ -30,7 +27,7 @@ public class Safelink extends PluginForDecrypt {
      */
     @Override
     public String getCoder() {
-        return "DwD aka James";
+        return "DwD";
     }
 
     @Override
@@ -40,7 +37,7 @@ public class Safelink extends PluginForDecrypt {
 
     @Override
     public String getPluginID() {
-        return "Salfelink-1.0.0.";
+        return "Salfelink"+version;
     }
 
     @Override
@@ -65,30 +62,18 @@ public class Safelink extends PluginForDecrypt {
 
     @Override
     public PluginStep doStep(PluginStep step, String parameter) {
-        String cryptedLink = (String) parameter;
         switch (step.getStep()) {
-            case PluginStep.STEP_DECRYPT:
+            case PluginStep.STEP_DECRYPT :
+                System.out.println(parameter);
                 Vector<String> decryptedLinks = new Vector<String>();
-                try {
-                    progress.setRange(1);
-                    String html = getRequest(new URL(cryptedLink)).getHtmlCode();
-                    String id = getFirstMatch(html, Formp, 1);
-                    if (id != null) {
-                        String link = "http://rapidshare.com/files/" + getFirstMatch(html, Formp, 1);
-                        decryptedLinks.add(link);
-                    }
-                    progress.increase(1);
-                    // veraltet: firePluginEvent(new PluginEvent(this,
-                    // PluginEvent.PLUGIN_PROGRESS_FINISH, null));
-                    currentStep = null;
-                }
-                catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-
+                progress.setRange(1);
+                parameter=parameter.replaceFirst("http://.*?/r", "http://serienjunkies.org/safe/r");
+                System.out.println(parameter);
+                decryptedLinks.add(parameter);
+                progress.increase(1);
+                // veraltet: firePluginEvent(new PluginEvent(this,
+                // PluginEvent.PLUGIN_PROGRESS_FINISH, null));
+                currentStep = null;
                 step.setParameter(decryptedLinks);
                 break;
 
