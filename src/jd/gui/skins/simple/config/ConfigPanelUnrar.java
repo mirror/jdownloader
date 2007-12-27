764695b60052a0fd21d1e8e49384be96802321f2
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-12-27 22:00:48 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@598 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/skins/simple/config/ConfigPanelUnrar.java
+++ src/jd/gui/skins/simple/config/ConfigPanelUnrar.java
@@ -81,7 +81,7 @@ public class ConfigPanelUnrar extends ConfigPanel implements ActionListener {
         addGUIConfigEntry(ce);
         ce = new GUIConfigEntry(new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, configuration, Unrar.PROPERTY_WAIT_FOR_TERMINATION, JDLocale.L("gui.config.unrar.thread", "Erst nach dem Entpacken mit dem Download fortfahren")).setDefaultValue(false));
         addGUIConfigEntry(ce);
-        ce = new GUIConfigEntry(new ConfigEntry(ConfigContainer.TYPE_BROWSEFILE, configuration, Unrar.PROPERTY_EXTRACTFOLDER, JDLocale.L("gui.config.unrar.extractfolder", "Ziehlordner: ")).setDefaultValue(null).setExpertEntry(true));
+        ce = new GUIConfigEntry(new ConfigEntry(ConfigContainer.TYPE_BROWSEFOLDER, configuration, Unrar.PROPERTY_EXTRACTFOLDER, JDLocale.L("gui.config.unrar.extractfolder", "Ziehlordner: ")).setDefaultValue(null).setExpertEntry(true));
         addGUIConfigEntry(ce);
         ce = new GUIConfigEntry(new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, configuration, Unrar.PROPERTY_ENABLE_EXTRACTFOLDER, JDLocale.L("gui.config.unrar.extractfolderenabled", "Im Ziehlordner entpacken (Wenn die Dateien nicht im Downloadordner entpackt werden sollen)")).setDefaultValue(false).setExpertEntry(true));
         addGUIConfigEntry(ce);
