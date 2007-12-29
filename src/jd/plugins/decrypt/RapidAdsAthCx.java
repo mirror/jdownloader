1b1784420b550b19f583075f23eb0c417b60fdfe
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2007-12-29 23:59:46 +0000

    rapidads.ath.cx
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@617 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/RapidAdsAthCx.java
@@ -0,0 +1,91 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+import jd.plugins.DownloadLink;
+
+public class RapidAdsAthCx extends PluginForDecrypt {
+
+    static private final String host             = "rapidads.ath.cx";
+
+    private String              version          = "1.0.0.0";
+
+    private Pattern     patternSupported = getSupportPattern("http://[*]rapidads.ath.cx/crypter/[+]");
+
+    public RapidAdsAthCx() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+    }
+
+    @Override
+    public String getCoder() {
+        return "Botzi";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return "RapidAds.ath.cx-1.0.0.";
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+
+    @Override public PluginStep doStep(PluginStep step, String parameter) {
+    	if(step.getStep() == PluginStep.STEP_DECRYPT) {
+            Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+    		try {
+    			URL url = new URL(parameter);
+    			
+    			progress.setRange(1);
+    			RequestInfo reqinfo = getRequest(url, null, null, true);
+    			String[] helpa = getBetween(reqinfo.getHtmlCode(), "<p><p><form action=\"", "\"").split("&#");
+    			String help = "";
+    			
+    			for(int i=0; i<helpa.length; i++) {
+    				if(!helpa[i].equals(""))
+    					help = help + String.valueOf((char) Integer.parseInt(helpa[i]));
+    			}
+    			
+    			progress.increase(1);
+    			decryptedLinks.add(this.createDownloadlink(help));
+    			
+    			// Decrypten abschliessen
+    			step.setParameter(decryptedLinks);
+    		}
+    		catch(IOException e) {
+    			 e.printStackTrace();
+    		}
+    	}
+    	return null;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+}
