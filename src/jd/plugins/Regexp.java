ae1a269d1c7453b0f1fbbfca3fb3ff53410bb685
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-01-07 20:18:11 +0000

    Dokumentation
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@674 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/Regexp.java
+++ src/jd/plugins/Regexp.java
@@ -7,40 +7,44 @@ import java.util.regex.Pattern;
 public class Regexp {
     private Matcher matcher;
     public Regexp(Matcher matcher) {
-        if(matcher == null)
-            return;
+        if (matcher == null) return;
         this.matcher = matcher;
     }
     public Regexp(String data, Pattern pattern) {
-        if(data==null || pattern == null)
-            return;
+        if (data == null || pattern == null) return;
         this.matcher = pattern.matcher(data);
     }
     public Regexp(String data, String pattern) {
-        if(data==null || pattern == null)
-            return;
-        this.matcher = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL).matcher(data);
+        if (data == null || pattern == null) return;
+        this.matcher = Pattern.compile(pattern,
+                Pattern.CASE_INSENSITIVE | Pattern.DOTALL).matcher(data);
     }
     public Regexp(String data, String pattern, int flags) {
-        if(data==null || pattern == null)
-            return;
+        if (data == null || pattern == null) return;
         this.matcher = Pattern.compile(pattern, flags).matcher(data);
     }
+    /**
+     * 
+     * gibt den ersten Treffer aus fals groups existieren von
+     * group 1 sonst von group 0
+     */
     public String getFirstMatch() {
-        if(matcher==null)
-            return null;
+        if (matcher == null) return null;
         if (matcher.groupCount() == 0)
             return getFirstMatch(0);
-        else
-            return getFirstMatch(1);
+        else return getFirstMatch(1);
     }
+    /**
+     * gibt den ersten Treffer einer group aus
+     */
     public String getFirstMatch(int group) {
-        if(matcher==null)
-            return null;
-        if (matcher.find())
-            return matcher.group(group);
+        if (matcher == null) return null;
+        if (matcher.find()) return matcher.group(group);
         return null;
     }
+    /**
+     * Gibt alle Treffer eines Matches in einem 2D array aus
+     */
     public String[][] getMatches() {
         if(matcher==null)
             return null;
@@ -63,6 +67,9 @@ public class Regexp {
         }
         return ar.toArray(new String[ar.size()][group.length]);
     }
+    /**
+     * gibt alle Treffer in einer group als Array aus
+     */
     public String[] getMatches(int group) {
         if(matcher==null)
             return null;
@@ -72,17 +79,25 @@ public class Regexp {
         }
         return ar.toArray(new String[ar.size()]);
     }
+    /**
+     * gibt die Anzahl der Treffer zurück
+     */
     public int count() {
-        if(matcher==null)
-            return 0;
+        if (matcher == null) return 0;
         int c = 0;
         while (matcher.find())
             c++;
         return c;
     }
+    /**
+     * gibt den matcher aus
+     */
     public Matcher getMatcher() {
         return this.matcher;
     }
+    /**
+     * setzt den matcher
+     */
     public void setMatcher(Matcher matcher) {
         this.matcher = matcher;
     }
