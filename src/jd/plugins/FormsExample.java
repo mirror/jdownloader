7cea870154cee69892f5bfabe5e0005052ffeb96
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-01-08 22:58:15 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@682 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/FormsExample.java
+++ src/jd/plugins/FormsExample.java
@@ -1,35 +1,30 @@
 package jd.plugins;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class FormsExample {
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-        try {
+ 
             //Suche auf Glück bei Google
             System.out.println("Suche auf Glück bei Google");
-            RequestInfo req = Plugin.getRequest(new URL("http://www.google.de/"));
-            Form[] forms = Form.getForms(req);
+            Form[] forms = Form.getForms("http://www.google.de/");
             for (int i = 0; i < forms.length; i++) {
                 System.out.println(forms[i].toString());
             }
-            Form form = forms[0];
+            Form form = Form.getForms("http://www.google.de/")[0];
             form.vars.remove("btnG");
             form.vars.put("q", "jDownloader");
-            req = form.getRequestInfo();
+            System.out.println(form.getRequestInfo().getLocation());
+            RequestInfo req = form.getRequestInfo();
 
             System.out.println(req.getConnection().getURL().toString());
             System.out.println(req.getLocation()+"\n\n");
             
             //Eintrag auf Pastebin
             System.out.println("Eintrag auf Pastebin");
-            req = Plugin.getRequest(new URL("http://jdownloader.pastebin.com/"));
-            forms = Form.getForms(req);
+            forms = Form.getForms("http://jdownloader.pastebin.com/");
             for (int i = 0; i < forms.length; i++) {
                 System.out.println(forms[i].toString());
             }
@@ -41,13 +36,6 @@ public class FormsExample {
             req = form.getRequestInfo();
             System.out.println(req.getConnection().getURL().toString());
 
-        } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
 
     }
 
