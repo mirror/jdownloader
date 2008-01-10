70e9fb488d197aa631d0c71f901efd66ca9b617a
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-01-10 22:34:16 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@695 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/unrar/Utilities.java
+++ src/jd/unrar/Utilities.java
@@ -1,108 +1,15 @@
 package jd.unrar;
 
-import java.beans.XMLDecoder;
-import java.beans.XMLEncoder;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.logging.Logger;
 
+import jd.gui.skins.simple.SimpleGUI;
 import jd.utils.JDUtilities;
 
 public class Utilities {
-
-    /**
-     * Lädt ein Objekt aus einer Datei
-     * 
-     * @param frame
-     *            Ein übergeordnetes Fenster
-     * @param fileInput
-     *            Falls das Objekt aus einer bekannten Datei geladen werden
-     *            soll, wird hier die Datei angegeben. Falls nicht, kann der
-     *            Benutzer über einen Dialog eine Datei aussuchen
-     * @param asXML
-     *            Soll das Objekt von einer XML Datei aus geladen werden?
-     * @return Das geladene Objekt
-     */ 
     public static Object loadObject(File fileInput, boolean asXML) {
-        Object objectLoaded = null;
-        if (fileInput != null && fileInput.exists()) {
-            try {
-                FileInputStream fis = new FileInputStream(fileInput);
-                if (asXML) {
-                    XMLDecoder xmlDecoder = new XMLDecoder(new BufferedInputStream(fis));
-                    objectLoaded = xmlDecoder.readObject();
-                    xmlDecoder.close();
-                } else {
-                    ObjectInputStream ois = new ObjectInputStream(fis);
-                    objectLoaded = ois.readObject();
-                    ois.close();
-                }
-                return objectLoaded;
-            } catch (ClassNotFoundException e) {
-                 e.printStackTrace();
-                 JDUtilities.getLogger().severe("Error while loading "+fileInput.getName()+" try to use bak file");
-                 return loadObject(new File(fileInput.getParent(), fileInput.getName()+".bak"), asXML);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                JDUtilities.getLogger().severe("Error while loading "+fileInput.getName()+" try to use bak file");
-                return loadObject(new File(fileInput.getParent(), fileInput.getName()+".bak"), asXML);
-            } catch (IOException e) {
-                e.printStackTrace();
-                JDUtilities.getLogger().severe("Error while loading "+fileInput.getName()+" try to use bak file");
-                return loadObject(new File(fileInput.getParent(), fileInput.getName()+".bak"), asXML);
-            } catch (Exception e) {
-                e.printStackTrace();
-                JDUtilities.getLogger().severe("Error while loading "+fileInput.getName()+" try to use bak file");
-                return loadObject(new File(fileInput.getParent(), fileInput.getName()+".bak"), asXML);
-            }
-        }
-        return null;
+      return JDUtilities.loadObject(((SimpleGUI) JDUtilities.getGUI()).getFrame(), fileInput, asXML);
     }
-    /**
-     * Speichert ein Objekt
-     * 
-     * @param frame
-     *            ein Fenster
-     * @param objectToSave
-     *            Das zu speichernde Objekt
-     * @param fileOutput
-     *            Das File, in das geschrieben werden soll. Falls das File ein
-     *            Verzeichnis ist, wird darunter eine Datei erstellt Falls keins
-     *            angegeben wird, soll der Benutzer eine Datei auswählen
-     * @param name
-     *            Dateiname
-     * @param extension
-     *            Dateiendung (mit Punkt)
-     * @param asXML
-     *            Soll das Objekt in eine XML Datei gespeichert werden?
-     */
     public static void saveObject(Object objectToSave, File fileOutput, boolean asXML) {
-        if (fileOutput != null) {
-            if (fileOutput.exists())
-                fileOutput.renameTo(new File(fileOutput.getParent(), fileOutput.getName()+".bak"));
-            try {
-                FileOutputStream fos = new FileOutputStream(fileOutput);
-                if (asXML) {
-                    XMLEncoder xmlEncoder = new XMLEncoder(new BufferedOutputStream(fos));
-                    xmlEncoder.writeObject(objectToSave);
-                    xmlEncoder.close();
-                } else {
-                    ObjectOutputStream oos = new ObjectOutputStream(fos);
-                    oos.writeObject(objectToSave);
-                    oos.close();
-                }
-            } catch (FileNotFoundException e) {
-                 e.printStackTrace();
-            } catch (IOException e) {
-                 e.printStackTrace();
-            }
-        }
+        JDUtilities.saveObject(((SimpleGUI) JDUtilities.getGUI()).getFrame(), objectToSave, fileOutput, null, null, asXML);
     }
 }
