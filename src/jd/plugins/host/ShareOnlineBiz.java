1433ac6db14b285a061d0e668fa761642effc109
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-04 13:45:09 +0000

    share-online.biz fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@792 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/ShareOnlineBiz.java
+++ src/jd/plugins/host/ShareOnlineBiz.java
@@ -311,10 +311,12 @@ public class ShareOnlineBiz extends PluginForHost {
 					ctime = Integer.parseInt(getFirstMatch(requestInfo
 							.getHtmlCode(), countDown, 1));
 				}
-				dlink = getFirstMatch(requestInfo.getHtmlCode(), downloadl, 1);
+				System.out.println(requestInfo.getHtmlCode());
+				/*dlink = getFirstMatch(requestInfo.getHtmlCode(), downloadl, 1);
 				if(!dlink.matches("http://.*"))
-					dlink = "http://" + url.getHost() + "/"+dlink;
-				logger.info(dlink);
+					dlink = "http://" + url.getHost() + "/"+dlink;*/
+				dlink = getBetween(requestInfo.getHtmlCode(), "document.location=\"", "\"");
+				//logger.info(dlink);
 				return step;
 
 			case PluginStep.STEP_PENDING:
