21663f8d34dcfe53f7845672417b71787f5065bf
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-07 20:45:50 +0000

    RamZal added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@807 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/RamZal.java
@@ -0,0 +1,141 @@
+package jd.plugins.host;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Pattern;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginStep;
+
+public class RamZal extends PluginForHost {
+	private static final String HOST = "ramzal.com";
+	private static final String VERSION = "1.0.0.0";
+	// http://ramzal.com//upload_files/1280838337_wallpaper-1280x1024-007.jpg
+	static private final Pattern patternSupported = Pattern.compile(
+			"http://.*?ramzal\\.com//?upload_files/.*",
+			Pattern.CASE_INSENSITIVE);
+
+	//
+	@Override
+	public boolean doBotCheck(File file) {
+		return false;
+	} // kein BotCheck
+
+	@Override
+	public String getCoder() {
+		return "GforE";
+	}
+
+	@Override
+	public String getPluginName() {
+		return HOST;
+	}
+
+	@Override
+	public String getHost() {
+		return HOST;
+	}
+
+	@Override
+	public String getVersion() {
+		return VERSION;
+	}
+
+	@Override
+	public String getPluginID() {
+		return HOST + "-" + VERSION;
+	}
+
+	@Override
+	public Pattern getSupportedLinks() {
+		return patternSupported;
+	}
+
+	public RamZal() {
+		super();
+		steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
+	}
+
+	public PluginStep doStep(PluginStep step, DownloadLink downloadLink) {
+		if (aborted) {
+			logger.warning("Plugin abgebrochen");
+			downloadLink.setStatus(DownloadLink.STATUS_TODO);
+			step.setStatus(PluginStep.STATUS_TODO);
+			return step;
+		}
+		try {
+			requestInfo = getRequestWithoutHtmlCode(new URL(downloadLink
+					.getDownloadURL()), null, null, true);
+
+			URLConnection urlConnection = requestInfo.getConnection();
+			downloadLink.setName(getFileNameFormHeader(urlConnection));
+			downloadLink.setDownloadMax(urlConnection.getContentLength());
+			if (!hasEnoughHDSpace(downloadLink)) {
+				downloadLink.setStatus(DownloadLink.STATUS_ERROR_NO_FREE_SPACE);
+				step.setStatus(PluginStep.STATUS_ERROR);
+				return step;
+			}
+			if (download(downloadLink, urlConnection)) {
+				step.setStatus(PluginStep.STATUS_DONE);
+				downloadLink.setStatus(DownloadLink.STATUS_DONE);
+				return null;
+			} else if (aborted) {
+				logger.warning("Plugin abgebrochen");
+				downloadLink.setStatus(DownloadLink.STATUS_TODO);
+				step.setStatus(PluginStep.STATUS_TODO);
+			} else {
+				logger.severe("Datei nicht gefunden");
+				downloadLink
+						.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
+				step.setStatus(PluginStep.STATUS_ERROR);
+			}
+
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return step;
+	}
+
+	@Override
+	public boolean getFileInformation(DownloadLink downloadLink) {
+		try {
+			requestInfo = getRequestWithoutHtmlCode(new URL(downloadLink
+					.getDownloadURL()), null, null, true);
+			URLConnection urlConnection = requestInfo.getConnection();
+			downloadLink.setName(getFileNameFormHeader(urlConnection));
+			downloadLink.setDownloadMax(urlConnection.getContentLength());
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return false;
+	}
+
+	@Override
+	public int getMaxSimultanDownloadNum() {
+		return Integer.MAX_VALUE;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Automatisch erstellter Methoden-Stub
+	}
+
+	@Override
+	public void resetPluginGlobals() {
+		// TODO Automatisch erstellter Methoden-Stub
+	}
+
+	@Override
+	public String getAGBLink() {
+
+		return "hhttp://ramzal.com/";
+	}
+}
