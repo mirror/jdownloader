6699cf0f8c89f20459ae12b9661a291e18ae45fe
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-08 17:09:46 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@818 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/BackupLink.java
@@ -0,0 +1,78 @@
+package jd.plugins;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+
+import jd.config.Configuration;
+import jd.controlling.SpeedMeter;
+import jd.utils.JDUtilities;
+
+
+public class BackupLink implements Serializable {
+  
+
+    public static final int LINKTYPE_NORMAL = 0;
+    public static final int LINKTYPE_CONTAINER = 1;
+  
+
+    /**
+     * Von hier soll de Download stattfinden
+     */
+    private String            urlDownload;
+ 
+    /**
+     * Containername
+     */
+    private String            container;
+    
+
+    /**
+     * Dateiname des Containers
+     */
+    private String            containerFile;
+    /**
+     * Index dieses DownloadLinks innerhalb der Containerdatei
+     */
+    private int               containerIndex=-1;
+    private int linkType;
+    private String containerType;
+  
+  
+    public BackupLink(String urlDownload) {       
+        linkType=LINKTYPE_NORMAL;
+            this.urlDownload = urlDownload;
+    
+    }
+    public BackupLink(File containerfile, int id,String containerType) {       
+        containerFile=containerfile.getAbsolutePath();
+        containerIndex=id;
+       this.containerType=containerType;
+        linkType=LINKTYPE_CONTAINER;
+
+}
+    public String getUrlDownload() {
+        return urlDownload;
+    }
+    public String getContainer() {
+        return container;
+    }
+    public String getContainerFile() {
+        return containerFile;
+    }
+    public int getContainerIndex() {
+        return containerIndex;
+    }
+    public int getLinkType() {
+        return linkType;
+    }
+    public String getContainerType() {
+        return containerType;
+    }
+public String toString(){
+    return " containerType :"+containerType+" linkType :"+linkType+" containerIndex :"+containerIndex+" container :"+container+" urlDownload :"+urlDownload;
+}
+   
+}
