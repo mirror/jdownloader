bce86c19fee6f593d9c096bd83e1fd84f95233c5
eXecuTe <eXecuTe@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-12 16:50:12 +0000

    yourfiles.biz folder added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@843 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/YourFilesBizFolder.java
@@ -0,0 +1,118 @@
+package jd.plugins.decrypt;
+  import jd.plugins.DownloadLink;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.Regexp;
+import jd.plugins.RequestInfo;
+import jd.utils.JDLocale;
+import jd.utils.JDUtilities;
+
+public class YourFilesBizFolder extends PluginForDecrypt {
+
+    final static String host             = "yourfiles.biz";
+    final static String name             = "yourfiles.biz Folder";
+
+    private String      version          = "0.1.0";
+
+    private Pattern     patternSupported = getSupportPattern(
+    		"http://[*]yourfiles\\.biz/[*]/folders/[0-9]+/[+].html");
+    
+    public YourFilesBizFolder() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+    }
+
+    @Override
+    public String getCoder() {
+        return "eXecuTe";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return host+"-"+version;
+    }
+
+    @Override
+    public String getPluginName() {
+        return name;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+    
+    @Override public PluginStep doStep(PluginStep step, String parameter) {
+    	
+    	if(step.getStep() == PluginStep.STEP_DECRYPT) {
+    		
+            Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+    		
+    		try {
+    			
+    			RequestInfo reqinfo = getRequest(new URL(parameter));
+    			
+    			if ( reqinfo.getHtmlCode().contains("Ordner Passwort") ) {
+    				
+    				String url = parameter.substring(0,parameter.lastIndexOf("/")+1)
+    						+ new Regexp(reqinfo.getHtmlCode(),"action\\=(folders\\.php\\?fid\\=.*)method\\=post>").getFirstMatch().trim();
+    				String cookie = reqinfo.getCookie();
+    				String password = JDUtilities.getController().getUiInterface().showUserInputDialog(JDLocale.L("plugins.decrypt.passwordProtected","Die Links sind mit einem Passwort gesch\u00fctzt. Bitte geben Sie das Passwort ein:"));
+    				String post = "act=login&password="+password+"&login=Einloggen";
+    				HashMap<String,String> reqinfoHeaders = new HashMap<String,String>();
+    				reqinfoHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+    				
+    				reqinfo = postRequest(new URL(url), cookie, parameter, reqinfoHeaders, post, false);
+    				
+    				url = reqinfo.getConnection().getHeaderField("Location");
+    				reqinfo = getRequest(new URL(url), reqinfo.getCookie(), parameter, false);
+    				
+    			}
+    			
+    			Vector<Vector<String>> ids = getAllSimpleMatches(reqinfo.getHtmlCode(), "href='http://yourfiles.biz/?d=°'");
+    			progress.setRange(ids.size());
+    			
+    			for ( int i=0; i<ids.size(); i++ ) {
+    				
+    				decryptedLinks.add(this.createDownloadlink("http://yourfiles.biz/?d="+ids.get(i).get(0)));
+    				progress.increase(1);
+    				
+	    		}
+				
+				step.setParameter(decryptedLinks);
+				
+    		} catch(IOException e) {
+    			e.printStackTrace();
+    		}
+	        
+    	}
+    	
+    	return null;
+    	
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+    
+}
