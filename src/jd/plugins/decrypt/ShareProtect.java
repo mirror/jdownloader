953bd40b17ff7ef1445ae08daa9a2918f05c73f4
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-22 17:10:18 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@893 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/ShareProtect.java
@@ -0,0 +1,100 @@
+package jd.plugins.decrypt;
+
+import jd.plugins.DownloadLink;
+import java.io.File;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import jd.plugins.Form;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.utils.JDUtilities;
+
+public class ShareProtect extends PluginForDecrypt {
+	final static String host = "shareprotect.t-w.at";
+//http://shareprotect.t-w.at/?id=F6FR
+	private String version = "1.0.0.0";
+
+	// http://s2.saug.us/folder-m304wzg6qreym6d8xvtpt3gmyfqzjh0.html
+	private Pattern patternSupported = Pattern.compile(
+			"http://shareprotect.t-w.at/\\?id\\=[a-zA-Z0-9\\-]{3,10}",
+			Pattern.CASE_INSENSITIVE);
+
+	public ShareProtect() {
+		super();
+		steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+		currentStep = steps.firstElement();
+	}
+
+	@Override
+	public String getCoder() {
+		return "JD-Team";
+	}
+
+	@Override
+	public String getHost() {
+		return host;
+	}
+
+	@Override
+	public String getPluginID() {
+		return host + "-" + VERSION;
+	}
+
+	@Override
+	public String getPluginName() {
+		return host;
+	}
+
+	@Override
+	public Pattern getSupportedLinks() {
+		return patternSupported;
+	}
+
+	@Override
+	public String getVersion() {
+		return version;
+	}
+
+	@Override
+	public PluginStep doStep(PluginStep step, String parameter) {
+		if (step.getStep() == PluginStep.STEP_DECRYPT) {
+			Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+			try {
+
+				request.getRequest(parameter);
+				String[] matches = request.getRegexp("unescape\\(\\'(.*?)'\\)").getMatches(1);
+				StringBuffer htmlc = new StringBuffer();
+				for (int i = 0; i < matches.length; i++) {
+					htmlc.append(JDUtilities.htmlDecode(matches[i])+"\n");
+				}
+				requestInfo=request.getRequestInfo();
+				requestInfo.setHtmlCode(htmlc.toString());
+
+				String[] links = request.getRegexp("<input type=\"button\" value=\"Free\" onClick=.*? window\\.open\\(\\'\\./(.*?)\\'").getMatches(1);
+				progress.setRange(links.length);
+				htmlc = new StringBuffer();
+				for (int i = 0; i < links.length; i++) {
+					request.getRequest("http://"+request.getHost()+"/"+links[i]);
+					htmlc.append(JDUtilities.htmlDecode(request.getRegexp("unescape\\(\\'(.*?)'\\)").getFirstMatch())+"\n");
+					progress.increase(1);
+				}
+				requestInfo.setHtmlCode(htmlc.toString());
+				Form[] forms = requestInfo.getForms();
+				for (int i = 0; i < forms.length; i++) {
+					decryptedLinks.add(createDownloadlink(forms[i].action));
+				}
+
+				step.setParameter(decryptedLinks);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean doBotCheck(File file) {
+		return false;
+	}
+}
