fc64a9acbf1c160384adf0936eefdc4362ea4df2
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-02-26 14:43:21 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@921 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/TestJACController.java
+++ src/jd/captcha/TestJACController.java
@@ -1,7 +1,10 @@
 package jd.captcha;
 
+import jd.utils.JDLocale;
+
 public class TestJACController {
     public static void main(String[] args) {
+        JDLocale.setLocale("german");
     JACController.showDialog(true);
     }
 }
