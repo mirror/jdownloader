0381ed0449c3da21ee6ef09cbcfbe3231b216464
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-04-29 13:08:18 +0000

    ces
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1204 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/captcha/CES.java
@@ -0,0 +1,160 @@
+package jd.captcha;
+
+import java.awt.BorderLayout;
+import java.awt.Dialog;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import jd.config.Configuration;
+import jd.gui.skins.simple.CaptchaDialog;
+import jd.gui.skins.simple.SimpleGUI;
+import jd.plugins.Plugin;
+import jd.utils.JDLocale;
+import jd.utils.JDTheme;
+import jd.utils.JDUtilities;
+
+
+public class CES {
+   private static boolean enabled = false;
+   private static String loginDialog()
+   {
+       return new Dialog(((SimpleGUI) JDUtilities.getGUI()).getFrame()) {
+           JTextField user = new JTextField();
+           JTextField pass = new JTextField();
+           String ret = null;
+           /**
+            * 
+            */
+           private static final long serialVersionUID = -5144850223169000644L;
+
+           String init() {
+               setLayout(new BorderLayout());
+               setModal(true);
+               setTitle(JDLocale.L("jac.CES.login.title", "CaptchaExchangeServer login"));
+               setAlwaysOnTop(true);
+               setSize(400, 120);
+               setLocation(JDUtilities.getCenterOfComponent(getOwner(), this));
+               JPanel panel = new JPanel(new GridBagLayout());
+               addWindowListener(new WindowListener() {
+
+                   public void windowActivated(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+
+                   public void windowClosed(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+
+                   public void windowClosing(WindowEvent e) {
+                       dispose();
+
+                   }
+
+                   public void windowDeactivated(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+
+                   public void windowDeiconified(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+
+                   public void windowIconified(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+
+                   public void windowOpened(WindowEvent e) {
+                       // TODO Auto-generated method stub
+
+                   }
+               });
+               user = new JTextField(100);
+               
+               pass = new JTextField(100);
+               panel.setLayout(new GridLayout(2, 2));
+               panel.add(new JLabel(JDLocale.L("jac.CES.login.user", "Benutzername:"), JLabel.CENTER));
+               panel.add(user);
+               panel.add(new JLabel(JDLocale.L("jac.CES.login.pass", "Passwort:"), JLabel.CENTER));
+               panel.add(pass);
+               
+               JButton btnOK = new JButton(JDLocale.L("gui.btn_continue", "OK"));
+               btnOK.addActionListener(new ActionListener() {
+
+                   public void actionPerformed(ActionEvent e) {
+                       String un = user.getText();
+                       String pw = pass.getText();
+                       if(un!=null && !un.matches("[\\s]*") && pw!=null && !pw.matches("[\\s]*") )
+                       {
+                           ret="user="+JDUtilities.urlEncode(un)+"&pass="+JDUtilities.urlEncode(pw);
+                       }
+                       dispose();
+                   }
+
+               });
+
+               add(panel, BorderLayout.NORTH);
+               add(btnOK, BorderLayout.SOUTH);
+               setVisible(true);
+               return ret;
+           }
+
+       }.init();
+   }
+   public static String getLonginString()
+   {
+       String loginstring =  JDUtilities.getConfiguration().getStringProperty(Configuration.JAC_CES_LOGIN, null);
+       if(loginstring==null)
+       {
+           loginstring=loginDialog();
+       }
+       return loginstring;
+   }
+   public static boolean isEnabled()
+   {
+       return enabled;
+   }
+   public static void setEnabled(boolean bool)
+   {
+       enabled=bool;
+   }
+   public static boolean toggleActivation()
+   {
+       setEnabled(!enabled);
+       return enabled;
+   }
+   public static String getCesImageString() {
+       if (CES.isEnabled())
+           return JDTheme.V("gui.images.ceson");
+       else
+           return JDTheme.V("gui.images.cesoff");
+
+   }
+   public static ImageIcon getImage()
+   {
+       return new ImageIcon(JDUtilities.getImage(getCesImageString()));
+   }
+   public static String captchaDialog(Plugin plugin, File captcha)
+   {
+       CaptchaDialog captchaDialog = new CaptchaDialog(((SimpleGUI) JDUtilities.getGUI()).getFrame(), plugin, captcha, "");
+       captchaDialog.countdown=30;
+       // frame.toFront();
+      // captchaDialog.
+       captchaDialog.setVisible(true);
+       return captchaDialog.getCaptchaText();
+   }
+}
