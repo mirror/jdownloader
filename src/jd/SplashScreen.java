3137b4654e547765728e8f792b8d0caad17178fe
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-05-12 14:11:33 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1264 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/SplashScreen.java
+++ src/jd/SplashScreen.java
@@ -17,7 +17,6 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
