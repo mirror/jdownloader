0f6d834d9586a4f7c2c1b28e12954e25dac839db
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-05-24 00:28:46 +0000

    Filer.net Hosterplugin fix.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1373 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/Filer.java
+++ src/jd/plugins/host/Filer.java
@@ -14,7 +14,6 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 package jd.plugins.host;
 
 import java.io.File;
@@ -34,7 +33,7 @@ import jd.utils.JDUtilities;
 public class Filer extends PluginForHost {
     /*
      * Pseudolink um getFileInformation zu ermöglichen
-    */
+     */
     static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?filer.net/(file[\\d]+|get)/.*", Pattern.CASE_INSENSITIVE);
     static private final Pattern GETID = Pattern.compile("http://[\\w\\.]*?filer.net/file([\\d]+)/.*?", Pattern.CASE_INSENSITIVE);
     static private final String HOST = "filer.net";
@@ -44,8 +43,9 @@ public class Filer extends PluginForHost {
     static private final String CODER = "JD-Team";
     static private final String FILE_NOT_FOUND = "Konnte Download nicht finden";
     static private final String FILE_NOT_FOUND2 = "Oops! We couldn't find this page for you.";
-    static private final String FREE_USER_LIMIT="Momentan sind die Limits f&uuml;r Free-Downloads erreicht";
+    static private final String FREE_USER_LIMIT = "Momentan sind die Limits f&uuml;r Free-Downloads erreicht";
     static private final String CAPTCHAADRESS = "http://www.filer.net/captcha.png";
+    //<form method="post" action="/get/e14eaf55d686a2b.html"><p class="center">
     static private final Pattern DOWNLOAD = Pattern.compile("<form method=\"post\" action=\"(\\/dl\\/.*?)\">", Pattern.CASE_INSENSITIVE);
     static private final Pattern WAITTIME = Pattern.compile("Bitte warten Sie ([\\d]+)", Pattern.CASE_INSENSITIVE);
     static private final Pattern INFO = Pattern.compile("(?s)<td><a href=\"(\\/get\\/.*?.html)\">(.*?)</a></td>.*?<td>([0-9\\.]+) .*?</td>", Pattern.CASE_INSENSITIVE);
@@ -62,18 +62,22 @@ public class Filer extends PluginForHost {
         steps.add(new PluginStep(PluginStep.STEP_PENDING, null));
         steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
     }
+
     @Override
     public String getCoder() {
         return CODER;
     }
+
     @Override
     public String getPluginName() {
         return HOST;
     }
+
     @Override
     public Pattern getSupportedLinks() {
         return PAT_SUPPORTED;
     }
+
     @Override
     public String getHost() {
         return HOST;
@@ -83,195 +87,192 @@ public class Filer extends PluginForHost {
     public String getVersion() {
         return PLUGIN_VERSION;
     }
+
     @Override
     public String getPluginID() {
         return PLUGIN_ID;
     }
+
     public PluginStep doStep(PluginStep step, DownloadLink parameter) {
         RequestInfo requestInfo;
-        logger.info("Step: "+step);
+        logger.info("Step: " + step);
         try {
             DownloadLink downloadLink = (DownloadLink) parameter;
             switch (step.getStep()) {
-                case PluginStep.STEP_WAIT_TIME :
-                    String strId = getFirstMatch(downloadLink.getDownloadURL(), GETID, 1);
-                    if(strId!=null)
-                    {
-                        int id=Integer.parseInt(strId);
-                        url=downloadLink.getDownloadURL().replaceFirst("filer.net\\/file[0-9]+\\/", "filer.net/folder/");
-                        url=url.replaceFirst("\\/filename\\/.*","");
-                        requestInfo = getRequest(new URL(url));
-                            // Datei geloescht?
-                            if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND2)) {
-                                return step;
-                            }
-                            ArrayList<ArrayList<String>> matches = getAllSimpleMatches(requestInfo.getHtmlCode(), INFO);
-                            if(matches.size()<id)
-                                return step;
-                            ArrayList<String> link = matches.get(id);
-                        url="http://www.filer.net"+link.get(0);
+            case PluginStep.STEP_WAIT_TIME:
+                String strId = getFirstMatch(downloadLink.getDownloadURL(), GETID, 1);
+                if (strId != null) {
+                    int id = Integer.parseInt(strId);
+                    url = downloadLink.getDownloadURL().replaceFirst("filer.net\\/file[0-9]+\\/", "filer.net/folder/");
+                    url = url.replaceFirst("\\/filename\\/.*", "");
+                    requestInfo = getRequest(new URL(url));
+                    // Datei geloescht?
+                    if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND2)) { return step; }
+                    ArrayList<ArrayList<String>> matches = getAllSimpleMatches(requestInfo.getHtmlCode(), INFO);
+                    if (matches.size() < id) return step;
+                    ArrayList<String> link = matches.get(id);
+                    url = "http://www.filer.net" + link.get(0);
+                    cookie = requestInfo.getCookie();
+                    requestInfo = getRequest(new URL(url));
+                    if(cookie==null)cookie = requestInfo.getCookie();
+                } else
+                    url = downloadLink.getDownloadURL();
 
-                        requestInfo = getRequest(new URL(url));
-                        cookie = requestInfo.getCookie();  
-                    }
-                    else
-                        url=downloadLink.getDownloadURL();
-                 
-                    break;
-                case PluginStep.STEP_GET_CAPTCHA_FILE :
-                    File file = this.getLocalCaptchaFile(this);
-                    requestInfo = getRequestWithoutHtmlCode(new URL(CAPTCHAADRESS), cookie, url, false);
-                    cookie=requestInfo.getConnection().getHeaderField("Set-Cookie");
-                    if (!JDUtilities.download(file, requestInfo.getConnection()) || !file.exists()) {
-                        logger.severe("Captcha Download fehlgeschlagen: " + CAPTCHAADRESS);
-                        step.setParameter(null);
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_IMAGEERROR);
-                        return step;
-                    } else {
-                        step.setParameter(file);
-                        step.setStatus(PluginStep.STATUS_USER_INPUT);
-                    }
-                    break;
-                case PluginStep.STEP_PENDING :
-                    String code = (String) steps.get(1).getParameter();
-                    requestInfo = postRequest(new URL(url), cookie, url, null, "captcha=" + code + "&Download=Download", true);
-                    dlink = getFirstMatch(requestInfo.getHtmlCode(), DOWNLOAD, 1);
-                   
-                    
-                   
-                    if (requestInfo.containsHTML(FREE_USER_LIMIT)) {
-                        logger.severe("Free User Limit reached");
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_PLUGIN_SPECIFIC);
-                        step.setParameter("Free User Limit");
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        return step;
-                    }
-                    if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND)) {
-                        logger.severe("Die Datei existiert nicht");
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        return step;
-                    }
-
-                    String strWaitTime = getFirstMatch(requestInfo.getHtmlCode(), WAITTIME, 1);
-                    if (strWaitTime != null) {
-                        logger.severe("wait " + strWaitTime + " minutes");
-                        waitTime = Integer.parseInt(strWaitTime) * 60 * 1000;
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_DOWNLOAD_LIMIT);
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        logger.info(" WARTEZEIT SETZEN IN " + step + " : " + waitTime);
-                        step.setParameter((long) waitTime);
-                        return step;
-                    }
-
-                    if (dlink == null) {
-                        logger.severe("Der Downloadlink konnte nicht gefunden werden");
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        return step;
-                    }
-                    dlink = "http://www.filer.net" +dlink;
-                    if(requestInfo.getHtmlCode().contains(WRONG_CAPTCHACODE))
-                    {
-                        logger.severe("Wrog captcah code retry");
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
-                        return step;
-                    }
-                    //auf 61 sec gesetzt weil der server sonst zickt
-                    step.setParameter(61000l);
-                    break;
-                case PluginStep.STEP_DOWNLOAD :
-                  
-
-                    requestInfo = postRequestWithoutHtmlCode(new URL(dlink), cookie, url, "Download!=Download!", true);
-                    if (requestInfo.getConnection().getHeaderField("Location") != null && requestInfo.getConnection().getHeaderField("Location").indexOf("error") > 0) {
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
-                        step.setParameter(20000l);
-                        return step;
-                    }
-                    int length = requestInfo.getConnection().getContentLength();
-                    downloadLink.setDownloadMax(length);
-                    logger.info("Filename: " + getFileNameFormHeader(requestInfo.getConnection()));
-                    if (getFileNameFormHeader(requestInfo.getConnection()) == null || getFileNameFormHeader(requestInfo.getConnection()).indexOf("?") >= 0) {
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
-                        step.setParameter(20000l);
-                        return step;
-                    }
-                    downloadLink.setName(getFileNameFormHeader(requestInfo.getConnection()));
-                    if (!hasEnoughHDSpace(downloadLink)) {
-                        downloadLink.setStatus(DownloadLink.STATUS_ERROR_NO_FREE_SPACE);
-                        step.setStatus(PluginStep.STATUS_ERROR);
-                        return step;
-                    }
-                   dl = new RAFDownload(this, downloadLink, requestInfo.getConnection());
-                    dl.startDownload();
-              
+                break;
+            case PluginStep.STEP_GET_CAPTCHA_FILE:
+                File file = this.getLocalCaptchaFile(this);
+                requestInfo = getRequestWithoutHtmlCode(new URL(CAPTCHAADRESS), cookie, url, false);
+                //cookie = requestInfo.getConnection().getHeaderField("Set-Cookie");
+                if (!JDUtilities.download(file, requestInfo.getConnection()) || !file.exists()) {
+                    logger.severe("Captcha Download fehlgeschlagen: " + CAPTCHAADRESS);
+                    step.setParameter(null);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_IMAGEERROR);
                     return step;
+                } else {
+                    step.setParameter(file);
+                    step.setStatus(PluginStep.STATUS_USER_INPUT);
+                }
+                break;
+            case PluginStep.STEP_PENDING:
+                String code = (String) steps.get(1).getParameter();
+                requestInfo = postRequest(new URL(url), cookie, url, null, "captcha=" + code + "&Download=Download", true);
+                dlink = getFirstMatch(requestInfo.getHtmlCode(), DOWNLOAD, 1);
+
+                if (requestInfo.containsHTML(FREE_USER_LIMIT)) {
+                    logger.severe("Free User Limit reached");
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_PLUGIN_SPECIFIC);
+                    step.setParameter("Free User Limit");
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+                if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND)) {
+                    logger.severe("Die Datei existiert nicht");
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+
+                String strWaitTime = getFirstMatch(requestInfo.getHtmlCode(), WAITTIME, 1);
+                if (strWaitTime != null) {
+                    logger.severe("wait " + strWaitTime + " minutes");
+                    waitTime = Integer.parseInt(strWaitTime) * 60 * 1000;
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_DOWNLOAD_LIMIT);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    logger.info(" WARTEZEIT SETZEN IN " + step + " : " + waitTime);
+                    step.setParameter((long) waitTime);
+                    return step;
+                }
+
+                if (dlink == null) {
+                    logger.info(requestInfo+"");
+                    logger.severe("Der Downloadlink konnte nicht gefunden werden");
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+                dlink = "http://www.filer.net" + dlink;
+                if (requestInfo.getHtmlCode().contains(WRONG_CAPTCHACODE)) {
+                    logger.severe("Wrog captcah code retry");
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
+                    return step;
+                }
+                // auf 61 sec gesetzt weil der server sonst zickt
+                step.setParameter(61000l);
+                break;
+            case PluginStep.STEP_DOWNLOAD:
+
+                requestInfo = postRequestWithoutHtmlCode(new URL(dlink), cookie, url, "Download!=Download!", true);
+                if (requestInfo.getConnection().getHeaderField("Location") != null && requestInfo.getConnection().getHeaderField("Location").indexOf("error") > 0) {
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
+                    step.setParameter(20000l);
+                    return step;
+                }
+                int length = requestInfo.getConnection().getContentLength();
+                downloadLink.setDownloadMax(length);
+                logger.info("Filename: " + getFileNameFormHeader(requestInfo.getConnection()));
+                if (getFileNameFormHeader(requestInfo.getConnection()) == null || getFileNameFormHeader(requestInfo.getConnection()).indexOf("?") >= 0) {
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN_RETRY);
+                    step.setParameter(20000l);
+                    return step;
+                }
+                downloadLink.setName(getFileNameFormHeader(requestInfo.getConnection()));
+                if (!hasEnoughHDSpace(downloadLink)) {
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_NO_FREE_SPACE);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+                dl = new RAFDownload(this, downloadLink, requestInfo.getConnection());
+                dl.startDownload();
+
+                return step;
             }
             return step;
         } catch (IOException e) {
-             e.printStackTrace();
+            e.printStackTrace();
             return null;
         }
     }
+
     @Override
     public boolean doBotCheck(File file) {
         return false;
     }
+
     @Override
     public void reset() {
         this.url = null;
     }
+
     public String getFileInformationString(DownloadLink downloadLink) {
         return downloadLink.getName() + " (" + JDUtilities.formatBytesToMB(downloadLink.getDownloadMax()) + ")";
     }
+
     @Override
     public boolean getFileInformation(DownloadLink downloadLink) {
         RequestInfo requestInfo;
         try {
             String strId = getFirstMatch(downloadLink.getDownloadURL(), GETID, 1);
-            if(strId==null)
-                return true;
-            int id=Integer.parseInt(strId);
-            url=downloadLink.getDownloadURL().replaceFirst("filer.net\\/file[0-9]+\\/", "filer.net/folder/");
-            url=url.replaceFirst("\\/filename\\/.*","");
+            if (strId == null) return true;
+            int id = Integer.parseInt(strId);
+            url = downloadLink.getDownloadURL().replaceFirst("filer.net\\/file[0-9]+\\/", "filer.net/folder/");
+            url = url.replaceFirst("\\/filename\\/.*", "");
             requestInfo = getRequest(new URL(url));
-                // Datei geloescht?
-                if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND2)) {
-                    return false;
+            // Datei geloescht?
+            if (requestInfo.getHtmlCode().contains(FILE_NOT_FOUND2)) { return false; }
+            ArrayList<ArrayList<String>> matches = getAllSimpleMatches(requestInfo.getHtmlCode(), INFO);
+            if (matches.size() < id) return false;
+            ArrayList<String> link = matches.get(id);
+            downloadLink.setName(link.get(1));
+            if (link != null) {
+                try {
+                    int length = (int) (Double.parseDouble(link.get(2)) * 1024 * 1024);
+                    downloadLink.setDownloadMax(length);
+                } catch (Exception e) {
                 }
-                ArrayList<ArrayList<String>> matches = getAllSimpleMatches(requestInfo.getHtmlCode(), INFO);
-                if(matches.size()<id)
-                    return false;
-                ArrayList<String> link = matches.get(id);
-                downloadLink.setName(link.get(1));
-                if (link != null) {
-                    try {
-                        int length = (int) (Double.parseDouble(link.get(2)) * 1024 * 1024);
-                        downloadLink.setDownloadMax(length);
-                    } catch (Exception e) {
-                    }
-                }
-            
+            }
+
             return true;
         } catch (MalformedURLException e) {
         } catch (IOException e) {
         }
         return false;
     }
+
     @Override
     public int getMaxSimultanDownloadNum() {
         return 1;
     }
+
     @Override
     public void resetPluginGlobals() {
         // TODO Auto-generated method stub
-        
+
     }
+
     @Override
     public String getAGBLink() {
         // TODO Auto-generated method stub
