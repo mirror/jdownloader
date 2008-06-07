5e321b0813159b520394bf2ea56abeb1524eda21
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-06-07 19:42:42 +0000

    data.hu added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1552 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/DataHu.java
@@ -0,0 +1,169 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+package jd.plugins.host;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTPConnection;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginStep;
+import jd.plugins.download.RAFDownload;
+
+public class DataHu extends PluginForHost {
+
+    private static final String  HOST             = "data.hu";
+
+    private static final String  VERSION          = "1.0.0";
+
+    static private final Pattern patternSupported = getSupportPattern("http://[w]data.hu/get/[+]/[+]");
+
+    //
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getPluginName() {
+        return HOST;
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    @Override
+    public String getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public String getPluginID() {
+        return HOST + "-" + VERSION;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    public DataHu() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
+    }
+
+    public PluginStep doStep(PluginStep step, final DownloadLink downloadLink) {
+        if (aborted) {
+            logger.warning("Plugin aborted");
+            downloadLink.setStatus(DownloadLink.STATUS_TODO);
+            step.setStatus(PluginStep.STATUS_TODO);
+            return step;
+        }
+        try {
+            String url = downloadLink.getDownloadURL();                    
+            requestInfo = getRequest(new URL(url));
+            
+            String link = getBetween(requestInfo.getHtmlCode(), "window.location.href='", "'");
+            String[] test = link.split("/");
+            String name = test[test.length-1];
+            downloadLink.setName(name);
+            
+            requestInfo = getRequestWithoutHtmlCode(new URL(link), null, url, false);
+            
+            HTTPConnection urlConnection = requestInfo.getConnection();
+            if (!getFileInformation(downloadLink)) {
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
+                step.setStatus(PluginStep.STATUS_ERROR);
+                return step;
+            }
+            
+            downloadLink.setDownloadMax(urlConnection.getContentLength());
+            final long length = downloadLink.getDownloadMax();
+            
+
+            dl = new RAFDownload(this, downloadLink, urlConnection);
+            dl.setFilesize(length);
+            //dl.setChunkNum(JDUtilities.getSubConfig("DOWNLOAD").getIntegerProperty(Configuration.PARAM_DOWNLOAD_MAX_CHUNKS, 3));
+            
+            if (!dl.startDownload() && step.getStatus() != PluginStep.STATUS_ERROR && step.getStatus() != PluginStep.STATUS_TODO) {
+
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+                step.setStatus(PluginStep.STATUS_ERROR);
+                return step;
+            }
+            return step;
+
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        step.setStatus(PluginStep.STATUS_ERROR);
+        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
+
+        return step;
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) {
+        try {
+            String url = downloadLink.getDownloadURL();
+            requestInfo = getRequest(new URL(url));
+            if(requestInfo.getHtmlCode().length() == 0)
+                return false;
+            
+            String[] test = getBetween(requestInfo.getHtmlCode(), "window.location.href='", "'").split("/");
+            String name = test[test.length-1];
+            downloadLink.setName(name);
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanDownloadNum() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void reset() {}
+
+    @Override
+    public void resetPluginGlobals() {}
+
+    @Override
+    public String getAGBLink() {
+        return "http://filebase.to/tos/";
+    }
+}
