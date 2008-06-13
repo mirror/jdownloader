3787298df35956a9457b3512ded394cb4c53638c
eXecuTe <eXecuTe@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-06-13 16:49:50 +0000

    newzfind.com decrypt
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1588 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/NewzFindCom.java
@@ -0,0 +1,121 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.Regexp;
+import jd.plugins.RequestInfo;
+import jd.utils.JDLocale;
+import jd.utils.JDUtilities;
+
+public class NewzFindCom extends PluginForDecrypt {
+
+    final static String host             = "newzfind.com";
+    final static String name             = "newzfind.com";
+    private String      version          = "0.1.0";
+    private Pattern     patternSupported = getSupportPattern("http://[*]newzfind\\.com/(video|music|games|software|mac|graphics|unix|magazines|e-books|xxx|other)/[+]");
+    
+    public NewzFindCom() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+    }
+
+    @Override
+    public String getCoder() {
+        return "jD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return host+"-"+version;
+    }
+
+    @Override
+    public String getPluginName() {
+        return name;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+    
+    @Override public PluginStep doStep(PluginStep step, String parameter) {
+    	
+    	if(step.getStep() == PluginStep.STEP_DECRYPT) {
+    		
+            Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+    		
+    		try {
+    			
+    			String url = "http://newzfind.com/ajax/links.html?a=" + parameter.substring(parameter.lastIndexOf("/")+1);
+    			RequestInfo reqinfo = getRequest(new URL(url));
+    			ArrayList<ArrayList<String>> links = getAllSimpleMatches(reqinfo.getHtmlCode(), "<link title=\"°\"");
+                
+    			reqinfo = getRequest(new URL(parameter));
+    			Vector<String> pws = findPasswords(reqinfo.getHtmlCode());
+                default_password.addAll(pws);
+                
+    			progress.setRange(links.size());
+    			
+    			for ( int i=0; i<links.size(); i++ ) {
+    				
+    				decryptedLinks.add(this.createDownloadlink(links.get(i).get(0)));
+    				progress.increase(1);
+    				
+	    		}
+				
+				step.setParameter(decryptedLinks);
+				
+    		} catch(IOException e) {
+    			e.printStackTrace();
+    		}
+	        
+    	}
+    	
+    	return null;
+    	
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+    
+}
