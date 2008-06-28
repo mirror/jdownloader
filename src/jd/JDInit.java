cc3b1f9949bec1354d7db2486e69096a0e7e3f06
eXecuTe <eXecuTe@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-06-28 18:50:50 +0000

    rss/atom feed plugin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1709 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInit.java
+++ src/jd/JDInit.java
@@ -401,6 +401,7 @@ public class JDInit {
         optionalPluginsVersionsArray.add(new optionalPluginsVersions("JDLowSpeed", 1.5));
         optionalPluginsVersionsArray.add(new optionalPluginsVersions("HTTPLiveHeaderScripter", 1.5));
         optionalPluginsVersionsArray.add(new optionalPluginsVersions("jdchat.JDChat", 1.5));
+        optionalPluginsVersionsArray.add(new optionalPluginsVersions("Newsfeeds", 1.5));
         
         
         JDClassLoader jdClassLoader = JDUtilities.getJDClassLoader();
