fac704caa4636136c5d929645a997fd6dd19d56b
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-03 15:01:44 +0000

    Przeslijnet Hoster added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1749 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/Przeslijnet.java
@@ -0,0 +1,168 @@
+package jd.plugins.host;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.parser.SimpleMatches;
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTP;
+import jd.plugins.HTTPConnection;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+import jd.plugins.download.RAFDownload;
+import jd.utils.JDUtilities;
+
+public class Przeslijnet extends PluginForHost {
+
+    private static final String HOST = "przeslij.net";
+    private static final String VERSION = "1.0.0";
+    static private final Pattern patternSupported = Pattern.compile("http://www2\\.przeslij\\.net/download.php\\?file=(.*)", Pattern.CASE_INSENSITIVE);
+    private String url;
+    private RequestInfo requestInfo;
+
+    //
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getPluginName() {
+        return HOST;
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    @Override
+    public String getVersion() {
+        return VERSION;
+    }
+
+    @Override
+    public String getPluginID() {
+        return HOST + "-" + VERSION;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    public Przeslijnet() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_PAGE, null));
+        steps.add(new PluginStep(PluginStep.STEP_PENDING, null));
+        steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
+    }
+
+    public PluginStep doStep(PluginStep step, DownloadLink downloadLink) {
+        try {
+            switch (step.getStep()) {
+            case PluginStep.STEP_PAGE:
+                url = downloadLink.getDownloadURL();
+                /* Nochmals das File überprüfen */
+                if (!getFileInformation(downloadLink)) {
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+                requestInfo = HTTP.getRequest(new URL(url));
+                return step;
+            case PluginStep.STEP_PENDING:
+                /* Zwangswarten, 15seks */
+                step.setParameter(15000l);
+                return step;
+            case PluginStep.STEP_DOWNLOAD:
+                /* Link holen */
+                String linkurl = JDUtilities.htmlDecode(new Regex(requestInfo.getHtmlCode(), "onClick=\"window\\.location=\\\\\'(.*?)\\\\\'").getFirstMatch());
+                /* Datei herunterladen */
+                requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(linkurl), requestInfo.getCookie(), url.toString(), false);
+                HTTPConnection urlConnection = requestInfo.getConnection();
+                String filename = getFileNameFormHeader(urlConnection);
+                if (urlConnection.getContentLength() == 0) {
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+                    step.setStatus(PluginStep.STATUS_RETRY);
+                    return step;
+                }
+                downloadLink.setDownloadMax(urlConnection.getContentLength());
+                downloadLink.setName(filename);
+                long length = downloadLink.getDownloadMax();
+                dl = new RAFDownload(this, downloadLink, urlConnection);
+                dl.setFilesize(length);
+                if (!dl.startDownload() && step.getStatus() != PluginStep.STATUS_ERROR && step.getStatus() != PluginStep.STATUS_TODO) {
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    return step;
+                }
+                return step;
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        step.setStatus(PluginStep.STATUS_ERROR);
+        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
+        return step;
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) {
+        try {
+            String url = downloadLink.getDownloadURL();
+
+            requestInfo = HTTP.getRequest(new URL(url));
+            if (!requestInfo.containsHTML("Invalid download link")) {
+                downloadLink.setName(JDUtilities.htmlDecode(SimpleMatches.getBetween(requestInfo.getHtmlCode(), "<font color=#000000>", "</font>")));
+                String filesize = null;
+                if ((filesize = new Regex(requestInfo.getHtmlCode(), "File Size:</td><td bgcolor=\\#EEF4FB background=\"img\\/button03.gif\"><font color=#000080>(.*)MB</td>").getFirstMatch()) != null) {
+                    downloadLink.setDownloadMax((int) Math.round(Double.parseDouble(filesize)) * 1024 * 1024);
+                } else if ((filesize = new Regex(requestInfo.getHtmlCode(), "File Size:</td><td bgcolor=\\#EEF4FB background=\"img\\/button03.gif\"><font color=#000080>(.*)KB</td>").getFirstMatch()) != null) {
+                    downloadLink.setDownloadMax((int) Math.round(Double.parseDouble(filesize)) * 1024);
+                }
+                return true;
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        downloadLink.setAvailable(false);
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanDownloadNum() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "hhttp://www2.przeslij.net/#";
+    }
+}
