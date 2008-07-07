5e4f056ca1464329471a0ef805b33b1ac67664b3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-07 22:08:48 +0000

    ShareOnline update (non working)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1808 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/ShareOnlineBiz.java
+++ src/jd/plugins/host/ShareOnlineBiz.java
@@ -18,6 +18,7 @@ package jd.plugins.host;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Pattern;
 
@@ -27,7 +28,7 @@ import jd.plugins.HTTP;
 import jd.plugins.HTTPConnection;
 import jd.plugins.PluginForHost;
 import jd.plugins.PluginStep;
-import jd.plugins.download.RAFDownload;
+import jd.plugins.RequestInfo;
 import jd.utils.JDUtilities;
 
 public class ShareOnlineBiz extends PluginForHost {
@@ -41,56 +42,23 @@ public class ShareOnlineBiz extends PluginForHost {
 
     private static final String PLUGIN_ID = PLUGIN_NAME + "-" + PLUGIN_VERSION;
 
-    // http://share-online.biz/?d=28TUT6W93
-    // http://share-online.biz/dl/1/48DQA0U39
-    // http://dl-o-1.share-online.biz/1051994
-    // http://www.share-online.biz/download.php?id=D90258485
-    //http://.*?share-online\\.biz/(download.php\\?id\\=[a-zA-Z0-9]{9}|\\?d\\=[a-zA-Z0-9]{9}).*
-    static private final Pattern PAT_SUPPORTED = Pattern.compile("http://.*?share-online\\.biz/(download.php\\?id\\=[a-zA-Z0-9]{9}|\\?d\\=[a-zA-Z0-9]{9}).*", Pattern.CASE_INSENSITIVE);
+    static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?share\\-online\\.biz/download.php\\?id\\=[a-zA-Z0-9]{9}", Pattern.CASE_INSENSITIVE);
+    private RequestInfo requestInfo;
+    private String url;
+    private File captchaFile;
 
-    private static final String PATTERN_FILENAME = "<span class=\"locatedActive\">Download °</span>";
-
-    private static final String PATTERN_FILESIZE = "</font> (° °) angefordert";
-
-    private static final String PATTERN_DLINK = "loadfilelink.decode(\"°\"); document.";
-
-//    private static String ERROR_DOWNLOAD_NOT_FOUND = "Invalid download link";
-
-//    private static Pattern firstIFrame = Pattern.compile("<iframe src=\"(http://.*?share-online\\.biz/dl_page.php\\?file=.*?)\" style=\"border", Pattern.CASE_INSENSITIVE);
-
-//    private static Pattern dlButtonFrame = Pattern.compile("<iframe name=.download..*?src=.(dl_button.php\\?file=.*?). marginwidth\\=", Pattern.CASE_INSENSITIVE);
-//
-//    private static Pattern countDown = Pattern.compile("<script language=\"Javascript\">[\n\r]+.*?\\=([0-9]+)", Pattern.CASE_INSENSITIVE);
-//
-//    private static Pattern filename = Pattern.compile("<center>.*?(File Name.*?|Dateiname.*?)</center>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-//
-//    private static Pattern dloc = Pattern.compile("src=\'(dl.php?.*?)\'", Pattern.CASE_INSENSITIVE);
-
-    // src='dl.php?a=48DQA0U39&b=344f4df81f101a8393e73fe5c61a6fe2'
-    private long ctime = 0;
-
-    private String dlink = "";
-
-    // <center><b>File
-    // Name:</b><br>kompas.www.godlike-project.com.part6.rar<br><i>(M&ouml;glicherweise
-    // gek&uuml;rzt)</i><br><b>File Size:</b> 100 MB<br><b>
     public ShareOnlineBiz() {
         super();
-
         steps.add(new PluginStep(PluginStep.STEP_PAGE, null));
+        steps.add(new PluginStep(PluginStep.STEP_GET_CAPTCHA_FILE, null));
         steps.add(new PluginStep(PluginStep.STEP_PENDING, null));
         steps.add(new PluginStep(PluginStep.STEP_DOWNLOAD, null));
     }
 
-    /*
-     * Funktionen
-     */
-    // muss aufgrund eines Bugs in DistributeData true zurÃ¼ckgeben, auch wenn
-    // die Zwischenablage nicht vom Plugin verarbeitet wird
     @Override
     public boolean doBotCheck(File file) {
         return false;
-    } // kein BotCheck
+    }
 
     @Override
     public String getCoder() {
@@ -133,95 +101,100 @@ public class ShareOnlineBiz extends PluginForHost {
 
     @Override
     public boolean getFileInformation(DownloadLink downloadLink) {
-        try {
-            requestInfo = HTTP.getRequest(new URL(downloadLink.getDownloadURL()));
-            String filename = SimpleMatches.getSimpleMatch(requestInfo, PATTERN_FILENAME, 0);
-            String[] sizev = SimpleMatches.getSimpleMatches(requestInfo, PATTERN_FILESIZE);
-            double size = Double.parseDouble(sizev[0].trim());
-            String type = sizev[1].trim().toLowerCase();
-            int filesize = 0;
-            if (type.equals("mb")) {
-                filesize = (int) (1024 * 1024 * size);
-            } else if (type.equals("kb")) {
-                filesize = (int) (1024 * size);
-            } else {
-                filesize = (int) (size);
+        url = downloadLink.getDownloadURL();
+        for (int i = 1; i < 3; i++) {
+            try {
+                Thread.sleep(1000);/* Sicherheitspause, sonst gibts 403 Response */
+                requestInfo = HTTP.getRequest(new URL(url));
+                if (requestInfo != null && requestInfo.getLocation() == null) {
+                    String filename = SimpleMatches.getSimpleMatch(requestInfo.getHtmlCode(), "<span class=\"locatedActive\">Download °</span>", 0);
+                    String[] sizev = SimpleMatches.getSimpleMatches(requestInfo.getHtmlCode(), "</font> (° °) angefordert");
+                    double size = Double.parseDouble(sizev[0].trim());
+                    String type = sizev[1].trim().toLowerCase();
+                    int filesize = 0;
+                    if (type.equals("mb")) {
+                        filesize = (int) (1024 * 1024 * size);
+                    } else if (type.equals("kb")) {
+                        filesize = (int) (1024 * size);
+                    } else {
+                        filesize = (int) (size);
+                    }
+                    downloadLink.setDownloadMax(filesize);
+                    downloadLink.setName(filename);
+                    return true;
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-            downloadLink.setDownloadMax(filesize);
-            downloadLink.setName(filename);
-
-            return true;
         }
-
-        catch (Exception e) {
-            // TODO Auto-generated catch block
-            // e.printStackTrace();
-        }
+        downloadLink.setAvailable(false);
         return false;
     }
 
     public PluginStep doStep(PluginStep step, DownloadLink downloadLink) {
         try {
-
             switch (step.getStep()) {
             case PluginStep.STEP_PAGE:
-                requestInfo = HTTP.getRequest(new URL(downloadLink.getDownloadURL()));
-//                http://www.share-online.biz/download.php?id=D90258d485
-                logger.info(requestInfo + "");
-                if(requestInfo.getLocation()!=null){
-                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_PLUGIN_SPECIFIC);
-                    step.setParameter(requestInfo.getLocation().substring(requestInfo.getLocation().lastIndexOf("=")+1));
+                url = downloadLink.getDownloadURL();
+                /* Nochmals das File überprüfen */
+                if (!getFileInformation(downloadLink)) {
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
                     step.setStatus(PluginStep.STATUS_ERROR);
                     return step;
                 }
-                dlink = JDUtilities.Base64Decode(SimpleMatches.getSimpleMatch(requestInfo, PATTERN_DLINK, 0));
-             
                 return step;
-
+            case PluginStep.STEP_GET_CAPTCHA_FILE:
+                captchaFile = this.getLocalCaptchaFile(this);
+                HTTPConnection captcha_con = new HTTPConnection(new URL("http://www.share-online.biz/captcha.php").openConnection());
+                captcha_con.setRequestProperty("Referer", url);
+                captcha_con.setRequestProperty("Cookie", requestInfo.getCookie());
+                if (!JDUtilities.download(captchaFile, captcha_con) || !captchaFile.exists()) {
+                    /* Fehler beim Captcha */
+                    logger.severe("Captcha Download fehlgeschlagen!");
+                    step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_IMAGEERROR);
+                    return step;
+                }
+                step.setParameter(captchaFile);
+                return step;
             case PluginStep.STEP_PENDING:
-                step.setParameter(ctime * 1000);
-                return step;
-
-            case PluginStep.STEP_DOWNLOAD:
-
-                requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(dlink), requestInfo.getCookie(), null, false);
-                if(requestInfo.getLocation()!=null){
-                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_PLUGIN_SPECIFIC);
-                    step.setParameter(requestInfo.getLocation().substring(requestInfo.getLocation().lastIndexOf("=")+1));
+                String captchaCode = (String) steps.get(1).getParameter();
+                requestInfo = HTTP.postRequest((new URL(url)), requestInfo.getCookie(), url, null, "captchacode=" + captchaCode, true);
+                if (requestInfo.getHtmlCode().contains("<span>Die Nummer ist leider nicht richtig oder ausgelaufen!</span>")) {
                     step.setStatus(PluginStep.STATUS_ERROR);
+                    downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
                     return step;
                 }
-                HTTPConnection urlConnection = requestInfo.getConnection();
-                int length = urlConnection.getContentLength();
-                downloadLink.setDownloadMax(length);
-                String filename = getFileNameFormHeader(urlConnection);
-
-                downloadLink.setName(filename);
-
-                dl = new RAFDownload(this, downloadLink, urlConnection);
-
-                dl.startDownload();
-
+                // hier müsste jetzt der download link zusammengebaut werden
+                step.setParameter(15 * 1000L);
                 return step;
-
+            case PluginStep.STEP_DOWNLOAD:
+                // hab ich nur zu testzwecken hier so
+                step.setStatus(PluginStep.STATUS_ERROR);
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
+                return step;
             }
-            return step;
-        } catch (IOException e) {
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
-            return null;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+
+        return step;
+
     }
 
     @Override
     public void resetPluginGlobals() {
         // TODO Auto-generated method stub
-
     }
 
     @Override
     public String getAGBLink() {
-
-        return "http://share-online.biz/?page=tos";
+        return "http://share-online.biz/rules.php";
     }
 
 }
