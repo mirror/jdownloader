a4216f957f7a684e81eceaeaf269bbe5aa0fc2ac
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-09 16:43:51 +0000

    Moosharede Hoster added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1840 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/Moosharede.java
@@ -0,0 +1,156 @@
+package jd.plugins.host;
+
+import java.io.File;
+import java.net.URL;
+import java.util.regex.Pattern;
+import jd.config.Configuration;
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTP;
+import jd.plugins.HTTPConnection;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+import jd.plugins.download.RAFDownload;
+import jd.utils.JDUtilities;
+
+public class Moosharede extends PluginForHost {
+    private static final String CODER = "JD-Team";
+
+    private static final String HOST = "mooshare.de";
+
+    private static final String PLUGIN_NAME = HOST;
+
+    private static final String PLUGIN_VERSION = "1.0.0.0";
+
+    private static final String PLUGIN_ID = PLUGIN_NAME + "-" + PLUGIN_VERSION;
+
+    static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?mooshare\\.de/index\\.php\\?pid\\=[a-zA-Z0-9]+", Pattern.CASE_INSENSITIVE);
+    private RequestInfo requestInfo;
+    private String downloadurl;
+
+    public Moosharede() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_COMPLETE, null));
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return CODER;
+    }
+
+    @Override
+    public String getPluginName() {
+        return PLUGIN_NAME;
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    @Override
+    public String getVersion() {
+        return PLUGIN_VERSION;
+    }
+
+    @Override
+    public String getPluginID() {
+        return PLUGIN_ID;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return PAT_SUPPORTED;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanDownloadNum() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) {
+        downloadurl = downloadLink.getDownloadURL();
+        try {
+            requestInfo = HTTP.getRequest(new URL(downloadurl));
+            if (requestInfo != null && requestInfo.getLocation() == null) {
+                String filename = requestInfo.getRegexp("<center>Dateiname: <b>(.*?)</b>").getFirstMatch();
+                if (filename != "") {
+                    downloadLink.setName(filename);
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        downloadLink.setAvailable(false);
+        return false;
+    }
+
+    public PluginStep doStep(PluginStep step, DownloadLink downloadLink) {
+        if (step == null) return null;
+        try {
+            /* Nochmals das File überprüfen */
+            if (!getFileInformation(downloadLink)) {
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_FILE_NOT_FOUND);
+                step.setStatus(PluginStep.STATUS_ERROR);
+                return step;
+            }
+
+            /* DownloadLink holen */
+            downloadurl = requestInfo.getRegexp("popup\\('(.*?)',").getFirstMatch();
+            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(downloadurl), null, downloadLink.getDownloadURL(), false);
+            downloadurl = "http://" + requestInfo.getConnection().getURL().getHost() + requestInfo.getLocation();
+
+            /* Datei herunterladen */
+            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(downloadurl), null, downloadLink.getDownloadURL(), false);
+            HTTPConnection urlConnection = requestInfo.getConnection();
+            String filename = getFileNameFormHeader(urlConnection);
+            if (urlConnection.getContentLength() == 0) {
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+                step.setStatus(PluginStep.STATUS_ERROR);
+                return step;
+            }
+            downloadLink.setDownloadMax(urlConnection.getContentLength());
+            downloadLink.setName(filename);
+            long length = downloadLink.getDownloadMax();
+            dl = new RAFDownload(this, downloadLink, urlConnection);
+            dl.setFilesize(length);
+            dl.setChunkNum(JDUtilities.getSubConfig("DOWNLOAD").getIntegerProperty(Configuration.PARAM_DOWNLOAD_MAX_CHUNKS, 2));
+            dl.setResume(true);
+            if (!dl.startDownload() && step.getStatus() != PluginStep.STATUS_ERROR && step.getStatus() != PluginStep.STATUS_TODO) {
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+                step.setStatus(PluginStep.STATUS_ERROR);
+                return step;
+            }
+            return step;
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        step.setStatus(PluginStep.STATUS_ERROR);
+        downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
+        return step;
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.mooshare.de/infos.php";
+    }
+
+}
