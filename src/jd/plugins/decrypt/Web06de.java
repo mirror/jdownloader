7c977d077d7896e90c8cbed06ccac2fdda2de8e8
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-11 00:40:23 +0000

    Web06 Decrypter added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1870 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/Web06de.java
@@ -0,0 +1,71 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+
+public class Web06de extends PluginForDecrypt {
+
+    static private final String host = "web06.de";
+
+    private String version = "1.0.0.0";
+
+    static private final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?web06\\.de/\\?user=\\d+site=(.*)", Pattern.CASE_INSENSITIVE);
+
+    public Web06de() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+        currentStep = steps.firstElement();
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return host + "-" + version;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+
+    @Override
+    public PluginStep doStep(PluginStep step, String parameter) {
+        String cryptedLink = (String) parameter;
+        if (step.getStep() == PluginStep.STEP_DECRYPT) {
+            Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+            String link = new Regex(cryptedLink, "user=\\d+site=(.*)").getFirstMatch();
+            if (link != null) decryptedLinks.add(this.createDownloadlink(link));
+            step.setParameter(decryptedLinks);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+}
