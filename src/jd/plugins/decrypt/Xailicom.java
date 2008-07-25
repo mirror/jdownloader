ef46162120b811631efbd4b340b1d77d47ec603c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-25 19:19:31 +0000

    Xailicom Decrypter added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@1993 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/Xailicom.java
@@ -0,0 +1,91 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Vector;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTP;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginStep;
+import jd.plugins.RequestInfo;
+import jd.utils.JDUtilities;
+
+public class Xailicom extends PluginForDecrypt {
+
+    static private String host = "xaili.com";
+
+    private String version = "1.0.0.0";
+    final static private Pattern patternSupported = Pattern.compile("http://.*?xaili\\.com/\\?site=protect\\&id=[0-9]+", Pattern.CASE_INSENSITIVE);
+
+    public Xailicom() {
+        super();
+        steps.add(new PluginStep(PluginStep.STEP_DECRYPT, null));
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginID() {
+        return host + "-" + version;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        return version;
+    }
+
+    public PluginStep doStep(PluginStep step, String parameter) {
+        String cryptedLink = (String) parameter;
+        if (step.getStep() == PluginStep.STEP_DECRYPT) {
+            Vector<DownloadLink> decryptedLinks = new Vector<DownloadLink>();
+            try {
+                URL url = new URL(cryptedLink);
+                RequestInfo reqinfo = HTTP.getRequest(url);
+
+                String links[] = new Regex(reqinfo.getHtmlCode(), Pattern.compile("onClick='popuptt\\(\"(.*?)\"\\)", Pattern.CASE_INSENSITIVE)).getMatches(1);
+                progress.setRange(links.length);
+                for (int i = 0; i < links.length; i++) {
+                    reqinfo = HTTP.getRequest(new URL("http://www.xaili.com/include/get.php?link=" + links[i]));
+                    String link = new Regex(reqinfo.getHtmlCode(), Pattern.compile("src=\"(.*?)\"", Pattern.CASE_INSENSITIVE)).getFirstMatch();
+                    if (link != null) {
+                        link = JDUtilities.htmlDecode(link);
+                        decryptedLinks.add(this.createDownloadlink(link));
+                    }
+                    progress.increase(1);
+                }
+
+                step.setParameter(decryptedLinks);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+}
