432015c26d4e3b4f9383b3c6e1fde7e8a839ed5a
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-26 22:51:29 +0000

    SharedZillacom Hoster updated (Password support added)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2009 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/SharedZillacom.java
+++ src/jd/plugins/host/SharedZillacom.java
@@ -11,6 +11,7 @@ import jd.plugins.PluginForHost;
 import jd.plugins.PluginStep;
 import jd.plugins.RequestInfo;
 import jd.plugins.download.RAFDownload;
+import jd.utils.JDUtilities;
 
 public class SharedZillacom extends PluginForHost {
     private static final String CODER = "JD-Team";
@@ -22,10 +23,11 @@ public class SharedZillacom extends PluginForHost {
     private static final String PLUGIN_VERSION = "1.0.0.0";
 
     private static final String PLUGIN_ID = PLUGIN_NAME + "-" + PLUGIN_VERSION;
-    
+
     static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?sharedzilla\\.com/(en|ru)/get\\?id=\\d+", Pattern.CASE_INSENSITIVE);
     private RequestInfo requestInfo;
-    
+    private String passCode = "";
+
     public SharedZillacom() {
         super();
         steps.add(new PluginStep(PluginStep.STEP_COMPLETE, null));
@@ -76,14 +78,14 @@ public class SharedZillacom extends PluginForHost {
     }
 
     @Override
-    public boolean getFileInformation(DownloadLink downloadLink) {        
+    public boolean getFileInformation(DownloadLink downloadLink) {
         try {
             requestInfo = HTTP.getRequest(new URL(downloadLink.getDownloadURL()));
-            if (!requestInfo.containsHTML("Upload not found")) {                
+            if (!requestInfo.containsHTML("Upload not found")) {
                 String filename = new Regex(requestInfo.getHtmlCode(), Pattern.compile("nowrap title=\"(.*?)\">", Pattern.CASE_INSENSITIVE)).getFirstMatch();
                 String filesize = new Regex(requestInfo.getHtmlCode(), Pattern.compile("<span title=\"(.*?) Bytes\">", Pattern.CASE_INSENSITIVE)).getFirstMatch();
-                if (filesize!=null) {
-                    downloadLink.setDownloadMax(new Integer(filesize));                
+                if (filesize != null) {
+                    downloadLink.setDownloadMax(new Integer(filesize));
                 }
                 downloadLink.setName(filename);
                 return true;
@@ -105,16 +107,32 @@ public class SharedZillacom extends PluginForHost {
                 step.setStatus(PluginStep.STATUS_ERROR);
                 return step;
             }
-            /*ID holen*/
-            String id=new Regex(downloadLink.getDownloadURL(),Pattern.compile("get\\?id=(\\d+)",Pattern.CASE_INSENSITIVE)).getFirstMatch();        
-            /*Free Download starten*/
-            requestInfo = HTTP.postRequestWithoutHtmlCode(new URL("http://sharedzilla.com/en/downloaddo"), requestInfo.getCookie(), downloadLink.getFileInfomationString(), "id="+id, false);
-            if (requestInfo.getLocation()==null){
-                downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
+            /* ID holen */
+            String id = new Regex(downloadLink.getDownloadURL(), Pattern.compile("get\\?id=(\\d+)", Pattern.CASE_INSENSITIVE)).getFirstMatch();
+            /* Password checken */
+            if (requestInfo.containsHTML("Password protected")) {
+                if (downloadLink.getStringProperty("pass", null) == null) {
+                    if ((passCode = JDUtilities.getGUI().showUserInputDialog("Code?")) == null) passCode = "";
+                }else{
+                    /*gespeicherten PassCode holen*/
+                    passCode=downloadLink.getStringProperty("pass", null);
+                }
+            }
+            /* Free Download starten */
+            requestInfo = HTTP.postRequestWithoutHtmlCode(new URL("http://sharedzilla.com/en/downloaddo"), requestInfo.getCookie(), downloadLink.getDownloadURL(), "id=" + id + "&upload_password=" + JDUtilities.urlEncode(passCode), false);
+            if (requestInfo.getLocation() == null) {
+                requestInfo = HTTP.postRequest(new URL("http://sharedzilla.com/en/downloaddo"), requestInfo.getCookie(), downloadLink.getDownloadURL(), null, "id=" + id + "&upload_password=" + JDUtilities.urlEncode(passCode), false);
+                if (requestInfo.containsHTML("<p>Password is wrong!</p>")) {
+                    /*PassCode war falsch, also Löschen*/
+                    downloadLink.setProperty("pass", null);
+                }
                 step.setStatus(PluginStep.STATUS_ERROR);
+                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
                 return step;
             }
-            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(requestInfo.getLocation()), requestInfo.getCookie(), requestInfo.getConnection().getURL().toString(), false);
+            /*PassCode war richtig, also Speichern*/
+            downloadLink.setProperty("pass", passCode);
+            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(requestInfo.getLocation()), requestInfo.getCookie(), downloadLink.getDownloadURL(), false);
             /* Datei herunterladen */
             HTTPConnection urlConnection = requestInfo.getConnection();
             String filename = getFileNameFormHeader(urlConnection);
@@ -128,7 +146,10 @@ public class SharedZillacom extends PluginForHost {
             long length = downloadLink.getDownloadMax();
             dl = new RAFDownload(this, downloadLink, urlConnection);
             dl.setFilesize(length);
-            dl.setChunkNum(1);/*bei dem speed lohnen mehrere chunks nicht, da es nicht schneller wird*/
+            dl.setChunkNum(1);/*
+                               * bei dem speed lohnen mehrere chunks nicht, da
+                               * es nicht schneller wird
+                               */
             dl.setResume(true);
             if (!dl.startDownload() && step.getStatus() != PluginStep.STATUS_ERROR && step.getStatus() != PluginStep.STATUS_TODO) {
                 downloadLink.setStatus(DownloadLink.STATUS_ERROR_TEMPORARILY_UNAVAILABLE);
