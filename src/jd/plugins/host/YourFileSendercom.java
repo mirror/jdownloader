8b94f66601a71e0b8ac28421a700026f27c6052b
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-07-28 16:08:29 +0000

    YourFileSendercom Hoster added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2026 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/YourFileSendercom.java
+++ src/jd/plugins/host/YourFileSendercom.java
@@ -4,22 +4,20 @@ import java.io.File;
 import java.net.URL;
 import java.util.regex.Pattern;
 
-import jd.parser.Form;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.HTTP;
 import jd.plugins.HTTPConnection;
-import jd.plugins.Plugin;
 import jd.plugins.PluginForHost;
 import jd.plugins.PluginStep;
 import jd.plugins.RequestInfo;
 import jd.plugins.download.RAFDownload;
 import jd.utils.JDUtilities;
 
-public class Upsharenet extends PluginForHost {
+public class YourFileSendercom extends PluginForHost {
     private static final String CODER = "JD-Team";
 
-    private static final String HOST = "upshare.net";
+    private static final String HOST = "yourfilesender.com";
 
     private static final String PLUGIN_NAME = HOST;
 
@@ -27,14 +25,12 @@ public class Upsharenet extends PluginForHost {
 
     private static final String PLUGIN_ID = PLUGIN_NAME + "-" + PLUGIN_VERSION;
 
-    static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?upshare\\.(net|eu)/download\\.php\\?id=[a-zA-Z0-9]+", Pattern.CASE_INSENSITIVE);
+    static private final Pattern PAT_SUPPORTED = Pattern.compile("http://[\\w\\.]*?yourfilesender\\.com/v/\\d+/(.*?\\.html)", Pattern.CASE_INSENSITIVE);
     private RequestInfo requestInfo;
     private String downloadurl;
-    private File captchaFile;
-    private String captchaCode;
-    private String passCode=null;
+;
 
-    public Upsharenet() {
+    public YourFileSendercom() {
         super();
         steps.add(new PluginStep(PluginStep.STEP_COMPLETE, null));
     }
@@ -85,21 +81,16 @@ public class Upsharenet extends PluginForHost {
 
     @Override
     public boolean getFileInformation(DownloadLink downloadLink) {
-        /*.eu zu .net weiterleitung*/        
-        downloadLink.setUrlDownload(downloadLink.getDownloadURL().replaceAll("upshare\\.(net|eu)", "upshare\\.net"));
-        
-        downloadurl = downloadLink.getDownloadURL();        
+        downloadurl = downloadLink.getDownloadURL();
         try {
             requestInfo = HTTP.getRequest(new URL(downloadurl));
-            if (!requestInfo.containsHTML("Your requested file is not found")) {
-                String linkinfo[][] = new Regex(requestInfo.getHtmlCode(), Pattern.compile("<b>File size:</b></td>[\\r\\n\\s]*<td align=left>([0-9\\.]*) ([GKMB]*)</td>", Pattern.CASE_INSENSITIVE)).getMatches();
+            if (!requestInfo.containsHTML("alert('File Not Found")) {
+                String linkinfo[][] = new Regex(requestInfo.getHtmlCode(), Pattern.compile("<P>You have requested the file <strong>(.*?)</strong> \\(([0-9\\.,]+) (.*?)\\)\\.<br", Pattern.CASE_INSENSITIVE)).getMatches();
 
-                if (linkinfo[0][1].matches("MB")) {
-                    downloadLink.setDownloadMax((int) Math.round(Double.parseDouble(linkinfo[0][0]) * 1024 * 1024));
-                } else if (linkinfo[0][1].matches("KB")) {
-                    downloadLink.setDownloadMax((int) Math.round(Double.parseDouble(linkinfo[0][0]) * 1024));
+                if (linkinfo[0][2].matches("KBytes")) {
+                    downloadLink.setDownloadMax((int) Math.round(Double.parseDouble(linkinfo[0][1].replaceAll(",", "").replaceAll("\\.0+", ""))*1024.0 ));
                 }
-                downloadLink.setName(new URL(downloadurl).getQuery().substring(3));
+                downloadLink.setName(linkinfo[0][0]);
                 return true;
             }
         } catch (Exception e) {
@@ -119,67 +110,23 @@ public class Upsharenet extends PluginForHost {
                 step.setStatus(PluginStep.STATUS_ERROR);
                 return step;
             }
-            Form form = requestInfo.getForms()[1];
-            /* Captcha File holen */
-            captchaFile = getLocalCaptchaFile(this);
-            HTTPConnection captcha_con = new HTTPConnection(new URL("http://www.upshare.net/captcha.php").openConnection());
-            captcha_con.setRequestProperty("Referer", downloadLink.getDownloadURL());
-            captcha_con.setRequestProperty("Cookie", requestInfo.getCookie());
-            if (!JDUtilities.download(captchaFile, captcha_con) || !captchaFile.exists()) {
-                /* Fehler beim Captcha */
-                logger.severe("Captcha Download fehlgeschlagen!");
-                step.setStatus(PluginStep.STATUS_ERROR);
-                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_IMAGEERROR);
-                return step;
-            }
-            /* CaptchaCode holen */
-            if ((captchaCode = Plugin.getCaptchaCode(captchaFile, this)) == null) {
-                step.setStatus(PluginStep.STATUS_ERROR);
-                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
-                return step;
-            }
-            form.vars.put("captchacode", captchaCode);
-            /* Passwort holen holen */
-            if (form.vars.containsKey("downloadpw")) {
-                if (downloadLink.getStringProperty("pass", null) == null) {
-                    if ((passCode = JDUtilities.getGUI().showUserInputDialog("Code?")) == null) passCode = "";
-                } else {
-                    /* gespeicherten PassCode holen */
-                    passCode = downloadLink.getStringProperty("pass", null);
-                }
-                form.vars.put("downloadpw", passCode);
-            }
-            /* Pass/Captcha check */
-            requestInfo = form.getRequestInfo(false);
-            if (requestInfo.containsHTML("<span>Password Error</span>")){
-                /* PassCode war falsch, also Löschen */
-                downloadLink.setProperty("pass", null);
-                step.setStatus(PluginStep.STATUS_ERROR);
-                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
-                return step;
-            }            
-            if (requestInfo.containsHTML("<span>Captcha number error or expired</span>")){                
-                step.setStatus(PluginStep.STATUS_ERROR);
-                downloadLink.setStatus(DownloadLink.STATUS_ERROR_CAPTCHA_WRONG);
-                return step;
-            }            
-            if (requestInfo.containsHTML("<span>You have got max allowed download sessions from the same IP!</span>")){                
+            if (requestInfo.containsHTML("<span>You have got max allowed download sessions from the same IP!</span>")) {
                 step.setStatus(PluginStep.STATUS_ERROR);
                 step.setParameter(60 * 60 * 1000L);
                 downloadLink.setStatus(DownloadLink.STATUS_ERROR_DOWNLOAD_LIMIT);
                 return step;
             }
-            /* PassCode war richtig, also Speichern */
-            downloadLink.setProperty("pass", passCode);
-            /*DownloadLink holen*/
-            String link = new Regex(requestInfo.getHtmlCode(),Pattern.compile("document.location=\"(.*?)\"",Pattern.CASE_INSENSITIVE)).getFirstMatch();
-            if (link== null){
+            
+            String link = JDUtilities.htmlDecode(new Regex(requestInfo.getHtmlCode(), Pattern.compile("unescape\\('(.*?)'\\)", Pattern.CASE_INSENSITIVE)).getFirstMatch());
+            if (link == null) {
                 step.setStatus(PluginStep.STATUS_ERROR);
                 downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
                 return step;
             }
+            /*10 Seks warten*/
+            this.sleep(10000, downloadLink);
             requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(link), requestInfo.getCookie(), downloadLink.getDownloadURL(), false);
-            if (requestInfo.getLocation()!=null){
+            if (requestInfo.getLocation() != null) {
                 step.setStatus(PluginStep.STATUS_ERROR);
                 downloadLink.setStatus(DownloadLink.STATUS_ERROR_UNKNOWN);
                 return step;
@@ -221,7 +168,7 @@ public class Upsharenet extends PluginForHost {
 
     @Override
     public String getAGBLink() {
-        return "http://www.upshare.net/faq.php?setlang=en";
+        return "http://www.yourfilesender.com/terms.php";
     }
 
 }
