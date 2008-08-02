c58b07c8fcd386c5553fe843e882e9da04144d39
1337331 <1337331@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-02 22:22:50 +0000

    FalinksCom Decrypter hinzugefügt.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2109 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/FalinksCom.java
@@ -0,0 +1,95 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTP;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.RequestInfo;
+
+public class FalinksCom extends PluginForDecrypt {
+
+    static private String host = "falinks.com";
+
+    private Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?falinks\\.com/\\?fa=link&id=[0-9]+", Pattern.CASE_INSENSITIVE);
+
+    
+
+    public FalinksCom() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(String parameter) {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        try {
+            URL url = new URL(parameter);
+            RequestInfo reqinfo = HTTP.getRequest(url);
+            String[] links = new Regex(reqinfo.getHtmlCode(), "\\<input type=\"hidden\" name=\"url\" value=\"(.*?)\" \\/\\>").getMatches(1);
+            progress.setRange(links.length);
+            for (int i = 0; i < links.length; i++) {
+                //logger.info(links[i]);
+                    decryptedLinks.add(createDownloadlink(links[i]));
+                
+                progress.increase(1);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2072 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+        return ret == null ? "0.0" : ret;
+    }
+}
