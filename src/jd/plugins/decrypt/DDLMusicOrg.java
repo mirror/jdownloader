d0d510c4e3fa63fad302113955b4ea804c9330c3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-02 00:13:43 +0000

    DDLMusic Decrypter updated
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2098 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/DDLMusicOrg.java
+++ src/jd/plugins/decrypt/DDLMusicOrg.java
@@ -21,9 +21,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
-
 import jd.parser.Regex;
-import jd.parser.SimpleMatches;
 import jd.plugins.DownloadLink;
 import jd.plugins.HTTP;
 import jd.plugins.PluginForDecrypt;
@@ -31,9 +29,10 @@ import jd.plugins.RequestInfo;
 
 public class DDLMusicOrg extends PluginForDecrypt {
     final static String host = "ddl-music.org";
-    private Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?ddl-music\\.org/(music_crypth\\.php\\?.+|index\\.php\\?site=view_download.+)", Pattern.CASE_INSENSITIVE);
 
-    
+    private static final Pattern patternLink_Main = Pattern.compile("http://[\\w\\.]*?ddl-music\\.org/index\\.php\\?site=view_download&cat=.+&id=\\d+", Pattern.CASE_INSENSITIVE);
+    private static final Pattern patternLink_Crypt = Pattern.compile("http://[\\w\\.]*?ddl-music\\.org/ddlm_cr\\.php\\?\\d+\\?\\d+", Pattern.CASE_INSENSITIVE);
+    private static final Pattern patternSupported = Pattern.compile(patternLink_Main.pattern() + "|" + patternLink_Crypt.pattern(), Pattern.CASE_INSENSITIVE);
 
     public DDLMusicOrg() {
         super();
@@ -41,48 +40,47 @@ public class DDLMusicOrg extends PluginForDecrypt {
 
     @Override
     public ArrayList<DownloadLink> decryptIt(String parameter) {
+        String cryptedLink = (String) parameter;
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         try {
 
-            if (parameter.indexOf("music_crypth.php") != -1) {
-                parameter = parameter.replace("music_crypth.php", "frame_crypth.php");
-                RequestInfo reqinfo = HTTP.getRequest(new URL(parameter));
-                decryptedLinks.add(createDownloadlink("http://" + SimpleMatches.getBetween(reqinfo.getHtmlCode(), "src=http://", " target=\"_self\">")));
-            } else if (parameter.indexOf("site=view_download") != -1) {
+            if (new Regex(cryptedLink, patternLink_Crypt).matches()) {
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                }
+                RequestInfo reqinfo = HTTP.getRequest(new URL(cryptedLink.replace("ddlm_cr.php", "test2.php")), null, cryptedLink, false);
+                String link = new Regex(reqinfo.getHtmlCode(), "<form action=\"(.*?)\" method=\"post\">", Pattern.CASE_INSENSITIVE).getFirstMatch();
+                if (link == null) return null;
+                decryptedLinks.add(createDownloadlink(link));
+            } else if (new Regex(cryptedLink, patternLink_Main).matches()) {
                 RequestInfo reqinfo = HTTP.getRequest(new URL(parameter));
                 // passwort auslesen
-                if (reqinfo.getHtmlCode().indexOf("<td class=\"normalbold\"><div align=\"center\">Passwort</div></td>") != -1) {
-                    String password = SimpleMatches.getBetween(reqinfo.getHtmlCode(), "<td class=\"normalbold\"><div align=\"center\">Passwort</div></td>\n" + "                      </tr>\n" + "                      <tr>\n" + "                      <td class=\"normal\"><div align=\"center\">", "</div></td>");
-                    default_password.add(password);
-                }
+                String password = new Regex(reqinfo.getHtmlCode(), "<td class=\"normalbold\"><div align=\"center\">Passwort</div></td>\n.*?</tr>\n.*?<tr>\n.*?<td class=\"normal\"><div align=\"center\">(.*?)</div></td>", Pattern.CASE_INSENSITIVE).getFirstMatch();
+                if (password != null && password.contains("kein Passwort")) password = null;
 
-                ArrayList<ArrayList<String>> ids = SimpleMatches.getAllSimpleMatches(reqinfo.getHtmlCode(), "href=\"/music_crypth.php°\" target=\"_blank\"");
-                progress.setRange(ids.size());
+                String ids[] = new Regex(reqinfo.getHtmlCode(), "href=\"(.*?)\n?\" target=\"\\_blank\" onmouseout=\"MM_swapImgRestore", Pattern.CASE_INSENSITIVE).getMatches(1);
 
-                int j = 0;
-
-                for (int i = 0; i < ids.size(); i++) {
-
-                    reqinfo = HTTP.getRequest(new URL("http://ddl-music.org/frame_crypth.php" + ids.get(i).get(0)));
-                    logger.info("http://ddl-music.org/frame_crypth.php" + ids.get(i).get(0));
-                    decryptedLinks.add(createDownloadlink("http://" + SimpleMatches.getBetween(reqinfo.getHtmlCode(), "src=http://", " target=\"_self\">")));
-                    progress.increase(1);
-
-                    // nach 3 anfragen schnell hintereinander streikt der
-                    // server -> http 403
-                    // => ab 3. aufruf jeweils 0.5 sekunden warten
-                    if (j >= 2) {
+                progress.setRange(ids.length);
+                DownloadLink link;
+                for (int i = 0; i < ids.length; i++) {
+                    if (ids[i].startsWith("/ddlm_cr.php")) {
+                        link=createDownloadlink("http://ddl-music.org" + ids[i]);                        
+                    } else {
+                        link=createDownloadlink(ids[i]);                        
                         try {
-                            Thread.sleep(500);
+                            Thread.sleep(250);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
                         }
                     }
-                    j++;
+                    link.addSourcePluginPassword(password);
+                    decryptedLinks.add(link);
+                    progress.increase(1);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
         return decryptedLinks;
     }
@@ -94,7 +92,7 @@ public class DDLMusicOrg extends PluginForDecrypt {
 
     @Override
     public String getCoder() {
-        return "jD-Team";
+        return "JD-Team";
     }
 
     @Override
