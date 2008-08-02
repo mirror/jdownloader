518f7f851ae897f9bbb9affc0264acbe5bd2ccf1
gluewurm <gluewurm@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-02 22:25:01 +0000

    YouPorn Decrypter, LinkAdresse der Vorschau kopieren
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2110 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/YouPornCom.java
@@ -0,0 +1,82 @@
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.http.Browser;
+import jd.http.GetRequest;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HTTP;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.RequestInfo;
+
+public class YouPornCom extends PluginForDecrypt {
+	private static final String CODER = "JD-Team";
+	private static final String HOST = "youporn.com";
+	private static final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?youporn\\.com/watch/[\\d]{3,9}/.*/", Pattern.CASE_INSENSITIVE);
+	private static final Pattern DOWNLOADFILE =  Pattern.compile("download\\.youporn\\.com/download/[\\d]{3,9}/flv/[^\\?]*", Pattern.CASE_INSENSITIVE);
+	 
+	@Override
+	public ArrayList<DownloadLink> decryptIt(String parameter) {
+		RequestInfo loader;
+		ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+		try {
+			loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
+			String matches = new Regex(loader.getHtmlCode(), DOWNLOADFILE).getFirstMatch();
+			DownloadLink dlink = this.createDownloadlink("httpviajd://" + matches);
+			dlink.setBrowserUrl(parameter);
+			decryptedLinks.add(dlink);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return decryptedLinks;
+	}
+
+	@Override
+	public boolean doBotCheck(File file) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getCoder() {
+		// TODO Auto-generated method stub
+		return CODER;
+	}
+
+	@Override
+	public String getHost() {
+		// TODO Auto-generated method stub
+		return HOST;
+	}
+
+	@Override
+	public String getPluginName() {
+		// TODO Auto-generated method stub
+		return HOST;
+	}
+
+	@Override
+	public Pattern getSupportedLinks() {
+		// TODO Auto-generated method stub
+		return patternSupported;
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		String ret = new Regex("$Revision: 2107 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+        return ret == null ? "0.0" : ret;
+	}
+
+}
