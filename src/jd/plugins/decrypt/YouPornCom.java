bb48b20042ce9768bc2a51101cd4814c2cfbe312
gluewurm <gluewurm@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-03 10:19:04 +0000

    Ganze Kategorien (http://youporn.com/?page=1) werden nun auch entschlüsselt
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2113 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/YouPornCom.java
+++ src/jd/plugins/decrypt/YouPornCom.java
@@ -18,25 +18,44 @@ import jd.plugins.RequestInfo;
 public class YouPornCom extends PluginForDecrypt {
 	private static final String CODER = "JD-Team";
 	private static final String HOST = "youporn.com";
-	private static final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?youporn\\.com/watch/[\\d]{3,9}/.*/", Pattern.CASE_INSENSITIVE);
+	private static final Pattern patternSupported = Pattern.compile("(http://[\\w\\.]*?youporn\\.com/watch/[\\d]{3,9}/.*/)|(http://[\\w\\.]*?youporn\\.com/\\?page=[\\d]{1,3})", Pattern.CASE_INSENSITIVE);
 	private static final Pattern DOWNLOADFILE =  Pattern.compile("download\\.youporn\\.com/download/[\\d]{3,9}/flv/[^\\?]*", Pattern.CASE_INSENSITIVE);
-	 
+	private static final Pattern PAGEENTRY =  Pattern.compile("<h1><a href=\"/watch/[\\d]{3,9}/.*/[^\">.*\\</a></h1>]", Pattern.CASE_INSENSITIVE);
+	
 	@Override
 	public ArrayList<DownloadLink> decryptIt(String parameter) {
 		RequestInfo loader;
 		ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-		try {
-			loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
-			String matches = new Regex(loader.getHtmlCode(), DOWNLOADFILE).getFirstMatch();
-			DownloadLink dlink = this.createDownloadlink("httpviajd://" + matches);
-			dlink.setBrowserUrl(parameter);
-			decryptedLinks.add(dlink);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(new Regex(parameter, Pattern.compile("http://[\\w\\.]*?youporn\\.com/\\?page=[\\d]{1,3}")).count() == 1) {
+			try {
+				loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
+				String[] matches = new Regex(loader.getHtmlCode(), Pattern.compile("<h1><a href=\"([^\"]+)\"", Pattern.CASE_INSENSITIVE)).getMatches(1);
+				for(String link : matches) {
+						DownloadLink dlink = this.createDownloadlink("http://youporn.com" + link);
+						dlink.setBrowserUrl(parameter);
+						decryptedLinks.add(dlink);
+				}
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
+				String matches = new Regex(loader.getHtmlCode(), DOWNLOADFILE).getFirstMatch();
+				DownloadLink dlink = this.createDownloadlink("httpviajd://" + matches);
+				dlink.setBrowserUrl(parameter);
+				decryptedLinks.add(dlink);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
         
         return decryptedLinks;
