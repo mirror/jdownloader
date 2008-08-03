41a171b29bdbe15f43d786759d72381b2164db81
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-03 23:03:54 +0000

    YouPornCom Hoster/Decrypter updated
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2129 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/YouPornCom.java
+++ src/jd/plugins/decrypt/YouPornCom.java
@@ -1,3 +1,19 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://gnu.org/licenses/>.
+
 package jd.plugins.decrypt;
 
 import java.io.File;
@@ -6,9 +22,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
-
-import jd.http.Browser;
-import jd.http.GetRequest;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.HTTP;
@@ -16,86 +29,78 @@ import jd.plugins.PluginForDecrypt;
 import jd.plugins.RequestInfo;
 
 public class YouPornCom extends PluginForDecrypt {
-	private static final String CODER = "JD-Team";
-	private static final String HOST = "youporn.com";
-	private static final Pattern patternSupported = Pattern.compile("(http://[\\w\\.]*?youporn\\.com/watch/[\\d]{3,9}/.*/)|(http://[\\w\\.]*?youporn\\.com/\\?page=[\\d]{1,3})", Pattern.CASE_INSENSITIVE);
-	private static final Pattern DOWNLOADFILE =  Pattern.compile("download\\.youporn\\.com/download/[\\d]{3,9}/flv/[^\\?]*", Pattern.CASE_INSENSITIVE);
-	private static final Pattern PAGEENTRY =  Pattern.compile("<h1><a href=\"/watch/[\\d]{3,9}/.*/[^\">.*\\</a></h1>]", Pattern.CASE_INSENSITIVE);
-	
-	@Override
-	public ArrayList<DownloadLink> decryptIt(String parameter) {
-		RequestInfo loader;
-		ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-		if(new Regex(parameter, Pattern.compile("http://[\\w\\.]*?youporn\\.com/\\?page=[\\d]{1,3}")).count() == 1) {
-			try {
-				loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
-				String[] matches = new Regex(loader.getHtmlCode(), Pattern.compile("<h1><a href=\"([^\"]+)\"", Pattern.CASE_INSENSITIVE)).getMatches(1);
-				for(String link : matches) {
-						DownloadLink dlink = this.createDownloadlink("http://youporn.com" + link);
-						dlink.setBrowserUrl(parameter);
-						decryptedLinks.add(dlink);
-				}
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {
-			try {
-				loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
-				String matches = new Regex(loader.getHtmlCode(), DOWNLOADFILE).getFirstMatch();
-				DownloadLink dlink = this.createDownloadlink("http://" + matches);
-				dlink.setBrowserUrl(parameter);
-				decryptedLinks.add(dlink);
-			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-        
+    private static final String CODER = "JD-Team";
+    private static final String HOST = "youporn.com";
+    private static final Pattern patternSupported_File = Pattern.compile("http://[\\w\\.]*?youporn\\.com/watch/\\d+/?.*/?", Pattern.CASE_INSENSITIVE);
+    private static final Pattern patternSupported_Other = Pattern.compile("http://[\\w\\.]*?youporn\\.com/(.*?page=\\d+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern patternSupported = Pattern.compile(patternSupported_File.pattern() + "|" + patternSupported_Other.pattern(), Pattern.CASE_INSENSITIVE);
+    private static final Pattern DOWNLOADFILE = Pattern.compile("download\\.youporn\\.com/download/\\d+/flv/[^\\?]*", Pattern.CASE_INSENSITIVE);
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(String parameter) {
+        RequestInfo loader;
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        try {
+            if (new Regex(parameter, patternSupported_Other).matches()) {                                
+                loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
+                String[] matches = new Regex(loader.getHtmlCode(), Pattern.compile("<a href=\"(/watch[^\"]+)\">.*?</.*?></", Pattern.CASE_INSENSITIVE)).getMatches(1);
+                for (String link : matches) {
+                    DownloadLink dlink = this.createDownloadlink("http://youporn.com" + link);                    
+                    decryptedLinks.add(dlink);
+                }
+            } else {
+                loader = HTTP.getRequest(new URL(parameter), "age_check=1", "", true);
+                String matches = new Regex(loader.getHtmlCode(), DOWNLOADFILE).getFirstMatch();
+                if (matches==null) return null;
+                DownloadLink dlink = this.createDownloadlink("http://" + matches);
+                dlink.setBrowserUrl(parameter);
+                decryptedLinks.add(dlink);
+            }
+        } catch (MalformedURLException e) {            
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {                  
+            e.printStackTrace();
+            return null;
+        }
         return decryptedLinks;
-	}
+    }
 
-	@Override
-	public boolean doBotCheck(File file) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean doBotCheck(File file) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public String getCoder() {
-		// TODO Auto-generated method stub
-		return CODER;
-	}
+    @Override
+    public String getCoder() {
+        // TODO Auto-generated method stub
+        return CODER;
+    }
 
-	@Override
-	public String getHost() {
-		// TODO Auto-generated method stub
-		return HOST;
-	}
+    @Override
+    public String getHost() {
+        // TODO Auto-generated method stub
+        return HOST;
+    }
 
-	@Override
-	public String getPluginName() {
-		// TODO Auto-generated method stub
-		return HOST;
-	}
+    @Override
+    public String getPluginName() {
+        // TODO Auto-generated method stub
+        return HOST;
+    }
 
-	@Override
-	public Pattern getSupportedLinks() {
-		// TODO Auto-generated method stub
-		return patternSupported;
-	}
+    @Override
+    public Pattern getSupportedLinks() {
+        // TODO Auto-generated method stub
+        return patternSupported;
+    }
 
-	@Override
-	public String getVersion() {
-		// TODO Auto-generated method stub
-		String ret = new Regex("$Revision: 2107 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+    @Override
+    public String getVersion() {
+        // TODO Auto-generated method stub
+        String ret = new Regex("$Revision: 2107 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
         return ret == null ? "0.0" : ret;
-	}
+    }
 
 }
--- src/jd/plugins/host/YouPornCom.java
+++ src/jd/plugins/host/YouPornCom.java
@@ -1,3 +1,19 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://gnu.org/licenses/>.
+
 package jd.plugins.host;
 
 import java.io.File;
@@ -5,8 +21,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.regex.Pattern;
-
-import jd.config.Configuration;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.HTTP;
@@ -14,53 +28,50 @@ import jd.plugins.HTTPConnection;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginForHost;
 import jd.plugins.download.RAFDownload;
-import jd.utils.JDUtilities;
 
 public class YouPornCom extends PluginForHost {
-	private static final String CODER = "JD-Team";
-	private static final String HOST = "youporn.com";
-	private static final Pattern patternSupported =  Pattern.compile("http://download\\.youporn\\.com/download/[\\d]{3,9}/flv/.*", Pattern.CASE_INSENSITIVE);
-	
-	@Override
-	public String getAGBLink() {
-		// TODO Auto-generated method stub
-		return "http://youporn.com/terms";
-	}
+    private static final String CODER = "JD-Team";
+    private static final String HOST = "youporn.com";
+    private static final Pattern patternSupported = Pattern.compile("http://download\\.youporn\\.com/download/\\d+/flv/.*", Pattern.CASE_INSENSITIVE);
 
-	@Override
-	public boolean getFileInformation(DownloadLink parameter) {
-		try {
-			requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(parameter.getDownloadURL()), null, null, true);
-			HTTPConnection urlConnection = requestInfo.getConnection();
-			parameter.setName(getFileNameFormHeader(urlConnection));
-			parameter.setDownloadMax(urlConnection.getContentLength());
-			return true;
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		parameter.getLinkStatus().addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
-		return false;
-	}
+    @Override
+    public String getAGBLink() {
+        return "http://youporn.com/terms";
+    }
 
-	@Override
-	public int getMaxSimultanDownloadNum() {
-		// TODO Auto-generated method stub
-		return Integer.MAX_VALUE;
-	}
+    @Override
+    public boolean getFileInformation(DownloadLink parameter) {
+        try {
+            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(parameter.getDownloadURL()), null, null, true);
+            HTTPConnection urlConnection = requestInfo.getConnection();
+            parameter.setName(getFileNameFormHeader(urlConnection));
+            parameter.setDownloadMax(urlConnection.getContentLength());
+            return true;
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        parameter.getLinkStatus().addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
+        return false;
+    }
 
-	@Override
-	public void handleFree(DownloadLink link) throws Exception {
+    @Override
+    public int getMaxSimultanDownloadNum() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
         if (!getFileInformation(link)) {
             link.getLinkStatus().addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
             return;
         }
 
         requestInfo = HTTP.getRequestWithoutHtmlCode(new URL(link.getDownloadURL()), null, null, true);
-        HTTPConnection urlConnection = requestInfo.getConnection();        
+        HTTPConnection urlConnection = requestInfo.getConnection();
         if (urlConnection.getContentLength() == 0) {
             link.getLinkStatus().addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
             return;
@@ -70,48 +81,41 @@ public class YouPornCom extends PluginForHost {
         dl.setChunkNum(1);
         dl.setResume(false);
         dl.startDownload();
-	}
+    }
 
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void reset() {
+    }
 
-	@Override
-	public boolean doBotCheck(File file) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
 
-	@Override
-	public String getCoder() {
-		// TODO Auto-generated method stub
-		return CODER;
-	}
+    @Override
+    public String getCoder() {
+        return CODER;
+    }
 
-	@Override
-	public String getHost() {
-		// TODO Auto-generated method stub
-		return HOST;
-	}
+    @Override
+    public String getHost() {
+        // TODO Auto-generated method stub
+        return HOST;
+    }
 
-	@Override
-	public String getPluginName() {
-		// TODO Auto-generated method stub
-		return HOST;
-	}
+    @Override
+    public String getPluginName() {        
+        return HOST;
+    }
 
-	@Override
-	public Pattern getSupportedLinks() {
-		// TODO Auto-generated method stub
-		return patternSupported;
-	}
+    @Override
+    public Pattern getSupportedLinks() {        
+        return patternSupported;
+    }
 
-	@Override
-	public String getVersion() {
-		String ret = new Regex("$Revision: 2070 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2070 $", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
         return ret == null ? "0.0" : ret;
-	}
-
+    }
 }
