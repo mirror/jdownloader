28d6f5315cdd500357b19250dc95271810818073
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-06 11:06:51 +0000

    added RapidSpreadCom decrypter
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2156 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/RapidSpreadCom.java
@@ -0,0 +1,77 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class RapidSpreadCom extends PluginForDecrypt {
+    static private final String host = "rapidspread.com";
+    private static final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?rapidspread\\.com/file\\.jsp\\?id=\\w+", Pattern.CASE_INSENSITIVE);
+
+    public RapidSpreadCom() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(String parameter) {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String page = br.getPage(parameter);
+        String[] links = new Regex(page, Pattern.compile("<a href=\"/redirect\\?link=(.*?)\"", Pattern.CASE_INSENSITIVE)).getMatches(1);
+        for (String element : links) {
+            br.getPage("http://www.rapidspread.com/redirect?link=" + element);
+            decryptedLinks.add(createDownloadlink(br.getRedirectLocation()));
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision$", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+        return ret == null ? "0.0" : ret;
+    }
+}
