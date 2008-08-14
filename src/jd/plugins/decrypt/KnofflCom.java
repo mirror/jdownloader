a58d47763c738dd12b25526ad62194e57b14d416
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-14 11:28:13 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2278 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/KnofflCom.java
@@ -0,0 +1,75 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class KnofflCom extends PluginForDecrypt {
+    static private final String host = "knoffl.com";
+    private static final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?knoffl\\.com/u/\\w+", Pattern.CASE_INSENSITIVE);
+
+    public KnofflCom() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(String parameter) {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String[] links = new Regex(br.getPage(parameter), Pattern.compile("dl\\('(.*?)'\\)", Pattern.CASE_INSENSITIVE)).getMatches(1);
+        for (String element : links) {
+            decryptedLinks.add(createDownloadlink(element));
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision$", "\\$Revision: ([\\d]*?) \\$").getFirstMatch();
+        return ret == null ? "0.0" : ret;
+    }
+}
