70e488b5c44af16322cf8eef48af6b03a5385aa9
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-15 03:58:13 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2308 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/utils/SnifferException.java
@@ -0,0 +1,13 @@
+package jd.utils;
+
+import java.io.IOException;
+
+public class SnifferException extends IOException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4313450544925627349L;
+
+    
+}
