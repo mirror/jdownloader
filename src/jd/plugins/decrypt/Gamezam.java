d49121b96b5dae126273658e0825800dd4184238
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-15 18:48:38 +0000

    -updated Fyadorg & Gamezam decrypter
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2350 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/Gamezam.java
+++ src/jd/plugins/decrypt/Gamezam.java
@@ -17,18 +17,13 @@
 package jd.plugins.decrypt;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import jd.http.Browser;
-import jd.http.HTTPConnection;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
-import jd.plugins.HTTP;
 import jd.plugins.PluginForDecrypt;
-import jd.plugins.RequestInfo;
 import jd.utils.JDUtilities;
 
 public class Gamezam extends PluginForDecrypt {
@@ -41,78 +36,69 @@ public class Gamezam extends PluginForDecrypt {
 
     @Override
     public ArrayList<DownloadLink> decryptIt(String parameter) throws Exception {
-        String cryptedLink = parameter;
-        String id = new Regex(cryptedLink, patternSupported).getFirstMatch();
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        try {
-            boolean gamez_continue = false;
 
-            URL url;
-            RequestInfo reqInfo = HTTP.getRequest(new URL("http://www.gamez.am/start.php?"), null, null, false);
-            String cookie = reqInfo.getConnection().getHeaderFields().get("Set-Cookie").toString().replaceAll("\\[|\\]", "");
-            /* Passwort suchen */
-            reqInfo = HTTP.getRequest(new URL("http://www.gamez.am/include/infos.php?id=" + id), cookie, "http://www.gamez.am/start.php?", false);
-            String pw = new Regex(reqInfo.getHtmlCode(), Pattern.compile("<tr><td>Passwort:</td><td>(.*?)</td></tr>", Pattern.CASE_INSENSITIVE)).getFirstMatch();
-            for (int retrycounter = 1; retrycounter <= 5; retrycounter++) {
-                HTTPConnection captcha_con = new HTTPConnection(new URL("http://www.gamez.am/captcha.php").openConnection());
-                captcha_con.setRequestProperty("Referer", "http://www.gamez.am/start.php?");
-                captcha_con.setRequestProperty("Cookie", cookie);
-                File captchaFile = this.getLocalCaptchaFile(this);
-                if (!Browser.download(captchaFile, captcha_con) || !captchaFile.exists()) {
-                    /* Fehler beim Captcha */
-                    return null;
-                }
-                String captchaCode = JDUtilities.getCaptcha(this, "gamez.am", captchaFile, false);
-                if (captchaCode == null) {
-                    /* abbruch geklickt */
-                    return null;
-                }
-                url = new URL("http://www.gamez.am/include/check.php?id=" + id + "&captcha=" + captchaCode);
-                reqInfo = HTTP.getRequest(url, cookie, "http://www.gamez.am/start.php?", false);
-                if (!reqInfo.containsHTML("Falscher Code")) {
-                    gamez_continue = true;
-                    break;
-                }
+        String id = new Regex(parameter, patternSupported).getFirstMatch();
+
+        boolean gamez_continue = false;
+
+        /* Passwort suchen */
+        String pw = new Regex(br.getPage("http://www.gamez.am/start.php?"), Pattern.compile("<tr><td>Passwort:</td><td>(.*?)</td></tr>", Pattern.CASE_INSENSITIVE)).getFirstMatch();
+        String page = null;
+        for (int retrycounter = 1; retrycounter <= 5; retrycounter++) {
+            File captchaFile = this.getLocalCaptchaFile(this);
+            if (!Browser.download(captchaFile, br.openGetConnection("http://www.gamez.am/captcha.php")) || !captchaFile.exists()) {
+                /* Fehler beim Captcha */
+                return null;
             }
-            if (gamez_continue == true) {
-                /* gamez.am hat böse üble probleme mit falschen links */
-                String direct_links[][] = new Regex(reqInfo.getHtmlCode(), Pattern.compile("<a href=\"(.*?)\" target=\"_blank\"", Pattern.CASE_INSENSITIVE)).getMatches();
-                String extern_links[][] = new Regex(reqInfo.getHtmlCode(), Pattern.compile("window\\.open\\('extern\\.php\\?nr=(.*?)'\\);", Pattern.CASE_INSENSITIVE)).getMatches();
-                for (String[] element : direct_links) {
-                    DownloadLink link = createDownloadlink(element[0]);
-                    link.addSourcePluginPassword(pw);
-                    decryptedLinks.add(link);
-                    progress.increase(1);
-                }
-                progress.setRange(extern_links.length + direct_links.length);
-                for (String[] element : extern_links) {
-                    for (int retry = 0; retry < 3; retry++) {
-                        RequestInfo ri_extern_link = HTTP.getRequest(new URL("http://www.gamez.am/extern.php?nr=" + element[0]), cookie, "http://www.gamez.am/start.php?", false);
-                        if (ri_extern_link.getLocation() != null) {
-                            DownloadLink link = createDownloadlink(ri_extern_link.getLocation());
+            String captchaCode = JDUtilities.getCaptcha(this, "gamez.am", captchaFile, false);
+            if (captchaCode == null) {
+                /* Abbruch geklickt */
+                return null;
+            }
+
+            page = br.getPage("http://www.gamez.am/include/check.php?id=" + id + "&captcha=" + captchaCode);
+            if (page.indexOf("Falscher Code") == -1) {
+                gamez_continue = true;
+                break;
+            }
+        }
+
+        if (gamez_continue == true) {
+            /* gamez.am hat böse üble probleme mit falschen links */
+            String direct_links[][] = new Regex(page, Pattern.compile("<a href=\"(.*?)\" target=\"_blank\"", Pattern.CASE_INSENSITIVE)).getMatches();
+            String extern_links[][] = new Regex(page, Pattern.compile("window\\.open\\('extern\\.php\\?nr=(.*?)'\\);", Pattern.CASE_INSENSITIVE)).getMatches();
+            for (String[] element : direct_links) {
+                DownloadLink link = createDownloadlink(element[0]);
+                link.addSourcePluginPassword(pw);
+                decryptedLinks.add(link);
+                progress.increase(1);
+            }
+            progress.setRange(extern_links.length + direct_links.length);
+            for (String[] element : extern_links) {
+                for (int retry = 0; retry < 3; retry++) {
+                    page = br.getPage("http://www.gamez.am/extern.php?nr=" + element[0]);
+                    if (br.getRedirectLocation() != null) {
+                        DownloadLink link = createDownloadlink(br.getRedirectLocation());
+                        link.addSourcePluginPassword(pw);
+                        decryptedLinks.add(link);
+                        progress.increase(1);
+                        break;
+                    } else {
+                        String follow_link = new Regex(page, Pattern.compile("extern\\.php\\?aktion=unten&nr=(.*?)\"", Pattern.CASE_INSENSITIVE)).getFirstMatch();
+                        follow_link = new Regex(br.getPage("http://www.gamez.am/extern.php?aktion=unten&nr=" + follow_link), Pattern.compile("<form action=\"(.*?)\" method=\"post\">", Pattern.CASE_INSENSITIVE)).getFirstMatch();
+                        if (follow_link != null) {
+                            DownloadLink link = createDownloadlink(follow_link);
                             link.addSourcePluginPassword(pw);
                             decryptedLinks.add(link);
                             progress.increase(1);
                             break;
-                        } else {
-                            String follow_link = new Regex(ri_extern_link.getHtmlCode(), Pattern.compile("extern\\.php\\?aktion=unten&nr=(.*?)\"", Pattern.CASE_INSENSITIVE)).getFirstMatch();
-                            ri_extern_link = HTTP.getRequest(new URL("http://www.gamez.am/extern.php?aktion=unten&nr=" + follow_link), cookie, "http://www.gamez.am/start.php?", false);
-                            follow_link = new Regex(ri_extern_link.getHtmlCode(), Pattern.compile("<form action=\"(.*?)\" method=\"post\">", Pattern.CASE_INSENSITIVE)).getFirstMatch();
-                            if (follow_link != null) {
-                                DownloadLink link = createDownloadlink(follow_link);
-                                link.addSourcePluginPassword(pw);
-                                decryptedLinks.add(link);
-                                progress.increase(1);
-                                break;
-                            }
                         }
                     }
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         }
+
         return decryptedLinks;
     }
 
