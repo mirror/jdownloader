25f0cee8b1e482608ef5dc23cfb3d69766f58e2f
Viperb0y <Viperb0y@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-18 00:02:59 +0000

    Added Decrypter: IsGd.java (http://is.gd)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2377 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/IsGd.java
@@ -0,0 +1,71 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class IsGd extends PluginForDecrypt {
+
+    static private final String host = "is.gd";
+
+    static private final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?is\\.gd/[a-zA-Z0-9]+", Pattern.CASE_INSENSITIVE);
+
+    public IsGd() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(String parameter) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+
+        br.getPage(parameter);
+        decryptedLinks.add(createDownloadlink(br.getRedirectLocation()));
+
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2354 $", "\\$Revision: ([\\d]*?) \\$").getMatch(0);
+        return ret == null ? "0.0" : ret;
+    }
+}
