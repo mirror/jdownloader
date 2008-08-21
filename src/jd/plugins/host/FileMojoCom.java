d08d0341b06397438a0a14bada8e2524a2373682
Viperb0y <Viperb0y@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-08-21 00:28:57 +0000

    Added Hoster: FileMojoCom.java (http://filemojo.com)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2445 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/FileMojoCom.java
@@ -0,0 +1,128 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.host;
+
+import java.io.File;
+import java.util.regex.Pattern;
+
+import jd.http.Browser;
+import jd.http.HTTPConnection;
+import jd.parser.Form;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginForHost;
+import jd.plugins.download.RAFDownload;
+
+public class FileMojoCom extends PluginForHost {
+
+    private static final String HOST = "filemojo.com";
+
+    //static private final Pattern patternSupported = Pattern.compile(".*filemojo\\.com.*", Pattern.CASE_INSENSITIVE);
+    static private final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?filemojo\\.com/[0-9]+/.*", Pattern.CASE_INSENSITIVE);
+
+    public FileMojoCom() {
+        super();
+    }
+
+    @Override
+    public boolean doBotCheck(File file) {
+        return false;
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.filemojo.com/help/tos.php";
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) {
+        try {
+            
+            Browser.clearCookies(HOST);
+            
+            String url = downloadLink.getDownloadURL();
+            
+            String fileId = new Regex(url, "filemojo\\.com/(.*?)/.*").getMatch(0);
+            url = "http://www.filemojo.com/l.php?flink=" + fileId + "&fx=";
+            
+            br.getPage(url);
+            
+            String size = br.getRegex("(\\d+\\.\\d+) (MB|KB)").getMatch(-1);
+            downloadLink.setDownloadSize(Regex.getSize(size));
+            
+            
+            String name = br.getRegex("<b>File Name.*?size=\"2\">(\r\n|\n)(.*?)<br>").getMatch(1);
+            downloadLink.setName(name);
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    public String getPluginName() {
+        return HOST;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2398 $", "\\$Revision: ([\\d]*?) \\$").getMatch(0);
+        return ret == null ? "0.0" : ret;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        LinkStatus linkStatus = downloadLink.getLinkStatus();
+
+        if (!getFileInformation(downloadLink)) {
+            linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
+            return;
+        }
+
+        HTTPConnection urlConnection = br.openFormConnection(1);
+        downloadLink.setDownloadSize(urlConnection.getContentLength());
+        dl = new RAFDownload(this, downloadLink, urlConnection);
+        dl.setResume(false);
+        dl.setChunkNum(1);
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+}
