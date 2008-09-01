1f91ebf49a3a5dd825d641ef83ff3df897841a4a
gluewurm <gluewurm@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-01 20:46:50 +0000

    RomsZopharNet Decrypter added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2630 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/RomsZopharNet.java
@@ -0,0 +1,74 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class RomsZopharNet extends PluginForDecrypt {
+
+    static private final String host = "roms.zophar.net";
+
+    static private final Pattern patternSupported = Pattern.compile("http://[\\w.]*?roms\\.zophar\\.net/(.+)/(.+)", Pattern.CASE_INSENSITIVE);
+
+    public RomsZopharNet() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+
+        String file[] = new Regex(parameter, patternSupported.pattern()).getRow(0);
+        if (file.length != 2) return null;
+        decryptedLinks.add(createDownloadlink("http://" + host + "/" + file[0] + "/" + file[1]));
+
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2354 $", "\\$Revision: ([\\d]*?) \\$").getMatch(0);
+        return ret == null ? "0.0" : ret;
+    }
+}
