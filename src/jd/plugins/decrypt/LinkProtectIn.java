996ba1e8afb4ae5429e48d514e7e1ec87497ec29
gluewurm <gluewurm@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-01 13:54:37 +0000

    LinkProtectIn (Decrypter) hinzugefügt (Decryption mit PW und Captcha funktioniert noch nicht, weil die Browser Klasse das Cookie beim Laden des Captchas vergisst)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2614 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/LinkProtectIn.java
@@ -0,0 +1,147 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import jd.http.Browser;
+import jd.parser.Form;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForDecrypt;
+import jd.utils.JDUtilities;
+
+public class LinkProtectIn extends PluginForDecrypt {
+
+    static private final String host = "linkprotect.in";
+    
+    static private final Pattern patternName = Pattern.compile("Ordnername: <b>(.*?)</b>");
+    static private final Pattern patternPassword = Pattern.compile("<input type=\"text\" name=\"pw\" class=\"[a-zA-Z0-9]{1,50}\" size=\"[0-9]{1,3}\" />");
+    static private final Pattern patternCaptcha = Pattern.compile("<img src=\"(.*?securimage_show.*?)\"");
+    static private final Pattern patternDownload = Pattern.compile("http://[\\w\\.]*?linkprotect\\.in/includes/dl.php\\?id=[a-zA-Z0-9]{1,50}");
+    static private final Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?linkprotect\\.in/index.php\\?site=folder&id=[a-zA-Z0-9]{1,50}", Pattern.CASE_INSENSITIVE);
+
+    public LinkProtectIn() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        
+        try {
+
+            boolean lp_continue = false;
+            Matcher matcher;
+            Form form;
+            
+            /* zuerst mal den evtl captcha abarbeiten */
+            br.getPage(parameter);
+            for (int retrycounter = 1; retrycounter <= 5; retrycounter++) {
+                matcher = patternCaptcha.matcher(br + "");
+                if (matcher.find()) {
+                    form = br.getForm(0);
+
+                    String captchaAddress = "http://" + getHost() + "/" + matcher.group(1);
+                    File captchaFile = this.getLocalCaptchaFile(this);
+                    if (!br.download(captchaFile, captchaAddress) || !captchaFile.exists()) {
+                        /* Fehler beim Captcha */
+                        logger.severe("Captcha Download fehlgeschlagen: " + captchaAddress);
+                        return null;
+                    }
+                    String captchaCode = Plugin.getCaptchaCode(captchaFile, this);
+                    if (captchaCode == null) {
+                        /* abbruch geklickt */
+                        return null;
+                    }
+                    captchaCode = captchaCode.toUpperCase();
+                    form.put("code", captchaCode);
+                    
+                    /* Herausfinden ob ein Passwort benötigt wird und ggf. abfragen */
+                    matcher = patternPassword.matcher(br + "");
+                    if(matcher.find()) {
+                        String password = "";
+                        password = JDUtilities.getController().getUiInterface().showUserInputDialog("Die Links sind mit einem Passwort gesch\u00fctzt. Bitte geben Sie das Passwort ein:");
+                        form.put("pw", password);
+                    }
+                    br.submitForm(form);
+                } else {
+                    lp_continue = true;
+                    break;
+                }
+            }
+
+            if (lp_continue == true) {
+                /* Links extrahieren */
+                String[] links = jd.parser.HTMLParser.getHttpLinks(br + "", host);
+                FilePackage fp = new FilePackage();
+                matcher = patternName.matcher(br + "");
+                if(matcher.find()) fp.setName(new Regex(br + "", patternName.pattern()).getMatch(0));
+                
+                for (int i=0; i <= links.length - 1; i++) {
+                    matcher = patternDownload.matcher(links[i]);
+                    if (matcher.find()) {
+                        /* EinzelLink gefunden */
+                        String link = matcher.group(0);
+                        br.getPage(link);
+                        String finalLink = br.getRedirectLocation();
+                        DownloadLink dlLink = createDownloadlink(finalLink);
+                        dlLink.setFilePackage(fp);
+                        decryptedLinks.add(dlLink);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2542 $", "\\$Revision: ([\\d]*?) \\$").getMatch(0);
+        return ret == null ? "0.0" : ret;
+    }
+}
