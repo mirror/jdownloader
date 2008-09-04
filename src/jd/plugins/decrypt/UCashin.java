37be9c450331084fd0ec9a068c1c0021d3cc8a3d
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-04 14:30:26 +0000

    UCashin Decrypter added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2692 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/UCashin.java
@@ -0,0 +1,74 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class UCashin extends PluginForDecrypt {
+
+    static private String host = "ucash.in";
+    private Pattern patternSupported = Pattern.compile("http://[\\w\\.]*?ucash\\.in/([a-zA-Z0-9]+)", Pattern.CASE_INSENSITIVE);
+
+    public UCashin() {
+        super();
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String id = new Regex(param.toString(), patternSupported).getMatch(0);
+        br.getPage("http://ucash.in/" + id);
+
+        String link = br.getRegex(Pattern.compile("<a href=\"(.*?)\">click to continue</a>", Pattern.CASE_INSENSITIVE)).getMatch(0);
+        if (link == null) { return null; }
+        decryptedLinks.add(createDownloadlink(link));
+
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getCoder() {
+        return "JD-Team";
+    }
+
+    @Override
+    public String getHost() {
+        return host;
+    }
+
+    @Override
+    public String getPluginName() {
+        return host;
+    }
+
+    @Override
+    public Pattern getSupportedLinks() {
+        return patternSupported;
+    }
+
+    @Override
+    public String getVersion() {
+        String ret = new Regex("$Revision: 2666 $", "\\$Revision: ([\\d]*?) \\$").getMatch(0);
+        return ret == null ? "0.0" : ret;
+    }
+}
