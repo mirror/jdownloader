30c74b2c5dacf0b2e8bfc9a85ddb229570bffee8
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-09 21:23:48 +0000

    FastLoadNet Hoster fixed, Dateigröße ermitteln muss noch eingebaut werden
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2808 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/FastLoadNet.java
+++ src/jd/plugins/host/FastLoadNet.java
@@ -16,20 +16,15 @@
 
 package jd.plugins.host;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.regex.Pattern;
 
-import jd.http.Browser;
 import jd.http.Encoding;
 import jd.http.HTTPConnection;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
-import jd.plugins.HTTP;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginForHost;
-import jd.plugins.RequestInfo;
 import jd.plugins.download.RAFDownload;
 import jd.utils.JDLocale;
 
@@ -37,16 +32,12 @@ public class FastLoadNet extends PluginForHost {
 
     private static final String CODER = "eXecuTe";
 
-    // Suchmasken
-    private static final String DOWNLOAD_INFO = "<th.*?><b>Datei</b></th>[\\s]*?<th.*?><b>Gr&ouml;sse</b></th>[\\s]*?</tr>[\\s]*?<tr>[\\s]*?<td.*?><font.*?>(.*?)</font></td>[\\s]*?<td.*?><font.*?>(.*?) MB</font></td>";
-
     private static final String HARDWARE_DEFECT = "Hardware-Defekt!";
 
     private static final String NOT_FOUND = "Datei existiert nicht";
 
     public FastLoadNet(String cfgName) {
         super(cfgName);
-
     }
 
     @Override
@@ -60,58 +51,37 @@ public class FastLoadNet extends PluginForHost {
     }
 
     @Override
-    public boolean getFileInformation(DownloadLink downloadLink) {
+    public boolean getFileInformation(DownloadLink downloadLink) throws IOException {
         LinkStatus linkStatus = downloadLink.getLinkStatus();
         String downloadurl = downloadLink.getDownloadURL() + "&lg=de";
-        try {
+        br.setFollowRedirects(true);
+        br.setCookiesExclusive(true);
+        br.clearCookies(getHost());
+        br.getPage(downloadurl);
 
-            RequestInfo requestInfo = HTTP.getRequest(new URL(downloadurl));
-
-            if (requestInfo.getHtmlCode().contains(NOT_FOUND)) {
-
-                linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
-                downloadLink.setName(downloadLink.getDownloadURL().substring(downloadurl.indexOf("pid=") + 4, downloadurl.length() - 6));
-                return false;
-
-            }
-
-            if (requestInfo.getHtmlCode().contains(HARDWARE_DEFECT)) {
-
-                linkStatus.addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
-                linkStatus.setValue(20 * 60 * 1000l);
-                downloadLink.setName(downloadLink.getDownloadURL().substring(downloadurl.indexOf("pid=") + 4, downloadurl.length() - 6));
-                return false;
-
-            }
-
-            String fileName = Encoding.htmlDecode(new Regex(requestInfo.getHtmlCode(), DOWNLOAD_INFO).getMatch(0)).trim();
-            Integer length = (int) Math.round(Double.parseDouble(new Regex(requestInfo.getHtmlCode(), DOWNLOAD_INFO).getMatch(1).trim()) * 1024 * 1024);
-
-            // downloadinfos gefunden? -> download verfügbar
-            if (fileName != null && length != null) {
-
-                downloadLink.setName(fileName);
-
-                try {
-                    downloadLink.setDownloadSize(length);
-                } catch (Exception e) {
-                }
-
-                return true;
-
-            } else {
-                downloadLink.setName(downloadurl.substring(downloadurl.indexOf("pid=") + 4));
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (br.containsHTML(NOT_FOUND)) {
+            linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
+            downloadLink.setName(downloadLink.getDownloadURL().substring(downloadurl.indexOf("pid=") + 4));
+            return false;
         }
 
-        // unbekannter fehler
-        return false;
+        if (br.containsHTML(HARDWARE_DEFECT)) {
+            linkStatus.addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
+            linkStatus.setValue(20 * 60 * 1000l);
+            downloadLink.setName(downloadLink.getDownloadURL().substring(downloadurl.indexOf("pid=") + 4));
+            return false;
+        }
 
+        String fileName = Encoding.htmlDecode(br.getRegex(Pattern.compile("<th.*?><b>Datei</b></th>.*?<font.*?;\">(.*?)</font>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0));
+        long length = 0;
+        // downloadinfos gefunden? -> download verfügbar
+        if (fileName != null) {
+            downloadLink.setName(fileName.trim());
+            downloadLink.setDownloadSize(length);
+            return true;
+        }
+        downloadLink.setName(downloadurl.substring(downloadurl.indexOf("pid=") + 4));
+        return false;
     }
 
     @Override
@@ -123,111 +93,63 @@ public class FastLoadNet extends PluginForHost {
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception {
         LinkStatus linkStatus = downloadLink.getLinkStatus();
-        String downloadurl = downloadLink.getDownloadURL() + "&lg=de";
+        br.setFollowRedirects(true);
+        br.setCookiesExclusive(true);
+        br.clearCookies(getHost());
 
-        RequestInfo requestInfo = HTTP.getRequest(new URL(downloadurl));
-        String cookie = requestInfo.getCookie();
-
-        if (requestInfo.getHtmlCode().contains(NOT_FOUND)) {
-
+        if (!getFileInformation(downloadLink)) {
+            linkStatus.addStatus(LinkStatus.ERROR_FATAL);
+            linkStatus.setErrorMessage(getHost() + " " + JDLocale.L("plugins.host.server.unavailable", "Serverfehler"));
+            return;
+        }
+        if (br.containsHTML(NOT_FOUND)) {
             linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
             return;
-
         }
 
-        if (requestInfo.getHtmlCode().contains(HARDWARE_DEFECT)) {
-
+        if (br.containsHTML(HARDWARE_DEFECT)) {
             linkStatus.addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
             linkStatus.setValue(20 * 60 * 1000l);
             return;
-
         }
 
-        int maxCaptchaTries = 10;
-        while (true) {
-            File file = this.getLocalCaptchaFile(this);
+        String dl_url = br.getRegex(Pattern.compile("type=\"button\" onclick=\"location='(.*?)'\" value=\"download", Pattern.CASE_INSENSITIVE)).getMatch(0);
 
-            requestInfo = HTTP.getRequestWithoutHtmlCode(new URL("http://fast-load.net/includes/captcha.php"), cookie, downloadurl, true);
+        // Download vorbereiten
+        HTTPConnection urlConnection = br.openGetConnection(dl_url);
+        long length = urlConnection.getContentLength();
 
-            Browser.download(file, requestInfo.getConnection());
+        if (urlConnection.getContentType() != null) {
 
-        
+            if (urlConnection.getContentType().contains("text/html")) {
 
-            String code = getCaptchaCode(file, downloadLink);
-            if (code == null) {
-                if (maxCaptchaTries-- > 0) {
-                    logger.warning("Captcha wrong. Retries left: " + maxCaptchaTries);
-                    continue;
+                if (length == 184) {
+                    logger.info("System overload: Retry in 20 seconds");
+                    linkStatus.addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
+                    linkStatus.setValue(20 * 60 * 1000l);
+                    return;
+                } else if (length == 169) {
+                    logger.severe("File not found: File is deleted from Server");
+                    linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    return;
                 } else {
-                    linkStatus.addStatus(LinkStatus.ERROR_CAPTCHA);
-                    linkStatus.setErrorMessage(requestInfo.getHtmlCode());
+                    logger.severe("Unknown error page - [Length: " + length + "]");
+                    linkStatus.addStatus(LinkStatus.ERROR_PLUGIN_DEFEKT);
                     return;
                 }
-
             }
-            String pid = downloadurl.substring(downloadurl.indexOf("pid=") + 4, downloadurl.indexOf("&"));
+            // Download starten
+            dl = new RAFDownload(this, downloadLink, urlConnection);
+            dl.setResume(false);
+            dl.setChunkNum(1);
+            dl.startDownload();
+            return;
 
-            requestInfo = HTTP.postRequestWithoutHtmlCode(new URL("http://fast-load.net/download.php"), cookie, downloadurl, "fid=" + pid + "&captcha_code=" + code, true);
-
-            // Download vorbereiten
-            HTTPConnection urlConnection = requestInfo.getConnection();
-            long length = urlConnection.getContentLength();
-
-            if (urlConnection.getContentType() != null) {
-
-                if (urlConnection.getContentType().contains("text/html")) {
-
-                    if (length == 13) {
-
-                        if (maxCaptchaTries-- > 0) {
-                            logger.warning("Captcha wrong. Retries left: " + maxCaptchaTries);
-                            continue;
-                        } else {
-                            linkStatus.addStatus(LinkStatus.ERROR_CAPTCHA);
-                            linkStatus.setErrorMessage(requestInfo.getHtmlCode());
-                            return;
-                        }
-
-                    } else if (length == 184) {
-
-                        logger.info("System overload: Retry in 20 seconds");
-                        linkStatus.addStatus(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
-                        linkStatus.setErrorMessage(requestInfo.getHtmlCode());
-                        linkStatus.setValue(20 * 60 * 1000l);
-                        return;
-
-                    } else if (length == 169) {
-
-                        logger.severe("File not found: File is deleted from Server");
-                        linkStatus.addStatus(LinkStatus.ERROR_FILE_NOT_FOUND);
-                        return;
-
-                    } else {
-
-                        logger.severe("Unknown error page - [Length: " + length + "]");
-                        linkStatus.addStatus(LinkStatus.ERROR_PLUGIN_DEFEKT);
-                        return;
-
-                    }
-
-                }
-
-                // Download starten
-                dl = new RAFDownload(this, downloadLink, urlConnection);
-                dl.setResume(false);
-                dl.setChunkNum(1);
-                dl.startDownload();
-                return;
-
-            } else {
-
-                logger.severe("Couldn't get HTTP connection");
-                linkStatus.addStatus(LinkStatus.ERROR_RETRY);
-                return;
-
-            }
+        } else {
+            logger.severe("Couldn't get HTTP connection");
+            linkStatus.addStatus(LinkStatus.ERROR_RETRY);
+            return;
         }
-
     }
 
     public int getMaxSimultanFreeDownloadNum() {
