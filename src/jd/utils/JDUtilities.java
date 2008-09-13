1c63841cb91a569340e25e2d3aa801a7a81b5231
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-13 15:33:32 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2888 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/utils/JDUtilities.java
+++ src/jd/utils/JDUtilities.java
@@ -133,7 +133,7 @@ public class JDUtilities {
 
     private static HashMap<String, PluginsC> containerPlugins = new HashMap<String, PluginsC>();
 
-    /**
+    /** 
      * Der DownloadController
      */
     private static JDController controller = null;
