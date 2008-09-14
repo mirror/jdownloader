5175c0b245ec3363d70a636a497d747a5ae1811e
scr4ve <scr4ve@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-14 14:54:59 +0000

    JDTwitter - defaultmessage & cut insertet
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2908 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/utils/JDTwitter.java
+++ src/jd/utils/JDTwitter.java
@@ -10,19 +10,26 @@ public class JDTwitter {
     	
     	long onedayback = System.currentTimeMillis();
     	onedayback = onedayback - 1000*60*60*24;
-    	System.out.println(JDUtilities.formatTime(onedayback));
+    	//System.out.println(JDUtilities.formatTime(onedayback));
     	Browser br = new Browser();
     	String status = null;
     	try { 
 			br.getPage(JDLocale.L("main.twitter.url", "http://twitter.com/statuses/user_timeline/jdownloader.xml") + "?count=1&since="+JDUtilities.formatTime(onedayback));
 			//System.out.println(br);
 			status = br.getRegex("<status>[\\s\\S]*?<text>(.*?)</text>[\\s\\S]*?</status>").getMatch(0);
+
 			//System.out.println(status);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	if((status==null)||(status=="")){status = JDLocale.L("sys.message.welcome", "Welcome to JDownloader");};
-        return status;
+		
+		if(status.matches(".*defaultmessage.*")){ status = ""; }
+		
+    	if((status==null)||(status=="")){status = JDLocale.L("sys.message.welcome", "Welcome to JDownloader");}
+    	else {if(status.length() > 70) { status = status.substring(0, 70) + "..." ;}}
+    	
+    	
+    	return status;
     }
 	
 }
