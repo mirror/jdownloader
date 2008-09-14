df0a46f77949b627ff9d72724c129bca49e78229
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-14 21:29:54 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2917 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/router/GetMacAdress.java
@@ -0,0 +1,83 @@
+package jd.router;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+import jd.parser.Regex;
+
+import jd.utils.JDUtilities;
+
+
+public class GetMacAdress {
+    public static String getMacAddress() throws SocketException, UnknownHostException 
+    { 
+    try {
+        return new GetMacAdress().getMacAddress(RouterInfoCollector.getRouterIP());
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    } 
+    return null;
+     
+    }
+    public String getMacAddress ( String host ) throws UnknownHostException, IOException, InterruptedException
+    {
+        InetAddress hostAddress = InetAddress.getByName( host );
+        String resultLine = callArpTool ( hostAddress.getHostAddress() );
+        return new Regex(resultLine, "..[:\\-]..[:\\-]..[:\\-]..[:\\-]..[:\\-]..").getMatch(-1).replaceAll("-", ":");
+    }
+    
+
+    
+    private String callArpTool ( String ipAddress ) throws IOException, InterruptedException
+    {
+        String OS = System.getProperty("os.name").toLowerCase();
+        if (OS.indexOf("nt") > -1 || OS.indexOf("windows") > -1)
+        {
+            return callArpToolWindows( ipAddress );
+        }
+ 
+        return callArpToolDefault( ipAddress );
+    }
+    
+    private String callArpToolWindows ( String ipAddress ) throws IOException, InterruptedException
+    {
+        ProcessBuilder pb = new ProcessBuilder(new String[] {"ping", ipAddress});
+        pb.start();
+        String[] parts = JDUtilities.runCommand("arp", new String[] {"-a"}, null,10).split(System.getProperty("line.separator"));
+        pb.directory();
+        for ( String part : parts )
+        {            
+            if ( part.indexOf(ipAddress) > -1 )
+            {
+                return part;
+            }
+        }
+        return null;
+     }
+    
+    private String callArpToolDefault ( String ipAddress ) throws IOException, InterruptedException
+    {
+        ProcessBuilder pb = new ProcessBuilder(new String[] {"ping", ipAddress});
+        pb.start();
+        String out = JDUtilities.runCommand("arp", new String[] {ipAddress}, null,10);
+        pb.directory();
+        return out;
+     }
+    public static void main(String[] args) {
+        try {
+            System.out.println(getMacAddress());
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/jd/router/RouterInfoCollector.java
@@ -0,0 +1,94 @@
+package jd.router;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import jd.controlling.interaction.HTTPLiveHeader;
+
+import jd.JDInit;
+
+import jd.http.Browser;
+
+import jd.utils.JDLocale;
+
+import jd.config.Configuration;
+import jd.utils.JDUtilities;
+
+public class RouterInfoCollector implements Serializable{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private String reconnectMethode = JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_REQUESTS, null).trim();
+    protected boolean isLiveHeaderReconnect = true;
+    protected String routerIp = null;
+    protected String routerSite = null;
+    protected String routerErrorPage = null;
+    protected String routerMAC = null;
+    protected String[] RouterMethodeNames = null;
+    
+    public RouterInfoCollector() {
+        String lh = JDLocale.L("modules.reconnect.types.liveheader", "LiveHeader/Curl");
+        if (!JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_RECONNECT_TYPE, lh).equals(lh) || reconnectMethode == null) {
+            isLiveHeaderReconnect = false;
+            return;
+        }
+        routerIp = getRouterIP();
+        Browser br = new Browser();
+        try {
+            routerSite = br.getPage("http://" + routerIp);
+        } catch (IOException e) {
+        }
+        try {
+            routerErrorPage = br.getPage("http://" + routerIp+"/error404");
+        } catch (IOException e) {
+        }
+        try {
+            routerMAC=new GetMacAdress().getMacAddress(routerIp);
+        } catch (SocketException e) {
+        } catch (UnknownHostException e) {
+        } catch (IOException e) {
+        } catch (InterruptedException e) {
+        }
+        String rn = JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_ROUTERNAME, null);
+        if(rn!=null)
+            RouterMethodeNames=new String[] {rn};
+        else
+        {
+        ArrayList<String> rmn = new ArrayList<String>();
+        for (String[] script:new HTTPLiveHeader().getLHScripts()) {
+            if(script[2].trim().equals(reconnectMethode) && !rmn.contains(script[1]))
+                rmn.add(script[1]);
+        }
+        RouterMethodeNames=rmn.toArray(new String[rmn.size()]);
+        }
+    }
+    @Override
+    public String toString() {
+       String sep = System.getProperty("line.separator");
+       String ret="";
+       if(RouterMethodeNames.length==1)
+           ret="RouterMethodeName:"+RouterMethodeNames[0]+sep;
+       else
+       for (int i = 0; i < RouterMethodeNames.length; i++) {
+           ret+="RouterMethodeName["+i+"]:"+RouterMethodeNames[i]+sep;
+       }
+       ret+="ReconnectMethode:"+reconnectMethode+sep+"isLiveHeaderReconnect:"+isLiveHeaderReconnect+sep+"RouterIp:"+routerIp+sep+"RouterMAC:"+routerMAC+sep+"RouterSite:"+routerSite+sep+"RouterErrorPage:"+routerErrorPage;
+        return ret;
+    }
+    public static String getRouterIP() {
+        String routerIp = JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_IP, null);
+        if (routerIp == null || routerIp.matches("\\s*")) routerIp = new GetRouterInfo(null).getAdress();
+        return routerIp;
+
+    }
+
+    public static void main(String[] args) {
+        new JDInit().loadConfiguration();
+        System.out.println(new RouterInfoCollector().toString());
+    }
+}
