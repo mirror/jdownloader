ab36cbe37217929b35c229d2cf098a4c9835e737
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-16 12:25:36 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2961 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/unrar/test.java
+++ src/jd/unrar/test.java
@@ -1,42 +1,40 @@
 package jd.unrar;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.PrintStream;
 
 public class test {
-public static void main(String[] args) {
-    ///home/dwd/.jd_home/downloads/Read Me.rar
-    try {
-        ProcessBuilder pb = new ProcessBuilder(new String[] {"unrar", "-ierr", "e", "ad.rar"});
-        pb.directory(new File("/home/dwd/.jd_home/downloads/"));
-        final Process p = pb.start();
-        
-         InputStreamReader inp = new InputStreamReader(p.getErrorStream());
+    public static void main(String[] args) {
+        // /home/dwd/.jd_home/downloads/Read Me.rar
+        try {
 
-
-        int c;
-        StringBuffer buff = new StringBuffer();
-        while ((c=inp.read())!=-1) {
-//            type type = (type) en.nextElement();
-            buff.append((char)c);
-            System.out.print((char)c);
-            if(buff.indexOf("password") != -1 && c==':')
-            {
-                            BufferedWriter opw = new BufferedWriter(new OutputStreamWriter(p
-                                    .getOutputStream()));
-                            opw.write("test");
-                            opw.newLine();
-                            opw.flush();
-                            opw.close();
-                            System.out.println("test");
-                            
+            ProcessBuilder pb = new ProcessBuilder(new String[] {"unrar", "e", "ad.rar"});
+            pb.directory(new File("/home/dwd/.jd_home/downloads/"));
+            pb.redirectErrorStream(true);
+            Process p = pb.start();
+            InputStreamReader inp = new InputStreamReader(p.getInputStream());
+            int c;
+            StringBuffer buff = new StringBuffer();
+            while ((c = inp.read()) != -1) {
+                // type type = (type) en.nextElement();
+                buff.append((char) c);
+                System.out.print((char) c);
+                if (buff.indexOf("password") != -1 && c == ':') {
+                    c = inp.read();
+                    System.out.print((char) c);
+                    Thread.sleep(200);
+                    PrintStream s = new PrintStream(p.getOutputStream());
+                    s.print("test\r\n");
+                    s.flush();
+                    s.close();
+                    System.out.println("test");
+//                    c = inp.read();
+                }
             }
+
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-            
-    } catch (Exception e) {
-        // TODO: handle exception
     }
-}
 }
