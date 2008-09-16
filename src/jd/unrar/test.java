b5cb1e74c4aa26d1e812a70c2d4d258b71ce699f
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-16 11:01:21 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@2960 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/unrar/test.java
+++ src/jd/unrar/test.java
@@ -1,58 +1,39 @@
 package jd.unrar;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 
 public class test {
 public static void main(String[] args) {
     ///home/dwd/.jd_home/downloads/Read Me.rar
     try {
-        ProcessBuilder pb = new ProcessBuilder(new String[] {"unrar","-ierr", "e", "ReadMe.rar"});
+        ProcessBuilder pb = new ProcessBuilder(new String[] {"unrar", "-ierr", "e", "ad.rar"});
         pb.directory(new File("/home/dwd/.jd_home/downloads/"));
-        Process p = pb.start();
-        BufferedInputStream buffer =
-            new BufferedInputStream(p.getErrorStream());
-            BufferedReader commandResult =
-            new BufferedReader(new InputStreamReader(buffer));
+        final Process p = pb.start();
+        
+         InputStreamReader inp = new InputStreamReader(p.getErrorStream());
 
-            BufferedOutputStream bufferout =
-            new BufferedOutputStream(p.getOutputStream());
-            PrintWriter commandInput =
-            new PrintWriter((new OutputStreamWriter(bufferout)), true);
-            String s = null;
-            try {
 
-            while (true){
-            s = commandResult.readLine();
-
-            System.out.println("Output: " + s);
-            System.out.flush();
-            if(s.contains("password"))
-            break;
+        int c;
+        StringBuffer buff = new StringBuffer();
+        while ((c=inp.read())!=-1) {
+//            type type = (type) en.nextElement();
+            buff.append((char)c);
+            System.out.print((char)c);
+            if(buff.indexOf("password") != -1 && c==':')
+            {
+                            BufferedWriter opw = new BufferedWriter(new OutputStreamWriter(p
+                                    .getOutputStream()));
+                            opw.write("test");
+                            opw.newLine();
+                            opw.flush();
+                            opw.close();
+                            System.out.println("test");
+                            
             }
-            System.out.println("test");
-            Thread.sleep(100);
-            commandInput.write("test\r\n");
-            commandInput.flush();
-            while ((s = commandResult.readLine()) != null)
-            System.out.println("Output: " + s);
-
-            commandInput.close();
-            commandResult.close();
-
-            if (p.exitValue() != 0) {
-            System.out.println(" -- p.exitValue() != 0");
-            }
-            } catch (Exception e) {
-            System.out.println("Exception ::" + e);
-            }
+        }
             
     } catch (Exception e) {
         // TODO: handle exception
