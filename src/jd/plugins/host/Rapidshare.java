99379eb4493b84b34cad5ed3c46772f8067dfb2a
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-09-20 01:01:19 +0000

    Rapidshare Premium Traffic Left fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3012 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/Rapidshare.java
+++ src/jd/plugins/host/Rapidshare.java
@@ -767,7 +767,7 @@ public class Rapidshare extends PluginForHost {
         String rapidPoints = br.getRegex("<td>RapidPoints:</td><td.*?><b>(.*?)</b></td>").getMatch(0).trim();
         String usedSpace = br.getRegex("<td>(Used storage:|Belegter Speicher:)</td><td.*?><b>(.*?)</b></td>").getMatch(1).trim();
         String trafficShareLeft = br.getRegex("<td>(TrafficShare left:|TrafficShare &uuml;brig:)</td><td.*?><b>(.*?)</b></td>").getMatch(1).trim();
-        ai.setTrafficLeft(Regex.getSize(trafficLeft + " kb"));
+        ai.setTrafficLeft(Regex.getSize(trafficLeft + " Mb")/1000);
         ai.setTrafficMax(50 * 1024 * 1024 * 1024l);
         ai.setFilesNum(Integer.parseInt(files));
         ai.setPremiumPoints(Integer.parseInt(rapidPoints));
