eb790f1847a5549b5655235d0e96d2e2f62d00ca
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-09 21:03:14 +0000

    LinkShareOrg updated
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3285 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/LinkShareOrg.java
+++ src/jd/plugins/decrypt/LinkShareOrg.java
@@ -41,106 +41,105 @@ public class LinkShareOrg extends PluginForDecrypt {
         String containerId = new Regex(parameter, "\\?url=([a-zA-Z0-9]{32})").getMatch(0);
 
         // Prüfen ob Containerfiles vorhanden sind
-        if (!br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".dlc").toString().contains("Die Datei wurde nicht gefunden"))
-        {
+        if (!br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".dlc").toString().contains("Die Datei wurde nicht gefunden")) {
             File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".dlc");
             Browser.download(container, "http://www.link-share.org/container/download.php?id=" + containerId + ".dlc");
             decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
             container.delete();
         }
-        else if (!br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".ccf").toString().contains("Die Datei wurde nicht gefunden"))
-        {
+        if (decryptedLinks.size() == 0 && !br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".ccf").toString().contains("Die Datei wurde nicht gefunden")) {
             File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".ccf");
             Browser.download(container, "http://www.link-share.org/container/download.php?id=" + containerId + ".ccf");
             decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
             container.delete();
         }
-        else if (!br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".rsdf").toString().contains("Die Datei wurde nicht gefunden"))
-        {
+        if (decryptedLinks.size() == 0 && !br.getPage("http://www.link-share.org/container/download.php?id=" + containerId + ".rsdf").toString().contains("Die Datei wurde nicht gefunden")) {
             File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".rsdf");
             Browser.download(container, "http://www.link-share.org/container/download.php?id=" + containerId + ".rsdf");
             decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
             container.delete();
         }
-        else 
-        {
+        if (decryptedLinks.size() == 0) {
             // 150 Links können maximal verwendet werden
-            // Hier läuft es durch und wartet bis encodedLink nichtmehr vorhanden
+            // Hier läuft es durch und wartet bis encodedLink nichtmehr
+            // vorhanden
             // ist und somit die Links "zuende"
-            progress.setRange(150);
-            for (int i = 0; i < 150; i++)
-            {
+
+            for (int i = 0; i < 150; i++) {
                 String encodedLink = Encoding.htmlDecode(new Regex(br.getPage("http://link-share.org/show.php?url=" + containerId + "&id=" + i), "<iframe src=\"(.*?)\" allow").getMatch(0));
-                if (encodedLink.equals(""))
-                    break;
-                
+                if (encodedLink.equals("")) break;
+
                 decryptedLinks.add(createDownloadlink(encodedLink));
-                progress.increase(1);
+
             }
         }
 
-        
-        /* Captcha
-        if (br.containsHTML("<input type='hidden' name='captcha_encode'"))
-        {
-            String captchaCode = br.getRegex("name='captcha_encode' value='(.*?)'><img").getMatch(0);
-            File file = this.getLocalCaptchaFile(this);
-            Form form = br.getForm(0);
-            Browser.download(file, br.cloneBrowser().openGetConnection("http://link-share.org/captcha.php?captcha_code=" + captchaCode));
-            String capTxt = Plugin.getCaptchaCode(file, this, param);
-            form.put("captcha_code", capTxt);
-            br.submitForm(form);
-            
-            if (br.containsHTML("<input type='hidden' name='captcha_encode'"))
-            {
-                throw new DecrypterException(DecrypterException.CAPTCHA);
-            }
-        }*/
-        
-        /*if (br.containsHTML("<image src=\"images/dlc.gif\">"))
-        {
-            URL url;
-            String containerId = new Regex(parameter, "\\?url=([a-zA-Z0-9]{32})").getMatch(0);
-            url = new URL("http://www.link-share.org/container/download.php?id=" + containerId + ".dlc");
-            File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".dlc");
-            HTTPConnection dlc_con = new HTTPConnection(url.openConnection());
-            Browser.download(container, dlc_con);
-            decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
-            container.delete();
-        }
-        else if (br.containsHTML("<image src=\"images/ccf.gif\">"))
-        {
-            URL url;
-            String containerId = new Regex(parameter, "\\?url=([a-zA-Z0-9]{32})").getMatch(0);
-            url = new URL("http://www.link-share.org/container/download.php?id=" + containerId + ".ccf");
-            File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".dlc");
-            HTTPConnection ccf_con = new HTTPConnection(url.openConnection());
-            Browser.download(container, ccf_con);
-            decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
-            container.delete();
-        }
-        else if (br.containsHTML("<image src=\"images/rsdf.gif\">"))
-        {
-            URL url;
-            String containerId = new Regex(parameter, "\\?url=([a-zA-Z0-9]{32})").getMatch(0);
-            url = new URL("http://www.link-share.org/container/download.php?id=" + containerId + ".rsdf");
-            File container = JDUtilities.getResourceFile("container/" + System.currentTimeMillis() + ".dlc");
-            HTTPConnection rsdf_con = new HTTPConnection(url.openConnection());
-            Browser.download(container, rsdf_con);
-            decryptedLinks.addAll(JDUtilities.getController().getContainerLinks(container));
-            container.delete();
-        }*/
-        
-        /*String links[] = br.getRegex("<a href=\"show.php\\?url=(.*?)\" target=\"_blank\">").getColumn(0);
+        /*
+         * Captcha if
+         * (br.containsHTML("<input type='hidden' name='captcha_encode'")) {
+         * String captchaCode =
+         * br.getRegex("name='captcha_encode' value='(.*?)'><img").getMatch(0);
+         * File file = this.getLocalCaptchaFile(this); Form form =
+         * br.getForm(0); Browser.download(file,
+         * br.cloneBrowser().openGetConnection
+         * ("http://link-share.org/captcha.php?captcha_code=" + captchaCode));
+         * String capTxt = Plugin.getCaptchaCode(file, this, param);
+         * form.put("captcha_code", capTxt); br.submitForm(form);
+         * 
+         * if (br.containsHTML("<input type='hidden' name='captcha_encode'")) {
+         * throw new DecrypterException(DecrypterException.CAPTCHA); } }
+         */
 
-        
-        progress.setRange(links.length);
-        for (String element : links) {
-            String encodedLink = Encoding.htmlDecode(new Regex(br.getPage("http://link-share.org/show.php?url=" + element), "<iframe src=\"(.*?)\" allow").getMatch(0));
-            
-            decryptedLinks.add(createDownloadlink(encodedLink));
-            progress.increase(1);
-        }*/
+        /*
+         * if (br.containsHTML("<image src=\"images/dlc.gif\">")) { URL url;
+         * String containerId = new Regex(parameter,
+         * "\\?url=([a-zA-Z0-9]{32})").getMatch(0); url = new
+         * URL("http://www.link-share.org/container/download.php?id=" +
+         * containerId + ".dlc"); File container =
+         * JDUtilities.getResourceFile("container/" + System.currentTimeMillis()
+         * + ".dlc"); HTTPConnection dlc_con = new
+         * HTTPConnection(url.openConnection()); Browser.download(container,
+         * dlc_con);
+         * decryptedLinks.addAll(JDUtilities.getController().getContainerLinks
+         * (container)); container.delete(); } else if
+         * (br.containsHTML("<image src=\"images/ccf.gif\">")) { URL url; String
+         * containerId = new Regex(parameter,
+         * "\\?url=([a-zA-Z0-9]{32})").getMatch(0); url = new
+         * URL("http://www.link-share.org/container/download.php?id=" +
+         * containerId + ".ccf"); File container =
+         * JDUtilities.getResourceFile("container/" + System.currentTimeMillis()
+         * + ".dlc"); HTTPConnection ccf_con = new
+         * HTTPConnection(url.openConnection()); Browser.download(container,
+         * ccf_con);
+         * decryptedLinks.addAll(JDUtilities.getController().getContainerLinks
+         * (container)); container.delete(); } else if
+         * (br.containsHTML("<image src=\"images/rsdf.gif\">")) { URL url;
+         * String containerId = new Regex(parameter,
+         * "\\?url=([a-zA-Z0-9]{32})").getMatch(0); url = new
+         * URL("http://www.link-share.org/container/download.php?id=" +
+         * containerId + ".rsdf"); File container =
+         * JDUtilities.getResourceFile("container/" + System.currentTimeMillis()
+         * + ".dlc"); HTTPConnection rsdf_con = new
+         * HTTPConnection(url.openConnection()); Browser.download(container,
+         * rsdf_con);
+         * decryptedLinks.addAll(JDUtilities.getController().getContainerLinks
+         * (container)); container.delete(); }
+         */
+
+        /*
+         * String links[] =
+         * br.getRegex("<a href=\"show.php\\?url=(.*?)\" target=\"_blank\">"
+         * ).getColumn(0);
+         * 
+         * 
+         * progress.setRange(links.length); for (String element : links) {
+         * String encodedLink = Encoding.htmlDecode(new
+         * Regex(br.getPage("http://link-share.org/show.php?url=" + element),
+         * "<iframe src=\"(.*?)\" allow").getMatch(0));
+         * 
+         * decryptedLinks.add(createDownloadlink(encodedLink));
+         * progress.increase(1); }
+         */
         return decryptedLinks;
     }
 
