18bbc241eb0bea290fa0e4473cef81661f82f355
gocsp <gocsp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-11 16:31:16 +0000

    mac os quit handler
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3299 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/utils/MacOSQuitController.java
@@ -0,0 +1,23 @@
+package jd.utils;
+
+import jd.controlling.JDController;
+
+import com.apple.eawt.Application;
+import com.apple.eawt.ApplicationAdapter;
+import com.apple.eawt.ApplicationEvent;
+
+public class MacOSQuitController extends Application {
+
+	
+	public MacOSQuitController() {
+		addApplicationListener(new QuitHandler());
+	}
+	
+	class QuitHandler extends ApplicationAdapter {
+		public void handleQuit(ApplicationEvent e) {
+			JDController controller = new JDController();
+			System.out.println("BLAAA");
+			controller.exit();			
+		}	
+	}
+}
