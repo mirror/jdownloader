c145fe12eb4da7e8e1e0535df67a95645e53435d
tokam_one <tokam_one@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-12 23:23:55 +0000

    Sexuria.com -> Code-style verbessert (Patterns als static rausgezogen) -> Versucht Bug zu beheben, beschreibung als TODO am Anfang der Datei!
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3316 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/Sexuriacom.java
+++ src/jd/plugins/decrypt/Sexuriacom.java
@@ -14,11 +14,23 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/*
+ * TODO bug fixen: 
+ * Bei diesem Link geht das decryptete Passwort intern unter. 
+ * Vermutlich da auf die Redirecturl kein Pattern passt und kein Plugin arbeitet.
+ * Habe schon in der DistributeData nach ner Lösung geschaut, aber keine Gefunden,
+ * es steht ja dort in Zeile 250 und 251:
+ * dLinks.get(c).addSourcePluginPasswords(foundpassword);
+ * dLinks.get(c).addSourcePluginPasswords(decrypted.getSourcePluginPasswords());
+ * Hier der Link mit dem Bug:
+ * http://sexuria.com/Pornos_Kostenlos_Crimson-Mansion-4-The-Catacombs_28460.html
+ * Das Sexuria plugin scheint zu funktionieren, das habe ich mit Debug Ausgaben überprüft!
+ */
+
 package jd.plugins.decrypt;
 
 import java.util.ArrayList;
 import java.util.regex.Pattern;
-
 import jd.PluginWrapper;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
@@ -27,10 +39,14 @@ import jd.plugins.PluginForDecrypt;
 
 public class Sexuriacom extends PluginForDecrypt {
 
-    private static final Pattern PATTEREN_SUPPORTED_MAIN = Pattern.compile("http://[\\w\\.]*?sexuria\\.com/Pornos_Kostenlos_.+?_(\\d+)\\.html", Pattern.CASE_INSENSITIVE);
-    private static final Pattern PATTERN_SUPPORTED_CRYPT = Pattern.compile("http://[\\w\\.]*?sexuria\\.com/dl_links_\\d+_(\\d+)\\.html", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTEREN_SUPPORTED_MAIN    = Pattern.compile("http://[\\w\\.]*?sexuria\\.com/Pornos_Kostenlos_.+?_(\\d+)\\.html", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTERN_SUPPORTED_CRYPT    = Pattern.compile("http://[\\w\\.]*?sexuria\\.com/dl_links_\\d+_(\\d+)\\.html", Pattern.CASE_INSENSITIVE);
     private static final Pattern PATTERN_SUPPORTED_REDIRECT = Pattern.compile("http://[\\w\\.]*?sexuria\\.com/out.php\\?id=([0-9]+)\\&part=[0-9]+\\&link=[0-9]+", Pattern.CASE_INSENSITIVE);
-
+    
+    private static final Pattern PATTERN_PASSWORD           = Pattern.compile("<strong>Passwort: </strong></div></td>.*?bgcolor=\"#EFEFEF\">(.*?)</td>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+    private static final Pattern PATTERN_DL_LINK_PAGE       = Pattern.compile("href=\"dl_links_(.*?)\" target=\"_blank\">", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTERN_REDIRECT_LINKS     = Pattern.compile("value=\"(http://sexuria\\.com/out\\.php\\?id=\\d+\\&part=\\d+\\&link=\\d+)\" readonly", Pattern.CASE_INSENSITIVE);
+    
     public Sexuriacom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -46,22 +62,22 @@ public class Sexuriacom extends PluginForDecrypt {
 
         if (new Regex(parameter, PATTEREN_SUPPORTED_MAIN).matches()) {
             String page = br.getPage(parameter);
-            String Links[] = new Regex(page, "href=\"dl_links_(.*?)\" target=\"_blank\">", Pattern.CASE_INSENSITIVE).getColumn(0);
-            for (String link : Links) {
+            String links[] = new Regex(page, PATTERN_DL_LINK_PAGE).getColumn(0);
+            for (String link : links) {
                 decryptedLinks.add(createDownloadlink("http://sexuria.com/dl_links_" + link));
             }
             return decryptedLinks;
         } else if (new Regex(parameter, PATTERN_SUPPORTED_CRYPT).matches()) {
             downloadId = new Regex(parameter, PATTERN_SUPPORTED_CRYPT).getMatch(0);
             String page = br.getPage("http://sexuria.com/Pornos_Kostenlos_info_" + downloadId + ".html");
-            password = new Regex(page, "<strong>Passwort: </strong></div></td>.*?bgcolor=\"#EFEFEF\">(.*?)</td>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL).getMatch(0);
+            password = new Regex(page, PATTERN_PASSWORD).getMatch(0);
             page = br.getPage(parameter);
-            String Links[] = new Regex(page, "value=\"(http://sexuria\\.com/out\\.php\\?id=\\d+\\&part=\\d+\\&link=\\d+)\" readonly", Pattern.CASE_INSENSITIVE).getColumn(0);
-            for (String Link : Links) {
-                br.getPage(Link);
-                DownloadLink dl_link = createDownloadlink(br.getRedirectLocation());
-                dl_link.addSourcePluginPassword(password);
-                decryptedLinks.add(dl_link);
+            String links[] = new Regex(page, PATTERN_REDIRECT_LINKS).getColumn(0);
+            for (String link : links) {
+                br.getPage(link);
+                DownloadLink dlLink = createDownloadlink(br.getRedirectLocation());
+                dlLink.addSourcePluginPassword(password);
+                decryptedLinks.add(dlLink);
             }
             return decryptedLinks;
         } else if (new Regex(parameter, PATTERN_SUPPORTED_REDIRECT).matches()) {
