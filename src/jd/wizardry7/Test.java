9a4e55e1c68d933c1d207975fa4b5213b184c919
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-16 18:00:41 +0000

    -Bye bye Wizard
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3402 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/wizardry7/Test.java
+++
@@ -1,8 +0,0 @@
-package jd.wizardry7;
-
-public class Test {
-
-    public static void main(String[] args) {
-        MVCWizard.getInstance().getWizardFrame(true, MVCWizard.SETTINGS_WIZARD).setVisible(true);
-    }
-}
