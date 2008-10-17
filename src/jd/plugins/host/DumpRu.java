b0f95d0ab661a5c0c8ee5c656c73691f940531bd
Viperb0y <Viperb0y@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-17 12:57:49 +0000

    Created DumpRu.java  * New Hoster
    Updated JDInit.java
     * Added DumpRu to the list
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3415 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/host/DumpRu.java
@@ -0,0 +1,95 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.host;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.download.RAFDownload;
+
+public class DumpRu extends PluginForHost {
+
+    public DumpRu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://dump.ru/pages/about/";
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) {
+        try {
+            br.setCookiesExclusive(true);
+            br.clearCookies(getHost());
+
+            String url = downloadLink.getDownloadURL();
+
+            br.getPage(url);
+            
+            // File not found
+            if (br.containsHTML("Запрошенный файл не обнаружен"))
+            {
+                logger.warning("File not found");
+                return false;
+            }
+            
+            // Filesize
+            String size = br.getRegex("Размер: <span class=\"comment\">(.*?)</span>").getMatch(0);
+            downloadLink.setDownloadSize(Regex.getSize(size.replaceAll("Кб", "KB").replaceAll("Mб", "MB")));
+            
+            // Filename
+            String name = br.getRegex("<span class=\"name_of_file\">(.*?)</span>").getMatch(0).trim();
+            downloadLink.setName(name);
+            
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return getVersion("$Revision: 3397 $");
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        if (!getFileInformation(downloadLink)) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        dl = RAFDownload.download(downloadLink, br.createFormRequest(br.getForm(1)));
+        dl.startDownload();
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        /* TODO: Wert nachprüfen */
+        return 1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+}
