abda0799a0c553ae9e15b3452f3dc6e10e53bb22
Viperb0y <Viperb0y@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-23 15:59:28 +0000

    Created OutlinkrCom.java  * New Decrypter
    Updated JDInit.java
     * Added OutlinkrCom to the list
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3517 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInit.java
+++ src/jd/JDInit.java
@@ -601,6 +601,7 @@ public class JDInit {
         new DecryptPluginWrapper("myvideo.de", "MyvideoDe", "http://[\\w\\.]*?myvideo\\.de/watch/[0-9]+/");
         new DecryptPluginWrapper("netfolder.in", "NetfolderIn", "http://[\\w\\.]*?netfolder\\.in/folder\\.php\\?folder_id\\=[a-zA-Z0-9]{7}|http://[\\w\\.]*?netfolder\\.in/[a-zA-Z0-9]{7}/.*?");
         new DecryptPluginWrapper("newzfind.com", "NewzFindCom", "http://[\\w\\.]*?newzfind\\.com/(video|music|games|software|mac|graphics|unix|magazines|e-books|xxx|other)/.+");
+        new DecryptPluginWrapper("outlinkr.com", "OutlinkrCom", "http://[\\w\\.]*?outlinkr\\.com/(files|cluster)/[0-9]+/.+");
         new DecryptPluginWrapper("Protect.Tehparadox.com", "ProtectTehparadoxcom", "http://[\\w\\.]*?protect\\.tehparadox\\.com\\/[a-zA-Z0-9]+\\!");
         new DecryptPluginWrapper("raidrush.org", "RaidrushOrg", "http://[\\w\\.]*?raidrush\\.org/ext/\\?fid\\=[a-zA-Z0-9]+");
         new DecryptPluginWrapper("rapidfolder.com", "RapidFolderCom", "http://[\\w\\.]*?rapidfolder\\.com/\\?\\w+");
---
+++ src/jd/plugins/decrypt/OutlinkrCom.java
@@ -0,0 +1,62 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team jdownloader@freenet.de
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class OutlinkrCom extends PluginForDecrypt {
+
+    public OutlinkrCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+
+        if (parameter.contains("outlinkr.com/files/"))
+        {
+            br.getPage(parameter);
+            String link = br.getRegex("Target : <a href=\"(.*?)\">").getMatch(0);
+            decryptedLinks.add(createDownloadlink(link));
+        } else {
+            br.getPage(parameter);
+            String[] links = br.getRegex("<a target=\"_blank\" href=(.*?)>").getColumn(0);
+            
+            progress.setRange(links.length);
+            for (String element : links)
+            {
+                decryptedLinks.add(createDownloadlink(element));
+                progress.increase(1);
+            }
+        }
+
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getVersion() {
+        return getVersion("$Revision$");
+    }
+}
