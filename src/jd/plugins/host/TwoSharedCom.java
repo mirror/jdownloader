e1f811d11b04241763a4984011fee842ecd6bdf5
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-10-25 18:04:28 +0000

    TwoSharedCom Hoster Password support added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@3532 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/TwoSharedCom.java
+++ src/jd/plugins/host/TwoSharedCom.java
@@ -20,9 +20,11 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
+import jd.parser.Form;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
@@ -41,6 +43,13 @@ public class TwoSharedCom extends PluginForHost {
     public boolean getFileInformation(DownloadLink downloadLink) throws PluginException, IOException {
         br.setCookiesExclusive(true);
         br.getPage(downloadLink.getDownloadURL());
+        Form pwform = br.getForm(0);
+        if (pwform != null) {
+            String filename = br.getRegex("<td class=\"header\" align=\"center\">Download (.*?)</td>").getMatch(0);
+            if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            downloadLink.setName(filename.trim());
+            return true;
+        }
         String filesize = br.getRegex(Pattern.compile("<span class=.*?>File size:</span>(.*?)&nbsp; &nbsp;", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
         String filename = br.getRegex("<title>2shared - download(.*?)</title>").getMatch(0);
         if (filesize == null || filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
@@ -59,6 +68,23 @@ public class TwoSharedCom extends PluginForHost {
         /* Nochmals das File überprüfen */
         getFileInformation(downloadLink);
         br.setFollowRedirects(true);
+        Form pwform = br.getForm(0);
+        if (pwform != null) {
+            String passCode;
+            if (downloadLink.getStringProperty("pass", null) == null) {
+                passCode = Plugin.getUserInput(null, downloadLink);
+            } else {
+                passCode = downloadLink.getStringProperty("pass", null);
+            }
+            pwform.put("userPass2", passCode);
+            br.submitForm(pwform);
+            if (br.containsHTML("passError\\(\\);")) {
+                downloadLink.setProperty("pass", null);
+                throw new PluginException(LinkStatus.ERROR_CAPTCHA, "PW wrong");
+            } else {
+                downloadLink.setProperty("pass", passCode);
+            }
+        }
         String link = br.getRegex(Pattern.compile("function startDownload.*?window.location = \"(.*?)\"", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
         if (link == null) throw new PluginException(LinkStatus.ERROR_FATAL);
         dl = br.openDownload(downloadLink, link);
