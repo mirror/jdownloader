1376940e02c2cd152eb0de2114f8d3736f464763
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-12-17 01:49:15 +0000

    ShareOnlineBiz: hoster, PremiumSupport added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4056 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInit.java
+++ src/jd/JDInit.java
@@ -759,7 +759,7 @@ public class JDInit {
         new HostPluginWrapper("RomHustler.Net", "RomHustlerNet", "http://[\\w.]*?romhustler\\.net/download/.*?/\\d+");
         new HostPluginWrapper("SharedZilla.com", "SharedZillacom", "http://[\\w\\.]*?sharedzilla\\.com/(en|ru)/get\\?id=\\d+");
         new HostPluginWrapper("Share-Now.net", "ShareNownet", "http://[\\w\\.]*?share-now\\.net/{1,}files/\\d+-(.*?)\\.html");
-        new HostPluginWrapper("Share-Online.Biz", "ShareOnlineBiz", "http://[\\w\\.]*?share\\-online\\.biz/download.php\\?id\\=[a-zA-Z0-9]+");
+        new HostPluginWrapper("Share-Online.Biz", "ShareOnlineBiz", "http://[\\w\\.]*?share\\-online\\.biz/download.php\\?id\\=[a-zA-Z0-9]+", PluginWrapper.LOAD_ON_INIT);
         new HostPluginWrapper("Shareplace.com", "Shareplacecom", "http://[\\w\\.]*?shareplace\\.com/\\?[a-zA-Z0-9]+/.*?");
         new HostPluginWrapper("Shareplace.com", "Shareplacecom", "http://[\\w\\.]*?datei\\.in/\\?[a-zA-Z0-9]+/.*?");
         new HostPluginWrapper("shragle.com", "ShragleCom", "http://[\\w\\.]*?shragle\\.(com|de)/files/[a-zA-Z0-9]+/.*", PluginWrapper.LOAD_ON_INIT);
--- src/jd/plugins/host/ShareOnlineBiz.java
+++ src/jd/plugins/host/ShareOnlineBiz.java
@@ -17,12 +17,16 @@
 package jd.plugins.host;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
 import jd.http.Browser;
+import jd.http.Encoding;
 import jd.parser.Form;
 import jd.parser.Regex;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
@@ -39,11 +43,60 @@ public class ShareOnlineBiz extends PluginForHost {
 
     public ShareOnlineBiz(PluginWrapper wrapper) {
         super(wrapper);
+        this.enablePremium("http://www.share-online.biz/service.php");
     }
 
     @Override
     public String getAGBLink() {
         return "http://share-online.biz/rules.php";
+    }
+
+    public void login(Account account) throws IOException, PluginException {
+        br.postPage("http://www.share-online.biz/login.php", "act=login&location=service.php&dieseid=&user=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()) + "&login=Log+me+in&folder_autologin=1");
+        String cookie = br.getCookie("http://www.share-online.biz", "king_passhash");
+        if (cookie == null) {
+            account.setEnabled(false);
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, LinkStatus.VALUE_ID_PREMIUM_DISABLE);
+        }
+        br.getPage("http://www.share-online.biz/members.php");
+        String expired = br.getRegex(Pattern.compile("<b>Expired\\?</b></td>.*?<td align=\"left\">(.*?)<a", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
+        if (expired == null || !expired.trim().equalsIgnoreCase("no")) {
+            account.setEnabled(false);
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, LinkStatus.VALUE_ID_PREMIUM_DISABLE);
+        }
+    }
+
+    public boolean isPremium() throws IOException {
+        if (br.getURL() == null || !br.getURL().equalsIgnoreCase("http://www.share-online.biz/members.php")) {
+            br.getPage("http://www.share-online.biz/members.php");
+        }
+        if (br.containsHTML("<b>Premium account</b>")) { return true; }
+        return false;
+    }
+
+    @Override
+    public AccountInfo getAccountInformation(Account account) throws Exception {
+        AccountInfo ai = new AccountInfo(this, account);
+        setBrowserExclusive();
+        try {
+            login(account);
+        } catch (PluginException e) {
+            ai.setValid(false);
+            return ai;
+        }
+        if (!isPremium()) {
+            ai.setValid(false);
+            ai.setStatus("No Premium Account!");
+            return ai;
+        }
+        br.getPage("http://www.share-online.biz/members.php");
+        String points = br.getRegex(Pattern.compile("<b>Total Points:</b></td>.*?<td align=\"left\">(.*?)</td>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
+        if (points != null) ai.setPremiumPoints(points);
+        String expire = br.getRegex(Pattern.compile("<b>Package Expire Date:</b></td>.*?<td align=\"left\">(.*?)</td>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
+        ai.setValidUntil(Regex.getMilliSeconds(expire, "dd/MM/yy", null));
+        ai.setTrafficLeft(-1);
+        ai.setValid(true);
+        return ai;
     }
 
     @Override
@@ -77,6 +130,42 @@ public class ShareOnlineBiz extends PluginForHost {
     public String getVersion() {
 
         return getVersion("$Revision$");
+    }
+
+    public void handlePremium(DownloadLink parameter, Account account) throws Exception {
+        DownloadLink downloadLink = (DownloadLink) parameter;
+        LinkStatus linkStatus = downloadLink.getLinkStatus();
+        getFileInformation(parameter);
+        login(account);
+        if (!this.isPremium()) { throw new PluginException(LinkStatus.ERROR_PREMIUM, LinkStatus.VALUE_ID_PREMIUM_DISABLE); }
+        br.getPage(downloadLink.getDownloadURL());
+        Form form = br.getForm(1);
+        if (form.containsHTML("name=downloadpw")) {
+            if (downloadLink.getStringProperty("pass", null) == null) {
+                passCode = Plugin.getUserInput(null, downloadLink);
+            } else {
+                /* gespeicherten PassCode holen */
+                passCode = downloadLink.getStringProperty("pass", null);
+            }
+            form.put("downloadpw", passCode);
+            br.submitForm(form);
+            if (br.containsHTML("Unfortunately the password you entered is not correct")) {
+                /* PassCode war falsch, also Löschen */
+                downloadLink.setProperty("pass", null);
+                linkStatus.addStatus(LinkStatus.ERROR_CAPTCHA);
+                return;
+            }
+
+            /* PassCode war richtig, also Speichern */
+            downloadLink.setProperty("pass", passCode);
+        }
+
+        url = br.getRegex("loadfilelink\\.decode\\(\"(.*?)\"\\);").getMatch(0);
+
+        br.setFollowRedirects(true);
+        /* Datei herunterladen */
+        dl = br.openDownload(downloadLink, url, true, 1);
+        dl.startDownload();
     }
 
     @Override
