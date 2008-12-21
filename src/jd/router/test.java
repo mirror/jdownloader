2c4364883a4f7b6ab1430ed4c8d1d4dfac85fb70
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2008-12-21 17:16:04 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4077 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/router/test.java
+++ src/jd/router/test.java
@@ -3,14 +3,36 @@ package jd.router;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import jd.utils.JDUtilities;
 
 import jd.http.Browser;
 
 public class test {
-
+    public static Map revSortByValue(Map map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getKey()).compareTo(((Map.Entry) (o2)).getKey());
+            }
+        });
+        // logger.info(list);
+        Map result = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
     /**
      * @param args
      */
@@ -25,21 +47,15 @@ public class test {
         try {
             String st = br.postPage("http://service.jdownloader.net/routerdb/getRouters.php", he);
             ArrayList<RInfo> ra = (ArrayList<RInfo>) JDUtilities.xmlStringToObjekt(st);
-            RInfo best = null;
-            int bestval = Integer.MAX_VALUE;
+            HashMap<Integer, RInfo> routers = new HashMap<Integer, RInfo>();
             for (RInfo info : ra) {
-                int b = info.compare(infos);
-                if(b<bestval)
-                {
-                    bestval=b;
-                    best=info;
-                }
+                Integer b = info.compare(infos);
+                routers.put(b, info);
             }
-            if(best!=null)
-            {
-                System.out.println(bestval);
-                System.out.println(best.getRouterName());
-                System.out.println(best.getReconnectMethode());
+            routers=(HashMap<Integer, RInfo>) revSortByValue(routers);
+
+             for (Entry<Integer, RInfo> rfo : routers.entrySet()) {
+                System.out.println(rfo.getKey()+":"+rfo.getValue().getRouterName());
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
