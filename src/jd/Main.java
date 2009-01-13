4f090b672efdf869b58fd91f52ef66bf70dcba6b
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-01-13 01:10:38 +0000

    Email Aktuallisiert
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4227 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Main.java
+++ src/jd/Main.java
@@ -1,5 +1,5 @@
 //    jDownloader - Downloadmanager
-//    Copyright (C) 2008  JD-Team jdownloader@freenet.de  http://jdownloader.org
+//    Copyright (C) 2008  JD-Team support@jdownloader.org  http://jdownloader.org
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
