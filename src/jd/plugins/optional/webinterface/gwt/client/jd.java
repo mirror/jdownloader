84acb8bd6afa07211c48127a6123b587e21719c4
botzi <botzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-02-01 21:01:36 +0000

    Fixed Smartgwt integration
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4414 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/optional/webinterface/gwt/client/jd.java
+++ src/jd/plugins/optional/webinterface/gwt/client/jd.java
@@ -3,15 +3,12 @@ package jd.plugins.optional.webinterface.gwt.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
-import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.smartgwt.client.util.*;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
 public class jd implements EntryPoint {
 
   /**
@@ -35,29 +32,9 @@ public class jd implements EntryPoint {
     // Add image and button to the RootPanel
     RootPanel.get().add(vPanel);
 
-    // Create the dialog box
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Welcome to GWT!");
-    dialogBox.setAnimationEnabled(true);
-    Button closeButton = new Button("close");
-    VerticalPanel dialogVPanel = new VerticalPanel();
-    dialogVPanel.setWidth("100%");
-    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-    dialogVPanel.add(closeButton);
-
-    closeButton.addClickListener(new ClickListener() {
-      public void onClick(Widget sender) {
-        dialogBox.hide();
-      }
-    });
-
-    // Set the contents of the Widget
-    dialogBox.setWidget(dialogVPanel);
-    
     button.addClickListener(new ClickListener() {
       public void onClick(Widget sender) {
-        dialogBox.center();
-        dialogBox.show();
+        SC.say("Test erfolgreich");
       }
     });
   }
