d99dac944adbe10d3dec6afa7738d01eec8c04fa
djuzi <djuzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-02-01 22:37:12 +0000

    NEW hoster added: Pliczek.net
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4415 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInit.java
+++ src/jd/JDInit.java
@@ -806,6 +806,7 @@ public class JDInit {
         new HostPluginWrapper("hostplik.com", "HostPlikCom", "http://[\\w\\.]*?hostplik\\.com/.*");
         new HostPluginWrapper("uploader.pl", "UploaderPl", "http://[\\w\\.]*?uploader\\.pl/\\?d=[A-F0-9]+", PluginWrapper.LOAD_ON_INIT);
         new HostPluginWrapper("upload-drive.com", "UploadDriveCom", "http://[\\w\\.]*?upload-drive\\.com/\\d+/.+");
+        new HostPluginWrapper("pliczek.net", "PliczekNet", "http://[\\w\\.]*?pliczek\\.net/index\\.php\\?p=\\d+");
 
     }
 
---
+++ src/jd/plugins/host/PliczekNet.java
@@ -0,0 +1,83 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.host;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import jd.PluginWrapper;
+import jd.http.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+public class PliczekNet extends PluginForHost {
+
+
+    public PliczekNet(PluginWrapper wrapper) {
+        super(wrapper);
+        this.setStartIntervall(5000l);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.pliczek.net/index.php?p=terms";
+    }
+
+    @Override
+    public boolean getFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
+        this.setBrowserExclusive();
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("Not Found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = Encoding.htmlDecode(br.getRegex(Pattern.compile("Nazwa pliku: <strong>(.*?)</strong>", Pattern.CASE_INSENSITIVE)).getMatch(0));
+        String filesize = br.getRegex("Wielko.. (.*?)<br>").getMatch(0);
+        if (filename == null || filesize == null ) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        downloadLink.setName(filename.trim());
+        downloadLink.setDownloadSize(Regex.getSize(filesize.replaceAll(",", "\\.")));
+        return true;
+    }
+
+    @Override
+    public String getVersion() {
+        return getVersion("$Revision: 4390 $");
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        getFileInformation(downloadLink);
+        String linkurl =  br.getRegex("<script[^>]*></script></div>\\s*<a href=\"(.*?)\"><img").getMatch(0);
+        if (linkurl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        br.setFollowRedirects(true);
+        dl = br.openDownload(downloadLink, linkurl);
+        dl.startDownload();
+        
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        return 10;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+}
