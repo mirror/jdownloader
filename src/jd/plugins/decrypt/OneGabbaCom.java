31c9c6259459a96814c6b601822413c6ce359912
gluewurm <gluewurm@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-02-16 10:25:39 +0000

    1GabbaCom Decrypter added
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4601 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/OneGabbaCom.java
@@ -0,0 +1,53 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class OneGabbaCom extends PluginForDecrypt {
+    public OneGabbaCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setCookiesExclusive(true);
+        br.getPage(parameter);
+        
+        String links[][] = br.getRegex("<pre>(.*?)</pre>").getMatches();
+        if(links.length > 0) {
+	        for(int i=0; i< links.length; i++) decryptedLinks.add(createDownloadlink(links[i][0]));
+        } else {
+        	throw new DecrypterException("Keine Links gefunden");
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getVersion() {
+        return getVersion("$Revision: 4565 $");
+    }
+}
