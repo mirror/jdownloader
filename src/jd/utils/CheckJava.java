ebf14ce4f4d5340037f56110734d1899379988d1
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-02-17 00:02:28 +0000

    - should fix bug https://www.syncom.org/bugzilla/jdownloader/show_bug.cgi?id=93
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4616 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/utils/CheckJava.java
+++ src/jd/utils/CheckJava.java
@@ -27,7 +27,7 @@ public class CheckJava {
         if (runtimeName == null || runtimeVersion == null) return false;
         runtimeName = runtimeName.toLowerCase();
         runtimeVersion = runtimeVersion.toLowerCase();
-        if (!new Regex(runtimeVersion, "1\\.(5|6)").matches()) {
+        if (!new Regex(runtimeVersion, "1\\.(5|6|7)").matches()) {
             String html = JDLocale.LF("gui.javacheck.html", "<link href='http://jdownloader.org/jdcss.css' rel='stylesheet' type='text/css' /><div style='width:534px;height;200px'><h2>You useses a wrong Java version. Please use a original Sun Java. Start jDownloader anyway?<table width='100%%'><tr><th colspan='2'>Your Java Version:</th></tr><tr><th>Runtime Name</th><td>%s</td></tr><tr><th>Runtime Version</th><td>%s</td></tr></table></div>", runtimeName, runtimeVersion);
             return HTMLDialog.showDialog(null, JDLocale.L("gui.javacheck.title", "Wrong Java Version"), html);
         }
