209cbb911db7b68f7cec45e533348a184a96112f
Jiaz <Jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-02-18 20:48:34 +0000

    CineTo: decrypter, fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4657 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypt/CineTo.java
+++ src/jd/plugins/decrypt/CineTo.java
@@ -16,108 +16,56 @@
 
 package jd.plugins.decrypt;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DownloadLink;
+import jd.plugins.Plugin;
 import jd.plugins.PluginForDecrypt;
 
 public class CineTo extends PluginForDecrypt {
 
     private static final String patternLink_Protected = "http://[\\w\\.]*?cine\\.to/index\\.php\\?do=protect\\&id=[a-zA-Z0-9]+|http://[\\w\\.]*?cine\\.to/index\\.php\\?do=protect\\&id=[a-zA-Z0-9]+|http://[\\w\\.]*?cine\\.to/pre/index\\.php\\?do=protect\\&id=[a-zA-Z0-9]+";
+    static private Integer lock = 0;
 
     public CineTo(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         String parameter = param.toString();
-
-        br.getPage(parameter);
-        if (parameter.matches(patternLink_Protected)) {
-            String[] captcha = br.getRegex("span class=\"([ws]?)\"").getColumn(0);
-            String capText = "";
-            if (captcha.length == 80) {
-                for (int j = 1; j < 5; j++) {
-                    capText = capText + extractCaptcha(captcha, j);
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        synchronized (lock) {
+            br.getPage(parameter);
+            if (parameter.matches(patternLink_Protected)) {
+                File file = this.getLocalCaptchaFile(this);
+                URLConnectionAdapter con = br.openGetConnection("http://cine.to/securimage_show.php");
+                Browser.download(file, con);
+                String code = Plugin.getCaptchaCode(file, this, param);
+                br.postPage(param.toString(), "captcha=" + code + "&submit=Senden");
+                if (br.containsHTML("Code ist falsch")) throw new DecrypterException(DecrypterException.CAPTCHA);
+                String[] links = br.getRegex("window\\.open\\('(.*?)'").getColumn(0);
+                progress.setRange(links.length);
+                for (String element : links) {
+                    DownloadLink link = createDownloadlink(element);
+                    link.addSourcePluginPassword("cine.to");
+                    decryptedLinks.add(link);
+                    progress.increase(1);
+                }
+            } else {
+                String[] mirrors = br.getRegex("href=\"(index\\.php\\?do=protect\\&id=[\\w]+)\"").getColumn(0);
+                for (String element : mirrors) {
+                    decryptedLinks.add(createDownloadlink("http://cine.to/" + element));
                 }
             }
-            br.postPage(parameter, "captcha=" + capText + "&submit=Senden");
-            String[] links = br.getRegex("window\\.open\\('(.*?)'").getColumn(0);
-            progress.setRange(links.length);
-            for (String element : links) {
-                DownloadLink link = createDownloadlink(element);
-                link.addSourcePluginPassword("cine.to");
-                decryptedLinks.add(link);
-                progress.increase(1);
-            }
-        } else {
-            String[] mirrors = br.getRegex("href=\"(index\\.php\\?do=protect\\&id=[\\w]+)\"").getColumn(0);
-            for (String element : mirrors) {
-                decryptedLinks.add(createDownloadlink("http://cine.to/" + element));
-            }
+            return decryptedLinks;
         }
-        return decryptedLinks;
-    }
-
-    private String extractCaptcha(String[] source, int captchanumber) {
-        String[] erg = new String[15];
-
-        erg[0] = source[captchanumber * 4 - 4];
-        erg[1] = source[captchanumber * 4 - 3];
-        erg[2] = source[captchanumber * 4 - 2];
-
-        erg[3] = source[captchanumber * 4 + 12];
-        erg[4] = source[captchanumber * 4 + 13];
-        erg[5] = source[captchanumber * 4 + 14];
-
-        erg[6] = source[captchanumber * 4 + 28];
-        erg[7] = source[captchanumber * 4 + 29];
-        erg[8] = source[captchanumber * 4 + 30];
-
-        erg[9] = source[captchanumber * 4 + 44];
-        erg[10] = source[captchanumber * 4 + 45];
-        erg[11] = source[captchanumber * 4 + 46];
-
-        erg[12] = source[captchanumber * 4 + 60];
-        erg[13] = source[captchanumber * 4 + 61];
-        erg[14] = source[captchanumber * 4 + 62];
-
-        String[] wert0 = { "s", "s", "s", "s", "w", "s", "s", "w", "s", "s", "s", "w", "s", "s", "s" };
-        if (Arrays.equals(erg, wert0)) { return "0"; }
-
-        String[] wert1 = { "w", "w", "s", "w", "s", "s", "w", "w", "s", "w", "w", "s", "w", "w", "s" };
-        if (Arrays.equals(erg, wert1)) { return "1"; }
-
-        String[] wert2 = { "s", "s", "s", "w", "w", "s", "w", "s", "s", "s", "w", "w", "s", "s", "s" };
-        if (Arrays.equals(erg, wert2)) { return "2"; }
-
-        String[] wert3 = { "s", "s", "s", "w", "w", "s", "w", "s", "s", "w", "w", "s", "s", "s", "s" };
-        if (Arrays.equals(erg, wert3)) { return "3"; }
-
-        String[] wert4 = { "s", "w", "w", "s", "w", "s", "s", "s", "s", "w", "w", "s", "w", "w", "s" };
-        if (Arrays.equals(erg, wert4)) { return "4"; }
-
-        String[] wert5 = { "s", "s", "s", "s", "w", "w", "s", "s", "s", "w", "w", "s", "s", "s", "s" };
-        if (Arrays.equals(erg, wert5)) { return "5"; }
-
-        String[] wert6 = { "s", "s", "s", "s", "w", "w", "s", "s", "s", "s", "w", "s", "s", "s", "s" };
-        if (Arrays.equals(erg, wert6)) { return "6"; }
-
-        String[] wert7 = { "s", "s", "s", "w", "w", "s", "w", "s", "s", "w", "w", "s", "w", "w", "s" };
-        if (Arrays.equals(erg, wert7)) { return "7"; }
-
-        String[] wert8 = { "s", "s", "s", "s", "w", "s", "s", "s", "s", "s", "w", "s", "s", "s", "s" };
-        if (Arrays.equals(erg, wert8)) { return "8"; }
-
-        String[] wert9 = { "s", "s", "s", "s", "w", "s", "s", "s", "s", "w", "w", "s", "s", "s", "s" };
-        if (Arrays.equals(erg, wert9)) { return "9"; }
-
-        return "0";
     }
 
     @Override
