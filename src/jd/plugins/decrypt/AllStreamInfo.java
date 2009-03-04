226b062bae48429d8925c3e57857347d5ea739ba
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-03-04 22:39:24 +0000

    Decrypter added AllStreamInfo
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@4845 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypt/AllStreamInfo.java
@@ -0,0 +1,46 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypt;
+import java.util.ArrayList;
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+public class AllStreamInfo extends PluginForDecrypt {
+
+    public AllStreamInfo(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        br.getPage(param.toString());
+        String[] links = br.getRegex("<input onClick=\"window.open\\('(.*?)'").getColumn(0);
+        for (String string : links) {
+            decryptedLinks.add(createDownloadlink(string));
+        }
+        return decryptedLinks;
+    }
+
+    @Override
+    public String getVersion() {
+        return getVersion("$Revision: 4839 $");
+    }
+}
