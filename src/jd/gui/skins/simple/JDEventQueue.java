277dab6d7aa30ca38a94840ff43d612d29fec088
jago <jago@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-03-18 23:45:26 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@5003 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/skins/simple/JDEventQueue.java
+++ src/jd/gui/skins/simple/JDEventQueue.java
@@ -41,10 +41,6 @@ import jd.utils.JDTheme;
 public class JDEventQueue extends EventQueue {
     public JDEventQueue() {
         super();
-        // comment out in production code
-        // JDUtilities.getLogger().fine("Enter " +
-        // JDEventQueue.class.getSimpleName());
-        // eventQueueInteruptionTest();
     }
 
     abstract class MenuAbstractAction extends AbstractAction {
@@ -148,73 +144,4 @@ public class JDEventQueue extends EventQueue {
         menu.show(t, pt.x, pt.y);
     }
 
-    // /**
-    // * Since we are using our own EventQueue, lengthy tasks could lead to
-    // * interruptions which may freeze the GUI or crash the program. This
-    // * function tests how severe the impact is when executed in the current
-    // * thread or a seperate thread.
-    // *
-    // * Do not execute this function in production code. This is only a test.
-    // */
-    // private static void eventQueueInteruptionTest() {
-    // Runnable runnable = new Runnable() {
-    // public void run() {
-    // // long task, like calculating something.
-    // int sum = 0;
-    // byte[] test = new byte[] { 104, 116, 116, 112, 58, 47, 47, 119, 119, 119,
-    // 46, 121, 111, 117, 116, 117, 98, 101, 46, 99, 111, 109, 47, 119, 97, 116,
-    // 99, 104, 63, 118, 61, 75, 82, 95, 72, 85, 56, 69, 122, 97, 79, 48, };
-    // for (byte b : test) {
-    // sum += b;
-    // }
-    // String result = new String(sum + "");
-    // result = new String(test);
-    // // JDUtilities.getLogger().fine(result);
-    //
-    // // task of unpredictable length, like downloading a file
-    // unpredictableLengthTask(result);
-    // }
-    //
-    // };
-    //
-    // // run in current thread
-    // // runnable.run();
-    //
-    // // run in new thread
-    // Thread thread = new Thread(runnable);
-    // thread.setPriority(Thread.MIN_PRIORITY);
-    // thread.start();
-    // }
-
-    // private static void unpredictableLengthTask(String url) {
-    // // JDUtilities.getLogger().info("Start Event Queue Interruption Test");
-    // try {
-    // for (int i = 0; i < 1; i++) {
-    // Browser br = new Browser();
-    // br.setCurrentURL(url);
-    //                
-    // br.setFollowRedirects(true);
-    // br.setCookiesExclusive(true);
-    // br.clearCookies("youtube.com");
-    //
-    // String page = br.getPage(url);
-    // // System.out.println(page);
-    // HashMap<String, HashMap<String, Cookie>> cookies = br.getCookies();
-    // // printCookies(cookies);
-    // }
-    // } catch (Exception e) {
-    // System.err.println("Error");
-    // }
-    // }
-
-    // private static void printCookies(HashMap<String, HashMap<String, Cookie>>
-    // hashMap) {
-    // StringBuilder sb = new StringBuilder();
-    // for (Entry<String, HashMap<String, Cookie>> cookie : hashMap.entrySet())
-    // {
-    // sb.append(cookie.getKey() + " -> " + cookie.getValue());
-    // sb.append("\n");
-    // }
-    // System.out.println(sb.toString());
-    // }
 }
