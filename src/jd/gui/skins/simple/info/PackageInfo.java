4f52bc58675031c1bd88931f1a08d5956a698c1f
gocsp <gocsp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-03-20 10:03:18 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@5031 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/skins/simple/info/PackageInfo.java
+++ src/jd/gui/skins/simple/info/PackageInfo.java
@@ -100,7 +100,7 @@ public class PackageInfo extends JPanel {
         
         tabbedPane.add(JDLocale.L("gui.linkgrabber.packagetab.toggleview2", "Extended"), extendedPanel);
 
-        this.setLayout(new MigLayout("insets 0 16 16 16", "[grow,right]", "[15px]"));
+        this.setLayout(new MigLayout("insets 8 16 8 16", "[grow,right]", "[8px]0px[grow]"));
         this.add(new JLabel("x"), "wrap");
         this.add(tabbedPane, "grow");
     }
