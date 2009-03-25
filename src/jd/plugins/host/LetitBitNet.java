f7b0c50ee1e442dacc54c2c49e25aee430392152
djuzi <djuzi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-03-25 10:04:00 +0000

    Letitbit FREE fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@5069 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/LetitBitNet.java
+++ src/jd/plugins/host/LetitBitNet.java
@@ -94,7 +94,7 @@ public class LetitBitNet extends PluginForHost {
             br.getPage("http://www.gur" + randomain + ".info/index.php");
             br.postPage("http://www.gur" + randomain + ".info/index.php", "q=" + downloadLink.getDownloadURL() + "&hl[include_form]=0&hl[remove_scripts]=0&hl[accept_cookies]=1&hl[show_images]=1&hl[show_referer]=0&hl[strip_meta]=0&hl[strip_title]=0&hl[session_cookies]=0");
             forms = br.getForms();
-            captchaurl = br.getRegex(Pattern.compile("<div.*?class=\"cont.*?<img src=\"(.*?)\"", Pattern.DOTALL | Pattern.CASE_INSENSITIVE)).getMatch(0);
+            captchaurl = br.getRegex(Pattern.compile("<div\\sclass=\"cont\\sc2[^>]*>\\s+<br /><br />\\s+<img src=\"(.*?)\"", Pattern.DOTALL | Pattern.CASE_INSENSITIVE)).getMatch(0);
 
             // formaction = forms[3].action;
             if (captchaurl == null) throw new PluginException(LinkStatus.ERROR_FATAL, JDLocale.L("plugins.hoster.letitbitnet.errors.countryblock", "Letitbit forbidden downloading this file in your country"));
