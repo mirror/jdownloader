659bd77716f343511b496b13d6afc5144e1fa4ae
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-05-25 15:18:36 +0000

    - added redirect.musicalmente.info decrypter ( https://www.syncom.org/bugzilla/jdownloader/show_bug.cgi?id=395 )
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@5838 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInit.java
+++ src/jd/JDInit.java
@@ -367,6 +367,7 @@ public class JDInit {
         new DecryptPluginWrapper("linkshield.com", "Linkshield", "http://[\\w\\.]*?linkshield\\.com/[sc]/[\\d]+_[\\d]+");
         new DecryptPluginWrapper("lix.in", "Lixin", "http://[\\w\\.]*?lix\\.in/[-]{0,1}[a-zA-Z0-9]{6,10}");
         new DecryptPluginWrapper("mediafire.com", "MediafireFolder", "http://[\\w\\.]*?mediafire\\.com/\\?sharekey=.+");
+        new DecryptPluginWrapper("redirect.musicalmente.info", "MusicalmenteInfo", "http://[\\w\\.]*?redirect\\.musicalmente\\.info/.+");
         new DecryptPluginWrapper("music-base.ws", "MusicBaseWs", "http://[\\w\\.]*?music-base\\.ws/dl\\.php.*?c=[\\w]+");
         new DecryptPluginWrapper("myref.de", "MyRef", "http://[\\w\\.]*?myref\\.de(\\/){0,1}\\?\\d{0,10}");
         new DecryptPluginWrapper("myspace.com", "MySpaceCom", "http://[\\w\\.]*?myspace\\.(com|de)/.+");
