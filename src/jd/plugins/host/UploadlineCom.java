e5a7308f16b6ab09e96ab03c2b7bf8c1fa74da9f
ManiacMansion <ManiacMansion@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-05-26 18:39:26 +0000

    uploadline.com fix
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@5863 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/host/UploadlineCom.java
+++ src/jd/plugins/host/UploadlineCom.java
@@ -20,7 +20,9 @@ import java.io.IOException;
 
 import jd.PluginWrapper;
 import jd.http.Encoding;
+import jd.http.URLConnectionAdapter;
 import jd.parser.Regex;
+import jd.parser.html.Form;
 import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
@@ -37,14 +39,49 @@ public class UploadlineCom extends PluginForHost {
     public void handleFree(DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
         br.setFollowRedirects(false);
+        String dllink = "";
 
+        /* variation 1 for small files */
         if (br.containsHTML("for your IP next 24 hours")) {
             if (!br.containsHTML("7px;\">\\s+<a\\shref=\"(.*?)\"")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
-            String dllink = br.getRegex("7px;\">\\s+<a\\shref=\"(.*?)\"").getMatch(0);
+            dllink = br.getRegex("7px;\">\\s+<a\\shref=\"(.*?)\"").getMatch(0);
+        }
 
+        /* variation 2 for big files */
+        Form form = br.getForm(0);
+        if (form != null && form.hasInputFieldByName("method_free")) {
+            form.setAction(downloadLink.getDownloadURL());
+            form.remove("method_premium");
+            form.put("referer", Encoding.urlEncode(downloadLink.getDownloadURL()));
+            br.submitForm(form);
+            if (br.containsHTML("You have to wait")) {
+                int minutes = 0, seconds = 0, hours = 0;
+                String tmphrs = br.getRegex("\\s(\\d+)\\shours?").getMatch(0);
+                if (tmphrs != null) hours = Integer.parseInt(tmphrs);
+                String tmpmin = br.getRegex("\\s(\\d+)\\sminutes?").getMatch(0);
+                if (tmpmin != null) minutes = Integer.parseInt(tmpmin);
+                String tmpsec = br.getRegex("\\s(\\d+)\\sseconds?").getMatch(0);
+                if (tmpsec != null) seconds = Integer.parseInt(tmpsec);
+                int waittime = ((3600 * hours) + (60 * minutes) + seconds + 1) * 1000;
+                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, null, waittime);
+            } else {
+                form = br.getFormbyProperty("name", "F1");
+                if (form == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+                form.setAction(downloadLink.getDownloadURL());
+                br.submitForm(form);
+                URLConnectionAdapter con2 = br.getHttpConnection();
+                dllink = br.getRedirectLocation();
+                if (con2.getContentType().contains("html")) {
+                    if (br.containsHTML("Download Link Generated")) dllink = br.getRegex("7px;\">\\s+<a\\shref=\"(.*?)\">").getMatch(0);
+                }
+            }
+        }
+
+        if (dllink != null && dllink != "") {
             dl = br.openDownload(downloadLink, dllink, true, -20);
             dl.startDownload();
-        }
+        } else
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
     }
 
     // @Override
@@ -59,12 +96,19 @@ public class UploadlineCom extends PluginForHost {
 
     // @Override
     public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        String filename = null;
+        String filesize = null;
         this.setBrowserExclusive();
         br.setCookie("http://www.uploadline.com/", "lang", "english");
         br.getPage(downloadLink.getDownloadURL());
         if (br.containsHTML("(No such file)|(No such user exist)|(Link expired)|(File Not Found)")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String filename = Encoding.htmlDecode(br.getRegex("Filename:\\s<b>(.*?)</b>").getMatch(0));
-        String filesize = br.getRegex("<br>\\s+Size:\\s(.*?)\\s<small>").getMatch(0);
+        if (br.getForm(0) == null) {
+            filename = Encoding.htmlDecode(br.getRegex("Filename:\\s<b>(.*?)</b>").getMatch(0));
+            filesize = br.getRegex("<br>\\s+Size:\\s(.*?)\\s<small>").getMatch(0);
+        } else {
+            filename = Encoding.htmlDecode(br.getRegex("You\\shave\\srequested\\s<font\\scolor=\"red\">http://[\\w\\.]*?uploadline\\.com/\\d+/(.*?)</font>").getMatch(0));
+            filesize = br.getRegex("</font>\\s\\((.*?)\\)</font>").getMatch(0);
+        }
         if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
         downloadLink.setName(filename);
         downloadLink.setDownloadSize(Regex.getSize(filesize));
