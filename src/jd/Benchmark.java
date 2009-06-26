a7020725cc9d949f28961b310ba35f342c8bd69b
Coalado <Coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-06-26 16:09:13 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6406 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/Benchmark.java
@@ -0,0 +1,27 @@
+package jd;
+
+public class Benchmark {
+
+    private static final long DURATION = 5000;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        while (true) {
+            long start = System.currentTimeMillis();
+            long c;
+            long i = 0;
+            while (System.currentTimeMillis() - start < DURATION) {
+                c = start / 945756;
+                i++;
+                if (i == Long.MAX_VALUE) {
+                    i = 0;
+                    System.out.println("overflow");
+                }
+            }
+            System.out.println(start+" : "+(i / DURATION));
+        }
+    }
+
+}
