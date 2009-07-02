d6f0456408b3b090624aec0aef4c5bd1096007fe
gamewalker <gamewalker@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-02 10:46:02 +0000

    fixxed Vip-File.com hat seine Ids der Files geändert.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6483 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Vipfilecom.java
+++ src/jd/plugins/hoster/Vipfilecom.java
@@ -32,7 +32,7 @@ import jd.plugins.HostPlugin;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.utils.locale.JDL;
 
-@HostPlugin(name = "vip-file.com", urls = "http://[\\w\\.]*?vip-file\\.com/download/[\\w]+/(.*?)\\.html", flags = 2)
+@HostPlugin(name = "vip-file.com", urls = "http://[\\w\\.]*?vip-file\\.com/download/[\\w\\.]+/(.*?)\\.html", flags = 2)
 public class Vipfilecom extends PluginForHost {
 
     public Vipfilecom(PluginWrapper wrapper) {
