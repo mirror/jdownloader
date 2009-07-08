3bacf69d7e2a3b7c5f26c8d4d2df7bb3b6eb612d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-08 19:12:51 +0000

    fixes #164
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6556 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/EasyShareCom.java
+++ src/jd/plugins/hoster/EasyShareCom.java
@@ -35,7 +35,7 @@ import jd.plugins.PluginForHost;
 import jd.plugins.HostPlugin;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision="$Revision", interfaceVersion=2, names = { "easy-share.com"}, urls ={ "http://[\\w\\d\\.]*?easy-share\\.com/\\d{6}.*"}, flags = {2})
+@HostPlugin(revision = "$Revision", interfaceVersion = 2, names = { "easy-share.com" }, urls = { "http://[\\w\\d\\.]*?easy-share\\.com/\\d{6}.*" }, flags = { 2 })
 public class EasyShareCom extends PluginForHost {
 
     public EasyShareCom(PluginWrapper wrapper) {
@@ -63,7 +63,6 @@ public class EasyShareCom extends PluginForHost {
 
         if (br.getCookie("http://www.easy-share.com/", "PREMIUM") == null) {
             account.setEnabled(false);
-
             throw new PluginException(LinkStatus.ERROR_PREMIUM, LinkStatus.VALUE_ID_PREMIUM_DISABLE);
         }
 
@@ -106,8 +105,8 @@ public class EasyShareCom extends PluginForHost {
         br.getPage(downloadLink.getDownloadURL());
         br.setCookie("http://www.easy-share.com", "language", "en");
         if (br.containsHTML("Requested file is deleted")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String filename = br.getRegex(Pattern.compile("You are requesting<strong>(.*?)</strong>", Pattern.DOTALL | Pattern.CASE_INSENSITIVE)).getMatch(0);
-        String filesize = br.getRegex("You are requesting<strong>.*?</strong>(.*?)<").getMatch(0);
+        String filename = br.getRegex(Pattern.compile("You are requesting (.*?)\\(", Pattern.DOTALL | Pattern.CASE_INSENSITIVE)).getMatch(0);
+        String filesize = br.getRegex("You are requesting.*?\\((.*?)\\)").getMatch(0);
         if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         downloadLink.setName(filename.trim());
         downloadLink.setDownloadSize(Regex.getSize(filesize));
@@ -115,29 +114,26 @@ public class EasyShareCom extends PluginForHost {
     }
 
     // @Override
-    /* /* public String getVersion() {
-        return getVersion("$Revision$");
-    } */
+    /*
+     * /* public String getVersion() { return getVersion("$Revision$"); }
+     */
 
     // @Override
     public void handleFree(DownloadLink downloadLink) throws Exception {
         /* Nochmals das File überprüfen */
         requestFileInformation(downloadLink);
-        String wait = br.getRegex("id=\"freeButton\" value=\" Seconds to wait:(.*?)\"").getMatch(0);
+        String wait = br.getRegex("w='(.*?)'").getMatch(0);
         int waittime = 0;
         if (wait != null) waittime = Integer.parseInt(wait.trim());
-        if (waittime > 300) {
+        if (waittime > 90) {
             throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, waittime * 1000l);
         } else {
             sleep(waittime * 1000l, downloadLink);
         }
         br.getPage(downloadLink.getDownloadURL());
         if (br.containsHTML("Please wait or buy a Premium membership")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1000l);
-        String id = new Regex(downloadLink.getDownloadURL(), "/(\\d+)").getMatch(0);
-        br.getPage("http://www.easy-share.com/c/" + id);
-
         Form form = br.getForm(3);
-        String captcha = br.getRegex("<p><img src=\"(.*?)\"").getMatch(0);
+        String captcha = br.getRegex("<img src=\"(.*?)\"").getMatch(0);
         String captchaUrl = "http://" + br.getHost() + "/" + captcha;
         if (captcha != null) {
             String captchaCode = getCaptchaCode(captchaUrl, downloadLink);
