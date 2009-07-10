4d2fabf3fe2a9e235cbdd0c0754b01a1d49b2f19
ManiacMansion <ManiacMansion@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-10 16:07:55 +0000

    ManiacMansion proudly presents a pspzockerscene production: new hoster: appscene.org new ocr: appscene.org
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6645 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/AppSceneOrg.java
@@ -0,0 +1,86 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org & pspzockerscene
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "appscene.org" }, urls = { "http://[\\w\\.]*?appscene.org/download.php\\?id=[0-9]+" }, flags = { 0 })
+public class AppSceneOrg extends PluginForHost {
+
+    public AppSceneOrg(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    // @Override
+    public String getAGBLink() {
+        return "http://www.appscene.org/about.php";
+    }
+
+    // @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
+        setBrowserExclusive();
+        String url = downloadLink.getDownloadURL();
+        br.getPage(url);
+        if (br.containsHTML("The file you have requested does not exist or has been deleted")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        return AvailableStatus.TRUE;
+    }
+
+    // @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        String sessionid = br.getRegex("<form method=\"post\" action=\"download\\.php\\?stage=verify&download_session=(.*?)\">").getMatch(0);
+        String captchaurl = br.getBaseURL() + "captcha.php?sess=" + sessionid;
+        String code = getCaptchaCode(captchaurl, downloadLink);
+        Form captchaForm = br.getForm(0);
+        if (captchaForm == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        captchaForm.put("captcha", code);
+        br.submitForm(captchaForm);
+        if (br.getRedirectLocation().contains("appscene.org/download.php?id=")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+        dl = br.openDownload(downloadLink, br.getRedirectLocation(), false, 1);
+        if (br.containsHTML("This download session doesn't exist or has been deleted")) throw new PluginException(LinkStatus.ERROR_RETRY);
+        dl.startDownload();
+    }
+
+    // @Override
+    public void reset() {
+    }
+
+    // @Override
+    public void resetPluginGlobals() {
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+    // @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+    /*
+     * public String getVersion() { return getVersion("$Revision$"); }
+     */
+}
