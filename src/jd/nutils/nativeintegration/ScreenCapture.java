1e526a864e47797431054792f1f4538c5f8d2abc
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-14 22:33:54 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6677 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/nutils/nativeintegration/ScreenCapture.java
+++ src/jd/nutils/nativeintegration/ScreenCapture.java
@@ -28,20 +28,11 @@ import jd.config.SubConfiguration;
 import jd.gui.skins.simple.SimpleGuiConstants;
 
 public class ScreenCapture {
-    class DeviceInfo {
-        Robot robot;
-        int width;
-        int height;
-        int posx;
-        int posy;
-        int maxx;
-        int maxy;
-    }
 
-    private static boolean XineramaWorkaround = SubConfiguration.getConfig(SimpleGuiConstants.GUICONFIGNAME).getBooleanProperty("XINERAMAWORKAROUND", false);
+   
     private static ScreenCapture INSTANCE = new ScreenCapture();
 
-    private static DeviceInfo screens[] = new DeviceInfo[0];
+    private static DeviceInfo SCREENS[] = new DeviceInfo[0];
     static {
         try {
             ArrayList<DeviceInfo> robint = new ArrayList<DeviceInfo>();
@@ -60,30 +51,40 @@ public class ScreenCapture {
                     e.printStackTrace();
                 }
             }
-            screens = robint.toArray(new DeviceInfo[robint.size()]);
+            SCREENS = robint.toArray(new DeviceInfo[robint.size()]);
         } catch (Exception e) {
         }
     }
 
     public static boolean gotRobots() {
-        return screens.length > 0;
+        return SCREENS.length > 0;
     }
 
     public static BufferedImage getScreenShot(Rectangle r) throws AWTException {
-        synchronized (screens) {
-            if (screens.length == 0) { return new Robot().createScreenCapture(r); }
-            if (XineramaWorkaround) {
-                for (DeviceInfo dv : screens) {
+        synchronized (SCREENS) {
+            if (SCREENS.length == 0) { return new Robot().createScreenCapture(r); }
+            if (SubConfiguration.getConfig(SimpleGuiConstants.GUICONFIGNAME).getBooleanProperty("XINERAMAWORKAROUND", false)) {
+                for (DeviceInfo dv : SCREENS) {
                     if (r.x >= dv.posx && r.x <= dv.maxx && r.y >= dv.posy && r.y <= dv.maxy) {
                         r.x = r.x - dv.posx;
                         r.y = r.y - dv.posy;
                         return dv.robot.createScreenCapture(r);
                     }
                 }
-                return screens[0].robot.createScreenCapture(r);
+                return SCREENS[0].robot.createScreenCapture(r);
             } else {
-                return screens[0].robot.createScreenCapture(r);
+                return SCREENS[0].robot.createScreenCapture(r);
             }
         }
     }
+    class DeviceInfo {
+        Robot robot;
+        int width;
+        int height;
+        int posx;
+        int posy;
+        int maxx;
+        int maxy;
+    }
+
 }
