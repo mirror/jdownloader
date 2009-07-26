433e4c0f7ed1f77c9bba60c67a8c374e1e52d11c
gamewalker <gamewalker@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-26 22:43:41 +0000

    Debug entfernt und Revision gesetzt refs #1
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6818 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/NarodRu.java
+++ src/jd/plugins/hoster/NarodRu.java
@@ -48,7 +48,6 @@ public class NarodRu extends PluginForHost {
     public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
 
         br.getPage(downloadLink.getDownloadURL());
-        System.out.print(br);
         if (br.containsHTML("<title>404</title>")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
 
         String name = br.getRegex(Pattern.compile("<dt class=\"name\"><i class=\"b-old-icon b-old-icon-arc\"></i>(.*?)</dt>")).getMatch(0);
@@ -57,6 +56,7 @@ public class NarodRu extends PluginForHost {
         String md5Hash = br.getRegex(Pattern.compile("<dt class=\"size\">md5:</dt>.*<dd class=\"size\">(.*?)</dd>", Pattern.DOTALL)).getMatch(0);
         if (md5Hash == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
 
+        //TODO: kyrillische Zeichen in der Dateigrš§e
         String fileSize = br.getRegex(Pattern.compile("<td class=\"l-download-info-right\">.*<dl class=\"b-download-item g-line\">.*<dt class=\"size\">.*</dt>.*<dd class=\"size\">(.*?) ??</dd>", Pattern.DOTALL)).getMatch(0);
         if (fileSize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
 
