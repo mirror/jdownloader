918ecbe8b33b0b1de82441059e37c01216176cdf
dwd <dwd@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-28 01:04:45 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6846 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/JACTrain.java
+++ src/jd/captcha/JACTrain.java
@@ -27,22 +27,46 @@ import jd.utils.JDUtilities;
  * @author JD-Team
  */
 public class JACTrain {
-
+    /**
+     * @param args
+     */
     public static void main(String args[]) {
-        new JACTrain().go();
+
+        JACTrain main = new JACTrain();
+
+        main.go();
     }
 
     private Logger logger = Utilities.getLogger();
-    
-    private void go() {
-        String hoster = "badongo.com";
-        JAntiCaptcha jac = new JAntiCaptcha(Utilities.getMethodDir(), hoster);
 
+    private void go() {
+
+        // String hoster="rscat.com";
+        String hoster = "uploadbox.com";
+        JAntiCaptcha jac = new JAntiCaptcha(Utilities.getMethodDir(), hoster);
+        // jac.runTestMode(new File("1186941165349_captcha.jpg"));
         jac.displayLibrary();
 
+        // jac.setShowDebugGui(true);
+        // jac.showPreparedCaptcha(new
+        // File("/home/dwd/.jd_home/captchas/datenklo.net/08.01.2008_18.08.52.gif"));
+
+        // jac.trainCaptcha(new
+        // File("C:/Users/coalado/.jd_home/jd/captcha/methods/"+hoster+"/captchas/"+"captcha08_05_2008_22_20_01"+".jpg"),
+        // 4);
         jac.trainAllCaptchas(JDUtilities.getJDHomeDirectoryFromEnvironment().getAbsolutePath() + "/captchas/" + hoster);
 
+        // jac.saveMTHFile();
         logger.info("Training Ende");
+        // jac.addLetterMap();
+        // jac.saveMTHFile();
     }
-
+    // private static class FilterJAR implements FileFilter{
+    // public boolean accept(File f) {
+    // if(f.getName().endsWith(".jar"))
+    // return true;
+    // else
+    // return false;
+    // }
+    // }
 }
