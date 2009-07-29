b3dcb200252a21ee255d4e54d9e4b5e7aefa0bf1
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-29 17:27:53 +0000

    -new hoster filebox.com
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6897 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FileBoxCom.java
@@ -0,0 +1,111 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+//filebox by pspzockerscene
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filebox.com" }, urls = { "http://[\\w\\.]*?filebox\\.com/[a-zA-Z|0-9|/]+" }, flags = { 0 })
+public class FileBoxCom extends PluginForHost {
+
+    public FileBoxCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.filebox.com/tos.html";
+    }
+
+    // @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setCookie("http://www.filebox.com", "lang", "english");
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML("No such file")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("No such user exist")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("File Not Found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = Encoding.htmlDecode(br.getRegex("<td nowrap>(.*?)</b></td>").getMatch(0));
+        String filesize = br.getRegex("Size:.*?<small>\\((.*?)\\)</small>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        link.setName(filename);
+        link.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    // @Override
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        br.setFollowRedirects(true);
+        // Form um auf "Datei herunterladen" zu klicken
+            Form DLForm = br.getFormbyProperty("name", "F1");
+            if (DLForm == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+            String passCode = null;
+            if (br.containsHTML("valign=top><b>Password:</b></td>")) {
+                if (downloadLink.getStringProperty("pass", null) == null) {
+                    passCode = Plugin.getUserInput("Password?", downloadLink);
+                } else {
+                    /* gespeicherten PassCode holen */
+                    passCode = downloadLink.getStringProperty("pass", null);
+                }
+                DLForm.put("password", passCode);
+            }
+            int tt = Integer.parseInt(br.getRegex("countdown\">(\\d+)</span>").getMatch(0));
+            sleep(tt * 1001, downloadLink);
+            br.openDownload(downloadLink, DLForm, false, 1);
+            if (!(dl.getConnection().isContentDisposition())) {
+                br.followConnection();
+                if (br.containsHTML("Wrong password")) {
+                    logger.warning("Wrong password!");
+                    downloadLink.setProperty("pass", null);
+                    throw new PluginException(LinkStatus.ERROR_RETRY);
+                }
+            }
+            if (passCode != null) {
+                downloadLink.setProperty("pass", passCode);
+            }
+            dl.startDownload();
+        
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
