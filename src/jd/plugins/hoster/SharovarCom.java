fabd98759b93da1822d4f5171e48e1cf8337cad0
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-07-31 23:16:46 +0000

    -new hoster sharovar.com refs#198 Gamewalker pls skip the limits ;)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6969 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/SharovarCom.java
@@ -0,0 +1,92 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Encoding;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision", interfaceVersion = 2, names = { "sharovar.com" }, urls = { "http://[\\w\\.]*?sharovar\\.com/files/.+{1,}" }, flags = { 0 })
+public class SharovarCom extends PluginForHost {
+
+    public SharovarCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://sharovar.com/";
+    }
+
+    // @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML("File not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = Encoding.htmlDecode(br.getRegex("http://sharovar.com/.../<b>(.*?)</b></div>").getMatch(0));
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        link.setName(filename);
+        return AvailableStatus.TRUE;
+    }
+
+    // @Override
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        Form DLForm = br.getForm(3);
+        if (DLForm == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        br.submitForm(DLForm);
+        String dllink = br.getRegex("Now you can download file</h3><form action=\"(.*)\" method=\"post\" enctype=\"application/").getMatch(0);
+        br.openDownload(downloadLink, dllink, false, 1);
+        {
+            if (!(dl.getConnection().isContentDisposition())) br.followConnection();
+            // check ob limit aktiv, falls ja wird auf reconnect gewartet
+            if (br.containsHTML("You have reached the download limit")) {
+                int minutes = 0;
+                String tmpmin = br.getRegex("ess! Or try again in about (\\d+) minutes.").getMatch(0);
+                if (tmpmin != null) minutes = Integer.parseInt(tmpmin);
+                int waittime = minutes * 60 * 1001;
+                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, null, waittime);
+            }
+            dl.getConnection().disconnect();
+        }
+        dl.startDownload();
+
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
