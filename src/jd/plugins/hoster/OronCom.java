2f06341c2629486347bf18d63908419b16e03f36
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-01 20:42:23 +0000

    -New Hoster oron.com
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@6987 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/OronCom.java
@@ -0,0 +1,126 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+//oron by pspzockerscene
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "oron.com" }, urls = { "http://[\\w\\.]*?oron\\.com/[a-z|0-9]+/.+" }, flags = { 0 })
+public class OronCom extends PluginForHost {
+
+    public OronCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://oron.com/tos.html";
+    }
+
+    // @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setCookie("http://www.oron.com", "lang", "english");
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML("No such file with this filename")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("No such user exist")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("File Not Found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = Encoding.htmlDecode(br.getRegex("<h3>Filename: (.*?)</h3>").getMatch(0));
+        String filesize = br.getRegex("<h3>Size: (.*?)</h3>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        link.setName(filename);
+        link.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    // @Override
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        br.setFollowRedirects(true);
+        // Form um auf free zu "klicken"
+        Form DLForm0 = br.getForm(0);
+        if (DLForm0 == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        DLForm0.remove("method_premium");
+        br.submitForm(DLForm0);
+        if (br.containsHTML("You have to wait")) {
+            int minutes = 0, seconds = 0, hours = 0;
+            String tmphrs = br.getRegex("\\s+(\\d+)\\s+hours?").getMatch(0);
+            if (tmphrs != null) hours = Integer.parseInt(tmphrs);
+            String tmpmin = br.getRegex("\\s+(\\d+)\\s+minutes?").getMatch(0);
+            if (tmpmin != null) minutes = Integer.parseInt(tmpmin);
+            String tmpsec = br.getRegex("\\s+(\\d+)\\s+seconds?").getMatch(0);
+            if (tmpsec != null) seconds = Integer.parseInt(tmpsec);
+            int waittime = ((3600 * hours) + (60 * minutes) + seconds + 1) * 1000;
+            throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, null, waittime);
+        } else {
+            Form DLForm1 = br.getFormbyProperty("name", "F1");
+            if (DLForm1 == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+            String passCode = null;
+            if (br.containsHTML("Password:")) {
+                if (downloadLink.getStringProperty("pass", null) == null) {
+                    passCode = Plugin.getUserInput("Password?", downloadLink);
+                } else {
+                    /* gespeicherten PassCode holen */
+                    passCode = downloadLink.getStringProperty("pass", null);
+                }
+                DLForm1.put("password", passCode);
+            }
+            int tt = Integer.parseInt(br.getRegex("countdown\">(\\d+)</span>").getMatch(0));
+            sleep(tt * 1001, downloadLink);
+            br.submitForm(DLForm1);
+            if (br.containsHTML("Wrong password")) {
+                logger.warning("Wrong password!");
+                downloadLink.setProperty("pass", null);
+                throw new PluginException(LinkStatus.ERROR_RETRY);
+            }
+            if (passCode != null) {
+                downloadLink.setProperty("pass", passCode);
+            }
+            String dllink = br.getRegex("center\"><a href=\"(.*?)\" class=\"atitle\">Download File").getMatch(0);
+            br.openDownload(downloadLink, dllink, true, 1);
+
+            dl.startDownload();
+        }
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
