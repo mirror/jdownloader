edbed688cf64aba192c5cfd8486dd4825bbf61c7
gigant <gigant@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-03 01:04:19 +0000

    #253
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7017 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/DnbShareCom.java
@@ -0,0 +1,97 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.parser.html.Form.MethodType;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "dnbshare.com" }, urls = { "http://[\\w\\.]*?dnbshare\\.com/download/.*" }, flags = { 2 })
+public class DnbShareCom extends PluginForHost {
+
+    public DnbShareCom(PluginWrapper wrapper) {
+        super(wrapper);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.dnbshare.com/faq#tos";
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        String file = null;
+        String payload = null;
+        file = br.getRegex("document.write\\('<input type=hidden name=\"file\" value=\"(.*?)\" />'\\);").getMatch(0);
+        payload = br.getRegex("document.write\\('<input type=hidden name=\"payload\" value=\"(.*?)\" />'\\);").getMatch(0);
+        if (file == null || payload == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+
+        Form dlform = new Form();
+
+        dlform.setMethod(MethodType.POST);
+        dlform.put("file", file);
+        dlform.put("payload", payload);
+        dlform.put("submit", "Starting download..");
+        br.setFollowRedirects(false);
+        br.submitForm(dlform);
+
+        dl = br.openDownload(link, br.getRedirectLocation(), true, -20);
+        dl.startDownload();
+
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(parameter.getDownloadURL());
+        if (br.containsHTML("not found.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<h1>(.*?)</h1>").getMatch(0);
+        String filesize = br.getRegex("<em>Filesize</em>: (.*?)</li></ul>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        parameter.setName(filename.trim());
+        parameter.setDownloadSize(Regex.getSize(filesize.replaceAll(",", "\\.")));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        return 15;
+    }
+
+}
