0dac56befb289b309ef1f5b16a98c4f580d649e9
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-16 22:28:43 +0000

    -new hoster loaded.it
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7355 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/LoadedIt.java
@@ -0,0 +1,86 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import jd.PluginWrapper;
+import jd.parser.html.Form;
+import jd.plugins.BrowserAdapter;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "loaded.it" }, urls = { "http://[\\w\\.]*?loaded\\.it/show/[a-z|0-9]+/.+" }, flags = { 0 })
+public class LoadedIt extends PluginForHost {
+
+    public LoadedIt(PluginWrapper wrapper) {
+        super(wrapper);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://loaded.it/agb/";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(parameter.getDownloadURL());
+        if (br.containsHTML("Datei konnte nicht gefunden werden")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        Form DLForm = br.getFormbyProperty("name", "wait");
+        if (DLForm == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        int tt = Integer.parseInt(br.getRegex("var time_wait = (.*?);").getMatch(0));
+        sleep(tt * 1001, link);
+        br.submitForm(DLForm);
+        String dllink = br.getRegex("type=\"video/divx\" src=\"(.*?)\" custommode=\"Stage6").getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        //Bad try
+//        Regex reg = br.getRegex("GR&Ouml;SSE <b>(.*?)</b> (.*?)</td>");
+//        String filesize = reg.getMatch(0) + " " + reg.getMatch(1);
+//        link.setDownloadSize(Regex.getSize(filesize));
+        BrowserAdapter.openDownload(br, link, dllink, true, 0);
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+        // TODO Auto-generated method stub
+
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+}
