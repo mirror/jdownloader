89bd3cc60b4562546f9c6ce3e951c6227f9f2388
vivalostioz <vivalostioz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-20 22:11:47 +0000

    fixes #278
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7571 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/FlameUploadCom.java
@@ -0,0 +1,56 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "flameupload.com" }, urls = { "http://[\\w\\.]*?flameupload\\.com/files/[0-9A-Z]{8}/\\w+" }, flags = { 0 })
+public class FlameUploadCom extends PluginForDecrypt {
+
+    public FlameUploadCom(PluginWrapper wrapper) {
+      super(wrapper);
+    }
+    
+    // @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+      ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+      String parameter = param.toString();
+     
+      String id = parameter.split("/")[4]; 
+      parameter = "http://flameupload.com/status.php?uid=" + id;
+      
+      br.getPage(parameter);
+      String[] redirectLinks = br.getRegex("<a href=(/redirect/[0-9A-Z]{8}/\\d+) target=_blank><img src=/images/click.gif border='0' /></a>").getColumn(0);
+      
+      if (redirectLinks.length == 0) return null;
+      for (String link : redirectLinks) {
+          br.getPage(link);
+          String dllink = br.getRegex("<frame name=\"main\" src=\"(.*?)\">").getMatch(0);
+          decryptedLinks.add(createDownloadlink(dllink));
+      }
+          
+      return decryptedLinks;
+    }
+    
+}
