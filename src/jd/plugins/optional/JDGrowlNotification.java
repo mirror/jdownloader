aebae6c8252de84310bb16e17e79f499b4b3babf
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-21 12:28:23 +0000

    -JDGrowlNotification: no menuentries anymore
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7607 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/optional/JDGrowlNotification.java
+++ src/jd/plugins/optional/JDGrowlNotification.java
@@ -16,7 +16,6 @@
 
 package jd.plugins.optional;
 
-import java.awt.event.ActionEvent;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -35,22 +34,17 @@ import jd.plugins.PluginForHost;
 import jd.plugins.PluginOptional;
 import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
-@OptionalPlugin(rev="$Revision$", id="growl",interfaceversion=4,windows=false,linux=false)
+
+@OptionalPlugin(rev = "$Revision$", id = "growl", interfaceversion = 4, windows = false, linux = false)
 public class JDGrowlNotification extends PluginOptional {
 
+    private static final String JDL_PREFIX = "jd.plugins.optional.JDGrowlNotification.";
 
     public JDGrowlNotification(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final String PROPERTY_ENABLED = "PROPERTY_ENABLED";
-
-
-
- 
-
-
-    // @Override
+    @Override
     public boolean initAddon() {
         JDUtilities.getController().addControlListener(this);
         logger.info("Growl OK");
@@ -58,51 +52,35 @@ public class JDGrowlNotification extends PluginOptional {
         return true;
     }
 
-    public void actionPerformed(ActionEvent e) {
-        MenuAction mi = (MenuAction) e.getSource();
-        if (mi.getActionID() == 0) {
-            getPluginConfig().setProperty(PROPERTY_ENABLED, !getPluginConfig().getBooleanProperty(PROPERTY_ENABLED, false));
-            getPluginConfig().save();
-        }
-    }
-
-    // @Override
+    @Override
     public ArrayList<MenuAction> createMenuitems() {
-        ArrayList<MenuAction> menu = new ArrayList<MenuAction>();
-        MenuAction m;
-
-        menu.add(m = (MenuAction)new MenuAction(MenuAction.TOGGLE, JDL.L("addons.jdgrowlnotification.menu", "Growl Messages"), 0).setActionListener(this));
-        m.setSelected(this.getPluginConfig().getBooleanProperty(PROPERTY_ENABLED, false));
-
-        return menu;
+        return null;
     }
 
+    @Override
     public void controlEvent(ControlEvent event) {
+        switch (event.getID()) {
+        case ControlEvent.CONTROL_INIT_COMPLETE:
+            growlNotification(JDL.L(JDL_PREFIX + "started", "jDownloader gestartet..."), getDateAndTime(), JDL.L(JDL_PREFIX + "started.title", "Programmstart"));
+            break;
+        case ControlEvent.CONTROL_ALL_DOWNLOADS_FINISHED:
+            growlNotification(JDL.L(JDL_PREFIX + "allfinished", "Alle Downloads beendet"), "", JDL.L(JDL_PREFIX + "allfinished.title", "Alle Downloads fertig"));
+            break;
+        case ControlEvent.CONTROL_PLUGIN_INACTIVE:
+            if (!(event.getSource() instanceof PluginForHost)) return;
+            DownloadLink lastLink = ((SingleDownloadController) event.getParameter()).getDownloadLink();
+            if (lastLink.getLinkStatus().hasStatus(LinkStatus.FINISHED)) {
+                growlNotification(JDL.L(JDL_PREFIX + "finished", "Download beendet"), lastLink.getFinalFileName(), JDL.L(JDL_PREFIX + "finished.title", "Download erfolgreich beendet"));
+            }
+            break;
+        case ControlEvent.CONTROL_DOWNLOAD_TERMINATION_INACTIVE:
+            growlNotification(JDL.L(JDL_PREFIX + "aborted", "Download abgebrochen"), "", JDL.L(JDL_PREFIX + "aborted.title", "Download abgebrochen"));
+            break;
+        default:
+            break;
+        }
 
         super.controlEvent(event);
-
-        if (getPluginConfig().getBooleanProperty(PROPERTY_ENABLED, false)) {
-            switch (event.getID()) {
-            case ControlEvent.CONTROL_INIT_COMPLETE:
-                growlNotification("jDownloader gestartet...", getDateAndTime(), "Programmstart");
-                break;
-            case ControlEvent.CONTROL_ALL_DOWNLOADS_FINISHED:
-                growlNotification("Alle Downloads beendet", "", "Alle Downloads fertig");
-                break;
-            case ControlEvent.CONTROL_PLUGIN_INACTIVE:
-                if (!(event.getSource() instanceof PluginForHost)) { return; }
-                DownloadLink lastLink = ((SingleDownloadController) event.getParameter()).getDownloadLink();
-                if (lastLink.getLinkStatus().hasStatus(LinkStatus.FINISHED)) {
-                    growlNotification("Download beendet", lastLink.getFinalFileName(), "Download erfolgreich beendet");
-                }
-                break;
-            case ControlEvent.CONTROL_DOWNLOAD_TERMINATION_INACTIVE:
-                growlNotification("Download abgebrochen", "", "Download abgebrochen");
-                break;
-            default:
-                break;
-            }
-        }
     }
 
     private void growlNotification(String headline, String message, String title) {
@@ -111,13 +89,12 @@ public class JDGrowlNotification extends PluginOptional {
             exec.addParameter(JDUtilities.getResourceFile("jd/osx/growlNotification.scpt").getAbsolutePath());
             exec.addParameter(headline);
             exec.addParameter(message);
-
             exec.addParameter(title);
             exec.start();
         }
     }
 
-    // @Override
+    @Override
     public void onExit() {
         JDUtilities.getController().removeControlListener(this);
     }
