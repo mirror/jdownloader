efea41158b634b63bfbc274c472c507e79a09fec
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-22 23:49:20 +0000

    LoadAgain2
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7673 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/easy/load/LoadAgain.java
+++ src/jd/captcha/easy/load/LoadAgain.java
@@ -8,6 +8,8 @@ public class LoadAgain {
     public static void main(String[] args) {
         LoadImage li = LoadImage.loadFile("teradepot.com");
         try {
+            // von einer anderen url laden für Plugins nützlich
+            // li.baseUrl="http://www.teradepot.com/palo3ff7klxt/S.A.S_P.v4.27.1000.Multilingual_WA.I.Key.CRD.rar.html";
             System.out.println(li.load("teradepot.com").file);
             System.out.println(li.br);
         } catch (Exception e) {
