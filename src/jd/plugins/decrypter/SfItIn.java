431de1d50a48d94288a2b08831b6fa8b245e9a9a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-24 16:04:54 +0000

    -improved safe-it regex -added a new decrypter
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7829 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/SfItIn.java
+++ src/jd/plugins/decrypter/SfItIn.java
@@ -34,7 +34,7 @@ import jd.plugins.PluginForDecrypt;
 //import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "safe-it.in" }, urls = { "http://[\\w\\.]*?(safe-it\\.in|evil-warez\\.com)/\\?s=7&get=[0-9|a-z]+" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "safe-it.in" }, urls = { "http://[\\w\\.]*?(safe-it\\.in|evil-warez\\.com)(/\\?s|\\?s)=7&get=[0-9|a-z]+" }, flags = { 0 })
 public class SfItIn extends PluginForDecrypt {
 
     public SfItIn(PluginWrapper wrapper) {
