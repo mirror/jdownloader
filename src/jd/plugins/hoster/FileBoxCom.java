e579c56ed419ea7ee1135f8cdaac2b16dd70ce1c
vivalostioz <vivalostioz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-24 16:25:22 +0000

    fixes #413 regex fix
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7838 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FileBoxCom.java
+++ src/jd/plugins/hoster/FileBoxCom.java
@@ -92,7 +92,6 @@ public class FileBoxCom extends PluginForHost {
                 downloadLink.setProperty("pass", passCode);
             }
             dl.startDownload();
-        
     }
 
     @Override
