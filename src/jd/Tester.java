07cb2c35fc92e170402e7bbe0bbb22567956197a
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-08-26 21:14:37 +0000

    svn test
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@7952 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Tester.java
+++ src/jd/Tester.java
@@ -1,50 +1,34 @@
 package jd;
 
-import java.io.File;
+import java.awt.Dimension;
 
-import jd.http.Browser;
-import jd.http.URLConnectionAdapter;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
 
 public class Tester {
-
-    /**
-     * pings the updateservers to observe them
-     * 
-     * @param ss
-     * @throws Exception
-     */
     public static void main(String ss[]) throws Exception {
-
-        String[] list = new String[] { "http://update4ex.jdownloader.org/branches/Synthy/", "http://jdupdate.bluehost.to/branches/Synthy/", "http://update2.jdownloader.org/Synthy/", "http://update1.jdownloader.org/Synthy/", "http://update0.jdownloader.org/Synthy/" };
-
-        while (true) {
-            long time = 0;
-            long s, e;
-            Browser br = new Browser();
-            File file = File.createTempFile("jd" + System.currentTimeMillis(), "dummy");
-            for (String serv : list) {
-
-                s = System.currentTimeMillis();
-                try {
-                    URLConnectionAdapter con = br.openGetConnection(serv + "JDownloader.jar");
-                    System.out.println("Requesttime= " + (System.currentTimeMillis() - s) + " ms");
-                    br.downloadConnection(file, con);
-                    e = System.currentTimeMillis();
-                    con.disconnect();
-                } catch (Exception ee) {
-                    e = 100000;
-                    ee.printStackTrace();
-                }
-                System.out.println(serv + ": " + (e - s));
-                time += (e - s);
-
-            }
-
-            time /= (list.length);
-
-            System.err.println("Average speed: " + file.length() / time + " kb/s");
-            Thread.sleep(10000);
-        }
+        JFrame parent = new JFrame("Test");
+        parent.setSize(500, 400);
+        parent.setVisible(true);
+        System.out.println(parent.isAlwaysOnTop());
+        parent.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        new DDialog(parent);
     }
 
+    static class DDialog extends JDialog {
+        public DDialog(JFrame parent) {
+            super(parent);
+            this.setAlwaysOnTop(true);
+            System.out.println(parent.isAlwaysOnTop());
+
+            this.setModal(true);
+            this.setMinimumSize(new Dimension(200, 200));
+            this.setVisible(true);
+            System.out.println(parent.isAlwaysOnTop());
+
+        }
+
+    }
 }
+
