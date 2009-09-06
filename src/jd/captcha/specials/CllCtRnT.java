7d2abe53a4e3beb50bacec76d99e0f20df4dbb6e
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-09-06 16:01:21 +0000

    collctrnt 4 psp
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@8392 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/captcha/specials/CllCtRnT.java
@@ -0,0 +1,35 @@
+package jd.captcha.specials;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import jd.captcha.pixelgrid.Captcha;
+import jd.captcha.pixelgrid.Letter;
+import jd.captcha.pixelobject.PixelObject;
+
+public class CllCtRnT {
+    public static Letter[] getLetters(Captcha captcha) {
+
+        // BasicWindow.showImage(captcha.getImage());
+
+        Object[] cl = EasyCaptcha.clean(captcha);
+        int[] pixels = new int[] { (Integer) cl[0], (Integer) cl[1], (Integer) cl[2] };
+        @SuppressWarnings("unchecked")
+        ArrayList<PixelObject> os = (ArrayList<PixelObject>) cl[3];
+        Collections.sort(os);
+        int gab = pixels[2] / (captcha.owner.getLetterNum());
+        int[] mergeInfos = EasyCaptcha.mergeObjectsBasic(os, captcha, gab);
+        EasyCaptcha.getRightletters(os, captcha, pixels, mergeInfos);
+        Collections.sort(os);
+        ArrayList<Letter> ret = new ArrayList<Letter>();
+        for (PixelObject pixelObject : os) {
+            Letter let = pixelObject.toLetter();
+            let.toBlackAndWhite();
+            ret.add(let);
+//            BasicWindow.showImage(let.getImage());
+        }
+        // toBlack(captcha);
+        // BasicWindow.showImage(captcha.getImage());
+        return ret.toArray(new Letter[] {});
+
+    }
+}
