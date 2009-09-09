06d5e26c5113c9ddef82556b4163855228137226
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-09-09 13:38:50 +0000

    -Added a sharingmatrix.com folderdecrypter -Added sharingmatrix errorhandling for "only premium"- files
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@8446 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/SharingMatrixCom.java
+++ src/jd/plugins/hoster/SharingMatrixCom.java
@@ -46,8 +46,15 @@ public class SharingMatrixCom extends PluginForHost {
         br.getPage(parameter.getDownloadURL());
         if (br.containsHTML("File not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         String filename = br.getRegex("Filename:.*?<td>(.*?)</td>").getMatch(0);
+        if (filename == null) {
+            filename = br.getRegex("\"fname\">(.*?)</div>").getMatch(0);
+        }
         Regex reg = br.getRegex("<th>Size:.*?<td>(.*?)&nbsp;(.*?)</td>");
         String filesize = reg.getMatch(0) + reg.getMatch(1);
+        if (filesize.contains("null")) {
+            reg = br.getRegex("\"fsize_div\">(.*?)&nbsp;(.*?)</div>");
+            filesize = reg.getMatch(0) + reg.getMatch(1);
+        }
         if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         parameter.setName(filename.trim());
         parameter.setDownloadSize(Regex.getSize(filesize.replaceAll(",", "\\.")));
@@ -61,13 +68,16 @@ public class SharingMatrixCom extends PluginForHost {
         // if
         // (br.containsHTML("You have got max allowed bandwidth size per hour"))
         // { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED); }
+        if (br.containsHTML("no available free download slots left")) {
+
+        throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "No free slots available for this file"); }
         String linkid = br.getRegex("link_id = '(\\d+)';").getMatch(0);
         if (linkid == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT); }
         String freepage = "http://sharingmatrix.com/ajax_scripts/download.php?type_membership=free&link_id=" + linkid;
         br.getPage(freepage);
         String link_name = br.getRegex("link_name = '([^']+')").getMatch(0);
         // System.out.print(br.toString());
-//        long time = System.currentTimeMillis();
+        // long time = System.currentTimeMillis();
         String linkurl = br.getRegex("<input\\.*document\\.location=\"(.*?)\";").getMatch(0);
         // String captchalink =
         // "http://sharingmatrix.com/include/crypt/cryptographp.php?cfg=0&";
@@ -105,7 +115,7 @@ public class SharingMatrixCom extends PluginForHost {
         // System.out.println(br2.getCookies("http://sharingmatrix.com"));
         // System.out.print(br2.toString());
         if (linkurl == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT); }
-        dl = jd.plugins.BrowserAdapter.openDownload(br2, downloadLink, linkurl, false, 1);
+        dl = jd.plugins.BrowserAdapter.openDownload(br2, downloadLink, linkurl, true, 1);
         dl.startDownload();
     }
 
