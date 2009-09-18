1ded26a564e47d34b625af567d70601219fb64e6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-09-18 13:59:02 +0000

    -new host l4dmods.com, still got problems, pls finish it Sheadox
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@8581 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/L4dModsCom.java
@@ -0,0 +1,82 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "l4dmods.com" }, urls = { "http://[\\w\\.]*?l4dmods\\.com/index\\.php\\?option=com_joomloads\\&(view=package|controller=package&task=download)\\&Itemid=[0-9]\\&packageId=[0-9]+" }, flags = { 2 })
+public class L4dModsCom extends PluginForHost {
+
+    public L4dModsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public String getAGBLink() {
+        return "http://www.l4dmods.com/forums/ucp.php?mode=register";
+    }
+
+    
+    public void correctDownloadLink(DownloadLink link) {
+        link.setUrlDownload(link.getDownloadURL().replaceAll("(view=package|controller=package&task=download)", "view=package"));
+        link.setUrlDownload(link.getDownloadURL().replaceAll("http://filefactory", "http://www.filefactory"));
+    }
+    
+    
+    
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setDebug(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("<h3></h3>")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<h3>(.*?)</h3>").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        filename = filename.replaceAll("(&quot;|&#039;)", "");
+        downloadLink.setName(filename);
+        return AvailableStatus.TRUE;
+    }
+
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        String dllink = br.getRegex("oad\"><a href=\"(.*?)\"").getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        dllink = "http://www.l4dmods.com" + dllink;
+        br.setFollowRedirects(true);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        dl.startDownload();
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+    public void reset() {
+    }
+
+    public void resetPluginGlobals() {
+    }
+
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
