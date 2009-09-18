df4aea595700681feb3ed20e5cb196faf6e00997
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-09-18 22:32:01 +0000

    -New host fileshare.ro -Deleted host turkfile.com/net (offline for several weeks) closes #576
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@8586 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FileShareRo.java
@@ -0,0 +1,83 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.util.regex.Pattern;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "fileshare.ro" }, urls = { "http://[\\w\\.]*?fileshare\\.ro/[0-9]+\\.[0-9]+" }, flags = { 0 })
+public class FileShareRo extends PluginForHost {
+
+    public FileShareRo(PluginWrapper wrapper) {
+        super(wrapper);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.fileshare.ro/tu/";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(parameter.getDownloadURL());
+        if (br.containsHTML("Acest fisier nu exista")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<title>FileShare Download (.*?)</title>").getMatch(0);
+        String filesize = br.getRegex("</H3>.*?<br>(.*?),").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        parameter.setName(filename);
+        parameter.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        String dllink = br.getRegex(Pattern.compile("action=\"(/download/.*?)\"", Pattern.DOTALL)).getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT);
+        dllink = "http://www.fileshare.ro" + dllink;
+        jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, 1).startDownload();
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+        // TODO Auto-generated method stub
+
+    }
+
+    // @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+}
