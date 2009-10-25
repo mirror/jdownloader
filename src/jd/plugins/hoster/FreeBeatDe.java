f62bc62d4c255b67a5792e3b5a30aa1af12a996d
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-10-25 20:30:05 +0000

    -New hoster free-beat.de -New decrypter for shareapic.net refs #786 I'll add shareapic cancel button later but decrypter itself works 100%!
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9271 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FreeBeatDe.java
@@ -0,0 +1,71 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "free-beat.de" }, urls = { "http://[\\w\\.]*?free-beat\\.de/jgs_db,action-show,eintrags_id-[0-9]+,(sid-\\.|sid-[a-z0-9]+\\.)htm" }, flags = { 2 })
+public class FreeBeatDe extends PluginForHost {
+
+    public FreeBeatDe(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public String getAGBLink() {
+        return "http://www.free-beat.de/misc,action-imprint.html";
+    }
+
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("Der Benutzername existiert nicht. Du kannst dich sofort") || br.containsHTML("Die Datei ist keine MP3 und kann nicht im Player abgespielt werden") || !br.containsHTML("FlashVars")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<span class=\"normalfont\"><b>(.*?)</b>").getMatch(0);
+        if (filename == null) filename = br.getRegex("Datenbank</a>.*?href=.*?>.*?</a> »(.*?)</b>").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        downloadLink.setFinalFileName(filename.trim() + ".mp3");
+        return AvailableStatus.TRUE;
+    }
+
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        String dllink = br.getRegex("FlashVars\" value=.*?(http.*?)\"").getMatch(0);
+        if (dllink == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFEKT); }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, -5);
+        dl.startDownload();
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    public void reset() {
+    }
+
+    public void resetPluginGlobals() {
+    }
+
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
