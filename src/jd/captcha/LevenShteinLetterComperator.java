0dff480c1344b5bfbf4c6938f47e30b97860380b
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-10-29 14:52:09 +0000

    LevenShteinLetterComperator a lot of algo changes
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9381 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/LevenShteinLetterComperator.java
+++ src/jd/captcha/LevenShteinLetterComperator.java
@@ -15,10 +15,7 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package jd.captcha;
-
-import java.util.Arrays;
 import java.util.List;
-
 import jd.captcha.pixelgrid.Letter;
 
 public class LevenShteinLetterComperator {
@@ -106,22 +103,16 @@ public class LevenShteinLetterComperator {
     }
 
     private boolean[][][] getBooleanArrays(Letter letter) {
-        if (letter.getWidth() == 0 || letter.getWidth() == 0) return null;
-        boolean[][] leth1 = new boolean[letter.getWidth()][letter.getHeight()];
-        int avg = letter.getAverage();
+        int w = letter.getWidth(), h = letter.getHeight();
+        if (w == 0 || h == 0) return null;
+        boolean[][] leth1 = new boolean[w][h];
+        int avg = (int) (letter.getAverage() * letter.owner.getJas().getDouble("RelativeContrast"));
         for (int x = 0; x < leth1.length; x++) {
             for (int y = 0; y < leth1[0].length; y++) {
-                int pix = letter.getPixelValue(x, y);
-                if (pix == 0)
-                    leth1[x][y] = true;
-                else if (pix == 1)
-                    leth1[x][y] = false;
-                else
-                    leth1[x][y] = letter.isElement(pix, avg);
+                leth1[x][y] = letter.grid[x][y] < avg;
             }
         }
-        boolean[][] leth12 = new boolean[letter.getHeight()][letter.getWidth()];
-
+        boolean[][] leth12 = new boolean[h][w];
         for (int y = 0; y < leth1[0].length; y++) {
             for (int x = 0; x < leth1.length; x++) {
                 leth12[y][x] = leth1[x][y];
@@ -188,58 +179,54 @@ public class LevenShteinLetterComperator {
     private int[] getLevenshteinDistance(boolean[][][] ba, boolean[][][] bb, int best) {
         int res = 0;
         if (ba == null || bb == null) return null;
-        boolean[][] bba1 = ba[0];
-        boolean[][] bbb1 = bb[0];
-        boolean[][] bba2 = ba[1];
-        boolean[][] bbb2 = bb[1];
-
         int bounds1 = 0;
-        int diff1 = bba1.length - bbb1.length;
+        int diff1 = ba[0].length - bb[0].length;
         boolean swV = false;
         boolean swH = false;
 
         if (diff1 > 0) {
-            bounds1 = getBounds(bba1, bbb1, detectVerticalOffset);
+            bounds1 = getBounds(ba[0], bb[0], detectVerticalOffset);
         } else if (diff1 < 0) {
-            boolean[][] bac = bbb1;
-            bbb1 = bba1;
-            bba1 = bac;
+            boolean[][] bac = bb[0];
+            bb[0] = ba[0];
+            ba[0] = bac;
             swV = true;
-            bounds1 = getBounds(bba1, bbb1, detectVerticalOffset);
+            bounds1 = getBounds(ba[0], bb[0], detectVerticalOffset);
         } else
             bounds1 = 0;
-        res += getBoundDiff(bba1, bounds1, bbb1.length) * costs;
+        res += getBoundDiff(ba[0], bounds1, bb[0].length) * costs;
         if (best < res) return null;
 
         int bounds2 = 0;
-        int diff2 = bba2.length - bbb2.length;
+        int diff2 = ba[1].length - bb[1].length;
 
         if (diff2 > 0) {
-            bounds2 = getBounds(bba2, bbb2, detectHorizonalOffset);
+            bounds2 = getBounds(ba[1], bb[1], detectHorizonalOffset);
         } else if (diff2 < 0) {
-            boolean[][] bac = bbb2;
-            bbb2 = bba2;
-            bba2 = bac;
+            boolean[][] bac = bb[1];
+            bb[1] = ba[1];
+            ba[1] = bac;
             swH = true;
-            bounds2 = getBounds(bba2, bbb2, detectHorizonalOffset);
+            bounds2 = getBounds(ba[1], bb[1], detectHorizonalOffset);
         } else
             bounds2 = 0;
-        res += getBoundDiff(bba2, bounds2, bbb2.length) * costs;
+        res += getBoundDiff(ba[1], bounds2, bb[1].length) * costs;
 
         if (best < res) return null;
 
-        // res += (((Math.abs(bba1.length - bbb1.length) * Math.max(bba2.length,
-        // bbb2.length))+(Math.abs(bba2.length - bbb2.length) *
-        // Math.max(bba1.length, bbb1.length)))/dimension);
+        // res += (((Math.abs(ba[0].length - bb[0].length) *
+        // Math.max(ba[1].length,
+        // bb[1].length))+(Math.abs(ba[1].length - bb[1].length) *
+        // Math.max(ba[0].length, bb[0].length)))/dimension);
 
-        // System.out.println(bba1.length+":"+bbb1.length+":"+bounds1[1]+":"+bounds1[0]);
-        for (int c = 0; c < bbb1.length; c++) {
+        // System.out.println(ba[0].length+":"+bb[0].length+":"+bounds1[1]+":"+bounds1[0]);
+        for (int c = 0; c < bb[0].length; c++) {
             // System.out.println(c-bounds1[0]);
-            res += getLevenshteinDistance(bba1[c + bounds1], bbb1[c]);
+            res += getLevenshteinDistance(ba[0][c + bounds1], bb[0][c]);
             if (best < res) return null;
         }
-        for (int c = 0; c < bbb2.length; c++) {
-            res += getLevenshteinDistance(bba2[c + bounds2], bbb2[c]);
+        for (int c = 0; c < bb[1].length; c++) {
+            res += getLevenshteinDistance(ba[1][c + bounds2], bb[1][c]);
             if (best < res) return null;
         }
         return new int[] { res, swV ? -bounds1 : bounds1, swH ? -bounds2 : bounds2 };
@@ -254,43 +241,50 @@ public class LevenShteinLetterComperator {
         if (n == 0) {
             return m;
         } else if (m == 0) { return n; }
-
-        int p[] = new int[n + 1]; // 'previous' cost array, horizontally
-        int d[] = new int[n + 1]; // cost array, horizontally
-        int _d[] = null; // placeholder to assist in swapping p and d
-        int c[] = new int[n + 1]; // cost array, horizontally
-
+        int p[], d[], c[];
+        {
+            int n1 = n + 1;
+            p = new int[n1]; // 'previous' cost array, horizontally
+            d = new int[n1]; // cost array, horizontally
+            c = new int[n1]; // 'previous previous' cost array, horizontally
+        }
         // indexes into strings s and t
         int i; // iterates through s
         int j; // iterates through t
-
+        int j1, j2, i1, i2;
         boolean t_j;
 
-        int cost; // cost
+        int cost = 0; // cost
 
-        for (i = 0; i <= n; i++) {
+        for (i = 1; i <= n; i++) {
             p[i] = i;
         }
+        c=p;
         for (j = 1; j <= m; j++) {
-            t_j = l2[j - 1];
+            j1 = j;
+            j2 = --j1;
+            j2--;
+            t_j = l2[j1];
 
             d[0] = j;
 
             for (i = 1; i <= n; i++) {
-                cost = l1[i - 1] == t_j ? 0 : 1;
+                i1 = i-1;
+                cost = (l1[i1] == t_j) ? 1 : 0;
                 // minimum of cell to the left+1, to the top+1, diagonally left
                 // and up +cost
-                d[i] = Math.min(Math.min(d[i - 1] + costs, p[i] + costs), p[i - 1] + cost * costs);
+                d[i] = Math.min(d[i1] + costs, Math.min(p[i] + costs, p[i1] + cost * costs));
                 // Damerau
-                if ((i > 1) && (j > 1) && (l1[i - 1] == l2[j - 2]) && (l1[i - 2] == l2[j - 1])) {
-                    d[i] = Math.min(d[i], c[i - 2] + cost);
+                if ((i > 1) && (j > 1) && (l1[i1] == l2[j2]) && (l1[i2 = i1 - 1] == l2[j1])) {
+                    d[i] = Math.min(d[i], c[i2] + cost);
                 }
-
             }
             // previous of previous for Damerau
-            c = Arrays.copyOf(p, p.length);
+            for (i = 1; i <= n; i++) {
+                c[i] = p[i];
+            }
             // copy current distance counts to 'previous row' distance counts
-            _d = p;
+            int[] _d = p;
             p = d;
             d = _d;
         }
