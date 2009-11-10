72bdeab9b47a3d723a0df8db134d3ee9dd86906a
gigant <gigant@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-11-10 19:06:22 +0000

    refs #882 regex error
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9620 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/GoogleBooks.java
+++ src/jd/plugins/hoster/GoogleBooks.java
@@ -25,7 +25,7 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "books.google.com" }, urls = { "http://books.google.de/books\\?id=.*" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "books.google.com" }, urls = { "http://books.google.[a-z]+/books\\?id=.*&pg=.*" }, flags = { 0 })
 public class GoogleBooks extends PluginForHost {
 
     public GoogleBooks(PluginWrapper wrapper) {
