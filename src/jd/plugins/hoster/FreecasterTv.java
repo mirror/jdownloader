d5b22f8241b8b085cf6d409b970453fc201432a1
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-11-21 13:13:14 +0000

    Plugin reviewed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9804 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FreecasterTv.java
+++ src/jd/plugins/hoster/FreecasterTv.java
@@ -3,50 +3,57 @@ package jd.plugins.hoster;
 import jd.PluginWrapper;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision = "$Revision: 9647 $", interfaceVersion = 2, names = { "freecaster.tv" }, urls = { "http://videos.freecaster.com/.++" }, flags = { 2 })
+@HostPlugin(revision = "$Revision: 9647 $", interfaceVersion = 2, names = { "freecaster.tv" }, urls = { "http://videos.freecaster.com/.++" }, flags = { 0 })
 public class FreecasterTv extends PluginForHost {
 
-
-	public FreecasterTv(PluginWrapper wrapper) {
+    public FreecasterTv(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-	@Override
-	public String getAGBLink() {
-		return "http://freecaster.tv/info/general-terms-of-service";
-	}
-
-	@Override
-	public void handleFree(DownloadLink link) throws Exception {
-		dl = jd.plugins.BrowserAdapter.openDownload(br, link, link.getDownloadURL(), true, 0);
-        dl.startDownload();
-
-	}
-
-	@Override
-	public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws Exception {
-		this.setBrowserExclusive();
-        
-        if (br.openGetConnection(downloadLink.getDownloadURL()).getResponseCode() == 200) {
-            long size = br.getHttpConnection().getLongContentLength();
-            downloadLink.setDownloadSize(Long.valueOf(size));
-            return AvailableStatus.TRUE;
-        }
-
-        return AvailableStatus.FALSE;
+    @Override
+    public String getAGBLink() {
+        return "http://freecaster.tv/info/general-terms-of-service";
     }
 
-	@Override
-	public void reset() {
-	}
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, link.getDownloadURL(), true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            dl.getConnection().disconnect();
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        dl.startDownload();
+    }
 
-	@Override
-	public void resetDownloadlink(DownloadLink link) {
-	}
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws Exception {
+        this.setBrowserExclusive();
+        try {
+            if (!br.openGetConnection(downloadLink.getDownloadURL()).getContentType().contains("html")) {
+                long size = br.getHttpConnection().getLongContentLength();
+                downloadLink.setDownloadSize(Long.valueOf(size));
+                return AvailableStatus.TRUE;
+            }
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } finally {
+            if (br.getHttpConnection() != null) br.getHttpConnection().disconnect();
+        }
+    }
 
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+        link.setFinalFileName(link.getStringProperty("filename", "NA"));
+    }
 
     @Override
     public int getMaxSimultanFreeDownloadNum() {
