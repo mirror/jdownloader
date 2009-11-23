754649de8dd42bf02902a2db5854dae3f17b390d
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-11-23 14:05:33 +0000

    better mvwrldnt method
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9830 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/captcha/specials/MvWrLd.java
@@ -0,0 +1,35 @@
+package jd.captcha.specials;
+
+import java.util.Collections;
+import java.util.List;
+
+import jd.captcha.pixelgrid.Captcha;
+import jd.captcha.pixelgrid.Letter;
+import jd.captcha.pixelgrid.PixelGrid;
+import jd.captcha.pixelobject.PixelObject;
+
+public class MvWrLd {
+
+    private static void toBlack(PixelGrid captcha) {
+        for (int x = 0; x < captcha.getWidth(); x++) {
+            for (int y = 0; y < captcha.getHeight(); y++) {
+                if (captcha.grid[x][y] != 0xffffff) {
+                    captcha.grid[x][y] = 0x000000;
+                }
+
+            }
+        }
+    }
+    public static Letter[] getLetters(Captcha captcha) {
+        captcha.cleanBySaturation(0, 19);
+
+        List<PixelObject> ob = ColoredObject.getObjects(captcha, 16, 1, 3);
+        Collections.sort(ob);
+        Letter[] let = new Letter[ob.size()];
+        for (int i = 0; i < let.length; i++) {
+           let[i]=ob.get(i).toLetter();
+           toBlack(let[i]);
+        }
+        return let;
+    }
+}
