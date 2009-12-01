fbda4e1c68133fcb8d37b298280aec1bd46df782
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-01 15:27:31 +0000

    Do i have to comment that ?
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9928 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Zippysharecom.java
+++ src/jd/plugins/hoster/Zippysharecom.java
@@ -19,12 +19,9 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
-import jd.http.Browser;
 import jd.http.RandomUserAgent;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
-import jd.parser.html.HTMLParser;
-import jd.plugins.BrowserAdapter;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
@@ -32,7 +29,7 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "zippyshare.com" }, urls = { "http://www\\d{0,}\\.zippyshare\\.com/(v/\\d+/file\\.html|.*?key=\\d+)" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "zippyshare.com" }, urls = { "http://www\\d{0,}\\.zippyshare\\.com/(v/\\d+/file\\.html|.*?key=\\d+)" }, flags = { 2 })
 public class Zippysharecom extends PluginForHost {
 
     private Pattern linkIDPattern = Pattern.compile(".*?zippyshare\\.com/v/([0-9]+)/file.html");
@@ -40,7 +37,6 @@ public class Zippysharecom extends PluginForHost {
 
     public Zippysharecom(PluginWrapper wrapper) {
         super(wrapper);
-        this.setStartIntervall(2000l);
         br.setFollowRedirects(true);
     }
 
@@ -74,69 +70,31 @@ public class Zippysharecom extends PluginForHost {
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
-        br.setFollowRedirects(true);
-        int index = -1;
-        while (true) {
-            index++;
-            // String allreplaces =
-            // br.getRegex("ziptime.*?unescape(.*?)(var|'|;)").getMatch(0);
-            // String[][] replaces = new Regex(allreplaces,
-            // "replace\\((.*?),.*?\"(.*?)\"").getMatches();
-            String page = Encoding.urlDecode(br.toString(), true);
-            // for (String[] replace : replaces) {
-            // replace[0] = replace[0].substring(1);
-            // if (replace[0].endsWith("/")) replace[0] =
-            // replace[0].substring(0, replace[0].length() - 1);
-            // if (replace[0].endsWith("/g")) replace[0] =
-            // replace[0].substring(0, replace[0].length() - 2);
-            // if (replace[0].endsWith("/i")) replace[0] =
-            // replace[0].substring(0, replace[0].length() - 2);
-            // page = page.replace(replace[0], replace[1]);
-            // }
-            String rplces = br.getRegex("[ ]{5,1000000000}foken = unescape(.*?);").getMatch(0);
-            boolean fcku = false;
-            if (rplces != null) {
-                Regex wtf = new Regex(rplces, "\\(.*?\\.replace\\(/(.*?)/, \"(.*?)\"\\)\\.replace\\(/(.*?)/.*?, \"(.*?)\"\\)\\)");
-                if (wtf.getMatch(0) != null && wtf.getMatch(1) != null && wtf.getMatch(2) != null && wtf.getMatch(3) != null) {
-                    fcku = true;
-                    page = page.replace(wtf.getMatch(0), wtf.getMatch(1)).replace(wtf.getMatch(2), wtf.getMatch(3));
+        String dllink = br.getRegex("ziptime = 0; tuw\\(\\);.*?var.*?= 'fck(http.*?)';").getMatch(0);
+        if (dllink == null) dllink = br.getRegex("<script language=\"Javascript\">.*?(http.*?)';").getMatch(0);
+        logger.info("Encoded the zippyshare link...");
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dllink = Encoding.urlDecode((dllink), true);
+        if (!br.containsHTML("name=\"mp3player\"") || br.containsHTML("replace")) {
+            String[] replacessuck = br.getRegex("(\\(.*?\\.replace\\(.*?,.*?\\))").getColumn(0);
+            if (replacessuck != null) {
+                for (String fckU : replacessuck) {
+                    String rpl1 = new Regex(fckU, "replace\\((.*?),.*?\\)").getMatch(0).replace("/", "");
+                    String rpl2 = new Regex(fckU, "replace\\(.*?, \"(.*?)\"\\)").getMatch(0);
+                    if (!rpl1.equals("www")) {
+                        dllink = dllink.replace(rpl1, rpl2);
+                        logger.info("Replace(s) done");
+                    }
                 }
-            }
-            if (fcku != true) {
-                String rpl1 = new Regex(page, "http://([a-zA-Z]+)[0-9]+\\.zippyshare").getMatch(0);
-                String rpl2 = new Regex(page, "http://.*?\\.zippyshare\\.com/(.*?)/.*?/").getMatch(0);
-                if (rpl1 != null && rpl2 != null) {
-                    page = page.replace(rpl1, "www").replace(rpl2, "d");
-                } else {
-                    page = page.replaceAll("nnn", "www").replaceAll("uss", "d");
-                }
-            }
-            String[] links = HTMLParser.getHttpLinks(page, null);
-            if (index > links.length - 1) break;
-            String curlink = links[index];
-            if (!new Regex(curlink, ".*?www\\d{1,2}\\.zippyshare\\.com/[^\\?]*\\..{1,4}$").matches()) {
-                continue;
-            }
-            // Waiting makes no sense if there is no waittime :D
-            // sleep(10000l, downloadLink);
-            Browser brc = br.cloneBrowser();
-            brc.setDebug(true);
-            brc.getCookies(getHost()).remove(brc.getCookies(getHost()).get("zippop"));
-            dl = BrowserAdapter.openDownload(brc, downloadLink, curlink);
-            if (dl.getConnection().isContentDisposition()) {
-                dl.setFilenameFix(true);
-                dl.startDownload();
-                return;
-            } else {
-                dl.getConnection().disconnect();
-                br = new Browser();
-                this.setBrowserExclusive();
-                br.getHeaders().put("User-Agent", RandomUserAgent.generate());
-                br.setCookie("http://www.zippyshare.com", "ziplocale", "en");
-                br.getPage(downloadLink.getDownloadURL().replaceAll("locale=..", "locale=en"));
             }
         }
-        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, false, 1);
+        if (!(dl.getConnection().isContentDisposition())) {
+            logger.warning("Final link | " + dllink + " |Download failed");
+            br.getHeaders().put("User-Agent", RandomUserAgent.generate());
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
     }
 
     @Override
