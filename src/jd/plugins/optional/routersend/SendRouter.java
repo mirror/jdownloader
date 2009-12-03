8ca8af4aabdb6117ec2f557088d5ce9ffbeaf50b
gigant <gigant@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-03 02:12:01 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9973 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/optional/routersend/SendRouter.java
+++ src/jd/plugins/optional/routersend/SendRouter.java
@@ -39,12 +39,11 @@ import jd.utils.locale.JDL;
 @OptionalPlugin(rev = "$Revision$", defaultEnabled = true, id = "routersend", interfaceversion = 5)
 public class SendRouter extends PluginOptional implements ControlListener {
     private static final String JDL_PREFIX = "jd.plugins.optional.SendRouter.";
-    private String SCRIPTURL = "http://127.0.0.1/jd/index.php";
-    private String MANUFACTURURL = "http://127.0.0.1/jd/hersteller"; // List separated by ","
-    private String CurrentScript = "";
-    private int ReconnectCounter = 0;
+    private String scripturl = "http://127.0.0.1/jd/index.php";
+    private String manufactururl = "http://127.0.0.1/jd/hersteller"; // List separated by ","
+    private String currentScript = "";
+    private int reconnectCounter = 0;
     private Boolean send = false;
-    private boolean dontsend;
 
     public SendRouter(PluginWrapper wrapper) {
         super(wrapper);
@@ -53,12 +52,11 @@ public class SendRouter extends PluginOptional implements ControlListener {
 
     @Override
     public boolean initAddon() {
-        JDUtilities.getController().addControlListener(this);
-
-        ReconnectCounter = getPluginConfig().getIntegerProperty("ReconnectCounter", 0);
-        CurrentScript = getPluginConfig().getStringProperty("CurrentScript", "");
+        reconnectCounter = getPluginConfig().getIntegerProperty("ReconnectCounter", 0);
+        currentScript = getPluginConfig().getStringProperty("CurrentScript", "");
         send = getPluginConfig().getBooleanProperty("send", false);
-        dontsend = getPluginConfig().getBooleanProperty("dontsend", false);
+        if(check() == false || send == false)
+            JDUtilities.getController().addControlListener(this);
         return false;
     }
 
@@ -77,58 +75,60 @@ public class SendRouter extends PluginOptional implements ControlListener {
     public void controlEvent(ControlEvent e) {
         if (e.getID() == ControlEvent.CONTROL_AFTER_RECONNECT) {
             if (JDUtilities.getConfiguration().getIntegerProperty(ReconnectMethod.PARAM_RECONNECT_TYPE, ReconnectMethod.LIVEHEADER) == ReconnectMethod.LIVEHEADER) {
-                ReconnectCounter++;
-                getPluginConfig().setProperty("ReconnectCounter", ReconnectCounter);
+                reconnectCounter++;
+                getPluginConfig().setProperty("ReconnectCounter", reconnectCounter);
                 getPluginConfig().save();
-                if (!CurrentScript.equals(JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_REQUESTS))) {
-                    CurrentScript = JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_REQUESTS);
-                    send = false;
-                    dontsend = false;
-                    ReconnectCounter = 0;
-                    getPluginConfig().setProperty("CurrentScript", CurrentScript);
-                    getPluginConfig().setProperty("send", send);
-                    getPluginConfig().setProperty("ReconnectCounter", ReconnectCounter);
-                    getPluginConfig().setProperty("dontsend", dontsend);
-                    getPluginConfig().save();
-                    return;
-                }
-                if (!send && ReconnectCounter > 5 && !dontsend) {
+                if (reconnectCounter > 5  && check()) {
                     int ret = UserIO.getInstance().requestConfirmDialog(UserIO.NO_COUNTDOWN, JDL.L(JDL_PREFIX + "info.topic", "Help to Improve JD"), JDL.L(JDL_PREFIX + "info.msg", "JD have detected that you hade 5 successfull reconnects \r\n You now can send the script to our server so we can include it permanently in JD"), UserIO.getInstance().getIcon(UserIO.ICON_INFO), null, null);
                     if (ret != 2) {
-                        dontsend = true;
+                        JDUtilities.getController().removeControlListener(this);
                         return;
                     }
-                    if (Subrmitdata() == 0) {
+                    if (submitdata() == 0) {
                         UserIO.getInstance().requestMessageDialog(JDL.L(JDL_PREFIX + "send.successfull", "Thank you for your help"));
                         send = true;
                         getPluginConfig().setProperty("send", send);
                         getPluginConfig().save();
+                        JDUtilities.getController().removeControlListener(this);
                     } else {
                         UserIO.getInstance().requestMessageDialog(JDL.L(JDL_PREFIX + "send.failed", "A error eccourd while sending your data.\r\n We will ask you again later."));
-                        ReconnectCounter = 0;
-                        getPluginConfig().setProperty("ReconnectCounter", ReconnectCounter);
+                        reconnectCounter = 0;
+                        getPluginConfig().setProperty("ReconnectCounter", reconnectCounter);
                         getPluginConfig().save();
-                        dontsend = true; // We wan't try again for this session
+                        JDUtilities.getController().removeControlListener(this);
                     }
                 }
             }
         }
     }
 
-    private int Subrmitdata() {
+    private boolean check(){
+        if (!currentScript.equals(JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_REQUESTS))) {
+            currentScript = JDUtilities.getConfiguration().getStringProperty(Configuration.PARAM_HTTPSEND_REQUESTS);
+            send = false;
+            reconnectCounter = 0;
+            getPluginConfig().setProperty("CurrentScript", currentScript);
+            getPluginConfig().setProperty("send", send);
+            getPluginConfig().setProperty("ReconnectCounter", reconnectCounter);
+            getPluginConfig().save();
+            return false;
+        }
+        return true;
+    }
+    private int submitdata() {
         Form data = new Form();
         data.setMethod(Form.MethodType.POST);
-        data.setAction(SCRIPTURL);
+        data.setAction(scripturl);
         String[] Manufactor_List;
         try {
-            Manufactor_List = br.getPage(MANUFACTURURL).split(",");
+            Manufactor_List = br.getPage(manufactururl).split(",");
         } catch (IOException e) {
             return 1;
         }
         int selection = UserIO.getInstance().requestComboDialog(UserIO.NO_COUNTDOWN | UserIO.NO_CANCEL_OPTION, JDL.L(JDL_PREFIX + "manufactor.list", "Manufactor"), JDL.L(JDL_PREFIX + "manufactor.message", "Which manufactor ?"), Manufactor_List, 0, null, JDL.L(JDL_PREFIX + "ok", "OK"), null, new DefaultListCellRenderer());
         InputField hersteller = new InputField("hersteller", Encoding.urlEncode(Manufactor_List[selection]));
         InputField name = new InputField("name", Encoding.urlEncode(UserIO.getInstance().requestInputDialog(UserIO.NO_COUNTDOWN | UserIO.NO_CANCEL_OPTION, JDL.L(JDL_PREFIX + "manufactor.name", "Routername?"), "")));
-        InputField script = new InputField("script", Encoding.Base64Encode(CurrentScript));
+        InputField script = new InputField("script", Encoding.Base64Encode(currentScript));
         data.addInputField(hersteller);
         data.addInputField(name);
         data.addInputField(script);
