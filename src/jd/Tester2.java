9d989f6f15747cd44086fc55fc534226396eb2be
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-06 20:20:02 +0000

    testenvironment for jwindow's issues with jspinner
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@9998 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Tester2.java
+++ src/jd/Tester2.java
@@ -1,10 +1,18 @@
 package jd;
 
+import javax.swing.JSpinner;
+import javax.swing.JWindow;
+
+import net.miginfocom.swing.MigLayout;
+
 public class Tester2 {
 
     public static void main(String[] args) {
-        System.out.println(Math.tan(45.0));
-        System.out.println(Math.tan(-45.0));
+        JWindow w = new JWindow();
+        w.setLayout(new MigLayout("ins 0"));
+        w.add(new JSpinner(), "w 200!");
+        w.pack();
+        w.setVisible(true);
     }
 
 }
