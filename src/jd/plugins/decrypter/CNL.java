8f3c8b277a957dcbf76e2d4804038271e49c06a2
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-09 13:33:20 +0000

    ALternativ CNL Plugin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10030 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/CNL.java
@@ -0,0 +1,48 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision: 7387 $", interfaceVersion = 2, names = { "Click'n'load" }, urls = { "cnl://.*?/" }, flags = { PluginWrapper.PATTERN_ACCEPTS_INVALID_URI })
+public class CNL extends PluginForDecrypt {
+
+    public CNL(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    // @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+
+
+
+        return decryptedLinks;
+    }
+
+    // @Override
+
+}
