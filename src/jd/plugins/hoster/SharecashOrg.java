6422781bb2700d71450987e5693b4b1eb140a143
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-10 19:23:33 +0000

    new hoster: sharecash.org
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10042 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/SharecashOrg.java
@@ -0,0 +1,105 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision: 9411 $", interfaceVersion = 2, names = { "sharecash.org" }, urls = { "http://[\\w\\.]*?sharecash\\.org/download.php\\?(file|id)=\\d+" }, flags = { 0 })
+public class SharecashOrg extends PluginForHost {
+    public SharecashOrg(PluginWrapper wrapper) {
+        super(wrapper);
+
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://sharecash.org/tos.php";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws PluginException, IOException {
+
+        this.setBrowserExclusive();
+        try {
+
+            br.getPage(downloadLink.getDownloadURL());
+            String filename = br.getRegex("<td width=\"120\"><strong>(.*?)</strong></td>").getMatch(0);
+            if(filename==null) return AvailableStatus.FALSE;
+            long size = Regex.getSize(br.getRegex("<b>Size:</b>(.*?)</td>").getMatch(0));
+            String md5 = br.getRegex("<b>MD5\\:<\\/b> (\\w{32})").getMatch(0);
+            downloadLink.setMD5Hash(md5);
+            downloadLink.setFinalFileName(filename);
+            downloadLink.setDownloadSize(size);
+            return AvailableStatus.TRUE;
+        } catch (NullPointerException e) {
+            return AvailableStatus.FALSE;
+        }
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+
+   
+        requestFileInformation(downloadLink);
+        String[] vars = br.getRegex("\"(\\\\.*?)\"").getColumn(0);
+        String url = null;
+        for (String v : vars) {
+            String decoded = Encoding.htmlDecode(v.replace("\\x", "%"));
+            url = new Regex(decoded, "<h2>Click <a href=\"(.*?)\"").getMatch(0);
+            if (url != null) break;
+        }
+
+        if (url == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, url, true, 1);
+
+        /* DownloadLimit? */
+//        if (!dl.getConnection().isContentDisposition()) {
+//            dl.getConnection().disconnect();
+//            throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 180000);
+//        }
+        
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
