f167778e4556c015ea358e5e09f1ce868de2d2ee
zm1gol <zm1gol@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-14 12:13:15 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10096 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/CPluginWrapper.java
+++ src/jd/CPluginWrapper.java
@@ -32,7 +32,9 @@ public class CPluginWrapper extends PluginWrapper {
 
     public CPluginWrapper(String name, String host, String classNamePrefix, String className, String patternSupported, int flags) {
         super(host, classNamePrefix, className, patternSupported, flags);
-        if (loadPlugin() != null) C_WRAPPER.add(this);
+        if (loadPlugin() != null) {
+            C_WRAPPER.add(this);
+        }
     }
 
     public CPluginWrapper(String host, String className, String patternSupported) {
@@ -44,18 +46,18 @@ public class CPluginWrapper extends PluginWrapper {
     }
 
     public PluginsC loadPlugin() {
-        JDClassLoader jdClassLoader = JDUtilities.getJDClassLoader();
+        final JDClassLoader jdClassLoader = JDUtilities.getJDClassLoader();
 
         logger.finer("Try to initialize " + this.getClassName());
         try {
 
-            Class<?> plgClass = jdClassLoader.loadClass(this.getClassName());
+            final Class<?> plgClass = jdClassLoader.loadClass(this.getClassName());
             if (plgClass == null) {
                 logger.info("PLUGIN NOT FOUND!");
                 return null;
             }
-            Class<?>[] classes = new Class[] { PluginWrapper.class };
-            Constructor<?> con = plgClass.getConstructor(classes);
+            final Class<?>[] classes = new Class[] { PluginWrapper.class };
+            final Constructor<?> con = plgClass.getConstructor(classes);
 
             this.loadedPlugin = (PluginsC) con.newInstance(new Object[] { this });
             logger.finer("Successfully loaded " + this.getClassName());
