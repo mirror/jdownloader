661d7b3d1f7114788868f31ba83c6bc20ec6ad35
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-23 19:38:56 +0000

    -Added new hoster filestock.ru -Added new hoster yabadaba.ru, still doesn't work because of no premium slots available refs #1119
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10163 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/YabaDabaRu.java
@@ -0,0 +1,97 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "yabadaba.ru" }, urls = { "http://[\\w\\.]*?yabadaba\\.ru/files/[0-9]+" }, flags = { 0 })
+public class YabaDabaRu extends PluginForHost {
+
+    public YabaDabaRu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://yabadaba.ru/pages/agreement";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("Этот файл не найден")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filesize = br.getRegex("Размер файла:(.*?<br").getMatch(0);
+        String filename = br.getRegex("<title>(.*?)&bul.*?Yabadaba").getMatch(0);
+        if (filename == null || filesize == null) filename = br.getRegex("Скачать файл</div>.*?<h1>(.*?)</h1>").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        downloadLink.setName(filename.trim());
+        downloadLink.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        String continuelink = br.getRegex("<h1><a href=\"(http.*?)\"").getMatch(0);
+        if (continuelink == null) {
+            continuelink = br.getRegex("\"(http://yaba[0-9]+\\.yabadaba\\.ru/download/.*?/[0-9]+)\"").getMatch(0);
+        }
+        if (continuelink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        br.getPage(continuelink);
+        if (br.containsHTML("All free download slots are busy")) {
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "No free slots available at this moment!");
+        } else {
+            logger.warning("Slots are available, browser says:" + br.toString());
+        }
+        // I have to ait till someone gives me a log where i can see how to
+        // download there!
+        String dllink = br.getRegex("dg35z3hzu46ujhrjhnr5j(.*?)wf34wg5e4uz4e").getMatch(0);
+        dllink = null;
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
