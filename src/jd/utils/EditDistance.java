e95d4a1ed6bceddf4258b6b24cd61c5a98db0cb8
zm1gol <zm1gol@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-25 13:42:03 +0000

    performance EditDistance
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10174 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/utils/EditDistance.java
+++ src/jd/utils/EditDistance.java
@@ -16,7 +16,13 @@
 
 package jd.utils;
 
-public class EditDistance {
+public final class EditDistance {
+    /**
+     * Don't let anyone instantiate this class.
+     */
+    private EditDistance() {
+    }
+
     /**
      * Gibt den Prozentualen Unterschied zwischen zwei Strings zurück verwendet
      * die LevenshteinDistance
@@ -25,14 +31,15 @@ public class EditDistance {
      * @param t
      * @return
      */
-    public static int getLevenshteinDifference(String s, String t) {
+    public static int getLevenshteinDifference(final String s, final String t) {
         if (s == null) {
             if (t == null) return 0;
             return t.length();
         }
-        if (t == null) return s.length();
+        final int sLength = s.length();
+        if (t == null) return sLength;
         if (s.equals(t)) return 0;
-        return 100 * getLevenshteinDistance(s, t) / Math.max(s.length(), t.length());
+        return 100 * getLevenshteinDistance(s, t) / Math.max(sLength, t.length());
     }
 
     /**
@@ -43,20 +50,20 @@ public class EditDistance {
      * @param t
      * @return
      */
-    public static int getLevenshteinDistance(String s, String t) {
+    public static int getLevenshteinDistance(final String s, final String t) {
         if (s == null) {
             if (t == null) return 0;
             return t.length();
         }
         if (t == null) return s.length();
 
-        int n = s.length();
-        int m = t.length();
+        final int n = s.length();
+        final int m = t.length();
         if (n == 0) {
             return m;
         } else if (m == 0) { return n; }
         int i;
-        int n1 = n + 1;
+        final int n1 = n + 1;
         int p[] = new int[n1];
         int d[] = new int[n1];
         int _d[];
@@ -90,20 +97,22 @@ public class EditDistance {
      * @param t
      * @return
      */
-    public static int damerauLevenshteinDistance(String src, String dest) {
-        int[][] d = new int[src.length() + 1][dest.length() + 1];
+    public static int damerauLevenshteinDistance(final String src, final String dest) {
+        final int str1Length = src.length();
+        final int str2Length = dest.length();
+        final int[][] d = new int[str1Length + 1][str2Length + 1];
         int i, j, cost;
-        char[] str1 = src.toCharArray();
-        char[] str2 = dest.toCharArray();
+        final char[] str1 = src.toCharArray();
+        final char[] str2 = dest.toCharArray();
 
-        for (i = 0; i <= str1.length; i++) {
+        for (i = 0; i <= str1Length; i++) {
             d[i][0] = i;
         }
-        for (j = 0; j <= str2.length; j++) {
+        for (j = 0; j <= str2Length; j++) {
             d[0][j] = j;
         }
-        for (i = 1; i <= str1.length; i++) {
-            for (j = 1; j <= str2.length; j++) {
+        for (i = 1; i <= str1Length; i++) {
+            for (j = 1; j <= str2Length; j++) {
 
                 if (str1[i - 1] == str2[j - 1])
                     cost = 0;
@@ -119,11 +128,10 @@ public class EditDistance {
                     // 2][j - 2] + cost));
                     d[i][j] = Math.min(d[i][j], d[i - 2][j - 2] + cost);
                     System.out.println("d:" + d[i - 2][j - 2]);
-
                 }
             }
         }
-        return d[str1.length][str2.length];
+        return d[str1Length][str2Length];
     }
 
 }
