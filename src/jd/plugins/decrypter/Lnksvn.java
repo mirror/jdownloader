4fbd61bcb9b704d03c7b9979d82669e279c53bef
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-29 18:06:57 +0000

    implemented multiple pages and connection interval limit of 1000 ms
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10211 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/Lnksvn.java
+++ src/jd/plugins/decrypter/Lnksvn.java
@@ -53,7 +53,9 @@ public class Lnksvn extends PluginForDecrypt {
         this.setBrowserExclusive();
         br.getHeaders().put("User-Agent", RandomUserAgent.generate());
         br.setCookie("http://linksave.in/", "Linksave_Language", "german");
+        br.setRequestIntervalLimit("linksave.in", 1000);
         br.getPage(param.getCryptedUrl());
+       
         if (br.containsHTML("Ordner nicht gefunden")) return decryptedLinks;
         Form form = br.getFormbyProperty("name", "form");
         for (int retry = 0; retry < 5; retry++) {
@@ -78,78 +80,92 @@ public class Lnksvn extends PluginForDecrypt {
         }
 
         String[] container = br.getRegex("\\.href\\=unescape\\(\\'(.*?)\\'\\)\\;").getColumn(0);
-        if (container != null && container.length > 0) {
-            for (String c : container) {
-                Browser clone = br.cloneBrowser();
-                String test = Encoding.htmlDecode(c);
-                File file = null;
-                if (test.endsWith(".cnl")) {
-                    URLConnectionAdapter con = clone.openGetConnection("http://linksave.in/" + test.replace("dlc://linksave.in/", ""));
-                    if (con.getResponseCode() == 200) {
-                        file = JDUtilities.getResourceFile("tmp/linksave/" + test.replace(".cnl", ".dlc").replace("dlc://", "http://").replace("http://linksave.in", ""));
-                        clone.downloadConnection(file, con);
-                    } else {
-                        con.disconnect();
-                    }
-                } else if (test.endsWith(".rsdf")) {
-                    URLConnectionAdapter con = clone.openGetConnection(test);
-                    if (con.getResponseCode() == 200) {
-                        file = JDUtilities.getResourceFile("tmp/linksave/" + test.replace("http://linksave.in", ""));
-                        clone.downloadConnection(file, con);
-                    } else {
-                        con.disconnect();
-                    }
-                }
-                if (file != null && file.exists() && file.length() > 100) {
-                    try {
-                        decryptedLinks = JDUtilities.getController().getContainerLinks(file);
-                    } catch (Exception e) {
-                    }
 
-                }
+        String[] pages = br.getRegex("\\?s=(\\d+)\\#down").getColumn(0);
+        int pageID = 1;
+        int foundlinks=0;
+        for (String page : pages) {
+            if (Integer.parseInt(page) < pageID) break;
+            pageID = Integer.parseInt(page);
+            if (!page.equals("1")) {
+                br.getPage(param.getCryptedUrl() + "?s=" + page + "#down");
             }
-        }
-        if (decryptedLinks.size() == 0) {
-            String[] links = br.getRegex("<a href=\"(http://linksave[^\"]*)\" onclick=\"javascript:document.getElementById").getColumn(0);
-            final class LsDirektLinkTH extends Thread {
-                Browser browser;
-                String result;
-
-                public LsDirektLinkTH(Browser browser) {
-                    this.browser = browser;
-                }
-
-                @Override
-                public void run() {
-                    try {
-                        result = getDirektLink(browser);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+            foundlinks=decryptedLinks.size();
+            if (container != null && container.length > 0) {
+                for (String c : container) {
+                    Browser clone = br.cloneBrowser();
+                    String test = Encoding.htmlDecode(c);
+                    File file = null;
+                    if (test.endsWith(".cnl")) {
+                        URLConnectionAdapter con = clone.openGetConnection("http://linksave.in/" + test.replace("dlc://linksave.in/", ""));
+                        if (con.getResponseCode() == 200) {
+                            file = JDUtilities.getResourceFile("tmp/linksave/" + test.replace(".cnl", ".dlc").replace("dlc://", "http://").replace("http://linksave.in", ""));
+                            clone.downloadConnection(file, con);
+                        } else {
+                            con.disconnect();
+                        }
+                    } else if (test.endsWith(".rsdf")) {
+                        URLConnectionAdapter con = clone.openGetConnection(test);
+                        if (con.getResponseCode() == 200) {
+                            file = JDUtilities.getResourceFile("tmp/linksave/" + test.replace("http://linksave.in", ""));
+                            clone.downloadConnection(file, con);
+                        } else {
+                            con.disconnect();
+                        }
                     }
-                    synchronized (this) {
-                        this.notify();
-                    }
-                }
-            }
-            LsDirektLinkTH[] dlinks = new LsDirektLinkTH[links.length];
-            for (int i = 0; i < dlinks.length; i++) {
-                Browser clone = br.cloneBrowser();
-                clone.getPage(links[i]);
-                dlinks[i] = new LsDirektLinkTH(clone);
-                dlinks[i].start();
-            }
-            for (LsDirektLinkTH lsDirektLinkTH : dlinks) {
-                while (lsDirektLinkTH.isAlive()) {
-                    synchronized (lsDirektLinkTH) {
+                    if (file != null && file.exists() && file.length() > 100) {
                         try {
-                            lsDirektLinkTH.wait();
-                        } catch (InterruptedException e) {
+                            decryptedLinks = JDUtilities.getController().getContainerLinks(file);
+                        } catch (Exception e) {
+                        }
+
+                    }
+                }
+            }
+            if (decryptedLinks.size() == foundlinks) {
+                String[] links = br.getRegex("<a href=\"(http://linksave[^\"]*)\" onclick=\"javascript:document.getElementById").getColumn(0);
+                final class LsDirektLinkTH extends Thread {
+                    Browser browser;
+                    String result;
+
+                    public LsDirektLinkTH(Browser browser) {
+                        this.browser = browser;
+                    }
+
+                    @Override
+                    public void run() {
+                        try {
+                            result = getDirektLink(browser);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        synchronized (this) {
+                            this.notify();
                         }
                     }
                 }
-                if (lsDirektLinkTH.result != null) decryptedLinks.add(createDownloadlink(lsDirektLinkTH.result));
+                LsDirektLinkTH[] dlinks = new LsDirektLinkTH[links.length];
+                progress.addToMax(links.length);
+                for (int i = 0; i < dlinks.length; i++) {
+                    Browser clone = br.cloneBrowser();
+                    clone.getPage(links[i]);
+                    dlinks[i] = new LsDirektLinkTH(clone);
+                    dlinks[i].start();
+                }
+                for (LsDirektLinkTH lsDirektLinkTH : dlinks) {
+                    while (lsDirektLinkTH.isAlive()) {
+                        synchronized (lsDirektLinkTH) {
+                            try {
+                                lsDirektLinkTH.wait();
+                            } catch (InterruptedException e) {
+                            }
+                            progress.increase(1);
+                        }
+                    }
+                    if (lsDirektLinkTH.result != null) decryptedLinks.add(createDownloadlink(lsDirektLinkTH.result));
+                }
+                if (decryptedLinks.size() == 0) throw new DecrypterException("Out of date. Try Click'n'Load");
             }
-            if (decryptedLinks.size() == 0) throw new DecrypterException("Out of date. Try Click'n'Load");
         }
         return decryptedLinks;
     }
