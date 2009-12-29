e8d21296e3123c046b6c137b3c888ca12ce2511e
zm1gol <zm1gol@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2009-12-29 19:43:11 +0000

    format JDInitFlags
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10213 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/JDInitFlags.java
+++ src/jd/JDInitFlags.java
@@ -17,14 +17,10 @@
 package jd;
 
 public class JDInitFlags {
-
-   
     public static boolean SWITCH_RETURNED_FROM_UPDATE = false;
-    public static boolean SWITCH_DEBUG=false;
-    public static boolean ENOUGH_MEMORY=true;
-    public static boolean SHOW_SPLASH=true;
-    public static boolean SWITCH_NEW_INSTANCE=false;
-    public static boolean STOP=false;
-  
- 
+    public static boolean SWITCH_DEBUG = false;
+    public static boolean ENOUGH_MEMORY = true;
+    public static boolean SHOW_SPLASH = true;
+    public static boolean SWITCH_NEW_INSTANCE = false;
+    public static boolean STOP = false;
 }
