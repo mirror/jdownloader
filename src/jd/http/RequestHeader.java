51c6f24a2615e56a3ee5311575a4755898b46382
zm1gol <zm1gol@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-02 14:57:14 +0000

    performance RequestHeader
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10237 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/http/RequestHeader.java
+++ src/jd/http/RequestHeader.java
@@ -32,7 +32,7 @@ public class RequestHeader {
      * 
      * @param dominant
      */
-    public void setDominant(boolean dominant) {
+    public void setDominant(final boolean dominant) {
         this.dominant = dominant;
     }
 
@@ -41,14 +41,16 @@ public class RequestHeader {
         this.values = new ArrayList<String>();
     }
 
-    public RequestHeader(HashMap<String, String> h) {
+    public RequestHeader(final HashMap<String, String> h) {
         this();
         this.putAll(h);
     }
 
-    public void put(String key, String value) {
-        for (int i = 0; i < keys.size(); i++) {
-            if (keys.get(i).equalsIgnoreCase(key.trim())) {
+    public void put(final String key, final String value) {
+        final int keysSize = keys.size();
+        final String trim = key.trim();
+        for (int i = 0; i < keysSize; i++) {
+            if (keys.get(i).equalsIgnoreCase(trim)) {
                 keys.set(i, key);
                 values.set(i, value);
                 return;
@@ -58,11 +60,11 @@ public class RequestHeader {
         values.add(value);
     }
 
-    public String getKey(int i) {
+    public String getKey(final int i) {
         return keys.get(i);
     }
 
-    public String getValue(int i) {
+    public String getValue(final int i) {
         return values.get(i);
     }
 
@@ -71,37 +73,37 @@ public class RequestHeader {
     }
 
     public Object clone() {
-        RequestHeader mh = new RequestHeader();
+        final RequestHeader mh = new RequestHeader();
         mh.keys.addAll(keys);
         mh.values.addAll(values);
         return mh;
     }
 
     public String remove(String key) {
-        int index = keys.indexOf(key);
+        final int index = keys.indexOf(key);
         if (index >= 0) {
             keys.remove(index);
             return values.remove(index);
+        } else {
+            return null;
         }
-        return null;
     }
 
-    public void putAll(HashMap<String, String> properties) {
+    public void putAll(final HashMap<String, String> properties) {
         for (Iterator<String> it = properties.keySet().iterator(); it.hasNext();) {
-            String key = it.next();
-            String value;
-            if ((value = properties.get(key)) == null) {
+            final String key = it.next();
+            final String value = properties.get(key);
+            if (value == null) {
                 remove(key);
             } else {
                 put(key, value);
             }
-
         }
-
     }
 
-    public void putAll(RequestHeader headers) {
-        for (int i = 0; i < headers.size(); i++) {
+    public void putAll(final RequestHeader headers) {
+        final int size = headers.size();
+        for (int i = 0; i < size; i++) {
             String key = headers.getKey(i);
             String value = headers.getValue(i);
             if (value == null) {
@@ -110,13 +112,11 @@ public class RequestHeader {
                 put(key, value);
             }
         }
-
     }
 
-    public String get(String key) {
-        int index = keys.indexOf(key);
-        if (index >= 0) { return values.get(index); }
-        return null;
+    public String get(final String key) {
+        final int index = keys.indexOf(key);
+        return (index >= 0) ? values.get(index) : null;
     }
 
     public void clear() {
@@ -124,11 +124,11 @@ public class RequestHeader {
         values.clear();
     }
 
-    public boolean contains(String string) {
+    public boolean contains(final String string) {
         return keys.contains(string);
     }
 
-    public void setAt(int i, String key, String value) {
+    public void setAt(final int i, final String key, final String value) {
         if (keys.size() == 0) {
             this.put(key, value);
             return;
@@ -136,13 +136,11 @@ public class RequestHeader {
         keys.add(keys.get(keys.size() - 1));
         values.add(values.get(values.size() - 1));
         for (int e = keys.size() - 3; e >= i; e--) {
-
             keys.set(e + 1, keys.get(e));
             values.set(e + 1, values.get(e));
         }
         keys.set(i, key);
         values.set(i, value);
-
     }
 
     public boolean isDominant() {
