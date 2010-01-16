e91d7282bc978c959b469f46683bed8556d5878b
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-16 16:29:07 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10384 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/dynamics/Survey.java
+++ src/jd/dynamics/Survey.java
@@ -26,7 +26,7 @@ public class Survey extends DynamicPluginInterface {
                     if (JDL.isGerman()) {
                         UserIO.getInstance().requestMessageDialog(UserIO.NO_COUNTDOWN,"Umfrage", "Um JDownloader noch besser zu machen, führen wir eine anonyme Umfrage zur Nutzerzufriedenheit durch. Es wäre sehr schön wenn du dir einige Minuten Zeit nehmen könntest.\r\nDein JD-Team\r\n\r\n");
                     } else {
-                        UserIO.getInstance().requestMessageDialog(UserIO.NO_COUNTDOWN,"Survey", "To make JDownloader event better, we do an Survey about user satisfaction. We would be very happy if you could spend a few minutes.\r\nGreetings, your JD-Team");
+                        UserIO.getInstance().requestMessageDialog(UserIO.NO_COUNTDOWN,"Survey", "To make JDownloader event better, we do a Survey about user satisfaction. We would be very happy if you could spend a few minutes.\r\nGreetings, your JD-Team");
                     }
                     try {
                         LocalBrowser.openDefaultURL(new URL("http://jdownloader.org/survey"));
