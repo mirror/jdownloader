cef56d1c2e96c1ce63570286588aca46c1204156
scr4ve <scr4ve@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-18 19:16:44 +0000

    Awesomebar: ToolbarController testcase.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10415 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/optional/awesomebar/Awesomebar.java
+++ src/jd/plugins/optional/awesomebar/Awesomebar.java
@@ -1,145 +1,109 @@
 package jd.plugins.optional.awesomebar;
 
-import java.awt.Dimension;
-import java.awt.event.KeyEvent;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
 
-import javax.swing.JFrame;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
 
-import jd.plugins.optional.awesomebar.awesome.Awesome;
-import jd.plugins.optional.awesomebar.awesome.AwesomeAction;
-import jd.plugins.optional.awesomebar.awesome.AwesomeProposalDetailPanel;
-import jd.plugins.optional.awesomebar.awesome.jlist.AwesomeProposalJList;
-import jd.plugins.optional.awesomebar.awesome.jlist.AwesomeProposalListModel;
-import jd.plugins.optional.awesomebar.awesome.jlist.AwesomeProposalListSelectionModel;
+import jd.PluginWrapper;
+import jd.gui.swing.jdgui.actions.ActionController;
+import jd.gui.swing.jdgui.actions.CustomToolbarAction;
+import jd.gui.swing.jdgui.components.toolbar.ToolBar;
+import jd.gui.swing.jdgui.menu.MenuAction;
+import jd.gui.swing.jdgui.settings.panels.gui.ToolbarController;
+import jd.plugins.OptionalPlugin;
+import jd.plugins.PluginOptional;
 import net.miginfocom.swing.MigLayout;
 
-public class Awesomebar extends JFrame {
+@OptionalPlugin(rev = "$Revision: 10379 $", id = "addons.awesomebar", hasGui = true, interfaceversion = 5)
+public class Awesomebar extends PluginOptional  {
+    
+    private CustomToolbarAction toolbarAction;
+    
+    public Awesomebar(PluginWrapper wrapper) {
+        super(wrapper);
+        this.toolbarAction = new CustomToolbarAction("addons.awesomebar") {
+            private static final long serialVersionUID = 3329576457034851778L;
+            private JPanel cp;
 
-    private static final long serialVersionUID = 1L;
-    private JPanel jContentPane = null;
-    private JTextField jTextField = null;
-    private Awesome awesome = new Awesome(); // @jve:decl-index=0:
-    private AwesomeProposalJList proposalList = null;
-    private AwesomeProposalDetailPanel detailPanel = null;
+            /**
+             * is called before every toolbar rebuild
+             */
+            @Override
+            public void init() {
+            }
 
-    /**
-     * This is the default constructor
-     */
-    public Awesomebar() {
-        super();
-        initialize();
-    }
+            // cannot be disabled
 
-    /**
-     * This method initializes this
-     * 
-     * @return void
-     */
-    private void initialize() {
-        this.setContentPane(getJContentPane());
-        this.setTitle("Awesomebar");
-        this.setSize(new Dimension(420, 300));
+            public boolean force() {
+                return true;
+            }
 
-    }
+            /**
+             * Gets called when initializing the instance
+             */
+            @Override
+            public void initDefaults() {
+                cp = new JPanel(new MigLayout("ins 1"));
+                // textfield
+                JTextField bar = new JTextField();
+                cp.add(bar, "width 150px!, wrap");
+                JLabel desc = new JLabel("description!");
+                desc.setForeground(Color.gray);
+                desc.setFont(new Font("Arial", Font.PLAIN, 9));
+                desc.setSize(150, 9);
+                cp.add(desc,"width 150px!, height 9px!");
+            }
 
-    /**
-     * This method initializes jContentPane
-     * 
-     * @return javax.swing.JPanel
-     */
-    private JPanel getJContentPane() {
-        if (jContentPane == null) {
-            MigLayout layout = new MigLayout("insets 0,fill", "[c]");
-
-            jContentPane = new JPanel();
-            jContentPane.setLayout(layout);
-            jContentPane.add(getJTextField(), "wrap, w 360px!");
-            jContentPane.add(getAwesomeProposalList(), "grow, wrap, width ::400");
-            jContentPane.add(getDetailPanel(), "grow, wrap");
-        }
-        return jContentPane;
-    }
-
-    /**
-     * This method initializes jTextField
-     * 
-     * @return javax.swing.JTextField
-     */
-    private JTextField getJTextField() {
-        if (jTextField == null) {
-            jTextField = new JTextField();
-            jTextField.addKeyListener(new java.awt.event.KeyAdapter() {
-                public void keyPressed(java.awt.event.KeyEvent e) {
-
-                    if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {
-                        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                            // Perform Action
-                            new AwesomeAction(awesome, getAwesomeProposalList().getSelectedProposal());
-                        } else if (e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
-                            awesome.requestProposal(getJTextField().getText().substring(0, getJTextField().getCaretPosition()) + e.getKeyChar() + getJTextField().getText().substring(getJTextField().getCaretPosition()));
-                        } else {
-                            if (getJTextField().getCaretPosition() > 0) {
-                                awesome.requestProposal(getJTextField().getText().substring(0, getJTextField().getCaretPosition() - 1) + getJTextField().getText().substring(getJTextField().getCaretPosition()));
-                            }
-                        }
-                    } else {
-                        switch (e.getKeyCode()) {
-                        case KeyEvent.VK_DOWN:
-                            // Change Selection, go down!
-                            try {
-                                getAwesomeProposalList().getModel().countLock.readLock().lock();
-                                if (getAwesomeProposalList().getSelectedIndex() != getAwesomeProposalList().getModel().getSize() - 1) {
-                                    getAwesomeProposalList().setSelectedIndices(new int[] { getAwesomeProposalList().getSelectedIndex() + 1 });
-                                    getDetailPanel().updateDetailPanel();
-                                }
-                            } finally {
-                                getAwesomeProposalList().getModel().countLock.readLock().unlock();
-                            }
-                            break;
-                        case KeyEvent.VK_UP:
-                            // Change Selection, go up!
-                            if (getAwesomeProposalList().getSelectedIndex() > 0) {
-                                getAwesomeProposalList().setSelectedIndices(new int[] { getAwesomeProposalList().getSelectedIndex() - 1 });
-                                getDetailPanel().updateDetailPanel();
-                            }
-                            break;
-                        }
-                    }
+            /**
+             * has to add something to the toolbar
+             */
+            @Override
+            public void addTo(Object toolBar) {
+                // Toolbara ctions might be used by other components, too
+                // iot's up to the custom action to implement them
+                if (toolBar instanceof ToolBar) {
+                    ToolBar tb = (ToolBar) toolBar;
+                    tb.add(cp, "");
                 }
-            });
-        }
-        return jTextField;
+
+            }
+
+        };
+        initAddon();
     }
 
-    /**
-     * This method initializes jList
-     * 
-     * @return javax.swing.JList
-     */
-    private AwesomeProposalJList getAwesomeProposalList() {
-        if (proposalList == null) {
-            ListSelectionModel selectionModel = new AwesomeProposalListSelectionModel();
-            AwesomeProposalListModel listModel = new AwesomeProposalListModel(awesome, selectionModel);
-            proposalList = new AwesomeProposalJList(listModel, selectionModel);
-        }
-        return proposalList;
+    @Override
+    public boolean initAddon() {
+        setGuiEnable(true);
+        return false;
     }
 
-    /**
-     * This method initializes jPanel
-     * 
-     * @return javax.swing.JPanel
-     */
-    private AwesomeProposalDetailPanel getDetailPanel() {
-        if (detailPanel == null) {
-            detailPanel = new AwesomeProposalDetailPanel(getAwesomeProposalList());
-            detailPanel.setLayout(new MigLayout("insets 0,fill"));
-            getAwesomeProposalList().getSelectionModel().addListSelectionListener(detailPanel);
-        }
-        return detailPanel;
+    @Override
+    public void onExit() {
+        
     }
 
-} // @jve:decl-index=0:visual-constraint="10,10"
+    @Override
+    public ArrayList<MenuAction> createMenuitems() {
+        return null;
+    }
+    
+    @Override
+    public void setGuiEnable(boolean b) {
+        System.out.println("setGuiEnable");
+        if (b) {
+            ActionController.register(toolbarAction);
+            ToolbarController.setActions(ActionController.getActions());
+
+        } else {
+            ActionController.unRegister(toolbarAction);
+            ToolbarController.setActions(ActionController.getActions());
+        }
+    }
+    
+}
