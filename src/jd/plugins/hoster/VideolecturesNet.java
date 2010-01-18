ff3a0806e8c67f1f4566991abd863594df4e29c0
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-18 11:37:00 +0000

    Unfinished rtmp Plugin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10402 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/VideolecturesNet.java
@@ -0,0 +1,75 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision: 10393 $", interfaceVersion = 2, names = { "videolectures.net" }, urls = { "http://[\\w\\.]*?videolectures\\.net/.+" }, flags = { 0 })
+public class VideolecturesNet extends PluginForHost {
+
+    private String rtmpLink;
+
+    public VideolecturesNet(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://videolectures.net/site/about/";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        br.getPage(downloadLink.getDownloadURL());
+        
+        String[] data = br.getRegex("streamer: \"(.+?)/([^/]+?)\",.*?file: \"(.+?)\"").getRow(0);
+        downloadLink.setFinalFileName(br.getRegex("<title>(.+?)</title>").getMatch(0).trim()+".flv");
+        rtmpLink = data[0] + "/" + data[1] + "" + data[2];
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+     
+        // Download here 
+        /** See {@link #Ftp} for example download */
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 20;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
