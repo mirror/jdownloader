3f50563c12b431a4bffbc3b0b3ef2be946ee6182
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-20 21:39:43 +0000

    - removed compile error (we're under jre 1.5, no @override for interface methods ;-) ) - maybe we should commit the eclipse project settings to the repository ?? ^^
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10434 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/optional/awesomebar/awesome/AwesomeProposal.java
+++ src/jd/plugins/optional/awesomebar/awesome/AwesomeProposal.java
@@ -1,49 +1,49 @@
 package jd.plugins.optional.awesomebar.awesome;
 
 import java.awt.Component;
-import java.awt.Container;
-
 
 public class AwesomeProposal implements Comparable<AwesomeProposal> {
-	private final float relevance;
-	private final AwesomeProposalRequestListener source;
-	private final AwesomeProposalRequest request;
-	private final Component proposal;
-	private final Component proposalListElement;
-	private final Object actionid;
+    private final float relevance;
+    private final AwesomeProposalRequestListener source;
+    private final AwesomeProposalRequest request;
+    private final Component proposal;
+    private final Component proposalListElement;
+    private final Object actionid;
 
-	/**
-	 * @return the original proposal request
-	 */
-	public final AwesomeProposalRequest getRequest() {
-		return request;
-	}
-	/**
-	 * @return the match relevance. 1 is 100%
-	 */
-	public final float getRelevance() {
-		return relevance;
-	}
-	/**
-	 * @return the proposal message
-	 */
-	public final Component getProposal() {
-		return proposal;
-	}
+    /**
+     * @return the original proposal request
+     */
+    public final AwesomeProposalRequest getRequest() {
+        return request;
+    }
 
-	/**
-	 * @return the proposal message
-	 */
-	public final Component getProposalListElement() {
-		return proposalListElement;
-	}
+    /**
+     * @return the match relevance. 1 is 100%
+     */
+    public final float getRelevance() {
+        return relevance;
+    }
 
-	/**
-	 * @return return the source of the proposal
-	 */
-	public final AwesomeProposalRequestListener getSource() {
-		return source;
-	}
+    /**
+     * @return the proposal message
+     */
+    public final Component getProposal() {
+        return proposal;
+    }
+
+    /**
+     * @return the proposal message
+     */
+    public final Component getProposalListElement() {
+        return proposalListElement;
+    }
+
+    /**
+     * @return return the source of the proposal
+     */
+    public final AwesomeProposalRequestListener getSource() {
+        return source;
+    }
 
     /**
      * @return the actionid
@@ -52,53 +52,53 @@ public class AwesomeProposal implements Comparable<AwesomeProposal> {
         return actionid;
     }
 
-	public int compareTo(AwesomeProposal o) {
-		int ratingThis = 0;
-		int ratingCompare = 0;
-		final String command;
-		try {
-			this.request.getSource().commandLock.readLock().lock();
-			command = this.request.getSource().getCommand();
-		} finally {
-			this.request.getSource().commandLock.readLock().unlock();
-		}
+    public int compareTo(AwesomeProposal o) {
+        int ratingThis = 0;
+        int ratingCompare = 0;
+        final String command;
+        try {
+            this.request.getSource().commandLock.readLock().lock();
+            command = this.request.getSource().getCommand();
+        } finally {
+            this.request.getSource().commandLock.readLock().unlock();
+        }
 
-		for (String keyword : this.source.getKeywords()) {
-			while ((keyword.length() > ratingThis) && command.startsWith(keyword.substring(0, ratingThis + 1))) {
-				ratingThis++;
-			}
-		}
+        for (String keyword : this.source.getKeywords()) {
+            while ((keyword.length() > ratingThis) && command.startsWith(keyword.substring(0, ratingThis + 1))) {
+                ratingThis++;
+            }
+        }
 
-		for (String keyword : o.source.getKeywords()) {
-			while ((keyword.length() > ratingCompare) && (command.startsWith(keyword.substring(0, ratingCompare + 1)))) {
-				ratingCompare++;
-			}
-		}
+        for (String keyword : o.source.getKeywords()) {
+            while ((keyword.length() > ratingCompare) && (command.startsWith(keyword.substring(0, ratingCompare + 1)))) {
+                ratingCompare++;
+            }
+        }
 
-		if (ratingThis == ratingCompare) {
-			return Float.compare(o.getRelevance(), this.getRelevance());
-		} else {
-			return Integer.valueOf(ratingCompare).compareTo(ratingThis);
-		}
-	}
+        if (ratingThis == ratingCompare) {
+            return Float.compare(o.getRelevance(), this.getRelevance());
+        } else {
+            return Integer.valueOf(ratingCompare).compareTo(ratingThis);
+        }
+    }
 
-	public AwesomeProposal(AwesomeProposalRequestListener source, AwesomeProposalRequest request, Component proposal, Object actionid, float match) {
-	    this.source = source;
-		this.request = request;
-		this.proposal = proposal;
-		this.actionid = actionid;
-		this.proposalListElement = AwesomeUtils.createProposalListElement(this.source, this.request);
-		this.relevance = match;
-		this.request.getSource().addProposal(this);
-	}
+    public AwesomeProposal(AwesomeProposalRequestListener source, AwesomeProposalRequest request, Component proposal, Object actionid, float match) {
+        this.source = source;
+        this.request = request;
+        this.proposal = proposal;
+        this.actionid = actionid;
+        this.proposalListElement = AwesomeUtils.createProposalListElement(this.source, this.request);
+        this.relevance = match;
+        this.request.getSource().addProposal(this);
+    }
 
-	public AwesomeProposal(AwesomeProposalRequestListener source, AwesomeProposalRequest request, Component proposal, Object actionid, Component proposalListElement, float match) {
-		this.source = source;
-		this.request = request;
-		this.proposal = proposal;
-		this.actionid = actionid;
-		this.proposalListElement = proposalListElement;
-		this.relevance = match;
-		this.request.getSource().addProposal(this);
-	}
+    public AwesomeProposal(AwesomeProposalRequestListener source, AwesomeProposalRequest request, Component proposal, Object actionid, Component proposalListElement, float match) {
+        this.source = source;
+        this.request = request;
+        this.proposal = proposal;
+        this.actionid = actionid;
+        this.proposalListElement = proposalListElement;
+        this.relevance = match;
+        this.request.getSource().addProposal(this);
+    }
 }
