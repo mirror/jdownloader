a6facad2ddd1f17600f8221e7c7d99e5926640d1
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-24 21:11:22 +0000

    This file belongs to the restarter.jar DO NOT ADD add. dependencies.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10469 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/nutils/OutdatedParser.java
+++ src/jd/nutils/OutdatedParser.java
@@ -16,8 +16,6 @@
 
 package jd.nutils;
 
-import static jd.nutils.io.JDIO.removeDirectoryOrFile;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
@@ -27,23 +25,17 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import jd.parser.Regex;
-import jd.utils.StringUtil;
 
-public final class OutdatedParser {
-    /**
-     * Don't let anyone instantiate this class.
-     */
-    private OutdatedParser() {
-    }
+public class OutdatedParser {
 
     public static boolean parseFile(final File outdated) {
-        final String[] remove = Regex.getLines(getLocalFile(outdated));
-        final String homedir = outdated.getParent();
+        String[] remove = Regex.getLines(getLocalFile(outdated));
+        String homedir = outdated.getParent();
         boolean ret = true;
         File delete;
         File[] deletes;
         if (remove != null) {
-            for (final String file : remove) {
+            for (String file : remove) {
                 if (file.length() == 0) continue;
                 if (!file.matches(".*?" + File.separator + "?\\.+" + File.separator + ".*?")) {
                     if (file.contains("|")) {
@@ -57,7 +49,7 @@ public final class OutdatedParser {
                             }
 
                         });
-                        for (final File del : deletes) {
+                        for (File del : deletes) {
                             if (removeDirectoryOrFile(del)) {
                                 System.out.println("Removed " + del.getName() + " [" + file + "]");
                             } else {
@@ -77,7 +69,7 @@ public final class OutdatedParser {
                             }
 
                         });
-                        for (final File del : deletes) {
+                        for (File del : deletes) {
                             if (removeDirectoryOrFile(del)) {
                                 System.out.println("Removed " + del.getName() + " [" + file + "]");
                             } else {
@@ -101,16 +93,15 @@ public final class OutdatedParser {
         return ret;
     }
 
-    private static String getLocalFile(final File file) {
+    private static String getLocalFile(File file) {
         if (file == null) return null;
         if (!file.exists()) return "";
         try {
-            final BufferedReader f = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+            BufferedReader f = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
 
             String line;
             StringBuffer ret = new StringBuffer();
-            // final String sep = System.getProperty("line.separator");
-            final String sep = StringUtil.LINE_SEPARATOR;
+            String sep = System.getProperty("line.separator");
             while ((line = f.readLine()) != null) {
                 ret.append(line + sep);
             }
@@ -122,26 +113,26 @@ public final class OutdatedParser {
         return "";
     }
 
-//    private static boolean removeDirectoryOrFile(final File dir) {
-//        if (dir.isDirectory()) {
-//            final String[] children = dir.list();
-//            for (final String element : children) {
-//                boolean success = removeDirectoryOrFile(new File(dir, element));
-//                if (!success) return false;
-//            }
-//        }
-//        return dir.delete();
-//    }
+    private static boolean removeDirectoryOrFile(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (String element : children) {
+                boolean success = removeDirectoryOrFile(new File(dir, element));
+                if (!success) return false;
+            }
+        }
+        return dir.delete();
+    }
 
-    private static ArrayList<String> parseHashList(final File file) {
+    private static ArrayList<String> parseHashList(File file) {
         if (!file.exists()) {
             System.out.println("HashList not available");
             return null;
         }
 
-        final String[] matches = new Regex(getLocalFile(file), "[\r\n\\;]*([^=]+)=(.*?)\\;").getColumn(0);
-        final ArrayList<String> result = new ArrayList<String>();
-        for (final String match : matches) {
+        String[] matches = new Regex(getLocalFile(file), "[\r\n\\;]*([^=]+)=(.*?)\\;").getColumn(0);
+        ArrayList<String> result = new ArrayList<String>();
+        for (String match : matches) {
             result.add(match);
         }
         return result;
