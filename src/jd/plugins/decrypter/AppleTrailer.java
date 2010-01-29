497ef5ad5199c37cea3df1c9e939b597ce87f985
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-01-29 01:38:30 +0000

    AppleTrailers: plugin to download trailers from apple :)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10491 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/AppleTrailer.java
@@ -0,0 +1,51 @@
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision: 7387 $", interfaceVersion = 2, names = { "apple.com" }, urls = { "http://[\\w\\.]*?apple\\.com/trailers/[a-zA-Z0-9_/]+/" }, flags = { 0 })
+public class AppleTrailer extends PluginForDecrypt {
+
+    public AppleTrailer(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        br.getPage(parameter.toString());
+        String[] hits = br.getRegex("class=\"hd\".*?href=\"(http://.*?apple.*?_h?\\d+p\\.mov)\"").getColumn(0);
+        if (hits.length == 0) {
+            /* custom trailer page */
+            br.getPage(parameter.toString() + "/hd/");
+        }
+        hits = br.getRegex("class=\"hd\".*?href=\"(http://.*?apple.*?_h?\\d+p\\.mov)\"").getColumn(0);
+        if (hits.length == 0) return decryptedLinks;
+        String title = br.getRegex("var trailerTitle = '(.*?)';").getMatch(0);
+        FilePackage fp = FilePackage.getInstance();
+        if (title != null) fp.setName(title.trim() + " Trailers");
+        for (String hit : hits) {
+            String url = hit.replaceFirst("http://.*?\\.com/", "http://www.apple.com/");
+            String format = new Regex(url, "_h?(\\d+)p").getMatch(0);
+            if (format == null) continue;
+            String size = br.getRegex("class=\"hd\".*?>" + format + "p \\((\\d+ ?MB)\\)").getMatch(0);
+            /* TODO: get correct size for custom trailer page */
+            url = url.replaceFirst("_h?" + format, "_h" + format);
+            DownloadLink dlLink = createDownloadlink(url);
+            if (size != null) dlLink.setDownloadSize(Regex.getSize(size));
+            dlLink.setAvailable(true);
+            decryptedLinks.add(dlLink);
+        }
+        if (title != null) fp.addLinks(decryptedLinks);
+        return decryptedLinks;
+    }
+
+}
