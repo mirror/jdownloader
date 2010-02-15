71cb66f0707cac6a07bf3ace0f01dd1636029c36
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-02-15 12:53:24 +0000

    -Cleaned the ifile plugin and added a folder decrypter closes #1264
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@10589 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/IFileItFldr.java
@@ -0,0 +1,73 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ifile.it" }, urls = { "http://[\\w\\.]*?ifile\\.it/_[a-z0-9]+" }, flags = { 0 })
+public class IFileItFldr extends PluginForDecrypt {
+
+    public IFileItFldr(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.getPage(parameter);
+        String fpName = br.getRegex("<title>(.*?)\\| ifile\\.it</title>").getMatch(0);
+        if (fpName == null) fpName = br.getRegex("<legend>(.*?)</legend>").getMatch(0);
+        String[] linkinformation = br.getRegex("<tr class=\"context_row\" id=\"row.*?\">(.*?)</tr>").getColumn(0);
+        boolean fail = false;
+        if (linkinformation == null || linkinformation.length == 0) {
+            fail = true;
+            linkinformation = br.getRegex("class=\"file_link\" href=\"(http.*?)\"").getColumn(0);
+        }
+        if (linkinformation == null || linkinformation.length == 0) return null;
+        for (String info : linkinformation) {
+            String filename = new Regex(info, "class=\"file_name\" id=\".*?\">(.*?)</span>").getMatch(0);
+            String filesize = new Regex(info, "<td align=\"center\" valign=\"middle\"  class=\"(even|odd)\">(.*?)</td>").getMatch(1);
+            String filelink = new Regex(info, "class=\"file_link\" href=\"(http.*?)\"").getMatch(0);
+            if (fail) filelink = info;
+            DownloadLink dl = createDownloadlink(filelink);
+            if (filename != null && filesize != null) dl.setAvailable(true);
+            if (filesize != null) dl.setDownloadSize(Regex.getSize(filesize.trim()));
+            if (filename != null) dl.setName(filename.trim());
+            if (filelink != null) {
+                decryptedLinks.add(dl);
+            } else {
+                logger.warning("filelink for \"" + info + "\" could not be found!");
+            }
+        }
+        if (fpName != null) {
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(fpName.trim());
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+
+}
--- src/jd/plugins/hoster/IFileIt.java
+++ src/jd/plugins/hoster/IFileIt.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 
 import jd.PluginWrapper;
 import jd.http.Browser;
+import jd.http.RandomUserAgent;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.parser.html.Form;
@@ -33,10 +34,10 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ifile.it" }, urls = { "http://[\\w\\.]*?ifile\\.it/[\\w]+/?" }, flags = { 2 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ifile.it" }, urls = { "http://[\\w\\.]*?ifile\\.it/[a-z0-9]+" }, flags = { 2 })
 public class IFileIt extends PluginForHost {
 
-    private String useragent = "Mozilla/5.0 (Windows; U; Windows NT 6.0; chrome://global/locale/intl.properties; rv:1.8.1.12) Gecko/2008102920  Firefox/3.0.0";
+    private String useragent = RandomUserAgent.generate();
 
     public IFileIt(PluginWrapper wrapper) {
         super(wrapper);
@@ -87,62 +88,7 @@ public class IFileIt extends PluginForHost {
         br.setDebug(true);
         br.setFollowRedirects(true);
         br.getPage(downloadLink.getDownloadURL());
-        String downlink = br.getRegex("var.*?fsa.*?=.*?'(.*?)'").getMatch(0);
-        String downid = br.getRegex("var.*?fs =.*?'(.*?)'").getMatch(0);
-        String esn = br.getRegex("var.*?esn.*?=.*?(\\d+);<").getMatch(0);
-        if (downlink == null || esn == null || downid == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        // Example how current links(last updated plugin-links) look(ed) like
-        // http://ifile.it/download:dl_request?x64=741603&type=na&esn=1&b3da197159a22d301ad99f58f8137557=9bf31c7ff062936a96d3c8bd1f8f2ff3
-        String finaldownlink = "http://ifile.it/download:dl_request?" + downlink + "&type=na&esn=" + esn + "&" + downid;
-        xmlrequest(br, finaldownlink);
-        if (!br.containsHTML("status\":\"ok\"")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        br.getPage("http://ifile.it/dl");
-        if (br.containsHTML("download:captcha")) {
-            Browser br2 = br.cloneBrowser();
-            for (int i = 0; i <= 5; i++) {
-                String captchashit = br.getRegex("url \\+=.*?\\+.*?\\+.*?\"(.*?)\"").getMatch(0);
-                String captchacrap = br.getRegex("var.*?x.*?c = '(.*?)'").getMatch(0);
-                if (captchashit == null || captchacrap == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-                String code = getCaptchaCode("http://ifile.it/download:captcha?0." + Math.random(), downloadLink);
-                String captchaget = "http://ifile.it/download:dl_request?" + downlink + "&type=simple&esn=0&" + captchacrap + "=" + code + "&" + downid + captchashit;
-                logger.info("Captchagetpage = " + captchaget);
-                // Example of the last working captchaget
-                // http://ifile.it/download:dl_request?x65=549427&type=simple&esn=1&8a1e7=9fa&920e4e7d3666c587258c93ef87cb3365=a8c5e3fdae3471388ec44741b41b3c2d&d51500b7a7cd5292d9db0b98dc022447=98f13708210194c475687be6106a3b84
-                xmlrequest(br2, captchaget);
-                if (br2.containsHTML("\"retry\":\"retry\"")) continue;
-                br.getPage("http://ifile.it/dl");
-                break;
-            }
-            if (br2.containsHTML("\"retry\":\"retry\"")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
-        }
-        String dllink = br.getRegex("req_btn.*?target=\".*?\" href=\"(http.*?)\"").getMatch(0);
-        if (dllink == null) {
-            logger.info("first try getting dllink failed");
-            dllink = br.getRegex("\"(http://s[0-9]+\\.ifile\\.it/.*?/.*?/.*?/.*?)\"").getMatch(0);
-            if (dllink == null) {
-                logger.info("second try getting dllink failed");
-                String pp = br.getRegex("<br /><br />(.*?)</div>").getMatch(0);
-                String[] lol = HTMLParser.getHttpLinks(pp, "");
-                if (lol.length != 1) {
-                } else {
-                    for (String link : lol) {
-                        dllink = link;
-                    }
-                }
-            }
-        }
-        if (dllink == null) {
-            logger.info("last try getting dllink failed, plugin must be defect!");
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        br.setFollowRedirects(false);
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 1);
-        if (dl.getConnection().getContentType().contains("html")) {
-            br.followConnection();
-            if (dl.getConnection().getResponseCode() == 503) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 10 * 60 * 1000l);
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
+        doFree(downloadLink);
     }
 
     @Override
@@ -182,7 +128,12 @@ public class IFileIt extends PluginForHost {
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
         /* Nochmals das File überprüfen */
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
         requestFileInformation(downloadLink);
+        doFree(downloadLink);
+    }
+
+    public void doFree(DownloadLink downloadLink) throws Exception, PluginException {
         br.setDebug(true);
         br.setFollowRedirects(true);
         String downlink = br.getRegex("var.*?fsa.*?=.*?'(.*?)'").getMatch(0);
