898962cf37c10ec8572ed15e60dc124031e096ad
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-04-19 15:44:30 +0000

    -Added support for u115 closes #1614 -Added support for webcams.com closes #1615
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11155 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/U115Com.java
@@ -0,0 +1,171 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.config.ConfigContainer;
+import jd.config.ConfigEntry;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.utils.locale.JDL;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "u.115.com" }, urls = { "http://[\\w\\.]*?u\\.115\\.com/file/[a-z0-9]+" }, flags = { 2 })
+public class U115Com extends PluginForHost {
+
+    private static String u115servers = "u115servers";
+
+    /** The list of servers displayed in the plugin configuration pane */
+    // private static final String[] U115_SERVERS = new String[] { "cnc", "tel",
+    // "bak" };
+    private static final String[] U115_SERVERS;
+
+    static {
+        U115_SERVERS = new String[] { "cnc", "tel", "bak" };
+    }
+
+    public U115Com(PluginWrapper wrapper) {
+        super(wrapper);
+        setConfigElements();
+        this.setStartIntervall(5000l);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://u.115.com/tos.html";
+    }
+
+    private void setConfigElements() {
+        config.addEntry(new ConfigEntry(ConfigContainer.TYPE_COMBOBOX_INDEX, getPluginConfig(), u115servers, U115_SERVERS, JDL.L("plugins.host.U115Com.servers", "Use this server: ")).setDefaultValue(0));
+    }
+
+    private int getConfiguredServer() {
+        switch (getPluginConfig().getIntegerProperty(u115servers, -1)) {
+        case 0:
+            logger.fine("The server " + U115_SERVERS[0] + " is configured");
+            return 0;
+        case 1:
+            logger.fine("The server " + U115_SERVERS[1] + " is configured");
+            return 1;
+        case 2:
+            logger.fine("The server " + U115_SERVERS[2] + " is configured");
+            return 2;
+        default:
+            logger.fine("No server is configured, returning default server [" + U115_SERVERS[2] + "]");
+            return 2;
+        }
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        br.setCustomCharset("utf-8");
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        if (!br.containsHTML("class=\"alert-box\"")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("file_name = '(.*?)';").getMatch(0);
+        if (filename == null) {
+            filename = br.getRegex("<title>(.*?)- 115网络U盘").getMatch(0);
+            if (filename == null) {
+                filename = br.getRegex("class=\"nowrap file-name [a-z]+\">(.*?)</h2>").getMatch(0);
+            }
+        }
+        String filesize = br.getRegex("文件大小：(.*?)\\\\r\\\\n").getMatch(0);
+        if (filesize == null) filesize = br.getRegex("文件大小：(.*?)</td>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        filesize = filesize.replace(",", "");
+        link.setName(filename);
+        link.setDownloadSize(Regex.getSize(filesize));
+        String sh1 = br.getRegex("nsha1：([A-Z0-9]+)\\\\r\\\\n").getMatch(0);
+        if (sh1 == null) sh1 = br.getRegex("colspan=\"4\">SHA1：(A-Z0-9]+)").getMatch(0);
+        if (sh1 != null) link.setSha1Hash(sh1);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        requestFileInformation(link);
+        String dllink = findLink();
+        if (dllink == null) dllink = findLink2();
+        if (dllink == null) {
+            logger.warning("dllink is null, seems like the regexes are defect!");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dllink = Encoding.urlDecode(dllink, true);
+        dllink = Encoding.htmlDecode(dllink);
+        int maxchunks = 0;
+        int chosenServer = getConfiguredServer();
+        if (chosenServer == 0)
+            maxchunks = -2;
+        else if (chosenServer == 1) maxchunks = -3;
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            if (br.containsHTML("(help_down.html|onclick=\"WaittingManager|action=submit_feedback|\"report_box\"|UploadErrorMsg)")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error", 10 * 60 * 1000l);
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    public String findLink() throws Exception {
+        int chosenServer = getConfiguredServer();
+        String serverext = U115_SERVERS[chosenServer];
+        String finalRegex = "(http://\\d+\\." + serverext + "\\.\\d+cdn\\.com(:\\d+)?/.*?\\?key=.*?\\&key1=.*?\\&file=.*?\\&key2=[a-z0-9]+)";
+        String dllink = br.getRegex(finalRegex).getMatch(0);
+        if (dllink == null) logger.info("Dllink for chosen server " + U115_SERVERS[chosenServer] + " couldn't be found");
+        return dllink;
+    }
+
+    public String findLink2() throws Exception {
+        String dllink = null;
+        for (int i = 0; i <= U115_SERVERS.length - 1; i++) {
+            String serverext = U115_SERVERS[i];
+            String finalRegex = "(http://\\d+\\." + serverext + "\\.(\\d+cdn|s\\d+\\.115)\\.com(:\\d+)?/.*?\\?key=.*?\\&key1=.*?\\&file=.*?\\&key2=[a-z0-9]+)";
+            dllink = br.getRegex(finalRegex).getMatch(0);
+            if (dllink != null) break;
+        }
+        return dllink;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        int maxdls = -1;
+        int chosenServer = getConfiguredServer();
+        if (chosenServer == 0)
+            maxdls = 1;
+        else if (chosenServer == 1) maxdls = 2;
+        return maxdls;
+    }
+
+}
---
+++ src/jd/plugins/hoster/WebCamsCom.java
@@ -0,0 +1,99 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "webcams.com" }, urls = { "http://[\\w\\.]*?webcams\\.com/index\\.php\\?action=ModelPage\\&page=movie\\&model_id=\\d+\\&media_id=\\d+(\\&mmcat_id=\\d+)?" }, flags = { 0 })
+public class WebCamsCom extends PluginForHost {
+
+    public WebCamsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public String dllink = null;
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.webcams.com/index.php?page=terms";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(false);
+        br.getPage(downloadLink.getDownloadURL());
+        if (!br.containsHTML("swfobject.js")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String model = new Regex(downloadLink.getDownloadURL(), "model_id=(\\d+)").getMatch(0);
+        String mediaid = new Regex(downloadLink.getDownloadURL(), "media_id=(\\d+)").getMatch(0);
+        if (model == null || mediaid == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dllink = br.getRegex("name=\"flashvars\" value=\"file=(http.*?\\.flv).*?\"").getMatch(0);
+        if (dllink == null) dllink = new Regex(Encoding.urlDecode(br.toString(), false), "(http://static\\d+\\.webcams\\.com/images/models/.*?\\.flv)").getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        dllink = Encoding.urlDecode(dllink, true);
+        downloadLink.setFinalFileName("Model " + model + " movie " + mediaid + ".flv");
+        Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = br2.openGetConnection(dllink);
+        if (!con.getContentType().contains("html"))
+            downloadLink.setDownloadSize(con.getLongContentLength());
+        else
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
