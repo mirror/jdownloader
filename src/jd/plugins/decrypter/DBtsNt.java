5d57d24ca699187d50ba26e3f52298f350a21f50
Gamewalker <Gamewalker@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-06-05 14:57:29 +0000

    Erweitert um hinzufügen von Artisten und Events, ist aber noch unschön da der Linkgrabber gleiche links nicht erkennt auf verschiedenen Hostern
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11605 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/DBtsNt.java
+++ src/jd/plugins/decrypter/DBtsNt.java
@@ -16,18 +16,19 @@
 
 package jd.plugins.decrypter;
 
-import java.util.ArrayList;
-
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
-import jd.plugins.CryptedLink;
-import jd.plugins.DecrypterPlugin;
-import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
-import jd.plugins.PluginForDecrypt;
+import jd.parser.Regex;
+import jd.plugins.*;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "audiobeats.net" }, urls = { "http://[\\w\\.]*?audiobeats\\.net/liveset\\?id=\\d+" }, flags = { 0 })
+import java.io.IOException;
+import java.util.ArrayList;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "audiobeats.net" }, urls = { "http://[\\w\\.]*?audiobeats\\.net/(liveset|link|event|artist)\\?id=\\d+" }, flags = { 0 })
 public class DBtsNt extends PluginForDecrypt {
+    
+    private String fpName;
+    private ArrayList<DownloadLink> decryptedLinks;
 
     public DBtsNt(PluginWrapper wrapper) {
         super(wrapper);
@@ -35,29 +36,96 @@ public class DBtsNt extends PluginForDecrypt {
 
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        br.getPage(parameter.toString());
-        String fpName = br.getRegex("<title>AudioBeats\\.net - (.*?)</title>").getMatch(0);
-        if (fpName == null) fpName = br.getRegex("rel=\"alternate\" type=\".*?\" title=\"(.*?)\"").getMatch(0);
-        String[] allLinks = br.getRegex("\"(/link\\?id=\\d+)\"").getColumn(0);
-        if (allLinks == null || allLinks.length == 0) return null;
-        progress.setRange(allLinks.length);
-        for (String aLink : allLinks) {
-            br.setFollowRedirects(false);
-            br.getPage("http://www.audiobeats.net" + aLink);
-            if (br.getRedirectLocation() == null) {
-                logger.warning("Decrypter must be defect, link = " + parameter.toString());
-                return null;
+        decryptedLinks = new ArrayList<DownloadLink>();
+
+        //TODO: beim hinzufügen von Events oder Artisten etc. sollte der Linkgrabber gleiche links auch als gleiche Links erkennen
+
+        Regex urlInfo = new Regex(parameter, "http://[\\w\\.]*?audiobeats\\.net/(liveset|link|event|artist)\\?id=(\\d+)");
+        String type = urlInfo.getMatch(0);
+        String id = urlInfo.getMatch(1);
+
+        if (type.equals("link")) {
+
+            progress.increase(1);            
+            String link = "/link?id=" + id;
+            if (!decryptSingleLink(parameter.toString(), progress, decryptedLinks, link)) return null;
+        } else if(type.equals("liveset")) {
+
+            if (fpName == null) fpName = br.getRegex("<title>AudioBeats\\.net - (.*?)</title>").getMatch(0);
+            
+            if (!decryptLiveset(parameter.toString(), progress, decryptedLinks)) return null;
+        } else if(type.equals("event") || type.equals("artist")) {
+            
+            br.getPage(parameter.toString());
+            String[] allLivesets = br.getRegex("\"(/liveset\\?id=\\d+)\"").getColumn(0);
+
+            if (allLivesets == null || allLivesets.length == 0) return null;
+            if (fpName == null) fpName = br.getRegex("<title>AudioBeats\\.net - (.*?)</title>").getMatch(0);
+
+            progress.setRange(allLivesets.length);
+            for (String aLiveset : allLivesets) {
+                if (!decryptLiveset(aLiveset, progress, decryptedLinks)) return null;
+                Thread.sleep(1000);
             }
-            decryptedLinks.add(createDownloadlink(br.getRedirectLocation()));
-            progress.increase(1);
         }
+
+        return decryptedLinks;
+    }
+
+    private boolean decryptLiveset(String parameter, ProgressController progress, ArrayList<DownloadLink> decryptedLinks) throws IOException {
+        int i = 0;
+        do{
+            br.getPage(parameter);
+
+            if(br.containsHTML("Error 403")) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            } else {
+
+                String[] allLinks = br.getRegex("\"(/link\\?id=\\d+)\"").getColumn(0);
+
+                if (allLinks == null || allLinks.length == 0) return false;
+                progress.setRange(progress.getMax() + allLinks.length);
+                for (String aLink : allLinks) {
+                    if (!decryptSingleLink(parameter, progress, decryptedLinks, aLink)) return false;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {}
+                }
+
+                String fpName = br.getRegex("<title>AudioBeats\\.net - (.*?)</title>").getMatch(0);
+                if (fpName == null) fpName = br.getRegex("rel=\"alternate\" type=\".*?\" title=\"(.*?)\"").getMatch(0);
+
+                setPackageName();
+                break;
+            }
+
+        } while(i<5);
+
+        return true;
+    }
+
+    private void setPackageName() {
         if (fpName != null) {
+
             FilePackage fp = FilePackage.getInstance();
             fp.setName(fpName.trim());
             fp.addLinks(decryptedLinks);
         }
-        return decryptedLinks;
+    }
+
+    private boolean decryptSingleLink(String parameter, ProgressController progress, ArrayList<DownloadLink> decryptedLinks, String aLink) throws IOException {
+        br.setFollowRedirects(false);
+        br.getPage(aLink);
+        if (br.getRedirectLocation() == null) {
+            logger.warning("Decrypter must be defect, link = " + parameter);
+            return false;
+        }
+        decryptedLinks.add(createDownloadlink(br.getRedirectLocation()));
+        progress.increase(1);
+
+        return true;
     }
 
 }
