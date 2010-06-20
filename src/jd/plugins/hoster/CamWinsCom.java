d838dc403a52d79534053822dfdcd6ea803397c6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-06-20 15:46:01 +0000

    -Added 2 new plugins for anybee to fix -hosterplugin for camwins refs #1926 refs #1956 -hosterplugin for tgf-services refs #1948 refs #1956
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11759 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/CamWinsCom.java
@@ -0,0 +1,112 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "camwins.com" }, urls = { "http://[\\w\\.]*?camwins\\.com/video/\\d+" }, flags = { 0 })
+public class CamWinsCom extends PluginForHost {
+
+    public CamWinsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    private String dllink = null;
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.camwins.com/static/terms";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        boolean pluginUnfinished = true;
+        if (pluginUnfinished) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "Plugin unfinished");
+        br.setFollowRedirects(false);
+        // G-Zip browser error when accessing that page...
+        br.getPage("http://www.camwins.com/enter.php");
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.getRedirectLocation() != null) {
+            if (br.getRedirectLocation().contains("error/video_missing")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        String filename = br.getRegex("<h1>(.*?)</h1>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<meta name=\"description\" content=\"(.*?)\"").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dllink = downloadLink.getProperty("dllink").toString();
+        if (dllink == null) {
+            String vidKey = new Regex(downloadLink.getDownloadURL(), "camwins\\.com/video/(\\d+)").getMatch(0);
+            br.getPage("http://www.camwins.com/media/player/config.php?vkey=" + vidKey);
+            dllink = br.getRegex("<src>(.*?)</src>").getMatch(0);
+            if (dllink == null) dllink = br.getRegex("(http://media\\d+\\.camwins\\.com/media/videos/flv/\\d+\\.flv)").getMatch(0);
+            if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            dllink = dllink.trim();
+            downloadLink.setProperty("dllink", dllink);
+        }
+        filename = filename.trim();
+        downloadLink.setFinalFileName(filename + ".flv");
+        Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = br2.openGetConnection(dllink);
+        if (!con.getContentType().contains("html"))
+            downloadLink.setDownloadSize(con.getLongContentLength());
+        else
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
---
+++ src/jd/plugins/hoster/TgfServicesCom.java
@@ -0,0 +1,119 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.File;
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.utils.JDUtilities;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "tgf-services.com" }, urls = { "http://[\\w\\.]*?tgf-services\\.com/UserDownloads/.+" }, flags = { 0 })
+public class TgfServicesCom extends PluginForHost {
+
+    public TgfServicesCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://about.socifiles.com/terms-of-service";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(false);
+        br.getPage(link.getDownloadURL());
+        if (br.getRedirectLocation() != null) {
+            if (br.getRedirectLocation().contains("/Warning/?err_num=15")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        String filename = br.getRegex("<h2>Part \\d+\\.</h2></td>[\t\r\n ]+<td><h2>(.*?)</h2></td>").getMatch(0);
+        String filesize = br.getRegex("<h2>File size:</h2></td>[\t\n\r ]+<td><h2>(.*?)</h2></td>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        String ext = br.getRegex("<h2>Bitrate: \\d+, Frequency: \\d+, Mode: (Stereo|Mono), (.*?)</h2>").getMatch(1);
+        if (ext != null) filename += "." + ext;
+        link.setName(filename.trim());
+        link.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        boolean pluginUnfinished = true;
+        if (pluginUnfinished) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "Plugin unfinished");
+        String reCaptchaID = br.getRegex("\\?k=(.*?)\"").getMatch(0);
+        if (reCaptchaID == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        br.getPage("http://tgf-services.com/js/scripts.js");
+        String waittime = br.getRegex("secs=(\\d+);").getMatch(0);
+        PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+        jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+        rc.setId(reCaptchaID);
+        rc.load();
+        File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+        String c = getCaptchaCode(cf, downloadLink);
+        // Those requests seem to be wrong anybee
+        String postURL = "http://tgf-services.com/pages/checkCapture.php?start=1&recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + Encoding.urlEncode(c) + "&PHPSESSID=" + br.getCookie("http://tgf-services.com", "PHPSESSID") + "&" + System.currentTimeMillis() * 10 + "-xml";
+        br.postPage(postURL, "dump=1");
+        if (!br.containsHTML("'responce_result': 'GOOD\\!\\!\\!\\!\\!'")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        System.out.print(br.toString());
+        // Ticket Time
+        int tt = 50;
+        if (waittime != null) {
+            logger.info("Waittime detected, waiting " + waittime + " seconds from now on...");
+            tt = Integer.parseInt(waittime);
+        }
+        sleep(tt * 1001, downloadLink);
+        String theHash = new Regex(downloadLink.getDownloadURL(), "tgf-services\\.com/UserDownloads/(.+)/").getMatch(0);
+        if (theHash == null) theHash = new Regex(downloadLink.getDownloadURL(), "tgf-services\\.com/UserDownloads/(.+)").getMatch(0);
+        String postURL2 = "http://tgf-services.com/pages/getDownloadPage.php?start=1&hash=" + theHash + "&PHPSESSID=" + br.getCookie("http://tgf-services.com", "PHPSESSID") + "&" + System.currentTimeMillis() * 10 + "-xml";
+        br.postPage(postURL2, "dump=1");
+        System.out.print(br.toString());
+        String dllink = null;
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
