68acbee27145c3680d181105c0d0c612f4e02c4a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-06-24 20:20:20 +0000

    -Added support for extradj closes #1950
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11787 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/ExtraDjCom.java
@@ -0,0 +1,94 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "extradj.com" }, urls = { "http://[\\w\\.]*?extradj.(net\\.ua|com)/music/.+/.+" }, flags = { 0 })
+public class ExtraDjCom extends PluginForHost {
+
+    public ExtraDjCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "info@extradj.com";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(false);
+        br.getPage(link.getDownloadURL());
+        // Link has changed ? Also change it in the plugin!
+        if (br.getRedirectLocation() != null) {
+            link.setUrlDownload(br.getRedirectLocation());
+            br.getPage(link.getDownloadURL());
+        }
+        if (br.containsHTML("<p><nobr>Извините, файл не найден. Попробуйте поискать на<nobr><b>")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<p><b>Имя файла:</b>(.*?)</p>").getMatch(0);
+        if (filename == null) filename = br.getRegex("trackPageview\\(\"downloads/(.*?)\"\\)").getMatch(0);
+        String filesize = br.getRegex("<p><b>Размер:</b>(.*?)\\.</p>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(filename.trim());
+        filesize = filesize.replace("Г", "G");
+        filesize = filesize.replaceAll("(М|м)", "M");
+        filesize = filesize.replace("к", "k");
+        filesize = filesize.replaceAll("(Б|б)", "");
+        filesize = filesize + "b";
+        link.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        String dllink = br.getRegex("id=\"url\" value=\"(.*?)\"").getMatch(0);
+        if (dllink == null) dllink = br.getRegex("\"(http://extradj\\.net\\.ua/music/[a-z0-9]+/.*?)\"").getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
