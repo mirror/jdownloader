4daca7367dbb40bf8d187e9f66af16f494b1e5b3
DwD <DwD@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-07-11 11:34:02 +0000

    new Captcha method uploadbox
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11978 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/specials/UploadBox.java
+++ src/jd/captcha/specials/UploadBox.java
@@ -21,7 +21,6 @@ import java.awt.Color;
 import jd.captcha.pixelgrid.Captcha;
 import jd.captcha.pixelgrid.Letter;
 import jd.captcha.pixelgrid.PixelGrid;
-import jd.nutils.Colors;
 
 /**
  * 
@@ -29,101 +28,107 @@ import jd.nutils.Colors;
  * @author JD-Team
  */
 public class UploadBox {
-	private static void clean(Captcha captcha) {
-		int[][] newgrid = new int[captcha.getWidth()][captcha.getHeight()];
-		Color lastC = null;
-		int[] lastpos = new int[] {0,0};
-		for (int x = 0; x < captcha.getWidth(); x++) {
-			for (int y = 0; y < captcha.getHeight(); y++) {
+    private static void clean(Captcha captcha) {
+        int[][] newgrid = new int[captcha.getWidth()][captcha.getHeight()];
+        int dist = 2;
+        for (int x = 0; x < captcha.getWidth(); x++) {
+            for (int y = 0; y < captcha.getHeight(); y++) {
 
-				int p = captcha.getPixelValue(x, y);
+                int p = captcha.getPixelValue(x, y);
 
-				Color c = new Color(p);
+                Color c = new Color(p);
 
+                if (c.getBlue() > 250 && c.getRed() > 250 && c.getGreen() > 250) {
+                    PixelGrid.setPixelValue(x, y, newgrid, Color.white.getRGB());
+                } else {
+                    if (c.getBlue() > 120 && c.getRed() > 120 && c.getGreen() > 120) {
+                        int backg = 0;
+                        int fordg = 0;
+                        for (int x1 = x - dist; x1 < x + dist; x1++) {
+                            for (int y1 = y - dist; y1 < y + dist; y1++) {
+                                if (x1 > 0 && y1 > 0 && x1 < captcha.getWidth() && y1 < captcha.getHeight()) {
+                                    int p1 = captcha.getPixelValue(x1, y1);
 
-				if (c.getBlue() > 180 && c.getRed() > 180 && c.getGreen() > 180) {
-					if (lastC == null || !((Math.abs(lastpos[0]-x)+Math.abs(lastpos[1]-y))<1 && Colors.getColorDifference(lastC.getRGB(), c.getRGB()) < 80))
-						PixelGrid.setPixelValue(x, y, newgrid, captcha
-								.getMaxPixelValue());
-					else
-						newgrid[x][y] = captcha.grid[x][y];
-				} else {
-					newgrid[x][y] = captcha.grid[x][y];
-					lastC = c;
-					lastpos=new int[] {x,y};
-				}
+                                    Color c1 = new Color(p1);
+                                    if (c1.getBlue() > 250 && c1.getRed() > 250 && c1.getGreen() > 250)
+                                        backg++;
+                                    else
+                                        fordg++;
+                                }
+                            }
+                        }
+                        if (backg * 7 > fordg)
+                            PixelGrid.setPixelValue(x, y, newgrid, 0xffffff);
+                        else
+                            PixelGrid.setPixelValue(x, y, newgrid, 0xff0000);
 
-			}
-		}
-		captcha.grid = newgrid;
+                    } else
+                        PixelGrid.setPixelValue(x, y, newgrid, 0x000000);
+                }
 
-	}
-	private static int getGapAt(Captcha captcha, int position)
-	{
-		int avg = captcha.getAverage();
-		int empty = -1;
-		for (int x = position-5; x < position+5; x++) {
-			if(captcha.getPixelValue(x, 25)!=avg)
-			{
-				if(empty==-1)
-					empty++;
-				else
-					return x;
-			}
-			else
-			{
-				if(empty>5)
-					empty=-1;
-				else
-					empty++;
-			}
-				
-		}
-		for (int x = position-5; x < position+5; x++) {
-			if(captcha.getPixelValue(x, 24)!=avg)
-			{
-				if(empty==-1)
-					empty++;
-				else
-					return x;
-			}
-			else
-			{
-				if(empty>5)
-					empty=-1;
-				else
-					empty++;
-			}
-				
-		}
-		for (int x = position-5; x < position+5; x++) {
-			if(captcha.getPixelValue(x, 26)!=avg)
-			{
-				if(empty==-1)
-					empty++;
-				else
-					return x;
-			}
-			else
-			{
-				if(empty>5)
-					empty=-1;
-				else
-					empty++;
-			}
-				
-		}
-		return position;
-	}
-	private static int[] getGaps(Captcha captcha) {
-		return new int[] {getGapAt(captcha, 25), getGapAt(captcha, 50), getGapAt(captcha, 75), getGapAt(captcha, 100), getGapAt(captcha, 125), 150};
-	}
-	public static Letter[] getLetters(Captcha captcha) {
-		clean(captcha);
-		captcha.removeSmallObjects(0.75, 0.75, 6);
-		captcha.toBlackAndWhite(0.95);
-		return captcha.getLetters(6, getGaps(captcha));
+            }
+        }
+        captcha.grid = newgrid;
 
-	}
+    }
+
+    private static int getGapAt(Captcha captcha, int position) {
+        int white = 0xffffff;
+        int red = 0xff0000;
+
+        int dist = 4;
+        int lastBackground = -1;
+        int gabX = position;
+        for (int x = Math.max(0, position - dist); x < Math.min(position + dist, captcha.getWidth()); x++) {
+            int background = 0;
+            for (int y = 0; y < captcha.getHeight(); y++) {
+                int col = captcha.getPixelValue(x, y);
+                if (col == white)
+                    background += 3;
+                else if (col == red) background++;
+
+            }
+            if (lastBackground < background) {
+                lastBackground = background;
+                gabX = x;
+            }
+        }
+        return gabX;
+    }
+
+    @SuppressWarnings("unused")
+    private static int[] getGaps(Captcha captcha) {
+        int x = 0;
+        int white = 0xffffff;
+
+        outer: for (; x < captcha.getWidth(); x++) {
+            for (int y = 0; y < captcha.getHeight(); y++) {
+
+                if (captcha.getPixelValue(x, y) != white) break outer;
+            }
+        }
+        // System.out.println("asdf:" + x);
+        int dist = 22;
+        int[] gabs = new int[] { x = getGapAt(captcha, x + dist * 2), x = getGapAt(captcha, x + dist), x = getGapAt(captcha, x + dist), x = getGapAt(captcha, x + dist), x = getGapAt(captcha, x + dist), x = getGapAt(captcha, x + dist) };
+
+        return gabs;
+    }
+
+    public static Letter[] getLetters(Captcha captcha) {
+
+        clean(captcha);
+        captcha.removeSmallObjects(0.75, 0.75, 6);
+        // captcha.toBlackAndWhite(0.95);
+
+        for (int x = 0; x < captcha.getWidth(); x++) {
+            for (int y = 0; y < captcha.getHeight(); y++) {
+
+                if (captcha.getPixelValue(x, y) == 0xff0000) captcha.setPixelValue(x, y, 0x000000);
+            }
+        }
+
+        return EasyCaptcha.getLetters(captcha);
+
+    }
 
 }
