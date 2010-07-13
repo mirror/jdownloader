3bdff13f22a17911c28d817405508d99ec82ff7c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-07-13 00:51:26 +0000

    -Added premium support for usershare closes #2047
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@11990 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Usershare.java
+++ src/jd/plugins/hoster/Usershare.java
@@ -16,9 +16,14 @@
 
 package jd.plugins.hoster;
 
+import java.util.regex.Pattern;
+
 import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
@@ -27,12 +32,15 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "usershare.net" }, urls = { "http(s)?://[\\w\\.]*?usershare\\.net/(.*?/[0-9a-z]{12}|[0-9a-z]{12})" }, flags = { 0 })
-public class Usershare extends PluginForHost {// http://usershare.net/RickyTheKidd/gwbq65hzbeg6
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "usershare.net" }, urls = { "http(s)?://[\\w\\.]*?usershare\\.net/(.*?/[0-9a-z]{12}|[0-9a-z]{12})" }, flags = { 2 })
+public class Usershare extends PluginForHost {
 
     public Usershare(PluginWrapper wrapper) {
         super(wrapper);
+        this.enablePremium(COOKIE_HOST + "/premium.html");
     }
+
+    private static final String COOKIE_HOST = "http://usershare.net";
 
     @Override
     public String getAGBLink() {
@@ -70,10 +78,7 @@ public class Usershare extends PluginForHost {// http://usershare.net/RickyTheKi
             }
             linkurl = br.getRedirectLocation();
         } else {
-            linkurl = br.getRegex("\"(http://[a-zA-Z0-9]+\\.usershare\\.net/files/.*?/.*?/.*?)\"").getMatch(0);
-            if (linkurl == null) {
-                linkurl = br.getRegex("href=\"(http://[0-9]+\\..*?:[0-9]+/d/.*?/.*?)\"").getMatch(0);
-            }
+            linkurl = getDllink();
         }
         if (passCode != null) {
             link.setProperty("pass", passCode);
@@ -82,6 +87,121 @@ public class Usershare extends PluginForHost {// http://usershare.net/RickyTheKi
         br.setDebug(true);
         dl = jd.plugins.BrowserAdapter.openDownload(br, link, linkurl, true, 1);
         dl.startDownload();
+    }
+
+    private String getDllink() {
+        String linkurl = br.getRegex("\"(http://[a-zA-Z0-9]+\\.usershare\\.net/files/.*?/.*?/.*?)\"").getMatch(0);
+        if (linkurl == null) {
+            linkurl = br.getRegex("href=\"(http://[0-9]+\\..*?:[0-9]+/d/.*?/.*?)\"").getMatch(0);
+        }
+        return linkurl;
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        AccountInfo ai = new AccountInfo();
+        try {
+            login(account);
+        } catch (PluginException e) {
+            account.setValid(false);
+            return ai;
+        }
+        String space = br.getRegex(Pattern.compile("<td>Used space:</td>.*?<td.*?b>(.*?)of.*?Mb</b>", Pattern.DOTALL | Pattern.CASE_INSENSITIVE)).getMatch(0);
+        if (space != null) ai.setUsedSpace(space.trim() + " Mb");
+        String points = br.getRegex(Pattern.compile("<td>You have collected:</td.*?b>(.*?)premium points", Pattern.CASE_INSENSITIVE)).getMatch(0);
+        if (points != null) {
+            // Who needs half points ? If we have a dot in the points, just
+            // remove it
+            if (points.contains(".")) {
+                String dot = new Regex(points, ".*?(\\.(\\d+))").getMatch(0);
+                points = points.replace(dot, "");
+            }
+            ai.setPremiumPoints(Long.parseLong(points.trim()));
+        }
+        account.setValid(true);
+        String availabletraffic = new Regex(br.toString(), "Traffic available.*?:</TD><TD><b>(.*?)</b>").getMatch(0);
+        if (availabletraffic != null) {
+            ai.setTrafficLeft(Regex.getSize(availabletraffic));
+        } else {
+            ai.setUnlimitedTraffic();
+        }
+        String expire = new Regex(br.toString(), "<td>Premium-Account expire:</td>.*?<td>(.*?)</td>").getMatch(0);
+        if (expire == null) {
+            ai.setExpired(true);
+            account.setValid(false);
+            return ai;
+        } else {
+            expire = expire.replaceAll("(<b>|</b>)", "");
+            ai.setValidUntil(Regex.getMilliSeconds(expire, "dd MMMM yyyy", null));
+        }
+        ai.setStatus("Premium User");
+        return ai;
+    }
+
+    private void login(Account account) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(COOKIE_HOST + "/login.html");
+        Form loginform = br.getForm(1);
+        if (loginform == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        loginform.put("login", Encoding.urlEncode(account.getUser()));
+        loginform.put("password", Encoding.urlEncode(account.getPass()));
+        br.submitForm(loginform);
+        br.getPage(COOKIE_HOST + "/?op=my_account");
+        if (br.getCookie(COOKIE_HOST, "login") == null || br.getCookie(COOKIE_HOST, "xfss") == null) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+        if (!br.containsHTML("Premium-Account expire") && !br.containsHTML(">Renew premium<")) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+    }
+
+    @Override
+    public void handlePremium(DownloadLink link, Account account) throws Exception {
+        String passCode = null;
+        requestFileInformation(link);
+        login(account);
+        br.setFollowRedirects(false);
+        br.getPage(link.getDownloadURL());
+        String dllink = br.getRedirectLocation();
+        if (dllink == null) {
+            Form DLForm = br.getFormbyProperty("name", "F1");
+            if (DLForm == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            if (br.containsHTML("(<b>Passwort:</b>|<b>Password:</b>)")) {
+                logger.info("The downloadlink seems to be password protected.");
+                if (link.getStringProperty("pass", null) == null) {
+                    passCode = Plugin.getUserInput("Password?", link);
+                } else {
+                    /* gespeicherten PassCode holen */
+                    passCode = link.getStringProperty("pass", null);
+                }
+                DLForm.put("password", passCode);
+                logger.info("Put password \"" + passCode + "\" entered by user in the DLForm and submitted it.");
+            }
+            br.submitForm(DLForm);
+            if (br.containsHTML("(Wrong password|<b>Passwort:</b>|<b>Password:</b>)")) {
+                logger.warning("Wrong password, the entered password \"" + passCode + "\" is wrong, retrying...");
+                link.setProperty("pass", null);
+                throw new PluginException(LinkStatus.ERROR_RETRY);
+            }
+            dllink = br.getRedirectLocation();
+            if (dllink == null) dllink = getDllink();
+        }
+        if (dllink == null) {
+            logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        logger.info("Final downloadlink = " + dllink + " starting the download...");
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, 0);
+        if (passCode != null) {
+            link.setProperty("pass", passCode);
+        }
+        if (dl.getConnection().getContentType() != null && dl.getConnection().getContentType().contains("html")) {
+            logger.warning("The final dllink seems not to be a file!");
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
     }
 
     @Override
