fe4452fc23d0223e4ea658df2a348f4bef7a8d21
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-07-23 00:35:50 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@12077 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/optional/jdpremclient/JDPremium.java
@@ -0,0 +1,142 @@
+package jd.plugins.optional.jdpremclient;
+
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import java.util.ArrayList;
+
+import jd.HostPluginWrapper;
+import jd.PluginWrapper;
+import jd.controlling.AccountController;
+import jd.gui.swing.jdgui.menu.MenuAction;
+import jd.plugins.Account;
+import jd.plugins.OptionalPlugin;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginOptional;
+import jd.utils.JDUtilities;
+import jd.utils.locale.JDL;
+
+class PremShareHost extends HostPluginWrapper {
+
+    private HostPluginWrapper replacedone = null;
+
+    public PremShareHost(String host, String className, String patternSupported, int flags) {
+        super(host, "jd.plugins.optional.premcol.", "PremShare", patternSupported, flags, "$Revision: 6506 $");
+        for (HostPluginWrapper wrapper : HostPluginWrapper.getHostWrapper()) {
+            if (wrapper.getPattern().toString().equalsIgnoreCase(patternSupported) && wrapper != this) replacedone = wrapper;
+        }
+        if (replacedone != null) {
+            HostPluginWrapper.getHostWrapper().remove(replacedone);
+        }
+    }
+
+    public HostPluginWrapper getReplacedPlugin() {
+        return replacedone;
+    }
+
+    @Override
+    public synchronized PluginForHost getPlugin() {
+        Plugin tmp = super.getPlugin();
+        if (replacedone != null) {
+            PluginForHost tmp2 = (PluginForHost) replacedone.getPlugin();
+            ((PremShare) tmp).setReplacedPlugin(tmp2);
+        }
+        return (PluginForHost) tmp;
+    }
+
+    @Override
+    public PluginForHost getNewPluginInstance() {
+        PluginForHost tmp = super.getNewPluginInstance();
+        if (replacedone != null) {
+            PluginForHost tmp2 = (PluginForHost) replacedone.getNewPluginInstance();
+            ((PremShare) tmp).setReplacedPlugin(tmp2);
+        }
+        return tmp;
+    }
+}
+
+@OptionalPlugin(rev = "$Revision: 6506 $", id = "jdpremium", interfaceversion = 5)
+public class JDPremium extends PluginOptional {
+
+    private static PremShareHost jdpremium = null;
+
+    public JDPremium(PluginWrapper wrapper) {
+        super(wrapper);
+
+    }
+
+    private void replaceHosterPlugin(String host) {
+        PluginForHost old = JDUtilities.getPluginForHost(host);
+        if (old != null) {
+            logger.info("Replacing " + host + " Plugin with JDPremium Plugin");
+            new PremShareHost(old.getHost(), "PremShare", old.getWrapper().getPattern().toString(), ((HostPluginWrapper) old.getWrapper()).getFlags() + PluginWrapper.ALLOW_DUPLICATE);
+        }
+    }
+
+    // @Override
+    public String getRequirements() {
+        return "JRE 1.5+";
+    }
+
+    // @Override
+    public boolean initAddon() {
+        replaceHosterPlugin("rapidshare.com");
+        replaceHosterPlugin("uploaded.to");
+        replaceHosterPlugin("megaUpload.com");
+        replaceHosterPlugin("netload.in");
+        replaceHosterPlugin("hotfile.com");
+        jdpremium = new PremShareHost("jdownloader.org", "JDPremServ", "NEVERUSETHISREGEX:\\)", 2);
+        logger.info("JDPremium init ok!");
+        return true;
+    }
+
+    public static Account getAccount() {
+        synchronized (jdpremium) {
+            return AccountController.getInstance().getValidAccount(JDPremium.getJDPremium());
+        }
+    }
+
+    public static PluginForHost getJDPremium() {
+        return jdpremium.getPlugin();
+    }
+
+    public void onExit() {
+        logger.info("Cannot replace Plugins on runtime. Restart is neccessary!");
+    }
+
+    public String getIconKey() {
+        return "gui.images.taskpanes.premium";
+    }
+
+    public String getHost() {
+        return JDL.L("plugins.optional.jdpremium.name", "JDPremium");
+    }
+
+    public String getVersion() {
+        return getVersion("$Revision: 5887 $");
+    }
+
+    public static int getAddonInterfaceVersion() {
+        return 3;
+    }
+
+    @Override
+    public ArrayList<MenuAction> createMenuitems() {
+        return null;
+    }
+
+}
---
+++ src/jd/plugins/optional/jdpremclient/PremShare.java
@@ -0,0 +1,256 @@
+package jd.plugins.optional.jdpremclient;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.config.ConfigContainer;
+import jd.http.Browser;
+import jd.nutils.Formatter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.parser.html.Form.MethodType;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+public class PremShare extends PluginForHost {
+
+    private boolean proxyused = false;
+    // private static ImageIcon Icon = new
+    // ImageIcon(JDImage.getImage("logo/logo_16_16"));
+    private String infostring = null;
+    private PluginForHost plugin = null;
+    private static final String server = "";
+
+    public void setReplacedPlugin(PluginForHost plugin) {
+        this.plugin = plugin;
+    }
+
+    public PremShare(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium();
+        infostring = "JDPremium @ " + wrapper.getHost();
+        // if (((PremShareHost) wrapper).getReplacedPlugin() != null) plugin =
+        // (PluginForHost) ((PremShareHost)
+        // wrapper).getReplacedPlugin().getNewPluginInstance();
+    }
+
+    public synchronized int getFreeConnections() {
+        if (plugin != null) return plugin.getFreeConnections();
+        return super.getFreeConnections();
+    }
+
+    public int getMaxConnections() {
+        if (plugin != null) return plugin.getMaxConnections();
+        return super.getMaxConnections();
+    }
+
+    public String getVersion() {
+        if (plugin == null) return getVersion("$Revision: 86 $");
+        return plugin.getVersion();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (plugin != null) {
+            plugin.actionPerformed(e);
+        } else {
+            super.actionPerformed(e);
+        }
+    }
+
+    public String getHost() {
+        if (plugin == null) return "jdownloader.org";
+        return plugin.getHost();
+    }
+
+    public boolean isAGBChecked() {
+        if (plugin == null) return true;
+        return plugin.isAGBChecked();
+    }
+
+    public void setAGBChecked(boolean value) {
+        if (plugin == null) return;
+        plugin.setAGBChecked(value);
+    }
+
+    public ConfigContainer getConfig() {
+        if (plugin == null) return super.getConfig();
+        return plugin.getConfig();
+    }
+
+    public String getBuyPremiumUrl() {
+        if (plugin == null) return "http://www.jdownloader.org";
+        return plugin.getBuyPremiumUrl();
+    }
+
+    @Override
+    public String getAGBLink() {
+        if (plugin == null) return "http://www.jdownloader.org";
+        return plugin.getAGBLink();
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        if (plugin == null) return;
+        proxyused = false;
+        // if (handleProxy(link)) return;
+        if (handleJDPremServ(link)) return;
+        plugin.clean();
+        plugin.handleFree(link);
+    }
+
+    public void clean() {
+        super.clean();
+        if (plugin != null) plugin.clean();
+    }
+
+    private boolean handleJDPremServ(DownloadLink link) throws Exception {
+        Account acc = JDPremium.getAccount();
+        if (acc == null) return false;
+        requestFileInformation(link);
+        br = new Browser();
+        br.setDebug(true);
+        Form form = new Form();
+        br.getPage(server);
+        /* add and force download */
+        form.setAction("/?force=" + link.getDownloadURL());
+        form.setMethod(MethodType.POST);
+        form.put("username", Encoding.urlEncode(acc.getUser()));
+        form.put("password", Encoding.urlEncode(acc.getPass()));
+
+        /* first request,with force */
+        String status = br.submitForm(form);
+        int refresh = 10 * 1000;
+        while (true) {
+            if (status.contains("OK: 100")) {
+                break;
+            }
+            if (status == null || status.length() == 0 || status.contains("ERROR")) {
+                logger.info(status);
+                return false;
+            }
+            if (status.contains("OK: 0")) {
+                refresh = 10 * 1000;
+                link.getLinkStatus().setStatusText("Queued");
+            } else if (status.contains("OK: 1")) {
+                /* show speed and ETA */
+                String ints[] = new Regex(status, "(\\d+)/(\\d+)/(\\d+)").getRow(0);
+                Long size = Long.parseLong(ints[1]);
+                Long current = Long.parseLong(ints[0]);
+                Long speed = Long.parseLong(ints[2]);
+                String sp = Formatter.formatReadable(speed) + "/s";
+                if (speed != 0 && size > 0) {
+                    Long left = (size - current) / speed;
+                    link.getLinkStatus().setStatusText("Downloading " + Formatter.formatSeconds(left) + " " + sp);
+                } else {
+                    link.getLinkStatus().setStatusText("Downloading " + sp);
+                }
+                refresh = 5 * 1000;
+            }
+            Thread.sleep(refresh);
+            status = br.submitForm(form);
+        }
+        /* download now */
+        form.setAction("/?download=1&force=" + link.getDownloadURL());
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, form, true, 0);
+        if (!dl.getConnection().isContentDisposition()) {
+            br.followConnection();
+            logger.info(br.toString());
+            return false;
+        }
+        return dl.startDownload();
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        if (plugin == null) return AvailableStatus.UNCHECKABLE;
+        return plugin.requestFileInformation(parameter);
+    }
+
+    @Override
+    public void reset() {
+        if (plugin != null) plugin.reset();
+    }
+
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        if (plugin == null) {
+            AccountInfo ac = new AccountInfo();
+            account.setValid(true);
+            return ac;
+        } else
+            return plugin.fetchAccountInfo(account);
+    }
+
+    public void handlePremium(DownloadLink downloadLink, Account account) throws Exception {
+        if (plugin == null) return;
+        proxyused = false;
+        // if (handleProxy(link)) return;
+        if (handleJDPremServ(downloadLink)) return;
+        plugin.clean();
+        plugin.handlePremium(downloadLink, account);
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+        if (plugin != null) plugin.resetDownloadlink(link);
+    }
+
+    public String getSessionInfo() {
+        if (proxyused || plugin == null) return infostring;
+        return plugin.getSessionInfo();
+    }
+
+    public void correctDownloadLink(DownloadLink link) throws Exception {
+        if (plugin != null) plugin.correctDownloadLink(link);
+    }
+
+    public int getMaxSimultanFreeDownloadNum() {
+        if (plugin == null) return super.getMaxSimultanFreeDownloadNum();
+        return plugin.getMaxSimultanFreeDownloadNum();
+    }
+
+    public int getMaxSimultanPremiumDownloadNum() {
+        if (plugin == null) return super.getMaxSimultanPremiumDownloadNum();
+        return plugin.getMaxSimultanPremiumDownloadNum();
+    }
+
+    public int getMaxSimultanDownload(final Account account) {
+        if (plugin != null) return plugin.getMaxSimultanDownload(account);
+        return 0;
+    }
+
+    public boolean checkLinks(DownloadLink[] urls) {
+        if (plugin == null) return false;
+        return plugin.checkLinks(urls);
+    }
+
+    public String getFileInformationString(DownloadLink downloadLink) {
+        if (proxyused || plugin == null) return "";
+        return plugin.getFileInformationString(downloadLink);
+    }
+
+    public ArrayList<jd.gui.swing.jdgui.menu.MenuAction> createMenuitems() {
+        if (plugin == null) return super.createMenuitems();
+        return plugin.createMenuitems();
+    }
+
+    public ArrayList<Account> getPremiumAccounts() {
+        if (plugin != null) return plugin.getPremiumAccounts();
+        return super.getPremiumAccounts();
+    }
+
+    public boolean hasAccountSupport() {
+        if (plugin != null) return plugin.hasAccountSupport();
+        return false;
+    }
+
+    public String getCustomFavIconURL() {
+        if (proxyused) return "http://www.jdownloader.org/";
+        return null;
+    }
+
+}
