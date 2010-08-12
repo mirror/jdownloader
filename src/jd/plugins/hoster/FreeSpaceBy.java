177934afb97a67f541a73d304ff046dd8b6e621f
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-08-12 15:33:09 +0000

    -Added support for freespace.by (still broken) refs #2184 -Added support for up.avaga.by (untested) refs #2183
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@12279 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FreeSpaceBy.java
@@ -0,0 +1,157 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "freespace.by" }, urls = { "http://[\\w\\.]*?freespace\\.by/download/[a-z0-9]+" }, flags = { 2 })
+public class FreeSpaceBy extends PluginForHost {
+
+    public FreeSpaceBy(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium();
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://freespace.by/info/rules";
+    }
+
+    private static final String MAINPAGE = "http://freespace.by/";
+    private static final String COUNTRYBLOCKED = "Услуги FreeSpace доступны только для белорусских сетей\\.";
+    private static final String COUNTRYBLOCKEDTEXT = "This hoster is now available in your country!";
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML(COUNTRYBLOCKED)) {
+            link.getLinkStatus().setStatusText(COUNTRYBLOCKEDTEXT);
+            return AvailableStatus.UNCHECKABLE;
+        }
+        if (br.containsHTML(">Файл не найден<")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("Имя файла:</td> <td>(.*?)</td>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<textarea  class=\"links\" onclick=\"this\\.focus\\(\\);this.select\\(\\)\" readonly=\"readonly\"><a href=\"http://freespace\\.by/download/[a-z0-9]+\">(.*?)</a></textarea><br>").getMatch(0);
+        String filesize = br.getRegex("<tr><td>Размер:</td> <td>(.*?)</td></tr>").getMatch(0);
+        if (filesize != null) filesize = br.getRegex("<td>Размер:</td><td>(.*?)</td>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(filename.trim());
+        link.setDownloadSize(Regex.getSize(filesize.replace(",", ".").replace(" ", "")));
+        String sh1 = br.getRegex("SHA1</a>:</td> <td>(.*?)</td>").getMatch(0);
+        if (sh1 != null) link.setSha1Hash(sh1.trim());
+        String md5 = br.getRegex("MD5</a>:</td> <td>(.*?)</td>").getMatch(0);
+        if (md5 != null) link.setMD5Hash(sh1.trim());
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        throw new PluginException(LinkStatus.ERROR_FATAL, "This hoster doesn't allow any free downloads.");
+    }
+
+    public void doFree(DownloadLink downloadLink) throws Exception, PluginException {
+        if (br.containsHTML(COUNTRYBLOCKED)) {
+            downloadLink.getLinkStatus().setStatusText(COUNTRYBLOCKEDTEXT);
+            throw new PluginException(LinkStatus.ERROR_FATAL, COUNTRYBLOCKEDTEXT);
+        }
+        br.setFollowRedirects(false);
+        String dllink = null;
+        String shortWaittime = br.getRegex("id=\"remaining_time\">(\\d+)</span>").getMatch(0);
+        if (shortWaittime != null) {
+            sleep(Integer.parseInt(shortWaittime) * 1001l, downloadLink);
+            String postPage = "http://freespace.by/api.php?format=ajax&lang=ru&PHPSESSID=" + br.getCookie(MAINPAGE, "PHPSESSID") + "&JsHttpRequest=" + System.currentTimeMillis() + "-xml";
+            String post = "tion[0]=Download.getFreeDownloadLink&download_token[0]=" + new Regex(downloadLink.getDownloadURL(), "freespace\\.by/download/([a-z0-9]+)").getMatch(0);
+            br.postPage(postPage, post);
+            dllink = br.getRegex("response\":\"(http:.*?)\"").getMatch(0);
+        } else {
+            dllink = br.getRegex("id=\"free_download_link_wrapper\">[\n\t\r ]+<a href=\"http://.*?)\"").getMatch(0);
+            if (dllink == null) dllink = br.getRegex("\"(http://\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+/get/.*?)\"").getMatch(0);
+        }
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dllink = dllink.replace("\\", "");
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    private void login(Account account) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(MAINPAGE);
+        String post = "action[0]=AuthExt.logon&username[0]=" + Encoding.urlEncode(account.getUser()) + "&password[0]=" + Encoding.urlEncode(account.getPass()) + "&remember[0]=1";
+        String postPage = "http://freespace.by/api.php?format=ajax&lang=ru&PHPSESSID=" + br.getCookie(MAINPAGE, "PHPSESSID") + "&JsHttpRequest=" + System.currentTimeMillis() + "-xml";
+        br.postPage(postPage, post);
+        if (br.getCookie(MAINPAGE, "lms_auth") == null) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        AccountInfo ai = new AccountInfo();
+        try {
+            login(account);
+        } catch (PluginException e) {
+            account.setValid(false);
+            return ai;
+        }
+        account.setValid(true);
+        ai.setUnlimitedTraffic();
+        ai.setStatus("Account is OK");
+        return ai;
+    }
+
+    @Override
+    public void handlePremium(DownloadLink link, Account account) throws Exception {
+        requestFileInformation(link);
+        login(account);
+        br.getPage(link.getDownloadURL());
+        doFree(link);
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
---
+++ src/jd/plugins/hoster/UpAvagaBy.java
@@ -0,0 +1,108 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "up.uvaga.by" }, urls = { "http://[\\w\\.]*?up\\.uvaga\\.by/download\\.php\\?filename=[a-z0-9]+" }, flags = { 0 })
+public class UpAvagaBy extends PluginForHost {
+
+    public UpAvagaBy(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://up.uvaga.by/rules.html";
+    }
+
+    private static final String MAINPAGE = "http://up.uvaga.by/";
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setCustomCharset("windows-1251");
+        br.getPage(link.getDownloadURL());
+        Regex fileInformation = br.getRegex("<p>Имя файла <b>(.*?)</b> \\((.*?)\\)\\.</p>");
+        if (br.containsHTML("<p>Неправильная ссылка на файл либо файл был удален\\.</p>")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = fileInformation.getMatch(0);
+        String filesize = fileInformation.getMatch(1);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(filename.trim());
+        link.setDownloadSize(Regex.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        br.setFollowRedirects(false);
+        Regex waittime = br.getRegex("id='myclock'>(\\d+)\\.(\\d+)</span>");
+        if (waittime.getMatch(0) == null || waittime.getMatch(1) == null) waittime = br.getRegex("var t=(\\d+)\\.(\\d+);");
+        String minutes = waittime.getMatch(0);
+        String seconds = waittime.getMatch(1);
+        if (minutes == null || seconds == null) {
+            logger.warning("The waittime regex is broken...");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        sleep(((Integer.parseInt(minutes) * 60) + Integer.parseInt(seconds)) * 1001l, downloadLink);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.getCookie(MAINPAGE, "PHPSESSID") == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        String dllink = null;
+        for (int i = 0; i <= 5; i++) {
+            String captchaImageLink = "http://up.uvaga.by/kcaptcha/kap.php?ID=" + br.getCookie(MAINPAGE, "PHPSESSID");
+            String captchaPage = "http://up.uvaga.by/get_file.php?keystring=" + getCaptchaCode(captchaImageLink, downloadLink) + "&filename=" + new Regex(downloadLink.getDownloadURL(), "filename=([a-z0-9]+)").getMatch(0);
+            br.getPage(captchaPage);
+            dllink = br.getRedirectLocation();
+            if (dllink == null) {
+                br.getPage(downloadLink.getDownloadURL());
+                continue;
+            }
+            break;
+        }
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
