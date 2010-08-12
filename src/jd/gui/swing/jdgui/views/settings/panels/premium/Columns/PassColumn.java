74c72facf01108d3b2f93f0041883726c1169581
Greeny <Greeny@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-08-12 09:04:04 +0000

    - refs #2200 (changed for PassColumn, change it for other columns too? see ticket)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@12276 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/swing/jdgui/views/settings/panels/premium/Columns/PassColumn.java
+++ src/jd/gui/swing/jdgui/views/settings/panels/premium/Columns/PassColumn.java
@@ -17,11 +17,6 @@
 package jd.gui.swing.jdgui.views.settings.panels.premium.Columns;
 
 import java.awt.Component;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.ClipboardOwner;
-import java.awt.datatransfer.StringSelection;
-import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -35,55 +30,18 @@ import jd.utils.JDUtilities;
 
 import org.jdesktop.swingx.renderer.JRendererLabel;
 
-class JDPasswordField extends JPasswordField implements ClipboardOwner {
-
-    private static final long serialVersionUID = -7981118302661369727L;
-
-    public JDPasswordField() {
-        super();
-    }
-
-    @Override
-    public void copy() {
-        if (JDUtilities.getRunType() == JDUtilities.RUNTYPE_LOCAL) {
-            StringSelection stringSelection = new StringSelection(String.valueOf(this.getSelectedText()));
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(stringSelection, this);
-        }
-    }
-
-    @Override
-    public void cut() {
-        if (JDUtilities.getRunType() == JDUtilities.RUNTYPE_LOCAL) {
-            StringSelection stringSelection = new StringSelection(String.valueOf(this.getSelectedText()));
-            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-            clipboard.setContents(stringSelection, this);
-
-            String text = String.valueOf(this.getPassword());
-            int position = this.getSelectionStart();
-            String s1 = text.substring(0, position);
-            String s2 = text.substring(this.getSelectionEnd(), text.length());
-            this.setText(s1 + s2);
-
-            this.setSelectionStart(position);
-            this.setSelectionEnd(position);
-        }
-    }
-
-    public void lostOwnership(Clipboard arg0, Transferable arg1) {
-    }
-
-}
-
 public class PassColumn extends JDTableColumn implements ActionListener {
 
     private static final long serialVersionUID = -5291590062503352550L;
-    private JRendererLabel jlr;
-    private JDPasswordField passw;
+    private final JRendererLabel jlr;
+    private final JPasswordField passw;
 
     public PassColumn(String name, JDTableModel table) {
         super(name, table);
-        passw = new JDPasswordField();
+        passw = new JPasswordField();
+        if (JDUtilities.getRunType() == JDUtilities.RUNTYPE_LOCAL) {
+            passw.putClientProperty("JPasswordField.cutCopyAllowed", Boolean.TRUE);
+        }
         jlr = new JRendererLabel();
         jlr.setBorder(null);
         setClickstoEdit(2);
@@ -103,12 +61,11 @@ public class PassColumn extends JDTableColumn implements ActionListener {
 
     @Override
     public boolean isEditable(Object ob) {
-        return ob != null && ob instanceof Account;
+        return ob instanceof Account;
     }
 
     @Override
     public boolean isEnabled(Object obj) {
-        if (obj == null) return false;
         if (obj instanceof Account) return ((Account) obj).isEnabled();
         if (obj instanceof HostAccounts) return ((HostAccounts) obj).isEnabled();
         return true;
@@ -130,7 +87,7 @@ public class PassColumn extends JDTableColumn implements ActionListener {
     @Override
     public Component myTableCellRendererComponent(JDTableModel table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         if (value instanceof Account) {
-            jlr.setText("*****");
+            jlr.setText("********");
         } else {
             jlr.setText("");
         }
@@ -139,12 +96,10 @@ public class PassColumn extends JDTableColumn implements ActionListener {
 
     @Override
     public void setValue(Object value, Object o) {
-        String pw = (String) value;
-        if (o instanceof Account) ((Account) o).setPass(pw);
-    }
-
-    @Override
-    public void sort(Object obj, boolean sortingToggle) {
+        if (o instanceof Account) {
+            String pw = (String) value;
+            ((Account) o).setPass(pw);
+        }
     }
 
 }
