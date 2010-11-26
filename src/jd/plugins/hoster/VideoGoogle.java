7e0ff2afcf25d237f2d3de710aa854bc26c108e6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-11-26 17:30:36 +0000

    -Fixed jiaz's regex, added errorhandling and file size is now displayed in the linkgrabber
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13042 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/VideoGoogle.java
+++ src/jd/plugins/hoster/VideoGoogle.java
@@ -1,6 +1,8 @@
 package jd.plugins.hoster;
 
 import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
@@ -8,6 +10,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.DownloadLink.AvailableStatus;
+import jd.utils.locale.JDL;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "video.google.com" }, urls = { "http://[\\w\\.]*?video\\.google\\.com/videoplay\\?docid=-?\\d+" }, flags = { 0 })
 public class VideoGoogle extends PluginForHost {
@@ -21,15 +24,16 @@ public class VideoGoogle extends PluginForHost {
         return "http://www.google.com/accounts/TOS?loc=ZZ&hl=en";
     }
 
+    private String DLLINK = null;
+
     @Override
     public void handleFree(DownloadLink link) throws Exception {
         requestFileInformation(link);
-        String url = br.getRegex("videoUrl.x3d(http://.*?).x26th").getMatch(0);
-        if (url == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        url = Encoding.urlDecode(url, true);
-        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, url, true, 1);
+        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, DLLINK, true, 0);
         if (!dl.getConnection().isContentDisposition() && !dl.getConnection().getContentType().startsWith("video")) {
+            br.followConnection();
             this.dl.getConnection().disconnect();
+            if (br.containsHTML("No htmlCode read")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.videogoogle.videotemporaryunavailable", "This video is temporary unavailable!"), 60 * 60 * 1000l);
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
@@ -42,8 +46,32 @@ public class VideoGoogle extends PluginForHost {
         String name = br.getRegex("<title>(.*?)</title>").getMatch(0);
         if (br.containsHTML("but this video may not be availabl")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         if (name == null || "Google Videos Error".equals(name)) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        parameter.setFinalFileName(name + ".flv");
-        return AvailableStatus.TRUE;
+        parameter.setFinalFileName(Encoding.htmlDecode(name) + ".flv");
+        DLLINK = br.getRegex("videoUrl\\\\x3d(http://.*?\\.googlevideo\\.com/videoplayback.*?)\\\\x26thumbnailUrl").getMatch(0);
+        if (DLLINK == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        DLLINK = Encoding.urlDecode(DLLINK, true);
+        Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openGetConnection(DLLINK);
+            if (!con.getContentType().contains("html"))
+                parameter.setDownloadSize(con.getLongContentLength());
+            else
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
     }
 
     @Override
