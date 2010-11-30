fec8cd1a2028699b061c9b0d95c9e0f1d08db700
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-11-30 20:23:49 +0000

    -Added support for xtube closes #2256 (tested & working) -Added support for drtuber refs #2691 (still broken for jiaz)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13081 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/DrTuberCom.java
@@ -0,0 +1,112 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.JDHash;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "drtuber.com" }, urls = { "http://(www\\.)?drtuber\\.com/video/\\d+" }, flags = { 0 })
+public class DrTuberCom extends PluginForHost {
+
+    public DrTuberCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    private String DLLINK = null;
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.drtuber.com/static/terms";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.setDebug(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("The file you have requested was not found on this server") || br.getURL().contains("missing=true")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<a href=\"\" class=\"vtitle\">(.*?)</a>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<title>(.*?) - Free Videos Adult Sex Tube - DrTuber.com</title>").getMatch(0);
+        String vKey = br.getRegex("vkey=(\\d+)").getMatch(0);
+        if (vKey == null) {
+            logger.warning("vKey is null...");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getHeaders().put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
+        br.getHeaders().put("Referer", "");
+        br.getPage("http://drtuber.com/player/config.php?vkey=" + vKey + "&pkey=" + JDHash.getMD5(vKey + Encoding.Base64Decode("MzlkY24yNHQwOThz")));
+        DLLINK = br.getRegex("<video_file>(http://.*?\\.flv)</video_file>").getMatch(0);
+        if (filename == null || DLLINK == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        filename = filename.trim();
+        downloadLink.setFinalFileName(filename + ".flv");
+        Browser br2 = br.cloneBrowser();
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openGetConnection(DLLINK);
+            if (!con.getContentType().contains("html"))
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            else
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
---
+++ src/jd/plugins/hoster/XTubeCom.java
@@ -0,0 +1,117 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2010  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xtube.com" }, urls = { "http://(www\\.)?xtube\\.com/(watch|play_re)\\.php\\?v=[A-Za-z0-9_-]+" }, flags = { 0 })
+public class XTubeCom extends PluginForHost {
+
+    public XTubeCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    private String DLLINK = null;
+
+    @Override
+    public String getAGBLink() {
+        return "http://wiki2.xtube.com/index.php?title=Terms_of_Use&action=purge";
+    }
+
+    private static final String MAINPAGE = "http://www.xtube.com";
+
+    public void correctDownloadLink(DownloadLink link) {
+        link.setUrlDownload(link.getDownloadURL().replace("play_re", "watch"));
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setCookie(MAINPAGE, "cookie_warning", "deleted");
+        br.setCookie(MAINPAGE, "cookie_warning", "s");
+        br.setFollowRedirects(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("(sorry the video \"UNKNOWN\" is unavailable<|\">There was an unknown error with the video you have selected)") || br.getURL().contains("xtube.com/index.php")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<div class=\"p_5px font_b_12px f_left\">(.*?)</div>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<div class=\"font_b_12px\">(.*?)</div><div").getMatch(0);
+        String fileID = new Regex(downloadLink.getDownloadURL(), "xtube\\.com/watch\\.php\\?v=(.+)").getMatch(0);
+        String ownerName = br.getRegex("\\.addVariable\\(\"user_id\", \"(.*?)\"\\);").getMatch(0);
+        String server = br.getRegex("addVariable\\(\"swfURL\", \"(http://.*?)\"\\);").getMatch(0);
+        if (fileID == null || ownerName == null || server == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        br.postPage("http://video2.xtube.com/find_video.php", "user%5Fid=" + Encoding.urlEncode(ownerName) + "&clip%5Fid=&video%5Fid=" + Encoding.urlEncode(fileID));
+        DLLINK = br.getRegex("\\&filename=(%2Fvideos.*?hash.+)").getMatch(0);
+        if (filename == null || DLLINK == null || DLLINK.length() > 500) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        DLLINK = server + Encoding.htmlDecode(DLLINK);
+        filename = filename.trim();
+        downloadLink.setFinalFileName(filename + ".flv");
+        Browser br2 = br.cloneBrowser();
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openGetConnection(DLLINK);
+            if (!con.getContentType().contains("html"))
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            else
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
