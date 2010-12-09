616b895e3e762d1b13666b608a73fecc825bd40c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-12-09 14:13:20 +0000

    FreakShare: fixed filenotfound for premium
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13125 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Freaksharenet.java
+++ src/jd/plugins/hoster/Freaksharenet.java
@@ -29,11 +29,11 @@ import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
-import jd.plugins.DownloadLink.AvailableStatus;
 import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
 
@@ -143,6 +143,23 @@ public class Freaksharenet extends PluginForHost {
             }
             if (url == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, url, true, 0);
+            if (!dl.getConnection().isContentDisposition()) {
+                logger.info("The finallink is no file, trying to handle errors...");
+                br.followConnection();
+                if (br.containsHTML(MAXDLSLIMITMESSAGE)) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Reached max free DLs", 10 * 60 * 1000l);
+                if (br.containsHTML("File can not be found")) {
+                    logger.info("File for the following is offline (server error): " + downloadLink.getDownloadURL());
+                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                }
+                if (br.containsHTML("bad try")) {
+                    logger.warning("Hoster said \"bad try\" which means that jd didn't wait enough time before trying to start the download!");
+                    throw new PluginException(LinkStatus.ERROR_RETRY);
+                }
+                if (br.containsHTML("your Traffic is used up for today")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1001);
+                if (br.containsHTML("No Downloadserver. Please try again")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "No Downloadserver. Please try again later", 15 * 60 * 1000l);
+                if (br.containsHTML("you cant  download more then 1 at time")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 10 * 60 * 1001);
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
             dl.startDownload();
         }
     }
