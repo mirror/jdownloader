592044f630ddbddd189d9dcc5622fc30a6f5af33
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-12-19 18:37:50 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13176 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/GrooveShark.java
+++ src/jd/plugins/hoster/GrooveShark.java
@@ -33,7 +33,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision: 12299 $", interfaceVersion = 2, names = { "grooveshark.com", "grooveshark.com" }, urls = { "http://listen\\.grooveshark\\.viajd/(#/)?.+", "http://grooveshark\\.viajd/song/\\d+" }, flags = { 0, 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "grooveshark.com", "grooveshark.com" }, urls = { "http://listen\\.grooveshark\\.viajd/(#/)?.+", "http://grooveshark\\.viajd/song/\\d+" }, flags = { 0, 0 })
 public class GrooveShark extends PluginForHost {
 
     private static final String LISTEN  = "http://listen.grooveshark.com/";
