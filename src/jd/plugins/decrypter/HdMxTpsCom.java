320042df99245714869a5a5c0613734fd4d54a15
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2010-12-31 16:10:43 +0000

    -Improved decrypterregex
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13242 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/HdMxTpsCom.java
+++ src/jd/plugins/decrypter/HdMxTpsCom.java
@@ -30,7 +30,7 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hdmixtapes.com" }, urls = { "http://(www\\.)?hdmixtapes\\.com/(newsingles|mixtapes)/.*?_\\d+" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hdmixtapes.com" }, urls = { "http://(www\\.)?hdmixtapes\\.com/(newsingles|mixtapes)/.+" }, flags = { 0 })
 public class HdMxTpsCom extends PluginForDecrypt {
 
     /* must be static so all plugins share same lock */
