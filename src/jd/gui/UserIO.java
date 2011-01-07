d4b7dd0a6f8f6005109a9450d54949b774507430
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-01-07 13:22:04 +0000

    Fixed Nullpointerbug in Dialog System
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13268 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/UserIO.java
+++ src/jd/gui/UserIO.java
@@ -321,7 +321,8 @@ public class UserIO {
      * @return an array of files or null if the user cancel the dialog
      */
     public File[] requestFileChooser(final String id, final String title, final Integer fileSelectionMode, final FileFilter fileFilter, final Boolean multiSelection, final File startDirectory, final Integer dialogType) {
-        return Dialog.getInstance().showFileChooser(id, title, fileSelectionMode, fileFilter, multiSelection == null ? false : multiSelection, dialogType, null);
+
+        return Dialog.getInstance().showFileChooser(id, title, fileSelectionMode == null ? -1 : fileSelectionMode, fileFilter, multiSelection == null ? false : multiSelection, dialogType == null ? -1 : dialogType, null);
     }
 
     /**
