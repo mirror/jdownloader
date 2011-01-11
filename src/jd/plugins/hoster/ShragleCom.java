9a038a2399af0343fea260d02d3bd054019ee68f
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-01-11 23:15:23 +0000

    -Fixed #2820
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13302 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ShragleCom.java
+++ src/jd/plugins/hoster/ShragleCom.java
@@ -28,54 +28,34 @@ import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
-import jd.plugins.DownloadLink.AvailableStatus;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "shragle.com" }, urls = { "http://[\\w\\.]*?shragle\\.(com|de)/files/[\\w]+/.*" }, flags = { 2 })
 public class ShragleCom extends PluginForHost {
 
     static String apikey = "078e5ca290d728fd874121030efb4a0d";
 
-    public ShragleCom(PluginWrapper wrapper) {
+    public ShragleCom(final PluginWrapper wrapper) {
         super(wrapper);
         this.enablePremium("http://www.shragle.com/index.php?p=accounts&ref=386");
-        setStartIntervall(5000l);
+        this.setStartIntervall(5000l);
     }
 
     @Override
-    public void init() {
-        br.setRequestIntervalLimit(this.getHost(), 800);
+    public void correctDownloadLink(final DownloadLink link) {
+        link.setUrlDownload(link.getDownloadURL().replaceAll("\\.de/", "\\.com/"));
     }
 
     @Override
-    public String getAGBLink() {
-        return "http://www.shragle.com/index.php?cat=about&p=faq";
-    }
-
-    private void login(Account account) throws IOException, PluginException {
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        final AccountInfo ai = new AccountInfo();
         this.setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.getPage("http://www.shragle.com/index.php?p=login");
-        br.postPage("http://www.shragle.com/index.php?p=login", "username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(account.getPass()) + "&cookie=1&submit=Login");
-        String Cookie = br.getCookie("http://www.shragle.com", "userID");
-        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
-        Cookie = br.getCookie("http://www.shragle.com", "username");
-        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
-        Cookie = br.getCookie("http://www.shragle.com", "password");
-        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
-        br.getPage("http://www.shragle.com/?cat=user");
-        if (br.containsHTML(">Premium-Upgrade<")) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
-    }
-
-    @Override
-    public AccountInfo fetchAccountInfo(Account account) throws Exception {
-        AccountInfo ai = new AccountInfo();
-        this.setBrowserExclusive();
-        br.getPage("http://www.shragle.com/api.php?key=" + apikey + "&action=checkUser&useMD5=true&username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(JDHash.getMD5(account.getPass())));
-        String accountinfos[] = br.getRegex("(.*?)\\|(.*?)\\|(.+)").getRow(0);
+        this.br.getPage("http://www.shragle.com/api.php?key=" + ShragleCom.apikey + "&action=checkUser&useMD5=true&username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(JDHash.getMD5(account.getPass())));
+        final String accountinfos[] = this.br.getRegex("(.*?)\\|(.*?)\\|(.+)").getRow(0);
         if (accountinfos == null) {
             account.setValid(false);
             return ai;
@@ -92,90 +72,8 @@ public class ShragleCom extends PluginForHost {
     }
 
     @Override
-    public void correctDownloadLink(DownloadLink link) {
-        link.setUrlDownload(link.getDownloadURL().replaceAll("\\.de/", "\\.com/"));
-    }
-
-    @Override
-    public void handlePremium(DownloadLink downloadLink, Account account) throws Exception {
-        requestFileInformation(downloadLink);
-        login(account);
-        br.setFollowRedirects(false);
-        br.setCookie("http://www.shragle.com", "lang", "de_DE");
-        br.getPage(downloadLink.getDownloadURL());
-        if (br.getRedirectLocation() != null && br.getRedirectLocation().contains("index.php")) br.getPage(br.getRedirectLocation());
-        if (br.getRedirectLocation() != null) {
-            br.setFollowRedirects(true);
-            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, br.getRedirectLocation(), true, -4);
-        } else {
-            Form form = br.getFormbyProperty("name", "download");
-            br.setFollowRedirects(true);
-            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, form, true, 0);
-        }
-        URLConnectionAdapter con = dl.getConnection();
-        if (con.getContentType() != null && con.getContentType().contains("html")) {
-            br.followConnection();
-            if (br.containsHTML("bereits eine Datei herunter")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "IP is already loading, please wait!", 10 * 60 * 1000l);
-            if (br.containsHTML("The selected file was not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            if ((br.containsHTML("Die von Ihnen angeforderte Datei") && br.containsHTML("Bitte versuchen Sie es"))) {
-                if (downloadLink.getLinkStatus().getRetryCount() == 1) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "ServerError", 30 * 60 * 1000l);
-            }
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
-    }
-
-    @Override
-    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws PluginException, IOException {
-        setBrowserExclusive();
-        String id = new Regex(downloadLink.getDownloadURL(), "shragle.com/files/(.*?)/").getMatch(0);
-        String[] data = Regex.getLines(br.getPage("http://www.shragle.com/api.php?key=" + apikey + "&action=getStatus&fileID=" + id));
-        if (data.length != 4) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String name = data[0];
-        String size = data[1];
-        String md5 = data[2];
-        // status 0: all ok 1: abused
-        String status = data[3];
-        if (!status.equals("0")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        downloadLink.setFinalFileName(name.trim());
-        downloadLink.setDownloadSize(Long.parseLong(size));
-        downloadLink.setMD5Hash(md5.trim());
-        return AvailableStatus.TRUE;
-    }
-
-    @Override
-    public void handleFree(DownloadLink downloadLink) throws Exception {
-        br.setFollowRedirects(true);
-        requestFileInformation(downloadLink);
-        br.setCookie("http://www.shragle.com", "lang", "de_DE");
-        if (downloadLink.getDownloadURL().contains("?")) {
-            br.getPage(downloadLink.getDownloadURL() + "&jd=1");
-        } else {
-            br.getPage(downloadLink.getDownloadURL() + "?jd=1");
-        }
-        boolean mayfail = br.getRegex("Download-Server ist unter").matches();
-        String wait = br.getRegex(Pattern.compile("Bitte warten Sie(.*?)Minuten", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
-        if (wait != null) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(wait.trim()) * 60 * 1000l); }
-        wait = br.getRegex("var downloadWait =(.*?);").getMatch(0);
-        Form form = br.getFormbyProperty("name", "download");
-        if (form == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        if (wait == null) wait = "10";
-        sleep(Long.parseLong(wait.trim()) * 1000l, downloadLink);
-        form.setAction(form.getAction() + "?jd=1");
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, form, true, 1);
-        URLConnectionAdapter con = dl.getConnection();
-        if (con.getContentType() != null && con.getContentType().contains("html")) {
-            br.followConnection();
-            if (br.containsHTML("bereits eine Datei herunter")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "IP is already loading, please wait!", 10 * 60 * 1000l);
-            if (br.containsHTML("The selected file was not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            if ((br.containsHTML("Die von Ihnen angeforderte Datei") && br.containsHTML("Bitte versuchen Sie es")) || mayfail) {
-                if (downloadLink.getLinkStatus().getRetryCount() == 1) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "ServerError", 30 * 60 * 1000l);
-            }
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
+    public String getAGBLink() {
+        return "http://www.shragle.com/index.php?cat=about&p=faq";
     }
 
     @Override
@@ -189,14 +87,123 @@ public class ShragleCom extends PluginForHost {
     }
 
     @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+        this.br.setFollowRedirects(true);
+        this.requestFileInformation(downloadLink);
+        this.br.setCookie("http://www.shragle.com", "lang", "de_DE");
+        if (downloadLink.getDownloadURL().contains("?")) {
+            this.br.getPage(downloadLink.getDownloadURL() + "&jd=1");
+        } else {
+            this.br.getPage(downloadLink.getDownloadURL() + "?jd=1");
+        }
+        final boolean mayfail = this.br.getRegex("Download-Server ist unter").matches();
+        String wait = this.br.getRegex(Pattern.compile("Bitte warten Sie(.*?)Minuten", Pattern.CASE_INSENSITIVE | Pattern.DOTALL)).getMatch(0);
+        if (wait != null) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(wait.trim()) * 60 * 1000l); }
+        wait = this.br.getRegex("var downloadWait =(.*?);").getMatch(0);
+        final Form form = this.br.getFormbyProperty("name", "download");
+        if (form == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+        if (wait == null) {
+            wait = "10";
+        }
+        this.sleep(Long.parseLong(wait.trim()) * 1000l, downloadLink);
+        form.setAction(form.getAction());
+        form.remove("submit");
+        this.dl = jd.plugins.BrowserAdapter.openDownload(this.br, downloadLink, form, true, 1);
+        final URLConnectionAdapter con = this.dl.getConnection();
+        if ((con.getContentType() != null) && con.getContentType().contains("html")) {
+            this.br.followConnection();
+            if (this.br.containsHTML("bereits eine Datei herunter")) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "IP is already loading, please wait!", 10 * 60 * 1000l); }
+            if (this.br.containsHTML("The selected file was not found")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+            if ((this.br.containsHTML("Die von Ihnen angeforderte Datei") && this.br.containsHTML("Bitte versuchen Sie es")) || mayfail) {
+                if (downloadLink.getLinkStatus().getRetryCount() == 1) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "ServerError", 30 * 60 * 1000l);
+            }
+            con.disconnect();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        this.dl.startDownload();
+    }
+
+    @Override
+    public void handlePremium(final DownloadLink downloadLink, final Account account) throws Exception {
+        this.requestFileInformation(downloadLink);
+        this.login(account);
+        this.br.setCookie("http://www.shragle.com", "lang", "de_DE");
+        this.br.getPage(downloadLink.getDownloadURL());
+        this.br.setFollowRedirects(false);
+        if ((this.br.getRedirectLocation() != null) && this.br.getRedirectLocation().contains("index.php")) {
+            this.br.getPage(this.br.getRedirectLocation());
+        }
+        if (this.br.getRedirectLocation() != null) {
+            this.br.setFollowRedirects(true);
+            this.dl = jd.plugins.BrowserAdapter.openDownload(this.br, downloadLink, this.br.getRedirectLocation(), true, -4);
+        } else {
+            final Form form = this.br.getFormbyProperty("name", "download");
+            this.br.setFollowRedirects(true);
+            this.dl = jd.plugins.BrowserAdapter.openDownload(this.br, downloadLink, form, true, 0);
+        }
+        final URLConnectionAdapter con = this.dl.getConnection();
+        if ((con.getContentType() != null) && con.getContentType().contains("html")) {
+            this.br.followConnection();
+            if (this.br.containsHTML("bereits eine Datei herunter")) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "IP is already loading, please wait!", 10 * 60 * 1000l); }
+            if (this.br.containsHTML("The selected file was not found")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+            if ((this.br.containsHTML("Die von Ihnen angeforderte Datei") && this.br.containsHTML("Bitte versuchen Sie es"))) {
+                if (downloadLink.getLinkStatus().getRetryCount() == 1) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "ServerError", 30 * 60 * 1000l);
+            }
+            con.disconnect();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        this.dl.startDownload();
+    }
+
+    @Override
+    public void init() {
+        this.br.setRequestIntervalLimit(this.getHost(), 800);
+    }
+
+    private void login(final Account account) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        this.br.setFollowRedirects(true);
+        this.br.getPage("http://www.shragle.com/index.php?p=login");
+        this.br.postPage("http://www.shragle.com/index.php?p=login", "username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(account.getPass()) + "&cookie=1&submit=Login");
+        String Cookie = this.br.getCookie("http://www.shragle.com", "userID");
+        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+        Cookie = this.br.getCookie("http://www.shragle.com", "username");
+        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+        Cookie = this.br.getCookie("http://www.shragle.com", "password");
+        if (Cookie == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+        this.br.getPage("http://www.shragle.com/?cat=user");
+        if (this.br.containsHTML(">Premium-Upgrade<")) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws PluginException, IOException {
+        this.setBrowserExclusive();
+        final String id = new Regex(downloadLink.getDownloadURL(), "shragle.com/files/(.*?)/").getMatch(0);
+        final String[] data = org.appwork.utils.Regex.getLines(this.br.getPage("http://www.shragle.com/api.php?key=" + ShragleCom.apikey + "&action=getStatus&fileID=" + id));
+        if (data.length != 4) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        final String name = data[0];
+        final String size = data[1];
+        final String md5 = data[2];
+        // status 0: all ok 1: abused
+        final String status = data[3];
+        if (!status.equals("0")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        downloadLink.setFinalFileName(name.trim());
+        downloadLink.setDownloadSize(Long.parseLong(size));
+        downloadLink.setMD5Hash(md5.trim());
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
     public void reset() {
     }
 
     @Override
-    public void resetPluginGlobals() {
+    public void resetDownloadlink(final DownloadLink link) {
     }
 
     @Override
-    public void resetDownloadlink(DownloadLink link) {
+    public void resetPluginGlobals() {
     }
 }
