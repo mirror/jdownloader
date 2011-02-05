12b309acbbdb83dc534744a9cf9378a5002312a1
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-02-05 19:06:11 +0000

    -Made a new plugin for filedude so the appscene plugin can be deleted soon refs #2946
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13536 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/AppSceneOrg.java
+++ src/jd/plugins/hoster/AppSceneOrg.java
@@ -18,28 +18,18 @@ package jd.plugins.hoster;
 import java.io.IOException;
 
 import jd.PluginWrapper;
-import jd.http.RandomUserAgent;
-import jd.nutils.encoding.Encoding;
-import jd.parser.html.Form;
-import jd.plugins.Account;
-import jd.plugins.AccountInfo;
-import jd.plugins.BrowserAdapter;
 import jd.plugins.DownloadLink;
-import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
-import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
 
-import org.appwork.utils.formatter.SizeFormatter;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "appscene.org" }, urls = { "http://[\\w\\.]*?(appscene\\.org|filedude\\.com)/(download/[0-9a-zA-Z]+|download\\.php\\?id=\\d+)" }, flags = { 2 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "appscene.org2" }, urls = { "http://[\\w\\.]*?(appscene\\.org|filedude\\.com)/(download/[0-9a-zA-Z]+|download\\.php\\?id=\\d+)" }, flags = { 0 })
 public class AppSceneOrg extends PluginForHost {
     // TODO: rename the whole class (delete this one and use "FileDudeCom" as
     // new classname
     public AppSceneOrg(PluginWrapper wrapper) {
         super(wrapper);
-        this.enablePremium("http://www.filedude.com/premium");
     }
 
     public void correctDownloadLink(DownloadLink link) {
@@ -51,90 +41,13 @@ public class AppSceneOrg extends PluginForHost {
         return "http://www.filedude.com/terms";
     }
 
-    private String FILEDUDECAPTCHAREGEX = "\"(http://www.filedude\\.com/captcha/.*?\\.jpg)\"";
-
     @Override
     public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
-        setBrowserExclusive();
-        br.getHeaders().put("User-Agent", RandomUserAgent.generate());
-        String url = downloadLink.getDownloadURL();
-        br.getPage(url);
-        if (br.containsHTML("or has been deleted")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         return AvailableStatus.TRUE;
     }
 
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
-        requestFileInformation(downloadLink);
-        boolean failed = true;
-        for (int i = 0; i <= 4; i++) {
-            Form captchaForm = br.getForm(0);
-            String captchaurl = br.getRegex(FILEDUDECAPTCHAREGEX).getMatch(0);
-            if (captchaForm == null || captchaurl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            String code;
-            if (i > 2) {
-                code = getCaptchaCode(null, captchaurl, downloadLink);
-            } else {
-                code = getCaptchaCode(captchaurl, downloadLink);
-            }
-            captchaForm.put("captcha", code);
-            br.submitForm(captchaForm);
-            if (br.getRedirectLocation() != null) {
-                br.getPage(br.getRedirectLocation());
-                continue;
-            }
-            failed = false;
-            break;
-        }
-        if (failed) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
-        String dllink = br.getRegex("<div style=\"text-align: center; font-size: 30px;\">[\n\r\t ]+<a href=\"(http://.*?)\"").getMatch(0);
-        if (dllink == null) dllink = br.getRegex("\"(http://srv\\d+\\..*?\\.filedude\\.com/getN/.*?/.*?)\"").getMatch(0);
-        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        dl = BrowserAdapter.openDownload(br, downloadLink, dllink, false, 1);
-        if (dl.getConnection().getContentType().contains("html")) {
-            br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
-    }
-
-    private void login(Account account) throws Exception {
-        this.setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.getPage("http://www.appscene.org/premium");
-        br.postPage("http://www.filedude.com/premium_login", "user=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()));
-        if (!br.containsHTML("Premium Area") || br.getCookie("http://www.filedude.com", "premium_id") == null || br.getCookie("http://www.filedude.com/", "premium_pass") == null) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-    }
-
-    @Override
-    public AccountInfo fetchAccountInfo(Account account) throws Exception {
-        AccountInfo ai = new AccountInfo();
-        try {
-            login(account);
-        } catch (PluginException e) {
-            account.setValid(false);
-            return ai;
-        }
-        account.setValid(true);
-        String trafficleft = br.getRegex("\\(( [0-9]+ B )\\)").getMatch(0);
-        if (trafficleft == null) trafficleft = br.getRegex("Bandwidth remaining:(.*? GB)").getMatch(0);
-        if (trafficleft != null) ai.setTrafficLeft(SizeFormatter.getSize(trafficleft));
-        ai.setStatus("Premium User");
-        return ai;
-    }
-
-    public void handlePremium(DownloadLink link, Account account) throws Exception {
-        requestFileInformation(link);
-        login(account);
-        br.setFollowRedirects(false);
-        br.getPage(link.getDownloadURL());
-        String finallink = br.getRedirectLocation();
-        jd.plugins.BrowserAdapter.openDownload(br, link, finallink, true, 1);
-        if (dl.getConnection().getContentType().contains("html")) {
-            br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
     }
 
     @Override
@@ -143,11 +56,6 @@ public class AppSceneOrg extends PluginForHost {
 
     @Override
     public void resetPluginGlobals() {
-    }
-
-    @Override
-    public int getMaxSimultanPremiumDownloadNum() {
-        return -1;
     }
 
     @Override
