030cf4022ff234f4654c7175b5edf17d5eab5278
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-02-26 16:43:59 +0000

    -Fixed a decrypter and cleaned it up
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13712 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/PWProtectedRedirectorsDecrypter.java
+++ src/jd/plugins/decrypter/PWProtectedRedirectorsDecrypter.java
@@ -29,14 +29,13 @@ import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 import jd.utils.locale.JDL;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "cliccami.info", "lempar.co.tv", "mylink4u.info", "linkoculto.com", "urlaxe.net", "dwarfurl.com", "skracaj.org", "l-x.pl", "xlurl.com", "cbuz.com", "shorten.ws", "smallizer.com", "zi.ma" }, urls = { "http://[\\w\\.]*?cliccami\\.info/[0-9a-z]+", "http://[\\w\\.]*?lempar\\.co\\.tv/[0-9a-z]+", "http://[\\w\\.]*?mylink4u\\.info/[a-z0-9]+", "http://[\\w\\.]*?linkoculto\\.com/[a-z0-9]+", "http://[\\w\\.]*?urlaxe\\.net/[0-9]+", "http://[\\w\\.]*?dwarfurl\\.com/[a-z0-9]+", "http://[\\w\\.]*?skracaj\\.org/[a-z0-9]+", "http://[\\w\\.]*?l-x\\.pl/[a-z0-9]+", "http://[\\w\\.]*?xlurl\\.com/[a-z0-9]+", "http://[\\w\\.]*?cbuz\\.com/[a-z0-9]+", "http://[\\w\\.]*?shorten\\.ws/[a-z0-9]+", "http://[\\w\\.]*?smallizer\\.com/[a-z0-9]+", "http://[\\w\\.]*?zi\\.ma/[a-z0-9]+" }, flags = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "cliccami.info", "mylink4u.info", "urlaxe.net", "cbuz.com", "shorten.ws" }, urls = { "http://[\\w\\.]*?cliccami\\.info/[0-9a-z]+", "http://[\\w\\.]*?mylink4u\\.info/[a-z0-9]+", "http://[\\w\\.]*?urlaxe\\.net/[0-9]+", "http://[\\w\\.]*?cbuz\\.com/[a-z0-9]+", "http://(www\\.)?shorten\\.ws/[a-z0-9-]+" }, flags = { 0, 0, 0, 0, 0 })
 public class PWProtectedRedirectorsDecrypter extends PluginForDecrypt {
 
     public PWProtectedRedirectorsDecrypter(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    // TODO: Implement dwarfurl.com pw-protected links handling
     // TODO: Make a single handling for every site so it isn't such a mess
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
@@ -46,17 +45,11 @@ public class PWProtectedRedirectorsDecrypter extends PluginForDecrypt {
         br.setFollowRedirects(false);
         br.getPage(parameter);
         String domain = new Regex(parameter, "([a-z-]+)\\.").getMatch(0);
-        if (br.containsHTML("(non ci sono URL|There is no such URL in our database|Esa url no se encuentra|Taki skrót <b>nie istnieje|Witaj na l-x.pl|URL proibito|questo URL e' scaduto|This URL expired)") || (br.getRedirectLocation() != null && br.getRedirectLocation().contains(domain)) || (br.getRedirectLocation() == null && !br.containsHTML("ADULT CONTENT WARNING"))) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
+        if (br.containsHTML("(non ci sono URL|There is no such URL in our database|Esa url no se encuentra|Taki skrót <b>nie istnieje|Witaj na l-x.pl|URL proibito|questo URL e' scaduto|This URL expired)") || (br.getRedirectLocation() != null && br.getRedirectLocation().contains(domain))) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
         String finallink = br.getRedirectLocation();
         if (finallink == null) {
             // For iframe stuff
             finallink = br.getRegex("id=\"frame\" src=\"(.*?)\"").getMatch(0);
-            // Handling for ncane adult links
-            if (finallink == null) finallink = br.getRegex("or older, click <a href=\"(.*?)\"").getMatch(0);
-            // Handling for xlurl.com links
-            if (finallink == null) finallink = br.getRegex("window\\.location = \"(.*?)\"").getMatch(0);
-            // Handling for smallizer.com links
-            if (finallink == null) finallink = br.getRegex("\"removeFrame\".*?href=\"(.*?)\"").getMatch(0);
             if (finallink == null && (br.containsHTML("(name=\"pass\"|name=\"p\"|name=\"shortcut_password\"|name=\"urlpass\")"))) {
                 for (int i = 0; i <= 2; i++) {
                     Form pwform = br.getForm(0);
@@ -64,10 +57,6 @@ public class PWProtectedRedirectorsDecrypter extends PluginForDecrypt {
                     String passCode = getUserInput(null, param);
                     if (parameter.contains("urlaxe.net")) {
                         pwform.put("p", passCode);
-                    } else if (parameter.contains("skracaj.org") || parameter.contains("l-x.pl")) {
-                        pwform.put("shortcut_password", passCode);
-                    } else if (parameter.contains("zi.ma")) {
-                        pwform.put("urlpass", passCode);
                     } else {
                         pwform.put("pass", passCode);
                     }
