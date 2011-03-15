b9aaecec0e8a7990c0e9058f92efcea7459a22b9
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-03-15 15:55:45 +0000

    TuDouCom: updated
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13823 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/TuDouCom.java
+++ src/jd/plugins/hoster/TuDouCom.java
@@ -30,7 +30,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "tudou.com" }, urls = { "http://[\\w\\.]*?tudou\\.com/programs/view/[A-Za-z0-9]+" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "tudou.com" }, urls = { "http://[\\w\\.]*?tudou\\.com/programs/view/[A-Za-z0-9\\-_]+" }, flags = { 0 })
 public class TuDouCom extends PluginForHost {
 
     public TuDouCom(PluginWrapper wrapper) {
