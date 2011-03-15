480d13c1bf1990368eba18f883c71b04811afda7
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-03-15 18:48:24 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13825 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/update/JDUpdater.java
+++ src/org/jdownloader/update/JDUpdater.java
@@ -86,6 +86,15 @@ public class JDUpdater extends Updater implements Runnable {
 
     }
 
+    public void start() throws Exception {
+        try {
+            super.start();
+        } catch (Exception e) {
+            if (isInterrupted()) { throw new InterruptedException(e.getMessage()); }
+            throw e;
+        }
+    }
+
     public void setBranchInUse(String branch) {
         storage.setProperty(PARAM_BRANCH, branch);
 
