ac9231256bc25bcd9457a8208ff9c32ee8f72c10
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-03-28 22:10:21 +0000

    -Fixes #3209
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13951 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PornHubCom.java
+++ src/jd/plugins/hoster/PornHubCom.java
@@ -10,13 +10,15 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
+import org.appwork.utils.Regex;
+
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornhub.com" }, urls = { "http://[\\w\\.]*?pornhub\\.com/view_video\\.php\\?viewkey=[a-z0-9]+" }, flags = { 0 })
 public class PornHubCom extends PluginForHost {
 
     private static String post_element = "add299463d4410c6d1b1c418868225f7";
-    private String dlUrl = null;
+    private String        dlUrl        = null;
 
-    public PornHubCom(PluginWrapper wrapper) {
+    public PornHubCom(final PluginWrapper wrapper) {
         super(wrapper);
     }
 
@@ -26,9 +28,14 @@ public class PornHubCom extends PluginForHost {
     }
 
     @Override
-    public void handleFree(DownloadLink link) throws Exception {
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception {
         requestVideo(link);
-        if (dlUrl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (dlUrl == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
         dl = jd.plugins.BrowserAdapter.openDownload(br, link, dlUrl, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
             dl.getConnection().disconnect();
@@ -37,37 +44,10 @@ public class PornHubCom extends PluginForHost {
         dl.startDownload();
     }
 
-    private AvailableStatus requestVideo(DownloadLink downloadLink) throws IOException, PluginException {
-        this.setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.getPage(downloadLink.getDownloadURL());
-        dlUrl = null;
-        String file_name = br.getRegex("video-title-nf\" style=\"height:[0-9]+px;\">.*<h1>(.*?)</h1>").getMatch(0);
-        if (file_name == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String[] linksplit = downloadLink.getDownloadURL().split("=");
-        String video_id = linksplit[linksplit.length - 1];
-
-        byte[] b = new byte[] { 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0c, 0x70, 0x6c, 0x61, 0x79, 0x65, 0x72, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x00, 0x02, 0x2f, 0x31, 0x00, 0x00, 0x00, 0x44, 0x0a, 0x00, 0x00, 0x00, 0x03, 0x02, 0x00 };
-        byte[] b2 = new byte[] { 0x02, 0x00, 0x02, 0x2d, 0x31, 0x02, 0x00, 0x20 };
-
-        String url = "http://www.pornhub.com//gateway.php";
-        String postdata = new String(b) + new String(new byte[] { (byte) video_id.length() }) + video_id + new String(b2) + post_element;
-
-        br.getHeaders().put("Content-Type", "application/x-amf");
-        br.setFollowRedirects(false);
-        br.postPageRaw(url, postdata);
-
-        dlUrl = br.getRegex("flv_url.*(http.*?)\\?r=.*").getMatch(0);
-        if (dlUrl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        linksplit = dlUrl.split("\\.");
-        downloadLink.setFinalFileName(file_name + "." + linksplit[linksplit.length - 1]);
-        return AvailableStatus.TRUE;
-    }
-
     @Override
-    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws Exception {
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
         requestVideo(downloadLink);
-        this.setBrowserExclusive();
+        setBrowserExclusive();
         br.setFollowRedirects(true);
         try {
             if (!br.openGetConnection(dlUrl).getContentType().contains("html")) {
@@ -76,9 +56,45 @@ public class PornHubCom extends PluginForHost {
                 return AvailableStatus.TRUE;
             }
         } finally {
-            if (br.getHttpConnection() != null) br.getHttpConnection().disconnect();
+            if (br.getHttpConnection() != null) {
+                br.getHttpConnection().disconnect();
+            }
         }
         throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+    }
+
+    private AvailableStatus requestVideo(final DownloadLink downloadLink) throws IOException, PluginException {
+        setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(downloadLink.getDownloadURL());
+        dlUrl = null;
+        final String file_name = br.getRegex("video-title-nf\" style=\"height:[0-9]+px;\">.*<h1>(.*?)</h1>").getMatch(0);
+        if (file_name == null || br.containsHTML("Video has been reported as copyrighted material")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        String[] linksplit = downloadLink.getDownloadURL().split("=");
+        final String video_id = linksplit[linksplit.length - 1];
+
+        final byte[] b = new byte[] { 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0c, 0x70, 0x6c, 0x61, 0x79, 0x65, 0x72, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x00, 0x02, 0x2f, 0x31, 0x00, 0x00, 0x00, 0x44, 0x0a, 0x00, 0x00, 0x00, 0x03, 0x02, 0x00 };
+        final byte[] b2 = new byte[] { 0x02, 0x00, 0x02, 0x2d, 0x31, 0x02, 0x00, 0x20 };
+
+        final String url = "http://www.pornhub.com//gateway.php";
+        final String postdata = new String(b) + new String(new byte[] { (byte) video_id.length() }) + video_id + new String(b2) + post_element;
+
+        br.getHeaders().put("Content-Type", "application/x-amf");
+        br.setFollowRedirects(false);
+        br.postPageRaw(url, postdata);
+
+        final byte[] raw = br.toString().getBytes();
+        for (int i = 0; i <= raw.length - 1; i++) {
+            if (raw[i] < 32 || raw[i] > 176) {
+                raw[i] = 35; // #
+            }
+        }
+
+        dlUrl = new Regex(new String(raw), "flv_url.*?(http.*?)##post_roll").getMatch(0);
+        if (dlUrl == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+        linksplit = dlUrl.split("\\.");
+        downloadLink.setFinalFileName(file_name + ".flv");
+        return AvailableStatus.TRUE;
     }
 
     @Override
@@ -86,11 +102,6 @@ public class PornHubCom extends PluginForHost {
     }
 
     @Override
-    public void resetDownloadlink(DownloadLink link) {
-    }
-
-    @Override
-    public int getMaxSimultanFreeDownloadNum() {
-        return -1;
+    public void resetDownloadlink(final DownloadLink link) {
     }
 }
