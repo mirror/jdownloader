12aa24ad85930720fb3c2118616af2164031f14d
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-04-01 09:07:03 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@13999 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/antireconnect/JDAntiReconnectTranslation.java
+++ src/org/jdownloader/extensions/antireconnect/JDAntiReconnectTranslation.java
@@ -7,22 +7,22 @@ import org.appwork.txtresource.TranslateInterface;
 @Defaults(lngs = { "en" })
 public interface JDAntiReconnectTranslation extends TranslateInterface {
 
-    @Default(lngs = { "en", }, values = { "Disabled" })
+    @Default(lngs = { "en" }, values = { "Disabled" })
     String mode_disabled();
 
-    @Default(lngs = { "en", }, values = { "Detect only by Ping (faster)" })
+    @Default(lngs = { "en" }, values = { "Detect only by Ping (faster)" })
     String mode_ping();
 
-    @Default(lngs = { "en", }, values = { "Ping & ARP (recommended)" })
+    @Default(lngs = { "en" }, values = { "Ping & ARP (recommended)" })
     String mode_arp();
 
-    @Default(lngs = { "en", }, values = { "Anti Reconnect" })
+    @Default(lngs = { "en" }, values = { "Anti Reconnect" })
     String name();
 
-    @Default(lngs = { "en", }, values = { "Could not start addon. Error: %s1" })
+    @Default(lngs = { "en" }, values = { "Could not start addon. Error: %s1" })
     String start_failed(String message);
 
-    @Default(lngs = { "en", }, values = { "Automaticly disables reconnect features of one ore more network conditions match." })
+    @Default(lngs = { "en" }, values = { "Automaticly disables reconnect features of one ore more network conditions match." })
     String description();
 
 }
