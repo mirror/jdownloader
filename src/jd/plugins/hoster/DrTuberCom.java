575bf6db43d90f2818f3c08e4892e92a457cbfeb
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-04-07 15:26:56 +0000

    -Fixes #3251
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14044 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/DrTuberCom.java
+++ src/jd/plugins/hoster/DrTuberCom.java
@@ -24,20 +24,20 @@ import jd.http.URLConnectionAdapter;
 import jd.nutils.JDHash;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
-import jd.plugins.DownloadLink.AvailableStatus;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "drtuber.com" }, urls = { "http://(www\\.)?drtuber\\.com/video/\\d+" }, flags = { 0 })
 public class DrTuberCom extends PluginForHost {
 
-    public DrTuberCom(PluginWrapper wrapper) {
+    private String DLLINK = null;
+
+    public DrTuberCom(final PluginWrapper wrapper) {
         super(wrapper);
     }
-
-    private String DLLINK = null;
 
     @Override
     public String getAGBLink() {
@@ -45,47 +45,12 @@ public class DrTuberCom extends PluginForHost {
     }
 
     @Override
-    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
-        this.setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.setDebug(true);
-        br.getPage(downloadLink.getDownloadURL());
-        if (br.containsHTML("(This video cannot be found\\.|Are you sure you typed in the correct url\\?<)") || br.getURL().contains("missing=true")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String filename = br.getRegex("<title>(.*?) - DrTuber\\.com</title>").getMatch(0);
-        if (filename == null) filename = br.getRegex("<h1 class=\"name\">(.*?)</h1>").getMatch(0);
-        String vKey = br.getRegex("vkey=([0-9a-z]+)").getMatch(0);
-        if (vKey == null) {
-            logger.warning("vKey is null...");
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        br.getHeaders().put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
-        String continueLink = br.getRegex("addVariable\\(\\'config\\', \\'(/.*?)\\'\\);").getMatch(0);
-        if (continueLink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        continueLink = "http://drtuber.com" + Encoding.htmlDecode(continueLink) + "&pkey=" + JDHash.getMD5(vKey + Encoding.Base64Decode("bm0zOWRjbjI0dDA5OHM="));
-        br.getPage(continueLink);
-        DLLINK = br.getRegex("<video_file>(http://.*?\\.flv)</video_file>").getMatch(0);
-        if (filename == null || DLLINK == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        filename = filename.trim();
-        downloadLink.setFinalFileName(filename + ".flv");
-        Browser br2 = br.cloneBrowser();
-        URLConnectionAdapter con = null;
-        try {
-            con = br2.openGetConnection(DLLINK);
-            if (!con.getContentType().contains("html"))
-                downloadLink.setDownloadSize(con.getLongContentLength());
-            else
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            return AvailableStatus.TRUE;
-        } finally {
-            try {
-                con.disconnect();
-            } catch (Throwable e) {
-            }
-        }
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
     }
 
     @Override
-    public void handleFree(DownloadLink downloadLink) throws Exception {
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 1);
         if (dl.getConnection().getContentType().contains("html")) {
@@ -96,8 +61,46 @@ public class DrTuberCom extends PluginForHost {
     }
 
     @Override
-    public int getMaxSimultanFreeDownloadNum() {
-        return -1;
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws IOException, PluginException {
+        setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.setDebug(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("(This video cannot be found\\.|Are you sure you typed in the correct url\\?<)") || br.getURL().contains("missing=true")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        String filename = br.getRegex("<title>(.*?) - DrTuber\\.com</title>").getMatch(0);
+        if (filename == null) {
+            filename = br.getRegex("<h1 class=\"name\">(.*?)</h1>").getMatch(0);
+        }
+        final String vKey = br.getRegex("vkey=([0-9a-z]+)").getMatch(0);
+        if (vKey == null) {
+            logger.warning("vKey is null...");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getHeaders().put("Accept-Language", "de-de,de;q=0.8,en-us;q=0.5,en;q=0.3");
+        String continueLink = br.getRegex("addVariable\\(\\'config\\', \\'(/.*?)\\'\\);").getMatch(0);
+        if (continueLink == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+        continueLink = "http://drtuber.com" + Encoding.htmlDecode(continueLink) + "&pkey=" + JDHash.getMD5(vKey + Encoding.Base64Decode("ZnJVejg4cnVmUmE2cjM="));
+        br.getPage(continueLink);
+        DLLINK = br.getRegex("<video_file>(http://.*?\\.flv)</video_file>").getMatch(0);
+        if (filename == null || DLLINK == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+        filename = filename.trim();
+        downloadLink.setFinalFileName(filename + ".flv");
+        final Browser br2 = br.cloneBrowser();
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openGetConnection(DLLINK);
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Throwable e) {
+            }
+        }
     }
 
     @Override
@@ -105,10 +108,10 @@ public class DrTuberCom extends PluginForHost {
     }
 
     @Override
-    public void resetPluginGlobals() {
+    public void resetDownloadlink(final DownloadLink link) {
     }
 
     @Override
-    public void resetDownloadlink(DownloadLink link) {
+    public void resetPluginGlobals() {
     }
 }
