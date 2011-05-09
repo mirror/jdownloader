4382f9a284e5404d99702df834277f85e5db2cbd
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-05-09 16:48:41 +0000

    -Improved handling for files.mail.ru errors
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14278 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FilesMailRu.java
+++ src/jd/plugins/hoster/FilesMailRu.java
@@ -22,11 +22,11 @@ import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
-import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.utils.locale.JDL;
 
 import org.appwork.utils.formatter.SizeFormatter;
@@ -52,10 +52,10 @@ public class FilesMailRu extends PluginForHost {
         link.setUrlDownload(link.getDownloadURL().replaceAll("wge4zu4rjfsdehehztiuxw", "files.mail.ru"));
     }
 
-    private static final String DLLINKREGEX = "\"(http://[a-z0-9-]+\\.files\\.mail\\.ru/.*?/.*?)\"";
+    private static final String DLLINKREGEX  = "\"(http://[a-z0-9-]+\\.files\\.mail\\.ru/.*?/.*?)\"";
     private static final String UNAVAILABLE1 = ">В обработке<";
     private static final String UNAVAILABLE2 = ">In process<";
-    private static final String INFOREGEX = "<td class=\"name\">(.*?<td class=\"do\">.*?)</td>";
+    private static final String INFOREGEX    = "<td class=\"name\">(.*?<td class=\"do\">.*?)</td>";
 
     // private static final boolean RESUME = true;
     // private static final int MAXCHUNKS = -10;
@@ -142,17 +142,19 @@ public class FilesMailRu extends PluginForHost {
         // Errorhandling, sometimes the link which is usually renewed by the
         // linkgrabber doesn't work and needs to be refreshed again!
         int chunks = -10;
-        if (downloadLink.getStringProperty("disablechunks") != null) chunks = 1;
+        boolean resume = true;
+        if (downloadLink.getStringProperty("disablechunks") != null) {
+            chunks = 1;
+        }
         if (premium) chunks = 0;
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, downloadLink.getDownloadURL(), true, chunks);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, downloadLink.getDownloadURL(), resume, chunks);
         if (dl.getConnection().getResponseCode() == 503) { throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Too many simultan downloads!"); }
-        dl.startDownload();
-        if (downloadLink.getLinkStatus().getErrorMessage() != null) {
-            if (downloadLink.getLinkStatus().getErrorMessage().contains("Service Temporarily Unavailable")) {
-                downloadLink.setProperty("disablechunks", "disable");
-                downloadLink.setChunksProgress(null);
-                throw new PluginException(LinkStatus.ERROR_RETRY, "Chunkerror");
-            }
+        if (!this.dl.startDownload()) {
+            // Chunk-Error or server-error
+            downloadLink.setProperty("disablechunks", "disable");
+            downloadLink.reset();
+            logger.warning(downloadLink.getLinkStatus().getErrorMessage());
+            throw new PluginException(LinkStatus.ERROR_RETRY, "Chunkerror");
         }
     }
 
