d8c5a185be01024a964659ae481dd3f0429d48f5
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-05-22 16:44:55 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14345 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/network/rtmp/url/CustomUrlStreamHandlerFactory.java
+++ src/jd/network/rtmp/url/CustomUrlStreamHandlerFactory.java
@@ -5,19 +5,18 @@ import java.net.URLStreamHandlerFactory;
 
 public class CustomUrlStreamHandlerFactory implements URLStreamHandlerFactory {
 
-    public final static String URL_PROTOCOL_RTMP = "rtmp";
-    public final static String URL_PROTOCOL_RTMPT = "rtmpt";
+    public final static String URL_PROTOCOL_RTMP   = "rtmp";
+    public final static String URL_PROTOCOL_RTMPT  = "rtmpt";
 
-    public final static String URL_PROTOCOL_RTMPS = "rtmps";
-    public final static String URL_PROTOCOL_RTMPE = "rtmpe";
+    public final static String URL_PROTOCOL_RTMPS  = "rtmps";
+    public final static String URL_PROTOCOL_RTMPE  = "rtmpe";
     public final static String URL_PROTOCOL_RTMPTE = "rtmpte";
 
-    public final static String URL_PROTOCOL_RTMFP = "rtmfp";
+    public final static String URL_PROTOCOL_RTMFP  = "rtmfp";
 
     public URLStreamHandler createURLStreamHandler(String protocol) {
         System.out.println("RtmpUrlStreamHandlerFactory # protocol: " + protocol);
-
-        if (URL_PROTOCOL_RTMP.equalsIgnoreCase(protocol)) { return new RtmpUrlStreamHandler(); }
+        if (protocol != null && protocol.startsWith("rtmp")) return new RtmpUrlStreamHandler();
         return null;
     }
 }
