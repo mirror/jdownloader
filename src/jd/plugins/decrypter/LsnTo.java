73028502ecd62abac3134fd4d2857988edee35c4
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-06-04 08:22:00 +0000

    -Fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14400 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/LsnTo.java
+++ src/jd/plugins/decrypter/LsnTo.java
@@ -34,46 +34,51 @@ import jd.utils.locale.JDL;
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "lesen.to" }, urls = { "http://(www\\.)?lesen\\.to/protection/folder_\\d+\\.html" }, flags = { 0 })
 public class LsnTo extends PluginForDecrypt {
 
-    public LsnTo(PluginWrapper wrapper) {
+    private static final String RECAPTCHA = "(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)";
+
+    public LsnTo(final PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final String RECAPTCHA = "(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)";
-
-    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString();
+    @Override
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, final ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
         br.getPage(parameter);
         boolean failed = true;
-        for (int i = 0; i <= 5; i++) {
-            PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-            jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
-            rc.parse();
-            rc.load();
-            File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-            String c = getCaptchaCode(cf, param);
-            rc.setCode(c);
-            if (br.containsHTML(RECAPTCHA)) {
-                br.getPage(parameter);
-                continue;
+        if (br.containsHTML(RECAPTCHA)) {
+            for (int i = 0; i <= 5; i++) {
+                final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+                final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+                rc.parse();
+                rc.load();
+                final File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+                final String c = getCaptchaCode(cf, param);
+                rc.setCode(c);
+                if (br.containsHTML(RECAPTCHA)) {
+                    br.getPage(parameter);
+                    continue;
+                }
+                failed = false;
+                break;
             }
-            failed = false;
-            break;
+            if (failed) { throw new DecrypterException(DecrypterException.CAPTCHA); }
         }
-        if (failed) throw new DecrypterException(DecrypterException.CAPTCHA);
-        if (br.containsHTML("Anfrage abgefangen")) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
+        if (br.containsHTML("Anfrage abgefangen")) { throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore.")); }
         String[] links = br.getRegex("class=\"container\"><a href=\"(http://.*?)\"").getColumn(0);
-        if (links == null || links.length == 0) links = br.getRegex("\"(http://(www\\.)?lesen\\.to/protection/.*?)\"").getColumn(0);
-        if (links == null || links.length == 0) return null;
+        if (links == null || links.length == 0) {
+            links = br.getRegex("\"(http://(www\\.)?lesen\\.to/protection/.*?)\"").getColumn(0);
+        }
+        if (links == null || links.length == 0) { return null; }
         progress.setRange(links.length);
-        for (String singleLink : links) {
+        for (final String singleLink : links) {
             br.getPage(singleLink);
-            String finallink = br.getRedirectLocation();
-            if (finallink == null) return null;
+            final String finallink = br.getRedirectLocation();
+            if (finallink == null) { return null; }
             decryptedLinks.add(createDownloadlink(finallink));
             progress.increase(1);
         }
-
+        if (decryptedLinks == null || decryptedLinks.size() == 0) { return null; }
         return decryptedLinks;
     }
 
