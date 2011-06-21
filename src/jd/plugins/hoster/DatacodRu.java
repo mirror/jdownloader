6e2a39d06fec43c7f4c05f34a71e19db663a9855
nvv13 <nvv13@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-06-21 09:28:37 +0000

    add new plugin for host .Data.cod.Ru and Datagrad.RU
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14485 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/DatacodRu.java
@@ -0,0 +1,89 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+import org.appwork.utils.formatter.SizeFormatter;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "data.cod.ru" }, urls = { "http://[a-zA-Z.]{0,}data.cod.ru/[0-9]{1,}" }, flags = { 0 })
+public class DatacodRu extends PluginForHost {
+
+    public DatacodRu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public String getAGBLink() {
+        return "http://data.cod.ru/terms/";
+    }
+
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
+        this.setBrowserExclusive();
+        br.setCustomCharset("UTF-8");
+        br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; chrome://global/locale/intl.properties; rv:1.8.1.12) Gecko/2008102920  Firefox/3.0.0 YB/4.2.0");
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("<title>404</title>") || br.containsHTML("Файл не найден") || br.containsHTML("Закончился срок хранения файла\\.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("Доступ на сервер разрешён только для пользователей Авангарда")) throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE);
+        String name = br.getRegex(Pattern.compile("файл: <b title=\"(.*?)\">")).getMatch(0);
+        if (name == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String fileSize = br.getRegex(Pattern.compile("размер: <b>(.*?)</b></li>")).getMatch(0);
+        if (fileSize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        // 15.58 МБ
+        fileSize = fileSize.replaceAll("Г", "G");
+        fileSize = fileSize.replaceAll("М", "M");
+        fileSize = fileSize.replaceAll("к", "k");
+        fileSize = fileSize.replaceAll("К", "k");
+        fileSize = fileSize.replaceAll("Б", "");
+        fileSize = fileSize + "b";
+        downloadLink.setName(name.trim());
+        downloadLink.setDownloadSize(SizeFormatter.getSize(fileSize));
+        return AvailableStatus.TRUE;
+    }
+
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        String dlLink = br.getRegex(Pattern.compile("<a href=\"(http://files.*?)\" class=\"button\">Скачать файл</a>")).getMatch(0);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dlLink, true, 1);
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
---
+++ src/jd/plugins/hoster/DatagradRU.java
@@ -0,0 +1,77 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2008  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.URLConnectionAdapter;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "datagrad.ru" }, urls = { "http://[a-zA-Z-0-9.]{0,}datagrad.ru/download/[0-9]{1,}" }, flags = { 0 })
+public class DatagradRU extends PluginForHost {
+
+    public DatagradRU(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public String getAGBLink() {
+        return "http://www.datagrad.ru/auth/hosting.php";
+    }
+
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        URLConnectionAdapter con = br.openGetConnection(downloadLink.getDownloadURL());
+        if (con.isContentDisposition()) {
+            downloadLink.setFinalFileName(Plugin.getFileNameFromHeader(con));
+            downloadLink.setDownloadSize(con.getLongContentLength());
+            con.disconnect();
+            return AvailableStatus.TRUE;
+        } else {
+            return AvailableStatus.FALSE;
+        }
+    }
+
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        // requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, downloadLink.getDownloadURL(), true, 1);
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
