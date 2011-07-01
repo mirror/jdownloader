74dae7368bee3a7fdcbc81d5f81c83ea762a97b7
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-07-01 19:21:36 +0000

    -Fixed a decrypter
    Guys i'll soon be back, local internet connection is not yet fixed, online through O2 internet stick via shared internet in our house :D
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14520 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/PcsaGgleCom.java
+++ src/jd/plugins/decrypter/PcsaGgleCom.java
@@ -30,7 +30,7 @@ import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 import jd.utils.locale.JDL;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "picasaweb.google.com" }, urls = { "http://[\\w\\.]*?picasaweb\\.google\\.com/.*?/.*?(\\?feat=(featured#[0-9]+|featured#)|#[0-9]+|#)" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "picasaweb.google.com" }, urls = { "http(s)?://(www\\.)?picasaweb\\.google\\.com/.*?/.*?(\\?feat=(featured#[0-9]+|featured#)|#[0-9]+|#|\\?authkey=[A-Za-z0-9\\-]+)" }, flags = { 0 })
 public class PcsaGgleCom extends PluginForDecrypt {
 
     public PcsaGgleCom(PluginWrapper wrapper) {
@@ -41,7 +41,7 @@ public class PcsaGgleCom extends PluginForDecrypt {
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString();
+        String parameter = param.toString().replace("https://", "http://");
         br.getPage(parameter);
         /* Error handling */
         if (br.containsHTML("(Hier gibt es nichts zu sehen|Entweder haben Sie keinen Zugriff auf diese Fotos oder es gibt unter dieser Adresse keine)")) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
@@ -55,13 +55,14 @@ public class PcsaGgleCom extends PluginForDecrypt {
         } else {
             String fpname = br.getRegex("title\" content=\"(.*?)\"").getMatch(0);
             if (fpname == null) {
-                fpname = br.getRegex("title:'(.*?)'").getMatch(0);
+                fpname = br.getRegex("title:\\'(.*?)\\'").getMatch(0);
                 if (fpname == null) {
-                    fpname = br.getRegex("album.title_wb = \"(.*?)\"").getMatch(0);
+                    fpname = br.getRegex("album\\.title_wb = \"(.*?)\"").getMatch(0);
                 }
             }
             // Make the regex to find alle the pic ids
             String galleryName = new Regex(parameter, "picasaweb\\.google\\.com/(.*?/.*?)(\\?feat|#)").getMatch(0);
+            if (galleryName == null) galleryName = new Regex(parameter, "picasaweb\\.google\\.com/.*?\\?authkey=(.+)").getMatch(0);
             if (galleryName == null) return null;
             String theFinalRegex = "(" + galleryName + "#[0-9]+)\"";
             // Use the regex
@@ -70,11 +71,15 @@ public class PcsaGgleCom extends PluginForDecrypt {
             progress.setRange(picLinks.length);
             int i = 1;
             for (String piclink : picLinks) {
-                br.getPage("http://picasaweb.google.com/" + piclink);
+                String pictureoverview = null;
+                if (parameter.contains("?authkey=")) {
+                    pictureoverview = "http://picasaweb.google.com/" + new Regex(parameter, "picasaweb\\.google\\.com/(.*?\\?authkey=)").getMatch(0) + piclink;
+                } else {
+                    pictureoverview = "http://picasaweb.google.com/" + piclink;
+                }
+                br.getPage(pictureoverview);
                 String picid = new Regex(piclink, "#(\\d+)").getMatch(0);
-                String directLinkRegex = "\"" + picid + "\",\"albumId\":\".*?\",\"access\":\"public\",.*?media\":\\{\"content\":\\[\\{\"url\":\"(http.*?)\"";
-                String finallink = br.getRegex(directLinkRegex).getMatch(0);
-                if (finallink == null) return null;
+                String finallink = br.getRegex("\":\"" + picid + "\",\"albumId\":\"(\\d+)\",\"access\":\"(private|public)\",\"width\":\"\\d+\",\"height\":\"\\d+\",\"size\":\"\\d+\",\"commentingEnabled\":\"(true|false)\",\"allowNameTags\":\"(false|true)\",\"media\":\\{\"content\":\\[\\{\"url\":\"(http://.*?)\"").getMatch(4);
                 DownloadLink dl = createDownloadlink("directhttp://" + finallink);
                 String ending = new Regex(finallink, "\\.com/.*?/.{1,}(\\..{3,4})").getMatch(0);
                 if (fpname != null && ending != null) {
