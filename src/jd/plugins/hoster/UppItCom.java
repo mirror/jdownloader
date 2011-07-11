e8a58078b3b1a0944dccfb3d8e2bcd3a9712dd9a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-07-11 15:18:40 +0000

    -Added a new decrypter closes #3554 -Added support for rnbload closes #3553 -Added support for uppit closes #3555 -Added support for wetransfer closes #3550
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14582 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/UppItCom.java
@@ -0,0 +1,123 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2010  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+import org.appwork.utils.formatter.SizeFormatter;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "uppit.com" }, urls = { "http://(www\\.)?uppit\\.com/([a-z0-9]+/.+|[A-Z0-9]{6})" }, flags = { 0 })
+public class UppItCom extends PluginForHost {
+
+    public UppItCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://uppit.com/tos.php";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML("(>Invalid download link|>File Not Found<|The file you were looking for could not be found,|<li>The file expired|<li>The file was deleted by its owner)")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        // Regexes for 3 kinds of links
+        Regex finfo = br.getRegex(">You have requested the file<br /><b>(.*?)</b> \\((.*?)\\)<br /><br");
+        String filename = br.getRegex("<font size=\"20\"><b>(.*?)</b></font>").getMatch(0);
+        if (filename == null) {
+            filename = br.getRegex("<title>UppIT \\- Free File Sharing \\-(.*?)</title>").getMatch(0);
+            if (filename == null) {
+                filename = finfo.getMatch(0);
+                if (filename == null) filename = br.getRegex(">File download:\\&nbsp;<strong>(.*?)</strong>").getMatch(0);
+            }
+        }
+        String filesize = br.getRegex("<h3>File Size:</h3></td><td align=\"right\"><h3>(.*?)</h3>").getMatch(0);
+        if (filesize == null) {
+            filesize = finfo.getMatch(1);
+            if (filesize == null) filesize = br.getRegex("Size:\\&nbsp;\\&nbsp;<strong>(.*?)</strong><br").getMatch(0);
+        }
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(filename.trim());
+        link.setDownloadSize(SizeFormatter.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        br.setFollowRedirects(false);
+        if (downloadLink.getDownloadURL().matches("uppit\\.com/[a-z0-9]{2,}/.+")) {
+            Form dlform = br.getFormbyProperty("name", "F1");
+            if (dlform == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            br.submitForm(dlform);
+        }
+        String dllink = br.getRegex("<span style=\"background:#f9f9f9;border:1px dotted #bbb;padding:7px;\">[\t\n\r ]+<a href=\"(http://.*?)\"").getMatch(0);
+        if (dllink == null) {
+            dllink = br.getRegex("<meta HTTP-EQUIV=\"REFRESH\" content=\"\\d+; url=(http://.*?)\">").getMatch(0);
+            if (dllink == null) {
+                dllink = br.getRegex("(http://server\\d+\\.uppit\\.com/files/\\d+/[a-z0-9]+/.*?)\"").getMatch(0);
+                if (dllink == null) {
+                    dllink = br.getRegex("getElementById\\(\"dl\"\\)\\.innerHTML = \\'<a href=\\\\'(http://.*?)\\\\'\"").getMatch(0);
+                    if (dllink == null) {
+                        dllink = br.getRegex("\\'(http://uppit\\.com/d2/[A-Z0-9]{6}/[a-z0-9]+/.*?)\\\\'").getMatch(0);
+                        if (dllink == null) {
+                            dllink = br.getRegex("var downloadlink = unescape\\(\\'(http://.*?)\\'\\)").getMatch(0);
+                            if (dllink == null) {
+                                dllink = br.getRegex("\\'(http://wickers\\.uppit\\.com/save/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+/.*?)\\'").getMatch(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        // Chunkload is working for some links but not for all!
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 1);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
---
+++ src/jd/plugins/hoster/WeTransferCom.java
@@ -0,0 +1,105 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2010  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "wetransfer.com" }, urls = { "http(s)?://(www\\.)?(wtrns\\.fr/[A-Za-z0-9\\-]+|wetransfer\\.com/dl/[A-Za-z0-9]+/[a-z0-9]+)" }, flags = { 0 })
+public class WeTransferCom extends PluginForHost {
+
+    public WeTransferCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        // No TOS/TOSlink found
+        return "https://wetransfer.com/";
+    }
+
+    private String              HASH             = null;
+    private String              CODE             = null;
+    private static final String POSTDOWNLOADLINK = "https://krusty.wetransfer.com/fsdl.php";
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(false);
+        br.getPage(link.getDownloadURL());
+        String nextLink = br.getRedirectLocation();
+        if (nextLink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        HASH = new Regex(nextLink, "hash=([a-z0-9]+)").getMatch(0);
+        CODE = new Regex(nextLink, "code=([A-Za-z0-9]+)").getMatch(0);
+        if (HASH == null || CODE == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openPostConnection(POSTDOWNLOADLINK, "hash=" + HASH + "&flash=WIN%2010%2C3%2C181%2C26&profile=1&corporate=null&code=" + CODE);
+            if (!con.getContentType().contains("html")) {
+                link.setDownloadSize(con.getLongContentLength());
+                link.setFinalFileName(getFileNameFromHeader(con));
+            } else {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        // More chunks are possible for some links but not for all
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, POSTDOWNLOADLINK, "hash=" + HASH + "&flash=WIN%2010%2C3%2C181%2C26&profile=1&corporate=null&code=" + CODE, true, -2);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
