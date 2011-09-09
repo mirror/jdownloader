539dec4788ff93a03950a0f19ba51ab16d15acc7
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-09-09 16:05:22 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14913 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/neembuu/NeembuuConfig.java
+++ src/org/jdownloader/extensions/neembuu/NeembuuConfig.java
@@ -2,6 +2,21 @@ package org.jdownloader.extensions.neembuu;
 
 import jd.plugins.ExtensionConfigInterface;
 
-public interface NeembuuConfig extends ExtensionConfigInterface {
+import org.appwork.storage.config.annotations.DefaultIntValue;
+import org.appwork.storage.config.annotations.Description;
+import org.appwork.storage.config.annotations.PlainStorage;
+import org.jdownloader.settings.annotations.AboutConfig;
 
+@PlainStorage
+public interface NeembuuConfig extends ExtensionConfigInterface {
+    // adds the enbtry to jd advanced config table
+
+    @AboutConfig
+    // default value
+    @DefaultIntValue(4)
+    // will be shown in the advanced config
+    @Description("This is a bla value")
+    int getBlaNum();
+
+    void setBlaNum(int num);
 }
