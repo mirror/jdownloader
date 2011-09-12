108dca6c0a5baa576a0083736813fa225cabc0e5
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-09-12 16:46:44 +0000

    -Fixed easybytez premium fixes #3739 -Set correct premium limits for enterupload
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14929 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/EnteruploadCom.java
+++ src/jd/plugins/hoster/EnteruploadCom.java
@@ -30,11 +30,11 @@ import jd.parser.html.HTMLParser;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
-import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.utils.JDUtilities;
 
 import org.appwork.utils.formatter.SizeFormatter;
@@ -362,13 +362,8 @@ public class EnteruploadCom extends PluginForHost {
                 logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
-            // logger.info("Final downloadlink = " + dllink +
-            // " starting the download...");
-            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, 1);// chunk
-            // limit
-            // before
-            // was
-            // -10
+            // Max 10 chunks per download
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, -10);
             if (passCode != null) {
                 link.setProperty("pass", passCode);
             }
@@ -387,7 +382,7 @@ public class EnteruploadCom extends PluginForHost {
 
     @Override
     public int getMaxSimultanPremiumDownloadNum() {
-        return 1;
+        return -1;
     }
 
     @Override
