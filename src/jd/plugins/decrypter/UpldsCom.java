f38020b7a0a61cb65e3ee9c8d28d94079d7472bd
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-09-24 13:12:13 +0000

    added: support for URL shortening service
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@14993 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/UpldsCom.java
@@ -0,0 +1,50 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+import jd.utils.locale.JDL;
+
+@DecrypterPlugin(revision = "$Revision: 7387 $", interfaceVersion = 2, names = { "uplds.com" }, urls = { "http://[\\w\\.]*?uplds\\.com/\\d+" }, flags = { 0 })
+public class UpldsCom extends PluginForDecrypt {
+
+    public UpldsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.getPage(parameter);
+        if (br.containsHTML("Link Not Found")) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or page no longer exists."));
+        String[] links = br.getRegex("form action=\"(.*?)\" method=\"post\"").getColumn(0);
+        if ((links == null || links.length == 0)) return null;
+        if (links != null && links.length != 0) {
+            for (String dl : links)
+                decryptedLinks.add(createDownloadlink(dl));
+        }
+        return decryptedLinks;
+    }
+}
