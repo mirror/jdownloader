1368a4b06a602b64555e2036c4d15a4d213f6f2b
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-10-04 10:44:01 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15037 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/controlling/filter/NoDownloadLinkException.java
+++ src/org/jdownloader/controlling/filter/NoDownloadLinkException.java
@@ -12,16 +12,6 @@ public class NoDownloadLinkException extends Exception {
     /**
      * @param message
      * @param cause
-     * @param enableSuppression
-     * @param writableStackTrace
-     */
-    public NoDownloadLinkException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
-    /**
-     * @param message
-     * @param cause
      */
     public NoDownloadLinkException(String message, Throwable cause) {
         super(message, cause);
