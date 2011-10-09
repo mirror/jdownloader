38b6e20347d3371a9ab71af22d980990d553566f
phg <phg@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-10-09 01:38:34 +0000

    Some links are case sensitive, remove the toLowerCase() function on URL link
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15077 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/SrBoxCom.java
+++ src/jd/plugins/decrypter/SrBoxCom.java
@@ -76,7 +76,7 @@ public class SrBoxCom extends PluginForDecrypt {
         // We look if the link in the array is supported by a hoster plug-in
         int iLink = 0;
         for (int iIndex = 0; iIndex < TabTemp.length; iIndex++) {
-            strLink = TabTemp[iIndex].toLowerCase();
+            strLink = TabTemp[iIndex];
             for (HostPluginWrapper wrapper : AllHosts) {
 
                 if (wrapper.canHandle(strLink)) {
@@ -90,7 +90,7 @@ public class SrBoxCom extends PluginForDecrypt {
         // We look if the link in the array is supported by a decrypter plug-in
         ArrayList<DecryptPluginWrapper> AllDecrypter = new ArrayList<DecryptPluginWrapper>(DecryptPluginWrapper.getDecryptWrapper());
         for (int iIndex = 0; iIndex < TabTemp.length; iIndex++) {
-            strLink = TabTemp[iIndex].toLowerCase();
+            strLink = TabTemp[iIndex];
             for (DecryptPluginWrapper wrapper : AllDecrypter) {
                 if (wrapper.compareTo(this.getWrapper()) != 0) {
                     if (wrapper.canHandle(strLink)) {
