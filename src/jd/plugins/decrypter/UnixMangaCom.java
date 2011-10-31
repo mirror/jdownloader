4c5a44a8384f9b6b4c6f6fbbb3e23ef0222f3641
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-10-31 09:04:56 +0000

    UnixMangaCom: reviewed, refs #3274
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15193 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/UnixMangaCom.java
+++ src/jd/plugins/decrypter/UnixMangaCom.java
@@ -13,64 +13,53 @@ import jd.plugins.PluginForDecrypt;
 @DecrypterPlugin(revision = "$Revision: 14904 $", interfaceVersion = 2, names = { "unixmanga.com" }, urls = { "http://[\\w\\.]*?unixmanga\\.com/onlinereading/.*?/.*?(c|ch)\\d+.*" }, flags = { 0 })
 public class UnixMangaCom extends PluginForDecrypt {
 
-	public UnixMangaCom(PluginWrapper wrapper) {
-		super(wrapper);
-	}
+    public UnixMangaCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
 
-	@Override
-	public ArrayList<DownloadLink> decryptIt(CryptedLink parameter,
-			ProgressController progress) throws Exception {
-		br.setFollowRedirects(true);
-		String url = parameter.toString();
-		br.getPage(url);
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        br.setFollowRedirects(true);
+        String url = parameter.toString();
+        br.getPage(url);
 
-		// We get the title
-		String title = br.getRegex(".*?/(.+?)/.*?").getMatch(0);
-		if (title == null) {
-			return null;
-		}
+        // We get the title
+        String title = br.getRegex(".*?/(.+?)/.*?").getMatch(0);
+        if (title == null) { return null; }
 
-		// We get the number of pages in the chapter
-		String[][] hrefArray = br
-				.getRegex(
-						"<A[^>]*?\"td2\"[^>]*?HREF[^>]*?\"([^>]*?)\">[^>]*?\\d+[^>]*?\\.(png|jpg|jpeg|gif)[^>]*?</A")
-				.getMatches();
-		int numberOfPages = hrefArray.length;
-		String format = "%02d";
-		if (numberOfPages > 0) {
-			format = String.format("%%0%dd",
-					(int) Math.log10(numberOfPages) + 1);
-		}
+        // We get the number of pages in the chapter
+        String[][] hrefArray = br.getRegex("<A[^>]*?\"td2\"[^>]*?HREF[^>]*?\"([^>]*?)\">[^>]*?\\d+[^>]*?\\.(png|jpg|jpeg|gif)[^>]*?</A").getMatches();
+        int numberOfPages = hrefArray.length;
+        String format = "%02d";
+        if (numberOfPages > 0) {
+            format = String.format("%%0%dd", (int) Math.log10(numberOfPages) + 1);
+        }
 
-		progress.setRange(numberOfPages);
+        progress.setRange(numberOfPages);
 
-		ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-		// We load each page and retrieve the URL of the picture
-		FilePackage fp = FilePackage.getInstance();
-		fp.setName(title);
-		for (int i = 0; i < numberOfPages; i++) {
-			br.getPage(hrefArray[i][0]);
-			System.out.println(br.getURL());
-			String[][] unformattedSource = br.getRegex(
-					"<IMG.*?SRC.*?\"(.*?)\\.(png|jpg|jpeg|gif)\"").getMatches();
-			String source = unformattedSource[0][0];
-			String extension = unformattedSource[0][1];
-			DownloadLink link = createDownloadlink("directhttp://" + source
-					+ "." + extension);
-			String pageNumber = String.format(format, (i + 1));
-			link.setFinalFileName(title + "–page_" + pageNumber + "."
-					+ extension);
-			fp.add(link);
-			try {
-				distribute(link);
-			} catch (final Throwable e) {
-				/* does not exist in 09581 */
-			}
-			decryptedLinks.add(link);
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        // We load each page and retrieve the URL of the picture
+        FilePackage fp = FilePackage.getInstance();
+        fp.setName(title);
+        for (int i = 0; i < numberOfPages; i++) {
+            br.getPage(hrefArray[i][0]);
+            String[][] unformattedSource = br.getRegex("<IMG.*?SRC.*?\"(.*?)\\.(png|jpg|jpeg|gif)\"").getMatches();
+            String source = unformattedSource[0][0];
+            String extension = unformattedSource[0][1];
+            DownloadLink link = createDownloadlink("directhttp://" + source + "." + extension);
+            String pageNumber = String.format(format, (i + 1));
+            link.setFinalFileName(title + "–page_" + pageNumber + "." + extension);
+            fp.add(link);
+            try {
+                distribute(link);
+            } catch (final Throwable e) {
+                /* does not exist in 09581 */
+            }
+            decryptedLinks.add(link);
 
-			progress.increase(1);
-		}
+            progress.increase(1);
+        }
 
-		return decryptedLinks;
-	}
+        return decryptedLinks;
+    }
 }
