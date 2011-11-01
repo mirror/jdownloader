306fed7f281b2f46c49852d075448122988bb69c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-11-01 10:57:10 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15200 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/ZeroAPremium.java
@@ -0,0 +1,42 @@
+package jd.plugins.hoster;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision: 15005 $", interfaceVersion = 2, names = { "0premium" }, urls = { "NOREALREGEXJUSTDUMMY" }, flags = { 2 })
+public class ZeroAPremium extends PluginForHost {
+
+    public ZeroAPremium(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium("http://0premium.com");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return null;
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        return AvailableStatus.FALSE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        throw new PluginException(LinkStatus.ERROR_FATAL, "DUMMY PLUGIN");
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
