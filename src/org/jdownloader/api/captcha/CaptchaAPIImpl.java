959325a0b570be41759cebd544a225e4f05201ec
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-11-04 12:00:37 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15236 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/captcha/CaptchaAPIImpl.java
+++ src/org/jdownloader/api/captcha/CaptchaAPIImpl.java
@@ -53,7 +53,7 @@ public class CaptchaAPIImpl implements CaptchaAPI, CaptchaEventListener {
             if (returnAsDataURL) {
                 OutputStream out = RemoteAPI.getOutputStream(response, request, RemoteAPI.gzip(request), true);
                 String mime = FileResponse.getMimeType(captcha.getFile().getName());
-                String header = "{\r\n\"data:\" : \"" + mime + ";base64,";
+                String header = "{\r\n\"data\":\"" + mime + ";base64,";
                 out.write(header.getBytes("UTF-8"));
                 Base64OutputStream b64os = new Base64OutputStream(out);
                 FileInputStream fis = null;
