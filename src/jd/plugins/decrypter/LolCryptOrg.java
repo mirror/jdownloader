c6da6d42e394c9787b2fda7fba9ba8e1da159231
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2011-12-23 21:12:51 +0000

    -psp and me added support for lolcrypt.org closes #4000
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15545 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/LolCryptOrg.java
@@ -0,0 +1,77 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "lolcrypt.org" }, urls = { "http://(www\\.)?lolcrypt\\.org/folder\\?fid=[a-z0-9]+" }, flags = { 0 })
+public class LolCryptOrg extends PluginForDecrypt {
+
+    public LolCryptOrg(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /** Only works for megaupload links at the moment */
+    @Override
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, final ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.getPage(parameter);
+        final String[] links = br.getRegex("(/decrypt\\?fid=[a-z0-9]+\\&lid=\\d+\\&key=[a-z0-9]+)\\'").getColumn(0);
+        if (links == null || links.length == 0) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
+        }
+        for (final String singleLink : links) {
+            br.getPage("http://lolcrypt.org" + singleLink);
+            /**
+             * Das ist eine Falle, kann man den originallink überhaupt bekommen?
+             */
+            String finallink = br.getRegex("href=\"javascript:clipboardcopy\\(\\'(http://(www\\.)?megaupload\\.com/\\?d=[A-Z0-9]+)\\'\\)").getMatch(0);
+            final Boolean mu = finallink != null ? true : false;
+            /**
+             * Direktlink kann man holen, ist allerdings erst nach der Wartezeit
+             * gültig
+             */
+            finallink = br.getRegex("id=\"dlbuttondisabled\"></a> [\t\n\r ]+<a href=\"(http://.*?)\"").getMatch(0);
+            if (finallink == null) {
+                logger.warning("Decrypter broken for link: " + parameter);
+                return null;
+            }
+
+            if (mu) {
+                br.openGetConnection(finallink);
+                finallink = br.getRedirectLocation();
+                try {
+                    finallink = finallink == null ? "http://www.megaupload.com/?d=" + br.getRequest().getHttpConnection().getHeaderField("etag").replace("\"", "") : finallink;
+                } catch (final Throwable e) {
+                    continue;
+                }
+            }
+            decryptedLinks.add(createDownloadlink(finallink));
+        }
+        return decryptedLinks;
+    }
+
+}
