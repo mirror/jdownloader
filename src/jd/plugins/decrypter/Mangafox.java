326ef4e597d1ced543cd858dd7e1a9f8fe888171
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-01-17 10:50:41 +0000

    Fixed: Mangafox - listening regex
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15678 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/Mangafox.java
+++ src/jd/plugins/decrypter/Mangafox.java
@@ -1,3 +1,19 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2011  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
@@ -10,7 +26,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangafox.com" }, urls = { "http://[\\w\\.]*?mangafox\\.com/manga/.*?/(v\\d+/c\\d+|c\\d+)" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangafox.com" }, urls = { "http://[\\w\\.]*?mangafox\\.com/manga/.*?/(v\\d+/c[\\d\\.]+|c[\\d\\.]+)" }, flags = { 0 })
 public class Mangafox extends PluginForDecrypt {
 
     public Mangafox(PluginWrapper wrapper) {
