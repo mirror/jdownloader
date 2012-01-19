409f3b3bb3cf480d212eccf0fbebc97d103ae2b3
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-01-19 12:00:42 +0000

    Fixed: UnixManga - Changed how we source page count + images. - closes #4154
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15693 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/UnixMangaCom.java
+++ src/jd/plugins/decrypter/UnixMangaCom.java
@@ -1,9 +1,26 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2011  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
@@ -17,6 +34,12 @@ public class UnixMangaCom extends PluginForDecrypt {
         super(wrapper);
     }
 
+    /*
+     * DEV NOTES: The index page seems only only list the first 30 pages/images
+     * links, so we need to jump to the first image and catch the last page
+     * within reader navigation panel. -raztoki
+     */
+
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
         br.setFollowRedirects(true);
@@ -25,41 +48,70 @@ public class UnixMangaCom extends PluginForDecrypt {
 
         // We get the title
         String title = br.getRegex(".*?/(.+?)/.*?").getMatch(0);
-        if (title == null) { return null; }
-
-        // We get the number of pages in the chapter
-        String[][] hrefArray = br.getRegex("<A[^>]*?\"td2\"[^>]*?HREF[^>]*?\"([^>]*?)\">[^>]*?\\d+[^>]*?\\.(png|jpg|jpeg|gif)[^>]*?</A").getMatches();
-        int numberOfPages = hrefArray.length;
-        String format = "%02d";
-        if (numberOfPages > 0) {
-            format = String.format("%%0%dd", (int) Math.log10(numberOfPages) + 1);
+        if (title == null) {
+            logger.warning("UnixManga :: Title not found! : " + parameter);
+            return null;
         }
+        // grab first page within the viewer
+        br.getPage(br.getRegex("(?i)<A class=\"td2\" rel=\"nofollow\" HREF=\"(http[^>]+)\"").getMatch(0));
+        // set sub + domain used, used later in page views
+        String Url = new Regex(br.getURL(), "(?i)(https?://[^/]+)").getMatch(0);
+        // grab the total pages within viewer
+        String TotalPages = br.getRegex("(?i)<a href=\"\\?image=[^>]+\\.html\">(\\d+)</a>[\r\n]{1}</center></div>").getMatch(0);
+        if (TotalPages == null) {
+            logger.warning("UnixManga :: Intial page not found! : " + parameter);
+            return null;
+        } else {
 
-        progress.setRange(numberOfPages);
-
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        // We load each page and retrieve the URL of the picture
-        FilePackage fp = FilePackage.getInstance();
-        fp.setName(title);
-        for (int i = 0; i < numberOfPages; i++) {
-            br.getPage(hrefArray[i][0]);
-            String[][] unformattedSource = br.getRegex("<IMG.*?SRC.*?\"(.*?)\\.(png|jpg|jpeg|gif)\"").getMatches();
-            String source = unformattedSource[0][0];
-            String extension = unformattedSource[0][1];
-            DownloadLink link = createDownloadlink("directhttp://" + source + "." + extension);
-            String pageNumber = String.format(format, (i + 1));
-            link.setFinalFileName(title + "–page_" + pageNumber + "." + extension);
-            fp.add(link);
-            try {
-                distribute(link);
-            } catch (final Throwable e) {
-                /* does not exist in 09581 */
+            int numberOfPages = Integer.parseInt(TotalPages);
+            String format = "%02d";
+            if (numberOfPages > 0) {
+                format = String.format("%%0%dd", (int) Math.log10(numberOfPages) + 1);
             }
-            decryptedLinks.add(link);
 
-            progress.increase(1);
+            progress.setRange(numberOfPages);
+
+            ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+            // We load each page and retrieve the URL of the picture
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(title);
+            for (int i = 0; i < numberOfPages; i++) {
+                String pageNumber = String.format(format, (i + 1));
+                // grab the image source
+                String Img = br.getRegex("(?i)<IMG ALT=\" \" STYLE=\"border: solid 1px #\\d+\" SRC=\"(http[^>]+)\" >").getMatch(0);
+                if (Img == null) {
+                    logger.warning("UnixManga :: No images found for page : " + pageNumber + " : " + parameter);
+                    logger.warning("UnixManga :: Continuing...");
+                }
+                String extension = Img.substring(Img.lastIndexOf("."));
+                DownloadLink link = createDownloadlink("directhttp://" + Img);
+                link.setFinalFileName(title + "–page_" + pageNumber + extension);
+                fp.add(link);
+                try {
+                    distribute(link);
+                } catch (final Throwable e) {
+                    /* does not exist in 09581 */
+                }
+                decryptedLinks.add(link);
+                progress.increase(1);
+                // We look for next page and load it ready for the 'for' loop.
+                String NextPage = br.getRegex("(?i)<a class=\"ne\" href =\"(http[^>]+)\">").getMatch(0);
+                if (NextPage != null) {
+                    br.getPage(NextPage);
+                    continue;
+                }
+                if (NextPage == null) {
+                    NextPage = br.getRegex("(?i)<a class=\"ne\" href =\"(\\?image=[^>]+)\">").getMatch(0);
+                }
+                if (NextPage != null) {
+                    br.getPage(Url + "/onlinereading/" + NextPage);
+                    continue;
+                } else if ((NextPage == null) && (!pageNumber.equals(TotalPages))) {
+                    logger.warning("UnixManga :: Plugin broken : " + parameter);
+                    return null;
+                }
+            }
+            return decryptedLinks;
         }
-
-        return decryptedLinks;
     }
 }
