261907256b400a0c96959f92d693d9a58dd857a3
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-01-25 17:57:25 +0000

    -hitfile.net: closes #4187
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15730 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/HitFileNet.java
+++ src/jd/plugins/hoster/HitFileNet.java
@@ -1,5 +1,5 @@
 //jDownloader - Downloadmanager
-//Copyright (C) 2009  JD-Team support@jdownloader.org
+//Copyright (C) 2012  JD-Team support@jdownloader.org
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -19,6 +19,9 @@ package jd.plugins.hoster;
 import java.io.File;
 import java.io.IOException;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 import jd.PluginWrapper;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -31,6 +34,8 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.plugins.decrypter.LnkCrptWs;
+import jd.utils.JDHexUtils;
 import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
 
@@ -49,26 +54,26 @@ public class HitFileNet extends PluginForHost {
     private static final String WAITTIMEREGEX2 = "id=\\'timeout\\'>(\\d+)</span>";
 
     public static final Object  LOCK           = new Object();
-    private static final String MAINPAGE       = "http://hitfile.net/";
+    private static final String MAINPAGE       = "http://hitfile.net";
     private static final String ENPAGE         = "http://hitfile.net/lang/en";
 
-    public HitFileNet(PluginWrapper wrapper) {
+    public HitFileNet(final PluginWrapper wrapper) {
         super(wrapper);
         this.enablePremium("http://hitfile.net/premium/emoney/5");
     }
 
     @Override
-    public AccountInfo fetchAccountInfo(Account account) throws Exception {
-        AccountInfo ai = new AccountInfo();
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        final AccountInfo ai = new AccountInfo();
         try {
             login(account);
-        } catch (PluginException e) {
+        } catch (final PluginException e) {
             account.setValid(false);
             return ai;
         }
         account.setValid(true);
         ai.setUnlimitedTraffic();
-        String expire = br.getRegex("Account: <b>premium</b> \\(<a href=\\'/premium\\'>(.*?)</a>\\)").getMatch(0);
+        final String expire = br.getRegex("Account: <b>premium</b> \\(<a href=\\'/premium\\'>(.*?)</a>\\)").getMatch(0);
         if (expire == null) {
             ai.setExpired(true);
             account.setValid(false);
@@ -96,59 +101,75 @@ public class HitFileNet extends PluginForHost {
     }
 
     @Override
-    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
         requestFileInformation(downloadLink);
         br.setFollowRedirects(false);
-        String fileID = new Regex(downloadLink.getDownloadURL(), "hitfile\\.net/(.+)").getMatch(0);
-        if (fileID == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        final String fileID = new Regex(downloadLink.getDownloadURL(), "hitfile\\.net/(.+)").getMatch(0);
+        if (fileID == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
         br.getPage("http://hitfile.net/download/free/" + fileID);
         if (br.getRedirectLocation() != null) {
-            if (br.getRedirectLocation().equals(downloadLink.getDownloadURL().replace("www.", ""))) throw new PluginException(LinkStatus.ERROR_FATAL, JDL.LF("plugins.hoster.hitfilenet.only4premium", "This file is only available for premium users!"));
+            if (br.getRedirectLocation().equals(downloadLink.getDownloadURL().replace("www.", ""))) { throw new PluginException(LinkStatus.ERROR_FATAL, JDL.LF("plugins.hoster.hitfilenet.only4premium", "This file is only available for premium users!")); }
             logger.warning("Unexpected redirect!");
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         String longWaittime = br.getRegex(WAITTIMEREGEX1).getMatch(0);
-        if (longWaittime == null) longWaittime = br.getRegex(WAITTIMEREGEX2).getMatch(0);
-        if (longWaittime != null) if (Integer.parseInt(longWaittime) > 60) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(longWaittime) * 1001l);
-        if (br.containsHTML("(have reached the limit|>From your IP range the limit of connections is reached<)")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED);
+        if (longWaittime == null) {
+            longWaittime = br.getRegex(WAITTIMEREGEX2).getMatch(0);
+        }
+        if (longWaittime != null) {
+            if (Integer.parseInt(longWaittime) > 60) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(longWaittime) * 1001l); }
+        }
+        if (br.containsHTML("(have reached the limit|>From your IP range the limit of connections is reached<)")) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED); }
         if (br.containsHTML(RECAPTCHATEXT)) {
-            PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-            jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+            final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+            final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
             rc.parse();
             rc.load();
-            File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-            String c = getCaptchaCode(cf, downloadLink);
+            final File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+            final String c = getCaptchaCode(cf, downloadLink);
             rc.setCode(c);
-            if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(CAPTCHATEXT)) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(CAPTCHATEXT)) { throw new PluginException(LinkStatus.ERROR_CAPTCHA); }
         } else {
-            if (!br.containsHTML(CAPTCHATEXT)) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            String captchaUrl = br.getRegex("<div><img alt=\"Captcha\" src=\"(http://hitfile\\.net/captcha/.*?)\"").getMatch(0);
-            Form captchaForm = br.getForm(2);
-            if (captchaForm == null || captchaUrl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            String code = getCaptchaCode(captchaUrl, downloadLink);
+            if (!br.containsHTML(CAPTCHATEXT)) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+            final String captchaUrl = br.getRegex("<div><img alt=\"Captcha\" src=\"(http://hitfile\\.net/captcha/.*?)\"").getMatch(0);
+            final Form captchaForm = br.getForm(2);
+            if (captchaForm == null || captchaUrl == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
+            final String code = getCaptchaCode(captchaUrl, downloadLink);
             captchaForm.put("captcha_response", code);
             br.submitForm(captchaForm);
-            if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(CAPTCHATEXT)) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(CAPTCHATEXT)) { throw new PluginException(LinkStatus.ERROR_CAPTCHA); }
         }
         int waittime = 60;
-        String regexedWaittime = br.getRegex(WAITTIMEREGEX1).getMatch(0);
-        if (regexedWaittime != null) waittime = Integer.parseInt(regexedWaittime);
-        if (waittime > 250) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "Limit reached or IP already loading", waittime * 1001l);
-        sleep(waittime * 1001l, downloadLink);
-        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        boolean waittimeFail = true;
-        for (int i = 0; i <= 4; i++) {
-            br.getPage("http://hitfile.net/download/getLinkAfterTimeout/" + fileID + "/0/");
-            String additionalWaittime = br.getRegex("Timeout\\.limit = (\\d+);").getMatch(0);
-            if (additionalWaittime != null) {
-                sleep(Integer.parseInt(additionalWaittime) * 1001l, downloadLink);
-            } else {
-                logger.info("No additional waittime found...");
-                waittimeFail = false;
-                break;
-            }
-            logger.info("Waittime-Try " + (i + 1) + ", waited additional " + additionalWaittime + "seconds");
+        final String regexedWaittime = br.getRegex(WAITTIMEREGEX1).getMatch(0);
+        if (regexedWaittime != null) {
+            waittime = Integer.parseInt(regexedWaittime);
         }
+        if (waittime > 250) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "Limit reached or IP already loading", waittime * 1001l); }
+        sleep(waittime * 1001l, downloadLink);
+
+        boolean waittimeFail = true;
+
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+        final String res = parseImageUrl(br.getRegex(LnkCrptWs.IMAGEREGEX(null)).getMatch(0));
+        if (res != null) {
+            String fReq = res;
+            if (!res.startsWith("http")) {
+                fReq = MAINPAGE + res;
+            }
+            for (int i = 0; i <= 4; i++) {
+                br.getPage(fReq);
+                final String additionalWaittime = br.getRegex(JDHexUtils.toString(LnkCrptWs.IMAGEREGEX("FE8CFBFAFA57CDE31BC2B798DF5141AB2DC171EC0852D89A1A135E3F116C83D05D8BF8328B5AE238254154B5EA27"))).getMatch(0);
+                if (additionalWaittime != null) {
+                    sleep(Integer.parseInt(additionalWaittime) * 1001l, downloadLink);
+                } else {
+                    logger.info("No additional waittime found...");
+                    waittimeFail = false;
+                    break;
+                }
+                logger.info("Waittime-Try " + (i + 1) + ", waited additional " + additionalWaittime + "seconds");
+            }
+        }
+
         if (waittimeFail) {
             logger.warning(br.toString());
             throw new PluginException(LinkStatus.ERROR_FATAL, "FATAL waittime error");
@@ -164,7 +185,9 @@ public class HitFileNet extends PluginForHost {
             logger.warning("dllink couldn't be found...");
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        dllink = "http://hitfile.net" + dllink;
+        if (!dllink.startsWith("http")) {
+            dllink = MAINPAGE + dllink;
+        }
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 1);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
@@ -174,12 +197,14 @@ public class HitFileNet extends PluginForHost {
     }
 
     @Override
-    public void handlePremium(DownloadLink link, Account account) throws Exception {
+    public void handlePremium(final DownloadLink link, final Account account) throws Exception {
         requestFileInformation(link);
         login(account);
         br.getPage(link.getDownloadURL());
         String dllink = br.getRegex("<h1><a href=\\'(http://.*?)\\'><b>").getMatch(0);
-        if (dllink == null) dllink = br.getRegex("\\'(http://hitfile\\.net//download/redirect/[a-z0-9]+/[A-Za-z0-9]+)\\'").getMatch(0);
+        if (dllink == null) {
+            dllink = br.getRegex("\\'(http://hitfile\\.net//download/redirect/[a-z0-9]+/[A-Za-z0-9]+)\\'").getMatch(0);
+        }
         if (dllink == null) {
             logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
@@ -211,34 +236,55 @@ public class HitFileNet extends PluginForHost {
         return true;
     }
 
-    private void login(Account account) throws Exception {
-        this.setBrowserExclusive();
+    private void login(final Account account) throws Exception {
+        setBrowserExclusive();
         br.setFollowRedirects(true);
         br.getPage(MAINPAGE);
         br.setCookie(MAINPAGE, "user_lang", "en");
         br.postPage("http://hitfile.net/user/login", "user%5Blogin%5D=" + Encoding.urlEncode(account.getUser()) + "&user%5Bpass%5D=" + Encoding.urlEncode(account.getPass()) + "&user%5Bmemory%5D=on&user%5Bsubmit%5D=");
-        if ((br.getCookie(MAINPAGE, "kohanasession") == null && br.getCookie(MAINPAGE, "sid") == null)) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-        if (!br.containsHTML("Account: <b>premium</b>")) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+        if (br.getCookie(MAINPAGE, "kohanasession") == null && br.getCookie(MAINPAGE, "sid") == null) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+        if (!br.containsHTML("Account: <b>premium</b>")) { throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE); }
+    }
+
+    private String parseImageUrl(final String fun) {
+        if (fun == null) { return null; }
+        final String[] next = new Regex(fun, JDHexUtils.toString(LnkCrptWs.IMAGEREGEX(Encoding.Base64Decode("Rjk4QUZFQTVGOTUwQzlFMjE4QzdCMjk1REE1NzQ2RkIyQUM2NzJFQzA5NUNEQjlEMUU0RTVDNkYxMTNFODI4NjVBRDhGODMzOEI1QUU2NjkyMTQwNTBFQUVGNzQxOTIyRTcyNDI5OTFDMUNDNjM2N0E4MjlENkIzNkI1RDMzNkE1RUZFQzk3ODU5NzlGODlFMjVGOTJGRUY1NTNCQzhCMzQyRkM4RjhFNkJBRTk4QTE5Q0RGMkI5NTI5NjU3ODRFQzQyMDNBNUI=")))).getRow(0);
+        if (next == null || next.length != 2) {
+            return new Regex(fun, JDHexUtils.toString(LnkCrptWs.IMAGEREGEX("F98AFEA5F950C9E218C7B295DA5746FB2AC672EC095CDB9D1E4E5C6F113E82865AD8F8338B5AE669214050EAEF741922E7242991C1CC6367A829D6B36B5D336A5EFEC9785979FFC520A92BEA553BC8E845A6"))).getMatch(0);
+        } else {
+            Object result = new Object();
+            final ScriptEngineManager manager = new ScriptEngineManager();
+            final ScriptEngine engine = manager.getEngineByName("javascript");
+            try {
+                engine.eval(fun);
+                result = ((Double) engine.eval(next[1])).longValue();
+            } catch (final Throwable e) {
+                return null;
+            }
+            return next[0] + result.toString();
+        }
     }
 
     // Also check TurboBitNet plugin if this one is broken
     @Override
-    public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException, InterruptedException {
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException, InterruptedException {
         synchronized (LOCK) {
-            if (this.isAborted(link)) return AvailableStatus.TRUE;
+            if (isAborted(link)) { return AvailableStatus.TRUE; }
             /* wait 3 seconds between filechecks, otherwise they'll block our IP */
             Thread.sleep(3000);
         }
-        this.setBrowserExclusive();
+        setBrowserExclusive();
         br.setFollowRedirects(true);
         br.getHeaders().put("Referer", link.getDownloadURL());
         br.getPage(ENPAGE);
-        if (!br.getURL().equals(link.getDownloadURL())) br.getPage(link.getDownloadURL());
-        if (br.containsHTML("(<h1>File was not found|It could possibly be deleted\\.)")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        Regex fileInfo = br.getRegex("class=\\'file-icon\\d+ [a-z0-9]+\\'></span><span>(.*?)</span>[\n\t\r ]+<span style=\"color: #626262; font-weight: bold; font-size: 14px;\">\\((.*?)\\)</span>");
-        String filename = fileInfo.getMatch(0);
-        String filesize = fileInfo.getMatch(1);
-        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (!br.getURL().equals(link.getDownloadURL())) {
+            br.getPage(link.getDownloadURL());
+        }
+        if (br.containsHTML("(<h1>File was not found|It could possibly be deleted\\.)")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        final Regex fileInfo = br.getRegex("class=\\'file-icon\\d+ [a-z0-9]+\\'></span><span>(.*?)</span>[\n\t\r ]+<span style=\"color: #626262; font-weight: bold; font-size: 14px;\">\\((.*?)\\)</span>");
+        final String filename = fileInfo.getMatch(0);
+        final String filesize = fileInfo.getMatch(1);
+        if (filename == null || filesize == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
         link.setName(filename.trim());
         link.setDownloadSize(SizeFormatter.getSize(filesize.replace(",", ".")));
         return AvailableStatus.TRUE;
@@ -249,7 +295,7 @@ public class HitFileNet extends PluginForHost {
     }
 
     @Override
-    public void resetDownloadlink(DownloadLink link) {
+    public void resetDownloadlink(final DownloadLink link) {
     }
 
 }
