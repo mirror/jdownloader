231c9e8e17be848973789d4d0d5f0d27a71fee42
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-01-26 20:28:35 +0000

    -Removed 2 dead plugins and one captcha method -Fixed slingfile
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@15736 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/SlingFileCom.java
+++ src/jd/plugins/hoster/SlingFileCom.java
@@ -53,20 +53,31 @@ public class SlingFileCom extends PluginForHost {
 
     public void handleFree(DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
-        String waittime = br.getRegex("\\)\\.innerHTML=\\'(\\d+)\\'").getMatch(0);
-        if (waittime == null) waittime = br.getRegex("id=\"dltimer\">(\\d+)</span><br>").getMatch(0);
         String waitthat = br.getRegex("Please wait for another (\\d+) minutes to download another file").getMatch(0);
         if (waitthat != null) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(waitthat) * 60 * 1001l);
+        // int wait = 30;
+        // String waittime =
+        // br.getRegex("\\)\\.innerHTML=\\'(\\d+)\\'").getMatch(0);
+        // if (waittime == null) waittime =
+        // br.getRegex("id=\"dltimer\">(\\d+)</span><br>").getMatch(0);
+        // if (waittime != null) wait = Integer.parseInt(waittime);
+        // sleep(wait * 1001l, downloadLink);
         if (br.containsHTML("Please wait until the download is complete")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "Too many simultan downloads", 10 * 60 * 1000l);
-        br.postPage(downloadLink.getDownloadURL(), "show_captcha=yes");
-        PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-        jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
-        rc.parse();
-        rc.load();
-        File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-        String c = getCaptchaCode(cf, downloadLink);
-        rc.setCode(c);
-        if (br.containsHTML("(api\\.recaptcha\\.net|g>Invalid captcha entered\\. Please try again\\.<)")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+        /**
+         * TODO: Not sure if this will always work, maybe its different if
+         * captcha appears after this step
+         */
+        br.postPage(downloadLink.getDownloadURL(), "download=yes");
+        if (br.containsHTML("(api\\.recaptcha\\.net|g>Invalid captcha entered\\. Please try again\\.<)")) {
+            PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+            jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+            rc.parse();
+            rc.load();
+            File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+            String c = getCaptchaCode(cf, downloadLink);
+            rc.setCode(c);
+            if (br.containsHTML("(api\\.recaptcha\\.net|g>Invalid captcha entered\\. Please try again\\.<)")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+        }
         String dllink = br.getRegex("(http://sf[0-9\\-].*?.slingfile\\.com/gdl/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+/.*?)('|\")").getMatch(0);
         if (dllink == null) dllink = br.getRegex("<td valign=\\'middle\\' align=\\'center\\' colspan=\\'2\\'>[\t\n\r ]+<a href=\"(http://.*?)\"").getMatch(0);
         if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
