f8a73b5f67656a0c798f1f93b2a6148282fab491
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-05-11 08:16:22 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@16584 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Main.java
+++ src/jd/Main.java
@@ -30,6 +30,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             ShutdownController.getInstance().addShutdownEvent(RunUpdaterOnEndAtLeastOnceDaily.getInstance());
+            System.out.println("StartTest");
             jd.Launcher.mainStart(args);
         } catch (Throwable e) {
             e.printStackTrace();
