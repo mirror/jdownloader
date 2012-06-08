610c51e66cb15bc9a48718b34431ddbdb3751c6c
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-06-08 08:45:50 +0000

    USe BalloonNotificationEnabled in Advanced Configs to disable the help dialogs
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@17230 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/helpdialogs/HelpDialog.java
+++ src/org/jdownloader/gui/helpdialogs/HelpDialog.java
@@ -5,6 +5,7 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 
+import org.appwork.storage.config.JsonConfig;
 import org.appwork.utils.BinaryLogic;
 import org.appwork.utils.logging.Log;
 import org.appwork.utils.os.CrossSystem;
@@ -15,6 +16,7 @@ import org.appwork.utils.swing.dialog.DialogClosedException;
 import org.appwork.utils.swing.dialog.OffScreenException;
 import org.appwork.utils.swing.dialog.SimpleTextBallon;
 import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.settings.GraphicalUserInterfaceSettings;
 
 public class HelpDialog {
 
@@ -23,10 +25,12 @@ public class HelpDialog {
     }
 
     public static void show(final Boolean expandToBottom, final Boolean expandToRight, final Point point, final String dontShowAgainKey, int flags, String title, String msg, ImageIcon icon) {
+        final boolean test = jd.Launcher.PARAMETERS.hasCommandSwitch("translatortest");
+        if (!JsonConfig.create(GraphicalUserInterfaceSettings.class).isBalloonNotificationEnabled()) return;
         if (CrossSystem.isWindows()) {
             try {
 
-                SimpleTextBallon d = new SimpleTextBallon(flags | Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, title, msg, icon) {
+                SimpleTextBallon d = new SimpleTextBallon(test ? flags : (flags | Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN), title, msg, icon) {
 
                     @Override
                     protected boolean doExpandToBottom(boolean b) {
@@ -47,8 +51,9 @@ public class HelpDialog {
 
                     @Override
                     protected String getDontShowAgainKey() {
+                        if (test) return "bla_" + System.currentTimeMillis();
                         if (dontShowAgainKey == null) return super.getDontShowAgainKey();
-                        Log.L.info(dontShowAgainKey);
+
                         return dontShowAgainKey;
                     }
 
@@ -68,6 +73,7 @@ public class HelpDialog {
                 ConfirmDialog d = new ConfirmDialog(flags | Dialog.BUTTONS_HIDE_CANCEL | Dialog.BUTTONS_HIDE_OK | Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, title, _GUI._.literall_usage_tipp() + "\r\n\r\n..." + msg, icon, null, null) {
                     @Override
                     protected String getDontShowAgainKey() {
+                        if (test) return "bla_" + System.currentTimeMillis();
                         if (dontShowAgainKey == null) return super.getDontShowAgainKey();
                         return dontShowAgainKey;
                     }
