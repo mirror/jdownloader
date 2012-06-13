4e0cfb78e13edae348ad90a0d848c2dfa5b10e57
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-06-13 17:10:49 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@17401 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/settings/staticreferences/CFG_LINKGRABBER.java
+++ src/org/jdownloader/settings/staticreferences/CFG_LINKGRABBER.java
@@ -22,8 +22,7 @@ public class CFG_LINKGRABBER {
     // access is faster, and we get an error on init if mappings are wrong.
     // true
     /**
-     * If true, Linkcollector will create an extra package for each multipart or
-     * *.rar archive
+     * If true, Linkcollector will create an extra package for each multipart or *.rar archive
      **/
     public static final BooleanKeyHandler                   ARCHIVE_PACKAGIZER_ENABLED                   = SH.getKeyHandler("ArchivePackagizerEnabled", BooleanKeyHandler.class);
     // 1
@@ -33,49 +32,41 @@ public class CFG_LINKGRABBER {
     public static final IntegerKeyHandler                   VARIOUS_PACKAGE_LIMIT                        = SH.getKeyHandler("VariousPackageLimit", IntegerKeyHandler.class);
     // true
     /**
-     * If true, Offline Links, that do not fit in a existing package, will be
-     * moved to a offline package.
+     * If true, Offline Links, that do not fit in a existing package, will be moved to a offline package.
      **/
     public static final BooleanKeyHandler                   OFFLINE_PACKAGE_ENABLED                      = SH.getKeyHandler("OfflinePackageEnabled", BooleanKeyHandler.class);
     // C:\Users\Thomas\downloads
     public static final StringKeyHandler                    LATEST_DOWNLOAD_DESTINATION_FOLDER           = SH.getKeyHandler("LatestDownloadDestinationFolder", StringKeyHandler.class);
     // true
     /**
-     * If true, AddLinks Dialogs will use the last used downloadfolder as
-     * defaultvalue. IF False, the Default Download Paath (settings) will be
-     * used
+     * If true, AddLinks Dialogs will use the last used downloadfolder as defaultvalue. IF False, the Default Download Paath (settings) will be used
      **/
     public static final BooleanKeyHandler                   USE_LAST_DOWNLOAD_DESTINATION_AS_DEFAULT     = SH.getKeyHandler("UseLastDownloadDestinationAsDefault", BooleanKeyHandler.class);
     // true
     public static final BooleanKeyHandler                   AUTO_EXTRACTION_ENABLED                      = SH.getKeyHandler("AutoExtractionEnabled", BooleanKeyHandler.class);
     // true
     /**
-     * If false, The AddLinks Dialog in Linkgrabber works on the pasted text,
-     * and does not prefilter URLS any more
+     * If false, The AddLinks Dialog in Linkgrabber works on the pasted text, and does not prefilter URLS any more
      **/
     public static final BooleanKeyHandler                   ADD_LINKS_PRE_PARSER_ENABLED                 = SH.getKeyHandler("AddLinksPreParserEnabled", BooleanKeyHandler.class);
     // true
     /**
-     * Set to false to hide the 'Add Downloads' Context Menu Action in
-     * Linkgrabber
+     * Set to false to hide the 'Add Downloads' Context Menu Action in Linkgrabber
      **/
     public static final BooleanKeyHandler                   CONTEXT_MENU_ADD_LINKS_ACTION_ALWAYS_VISIBLE = SH.getKeyHandler("ContextMenuAddLinksActionAlwaysVisible", BooleanKeyHandler.class);
     // []
     public static final ObjectKeyHandler                    PACKAGE_NAME_HISTORY                         = SH.getKeyHandler("PackageNameHistory", ObjectKeyHandler.class);
     // true
     /**
-     * If true, Plugins will try to correct filenames to match to others. For
-     * example in splitted archives.
+     * If true, Plugins will try to correct filenames to match to others. For example in splitted archives.
      **/
     public static final BooleanKeyHandler                   AUTO_FILENAME_CORRECTION_ENABLED             = SH.getKeyHandler("AutoFilenameCorrectionEnabled", BooleanKeyHandler.class);
     // [C:\Users\Thomas\downloads]
     public static final ObjectKeyHandler                    DOWNLOAD_DESTINATION_HISTORY                 = SH.getKeyHandler("DownloadDestinationHistory", ObjectKeyHandler.class);
-    // -1
-    public static final IntegerKeyHandler                   ADD_DIALOG_XLOCATION                         = SH.getKeyHandler("AddDialogXLocation", IntegerKeyHandler.class);
+
     // true
     /**
-     * Selecting Views in Linkgrabber Sidebar autoselects the matching links in
-     * the table. Set this to false to avoid this.
+     * Selecting Views in Linkgrabber Sidebar autoselects the matching links in the table. Set this to false to avoid this.
      **/
     public static final BooleanKeyHandler                   QUICK_VIEW_SELECTION_ENABLED                 = SH.getKeyHandler("QuickViewSelectionEnabled", BooleanKeyHandler.class);
     // 15000
@@ -88,17 +79,10 @@ public class CFG_LINKGRABBER {
      * If set, the addlinks dialog has this text. Use it for debug reasons.
      **/
     public static final StringKeyHandler                    PRESET_DEBUG_LINKS                           = SH.getKeyHandler("PresetDebugLinks", StringKeyHandler.class);
-    // -1
-    public static final IntegerKeyHandler                   ADD_DIALOG_YLOCATION                         = SH.getKeyHandler("AddDialogYLocation", IntegerKeyHandler.class);
     // true
     /**
-     * If true, the Linkcollector asks the Hosterplugins to filter the
-     * packageidentifier. This helps to map corrupt filenames into the correct
-     * packages.
+     * If true, the Linkcollector asks the Hosterplugins to filter the packageidentifier. This helps to map corrupt filenames into the correct packages.
      **/
     public static final BooleanKeyHandler                   AUTO_PACKAGE_MATCHING_CORRECTION_ENABLED     = SH.getKeyHandler("AutoPackageMatchingCorrectionEnabled", BooleanKeyHandler.class);
-    // 600
-    public static final IntegerKeyHandler                   ADD_DIALOG_WIDTH                             = SH.getKeyHandler("AddDialogWidth", IntegerKeyHandler.class);
-    // 290
-    public static final IntegerKeyHandler                   ADD_DIALOG_HEIGHT                            = SH.getKeyHandler("AddDialogHeight", IntegerKeyHandler.class);
+
 }
