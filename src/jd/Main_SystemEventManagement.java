0756ad1c8c2224a31347b2a9797cca00cf587e16
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-06-20 12:40:45 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@17521 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Main_SystemEventManagement.java
+++ src/jd/Main_SystemEventManagement.java
@@ -1,6 +1,10 @@
 package jd;
 
 import org.appwork.javaexe.JavaExe_SystemEventManagement;
+import org.appwork.javaexe.LogOffTyp;
+import org.appwork.javaexe.PowerBroadcastEvent;
+import org.appwork.javaexe.SystemEventHandler;
+import org.appwork.utils.swing.dialog.Dialog;
 
 /**
  * Class for javaexe launcher
@@ -8,7 +12,7 @@ import org.appwork.javaexe.JavaExe_SystemEventManagement;
  * @author Thomas
  * 
  */
-public class Main_SystemEventManagement extends JavaExe_SystemEventManagement {
+public class Main_SystemEventManagement extends JavaExe_SystemEventManagement implements SystemEventHandler {
     private static final Main_SystemEventManagement INSTANCE = new Main_SystemEventManagement();
 
     /**
@@ -30,6 +34,51 @@ public class Main_SystemEventManagement extends JavaExe_SystemEventManagement {
 
     public static int notifyEvent(int msg, int val1, int val2, String val3, int[] arr1, byte[] arr2) {
         return getInstance().onEvent(msg, val1, val2, val3, arr1, arr2);
+    }
+
+    @Override
+    protected SystemEventHandler getHandler() {
+        return this;
+    }
+
+    @Override
+    public boolean onQueryEndSession(LogOffTyp logOffTyp) {
+        return true;
+    }
+
+    @Override
+    public void onSessionEnd(boolean b, LogOffTyp logOffTyp) {
+    }
+
+    @Override
+    public void onDisplayChange(int w, int h, int val1) {
+
+        Dialog.getInstance().showMessageDialog("Display: " + w + "x" + h + " - " + val1 + " bits/pixel");
+    }
+
+    @Override
+    public void onScreenSaverState(boolean b) {
+    }
+
+    @Override
+    public void onPowerBroadcast(PowerBroadcastEvent ev) {
+    }
+
+    @Override
+    public boolean onQuerySuspend(boolean b) {
+        return true;
+    }
+
+    @Override
+    public void onPowerStatusChanged(boolean ac, byte chargingStatus, String percentageCharging, int secondsLeft, int secondsTotal) {
+    }
+
+    @Override
+    public void onOEMEvent(int val2) {
+    }
+
+    @Override
+    public void onCompacting(double percent) {
     }
 
 }
