c614046b4db9a18116872478ce142e9a05931079
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-06-21 09:42:42 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@17547 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/Main_SystemEventManagement.java
+++ src/jd/Main_SystemEventManagement.java
@@ -10,6 +10,8 @@ import org.appwork.javaexe.PowerBroadcastEvent;
 import org.appwork.javaexe.SessionEvent;
 import org.appwork.javaexe.SystemEventHandler;
 import org.appwork.shutdown.ShutdownController;
+import org.jdownloader.logging.LogController;
+import org.jdownloader.logging.LogSource;
 
 /**
  * Class for javaexe launcher
@@ -29,17 +31,20 @@ public class Main_SystemEventManagement extends JavaExe_SystemEventManagement im
         return Main_SystemEventManagement.INSTANCE;
     }
 
+    private LogSource logger;
+
     /**
      * Create a new instance of Main_SystemEventManagement. This is a singleton class. Access the only existing instance by using
      * {@link #getInstance()}.
      */
     private Main_SystemEventManagement() {
         super();
+        logger = LogController.getInstance().getLogger("WindowsSystemEventHandling");
 
     }
 
     public static int notifyEvent(int msg, int val1, int val2, String val3, int[] arr1, byte[] arr2) {
-        System.out.println("SystemEvent..... " + msg);
+
         while (!Launcher.GUI_COMPLETE.isReached()) {
             try {
                 Thread.sleep(100);
@@ -59,101 +64,101 @@ public class Main_SystemEventManagement extends JavaExe_SystemEventManagement im
     @Override
     public boolean onQueryEndSession(LogOffTyp logOffTyp) {
 
-        return ShutdownController.getInstance().requestShutdown(false);
+        return ShutdownController.getInstance().requestShutdown(true);
     }
 
     @Override
     public void onSessionEnd(boolean queryResponse, LogOffTyp logOffTyp) {
-        System.out.println("SessuinEnd: REsult: " + queryResponse + " Type: " + logOffTyp);
+        logger.info("SessuinEnd: REsult: " + queryResponse + " Type: " + logOffTyp);
     }
 
     @Override
     public void onDisplayChange(int w, int h, int val1) {
 
-        System.out.println("Display: " + w + "x" + h + " - " + val1 + " bits/pixel");
+        logger.info("Display: " + w + "x" + h + " - " + val1 + " bits/pixel");
     }
 
     @Override
     public void onScreenSaverState(boolean b) {
-        System.out.println("Screensaver Active: " + b);
+        logger.info("Screensaver Active: " + b);
     }
 
     @Override
     public void onPowerBroadcast(PowerBroadcastEvent ev) {
-        System.out.println("Powerbroadcast: " + ev);
+        logger.info("Powerbroadcast: " + ev);
     }
 
     @Override
     public boolean onQuerySuspend(boolean b) {
-        System.out.println("Suspend system: " + b);
+        logger.info("Suspend system: " + b);
         return true;
     }
 
     @Override
     public void onPowerStatusChanged(boolean ac, byte chargingStatus, String percentageCharging, int secondsLeft, int secondsTotal) {
-        System.out.println("PowerStatus: AC:" + ac + " Charging: " + chargingStatus + " " + percentageCharging + "% seconds left: " + secondsLeft + "/" + secondsTotal);
+        logger.info("PowerStatus: AC:" + ac + " Charging: " + chargingStatus + " " + percentageCharging + "% seconds left: " + secondsLeft + "/" + secondsTotal);
     }
 
     @Override
     public void onOEMEvent(int val2) {
-        System.out.println("OEM " + val2);
+        logger.info("OEM " + val2);
     }
 
     @Override
     public void onCompacting(double percent) {
-        System.out.println("Compacting RAM " + percent);
+        logger.info("Compacting RAM " + percent);
     }
 
     @Override
     public void onSessionChange(SessionEvent sessionEvent, int sessionid, String username, boolean current) {
-        System.out.println("Session CHanged: " + sessionEvent + " " + sessionid + " " + username + " " + current);
+        logger.info("Session CHanged: " + sessionEvent + " " + sessionid + " " + username + " " + current);
     }
 
     @Override
     public void onDeviceChangeEvent(DeviceChangeType deviceChangeType, DeviceType deviceType) {
-        System.out.println("DeviceChange: " + deviceChangeType + " - " + deviceChangeType);
+        logger.info("DeviceChange: " + deviceChangeType + " - " + deviceChangeType);
     }
 
     @Override
     public boolean onDeviceRemoveQuery(DeviceType deviceType) {
-        System.out.println("Remove ? " + deviceType);
+        logger.info("Remove ? " + deviceType);
         return true;
     }
 
     @Override
     public boolean onDeviceConfigChangeQuery() {
-        System.out.println("onDeviceConfigChangeQuery");
+        logger.info("onDeviceConfigChangeQuery");
         return true;
     }
 
     @Override
     public void onDeviceConfigChange() {
-        System.out.println("onDeviceConfigChange");
+        logger.info("onDeviceConfigChange");
     }
 
     @Override
     public void onDeviceConfigChangeCanceled() {
-        System.out.println("onDeviceConfigChangeCanceled");
+        logger.info("onDeviceConfigChangeCanceled");
     }
 
     @Override
     public void onNetworkConnected(String device, NetworkType networkType, String ip, String gateway, String mask) {
-        System.out.println("Network: " + device + " " + networkType + " IP:" + ip + " gateway:" + gateway + " Mask:" + mask);
+        logger.info("Network: " + device + " " + networkType + " IP:" + ip + " gateway:" + gateway + " Mask:" + mask);
     }
 
     @Override
     public void onNetworkDisconnect(String device) {
-        System.out.println("Network disconnect: " + device);
+        logger.info("Network disconnect: " + device);
     }
 
     @Override
     public void onNetworkConnecting(String device) {
-        System.out.println("Network connecting: " + device);
+        logger.info("Network connecting: " + device);
     }
 
     @Override
     public boolean onConsoleEvent(ConsoleEventType type) {
-        System.out.println(type);
+        logger.info(type + "");
         return true;
     }
 
