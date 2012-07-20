31e829e219c379dceff9c3c9ea0130ca764fa404
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-07-20 18:57:40 +0000

    Fixes: depositfile, double 'user-agent', values should not contain this also.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@17987 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/DepositFiles.java
+++ src/jd/plugins/hoster/DepositFiles.java
@@ -55,7 +55,7 @@ import org.appwork.utils.formatter.TimeFormatter;
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "depositfiles.com" }, urls = { "https?://[\\w\\.]*?depositfiles\\.com(/\\w{1,3})?/files/[\\w]+" }, flags = { 2 })
 public class DepositFiles extends PluginForHost {
 
-    private static final String UA                       = "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0";
+    private static final String UA                       = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:11.0) Gecko/20100101 Firefox/11.0";
     private static final String FILE_NOT_FOUND           = "Dieser File existiert nicht|Entweder existiert diese Datei nicht oder sie wurde";
     private static final String PATTERN_PREMIUM_FINALURL = "<div id=\"download_url\">.*?<a href=\"(.*?)\"";
     private static final String MAINPAGE                 = "http://depositfiles.com";
