69fee889bd23a251c98885f53ed0bf15defcec15
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-07-22 04:04:58 +0000

    Hoster: coraldrive.net - Handle msg "Invalid or Deleted File"
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18006 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/CoralDriveNet.java
+++ src/jd/plugins/hoster/CoralDriveNet.java
@@ -114,7 +114,7 @@ public class CoralDriveNet extends PluginForHost {
         br.setFollowRedirects(false);
         prepBrowser();
         getPage(link.getDownloadURL());
-        if (new Regex(correctedBR, Pattern.compile("(No such file|>File Not Found<|>The file was removed by|Reason (of|for) deletion:\n)", Pattern.CASE_INSENSITIVE)).matches()) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (new Regex(correctedBR, Pattern.compile("(>Invalid or Deleted File<|No such file|>File Not Found<|>The file was removed by|Reason (of|for) deletion:\n)", Pattern.CASE_INSENSITIVE)).matches()) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         if (correctedBR.contains(MAINTENANCE)) {
             link.getLinkStatus().setStatusText(JDL.L("plugins.hoster.xfilesharingprobasic.undermaintenance", MAINTENANCEUSERTEXT));
             return AvailableStatus.TRUE;
