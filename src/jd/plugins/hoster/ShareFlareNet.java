900cd26c4cdc64ad3a1d760455ce28e126f76b97
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-07-25 20:23:19 +0000

    Added skymonk support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18057 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ShareFlareNet.java
+++ src/jd/plugins/hoster/ShareFlareNet.java
@@ -16,15 +16,23 @@
 
 package jd.plugins.hoster;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
+import jd.config.ConfigContainer;
+import jd.config.ConfigEntry;
+import jd.gui.UserIO;
 import jd.http.Browser;
 import jd.http.RandomUserAgent;
 import jd.http.URLConnectionAdapter;
+import jd.nutils.JDHash;
 import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
 import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
@@ -48,6 +56,7 @@ public class ShareFlareNet extends PluginForHost {
 
     public ShareFlareNet(PluginWrapper wrapper) {
         super(wrapper);
+        setConfigElements();
         this.setAccountwithoutUsername(true);
         enablePremium("http://shareflare.net/page/premium.php");
     }
@@ -110,52 +119,98 @@ public class ShareFlareNet extends PluginForHost {
         return 1;
     }
 
+    private boolean validateEmail(String email) {
+        return new Regex(email, ".+@.+\\.[a-z]+").matches();
+    }
+
+    private String getLinkViaSkymonkDownloadMethod(String s) throws IOException {
+        String appId = getPluginConfig().getStringProperty("APPID", null);
+        boolean validate = getPluginConfig().getBooleanProperty("APPIDVALIDATE", false);
+
+        if (!validate || !getPluginConfig().getBooleanProperty("STATUS", false)) return null;
+        Browser skymonk = new Browser();
+        skymonk.setCustomCharset("UTF-8");
+        skymonk.getHeaders().put("Pragma", null);
+        skymonk.getHeaders().put("Cache-Control", null);
+        skymonk.getHeaders().put("Accept-Charset", null);
+        skymonk.getHeaders().put("Accept-Encoding", null);
+        skymonk.getHeaders().put("Accept", null);
+        skymonk.getHeaders().put("Accept-Language", null);
+        skymonk.getHeaders().put("User-Agent", null);
+        skymonk.getHeaders().put("Referer", null);
+        skymonk.getHeaders().put("Content-Type", "application/x-www-form-urlencoded");
+
+        skymonk.postPage("http://api.letitbit.net/internal/index2.php", "action=LINK_GET_DIRECT&link=" + s + "&free_link=1&appid=" + appId + "&version=2.0 beta1");
+        String[] result = skymonk.getRegex("([^\r\n]+)").getColumn(0);
+        if (result == null || result.length == 0) return null;
+
+        if ("NO".equals(result[0].trim())) {
+            if (result.length > 1) {
+                if ("activation".equals(result[1].trim())) {
+                    logger.warning("SkyMonk activation not completed!");
+                }
+            }
+        }
+
+        ArrayList<String> res = new ArrayList<String>();
+        for (String r : result) {
+            if (r.startsWith("http")) {
+                res.add(r);
+            }
+        }
+        if (res.size() > 1) return res.get(1);
+        return res.size() == 1 ? res.get(0) : null;
+    }
+
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
         requestFileInformation(downloadLink);
-        String waittime = br.getRegex("You can wait download for ([\t\n\r0-9]+) minutes or upgrade to premium").getMatch(0);
-        if (waittime != null) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(waittime.trim()) * 60 * 1001l);
-        if (br.containsHTML("You reached your hourly traffic limit\\.")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 30 * 60 * 1001l);
-        if (br.containsHTML(FREELIMIT)) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1000l);
-        if (br.containsHTML("(В бесплатном режиме вы можете скачивать только один файл|You are currently downloading|Free users are allowed to only one parallel download\\.\\.)")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED);
-        br.setFollowRedirects(false);
-        String debug = br.toString();
-        final Form fastDl = br.getFormbyProperty("id", "fast_download_form");
-        if (fastDl != null) br.submitForm(fastDl);
-        Form dlform = br.getFormbyProperty("id", "dvifree");
-        if (dlform == null) {
-            if (!br.containsHTML(FREEDOWNLOADPOSSIBLE)) throw new PluginException(LinkStatus.ERROR_FATAL, JDL.L("plugins.hoster.shareflarenet.nofreedownloadlink", "No free download link for this file"));
-            logger.warning("dlform is null");
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        debug = debug + br.submitForm(dlform);
-        dlform = br.getFormbyProperty("id", "dvifree");
-        if (dlform == null) {
-            logger.warning("dlform#2 is null...");
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        br.submitForm(dlform);
-        // Waittime before captcha is skippable
-        final String rcID = br.getRegex("challenge\\?k=([^<>\"]*?)\"").getMatch(0);
-        if (rcID == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        final Browser ajaxBR = br.cloneBrowser();
-        ajaxBR.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-        final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
-        rc.setId(rcID);
-        rc.load();
-        for (int i = 0; i <= 3; i++) {
-            File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-            final String c = getCaptchaCode(cf, downloadLink);
-            ajaxBR.postPage("http://shareflare.net/ajax/check_recaptcha.php", "recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + c);
-            if (ajaxBR.containsHTML("error_wrong_captcha")) {
-                rc.reload();
-                continue;
+        String dllink = getLinkViaSkymonkDownloadMethod(downloadLink.getDownloadURL());
+        if (dllink == null) {
+            String waittime = br.getRegex("You can wait download for ([\t\n\r0-9]+) minutes or upgrade to premium").getMatch(0);
+            if (waittime != null) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, Integer.parseInt(waittime.trim()) * 60 * 1001l);
+            if (br.containsHTML("You reached your hourly traffic limit\\.")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 30 * 60 * 1001l);
+            if (br.containsHTML(FREELIMIT)) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1000l);
+            if (br.containsHTML("(В бесплатном режиме вы можете скачивать только один файл|You are currently downloading|Free users are allowed to only one parallel download\\.\\.)")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED);
+            br.setFollowRedirects(false);
+            String debug = br.toString();
+            final Form fastDl = br.getFormbyProperty("id", "fast_download_form");
+            if (fastDl != null) br.submitForm(fastDl);
+            Form dlform = br.getFormbyProperty("id", "dvifree");
+            if (dlform == null) {
+                if (!br.containsHTML(FREEDOWNLOADPOSSIBLE)) throw new PluginException(LinkStatus.ERROR_FATAL, JDL.L("plugins.hoster.shareflarenet.nofreedownloadlink", "No free download link for this file"));
+                logger.warning("dlform is null");
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
-            break;
+            debug = debug + br.submitForm(dlform);
+            dlform = br.getFormbyProperty("id", "dvifree");
+            if (dlform == null) {
+                logger.warning("dlform#2 is null...");
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            br.submitForm(dlform);
+            // Waittime before captcha is skippable
+            final String rcID = br.getRegex("challenge\\?k=([^<>\"]*?)\"").getMatch(0);
+            if (rcID == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            final Browser ajaxBR = br.cloneBrowser();
+            ajaxBR.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+            final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+            final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+            rc.setId(rcID);
+            rc.load();
+            for (int i = 0; i <= 3; i++) {
+                File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+                final String c = getCaptchaCode(cf, downloadLink);
+                ajaxBR.postPage("http://shareflare.net/ajax/check_recaptcha.php", "recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + c);
+                if (ajaxBR.containsHTML("error_wrong_captcha")) {
+                    rc.reload();
+                    continue;
+                }
+                break;
+            }
+            if (ajaxBR.containsHTML("error_wrong_captcha")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            dllink = getDllink(ajaxBR);
         }
-        if (ajaxBR.containsHTML("error_wrong_captcha")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
-        String dllink = getDllink(ajaxBR);
         if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         dllink = dllink.replace("\\", "");
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 1);
@@ -236,4 +291,89 @@ public class ShareFlareNet extends PluginForHost {
     public void resetDownloadlink(DownloadLink link) {
     }
 
+    private void setConfigElements() {
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "The SkyMonk method without waittime and captcha needs an activation!"));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_SEPARATOR));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "IMPORTANT note!"));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "JDownloader only uses the download technique which skymonk uses, the programm \"skymonk\" is NOT required for JDownloader to use this method!"));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_LABEL, "Just enter a mailadress, click on activate and wait for the confirmation window, that's all!"));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_SEPARATOR));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getPluginConfig(), "STATUS", JDL.L("plugins.hoster.shareflare.status", "Use SkyMonk?")).setDefaultValue(false));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_SEPARATOR));
+        final ConfigEntry configEntry;
+        getConfig().addEntry(configEntry = new ConfigEntry(ConfigContainer.TYPE_TEXTFIELD, getPluginConfig(), "SKYMONKEMAIL", JDL.L("plugins.hoster.shareflare.email", "E-Mail:")));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_SEPARATOR));
+        getConfig().addEntry(new ConfigEntry(ConfigContainer.TYPE_BUTTON, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Thread() {
+                    public void run() {
+                        String email = getPluginConfig().getStringProperty("SKYMONKEMAIL", null);
+                        try {
+                            jd.config.GuiConfigListener listener = configEntry.getGuiListener();
+                            if (listener != null) {
+                                email = (String) listener.getText();
+                            }
+                        } catch (Throwable e2) {
+                            /* does not exist in 09581 */
+                        }
+                        String emailChanged = getPluginConfig().getStringProperty("SKYMONKEMAILCHANGED", null);
+                        if (!email.equalsIgnoreCase(emailChanged)) {
+                            getPluginConfig().setProperty("APPID", null);
+                            getPluginConfig().setProperty("SKYMONKVALIDATE", null);
+                            getPluginConfig().setProperty("APPIDVALIDATE", false);
+                        }
+                        String appId = getPluginConfig().getStringProperty("APPID", null);
+                        appId = appId == null ? JDHash.getMD5(String.valueOf(Math.random())) : appId;
+                        boolean validate = getPluginConfig().getBooleanProperty("SKYMONKVALIDATE", false);
+
+                        if (email == null || email.length() == 0) {
+                            UserIO.getInstance().requestMessageDialog("E-Mail is empty!");
+                            return;
+                        }
+                        if (!validateEmail(email)) {
+                            logger.warning("E-Mail is no valid --> " + email);
+                            UserIO.getInstance().requestMessageDialog("E-Mail is not valid!");
+                            return;
+                        }
+                        if (!validate) {
+                            Browser skymonk = new Browser();
+                            skymonk.setCookie("http://shareflare.net/", "lang", "en");
+                            skymonk.setCustomCharset("UTF-8");
+                            try {
+                                skymonk.postPage("http://skymonk.net/?page=activate", "act=get_activation_key&phone=+49" + String.valueOf((int) (Math.random() * (999999999 - 1111111111) + 1111111111)) + "&email=" + email + "&app_id=" + appId + "&app_version=2");
+                            } catch (Throwable e1) {
+                            }
+                            String msg = skymonk.getRegex("content:\'(.*?)\'").getMatch(0);
+                            if (skymonk.containsHTML("status:\'error\'")) {
+                                msg = msg == null ? "Error occured!" : msg;
+                                UserIO.getInstance().requestMessageDialog("Error occured", msg);
+                                return;
+                            } else if (skymonk.containsHTML("status:\'ok\'")) {
+                                if (skymonk.containsHTML("activation code has been sent to your e\\-mail")) {
+                                    getPluginConfig().setProperty("APPID", appId);
+                                    getPluginConfig().setProperty("APPIDVALIDATE", true);
+                                    getPluginConfig().setProperty("SKYMONKEMAIL", email);
+                                    getPluginConfig().setProperty("SKYMONKEMAILCHANGED", email);
+                                    getPluginConfig().setProperty("SKYMONKVALIDATE", true);
+                                    UserIO.getInstance().requestMessageDialog("Activation succesfully!");
+                                } else {
+                                    msg = msg == null ? "OK!" : msg;
+                                    UserIO.getInstance().requestMessageDialog("SkyMonk server answer", msg);
+                                }
+                            } else {
+                                logger.warning("SkyMonk debug output: " + skymonk.toString());
+                                UserIO.getInstance().requestMessageDialog("SkyMonk: Unknown error occured", "Please upload now a logfile, contact our support and add this loglink to your bugreport!");
+                            }
+                            getPluginConfig().save();
+                        } else {
+                            UserIO.getInstance().requestMessageDialog("SkyMonk is already activated!");
+                        }
+                    };
+                }.start();
+
+            }
+        }, "Activation", null, null));
+    }
 }
