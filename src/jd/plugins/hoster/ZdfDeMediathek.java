9827adde06d837334cf0e4b4cef36abad96fa226
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-08-13 18:05:37 +0000

    -improved zdf regex
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18249 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ZdfDeMediathek.java
+++ src/jd/plugins/hoster/ZdfDeMediathek.java
@@ -28,7 +28,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "zdf.de" }, urls = { "http://(www\\.)?zdf\\.de/ZDFmediathek#?/(hauptnavigation/startseite(/[a-z0-9\\-_]+)?#?/)?beitrag/video/\\d+/[^<>\"/\\?#]{1}" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "zdf.de" }, urls = { "http://(www\\.)?zdf\\.de/ZDFmediathek#?/[^<>\"]*?beitrag/video/\\d+/[^<>\"/\\?#]{1}" }, flags = { 0 })
 public class ZdfDeMediathek extends PluginForHost {
 
     public ZdfDeMediathek(PluginWrapper wrapper) {
