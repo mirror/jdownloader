6567e1c5ee3d590b2533c334e0c680d09b68736c
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-08-29 08:32:36 +0000

    Host: GlumboUploadsCom Test another fix for another problem, plugin out of date.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18352 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/GlumboUploadsCom.java
+++ src/jd/plugins/hoster/GlumboUploadsCom.java
@@ -265,7 +265,7 @@ public class GlumboUploadsCom extends PluginForHost {
         if (dllink == null) {
             checkErrors(downloadLink, false, passCode);
             if (BRBEFORE.contains("\"download1\"")) {
-                br.postPage(downloadLink.getDownloadURL(), "op=download1&usr_login=&id=" + new Regex(downloadLink.getDownloadURL(), COOKIE_HOST.replace("http://", "") + "/" + "([a-z0-9]{12})").getMatch(0) + "&fname=" + Encoding.urlEncode(downloadLink.getName()) + "&referer=&method_free=Slow+Download");
+                br.postPage(downloadLink.getDownloadURL(), "op=download1&usr_login=&id=" + new Regex(downloadLink.getDownloadURL(), COOKIE_HOST.replace("http://", "") + "/" + "([a-z0-9]{12})").getMatch(0) + "&fname=&referer=&method_free=Free+Download");
                 doSomething();
                 checkErrors(downloadLink, false, passCode);
             }
