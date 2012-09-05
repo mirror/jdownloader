fadcbe4523f275d01aa3cf6f6d77403dbf8751b5
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-09-05 11:55:43 +0000

    Host: Fshare.vn Fix file not found.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18402 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FShareVn.java
+++ src/jd/plugins/hoster/FShareVn.java
@@ -74,7 +74,8 @@ public class FShareVn extends PluginForHost {
         br.getPage(link.getDownloadURL());
         if (br.containsHTML("(<title>Fshare \\– Dịch vụ chia sẻ số 1 Việt Nam \\– Cần là có \\- </title>|b>Liên kết bạn chọn không tồn tại trên hệ thống Fshare</|<li>Liên kết không chính xác, hãy kiểm tra lại|<li>Liên kết bị xóa bởi người sở hữu\\.<)")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         String filename = br.getRegex("<p><b>Tên file:</b> (.*?)</p>").getMatch(0);
-        if (filename == null) filename = br.getRegex("<title>(.*?) \\- Fshare \\- Dịch vụ chia sẻ, lưu trữ dữ liệu miễn phí tốt nhất </title>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<p><b>Tên tập tin:</b> (.*?)</p>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<title>(.*?) - Fshare - Dịch vụ chia sẻ, lưu trữ dữ liệu miễn phí tốt nhất </title>").getMatch(0);
         String filesize = br.getRegex("<p><b>Dung lượng: </b>(.*?)</p>").getMatch(0);
         if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         // Server sometimes sends bad filenames
