f1409f2532bb843ef42c0ef61201ebbcfa776bfd
flubshi <flubshi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-09-15 18:43:32 +0000

    + ported 'old' premium4.me plugin to jdownloader 2, see #4911
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18496 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/Premium4Me.java
@@ -0,0 +1,227 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import jd.PluginWrapper;
+import jd.config.Property;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "premium4.me" }, urls = { "REGEX_NOT_POSSIBLE_RANDOM-asdfasdfsadfsdgfd5654" }, flags = { 2 })
+public class Premium4Me extends PluginForHost {
+
+    private static HashMap<Account, HashMap<String, Long>> hostUnavailableMap = new HashMap<Account, HashMap<String, Long>>();
+
+    public Premium4Me(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium("http://premium4.me/");
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        AccountInfo ac = new AccountInfo();
+        br.setConnectTimeout(60 * 1000);
+        br.setReadTimeout(60 * 1000);
+        br.setDebug(true);
+        String username = Encoding.urlEncode(account.getUser());
+        String pass = Encoding.urlEncode(account.getPass());
+        // String page = null;
+        String hosts = null;
+        String traffic = null;
+        br.setFollowRedirects(true);
+        br.setAcceptLanguage("en, en-gb;q=0.8");
+        try {
+            br.postPageRaw("http://premium4.me/login.php", "{\"u\":\"" + username + "\", \"p\":\"" + pass + "\", \"r\":true}");
+            if (br.getCookie("http://premium4.me", "auth") != null) {
+                // page = br.getPage("http://premium4.me/account.php");
+                traffic = br.getPage("http://premium4.me/traffic.php").trim() + " MB";
+                hosts = br.getPage("http://premium4.me/hosters.php");
+            }
+        } catch (Exception e) {
+            account.setTempDisabled(true);
+            account.setValid(true);
+            ac.setProperty("multiHostSupport", Property.NULL);
+            ac.setStatus("Premium4Me Server Error, temp disabled");
+            return ac;
+        }
+        if (br.getCookie("http://premium4.me", "auth") == null) {
+            ac.setProperty("multiHostSupport", Property.NULL);
+            account.setValid(false);
+            return ac;
+        }
+        ac.setTrafficLeft(traffic);
+        // String date = new Regex(page, "\"d\":\"(.*?)\",").getMatch(0);
+        account.setValid(true);
+        /* expire date does currently not work */
+        // ac.setValidUntil(TimeFormatter.getMilliSeconds(date,
+        // "dd MMM yyyy", null));
+        ArrayList<String> supportedHosts = new ArrayList<String>();
+        String hoster[] = new Regex(hosts.trim(), "(.+?)(;|$)").getColumn(0);
+        if (hoster != null) {
+            for (String host : hoster) {
+                if (hosts == null || host.length() == 0) continue;
+                supportedHosts.add(host.trim());
+            }
+        }
+        if (account.isValid()) {
+            if (supportedHosts.size() == 0) {
+                ac.setProperty("multiHostSupport", Property.NULL);
+                ac.setStatus("Account invalid");
+            } else {
+                ac.setStatus("Account valid: " + supportedHosts.size() + " Hosts via premium4.me available");
+                ac.setProperty("multiHostSupport", supportedHosts);
+            }
+        } else {
+            account.setTempDisabled(false);
+            account.setValid(false);
+            ac.setProperty("multiHostSupport", Property.NULL);
+            ac.setStatus("Account invalid");
+        }
+        return ac;
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://premium4.me/";
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 0;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_ONLY);
+    }
+
+    private void showMessage(DownloadLink link, String message) {
+        link.getLinkStatus().setStatusText(message);
+    }
+
+    /** no override to keep plugin compatible to old stable */
+    public void handleMultiHost(DownloadLink link, Account acc) throws Exception {
+        boolean dofollow = br.isFollowingRedirects();
+        try {
+            br.setFollowRedirects(true);
+            br.setConnectTimeout(90 * 1000);
+            br.setReadTimeout(90 * 1000);
+            br.setDebug(true);
+            dl = null;
+            String user = Encoding.urlEncode(acc.getUser());
+            String pw = Encoding.urlEncode(acc.getPass());
+            String url = link.getDownloadURL().replaceFirst("https?://", "");
+            url = Encoding.urlEncode(url);
+            showMessage(link, "Phase 1/3: Login...");
+            br.postPageRaw("http://premium4.me/login.php", "{\"u\":\"" + user + "\", \"p\":\"" + pw + "\", \"r\":true}");
+            if (br.getCookie("http://premium4.me", "auth") == null) {
+                resetAvailablePremium(acc);
+                acc.setValid(false);
+                throw new PluginException(LinkStatus.ERROR_RETRY);
+            }
+            br.setFollowRedirects(true);
+            showMessage(link, "Phase 2/3: Get link");
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, "http://premium4.me/getfile.php?link=" + url, true, 1);
+            if (dl.getConnection().getResponseCode() == 404) {
+                /* file offline */
+                dl.getConnection().disconnect();
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            if (!dl.getConnection().isContentDisposition()) {
+                br.followConnection();
+                logger.severe("Premium4Me(Error): " + br.toString());
+                /*
+                 * after x retries we disable this host and retry with normal plugin
+                 */
+                if (link.getLinkStatus().getRetryCount() >= 3) {
+                    /* disable hoster for 1h */
+                    tempUnavailableHoster(acc, link, 60 * 60 * 1000);
+                    /* reset retrycounter */
+                    link.getLinkStatus().setRetryCount(0);
+                    throw new PluginException(LinkStatus.ERROR_RETRY);
+                }
+                String msg = "(" + link.getLinkStatus().getRetryCount() + 1 + "/" + 3 + ")";
+                showMessage(link, msg);
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Retry in few secs" + msg, 10 * 1000l);
+            }
+            showMessage(link, "Phase 3/3: Download...");
+            dl.startDownload();
+        } finally {
+            br.setFollowRedirects(dofollow);
+        }
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws Exception {
+        return AvailableStatus.UNCHECKABLE;
+    }
+
+    private void tempUnavailableHoster(Account account, DownloadLink downloadLink, long timeout) throws PluginException {
+        if (downloadLink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "Unable to handle this errorcode!");
+        synchronized (hostUnavailableMap) {
+            HashMap<String, Long> unavailableMap = hostUnavailableMap.get(account);
+            if (unavailableMap == null) {
+                unavailableMap = new HashMap<String, Long>();
+                hostUnavailableMap.put(account, unavailableMap);
+            }
+            /* wait to retry this host */
+            unavailableMap.put(downloadLink.getHost(), (System.currentTimeMillis() + timeout));
+        }
+        throw new PluginException(LinkStatus.ERROR_RETRY);
+    }
+
+    @Override
+    public boolean canHandle(DownloadLink downloadLink, Account account) {
+        synchronized (hostUnavailableMap) {
+            HashMap<String, Long> unavailableMap = hostUnavailableMap.get(account);
+            if (unavailableMap != null) {
+                Long lastUnavailable = unavailableMap.get(downloadLink.getHost());
+                if (lastUnavailable != null && System.currentTimeMillis() < lastUnavailable) {
+                    return false;
+                } else if (lastUnavailable != null) {
+                    unavailableMap.remove(downloadLink.getHost());
+                    if (unavailableMap.size() == 0) hostUnavailableMap.remove(account);
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    private void resetAvailablePremium(Account ac) {
+        ac.setProperty("multiHostSupport", Property.NULL);
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
