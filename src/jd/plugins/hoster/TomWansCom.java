e39a23645a7eae7bedd0a19fe8dfcf934c743814
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-10-18 17:59:06 +0000

    Added support for tomwans.com closes #6603 thx to psp for the preparatory work
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18750 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/TomWansCom.java
@@ -0,0 +1,105 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2012  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "tomwans.com" }, urls = { "http://(www\\.)?tomwans\\.com/video/[a-z0-9]{32}/" }, flags = { 0 })
+public class TomWansCom extends PluginForHost {
+
+    public TomWansCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.tomwans.com/terms_of_use.html";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML(">404: Video not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setFinalFileName(Encoding.htmlDecode(filename.trim()) + ".flv");
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        final Form humanform = br.getForm(0);
+        if (humanform == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        br.submitForm(humanform);
+        final String cryptedScript = br.getRegex("eval(.*?)[\r\n]+").getMatch(0);
+        final String dllink = getFinalLink(cryptedScript);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    private String getFinalLink(String s) {
+        Object result = null;
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("javascript");
+        try {
+            /* unpacked js */
+            result = engine.eval(s);
+            /* make a function and return jwplayer("mediaspace").setup({'file': value,...}) */
+            engine.eval("function jwplayer(name) { var newObj = new Object(); function setup(array) { return array.file; } newObj.setup = setup; return newObj; }");
+            /* splitting with delimiters and execute jwplayer().setup() function */
+            result = engine.eval(result.toString().replaceAll("\\);", "\\);@@@").split("@@@")[0]);
+        } catch (final Throwable e) {
+            return null;
+        }
+        return result != null ? result.toString() : null;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(final DownloadLink link) {
+    }
+
+}
