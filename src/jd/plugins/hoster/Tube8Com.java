4318f4fa6a8338d90180dc5a034f7845dddab686
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-10-24 18:05:05 +0000

    closes #6801
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18783 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Tube8Com.java
+++ src/jd/plugins/hoster/Tube8Com.java
@@ -17,13 +17,26 @@
 package jd.plugins.hoster;
 
 import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import jd.PluginWrapper;
 import jd.config.ConfigContainer;
 import jd.config.ConfigEntry;
+import jd.gui.UserIO;
 import jd.http.Browser;
 import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Base64;
 import jd.nutils.encoding.Encoding;
+import jd.nutils.nativeintegration.LocalBrowser;
 import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.AccountInfo;
@@ -39,8 +52,8 @@ import jd.utils.locale.JDL;
 public class Tube8Com extends PluginForHost {
 
     private boolean             setEx  = true;
+    private String              dllink = null;
     private static final String mobile = "mobile";
-    public String               dllink = null;
 
     public Tube8Com(PluginWrapper wrapper) {
         super(wrapper);
@@ -48,12 +61,13 @@ public class Tube8Com extends PluginForHost {
         this.enablePremium("http://www.tube8.com/signin.html");
     }
 
+    @Override
     public void correctDownloadLink(final DownloadLink link) {
         link.setUrlDownload(link.getDownloadURL().replace("/embed", ""));
     }
 
     @Override
-    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
         if (setEx) this.setBrowserExclusive();
         br.setFollowRedirects(false);
         br.getPage(downloadLink.getDownloadURL());
@@ -70,80 +84,101 @@ public class Tube8Com extends PluginForHost {
             }
         }
         if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+
+        boolean failed = true;
+        boolean preferMobile = getPluginConfig().getBooleanProperty(mobile, false);
+        String videoDownloadUrls = "";
+
+        /* streaming link */
+        findStreamingLink();
+        if (dllink != null && requestVideo(downloadLink)) failed = false;
+        /* decrease HTTP requests */
+        if (failed || preferMobile) videoDownloadUrls = standardAndMobile(downloadLink);
+        /* normal link */
+        if (failed) findNormalLink(videoDownloadUrls);
+        if (failed && dllink != null && requestVideo(downloadLink)) failed = false;
+        /* 3gp link */
+        if (failed || preferMobile) findMobileLink(videoDownloadUrls);
+        if ((failed || preferMobile) && dllink != null && requestVideo(downloadLink)) failed = false;
+
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+
+        filename = filename.trim();
+        if (dllink.contains(".3gp")) {
+            downloadLink.setFinalFileName((filename + ".3gp"));
+        } else if (dllink.contains(".mp4")) {
+            downloadLink.setFinalFileName((filename + ".mp4"));
+        } else {
+            downloadLink.setFinalFileName(filename + ".flv");
+        }
+        if (failed) return AvailableStatus.UNCHECKABLE;
+        return AvailableStatus.TRUE;
+    }
+
+    private boolean requestVideo(final DownloadLink downloadLink) throws Exception {
+        URLConnectionAdapter con = null;
+        final Browser br2 = br.cloneBrowser();
+        try {
+            con = br2.openGetConnection(dllink);
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else if (br2.getHttpConnection().getResponseCode() == 401) {
+                downloadLink.setProperty("401", 401);
+                return true;
+            } else {
+                return false;
+            }
+            return true;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Throwable e) {
+            }
+        }
+    }
+
+    private String standardAndMobile(DownloadLink downloadLink) throws Exception {
         final Browser br2 = br.cloneBrowser();
         br2.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        final String hash = br.getRegex("var hash = \"([a-z0-9]+)\"").getMatch(0);
-        if (hash == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        br2.getPage("http://www.tube8.com/ajax/getVideoDownloadURL.php?_=" + System.currentTimeMillis() + "&hash=" + hash + "&video=" + new Regex(downloadLink.getDownloadURL(), ".*?(\\d+)$").getMatch(0));
-        final String correctedBR = br2.toString().replace("\\", "");
-        boolean failed = false;
-        for (int i = 1; i <= 2; i++) {
-            boolean prefer3gp = getPluginConfig().getBooleanProperty(mobile, false);
-            if (prefer3gp || failed) {
-                find3gpLinks(correctedBR);
-                if (dllink == null) {
-                    logger.warning("Couldn't find 3gp links even if they're prefered, trying to get the normal links now!");
-                    findNormalLinks(correctedBR);
-                }
-            } else {
-                findNormalLinks(correctedBR);
-                if (dllink == null) {
-                    logger.warning("Couldn't find normal links even if they're prefered, trying to get the 3gp links now!");
-                    find3gpLinks(correctedBR);
-                }
-            }
-            if (dllink == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            dllink = Encoding.htmlDecode(dllink);
-            filename = filename.trim();
-            if (dllink.contains(".3gp")) {
-                downloadLink.setFinalFileName((filename + ".3gp"));
-            } else {
-                downloadLink.setFinalFileName(filename + ".flv");
-            }
-            URLConnectionAdapter con = null;
-            try {
-                con = br.openGetConnection(Encoding.htmlDecode(dllink));
-                if (!con.getContentType().contains("html")) {
-                    downloadLink.setDownloadSize(con.getLongContentLength());
-                } else {
-                    failed = true;
-                    continue;
-                }
-                return AvailableStatus.TRUE;
-            } finally {
-                try {
-                    con.disconnect();
-                } catch (final Throwable e) {
-                }
-            }
-        }
-        return AvailableStatus.FALSE;
+        br2.getPage("http://www.tube8.com/ajax/getVideoDownloadURL.php?_=" + System.currentTimeMillis() + "&hash=" + br.getRegex("var hash = \"([a-z0-9]+)\"").getMatch(0) + "&video=" + new Regex(downloadLink.getDownloadURL(), ".*?(\\d+)$").getMatch(0));
+        String ret = br2.getRegex("^(.*?)$").getMatch(0);
+        return ret != null ? ret.replace("\\", "") : "";
     }
 
-    @Override
-    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
-        final AccountInfo ai = new AccountInfo();
-        try {
-            this.login(account, this.br);
-        } catch (final PluginException e) {
-            account.setValid(false);
-            return ai;
-        }
-        /* only support for free accounts at the moment */
-        ai.setUnlimitedTraffic();
-        ai.setStatus("Account ok");
-        account.setValid(true);
-        return ai;
-    }
-
-    public void find3gpLinks(final String correctedBR) throws NumberFormatException, PluginException {
+    private void findMobileLink(final String correctedBR) throws Exception {
         dllink = new Regex(correctedBR, "\"mobile_url\":\"(http:.*?)\"").getMatch(0);
         if (dllink == null) dllink = new Regex(correctedBR, "\"(http://cdn\\d+\\.mobile\\.tube8\\.com/.*?)\"").getMatch(0);
     }
 
-    public void findNormalLinks(final String correctedBR) throws NumberFormatException, PluginException, IOException {
+    private void findNormalLink(final String correctedBR) throws Exception {
         dllink = new Regex(correctedBR, "\"standard_url\":\"(http.*?)\"").getMatch(0);
         if (dllink == null) dllink = new Regex(correctedBR, "\"(http://cdn\\d+\\.public\\.tube8\\.com/.*?)\"").getMatch(0);
+    }
+
+    private void findStreamingLink() throws Exception {
+        String flashVars = br.getRegex("var flashvars = \\{([^\\}]+)").getMatch(0);
+        if (flashVars == null) return;
+        flashVars = flashVars.replaceAll("\"", "");
+        Map<String, String> values = new HashMap<String, String>();
+
+        for (String s : flashVars.split(",")) {
+            if (!s.matches(".+:.+")) continue;
+            values.put(s.split(":")[0], s.split(":", 2)[1]);
+        }
+
+        String isEncrypted = values.get("encrypted");
+        dllink = values.get("video_url");
+        if ("1".equals(isEncrypted) || Boolean.parseBoolean(isEncrypted)) {
+            String decrypted = values.get("video_url");
+            String key = values.get("video_title");
+            try {
+                dllink = new BouncyCastleAESCounterModeDecrypt().decrypt(decrypted, key, 256);
+            } catch (Throwable e) {
+                /* Fallback for stable version */
+                dllink = AESCounterModeDecrypt(decrypted, key, 256);
+            }
+            if (dllink != null && (dllink.startsWith("Error:") || !dllink.startsWith("http"))) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, dllink); }
+        }
     }
 
     @Override
@@ -161,6 +196,10 @@ public class Tube8Com extends PluginForHost {
         requestFileInformation(downloadLink);
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
+            if (downloadLink.getIntegerProperty("401", -1) == 401) {
+                downloadLink.getProperties().remove("401");
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 30 * 1000l);
+            }
             br.followConnection();
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
@@ -174,10 +213,30 @@ public class Tube8Com extends PluginForHost {
         requestFileInformation(downloadLink);
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
+            if (downloadLink.getIntegerProperty("401", -1) == 401) {
+                downloadLink.getProperties().remove("401");
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 30 * 1000l);
+            }
             br.followConnection();
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        final AccountInfo ai = new AccountInfo();
+        try {
+            this.login(account, this.br);
+        } catch (final PluginException e) {
+            account.setValid(false);
+            return ai;
+        }
+        /* only support for free accounts at the moment */
+        ai.setUnlimitedTraffic();
+        ai.setStatus("Account ok");
+        account.setValid(true);
+        return ai;
     }
 
     private void login(Account account, Browser br) throws IOException, PluginException {
@@ -207,8 +266,103 @@ public class Tube8Com extends PluginForHost {
     public void resetPluginGlobals() {
     }
 
+    /**
+     * AES CTR(Counter) Mode for Java ported from AES-CTR-Mode implementation in JavaScript by Chris Veness
+     * 
+     * @see <a
+     *      href="http://csrc.nist.gov/publications/nistpubs/800-38a/sp800-38a.pdf">"Recommendation for Block Cipher Modes of Operation - Methods and Techniques"</a>
+     */
+    private String AESCounterModeDecrypt(String cipherText, String key, int nBits) throws Exception {
+        if (!(nBits == 128 || nBits == 192 || nBits == 256)) { return "Error: Must be a key mode of either 128, 192, 256 bits"; }
+        if (cipherText == null || key == null) { return "Error: cipher and/or key equals null"; }
+        String res = null;
+        nBits = nBits / 8;
+        byte[] data = Base64.decode(cipherText.toCharArray());
+        byte[] k = Arrays.copyOf(key.getBytes(), nBits);
+
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        SecretKey secretKey = generateSecretKey(k, nBits);
+        byte[] nonceBytes = Arrays.copyOf(Arrays.copyOf(data, 8), nBits / 2);
+        IvParameterSpec nonce = new IvParameterSpec(nonceBytes);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, nonce);
+        res = new String(cipher.doFinal(data, 8, data.length - 8));
+        return res;
+    }
+
+    private SecretKey generateSecretKey(byte[] keyBytes, int nBits) throws Exception {
+        try {
+            SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            keyBytes = cipher.doFinal(keyBytes);
+        } catch (InvalidKeyException e) {
+            if (e.getMessage().contains("Illegal key size")) {
+                getPolicyFiles();
+            }
+            throw new PluginException(LinkStatus.ERROR_FATAL, "Unlimited Strength JCE Policy Files needed!");
+        } catch (Throwable e1) {
+            return null;
+        }
+        System.arraycopy(keyBytes, 0, keyBytes, nBits / 2, nBits / 2);
+        return new SecretKeySpec(keyBytes, "AES");
+    }
+
+    private class BouncyCastleAESCounterModeDecrypt {
+        private String decrypt(String cipherText, String key, int nBits) throws Exception {
+            if (!(nBits == 128 || nBits == 192 || nBits == 256)) { return "Error: Must be a key mode of either 128, 192, 256 bits"; }
+            if (cipherText == null || key == null) { return "Error: cipher and/or key equals null"; }
+            byte[] decrypted;
+            nBits = nBits / 8;
+            byte[] data = Base64.decode(cipherText.toCharArray());
+            byte[] k = Arrays.copyOf(key.getBytes(), nBits);
+            /* AES/CTR/NoPadding (SIC == CTR) */
+            org.bouncycastle.crypto.BufferedBlockCipher cipher = new org.bouncycastle.crypto.BufferedBlockCipher(new org.bouncycastle.crypto.modes.SICBlockCipher(new org.bouncycastle.crypto.engines.AESEngine()));
+            cipher.reset();
+            SecretKey secretKey = generateSecretKey(k, nBits);
+            byte[] nonceBytes = Arrays.copyOf(Arrays.copyOf(data, 8), nBits / 2);
+            IvParameterSpec nonce = new IvParameterSpec(nonceBytes);
+            /* true == encrypt; false == decrypt */
+            cipher.init(true, new org.bouncycastle.crypto.params.ParametersWithIV(new org.bouncycastle.crypto.params.KeyParameter(secretKey.getEncoded()), ((IvParameterSpec) nonce).getIV()));
+            decrypted = new byte[cipher.getOutputSize(data.length - 8)];
+            int decLength = cipher.processBytes(data, 8, data.length - 8, decrypted, 0);
+            cipher.doFinal(decrypted, decLength);
+            return new String(decrypted);
+        }
+
+        private SecretKey generateSecretKey(byte[] keyBytes, int nBits) throws Exception {
+            try {
+                SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+                /* AES/ECB/NoPadding */
+                org.bouncycastle.crypto.BufferedBlockCipher cipher = new org.bouncycastle.crypto.BufferedBlockCipher(new org.bouncycastle.crypto.engines.AESEngine());
+                cipher.init(true, new org.bouncycastle.crypto.params.KeyParameter(secretKey.getEncoded()));
+                keyBytes = new byte[cipher.getOutputSize(secretKey.getEncoded().length)];
+                int decLength = cipher.processBytes(secretKey.getEncoded(), 0, secretKey.getEncoded().length, keyBytes, 0);
+                cipher.doFinal(keyBytes, decLength);
+            } catch (Throwable e) {
+                return null;
+            }
+            System.arraycopy(keyBytes, 0, keyBytes, nBits / 2, nBits / 2);
+            return new SecretKeySpec(keyBytes, "AES");
+        }
+    }
+
+    private void getPolicyFiles() throws Exception {
+        int ret = -100;
+        UserIO.setCountdownTime(120);
+        ret = UserIO.getInstance().requestConfirmDialog(UserIO.STYLE_LARGE, "Java Cryptography Extension (JCE) Error: 32 Byte keylength is not supported!", "At the moment your Java version only supports a maximum keylength of 16 Bytes but the keezmovies plugin needs support for 32 byte keys.\r\nFor such a case Java offers so called \"Policy Files\" which increase the keylength to 32 bytes. You have to copy them to your Java-Home-Directory to do this!\r\nExample path: \"jre6\\lib\\security\\\". The path is different for older Java versions so you might have to adapt it.\r\n\r\nBy clicking on CONFIRM a browser instance will open which leads to the downloadpage of the file.\r\n\r\nThanks for your understanding.", null, "CONFIRM", "Cancel");
+        if (ret != -100) {
+            if (UserIO.isOK(ret)) {
+                LocalBrowser.openDefaultURL(new URL("http://www.oracle.com/technetwork/java/javase/downloads/jce-6-download-429243.html"));
+                LocalBrowser.openDefaultURL(new URL("http://h10.abload.de/img/jcedp50.png"));
+            } else {
+                return;
+            }
+        }
+    }
+
     private void setConfigElements() {
         ConfigEntry cond = new ConfigEntry(ConfigContainer.TYPE_CHECKBOX, getPluginConfig(), mobile, JDL.L("plugins.hoster.Tube8Com.setting.preferVideosForMobilePhones", "Prefer videos for mobile phones (3gp format)")).setDefaultValue(false);
         getConfig().addEntry(cond);
     }
+
 }
