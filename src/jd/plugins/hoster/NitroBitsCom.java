d4d51a447cafc86cb0522c00cb637f23a0ece765
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-11-13 16:59:04 +0000

    -added support for nitrobits (+ premium) refs #6991 free not yet completely finished
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@18887 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/NitroBitsCom.java
@@ -0,0 +1,235 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import jd.PluginWrapper;
+import jd.config.Property;
+import jd.http.Cookie;
+import jd.http.Cookies;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginForHost;
+import jd.plugins.decrypter.LnkCrptWs;
+import jd.utils.JDUtilities;
+
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "nitrobits.com" }, urls = { "http://(www\\.)?nitrobits\\.com/file/[A-Za-z0-9]+" }, flags = { 2 })
+public class NitroBitsCom extends PluginForHost {
+
+    public NitroBitsCom(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium("");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://nitrobits.com/premium.php";
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML(">File is not available<|The requested file was not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        String filename = br.getRegex("<div style=\" margin\\-left:10px;\">[\t\n\r ]+<b>([^<>\"]*?)</b></div>").getMatch(0);
+        if (filename == null) filename = br.getRegex("<title>Nitrobits\\.com \\- Download ([^<>\"]*?)</title>").getMatch(0);
+        String filesize = br.getRegex("<h1>[^<>\"/]*? \\- (\\d+(\\.\\d+)? [A-Za-z]{1,7})</h1>").getMatch(0);
+        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(Encoding.htmlDecode(filename.trim()));
+        link.setDownloadSize(SizeFormatter.getSize(filesize));
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        final Regex reconnect = br.getRegex("id=\"timer\">(\\d{2}):(\\d{2})</span> minutes");
+        if (reconnect.getMatches().length == 1) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, (Integer.parseInt(reconnect.getMatch(0)) * 60 + Integer.parseInt(reconnect.getMatch(1))) * 1001l);
+        br.setFollowRedirects(false);
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+        br.getHeaders().put("Accept", "*/*");
+        final String downSess = br.getRegex("var downsess = \"([a-z0-9]+)\"").getMatch(0);
+        if (downSess == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        final long timeBefore = System.currentTimeMillis();
+        for (int i = 0; i <= 3; i++) {
+            final PluginForDecrypt solveplug = JDUtilities.getPluginForDecrypt("linkcrypt.ws");
+            final jd.plugins.decrypter.LnkCrptWs.SolveMedia sm = ((LnkCrptWs) solveplug).getSolveMedia(br);
+            final File cf = sm.downloadCaptcha(getLocalCaptchaFile());
+            final String code = getCaptchaCode(cf, downloadLink);
+
+            if (i == 0) {
+                waitTime(timeBefore, downloadLink);
+            }
+
+            br.postPage("http://nitrobits.com/ajax.solvcaptcha.php", "downsess=" + downSess + "&adcopy_challenge=2@" + Encoding.urlEncode(sm.getChallengeId()) + "&adcopy_response=" + Encoding.urlEncode(code));
+            if (br.containsHTML("status\":\"ERROR\"")) {
+                br.getPage(downloadLink.getDownloadURL());
+                continue;
+            }
+        }
+        if (br.containsHTML("status\":\"ERROR\"")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+        if (!br.containsHTML("\"status\":\"SUCCESS\"")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        br.postPage("http://nitrobits.com/getdownlink.php", "");
+        String dllink = br.getRegex("\"link\":\"(http:[^<>\"]*?)\"").getMatch(0);
+        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        dllink = dllink.replace("\\", "");
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            // This case probably never happens
+            if (br.containsHTML("Expired Session\\!")) throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "Server error", 1 * 60 * 1000l);
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    private void waitTime(long timeBefore, final DownloadLink downloadLink) throws PluginException {
+        int passedTime = (int) ((System.currentTimeMillis() - timeBefore) / 1000) - 1;
+        /** Ticket Time */
+        int tt = 20;
+        final String ttt = br.getRegex("var wait = (\\d+);").getMatch(0);
+        if (ttt != null) {
+            tt = Integer.parseInt(ttt);
+            tt -= passedTime;
+        }
+        if (tt > 0) sleep(tt * 1000l, downloadLink);
+    }
+
+    private static final String MAINPAGE = "http://nitrobits.com";
+    private static Object       LOCK     = new Object();
+
+    @SuppressWarnings("unchecked")
+    private void login(final Account account, final boolean force) throws Exception {
+        synchronized (LOCK) {
+            try {
+                // Load cookies
+                br.setCookiesExclusive(true);
+                final Object ret = account.getProperty("cookies", null);
+                boolean acmatch = Encoding.urlEncode(account.getUser()).equals(account.getStringProperty("name", Encoding.urlEncode(account.getUser())));
+                if (acmatch) acmatch = Encoding.urlEncode(account.getPass()).equals(account.getStringProperty("pass", Encoding.urlEncode(account.getPass())));
+                if (acmatch && ret != null && ret instanceof HashMap<?, ?> && !force) {
+                    final HashMap<String, String> cookies = (HashMap<String, String>) ret;
+                    if (account.isValid()) {
+                        for (final Map.Entry<String, String> cookieEntry : cookies.entrySet()) {
+                            final String key = cookieEntry.getKey();
+                            final String value = cookieEntry.getValue();
+                            this.br.setCookie(MAINPAGE, key, value);
+                        }
+                        return;
+                    }
+                }
+                br.setFollowRedirects(true);
+                br.postPage("http://nitrobits.com/login.php", "submit=Sign+In&remember=on&username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(account.getPass()));
+                if (br.getCookie(MAINPAGE, "nitrobits_user") == null || br.getCookie(MAINPAGE, "nitrobits_pass") == null) throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                if (!br.containsHTML("class=\"statusdet\">Premium</span></div>")) {
+                    logger.info("Login handling broken or invalid account type!");
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                // Save cookies
+                final HashMap<String, String> cookies = new HashMap<String, String>();
+                final Cookies add = this.br.getCookies(MAINPAGE);
+                for (final Cookie c : add.getCookies()) {
+                    cookies.put(c.getKey(), c.getValue());
+                }
+                account.setProperty("name", Encoding.urlEncode(account.getUser()));
+                account.setProperty("pass", Encoding.urlEncode(account.getPass()));
+                account.setProperty("cookies", cookies);
+            } catch (final PluginException e) {
+                account.setProperty("cookies", Property.NULL);
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        AccountInfo ai = new AccountInfo();
+        try {
+            login(account, true);
+        } catch (PluginException e) {
+            account.setValid(false);
+            return ai;
+        }
+        br.getPage("http://nitrobits.com/account.php");
+        ai.setUnlimitedTraffic();
+        final String expire = br.getRegex("Premium Expiration[\t\n\r ]+</td>[\t\n\r ]+<td>[\t\n\r ]+<div>(\\d{4}\\-\\d{2}\\-\\d{2} \\d{2}:\\d{2}:\\d{2})</div>").getMatch(0);
+        if (expire == null) {
+            account.setValid(false);
+            return ai;
+        } else {
+            ai.setValidUntil(TimeFormatter.getMilliSeconds(expire, "yyyy-MM-dd hh:mm:ss", Locale.ENGLISH));
+        }
+        account.setValid(true);
+        ai.setStatus("Premium User");
+        return ai;
+    }
+
+    @Override
+    public void handlePremium(final DownloadLink link, final Account account) throws Exception {
+        requestFileInformation(link);
+        login(account, false);
+        br.setFollowRedirects(false);
+        br.getPage(link.getDownloadURL());
+        String dllink = br.getRedirectLocation();
+        if (dllink == null) {
+            logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, Encoding.htmlDecode(dllink), true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            logger.warning("The final dllink seems not to be a file!");
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
