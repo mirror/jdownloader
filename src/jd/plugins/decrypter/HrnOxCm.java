7af3fc2e2a6993c563ece5e1ff19a577c67c44c7
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2012-12-19 08:17:39 +0000

    Added: hornoxe.com (video) support - re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19151 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/HrnOxCm.java
@@ -0,0 +1,71 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2012  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision: 17655 $", interfaceVersion = 2, names = { "hornoxe.com" }, urls = { "http://(www\\.)?hornoxe\\.com/[^/]+/" }, flags = { 0 })
+public class HrnOxCm extends PluginForDecrypt {
+
+    public HrnOxCm(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.getPage(parameter);
+
+        String pageName = br.getRegex("og:title\" content=\"(.*?)\" />").getMatch(0);
+        if (pageName == null) pageName = br.getRegex("<title>(.*?) \\- Hornoxe\\.com</title>").getMatch(0);
+
+        String file = br.getRegex("file\":\"(https?://videos\\.hornoxe\\.com/[^\"]+)").getMatch(0);
+
+        String image = br.getRegex("image\":\"(https?://(www\\.)hornoxe\\.com/wp\\-content/images/\\d+[^\"]+)").getMatch(0);
+
+        if (file == null || pageName == null) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
+        } else {
+            pageName = Encoding.htmlDecode(pageName.trim());
+            if (image != null && image.length() > 0) {
+                DownloadLink img = createDownloadlink("directhttp://" + image);
+                img.setFinalFileName(pageName + image.substring(image.lastIndexOf(".")));
+                decryptedLinks.add(img);
+            }
+            DownloadLink vid = createDownloadlink(parameter.replace("hornoxe.com", "hornoxedecrypted"));
+            vid.setFinalFileName(pageName + file.substring(file.lastIndexOf(".")));
+            decryptedLinks.add(vid);
+        }
+
+        FilePackage fp = FilePackage.getInstance();
+        fp.setName(pageName);
+        fp.addLinks(decryptedLinks);
+
+        return decryptedLinks;
+    }
+
+}
---
+++ src/jd/plugins/hoster/HornOxeCom.java
@@ -0,0 +1,110 @@
+package jd.plugins.hoster;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.utils.locale.JDL;
+
+@HostPlugin(revision = "$Revision: 18671 $", interfaceVersion = 2, names = { "hornoxe.com" }, urls = { "https?://(www\\.)hornoxedecrypted/.+" }, flags = { 0 })
+public class HornOxeCom extends PluginForHost {
+
+    public HornOxeCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.hornoxe.com/";
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    public void correctDownloadLink(DownloadLink link) {
+        link.setUrlDownload(link.getDownloadURL().replace("hornoxedecrypted", "hornoxe.com"));
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        // stupid site doesn't always accept connection
+        int repeat = 3;
+        for (int i = 0; i <= repeat; i++) {
+            Browser br2 = br.cloneBrowser();
+            try {
+                dl = jd.plugins.BrowserAdapter.openDownload(br, link, br2.getURL(), true, 1);
+                if (!dl.getConnection().isContentDisposition() && !dl.getConnection().getContentType().startsWith("video")) {
+                    br2.followConnection();
+                    if (br2.containsHTML("No htmlCode read")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.hornoxe.videotemporaryunavailable", "This video is temporary unavailable!"), 60 * 60 * 1000l);
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                dl.startDownload();
+                break;
+            } catch (Throwable e) {
+                if (i == repeat && (!dl.getConnection().isContentDisposition() && !dl.getConnection().getContentType().startsWith("video"))) {
+                    br2.followConnection();
+                    if (br2.containsHTML("No htmlCode read")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.hornoxe.videotemporaryunavailable", "This video is temporary unavailable!"), 60 * 60 * 1000l);
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        br.getPage(link.getDownloadURL());
+        String file = br.getRegex("file\":\"(https?://videos\\.hornoxe\\.com/[^\"]+)").getMatch(0);
+        if (file == null) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        URLConnectionAdapter con = null;
+        br.setConnectTimeout(3 * 60 * 1000);
+        br.setReadTimeout(3 * 60 * 1000);
+
+        // stupid site doesn't always accept connection
+        boolean worked = false;
+        int repeat = 3;
+        for (int i = 0; i <= repeat; i++) {
+            if (worked) break;
+            try {
+                con = br.openGetConnection(file);
+                if (!con.getContentType().contains("html")) {
+                    link.setDownloadSize(con.getLongContentLength());
+                    worked = true;
+                }
+            } catch (Throwable e) {
+            } finally {
+                try {
+                    con.disconnect();
+                } catch (Throwable e) {
+                }
+            }
+            if (!worked) {
+                br.getHeaders().put("Referer", link.getDownloadURL());
+                continue;
+            }
+        }
+        if (!worked) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+
+        return AvailableStatus.TRUE;
+
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
