8f5eaf6c31ab8dfb93d96ecc1eda1e0aad9d61ed
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-01-17 01:20:28 +0000

    fixes #7278
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19411 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PornHubCom.java
+++ src/jd/plugins/hoster/PornHubCom.java
@@ -1,8 +1,21 @@
 package jd.plugins.hoster;
 
-import java.io.IOException;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import jd.PluginWrapper;
+import jd.gui.UserIO;
+import jd.nutils.encoding.Base64;
+import jd.nutils.encoding.Encoding;
+import jd.nutils.nativeintegration.LocalBrowser;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -10,13 +23,10 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-import org.appwork.utils.Regex;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornhub.com" }, urls = { "http://(www\\.)?((de|fr|it|es|pt)\\.)?pornhub\\.com/view_video\\.php\\?viewkey=[a-z0-9]+" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornhub.com" }, urls = { "http://(www\\.)?((de|fr|it|es|pt)\\.)?pornhub\\.com/(view_video\\.php\\?viewkey=|embed/)\\d+" }, flags = { 0 })
 public class PornHubCom extends PluginForHost {
 
-    private String post_element = "add299463d4410c6d1b1c418868225f7";
-    private String dlUrl        = null;
+    private String dlUrl = null;
 
     public PornHubCom(final PluginWrapper wrapper) {
         super(wrapper);
@@ -24,6 +34,7 @@ public class PornHubCom extends PluginForHost {
 
     public void correctDownloadLink(DownloadLink link) {
         link.setUrlDownload(link.getDownloadURL().replaceAll("((de|fr|it|es|pt)\\.)pornhub\\.com/", "pornhub.com/"));
+        link.setUrlDownload(link.getDownloadURL().replaceAll("embed/", "view_video.php?viewkey="));
     }
 
     @Override
@@ -34,45 +45,6 @@ public class PornHubCom extends PluginForHost {
     @Override
     public int getMaxSimultanFreeDownloadNum() {
         return -1;
-    }
-
-    private AvailableStatus requestVideo(final DownloadLink downloadLink) throws IOException, PluginException {
-        setBrowserExclusive();
-        br.setCookie("http://pornhub.com/", "age_verified", "1");
-        br.setFollowRedirects(true);
-        br.getPage(downloadLink.getDownloadURL());
-        if (br.getURL().equals("http://www.pornhub.com/") || !br.containsHTML("\\.swf")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        dlUrl = null;
-        String file_name = br.getRegex("class=\"section_bar\"><h1 class=\"section_title\">([^<>]*?)</h1>").getMatch(0);
-        if (file_name == null) file_name = br.getRegex("<title([^<>]*?) \\- Pornhub\\.com</title>").getMatch(0);
-        if (file_name == null) file_name = br.getRegex("<h1 class=\"section_title\">([^<>]*?)</h1>").getMatch(0);
-        if (file_name == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        String[] linksplit = downloadLink.getDownloadURL().split("=");
-        final String video_id = linksplit[linksplit.length - 1];
-
-        final byte[] b = new byte[] { 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x00, 0x0c, 0x70, 0x6c, 0x61, 0x79, 0x65, 0x72, 0x43, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x00, 0x02, 0x2f, 0x31, 0x00, 0x00, 0x00, 0x44, 0x0a, 0x00, 0x00, 0x00, 0x03, 0x02, 0x00 };
-        final byte[] b2 = new byte[] { 0x02, 0x00, 0x02, 0x2d, 0x31, 0x02, 0x00, 0x20 };
-
-        final String url = "http://www.pornhub.com//gateway.php";
-        final String postdata = new String(b) + new String(new byte[] { (byte) video_id.length() }) + video_id + new String(b2) + post_element;
-
-        br.getHeaders().put("Content-Type", "application/x-amf");
-        br.setFollowRedirects(false);
-        br.postPageRaw(url, postdata);
-
-        final byte[] raw = br.toString().getBytes();
-        if (raw == null || raw.length == 0) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
-        for (int i = 0; i <= raw.length - 1; i++) {
-            if (raw[i] < 32 || raw[i] > 176) {
-                raw[i] = 35; // #
-            }
-        }
-
-        dlUrl = new Regex(new String(raw), "flv_url.*?(http.*?)##post_roll").getMatch(0);
-        if (dlUrl == null) { throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT); }
-        linksplit = dlUrl.split("\\.");
-        downloadLink.setFinalFileName(file_name.replace("\"", "'") + ".flv");
-        return AvailableStatus.TRUE;
     }
 
     @Override
@@ -106,6 +78,144 @@ public class PornHubCom extends PluginForHost {
         throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
     }
 
+    private AvailableStatus requestVideo(final DownloadLink downloadLink) throws Exception {
+        setBrowserExclusive();
+        br.setCookie("http://pornhub.com/", "age_verified", "1");
+        br.setFollowRedirects(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.getURL().equals("http://www.pornhub.com/") || !br.containsHTML("\\.swf")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+
+        getVideoLink(downloadLink.getDownloadURL());
+
+        String file_name = br.getRegex("class=\"section_bar\"><h1 class=\"section_title\">([^<>]*?)</h1>").getMatch(0);
+        if (file_name == null) file_name = br.getRegex("<title([^<>]*?) \\- Pornhub\\.com</title>").getMatch(0);
+        if (file_name == null) file_name = br.getRegex("<h1 class=\"section_title\">([^<>]*?)</h1>").getMatch(0);
+        if (file_name == null || dlUrl == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        downloadLink.setFinalFileName(file_name.replace("\"", "'") + ".mp4");
+        return AvailableStatus.TRUE;
+    }
+
+    private void getVideoLink(String dllink) throws Exception {
+        String flashVars = br.getRegex("var flashvars = \\{([^\\}]+)").getMatch(0);
+        if (flashVars == null) return;
+        flashVars = flashVars.replaceAll("\"", "");
+        Map<String, String> values = new HashMap<String, String>();
+
+        for (String s : flashVars.split(",")) {
+            if (!s.matches(".+:.+")) continue;
+            values.put(s.split(":")[0], Encoding.htmlDecode(s.split(":", 2)[1]));
+        }
+
+        String isEncrypted = values.get("encrypted");
+        dlUrl = values.get("video_url");
+        if ("1".equals(isEncrypted) || Boolean.parseBoolean(isEncrypted)) {
+            String encrypted = values.get("video_url");
+            String key = values.get("video_title");
+            try {
+                dlUrl = new BouncyCastleAESCounterModeDecrypt().decrypt(encrypted, key, 256);
+            } catch (Throwable e) {
+                /* Fallback for stable version */
+                dlUrl = AESCounterModeDecrypt(encrypted, key, 256);
+            }
+            if (dlUrl != null && (dlUrl.startsWith("Error:") || !dlUrl.startsWith("http"))) {
+                logger.warning("pornhub.com: " + dlUrl);
+                dlUrl = null;
+            }
+        }
+    }
+
+    /**
+     * AES CTR(Counter) Mode for Java ported from AES-CTR-Mode implementation in JavaScript by Chris Veness
+     * 
+     * @see <a
+     *      href="http://csrc.nist.gov/publications/nistpubs/800-38a/sp800-38a.pdf">"Recommendation for Block Cipher Modes of Operation - Methods and Techniques"</a>
+     */
+    private String AESCounterModeDecrypt(String cipherText, String key, int nBits) throws Exception {
+        if (!(nBits == 128 || nBits == 192 || nBits == 256)) return "Error: Must be a key mode of either 128, 192, 256 bits";
+        if (cipherText == null || key == null) return "Error: cipher and/or key equals null";
+        String res = null;
+        nBits = nBits / 8;
+        byte[] data = Base64.decode(cipherText.toCharArray());
+        byte[] k = Arrays.copyOf(key.getBytes(), nBits);
+
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        SecretKey secretKey = generateSecretKey(k, nBits);
+        byte[] nonceBytes = Arrays.copyOf(Arrays.copyOf(data, 8), nBits / 2);
+        IvParameterSpec nonce = new IvParameterSpec(nonceBytes);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, nonce);
+        res = new String(cipher.doFinal(data, 8, data.length - 8));
+        return res;
+    }
+
+    private SecretKey generateSecretKey(byte[] keyBytes, int nBits) throws Exception {
+        try {
+            SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            keyBytes = cipher.doFinal(keyBytes);
+        } catch (InvalidKeyException e) {
+            if (e.getMessage().contains("Illegal key size")) getPolicyFiles();
+            throw new PluginException(LinkStatus.ERROR_FATAL, "Unlimited Strength JCE Policy Files needed!");
+        } catch (Throwable e1) {
+            return null;
+        }
+        System.arraycopy(keyBytes, 0, keyBytes, nBits / 2, nBits / 2);
+        return new SecretKeySpec(keyBytes, "AES");
+    }
+
+    private class BouncyCastleAESCounterModeDecrypt {
+        private String decrypt(String cipherText, String key, int nBits) throws Exception {
+            if (!(nBits == 128 || nBits == 192 || nBits == 256)) return "Error: Must be a key mode of either 128, 192, 256 bits";
+            if (cipherText == null || key == null) return "Error: cipher and/or key equals null";
+            byte[] decrypted;
+            nBits = nBits / 8;
+            byte[] data = Base64.decode(cipherText.toCharArray());
+            byte[] k = Arrays.copyOf(key.getBytes(), nBits);
+            /* AES/CTR/NoPadding (SIC == CTR) */
+            org.bouncycastle.crypto.BufferedBlockCipher cipher = new org.bouncycastle.crypto.BufferedBlockCipher(new org.bouncycastle.crypto.modes.SICBlockCipher(new org.bouncycastle.crypto.engines.AESEngine()));
+            cipher.reset();
+            SecretKey secretKey = generateSecretKey(k, nBits);
+            byte[] nonceBytes = Arrays.copyOf(Arrays.copyOf(data, 8), nBits / 2);
+            IvParameterSpec nonce = new IvParameterSpec(nonceBytes);
+            /* true == encrypt; false == decrypt */
+            cipher.init(true, new org.bouncycastle.crypto.params.ParametersWithIV(new org.bouncycastle.crypto.params.KeyParameter(secretKey.getEncoded()), ((IvParameterSpec) nonce).getIV()));
+            decrypted = new byte[cipher.getOutputSize(data.length - 8)];
+            int decLength = cipher.processBytes(data, 8, data.length - 8, decrypted, 0);
+            cipher.doFinal(decrypted, decLength);
+            return new String(decrypted);
+        }
+
+        private SecretKey generateSecretKey(byte[] keyBytes, int nBits) throws Exception {
+            try {
+                SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+                /* AES/ECB/NoPadding */
+                org.bouncycastle.crypto.BufferedBlockCipher cipher = new org.bouncycastle.crypto.BufferedBlockCipher(new org.bouncycastle.crypto.engines.AESEngine());
+                cipher.init(true, new org.bouncycastle.crypto.params.KeyParameter(secretKey.getEncoded()));
+                keyBytes = new byte[cipher.getOutputSize(secretKey.getEncoded().length)];
+                int decLength = cipher.processBytes(secretKey.getEncoded(), 0, secretKey.getEncoded().length, keyBytes, 0);
+                cipher.doFinal(keyBytes, decLength);
+            } catch (Throwable e) {
+                return null;
+            }
+            System.arraycopy(keyBytes, 0, keyBytes, nBits / 2, nBits / 2);
+            return new SecretKeySpec(keyBytes, "AES");
+        }
+    }
+
+    private void getPolicyFiles() throws Exception {
+        int ret = -100;
+        UserIO.setCountdownTime(120);
+        ret = UserIO.getInstance().requestConfirmDialog(UserIO.STYLE_LARGE, "Java Cryptography Extension (JCE) Error: 32 Byte keylength is not supported!", "At the moment your Java version only supports a maximum keylength of 16 Bytes but the keezmovies plugin needs support for 32 byte keys.\r\nFor such a case Java offers so called \"Policy Files\" which increase the keylength to 32 bytes. You have to copy them to your Java-Home-Directory to do this!\r\nExample path: \"jre6\\lib\\security\\\". The path is different for older Java versions so you might have to adapt it.\r\n\r\nBy clicking on CONFIRM a browser instance will open which leads to the downloadpage of the file.\r\n\r\nThanks for your understanding.", null, "CONFIRM", "Cancel");
+        if (ret != -100) {
+            if (UserIO.isOK(ret)) {
+                LocalBrowser.openDefaultURL(new URL("http://www.oracle.com/technetwork/java/javase/downloads/jce-6-download-429243.html"));
+                LocalBrowser.openDefaultURL(new URL("http://h10.abload.de/img/jcedp50.png"));
+            } else {
+                return;
+            }
+        }
+    }
+
     @Override
     public void reset() {
     }
@@ -113,4 +223,5 @@ public class PornHubCom extends PluginForHost {
     @Override
     public void resetDownloadlink(final DownloadLink link) {
     }
+
 }
