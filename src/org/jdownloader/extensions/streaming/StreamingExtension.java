38c77d881a82b67c81b57cf775429639a3ee2676
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-01-19 10:47:36 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19448 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/streaming/StreamingExtension.java
+++ src/org/jdownloader/extensions/streaming/StreamingExtension.java
@@ -65,7 +65,7 @@ public class StreamingExtension extends AbstractExtension<StreamingConfig, Strea
     private LogSource           logger;
     protected StreamingProvider streamProvider = null;
     static {
-        // we need a testcommit
+        // we need a testcommit 2
         // we need to load the profiles
         Profile.init();
     }
