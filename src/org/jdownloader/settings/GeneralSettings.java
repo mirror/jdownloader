d40447108360ea62e27ee27b99a3c8e499800327
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-09 16:58:17 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19836 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/settings/GeneralSettings.java
+++ src/org/jdownloader/settings/GeneralSettings.java
@@ -380,6 +380,7 @@ public interface GeneralSettings extends ConfigInterface {
 
     void setShowFileNameInCaptchaDialogEnabled(boolean b);
 
+    //
     @AboutConfig
     @DefaultBooleanValue(true)
     @DescriptionForConfigEntry("If Enabled, JDownloader will save the linkgrabber list when you exit jd, and restore it on next startup")
