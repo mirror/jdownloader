394cc660e00d50e65a9ea74ec7fdf652ec83ccba
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-09 16:47:49 +0000

    fixes #7088
    Added a Contextmenu to the advanced config table. 
    
    Added an entry: Reset Selected entries to default.
    
    To reset all entries:
    ctrl+A (Select all) -> right click -> reset
    
    
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19833 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/swing/jdgui/views/settings/panels/advanced/AdvancedTable.java
+++ src/jd/gui/swing/jdgui/views/settings/panels/advanced/AdvancedTable.java
@@ -1,7 +1,20 @@
 package jd.gui.swing.jdgui.views.settings.panels.advanced;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
+import javax.swing.JPopupMenu;
+
 import jd.gui.swing.jdgui.BasicJDTable;
 
+import org.appwork.swing.exttable.ExtColumn;
+import org.appwork.utils.swing.dialog.DialogCanceledException;
+import org.appwork.utils.swing.dialog.DialogClosedException;
+import org.jdownloader.actions.AppAction;
+import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.gui.uiserio.NewUIO;
+import org.jdownloader.images.NewTheme;
 import org.jdownloader.settings.advanced.AdvancedConfigEntry;
 
 public class AdvancedTable extends BasicJDTable<AdvancedConfigEntry> {
@@ -9,6 +22,55 @@ public class AdvancedTable extends BasicJDTable<AdvancedConfigEntry> {
 
     public AdvancedTable() {
         super(new AdvancedTableModel("AdvancedTable"));
+    }
+
+    @Override
+    protected JPopupMenu onContextMenu(JPopupMenu popup, AdvancedConfigEntry contextObject, final List<AdvancedConfigEntry> selection, ExtColumn<AdvancedConfigEntry> column, MouseEvent mouseEvent) {
+        JPopupMenu p = new JPopupMenu();
+        p.add(new AppAction() {
+            {
+                setSmallIcon(NewTheme.I().getIcon("reset", 20));
+                setName(_GUI._.AdvancedTable_onContextMenu_reset_selection());
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    NewUIO.I().showConfirmDialog(0, _GUI._.lit_are_you_sure(), _GUI._.AdvancedTablecontextmenu_reset(selection.size()));
+                    for (AdvancedConfigEntry ce : selection) {
+                        ce.setValue(ce.getDefault());
+                    }
+                    repaint();
+                } catch (DialogClosedException e1) {
+                    e1.printStackTrace();
+                } catch (DialogCanceledException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        p.add(new AppAction() {
+            {
+                setSmallIcon(NewTheme.I().getIcon("reset", 20));
+                setName(_GUI._.AdvancedTable_onContextMenu_reset_all());
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    NewUIO.I().showConfirmDialog(0, _GUI._.lit_are_you_sure(), _GUI._.AdvancedTablecontextmenu_reset(selection.size()));
+                    for (AdvancedConfigEntry ce : selection) {
+                        ce.setValue(ce.getDefault());
+                    }
+                    repaint();
+                } catch (DialogClosedException e1) {
+                    e1.printStackTrace();
+                } catch (DialogCanceledException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        return p;
     }
 
     @Override
