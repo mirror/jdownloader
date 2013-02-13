beb116f32096f8ce7a4957b96d6ca39d908d721d
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-13 09:04:14 +0000

    build>full fixes #2806 Added the do not show again option to the reconnect action
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19917 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/swing/jdgui/components/toolbar/actions/ReconnectAction.java
+++ src/jd/gui/swing/jdgui/components/toolbar/actions/ReconnectAction.java
@@ -3,11 +3,14 @@ package jd.gui.swing.jdgui.components.toolbar.actions;
 import java.awt.event.ActionEvent;
 
 import jd.controlling.reconnect.Reconnecter;
-import jd.gui.UserIO;
-import jd.nutils.JDFlags;
 
+import org.appwork.utils.swing.dialog.Dialog;
+import org.appwork.utils.swing.dialog.DialogCanceledException;
+import org.appwork.utils.swing.dialog.DialogClosedException;
 import org.jdownloader.gui.shortcuts.ShortcutController;
 import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.gui.userio.NewUIO;
+import org.jdownloader.images.NewTheme;
 
 public class ReconnectAction extends AbstractToolbarAction {
     private static final ReconnectAction INSTANCE = new ReconnectAction();
@@ -30,14 +33,19 @@ public class ReconnectAction extends AbstractToolbarAction {
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (JDFlags.hasSomeFlags(UserIO.getInstance().requestConfirmDialog(0, _GUI._.gui_reconnect_confirm()), UserIO.RETURN_OK, UserIO.RETURN_DONT_SHOW_AGAIN)) {
-            /* forceReconnect is running in its own thread */
+        try {
+            NewUIO.I().showConfirmDialog(Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI._.lit_are_you_sure(), _GUI._.gui_reconnect_confirm(), NewTheme.I().getIcon("reconnect", 32), _GUI._.lit_yes(), _GUI._.lit_no());
             new Thread(new Runnable() {
                 public void run() {
                     Reconnecter.getInstance().forceReconnect();
                 }
             }).start();
+        } catch (DialogClosedException e1) {
+            e1.printStackTrace();
+        } catch (DialogCanceledException e1) {
+            e1.printStackTrace();
         }
+
     }
 
     @Override
