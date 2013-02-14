0b0c9379ce7956d3e77e3b69587bcb30fea409e2
nvv13 <nvv13@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-14 11:58:18 +0000

    fixed
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@19943 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/DatacodRu.java
+++ src/jd/plugins/hoster/DatacodRu.java
@@ -31,65 +31,87 @@ import org.appwork.utils.formatter.SizeFormatter;
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "data.cod.ru" }, urls = { "http://[a-zA-Z.]{0,}data.cod.ru/[0-9]{1,}" }, flags = { 0 })
 public class DatacodRu extends PluginForHost {
 
-    public DatacodRu(PluginWrapper wrapper) {
-        super(wrapper);
-    }
+	public DatacodRu(PluginWrapper wrapper) {
+		super(wrapper);
+	}
 
-    public String getAGBLink() {
-        return "http://data.cod.ru/terms/";
-    }
+	public String getAGBLink() {
+		return "http://data.cod.ru/terms/";
+	}
 
-    @Override
-    public int getMaxSimultanFreeDownloadNum() {
-        return 1;
-    }
+	@Override
+	public int getMaxSimultanFreeDownloadNum() {
+		return 1;
+	}
 
-    public void handleFree(DownloadLink downloadLink) throws Exception {
-        requestFileInformation(downloadLink);
-        final String dlLink = br.getRegex(Pattern.compile("<a href=\"(http://files.*?)\" class=\"button\">Скачать файл</a>")).getMatch(0);
-        if (dlLink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dlLink, true, 1);
-        if (dl.getConnection().getContentType().contains("html")) {
-            logger.warning("the dllink doesn't seem to be a file, following the connection...");
-            br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
-    }
+	public void handleFree(DownloadLink downloadLink) throws Exception {
+		requestFileInformation(downloadLink);
+		String dlLink = br
+				.getRegex(
+						Pattern.compile("<a href=\"(http://files.*?)\" class=\"button\">Скачать файл</a>"))
+				.getMatch(0);
+		if (dlLink == null)
+			dlLink = br
+					.getRegex(
+							Pattern.compile("<a href=\"(http://out.*?)\" class=\"button\">Скачать файл</a>"))
+					.getMatch(0);
+		if (dlLink == null)
+			throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+		dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dlLink,
+				true, 1);
+		if (dl.getConnection().getContentType().contains("html")) {
+			logger.warning("the dllink doesn't seem to be a file, following the connection...");
+			br.followConnection();
+			throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+		}
+		dl.startDownload();
+	}
 
-    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, InterruptedException, PluginException {
-        this.setBrowserExclusive();
-        br.setCustomCharset("UTF-8");
-        br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.0; chrome://global/locale/intl.properties; rv:1.8.1.12) Gecko/2008102920  Firefox/3.0.0 YB/4.2.0");
-        br.getPage(downloadLink.getDownloadURL());
-        if (br.containsHTML("<title>404</title>") || br.containsHTML("Файл не найден") || br.containsHTML("Закончился срок хранения файла\\.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        if (br.containsHTML("Доступ на сервер разрешён только для пользователей Авангарда")) throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE);
-        String name = br.getRegex(Pattern.compile("файл: <b title=\"(.*?)\">")).getMatch(0);
-        if (name == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        String fileSize = br.getRegex(Pattern.compile("размер: <b>(.*?)</b></li>")).getMatch(0);
-        if (fileSize == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        // 15.58 МБ
-        fileSize = fileSize.replaceAll("Г", "G");
-        fileSize = fileSize.replaceAll("М", "M");
-        fileSize = fileSize.replaceAll("к", "k");
-        fileSize = fileSize.replaceAll("К", "k");
-        fileSize = fileSize.replaceAll("Б", "");
-        fileSize = fileSize + "b";
-        downloadLink.setName(name.trim());
-        downloadLink.setDownloadSize(SizeFormatter.getSize(fileSize));
-        return AvailableStatus.TRUE;
-    }
+	public AvailableStatus requestFileInformation(DownloadLink downloadLink)
+			throws IOException, InterruptedException, PluginException {
+		this.setBrowserExclusive();
+		br.setCustomCharset("UTF-8");
+		br.getHeaders()
+				.put("User-Agent",
+						"Mozilla/5.0 (Windows; U; Windows NT 6.0; chrome://global/locale/intl.properties; rv:1.8.1.12) Gecko/2008102920  Firefox/3.0.0 YB/4.2.0");
+		br.getPage(downloadLink.getDownloadURL());
+		if (br.containsHTML("<title>404</title>")
+				|| br.containsHTML("Файл не найден")
+				|| br.containsHTML("Закончился срок хранения файла\\."))
+			throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+		if (br.containsHTML("Доступ на сервер разрешён только для пользователей Авангарда"))
+			throw new PluginException(
+					LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE);
+		String name = br.getRegex(Pattern.compile("файл: <b title=\"(.*?)\">"))
+				.getMatch(0);
+		if (name == null)
+			throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+		String fileSize = br.getRegex(
+				Pattern.compile("размер: <b>(.*?)</b></li>")).getMatch(0);
+		if (fileSize == null)
+			throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+		// 15.58 МБ
+		fileSize = fileSize.replaceAll("Г", "G");
+		fileSize = fileSize.replaceAll("М", "M");
+		fileSize = fileSize.replaceAll("к", "k");
+		fileSize = fileSize.replaceAll("К", "k");
+		fileSize = fileSize.replaceAll("Б", "");
+		fileSize = fileSize + "b";
+		downloadLink.setName(name.trim());
+		downloadLink.setDownloadSize(SizeFormatter.getSize(fileSize));
+		return AvailableStatus.TRUE;
+	}
 
-    @Override
-    public void reset() {
-    }
+	@Override
+	public void reset() {
+	}
 
-    @Override
-    public void resetDownloadlink(DownloadLink link) {
-    }
+	@Override
+	public void resetDownloadlink(DownloadLink link) {
+	}
 
-    @Override
-    public void resetPluginGlobals() {
-    }
+	@Override
+	public void resetPluginGlobals() {
+	}
 
 }
