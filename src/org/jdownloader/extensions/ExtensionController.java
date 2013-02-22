6eeffbd14a4fe80c5398d0d288a7c4774c59a288
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-22 12:11:07 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20027 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/ExtensionController.java
+++ src/org/jdownloader/extensions/ExtensionController.java
@@ -52,8 +52,7 @@ public class ExtensionController {
     private LogSource                      logger;
 
     /**
-     * Create a new instance of ExtensionController. This is a singleton class. Access the only existing instance by using
-     * {@link #getInstance()}.
+     * Create a new instance of ExtensionController. This is a singleton class. Access the only existing instance by using {@link #getInstance()}.
      */
     private ExtensionController() {
         eventSender = new ExtensionControllerEventSender();
@@ -175,7 +174,6 @@ public class ExtensionController {
             if (Application.isJared(ExtensionController.class)) {
                 ret = loadJared();
             } else {
-                ret = loadJared();
                 ret = loadUnpacked();
             }
             JSonStorage.saveTo(getCache(), ret);
