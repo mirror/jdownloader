b23049389f8ed36314c67521b7fe11225cca24b4
bismarck <bismarck@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-02-23 14:29:09 +0000

    added rtmp support closes #7672
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20038 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/SloozieCom.java
+++ src/jd/plugins/hoster/SloozieCom.java
@@ -28,6 +28,7 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.plugins.download.DownloadInterface;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "sloozie.com" }, urls = { "http://(www\\.)?sloozie\\.com/(galleries/[a-z0-9]+|videos/[a-z0-9]+/.{1})" }, flags = { 0 })
 public class SloozieCom extends PluginForHost {
@@ -52,6 +53,7 @@ public class SloozieCom extends PluginForHost {
         br.getPage(downloadLink.getDownloadURL());
         if (br.getURL().equals("http://www.sloozie.com/")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         String filename = null;
+        final Browser br2 = br.cloneBrowser();
         if (downloadLink.getDownloadURL().matches(PICLINK)) {
             filename = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
             if (filename == null) filename = br.getRegex("").getMatch(0);
@@ -62,7 +64,6 @@ public class SloozieCom extends PluginForHost {
             String ext = DLLINK.substring(DLLINK.lastIndexOf("."));
             if (ext == null || ext.length() > 5) ext = ".jpg";
             filename = Encoding.htmlDecode(filename) + ext;
-            final Browser br2 = br.cloneBrowser();
             // In case the link redirects to the finallink
             br2.setFollowRedirects(true);
             URLConnectionAdapter con = null;
@@ -79,7 +80,12 @@ public class SloozieCom extends PluginForHost {
             filename = br.getRegex("id=\"txtVDCaption\">([^<>\"]*?)\\-\\-</div>").getMatch(0);
             if (filename == null) filename = br.getRegex("<title>Sloozie\\&#039;s homemade videos \\-([^<>\"]*?)</title>").getMatch(0);
             if (filename == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            filename = Encoding.htmlDecode(filename) + ".flv";
+            filename = Encoding.htmlDecode(filename.trim()) + ".mp4";
+            String flashParameter[] = br.getRegex("V4CORE\\.put\\(\'d\',\\{\"m\":\"\\d+\",\"p\":\"\\d+\",\"v\":\"([^\"]+)\",\"q\":\"([a-z]+)\",\"fs\"").getRow(0);
+            String jsCache = br.getRegex("src=\"(/js/cache/[0-9a-f]+\\.js)\">").getMatch(0);
+            if (flashParameter == null || jsCache == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            DLLINK = "rtmp://video.sloozie.com/vod@mp4:sloozie/" + flashParameter[1] + "/" + flashParameter[0].replace("\\", "");
+            // br2.getPage(jsCache);
         }
         downloadLink.setFinalFileName(filename);
         return AvailableStatus.TRUE;
@@ -88,13 +94,33 @@ public class SloozieCom extends PluginForHost {
     @Override
     public void handleFree(final DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
-        if (!downloadLink.getDownloadURL().matches(PICLINK)) throw new PluginException(LinkStatus.ERROR_FATAL, "Sloozie videos aren't downloadable yet, plugin is not yet 100% finished!");
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
-        if (dl.getConnection().getContentType().contains("html")) {
-            br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        download(downloadLink);
+    }
+
+    private void setupRTMPConnection(String[] stream, DownloadInterface dl) {
+        jd.network.rtmp.url.RtmpUrlConnection rtmp = ((RTMPDownload) dl).getRtmpConnection();
+        rtmp.setPlayPath(stream[1]);
+        rtmp.setUrl(stream[0]);
+        rtmp.setResume(true);
+        rtmp.setSwfVfy("http://www.sloozie.com/swf/flowplayer.rtmp-3.2.3.swf");
+        // rtmp.setRealTime();
+    }
+
+    private void download(final DownloadLink downloadLink) throws Exception {
+        String stream[] = DLLINK.split("@");
+        if (DLLINK.startsWith("rtmp")) {
+            // downloadLink.setProperty("FLVFIXER", true);
+            dl = new RTMPDownload(this, downloadLink, stream[0]);
+            setupRTMPConnection(stream, dl);
+            ((RTMPDownload) dl).startDownload();
+        } else {
+            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
+            if (dl.getConnection().getContentType().contains("html")) {
+                br.followConnection();
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dl.startDownload();
         }
-        dl.startDownload();
     }
 
     @Override
