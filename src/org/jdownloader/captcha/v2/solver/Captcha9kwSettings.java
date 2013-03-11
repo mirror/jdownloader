6eea4d031a6cb6cc10c22e1f663d3010f8967545
stefanos <stefanos@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-03-11 10:57:47 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20192 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/captcha/v2/solver/Captcha9kwSettings.java
@@ -0,0 +1,21 @@
+package org.jdownloader.captcha.v2.solver;
+
+import org.appwork.storage.config.ConfigInterface;
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+
+public interface Captcha9kwSettings extends ConfigInterface {
+    @AboutConfig
+    @DescriptionForConfigEntry("Your (User) ApiKey from 9kw.eu")
+    String getApiKey();
+
+    void setApiKey(String jser);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("Active the 9kw.eu service")
+    boolean isEnabled();
+
+    void setEnabled(boolean b);
+}
