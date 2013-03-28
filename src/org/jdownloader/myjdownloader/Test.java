dcf549adec5ac79cc987de8e9b9d44885b493bb9
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-03-28 14:45:17 +0000

    build>full MYJDownloader Client API
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20311 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/myjdownloader/Test.java
+++ src/org/jdownloader/myjdownloader/Test.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import jd.http.Browser;
 import jd.http.Browser.BrowserException;
@@ -12,7 +13,15 @@ import jd.http.URLConnectionAdapter;
 
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
+import org.appwork.storage.jackson.JacksonMapper;
 import org.appwork.utils.encoding.Base64;
+import org.appwork.utils.swing.dialog.Dialog;
+import org.appwork.utils.swing.dialog.DialogCanceledException;
+import org.appwork.utils.swing.dialog.DialogClosedException;
+import org.appwork.utils.swing.dialog.LoginDialog;
+import org.appwork.utils.swing.dialog.LoginDialog.LoginData;
+import org.jdownloader.api.captcha.CaptchaAPI;
+import org.jdownloader.api.captcha.CaptchaJob;
 import org.jdownloader.api.jd.JDAPI;
 import org.jdownloader.myjdownloader.client.AbstractMyJDClient;
 import org.jdownloader.myjdownloader.client.exceptions.APIException;
@@ -28,10 +37,13 @@ public class Test {
      * @throws InvalidKeyException
      * @throws APIException
      * @throws MyJDownloaderException
+     * @throws DialogCanceledException
+     * @throws DialogClosedException
      */
-    public static void main(String[] args) throws APIException, MyJDownloaderException {
+    public static void main(String[] args) throws APIException, MyJDownloaderException, DialogClosedException, DialogCanceledException {
         final Browser br = new Browser();
         br.setAllowedResponseCodes(200);
+        JSonStorage.setMapper(new JacksonMapper());
         // br.forceDebug(true);
         // Log.L.setLevel(Level.ALL);
         AbstractMyJDClient api = new AbstractMyJDClient() {
@@ -40,7 +52,7 @@ public class Test {
             protected String post(String query, String object) throws MyJDownloaderException {
                 try {
 
-                    String ret = br.postPage(getServerRoot() + query, object == null ? "" : object);
+                    String ret = br.postPageRaw(getServerRoot() + query, object == null ? "" : object);
 
                     URLConnectionAdapter con = br.getRequest().getHttpConnection();
 
@@ -93,15 +105,15 @@ public class Test {
             }
 
         };
-
-        api.connect("", "");
-        JDAPI jdapi = api.link(JDAPI.class, "jd");
-        System.out.println(jdapi.uptime());
+        LoginData li = Dialog.getInstance().showDialog(new LoginDialog(0));
+        api.connect(li.getUsername(), li.getPassword());
+        JDAPI jda;
 
         // List<FilePackageAPIStorable> ret = api.link(DownloadsAPI.class, "downloads").queryPackages(new APIQuery());
-        // // api.link(DownloadsAPI.class, "downloads").start();
+        List<CaptchaJob> list = api.link(CaptchaAPI.class, "captcha").list();
+        System.out.println(list);
         // System.out.println(ret);
         api.disconnect();
-        System.out.println(jdapi.uptime());
+        // System.out.println(jdapi.uptime());
     }
 }
