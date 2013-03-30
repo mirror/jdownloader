0537728475a3ea9ebb1399045c42de86e522d48a
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-03-30 17:42:39 +0000

    Fixed && Added: docs.google.com, hoster plugin && folder support. - refs #7283 && http://board.jdownloader.org/showthread.php?t=43382
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20332 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/DocsGoogleCom.java
@@ -0,0 +1,112 @@
+//  jDownloader - Downloadmanager
+//  Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+import jd.utils.JDUtilities;
+
+@DecrypterPlugin(revision = "$Revision: 20105 $", interfaceVersion = 2, names = { "dropbox.com" }, urls = { "https?://(www\\.)?docs\\.google\\.com/folder/d/[a-zA-Z0-9\\-_]+" }, flags = { 0 })
+public class DocsGoogleCom extends PluginForDecrypt {
+
+    /**
+     * @author raztoki
+     * */
+    public DocsGoogleCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    // DEV NOTES
+    // https://docs.google.com/folder/d/0B4lNqBSBfg_dbEdISXAyNlBpLUk/edit?pli=1 :: folder view of dir and files, can't seem to view dir unless edit present.
+    // https://docs.google.com/folder/d/0B4lNqBSBfg_dOEVERmQzcU9LaWc/edit?pli=1&docId=0B4lNqBSBfg_deEpXNjJrZy1MSGM :: above sub dir of docs they don't provide data constistantly.
+    // - with /edit?pli=1 they provide via javascript section partly escaped
+    // - with /list?rm=whitebox&hl=en_GB&forcehl=1&pref=2&pli=1"; - not used and commented out, supported except for scanLinks
+    // language determined by the accept-language
+    // user-agent required to use new ones otherwise blocks with javascript notice.
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+
+        JDUtilities.getPluginForDecrypt("docs.google.com");
+        jd.plugins.hoster.DocsGoogleCom.prepBrowser(br);
+
+        br.getPage(parameter + "/edit?pli=1");
+        // Browser br2 = br.cloneBrowser();
+
+        if (br.containsHTML("<p class=\"errorMessage\" style=\"padding\\-top: 50px\">Sorry, the file you have requested does not exist\\.</p>")) {
+            logger.info("Link is offline or Invalid URL been provided " + parameter);
+            return decryptedLinks;
+        }
+
+        String fpName = br.getRegex("\"title\":\"([^\"]+)\",\"urlPrefix\"").getMatch(0);
+        if (fpName == null) fpName = br.getRegex("").getMatch(0);
+
+        String[] results = br.getRegex("(\\{\"description\":[^\\}]+),").getColumn(0);
+        // if (results == null || results.length == 0) {
+        // br2.getPage(parameter + "/list?rm=whitebox&hl=en_GB&forcehl=1&pref=2&pli=1");
+        // results = br2.getRegex("(<td class=\"list\\-entry\\-title\".+</a></td></tr>)").getColumn(0);
+        // }
+        if (results != null && results.length != 0) {
+            for (String result : results) {
+                String link = new Regex(result, "\"openURL\":\"(http.+(\\\\/|/)folder(\\\\/|/)d(\\\\/|/)[^\"]+)").getMatch(0);
+                // if (link == null) link = new Regex(result, "(.+)").getMatch(0);
+                if (link != null) {
+                    // return folder links back into the plugin again.
+                    decryptedLinks.add(createDownloadlink(link.replaceAll("\\\\/", "/")));
+                } else {
+                    // folder could just contain /file/ links
+                    scanLinks(result, decryptedLinks);
+                }
+            }
+        }
+        if (decryptedLinks.size() == 0) {
+            logger.info("Found nothing to download: " + parameter);
+            return decryptedLinks;
+        }
+        if (fpName != null) {
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+
+    private void scanLinks(String result, ArrayList<DownloadLink> ret) {
+        if (result != null) {
+            String link = new Regex(result, "\"openURL\":\"(http.+(\\\\/|/)file(\\\\/|/)d(\\\\/|/)[^\"]+)").getMatch(0);
+            String filename = new Regex(result, "\"name\":\"([^\"]+)").getMatch(0);
+            if (filename == null) filename = new Regex(result, ">(.*?)</a>").getMatch(0);
+            if (link != null && filename != null) {
+                DownloadLink dl = createDownloadlink(link.replaceAll("\\\\/", "/"));
+                dl.setName(filename);
+                dl.setAvailable(true);
+                ret.add(dl);
+            }
+        }
+    }
+
+}
--- src/jd/plugins/hoster/DocsGoogleCom.java
+++ src/jd/plugins/hoster/DocsGoogleCom.java
@@ -1,17 +1,36 @@
+//  jDownloader - Downloadmanager
+//  Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package jd.plugins.hoster;
 
 import jd.PluginWrapper;
+import jd.http.Browser;
 import jd.nutils.encoding.HTMLEntities;
+import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
+import jd.utils.JDUtilities;
 
 import org.appwork.utils.formatter.SizeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "docs.google.com" }, urls = { "https?://(www\\.)?docs.google.com/(leaf\\?|uc\\?export=download.*?&)id=[A-Za-z0-9\\-_]+(&.*?&pid=[A-Za-z0-9\\-_]++)?" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "docs.google.com" }, urls = { "https?://(www\\.)?docs\\.google\\.com/((leaf|open|uc)\\?(.+)?id=[A-Za-z0-9\\-_]+|file/d/[A-Za-z0-9\\-_]+)(.+)?" }, flags = { 0 })
 public class DocsGoogleCom extends PluginForHost {
 
     public DocsGoogleCom(PluginWrapper wrapper) {
@@ -20,7 +39,7 @@ public class DocsGoogleCom extends PluginForHost {
 
     @Override
     public String getAGBLink() {
-        return "http://www.google.com/google-d-s/intl/en/terms.html";
+        return "https://support.google.com/drive/bin/answer.py?hl=en_GB&answer=2450387";
     }
 
     @Override
@@ -28,43 +47,78 @@ public class DocsGoogleCom extends PluginForHost {
         return -1;
     }
 
+    public void correctDownloadLink(DownloadLink link) throws PluginException {
+        String id = getID(link);
+        if (id == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        } else {
+            link.setUrlDownload("https://docs.google.com/file/d/" + id);
+        }
+    }
+
+    private String getID(DownloadLink downloadLink) {
+        // known url formats
+        // https://docs.google.com/file/d/0B4AYQ5odYn-pVnJ0Z2V4d1E5UWc/preview?pli=1
+        // can't dl these particular links, same with document/doc, presentation/present and view
+        // https://docs.google.com/uc?id=0B4AYQ5odYn-pVnJ0Z2V4d1E5UWc&export=download
+        // https://docs.google.com/leaf?id=0B_QJaGmmPrqeZjJkZDFmYzEtMTYzMS00N2Y2LWI2NDUtMjQ1ZjhlZDhmYmY3
+        // https://docs.google.com/open?id=0B9Z2XD2XD2iQNmxzWjd1UTdDdnc
+
+        if (downloadLink == null) return null;
+        String id = new Regex(downloadLink.getDownloadURL(), "/file/d/([a-zA-Z0-9\\-_]+)").getMatch(0);
+        if (id == null) id = new Regex(downloadLink.getDownloadURL(), "(?!rev)id=([a-zA-Z0-9\\-_]+)").getMatch(0);
+        return id;
+    }
+
+    public static String agent = null;
+
+    public static Browser prepBrowser(Browser pbr) {
+        // used within the decrypter also, leave public
+        // language determined by the accept-language
+        // user-agent required to use new ones otherwise blocks with javascript notice.
+
+        if (pbr == null) pbr = new Browser();
+        if (agent == null) {
+            /* we first have to load the plugin, before we can reference it */
+            JDUtilities.getPluginForHost("mediafire.com");
+            agent = jd.plugins.hoster.MediafireCom.stringUserAgent();
+        }
+        pbr.getHeaders().put("User-Agent", agent);
+        pbr.getHeaders().put("Accept-Language", "en-gb, en;q=0.9");
+        pbr.setCustomCharset("utf-8");
+        pbr.setFollowRedirects(true);
+        return pbr;
+    }
+
     @Override
     public AvailableStatus requestFileInformation(DownloadLink link) throws Exception {
-        String url = link.getDownloadURL();
-        url = url.replaceAll("uc\\?export=download.*?&", "leaf?");
         this.setBrowserExclusive();
-        br.setCustomCharset("utf-8");
-        br.setFollowRedirects(true);
-        br.getPage(url);
-
-        String size = br.getRegex("<div role=\"button\"[^>]*>Download(?:&nbsp;|\\s)+\\(([0-9A-Za-z\\., ]+)\\)</div>").getMatch(0);
-        if (size == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-
-        String filename = br.getRegex("<title>(.*?) - Google Docs</title>").getMatch(0);
+        prepBrowser(br);
+        br.getPage("https://docs.google.com/leaf?id=" + getID(link));
+        if (br.containsHTML("<p class=\"error\\-caption\">Sorry, we are unable to retrieve this document\\.</p>")) { throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND); }
+        String filename = br.getRegex("\"filename\":\"([^\"]+)\",").getMatch(0);
+        String size = br.getRegex("\"sizeInBytes\":(\\d+),").getMatch(0);
         if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
 
-        link.setName(HTMLEntities.unhtmlentities(filename.trim()));
-        link.setDownloadSize(SizeFormatter.getSize(size.replace(",", ".")));
+        link.setName(filename.trim());
+        if (size != null) link.setDownloadSize(SizeFormatter.getSize(size));
         return AvailableStatus.TRUE;
     }
 
     @Override
     public void handleFree(DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
-
-        String url = downloadLink.getDownloadURL();
-        url = url.replaceAll("/leaf\\?", "/uc?export=download&");
         br.setFollowRedirects(false);
-        br.getPage(url);
+        br.getPage("https://docs.google.com/uc?id=" + getID(downloadLink) + "&export=download");
+        if (br.containsHTML("<p class=\"error\\-subcaption\">Too many users have viewed or downloaded this file recently\\. Please try accessing the file again later\\.")) {
+            // so its not possible to download at this time.
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Download not possible at this point in time.", 60 * 60 * 1000);
+        }
         String dllink = br.getRedirectLocation();
-
         if (dllink == null) {
-            // Tries to find download anyway link
-            if (br.getURL().contains("/uc?")) dllink = br.getRegex("<a href=\"([^\"]+)\">Download anyway</a>").getMatch(0);
+            dllink = br.getRegex("<a href=\"([^\"]+)\">Download anyway</a>").getMatch(0);
             if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-
-            // If it does, adds host, since links starts by /uc and repeats
-            br.getPage("https://docs.google.com" + HTMLEntities.unhtmlentities(dllink));
+            br.getPage(HTMLEntities.unhtmlentities(dllink));
             dllink = br.getRedirectLocation();
             if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
