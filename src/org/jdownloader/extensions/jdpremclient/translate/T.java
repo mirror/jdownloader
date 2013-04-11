0ecf36b943082d2d2ab924d8016e1dce150b05af
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-11 10:21:02 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20464 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/jdpremclient/translate/T.java
+++
@@ -1,5 +0,0 @@
-package org.jdownloader.extensions.jdpremclient.translate;
-import org.appwork.txtresource.TranslationFactory;
-public class T {
-public static final JdpremclientTranslation _ = TranslationFactory.create(JdpremclientTranslation.class);
-}
