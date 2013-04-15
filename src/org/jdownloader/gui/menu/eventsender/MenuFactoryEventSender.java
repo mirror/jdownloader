f4a88e50d05c01d649f738ae7252884a86e533b1
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-15 08:56:54 +0000

    MenuFactoryEventSender: catch errors, show what's possible
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20502 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/menu/eventsender/MenuFactoryEventSender.java
+++ src/org/jdownloader/gui/menu/eventsender/MenuFactoryEventSender.java
@@ -1,13 +1,13 @@
 package org.jdownloader.gui.menu.eventsender;
 
 import org.appwork.utils.event.Eventsender;
+import org.jdownloader.logging.LogController;
 
 public class MenuFactoryEventSender extends Eventsender<MenuFactoryListener, MenuFactoryEvent> {
     private static final MenuFactoryEventSender INSTANCE = new MenuFactoryEventSender();
 
     /**
-     * get the only existing instance of
-     * DownloadTableContextmenuFactoryEventSender. This is a singleton
+     * get the only existing instance of DownloadTableContextmenuFactoryEventSender. This is a singleton
      * 
      * @return
      */
@@ -16,8 +16,7 @@ public class MenuFactoryEventSender extends Eventsender<MenuFactoryListener, Men
     }
 
     /**
-     * Create a new instance of DownloadTableContextmenuFactoryEventSender. This
-     * is a singleton class. Access the only existing instance by using
+     * Create a new instance of DownloadTableContextmenuFactoryEventSender. This is a singleton class. Access the only existing instance by using
      * {@link #getInstance()}.
      */
     private MenuFactoryEventSender() {
@@ -28,9 +27,11 @@ public class MenuFactoryEventSender extends Eventsender<MenuFactoryListener, Men
     protected void fireEvent(MenuFactoryListener listener, MenuFactoryEvent event) {
         switch (event.getType()) {
         case EXTEND:
-
-            listener.onExtendPopupMenu(event.getContext());
-
+            try {
+                listener.onExtendPopupMenu(event.getContext());
+            } catch (final Throwable e) {
+                LogController.CL().log(e);
+            }
             break;
         default:
             System.out.println("Unhandled Event: " + event);
