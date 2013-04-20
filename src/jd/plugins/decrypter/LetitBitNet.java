7aeb3e91f951ce487c7545ce2c47e59ea4e57be0
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-20 14:09:46 +0000

    Reviewed: letitbit.net, - re:irc request - added spanning folder support. - fixed url listening regex - implemented basic nginx response code 400 work around.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20568 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/LetitBitNet.java
+++ src/jd/plugins/decrypter/LetitBitNet.java
@@ -1,5 +1,5 @@
 //jDownloader - Downloadmanager
-//Copyright (C) 2009  JD-Team support@jdownloader.org
+//Copyright (C) 2013  JD-Team support@jdownloader.org
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
@@ -30,25 +31,69 @@ import jd.utils.locale.JDL;
 
 import org.appwork.utils.formatter.SizeFormatter;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "letitbit.net" }, urls = { "http://(www\\.)?letitbit\\.net/(folder/\\d+/\\d+|page/folder/\\d+\\|[^<>\"\\'/]+)" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "letitbit.net" }, urls = { "http://(www\\.)?letitbit\\.net/{1,2}(folder/\\d+/\\d+|page/folder/\\d+(\\||%7C)[^<>\"\\'/]+)" }, flags = { 0 })
 public class LetitBitNet extends PluginForDecrypt {
 
     public LetitBitNet(PluginWrapper wrapper) {
         super(wrapper);
     }
 
+    public Browser prepBr(Browser newBr) {
+        newBr.setCookie("http://letitbit.net/", "lang", "en");
+        return newBr;
+    }
+
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString().replace("www.", "");
-        br.setCookie("http://letitbit.net/", "lang", "en");
+        prepBr(br);
         br.getPage(parameter);
         if (!br.containsHTML("<td width=\"700\" colspan=\"2\">") || br.containsHTML("<h2>Owner: </h2>")) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
+        // only this link format contains spanning pages. pages/folder/ doesn't seem to!
+        String uid = new Regex(parameter, "(/folder/\\d+/\\d+)").getMatch(0);
+        // first page
+        parsePage(decryptedLinks);
+        // lets scan for additional pages
+        String[] pages = br.getRegex("<li><a href=\"(" + uid + "/\\d+)\">\\d+</a></li>").getColumn(0);
+        if (pages != null && pages.length != 0) {
+            for (String page : pages) {
+                try {
+                    br.getPage(page);
+                } catch (Exception e) {
+                    if (br.getRequest().getHttpConnection().getResponseCode() == 400) {
+                        // nginx config/setup == lame, detects or limit the amount of page requests via cookie session. Approx ~205
+                        Thread.sleep(2000);
+                        Browser br = new Browser();
+                        prepBr(br);
+                        br.getPage(this.br.getURL());
+                        this.br = br;
+                    } else {
+                        throw e;
+                    }
+                }
+                parsePage(decryptedLinks);
+            }
+        }
+
+        return decryptedLinks;
+    }
+
+    private void parsePage(ArrayList<DownloadLink> ret) {
         String[] linkInfo = br.getRegex("<td width=\"700\" colspan=\"2\">(.*?)<hr />").getColumn(0);
-        if (linkInfo == null || linkInfo.length == 0) return null;
+        if (linkInfo == null || linkInfo.length == 0) {
+            logger.warning("Could not find 'linkInfo' Regex, Please inform JDownloader Development Team!");
+            return;
+        }
         for (String singleInfo : linkInfo) {
             String downloadUrl = new Regex(singleInfo, "\"(http://letitbit\\.net/download/.*?)\"").getMatch(0);
-            if (downloadUrl == null) new Regex(singleInfo, "(http://letitbit\\.net/download/[a-z0-9\\.]+/.*?\\.html)<br").getMatch(0);
-            if (downloadUrl == null) return null;
+            if (downloadUrl == null) {
+                new Regex(singleInfo, "(http://letitbit\\.net/download/[a-z0-9\\.]+/.*?\\.html)<br").getMatch(0);
+                if (downloadUrl == null) {
+                    logger.warning("Could not find 'downloadUrl', Please inform JDownloader Development Team!");
+                    return;
+                }
+            }
+
             DownloadLink dl = createDownloadlink(downloadUrl);
             String filename = new Regex(singleInfo, "target=\"_blank\"><font size=\"4\">(.*?)</font>").getMatch(0);
             if (filename != null) dl.setFinalFileName(filename.trim());
@@ -59,10 +104,8 @@ public class LetitBitNet extends PluginForDecrypt {
             } else if (singleInfo.contains("<b>BAD</b>")) {
                 dl.setAvailable(false);
             }
-            decryptedLinks.add(dl);
+            ret.add(dl);
         }
-
-        return decryptedLinks;
     }
 
     /* NO OVERRIDE!! */
