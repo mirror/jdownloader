698384572e5c90fc50afc7bfb4f74129c953c3a9
stefanos <stefanos@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-24 05:32:48 +0000

    new captcha (9kw) options
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20614 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/solver/Captcha9kwSettings.java
+++ src/org/jdownloader/captcha/v2/solver/Captcha9kwSettings.java
@@ -3,7 +3,9 @@ package org.jdownloader.captcha.v2.solver;
 import org.appwork.storage.config.ConfigInterface;
 import org.appwork.storage.config.annotations.AboutConfig;
 import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DefaultIntValue;
 import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.appwork.storage.config.annotations.SpinnerValidator;
 
 public interface Captcha9kwSettings extends ConfigInterface {
     @AboutConfig
@@ -25,4 +27,38 @@ public interface Captcha9kwSettings extends ConfigInterface {
     boolean ismouse();
 
     void setmouse(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("Confirm option for captchas")
+    boolean isconfirm();
+
+    void setconfirm(boolean b);
+
+    @AboutConfig
+    @DefaultIntValue(0)
+    @SpinnerValidator(min = 0, max = 10)
+    @DescriptionForConfigEntry("More priority for captchas")
+    int getprio();
+
+    void setprio(int seconds);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("Only https requests to 9kw.eu")
+    boolean ishttps();
+
+    void sethttps(boolean b);
+
+    @AboutConfig
+    @DescriptionForConfigEntry("Captcha whitelist for hoster")
+    String getwhitelist();
+
+    void setwhitelist(String jser);
+
+    @AboutConfig
+    @DescriptionForConfigEntry("Captcha blacklist for hoster")
+    String getblacklist();
+
+    void setblacklist(String jser);
 }
