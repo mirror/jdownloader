e7c13c7052aa117bcb6b4fa5c73989c234307043
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-04-29 16:38:17 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20675 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/extraction/contextmenu/downloadlist/ArchiveValidator.java
+++ src/org/jdownloader/extensions/extraction/contextmenu/downloadlist/ArchiveValidator.java
@@ -1,5 +1,7 @@
 package org.jdownloader.extensions.extraction.contextmenu.downloadlist;
 
+import java.util.WeakHashMap;
+
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
@@ -9,12 +11,41 @@ import org.jdownloader.gui.views.SelectionInfo;
 
 public class ArchiveValidator {
 
-    public static ExtractionExtension EXTENSION;
+    public static ExtractionExtension                                EXTENSION;
+
+    private static WeakHashMap<SelectionInfo, ValidateArchiveAction> VALIDATIONCACHE = new WeakHashMap<SelectionInfo, ValidateArchiveAction>();
+    private static WeakHashMap<SelectionInfo, Object>                VALIDATIONLOCKS = new WeakHashMap<SelectionInfo, Object>();
 
     public static ValidateArchiveAction<FilePackage, DownloadLink> validate(SelectionInfo<FilePackage, DownloadLink> selection) {
-        final ValidateArchiveAction<FilePackage, DownloadLink> validation = new ValidateArchiveAction<FilePackage, DownloadLink>(EXTENSION, (SelectionInfo<FilePackage, DownloadLink>) selection);
+        Object lock = null;
+        ValidateArchiveAction validation = null;
+        synchronized (VALIDATIONCACHE) {
+            validation = VALIDATIONCACHE.get(selection);
+            if (validation != null) return validation;
+            synchronized (VALIDATIONLOCKS) {
+                lock = VALIDATIONLOCKS.get(selection);
+                if (lock == null) {
+                    lock = new Object();
+                }
+            }
+        }
+        synchronized (lock) {
+            try {
+                synchronized (VALIDATIONCACHE) {
+                    validation = VALIDATIONCACHE.get(selection);
+                    if (validation != null) return validation;
+                }
+                validation = new ValidateArchiveAction<FilePackage, DownloadLink>(EXTENSION, (SelectionInfo<FilePackage, DownloadLink>) selection);
+                synchronized (VALIDATIONCACHE) {
+                    VALIDATIONCACHE.put(selection, validation);
+                }
+                return validation;
+            } finally {
+                synchronized (VALIDATIONLOCKS) {
+                    VALIDATIONLOCKS.remove(selection);
+                }
+            }
+        }
 
-        return validation;
     }
-
 }
