50da18334f34c56ae6e75f043e0ec4890d547af7
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-05-03 18:36:16 +0000

    Added: imgur.com, https url listening support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@20736 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/ImageHosterDecrypter.java
+++ src/jd/plugins/decrypter/ImageHosterDecrypter.java
@@ -30,7 +30,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "imgjug.com", "instagram.com", "pic4free.org", "cocoimage.com", "imagetwist.com", "postimage.org", "imgur.com", "imgchili.com", "pimpandhost.com", "turboimagehost.com", "imagehyper.com", "imagebam.com", "photobucket.com", "freeimagehosting.net", "pixhost.org", "sharenxs.com", "9gag.com" }, urls = { "http://(www\\.)?imgjug\\.com/(i/[A-Za-z0-9]+|\\?v=[A-Za-z0-9]+\\.jpg)", "http://(www\\.)?(instagram\\.com|instagr\\.am)/p/[A-Za-z0-9]+", "http://(www\\.)?pic4free\\.org/\\?v=[^<>\"/]+", "http://(www\\.)?img\\d+\\.cocoimage\\.com/img\\.php\\?id=\\d+", "http://(www\\.)?imagetwist\\.com/[a-z0-9]{12}", "http://(www\\.)?postimage\\.org/image/[a-z0-9]+", "http://((www|i)\\.)?imgur\\.com(/gallery|/a|/download)?/(?!register|contact|removalrequest|stats|https)[A-Za-z0-9]{5,}",
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "imgjug.com", "instagram.com", "pic4free.org", "cocoimage.com", "imagetwist.com", "postimage.org", "imgur.com", "imgchili.com", "pimpandhost.com", "turboimagehost.com", "imagehyper.com", "imagebam.com", "photobucket.com", "freeimagehosting.net", "pixhost.org", "sharenxs.com", "9gag.com" }, urls = { "http://(www\\.)?imgjug\\.com/(i/[A-Za-z0-9]+|\\?v=[A-Za-z0-9]+\\.jpg)", "http://(www\\.)?(instagram\\.com|instagr\\.am)/p/[A-Za-z0-9]+", "http://(www\\.)?pic4free\\.org/\\?v=[^<>\"/]+", "http://(www\\.)?img\\d+\\.cocoimage\\.com/img\\.php\\?id=\\d+", "http://(www\\.)?imagetwist\\.com/[a-z0-9]{12}", "http://(www\\.)?postimage\\.org/image/[a-z0-9]+", "https?://((www|i)\\.)?imgur\\.com(/gallery|/a|/download)?/(?!register|contact|removalrequest|stats|https)[A-Za-z0-9]{5,}",
         "http://(www\\.)?imgchili\\.com/show/\\d+/[a-z0-9_\\.]+", "http://(www\\.)?pimpandhost\\.com/image/(show/id/\\d+|\\d+\\-(original|medium|small)\\.html)", "http://(www\\.)?turboimagehost\\.com/p/\\d+/.*?\\.html", "http://(www\\.)?(img\\d+|serve)\\.imagehyper\\.com/img\\.php\\?id=\\d+\\&c=[a-z0-9]+", "http://[\\w\\.]*?imagebam\\.com/(image|gallery)/[a-z0-9]+", "http://(www\\.)?(media\\.photobucket.com/image/.+\\..{3,4}\\?o=[0-9]+|gs\\d+\\.photobucket\\.com/groups/[A-Za-z0-9]+/[A-Za-z0-9]+/\\?action=view\\&current=[^<>\"/]+)", "http://[\\w\\.]*?freeimagehosting\\.net/image\\.php\\?.*?\\..{3,4}", "http://(www\\.)?pixhost\\.org/show/\\d+/.+", "http://(www\\.)?sharenxs\\.com/view/\\?id=[a-z0-9-]+", "https?://(www\\.)?9gag\\.com/gag/\\d+" }, flags = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
 public class ImageHosterDecrypter extends PluginForDecrypt {
 
@@ -170,12 +170,11 @@ public class ImageHosterDecrypter extends PluginForDecrypt {
             }
             finallink = br.getRegex("ref=\"nofollow\" target=\"_blank\">[\t\n\r ]+<img src=\"(http://[^<>\"]*?)\"").getMatch(0);
         } else if (parameter.contains("imgur.com/a/")) {
-            br.getPage(parameter.replace("/all$", ""));
-            if (br.containsHTML("<h1>File not found!</h1>|")) {
-                logger.info("Link offline OR no longer exists: " + parameter);
+            br.getPage(parameter.replace("https://", "http://").replace("/all$", ""));
+            if (br.containsHTML("<h1>File not found!</h1>|<h2>This album is empty!</h2>")) {
+                logger.info("Link offline OR empty album: " + parameter);
                 return decryptedLinks;
             }
-
             String fpName = br.getRegex("<title>(.*?) \\- Imgur").getMatch(0);
             if (fpName == null) {
                 fpName = br.getRegex("data-title=\"([^\"]+)").getMatch(0);
@@ -191,7 +190,6 @@ public class ImageHosterDecrypter extends PluginForDecrypt {
                 logger.warning("If plugin is out of date please report issue to JDownloader Developement : " + parameter);
                 return decryptedLinks;
             }
-
             if (imgUIDs != null && imgUIDs.length != 0) {
                 for (String imgUID : imgUIDs) {
                     DownloadLink dl = createDownloadlink("http://imgurdecrypted/download/" + imgUID);
@@ -205,7 +203,7 @@ public class ImageHosterDecrypter extends PluginForDecrypt {
                 fp.addLinks(decryptedLinks);
             }
             return decryptedLinks;
-        } else if (parameter.matches("http://(((www|i)\\.)?imgur\\.com/[A-Za-z0-9]{5,}|(www\\.)?imgur\\.com/(download|gallery)/[A-Za-z0-9]{5,})")) {
+        } else if (parameter.matches("https?://(((www|i)\\.)?imgur\\.com/[A-Za-z0-9]{5,}|(www\\.)?imgur\\.com/(download|gallery)/[A-Za-z0-9]{5,})")) {
             String imgUID = new Regex(parameter, "([A-Za-z0-9]{5,})$").getMatch(0);
             final DownloadLink dl = createDownloadlink("http://imgurdecrypted/download/" + imgUID);
             dl.setProperty("imgUID", imgUID);
