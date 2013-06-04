de62b07dd68eb6d19387fba28dc9cb9b357c092a
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-06-04 19:19:17 +0000

    Added: hdd.tom.sk, from rnb @ IRC. Reviewed and Improved. - re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@21049 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/HddTomskRu.java
@@ -0,0 +1,132 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.config.SubConfiguration;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForDecrypt;
+import jd.utils.JDUtilities;
+import jd.utils.locale.JDL;
+
+@DecrypterPlugin(revision = "$Revision: 20458 $", interfaceVersion = 2, names = { "hdd.tomsk.ru" }, urls = { "http://(www|download\\.)?hdd\\.tomsk\\.ru/desk/(?!notfound)[a-z]{8}" }, flags = { 0 })
+public class HddTomskRu extends PluginForDecrypt {
+
+    private static final String DESK_ENTER_PASSWORD = "http://hdd.tomsk.ru/?rm=desk_enter_password";
+    private static final String PWTEXT              = ">Для доступа к столу необходим пароль<";
+
+    /**
+     * @author rnw
+     * */
+    public HddTomskRu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+
+        final SubConfiguration hosterPluginConfig = JDUtilities.getPluginForHost("hdd.tomsk.ru").getPluginConfig();
+        String hddSid = hosterPluginConfig.getStringProperty(jd.plugins.hoster.HddTomskRu.HDDSID);
+        if (hddSid != null) br.setCookie(jd.plugins.hoster.HddTomskRu.DOMAIN, jd.plugins.hoster.HddTomskRu.HDDSID, hddSid);
+        br.getPage(parameter);
+
+        final String deskId = new Regex(parameter, "([a-z]{8})$").getMatch(0);
+        final String deskTitle = "hdd.tomsk.ru - Стол " + deskId;
+
+        String redirectLocation = br.getRedirectLocation();
+
+        /* No such desk */
+        if (redirectLocation != null && redirectLocation.equals(jd.plugins.hoster.HddTomskRu.NOTFOUND)) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Wrong URL or the package no longer exists."));
+
+        /* Password protected desk */
+        if (redirectLocation == null && br.containsHTML(PWTEXT)) {
+            for (int i = 0; i <= 3; i++) {
+                final String passCode = Plugin.getUserInput("Enter password for: " + deskTitle, param);
+                br.postPage(DESK_ENTER_PASSWORD, "password=" + Encoding.urlEncode(passCode) + "&signature=" + deskId);
+                if (br.getRedirectLocation() != null) br.getPage(br.getRedirectLocation());
+                if (br.containsHTML(PWTEXT)) continue;
+                break;
+            }
+            if (br.containsHTML(PWTEXT)) throw new DecrypterException(DecrypterException.PASSWORD);
+            redirectLocation = br.getRedirectLocation();
+        }
+
+        /* Accept TOS */
+        if (redirectLocation != null && redirectLocation.equals(jd.plugins.hoster.HddTomskRu.TERMS)) {
+            br.postPage(jd.plugins.hoster.HddTomskRu.TERMS_ACCEPT, "accept=1");
+            if (br.getRedirectLocation() != null) {
+                br.getPage(br.getRedirectLocation());
+                final String cookie = br.getCookie(jd.plugins.hoster.HddTomskRu.DOMAIN, jd.plugins.hoster.HddTomskRu.HDDSID);
+                if (cookie != null) {
+                    hosterPluginConfig.setProperty(jd.plugins.hoster.HddTomskRu.HDDSID, cookie);
+                    hosterPluginConfig.save();
+                }
+            }
+        }
+
+        final String[] results = br.getRegex("Window_DeskItem_File[^\n\r]+(\"file_mime_type\"[^\r\n]+)").getColumn(0);
+        if (results == null || results.length == 0) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
+        }
+        for (String result : results) {
+            result = result.replaceAll("\\\\/", "/");
+            String uid = getRegex("file_signature", result);
+            String filename = getRegex("file_original_name", result);
+            String filesize = getRegex("size_in_bytes", result);
+            String sha1 = getRegex("file_sha1", result);
+            if (uid == null) continue;
+            DownloadLink dl = createDownloadlink("http://hdd.tomsk.ru/file/" + uid);
+            if (filename != null) dl.setFinalFileName(filename);
+            if (filesize != null) dl.setDownloadSize(Integer.parseInt(filesize));
+            if (sha1 != null) dl.setSha1Hash(sha1);
+            if (filename != null) dl.setAvailable(true);
+            decryptedLinks.add(dl);
+        }
+        if (decryptedLinks.size() == 0) {
+            logger.warning("Possible plugin error, Please confirm in your web browser and report back to JDownloader Development Team if theirs a problem! : " + parameter);
+        }
+
+        FilePackage fp = FilePackage.getInstance();
+        fp.setName(deskTitle);
+        fp.addLinks(decryptedLinks);
+
+        return decryptedLinks;
+    }
+
+    private String getRegex(String id, String source) {
+        String reg = new Regex(source, "\"" + id + "\":\"?([^:\"]+)\"?,").getMatch(0);
+        return reg;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+}
---
+++ src/jd/plugins/hoster/HddTomskRu.java
@@ -0,0 +1,151 @@
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import jd.PluginWrapper;
+import jd.config.SubConfiguration;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision: 20794 $", interfaceVersion = 2, names = { "hdd.tomsk.ru" }, urls = { "http://(www|download\\.)?hdd\\.tomsk\\.ru/file/(?!notfound)[a-z]{8}" }, flags = { 0 })
+public class HddTomskRu extends PluginForHost {
+
+    public static final String  DOMAIN              = "http://hdd.tomsk.ru";
+    public static final String  HDDSID              = "HDDSID";
+    public static final String  NOTFOUND            = "http://hdd.tomsk.ru/file/notfound";
+    public static final String  TERMS               = "http://hdd.tomsk.ru/terms";
+    public static final String  TERMS_ACCEPT        = "http://hdd.tomsk.ru/?rm=terms_accept";
+
+    private static final String FILE_ENTER_PASSWORD = "http://hdd.tomsk.ru/?rm=file_enter_password";
+    private static final String PWTEXT              = ">Для доступа к файлу необходим пароль<";
+
+    private String              DLLINK              = null;
+
+    /**
+     * @author rnw
+     * */
+    public HddTomskRu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return DOMAIN + "/terms";
+    }
+
+    // do not add @Override here to keep 0.* compatibility
+    public boolean hasAutoCaptcha() {
+        return false;
+    }
+
+    /* NO OVERRIDE!! We need to stay 0.9*compatible */
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        if (acc == null) {
+            /* no account, yes we can expect captcha */
+            return false;
+        }
+        if (Boolean.TRUE.equals(acc.getBooleanProperty("free"))) {
+            /* free accounts also have captchas */
+            return false;
+        }
+        return false;
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws IOException, PluginException {
+        this.setBrowserExclusive();
+
+        final SubConfiguration pluginConfig = getPluginConfig();
+        String hddSid = pluginConfig.getStringProperty(HDDSID);
+        if (hddSid != null) br.setCookie(DOMAIN, HDDSID, hddSid);
+        br.getPage(downloadLink.getDownloadURL());
+
+        final String fileId = new Regex(downloadLink.getDownloadURL(), "([a-z]{8})$").getMatch(0);
+        final String fileTitle = "hdd.tomsk.ru - Файл " + fileId;
+
+        String redirectLocation = br.getRedirectLocation();
+
+        /* No such file */
+        if (redirectLocation != null && redirectLocation.equals(NOTFOUND)) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+
+        /* Password protected file */
+        if (redirectLocation == null && br.containsHTML(PWTEXT)) {
+            for (int i = 0; i <= 3; i++) {
+                final String passCode = Plugin.getUserInput("Enter password for: " + fileTitle, downloadLink);
+                br.postPage(FILE_ENTER_PASSWORD, "password=" + Encoding.urlEncode(passCode) + "&signature=" + fileId);
+                if (br.getRedirectLocation() != null) br.getPage(br.getRedirectLocation());
+                if (br.containsHTML(PWTEXT)) continue;
+                break;
+            }
+            if (br.containsHTML(PWTEXT)) throw new PluginException(LinkStatus.ERROR_RETRY, "Wrong password entered");
+            redirectLocation = br.getRedirectLocation();
+        }
+
+        /* Accept TOS */
+        if (redirectLocation != null && redirectLocation.equals(TERMS)) {
+            br.postPage(TERMS_ACCEPT, "accept=1");
+            if (br.getRedirectLocation() != null) {
+                br.getPage(br.getRedirectLocation());
+                String cookie = br.getCookie(DOMAIN, HDDSID);
+                if (cookie != null) {
+                    pluginConfig.setProperty(HDDSID, cookie);
+                    pluginConfig.save();
+                }
+            }
+        }
+
+        final Regex linkRegex = br.getRegex("<a href=\"(http://download\\.hdd\\.tomsk\\.ru/download/" + fileId + "\\?[a-f0-9]{32})\" title=[^>]+>(.*?)</a>");
+        DLLINK = linkRegex.getMatch(0);
+        if (DLLINK == null) {
+            DLLINK = br.getRegex("(http://download\\.hdd\\.tomsk\\.ru/download/" + fileId + "\\?[a-f0-9]{32})").getMatch(0);
+            if (DLLINK == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+
+        String finalFileName = linkRegex.getMatch(1);
+        if (finalFileName == null) finalFileName = br.getRegex("<title>hdd\\.tomsk\\.ru &mdash; (.*?)</title>").getMatch(0);
+
+        URLConnectionAdapter con = null;
+        try {
+            con = br.openGetConnection(DLLINK);
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+                if (finalFileName != null) downloadLink.setFinalFileName(finalFileName);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
