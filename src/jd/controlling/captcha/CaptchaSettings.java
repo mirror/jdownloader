8c43198e299f8c2471a0cae8e27e2f41bb964836
Enrique <Enrique@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-06-07 14:53:50 +0000

    Added avanced opntion: CaptchaDialogResolutorCaptchaTimeout
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@21084 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/captcha/CaptchaSettings.java
+++ src/jd/controlling/captcha/CaptchaSettings.java
@@ -10,82 +10,90 @@ import org.appwork.storage.config.annotations.SpinnerValidator;
 
 public interface CaptchaSettings extends ConfigInterface {
 
-    public static final CaptchaSettings CFG = JsonConfig.create(CaptchaSettings.class);
+	public static final CaptchaSettings CFG = JsonConfig
+			.create(CaptchaSettings.class);
 
-    @AboutConfig
-    @DefaultBooleanValue(true)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("True to enable a countdown in captcha dialogs. Dialog will close automated after the coundown")
-    boolean isDialogCountdownForDownloadsEnabled();
+	@AboutConfig
+	@DefaultBooleanValue(true)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("True to enable a countdown in captcha dialogs. Dialog will close automated after the coundown")
+	boolean isDialogCountdownForDownloadsEnabled();
 
-    void setDialogCountdownForDownloadsEnabled(boolean b);
+	void setDialogCountdownForDownloadsEnabled(boolean b);
 
-    @AboutConfig
-    @DefaultBooleanValue(false)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("True to enable a countdown in crawler captcha dialogs. Dialog will close automated after the coundown")
-    boolean isDialogCountdownForCrawlerEnabled();
+	@AboutConfig
+	@DefaultBooleanValue(false)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("True to enable a countdown in crawler captcha dialogs. Dialog will close automated after the coundown")
+	boolean isDialogCountdownForCrawlerEnabled();
 
-    void setDialogCountdownForCrawlerEnabled(boolean b);
+	void setDialogCountdownForCrawlerEnabled(boolean b);
 
-    @AboutConfig
-    @DefaultBooleanValue(true)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("If the Dialog Countdown is reached, the link will be skipped. Disable this option to retry instead")
-    boolean isSkipDownloadLinkOnCaptchaTimeoutEnabled();
+	@AboutConfig
+	@DefaultBooleanValue(true)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("If the Dialog Countdown is reached, the link will be skipped. Disable this option to retry instead")
+	boolean isSkipDownloadLinkOnCaptchaTimeoutEnabled();
 
-    void setSkipDownloadLinkOnCaptchaTimeoutEnabled(boolean b);
+	void setSkipDownloadLinkOnCaptchaTimeoutEnabled(boolean b);
 
-    @AboutConfig
-    @DefaultIntValue(20)
-    @SpinnerValidator(min = 0, max = Integer.MAX_VALUE)
-    @DescriptionForConfigEntry("Seconds to wait until captcha dialog gets answered. Close dialog after this timeout unanswered")
-    int getCountdown();
+	@AboutConfig
+	@DefaultIntValue(20)
+	@SpinnerValidator(min = 0, max = Integer.MAX_VALUE)
+	@DescriptionForConfigEntry("Seconds to wait until captcha dialog gets answered. Close dialog after this timeout unanswered")
+	int getCountdown();
 
-    void setCountdown(int seconds);
+	void setCountdown(int seconds);
 
-    // @DefaultIntValue(0)
-    // int getLastCancelOption();
-    //
-    // void setLastCancelOption(int i);
+	// @DefaultIntValue(0)
+	// int getLastCancelOption();
+	//
+	// void setLastCancelOption(int i);
 
-    @AboutConfig
-    @DefaultBooleanValue(true)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Disable, if you do not want JDownloader to autosolve as many captchas as possible")
-    boolean isAutoCaptchaRecognitionEnabled();
+	@AboutConfig
+	@DefaultBooleanValue(true)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Disable, if you do not want JDownloader to autosolve as many captchas as possible")
+	boolean isAutoCaptchaRecognitionEnabled();
 
-    void setAutoCaptchaRecognitionEnabled(boolean b);
+	void setAutoCaptchaRecognitionEnabled(boolean b);
 
-    @AboutConfig
-    @DefaultIntValue(95)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Do not Change me unless you know 100000% what this value is used for!")
-    int getAutoCaptchaPriorityThreshold();
+	@AboutConfig
+	@DefaultIntValue(95)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Do not Change me unless you know 100000% what this value is used for!")
+	int getAutoCaptchaPriorityThreshold();
 
-    void setAutoCaptchaPriorityThreshold(int value);
+	void setAutoCaptchaPriorityThreshold(int value);
 
-    @AboutConfig
-    @DefaultIntValue(90)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Do not Change me unless you know 100000% what this value is used for!")
-    int getDefaultJACTrustThreshold();
+	@AboutConfig
+	@DefaultIntValue(90)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Do not Change me unless you know 100000% what this value is used for!")
+	int getDefaultJACTrustThreshold();
 
-    void setDefaultJACTrustThreshold(int value);
+	void setDefaultJACTrustThreshold(int value);
 
-    @AboutConfig
-    @DefaultIntValue(15000)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto AntiCaptcha is still running")
-    int getCaptchaDialogJAntiCaptchaTimeout();
+	@AboutConfig
+	@DefaultIntValue(15000)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto AntiCaptcha is still running")
+	int getCaptchaDialogJAntiCaptchaTimeout();
 
-    void setCaptchaDialogJAntiCaptchaTimeout(int ms);
+	void setCaptchaDialogJAntiCaptchaTimeout(int ms);
 
-    @AboutConfig
-    @DefaultIntValue(0)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto 9kw Service is still running")
-    int getCaptchaDialog9kwTimeout();
+	@AboutConfig
+	@DefaultIntValue(0)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto 9kw Service is still running")
+	int getCaptchaDialog9kwTimeout();
 
-    void setCaptchaDialog9kwTimeout(int ms);
+	void setCaptchaDialog9kwTimeout(int ms);
 
-    @AboutConfig
-    @DefaultIntValue(0)
-    @org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto CaptchaBroptherhood Service is still running")
-    int getCaptchaDialogCaptchaBroptherhoodTimeout();
+	@AboutConfig
+	@DefaultIntValue(0)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto CaptchaBroptherhood Service is still running")
+	int getCaptchaDialogCaptchaBroptherhoodTimeout();
 
-    void setCaptchaDialogCaptchaBroptherhoodTimeout(int ms);
+	void setCaptchaDialogCaptchaBroptherhoodTimeout(int ms);
+
+	@AboutConfig
+	@DefaultIntValue(120000)
+	@org.appwork.storage.config.annotations.DescriptionForConfigEntry("Milliseconds after which a Captcha Dialog will appear even if Auto ResolutorCaptcha Service is still running")
+	int getCaptchaDialogResolutorCaptchaTimeout();
+
+	void setCaptchaDialogResolutorCaptchaTimeout(int ms);
 
 }
