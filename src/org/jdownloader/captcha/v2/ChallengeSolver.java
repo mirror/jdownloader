3a6c4ba89a947731e4dba2d789cbbed4d7322640
Enrique <Enrique@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-06-07 14:42:35 +0000

    Added the exception IOException to the method solve
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@21082 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/ChallengeSolver.java
+++ src/org/jdownloader/captcha/v2/ChallengeSolver.java
@@ -1,5 +1,6 @@
 package org.jdownloader.captcha.v2;
 
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashMap;
@@ -104,7 +105,7 @@ public abstract class ChallengeSolver<T> {
         threadPool.allowCoreThreadTimeOut(true);
     }
 
-    public abstract void solve(SolverJob<T> solverJob) throws InterruptedException, SolverException, SkipException;
+    public abstract void solve(SolverJob<T> solverJob) throws InterruptedException, SolverException, SkipException, IOException;
 
     public Class<T> getResultType() {
         return resultType;
