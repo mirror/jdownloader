4101a7b34bbea64f232656357d91b833781638bb
oliver <oliver@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-07-07 19:07:42 +0000

    added return value to set-method in config api
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@21634 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/config/AdvancedConfigManagerAPI.java
+++ src/org/jdownloader/api/config/AdvancedConfigManagerAPI.java
@@ -12,26 +12,29 @@ import org.jdownloader.settings.advanced.AdvancedConfigAPIEntry;
 @ApiNamespace("config")
 public interface AdvancedConfigManagerAPI extends RemoteAPIInterface {
 
-    @ApiDoc("list all available config entries")
-    public ArrayList<AdvancedConfigAPIEntry> list();
+	@ApiDoc("list all available config entries")
+	public ArrayList<AdvancedConfigAPIEntry> list();
 
-    @ApiDoc("list entries based on the pattern regex")
-    public ArrayList<AdvancedConfigAPIEntry> list(String pattern, boolean returnDescription, boolean returnValues, boolean returnDefaultValues);
+	@ApiDoc("list entries based on the pattern regex")
+	public ArrayList<AdvancedConfigAPIEntry> list(String pattern,
+			boolean returnDescription, boolean returnValues,
+			boolean returnDefaultValues);
 
-    @AllowStorage(value = { Object.class })
-    @ApiDoc("get value from interface by key")
-    public Object get(String interfaceName, String storage, String key);
+	@AllowStorage(value = { Object.class })
+	@ApiDoc("get value from interface by key")
+	public Object get(String interfaceName, String storage, String key);
 
-    @AllowStorage(value = { Object.class })
-    @AllowNonStorableObjects
-    @ApiDoc("set value to interface by key")
-    public void set(String interfaceName, String storage, String key, Object value) throws InvalidValueException;
+	@AllowStorage(value = { Object.class })
+	@AllowNonStorableObjects
+	@ApiDoc("set value to interface by key")
+	public boolean set(String interfaceName, String storage, String key,
+			Object value) throws InvalidValueException;
 
-    @ApiDoc("reset interface by key to its default value")
-    public boolean reset(String interfaceName, String storage, String key);
+	@ApiDoc("reset interface by key to its default value")
+	public boolean reset(String interfaceName, String storage, String key);
 
-    @AllowStorage(value = { Object.class })
-    @ApiDoc("get default value from interface by key")
-    public Object getDefault(String interfaceName, String storage, String key);
+	@AllowStorage(value = { Object.class })
+	@ApiDoc("get default value from interface by key")
+	public Object getDefault(String interfaceName, String storage, String key);
 
 }
--- src/org/jdownloader/api/config/AdvancedConfigManagerAPIImpl.java
+++ src/org/jdownloader/api/config/AdvancedConfigManagerAPIImpl.java
@@ -16,135 +16,157 @@ import org.jdownloader.settings.advanced.AdvancedConfigManager;
 
 public class AdvancedConfigManagerAPIImpl implements AdvancedConfigManagerAPI {
 
-    public ArrayList<AdvancedConfigAPIEntry> list(String pattern, boolean returnDescription, boolean returnValues, boolean returnDefaultValues) {
-        ArrayList<AdvancedConfigAPIEntry> ret = new ArrayList<AdvancedConfigAPIEntry>();
-        java.util.List<AdvancedConfigEntry> entries = AdvancedConfigManager.getInstance().list();
-        Pattern cPat = StringUtils.isEmpty(pattern) ? null : Pattern.compile(pattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-        for (AdvancedConfigEntry entry : entries) {
-            String key = entry.getKeyHandler().getStorageHandler().getConfigInterface().getName() + "." + entry.getKeyHandler().getKey();
-            if (cPat == null || cPat.matcher(key).matches()) {
-                ret.add(new AdvancedConfigAPIEntry(entry, returnDescription, returnValues, returnDefaultValues));
-            }
-        }
-        return ret;
-    }
+	public ArrayList<AdvancedConfigAPIEntry> list(String pattern,
+			boolean returnDescription, boolean returnValues,
+			boolean returnDefaultValues) {
+		ArrayList<AdvancedConfigAPIEntry> ret = new ArrayList<AdvancedConfigAPIEntry>();
+		java.util.List<AdvancedConfigEntry> entries = AdvancedConfigManager
+				.getInstance().list();
+		Pattern cPat = StringUtils.isEmpty(pattern) ? null : Pattern.compile(
+				pattern, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+		for (AdvancedConfigEntry entry : entries) {
+			String key = entry.getKeyHandler().getStorageHandler()
+					.getConfigInterface().getName()
+					+ "." + entry.getKeyHandler().getKey();
+			if (cPat == null || cPat.matcher(key).matches()) {
+				ret.add(new AdvancedConfigAPIEntry(entry, returnDescription,
+						returnValues, returnDefaultValues));
+			}
+		}
+		return ret;
+	}
 
-    public Object get(String interfaceName, String storage, String key) {
-        KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
-        return kh.getValue();
-    }
+	public Object get(String interfaceName, String storage, String key) {
+		KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
+		return kh.getValue();
+	}
 
-    public void set(String interfaceName, String storage, String key, Object value) throws InvalidValueException {
-        KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
-        Type rc = kh.getRawType();
-        String json = JSonStorage.toString(value);
-        TypeRef<Object> type = new TypeRef<Object>(rc) {
-        };
+	public boolean set(String interfaceName, String storage, String key,
+			Object value) throws InvalidValueException {
+		KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
+		Type rc = kh.getRawType();
+		String json = JSonStorage.toString(value);
+		TypeRef<Object> type = new TypeRef<Object>(rc) {
+		};
 
-        try {
-            Object v;
-            synchronized (JSonStorage.LOCK) {
-                v = JSonStorage.getMapper().stringToObject(json, type);
-            }
+		try {
+			Object v;
+			synchronized (JSonStorage.LOCK) {
+				v = JSonStorage.getMapper().stringToObject(json, type);
+			}
 
-            kh.setValue(v);
-        } catch (Exception e) {
-            throw new InvalidValueException(e);
-        }
+			kh.setValue(v);
+		} catch (Exception e) {
+			return false;
+			// throw new InvalidValueException(e);
+		}
+		return true;
 
-    }
+	}
 
-    public boolean reset(String interfaceName, String storage, String key) {
-        KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
-        try {
-            kh.setValue(kh.getDefaultValue());
-        } catch (ValidationException e) {
-            return false;
-        }
-        return true;
-    }
+	public boolean reset(String interfaceName, String storage, String key) {
+		KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
+		try {
+			kh.setValue(kh.getDefaultValue());
+		} catch (ValidationException e) {
+			return false;
+		}
+		return true;
+	}
 
-    public Object getDefault(String interfaceName, String storage, String key) {
-        KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
-        return kh.getDefaultValue();
-    }
+	public Object getDefault(String interfaceName, String storage, String key) {
+		KeyHandler<Object> kh = getKeyHandler(interfaceName, storage, key);
+		return kh.getDefaultValue();
+	}
 
-    private KeyHandler<Object> getKeyHandler(String interfaceName, String storage, String key) {
-        StorageHandler<?> storageHandler = StorageHandler.getStorageHandler(interfaceName, storage);
-        if (storageHandler == null) return null;
-        KeyHandler<Object> kh = storageHandler.getKeyHandler(key);
-        return kh;
-    }
+	private KeyHandler<Object> getKeyHandler(String interfaceName,
+			String storage, String key) {
+		StorageHandler<?> storageHandler = StorageHandler.getStorageHandler(
+				interfaceName, storage);
+		if (storageHandler == null)
+			return null;
+		KeyHandler<Object> kh = storageHandler.getKeyHandler(key);
+		return kh;
+	}
 
-    // @Override
-    // public List<ConfigInterfaceAPIStorable> queryConfigInterfaces(APIQuery query) {
-    // AdvancedConfigManager acm = AdvancedConfigManager.getInstance();
-    // List<ConfigInterfaceAPIStorable> result = new ArrayList<ConfigInterfaceAPIStorable>();
-    //
-    // for (AdvancedConfigEntry ace : acm.list()) {
-    // if (!result.contains(new ConfigInterfaceAPIStorable(ace))) {
-    // result.add(new ConfigInterfaceAPIStorable(ace));
-    // } else {
-    // ConfigInterfaceAPIStorable cis = result.get(result.indexOf(new ConfigInterfaceAPIStorable(ace)));
-    // cis.getInfoMap().put("settingsCount", (Integer) cis.getInfoMap().get("settingsCount") + 1);
-    // }
-    // }
-    // return result;
-    // }
-    //
-    // @SuppressWarnings("rawtypes")
-    // @Override
-    // public List<ConfigEntryAPIStorable> queryConfigSettings(APIQuery query) {
-    // AdvancedConfigManager acm = AdvancedConfigManager.getInstance();
-    // List<ConfigEntryAPIStorable> result = new ArrayList<ConfigEntryAPIStorable>();
-    //
-    // // retrieve packageUUIDs from queryParams
-    // List<String> interfaceKeys = new ArrayList<String>();
-    // if (!query._getQueryParam("interfaceKeys", List.class, new ArrayList()).isEmpty()) {
-    // List uuidsFromQuery = query._getQueryParam("interfaceKeys", List.class, new ArrayList());
-    // for (Object o : uuidsFromQuery) {
-    // try {
-    // interfaceKeys.add((String) o);
-    // } catch (ClassCastException e) {
-    // continue;
-    // }
-    // }
-    // }
-    //
-    // for (AdvancedConfigEntry ace : acm.list()) {
-    // ConfigEntryAPIStorable ces = new ConfigEntryAPIStorable(ace);
-    // // if only certain interfaces selected, skip if interface not included
-    // if (!interfaceKeys.isEmpty()) {
-    // if (!interfaceKeys.contains(ces.getInterfaceKey().substring(ces.getInterfaceKey().lastIndexOf(".") + 1,
-    // ces.getInterfaceKey().length()))) {
-    // continue;
-    // }
-    // }
-    // KeyHandler<?> kh = ((AdvancedConfigInterfaceEntry) ace).getKeyHandler();
-    //
-    // QueryResponseMap infoMap = new QueryResponseMap();
-    // if (query.fieldRequested("value")) {
-    // infoMap.put("value", kh.getValue());
-    // }
-    // if (query.fieldRequested("defaultValue")) {
-    // infoMap.put("defaultValue", kh.getDefaultValue());
-    // }
-    // if (query.fieldRequested("dataType")) {
-    // infoMap.put("dataType", kh.getRawClass().getName());
-    // }
-    // if (query.fieldRequested("description")) {
-    // infoMap.put("description", ace.getDescription());
-    // }
-    //
-    // ces.setInfoMap(infoMap);
-    // result.add(ces);
-    // }
-    // return result;
-    // }
+	// @Override
+	// public List<ConfigInterfaceAPIStorable> queryConfigInterfaces(APIQuery
+	// query) {
+	// AdvancedConfigManager acm = AdvancedConfigManager.getInstance();
+	// List<ConfigInterfaceAPIStorable> result = new
+	// ArrayList<ConfigInterfaceAPIStorable>();
+	//
+	// for (AdvancedConfigEntry ace : acm.list()) {
+	// if (!result.contains(new ConfigInterfaceAPIStorable(ace))) {
+	// result.add(new ConfigInterfaceAPIStorable(ace));
+	// } else {
+	// ConfigInterfaceAPIStorable cis = result.get(result.indexOf(new
+	// ConfigInterfaceAPIStorable(ace)));
+	// cis.getInfoMap().put("settingsCount", (Integer)
+	// cis.getInfoMap().get("settingsCount") + 1);
+	// }
+	// }
+	// return result;
+	// }
+	//
+	// @SuppressWarnings("rawtypes")
+	// @Override
+	// public List<ConfigEntryAPIStorable> queryConfigSettings(APIQuery query) {
+	// AdvancedConfigManager acm = AdvancedConfigManager.getInstance();
+	// List<ConfigEntryAPIStorable> result = new
+	// ArrayList<ConfigEntryAPIStorable>();
+	//
+	// // retrieve packageUUIDs from queryParams
+	// List<String> interfaceKeys = new ArrayList<String>();
+	// if (!query._getQueryParam("interfaceKeys", List.class, new
+	// ArrayList()).isEmpty()) {
+	// List uuidsFromQuery = query._getQueryParam("interfaceKeys", List.class,
+	// new ArrayList());
+	// for (Object o : uuidsFromQuery) {
+	// try {
+	// interfaceKeys.add((String) o);
+	// } catch (ClassCastException e) {
+	// continue;
+	// }
+	// }
+	// }
+	//
+	// for (AdvancedConfigEntry ace : acm.list()) {
+	// ConfigEntryAPIStorable ces = new ConfigEntryAPIStorable(ace);
+	// // if only certain interfaces selected, skip if interface not included
+	// if (!interfaceKeys.isEmpty()) {
+	// if
+	// (!interfaceKeys.contains(ces.getInterfaceKey().substring(ces.getInterfaceKey().lastIndexOf(".")
+	// + 1,
+	// ces.getInterfaceKey().length()))) {
+	// continue;
+	// }
+	// }
+	// KeyHandler<?> kh = ((AdvancedConfigInterfaceEntry) ace).getKeyHandler();
+	//
+	// QueryResponseMap infoMap = new QueryResponseMap();
+	// if (query.fieldRequested("value")) {
+	// infoMap.put("value", kh.getValue());
+	// }
+	// if (query.fieldRequested("defaultValue")) {
+	// infoMap.put("defaultValue", kh.getDefaultValue());
+	// }
+	// if (query.fieldRequested("dataType")) {
+	// infoMap.put("dataType", kh.getRawClass().getName());
+	// }
+	// if (query.fieldRequested("description")) {
+	// infoMap.put("description", ace.getDescription());
+	// }
+	//
+	// ces.setInfoMap(infoMap);
+	// result.add(ces);
+	// }
+	// return result;
+	// }
 
-    @Override
-    public ArrayList<AdvancedConfigAPIEntry> list() {
-        return list(null, false, false, false);
-    }
+	@Override
+	public ArrayList<AdvancedConfigAPIEntry> list() {
+		return list(null, false, false, false);
+	}
 
 }
