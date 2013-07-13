1316941e0d16e9b99ab841d6a94fcf36239e5f0b
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-07-13 14:38:06 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@21736 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/swing/dialog/AbstractCaptchaDialog.java
+++ src/jd/gui/swing/dialog/AbstractCaptchaDialog.java
@@ -175,7 +175,7 @@ public abstract class AbstractCaptchaDialog extends AbstractDialog<Object> {
             getDialog().toFront();
 
             if (CFG_GUI.CFG.isCaptchaDialogAOTWorkaroundEnabled()) getDialog().setAlwaysOnTop(true);
-            getDialog().setFocusableWindowState(false);
+
             getDialog().getOwner().toFront();
             JDGui.getInstance().getMainFrame().toFront();
 
