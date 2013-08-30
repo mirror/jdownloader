3cedabec0ac7d70879d21e3b8ef6520f886b7d51
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-08-30 15:47:54 +0000

    -added support for akatsukisubs
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22283 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/AkatsukiSubsNet.java
@@ -0,0 +1,114 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "akatsuki-subs.net" }, urls = { "http://(www\\.)?akatsuki\\-subs\\.net/(projekte/(laufend|abgeschlossen)/[a-z0-9\\-]+/|\\d+/releases/[a-z0-9]+/[a-z0-9\\-]+\\-\\d+/)" }, flags = { 0 })
+public class AkatsukiSubsNet extends PluginForDecrypt {
+
+    public AkatsukiSubsNet(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    private static final String   PROJECTLINK = "http://(www\\.)?akatsuki\\-subs\\.net/projekte/(laufend|abgeschlossen)/[a-z0-9\\-]+/";
+    private static final String   RELEASELINK = "http://(www\\.)?akatsuki\\-subs\\.net/\\d+/releases/op/[a-z0-9\\-]+\\-\\d+/";
+    private static final String[] QUALITIES   = { "hd", "ru boxvideo", "sd", "rs", "bt" };
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.getPage(parameter);
+        if (br.containsHTML(">Seite nicht gefunden")) {
+            logger.info("Link offline: " + parameter);
+            return decryptedLinks;
+        }
+        if (parameter.matches(PROJECTLINK)) {
+            // Get all tables
+            final String[] tables = br.getRegex("<table (class|id)=\"dl\"(.*?)</table>").getColumn(1);
+            for (final String table : tables) {
+                // Get entries of each table
+                final String[] tableEntries = new Regex(table, "<tr(.*?)</tr>").getColumn(0);
+                for (final String tableEntry : tableEntries) {
+                    final String fpName = new Regex(tableEntry, "class=\"first\">([^<>\"]*?)</td>").getMatch(0);
+                    FilePackage fp = null;
+                    if (fpName != null) {
+                        fp = FilePackage.getInstance();
+                        fp.setName(Encoding.htmlDecode(fpName.trim()));
+                    }
+                    for (final String quality : QUALITIES) {
+                        final String[] currentLinks = new Regex(tableEntry, "class=\"" + quality + "\" href=\"(http[^<>\"]*?)\"").getColumn(0);
+                        if (currentLinks != null && currentLinks.length != 0) {
+                            for (final String currentLink : currentLinks) {
+                                final DownloadLink dl = createDownloadlink(currentLink);
+                                if (fp != null) dl._setFilePackage(fp);
+                                decryptedLinks.add(dl);
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            // Get all tables
+            final String fpName = br.getRegex("<title>([^<>\"]*?)\\| Akatsuki\\-Subs</title>").getMatch(0);
+            final String[] tables = br.getRegex("<table id=\"dl_[a-z0-9]+\"(.*?)</table>").getColumn(0);
+            for (final String table : tables) {
+                // Get entries of each table
+                final String[] tableEntries = new Regex(table, "<tr(.*?)</tr>").getColumn(0);
+                for (final String tableEntry : tableEntries) {
+                    for (final String quality : QUALITIES) {
+                        final String[] currentLinks = new Regex(tableEntry, "class=\"" + quality + "\" href=\"(http[^<>\"]*?)\"").getColumn(0);
+                        if (currentLinks != null && currentLinks.length != 0) {
+                            for (final String currentLink : currentLinks) {
+                                decryptedLinks.add(createDownloadlink(currentLink));
+                            }
+                        }
+                    }
+                }
+            }
+            final String[] externalMirrors = br.getRegex("href=\"(http[^<>\"]*?)\" target=\"_blank\" rel=\"nofollow\">[A-Za-z0-9\\-\\.]+ Mirror</a><br").getColumn(0);
+            if (externalMirrors != null && externalMirrors.length != 0) {
+                for (final String mirror : externalMirrors) {
+                    decryptedLinks.add(createDownloadlink(mirror));
+                }
+            }
+
+            if (fpName != null) {
+                final FilePackage fp = FilePackage.getInstance();
+                fp.setName(Encoding.htmlDecode(fpName.trim()));
+                fp.addLinks(decryptedLinks);
+            }
+        }
+        if (decryptedLinks == null || decryptedLinks.size() == 0) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
+        }
+
+        return decryptedLinks;
+    }
+
+}
---
+++ src/jd/plugins/hoster/AkatsukiSubsNet.java
@@ -0,0 +1,135 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2010  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.utils.JDUtilities;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "akatsuki-subs.net" }, urls = { "http://(www\\.)?downloads\\.akatsuki\\-subs\\.net/file\\-\\d+\\.htm" }, flags = { 0 })
+public class AkatsukiSubsNet extends PluginForHost {
+
+    public AkatsukiSubsNet(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://akatsuki-subs.net/";
+    }
+
+    // note: CAN NOT be negative or zero! (ie. -1 or 0) Otherwise math sections fail. .:. use [1-20]
+    private static AtomicInteger totalMaxSimultanFreeDownload = new AtomicInteger(20);
+    // don't touch the following!
+    private static AtomicInteger maxFree                      = new AtomicInteger(1);
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(link.getDownloadURL());
+        if (br.containsHTML("Kein Download mit der angegebenen ID gefunden")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        final String filename = br.getRegex(">Downloade: <span class=\"highlight\">([^<>\"]*?)</span>").getMatch(0);
+        if (filename == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        link.setName(Encoding.htmlDecode(filename.trim()));
+        final String md5 = br.getRegex("MD5: <span class=\"highlight\">([a-z0-9]{32})</span>").getMatch(0);
+        if (md5 != null) link.setMD5Hash(md5);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        // Limit on single link (IP)
+        if (br.containsHTML(">Du hast das Limit an Downloads für diese Datei innerhalb von")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1000l);
+        // Limit on all links (IP)
+        if (br.containsHTML(">Du hast das Limit an Downloads innerhalb von")) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, 60 * 60 * 1000l);
+        br.setFollowRedirects(false);
+        final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+        final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+        rc.findID();
+        rc.load();
+        for (int i = 1; i <= 5; i++) {
+            final File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+            final String c = getCaptchaCode(cf, downloadLink);
+            final String postData = "recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + Encoding.urlEncode(c) + "&submit=Download";
+            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, br.getURL(), postData, false, 1);
+            if (dl.getConnection().getContentType().contains("html")) {
+                br.followConnection();
+                if (br.containsHTML("(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)")) {
+                    rc.reload();
+                    continue;
+                }
+            }
+            break;
+        }
+        if (br.containsHTML("(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+        if (dl.getConnection().getContentType().contains("html")) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        try {
+            // add a download slot
+            controlFree(+1);
+            // start the dl
+            dl.startDownload();
+        } finally {
+            // remove download slot
+            controlFree(-1);
+        }
+    }
+
+    /**
+     * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
+     * which allows the next singleton download to start, or at least try.
+     * 
+     * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
+     * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
+     * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
+     * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
+     * minimal harm to downloading as slots are freed up soon as current download begins.
+     * 
+     * @param controlFree
+     *            (+1|-1)
+     */
+    public synchronized void controlFree(final int num) {
+        logger.info("maxFree was = " + maxFree.get());
+        maxFree.set(Math.min(Math.max(1, maxFree.addAndGet(num)), totalMaxSimultanFreeDownload.get()));
+        logger.info("maxFree now = " + maxFree.get());
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return maxFree.get();
+    }
+
+    @Override
+    public void resetDownloadlink(final DownloadLink link) {
+    }
+
+}
