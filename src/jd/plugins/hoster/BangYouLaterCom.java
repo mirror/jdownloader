9e0bd4406fd52bd91c726e913f4097a7132cd3f5
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-09-11 03:09:38 +0000

    Host: bangyoulater.com - fixes #9586 http://board.jdownloader.org/showthread.php?t=50582
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22348 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/BangYouLaterCom.java
+++ src/jd/plugins/hoster/BangYouLaterCom.java
@@ -56,7 +56,8 @@ public class BangYouLaterCom extends PluginForHost {
         downloadLink.setName(new Regex(downloadLink.getDownloadURL(), "bangyoulater\\.com/(.+)").getMatch(0));
         br.setFollowRedirects(true);
         br.getPage(downloadLink.getDownloadURL());
-        if (br.getURL().equals("http://www.bangyoulater.com/") || br.containsHTML(">This video is no longer available")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        // ">This video is no longer available" is always in the html
+        if (br.getURL().equals("http://www.bangyoulater.com/") || br.containsHTML("display: block")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         String filename = br.getRegex("<h1 itemprop=\"name\">([^<>\"]*?)</h1>").getMatch(0);
         if (filename == null) filename = br.getRegex("<title>([^<>\"]*?)\\- BangYouLater\\.com</title>").getMatch(0);
         DLLINK = br.getRegex("file: \\'(http://[^<>\"]*?)\\'").getMatch(0);
