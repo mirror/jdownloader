ef1499d698d99bd8dedd7c978389f39790fbfbc6
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-09-21 12:55:29 +0000

    Fixed: hides.at, - closes #9695
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22425 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/HidsAt.java
+++ src/jd/plugins/decrypter/HidsAt.java
@@ -20,39 +20,79 @@ import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
 import jd.parser.Regex;
 import jd.parser.html.HTMLParser;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
+import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hides.at" }, urls = { "https?://(www\\.)?hides\\.at/(link/)?[a-z0-9]{32}" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hides.at" }, urls = { "https?://(www\\.)?hides\\.at/(link/)?[a-f0-9]{32}" }, flags = { 0 })
 public class HidsAt extends PluginForDecrypt {
 
     public HidsAt(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final String CAPTCHATEXT = "securimage-1\\.0\\.3\\.1/securimage_show\\.php";
+    private final String  CAPTCHATEXT = "securimage-1\\.0\\.3\\.1/securimage_show\\.php";
+    private static String agent       = null;
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        if (agent == null) {
+            JDUtilities.getPluginForHost("mediafire.com");
+            agent = jd.plugins.hoster.MediafireCom.stringUserAgent();
+        }
+        br.getHeaders().put("User-Agent", agent);
         br.setFollowRedirects(true);
         String parameter = param.toString();
         br.getPage(parameter);
         if (br.containsHTML("Error loading list or invalid list")) throw new DecrypterException(JDL.L("plugins.decrypt.errormsg.unavailable", "Perhaps wrong URL or the download is not available anymore."));
         if (!br.containsHTML(CAPTCHATEXT)) return null;
         String linkID = new Regex(parameter, "hides\\.at/(link/)?(.+)").getMatch(1);
-        for (int i = 0; i <= 5; i++) {
-            String code = getCaptchaCode("http://hides.at/include/securimage-1.0.3.1/securimage_show.php", param);
-            br.getPage("http://hides.at/link/" + linkID + "?captcha_code=" + code + "&hash=" + linkID + "&btnSubmit=Submit");
-            if (br.containsHTML(CAPTCHATEXT)) continue;
-            break;
+
+        Browser br2 = br.cloneBrowser();
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openGetConnection("http://hides.at/include/securimage-1.0.3.1/securimage_show.php");
+            if (con.getContentType().contains("html")) {
+                br2.followConnection();
+                if (br2.containsHTML("<b>Fatal error</b>:"))
+                    br.getPage(parameter + "?captcha_code=+&hash=" + linkID);
+                else
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            } else {
+                int repeat = 4;
+                for (int i = 0; i <= repeat; i++) {
+                    String code = getCaptchaCode("http://hides.at/include/securimage-1.0.3.1/securimage_show.php", param);
+                    Browser cap = br.cloneBrowser();
+                    cap.getPage("http://hides.at/link/" + linkID + "?captcha_code=" + code + "&hash=" + linkID + "&btnSubmit=Submit");
+                    if (i + 1 == repeat && cap.containsHTML(CAPTCHATEXT)) {
+                        throw new DecrypterException(DecrypterException.CAPTCHA);
+                    } else if (cap.containsHTML(CAPTCHATEXT)) {
+                        continue;
+                    } else {
+                        br = cap.cloneBrowser();
+                        break;
+                    }
+                }
+            }
+        } catch (final Throwable e) {
+            if (e instanceof PluginException) throw (PluginException) e;
+            if (e instanceof DecrypterException) throw (DecrypterException) e;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Exception e) {
+            }
         }
-        if (br.containsHTML(CAPTCHATEXT)) throw new DecrypterException(DecrypterException.CAPTCHA);
         String list = br.getRegex("id=\"list2Copy\" style=\"display: none;\">(.*?)</div>").getMatch(0);
         if (list == null) return null;
         String[] links = HTMLParser.getHttpLinks(list, "");
