bb259fcf663fb41760e7c1b080508340fde5f548
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-09-24 18:55:27 +0000

    Fixed: anysend.com, reviewed, saw plenty of issues - re: http://board.jdownloader.org/showthread.php?t=50756
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22445 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/AnySendCom.java
+++ src/jd/plugins/hoster/AnySendCom.java
@@ -1,5 +1,5 @@
 //jDownloader - Downloadmanager
-//Copyright (C) 2010  JD-Team support@jdownloader.org
+//Copyright (C) 2013  JD-Team support@jdownloader.org
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -16,6 +16,7 @@
 
 package jd.plugins.hoster;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
@@ -32,12 +33,11 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
-import jd.plugins.hoster.EasyBytezCom.StringContainer;
 import jd.utils.JDUtilities;
 
 import org.appwork.utils.formatter.SizeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "anysend.com" }, urls = { "http://(www\\.)?anysend\\.com/[A-Z0-9]{32}" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "anysend.com" }, urls = { "http://(www\\.)?anysend\\.com/[A-F0-9]{32}" }, flags = { 0 })
 public class AnySendCom extends PluginForHost {
 
     public AnySendCom(PluginWrapper wrapper) {
@@ -49,38 +49,51 @@ public class AnySendCom extends PluginForHost {
         return "http://www.anysend.com/terms.html";
     }
 
-    Browser                        XMLBR = null;
     private static StringContainer agent = new StringContainer();
+
+    public static class StringContainer {
+        public String string = null;
+    }
+
+    private Browser prepBrowser(Browser prepBr) {
+        if (agent.string == null) {
+            /* we first have to load the plugin, before we can reference it */
+            JDUtilities.getPluginForHost("mediafire.com");
+            while (agent.string == null || agent.string.contains("MSIE"))
+                agent.string = jd.plugins.hoster.MediafireCom.stringUserAgent();
+        }
+        prepBr.getHeaders().put("User-Agent", agent.string);
+        prepBr.getHeaders().put("Accept-Language", "en-gb, en;q=0.9");
+        prepBr.setFollowRedirects(true);
+        return prepBr;
+    }
+
+    private Browser prepXML(Browser prepBr) {
+        prepBr.getHeaders().put("Accept", "*/*");
+        prepBr.getHeaders().put("Accept-Charset", null);
+        return prepBr;
+    }
 
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
         this.setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.getHeaders().put("Accept", "de,en-us;q=0.7,en;q=0.3");
-        if (agent.string == null) {
-            /* we first have to load the plugin, before we can reference it */
-            JDUtilities.getPluginForHost("mediafire.com");
-            agent.string = jd.plugins.hoster.MediafireCom.stringUserAgent();
-        }
-        br.getHeaders().put("User-Agent", agent.string);
+        prepBrowser(br);
         br.getPage(link.getDownloadURL());
-        XMLBR = br.cloneBrowser();
-        XMLBR.getHeaders().put("Accept", "*/*");
-        XMLBR.getHeaders().put("Accept-Charset", null);
-
+        Browser XMLBR = br.cloneBrowser();
+        prepXML(XMLBR);
         String visitorid = br.getCookie("http://www.anysend.com/", "PAPVisitorId");
-        XMLBR.getPage("http://affiliates.anysend.com/scripts/track.php?visitorId=" + visitorid + "&accountId=default1&tracking=1&url=H_anysend.com%2F%2F" + getFID(link) + "&referrer=&getParams=&anchor=&isInIframe=false&cookies=");
-        visitorid = XMLBR.getRegex("setVisitor\\(\\'([A-Za-z0-9]+)\\'\\);").getMatch(0);
+        XMLBR.getPage("http://affiliates.anysend.com/scripts/track.php?accountId=default1&tracking=1&url=H_anysend.com%2F%2F" + getFID(link) + "&referrer=&getParams=&anchor=&isInIframe=false&cookies=");
+        visitorid = XMLBR.getRegex("setVisitor\\('([A-Za-z0-9]+)'\\);").getMatch(0);
         String continuelink = br.getRegex("\"(http://(www\\.)?download\\.anysend\\.com/download/download\\.php\\?key=[A-Z0-9]{32}\\&aff=[A-Za-z0-9]+\\&visid=)\"").getMatch(0);
         if (continuelink == null || visitorid == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         br.setCookie("http://www.anysend.com/", "PAPVisitorId", visitorid);
         continuelink += visitorid;
         br.getPage(continuelink);
         final String filename = br.getRegex("class=\"filename\">([^<>\"]*?)</h1>").getMatch(0);
-        final String filesize = br.getRegex("id=\"files\\-size\">([^<>\"]*?)</td>").getMatch(0);
+        final String filesize = br.getRegex("id=\"files-size\">([^<>\"]*?)</td>").getMatch(0);
         if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         link.setName(Encoding.htmlDecode(filename.trim()));
-        link.setDownloadSize(SizeFormatter.getSize(filesize.replace(",", ".")));
+        link.setDownloadSize(SizeFormatter.getSize(filesize.trim()));
         return AvailableStatus.TRUE;
     }
 
@@ -89,22 +102,32 @@ public class AnySendCom extends PluginForHost {
         requestFileInformation(downloadLink);
         String dllink = checkDirectLink(downloadLink, "directlink");
         if (dllink == null) {
-            String code = br.getRegex("var dlcode=md5\\(\\'([A-Za-z0-9]+)\\'\\).toUpperCase\\(\\);").getMatch(0);
-            final String a = br.getRegex("a:\\'([A-Za-z0-9]+)\\'").getMatch(0);
-            final String v = br.getRegex("v:\\'([A-Za-z0-9]+)\\'").getMatch(0);
+            final String a = br.getRegex("a:'([A-Za-z0-9]+)'").getMatch(0);
+            final String v = br.getRegex("v:'([A-Za-z0-9]+)'").getMatch(0);
             final String key = new Regex(br.getURL(), "\\?key=([A-Z0-9]+)\\&").getMatch(0);
-            if (code == null || v == null || key == null || a == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            code = JDHash.getMD5(code).toUpperCase();
-
-            XMLBR.getPage("http://im.anysend.com/check_file.php?key=" + key + "&callback=jQuery" + System.currentTimeMillis() + "_" + new Random().nextInt(1000000000) + "&_=" + System.currentTimeMillis());
+            if (v == null || key == null || a == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            String code = br.getRegex("var dlcode=md5\\('([A-Za-z0-9]+)'").getMatch(0);
+            if (code == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            Browser XMLBR = br.cloneBrowser();
+            prepXML(XMLBR);
+            final long systime = System.currentTimeMillis();
+            final String random = (String.valueOf(new Random().nextLong()).replace("-", "") + String.valueOf(new Random().nextLong()).replace("-", "")).substring(5, 26);
+            XMLBR.getPage("http://im.anysend.com/check_file.php?key=" + key + "&callback=jQuery" + random + "_" + systime + "&_=" + (systime - 1));
             final String ip = XMLBR.getRegex("\\d+\\(\"([0-9\\.]*?)\"\\);").getMatch(0);
             if (ip == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-
-            XMLBR.getPage("http://download.anysend.com/download/getcode.php?a=" + a + "&v=" + v + "&key=" + key + "&code=" + code);
+            final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+            final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+            rc.findID();
+            rc.load();
+            final File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+            final String c = getCaptchaCode(cf, downloadLink);
+            XMLBR = br.cloneBrowser();
+            prepXML(XMLBR);
+            XMLBR.getPage("http://download.anysend.com/download/getcode.php?a=" + a + "&v=" + v + "&key=" + key + "&code=" + JDHash.getMD5(code + rc.getChallenge() + c).toUpperCase() + "&challenge=" + rc.getChallenge() + "&response=" + Encoding.urlEncode(c));
+            if (XMLBR.containsHTML("\"isRecaptchaError\":true,\"recaptchaMessage\":\"Incorrect response\"")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
             final String dlkey = XMLBR.getRegex("\"dlkey\":\"([A-Za-z0-9]+)\"").getMatch(0);
             if (dlkey == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            XMLBR.getPage("http://" + ip + "/anysend/info/" + key + "?callback=jQuery" + System.currentTimeMillis() + "_" + new Random().nextInt(1000000000) + "&_=" + System.currentTimeMillis());
-
+            XMLBR.getPage("http://" + ip + "/anysend/info/" + key + "?callback=jQuery" + System.currentTimeMillis() + "_" + random + "&_=" + System.currentTimeMillis());
             dllink = "http://" + ip + "/anysend/download/" + dlkey + "/" + Encoding.urlEncode(downloadLink.getName());
         }
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
