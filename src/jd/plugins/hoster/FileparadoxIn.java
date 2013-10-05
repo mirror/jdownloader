c2f1f362e631bd9a9e1c3ef00f2800518ab5d7d5
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-10-05 22:17:25 +0000

    -improved fileparadox errorhandling fixes #9808
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22523 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FileparadoxIn.java
+++ src/jd/plugins/hoster/FileparadoxIn.java
@@ -78,7 +78,7 @@ public class FileparadoxIn extends PluginForHost {
 
     // DEV NOTES
     // XfileSharingProBasic Version 2.5.8.7
-    // mods:
+    // mods: new premiumonly text
     // non account: 1 * 1
     // free account: untested, set same as free
     // premium account: 1 * 10
@@ -594,7 +594,7 @@ public class FileparadoxIn extends PluginForHost {
         if (correctedBR.contains("You're using all download slots for IP")) { throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, null, 10 * 60 * 1001l); }
         if (correctedBR.contains("Error happened when generating Download Link")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error!", 10 * 60 * 1000l);
         /** Error handling for only-premium links */
-        if (new Regex(correctedBR, "( can download files up to |Upgrade your account to download bigger files|>Upgrade your account to download larger files|>The file you requested reached max downloads limit for Free Users|Please Buy Premium To download this file<|This file reached max downloads limit)").matches()) {
+        if (new Regex(correctedBR, "( can download files up to |Upgrade your account to download bigger files|>Upgrade your account to download larger files|>The file you requested reached max downloads limit for Free Users|Please Buy Premium To download this file<|This file reached max downloads limit|>This file is available for Premium Users only)").matches()) {
             String filesizelimit = new Regex(correctedBR, "You can download files up to(.*?)only").getMatch(0);
             if (filesizelimit != null) {
                 filesizelimit = filesizelimit.trim();
