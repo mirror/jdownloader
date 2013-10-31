f3adec927087ecedaa6f89ded3948583986acc69
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-10-31 15:35:18 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22753 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/javax/enterprise/inject/Alternative2.java
@@ -0,0 +1,18 @@
+package javax.enterprise.inject;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target({ TYPE, METHOD, FIELD })
+@Retention(RUNTIME)
+@Documented
+public @interface Alternative2 {
+    //
+
+}
