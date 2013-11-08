3192109f2f53c82197c395ac0453112d14102be6
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-11-08 16:43:16 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22936 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/gui/swing/jdgui/JDGui.java
+++ src/jd/gui/swing/jdgui/JDGui.java
@@ -114,8 +114,6 @@ import org.appwork.utils.swing.windowmanager.WindowManager.WindowExtendedState;
 import org.jdownloader.actions.AppAction;
 import org.jdownloader.controlling.FileCreationManager;
 import org.jdownloader.crosssystem.idlegetter.IdleGetter;
-import org.jdownloader.extensions.StartException;
-import org.jdownloader.extensions.StopException;
 import org.jdownloader.gui.GuiUtils;
 import org.jdownloader.gui.KeyObserver;
 import org.jdownloader.gui.helpdialogs.HelpDialog;
@@ -1154,14 +1152,14 @@ public class JDGui implements UpdaterListener, OwnerFinder {
                     @Override
                     public Object edtRun() {
 
-                        try {
-                            // kill TrayIcon
-                            tray._setEnabled(false);
-                        } catch (StartException e) {
-                            e.printStackTrace();
-                        } catch (StopException e) {
-                            e.printStackTrace();
-                        }
+                        // try {
+                        // // kill TrayIcon
+                        // tray._setEnabled(false);
+                        // } catch (StartException e) {
+                        // e.printStackTrace();
+                        // } catch (StopException e) {
+                        // e.printStackTrace();
+                        // }
                         JDGui.this.mainTabbedPane.onClose();
 
                         JsonConfig.create(GraphicalUserInterfaceSettings.class).setLastFrameStatus(FrameStatus.create(mainFrame, JsonConfig.create(GraphicalUserInterfaceSettings.class).getLastFrameStatus()));
