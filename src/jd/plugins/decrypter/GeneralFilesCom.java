c8c1fa08b211f626c6644b5d11f802801f599194
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-11-10 10:49:02 +0000

    Decrypter: GeneralFilesCom - Added support for generalfiles.biz http://board.jdownloader.org/showthread.php?p=275363#post275363
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@22969 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/GeneralFilesCom.java
+++ src/jd/plugins/decrypter/GeneralFilesCom.java
@@ -29,7 +29,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "general-files.com" }, urls = { "http://(www\\.)?(general\\-files\\.com|generalfiles\\.org|generalfiles\\.me|general\\-files\\.org)/download/[a-z0-9]+/[^<>\"/]*?\\.html" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "general-files.com" }, urls = { "http://(www\\.)?(general\\-files\\.com|generalfiles\\.org|generalfiles\\.me|general\\-files\\.org|generalfiles\\.biz)/download/[a-z0-9]+/[^<>\"/]*?\\.html" }, flags = { 0 })
 public class GeneralFilesCom extends PluginForDecrypt {
 
     public GeneralFilesCom(PluginWrapper wrapper) {
