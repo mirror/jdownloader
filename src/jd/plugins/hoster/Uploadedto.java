d71de2588eec03eeb5feb32c57e820ff218b2a19
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2013-12-27 18:41:11 +0000

    -changed limit for free downloads uploaded filesize refs #10818
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@23677 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Uploadedto.java
+++ src/jd/plugins/hoster/Uploadedto.java
@@ -137,7 +137,7 @@ public class Uploadedto extends PluginForHost {
     }
 
     public boolean canHandle(DownloadLink downloadLink, Account account) {
-        if ((account == null || account.getBooleanProperty("free", false)) && downloadLink.getVerifiedFileSize() >= 1048575999)
+        if ((account == null || account.getBooleanProperty("free", false)) && downloadLink.getVerifiedFileSize() > 1073741824)
             return false;
         else
             return true;
