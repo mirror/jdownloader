cc99c89c573497a3f11571fa6c58d786a6569d15
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-01-31 14:49:37 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24049 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FileMonkeyIn.java
+++ src/jd/plugins/hoster/FileMonkeyIn.java
@@ -19,14 +19,19 @@ package jd.plugins.hoster;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
 import jd.PluginWrapper;
 import jd.config.Property;
+import jd.config.SubConfiguration;
 import jd.http.Browser;
 import jd.http.Cookie;
 import jd.http.Cookies;
@@ -43,6 +48,8 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.utils.JDUtilities;
+
+import org.appwork.utils.os.CrossSystem;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filemonkey.in" }, urls = { "https?://(www\\.)?filemonkey\\.in/file/[a-z0-9]+" }, flags = { 2 })
 public class FileMonkeyIn extends PluginForHost {
@@ -188,6 +195,67 @@ public class FileMonkeyIn extends PluginForHost {
         return true;
     }
 
+    private static void showFreeDialog(final String domain) {
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        String lng = System.getProperty("user.language");
+                        String message = null;
+                        String title = null;
+                        String tab = "                        ";
+                        if ("de".equalsIgnoreCase(lng)) {
+                            title = domain + " Free Download";
+                            message = "Du lädst im kostenlosen Modus von " + domain + ".\r\n";
+                            message += "Wie bei allen anderen Hostern holt JDownloader auch hier das Beste für dich heraus!\r\n\r\n";
+                            message += tab + "  Falls du allerdings mehrere Dateien\r\n" + "          - und das möglichst mit Fullspeed und ohne Unterbrechungen - \r\n" + "             laden willst, solltest du dir den Premium Modus anschauen.\r\n\r\nUnserer Erfahrung nach lohnt sich das - Aber entscheide am besten selbst. Jetzt ausprobieren?  ";
+                        } else {
+                            title = domain + " Free Download";
+                            message = "You are using the " + domain + " Free Mode.\r\n";
+                            message += "JDownloader always tries to get the best out of each hoster's free mode!\r\n\r\n";
+                            message += tab + "   However, if you want to download multiple files\r\n" + tab + "- possibly at fullspeed and without any wait times - \r\n" + tab + "you really should have a look at the Premium Mode.\r\n\r\nIn our experience, Premium is well worth the money. Decide for yourself, though. Let's give it a try?   ";
+                        }
+                        if (CrossSystem.isOpenBrowserSupported()) {
+                            int result = JOptionPane.showConfirmDialog(jd.gui.swing.jdgui.JDGui.getInstance().getMainFrame(), message, title, JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
+                            if (JOptionPane.OK_OPTION == result) CrossSystem.openURL(new URL("http://update3.jdownloader.org/jdserv/BuyPremiumInterface/redirect?" + domain + "&freedialog"));
+                        }
+                    } catch (Throwable e) {
+                    }
+                }
+            });
+        } catch (Throwable e) {
+        }
+    }
+
+    private void checkShowFreeDialog() {
+        SubConfiguration config = null;
+        try {
+            config = getPluginConfig();
+            if (config.getBooleanProperty("premAdShown", Boolean.FALSE) == false) {
+                if (config.getProperty("premAdShown2") == null) {
+                    File checkFile = JDUtilities.getResourceFile("tmp/flmnky");
+                    if (!checkFile.exists()) {
+                        checkFile.mkdirs();
+                        showFreeDialog("filemonkey.in");
+                    }
+                } else {
+                    config = null;
+                }
+            } else {
+                config = null;
+            }
+        } catch (final Throwable e) {
+        } finally {
+            if (config != null) {
+                config.setProperty("premAdShown", Boolean.TRUE);
+                config.setProperty("premAdShown2", "shown");
+                config.save();
+            }
+        }
+    }
+
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
         checkLinks(new DownloadLink[] { link });
@@ -215,6 +283,7 @@ public class FileMonkeyIn extends PluginForHost {
     }
 
     private void doFree(final DownloadLink downloadLink) throws Exception {
+        checkShowFreeDialog();
         prepareBrowser(br);
         String dllink = checkDirectLink(downloadLink, "directlink");
         if (dllink == null) {
