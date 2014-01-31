c7c7dd23b1742ac1fc8300fcd2cffaf2569b1551
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-01-31 15:00:33 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24052 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/folderwatch/translate/T.java
+++
@@ -1,8 +0,0 @@
-package org.jdownloader.extensions.folderwatch.translate;
-
-import org.appwork.txtresource.TranslationFactory;
-
-public class T {
-    public static final FolderWatchTranslation _ = TranslationFactory.create(FolderWatchTranslation.class);
-
-}
