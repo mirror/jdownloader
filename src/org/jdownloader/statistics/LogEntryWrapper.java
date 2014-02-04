6059da78a7fc1cef06c809ce3b401aeb2bd39c7d
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-02-04 09:11:24 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24105 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/statistics/LogEntryWrapper.java
+++ src/org/jdownloader/statistics/LogEntryWrapper.java
@@ -4,7 +4,7 @@ import org.appwork.storage.JSonStorage;
 import org.appwork.storage.Storable;
 
 public class LogEntryWrapper implements Storable {
-    public static final int VERSION = 3;
+    public static final int VERSION = 4;
     private String          data;
 
     public String getData() {
