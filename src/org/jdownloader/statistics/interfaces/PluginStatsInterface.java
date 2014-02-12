d7b65552dfaf22cf0106cd9d67d8dbf03be22445
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-02-12 15:57:43 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24241 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/statistics/interfaces/PluginStatsInterface.java
+++ src/org/jdownloader/statistics/interfaces/PluginStatsInterface.java
@@ -1,19 +1,26 @@
 package org.jdownloader.statistics.interfaces;
 
+import java.util.ArrayList;
+
 import org.appwork.remoteapi.RemoteAPIInterface;
 import org.appwork.remoteapi.RemoteAPIRequest;
 import org.appwork.remoteapi.annotations.ApiNamespace;
+import org.appwork.remoteapi.exceptions.BadParameterException;
 import org.jdownloader.statistics.ErrorDetails;
 import org.jdownloader.statistics.LogDetails;
+import org.jdownloader.statistics.LogEntryWrapper;
 import org.jdownloader.statistics.StatsManager.Response;
 import org.jdownloader.statistics.TimeWrapper;
 
 @ApiNamespace("plugins")
 public interface PluginStatsInterface extends RemoteAPIInterface {
 
-    Response push(TimeWrapper sendTo, RemoteAPIRequest request);
+    Response push2(TimeWrapper sendTo, RemoteAPIRequest request) throws BadParameterException;
 
     void sendError(ErrorDetails error);
 
     void sendLog(LogDetails error);
+
+    // old
+    Response push(ArrayList<LogEntryWrapper> timeWrapper, RemoteAPIRequest request) throws BadParameterException;
 }
