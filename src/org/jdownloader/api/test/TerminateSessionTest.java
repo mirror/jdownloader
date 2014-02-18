6c7be5407834e7466f3f95547f70d8b68a91ac46
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-02-18 09:54:08 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24343 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/api/test/TerminateSessionTest.java
@@ -0,0 +1,14 @@
+package org.jdownloader.api.test;
+
+import org.appwork.storage.Storage;
+import org.jdownloader.api.test.TestClient.Test;
+import org.jdownloader.myjdownloader.client.AbstractMyJDClientForDesktopJVM;
+
+public class TerminateSessionTest extends Test {
+    
+    @Override
+    public void run(Storage config, AbstractMyJDClientForDesktopJVM api) throws Exception {
+        api.terminateSession();
+    }
+    
+}
--- src/org/jdownloader/api/test/TestClient.java
+++ src/org/jdownloader/api/test/TestClient.java
@@ -58,29 +58,29 @@ import org.jdownloader.myjdownloader.client.json.NotificationRequestMessage;
 import org.jdownloader.myjdownloader.client.json.NotificationRequestMessage.TYPE;
 
 public class TestClient {
-
+    
     private static TestLogin LOGIN;
-
+    
     static ImageIcon createImage(final CaptchaChallenge challenge) throws IOException {
         final BufferedImage image = ImageIO.read(new ByteArrayInputStream(Base64.decode(challenge.getImage().substring(challenge.getImage().lastIndexOf(",")))));
-
+        
         return new ImageIcon(image);
     }
-
+    
     public static abstract class Test {
         private static DeviceData lastDevice;
-
+        
         public abstract void run(Storage config, AbstractMyJDClientForDesktopJVM api) throws Exception;
-
+        
         public String getName() {
             return getClass().getSimpleName();
         }
-
+        
         protected static String chooseDevice(final AbstractMyJDClientForDesktopJVM api) throws MyJDownloaderException, DialogClosedException, DialogCanceledException {
             final DeviceList list = api.listDevices();
             if (list.getList().size() == 0) { throw new RuntimeException("No Device Connected"); }
             if (list.getList().size() == 1) { return list.getList().get(0).getId(); }
-
+            
             int defIndex = 0;
             ArrayList<DeviceData> devList = list.getList();
             if (lastDevice != null) defIndex = devList.indexOf(lastDevice);
@@ -88,13 +88,13 @@ public class TestClient {
             final int device = Dialog.getInstance().showComboDialog(0, "Choose Device", "Choose Device", devList.toArray(new DeviceData[] {}), defIndex, null, null, null, null);
             lastDevice = list.getList().get(device);
             final String dev = lastDevice.getId();
-
+            
             return dev;
         }
     }
-
+    
     public static class TestLogin extends Test {
-
+        
         @Override
         public void run(Storage config, AbstractMyJDClientForDesktopJVM api) throws Exception {
             final LoginDialog login = new LoginDialog(0);
@@ -105,53 +105,53 @@ public class TestClient {
             final LoginData li = Dialog.getInstance().showDialog(login);
             if (li.isSave()) {
                 config.put("email", li.getUsername());
-
+                
                 config.put("password", li.getPassword());
             }
             try {
                 api.connect(li.getUsername(), li.getPassword());
             } catch (final EmailNotValidatedException e) {
                 final CaptchaChallenge challenge = api.getChallenge();
-
+                
                 final String response = Dialog.getInstance().showInputDialog(0, "Enter Captcha to register", "Enter", null, TestClient.createImage(challenge), null, null);
                 challenge.setCaptchaResponse(response);
-
+                
                 api.requestRegistrationEmail(challenge, li.getUsername(), null);
                 api.finishRegistration(Dialog.getInstance().showInputDialog(0, "Email Confirmal Key", "Enter", null, null, null, null), li.getUsername(), li.getPassword());
                 api.connect(li.getUsername(), li.getPassword());
-
+                
             }
         }
-
+        
         @Override
         public String getName() {
             return "Login";
         }
-
+        
     }
-
+    
     public static class ConfigTest extends Test {
-
+        
         @Override
         public void run(Storage config, AbstractMyJDClientForDesktopJVM api) throws Exception {
             final AdvancedConfigInterface link = api.link(AdvancedConfigInterface.class, chooseDevice(api));
             ArrayList<AdvancedConfigEntryDataStorable> fullList = link.list(".*", true, true, true, true);
-
+            
             ArrayList<AdvancedConfigEntryDataStorable> captchacondif = link.list(".*captcha.*", false, true, false);
             System.out.println(fullList);
             System.out.println("maxforceddownloads = " + link.get("org.jdownloader.settings.GeneralSettings", null, "maxforceddownloads"));
-
+            
             ArrayList<AdvancedConfigEntryDataStorable> trigger = link.list("org.jdownloader.settings.GeneralSettings.CreateFolderTrigger", false, true, false);
             ArrayList<EnumOptionStorable> enumValues = link.listEnum(trigger.get(0).getType());
             System.out.println(enumValues);
         }
-
+        
         @Override
         public String getName() {
             return "Test Config";
         }
     }
-
+    
     /**
      * @param args
      * @throws UnsupportedEncodingException
@@ -165,7 +165,7 @@ public class TestClient {
      * @throws InterruptedException
      */
     public static void main(final String[] args) throws APIException, MyJDownloaderException, DialogClosedException, DialogCanceledException, IOException, InterruptedException {
-
+        
         register(LOGIN = new TestLogin());
         register(new ConfigTest());
         register(new AccountTest());
@@ -173,29 +173,29 @@ public class TestClient {
         register(new DownloadListTest());
         register(new LinkgrabberTest());
         register(new KeepAliveTest());
-
+        
         register(new DialogTest());
         register(new ChangePasswordTest());
-
+        
         register(new ListDevicesTest());
         register(new DisconnectTest());
-
+        
         register(new FeedbackTest());
-
+        
         register(new ChallengeTest());
-
+        
         register(new RegainTest());
-
+        
         register(new GetUptimeTest());
-
+        
         register(new GenericCallTest());
-
+        
         register(new CaptchaListTest());
-
+        
         register(new WriteSessionTest());
-
+        
         register(new RegisterTest());
-
+        
         register(new RestoreSessionTest());
         register(new TerminateAccountTest());
         register(new StorageCreateTest());
@@ -204,76 +204,77 @@ public class TestClient {
         register(new StorageListKeysTest());
         register(new StorageGetValueTest());
         register(new StoragePutValueTest());
-
+        register(new TerminateSessionTest());
+        
         register(new CancelRegistrationTest());
         JacksonMapper jm;
         JSonStorage.setMapper(jm = new JacksonMapper());
-
+        
         MyJDJsonMapper.HANDLER = new JSonHandler<Type>() {
-
+            
             @Override
             public String objectToJSon(final Object payload) {
                 return JSonStorage.serializeToJson(payload);
             }
-
+            
             @Override
             public <T> T jsonToObject(final String dec, final Type clazz) {
                 return (T) JSonStorage.restoreFromString(dec, new TypeRef(clazz) {
-
+                    
                 });
             }
         };
-
+        
         jm.addSerializer(JsonFactoryInterface.class, new JsonSerializer<JsonFactoryInterface>() {
-
+            
             @Override
             public String toJSonString(final JsonFactoryInterface list) {
                 return list.toJsonString();
-
+                
             }
-
+            
             @Override
             public boolean canSerialize(final Object arg0) {
                 return arg0 instanceof JsonFactoryInterface;
             }
-
+            
         });
         AbstractDialog.setDefaultLocator(new RememberAbsoluteDialogLocator("MYJDTest"));
-
+        
         final AbstractMyJDClientForDesktopJVM api = new AbstractMyJDClientForDesktopJVM("Java Test Application") {
-
+            
             @Override
             protected byte[] base64decode(final String base64encodedString) {
                 return Base64.decode(base64encodedString);
             }
-
+            
             @Override
             protected void log(String json) {
                 System.out.println(json);
             }
-
+            
             @Override
             protected String base64Encode(final byte[] encryptedBytes) {
                 return Base64.encodeToString(encryptedBytes, false);
             }
-
+            
             @Override
             protected byte[] post(final String query, final String object, final byte[] keyAndIV) throws ExceptionResponse {
                 HTTPConnection con = null;
                 byte[] ret = null;
                 final BasicHTTP br = new BasicHTTP();
                 br.setLogger(new LogInterface() {
-
+                    
                     @Override
                     public void log(Throwable e) {
                         e.printStackTrace();
                     }
-
+                    
                     @Override
                     public void info(String msg) {
                         System.out.println(msg);
                     }
-
+                    
                     @Override
                     public void fine(String string) {
                         System.out.println(string);
@@ -285,7 +286,7 @@ public class TestClient {
                     codes[i] = i;
                 }
                 br.setAllowedResponseCodes(codes);
-
+                
                 try {
                     if (keyAndIV != null) {
                         br.putRequestHeader("Accept-Encoding", "gzip_aes");
@@ -316,7 +317,7 @@ public class TestClient {
                         ret = br.postPage(new URL(getServerRoot() + query), object == null ? "" : object).getBytes("UTF-8");
                         con = br.getConnection();
                     }
-
+                    
                     if (con != null && con.getResponseCode() > 0 && con.getResponseCode() != 200) {
                         //
                         throw new ExceptionResponse(new String(ret, "UTF-8"), con.getResponseCode());
@@ -332,16 +333,16 @@ public class TestClient {
                     } catch (final Throwable e) {
                     }
                 }
-
+                
             }
-
+            
         };
         final Storage config = JSonStorage.getPlainStorage("APiClientTest");
-
+        
         api.setServerRoot("http://api.jdownloader.org");
-
-        api.setServerRoot("http://192.168.2.110:10101");
-
+        
+        // api.setServerRoot("http://192.168.2.110:10101");
+        
         if (false) {
             api.connect(config.get("email", ""), config.get("password", ""));
             try {
@@ -382,9 +383,9 @@ public class TestClient {
             api.disconnect();
             return;
         }
-
+        
         try {
-
+            
             Test lastOption = LOGIN;
             while (true) {
                 try {
@@ -393,25 +394,25 @@ public class TestClient {
                         {
                             org = new JComboBox().getRenderer();
                         }
-
+                        
                         @Override
                         public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
                             return org.getListCellRendererComponent(list, ((Test) value).getName(), index, isSelected, cellHasFocus);
-
+                            
                         }
-
+                        
                     });
-
+                    
                     lastOption.run(config, api);
-
+                    
                 } catch (final DialogNoAnswerException e) {
                     System.exit(1);
                 } catch (final Exception e) {
-
+                    
                     Dialog.getInstance().showExceptionDialog("Error!!", e.getClass().getSimpleName() + ": " + e, e);
                 }
             }
-
+            
         } catch (final Exception e) {
             e.printStackTrace();
             Dialog.getInstance().showExceptionDialog("Error!!", e.getClass().getSimpleName() + ": " + e, e);
@@ -426,11 +427,11 @@ public class TestClient {
         // api.disconnect();
         // System.out.println(jdapi.uptime());
     }
-
+    
     private static List<Test> TESTS = new ArrayList<Test>();
-
+    
     private static void register(Test testLogin) {
         TESTS.add(testLogin);
     }
-
+    
 }
