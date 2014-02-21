321a9d7b00ca1628b736ea00cbce86bf08d73568
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-02-21 13:08:23 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24386 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/statistics/StatsManager.java
+++ src/org/jdownloader/statistics/StatsManager.java
@@ -484,7 +484,7 @@ public class StatsManager implements GenericConfigEventListener<Object>, Downloa
             //
             dl.setCounter(errorCounter.incrementAndGet());
             ;
-            if (dl.getErrorID() == null) dl.setErrorID(id + "/" + dl.getResult());
+            if (dl.getErrorID() == null && dl.getResult() != DownloadResult.FINISHED) dl.setErrorID(id + "/" + dl.getResult());
             if (dl.getErrorID() != null) {
                 ErrorDetails error = errors.get(dl.getErrorID());
                 if (error == null) {
