adb7aa6f7a2bc13d854edbe7525aeee12f7adef3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-14 10:16:33 +0000

    fixes #12620
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24652 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/views/downloads/action/ResetAction.java
+++ src/org/jdownloader/gui/views/downloads/action/ResetAction.java
@@ -8,22 +8,28 @@ import jd.gui.UserIO;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
+import org.appwork.uio.UIOManager;
 import org.appwork.utils.event.queue.QueueAction;
 import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.swing.EDTRunner;
+import org.appwork.utils.swing.dialog.ConfirmDialog;
+import org.appwork.utils.swing.dialog.Dialog;
+import org.appwork.utils.swing.dialog.DialogCanceledException;
+import org.appwork.utils.swing.dialog.DialogClosedException;
 import org.jdownloader.controlling.DownloadLinkAggregator;
 import org.jdownloader.controlling.contextmenu.CustomizableTableContextAppAction;
 import org.jdownloader.gui.translate._GUI;
 
 public class ResetAction extends CustomizableTableContextAppAction<FilePackage, DownloadLink> {
-
+    
     private static final long serialVersionUID = -5583373118359478729L;
-
+    
     public ResetAction() {
-
+        
         setIconKey("undo");
         setName(_GUI._.gui_table_contextmenu_reset());
     }
-
+    
     public void actionPerformed(ActionEvent e) {
         if (!isEnabled()) return;
         TaskQueue.getQueue().add(new QueueAction<Void, RuntimeException>() {
@@ -32,10 +38,28 @@ public class ResetAction extends CustomizableTableContextAppAction<FilePackage, 
                 DownloadLinkAggregator agg = new DownloadLinkAggregator();
                 agg.setLocalFileUsageEnabled(true);
                 agg.update(getSelection().getChildren());
-                if (UserIO.isOK(UserIO.getInstance().requestConfirmDialog(UserIO.DONT_SHOW_AGAIN | UserIO.DONT_SHOW_AGAIN_IGNORES_CANCEL, _GUI._.gui_downloadlist_reset2(agg.getTotalCount(), SizeFormatter.formatBytes(agg.getBytesLoaded()), agg.getLocalFileCount())))) {
-                    DownloadWatchDog.getInstance().reset(getSelection().getChildren());
-
-                }
+                final String question = _GUI._.gui_downloadlist_reset2(agg.getTotalCount(), SizeFormatter.formatBytes(agg.getBytesLoaded()), agg.getLocalFileCount());
+                new EDTRunner() {
+                    
+                    @Override
+                    protected void runInEDT() {
+                        ConfirmDialog confirmDialog = new ConfirmDialog(Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN | UIOManager.LOGIC_DONT_SHOW_AGAIN_IGNORES_CANCEL, _GUI._.jd_gui_userio_defaulttitle_confirm(), question, UserIO.getDefaultIcon(question), null, null) {
+                            @Override
+                            public String getDontShowAgainKey() {
+                                return "org.jdownloader.gui.views.downloads.action.ResetAction";
+                            }
+                        };
+                        try {
+                            Dialog.getInstance().showDialog(confirmDialog);
+                            DownloadWatchDog.getInstance().reset(getSelection().getChildren());
+                        } catch (DialogClosedException e) {
+                            e.printStackTrace();
+                        } catch (DialogCanceledException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                
                 return null;
             };
         });
