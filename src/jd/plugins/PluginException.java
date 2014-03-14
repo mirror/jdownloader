9db69168c8a45a408f7a97d5f22892b226976382
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-14 09:51:48 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24649 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/PluginException.java
+++ src/jd/plugins/PluginException.java
@@ -17,44 +17,45 @@
 package jd.plugins;
 
 public class PluginException extends Exception {
-
+    
     private static final long serialVersionUID              = -413339039711789194L;
-
+    
     public static int         VALUE_ID_PREMIUM_TEMP_DISABLE = 0;
     public static int         VALUE_ID_PREMIUM_DISABLE      = 1;
     /* do not final it, as the compiler will replace Reference with value, no more Exceptions but broken ErrorHandling in stable */
     public static int         VALUE_ID_PREMIUM_ONLY         = 2;
-
+    
     private final int         linkStatus;
     private final String      errorMessage;
     private final long        value;
-
+    
     public PluginException(int linkStatus) {
         this(linkStatus, null, -1);
     }
-
+    
     public PluginException(int linkStatus, String errorMessage, long value) {
+        super(errorMessage);
         this.linkStatus = linkStatus;
         this.errorMessage = errorMessage;
         this.value = value;
     }
-
+    
     public PluginException(int linkStatus, String errorMessage) {
         this(linkStatus, errorMessage, -1);
     }
-
+    
     public PluginException(int linkStatus, long value) {
         this(linkStatus, null, value);
     }
-
+    
     public String getErrorMessage() {
         return errorMessage;
     }
-
+    
     public long getValue() {
         return value;
     }
-
+    
     /**
      * JD2Only
      * 
@@ -63,5 +64,5 @@ public class PluginException extends Exception {
     public int getLinkStatus() {
         return linkStatus;
     }
-
+    
 }
