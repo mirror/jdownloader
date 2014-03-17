5e192e7a3ef7feaa077d369eb62522979d5c194c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-17 16:20:12 +0000

    AbstractNotifyWindow: fixed leaking window bug. every window must be disposed!
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24698 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/notify/AbstractBubbleSupport.java
+++ src/org/jdownloader/gui/notify/AbstractBubbleSupport.java
@@ -6,30 +6,32 @@ import org.appwork.storage.config.handler.BooleanKeyHandler;
 import org.jdownloader.gui.notify.gui.AbstractNotifyWindow;
 
 public abstract class AbstractBubbleSupport {
-
+    
     private String label;
-
+    
     public String getLabel() {
         return label;
     }
-
+    
     abstract public List<Element> getElements();
-
+    
     public BooleanKeyHandler getKeyHandler() {
         return keyHandler;
     }
-
+    
     protected BooleanKeyHandler keyHandler;
-
+    
     public AbstractBubbleSupport(String label, BooleanKeyHandler keyhandler) {
         this.label = label;
         this.keyHandler = keyhandler;
     }
-
+    
     protected void show(AbstractNotifyWindow no) {
         if (keyHandler.isEnabled()) {
             BubbleNotify.getInstance().show(no);
+        } else {
+            no.dispose();
         }
     }
-
+    
 }
--- src/org/jdownloader/gui/notify/BubbleNotify.java
+++ src/org/jdownloader/gui/notify/BubbleNotify.java
@@ -29,7 +29,7 @@ import org.jdownloader.gui.notify.reconnect.ReconnectBubbleSupport;
 
 public class BubbleNotify {
     private static final BubbleNotify INSTANCE = new BubbleNotify();
-
+    
     /**
      * get the only existing instance of BubbleNotify. This is a singleton
      * 
@@ -38,9 +38,9 @@ public class BubbleNotify {
     public static BubbleNotify getInstance() {
         return BubbleNotify.INSTANCE;
     }
-
+    
     private Balloner ballooner;
-
+    
     /**
      * Create a new instance of BubbleNotify. This is a singleton class. Access the only existing instance by using {@link #getInstance()}.
      */
@@ -51,143 +51,149 @@ public class BubbleNotify {
                 return JDGui.getInstance().getMainFrame();
             }
         };
-
+        
         GenericConfigEventListener<Object> update = new GenericConfigEventListener<Object>() {
-
+            
             @Override
             public void onConfigValueModified(KeyHandler<Object> keyHandler, Object newValue) {
                 ballooner.setScreenID(CFG_BUBBLE.CFG.getScreenID());
-
+                
                 if (CFG_BUBBLE.CFG.getAnimationStartPositionAnchor() == Anchor.SYSTEM_DEFAULT) {
                     switch (CrossSystem.getOS().getFamily()) {
-                    case WINDOWS:
-                        // bottom right position 10 pixel margin
-                        ballooner.setStartPoint(new Point(-11, -1), Anchor.TOP_RIGHT);
-                        break;
-                    default:
-                        // top right position 10 pixel margin
-                        ballooner.setStartPoint(new Point(-11, 0), Anchor.BOTTOM_RIGHT);
+                        case WINDOWS:
+                            // bottom right position 10 pixel margin
+                            ballooner.setStartPoint(new Point(-11, -1), Anchor.TOP_RIGHT);
+                            break;
+                        default:
+                            // top right position 10 pixel margin
+                            ballooner.setStartPoint(new Point(-11, 0), Anchor.BOTTOM_RIGHT);
                     }
-
+                    
                 } else {
                     ballooner.setStartPoint(new Point(CFG_BUBBLE.CFG.getAnimationStartPositionX(), CFG_BUBBLE.CFG.getAnimationStartPositionY()), CFG_BUBBLE.CFG.getAnimationStartPositionAnchor());
                 }
-
+                
                 if (CFG_BUBBLE.CFG.getAnimationEndPositionAnchor() == Anchor.SYSTEM_DEFAULT) {
                     switch (CrossSystem.getOS().getFamily()) {
-                    case WINDOWS:
-                        // move out to the right
-                        ballooner.setEndPoint(new Point(-1, -11), Anchor.BOTTOM_LEFT);
-                        break;
-                    default:
-                        // move out to the right
-                        ballooner.setEndPoint(new Point(-1, 10), Anchor.TOP_LEFT);
+                        case WINDOWS:
+                            // move out to the right
+                            ballooner.setEndPoint(new Point(-1, -11), Anchor.BOTTOM_LEFT);
+                            break;
+                        default:
+                            // move out to the right
+                            ballooner.setEndPoint(new Point(-1, 10), Anchor.TOP_LEFT);
                     }
-
+                    
                 } else {
                     ballooner.setEndPoint(new Point(CFG_BUBBLE.CFG.getAnimationEndPositionX(), CFG_BUBBLE.CFG.getAnimationEndPositionY()), CFG_BUBBLE.CFG.getAnimationEndPositionAnchor());
                 }
-
+                
                 if (CFG_BUBBLE.CFG.getFinalPositionAnchor() == Anchor.SYSTEM_DEFAULT) {
                     switch (CrossSystem.getOS().getFamily()) {
-                    case WINDOWS:
-                        ballooner.setAnchorPoint(new Point(-11, -11), Anchor.BOTTOM_RIGHT);
-                        break;
-                    default:
-                        ballooner.setAnchorPoint(new Point(-11, 10), Anchor.TOP_RIGHT);
+                        case WINDOWS:
+                            ballooner.setAnchorPoint(new Point(-11, -11), Anchor.BOTTOM_RIGHT);
+                            break;
+                        default:
+                            ballooner.setAnchorPoint(new Point(-11, 10), Anchor.TOP_RIGHT);
                     }
-
+                    
                 } else {
                     ballooner.setAnchorPoint(new Point(CFG_BUBBLE.CFG.getFinalPositionX(), CFG_BUBBLE.CFG.getFinalPositionY()), CFG_BUBBLE.CFG.getFinalPositionAnchor());
                 }
-
+                
             }
-
+            
             @Override
             public void onConfigValidatorError(KeyHandler<Object> keyHandler, Object invalidValue, ValidationException validateException) {
             }
-
+            
         };
-
+        
         CFG_BUBBLE.CFG._getStorageHandler().getEventSender().addListener(update);
         update.onConfigValueModified(null, null);
-
+        
         // if (ballooner != null) ballooner.add(new Notify(caption, text, NewTheme.I().getIcon("info", 32)));
-
+        
         synchronized (types) {
-
+            
             types.add(new LinkCrawlerBubbleSupport());
             types.add(new UpdatesBubbleSupport());
             types.add(new ReconnectBubbleSupport());
-
+            
             types.add(new CaptchaBubbleSupport());
             types.add(new StartDownloadsBubbleSupport());
             types.add(new StartStopPauseBubbleSupport());
-
+            
             types.add(CESBubbleSupport.getInstance());
-
+            
         }
     }
-
+    
     public void show(final AbstractNotifyWindow no) {
-
-        if (JDGui.getInstance().isSilentModeActive() && !CFG_BUBBLE.BUBBLE_NOTIFY_ENABLED_DURING_SILENT_MODE.isEnabled()) return;
-
         new EDTRunner() {
-
+            
             @Override
             protected void runInEDT() {
-
-                switch (CFG_BUBBLE.CFG.getBubbleNotifyEnabledState()) {
-                case JD_NOT_ACTIVE:
-                    if (WindowManager.getInstance().hasFocus()) { return; }
-                    break;
-
-                case NEVER:
-                    return;
-                case TASKBAR:
-                    if (WindowManager.getInstance().getExtendedState(JDGui.getInstance().getMainFrame()) != WindowExtendedState.ICONIFIED) { return; }
-                    break;
-
-                case TRAY:
-                    if (!JDGui.getInstance().getMainFrame().isVisible()) break;
-                    return;
-                case TRAY_OR_TASKBAR:
-
-                    if (WindowManager.getInstance().getExtendedState(JDGui.getInstance().getMainFrame()) == WindowExtendedState.ICONIFIED) {
-                        break;
+                boolean added = false;
+                try {
+                    if (JDGui.getInstance().isSilentModeActive() && !CFG_BUBBLE.BUBBLE_NOTIFY_ENABLED_DURING_SILENT_MODE.isEnabled()) return;
+                    switch (CFG_BUBBLE.CFG.getBubbleNotifyEnabledState()) {
+                        case JD_NOT_ACTIVE:
+                            if (WindowManager.getInstance().hasFocus()) { return; }
+                            break;
+                        
+                        case NEVER:
+                            return;
+                        case TASKBAR:
+                            if (WindowManager.getInstance().getExtendedState(JDGui.getInstance().getMainFrame()) != WindowExtendedState.ICONIFIED) { return; }
+                            break;
+                        
+                        case TRAY:
+                            if (!JDGui.getInstance().getMainFrame().isVisible()) break;
+                            return;
+                        case TRAY_OR_TASKBAR:
+                            
+                            if (WindowManager.getInstance().getExtendedState(JDGui.getInstance().getMainFrame()) == WindowExtendedState.ICONIFIED) {
+                                break;
+                            }
+                            if (!JDGui.getInstance().getMainFrame().isVisible()) break;
+                            return;
                     }
-                    if (!JDGui.getInstance().getMainFrame().isVisible()) break;
-                    return;
+                    System.out.println(" Show  bubble 3" + no);
+                    ballooner.add(no);
+                    added = true;
+                } finally {
+                    if (added == false) {
+                        /* creating a window and not disposing it = mem leak! in case we do not enqueue the window, we dispose it now! */
+                        no.dispose();
+                    }
                 }
-                System.out.println(" Show  bubble 3" + no);
-                ballooner.add(no);
             }
         };
-
+        
     }
-
+    
     public void hide(final AbstractNotifyWindow notify) {
         new EDTRunner() {
-
+            
             @Override
             protected void runInEDT() {
                 ballooner.hide(notify);
             }
         };
-
+        
     }
-
+    
     public void relayout() {
         ballooner.relayout();
     }
-
+    
     public void unregisterTypes(AbstractBubbleSupport type) {
         synchronized (types) {
             types.remove(type);
         }
         new EDTRunner() {
-
+            
             @Override
             protected void runInEDT() {
                 if (configPanel != null) {
@@ -196,13 +202,13 @@ public class BubbleNotify {
             }
         };
     }
-
+    
     public void registerType(AbstractBubbleSupport type) {
         synchronized (types) {
             types.add(type);
         }
         new EDTRunner() {
-
+            
             @Override
             protected void runInEDT() {
                 if (configPanel != null) {
@@ -211,21 +217,21 @@ public class BubbleNotify {
             }
         };
     }
-
+    
     private List<AbstractBubbleSupport> types = new ArrayList<AbstractBubbleSupport>();
     private BubbleNotifyConfigPanel     configPanel;
-
+    
     public List<AbstractBubbleSupport> getTypes() {
         synchronized (types) {
             return new ArrayList<AbstractBubbleSupport>(types);
         }
     }
-
+    
     public BubbleNotifyConfigPanel getConfigPanel() {
         if (configPanel == null) {
             configPanel = new BubbleNotifyConfigPanel();
         }
         return configPanel;
     }
-
+    
 }
