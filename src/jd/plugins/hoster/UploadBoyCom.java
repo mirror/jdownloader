ac97988faa30060b98e7708d1846d8136d4f76eb
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-23 12:44:30 +0000

    Uploadboy.com - Adjust file not found regex Re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24783 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UploadBoyCom.java
+++ src/jd/plugins/hoster/UploadBoyCom.java
@@ -140,7 +140,7 @@ public class UploadBoyCom extends PluginForHost {
         br.setFollowRedirects(true);
         prepBrowser(br);
         getPage(link.getDownloadURL());
-        if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n)").matches()) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (new Regex(correctedBR, "(No such file|>File Not Found<|The file was removed by|Reason for deletion:)").matches()) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         if (new Regex(correctedBR, MAINTENANCE).matches()) {
             link.getLinkStatus().setStatusText(MAINTENANCEUSERTEXT);
             return AvailableStatus.TRUE;
