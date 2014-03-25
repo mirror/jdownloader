db09c5d902c15fb52c35d6864b803aa09c5468f9
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-25 20:45:42 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24811 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Zippysharecom.java
+++ src/jd/plugins/hoster/Zippysharecom.java
@@ -234,7 +234,7 @@ public class Zippysharecom extends PluginForHost {
                 }
             } else {
                 DLLINK = br.getRegex("(document\\.getElementById\\(\\'dlbutton\\'\\)\\.href\\s*= \"/.*?\";)").getMatch(0);
-                String math = br.getRegex(".*<script type=\"text/javascript\">(.*?\\.href\\s*=.*?\\}\\s*)</script>\r?\n").getMatch(0);
+                String math = br.getRegex(".*<script type=\"text/javascript\">(.*?" + Pattern.quote(DLLINK) + ".*?\\}\\s*)</script>\r?\n").getMatch(0);
                 if (DLLINK != null && math != null) {
                     math = math.replace(DLLINK, "var result = " + DLLINK);
                     String data = execJS(math, false);
