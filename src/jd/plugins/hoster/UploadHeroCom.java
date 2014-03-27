55d9ab4a4c35642a5da9000afca013c223a2a543
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-03-27 08:37:53 +0000

    UploadHero: -fixed account handling for correct domains -fixes #21458 -fixes #22357 -fixes #23677 -fixes #21460 -fixes #23940 -fixes #21453 -fixes #21456 -fixes #21461 -fixes #21465 -fixes #22404 -fixes #21463 -fixes #21467
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24820 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UploadHeroCom.java
+++ src/jd/plugins/hoster/UploadHeroCom.java
@@ -40,30 +40,57 @@ import jd.plugins.PluginForHost;
 import org.appwork.utils.formatter.SizeFormatter;
 
 /**
- * They have a linkchecker but it's only available for registered users (well maybe it also works so) but it doesn't show the filenames of
- * the links: http://uploadhero.com/api/linktester.php postvalues: "linktest=" + links to check
+ * They have a linkchecker but it's only available for registered users (well maybe it also works so) but it doesn't show the filenames of the links:
+ * http://uploadhero.com/api/linktester.php postvalues: "linktest=" + links to check
  */
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "uploadhero.co", "uploadhero.com" }, urls = { "http://(www\\.)?uploadhero\\.com?/(dl|v)/[A-Za-z0-9]+", "http://NULL_REGEX_BLAAAH12312321" }, flags = { 2, 0 })
 public class UploadHeroCom extends PluginForHost {
-
+    
     private static final String  MAINPAGE = "http://uploadhero.co";
     private static Object        LOCK     = new Object();
     private static AtomicInteger maxPrem  = new AtomicInteger(1);
-
+    
     public UploadHeroCom(PluginWrapper wrapper) {
         super(wrapper);
         this.enablePremium(MAINPAGE + "/premium");
     }
-
+    
+    @Override
+    public boolean isPremiumEnabled() {
+        return "uploadhero.co".equals(getHost());
+    }
+    
+    public Boolean rewriteHost(final DownloadLink link) {
+        if ("uploadhero.co".equals(getHost())) {
+            if (link != null && "uploadhero.com".equals(link.getHost())) {
+                link.setHost("uploadhero.co");
+                return true;
+            }
+            return false;
+        }
+        return null;
+    }
+    
+    public Boolean rewriteHost(final Account acc) {
+        if ("uploadhero.co".equals(getHost())) {
+            if (acc != null && "uploadhero.com".equals(acc.getHoster())) {
+                acc.setHoster("uploadhero.co");
+                return true;
+            }
+            return false;
+        }
+        return null;
+    }
+    
     @Override
     public String getAGBLink() {
         return MAINPAGE + "/tos";
     }
-
+    
     public void correctDownloadLink(DownloadLink link) {
         link.setUrlDownload(link.getDownloadURL().replaceAll("http://(www\\.)?uploadhero\\.com?/(dl|v)/", MAINPAGE + "/dl/"));
     }
-
+    
     // They got a linkchecker but it's only available for registered users: http://uploadhero.co/link-checker
     @Override
     public AvailableStatus requestFileInformation(DownloadLink link) throws IOException, PluginException {
@@ -71,6 +98,8 @@ public class UploadHeroCom extends PluginForHost {
         br.setFollowRedirects(true);
         br.setCookie(MAINPAGE, "lang", "en");
         br.getPage(link.getDownloadURL());
+        generalErrorhandling();
+        if (br.containsHTML("<p>You are currently using a dedicated server") || br.containsHTML("<title>UploadHero - VPN</title>")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "VPN forbidden", 10 * 60 * 1000l);
         if (br.containsHTML("(>The following download is not available on our server|>The file link is invalid|>The uploader has deleted the file|>The file was illegal and was deleted|<title>UploadHero \\- File Sharing made easy\\!</title>)")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         String filename = br.getRegex("<div class=\"nom_de_fichier\">([^<>\"/]+)</div>").getMatch(0);
         if (filename == null) filename = br.getRegex("<title>(.*?) \\- UploadHero</title>").getMatch(0);
@@ -80,41 +109,49 @@ public class UploadHeroCom extends PluginForHost {
         link.setDownloadSize(SizeFormatter.getSize(filesize));
         return AvailableStatus.TRUE;
     }
-
+    
     @Override
     public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
         requestFileInformation(downloadLink);
         doFree(downloadLink);
     }
-
+    
     public void doFree(final DownloadLink downloadLink) throws Exception, PluginException {
         final Regex blockRegex = br.getRegex("/lightbox_block_download\\.php\\?min=(\\d+)\\&sec=(\\d+)\"");
         final String blockmin = blockRegex.getMatch(0);
         final String blocksec = blockRegex.getMatch(1);
         if (blockmin != null && blocksec != null) throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, ((Integer.parseInt(blockmin) + 5) * 60 + Integer.parseInt(blocksec)) * 1001l);
         final String captchaLink = br.getRegex("\"(/captchadl\\.php\\?[a-z0-9]+)\"").getMatch(0);
-        if (captchaLink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (captchaLink == null) {
+            generalErrorhandling();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
         String code = getCaptchaCode(MAINPAGE + captchaLink, downloadLink);
         br.getPage(downloadLink.getDownloadURL() + "?code=" + code);
-        if (!br.containsHTML("\"dddl\"")) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
         String dllink = getDllink();
-        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (dllink == null) {
+            if (!br.containsHTML("\"dddl\"") || br.getRegex("\"(/captchadl\\.php\\?[a-z0-9]+)\"").getMatch(0) != null) throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            generalErrorhandling();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, Encoding.htmlDecode(dllink), false, 1);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
+            if (br.containsHTML("File not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             if (br.containsHTML("404 Not Found")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 60 * 60 * 1000l);
+            if (br.containsHTML("Link sharing is forbidden")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Link sharing is forbidden", 10 * 60 * 1000l);
             generalErrorhandling();
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
     }
-
+    
     private String getDllink() {
         String dllink = br.getRegex("var magicomfg = \\'<a href=\"(http://[^<>\"]*?)\"").getMatch(0);
         if (dllink == null) dllink = br.getRegex("\"(http://storage\\d+\\.uploadhero\\.com?/\\?d=[A-Za-z0-9]+/[^<>\"/]+)\"").getMatch(0);
         return dllink;
     }
-
+    
     @SuppressWarnings("unchecked")
     private void login(Account account, boolean force) throws Exception {
         synchronized (LOCK) {
@@ -157,7 +194,7 @@ public class UploadHeroCom extends PluginForHost {
             }
         }
     }
-
+    
     @Override
     public AccountInfo fetchAccountInfo(final Account account) throws Exception {
         maxPrem.set(1);
@@ -202,7 +239,7 @@ public class UploadHeroCom extends PluginForHost {
         account.setValid(true);
         return ai;
     }
-
+    
     @Override
     public void handlePremium(final DownloadLink link, final Account account) throws Exception {
         requestFileInformation(link);
@@ -216,14 +253,17 @@ public class UploadHeroCom extends PluginForHost {
             if (dllink == null) dllink = getDllink();
             if (dllink == null) {
                 logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
+                generalErrorhandling();
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
             dl = jd.plugins.BrowserAdapter.openDownload(br, link, Encoding.htmlDecode(dllink), true, 0);
             if (dl.getConnection().getContentType().contains("html")) {
                 logger.warning("The final dllink seems not to be a file!");
                 br.followConnection();
+                generalErrorhandling();
                 if (br.containsHTML("File not found")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
                 if (br.containsHTML("404 Not Found")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 60 * 60 * 1000l);
+                if (br.containsHTML("Link sharing is forbidden")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Link sharing is forbidden", 10 * 60 * 1000l);
                 /* check if cookie is outdated */
                 synchronized (LOCK) {
                     Object ret = account.getProperty("cookies", null);
@@ -240,35 +280,35 @@ public class UploadHeroCom extends PluginForHost {
                         throw new PluginException(LinkStatus.ERROR_RETRY);
                     }
                 }
-                generalErrorhandling();
+                
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
             dl.startDownload();
         }
     }
-
+    
     private void generalErrorhandling() throws PluginException {
-        if (br.containsHTML(">UploadHero is on maintenance")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server is in maintenance mode", 30 * 60 * 1000l);
+        if (br.getURL().contains("/optmizing") || br.containsHTML(">UploadHero is on maintenance") || br.containsHTML("Maintenance en cours")) throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "Server is in maintenance mode", 30 * 60 * 1000l);
     }
-
+    
     @Override
     public int getMaxSimultanPremiumDownloadNum() {
         return maxPrem.get();
     }
-
+    
     @Override
     public void reset() {
     }
-
+    
     @Override
     public int getMaxSimultanFreeDownloadNum() {
         return 1;
     }
-
+    
     @Override
     public void resetDownloadlink(DownloadLink link) {
     }
-
+    
     /* NO OVERRIDE!! We need to stay 0.9*compatible */
     public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
         if (acc == null) {
