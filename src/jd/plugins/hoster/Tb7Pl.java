770073369f7289fcaf3bc2d495758fc2bc03522a
editestowy <editestowy@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-04-06 21:40:53 +0000

    Tb7Pl: Added new host to this multihoster
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24915 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Tb7Pl.java
+++ src/jd/plugins/hoster/Tb7Pl.java
@@ -95,7 +95,9 @@ public class Tb7Pl extends PluginForHost {
         }
 
         // unfortunatelly there is no list with supported hosts anywhere on the page
-        final List<String> supportedHostsList = asList("egofiles.com", "catshare.net", "turbobit.net", "rapidgator.net", "rg.to", "netload.in", "uploaded.to", "uploaded.net", "ul.to", "bitshare.com", "freakshare.net", "freakshare.com");
+        // only PNG image at the main page
+        final List<String> supportedHostsList = asList("egofiles.com", "catshare.net", "turbobit.net", "rapidgator.net", "rg.to", "netload.in",
+                "uploaded.to", "uploaded.net", "ul.to", "bitshare.com", "freakshare.net", "freakshare.com", "rapidu.net");
         final ArrayList<String> supportedHosts = new ArrayList<String>(supportedHostsList.size());
         supportedHosts.addAll(supportedHostsList);
 
