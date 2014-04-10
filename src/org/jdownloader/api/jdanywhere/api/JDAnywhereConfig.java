2db0e2cf0886c34736c10ae5c76c5c84c74f9e12
oliver <oliver@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-04-10 12:09:36 +0000

    JDAnywhere - Cfg verschlüsseln
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@24973 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/jdanywhere/api/JDAnywhereConfig.java
+++ src/org/jdownloader/api/jdanywhere/api/JDAnywhereConfig.java
@@ -3,7 +3,9 @@ package org.jdownloader.api.jdanywhere.api;
 import java.util.Map;
 
 import org.appwork.storage.config.ConfigInterface;
+import org.appwork.storage.config.annotations.CryptedStorage;
 
+@CryptedStorage(key = { 0x00, 0x02, 0x11, 0x01, 0x01, 0x54, 0x02, 0x01, 0x01, 0x01, 0x12, 0x01, 0x01, 0x01, 0x22, 0x01 })
 public interface JDAnywhereConfig extends ConfigInterface {
 
     Map<String, CaptchaPushRegistration> getList();
