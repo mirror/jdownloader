fe12c5fdbf3eedfa8726491ea80a930e3899e3ad
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-04-22 17:28:24 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@25135 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/LinkSnappyCom.java
+++ src/jd/plugins/hoster/LinkSnappyCom.java
@@ -366,6 +366,7 @@ public class LinkSnappyCom extends PluginForHost {
         synchronized (LOCK) {
             /** Load cookies */
             br.setCookiesExclusive(true);
+            br.setCookie(COOKIE_HOST, "lang", "en");
             final Object ret = account.getProperty("cookies", null);
             boolean acmatch = Encoding.urlEncode(account.getUser()).equals(account.getStringProperty("name", Encoding.urlEncode(account.getUser())));
             if (acmatch) acmatch = Encoding.urlEncode(account.getPass()).equals(account.getStringProperty("pass", Encoding.urlEncode(account.getPass())));
