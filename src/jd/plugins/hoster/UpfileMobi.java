37f8ef82c58d0a52e2d1dd9fec2d5c6a760d22a2
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-05-13 19:10:41 +0000

    Fixed: upfile.mobi, closes #41954
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@25408 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UpfileMobi.java
+++ src/jd/plugins/hoster/UpfileMobi.java
@@ -48,10 +48,16 @@ public class UpfileMobi extends PluginForHost {
         br.setFollowRedirects(true);
         br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
         br.getPage(link.getDownloadURL());
-        if (br.containsHTML("No file with id <")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("No file with id <"))
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML("Enter password:<br/>")) {
+            link.getLinkStatus().setStatusText("Requires Pre-Download Password!");
+            return AvailableStatus.UNCHECKABLE;
+        }
         final String filename = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
         final String filesize = br.getRegex(">Download (File)?</a> \\(([^<>\"]*?)\\)").getMatch(1);
-        if (filename == null || filesize == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (filename == null || filesize == null)
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         link.setName(Encoding.htmlDecode(filename.trim()));
         link.setDownloadSize(SizeFormatter.getSize(filesize));
         return AvailableStatus.TRUE;
@@ -63,17 +69,22 @@ public class UpfileMobi extends PluginForHost {
         final String fid = new Regex(downloadLink.getDownloadURL(), "(\\d+)$").getMatch(0);
         br.cloneBrowser().openGetConnection("http://upfile.mobi/index.php?page=screen&id=" + fid + "&p=");
         final String ad_sht = br.getRegex("\"(ga\\.php[^<>\"]*?)\"").getMatch(0);
-        if (ad_sht != null) br.cloneBrowser().openGetConnection("http://upfile.mobi/" + ad_sht);
+        if (ad_sht != null)
+            br.cloneBrowser().openGetConnection("http://upfile.mobi/" + ad_sht);
         String dllink = br.getRegex("\"(index\\.php\\?page=download[^<>\"]*?)\"").getMatch(0);
-        if (dllink == null) dllink = br.getRegex("href=\"(index[^<>\"]*?)\">Download File</a>").getMatch(0);
-        if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (dllink == null)
+            dllink = br.getRegex("href=\"(index[^<>\"]*?)\">Download File</a>").getMatch(0);
+        if (dllink == null)
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
 
         dllink = "http://upfile.mobi/" + dllink;
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
-            if (br.containsHTML("Download key is incorrect")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'Download key is incorrect'", 10 * 30 * 1000l);
-            if (br.containsHTML("file and needs moderation, after moderation")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'This is a video file and needs moderation, after moderation you can download this file'", 30 * 30 * 1000l);
+            if (br.containsHTML("Download key is incorrect"))
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'Download key is incorrect'", 10 * 30 * 1000l);
+            if (br.containsHTML("file and needs moderation, after moderation"))
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'This is a video file and needs moderation, after moderation you can download this file'", 30 * 30 * 1000l);
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
