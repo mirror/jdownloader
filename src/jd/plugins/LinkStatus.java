ac63c77fac68ad4884d9c41402c39580ae0c895d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-05-21 11:23:31 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@25495 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/LinkStatus.java
+++ src/jd/plugins/LinkStatus.java
@@ -23,117 +23,117 @@ import org.appwork.utils.BinaryLogic;
 import org.appwork.utils.formatter.StringFormatter;
 
 public class LinkStatus implements Serializable {
-
+    
     /**
      * Controlling & Downloadinterface: Link has been downloaded
      */
     public final static int FINISHED                             = 1 << 1;
-
+    
     /**
      * Plugins: a unknown error occured
      */
     public final static int ERROR_RETRY                          = 1 << 2;
-
+    
     /**
      * Plugins: captcha input has been wrong
      */
     public final static int ERROR_CAPTCHA                        = 1 << 3;
-
+    
     /**
      * Plugins: Download Limit has been reached. Ip is blocked
      */
     public final static int ERROR_IP_BLOCKED                     = 1 << 4;
-
+    
     /**
      * Plugins & Downloadinterface: File not found - file not available any more
      */
     public final static int ERROR_FILE_NOT_FOUND                 = 1 << 5;
-
+    
     /**
      * Plugins & Controlling: A Premium error occured. check value flag for further details
      */
     public static final int ERROR_PREMIUM                        = 1 << 8;
-
+    
     /**
      * this indicates an error during download and we should be able to resume/retry the download
      */
     public static final int ERROR_DOWNLOAD_INCOMPLETE            = 1 << 9;
-
+    
     /**
      * Plugins:Download is not possible right now. May be back later. Maybe server problems or anything like this
      */
     public static final int ERROR_TEMPORARILY_UNAVAILABLE        = 1 << 11;
-
+    
     /**
      * hoster is temporarily not available, dont try other links for this host
      */
     public static final int ERROR_HOSTER_TEMPORARILY_UNAVAILABLE = 1 << 12;
-
+    
     public static final int ERROR_ALREADYEXISTS                  = 1 << 13;
-
+    
     /**
      * download failed in a way we cannot simply retry/restart it
      */
     public static final int ERROR_DOWNLOAD_FAILED                = 1 << 14;
-
+    
     /**
      * Plugins & Downloadinterface: Serious fatal error. No retry. Download will be canceled finally.
      */
     public static final int ERROR_FATAL                          = 1 << 17;
-
+    
     /**
      * Plugin out of date. This flag says that the plugin noticed parsing problems and might be out of date.
      */
     public static final int ERROR_PLUGIN_DEFECT                  = 1 << 22;
-
+    
     /**
-     * timeout reached during download
+     * network IO issues during download
      */
-    public static final int VALUE_TIMEOUT_REACHED                = 1 << 1;
-
+    public static final int VALUE_NETWORK_IO_ERROR               = 1 << 1;
+    
     /**
      * local IO error, eg invalid rights, not existing folder...
      */
     public static final int VALUE_LOCAL_IO_ERROR                 = 1 << 2;
-
+    
     private void readObject(final java.io.ObjectInputStream stream) throws java.io.IOException, ClassNotFoundException {
         /* make sure we set transient variables here */
         stream.defaultReadObject();
     }
-
+    
     private static final long  serialVersionUID = 3885661829491436448L;
-
+    
     private final DownloadLink downloadLink;
-
+    
     private String             errorMessage;
-
+    
     private int                status           = ERROR_RETRY;
     private String             statusText       = null;
     private long               value            = 0;
     private int                retryCount       = 0;
-
+    
     public LinkStatus(final DownloadLink downloadLink) {
         this.downloadLink = downloadLink;
     }
-
+    
     public long getValue() {
         return value;
     }
-
+    
     @Deprecated
     public boolean hasStatus(final int status) {
         return (this.status & status) != 0;
     }
-
+    
     public void setErrorMessage(final String string) {
         errorMessage = string;
     }
-
+    
     private void notifyChanges(LinkStatusProperty property) {
         DownloadLink dl = this.downloadLink;
         if (dl != null) dl.getFilePackage().nodeUpdated(dl, jd.controlling.packagecontroller.AbstractNodeNotifier.NOTIFY.PROPERTY_CHANCE, property);
     }
-
+    
     /**
      * Setzt den Linkstatus. Es dürfen nur LInkStatus.*STATUS ids verwendet werden
      * 
@@ -142,18 +142,18 @@ public class LinkStatus implements Serializable {
     public void setStatus(final int status) {
         this.status = status;
     }
-
+    
     public void setStatusText(final String l) {
         if (statusText != null && statusText.equals(l)) return;
         statusText = l;
         notifyChanges(new LinkStatusProperty(this, LinkStatusProperty.Property.STATUSTEXT, l));
     }
-
+    
     public static String toString(int status) {
         final StringBuilder ret = new StringBuilder();
         for (Field f : LinkStatus.class.getDeclaredFields()) {
             if (java.lang.reflect.Modifier.isStatic(f.getModifiers()) && f.getType() == int.class) {
-
+                
                 try {
                     if (BinaryLogic.containsAll(status, f.getInt(null))) {
                         if (ret.length() > 0) ret.append(", ");
@@ -168,12 +168,12 @@ public class LinkStatus implements Serializable {
         }
         return ret.toString();
     }
-
+    
     @Override
     public String toString() {
         final StringBuilder ret = new StringBuilder();
         ret.append(StringFormatter.fillString(Integer.toBinaryString(status), "0", "", 32) + " < Statuscode\r\n");
-
+        
         if (statusText != null) {
             ret.append("StatusText: ").append(statusText).append("\r\n");
         }
@@ -182,25 +182,25 @@ public class LinkStatus implements Serializable {
         }
         return ret.toString();
     }
-
+    
     public void setRetryCount(final int retryCount) {
         this.retryCount = retryCount;
     }
-
+    
     public int getRetryCount() {
         return retryCount;
     }
-
+    
     public String getStatusText() {
         return statusText;
     }
-
+    
     public String getErrorMessage() {
         return errorMessage;
     }
-
+    
     public int getStatus() {
         return status;
     }
-
+    
 }
