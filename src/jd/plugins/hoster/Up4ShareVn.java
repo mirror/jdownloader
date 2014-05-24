51432851107a3931fff8c4e053abced858ff2a45
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-05-24 17:14:07 +0000

    Fixed: 4shared.vn, account services! This must be the worst hoster for connectivity issues _ever_ - re: PPL-950-49538
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@25596 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Up4ShareVn.java
+++ src/jd/plugins/hoster/Up4ShareVn.java
@@ -17,16 +17,15 @@
 package jd.plugins.hoster;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 import jd.PluginWrapper;
 import jd.config.Property;
+import jd.http.Browser.BrowserException;
 import jd.http.Cookie;
 import jd.http.Cookies;
-import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.Account;
@@ -59,23 +58,11 @@ public class Up4ShareVn extends PluginForHost {
     }
 
     @Override
-    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
         correctDownloadLink(link);
         prepBR();
         this.setBrowserExclusive();
-        URLConnectionAdapter con = null;
-        try {
-            con = br.openGetConnection(link.getDownloadURL());
-            if (con.getResponseCode() == 503) {
-                throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "Server error", 5 * 60 * 1000l);
-            }
-            br.followConnection();
-        } finally {
-            try {
-                con.disconnect();
-            } catch (Throwable e) {
-            }
-        }
+        getPage(link.getDownloadURL());
         if (br.containsHTML(">FID Không hợp lệ\\!|file not found")) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
@@ -100,8 +87,10 @@ public class Up4ShareVn extends PluginForHost {
             throw e;
         }
         account.setValid(true);
+        getPage("/member");
+
         ai.setUnlimitedTraffic();
-        String expire = br.getRegex("Hạn dùng: ([^<>\"]*?)  \\|").getMatch(0);
+        String expire = br.getRegex("Ngày hết hạn: <b>(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2})<").getMatch(0);
         if (expire == null) {
             ai.setExpired(true);
             account.setValid(false);
@@ -199,14 +188,17 @@ public class Up4ShareVn extends PluginForHost {
     public void handlePremium(final DownloadLink link, final Account account) throws Exception {
         requestFileInformation(link);
         login(account, false);
-        br.getPage(link.getDownloadURL());
-        String dllink = br.getRegex("class=\\'\\'> <a href=\\'(http://.*?)\\'").getMatch(0);
+        getPage(link.getDownloadURL());
+        String dllink = br.getRedirectLocation();
         if (dllink == null) {
-            dllink = br.getRegex("(\\'|\")(http://sv\\d+\\.4share\\.vn/[^<>\"]*?)(\\'|\")").getMatch(1);
-        }
-        if (dllink == null) {
-            logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            dllink = br.getRegex("class=\\'\\'> <a href=\\'(http://.*?)\\'").getMatch(0);
+            if (dllink == null) {
+                dllink = br.getRegex("(\\'|\")(http://sv\\d+\\.4share\\.vn/[^<>\"]*?)(\\'|\")").getMatch(1);
+                if (dllink == null) {
+                    logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+            }
         }
         int maxChunks = 0;
         if (link.getBooleanProperty(NOCHUNKS, false)) {
@@ -252,11 +244,11 @@ public class Up4ShareVn extends PluginForHost {
     @SuppressWarnings("unchecked")
     private void login(final Account account, final boolean force) throws Exception {
         synchronized (LOCK) {
+            boolean redirect = this.br.isFollowingRedirects();
             try {
                 /** Load cookies */
                 this.setBrowserExclusive();
                 prepBR();
-                br.setFollowRedirects(true);
                 final Object ret = account.getProperty("cookies", null);
                 boolean acmatch = Encoding.urlEncode(account.getUser()).equals(account.getStringProperty("name", Encoding.urlEncode(account.getUser())));
                 if (acmatch) {
@@ -273,18 +265,18 @@ public class Up4ShareVn extends PluginForHost {
                         return;
                     }
                 }
-
-                br.getPage("http://up.4share.vn/");
-                br.postPage("http://up.4share.vn/?control=login", "inputUserName=" + Encoding.urlEncode(account.getUser()) + "&inputPassword=" + Encoding.urlEncode(account.getPass()) + "&rememberlogin=on");
+                br.setFollowRedirects(true);
+                getPage("http://up.4share.vn/");
+                postPage("http://up.4share.vn/index/login", "username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(account.getPass()) + "&submit=+%C4%90%C4%82NG+NH%E1%BA%ACP+&remember_login=on");
                 final String lang = System.getProperty("user.language");
-                if (br.getCookie(MAINPAGE, "userid") == null || br.getCookie(MAINPAGE, "passwd") == null) {
+                if (br.getCookie(MAINPAGE, "info1") == null || br.getCookie(MAINPAGE, "info2") == null) {
                     if ("de".equalsIgnoreCase(lang)) {
                         throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nUngültiger Benutzername oder ungültiges Passwort!\r\nSchnellhilfe: \r\nDu bist dir sicher, dass dein eingegebener Benutzername und Passwort stimmen?\r\nFalls dein Passwort Sonderzeichen enthält, ändere es und versuche es erneut!", PluginException.VALUE_ID_PREMIUM_DISABLE);
                     } else {
                         throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nInvalid username/password!\r\nQuick help:\r\nYou're sure that the username and password you entered are correct?\r\nIf your password contains special characters, change it (remove them) and try again!", PluginException.VALUE_ID_PREMIUM_DISABLE);
                     }
                 }
-                if (!br.containsHTML("\\(<strong>VIP</strong>") && !br.containsHTML("\\[<strong>VIP</strong>\\]")) {
+                if (!br.containsHTML(">TK <b>VIP</b>") && !br.containsHTML("Hạn sử dụng VIP: \\d{2}-\\d{2}-\\d{4}<")) {
                     if ("de".equalsIgnoreCase(lang)) {
                         throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nUngültiger Account Typ!\r\nDas ist ein kostenloser Account.\r\nJDownloader unterstützt keine kostenlosen Accounts für diesen Hoster!", PluginException.VALUE_ID_PREMIUM_DISABLE);
                     } else {
@@ -303,7 +295,49 @@ public class Up4ShareVn extends PluginForHost {
             } catch (final PluginException e) {
                 account.setProperty("cookies", Property.NULL);
                 throw e;
+            } finally {
+                this.br.setFollowRedirects(redirect);
             }
+        }
+    }
+
+    private void getPage(final String string) throws Exception {
+        if (string == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        for (int i = 0; i != 4; i++) {
+            try {
+                br.getPage(string);
+            } catch (final BrowserException e) {
+                if (e.getCause() != null && e.getCause().toString().contains("ConnectException")) {
+                    if (i + 1 == 4) {
+                        throw e;
+                    } else {
+                        continue;
+                    }
+                } else {
+                    throw e;
+                }
+            }
+            break;
+        }
+    }
+
+    private void postPage(String string, String string2) throws Exception {
+        if (string == null || string2 == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        for (int i = 0; i != 4; i++) {
+            try {
+                br.postPage(string, string2);
+            } catch (final BrowserException e) {
+                if (e.getCause().toString().contains("ConnectException")) {
+                    continue;
+                } else {
+                    throw e;
+                }
+            }
+            break;
         }
     }
 
