b9f968063f4d14fbfe63635bb16339104464a8f1
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-06-13 16:35:23 +0000

    Fixed: upfile.mobi, pre-download password support - closes #42028
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@25914 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UpfileMobi.java
+++ src/jd/plugins/hoster/UpfileMobi.java
@@ -19,19 +19,24 @@ package jd.plugins.hoster;
 import java.io.IOException;
 
 import jd.PluginWrapper;
+import jd.config.Property;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
+import jd.parser.html.Form;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
 import org.appwork.utils.formatter.SizeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "upfile.mobi" }, urls = { "http://(www\\.)?upfile\\.mobi/\\d+" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "upfile.mobi" }, urls = { "http://(?:www\\.)?upfile\\.mobi/(\\d+(\\.[a-f0-9]{32})?|index\\.php\\?page=file&f=\\d+)" }, flags = { 0 })
 public class UpfileMobi extends PluginForHost {
+
+    private final String password_required = "Enter password:<br/>";
 
     public UpfileMobi(PluginWrapper wrapper) {
         super(wrapper);
@@ -48,16 +53,18 @@ public class UpfileMobi extends PluginForHost {
         br.setFollowRedirects(true);
         br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0");
         br.getPage(link.getDownloadURL());
-        if (br.containsHTML("No file with id <"))
+        if (br.containsHTML("No file with id <")) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        if (br.containsHTML("Enter password:<br/>")) {
+        }
+        if (br.containsHTML(password_required)) {
             link.getLinkStatus().setStatusText("Requires Pre-Download Password!");
             return AvailableStatus.UNCHECKABLE;
         }
         final String filename = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
         final String filesize = br.getRegex(">Download (File)?</a> \\(([^<>\"]*?)\\)").getMatch(1);
-        if (filename == null || filesize == null)
+        if (filename == null || filesize == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
         link.setName(Encoding.htmlDecode(filename.trim()));
         link.setDownloadSize(SizeFormatter.getSize(filesize));
         return AvailableStatus.TRUE;
@@ -66,28 +73,75 @@ public class UpfileMobi extends PluginForHost {
     @Override
     public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
         requestFileInformation(downloadLink);
-        final String fid = new Regex(downloadLink.getDownloadURL(), "(\\d+)$").getMatch(0);
+        final String fid = getFUID(downloadLink);
+        if (br.containsHTML(password_required)) {
+            Form password_form = br.getForm(0);
+            for (Form f : br.getForms()) {
+                if (f.containsHTML(password_required)) {
+                    password_form = f;
+                    break;
+                }
+            }
+            if (password_form != null) {
+                String passCode = downloadLink.getStringProperty("pass", null);
+                if (passCode == null) {
+                    passCode = Plugin.getUserInput("Password?", downloadLink);
+                }
+                if (passCode == null) {
+                    logger.info("Pre-Download Password: User has entered blank password, exiting.");
+                    passCode = null;
+                    downloadLink.setProperty("pass", Property.NULL);
+                    throw new PluginException(LinkStatus.ERROR_RETRY);
+                }
+                logger.info("Pre-Download Password: User entered: " + passCode);
+                password_form.put("key", Encoding.urlEncode(passCode));
+                br.submitForm(password_form);
+                if (br.containsHTML(password_required)) {
+                    logger.info("Incorrect Pre-Download Password!");
+                    throw new PluginException(LinkStatus.ERROR_RETRY);
+                }
+            } else {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+        }
+
         br.cloneBrowser().openGetConnection("http://upfile.mobi/index.php?page=screen&id=" + fid + "&p=");
         final String ad_sht = br.getRegex("\"(ga\\.php[^<>\"]*?)\"").getMatch(0);
-        if (ad_sht != null)
+        if (ad_sht != null) {
             br.cloneBrowser().openGetConnection("http://upfile.mobi/" + ad_sht);
+        }
         String dllink = br.getRegex("\"(index\\.php\\?page=download[^<>\"]*?)\"").getMatch(0);
-        if (dllink == null)
+        if (dllink == null) {
             dllink = br.getRegex("href=\"(index[^<>\"]*?)\">Download File</a>").getMatch(0);
-        if (dllink == null)
+        }
+        if (dllink == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
 
         dllink = "http://upfile.mobi/" + dllink;
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
-            if (br.containsHTML("Download key is incorrect"))
+            if (br.containsHTML("Download key is incorrect")) {
                 throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'Download key is incorrect'", 10 * 30 * 1000l);
-            if (br.containsHTML("file and needs moderation, after moderation"))
+            }
+            if (br.containsHTML("file and needs moderation, after moderation")) {
                 throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error: 'This is a video file and needs moderation, after moderation you can download this file'", 30 * 30 * 1000l);
+            }
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
+    }
+
+    private String getFUID(final DownloadLink downloadLink) {
+        if (downloadLink == null) {
+            return null;
+        }
+        String s = new Regex(downloadLink.getDownloadURL(), "http://(?:www\\.)?upfile\\.mobi/(\\d+)").getMatch(0);
+        if (s == null) {
+            s = new Regex(downloadLink.getDownloadURL(), "http://(?:www\\.)?upfile\\.mobi/index\\.php\\?page=file&f=(\\d+)").getMatch(0);
+        }
+        return s;
     }
 
     @Override
