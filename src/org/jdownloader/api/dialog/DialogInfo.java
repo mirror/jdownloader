954bd868db16316b944cbf2d3d68b5ea11222954
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-02 13:46:16 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26147 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/dialog/DialogInfo.java
+++ src/org/jdownloader/api/dialog/DialogInfo.java
@@ -1,6 +1,8 @@
 package org.jdownloader.api.dialog;
 
+import org.appwork.storage.JSonStorage;
 import org.appwork.storage.Storable;
+import org.appwork.utils.reflection.Clazz;
 import org.jdownloader.myjdownloader.client.bindings.DialogStorable;
 
 public class DialogInfo extends DialogStorable implements Storable {
@@ -8,4 +10,14 @@ public class DialogInfo extends DialogStorable implements Storable {
         super();
     }
 
+    public void put(final String key, final Object value) {
+        if (value == null) {
+            return;
+        }
+        if (Clazz.isPrimitive(value.getClass()) || value instanceof String) {
+            properties.put(key, value.toString());
+        } else {
+            properties.put(key, JSonStorage.toString(value));
+        }
+    }
 }
