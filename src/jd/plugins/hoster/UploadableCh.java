14e270a1e087b958b036b730c4aee427b1601356
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-18 09:04:50 +0000

    Host: Uploadable.ch - Adjust error handling fixes #47903
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26254 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UploadableCh.java
+++ src/jd/plugins/hoster/UploadableCh.java
@@ -151,10 +151,11 @@ public class UploadableCh extends PluginForHost {
         if (dl.getConnection().getContentType().contains("html")) {
             logger.info("Finallink does not lead to a file, continuing...");
             br.followConnection();
-            /* Error-links: =http://www.uploadable.ch/l-error.php?error_code=ERRORCODE */
+            /* Error-links: http://www.uploadable.ch/l-error.php?error_code=ERRORCODE */
             /* Your download link has expired */
             if (br.containsHTML("error_code=1702")) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 1702 ('Downloadlink has expired')", 5 * 60 * 1000l);
+                downloadLink.setProperty("uploadabledirectlink", null);
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Download link has expired", 6 * 1000l);
             }
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
