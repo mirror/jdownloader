f9c1fb5bdb3db782769f911f61588cf5f3c38a91
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-21 08:43:44 +0000

    Fixed: r8link.com, captcha loop - re: G
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26269 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/R8LinkCom.java
+++ src/jd/plugins/decrypter/R8LinkCom.java
@@ -21,12 +21,17 @@ import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
+import jd.plugins.hoster.DirectHTTP;
 import jd.utils.JDUtilities;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "r8link.com" }, urls = { "http://(www\\.)?r8link\\.com/[A-Za-z0-9]+" }, flags = { 0 })
@@ -36,6 +41,8 @@ public class R8LinkCom extends PluginForDecrypt {
         super(wrapper);
     }
 
+    private final String reCaptcha = "api\\.recaptcha\\.net|google\\.com/recaptcha/api/";
+
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         String parameter = param.toString();
@@ -44,22 +51,34 @@ public class R8LinkCom extends PluginForDecrypt {
             logger.info("Link offline: " + parameter);
             return decryptedLinks;
         }
-        final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-        final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((jd.plugins.hoster.DirectHTTP) recplug).getReCaptcha(br);
-        rc.parse();
-        rc.load();
-        for (int i = 0; i <= 5; i++) {
-            File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-            String c = getCaptchaCode(cf, param);
-            rc.setCode(c);
-            if (br.containsHTML("(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)")) {
-                rc.reload();
-                continue;
+        final int repeat = 4;
+        for (int i = 1; i <= repeat; i++) {
+            if (br.containsHTML(reCaptcha)) {
+                final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+                final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+                rc.parse();
+                final String[] v = br.getRegex("(\\w)\\.name\\s*=\\s*(\"|')(\\w+)\";\\s*\\1\\.value\\s*=\\s*(\"|')([a-f0-9]+)\\4").getRow(0);
+                if (v == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                Form r = rc.getForm();
+                r.put(v[2], v[4]);
+                rc.load();
+                final File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+                final String c = getCaptchaCode(cf, param);
+                r.put("recaptcha_challenge_field", rc.getChallenge());
+                r.put("recaptcha_response_field", Encoding.urlEncode(c));
+                br.submitForm(r);
+                if (br.containsHTML(reCaptcha) && i + 1 != repeat) {
+                    continue;
+                } else if (br.containsHTML(reCaptcha) && i + 1 == repeat) {
+                    throw new Exception(DecrypterException.CAPTCHA);
+                } else {
+                    break;
+                }
             }
-            break;
         }
-        if (br.containsHTML("(api\\.recaptcha\\.net|google\\.com/recaptcha/api/)")) throw new Exception(DecrypterException.CAPTCHA);
-        final String finallink = br.getRegex("HTTP\\-EQUIV=\\'Refresh\\' CONTENT=\\'\\d+;URL=(http://[^<>\"]*?)\\'>").getMatch(0);
+        final String finallink = br.getRegex("HTTP-EQUIV='Refresh' CONTENT='\\d+;URL=(http://[^<>\"]*?)'>").getMatch(0);
         if (finallink == null) {
             logger.warning("Decrypter broken for link: " + parameter);
             return null;
