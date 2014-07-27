84f5ff38c65c79eaf9ac5b98d1ce6bb823a742dd
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-27 21:02:06 +0000

    Fixed: divshare.com, audio download, removed unnecessary escaping, merged common outcomes, new code formatting - re: FTD-767-51117
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26338 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/DivShareCom.java
+++ src/jd/plugins/hoster/DivShareCom.java
@@ -67,14 +67,16 @@ public class DivShareCom extends PluginForHost {
             downloadLink.getLinkStatus().setStatusText("Divshare database connection error");
             return AvailableStatus.UNCHECKABLE;
         }
-        if (br.containsHTML("<title>DivShare \\- Password-Protected File</title>")) {
+        if (br.containsHTML("<title>DivShare - Password-Protected File</title>")) {
             Form pw = br.getForm(0);
             String password = null;
             // first look plugin
             password = this.getPluginConfig().getStringProperty("pass", password);
             // then downloadlink
             password = downloadLink.getStringProperty("pass", password);
-            if (password == null) password = PluginUtils.askPassword(this);
+            if (password == null) {
+                password = PluginUtils.askPassword(this);
+            }
             pw.put("gallery_password", password);
             br.submitForm(pw);
             pw = br.getFormbyKey("gallery_password");
@@ -96,11 +98,12 @@ public class DivShareCom extends PluginForHost {
                 getPluginConfig().save();
             }
         }
-        // handling für die links, die "direct" beinhalten (diese starten
-        // sofort)
+        // handling für die links, die "direct" beinhalten (diese starten sofort)
         if (br.getURL().contains("direct")) {
-            String redirect = br.getRegex("If it doesn\\'t\\, <a href=\"(.*?)\">click here<").getMatch(0);
-            if (redirect == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            String redirect = br.getRegex("If it doesn't, <a href=\"(.*?)\">click here<").getMatch(0);
+            if (redirect == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
             br.setFollowRedirects(true);
             URLConnectionAdapter con = null;
             try {
@@ -111,9 +114,12 @@ public class DivShareCom extends PluginForHost {
                     return AvailableStatus.TRUE;
                 }
                 br.followConnection();
-                if (br.containsHTML("This file is unavailable until")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
-                if (br.containsHTML("Sorry, we couldn't find this file.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                if (br.containsHTML("This file is secured by Divshare.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                if (br.containsHTML("This file is unavailable until")) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE);
+                }
+                if (br.containsHTML("Sorry, we couldn't find this file\\.|This file is secured by Divshare\\.")) {
+                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                }
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             } finally {
                 try {
@@ -123,26 +129,38 @@ public class DivShareCom extends PluginForHost {
                 br.setFollowRedirects(false);
             }
         } else {
-            // handling für die anderen Links (mit "download" bzw. "image" im
-            // Link
-            if (br.containsHTML("Sorry, we couldn't find this file.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            if (br.containsHTML("This file is secured by Divshare.")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            // handling für die anderen Links (mit "download" bzw. "image" im Link
+            if (br.containsHTML("Sorry, we couldn't find this file\\.|This file is secured by Divshare\\.")) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
             // alte Methode um an den filename der Downloadseite zu kommen
             // br.getRegex("<title>(.*?) - DivShare</title>").getMatch(0);
             String filename0 = downloadLink.getDownloadURL().replaceAll("divshare.com/(download|image)", "divshare.com/sharing");
             final Browser br2 = br.cloneBrowser();
             br2.getPage(filename0);
-            String filename = br2.getRegex("no\\-repeat left center;\">(.*?)</span>").getMatch(0);
-            if (filename == null) filename = br.getRegex("<div class=\"fileNameHeader\">([^<>\"]*?)</div>").getMatch(0);
-            if (filename == null) filename = br.getRegex("<title>([^<>\"]*?) \\- DivShare</title>").getMatch(0);
-            if (filename == null) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            String filename = br2.getRegex("no-repeat left center;\">(.*?)</span>").getMatch(0);
+            if (filename == null) {
+                filename = br.getRegex("<div class=\"fileNameHeader\">([^<>\"]+)</div>").getMatch(0);
+                if (filename == null) {
+                    filename = br.getRegex("<title>([^<>\"]+) - DivShare</title>").getMatch(0);
+                    if (filename == null) {
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    }
+                }
+            }
             downloadLink.setName(filename.replaceAll("(\\)|\\()", "").replace("_", "."));
             String id = br2.getRegex("divshare.com/download/([0-9a-f\\-]+)").getMatch(0);
-            if (id != null) br2.postPage("http://www.divshare.com/scripts/ajax/v5/fileStats.php", "code=" + id);
+            if (id != null) {
+                br2.postPage("http://www.divshare.com/scripts/ajax/v5/fileStats.php", "code=" + id);
+            }
             String size = br2.getRegex("File Size:.*?>.*?([0-9\\.]+)").getMatch(0);
             String sizef = br2.getRegex("tiny.*?>([KGTBM]+)").getMatch(0);
-            if (sizef != null) size = size + " " + sizef;
-            if (size != null) downloadLink.setDownloadSize(SizeFormatter.getSize(size.replaceAll(",", "\\.")));
+            if (sizef != null) {
+                size = size + " " + sizef;
+            }
+            if (size != null) {
+                downloadLink.setDownloadSize(SizeFormatter.getSize(size.replaceAll(",", "\\.")));
+            }
         }
         return AvailableStatus.TRUE;
     }
@@ -150,24 +168,29 @@ public class DivShareCom extends PluginForHost {
     @Override
     public void handleFree(final DownloadLink link) throws Exception {
         requestFileInformation(link);
-        if (br.containsHTML("Cannot connect to the rep database because:")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Cannot connect to the rep database because:", 10 * 60 * 1000l);
+        if (br.containsHTML("Cannot connect to the rep database because:")) {
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Cannot connect to the rep database because:", 10 * 60 * 1000l);
+        }
         br.setFollowRedirects(true);
         if (br.getURL().contains("direct")) {
             String dllink;
             dllink = br.getURL();
-            if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            if (dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
             link.setFinalFileName(null);
             dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, false, 1);
             dl.startDownload();
 
         } else {
             String infolink = link.getDownloadURL();
-            // Check ob ein Passwort verlangt wird, wenn keins verlangt wird
-            // wird der Link sofort geändert und geladen (unten ists dann
+            // Check ob ein Passwort verlangt wird, wenn keins verlangt wird wird der Link sofort geändert und geladen (unten ists dann
             // infolink2)!
             if (br.containsHTML("This file is password protected.")) {
                 Form form = br.getForm(0);
-                if (form == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                if (form == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
                 String passCode = null;
                 if (link.getStringProperty("pass", null) == null) {
                     passCode = getUserInput(null, link);
@@ -188,11 +211,13 @@ public class DivShareCom extends PluginForHost {
             }
             final String infolink2 = infolink.replaceAll("divshare\\.com/(download|image)", "divshare.com/download/launch");
             getPage(infolink2);
-            String dllink = br.getRegex("\"(http://[a-z0-9]+\\.divshare\\.com/launch[^<>\"]*?)\"").getMatch(0);
+            String dllink = br.getRegex("(https?://[a-z0-9]+\\.divshare\\.com/launch[^<>\"]+)\"").getMatch(0);
             if (dllink == null && br.containsHTML("application/mp3")) {
-                dllink = br.getRegex("var ch1  = escape\\(\\'(http://[^<>\"]*?)\\'\\);").getMatch(0);
+                dllink = br.getRegex("var ch1  = escape\\('(https?://[^<>']+)'\\);").getMatch(0);
             }
-            if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            if (dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
             dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, false, 1);
             if (dl.getConnection().getContentType().contains("html")) {
                 br.followConnection();
@@ -206,7 +231,9 @@ public class DivShareCom extends PluginForHost {
 
     private void getPage(final String url) throws IOException, PluginException {
         br.getPage(url);
-        if (br.containsHTML("Cannot connect to the rep database because:")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Cannot connect to the rep database because:", 10 * 60 * 1000l);
+        if (br.containsHTML("Cannot connect to the rep database because:")) {
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Cannot connect to the rep database because:", 10 * 60 * 1000l);
+        }
     }
 
     @Override
