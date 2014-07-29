0dc7f38cfc2086abdb7da8395b061bc02bd28e02
phg <phg@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-29 16:09:45 +0000

    Add the verification for the premium account in order to * Not have an account that is listed but not checked * Not have an account that is a free account
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26356 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/controlling/filter/CompiledPluginStatusFiler.java
+++ src/org/jdownloader/controlling/filter/CompiledPluginStatusFiler.java
@@ -1,8 +1,11 @@
 package org.jdownloader.controlling.filter;
 
+import java.util.ArrayList;
+
 import jd.controlling.AccountController;
 import jd.controlling.linkcrawler.CrawledLink;
 import jd.gui.swing.jdgui.views.settings.panels.linkgrabberfilter.editdialog.PluginStatusFilter;
+import jd.plugins.Account;
 
 public class CompiledPluginStatusFiler extends PluginStatusFilter {
 
@@ -17,9 +20,7 @@ public class CompiledPluginStatusFiler extends PluginStatusFilter {
         case IS:
             switch (getPluginStatus()) {
             case PREMIUM:
-                if (AccountController.getInstance().hasAccounts(link.getHost())) return true;
-                if (AccountController.getInstance().hasMultiHostAccounts(link.getHost())) return true;
-                return false;
+                return VerifyPremium(link);
             case AUTOCAPTCHA:
                 return link.hasAutoCaptcha() || !link.hasCaptcha(null);
             case NO_DIRECT_HTTP:
@@ -29,8 +30,7 @@ public class CompiledPluginStatusFiler extends PluginStatusFilter {
         case ISNOT:
             switch (getPluginStatus()) {
             case PREMIUM:
-                if (!AccountController.getInstance().hasAccounts(link.getHost()) && !AccountController.getInstance().hasMultiHostAccounts(link.getHost())) return true;
-                return false;
+                return !VerifyPremium(link);
             case AUTOCAPTCHA:
                 return !link.hasAutoCaptcha() && link.hasCaptcha(null);
             case NO_DIRECT_HTTP:
@@ -38,5 +38,33 @@ public class CompiledPluginStatusFiler extends PluginStatusFilter {
             }
         }
         return false;
+    }
+
+    /**
+     * Verify if there is at least one premium account valid
+     * 
+     * @param link
+     *            Link that we want to werify
+     * @return true if a premium account is associated, false otherwise.
+     */
+    private boolean VerifyPremium(CrawledLink link) {
+        boolean bPremium = false;
+        Boolean bAccount = AccountController.getInstance().hasAccounts(link.getHost());
+        Boolean bMultiHost = AccountController.getInstance().hasMultiHostAccounts(link.getHost());
+        if (bAccount || bMultiHost) {
+            // There is at least one account, verify if there is at least one premium account valid
+            ArrayList<Account> alAccount = AccountController.getInstance().getValidAccounts(link.getHost());
+            if (alAccount.size() > 0) {
+                // There is some valid account, verify if they are valid premium
+                for (Account AccountToVerify : alAccount) {
+                    long lgValidUntil = AccountToVerify.getAccountInfo().getValidUntil();
+                    if (lgValidUntil > 0) {
+                        bPremium = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return bPremium;
     }
 }
