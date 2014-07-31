906921a6ca06592bc6d524173fca03ee48e7e85a
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-07-31 18:35:19 +0000

    Fixed: fileom.com, download limit reached - closes #49070
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26375 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FileOmCom.java
+++ src/jd/plugins/hoster/FileOmCom.java
@@ -162,7 +162,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * @author raztoki
-     * 
+     *
      * @category 'Experimental', Mods written July 2012 - 2013
      * */
     public FileOmCom(PluginWrapper wrapper) {
@@ -332,7 +332,7 @@ public class FileOmCom extends PluginForHost {
     /**
      * Provides alternative linkchecking method for a single link at a time. Can be used as generic failover, though kinda pointless as this
      * method doesn't give filename...
-     * 
+     *
      * */
     private String[] altAvailStat(final DownloadLink downloadLink, final String[] fileInfo) throws Exception {
         Browser alt = new Browser();
@@ -510,7 +510,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Removes patterns which could break the plugin due to fake/hidden HTML, or false positives caused by HTML comments.
-     * 
+     *
      * @throws Exception
      * @author raztoki
      */
@@ -604,7 +604,8 @@ public class FileOmCom extends PluginForHost {
             }
         }
         // monitor this
-        if (cbr.containsHTML("(class=\"err\">(<center>)?You have reached the download(\\-| )limit[^<]+for last[^<]+)")) {
+        // class= "err"><center>You have reached the download-limit: 1024 Mb</center></div>
+        if (cbr.containsHTML("(class=\"err\">(<center>)?You have reached the download(\\-| )limit([^<]+for last[^<]+|: \\d+[^<]+))")) {
             /*
              * Indication of when you've reached the max download limit for that given session! Usually shows how long the session was
              * recorded from x time (hours|days) which can trigger false positive below wait handling. As its only indication of what's
@@ -1063,7 +1064,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Rules to prevent new downloads from commencing
-     * 
+     *
      * */
     public boolean canHandle(DownloadLink downloadLink, Account account) {
         if (downloadLink.getBooleanProperty("requiresPremiumAccount", false) && (account == null || account.getBooleanProperty("free", false))) {
@@ -1096,7 +1097,7 @@ public class FileOmCom extends PluginForHost {
      * Corrects downloadLink.urlDownload().<br/>
      * <br/>
      * The following code respect the hoster supported protocols via plugin boolean settings and users config preference
-     * 
+     *
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1171,7 +1172,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * resets provided accounts properties fields. This will allow next login to be a full login!
-     * 
+     *
      * @author raztoki
      * */
     private void resetAccount(final Account account) {
@@ -1183,7 +1184,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * If default browser contains cookie key name with a value comparison against any of 'loginCookies' array, it will return true
-     * 
+     *
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1206,7 +1207,7 @@ public class FileOmCom extends PluginForHost {
      * If default browser contains ALL cookies within 'loginCookies' array, it will return true<br />
      * <br />
      * NOTE: loginCookies[] can only contain true names! Remove all dead names from array!
-     * 
+     *
      * @author raztoki
      * */
     private boolean matchesLoginCookies() {
@@ -1233,7 +1234,7 @@ public class FileOmCom extends PluginForHost {
     /**
      * Gets page <br />
      * - natively supports silly cloudflare anti DDoS crapola
-     * 
+     *
      * @author raztoki
      */
     private void getPage(final String page) throws Exception {
@@ -1378,7 +1379,7 @@ public class FileOmCom extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     * 
+     *
      * @version 0.2
      * @author raztoki
      * */
@@ -1464,7 +1465,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * captcha processing can be used download/login/anywhere assuming the submit values are the same (they usually are)...
-     * 
+     *
      * @author raztoki
      * */
     private Form captchaForm(DownloadLink downloadLink, Form form) throws Exception {
@@ -1634,13 +1635,13 @@ public class FileOmCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     * 
+     *
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     * 
+     *
      * @param controlSlot
      *            (+1|-1)
      * */
@@ -1661,9 +1662,9 @@ public class FileOmCom extends PluginForHost {
     /**
      * ControlSimHost, On error it will set the upper mark for 'max sim dl per host'. This will be the new 'static' setting used going
      * forward. Thus prevents new downloads starting when not possible and is self aware and requires no coder interaction.
-     * 
+     *
      * @param account
-     * 
+     *
      * @category 'Experimental', Mod written February 2013
      * */
     private void controlSimHost(final Account account) {
@@ -1700,7 +1701,7 @@ public class FileOmCom extends PluginForHost {
      * This matches dllink against an array of used 'host' servers. Use this when site have multiple download servers and they allow x
      * connections to ip/host server. Currently JD allows a global connection controller and doesn't allow for handling of different
      * hosts/IP setup. This will help with those situations by allowing more connection when possible.
-     * 
+     *
      * @param Account
      *            Account that's been used, can be null
      * @param DownloadLink
@@ -1805,7 +1806,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Sets Key and Values to respective Account stored within hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param x
@@ -1855,7 +1856,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Returns String key from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1877,7 +1878,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Returns integer value from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1899,7 +1900,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Returns true if hostMap contains 'key'
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param key
@@ -1924,7 +1925,7 @@ public class FileOmCom extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     * 
+     *
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -1941,7 +1942,7 @@ public class FileOmCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     * 
+     *
      * @param key
      *            name
      * @param value
@@ -1971,9 +1972,9 @@ public class FileOmCom extends PluginForHost {
     /**
      * If form contain both " and ' quotation marks within input fields it can return null values, thus you submit wrong/incorrect data re:
      * InputField parse(final String data). Affects revision 19688 and earlier!
-     * 
+     *
      * TODO: remove after JD2 goes stable!
-     * 
+     *
      * @author raztoki
      * */
     private Form cleanForm(Form form) {
@@ -2003,7 +2004,7 @@ public class FileOmCom extends PluginForHost {
     /**
      * This allows backward compatibility for design flaw in setHtmlCode(), It injects updated html into all browsers that share the same
      * request id. This is needed as request.cloneRequest() was never fully implemented like browser.cloneBrowser().
-     * 
+     *
      * @param ibr
      *            Import Browser
      * @param t
