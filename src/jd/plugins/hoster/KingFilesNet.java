be4ded2f5843c9bc9b28ba00b96f9633e087578f
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-08-10 15:23:16 +0000

    Fixed: kingfiles.net, obstructions removed - re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26433 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/KingFilesNet.java
+++ src/jd/plugins/hoster/KingFilesNet.java
@@ -161,7 +161,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * @author raztoki
-     * 
+     *
      * @category 'Experimental', Mods written July 2012 - 2013
      * */
     public KingFilesNet(PluginWrapper wrapper) {
@@ -331,7 +331,7 @@ public class KingFilesNet extends PluginForHost {
     /**
      * Provides alternative linkchecking method for a single link at a time. Can be used as generic failover, though kinda pointless as this
      * method doesn't give filename...
-     * 
+     *
      * */
     private String[] altAvailStat(final DownloadLink downloadLink, final String[] fileInfo) throws Exception {
         Browser alt = new Browser();
@@ -463,7 +463,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Removes patterns which could break the plugin due to fake/hidden HTML, or false positives caused by HTML comments.
-     * 
+     *
      * @throws Exception
      * @author raztoki
      */
@@ -482,8 +482,7 @@ public class KingFilesNet extends PluginForHost {
             }
         }
         regexStuff.add("<!(--.*?--)>");
-        regexStuff.add("(<div[^>]+display: ?none;[^>]+>.*?</div>)");
-        regexStuff.add("(visibility:hidden>.*?<)");
+        regexStuff.add("(<\\s*(\\w+)[^>]*(\"|')[\\w:;\\s]*(visibility\\s*:\\s*hidden|display\\s*:\\s*none)[\\w:;\\s]*\\3[^>]*(>.*?<\\s*/\\2[^>]*>|/\\s*>))");
 
         for (String aRegex : regexStuff) {
             String results[] = new Regex(toClean, aRegex).getColumn(0);
@@ -949,7 +948,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Rules to prevent new downloads from commencing
-     * 
+     *
      * */
     public boolean canHandle(DownloadLink downloadLink, Account account) {
         if (downloadLink.getBooleanProperty("requiresPremiumAccount", false) && (account == null || account.getBooleanProperty("free", false))) {
@@ -982,7 +981,7 @@ public class KingFilesNet extends PluginForHost {
      * Corrects downloadLink.urlDownload().<br/>
      * <br/>
      * The following code respect the hoster supported protocols via plugin boolean settings and users config preference
-     * 
+     *
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1133,7 +1132,7 @@ public class KingFilesNet extends PluginForHost {
     /**
      * Gets page <br />
      * - natively supports silly cloudflare anti DDoS crapola
-     * 
+     *
      * @author raztoki
      */
     private void getPage(final String page) throws Exception {
@@ -1278,7 +1277,7 @@ public class KingFilesNet extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     * 
+     *
      * @version 0.2
      * @author raztoki
      * */
@@ -1364,7 +1363,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * captcha processing can be used download/login/anywhere assuming the submit values are the same (they usually are)...
-     * 
+     *
      * @author raztoki
      * */
     private Form captchaForm(DownloadLink downloadLink, Form form) throws Exception {
@@ -1534,13 +1533,13 @@ public class KingFilesNet extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     * 
+     *
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     * 
+     *
      * @param controlSlot
      *            (+1|-1)
      * */
@@ -1561,9 +1560,9 @@ public class KingFilesNet extends PluginForHost {
     /**
      * ControlSimHost, On error it will set the upper mark for 'max sim dl per host'. This will be the new 'static' setting used going
      * forward. Thus prevents new downloads starting when not possible and is self aware and requires no coder interaction.
-     * 
+     *
      * @param account
-     * 
+     *
      * @category 'Experimental', Mod written February 2013
      * */
     private void controlSimHost(final Account account) {
@@ -1600,7 +1599,7 @@ public class KingFilesNet extends PluginForHost {
      * This matches dllink against an array of used 'host' servers. Use this when site have multiple download servers and they allow x
      * connections to ip/host server. Currently JD allows a global connection controller and doesn't allow for handling of different
      * hosts/IP setup. This will help with those situations by allowing more connection when possible.
-     * 
+     *
      * @param Account
      *            Account that's been used, can be null
      * @param DownloadLink
@@ -1705,7 +1704,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Sets Key and Values to respective Account stored within hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param x
@@ -1750,7 +1749,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Returns String key from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1772,7 +1771,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Returns integer value from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1794,7 +1793,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Returns true if hostMap contains 'key'
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param key
@@ -1819,7 +1818,7 @@ public class KingFilesNet extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     * 
+     *
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -1836,7 +1835,7 @@ public class KingFilesNet extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     * 
+     *
      * @param key
      *            name
      * @param value
@@ -1866,9 +1865,9 @@ public class KingFilesNet extends PluginForHost {
     /**
      * If form contain both " and ' quotation marks within input fields it can return null values, thus you submit wrong/incorrect data re:
      * InputField parse(final String data). Affects revision 19688 and earlier!
-     * 
+     *
      * TODO: remove after JD2 goes stable!
-     * 
+     *
      * @author raztoki
      * */
     private Form cleanForm(Form form) {
@@ -1898,7 +1897,7 @@ public class KingFilesNet extends PluginForHost {
     /**
      * This allows backward compatibility for design flaw in setHtmlCode(), It injects updated html into all browsers that share the same
      * request id. This is needed as request.cloneRequest() was never fully implemented like browser.cloneBrowser().
-     * 
+     *
      * @param ibr
      *            Import Browser
      * @param t
