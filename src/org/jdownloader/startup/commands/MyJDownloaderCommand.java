4ab87c54f3ad80e4e8d7fc82ed238a275e149faa
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-08-21 12:59:17 +0000

    Added some CLI Options -set class [storage] key jsonvalue -myjd to connect to myJD -noerr to disable error out
    build>full
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26577 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/startup/commands/MyJDownloaderCommand.java
+++ src/org/jdownloader/startup/commands/MyJDownloaderCommand.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.appwork.shutdown.ShutdownController;
 import org.jdownloader.api.myjdownloader.MyJDownloaderConnectThread;
 import org.jdownloader.api.myjdownloader.MyJDownloaderConnectionStatus;
 import org.jdownloader.api.myjdownloader.MyJDownloaderController;
@@ -37,15 +38,26 @@ public class MyJDownloaderCommand extends AbstractStartupCommand {
             CFG_MYJD.AUTO_CONNECT_ENABLED.setValue(true);
             MyJDownloaderController.getInstance().connect();
             MyJDownloaderConnectThread th = MyJDownloaderController.getInstance().getConnectThread();
-            if (th == null || th.getConnectionStatus() == MyJDownloaderConnectionStatus.UNCONNECTED) {
+            int i = 15;
+            while ((th = MyJDownloaderController.getInstance().getConnectThread()) == null || th.getConnectionStatus() == MyJDownloaderConnectionStatus.UNCONNECTED) {
+                System.out.println("Wait For MyJDownloader Connection");
+                Thread.sleep(1000);
+                if (i-- <= 0) {
+                    break;
+                }
+            }
+            if ((th = MyJDownloaderController.getInstance().getConnectThread()) == null || th.getConnectionStatus() == MyJDownloaderConnectionStatus.UNCONNECTED) {
                 CFG_MYJD.PASSWORD.setValue(null);
-                System.err.println("Invalid Logins");
-                System.err.println("Error Details:" + CFG_MYJD.CFG.getLatestError());
-                System.exit(1);
+                System.out.println("Invalid Logins");
+                System.out.println("Error Details:" + CFG_MYJD.CFG.getLatestError());
+                ShutdownController.getInstance().requestShutdown();
             } else {
                 System.out.println("My JDownloader Connection Estabilished");
             }
+
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
