8609457552af2f91def40b6de11a601c5f9fa926
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-08-21 12:59:17 +0000

    Added some CLI Options -set class [storage] key jsonvalue -myjd to connect to myJD -noerr to disable error out
    build>full
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26577 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5d186c778f3083acf20a53a3b499462882591b16

--- src/org/jdownloader/startup/commands/SetConfigCommand.java
+++ src/org/jdownloader/startup/commands/SetConfigCommand.java
@@ -1,5 +1,7 @@
 package org.jdownloader.startup.commands;
 
+import jd.SecondLevelLaunch;
+
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
 import org.appwork.utils.Exceptions;
@@ -15,26 +17,34 @@ public class SetConfigCommand extends AbstractStartupCommand {
 
     @Override
     public void run(final String command, final String... parameters) {
+        SecondLevelLaunch.INIT_COMPLETE.executeWhenReached(new Runnable() {
 
-        if (parameters.length == 3) {
-            try {
-                RemoteAPIController.getInstance().getAdvancedConfigAPI().set(parameters[0], null, parameters[1], JSonStorage.restoreFromString(parameters[2], new TypeRef<Object>() {
-                }));
-            } catch (InvalidValueException e) {
-                System.out.println(Exceptions.getStackTrace(e));
+            @Override
+            public void run() {
+                if (parameters.length == 3) {
+                    try {
+                        RemoteAPIController.getInstance().getAdvancedConfigAPI().set(parameters[0], null, parameters[1], JSonStorage.restoreFromString(parameters[2], new TypeRef<Object>() {
+                        }));
+                        System.out.println("Set: " + parameters[0] + "." + parameters[1] + "=" + parameters[2]);
+                    } catch (InvalidValueException e) {
+                        System.out.println(Exceptions.getStackTrace(e));
+
+                    }
+                } else if (parameters.length == 4) {
+                    try {
+                        RemoteAPIController.getInstance().getAdvancedConfigAPI().set(parameters[0], parameters[1], parameters[2], JSonStorage.restoreFromString(parameters[3], new TypeRef<Object>() {
+                        }));
+                        System.out.println("Set: " + parameters[0] + "." + parameters[1] + "(" + parameters[2] + ")" + "=" + parameters[3]);
+                    } catch (InvalidValueException e) {
+                        System.out.println(Exceptions.getStackTrace(e));
+
+                    }
+                } else {
+                    System.out.println("Set Config -set class [storageid] key value");
+                }
 
             }
-        } else if (parameters.length == 4) {
-            try {
-                RemoteAPIController.getInstance().getAdvancedConfigAPI().set(parameters[0], parameters[1], parameters[2], JSonStorage.restoreFromString(parameters[3], new TypeRef<Object>() {
-                }));
-            } catch (InvalidValueException e) {
-                System.out.println(Exceptions.getStackTrace(e));
-
-            }
-        } else {
-            System.out.println("Set Config -set class [storageid] key value");
-        }
+        });
 
     }
 
