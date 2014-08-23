9d38ce229ce5c888465c753d1b046100e6760b90
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-08-23 10:54:50 +0000

    Host: Catshare.net - Accept 15 chars between / http://board.jdownloader.org/showthread.php?t=58750
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26611 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/CatShareNet.java
+++ src/jd/plugins/hoster/CatShareNet.java
@@ -48,7 +48,7 @@ import jd.utils.JDUtilities;
 import org.appwork.utils.formatter.SizeFormatter;
 import org.appwork.utils.formatter.TimeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "catshare.net" }, urls = { "http://(www\\.)?catshare\\.net/[A-Za-z0-9]{16}" }, flags = { 2 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "catshare.net" }, urls = { "http://(www\\.)?catshare\\.net/[A-Za-z0-9]{15,16}" }, flags = { 2 })
 public class CatShareNet extends PluginForHost {
 
     private String        BRBEFORE = "";
