571203fdaed406f3626b34768f638671c5dea3f6
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-09-05 06:41:32 +0000

    PluginScanner: added 1.6 Support build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@26864 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 458e9da48b4617ffd7734c6b2107bdfc939c28d1

--- src/org/jdownloader/plugins/controller/PluginController.java
+++ src/org/jdownloader/plugins/controller/PluginController.java
@@ -3,15 +3,8 @@ package org.jdownloader.plugins.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.lang.reflect.Modifier;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -20,100 +13,27 @@ import jd.plugins.Plugin;
 
 import org.appwork.utils.Application;
 import org.appwork.utils.logging2.LogSource;
-import org.jdownloader.plugins.controller.PluginClassLoader.PluginClassLoaderChild;
 
 public abstract class PluginController<T extends Plugin> {
 
-    private final static HashSet<String> ignoreList = new HashSet<String>();
+    protected final static HashSet<String> IGNORELIST = new HashSet<String>();
     static {
-        ignoreList.add("YoutubeDashConfigPanel");
-        ignoreList.add("RTMPDownload");
-        ignoreList.add("YoutubeHelper");
-        ignoreList.add("K2SApi");
-    }
-
-    protected List<PluginInfo<T>> scan(LogSource logger, String hosterpath, Map<LazyPluginClass, ArrayList<LazyPlugin>> pluginCache) throws Exception {
-        DirectoryStream<Path> stream = null;
-        PluginClassLoaderChild cl = null;
-        final ArrayList<PluginInfo<T>> ret = new ArrayList<PluginInfo<T>>();
-        MessageDigest md = null;
-        final String pkg = hosterpath.replace("/", ".");
-        final byte[] mdCache = new byte[32767];
-        final HashMap<String, LazyPluginClass> lazyPluginClassMap;
-        if (pluginCache != null && pluginCache.size() > 0) {
-            lazyPluginClassMap = new HashMap<String, LazyPluginClass>();
-            for (final LazyPluginClass lazyPluginClass : pluginCache.keySet()) {
-                lazyPluginClassMap.put(lazyPluginClass.getClassName() + ".class", lazyPluginClass);
-            }
-        } else {
-            lazyPluginClassMap = null;
-        }
-        final long timeStamp = System.currentTimeMillis();
-        try {
-            stream = Files.newDirectoryStream(Application.getRootByClass(jd.SecondLevelLaunch.class, hosterpath).toPath(), "*.class");
-            for (final Path path : stream) {
-                final String pathFileName = path.getFileName().toString();
-                final String className = pathFileName.substring(0, pathFileName.length() - 6);
-                if (className.indexOf("$") < 0 && !ignoreList.contains(className)) {
-                    byte[] sha256 = null;
-                    final BasicFileAttributes pathAttr = Files.readAttributes(path, BasicFileAttributes.class);
-                    if (lazyPluginClassMap != null) {
-                        final LazyPluginClass lazyPluginClass = lazyPluginClassMap.get(pathFileName);
-                        if (lazyPluginClass != null && (lazyPluginClass.getLastModified() == pathAttr.lastModifiedTime().toMillis() || ((md = MessageDigest.getInstance("SHA-256")) != null && (sha256 = getFileHashBytes(path.toFile(), md, mdCache)) != null && Arrays.equals(sha256, lazyPluginClass.getSha256())))) {
-                            for (final LazyPlugin lazyPlugin : pluginCache.get(lazyPluginClass)) {
-                                // logger.finer("Cached: " + className + "|" + lazyPlugin.getDisplayName() + "|" +
-                                // lazyPluginClass.getRevision());
-                                final PluginInfo<T> pluginInfo = new PluginInfo<T>(lazyPluginClass, null);
-                                pluginInfo.setLazyPlugin(lazyPlugin);
-                                ret.add(pluginInfo);
-                            }
-                            continue;
-                        }
-                    }
-                    Class<T> pluginClass = null;
-                    long[] infos = null;
-                    try {
-                        if (cl == null) {
-                            cl = PluginClassLoader.getInstance().getChild();
-                        }
-                        if (md == null) {
-                            md = MessageDigest.getInstance("SHA-256");
-                        }
-                        pluginClass = (Class<T>) cl.loadClass(pkg + "." + className);
-                        if (!Modifier.isAbstract(pluginClass.getModifiers()) && Plugin.class.isAssignableFrom(pluginClass)) {
-                            infos = getInfos(pluginClass);
-                            if (infos == null) {
-                                continue;
-                            }
-                        } else {
-                            continue;
-                        }
-                    } catch (final Throwable e) {
-                        logger.finer("Failed: " + className);
-                        logger.log(e);
-                        continue;
-                    }
-                    if (sha256 == null) {
-                        sha256 = getFileHashBytes(path.toFile(), md, mdCache);
-                    }
-                    final LazyPluginClass lazyPluginClass = new LazyPluginClass(className, sha256, pathAttr.lastModifiedTime().toMillis(), (int) infos[0], infos[1]);
-                    final PluginInfo<T> pluginInfo = new PluginInfo<T>(lazyPluginClass, pluginClass);
-                    // logger.finer("Scaned: " + className + "|" + lazyPluginClass.getRevision());
-                    ret.add(pluginInfo);
-                }
-            }
-            return ret;
-        } finally {
-            logger.info("@PluginController: scan took " + (System.currentTimeMillis() - timeStamp) + "ms for " + ret.size());
-            if (stream != null) {
-                stream.close();
-            }
-        }
+        IGNORELIST.add("YoutubeDashConfigPanel");
+        IGNORELIST.add("RTMPDownload");
+        IGNORELIST.add("YoutubeHelper");
+        IGNORELIST.add("K2SApi");
     }
 
     protected abstract long[] getInfos(Class<T> clazz);
 
-    private static byte[] getFileHashBytes(final File arg, final MessageDigest md, final byte[] mdCache) throws IOException {
+    protected List<PluginInfo<T>> scan(LogSource logger, String hosterpath, Map<LazyPluginClass, ArrayList<LazyPlugin>> pluginCache) throws Exception {
+        if (Application.getJavaVersion() >= Application.JAVA17) {
+            return new PluginScannerNIO<T>(this).scan(logger, hosterpath, pluginCache);
+        }
+        return new PluginScannerFiles<T>(this).scan(logger, hosterpath, pluginCache);
+    }
+
+    protected static byte[] getFileHashBytes(final File arg, final MessageDigest md, final byte[] mdCache) throws IOException {
         if (arg == null || !arg.exists() || arg.isDirectory()) {
             return null;
         }
