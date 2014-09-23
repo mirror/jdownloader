7fa72830ef9ac979494ea4d085204c5360e86392
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-09-23 13:12:59 +0000

    Fixed X-art.com
    UPdateclient deletes awf files  if a signature violation is detected
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27077 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c03632a12be2efe44b05c23125662164cafebf3f

--- src/jd/plugins/decrypter/XArt.java
+++ src/jd/plugins/decrypter/XArt.java
@@ -16,13 +16,15 @@
 
 package jd.plugins.decrypter;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 
 import jd.PluginWrapper;
 import jd.controlling.AccountController;
 import jd.controlling.ProgressController;
-import jd.nutils.encoding.Encoding;
 import jd.plugins.Account;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
@@ -33,7 +35,7 @@ import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.utils.JDUtilities;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "x-art.com" }, urls = { "^https?://(www\\.)?x-art\\.com/members/videos/[a-zA0-9\\-\\_]+/$" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "x-art.com" }, urls = { "^https?://(www\\.)?x-art\\.com/(members/)?.+" }, flags = { 0 })
 public class XArt extends PluginForDecrypt {
 
     public XArt(PluginWrapper wrapper) {
@@ -41,9 +43,46 @@ public class XArt extends PluginForDecrypt {
     }
 
     @Override
-    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+    protected DownloadLink createDownloadlink(String link) {
+        link = link.replaceFirst("x\\-art\\.com", "x-artdecrypted.com");
+        DownloadLink ret = super.createDownloadlink(link);
+        try {
+            ret.setContentUrl(link);
+        } catch (Throwable e) {
 
+        }
+        return ret;
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>() {
+            public boolean add(DownloadLink e) {
+                try {
+                    distribute(e);
+                } catch (Throwable ee) {
+
+                }
+                return super.add(e);
+            };
+
+            @Override
+            public boolean addAll(Collection<? extends DownloadLink> c) {
+                for (DownloadLink d : c) {
+                    add(d);
+                }
+                return true;
+            }
+        };
+        String url = parameter.getCryptedUrl();
+
+        if (!url.contains("/members/")) {
+            url = url.replaceAll("x\\-art\\.com\\/", "x-art.com/members/");
+        }
+        if (url.matches("(https?://([^\r\n\t\"']+\\.)?x-art\\.com/[^\r\n\t\"']+\\.(mp4|wmv|mov|zip)[^\r\n\t\"']*)")) {
+            ret.add(createDownloadlink(url));
+            return ret;
+        }
         boolean prem = false;
         final ArrayList<Account> accounts = AccountController.getInstance().getAllAccounts("x-art.com");
         if (accounts != null && accounts.size() != 0) {
@@ -60,33 +99,57 @@ public class XArt extends PluginForDecrypt {
             logger.warning("You need to use an Account with this provider");
             return ret;
         }
+        if (url.matches("^https?://(www\\.)?x-art\\.com/(members/)?(videos|galleries)/[a-zA0-9\\-\\_]+/?$")) {
+            parseUrl(ret, url);
+        } else {
+            br.getPage(url);
+            String[] sub = br.getRegex("(https?://(www\\.)?x-art\\.com/(members/)?(videos|galleries)/[a-zA0-9\\-\\_]+/?)").getColumn(0);
+            HashSet<String> dupe = new HashSet<String>();
+            for (String s : sub) {
+                try {
+                    if (isAbort()) {
+                        return ret;
+                    }
+                } catch (Throwable ee) {
+
+                }
+
+                if (dupe.add(s)) {
+                    parseUrl(ret, s);
+                }
+            }
+
+        }
+        return ret;
+    }
+
+    public void parseUrl(ArrayList<DownloadLink> ret, String url) throws IOException {
         br.setFollowRedirects(true);
 
-        br.getPage(parameter.toString());
-
-        String links[] = br.getRegex("href=\"([a-zA-Z0-9\\_\\-]*\\.(mp4|wmv|mov))\"").getColumn(0);
-        String lnks[] = br.getRegex("href=(\"|')(https?://[^\r\n\t\"]+\\.x-art\\.com/[^\r\n\t\"]+\\.(mp4|wmv|mov)\\?[^\r\n\t\"]+)\\1").getColumn(1);
-        if ((links == null || links.length == 0) && (lnks == null || lnks.length == 0)) {
-            logger.warning("Possible plugin defect, please confirm in browser. If their are links present please report to JDownloader Development Team : " + parameter.toString());
-            return null;
+        br.getPage(url);
+        ArrayList<DownloadLink> dlinks = new ArrayList<DownloadLink>();
+        String links[] = br.getRegex("href=\"([a-zA-Z0-9\\_\\-]*\\.(mp4|wmv|mov|zip))\"").getColumn(0);
+        String lnks[] = br.getRegex("href=(\"|')(https?://([^\r\n\t\"']+\\.)?x-art\\.com/[^\r\n\t\"']+\\.(mp4|wmv|mov|zip)[^\r\n\t\"']*)").getColumn(1);
+        if (links != null) {
+            for (String link : links) {
+                String fulllink = br.getURL() + link;
+                DownloadLink dl = createDownloadlink(fulllink);
+                dlinks.add(dl);
+            }
         }
-        for (String link : links) {
-            String fulllink = br.getURL() + link;
-            DownloadLink dl = createDownloadlink(Encoding.htmlDecode(fulllink));
-            ret.add(dl);
+        if (lnks != null) {
+            for (String link : lnks) {
+                DownloadLink dl = createDownloadlink((link));
+                dlinks.add(dl);
+            }
         }
-        for (String link : lnks) {
-            DownloadLink dl = createDownloadlink(Encoding.htmlDecode(link));
-            ret.add(dl);
-        }
-
+        ret.addAll(dlinks);
         String title = br.getRegex("<h1>([a-zA-Z0-9\\_\\-\\ ]*)<\\/h1>").getMatch(0);
         if (title != null) {
             FilePackage fp = FilePackage.getInstance();
-            fp.setName("XArt Movie: " + title);
-            fp.addLinks(ret);
+            fp.setName(title);
+            fp.addLinks(dlinks);
         }
-        return ret;
     }
 
     private boolean login(final Account account) throws Exception {
