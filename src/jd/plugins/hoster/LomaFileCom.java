6eb1a509ac193ed927efc466d92ceac4541126e9
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-09-24 18:57:12 +0000

    -fixed lomafile limits
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27133 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7f2d4ccd995ddd46295b817ab56e60db373c7530

--- src/jd/plugins/hoster/LomaFileCom.java
+++ src/jd/plugins/hoster/LomaFileCom.java
@@ -112,7 +112,7 @@ public class LomaFileCom extends PluginForHost {
     private void setConstants(final Account account) {
         if (account != null && account.getBooleanProperty("free")) {
             // free account
-            chunks = -2;
+            chunks = 1;
             resumes = true;
             acctype = "Free/Expired Account";
             directlinkproperty = "freelink2";
@@ -132,7 +132,7 @@ public class LomaFileCom extends PluginForHost {
             }
         } else {
             // non account
-            chunks = -2;
+            chunks = 1;
             resumes = true;
             acctype = "Non Account";
             directlinkproperty = "freelink";
@@ -170,7 +170,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * @author raztoki
-     * 
+     *
      * @category 'Experimental', Mods written July 2012 - 2013
      * */
     public LomaFileCom(PluginWrapper wrapper) {
@@ -347,7 +347,7 @@ public class LomaFileCom extends PluginForHost {
     /**
      * Provides alternative linkchecking method for a single link at a time. Can be used as generic failover, though kinda pointless as this
      * method doesn't give filename...
-     * 
+     *
      * */
     private String[] altAvailStat(final DownloadLink downloadLink, final String[] fileInfo) throws Exception {
         Browser alt = new Browser();
@@ -501,7 +501,7 @@ public class LomaFileCom extends PluginForHost {
             }
         }
         if (dl.getConnection().getContentType().contains("html")) {
-            if (dl.getConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") !=null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("nginx")) {
+            if (dl.getConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") != null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("nginx")) {
                 controlSimHost(account);
                 controlHost(account, downloadLink, false);
 
@@ -534,7 +534,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Removes patterns which could break the plugin due to fake/hidden HTML, or false positives caused by HTML comments.
-     * 
+     *
      * @throws Exception
      * @author raztoki
      */
@@ -1006,7 +1006,7 @@ public class LomaFileCom extends PluginForHost {
                 }
             }
             if (dl.getConnection().getContentType().contains("html")) {
-                if (dl.getConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") !=null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("nginx")) {
+                if (dl.getConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") != null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("nginx")) {
                     controlSimHost(account);
                     controlHost(account, downloadLink, false);
 
@@ -1077,7 +1077,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Rules to prevent new downloads from commencing
-     * 
+     *
      * */
     public boolean canHandle(DownloadLink downloadLink, Account account) {
         if (downloadLink.getBooleanProperty("requiresPremiumAccount", false) && (account == null || account.getBooleanProperty("free", false))) {
@@ -1110,7 +1110,7 @@ public class LomaFileCom extends PluginForHost {
      * Corrects downloadLink.urlDownload().<br/>
      * <br/>
      * The following code respect the hoster supported protocols via plugin boolean settings and users config preference
-     * 
+     *
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1186,7 +1186,7 @@ public class LomaFileCom extends PluginForHost {
     /**
      * Gets page <br />
      * - natively supports silly cloudflare anti DDoS crapola
-     * 
+     *
      * @author raztoki
      */
     private void getPage(final String page) throws Exception {
@@ -1200,7 +1200,7 @@ public class LomaFileCom extends PluginForHost {
                 throw (PluginException) e;
             }
             // should only be picked up now if not JD2
-            if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") !=null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("cloudflare-nginx")) {
+            if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") != null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("cloudflare-nginx")) {
                 logger.warning("Cloudflare anti DDoS measures enabled, your version of JD can not support this. In order to go any further you will need to upgrade to JDownloader 2");
                 throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Cloudflare anti DDoS measures enabled");
             } else {
@@ -1208,7 +1208,7 @@ public class LomaFileCom extends PluginForHost {
             }
         }
         // prevention is better than cure
-        if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") !=null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("cloudflare-nginx")) {
+        if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 503 && br.getHttpConnection().getHeaderField("server") != null && br.getHttpConnection().getHeaderField("server").toLowerCase(Locale.ENGLISH).contains("cloudflare-nginx")) {
             String host = new Regex(page, "https?://([^/]+)(:\\d+)?/").getMatch(0);
             Form cloudflare = br.getFormbyProperty("id", "ChallengeForm");
             if (cloudflare == null) {
@@ -1331,7 +1331,7 @@ public class LomaFileCom extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     * 
+     *
      * @version 0.2
      * @author raztoki
      * */
@@ -1417,7 +1417,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * captcha processing can be used download/login/anywhere assuming the submit values are the same (they usually are)...
-     * 
+     *
      * @author raztoki
      * */
     private Form captchaForm(DownloadLink downloadLink, Form form) throws Exception {
@@ -1587,13 +1587,13 @@ public class LomaFileCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     * 
+     *
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     * 
+     *
      * @param controlSlot
      *            (+1|-1)
      * */
@@ -1614,9 +1614,9 @@ public class LomaFileCom extends PluginForHost {
     /**
      * ControlSimHost, On error it will set the upper mark for 'max sim dl per host'. This will be the new 'static' setting used going
      * forward. Thus prevents new downloads starting when not possible and is self aware and requires no coder interaction.
-     * 
+     *
      * @param account
-     * 
+     *
      * @category 'Experimental', Mod written February 2013
      * */
     private void controlSimHost(final Account account) {
@@ -1653,7 +1653,7 @@ public class LomaFileCom extends PluginForHost {
      * This matches dllink against an array of used 'host' servers. Use this when site have multiple download servers and they allow x
      * connections to ip/host server. Currently JD allows a global connection controller and doesn't allow for handling of different
      * hosts/IP setup. This will help with those situations by allowing more connection when possible.
-     * 
+     *
      * @param Account
      *            Account that's been used, can be null
      * @param DownloadLink
@@ -1758,7 +1758,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Sets Key and Values to respective Account stored within hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param x
@@ -1803,7 +1803,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Returns String key from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1825,7 +1825,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Returns integer value from Account@usedHost from hostMap
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1847,7 +1847,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Returns true if hostMap contains 'key'
-     * 
+     *
      * @param account
      *            Account that's been used, can be null
      * @param key
@@ -1872,7 +1872,7 @@ public class LomaFileCom extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     * 
+     *
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -1889,7 +1889,7 @@ public class LomaFileCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     * 
+     *
      * @param key
      *            name
      * @param value
@@ -1919,9 +1919,9 @@ public class LomaFileCom extends PluginForHost {
     /**
      * If form contain both " and ' quotation marks within input fields it can return null values, thus you submit wrong/incorrect data re:
      * InputField parse(final String data). Affects revision 19688 and earlier!
-     * 
+     *
      * TODO: remove after JD2 goes stable!
-     * 
+     *
      * @author raztoki
      * */
     private Form cleanForm(Form form) {
@@ -1951,7 +1951,7 @@ public class LomaFileCom extends PluginForHost {
     /**
      * This allows backward compatibility for design flaw in setHtmlCode(), It injects updated html into all browsers that share the same
      * request id. This is needed as request.cloneRequest() was never fully implemented like browser.cloneBrowser().
-     * 
+     *
      * @param ibr
      *            Import Browser
      * @param t
