d8d0202099a02fdb90e6a5515160b8340c8e686f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-09-29 11:50:33 +0000

    fixes #53058
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27206 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/XxxBunkerCom.java
+++ src/jd/plugins/decrypter/XxxBunkerCom.java
@@ -128,7 +128,7 @@ public class XxxBunkerCom extends PluginForDecrypt {
             return decryptedLinks;
         }
         final Browser br2 = br.cloneBrowser();
-        externID = br.getRegex("%26amp%3Bpostbackurl%3D([^<>\"]*?)%26amp%3Brelayurl").getMatch(0);
+        externID = br.getRegex("%26amp%3Bpostbackurl%3D([^<>\"]*?)%26amp").getMatch(0);
         if (externID == null) {
             externID = br.getRegex("%26amp%3Bfile%3D(http[^<>\"]*?)%26amp").getMatch(0);
         }
