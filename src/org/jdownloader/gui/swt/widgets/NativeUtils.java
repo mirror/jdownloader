380d379d0264d95634ee429e03c802435cd3cd1f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-01 13:11:05 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27226 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: aeb511b81810872ad21b5789cb4e717ab99546ac

--- src/org/jdownloader/gui/swt/widgets/NativeUtils.java
+++ src/org/jdownloader/gui/swt/widgets/NativeUtils.java
@@ -1,49 +1,33 @@
 package org.jdownloader.gui.swt.widgets;
 
-import java.io.File;
-
-import jd.gui.swing.jdgui.JDGui;
-
 import org.appwork.utils.logging2.LogSource;
+import org.appwork.utils.os.CrossSystem;
 import org.jdownloader.logging.LogController;
-
-import chrriis.dj.nativeswing.swtimpl.NativeInterface;
-import chrriis.dj.nativeswing.swtimpl.components.JDirectoryDialog;
 
 public class NativeUtils {
 
-    public static LogSource LOGGER = LogController.getInstance().getLogger(NativeUtils.class.getName());
-    private static boolean  INIT   = false;
-
-    public static File open(final File path, final boolean packager, final String title) {
-
-        JDirectoryDialog directoryDialog = new JDirectoryDialog();
-        directoryDialog.setTitle(title);
-        directoryDialog.setSelectedDirectory(path.getAbsolutePath());
-
-        directoryDialog.show(JDGui.getInstance().getMainFrame());
-        String ret = directoryDialog.getSelectedDirectory();
-
-        return ret == null ? null : new File(ret);
-    }
+    private static boolean INIT = false;
 
     public synchronized static void init() {
-        if (INIT) {
-            return;
-        }
-        long start = System.currentTimeMillis();
-        try {
+        if (!INIT) {
+            try {
+                if (!CrossSystem.isRaspberryPi()) {
+                    final LogSource LOGGER = LogController.getInstance().getLogger(NativeUtils.class.getName());
+                    long start = System.currentTimeMillis();
+                    try {
 
-            NativeInterface.open();
-            NativeInterface.runEventPump();
-
-        } catch (Throwable e) {
-            LOGGER.log(e);
-
-        } finally {
-            LOGGER.info("Native Init took: " + (System.currentTimeMillis() - start) + " ms");
-            INIT = true;
+                        chrriis.dj.nativeswing.swtimpl.NativeInterface.open();
+                        chrriis.dj.nativeswing.swtimpl.NativeInterface.runEventPump();
+                        LOGGER.info("Native Init took: " + (System.currentTimeMillis() - start) + " ms");
+                    } catch (Throwable e) {
+                        LOGGER.log(e);
+                    } finally {
+                        LOGGER.close();
+                    }
+                }
+            } finally {
+                INIT = true;
+            }
         }
     }
-
 }
