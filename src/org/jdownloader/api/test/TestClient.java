44c6d59ef49daa12ad6613f27690135e9e7f3a2c
flubshi <flubshi@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-09 12:41:18 +0000

    API TestClient: Add /system/{shutdownOS}
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27356 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5bb4b9e23b8ffbfae65da643938db743c64d691d

--- src/org/jdownloader/api/test/TestClient.java
+++ src/org/jdownloader/api/test/TestClient.java
@@ -199,6 +199,8 @@ public class TestClient {
 
         register(new GenericCallTest());
 
+        register(new SystemTest());
+
         register(new CaptchaListTest());
 
         register(new WriteSessionTest());
