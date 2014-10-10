6493fe31da28241436279b42e5b74d54e22edb47
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-10 02:42:08 +0000

    Fixed: fourup.me, manual get page is bad idea. detection of redirect works fine (at least in JD2). - closes #54083
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27367 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c25788681143b5d3584d820eb7cebcfd72e892b3

--- src/jd/plugins/hoster/FourUpMe.java
+++ src/jd/plugins/hoster/FourUpMe.java
@@ -119,8 +119,6 @@ public class FourUpMe extends PluginForHost {
         br.setFollowRedirects(true);
         prepBrowser(br);
         getPage(link.getDownloadURL());
-        /* Workaround for broken/buggy redirection */
-        getPage("http://4upfiles.com/Ofiles.php");
         if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
@@ -382,13 +380,13 @@ public class FourUpMe extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     * 
+     *
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     * 
+     *
      * @param controlFree
      *            (+1|-1)
      */
@@ -529,7 +527,7 @@ public class FourUpMe extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     * 
+     *
      * @param key
      * @param value
      * @return
