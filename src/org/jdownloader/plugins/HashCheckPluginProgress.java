0789f40c02db08fda586c31d1f7731ae6e1660aa
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-14 08:41:07 +0000

    fixes #54350
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27461 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c5705ca8ae8b4359f46b5ed48f2227a0ff01c6eb

--- src/org/jdownloader/plugins/HashCheckPluginProgress.java
+++ src/org/jdownloader/plugins/HashCheckPluginProgress.java
@@ -3,21 +3,21 @@ package org.jdownloader.plugins;
 import java.awt.Color;
 import java.io.File;
 
-import jd.nutils.Formatter;
 import jd.plugins.PluginProgress;
 import jd.plugins.download.HashInfo.TYPE;
 
+import org.appwork.utils.formatter.TimeFormatter;
 import org.jdownloader.gui.views.downloads.columns.ETAColumn;
 import org.jdownloader.images.NewTheme;
 import org.jdownloader.translate._JDT;
 
 public class HashCheckPluginProgress extends PluginProgress {
-    
+
     private final String message;
     private long         lastCurrent    = -1;
     private long         startTimeStamp = -1;
     private final TYPE   type;
-    
+
     public HashCheckPluginProgress(File file, Color color, TYPE type) {
         super(0, file != null ? file.length() : 1, color);
         setIcon(NewTheme.I().getIcon("hashsum", 16));
@@ -27,28 +27,31 @@ public class HashCheckPluginProgress extends PluginProgress {
         } else {
             message = _JDT._.system_download_doCRC2_waiting();
         }
+
     }
-    
+
     @Override
     public PluginTaskID getID() {
         return PluginTaskID.HASH;
     }
-    
+
     @Override
     public String getMessage(Object requestor) {
         if (requestor instanceof ETAColumn) {
             if (type != null) {
-                return Formatter.formatSeconds(getETA());
+
+                return TimeFormatter.formatMilliSeconds(getETA(), 0);
             } else {
                 return "";
             }
         }
         return message;
     }
-    
+
     @Override
     public void setCurrent(long current) {
         super.setCurrent(current);
+
         if (lastCurrent == -1 || lastCurrent > current) {
             lastCurrent = current;
             startTimeStamp = System.currentTimeMillis();
@@ -56,11 +59,16 @@ public class HashCheckPluginProgress extends PluginProgress {
             return;
         }
         long currentTimeDifference = System.currentTimeMillis() - startTimeStamp;
-        if (currentTimeDifference <= 0) return;
+        if (currentTimeDifference <= 0) {
+            return;
+        }
         long speed = (current * 10000) / currentTimeDifference;
-        if (speed == 0) return;
+        if (speed == 0) {
+            return;
+        }
         long eta = ((total - current) * 10000) / speed;
         this.setETA(eta);
+
     }
-    
+
 }
