8dc0b1d72aec2895ba589e835a35accd3c0a3a22
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-16 02:12:52 +0000

    Fixed: xerver.co, premium only links - re: http://board.jdownloader.org/showthread.php?p=310170#post310170
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27503 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: b0b9c10b904dce40672b3fefd714f5cb80febda8

--- src/jd/plugins/hoster/XerverCo.java
+++ src/jd/plugins/hoster/XerverCo.java
@@ -792,7 +792,7 @@ public class XerverCo extends PluginForHost {
             throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error!", 10 * 60 * 1000l);
         }
         /** Error handling for only-premium links */
-        if (new Regex(correctedBR, "( can download files up to |Upgrade your account to download bigger files|>Upgrade your account to download larger files|>The file you requested reached max downloads limit for Free Users|Please Buy Premium To download this file<|This file reached max downloads limit)").matches()) {
+        if (new Regex(correctedBR, "( can download files up to |Upgrade your account to download bigger files|>Upgrade your account to download larger files|>The file you requested reached max downloads limit for Free Users|Please Buy Premium To download this file<|This file reached max downloads limit|>THIS FILE IS AVAILABLE TO PREMIUM USERS ONLY\\.)").matches()) {
             String filesizelimit = new Regex(correctedBR, "You can download files up to(.*?)only").getMatch(0);
             if (filesizelimit != null) {
                 filesizelimit = filesizelimit.trim();
