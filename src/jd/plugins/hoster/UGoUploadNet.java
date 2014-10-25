d0ae9e47df7350ad5f1747cc556cd1d01f57e90b
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-25 12:04:25 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27627 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UGoUploadNet.java
+++ src/jd/plugins/hoster/UGoUploadNet.java
@@ -43,7 +43,7 @@ import jd.utils.JDUtilities;
 import org.appwork.utils.formatter.SizeFormatter;
 import org.appwork.utils.formatter.TimeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ugoupload.net" }, urls = { "http://(www\\.)?ugoupload\\.net/(?:Tol/)?[A-Za-z0-9]+" }, flags = { 2 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ugoupload.net" }, urls = { "http://(www\\.)?ugoupload\\.net/[A-Za-z0-9]+" }, flags = { 2 })
 public class UGoUploadNet extends PluginForHost {
 
     public UGoUploadNet(PluginWrapper wrapper) {
