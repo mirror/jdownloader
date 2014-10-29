17582c443295498361c3611c0dc6e9f1c5f9847a
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-10-29 07:58:05 +0000

    Share.az - Adjustment fixes #55984
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27680 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d2c6a9e0c98f0670ab097d009dc59d91c8a36358

--- src/jd/plugins/hoster/ShareAz.java
+++ src/jd/plugins/hoster/ShareAz.java
@@ -56,7 +56,7 @@ import jd.utils.locale.JDL;
 import org.appwork.utils.formatter.SizeFormatter;
 import org.appwork.utils.formatter.TimeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "share.az" }, urls = { "https?://(www\\.)?share\\.az/(embed\\-)?[a-z0-9]{12}" }, flags = { 2 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "share.az" }, urls = { "https?://(www\\.)?share\\.az/([a-z0-9]+/|embed\\-)?[a-z0-9]{12}" }, flags = { 2 })
 public class ShareAz extends PluginForHost {
 
     private String                         correctedBR                  = "";
@@ -566,13 +566,13 @@ public class ShareAz extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -688,7 +688,7 @@ public class ShareAz extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
@@ -714,7 +714,7 @@ public class ShareAz extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -731,7 +731,7 @@ public class ShareAz extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
@@ -919,7 +919,7 @@ public class ShareAz extends PluginForHost {
     /**
      * Is intended to handle out of date errors which might occur seldom by re-tring a couple of times before throwing the out of date
      * error.
-     *
+     * 
      * @param dl
      *            : The DownloadLink
      * @param error
