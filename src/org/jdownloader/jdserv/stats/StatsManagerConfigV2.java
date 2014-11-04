cc6e2a896cc4207fc37a19aca0de6a317bcb9ee8
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-11-04 13:21:35 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@27784 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/jdserv/stats/StatsManagerConfigV2.java
+++ src/org/jdownloader/jdserv/stats/StatsManagerConfigV2.java
@@ -3,6 +3,7 @@ package org.jdownloader.jdserv.stats;
 import org.appwork.storage.config.ConfigInterface;
 import org.appwork.storage.config.annotations.AboutConfig;
 import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DefaultDoubleValue;
 
 public interface StatsManagerConfigV2 extends ConfigInterface {
 
@@ -18,4 +19,15 @@ public interface StatsManagerConfigV2 extends ConfigInterface {
 
     boolean isAlwaysAllowLogUploads();
 
+    @AboutConfig
+    @DefaultDoubleValue(0.95d)
+    double getCaptchaUploadPercentage();
+
+    void setCaptchaUploadPercentage(double d);
+
+    @DefaultBooleanValue(true)
+    @AboutConfig
+    boolean isCaptchaUploadEnabled();
+
+    void setCaptchaUploadEnabled(boolean b);
 }
