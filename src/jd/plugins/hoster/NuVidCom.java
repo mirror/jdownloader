d1d4c318e9004ca5de811223700bcd2ae6c3413d
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-11-23 08:44:34 +0000

    Nuvid.com - linkPart regex adjustment fixes #58767
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28035 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f0766db40c9afd2f5d7a20e0875f9938da370af5

--- src/jd/plugins/hoster/NuVidCom.java
+++ src/jd/plugins/hoster/NuVidCom.java
@@ -91,7 +91,7 @@ public class NuVidCom extends PluginForHost {
         if (filename == null) {
             filename = br.getRegex("<title>([^<>\"]*?) - Free Porn \\& Sex Video").getMatch(0);
         }
-        final String linkPart = br.getRegex("\\'http://www\\.nuvid\\.com(/player_config/\\?(check_speed=0%26)?t=.*?)\\'\\);").getMatch(0);
+        final String linkPart = br.getRegex("\\'http://www\\.nuvid\\.com(/player_config/\\?(check_speed=.%26)?t=.*?)\\'\\);").getMatch(0);
         if (filename == null || linkPart == null) {
             logger.info("filename = " + filename + "; linkPart = " + linkPart);
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
