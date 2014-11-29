2bcde850d0cba639f8d2120d01737f34b6ba0302
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-11-29 12:34:08 +0000

    Xvideos.com - URL listener http://board.jdownloader.org/showthread.php?t=55831
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28136 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/XvideosCom.java
+++ src/jd/plugins/hoster/XvideosCom.java
@@ -28,7 +28,7 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
 //xvideos.com by pspzockerscene
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xvideos.com" }, urls = { "http://((www\\.)?xvideos\\.com/video[0-9]+|\\w+\\.xvideos\\.com/embedframe/\\d+)" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xvideos.com" }, urls = { "http://((www\\.)?xvideos\\.com/video[0-9]+/|\\w+\\.xvideos\\.com/embedframe/\\d+)" }, flags = { 0 })
 public class XvideosCom extends PluginForHost {
 
     public XvideosCom(PluginWrapper wrapper) {
