6f1adf391de1e6cadc3269e80d2737a255571d96
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-11 17:28:27 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28318 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/captcha/utils/recaptcha/api2/RecaptchaException.java
@@ -0,0 +1,9 @@
+package org.jdownloader.captcha.utils.recaptcha.api2;
+
+public class RecaptchaException extends Exception {
+
+    public RecaptchaException(String string) {
+        super(string);
+    }
+
+}
