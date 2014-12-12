549846a1f92b1b12af9923cbec0634266c966e9d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-12 14:12:42 +0000

    UJCECheck: updated workaround to work with java 1.6 as well
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28340 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/UJCECheck.java
+++ src/jd/UJCECheck.java
@@ -1,6 +1,10 @@
 package jd;
 
 import java.io.File;
+import java.lang.reflect.Field;
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.crypto.Cipher;
@@ -36,33 +40,37 @@ public class UJCECheck {
     public static final void check() {
         if (checked.compareAndSet(false, true)) {
             if (Boolean.TRUE.equals(isRestrictedCryptography()) && isOracleJVM()) {
-                final String javaHome = System.getProperty("java.home", null);
-                final File securityFolder = new File(javaHome, "lib" + File.separatorChar + "security");
-                if (securityFolder.exists() && securityFolder.isDirectory()) {
-                    final long javaVersion = Application.getJavaVersion();
-                    final File uJCE;
-                    if (javaVersion >= Application.JAVA18) {
-                        uJCE = Application.getResource("security/ujce8");
-                    } else if (javaVersion >= Application.JAVA17) {
-                        uJCE = Application.getResource("security/ujce7");
-                    } else if (javaVersion >= Application.JAVA16) {
-                        uJCE = Application.getResource("security/ujce6");
-                    } else {
-                        return;
-                    }
-                    if (uJCE.exists() && uJCE.isDirectory()) {
-                        ShutdownController.getInstance().addShutdownEvent(new ShutdownEvent() {
+                if (true) {
+                    removeCryptographyRestrictions();
+                } else {
+                    final String javaHome = System.getProperty("java.home", null);
+                    final File securityFolder = new File(javaHome, "lib" + File.separatorChar + "security");
+                    if (securityFolder.exists() && securityFolder.isDirectory()) {
+                        final long javaVersion = Application.getJavaVersion();
+                        final File uJCE;
+                        if (javaVersion >= Application.JAVA18) {
+                            uJCE = Application.getResource("security/ujce8");
+                        } else if (javaVersion >= Application.JAVA17) {
+                            uJCE = Application.getResource("security/ujce7");
+                        } else if (javaVersion >= Application.JAVA16) {
+                            uJCE = Application.getResource("security/ujce6");
+                        } else {
+                            return;
+                        }
+                        if (uJCE.exists() && uJCE.isDirectory()) {
+                            ShutdownController.getInstance().addShutdownEvent(new ShutdownEvent() {
 
-                            @Override
-                            public void onShutdown(ShutdownRequest shutdownRequest) {
-                                try {
-                                    IO.copyFolderRecursive(uJCE, securityFolder, true);
-                                } catch (final Throwable e) {
-                                    e.printStackTrace();
+                                @Override
+                                public void onShutdown(ShutdownRequest shutdownRequest) {
+                                    try {
+                                        IO.copyFolderRecursive(uJCE, securityFolder, true);
+                                    } catch (final Throwable e) {
+                                        e.printStackTrace();
+                                    }
                                 }
-                            }
-                        });
+                            });
 
+                        }
                     }
                 }
             }
@@ -71,42 +79,55 @@ public class UJCECheck {
 
     /**
      * http://stackoverflow.com/questions/1179672/how-to-avoid-installing-unlimited-strength-jce-policy-files-when-deploying-an
+     * 
+     * http://stackoverflow.com/questions/18435227/java-patching-client-side-security-policy-from-applet-for-aes256
      */
-    // private static void removeCryptographyRestrictions() {
-    // if (!isRestrictedCryptography()) {
-    // System.out.println("Cryptography restrictions removal not needed");
-    // return;
-    // }
-    // try {
-    // /*
-    // * Do the following, but with reflection to bypass access checks:
-    // *
-    // * JceSecurity.isRestricted = false; JceSecurity.defaultPolicy.perms.clear();
-    // * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
-    // */
-    // final Class<?> jceSecurity = Class.forName("javax.crypto.JceSecurity");
-    // final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
-    // final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
-    //
-    // final Field isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
-    // isRestrictedField.setAccessible(true);
-    // isRestrictedField.set(null, false);
-    //
-    // final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
-    // defaultPolicyField.setAccessible(true);
-    // final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
-    //
-    // final Field perms = cryptoPermissions.getDeclaredField("perms");
-    // perms.setAccessible(true);
-    // ((Map<?, ?>) perms.get(defaultPolicy)).clear();
-    //
-    // final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
-    // instance.setAccessible(true);
-    // defaultPolicy.add((Permission) instance.get(null));
-    //
-    // System.out.println("Successfully removed cryptography restrictions");
-    // } catch (final Throwable e) {
-    // System.out.println("Failed to remove cryptography restrictions:" + e);
-    // }
-    // }
+    private static void removeCryptographyRestrictions() {
+        if (!isRestrictedCryptography()) {
+            System.out.println("Cryptography restrictions removal not needed");
+            return;
+        }
+        try {
+            /*
+             * Do the following, but with reflection to bypass access checks:
+             * 
+             * JceSecurity.isRestricted = false; JceSecurity.defaultPolicy.perms.clear();
+             * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
+             */
+            Field isRestrictedField;
+            Class<?> jceSecurity = null;
+            try {
+                jceSecurity = Class.forName("javax.crypto.JceSecurity");
+                isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
+            } catch (final ClassNotFoundException e) {
+                try {
+                    // Java 6 has obfuscated JCE classes
+                    jceSecurity = Class.forName("javax.crypto.SunJCE_b");
+                    isRestrictedField = jceSecurity.getDeclaredField("g");
+                } catch (final ClassNotFoundException e2) {
+                    throw e;
+                }
+            }
+            isRestrictedField.setAccessible(true);
+            isRestrictedField.set(null, false);
+
+            final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
+            final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
+            final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
+            defaultPolicyField.setAccessible(true);
+            final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
+
+            final Field perms = cryptoPermissions.getDeclaredField("perms");
+            perms.setAccessible(true);
+            ((Map<?, ?>) perms.get(defaultPolicy)).clear();
+
+            final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
+            instance.setAccessible(true);
+            defaultPolicy.add((Permission) instance.get(null));
+
+            System.out.println("Successfully removed cryptography restrictions");
+        } catch (final Throwable e) {
+            System.out.println("Failed to remove cryptography restrictions:" + e);
+        }
+    }
 }
