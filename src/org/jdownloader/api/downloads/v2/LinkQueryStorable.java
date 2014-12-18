e7dbcc8cac5f053ea359eebc86c682d4f5f9efd5
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-18 17:03:47 +0000

    refs #60193
    Started to refactor the soundcloud plugin to use the jsonparser.
    
    
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28445 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9815f22ad0d0f30712daa74fd4a04b7f637690b4

--- src/org/jdownloader/api/downloads/v2/LinkQueryStorable.java
+++ src/org/jdownloader/api/downloads/v2/LinkQueryStorable.java
@@ -1,9 +1,16 @@
 package org.jdownloader.api.downloads.v2;
 
 import org.appwork.storage.Storable;
+import org.appwork.storage.jackson.JacksonMapper;
 import org.jdownloader.myjdownloader.client.bindings.downloadlist.DownloadLinkQuery;
 
 public class LinkQueryStorable extends DownloadLinkQuery implements Storable {
+    public static void main(String[] args) {
+
+        System.out.println(LinkQueryStorable.class.getSimpleName() + "= ");
+        System.out.println(new JacksonMapper().objectToString(new LinkQueryStorable()));
+    }
+
     public static final LinkQueryStorable FULL = new LinkQueryStorable();
     static {
         FULL.setBytesLoaded(true);
--- src/org/jdownloader/api/linkcollector/v2/CrawledLinkQueryStorable.java
+++ src/org/jdownloader/api/linkcollector/v2/CrawledLinkQueryStorable.java
@@ -1,9 +1,17 @@
 package org.jdownloader.api.linkcollector.v2;
 
 import org.appwork.storage.Storable;
+import org.appwork.storage.jackson.JacksonMapper;
 import org.jdownloader.myjdownloader.client.bindings.linkgrabber.CrawledLinkQuery;
 
 public class CrawledLinkQueryStorable extends CrawledLinkQuery implements Storable {
+
+    public static void main(String[] args) {
+
+        System.out.println(CrawledLinkQueryStorable.class.getSimpleName() + "= ");
+        System.out.println(new JacksonMapper().objectToString(new CrawledLinkQueryStorable()));
+    }
+
     public static final CrawledLinkQueryStorable FULL = new CrawledLinkQueryStorable();
     static {
         FULL.setAvailability(true);
