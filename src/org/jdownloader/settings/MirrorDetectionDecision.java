773e39cb529ca9359a8dcba40ed495b90007f140
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2014-12-18 08:30:03 +0000

    MirrorDetectionDecision: added docs
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28434 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 517e0f6105ea0de3d7349aa6e798927ac132efda

--- src/org/jdownloader/settings/MirrorDetectionDecision.java
+++ src/org/jdownloader/settings/MirrorDetectionDecision.java
@@ -1,8 +1,21 @@
 package org.jdownloader.settings;
 
 public enum MirrorDetectionDecision {
+
+    /**
+     * checks fileName(needs verified or forced fileName), fileSize(needs verifiedFileSize). fileHash is optional
+     */
     SAFE,
+    /**
+     * checks fileName, fileSize(see ForceMirrorDetectionFileSizeCheck/MirrorDetectionFileSizeEquality). fileHash is optional
+     */
     AUTO,
+    /**
+     * checks fileName, fileSize(see ForceMirrorDetectionFileSizeCheck/MirrorDetectionFileSizeEquality)
+     */
     FILENAME_FILESIZE,
+    /**
+     * checks fileName
+     */
     FILENAME;
 }
