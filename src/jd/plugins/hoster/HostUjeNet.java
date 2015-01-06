2ca9071ca331a65bdf06d47672579a0175f26fc8
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-01-06 03:33:48 +0000

    Fixed: hostuje.net, free download - closes #61284
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28589 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/HostUjeNet.java
+++ src/jd/plugins/hoster/HostUjeNet.java
@@ -19,8 +19,10 @@ package jd.plugins.hoster;
 import java.io.IOException;
 
 import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
-import jd.parser.Regex;
+import jd.parser.html.Form;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -50,8 +52,9 @@ public class HostUjeNet extends PluginForHost {
     @SuppressWarnings("deprecation")
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws PluginException, IOException {
+        br = new Browser();
         this.setBrowserExclusive();
-        br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0");
+        br.getHeaders().put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36");
         br.getPage(downloadLink.getDownloadURL());
         if (br.containsHTML(">Podany plik nie został odnaleziony")) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
@@ -71,15 +74,36 @@ public class HostUjeNet extends PluginForHost {
     public void handleFree(final DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
         br.setFollowRedirects(false);
-        final String fidhash = new Regex(downloadLink.getDownloadURL(), "([a-z0-9]+)$").getMatch(0);
-        final String k = br.getRegex("name=\"k\" value=\"([^<>\"]*?)\"").getMatch(0);
-        final String mimetype = br.getRegex("name=\"mime\" value=\"([^<>\"]*?)\"").getMatch(0);
-        if (k == null || mimetype == null) {
+        // Browser o = br.cloneBrowser();
+        // o.getHeaders().put("Accept", "*/*");
+        // o.cloneBrowser().getPage("/show_ads.js");
+        // o.cloneBrowser().getPage("/swfobject2.js?sr");
+        // o.cloneBrowser().getPage("/style.css?10");
+        // o.cloneBrowser().getPage("/dll_kody.js.php?new7a&flash=0");
+        // o.cloneBrowser().getPage("/swfobject_34a.js?srmmaaserr");
+        // o.cloneBrowser().getPage("/obraz.php");
+        final String[] imgs = br.getRegex("(\"|')([^\"'\r\n\t]+\\.(?:png|jpe?g|gif))\\1").getColumn(1);
+        // HashSet<String> dupe = new HashSet<String>();
+        if (imgs != null) {
+            // for (final String img : imgs) {
+            // if (dupe.add(img)) {
+            // this.simulateBrowser(br, img);
+            // }
+            // }
+
+            /**
+             * THIS IS REQUIRED <br>
+             * captcha image is used to detect automation - raztoki 20150106
+             **/
+            this.simulateBrowser(br, "/oc123.php");
+        }
+
+        // do not use static posts!
+        Form f = br.getForm(1);
+        if (f == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        // br.getPage("http://hostuje.net/obraz.php");
-        // br.getPage("http://hostuje.net/dll_kody.js.php?new7a&flash=0");
-        br.postPage(br.getURL(), "REG=1&OK=1&hasz=" + fidhash + "&id=" + fidhash + "&name=" + Encoding.urlEncode(downloadLink.getName()) + "&mime=" + Encoding.urlEncode(mimetype) + "&k=" + k);
+        br.submitForm(f);
         String link = null;
         link = br.getRedirectLocation();
         if (link == null) {
@@ -87,6 +111,24 @@ public class HostUjeNet extends PluginForHost {
         }
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, link, false, 1);
         dl.startDownload();
+    }
+
+    private void simulateBrowser(final Browser br, final String url) {
+        if (br == null || url == null) {
+            return;
+        }
+        Browser rb = br.cloneBrowser();
+        rb.getHeaders().put("Accept", "image/webp,*/*;q=0.8");
+        URLConnectionAdapter con = null;
+        try {
+            con = rb.openGetConnection(url);
+        } catch (final Throwable e) {
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Exception e) {
+            }
+        }
     }
 
     // do not add @Override here to keep 0.* compatibility
