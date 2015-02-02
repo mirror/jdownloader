a78fbad955e6b4f72ea5edeeca803b9f8f19ea95
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-02 16:21:01 +0000

    LinkCollector: filteredStuff is now a COW-List ClearFilteredLinksAction: fixes #27805
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28859 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/views/linkgrabber/contextmenu/ClearFilteredLinksAction.java
+++ src/org/jdownloader/gui/views/linkgrabber/contextmenu/ClearFilteredLinksAction.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import jd.controlling.linkcollector.LinkCollector;
 
 import org.appwork.utils.event.queue.QueueAction;
+import org.appwork.utils.swing.EDTRunner;
 import org.appwork.utils.swing.dialog.Dialog;
 import org.appwork.utils.swing.dialog.DialogNoAnswerException;
 import org.jdownloader.controlling.contextmenu.CustomizableAppAction;
@@ -14,24 +15,36 @@ import org.jdownloader.gui.translate._GUI;
 public class ClearFilteredLinksAction extends CustomizableAppAction {
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -6341297356888158708L;
 
     public ClearFilteredLinksAction() {
-
         setName(_GUI._.ClearFilteredLinksAction());
         setIconKey("filter");
         setEnabled(LinkCollector.getInstance().getfilteredStuffSize() > 0);
     }
 
+    @Override
+    public void requestUpdate(Object requestor) {
+        super.requestUpdate(requestor);
+        new EDTRunner() {
+
+            @Override
+            protected void runInEDT() {
+                setEnabled(LinkCollector.getInstance().getfilteredStuffSize() > 0);
+            }
+        };
+    }
+
     public void actionPerformed(ActionEvent e) {
-        if (LinkCollector.getInstance().getfilteredStuffSize() == 0) {
+        final int size = LinkCollector.getInstance().getfilteredStuffSize();
+        if (size == 0) {
             Toolkit.getDefaultToolkit().beep();
             return;
         }
         try {
-            Dialog.getInstance().showConfirmDialog(0, _GUI._.literally_are_you_sure(), _GUI._.ClearFilteredLinksAction_msg(LinkCollector.getInstance().getfilteredStuffSize()), null, _GUI._.literally_yes(), _GUI._.literall_no());
+            Dialog.getInstance().showConfirmDialog(0, _GUI._.literally_are_you_sure(), _GUI._.ClearFilteredLinksAction_msg(size), null, _GUI._.literally_yes(), _GUI._.literall_no());
             LinkCollector.getInstance().getQueue().add(new QueueAction<Void, RuntimeException>() {
 
                 @Override
