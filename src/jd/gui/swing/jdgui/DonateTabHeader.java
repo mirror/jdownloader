0681f218e7794f89cb47cc3da127305b41da8f05
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-03 14:05:32 +0000

    Donate Button Mac
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@28873 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/gui/swing/jdgui/DonateTabHeader.java
@@ -0,0 +1,64 @@
+package jd.gui.swing.jdgui;
+
+import java.awt.event.MouseEvent;
+
+import jd.gui.swing.jdgui.interfaces.JDMouseAdapter;
+import jd.gui.swing.jdgui.interfaces.View;
+import jd.gui.swing.jdgui.maintab.TabHeader;
+
+import org.jdownloader.gui.mainmenu.DonateAction;
+
+public class DonateTabHeader extends TabHeader {
+
+    public DonateTabHeader(View view) {
+        super(view);
+
+    }
+
+    @Override
+    protected void initMouseForwarder() {
+        addMouseListener(new JDMouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                JDMouseAdapter.forwardEvent(e, MainTabbedPane.getInstance());
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                JDMouseAdapter.forwardEvent(e, MainTabbedPane.getInstance());
+            }
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                new DonateAction().actionPerformed(null);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                JDMouseAdapter.forwardEvent(e, MainTabbedPane.getInstance());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                JDMouseAdapter.forwardEvent(e, MainTabbedPane.getInstance());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setShown();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                setHidden();
+            }
+
+        });
+    }
+
+    public void onClick() {
+
+    }
+
+}
