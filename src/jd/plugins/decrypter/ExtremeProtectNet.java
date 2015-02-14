a0e51851fdba489f9aff885abd210d2b29728efc
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-14 02:19:27 +0000

    Added: extreme-project.net, API support - re: https://board.jdownloader.org/showthread.php?t=62008
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29060 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: e1b53e498869dd883c88189670e6dc8779d0582a

--- src/jd/plugins/decrypter/ExtremeProtectNet.java
+++ src/jd/plugins/decrypter/ExtremeProtectNet.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
@@ -31,7 +33,7 @@ import jd.plugins.PluginForHost;
 import jd.plugins.hoster.DirectHTTP;
 import jd.utils.JDUtilities;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "extreme-protect.net" }, urls = { "http://(www\\.)?extreme\\-protect\\.net/[a-z0-9\\-_]+" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "extreme-protect.net" }, urls = { "http://(?:www\\.)?extreme\\-protect\\.net/([a-z0-9\\-_]+)" }, flags = { 0 })
 public class ExtremeProtectNet extends PluginForDecrypt {
 
     public ExtremeProtectNet(PluginWrapper wrapper) {
@@ -44,64 +46,92 @@ public class ExtremeProtectNet extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         String parameter = param.toString();
-        br.setFollowRedirects(true);
-        try {
-            br.setAllowedResponseCodes(new int[] { 500 });
-        } catch (final Throwable e) {
-            logger.info("Link can only be decrypted in JDownloader 2: " + parameter);
-            return decryptedLinks;
-        }
-        br.getPage(parameter);
-        if (br.getHttpConnection().getResponseCode() == 403 || !br.containsHTML("class=\"contenu_liens\"")) {
-            logger.info("Link offline: " + parameter);
-            final DownloadLink offline = createDownloadlink("directhttp://" + parameter);
-            offline.setAvailable(false);
-            offline.setProperty("offline", true);
-            decryptedLinks.add(offline);
-            return decryptedLinks;
-        }
-        if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(RECAPTCHATEXT2)) {
-            boolean failed = true;
-            for (int i = 0; i <= 5; i++) {
-                final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
-                final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
-                rc.parse();
-                rc.load();
-                File cf = rc.downloadCaptcha(getLocalCaptchaFile());
-                final String c = getCaptchaCode("recaptcha", cf, param);
-                br.postPage(br.getURL(), "recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + c + "&submit_captcha=VALIDER");
-                if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(RECAPTCHATEXT2)) {
-                    br.getPage(parameter);
-                    continue;
+        String fpName = null;
+        String[] links = null;
+        {
+            // this bypasses captcha etc. problem is title isn't shown...
+            br.postPage("http://extreme-protect.net/api.php", "id=" + new Regex(parameter, this.getSupportedLinks()).getMatch(0));
+            fpName = getJson("titre");
+            String lnks = getJsonArray("liens");
+            if (lnks != null) {
+                links = new Regex(lnks, "http[^\"]+").getColumn(-1);
+                if (links != null) {
+                    for (String dl : links) {
+                        decryptedLinks.add(createDownloadlink(dl));
+                    }
                 }
-                failed = false;
-                break;
             }
-            if (failed) {
-                throw new DecrypterException(DecrypterException.CAPTCHA);
+        }
+        if (inValidate(fpName)) {
+            br = new Browser();
+            br.setFollowRedirects(true);
+            try {
+                br.setAllowedResponseCodes(new int[] { 500 });
+            } catch (final Throwable e) {
+                // logger.info("Link can only be decrypted in JDownloader 2: " + parameter);
+                // return decryptedLinks;
             }
-        } else {
-            final String pass = generatePass();
+            br.getPage(parameter);
+            if (inValidate(fpName)) {
+                fpName = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
+            }
+            if (decryptedLinks.isEmpty()) {
+                // fail over
+                if (br.getHttpConnection().getResponseCode() == 403 || !br.containsHTML("class=\"contenu_liens\"")) {
+                    logger.info("Link offline: " + parameter);
+                    final DownloadLink offline = createDownloadlink("directhttp://" + parameter);
+                    offline.setAvailable(false);
+                    offline.setProperty("offline", true);
+                    decryptedLinks.add(offline);
+                    return decryptedLinks;
+                }
+                if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(RECAPTCHATEXT2)) {
+                    boolean failed = true;
+                    for (int i = 0; i <= 5; i++) {
+                        final PluginForHost recplug = JDUtilities.getPluginForHost("DirectHTTP");
+                        final jd.plugins.hoster.DirectHTTP.Recaptcha rc = ((DirectHTTP) recplug).getReCaptcha(br);
+                        rc.parse();
+                        rc.load();
+                        File cf = rc.downloadCaptcha(getLocalCaptchaFile());
+                        final String c = getCaptchaCode("recaptcha", cf, param);
+                        br.postPage(br.getURL(), "recaptcha_challenge_field=" + rc.getChallenge() + "&recaptcha_response_field=" + c + "&submit_captcha=VALIDER");
+                        if (br.containsHTML(RECAPTCHATEXT) || br.containsHTML(RECAPTCHATEXT2)) {
+                            br.getPage(parameter);
+                            continue;
+                        }
+                        failed = false;
+                        break;
+                    }
+                    if (failed) {
+                        throw new DecrypterException(DecrypterException.CAPTCHA);
+                    }
+                } else {
+                    final String pass = generatePass();
 
-            br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-            br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
-            br.getHeaders().put("Cache-Control", null);
-            br.postPage("/requis/captcha_formulaire.php", "action=qaptcha&qaptcha_key=" + pass);
+                    br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+                    br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
+                    br.getHeaders().put("Cache-Control", null);
+                    br.postPage("/requis/captcha_formulaire.php", "action=qaptcha&qaptcha_key=" + pass);
 
-            br.getHeaders().put("Accept", "text/html, application/xhtml+xml, */*");
-            br.getHeaders().put("X-Requested-With", null);
-            br.postPage(parameter, pass + "=&submit_captcha=VALIDER");
+                    br.getHeaders().put("Accept", "text/html, application/xhtml+xml, */*");
+                    br.getHeaders().put("X-Requested-With", null);
+                    br.postPage(parameter, pass + "=&submit_captcha=VALIDER");
+                }
+
+                links = br.getRegex("class=\"lien\" ><a target=\"_blank\" href=\"(http[^<>\"]*?)\"").getColumn(0);
+                if (links == null || links.length == 0) {
+                    logger.warning("Decrypter broken for link: " + parameter);
+                    return null;
+                }
+                for (String dl : links) {
+                    decryptedLinks.add(createDownloadlink(dl));
+                }
+                if (inValidate(fpName)) {
+                    fpName = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
+                }
+            }
         }
 
-        String[] links = br.getRegex("class=\"lien\" ><a target=\"_blank\" href=\"(http[^<>\"]*?)\"").getColumn(0);
-        String fpName = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
-        if (links == null || links.length == 0) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
-        }
-        for (String dl : links) {
-            decryptedLinks.add(createDownloadlink(dl));
-        }
         if (fpName != null) {
             FilePackage fp = FilePackage.getInstance();
             fp.setName(fpName.trim());
@@ -120,6 +150,84 @@ public class ExtremeProtectNet extends PluginForDecrypt {
             pass += chars.substring(lool, lool + 1);
         }
         return pass;
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return value of key from JSon response, from String source.
+     *
+     * @author raztoki
+     * */
+    private String getJson(final String source, final String key) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJson(source, key);
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return value of key from JSon response, from default 'br' Browser.
+     *
+     * @author raztoki
+     * */
+    private String getJson(final String key) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJson(br.toString(), key);
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return value of key from JSon response, from provided Browser.
+     *
+     * @author raztoki
+     * */
+    private String getJson(final Browser ibr, final String key) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJson(ibr.toString(), key);
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return value given JSon Array of Key from JSon response provided String source.
+     *
+     * @author raztoki
+     * */
+    private String getJsonArray(final String source, final String key) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJsonArray(source, key);
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return value given JSon Array of Key from JSon response, from default 'br' Browser.
+     *
+     * @author raztoki
+     * */
+    private String getJsonArray(final String key) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJsonArray(br.toString(), key);
+    }
+
+    /**
+     * Wrapper<br/>
+     * Tries to return String[] value from provided JSon Array
+     *
+     * @author raztoki
+     * @param source
+     * @return
+     */
+    private String[] getJsonResultsFromArray(final String source) {
+        return jd.plugins.hoster.K2SApi.JSonUtils.getJsonResultsFromArray(source);
+    }
+
+    /**
+     * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
+     *
+     * @param s
+     *            Imported String to match against.
+     * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
+     * @author raztoki
+     * */
+    private boolean inValidate(final String s) {
+        if (s == null || s != null && (s.matches("[\r\n\t ]+") || s.equals(""))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /* NO OVERRIDE!! */
