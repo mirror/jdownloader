eb0d32dd00b5f8e0a43d921f991a180976cbef1f
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-16 10:47:34 +0000

    Fixed: antiDDoS method, shared in anitDDoSFor Host and Decrypt along with K2SApi. Only a problem when: multiple domains are aliased in regex, and br.getURL() doesn't match this.getHost(). Result was cookies were not been found, to be set in antiDDoSCookies HashMap. Added:  watchtvseries.vc, watchseries.ag, watchtvseries.se, decrypter support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29092 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/WtchSrs.java
@@ -0,0 +1,96 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+/**
+ * Note: do not decrypt entire series... this decrypter is already threaded by returning back into itself, it will cause high loads for long
+ * periods.<br />
+ * Using cloudflare!
+ *
+ * @author raztoki
+ *
+ */
+@DecrypterPlugin(revision = "$Revision: 29090 $", interfaceVersion = 2, names = { "watchseries.ag" }, urls = { "https?://(?:www\\.)?(?:watchseries\\.ag|watchtvseries\\.(?:se|vc))/(episode/.*?\\.html|open/cale/\\d+\\.html)" }, flags = { 0 })
+public class WtchSrs extends antiDDoSForDecrypt {
+
+    public WtchSrs(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        if (br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404) {
+            try {
+                decryptedLinks.add(createOfflinelink(parameter));
+            } catch (final Throwable t) {
+            }
+            return decryptedLinks;
+        }
+        if (parameter.matches(".+/open/cale/\\d+\\.html")) {
+            final String result = br.getRegex("<a class=(\"|'|)myButton\\1 href=(\"|'\"|)(.*?)\\2").getMatch(2);
+            if (result != null) {
+                decryptedLinks.add(createDownloadlink(result));
+            }
+            return decryptedLinks;
+        }
+
+        String fpName = br.getRegex("</h2><h3[^>]*>(.*?)</h3>").getMatch(0);
+        final String[] links = br.getRegex("<a[^>]+href=(\"|'|)(/open/cale/\\d+\\.html)\\1").getColumn(1);
+        if (links == null) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        HashSet<String> dupe = new HashSet<String>();
+        final String base = new Regex(parameter, "https?://[^/]+").getMatch(-1);
+        for (final String link : links) {
+            if (!dupe.add(link)) {
+                continue;
+            }
+            // lets thread this task. easiest way to do this is to return back into the decrypter.
+            decryptedLinks.add(createDownloadlink(base + link));
+        }
+        if (fpName != null) {
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+}
