d221562881f0e9dace6242d7d5dbffe964d4cc86
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-20 13:39:02 +0000

    -fixed swrmediathek and improved errorhandling -filecore another domain fixes #63091 -arte refs #63081
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29194 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FilecoreCoNz.java
+++ src/jd/plugins/hoster/FilecoreCoNz.java
@@ -46,7 +46,7 @@ import jd.utils.locale.JDL;
 
 import org.appwork.utils.formatter.SizeFormatter;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filecore.co.nz" }, urls = { "https?://(www\\.)?(filecore\\.co\\.nz|fcore\\.eu)/(vidembed\\-)?[a-z0-9]{12}" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filecore.co.nz" }, urls = { "https?://(www\\.)?(filecore\\.co\\.nz|filecore\\.co|fcore\\.eu)/(vidembed\\-)?[a-z0-9]{12}" }, flags = { 0 })
 public class FilecoreCoNz extends PluginForHost {
 
     private String                         correctedBR                  = "";
