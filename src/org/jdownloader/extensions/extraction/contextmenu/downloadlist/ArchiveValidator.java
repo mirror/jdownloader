bfaf8e5b355639bcb7afe886cba94ec313288cb5
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-25 16:56:14 +0000

    ArchiveValidator: added some more methods
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29292 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 65a67ecf52f461573052a86abde9e3c676758ce7

--- src/org/jdownloader/extensions/extraction/contextmenu/downloadlist/ArchiveValidator.java
+++ src/org/jdownloader/extensions/extraction/contextmenu/downloadlist/ArchiveValidator.java
@@ -5,9 +5,11 @@ import java.util.List;
 import java.util.WeakHashMap;
 
 import jd.controlling.linkcrawler.CrawledLink;
+import jd.controlling.packagecontroller.AbstractPackageChildrenNode;
 import jd.plugins.DownloadLink;
 
 import org.jdownloader.extensions.extraction.Archive;
+import org.jdownloader.extensions.extraction.ArchiveFactory;
 import org.jdownloader.extensions.extraction.ExtractionExtension;
 import org.jdownloader.extensions.extraction.bindings.crawledlink.CrawledLinkFactory;
 import org.jdownloader.extensions.extraction.bindings.downloadlink.DownloadLinkArchiveFactory;
@@ -61,35 +63,31 @@ public class ArchiveValidator {
     }
 
     public static List<Archive> getArchives(SelectionInfo<?, ?> si) {
-        ExtractionExtension extractor = EXTENSION;
-        ArrayList<Archive> archives = new ArrayList<Archive>();
-        if (extractor == null) {
-            return archives;
+        final ExtractionExtension extractor = EXTENSION;
+        if (extractor != null) {
+            final List<AbstractPackageChildrenNode> children = new ArrayList<AbstractPackageChildrenNode>();
+            for (Object l : si.getChildren()) {
+                if (l instanceof AbstractPackageChildrenNode) {
+                    children.add((AbstractPackageChildrenNode) l);
+                }
+            }
+            return getArchivesFromPackageChildren(children);
         }
+        return new ArrayList<Archive>();
+    }
 
-        nextLink: for (Object l : si.getChildren()) {
-            if (l instanceof CrawledLink) {
-                CrawledLinkFactory clf = new CrawledLinkFactory(((CrawledLink) l));
-                for (Archive a : archives) {
-                    if (a.contains(clf)) {
-                        continue nextLink;
+    public static List<Archive> getArchivesFromArchiveFactories(List<ArchiveFactory> archiveFactories) {
+        final ExtractionExtension extractor = EXTENSION;
+        final ArrayList<Archive> archives = new ArrayList<Archive>();
+        if (extractor != null && archiveFactories != null) {
+            for (ArchiveFactory af : archiveFactories) {
+                for (Archive archive : archives) {
+                    if (archive.contains(af)) {
+                        continue;
                     }
                 }
-                if (extractor.isLinkSupported(clf)) {
-                    Archive archive = extractor.getArchiveByFactory(clf);
-                    if (archive != null) {
-                        archives.add(archive);
-                    }
-                }
-            } else if (l instanceof DownloadLink) {
-                DownloadLinkArchiveFactory clf = new DownloadLinkArchiveFactory(((DownloadLink) l));
-                for (Archive a : archives) {
-                    if (a.contains(clf)) {
-                        continue nextLink;
-                    }
-                }
-                if (extractor.isLinkSupported(clf)) {
-                    Archive archive = extractor.getArchiveByFactory(clf);
+                if (extractor.isLinkSupported(af)) {
+                    final Archive archive = extractor.getArchiveByFactory(af);
                     if (archive != null) {
                         archives.add(archive);
                     }
@@ -97,5 +95,23 @@ public class ArchiveValidator {
             }
         }
         return archives;
+    }
+
+    public static List<Archive> getArchivesFromPackageChildren(List<AbstractPackageChildrenNode> childrenNodes) {
+        final ExtractionExtension extractor = EXTENSION;
+        if (extractor != null && childrenNodes != null) {
+            final List<ArchiveFactory> archiveFactories = new ArrayList<ArchiveFactory>();
+            for (AbstractPackageChildrenNode l : childrenNodes) {
+                if (l instanceof CrawledLink) {
+                    archiveFactories.add(new CrawledLinkFactory(((CrawledLink) l)));
+                } else if (l instanceof DownloadLink) {
+                    archiveFactories.add(new DownloadLinkArchiveFactory(((DownloadLink) l)));
+                } else {
+                    continue;
+                }
+            }
+            return getArchivesFromArchiveFactories(archiveFactories);
+        }
+        return new ArrayList<Archive>();
     }
 }
