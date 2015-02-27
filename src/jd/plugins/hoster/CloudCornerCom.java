b0c758b62fc011fcb0ec7954fce1c0584244c1df
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-02-27 15:32:29 +0000

    -added support for snakefilescom + premium RE: admin, forum -vkcom: https support for doc-links -added support for cloudcornercom - very basic as their site is pretty much broken as I was only able to upload one testfile closes #63403 -RIP: fixes #62611
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29327 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/CloudCornerCom.java
@@ -0,0 +1,298 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.util.LinkedHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "cloudcorner.com" }, urls = { "https?://(www\\.)?cloudcorner\\.com/file/[a-f0-9]{32}" }, flags = { 0 })
+public class CloudCornerCom extends PluginForHost {
+
+    public CloudCornerCom(PluginWrapper wrapper) {
+        super(wrapper);
+        // this.enablePremium("");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.cloudcorner.com/agreement/terms";
+    }
+
+    /** TODO: Add account support once we get a premium account */
+    /* INFO: New project of mixturecloud.com */
+    /* Connection stuff */
+    private static final boolean FREE_RESUME       = false;
+    private static final int     FREE_MAXCHUNKS    = 1;
+    private static final int     FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_FREE_RESUME = true;
+    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
+    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
+
+    /* don't touch the following! */
+    private static AtomicInteger maxPrem           = new AtomicInteger(1);
+
+    private String               dllink;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        dllink = null;
+        this.setBrowserExclusive();
+        br.getPage("https://api.cloudcorner.com/api/file/" + getFID(link) + "/");
+        /* Offline is usually empty page with 404 response and text/htmp content-type */
+        if (br.getHttpConnection().getResponseCode() == 404 || !this.br.getHttpConnection().getContentType().equals("application/json")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) jd.plugins.hoster.DummyScriptEnginePlugin.jsonToJavaObject(br.toString());
+        final LinkedHashMap<String, Object> data = (LinkedHashMap<String, Object>) entries.get("data");
+        final LinkedHashMap<String, Object> file_info = (LinkedHashMap<String, Object>) data.get("file_info");
+        final String filename = (String) file_info.get("name");
+        final long filesize = ((Number) file_info.get("size_num")).longValue();
+        dllink = (String) file_info.get("file_location");
+        if (filename == null || dllink == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        /* We got an API so we can trust this information */
+        link.setFinalFileName(filename);
+        link.setDownloadSize(filesize);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        doFree(downloadLink, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink downloadLink, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        // String dllink = checkDirectLink(downloadLink, directlinkproperty);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, resumable, maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        downloadLink.setProperty(directlinkproperty, dllink);
+        dl.startDownload();
+    }
+
+    // private String checkDirectLink(final DownloadLink downloadLink, final String property) {
+    // String dllink = downloadLink.getStringProperty(property);
+    // if (dllink != null) {
+    // URLConnectionAdapter con = null;
+    // try {
+    // final Browser br2 = br.cloneBrowser();
+    // if (isJDStable()) {
+    // con = br2.openGetConnection(dllink);
+    // } else {
+    // con = br2.openHeadConnection(dllink);
+    // }
+    // if (con.getContentType().contains("html") || con.getLongContentLength() == -1) {
+    // downloadLink.setProperty(property, Property.NULL);
+    // dllink = null;
+    // }
+    // } catch (final Exception e) {
+    // downloadLink.setProperty(property, Property.NULL);
+    // dllink = null;
+    // } finally {
+    // try {
+    // con.disconnect();
+    // } catch (final Throwable e) {
+    // }
+    // }
+    // }
+    // return dllink;
+    // }
+
+    private boolean isJDStable() {
+        return System.getProperty("jd.revision.jdownloaderrevision") == null;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    // private static final String MAINPAGE = "http://cloudcorner.com";
+    // private static Object LOCK = new Object();
+    //
+    // @SuppressWarnings("unchecked")
+    // private void login(final Account account, final boolean force) throws Exception {
+    // synchronized (LOCK) {
+    // try {
+    // // Load cookies
+    // br.setCookiesExclusive(true);
+    // final Object ret = account.getProperty("cookies", null);
+    // boolean acmatch = Encoding.urlEncode(account.getUser()).equals(account.getStringProperty("name",
+    // Encoding.urlEncode(account.getUser())));
+    // if (acmatch) {
+    // acmatch = Encoding.urlEncode(account.getPass()).equals(account.getStringProperty("pass", Encoding.urlEncode(account.getPass())));
+    // }
+    // if (acmatch && ret != null && ret instanceof HashMap<?, ?> && !force) {
+    // final HashMap<String, String> cookies = (HashMap<String, String>) ret;
+    // if (account.isValid()) {
+    // for (final Map.Entry<String, String> cookieEntry : cookies.entrySet()) {
+    // final String key = cookieEntry.getKey();
+    // final String value = cookieEntry.getValue();
+    // br.setCookie(MAINPAGE, key, value);
+    // }
+    // return;
+    // }
+    // }
+    // br.setFollowRedirects(false);
+    // br.getPage("");
+    // br.postPage("", "username=" + Encoding.urlEncode(account.getUser()) + "&password=" + Encoding.urlEncode(account.getPass()));
+    // if (br.getCookie(MAINPAGE, "") == null) {
+    // if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+    // throw new PluginException(LinkStatus.ERROR_PREMIUM,
+    // "\r\nUngültiger Benutzername oder ungültiges Passwort!\r\nSchnellhilfe: \r\nDu bist dir sicher, dass dein eingegebener Benutzername und Passwort stimmen?\r\nFalls dein Passwort Sonderzeichen enthält, ändere es und versuche es erneut!",
+    // PluginException.VALUE_ID_PREMIUM_DISABLE);
+    // } else {
+    // throw new PluginException(LinkStatus.ERROR_PREMIUM,
+    // "\r\nInvalid username/password!\r\nQuick help:\r\nYou're sure that the username and password you entered are correct?\r\nIf your password contains special characters, change it (remove them) and try again!",
+    // PluginException.VALUE_ID_PREMIUM_DISABLE);
+    // }
+    // }
+    // // Save cookies
+    // final HashMap<String, String> cookies = new HashMap<String, String>();
+    // final Cookies add = br.getCookies(MAINPAGE);
+    // for (final Cookie c : add.getCookies()) {
+    // cookies.put(c.getKey(), c.getValue());
+    // }
+    // account.setProperty("name", Encoding.urlEncode(account.getUser()));
+    // account.setProperty("pass", Encoding.urlEncode(account.getPass()));
+    // account.setProperty("cookies", cookies);
+    // } catch (final PluginException e) {
+    // account.setProperty("cookies", Property.NULL);
+    // throw e;
+    // }
+    // }
+    // }
+    //
+    // @Override
+    // public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+    // final AccountInfo ai = new AccountInfo();
+    // try {
+    // login(account, true);
+    // } catch (PluginException e) {
+    // account.setValid(false);
+    // throw e;
+    // }
+    // String space = br.getRegex("").getMatch(0);
+    // if (space != null) {
+    // ai.setUsedSpace(space.trim());
+    // }
+    // ai.setUnlimitedTraffic();
+    // if (account.getBooleanProperty("free", false)) {
+    // maxPrem.set(ACCOUNT_FREE_MAXDOWNLOADS);
+    // try {
+    // account.setType(AccountType.FREE);
+    // /* free accounts can still have captcha */
+    // account.setMaxSimultanDownloads(maxPrem.get());
+    // account.setConcurrentUsePossible(false);
+    // } catch (final Throwable e) {
+    // /* not available in old Stable 0.9.581 */
+    // }
+    // ai.setStatus("Registered (free) user");
+    // } else {
+    // final String expire = br.getRegex("").getMatch(0);
+    // if (expire == null) {
+    // final String lang = System.getProperty("user.language");
+    // if ("de".equalsIgnoreCase(lang)) {
+    // throw new PluginException(LinkStatus.ERROR_PREMIUM,
+    // "\r\nUngültiger Benutzername/Passwort oder nicht unterstützter Account Typ!\r\nSchnellhilfe: \r\nDu bist dir sicher, dass dein eingegebener Benutzername und Passwort stimmen?\r\nFalls dein Passwort Sonderzeichen enthält, ändere es und versuche es erneut!",
+    // PluginException.VALUE_ID_PREMIUM_DISABLE);
+    // } else {
+    // throw new PluginException(LinkStatus.ERROR_PREMIUM,
+    // "\r\nInvalid username/password or unsupported account type!\r\nQuick help:\r\nYou're sure that the username and password you entered are correct?\r\nIf your password contains special characters, change it (remove them) and try again!",
+    // PluginException.VALUE_ID_PREMIUM_DISABLE);
+    // }
+    // } else {
+    // ai.setValidUntil(TimeFormatter.getMilliSeconds(expire, "dd MMMM yyyy", Locale.ENGLISH));
+    // }
+    // maxPrem.set(ACCOUNT_PREMIUM_MAXDOWNLOADS);
+    // try {
+    // account.setType(AccountType.PREMIUM);
+    // account.setMaxSimultanDownloads(maxPrem.get());
+    // account.setConcurrentUsePossible(true);
+    // } catch (final Throwable e) {
+    // /* not available in old Stable 0.9.581 */
+    // }
+    // ai.setStatus("Premium Account");
+    // }
+    // account.setValid(true);
+    // return ai;
+    // }
+    //
+    // @Override
+    // public void handlePremium(final DownloadLink link, final Account account) throws Exception {
+    // requestFileInformation(link);
+    // login(account, false);
+    // br.setFollowRedirects(false);
+    // br.getPage(link.getDownloadURL());
+    // if (account.getBooleanProperty("free", false)) {
+    // doFree(link, ACCOUNT_FREE_RESUME, ACCOUNT_FREE_MAXCHUNKS, "account_free_directlink");
+    // } else {
+    // String dllink = this.checkDirectLink(link, "premium_directlink");
+    // if (dllink == null) {
+    // dllink = br.getRegex("").getMatch(0);
+    // if (dllink == null) {
+    // logger.warning("Final downloadlink (String is \"dllink\") regex didn't match!");
+    // throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+    // }
+    // }
+    // dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, ACCOUNT_PREMIUM_RESUME, ACCOUNT_PREMIUM_MAXCHUNKS);
+    // if (dl.getConnection().getContentType().contains("html")) {
+    // logger.warning("The final dllink seems not to be a file!");
+    // br.followConnection();
+    // throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+    // }
+    // link.setProperty("premium_directlink", dllink);
+    // dl.startDownload();
+    // }
+    // }
+
+    private String getFID(final DownloadLink dl) {
+        return new Regex(dl.getDownloadURL(), "([a-f0-9]{32})$").getMatch(0);
+    }
+
+    // @Override
+    // public int getMaxSimultanPremiumDownloadNum() {
+    // /* workaround for free/premium issue on stable 09581 */
+    // return maxPrem.get();
+    // }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
