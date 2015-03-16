016ba7b0815a7863eb417f77cd3f9c14db63038e
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-03-16 16:02:31 +0000

    Uploaded: fixed getLoginSHA1Hash
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@29564 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 456e9045adfdd03052fa2fea5c53cfa235a3ee3f

--- src/jd/plugins/hoster/Uploadedto.java
+++ src/jd/plugins/hoster/Uploadedto.java
@@ -1063,7 +1063,8 @@ public class Uploadedto extends PluginForHost {
             if (arg != null) {
                 getLogger().info("Input " + arg);
                 arg = arg.replaceAll("(\\\\|\\\"|\0|')", "\\\\$1");
-                arg = URLDecoder.decode(arg, "ISO-8859-1");
+                arg = arg.replaceAll("+", " ");
+                // arg = URLDecoder.decode(arg, "ISO-8859-1"); <<- causes issues with % in pw
                 arg = arg.replaceAll("[ \t\n\r\0\u000B]", "");
                 while (arg.startsWith("%20")) {
                     arg = arg.substring(3);
