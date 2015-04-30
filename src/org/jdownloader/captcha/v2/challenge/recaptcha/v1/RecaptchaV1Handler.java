652e872f495577fd8cbaa4c1090946e9d251787f
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-04-30 10:19:54 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30094 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/challenge/recaptcha/v1/RecaptchaV1Handler.java
+++ src/org/jdownloader/captcha/v2/challenge/recaptcha/v1/RecaptchaV1Handler.java
@@ -119,7 +119,7 @@ public abstract class RecaptchaV1Handler {
             ref.dispose();
         }
         ;
-        if (!BrowserSolverService.getInstance().getConfig().isBrowserLoopUserConfirmed()) {
+        if (!BrowserSolverService.getInstance().getConfig().isBrowserLoopUserConfirmed() || true) {
             ConfirmDialog d = new ConfirmDialog(UIOManager.LOGIC_COUNTDOWN, _GUI._.RecaptchaV1Handler_load_help__title(), _GUI._.RecaptchaV1Handler_load_help_msg(), new AbstractIcon(IconKey.ICON_OCR, 32), _GUI._.RecaptchaV1Handler_ok(), _GUI._.RecaptchaV1Handler_disable()) {
                 {
 
@@ -177,7 +177,7 @@ public abstract class RecaptchaV1Handler {
                     return 700;
                 };
             };
-
+            d.setTimeout(120000);
             UIOManager.I().show(ConfirmDialogInterface.class, d);
             try {
                 d.throwCloseExceptions();
