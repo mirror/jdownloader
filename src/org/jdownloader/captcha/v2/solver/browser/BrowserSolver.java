82e633d99bf06c83d4a3a0efe50f7cab3a44da14
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-04-30 08:23:10 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30091 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 554940ae592d5c7d5c6a1b5b33f8489217a3af7c

--- src/org/jdownloader/captcha/v2/solver/browser/BrowserSolver.java
+++ src/org/jdownloader/captcha/v2/solver/browser/BrowserSolver.java
@@ -1,8 +1,6 @@
 package org.jdownloader.captcha.v2.solver.browser;
 
-import org.appwork.utils.Application;
 import org.jdownloader.captcha.v2.Challenge;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.RecaptchaV1Challenge;
 import org.jdownloader.captcha.v2.solver.service.BrowserSolverService;
 import org.jdownloader.settings.advanced.AdvancedConfigManager;
 
@@ -21,12 +19,7 @@ public class BrowserSolver extends AbstractBrowserSolver {
 
     @Override
     public boolean canHandle(Challenge<?> c) {
-        if (Application.isJared(null) || true) {
-            // Not ready yet
-            if (c instanceof RecaptchaV1Challenge) {
-                return false;
-            }
-        }
+
         return c instanceof AbstractBrowserChallenge && super.canHandle(c);
     }
 
