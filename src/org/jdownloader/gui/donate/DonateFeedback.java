a807a3dcb1403e81fedcfe560874bb495dade60f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-05 13:36:39 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30174 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 78e000502cdae51147086bb7276de454046f2598

--- src/org/jdownloader/gui/donate/DonateFeedback.java
+++ src/org/jdownloader/gui/donate/DonateFeedback.java
@@ -2,8 +2,12 @@ package org.jdownloader.gui.donate;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.UnknownHostException;
 
+import jd.controlling.reconnect.ipcheck.BalancedWebIPCheck;
+import jd.controlling.reconnect.ipcheck.IP;
 import jd.http.Browser;
+import jd.http.Browser.BrowserException;
 
 import org.appwork.uio.InputDialogInterface;
 import org.appwork.uio.UIOManager;
@@ -22,21 +26,38 @@ public class DonateFeedback {
 
     public static void reportFailed(Throwable e) {
         try {
-            InputDialog d = null;
+            final InputDialog d = new InputDialog(Dialog.STYLE_LARGE, _GUI._.DonateFeedback_reportFailed_title_(), _GUI._.DonateFeedback_reportFailed_title_message(), null, new AbstractIcon(IconKey.ICON_QUESTION, 32), _GUI._.lit_send(), null);
 
-            d = new InputDialog(Dialog.STYLE_LARGE, _GUI._.DonateFeedback_reportFailed_title_(), _GUI._.DonateFeedback_reportFailed_title_message(), null, new AbstractIcon(IconKey.ICON_QUESTION, 32), _GUI._.lit_send(), null);
-
-            InputDialogInterface response = UIOManager.I().show(InputDialogInterface.class, d);
+            final InputDialogInterface response = UIOManager.I().show(InputDialogInterface.class, d);
 
             response.throwCloseExceptions();
-
-            String txt = response.getText();
-            if (e != null) {
-                txt += "\r\n\r\nException: \r\n" + Exceptions.getStackTrace(e);
+            final StringBuilder txt = new StringBuilder();
+            txt.append(response.getText());
+            if (txt.length() > 0) {
+                txt.append("\r\n\r\n");
             }
-            if (StringUtils.isNotEmpty(txt)) {
+            if (e != null) {
+                if (e instanceof BrowserException) {
+                    try {
+                        txt.append(((BrowserException) e).getRequest().printHeaders() + "\r\n\r\n");
+                    } catch (final Throwable ignore) {
+                    }
+                }
+                if (Exceptions.containsInstanceOf(e, UnknownHostException.class)) {
+                    try {
+                        final IP ip = new BalancedWebIPCheck().getExternalIP();
+                        txt.append("\r\n\r\n" + ip.getIP() + "\r\n\r\n");
+                    } catch (Throwable ignore) {
+                    }
+                }
+                txt.append("\r\n\r\nException: \r\n" + Exceptions.getStackTrace(e));
+                if (e.getCause() != null) {
+                    txt.append("\r\n\r\nCause: \r\n" + Exceptions.getStackTrace(e.getCause()));
+                }
+            }
+            if (txt.length() > 0) {
                 Browser br = new Browser();
-                br.postPageRaw("http://update3.jdownloader.org/jdserv/RedirectInterface/submitKayakoTicket", URLEncode.encodeRFC2396("noreply@jdownloader.org") + "&" + URLEncode.encodeRFC2396("Unknown User") + "&" + URLEncode.encodeRFC2396("Failed Transaction Feedback") + "&" + URLEncode.encodeRFC2396(txt));
+                br.postPageRaw("http://update3.jdownloader.org/jdserv/RedirectInterface/submitKayakoTicket", URLEncode.encodeRFC2396("noreply@jdownloader.org") + "&" + URLEncode.encodeRFC2396("Unknown User") + "&" + URLEncode.encodeRFC2396("Failed Transaction Feedback") + "&" + URLEncode.encodeRFC2396(txt.toString()));
             }
         } catch (DialogClosedException e1) {
             e1.printStackTrace();
