0589ff58aed5a2c885d56dbea91beb4d6e845a31
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-07 06:43:42 +0000

    Fixed: linkbucks.com, /url/ or UrlEncoded links - re: IRC Fixed: adf.ly, be aware that you must not use capture based () within host. It will fk with code below.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30210 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/LnkBcks.java
+++ src/jd/plugins/decrypter/LnkBcks.java
@@ -27,7 +27,6 @@ import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.PluginForDecrypt;
 import jd.utils.JDUtilities;
 
 // DEV NOTES:
@@ -46,10 +45,10 @@ names = { "cash4files.com", "megaline.co", "qqc.co", "theseblogs.com", "thesefor
 urls = { "http://([a-f0-9]{8}\\.cash4files\\.com|(www\\.)?cash4files\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.megaline\\.co|(www\\.)?megaline\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.qqc\\.co|(www\\.)?qqc\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.theseblogs\\.com|(www\\.)?theseblogs\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.theseforums\\.com|(www\\.)?theseforums\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.ultrafiles\\.net|(www\\.)?ultrafiles\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.urlbeat\\.net|(www\\.)?urlbeat\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.whackyvidz\\.com|(www\\.)?whackyvidz\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.yyv\\.co|(www\\.)?yyv\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.amy\\.gs|(www\\.)?amy\\.gs/(link/)?[a-zA-Z0-9]{4,8})",
         "http://([a-f0-9]{8}\\.deb\\.gs|(www\\.)?deb\\.gs/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.drstickyfingers\\.com|(www\\.)?drstickyfingers\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.fapoff\\.com|(www\\.)?fapoff\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.freean\\.us|(www\\.)?freean\\.us/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.freegaysitepass\\.com|(www\\.)?freegaysitepass\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.galleries\\.bz|(www\\.)?galleries\\.bz/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.hornywood\\.tv|(www\\.)?hornywood\\.tv/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.picbucks\\.com|(www\\.)?picbucks\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.poontown\\.net|(www\\.)?poontown\\.net/(link/)?[a-zA-Z0-9]{4,8})",
         "http://([a-f0-9]{8}\\.rqq\\.co|(www\\.)?rqq\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.sexpalace\\.gs|(www\\.)?sexpalace\\.gs/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.youfap\\.me|(www\\.)?youfap\\.me/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.zff\\.co|(www\\.)?zff\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.tubeviral\\.com|(www\\.)?tubeviral\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.whackyvidz\\.com|(www\\.)?whackyvidz\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.linkbabes\\.com|(www\\.)?linkbabes\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.dyo\\.gs|(www\\.)?dyo\\.gs/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.filesonthe\\.net|(www\\.)?filesonthe\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.cash4files\\.com|(www\\.)?cash4files\\.com/(link/)?[a-zA-Z0-9]{4,8})",
-        "http://([a-f0-9]{8}\\.seriousdeals\\.net|(www\\.)?seriousdeals\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.any\\.gs|(www\\.)?any\\.gs/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.goneviral\\.com|(www\\.)?goneviral\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.ultrafiles\\.net|(www\\.)?ultrafiles\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.miniurls\\.co|(www\\.)?miniurls\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.tinylinks\\.co|(www\\.)?tinylinks\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.yyv\\.co|(www\\.)?yyv\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-z0-9]{8}\\.allanalpass\\.com|(www\\.)?allanalpass\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://((www\\.)?linkbucks\\.com/(url/.+|[0-9a-zA-Z]{4,8}|\\d+)|[a-z0-9]{8}\\.linkbucks\\.com)" },
+        "http://([a-f0-9]{8}\\.seriousdeals\\.net|(www\\.)?seriousdeals\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.any\\.gs|(www\\.)?any\\.gs/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.goneviral\\.com|(www\\.)?goneviral\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.ultrafiles\\.net|(www\\.)?ultrafiles\\.net/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.miniurls\\.co|(www\\.)?miniurls\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.tinylinks\\.co|(www\\.)?tinylinks\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-f0-9]{8}\\.yyv\\.co|(www\\.)?yyv\\.co/(link/)?[a-zA-Z0-9]{4,8})", "http://([a-z0-9]{8}\\.allanalpass\\.com|(www\\.)?allanalpass\\.com/(link/)?[a-zA-Z0-9]{4,8})", "http://((www\\.)?linkbucks\\.com/(url/.+|[0-9a-zA-Z]{4,8}(?:/url/[a-f0-9]+)?|\\d+)|[a-z0-9]{8}\\.linkbucks\\.com)" },
 
-        flags = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
-public class LnkBcks extends PluginForDecrypt {
+flags = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
+public class LnkBcks extends antiDDoSForDecrypt {
 
     public LnkBcks(PluginWrapper wrapper) {
         super(wrapper);
@@ -143,16 +142,29 @@ public class LnkBcks extends PluginForDecrypt {
                 logger.warning("Decrypter broken for link: " + parameter);
                 return null;
             }
-            final long authKey = Long.parseLong(l1) + Long.parseLong(l2);
-            Browser br2 = br.cloneBrowser();
-            br2.getPage("/director/?t=" + token);
-            final long timeLeft = 5033 - (System.currentTimeMillis() - firstGet);
-            if (timeLeft > 0) {
-                sleep(timeLeft, param);
+            // for uid/url/hash links, seem to be the only ones encoded, but for now we will use the JS to determine it as its more likely
+            // to be correct longer -- raztoki 20150425
+            final String urlEncoded = new Regex(js, "UrlEncoded\\s*:\\s*(true|false|null)").getMatch(0);
+            // if (parameter.contains("/url/")) {
+            if ("true".equalsIgnoreCase(urlEncoded)) {
+                final String tt = new Regex(js, "TargetUrl:\\s*'([a-f0-9]+)'").getMatch(0);
+                final String x = this.convertFromHex(tt);
+                final String y = this.encode(x);
+                if (y != null) {
+                    link = y;
+                }
+            } else {
+                final long authKey = Long.parseLong(l1) + Long.parseLong(l2);
+                Browser br2 = br.cloneBrowser();
+                br2.getPage("/director/?t=" + token);
+                final long timeLeft = 5033 - (System.currentTimeMillis() - firstGet);
+                if (timeLeft > 0) {
+                    sleep(timeLeft, param);
+                }
+                Browser br3 = br.cloneBrowser();
+                br3.getPage("/intermission/loadTargetUrl?t=" + token + "&aK=" + authKey);
+                link = br3.getRegex("Url\":\"([^\"]+)").getMatch(0);
             }
-            Browser br3 = br.cloneBrowser();
-            br3.getPage("/intermission/loadTargetUrl?t=" + token + "&aK=" + authKey);
-            link = br3.getRegex("Url\":\"([^\"]+)").getMatch(0);
         }
         if (inValidate(link)) {
             logger.warning("Decrypter broken for link: " + parameter);
@@ -163,20 +175,78 @@ public class LnkBcks extends PluginForDecrypt {
         return decryptedLinks;
     }
 
+    @Override
+    protected boolean inValidate(final String s) {
+        return ("about:blank").equalsIgnoreCase(s) || super.inValidate(s);
+    }
+
     /**
-     * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
+     * translated into Java equivalent from source http://static.linkbucks.com/scripts/intermissionLink.js
      *
-     * @param s
-     *            Imported String to match against.
-     * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
+     * if (this.UrlEncoded == true) { this.TargetUrl = this.Encode(this.ConvertFromHex(this.TargetUrl)); }
+     *
+     *
+     * @param input
+     * @return
      * @author raztoki
-     * */
-    private boolean inValidate(final String s) {
-        if (s == null || s != null && (s.matches("[\r\n\t ]+") || s.equals("") || s.equalsIgnoreCase("about:blank"))) {
-            return true;
-        } else {
-            return false;
+     *
+     */
+    private String encode(final String input) {
+        try {
+            int[] s = new int[256];
+            int j = 0;
+            int i, x, y;
+            String res = "";
+            final String k = "function(str){vars=[],j=0,x,res='',k=arguments.callee.toString().replace(/\\s+/g,\"\");for(vari=0;i<256;i++){s[i]=i;}for(i=0;i<256;i++){j=(j+s[i]+k.charCodeAt(i%k.length))%256;x=s[i];s[i]=s[j];s[j]=x;}i=0;j=0;for(vary=0;y<str.length;y++){i=(i+1)%256;j=(j+s[i])%256;x=s[i];s[i]=s[j];s[j]=x;res+=String.fromCharCode(str.charCodeAt(y)^s[(s[i]+s[j])%256]);}returnres;}";
+
+            for (i = 0; i < 256; i++) {
+                s[i] = i;
+            }
+            for (i = 0; i < 256; i++) {
+                j = (j + s[i] + k.codePointAt((i % k.length()))) % 256;
+                x = s[i];
+                s[i] = s[j];
+                s[j] = x;
+            }
+            i = 0;
+            j = 0;
+            for (y = 0; y < input.length(); y++) {
+                i = (i + 1) % 256;
+                j = (j + s[i]) % 256;
+                x = s[i];
+                s[i] = s[j];
+                s[j] = x;
+                res += Character.toString((char) (input.codePointAt(y) ^ s[(s[i] + s[j]) % 256]));
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+
+    /**
+     *
+     * @param input
+     * @return
+     * @author raztoki
+     *
+     */
+    private String convertFromHex(final String input) {
+        try {
+            String playwiththis = input;
+            String result = "";
+            while (playwiththis.length() >= 2) {
+                final String value = playwiththis.substring(0, 2);
+                final int vi = Integer.parseInt(value, 16);
+                result += Character.toString((char) vi);
+                playwiththis = playwiththis.substring(2, playwiththis.length());
+            }
+            return result;
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /* NO OVERRIDE!! */
