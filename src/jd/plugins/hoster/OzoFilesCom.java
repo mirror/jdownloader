6c05c38ea44847c5c9d5754d44849a3dc3cfeb87
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-24 04:10:10 +0000

    Ozofiles.com - Wait time regex adjustment fixes #65718
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30355 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/OzoFilesCom.java
+++ src/jd/plugins/hoster/OzoFilesCom.java
@@ -92,7 +92,7 @@ public class OzoFilesCom extends PluginForHost {
     /* DEV NOTES */
     // XfileSharingProBasic Version 2.6.6.7
     // Tags: Script, template
-    // mods:
+    // mods: Wait time regex
     // limit-info:
     // protocol: no https
     // captchatype: null
@@ -559,13 +559,13 @@ public class OzoFilesCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -665,7 +665,7 @@ public class OzoFilesCom extends PluginForHost {
     private void waitTime(long timeBefore, final DownloadLink downloadLink) throws PluginException {
         int passedTime = (int) ((System.currentTimeMillis() - timeBefore) / 1000) - 1;
         /** Ticket Time */
-        final String ttt = new Regex(correctedBR, "id=\"countdown_str\">[^<>\"]+<span id=\"[^<>\"]+\"( class=\"[^<>\"]+\")?>([\n ]+)?(\\d+)([\n ]+)?</span>").getMatch(2);
+        final String ttt = new Regex(correctedBR, "id=\"countdown_str\">.*?class=\"count\">(\\d+)</span>").getMatch(0);
         if (ttt != null) {
             int wait = Integer.parseInt(ttt);
             wait -= passedTime;
@@ -681,7 +681,7 @@ public class OzoFilesCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
@@ -707,7 +707,7 @@ public class OzoFilesCom extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -724,7 +724,7 @@ public class OzoFilesCom extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
@@ -912,7 +912,7 @@ public class OzoFilesCom extends PluginForHost {
     /**
      * Is intended to handle out of date errors which might occur seldom by re-tring a couple of times before throwing the out of date
      * error.
-     *
+     * 
      * @param dl
      *            : The DownloadLink
      * @param error
