a96ff1793770e1434f3eeff4bec71326d5265e31
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-24 10:19:40 +0000

    Fixed: 4share.vn, - re: https://board.jdownloader.org/showthread.php?p=336126#post336126 - folder improvements, -- auto set filename, filesize, available status to reduce loads. -- does not need trailing / to make urls to work. -- folder ids are 16 a-f0-9 chars -- link ids are 16 a-f0-9 chars -- added packagename - hoster plugin improvements -- link ids are 16 a-f0-9 chars -- linkchecking regex for filename and filesize -- other regex improvements.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30357 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 6f99340427114ac41721a5cf4e2d6dec7eeb4ff9

--- src/jd/plugins/decrypter/Up4ShareVnFolderdecrypter.java
+++ src/jd/plugins/decrypter/Up4ShareVnFolderdecrypter.java
@@ -20,13 +20,18 @@ import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Request;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "up.4share.vn" }, urls = { "http://(www\\.)?(up\\.)?4share\\.vn/(d|dlist)/[a-z0-9]+" }, flags = { 0 })
+import org.appwork.utils.formatter.SizeFormatter;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "up.4share.vn" }, urls = { "http://(?:www\\.)?(?:up\\.)?4share\\.vn/(?:d|dlist)/[a-f0-9]{16}" }, flags = { 0 })
 public class Up4ShareVnFolderdecrypter extends PluginForDecrypt {
 
     public Up4ShareVnFolderdecrypter(PluginWrapper wrapper) {
@@ -36,6 +41,7 @@ public class Up4ShareVnFolderdecrypter extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         String parameter = param.toString().replace("up.4share.vn/", "4share.vn/");
+        br.setConnectTimeout(2 * 60 * 1000);
         br.getPage(parameter);
         if ((br.containsHTML(">Error: Not valid ID") && !br.containsHTML("up\\.4share\\.vn/f/")) || br.containsHTML("\\[Empty Folder\\]")) {
             final DownloadLink offline = createDownloadlink("directhttp://" + parameter);
@@ -44,28 +50,57 @@ public class Up4ShareVnFolderdecrypter extends PluginForDecrypt {
             logger.info("Link offline: " + parameter);
             return decryptedLinks;
         }
-        final String[] links = br.getRegex("((http://(up\\.)?4share\\.vn)?/(d/[a-z0-9]{1,}|f/[a-z0-9]+/[^<>\"]{1,}))").getColumn(0);
-        if (links == null || links.length == 0) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
-        }
-        for (String dl : links) {
-            if (!dl.startsWith("http://")) {
-                dl = "http://up.4share.vn" + dl;
-            }
-            final String fid = new Regex(dl, "f/([a-z0-9]+)/").getMatch(0);
-            if (fid != null) {
-                final DownloadLink dll = createDownloadlink(dl);
-                try {
-                    dll.setLinkID(fid);
-                } catch (Throwable e) {
-                    /* Not available in old 0.9.581 Stable */
-                    dll.setProperty("LINKDUPEID", fid);
+        final String fpName = br.getRegex("<b>Thư mục:\\s*(.*?)\\s*</b>").getMatch(0);
+        final String[] filter = br.getRegex("<tr>\\s*<td>.*?</td></tr>").getColumn(-1);
+        if (filter != null && filter.length > 0) {
+            for (final String f : filter) {
+                final String url = new Regex(f, "('|\")((?:https?://(?:up\\.)?4share\\.vn)?/(?:d/[a-f0-9]{16}|f/[a-f0-9]{16}/.*?))\\1").getMatch(1);
+                if (url == null) {
+                    continue;
                 }
-                decryptedLinks.add(dll);
+                String name = new Regex(f, ">\\s*([^<]+)\\s*</a>").getMatch(0);
+                if (name == null) {
+                    name = url.substring(url.lastIndexOf("/") + 1);
+                }
+                final String size = new Regex(f, ">\\s*(\\d+(?:\\.\\d+)?\\s*(?:B(?:yte)?|KB|MB|GB))\\s*<").getMatch(0);
+                final DownloadLink dl = createDownloadlink(Request.getLocation(url, br.getRequest()));
+                if (name != null) {
+                    dl.setName(name.trim());
+                    dl.setAvailableStatus(AvailableStatus.TRUE);
+                }
+                if (size != null) {
+                    dl.setDownloadSize(SizeFormatter.getSize(size));
+                }
+                decryptedLinks.add(dl);
             }
         }
-
+        if (decryptedLinks.isEmpty()) {
+            // fail over
+            final String[] links = br.getRegex("(?:https?://(?:up\\.)?4share\\.vn)?/(?:d/[a-f0-9]{16}|f/[a-f0-9]{16}/[^<>\"]{1,})").getColumn(-1);
+            if (links == null || links.length == 0) {
+                logger.warning("Decrypter broken for link: " + parameter);
+                return null;
+            }
+            for (String dl : links) {
+                dl = Request.getLocation(dl, br.getRequest()) + dl;
+                final String fid = new Regex(dl, "f/([a-f0-9]{16})/").getMatch(0);
+                if (fid != null) {
+                    final DownloadLink dll = createDownloadlink(dl);
+                    try {
+                        dll.setLinkID(fid);
+                    } catch (Throwable e) {
+                        /* Not available in old 0.9.581 Stable */
+                        dll.setProperty("LINKDUPEID", fid);
+                    }
+                    decryptedLinks.add(dll);
+                }
+            }
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(fpName);
+            fp.addLinks(decryptedLinks);
+        }
         return decryptedLinks;
     }
 
