c19708d1c7eb4a61bae2740a32f06695c3ee4aa5
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-26 04:10:20 +0000

    Crunchyroll.com - Login issue, https://board.jdownloader.org/showthread.php?t=38502&page=2
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30365 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/CrunchyRollCom.java
+++ src/jd/plugins/hoster/CrunchyRollCom.java
@@ -87,7 +87,7 @@ public class CrunchyRollCom extends PluginForHost {
 
     /**
      * Decrypt and convert the downloaded file from CrunchyRoll's own encrypted xml format into its .ass equivalent.
-     *
+     * 
      * @param downloadLink
      *            The DownloadLink to convert to .ass
      */
@@ -259,7 +259,7 @@ public class CrunchyRollCom extends PluginForHost {
 
     /**
      * Download the given file using the HTTP Android method. The file will be mp4 and have subtitles hardcoded.
-     *
+     * 
      * @param downloadLink
      *            The DownloadLink to try and download using RTMP
      */
@@ -280,7 +280,7 @@ public class CrunchyRollCom extends PluginForHost {
     /**
      * Attempt to download the given file using RTMP (rtmpdump). Needs to use the properties "valid", "rtmphost", "rtmpfile", "rtmpswf",
      * "swfdir". These are set by jd.plugins.decrypter.CrchyRollCom.setRMP() through requestFileInformation()
-     *
+     * 
      * @param downloadLink
      *            The DownloadLink to try and download using RTMP
      */
@@ -308,7 +308,7 @@ public class CrunchyRollCom extends PluginForHost {
 
     /**
      * Download subtitles and convert them to .ass
-     *
+     * 
      * @param downloadLink
      *            The DownloadLink to try and download convert to .ass
      */
@@ -391,7 +391,7 @@ public class CrunchyRollCom extends PluginForHost {
 
     /**
      * Attempt to log into crunchyroll.com using the given account. Cookies are cached to 'loginCookies'.
-     *
+     * 
      * @param account
      *            The account to use to log in.
      * @param br
@@ -438,7 +438,7 @@ public class CrunchyRollCom extends PluginForHost {
                 br.postPage("https://www.crunchyroll.com/?a=formhandler", post);
 
                 // Check if we successfully logged in
-                if (!br.containsHTML("Welcome back, .+?!") && !br.containsHTML("<title>Redirecting\\.\\.\\.</title>")) {
+                if (!br.containsHTML("Welcome back, .+?!")) {
                     // Not successful, display dialog if we were asked to
                     if (showDialog) {
                         UserIO.getInstance().requestMessageDialog(0, "Crunchyroll Login Error", "Account check needed for crunchyroll.com!");
@@ -471,7 +471,7 @@ public class CrunchyRollCom extends PluginForHost {
     /**
      * Pad and format version numbers so that the String.compare() method can be used simply. ("9.10.2", ".", 4) would result in
      * "000900100002".
-     *
+     * 
      * @param version
      *            The version number string to format (e.g. '9.10.2')
      * @param sep
@@ -603,7 +603,7 @@ public class CrunchyRollCom extends PluginForHost {
 
     /**
      * Generate the AES decryption key based on the subtitle's id using some obfuscation and SHA-1 hashing.
-     *
+     * 
      * @param id
      *            The id of the subtitles to generate the key for
      * @param size
