ab90604255739e47663054a3450f6917eb744eb1
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-28 11:20:44 +0000

    Added: RunCheckSumAction, user can run checksum action post download via right click context. TODO: theme icon
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30417 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/download/HashResult.java
+++ src/jd/plugins/download/HashResult.java
@@ -1,28 +1,60 @@
 package jd.plugins.download;
 
+import org.jdownloader.plugins.FinalLinkState;
 
 public class HashResult {
-    
+
     private final HashInfo hashInfo;
     private final String   fileHash;
-    
+
     public String getFileHash() {
         return fileHash;
     }
-    
+
     public HashResult(HashInfo hashInfo, String fileHash) {
         this.hashInfo = hashInfo;
         this.fileHash = fileHash;
     }
-    
+
+    public FinalLinkState getFinalLinkState() {
+        if (match()) {
+            switch (getHashInfo().getType()) {
+            case SHA256:
+                return FinalLinkState.FINISHED_SHA256;
+            case SHA1:
+                return FinalLinkState.FINISHED_SHA1;
+            case MD5:
+                return FinalLinkState.FINISHED_MD5;
+            case CRC32:
+                return FinalLinkState.FINISHED_CRC32;
+            default:
+                return FinalLinkState.FINISHED;
+            }
+        } else {
+            switch (getHashInfo().getType()) {
+            case SHA256:
+                return FinalLinkState.FAILED_SHA256;
+            case SHA1:
+                return FinalLinkState.FAILED_SHA1;
+            case MD5:
+                return FinalLinkState.FAILED_MD5;
+            case CRC32:
+                return FinalLinkState.FAILED_CRC32;
+            default:
+                return FinalLinkState.FAILED;
+
+            }
+        }
+    }
+
     public boolean match() {
         return fileHash != null && hashInfo.getHash().equalsIgnoreCase(fileHash);
     }
-    
+
     public HashInfo getHashInfo() {
         return hashInfo;
     }
-    
+
     @Override
     public String toString() {
         return "HashInfo: " + hashInfo + "|HashResult:" + getFileHash() + "=" + match();
---
+++ src/org/jdownloader/gui/views/downloads/action/RunCheckSumAction.java
@@ -0,0 +1,52 @@
+package org.jdownloader.gui.views.downloads.action;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import jd.controlling.TaskQueue;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.download.DownloadLinkDownloadable;
+import jd.plugins.download.Downloadable;
+import jd.plugins.download.HashInfo;
+import jd.plugins.download.HashResult;
+import jd.plugins.download.raf.OldRAFDownload;
+
+import org.appwork.utils.event.queue.QueueAction;
+import org.jdownloader.controlling.contextmenu.CustomizableTableContextAppAction;
+import org.jdownloader.gui.translate._GUI;
+
+public class RunCheckSumAction extends CustomizableTableContextAppAction<FilePackage, DownloadLink> {
+
+    public RunCheckSumAction() {
+        super();
+        setIconKey("checksum");
+        setName(_GUI._.gui_table_contextmenu_runchecksum());
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        for (final DownloadLink downloadLink : getSelection().getChildren()) {
+            final Downloadable downloadable = new DownloadLinkDownloadable(downloadLink);
+            if (downloadable.isHashCheckEnabled()) {
+                TaskQueue.getQueue().add(new QueueAction<Void, RuntimeException>() {
+
+                    @Override
+                    protected Void run() throws RuntimeException {
+                        final File file = new File(downloadable.getFileOutput());
+                        if (file.exists()) {
+                            synchronized (OldRAFDownload.HASHCHECKLOCK) {
+                                final HashInfo hashInfo = downloadable.getHashInfo();
+                                final HashResult result = downloadable.getHashResult(hashInfo, file);
+                                if (result != null) {
+                                    downloadLink.setFinalLinkState(result.getFinalLinkState());
+                                }
+                            }
+                        }
+                        return null;
+                    }
+                });
+
+            }
+        }
+    }
+}
