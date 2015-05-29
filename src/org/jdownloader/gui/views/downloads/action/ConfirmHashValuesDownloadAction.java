6526bd0ba74017bb4cf00618729b98a614a039c5
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-05-29 10:02:30 +0000

    ConfirmHashValues: -little changes -moved to TaskQueue
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30437 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 96e638555c7963b367ce7795eb6b60f9133ac9ea

--- src/org/jdownloader/gui/views/downloads/action/ConfirmHashValuesDownloadAction.java
+++ src/org/jdownloader/gui/views/downloads/action/ConfirmHashValuesDownloadAction.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
+import jd.controlling.TaskQueue;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
@@ -14,6 +15,7 @@ import org.appwork.swing.exttable.ExtTableListener;
 import org.appwork.swing.exttable.ExtTableModelEventWrapper;
 import org.appwork.swing.exttable.ExtTableModelListener;
 import org.appwork.utils.StringUtils;
+import org.appwork.utils.event.queue.QueueAction;
 import org.jdownloader.controlling.contextmenu.ActionContext;
 import org.jdownloader.controlling.contextmenu.CustomizableTableContextAppAction;
 import org.jdownloader.gui.IconKey;
@@ -29,7 +31,6 @@ public class ConfirmHashValuesDownloadAction extends CustomizableTableContextApp
     public ConfirmHashValuesDownloadAction() {
         super(true, true);
         setIconKey(IconKey.ICON_HASHSUM);
-
         setName(_GUI._.ConfirmHashValuesAction());
         addContextSetup(includedSelection = new IncludedSelectionSetup(DownloadsTable.getInstance(), this, this));
     }
@@ -53,77 +54,89 @@ public class ConfirmHashValuesDownloadAction extends CustomizableTableContextApp
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        final List<DownloadLink> links = new ArrayList<DownloadLink>();
-        final SelectionInfo<FilePackage, DownloadLink> selection = DownloadsTable.getInstance().getSelectionInfo();
+        final SelectionInfo<FilePackage, DownloadLink> selection = getSelection();
+        final List<DownloadLink> links;
         switch (includedSelection.getSelectionType()) {
         case NONE:
+            links = null;
             return;
         case SELECTED:
-            links.addAll(selection.getChildren());
+            links = selection.getChildren();
             break;
         case UNSELECTED:
             if (selection.getUnselectedChildren() != null) {
-                links.addAll(selection.getUnselectedChildren());
+                links = selection.getUnselectedChildren();
+            } else {
+                links = null;
             }
             break;
         case ALL:
-            links.addAll(DownloadsTable.getInstance().getSelectionInfo(false, true).getChildren());
+        default:
+            links = DownloadsTable.getInstance().getSelectionInfo(false, true).getChildren();
+            break;
         }
-        if (links.size() == 0) {
+        if (links == null || links.size() == 0) {
             return;
         }
-        HashMap<String, List<DownloadLink>> map = new HashMap<String, List<DownloadLink>>();
-        for (DownloadLink cl : links) {
-            List<DownloadLink> list = map.get(cl.getName());
-            if (list == null) {
-                list = new ArrayList<DownloadLink>();
-                map.put(cl.getName(), list);
-            }
-            list.add(cl);
-        }
-        main: for (Entry<String, List<DownloadLink>> se : map.entrySet()) {
-            List<DownloadLink> list = se.getValue();
-            String md5 = null;
-            String sha1 = null;
-            String sha256 = null;
-            for (DownloadLink cl : list) {
-                if (cl.getDownloadLink().getMD5Hash() != null) {
-                    if (md5 != null && !StringUtils.equalsIgnoreCase(md5, cl.getDownloadLink().getMD5Hash())) {
-                        // hashes do not match
-                        continue main;
-                    } else {
-                        md5 = cl.getDownloadLink().getMD5Hash();
+        TaskQueue.getQueue().add(new QueueAction<Void, RuntimeException>() {
+
+            @Override
+            protected Void run() throws RuntimeException {
+                HashMap<String, List<DownloadLink>> map = new HashMap<String, List<DownloadLink>>();
+                for (DownloadLink cl : links) {
+                    List<DownloadLink> list = map.get(cl.getName());
+                    if (list == null) {
+                        list = new ArrayList<DownloadLink>();
+                        map.put(cl.getName(), list);
+                    }
+                    list.add(cl);
+                }
+                main: for (Entry<String, List<DownloadLink>> se : map.entrySet()) {
+                    List<DownloadLink> list = se.getValue();
+                    String md5 = null;
+                    String sha1 = null;
+                    String sha256 = null;
+                    for (DownloadLink cl : list) {
+                        if (cl.getDownloadLink().getMD5Hash() != null) {
+                            if (md5 != null && !StringUtils.equalsIgnoreCase(md5, cl.getDownloadLink().getMD5Hash())) {
+                                // hashes do not match
+                                continue main;
+                            } else {
+                                md5 = cl.getDownloadLink().getMD5Hash();
+                            }
+                        }
+                        if (cl.getDownloadLink().getSha1Hash() != null) {
+                            if (sha1 != null && !StringUtils.equalsIgnoreCase(sha1, cl.getDownloadLink().getSha1Hash())) {
+                                // hashes do not match
+                                continue main;
+                            } else {
+                                sha1 = cl.getDownloadLink().getSha1Hash();
+                            }
+                        }
+                        if (cl.getDownloadLink().getSha256Hash() != null) {
+                            if (sha256 != null && !StringUtils.equalsIgnoreCase(sha256, cl.getDownloadLink().getSha256Hash())) {
+                                // hashes do not match
+                                continue main;
+                            } else {
+                                sha256 = cl.getDownloadLink().getSha256Hash();
+                            }
+                        }
+                    }
+                    for (DownloadLink cl : list) {
+                        if (md5 != null) {
+                            cl.getDownloadLink().setMD5Hash(md5);
+                        }
+                        if (sha1 != null) {
+                            cl.getDownloadLink().setSha1Hash(sha1);
+                        }
+                        if (sha256 != null) {
+                            cl.getDownloadLink().setSha1Hash(sha256);
+                        }
                     }
                 }
-                if (cl.getDownloadLink().getSha1Hash() != null) {
-                    if (sha1 != null && !StringUtils.equalsIgnoreCase(sha1, cl.getDownloadLink().getSha1Hash())) {
-                        // hashes do not match
-                        continue main;
-                    } else {
-                        sha1 = cl.getDownloadLink().getSha1Hash();
-                    }
-                }
-                if (cl.getDownloadLink().getSha256Hash() != null) {
-                    if (sha256 != null && !StringUtils.equalsIgnoreCase(sha256, cl.getDownloadLink().getSha256Hash())) {
-                        // hashes do not match
-                        continue main;
-                    } else {
-                        sha256 = cl.getDownloadLink().getSha256Hash();
-                    }
-                }
+                return null;
             }
-            for (DownloadLink cl : list) {
-                if (md5 != null) {
-                    cl.getDownloadLink().setMD5Hash(md5);
-                }
-                if (sha1 != null) {
-                    cl.getDownloadLink().setSha1Hash(sha1);
-                }
-                if (sha256 != null) {
-                    cl.getDownloadLink().setSha1Hash(sha256);
-                }
-            }
-        }
+        });
     }
 
     @Override
