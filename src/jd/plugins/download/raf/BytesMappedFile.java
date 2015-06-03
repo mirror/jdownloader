d7380adba2bc40fee2fa3805f3c13c6676754693
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-06-03 11:21:46 +0000

    SparseFile: -little helper class to create sparse files, refs #65820 BytesMappedFile: -added support for SparseFile
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30487 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: fd62b9b33ec9a40be412a603919ccc88ad98d701

--- src/jd/plugins/download/raf/BytesMappedFile.java
+++ src/jd/plugins/download/raf/BytesMappedFile.java
@@ -6,33 +6,44 @@ import java.io.RandomAccessFile;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import jd.plugins.download.SparseFile;
+
+import org.appwork.utils.Application;
+
 public class BytesMappedFile implements FileBytesCacheFlusher {
-    
+
     public static interface BytesMappedFileCallback {
         public void onFlush(BytesMappedFile bytesMappedFile, IOException ioException);
     }
-    
+
     protected final File                                          file;
-    
+
     protected final FileBytesMap                                  bytesMap;
-    
+
     protected volatile RandomAccessFile                           raf         = null;
-    
+
     protected final CopyOnWriteArrayList<BytesMappedFileCallback> callBacks   = new CopyOnWriteArrayList<BytesMappedFileCallback>();
-    
+
     protected final AtomicInteger                                 locks       = new AtomicInteger();
-    
+
     protected volatile IOException                                ioException = null;
-    
+
     protected volatile boolean                                    flushFlag   = false;
-    
+
+    private final boolean                                         trySparse;
+
     public BytesMappedFile(File file) {
-        this(file, new FileBytesMap());
+        this(file, new FileBytesMap(), true);
     }
-    
-    public BytesMappedFile(File file, FileBytesMap fileBytesMap) {
+
+    public BytesMappedFile(File file, boolean trySparse) {
+        this(file, new FileBytesMap(), trySparse);
+    }
+
+    public BytesMappedFile(File file, FileBytesMap fileBytesMap, boolean trySparse) {
         this.file = file;
         bytesMap = fileBytesMap;
+        this.trySparse = trySparse;
         final long fileSize = file.length();
         final long finalFileSize = fileBytesMap.getFinalSize();
         final long markedBytes = fileBytesMap.getMarkedBytes();
@@ -41,11 +52,11 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
             throw new IllegalStateException("File:" + file + "|Length:" + file.length() + "|FileBytesMap:" + fileBytesMap);
         }
     }
-    
+
     public synchronized void lock() {
         locks.incrementAndGet();
     }
-    
+
     public synchronized boolean unlock() {
         if (locks.get() > 0) {
             locks.decrementAndGet();
@@ -53,9 +64,15 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
         }
         return false;
     }
-    
+
     public synchronized boolean open(BytesMappedFileCallback callback) throws IOException {
         if (raf == null) {
+            try {
+                if (trySparse && Application.getJavaVersion() >= Application.JAVA17) {
+                    SparseFile.createSparseFile(file);
+                }
+            } catch (IOException e) {
+            }
             raf = new RandomAccessFile(file, "rw");
             callBacks.add(callback);
             return true;
@@ -64,27 +81,29 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
             return false;
         }
     }
-    
+
     public synchronized boolean isOpen() {
         return raf != null && callBacks.size() > 0;
     }
-    
+
     public synchronized boolean isLocked() {
         return locks.get() > 0;
     }
-    
+
     public File getFile() {
         return file;
     }
-    
+
     public FileBytesMap getFileBytesMap() {
         return bytesMap;
     }
-    
+
     public synchronized boolean close(BytesMappedFileCallback callback) throws IOException {
         if (callBacks.remove(callback) && callBacks.size() == 0) {
             try {
-                if (raf != null) raf.close();
+                if (raf != null) {
+                    raf.close();
+                }
                 return true;
             } finally {
                 ioException = null;
@@ -93,7 +112,7 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
         }
         return false;
     }
-    
+
     @Override
     public synchronized void flushed() {
         if (raf != null && flushFlag) {
@@ -103,19 +122,23 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
                     onFlushed();
                 }
             } catch (IOException e) {
-                if (this.ioException != null) this.ioException = e;
+                if (this.ioException != null) {
+                    this.ioException = e;
+                }
             } finally {
                 flushFlag = false;
                 for (BytesMappedFileCallback callback : callBacks) {
-                    if (callback != null) callback.onFlush(this, this.ioException);
+                    if (callback != null) {
+                        callback.onFlush(this, this.ioException);
+                    }
                 }
             }
         }
     }
-    
+
     protected void onFlushed() throws IOException {
     }
-    
+
     @Override
     public synchronized void flush(byte[] writeCache, int writeCachePosition, int length, long fileWritePosition) {
         if (raf != null && ioException == null) {
@@ -135,5 +158,5 @@ public class BytesMappedFile implements FileBytesCacheFlusher {
             }
         }
     }
-    
+
 }
