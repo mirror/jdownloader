a3e45ec62e68bacf9e6f89e975a3fbf55efc4702
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-06-24 16:44:18 +0000

    -fixed MOCH forced 20 chunks issue thx raztoki
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30746 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 58c241f2c02c190bcb318b6d7b5d929cd491d164

--- src/jd/plugins/hoster/JetDebridCom.java
+++ src/jd/plugins/hoster/JetDebridCom.java
@@ -510,12 +510,10 @@ public class JetDebridCom extends PluginForHost {
     private int correctChunks(int maxchunks) {
         if (maxchunks < 1) {
             maxchunks = 1;
-        } else if (maxchunks > 20) {
-            maxchunks = 20;
         } else if (maxchunks > 1) {
             maxchunks = -maxchunks;
         }
-        /* Else maxchunks = 1 */
+        /* Else maxchunks == 1 */
         return maxchunks;
     }
 
