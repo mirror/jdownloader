a3e879a87597be99567bbbf8e073898593df3e3c
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-06-27 07:02:23 +0000

    Fixed: freedisk.pl, folder decrypter. speedup&failover - closes #66532
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30786 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c68b318da9173db449f705c093a07a5a51a38f00

--- src/jd/plugins/decrypter/FreeDiscPlFolder.java
+++ src/jd/plugins/decrypter/FreeDiscPlFolder.java
@@ -21,60 +21,75 @@ import java.util.ArrayList;
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser.BrowserException;
+import jd.http.Request;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "freedisc.pl" }, urls = { "http://(www\\.)?freedisc\\.pl/[A-Za-z0-9\\-_]+,d\\-\\d+([A-Za-z0-9\\-_,]+)?" }, flags = { 0 })
+import org.appwork.utils.formatter.SizeFormatter;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "freedisc.pl" }, urls = { "http://(www\\.)?freedisc\\.pl/[A-Za-z0-9_\\-]+,d\\-\\d+([A-Za-z0-9_,\\-]+)?" }, flags = { 0 })
 public class FreeDiscPlFolder extends PluginForDecrypt {
 
     public FreeDiscPlFolder(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final String TYPE_FOLDER = "http://(www\\.)?freedisc\\.pl/[A-Za-z0-9\\-_]+,d\\-\\d+";
+    private static final String TYPE_FOLDER = "http://(www\\.)?freedisc\\.pl/[A-Za-z0-9\\-_]+,d-\\d+";
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         br.setFollowRedirects(true);
         try {
             br.getPage(parameter);
         } catch (final BrowserException e) {
-            final DownloadLink offline = createDownloadlink("directhttp://" + parameter);
-            offline.setAvailable(false);
-            offline.setProperty("offline", true);
-            decryptedLinks.add(offline);
+            decryptedLinks.add(createOfflinelink(parameter));
             return decryptedLinks;
         }
-        final String username = new Regex(parameter, "freedisc\\.pl/([A-Za-z0-9\\-_]+),d\\-\\d+").getMatch(0);
-        final String fpName = br.getRegex("<title>([^<>\"]*?)\\-  Freedisc\\.pl</title>").getMatch(0);
-        // style='float: left; overflow: auto;'><a href="
-        final String[] links = br.getRegex("class=\\'name\\'><a href=\"(/[^<>\"]*?,f\\-[^<>\"]*?)\"").getColumn(0);
-        final String[] folders = br.getRegex("class=\\'name\\'><a href=\"([A-Za-z0-9\\-_]+,d\\-\\d+[^<>\"]*?)\"").getColumn(0);
-        if ((links == null || links.length == 0) && (folders == null || folders.length == 0) && br.containsHTML("class=\"directoryText previousDirLinkFS\"")) {
-            final DownloadLink offline = createDownloadlink("directhttp://" + parameter);
-            offline.setAvailable(false);
-            offline.setProperty("offline", true);
-            decryptedLinks.add(offline);
-            return decryptedLinks;
-        }
-        if (links != null && links.length > 0) {
-            for (String singleLink : links) {
-                if (!singleLink.startsWith("/")) {
-                    singleLink = "/" + singleLink;
+        final String fpName = br.getRegex(">([^>]+)</h1>").getMatch(0);
+        final String[] entries = br.getRegex("div class=\"dir-item\"><div.*?</div></div></div>").getColumn(-1);
+        final String fileEntry = "class=('|\"|)[\\w -]+\\1><a href=\"(/[^<>\"]*?,f\\-[^<>\"]*?)\"[^>]*>(.*?)</a>";
+        final String folderEntry = "class=('|\"|)[\\w -]+\\1><a href=\"(/?[A-Za-z0-9\\-_]+,d\\-\\d+[^<>\"]*?)\"";
+        if (entries != null && entries.length > 0) {
+            for (final String e : entries) {
+                final String folder = new Regex(e, folderEntry).getMatch(1);
+                if (folder != null) {
+                    decryptedLinks.add(createDownloadlink(Request.getLocation(folder, br.getRequest())));
+                    continue;
                 }
-                singleLink = "http://freedisc.pl" + singleLink;
-                decryptedLinks.add(createDownloadlink(singleLink));
+                final String link = new Regex(e, fileEntry).getMatch(1);
+                final String filename = new Regex(e, fileEntry).getMatch(2);
+                final String filesize = new Regex(e, "info\">Rozmiar :(.*?)<").getMatch(0);
+                final DownloadLink dl = createDownloadlink(Request.getLocation(link, br.getRequest()));
+                dl.setName(filename);
+                dl.setDownloadSize(SizeFormatter.getSize(filesize.replace("Bajty", "Bytes")));
+                dl.setAvailableStatus(AvailableStatus.TRUE);
+                decryptedLinks.add(dl);
             }
-        }
-        if (folders != null && folders.length > 0) {
-            for (final String singleLink : folders) {
-                decryptedLinks.add(createDownloadlink("http://freedisc.pl" + singleLink));
+        } else {
+            // fail over
+            final String[] links = br.getRegex(fileEntry).getColumn(0);
+            final String[] folders = br.getRegex(folderEntry).getColumn(1);
+            if ((links == null || links.length == 0) && (folders == null || folders.length == 0) && br.containsHTML("class=\"directoryText previousDirLinkFS\"")) {
+                decryptedLinks.add(createOfflinelink(parameter));
+                return decryptedLinks;
+            }
+            if (links != null && links.length > 0) {
+                for (String singleLink : links) {
+                    singleLink = Request.getLocation(singleLink, br.getRequest());
+                    decryptedLinks.add(createDownloadlink(singleLink));
+                }
+            }
+            if (folders != null && folders.length > 0) {
+                for (final String singleLink : folders) {
+                    decryptedLinks.add(createDownloadlink("http://freedisc.pl" + singleLink));
+                }
             }
         }
 
