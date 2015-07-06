0c39d4bc45b209d3c416f10021798e1ad7ac823c
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-06 05:29:34 +0000

    Added: kissanime.com, decrypter support - re: https://board.jdownloader.org/showthread.php?t=64853
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30879 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/VideoGoogle.java
+++ src/jd/plugins/hoster/VideoGoogle.java
@@ -13,10 +13,11 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.utils.locale.JDL;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "video.google.com" }, urls = { "http://(www\\.)?video\\.google\\.(com|de)/(videoplay\\?docid=|googleplayer\\.swf\\?autoplay=1\\&fs=true\\&fs=true\\&docId=)(\\-)?\\d+" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "video.google.com" }, urls = { "http://(www\\.)?video\\.google\\.(com|de)/(videoplay\\?docid=|googleplayer\\.swf\\?autoplay=1\\&fs=true\\&fs=true\\&docId=)(\\-)?\\d+|https?://redirector\\.googlevideo\\.com/videoplayback\\?.+" }, flags = { 0 })
 public class VideoGoogle extends PluginForHost {
 
-    private String DLLINK = null;
+    private String       dllink = null;
+    private final String embed  = "https?://redirector\\.googlevideo\\.com/videoplayback\\?.+";
 
     public VideoGoogle(PluginWrapper wrapper) {
         super(wrapper);
@@ -32,50 +33,65 @@ public class VideoGoogle extends PluginForHost {
         return -1;
     }
 
-    public void correctDownloadLink(DownloadLink link) {
-        link.setUrlDownload("http://video.google.com/videoplay?docid=" + new Regex(link.getDownloadURL(), "((\\-)?\\d+)$").getMatch(0));
+    public void correctDownloadLink(final DownloadLink downloadLink) {
+        if (!downloadLink.getDownloadURL().matches(embed)) {
+            downloadLink.setUrlDownload("http://video.google.com/videoplay?docid=" + new Regex(downloadLink.getDownloadURL(), "((\\-)?\\d+)$").getMatch(0));
+        }
     }
 
     @Override
-    public void handleFree(DownloadLink link) throws Exception {
-        requestFileInformation(link);
-        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, DLLINK, true, 0);
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(this.br, downloadLink, dllink, true, 0);
         if (!dl.getConnection().isContentDisposition() && !dl.getConnection().getContentType().startsWith("video")) {
             br.followConnection();
-            if (br.containsHTML("No htmlCode read")) throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.videogoogle.videotemporaryunavailable", "This video is temporary unavailable!"), 60 * 60 * 1000l);
+            if (br.containsHTML("No htmlCode read")) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.videogoogle.videotemporaryunavailable", "This video is temporary unavailable!"), 60 * 60 * 1000l);
+            }
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
     }
 
     @Override
-    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
         this.setBrowserExclusive();
-        br.getPage(parameter.getDownloadURL());
-        // Check this way because language of site is different for everyone
-        if (!br.containsHTML("googleplayer\\.swf")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        final String videoID = new Regex(parameter.getDownloadURL(), "((\\-)?\\d+)$").getMatch(0);
-        String name = br.getRegex("<title>(.*?)</title>").getMatch(0);
-        if (name == null || "301 Moved".equals(name)) name = videoID;
-        parameter.setFinalFileName(Encoding.htmlDecode(name) + ".flv");
-        DLLINK = br.getRegex("videoUrl\\\\x3d(http://.*?\\.googlevideo\\.com/videoplayback.*?)\\\\x26thumbnailUrl").getMatch(0);
-        if (DLLINK == null) {
-            br.getPage("http://video.google.com/videofeed?fgvns=1&fai=1&docid=" + videoID);
-            DLLINK = br.getRegex("<media:content url=\"(http://[a-z0-9\\.]+\\.googlevideo\\.com/[^<>\"]*?)\"").getMatch(0);
+        if (downloadLink.getDownloadURL().matches(embed)) {
+            dllink = downloadLink.getDownloadURL();
+        } else {
+            br.getPage(downloadLink.getDownloadURL());
+            // Check this way because language of site is different for everyone
+            if (!br.containsHTML("googleplayer\\.swf")) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            final String videoID = new Regex(downloadLink.getDownloadURL(), "((\\-)?\\d+)$").getMatch(0);
+            String name = br.getRegex("<title>(.*?)</title>").getMatch(0);
+            if (name == null || "301 Moved".equals(name)) {
+                name = videoID;
+            }
+            downloadLink.setFinalFileName(Encoding.htmlDecode(name) + ".flv");
+            dllink = br.getRegex("videoUrl\\\\x3d(http://.*?\\.googlevideo\\.com/videoplayback.*?)\\\\x26thumbnailUrl").getMatch(0);
+            if (dllink == null) {
+                br.getPage("http://video.google.com/videofeed?fgvns=1&fai=1&docid=" + videoID);
+                dllink = br.getRegex("<media:content url=\"(http://[a-z0-9\\.]+\\.googlevideo\\.com/[^<>\"]*?)\"").getMatch(0);
+            }
+            if (dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dllink = Encoding.htmlDecode(dllink);
+            dllink = Encoding.urlDecode(dllink, true);
         }
-        if (DLLINK == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        DLLINK = Encoding.htmlDecode(DLLINK);
-        DLLINK = Encoding.urlDecode(DLLINK, true);
         Browser br2 = br.cloneBrowser();
         // In case the link redirects to the finallink
         br2.setFollowRedirects(true);
         URLConnectionAdapter con = null;
         try {
-            con = br2.openGetConnection(DLLINK);
-            if (!con.getContentType().contains("html"))
-                parameter.setDownloadSize(con.getLongContentLength());
-            else
+            con = br2.openGetConnection(dllink);
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
             return AvailableStatus.TRUE;
         } finally {
             try {
@@ -90,7 +106,7 @@ public class VideoGoogle extends PluginForHost {
     }
 
     @Override
-    public void resetDownloadlink(DownloadLink link) {
+    public void resetDownloadlink(final DownloadLink link) {
     }
 
 }
