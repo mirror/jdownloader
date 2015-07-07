c4355c020fa7f0968725217102eba8749a44beaf
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-07 11:59:19 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30901 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/download/SparseFile.java
+++ src/jd/plugins/download/SparseFile.java
@@ -9,11 +9,14 @@ public class SparseFile {
     public static boolean createSparseFile(File file) throws IOException {
         if (!file.exists()) {
             final java.nio.file.OpenOption[] options = { java.nio.file.StandardOpenOption.WRITE, java.nio.file.StandardOpenOption.CREATE_NEW, java.nio.file.StandardOpenOption.SPARSE };
-            final java.nio.channels.SeekableByteChannel channel = java.nio.file.Files.newByteChannel(file.toPath(), options);
-            channel.close();
-            return true;
+            try {
+                final java.nio.channels.SeekableByteChannel channel = java.nio.file.Files.newByteChannel(file.toPath(), options);
+                channel.close();
+                return true;
+            } catch (IllegalArgumentException e) {
+                throw new IOException(e);
+            }
         }
-
         return false;
     }
 }
