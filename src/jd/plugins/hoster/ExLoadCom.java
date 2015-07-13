ab8cc131b1be9a938a6f4e4ab9675cb840e571bf
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-13 02:31:17 +0000

    Ex-load.com - File name regex adjustment fixes #66922
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@30960 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d4150b93954b155a900116181b91ab68cf57d724

--- src/jd/plugins/hoster/ExLoadCom.java
+++ src/jd/plugins/hoster/ExLoadCom.java
@@ -164,7 +164,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * @author raztoki
-     *
+     * 
      * @category 'Experimental', Mods written July 2012 - 2013
      * */
     public ExLoadCom(PluginWrapper wrapper) {
@@ -315,6 +315,9 @@ public class ExLoadCom extends PluginForHost {
         if (inValidate(fileInfo[0])) {
             fileInfo[0] = cbr.getRegex("class=\"fileprem\"><strong>([^<>\"]*?) \\(\\d+").getMatch(0);
         }
+        if (inValidate(fileInfo[0])) {
+            fileInfo[0] = cbr.getRegex("class=\"fa fa-download\"></i>([^<>\"]*?)</h1>").getMatch(0);
+        }
         if (inValidate(fileInfo[1])) {
             fileInfo[1] = cbr.getRegex("\\(([0-9]+ bytes)\\)").getMatch(0);
             if (inValidate(fileInfo[1])) {
@@ -337,7 +340,7 @@ public class ExLoadCom extends PluginForHost {
     /**
      * Provides alternative linkchecking method for a single link at a time. Can be used as generic failover, though kinda pointless as this
      * method doesn't give filename...
-     *
+     * 
      * */
     private String[] altAvailStat(final DownloadLink downloadLink, final String[] fileInfo) throws Exception {
         Browser alt = new Browser();
@@ -517,7 +520,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Removes patterns which could break the plugin due to fake/hidden HTML, or false positives caused by HTML comments.
-     *
+     * 
      * @throws Exception
      * @author raztoki
      */
@@ -1063,7 +1066,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Rules to prevent new downloads from commencing
-     *
+     * 
      * */
     public boolean canHandle(DownloadLink downloadLink, Account account) {
         if (downloadLink.getBooleanProperty("requiresPremiumAccount", false) && (account == null || account.getBooleanProperty("free", false))) {
@@ -1096,7 +1099,7 @@ public class ExLoadCom extends PluginForHost {
      * Corrects downloadLink.urlDownload().<br/>
      * <br/>
      * The following code respect the hoster supported protocols via plugin boolean settings and users config preference
-     *
+     * 
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1172,7 +1175,7 @@ public class ExLoadCom extends PluginForHost {
     /**
      * Gets page <br />
      * - natively supports silly cloudflare anti DDoS crapola
-     *
+     * 
      * @author raztoki
      */
     private void getPage(final String page) throws Exception {
@@ -1317,7 +1320,7 @@ public class ExLoadCom extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
@@ -1403,7 +1406,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * captcha processing can be used download/login/anywhere assuming the submit values are the same (they usually are)...
-     *
+     * 
      * @author raztoki
      * */
     private Form captchaForm(DownloadLink downloadLink, Form form) throws Exception {
@@ -1581,13 +1584,13 @@ public class ExLoadCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlSlot
      *            (+1|-1)
      * */
@@ -1608,9 +1611,9 @@ public class ExLoadCom extends PluginForHost {
     /**
      * ControlSimHost, On error it will set the upper mark for 'max sim dl per host'. This will be the new 'static' setting used going
      * forward. Thus prevents new downloads starting when not possible and is self aware and requires no coder interaction.
-     *
+     * 
      * @param account
-     *
+     * 
      * @category 'Experimental', Mod written February 2013
      * */
     private void controlSimHost(final Account account) {
@@ -1647,7 +1650,7 @@ public class ExLoadCom extends PluginForHost {
      * This matches dllink against an array of used 'host' servers. Use this when site have multiple download servers and they allow x
      * connections to ip/host server. Currently JD allows a global connection controller and doesn't allow for handling of different
      * hosts/IP setup. This will help with those situations by allowing more connection when possible.
-     *
+     * 
      * @param Account
      *            Account that's been used, can be null
      * @param DownloadLink
@@ -1752,7 +1755,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Sets Key and Values to respective Account stored within hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * @param x
@@ -1797,7 +1800,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Returns String key from Account@usedHost from hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1819,7 +1822,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Returns integer value from Account@usedHost from hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1841,7 +1844,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Returns true if hostMap contains 'key'
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * @param key
@@ -1866,7 +1869,7 @@ public class ExLoadCom extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -1883,7 +1886,7 @@ public class ExLoadCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      *            name
      * @param value
@@ -1913,9 +1916,9 @@ public class ExLoadCom extends PluginForHost {
     /**
      * If form contain both " and ' quotation marks within input fields it can return null values, thus you submit wrong/incorrect data re:
      * InputField parse(final String data). Affects revision 19688 and earlier!
-     *
+     * 
      * TODO: remove after JD2 goes stable!
-     *
+     * 
      * @author raztoki
      * */
     private Form cleanForm(Form form) {
@@ -1945,7 +1948,7 @@ public class ExLoadCom extends PluginForHost {
     /**
      * This allows backward compatibility for design flaw in setHtmlCode(), It injects updated html into all browsers that share the same
      * request id. This is needed as request.cloneRequest() was never fully implemented like browser.cloneBrowser().
-     *
+     * 
      * @param ibr
      *            Import Browser
      * @param t
