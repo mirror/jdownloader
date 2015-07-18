6c18606a89300fd13c38ee09db39c09f42d52062
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-18 16:36:34 +0000

    Added: cloudflare, 525 response code handling - re: psp (better late than never)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31000 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: be5a44340c3df0ae0a6c1f443151fa4e374aa529

--- src/jd/plugins/hoster/antiDDoSForHost.java
+++ src/jd/plugins/hoster/antiDDoSForHost.java
@@ -71,7 +71,7 @@ public abstract class antiDDoSForHost extends PluginForHost {
         // define custom browser headers and language settings.
         // required for native cloudflare support, without the need to repeat requests.
         try {
-            prepBr.addAllowedResponseCodes(new int[] { 429, 503, 520, 521, 522 });
+            prepBr.addAllowedResponseCodes(new int[] { 429, 503, 520, 521, 522, 525 });
         } catch (final Throwable t) {
         }
         synchronized (antiDDoSCookies) {
@@ -469,16 +469,22 @@ public abstract class antiDDoSForHost extends PluginForHost {
                         ibr.getPage(ibr.getRedirectLocation());
                     }
                 } else if (responseCode == 521) {
-                    // HTTP/1.1 521 Origin Down
-                    // <title>api.share-online.biz | 521: Web server is down</title>
-                    throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "Webserver is Down: " + ibr.getHost(), 5 * 60 * 1000l);
-                } else if (responseCode == 520 || responseCode == 522) {
+                    // this basically indicates that the site is down, no need to retry.
+                    // HTTP/1.1 521 Origin Down || <title>api.share-online.biz | 521: Web server is down</title>
+                    responseCode52x++;
+                    throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "CloudFlare says \"Origin Sever\" is down!", 5 * 60 * 1000l);
+                } else if (responseCode == 520 || responseCode == 522 || responseCode == 525) {
+                    // these warrant retry instantly, as it could be just slave issue? most hosts have 2 DNS response to load balance.
+                    // additional request could work via additional IP
                     // HTTP/1.1 520 Origin Error
                     // HTTP/1.1 522 Origin Connection Time-out
+                    // HTTP/1.1 525 Origin SSL Handshake Error || >CloudFlare is unable to establish an SSL connection to the origin
+                    // server.<
                     // cache system with possible origin dependency... we will wait and retry
                     if (responseCode52x == 4) {
-                        throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE);
+                        throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "CloudFlare can not contact \"Origin Server\"", 5 * 60 * 1000l);
                     }
+                    responseCode52x++;
                     // this html based cookie, set by <meta (for responseCode 522)
                     // <meta http-equiv="set-cookie" content="cf_use_ob=0; expires=Sat, 14-Jun-14 14:35:38 GMT; path=/">
                     String[] metaCookies = ibr.getRegex("<meta http-equiv=\"set-cookie\" content=\"(.*?; expries=.*?; path=.*?\";?(?: domain=.*?;?)?)\"").getColumn(0);
@@ -502,7 +508,7 @@ public abstract class antiDDoSForHost extends PluginForHost {
                     try {
                         sendRequest(ibr, ibr.getRequest().cloneRequest());
                     } catch (final Throwable t) {
-                        throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE);
+                        throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "CloudFlare related issue", 5 * 60 * 1000l);
                     }
                     // new sendRequest saves.
                     return;
