a0a9cb8d52aad9992029b48f5eb97b8a485309a4
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-24 12:07:16 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31050 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 00c535c7bc1818f649e5975745dbb5fdcdaef119

--- src/jd/nutils/Colors.java
+++ src/jd/nutils/Colors.java
@@ -21,7 +21,7 @@ import java.awt.Color;
 public class Colors {
     /**
      * Returns the same color, but with the given alpha channel
-     * 
+     *
      * @param color
      * @param alpha
      *            (0-100)
@@ -36,7 +36,7 @@ public class Colors {
 
     /**
      * converts a rgb integer to cmyk Cyan, Magenta, Yellow and Key(black)
-     * 
+     *
      * @param rgb
      * @return
      */
@@ -49,9 +49,15 @@ public class Colors {
         float Y = 1.0f - (b / 255);
         float var_K = 1;
 
-        if (C < var_K) var_K = C;
-        if (M < var_K) var_K = M;
-        if (Y < var_K) var_K = Y;
+        if (C < var_K) {
+            var_K = C;
+        }
+        if (M < var_K) {
+            var_K = M;
+        }
+        if (Y < var_K) {
+            var_K = Y;
+        }
 
         C = (C - var_K) / (1 - var_K);
         M = (M - var_K) / (1 - var_K);
@@ -59,14 +65,21 @@ public class Colors {
         return new float[] { C * 100, M * 100, Y * 100, var_K * 100 };
     }
 
+    public static double getColorDifference(int[] rgbA, int[] rgbB) {
+        int[] labA = rgb2lab(rgbA[0], rgbA[1], rgbA[2]);
+        int[] labB = rgb2lab(rgbB[0], rgbB[1], rgbB[2]);
+        int dif0 = labA[0] - labB[0];
+        int dif1 = labA[1] - labB[1];
+        int dif2 = labA[2] - labB[2];
+        return Math.sqrt(dif0 * dif0 + dif1 * dif1 + dif2 * dif2);
+    }
+
     /**
-     * Rechnet RGB werte in den LAB Farbraum um. Der LAB Farbraum wird vor allem
-     * zu Farbabstandsberechnungen verwendet Wert für L* enthält die
-     * Helligkeitsinformation, und hat einen Wertebereich von 0 bis 100. Die
-     * Werte a* und b* enthalten die Farbinformation. a* steht für die
-     * Farbbalance zwischen Grün und Rot, b* steht für die Farbbalance zwischen
-     * Blau und Gelb. a* und b* haben einen Wertebereich von -120 bis + 120.
-     * 
+     * Rechnet RGB werte in den LAB Farbraum um. Der LAB Farbraum wird vor allem zu Farbabstandsberechnungen verwendet Wert für L* enthält
+     * die Helligkeitsinformation, und hat einen Wertebereich von 0 bis 100. Die Werte a* und b* enthalten die Farbinformation. a* steht für
+     * die Farbbalance zwischen Grün und Rot, b* steht für die Farbbalance zwischen Blau und Gelb. a* und b* haben einen Wertebereich von
+     * -120 bis + 120.
+     *
      * @param R
      * @param G
      * @param B
@@ -146,9 +159,8 @@ public class Colors {
     }
 
     /**
-     * Wandelt einen farbwert vom RGB Farbraum in HSB um (Hue, Saturation,
-     * Brightness)
-     * 
+     * Wandelt einen farbwert vom RGB Farbraum in HSB um (Hue, Saturation, Brightness)
+     *
      * @param r
      * @param g
      * @param b
@@ -166,9 +178,8 @@ public class Colors {
     }
 
     /**
-     * this is hsv we know from gimp or photoshop (hsb[0]*360 hsb[1]*100
-     * hsb[2]*100)
-     * 
+     * this is hsv we know from gimp or photoshop (hsb[0]*360 hsb[1]*100 hsb[2]*100)
+     *
      * @param pixelValue
      * @return
      */
@@ -180,7 +191,7 @@ public class Colors {
 
     /**
      * Wandelt ein RGB Array in die zugehöroge decimale hexzahl um.
-     * 
+     *
      * @param value
      * @return 32 BIt Farbwert
      */
@@ -189,10 +200,9 @@ public class Colors {
     }
 
     /**
-     * Mischt zwei decimal zahlen. dabei werden ga und gb als
-     * gewichtungsfaktoren verwendet mixColors(0xff0000,0x00ff00,3,1) mischt 3
-     * Teile Rot und einen teil grün
-     * 
+     * Mischt zwei decimal zahlen. dabei werden ga und gb als gewichtungsfaktoren verwendet mixColors(0xff0000,0x00ff00,3,1) mischt 3 Teile
+     * Rot und einen teil grün
+     *
      * @param a
      * @param b
      * @param ga
@@ -215,7 +225,7 @@ public class Colors {
 
     /**
      * Mischt zwei Fraben 1:1
-     * 
+     *
      * @param a
      * @param b
      * @return 32 Bit Farbwert
@@ -229,7 +239,7 @@ public class Colors {
 
     /**
      * Wandelt eine decimale hexzahl(0-256^3) in die 3 RGB Farbkomponenten um.
-     * 
+     *
      * @param value
      * @return RGB Werte
      */
@@ -248,20 +258,22 @@ public class Colors {
 
     /**
      * unterschied des Farbtons
-     * 
+     *
      * @param a
      * @param b
      * @return
      */
     public static double getHueColorDifference(int a, int b) {
-        if (a == b) return 0;
+        if (a == b) {
+            return 0;
+        }
         return getHueColorDifference(getRGB(a), getRGB(b));
 
     }
 
     /**
      * unterschied des Farbtons
-     * 
+     *
      * @param a
      * @param b
      * @return
@@ -275,20 +287,22 @@ public class Colors {
 
     /**
      * unterschied des Farbtons | hue*360° entspricht wirklichen hue werten
-     * 
+     *
      * @param a
      * @param b
      * @return
      */
     public static double getHueColorDifference360(int a, int b) {
-        if (a == b) return 0;
+        if (a == b) {
+            return 0;
+        }
         return getHueColorDifference360(getRGB(a), getRGB(b));
 
     }
 
     /**
      * unterschied des Farbtons | hue*360° entspricht wirklichen hue werten
-     * 
+     *
      * @param a
      * @param b
      * @return
@@ -302,20 +316,22 @@ public class Colors {
 
     /**
      * unterschied des Farbsättigung
-     * 
+     *
      * @param a
      * @param b
      * @return
      */
     public static double getSaturationColorDifference(int a, int b) {
-        if (a == b) return 0;
+        if (a == b) {
+            return 0;
+        }
         return getSaturationColorDifference(getRGB(a), getRGB(b));
 
     }
 
     /**
      * unterschied des Farbsättigung
-     * 
+     *
      * @param a
      * @param b
      * @return
@@ -329,20 +345,22 @@ public class Colors {
 
     /**
      * unterschied der absoluten Helligkeit
-     * 
+     *
      * @param a
      * @param b
      * @return
      */
     public static double getBrightnessColorDifference(int a, int b) {
-        if (a == b) return 0;
+        if (a == b) {
+            return 0;
+        }
         return getBrightnessColorDifference(getRGB(a), getRGB(b));
 
     }
 
     /**
      * unterschied der absoluten Helligkeit
-     * 
+     *
      * @param a
      * @param b
      * @return
@@ -356,22 +374,16 @@ public class Colors {
 
     /**
      * Farbunterschied durch LAB raum berechnet
-     * 
+     *
      * @param a
      * @param b
      * @return
      */
-    public static double getColorDifference(int[] rgbA, int[] rgbB) {
-        int[] labA = rgb2lab(rgbA[0], rgbA[1], rgbA[2]);
-        int[] labB = rgb2lab(rgbB[0], rgbB[1], rgbB[2]);
-        int dif0 = labA[0] - labB[0];
-        int dif1 = labA[1] - labB[1];
-        int dif2 = labA[2] - labB[2];
-        return Math.sqrt(dif0 * dif0 + dif1 * dif1 + dif2 * dif2);
-    }
 
     public static double getColorDifference(int a, int b) {
-        if (a == b) return 0;
+        if (a == b) {
+            return 0;
+        }
         return getColorDifference(getRGB(a), getRGB(b));
     }
 
@@ -386,15 +398,17 @@ public class Colors {
     }
 
     /**
-     * Schnell berrechneter Farbunterschied im RGB Raum nicht wirklich gut aber
-     * da der Farbunterschied unter umständen so berechnet wurde nützlich
-     * 
+     * Schnell berrechneter Farbunterschied im RGB Raum nicht wirklich gut aber da der Farbunterschied unter umständen so berechnet wurde
+     * nützlich
+     *
      * @param color
      * @param color2
      * @return
      */
     public static double getRGBColorDifference2(int color, int color2) {
-        if (color == color2) return 0;
+        if (color == color2) {
+            return 0;
+        }
         Color c = new Color(color);
         Color c2 = new Color(color2);
         int dif0 = c.getRed() - c2.getRed();
@@ -404,15 +418,17 @@ public class Colors {
     }
 
     /**
-     * Schnell berrechneter Farbunterschied im RGB Raum nicht wirklich gut aber
-     * da der Farbunterschied unter umständen so berechnet wurde nützlich
-     * 
+     * Schnell berrechneter Farbunterschied im RGB Raum nicht wirklich gut aber da der Farbunterschied unter umständen so berechnet wurde
+     * nützlich
+     *
      * @param color
      * @param color2
      * @return
      */
     public static double getRGBColorDifference3(int color, int color2) {
-        if (color == color2) return 0;
+        if (color == color2) {
+            return 0;
+        }
         Color c = new Color(color);
         Color c2 = new Color(color2);
         int dif0 = c.getRed() - c2.getRed();
@@ -423,13 +439,15 @@ public class Colors {
 
     /**
      * Farbunterschied im RGB Raum
-     * 
+     *
      * @param color
      * @param color2
      * @return
      */
     public static double getRGBColorDifference1(int color, int color2) {
-        if (color == color2) return 0;
+        if (color == color2) {
+            return 0;
+        }
         Color c = new Color(color);
         Color c2 = new Color(color2);
         int dif0 = c.getRed() - c2.getRed();
@@ -440,13 +458,15 @@ public class Colors {
 
     /**
      * CMYK Colordifference
-     * 
+     *
      * @param rgbcolor
      * @param rgbcolor2
      * @return
      */
     public static double getCMYKColorDifference1(int rgbcolor, int rgbcolor2) {
-        if (rgbcolor == rgbcolor2) return 0;
+        if (rgbcolor == rgbcolor2) {
+            return 0;
+        }
         float[] cmyk1 = rgb2cmyk(rgbcolor);
         float[] cmyk2 = rgb2cmyk(rgbcolor2);
         double dif0 = cmyk1[0] - cmyk2[0];
