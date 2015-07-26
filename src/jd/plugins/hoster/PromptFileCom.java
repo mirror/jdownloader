d7d8638463e9dc863a23d4840038048266689c0b
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-07-26 15:25:56 +0000

    Fixed: promptfile.com, free download - re: IRC
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31068 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PromptFileCom.java
+++ src/jd/plugins/hoster/PromptFileCom.java
@@ -106,7 +106,7 @@ public class PromptFileCom extends PluginForHost {
             }
             br.postPage(br.getURL(), "chash=" + cHash);
             // dllink = br.getRegex("clip: \\{\\s+url: \\'(http://(www\\.)?promptfile\\.com/file/[A-Za-z0-9=]+)(\\'|\")").getMatch(0);
-            dllink = br.getRegex("download below</p>\\s+<a href=\"(http://(www\\.)?promptfile\\.com/file/[A-Za-z0-9=]+)\"").getMatch(0);
+            dllink = br.getRegex("<a href=\"(http://(www\\.)?promptfile\\.com/file/[A-Za-z0-9=]{90,}+)\"").getMatch(0);
             if (dllink == null) {
                 dllink = br.getRegex(">Download<.+\\s+.+\\s+<a href=\"(http://www\\.promptfile\\.com[^<>\"]+)\"").getMatch(0);
                 logger.info("dllink = " + dllink);
