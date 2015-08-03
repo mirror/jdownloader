40f857e7cb51b6c6379de88ee355fcd1c78b2052
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-08-03 05:51:18 +0000

    Added: abc.net.au, /news/ support  - re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31142 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/AbcNtAu.java
@@ -0,0 +1,115 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2015  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.HTMLParser;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.FilePackage;
+
+/**
+ *
+ * @author raztoki
+ *
+ */
+@DecrypterPlugin(revision = "$Revision: 20458 $", interfaceVersion = 2, names = { "abc.net.au" }, urls = { "https?://(www\\.)?abc\\.net\\.au/news/[\\d\\-]+/[^/]+/\\d+" }, flags = { 0 })
+public class AbcNtAu extends antiDDoSForDecrypt {
+
+    public AbcNtAu(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.getPage(parameter);
+        final String[][] results = br.getRegex("inline(Video|Audio)Data\\.push\\((.*?)\\);").getMatches();
+        if (results != null) {
+            for (final String[] result : results) {
+                String urlPattern = null;
+                final ArrayList<Integer> qual = new ArrayList<Integer>();
+                String[] result_array = getJsonResultsFromArray(result[1]);
+                if (result_array != null) {
+                    for (final String results_a : result_array) {
+                        final String url = getJson(results_a, "url");
+                        // if video
+                        if ("Video".equalsIgnoreCase(result[0])) {
+                            // get qual
+                            final String q = new Regex(url, "(\\d+)k\\.mp4").getMatch(0);
+                            if (q != null) {
+                                qual.add(Integer.parseInt(q));
+                                if (urlPattern == null) {
+                                    urlPattern = url;
+                                }
+                            }
+                        }
+                        final String size = getJson(results_a, "fileSize");
+                        if (!inValidate(url)) {
+                            final DownloadLink dl = createDownloadlink(url);
+                            if (!inValidate(size)) {
+                                dl.setVerifiedFileSize(Long.parseLong(size));
+                            }
+                            dl.setAvailableStatus(AvailableStatus.TRUE);
+                            decryptedLinks.add(dl);
+                        }
+                    }
+                }
+                // analyse the results, because they don't include all qualities at times.. yet they're available.
+                if (urlPattern != null && !qual.isEmpty()) {
+                    // 1000k, 512k, 256k,
+                    final int[] k = new int[] { 1000, 512, 256 };
+                    for (final int kk : k) {
+                        if (!qual.contains(kk)) {
+                            decryptedLinks.add(createDownloadlink(urlPattern.replaceFirst("\\d+(k\\.mp4)", kk + "$1")));
+                        }
+                    }
+                }
+            }
+        }
+        // lets look for external links? youtube etc.
+        final String externlink = br.getRegex("<div class=\"inline-content full\">(.*?)</div>").getMatch(0);
+        if (externlink != null) {
+            final String[] links = HTMLParser.getHttpLinks(externlink, null);
+            if (links != null) {
+                for (final String link : links) {
+                    decryptedLinks.add(createDownloadlink(link));
+                }
+            }
+        }
+        final String fpName = br.getRegex("<h1>(.*?)</h1>").getMatch(0);
+        if (!inValidate(fpName)) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+}
