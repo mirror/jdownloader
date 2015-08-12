de8ab2a17427eb9297ee53f612454406671b48b1
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-08-12 05:36:33 +0000

    Added: itsjustlinks.com, decrypter support - re: IRC
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31239 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/ItJstLnkCm.java
@@ -0,0 +1,70 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2015  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+/**
+ * @author raztoki
+ */
+@DecrypterPlugin(revision = "$Revision: 30726 $", interfaceVersion = 3, names = { "itsjustlinks.com" }, urls = { "http://(www\\.)?itsjustlinks\\.com/link/get/(?i-)[a-f0-9]{13}" }, flags = { 0 })
+public class ItJstLnkCm extends antiDDoSForDecrypt {
+
+    public ItJstLnkCm(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.setFollowRedirects(true);
+        br.getPage(parameter);
+        if (br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404 || br.containsHTML("404 Not Found<|Sorry, the page could not be found")) {
+            decryptedLinks.add(createOfflinelink(parameter));
+            return decryptedLinks;
+        }
+        // ajax request to get links
+        br.getHeaders().put("Accept", "*/*");
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+        br.getHeaders().put("Content-Type", null);
+        br.postPage("/link/get", "");
+        // links are within json
+        final String[] links = getJsonResultsFromArray(getJsonArray("links"));
+        if (links != null) {
+            for (final String link : links) {
+                decryptedLinks.add(createDownloadlink(link));
+            }
+        }
+
+        return decryptedLinks;
+    }
+
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+    public boolean hasAutoCaptcha() {
+        return false;
+    }
+
+}
