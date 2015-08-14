1790d9ecae5d6237249af5d763a24e85af1b43c0
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-08-14 09:23:12 +0000

    Hugefiles.net - set ContentUrl ref.: #68631
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31282 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/HugeFilesNet.java
+++ src/jd/plugins/hoster/HugeFilesNet.java
@@ -64,7 +64,6 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
 import jd.plugins.PluginException;
-import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.plugins.SiteType.SiteTemplate;
 import jd.utils.JDUtilities;
@@ -163,7 +162,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * @author raztoki
-     *
+     * 
      * @category 'Experimental', Mods written July 2012 - 2013
      * */
     public HugeFilesNet(PluginWrapper wrapper) {
@@ -333,7 +332,7 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * Provides alternative linkchecking method for a single link at a time. Can be used as generic failover, though kinda pointless as this
      * method doesn't give filename...
-     *
+     * 
      * */
     private String[] altAvailStat(final DownloadLink downloadLink, final String[] fileInfo) throws Exception {
         Browser alt = new Browser();
@@ -527,7 +526,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Removes patterns which could break the plugin due to fake/hidden HTML, or false positives caused by HTML comments.
-     *
+     * 
      * @throws Exception
      * @author raztoki
      */
@@ -1074,7 +1073,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Rules to prevent new downloads from commencing
-     *
+     * 
      * */
     public boolean canHandle(DownloadLink downloadLink, Account account) {
         if (downloadLink.getBooleanProperty("requiresPremiumAccount", false) && (account == null || account.getBooleanProperty("free", false))) {
@@ -1107,7 +1106,7 @@ public class HugeFilesNet extends PluginForHost {
      * Corrects downloadLink.urlDownload().<br/>
      * <br/>
      * The following code respect the hoster supported protocols via plugin boolean settings and users config preference
-     *
+     * 
      * @author raztoki
      * */
     @SuppressWarnings("unused")
@@ -1122,6 +1121,7 @@ public class HugeFilesNet extends PluginForHost {
         }
         // strip video hosting url's to reduce possible duped links.
         downloadLink.setUrlDownload(downloadLink.getDownloadURL().replaceAll("/(vid)?embed-", "/"));
+        downloadLink.setContentUrl(downloadLink.getDownloadURL());
         // output the hostmask as we wish based on COOKIE_HOST url!
         String desiredHost = new Regex(COOKIE_HOST, "https?://([^/]+)").getMatch(0);
         String importedHost = new Regex(downloadLink.getDownloadURL(), "https?://([^/]+)").getMatch(0);
@@ -1183,7 +1183,7 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * Gets page <br />
      * - natively supports silly cloudflare anti DDoS crapola
-     *
+     * 
      * @author raztoki
      */
     private void getPage(final String page) throws Exception {
@@ -1342,7 +1342,7 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
@@ -1428,7 +1428,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * captcha processing can be used download/login/anywhere assuming the submit values are the same (they usually are)...
-     *
+     * 
      * @author raztoki
      * */
     private Form captchaForm(DownloadLink downloadLink, Form form) throws Exception {
@@ -1508,7 +1508,7 @@ public class HugeFilesNet extends PluginForHost {
             logger.info("Detected captcha method \"Solve Media\"");
             final Browser captcha = br.cloneBrowser();
             cleanupBrowser(captcha, form.getHtmlCode());
-           
+
             final org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia sm = new org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia(captcha);
             final File cf = sm.downloadCaptcha(getLocalCaptchaFile());
             String code = "";
@@ -1524,7 +1524,7 @@ public class HugeFilesNet extends PluginForHost {
             logger.info("Detected captcha method \"Key Captcha\"");
             final Browser captcha = br.cloneBrowser();
             cleanupBrowser(captcha, form.getHtmlCode());
-           
+
             String result = handleCaptchaChallenge(getDownloadLink(), new KeyCaptcha(this, captcha, getDownloadLink()).createChallenge(form.hasInputFieldByName("login") && form.hasInputFieldByName("password"), this));
 
             if (result == null || "CANCEL".equals(result)) {
@@ -1601,13 +1601,13 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlSlot
      *            (+1|-1)
      * */
@@ -1628,9 +1628,9 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * ControlSimHost, On error it will set the upper mark for 'max sim dl per host'. This will be the new 'static' setting used going
      * forward. Thus prevents new downloads starting when not possible and is self aware and requires no coder interaction.
-     *
+     * 
      * @param account
-     *
+     * 
      * @category 'Experimental', Mod written February 2013
      * */
     private void controlSimHost(final Account account) {
@@ -1667,7 +1667,7 @@ public class HugeFilesNet extends PluginForHost {
      * This matches dllink against an array of used 'host' servers. Use this when site have multiple download servers and they allow x
      * connections to ip/host server. Currently JD allows a global connection controller and doesn't allow for handling of different
      * hosts/IP setup. This will help with those situations by allowing more connection when possible.
-     *
+     * 
      * @param Account
      *            Account that's been used, can be null
      * @param DownloadLink
@@ -1773,7 +1773,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Sets Key and Values to respective Account stored within hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * @param x
@@ -1818,7 +1818,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Returns String key from Account@usedHost from hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1840,7 +1840,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Returns integer value from Account@usedHost from hostMap
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * */
@@ -1862,7 +1862,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Returns true if hostMap contains 'key'
-     *
+     * 
      * @param account
      *            Account that's been used, can be null
      * @param key
@@ -1887,7 +1887,7 @@ public class HugeFilesNet extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -1904,7 +1904,7 @@ public class HugeFilesNet extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      *            name
      * @param value
@@ -1934,9 +1934,9 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * If form contain both " and ' quotation marks within input fields it can return null values, thus you submit wrong/incorrect data re:
      * InputField parse(final String data). Affects revision 19688 and earlier!
-     *
+     * 
      * TODO: remove after JD2 goes stable!
-     *
+     * 
      * @author raztoki
      * */
     private Form cleanForm(Form form) {
@@ -1966,7 +1966,7 @@ public class HugeFilesNet extends PluginForHost {
     /**
      * This allows backward compatibility for design flaw in setHtmlCode(), It injects updated html into all browsers that share the same
      * request id. This is needed as request.cloneRequest() was never fully implemented like browser.cloneBrowser().
-     *
+     * 
      * @param ibr
      *            Import Browser
      * @param t
@@ -2098,9 +2098,9 @@ public class HugeFilesNet extends PluginForHost {
         }
     }
 
-	@Override
-	public SiteTemplate siteTemplateType() {
-		return SiteTemplate.SibSoft_XFileShare;
-	}
+    @Override
+    public SiteTemplate siteTemplateType() {
+        return SiteTemplate.SibSoft_XFileShare;
+    }
 
 }
