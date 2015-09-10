2b052ee6db5be91b759ef9b25bccc37a84d35df7
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-09-10 11:27:52 +0000

    DirectHTTP: 504 (gateway timeout) -> UNCHECKABLE NewsTweaknewsEu: -updated fetchAccountInfo to use Account.loadCookie and Account.saveCookie to avoid many logins -fixed expire date parsing in fetchAccountInfo -updated available ports CookieStorable: storable class for Cookie Account: added saveCookies,loadCookies,clearCookies build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31494 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4315fede6a5961b9f295b1d3f56908c124f60fd6

--- src/jd/plugins/hoster/NewsTweaknewsEu.java
+++ src/jd/plugins/hoster/NewsTweaknewsEu.java
@@ -3,6 +3,7 @@ package jd.plugins.hoster;
 import java.util.Arrays;
 
 import jd.PluginWrapper;
+import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.parser.html.Form;
@@ -31,67 +32,90 @@ public class NewsTweaknewsEu extends UseNet {
     @Override
     public AccountInfo fetchAccountInfo(Account account) throws Exception {
         setBrowserExclusive();
-        br.setFollowRedirects(true);
-        br.setCookie(getHost(), "language", "en");
-        br.getPage("https://members.tweaknews.eu/en");
-        Form login = br.getForm(0);
-        login.put("username", Encoding.urlEncode(account.getUser()));
-        login.put("password", Encoding.urlEncode(account.getPass()));
-        br.submitForm(login);
-        login = br.getForm(0);
-        if (login != null && login.containsHTML("username") && login.containsHTML("password")) {
-            final String alertDanger = br.getRegex("class=\"alert alert-danger\">(.*?)</").getMatch(0);
-            if (alertDanger != null) {
-                if (StringUtils.contains(alertDanger, "IP is blocked")) {
-                    throw new PluginException(LinkStatus.ERROR_PREMIUM, alertDanger, PluginException.VALUE_ID_PREMIUM_TEMP_DISABLE);
-                }
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, alertDanger, PluginException.VALUE_ID_PREMIUM_DISABLE);
-            }
-            final String inputInfo = br.getRegex("class=\"input-info\">(.*?)</").getMatch(0);
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, inputInfo, PluginException.VALUE_ID_PREMIUM_DISABLE);
-        }
-        if (br.getCookie(getHost(), "twn-SESS") == null) {
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-        }
         final AccountInfo ai = new AccountInfo();
-        final String packageType = br.getRegex("name\">Package:(.*?)</div>").getMatch(0);
-        if (!StringUtils.contains(packageType, "Block Package")) {
-            // time limit
-            ai.setStatus(packageType.trim());
-            ai.setUnlimitedTraffic();
-            final String expires = br.getRegex("Valid until: (\\d+-\\d+-\\d+)").getMatch(0);
-            if (expires != null) {
-                final long date = TimeFormatter.getMilliSeconds(expires, "YYYY'-MMM'-dd", null);
-                if (date > 0) {
-                    ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
+        br.setFollowRedirects(true);
+        final Cookies cookies = account.loadCookies("");
+        try {
+            Form login = null;
+            if (cookies != null) {
+                br.setCookies(getHost(), cookies);
+                br.getPage("https://members.tweaknews.eu/en");
+                login = br.getForm(0);
+                if (login != null && login.containsHTML("username") && login.containsHTML("password")) {
+                    br.getCookies(getHost()).clear();
+                } else if (br.getCookie(getHost(), "twn-SESS") == null) {
+                    br.getCookies(getHost()).clear();
                 }
             }
-        } else if (StringUtils.contains(packageType, "Free Trial")) {
-            // free trial, no traffic left?
-            ai.setStatus(packageType.trim());
-            final String expires = br.getRegex("Valid until: (\\d+-\\d+-\\d+)").getMatch(0);
-            if (expires != null) {
-                final long date = TimeFormatter.getMilliSeconds(expires, "YYYY'-MMM'-dd", null);
-                if (date > 0) {
-                    ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
+            if (br.getCookie(getHost(), "twn-SESS") == null) {
+                account.clearCookies("");
+                br.setCookie(getHost(), "language", "en");
+                br.getPage("https://members.tweaknews.eu/en");
+                login = br.getForm(0);
+                login.put("username", Encoding.urlEncode(account.getUser()));
+                login.put("password", Encoding.urlEncode(account.getPass()));
+                br.submitForm(login);
+                login = br.getForm(0);
+                if (login != null && login.containsHTML("username") && login.containsHTML("password")) {
+                    final String alertDanger = br.getRegex("class=\"alert alert-danger\">(.*?)</").getMatch(0);
+                    if (alertDanger != null) {
+                        if (StringUtils.contains(alertDanger, "IP is blocked")) {
+                            throw new PluginException(LinkStatus.ERROR_PREMIUM, alertDanger, PluginException.VALUE_ID_PREMIUM_TEMP_DISABLE);
+                        }
+                        throw new PluginException(LinkStatus.ERROR_PREMIUM, alertDanger, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                    }
+                    final String inputInfo = br.getRegex("class=\"input-info\">(.*?)</").getMatch(0);
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, inputInfo, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                if (br.getCookie(getHost(), "twn-SESS") == null) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
                 }
             }
-            final String blockPackageSize = new Regex(packageType, "(\\d+\\s*?GB)").getMatch(0);
-            final long max = SizeFormatter.getSize(blockPackageSize);
-            ai.setTrafficMax(max);
-        } else {
-            // traffic limit
-            ai.setStatus(packageType.trim());
-            final String blockPackageSize = new Regex(packageType, "(\\d+\\s*?GB)").getMatch(0);
-            final long max = SizeFormatter.getSize(blockPackageSize);
-            final String dataRemaining = br.getRegex("Data Remaining:</td>.*?<td>(.*?\\s*?GB)").getMatch(0);
-            final long left = SizeFormatter.getSize(dataRemaining);
-            ai.setValidUntil(-1);
-            ai.setTrafficMax(max);
-            ai.setTrafficLeft(left);
-            if (left <= 0) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "No more traffic left", PluginException.VALUE_ID_PREMIUM_DISABLE);
+            account.saveCookies(br.getCookies(getHost()), "");
+            final String packageType = br.getRegex("name\">Package:(.*?)</div>").getMatch(0);
+            if (!StringUtils.contains(packageType, "Block Package")) {
+                // time limit
+                ai.setStatus(packageType.trim());
+                ai.setUnlimitedTraffic();
+                final String expires = br.getRegex("Valid until: (\\d+-\\d+-\\d+)").getMatch(0);
+                if (expires != null) {
+                    final long date = TimeFormatter.getMilliSeconds(expires, "yyyy'-'MM'-'dd", null);
+                    if (date > 0) {
+                        ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
+                    }
+                }
+            } else if (StringUtils.contains(packageType, "Free Trial")) {
+                // free trial, no traffic left?
+                ai.setStatus(packageType.trim());
+                final String expires = br.getRegex("Valid until: (\\d+-\\d+-\\d+)").getMatch(0);
+                if (expires != null) {
+                    final long date = TimeFormatter.getMilliSeconds(expires, "yyyy'-'MM'-'dd", null);
+                    if (date > 0) {
+                        ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
+                    }
+                }
+                final String blockPackageSize = new Regex(packageType, "(\\d+\\s*?GB)").getMatch(0);
+                final long max = SizeFormatter.getSize(blockPackageSize);
+                ai.setTrafficMax(max);
+            } else {
+                // traffic limit
+                ai.setStatus(packageType.trim());
+                final String blockPackageSize = new Regex(packageType, "(\\d+\\s*?GB)").getMatch(0);
+                final long max = SizeFormatter.getSize(blockPackageSize);
+                final String dataRemaining = br.getRegex("Data Remaining:</td>.*?<td>(.*?\\s*?GB)").getMatch(0);
+                final long left = SizeFormatter.getSize(dataRemaining);
+                ai.setValidUntil(-1);
+                ai.setTrafficMax(max);
+                ai.setTrafficLeft(left);
+                if (left <= 0) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "No more traffic left", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
             }
+        } catch (final PluginException e) {
+            if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                account.clearCookies("");
+            }
+            throw e;
         }
         final String connections = br.getRegex("Threads:</td>.*?<td>(\\d+)").getMatch(0);
         if (connections != null) {
@@ -110,11 +134,11 @@ public class NewsTweaknewsEu extends UseNet {
 
     @Override
     protected int[] getAvailablePorts() {
-        return new int[] { 119 };
+        return new int[] { 119, 80 };
     }
 
     @Override
     protected int[] getAvailableSSLPorts() {
-        return new int[] { 563 };
+        return new int[] { 563, 443 };
     }
 }
