d814b9e137cb24c4173ebc2e8440e0ca6b0b2cda
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-09-15 12:32:52 +0000

    Reviewed: AdsCaptcha - removed isnotjd1 check as this class is only available in jd2 now. - added dev notes and annotations - reviewed regex - eclipse removed superfluous imports.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31537 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/challenge/adscaptcha/AdsCaptcha.java
+++ src/org/jdownloader/captcha/v2/challenge/adscaptcha/AdsCaptcha.java
@@ -9,9 +9,17 @@ import jd.parser.Regex;
 import jd.parser.html.Form;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
-import jd.plugins.decrypter.LnkCrptWs;
-import jd.utils.JDUtilities;
 
+/**
+ * primary domain 'adscaptcha.com' that run this captcha service is now dead, but still can be located at minteye.com (but captcha seems to
+ * fail also as subdomain 'api.minteye.com' has no dns record.).
+ *
+ * @author .bismarck
+ * @author coalado
+ * @author raztoki
+ *
+ *
+ */
 public class AdsCaptcha {
     private final Browser br;
     public Browser        acBr;
@@ -38,7 +46,7 @@ public class AdsCaptcha {
             } catch (final Throwable e) {
             }
         }
-        Form ret = new Form();
+        final Form ret = new Form();
         if (result == null) {
             return null;
         }
@@ -46,20 +54,6 @@ public class AdsCaptcha {
         ret.put("adscaptcha_challenge_field", challenge);
         ret.put("adscaptcha_response_field", result);
         return ret;
-    }
-
-    private boolean isStableEnviroment() {
-        String prev = JDUtilities.getRevision();
-        if (prev == null || prev.length() < 3) {
-            prev = "0";
-        } else {
-            prev = prev.replaceAll(",|\\.", "");
-        }
-        final int rev = Integer.parseInt(prev);
-        if (rev < 10000) {
-            return true;
-        }
-        return false;
     }
 
     private void load() throws Exception {
@@ -75,24 +69,20 @@ public class AdsCaptcha {
             throw new Exception("AdsCaptcha: challenge and/or publickey equal null!");
         }
 
-        if (!isStableEnviroment()) {
-            final URL[] images = imageUrls();
-            if (count <= 0 && images.length == 1) {
-                throw new Exception("AdsCaptcha modul broken!");
-            }
-            SwingUtilities.invokeAndWait(new Runnable() {
-
-                @Override
-                public void run() {
-                    SliderCaptchaDialog sc = new SliderCaptchaDialog(0, "AdsCaptcha - " + br.getHost(), images, count);
-                    sc.displayDialog();
-                    result = sc.getReturnValue();
-                }
-            });
-
-        } else {
-            throw new Exception("AdsCaptcha: currently not available in JD1!");
+        final URL[] images = imageUrls();
+        if (count <= 0 && images.length == 1) {
+            throw new Exception("AdsCaptcha modul broken!");
         }
+        SwingUtilities.invokeAndWait(new Runnable() {
+
+            @Override
+            public void run() {
+                SliderCaptchaDialog sc = new SliderCaptchaDialog(0, "AdsCaptcha - " + br.getHost(), images, count);
+                sc.displayDialog();
+                result = sc.getReturnValue();
+            }
+        });
+
     }
 
     private void getChallenge() {
@@ -102,7 +92,7 @@ public class AdsCaptcha {
     private void getPublicKey() {
         publicKey = acBr.getRegex("\"publicKey\":\"([0-9a-f\\-]+)\"").getMatch(0);
         if (publicKey == null) {
-            publicKey = new Regex(captchaAddress, "PublicKey=([0-9a-f\\-]+)\\&").getMatch(0);
+            publicKey = new Regex(captchaAddress, "PublicKey=([0-9a-f\\-]+)&").getMatch(0);
         }
     }
 
@@ -114,7 +104,7 @@ public class AdsCaptcha {
     }
 
     private boolean checkIfSupported() throws Exception {
-        captchaAddress = acBr.getRegex("src=\'(http://api\\.adscaptcha\\.com/Get\\.aspx\\?CaptchaId=\\d+\\&PublicKey=[^\'<>]+)").getMatch(0);
+        captchaAddress = acBr.getRegex("src=\'(https?://api\\.adscaptcha\\.com/Get\\.aspx\\?CaptchaId=\\d+&PublicKey=[^\'<>]+)").getMatch(0);
         captchaId = new Regex(captchaAddress, "CaptchaId=(\\d+)\\&").getMatch(0);
         if (captchaAddress == null || captchaId == null) {
             throw new Exception("AdsCaptcha: Captcha address not found!");
@@ -132,7 +122,7 @@ public class AdsCaptcha {
             urls = acBr.getRegex("\\{\'src\':\\s\'(//[^\']+)\'\\}").getColumn(0);
         }
         if (urls == null || urls.length == 0) {
-            urls = acBr.getRegex("(\'|\")spriteUrl(\'|\"):\\s*(\'|\")(.*?)(\'|\")").getColumn(3);
+            urls = acBr.getRegex("(\'|\")spriteUrl\\1:\\s*(\'|\")(.*?)\\2").getColumn(2);
         }
         if (urls == null || urls.length == 0) {
             throw new Exception("AdsCaptcha: Image urls not found!");
