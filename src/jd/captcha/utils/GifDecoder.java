eef0dc62791559f76e40b02833c329ec3d4a6e10
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-02 07:53:30 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31698 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/captcha/utils/GifDecoder.java
+++ src/jd/captcha/utils/GifDecoder.java
@@ -31,6 +31,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 import org.appwork.utils.URLStream;
+import org.jdownloader.logging.LogController;
 
 /**
  * Form:http://www.java2s.com/Code/Java/2D-Graphics-GUI/GiffileEncoder.htm Class GifDecoder - Decodes a GIF file into one or more frames. <br>
@@ -230,52 +231,52 @@ public class GifDecoder {
 
                 code = datum & code_mask;
                 datum >>= code_size;
-                    bits -= code_size;
+                bits -= code_size;
 
-                    // Interpret the code
+                // Interpret the code
 
-                    if (code > available || code == end_of_information) {
-                        break;
-                    }
-                    if (code == clear) {
-                        // Reset decoder.
-                        code_size = data_size + 1;
-                        code_mask = (1 << code_size) - 1;
-                        available = clear + 2;
-                        old_code = NullCode;
-                        continue;
-                    }
-                    if (old_code == NullCode) {
-                        pixelStack[top++] = suffix[code];
-                        old_code = code;
-                        first = code;
-                        continue;
-                    }
-                    in_code = code;
-                    if (code == available) {
-                        pixelStack[top++] = (byte) first;
-                        code = old_code;
-                    }
-                    while (code > clear) {
-                        pixelStack[top++] = suffix[code];
-                        code = prefix[code];
-                    }
-                    first = suffix[code] & 0xff;
-
-                    // Add a new string to the string table,
-
-                    if (available >= MaxStackSize) {
-                        break;
-                    }
+                if (code > available || code == end_of_information) {
+                    break;
+                }
+                if (code == clear) {
+                    // Reset decoder.
+                    code_size = data_size + 1;
+                    code_mask = (1 << code_size) - 1;
+                    available = clear + 2;
+                    old_code = NullCode;
+                    continue;
+                }
+                if (old_code == NullCode) {
+                    pixelStack[top++] = suffix[code];
+                    old_code = code;
+                    first = code;
+                    continue;
+                }
+                in_code = code;
+                if (code == available) {
                     pixelStack[top++] = (byte) first;
-                    prefix[available] = (short) old_code;
-                    suffix[available] = (byte) first;
-                    available++;
-                    if ((available & code_mask) == 0 && available < MaxStackSize) {
-                        code_size++;
-                        code_mask += available;
-                    }
-                    old_code = in_code;
+                    code = old_code;
+                }
+                while (code > clear) {
+                    pixelStack[top++] = suffix[code];
+                    code = prefix[code];
+                }
+                first = suffix[code] & 0xff;
+
+                // Add a new string to the string table,
+
+                if (available >= MaxStackSize) {
+                    break;
+                }
+                pixelStack[top++] = (byte) first;
+                prefix[available] = (short) old_code;
+                suffix[available] = (byte) first;
+                available++;
+                if ((available & code_mask) == 0 && available < MaxStackSize) {
+                    code_size++;
+                    code_mask += available;
+                }
+                old_code = in_code;
             }
 
             // Pop a pixel off the pixel stack.
@@ -384,6 +385,7 @@ public class GifDecoder {
         try {
             curByte = in.read();
         } catch (IOException e) {
+            LogController.CL().log(e);
             status = STATUS_FORMAT_ERROR;
         }
         return curByte;
@@ -473,6 +475,7 @@ public class GifDecoder {
             }
             status = read(in);
         } catch (IOException e) {
+            LogController.CL().log(e);
             status = STATUS_OPEN_ERROR;
         }
 
@@ -498,6 +501,7 @@ public class GifDecoder {
                     n += count;
                 }
             } catch (IOException e) {
+                LogController.CL().log(e);
             }
 
             if (n < blockSize) {
@@ -522,6 +526,7 @@ public class GifDecoder {
         try {
             n = in.read(c);
         } catch (IOException e) {
+            LogController.CL().log(e);
         }
         if (n < nbytes) {
             status = STATUS_FORMAT_ERROR;
@@ -598,13 +603,13 @@ public class GifDecoder {
         read(); // block size
         int packed = read(); // packed fields
         dispose = (packed & 0x1c) >> 2; // disposal method
-                    if (dispose == 0) {
-                        dispose = 1; // elect to keep old image if discretionary
-                    }
-                    transparency = (packed & 1) != 0;
-                    delay = readShort() * 10; // delay in milliseconds
-                    transIndex = read(); // transparent color index
-                    read(); // block terminator
+        if (dispose == 0) {
+            dispose = 1; // elect to keep old image if discretionary
+        }
+        transparency = (packed & 1) != 0;
+        delay = readShort() * 10; // delay in milliseconds
+        transIndex = read(); // transparent color index
+        read(); // block terminator
     }
 
     /**
@@ -619,7 +624,6 @@ public class GifDecoder {
             status = STATUS_FORMAT_ERROR;
             return;
         }
-
         readLSD();
         if (gctFlag && !err()) {
             gct = readColorTable(gctSize);
