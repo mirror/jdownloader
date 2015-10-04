479918b3622788eeec9cf9b542e2b491f022e9e2
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-04 19:14:45 +0000

    -added support for beehdnet closes #72341 -fixed otrkeyfindercom -added support for torrentajeesh closes #72194 -added support for skaigr closes #72342 -arte: more improvements fixes #72210 fixes #72211 -added support for pervcitycom closes #72051 -added profile decrypter for coubcom refs #71889
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31720 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 0677088598a3201027703f169934bfd9314208c2

---
+++ src/jd/plugins/decrypter/CoubComDecrypter.java
@@ -0,0 +1,79 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "coub.com" }, urls = { "https?://(?:www\\.)?coub\\.com/[a-f0-9]{32}" }, flags = { 0 })
+public class CoubComDecrypter extends PluginForDecrypt {
+
+    public CoubComDecrypter(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /** Using API: http://coub.com/dev/docs */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        final String lid = new Regex(parameter, "([a-f0-9]{32})$").getMatch(0);
+        this.br.setLoadLimit(this.br.getLoadLimit() * 4);
+        this.br.getPage("http://coub.com/api/v2/timeline/channel/" + lid + "?per_page=1000&permalink=" + lid + "&order_by=newest&page=1");
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
+            return decryptedLinks;
+        }
+        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) jd.plugins.hoster.DummyScriptEnginePlugin.jsonToJavaObject(br.toString());
+        final ArrayList<Object> ressources = (ArrayList) entries.get("coubs");
+        for (final Object ressource : ressources) {
+            entries = (LinkedHashMap<String, Object>) ressource;
+            final String coub_type = (String) entries.get("type");
+            final String fid = (String) entries.get("permalink");
+            if (coub_type == null || fid == null) {
+                return null;
+            }
+            final String url_content = "https://coub.com/view/" + fid;
+            final String filename = jd.plugins.hoster.CoubCom.getFilename(entries, fid);
+            if (!coub_type.equals("Coub::Simple")) {
+                /* Do not decrypt re-coups (similar to re-tweets) - only decrypt content which the user itself posted! */
+                continue;
+            }
+            final DownloadLink dl = this.createDownloadlink(url_content);
+            dl.setContentUrl(url_content);
+            dl.setName(filename);
+            dl.setAvailable(true);
+            decryptedLinks.add(dl);
+        }
+
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(lid);
+        fp.addLinks(decryptedLinks);
+
+        return decryptedLinks;
+    }
+
+}
---
+++ src/jd/plugins/hoster/SkaiGr.java
@@ -0,0 +1,210 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.Browser.BrowserException;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+import org.appwork.utils.formatter.TimeFormatter;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "skai.gr" }, urls = { "http://skaidecrypted\\d+" }, flags = { 0 })
+public class SkaiGr extends PluginForHost {
+
+    public SkaiGr(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /* DEV NOTES */
+    // Tags:
+    // protocol: no https
+    // other:
+
+    /* Extension which will be used if no correct extension is found */
+    private static final String  default_Extension = ".mp3";
+    /* Connection stuff */
+    private static final boolean free_resume       = true;
+    private static final int     free_maxchunks    = 0;
+    private static final int     free_maxdownloads = -1;
+
+    private String               DLLINK            = null;
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.skai.gr/";
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws IOException, PluginException {
+        DLLINK = null;
+        final String fid = new Regex(downloadLink.getDownloadURL(), "(\\d+)$").getMatch(0);
+        downloadLink.setLinkID(fid);
+        downloadLink.setName(fid);
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        this.br.getPage("http://www.skai.gr/Ajax.aspx?m=NewModules.LookupMultimedia&mmid=" + fid);
+        if (br.getHttpConnection().getResponseCode() == 404 || this.br.containsHTML("tv_trailers/preroll\\-")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String show = getXML("ShowName");
+        final String title = getXML("Title");
+        final String date = getXML("Date");
+        final String description = getXML("Description");
+        DLLINK = getXML("File");
+        if (show == null || title == null || date == null || DLLINK == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String date_formatted = formatDate(date);
+
+        if (description != null && downloadLink.getComment() == null) {
+            downloadLink.setComment(description);
+        }
+
+        String filename = date_formatted + "_skaigr_" + show + " - " + title;
+        DLLINK = Encoding.htmlDecode(DLLINK);
+        filename = Encoding.htmlDecode(filename);
+        filename = filename.trim();
+        filename = encodeUnicode(filename);
+        String ext = DLLINK.substring(DLLINK.lastIndexOf("."));
+        /* Make sure that we get a correct extension */
+        if (ext == null || !ext.matches("\\.[A-Za-z0-9]{3,5}")) {
+            ext = default_Extension;
+        }
+        if (!filename.endsWith(ext)) {
+            filename += ext;
+        }
+        downloadLink.setFinalFileName(filename);
+        final Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = null;
+        try {
+            try {
+                con = br2.openHeadConnection(DLLINK);
+            } catch (final BrowserException e) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            downloadLink.setProperty("directlink", DLLINK);
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, free_resume, free_maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            }
+            br.followConnection();
+            try {
+                dl.getConnection().disconnect();
+            } catch (final Throwable e) {
+            }
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    /** Avoid chars which are not allowed in filenames under certain OS' */
+    private static String encodeUnicode(final String input) {
+        String output = input;
+        output = output.replace(":", ";");
+        output = output.replace("|", "¦");
+        output = output.replace("<", "[");
+        output = output.replace(">", "]");
+        output = output.replace("/", "⁄");
+        output = output.replace("\\", "∖");
+        output = output.replace("*", "#");
+        output = output.replace("?", "¿");
+        output = output.replace("!", "¡");
+        output = output.replace("\"", "'");
+        return output;
+    }
+
+    private String formatDate(final String input) {
+        final long date = TimeFormatter.getMilliSeconds(input, "dd/MM/yyyy HH:mm", Locale.GERMANY);
+        String formattedDate = null;
+        final String targetFormat = "yyyy-MM-dd";
+        Date theDate = new Date(date);
+        try {
+            final SimpleDateFormat formatter = new SimpleDateFormat(targetFormat);
+            formattedDate = formatter.format(theDate);
+        } catch (Exception e) {
+            /* prevent input error killing plugin */
+            formattedDate = input;
+        }
+        return formattedDate;
+    }
+
+    private String getXML(final String parameter) {
+        return getXML(this.br.toString(), parameter);
+    }
+
+    private String getXML(final String source, final String parameter) {
+        String result = new Regex(source, "<" + parameter + "( type=\"[^<>\"/]*?\")?>([^<>]*?)</" + parameter + ">").getMatch(1);
+        if (result == null) {
+            result = new Regex(source, "<" + parameter + "><\\!\\[CDATA\\[([^<>]*?)\\]\\]></" + parameter + ">").getMatch(0);
+        }
+        return result;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return free_maxdownloads;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
