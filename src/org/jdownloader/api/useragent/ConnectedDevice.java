0bcf695d65c1918187c1023696c710782fa9ef59
kotz <kotz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-05 15:08:21 +0000

    - ConnectedDevice: Added app infos - DownloadsAPI: Added download password to query object
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31729 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/useragent/ConnectedDevice.java
+++ src/org/jdownloader/api/useragent/ConnectedDevice.java
@@ -4,6 +4,8 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.appwork.remoteapi.RemoteAPIRequest;
 import org.appwork.utils.StringUtils;
@@ -12,9 +14,11 @@ import org.jdownloader.api.myjdownloader.MyJDownloaderDirectHttpConnection;
 import org.jdownloader.api.myjdownloader.MyJDownloaderHttpConnection;
 
 public class ConnectedDevice {
-
-    private RemoteAPIRequest latestRequest;
-    private String           token;
+    private static final Pattern ANDROID_USERAGENT_PATTERN          = Pattern.compile("MyJDownloader Android App \\(Version: (.+) \\/ (.+)\\) \\(Android (.+);(.+)\\/(.+)\\)");
+    private static final Pattern WINMOB_FILERECON_USERAGENT_PATTERN = Pattern.compile("MyJDownloader file.recon App \\(Version: (.+) \\/ (.+)\\)");
+    private static final Pattern WIN_UNIVERSAL_USERAGENT_PATTERN    = Pattern.compile("MyJDownloader JD Universal App \\(Version: (.+) \\/ (.+)\\)");
+    private RemoteAPIRequest     latestRequest;
+    private String               token;
 
     public String getConnectToken() {
         return token;
@@ -63,9 +67,27 @@ public class ConnectedDevice {
     public String getDeviceName() {
         if (jdanywhere) {
             return "IPhone/IPad";
-        }
-        if (StringUtils.equals(getUserAgentString(), "JDownloader Android App")) {
-            return "Android";
+        } else if (isAndroidApp(getUserAgentString())) {
+            Matcher matcher = ANDROID_USERAGENT_PATTERN.matcher(getUserAgentString());
+            if (matcher.matches()) {
+                return matcher.group(4).replace(" ", "") + matcher.group(5).replace(" ", "") + "@Android" + matcher.group(3);
+            } else {
+                return "Android";
+            }
+        } else if (isFileReconApp(getUserAgentString())) {
+            Matcher matcher = WINMOB_FILERECON_USERAGENT_PATTERN.matcher(getUserAgentString());
+            if (matcher.matches()) {
+                return "file.recon" + matcher.group(1).replace(" ", "") + "(" + matcher.group(2).replace(" ", "") + ")" + "@WindowsMobile";
+            } else {
+                return "file.recon@WindowsMobile";
+            }
+        } else if (isJDUniversalApp(getUserAgentString())) {
+            Matcher matcher = WIN_UNIVERSAL_USERAGENT_PATTERN.matcher(getUserAgentString());
+            if (matcher.matches()) {
+                return "JDUniversal" + matcher.group(1).replace(" ", "") + "(" + matcher.group(2).replace(" ", "") + ")" + "@Win10";
+            } else {
+                return "JDUniversal@Win10";
+            }
         }
         if (info != null) {
             return info.getName() + "@" + info.getOs();
@@ -93,15 +115,30 @@ public class ConnectedDevice {
             return "Webinterface http://my.jdownloader.org";
         } else if (origin != null && origin.startsWith("chrome-extension://")) {
             return "Chrome Extension";
-        }
-        if (StringUtils.equals(getUserAgentString(), "JDownloader Android App")) {
-            return "My.JDownloader App";
+        } else if (isAndroidApp(getUserAgentString())) {
+            return "MyJDownloader by AppWork";
+        } else if (isFileReconApp(getUserAgentString())) {
+            return "file.recon by Pseudocode";
+        } else if (isJDUniversalApp(getUserAgentString())) {
+            return "JD Universal by Pseudocode";
         }
         if (origin != null) {
             return origin;
         }
         return "Unknown";
 
+    }
+
+    public boolean isAndroidApp(final String userAgent) {
+        return StringUtils.startsWithCaseInsensitive(userAgent, "MyJDownloader Android App");
+    }
+
+    public boolean isFileReconApp(final String userAgent) {
+        return StringUtils.startsWithCaseInsensitive(userAgent, "MyJDownloader file.recon App");
+    }
+
+    public boolean isJDUniversalApp(final String userAgent) {
+        return StringUtils.startsWithCaseInsensitive(userAgent, "MyJDownloader JD Universal App");
     }
 
     public static boolean isThisMyIpAddress(InetAddress addr) {
