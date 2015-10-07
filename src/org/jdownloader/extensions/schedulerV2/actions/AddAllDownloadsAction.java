6059aefc096644bcac36237c7d5d37f1f863b7b9
kotz <kotz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-07 10:15:46 +0000

    Scheduler: Added "Add all downloads" action
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31750 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f0d1bd9359e6d4d6f22df5accc52e71f5494ed96

---
+++ src/org/jdownloader/extensions/schedulerV2/actions/AddAllDownloadsAction.java
@@ -0,0 +1,32 @@
+package org.jdownloader.extensions.schedulerV2.actions;
+
+import java.util.List;
+
+import jd.controlling.downloadcontroller.DownloadController;
+import jd.controlling.linkcollector.LinkCollector;
+import jd.controlling.linkcrawler.CrawledLink;
+import jd.plugins.FilePackage;
+
+import org.jdownloader.extensions.schedulerV2.translate.T;
+
+@ScheduleActionIDAnnotation("ADD_ALL_DOWNLOADS")
+public class AddAllDownloadsAction extends AbstractScheduleAction<ScheduleActionEmptyConfig> {
+
+    public AddAllDownloadsAction(String configJson) {
+        super(configJson);
+    }
+
+    @Override
+    public String getReadableName() {
+        return T._.action_addAllDownloads();
+    }
+
+    @Override
+    public void execute() {
+        final List<CrawledLink> links = LinkCollector.getInstance().getAllChildren();
+        final List<FilePackage> convertedLinks = LinkCollector.getInstance().convert(links, true);
+        final boolean addTop = org.jdownloader.settings.staticreferences.CFG_LINKGRABBER.LINKGRABBER_ADD_AT_TOP.isEnabled();
+        DownloadController.getInstance().addAllAt(convertedLinks, addTop ? 0 : -(convertedLinks.size() + 10));
+    }
+
+}
