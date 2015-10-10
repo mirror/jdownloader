1356dffc5a9d3150861d1aa6dddfb277a56c3d34
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-10 14:19:24 +0000

    Vidce.tv - pre_download, https://board.jdownloader.org/showthread.php?t=66081
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31774 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/VidceTv.java
+++ src/jd/plugins/hoster/VidceTv.java
@@ -127,7 +127,7 @@ public class VidceTv extends PluginForHost {
     /* DEV NOTES */
     // XfileSharingProBasic Version 2.7.0.2
     // Tags: Script, template
-    // mods:
+    // mods: pre_download
     // limit-info:
     // protocol: no https
     // captchatype: null
@@ -176,6 +176,9 @@ public class VidceTv extends PluginForHost {
         altbr = br.cloneBrowser();
         setFUID(link);
         getPage(link.getDownloadURL());
+        if (br.containsHTML("pre_download")) {
+            getPage(new Regex(correctedBR, "(http://vidce.tv/.*?)\"").getMatch(0));
+        }
         if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
@@ -657,13 +660,13 @@ public class VidceTv extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -813,7 +816,7 @@ public class VidceTv extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
@@ -839,7 +842,7 @@ public class VidceTv extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -856,7 +859,7 @@ public class VidceTv extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
@@ -1027,7 +1030,7 @@ public class VidceTv extends PluginForHost {
     /**
      * Is intended to handle out of date errors which might occur seldom by re-tring a couple of times before throwing the out of date
      * error.
-     *
+     * 
      * @param dl
      *            : The DownloadLink
      * @param error
