3199abc1141c1a8c2a94eb1a20ba2c6ee2e40e2d
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-10-10 10:04:35 +0000

    Depfile.com - Adjustment fixes #72642
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@31773 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/IFilezComDecrypter.java
+++ src/jd/plugins/decrypter/IFilezComDecrypter.java
@@ -41,7 +41,7 @@ public class IFilezComDecrypter extends PluginForDecrypt {
     }
 
     private static final String DEPFILEDECRYPTED = "depfiledecrypted.com/";
-    private static final String INVALIDLINKS     = "https?://(www\\.)?depfile\\.com/(uploads|support|privacy|checkfiles|register|premium|terms|childpolicy|affiliate)";
+    private static final String INVALIDLINKS     = "https?://(www\\.)?depfile\\.com/(uploads|support|privacy|checkfiles|register|premium|terms|childpolicy|affiliate|report|data|dmca|favicon|ajax|API|robots)";
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
