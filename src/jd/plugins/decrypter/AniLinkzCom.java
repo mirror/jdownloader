000696f075250bd960053efc7a861d4878053a48
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-11-17 04:43:40 +0000

    Added: animegg.org, hoster support Fixed: anilinkz.com, support for links not within escape - closes #75468
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32102 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f1cce82ef11cce966da3d9b1d5490ad6dfb30e4a

--- src/jd/plugins/decrypter/AniLinkzCom.java
+++ src/jd/plugins/decrypter/AniLinkzCom.java
@@ -32,11 +32,11 @@ import jd.plugins.FilePackage;
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "anilinkz.com" }, urls = { "http://(?:www\\.)?anilinkz\\.(?:com|tv)/[^<>\"/]+(/[^<>\"/]+)?" }, flags = { 0 })
 /**
  * @author raztoki
- * */
+ */
 @SuppressWarnings("deprecation")
 public class AniLinkzCom extends antiDDoSForDecrypt {
 
-    private final String            supported_hoster = "(4shared\\.com|4vid\\.me|aniupload\\.com|animeuploads\\.com|animeultima\\.tv|arkvid\\.tv|auengine\\.(?:com|io)|bakavideo\\.tv|byzoo\\.org|chia\\-anime\\.com|cheesestream\\.com|cizgifilmlerizle\\.com|dailymotion\\.com|easyvideo\\.me|facebook\\.com/video|gogoanime\\.com|gorillavid\\.in|mp4star\\.com|mp4upload\\.com|movreel\\.com|myspace\\.com|nowvideo\\.eu|novamov\\.com|play44\\.net|(putlocker|firedrive)\\.com|playbb\\.me|playpanda\\.net|rutube\\.ru|sockshare\\.com|stagevu\\.com|upload2\\.com|uploadc\\.com|uploadcrazy\\.net|veevr\\.com|veoh\\.com|vidbox\\.yt|vidbull\\.com|vidcrazy\\.net|video44\\.net|videobb\\.com|videobam\\.com|videobull\\.com|videodrive\\.tv|videofun\\.me|videonest\\.net|videous\\.tv|videoweed\\.com|videowing\\.me|vidzur\\.com|videozoo\\.me|vimeo\\.com|vk\\.com|yourupload\\.com|youtube\\.com|zshare\\.net|220\\.ro|videos\\.sapo\\.pt)";
+    private final String            supported_hoster = "(4shared\\.com|4vid\\.me|aniupload\\.com|animegg\\.tv|animeuploads\\.com|animeultima\\.tv|arkvid\\.tv|auengine\\.(?:com|io)|bakavideo\\.tv|byzoo\\.org|chia\\-anime\\.com|cheesestream\\.com|cizgifilmlerizle\\.com|dailymotion\\.com|easyvideo\\.me|facebook\\.com/video|gogoanime\\.com|gorillavid\\.in|mp4star\\.com|mp4upload\\.com|movreel\\.com|myspace\\.com|nowvideo\\.eu|novamov\\.com|play44\\.net|(putlocker|firedrive)\\.com|playbb\\.me|playpanda\\.net|rutube\\.ru|sockshare\\.com|stagevu\\.com|upload2\\.com|uploadc\\.com|uploadcrazy\\.net|veevr\\.com|veoh\\.com|vidbox\\.yt|vidbull\\.com|vidcrazy\\.net|video44\\.net|videobb\\.com|videobam\\.com|videobull\\.com|videodrive\\.tv|videofun\\.me|videonest\\.net|videous\\.tv|videoweed\\.com|videowing\\.me|vidzur\\.com|videozoo\\.me|vimeo\\.com|vk\\.com|yourupload\\.com|youtube\\.com|zshare\\.net|220\\.ro|videos\\.sapo\\.pt)";
     private final String            invalid_links    = "http://(?:www\\.)?anilinkz\\.(?:com|tv)/(search|affiliates|get|img|dsa|forums|files|category|\\?page=|faqs|.*?-list|.*?-info|\\?random).*?";
     private String                  parameter        = null;
     private String                  fpName           = null;
@@ -191,6 +191,16 @@ public class AniLinkzCom extends antiDDoSForDecrypt {
                 // if (new Regex(escapeAll, "(?:https?:)?//[^\"]+(cizgifilmlerizle\\.com|animeuploads\\.com)/[^\"]+<div[^>]+").matches()) {
                 // escapeAll = escapeAll.replaceAll("<div[^>]+>", "");
                 // }
+            }
+            if (inValidate(escapeAll)) {
+                // seems they might not be using escape function.. just plain old iframe
+                final String[] iframes = br2.getRegex("<iframe[^>]+>").getColumn(-1);
+                if (iframes != null) {
+                    escapeAll = "";
+                    for (final String ifr : iframes) {
+                        escapeAll += ifr;
+                    }
+                }
             }
             if (inValidate(escapeAll) || new Regex(escapeAll, "(/img/\\w+dead\\.jpg|http://www\\./media)").matches()) {
                 // escapeAll == null / not online yet... || offline results within escapeAll
---
+++ src/jd/plugins/hoster/AnimeggOrg.java
@@ -0,0 +1,140 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.util.LinkedHashMap;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+/**
+ *
+ * @author raztoki
+ *
+ */
+@HostPlugin(revision = "$Revision: 21813 $", interfaceVersion = 2, names = { "animegg.org" }, urls = { "http://(www\\.)?animegg\\.org/(?:embed/\\d+|[\\w\\-]+episode-\\d+)" }, flags = { 0 })
+public class AnimeggOrg extends antiDDoSForHost {
+
+    // raztoki embed video player template.
+
+    private String dllink = null;
+
+    public AnimeggOrg(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.animegg.org/";
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void handleFree(DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        if (!dl.getConnection().isContentDisposition()) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws Exception {
+        this.setBrowserExclusive();
+        final String link = downloadLink.getDownloadURL();
+        br.getPage(link);
+        if (!link.matches(".+/embed/\\d+")) {
+            final String embed = br.getRegex("<iframe [^>]*src=(\"|')(.*?/embed/\\d+)\\1").getMatch(1);
+            br.getPage(embed);
+        }
+        final String filename = br.getRegex("<meta property=\"og:title\" content=\"(.*?)\"").getMatch(0);
+        // multiple qualities.
+        final String vidquals = br.getRegex("videoSources\\s*=\\s*(\\[.*?\\]);").getMatch(0);
+        final LinkedHashMap<Integer, String> results = new LinkedHashMap<Integer, String>();
+        final String[] quals = getJsonResultsFromArray(vidquals);
+        for (final String qual : quals) {
+            final String label = getJson(qual, "label");
+            final String lapel = new Regex(label, "(\\d+)p").getMatch(0);
+            final Integer p = lapel != null ? Integer.parseInt(lapel) : -1;
+            final String lnk = getJson(qual, "file");
+            if (lnk != null && lapel != null) {
+                results.put(p, lnk);
+            }
+        }
+        // always grabs best.
+        final Integer[] availQuals = { 1080, 720, 480, 360, 240 };
+        for (final Integer aQ : availQuals) {
+            if (results.containsKey(aQ)) {
+                dllink = results.get(aQ);
+                break;
+            }
+        }
+        if (dllink == null || filename == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dllink = Encoding.urlDecode(dllink, false);
+        Browser br2 = br.cloneBrowser();
+        // In case the link redirects to the finallink
+        br2.setFollowRedirects(true);
+        URLConnectionAdapter con = null;
+        try {
+            con = br2.openHeadConnection(dllink);
+            // only way to check for made up links... or offline is here
+            if (con.getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setFinalFileName(filename + ".mp4");
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            return AvailableStatus.TRUE;
+        } finally {
+            try {
+                con.disconnect();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+}
