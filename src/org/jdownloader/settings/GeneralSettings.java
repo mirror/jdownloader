57daaf6a1f62f92dae0d80c1a6011087c7e4712e
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-12-06 10:21:42 +0000

    GeneralSettings: added PreferBouncyCastleForTLS build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32253 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1ba2345e5e4a3b944173ff52d5242c6e88edd268

--- src/org/jdownloader/settings/GeneralSettings.java
+++ src/org/jdownloader/settings/GeneralSettings.java
@@ -3,6 +3,8 @@ package org.jdownloader.settings;
 import java.io.File;
 import java.util.ArrayList;
 
+import jd.utils.JDUtilities;
+
 import org.appwork.storage.config.ConfigInterface;
 import org.appwork.storage.config.annotations.AboutConfig;
 import org.appwork.storage.config.annotations.AbstractCustomValueGetter;
@@ -23,8 +25,6 @@ import org.appwork.utils.StringUtils;
 import org.appwork.utils.os.CrossSystem;
 import org.jdownloader.controlling.domainrules.DomainRule;
 import org.jdownloader.gui.translate._GUI;
-
-import jd.utils.JDUtilities;
 
 public interface GeneralSettings extends ConfigInterface {
     class DefaultBrowserCommand extends AbstractDefaultFactory<String[]> {
@@ -433,8 +433,10 @@ public interface GeneralSettings extends ConfigInterface {
     void setDeleteContainerFilesAfterAddingThemAction(DeleteContainerAction action);
 
     public static enum CreateFolderTrigger {
-        @EnumLabel("When the actual Download starts") ON_DOWNLOAD_START,
-        @EnumLabel("When the links are added to the Downloadlist") ON_LINKS_ADDED,
+        @EnumLabel("When the actual Download starts")
+        ON_DOWNLOAD_START,
+        @EnumLabel("When the links are added to the Downloadlist")
+        ON_LINKS_ADDED,
 
     }
 
@@ -606,4 +608,13 @@ public interface GeneralSettings extends ConfigInterface {
     boolean isSharedMemoryStateEnabled();
 
     void setSharedMemoryStateEnabled(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @RequiresRestart("A JDownloader Restart is Required")
+    @DescriptionForConfigEntry("Prefer BouncyCastle for TLS")
+    boolean isPreferBouncyCastleForTLS();
+
+    void setPreferBouncyCastleForTLS(boolean b);
+
 }
