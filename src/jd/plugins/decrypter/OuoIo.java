aab549f63e4171e10fc45bf5ba53ad0caf4f00aa
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2015-12-30 03:58:01 +0000

    Added: ouo.io, /s/ support - re: https://board.jdownloader.org/showthread.php?t=67309
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32351 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/OuoIo.java
+++ src/jd/plugins/decrypter/OuoIo.java
@@ -23,22 +23,41 @@ import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPlu
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "ouo.io" }, urls = { "http://ouo\\.io/[A-Za-z0-9]+" }, flags = { 0 })
+/**
+ *
+ * @author raztoki
+ * @author psp
+ *
+ */
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "ouo.io" }, urls = { "http://ouo\\.io/(s/)?([A-Za-z0-9]+)(?:\\?s=((?:http|ftp).+))?" }, flags = { 0 })
 public class OuoIo extends PluginForDecrypt {
 
     public OuoIo(PluginWrapper wrapper) {
         super(wrapper);
     }
 
+    private String parameter = null;
+    private String fuid      = null;
+    private String s_fuid    = null;
+    private String s_value   = null;
+
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        final String parameter = param.toString();
-        final String fid = parameter.substring(parameter.lastIndexOf("/") + 1);
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        set(param.toString());
+        if (s_value != null && s_fuid != null) {
+            decryptedLinks.add(createDownloadlink(Encoding.urlDecode(s_value, false)));
+            return decryptedLinks;
+        } else if (s_fuid != null && s_value == null) {
+            // fid is just a URL owner identifier, the sv is the end url! Without proper sv value you can't get the end URL!
+            decryptedLinks.add(createOfflinelink(parameter));
+            return decryptedLinks;
+        }
         br.getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 403 || br.getHttpConnection().getResponseCode() == 404) {
             decryptedLinks.add(this.createOfflinelink(parameter));
@@ -50,7 +69,7 @@ public class OuoIo extends PluginForDecrypt {
         }
         final String token = br.getRegex("name=\"_token\" type=\"hidden\" value=\"([^<>\"]*?)\"").getMatch(0);
         final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br).getToken();
-        br.postPage("http://ouo.io/go/" + fid, "_token=" + Encoding.urlEncode(token) + "&g-recaptcha-response=" + Encoding.urlEncode(recaptchaV2Response));
+        br.postPage("http://ouo.io/go/" + fuid, "_token=" + Encoding.urlEncode(token) + "&g-recaptcha-response=" + Encoding.urlEncode(recaptchaV2Response));
         final String finallink = getFinalLink();
         if (finallink == null) {
             return null;
@@ -61,12 +80,22 @@ public class OuoIo extends PluginForDecrypt {
     }
 
     private String getFinalLink() {
-        final String finallink = br.getRegex("\"\\s*(.*?)\\s*\"\\s+id=\"btn-main\"").getMatch(0);
+        final String finallink = br.getRegex("\"\\s*([^\r\n]+)\\s*\"\\s+id=\"btn-main\"").getMatch(0);
         return finallink;
     }
 
-    /* NO OVERRIDE!! We need to stay 0.9*compatible */
+    private void set(final String downloadLink) {
+        parameter = downloadLink;
+        fuid = new Regex(parameter, this.getSupportedLinks()).getMatch(1);
+        s_fuid = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        s_value = new Regex(parameter, this.getSupportedLinks()).getMatch(2);
+    }
+
     public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        set(link.getDownloadURL());
+        if (s_value != null && s_fuid != null) {
+            return false;
+        }
         return true;
     }
 
