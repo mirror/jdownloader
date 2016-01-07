bd9bf59fc81ef91c9f7608dfa9e9a8cc86b273df
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-01-07 17:54:20 +0000

    FirstplanetEu: changed limits, asked by admin FileCryptCc: fixed containsCaptcha regex NZB: updated to ignore parsing issues SnelNLUsenet: added usenet support for this service build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32395 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/SnelNLUsenet.java
@@ -0,0 +1,140 @@
+package jd.plugins.hoster;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import jd.PluginWrapper;
+import jd.http.Cookie;
+import jd.http.Cookies;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.TimeFormatter;
+
+@HostPlugin(revision = "$Revision: 31032 $", interfaceVersion = 3, names = { "snelnl.com" }, urls = { "" }, flags = { 0 })
+public class SnelNLUsenet extends UseNet {
+    public SnelNLUsenet(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium("https://www.snelnl.com/en/products");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.snelnl.com/en/general-terms";
+    }
+
+    @Override
+    protected String getUsername(Account account) {
+        return account.getStringProperty(USENET_USERNAME, account.getUser());
+    }
+
+    private boolean containsSessionCookie(final Cookies cookies) {
+        for (final Cookie cookie : cookies.getCookies()) {
+            if (StringUtils.startsWithCaseInsensitive(cookie.getKey(), "SSESS") && !StringUtils.equalsIgnoreCase(cookie.getValue(), "deleted")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private final String USENET_USERNAME = "USENET_USERNAME";
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        setBrowserExclusive();
+        final AccountInfo ai = new AccountInfo();
+        br.setFollowRedirects(true);
+        final Cookies cookies = account.loadCookies("");
+        try {
+            Form login = null;
+            if (cookies != null) {
+                br.setCookies(getHost(), cookies);
+                br.getPage("https://www.snelnl.com/en/user/login");
+                login = br.getFormbyActionRegex("/en/user/login\\?.*");
+                if (login != null && login.containsHTML("name") && login.containsHTML("pass")) {
+                    br.getCookies(getHost()).clear();
+                } else if (!containsSessionCookie(br.getCookies(getHost()))) {
+                    br.getCookies(getHost()).clear();
+                } else {
+                    if (!StringUtils.endsWithCaseInsensitive(br.getURL(), "en/user")) {
+                        br.getPage("https://www.snelnl.com/en/user");
+                    }
+                }
+            }
+            if (!containsSessionCookie(br.getCookies(getHost()))) {
+                account.clearCookies("");
+                final String username = account.getUser();
+                if (username == null || !username.matches("^.+?@.+?\\.[^\\.]+")) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "Please use your email address to login", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                br.setCookie(getHost(), "language", "en");
+                br.getPage("https://www.snelnl.com/en/user/login");
+                login = br.getFormbyActionRegex("/en/user/login\\?.*");
+                login.put("name", Encoding.urlEncode(username));
+                login.put("pass", Encoding.urlEncode(account.getPass()));
+                br.submitForm(login);
+                login = br.getFormbyActionRegex("/en/user/login\\?.*");
+                if (login != null && login.containsHTML("name") && login.containsHTML("pass")) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                if (!containsSessionCookie(br.getCookies(getHost()))) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+            }
+            account.saveCookies(br.getCookies(getHost()), "");
+            final String userName = br.getRegex("item-title\">Username:</div>.*?item-text\">(.*?)<").getMatch(0);
+            if (userName != null) {
+                account.setProperty(USENET_USERNAME, userName);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            final String packageType = br.getRegex("item-title\">Account type:</div>.*?item-text\">(.*?)<").getMatch(0);
+            if (StringUtils.contains(packageType, "Slow test")) {
+                // free trial
+                ai.setStatus("Free 3 day Trial");
+                ai.setUnlimitedTraffic();
+                final String endDate = br.getRegex("End date:</div>.*?item-text\">(.*?)<").getMatch(0);
+                if (endDate == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                ai.setValidUntil(TimeFormatter.getMilliSeconds(endDate, "MMM dd yyyy' - 'HH:mm", Locale.ENGLISH));
+                account.setMaxSimultanDownloads(3);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "Unknown account type(" + packageType + "), please contact JDownloader support");
+            }
+        } catch (final PluginException e) {
+            if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                account.clearCookies("");
+            }
+            throw e;
+        }
+        ai.setProperty("multiHostSupport", Arrays.asList(new String[] { "usenet" }));
+        return ai;
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return "reader.snelnl.com";
+    }
+
+    @Override
+    protected String getSSLServerAddress() throws Exception {
+        return "reader.snelnl.com";
+    }
+
+    @Override
+    protected int[] getAvailablePorts() {
+        return new int[] { 119, 443, 8080 };
+    }
+
+    @Override
+    protected int[] getAvailableSSLPorts() {
+        return new int[] { 80, 81, 563 };
+    }
+}
