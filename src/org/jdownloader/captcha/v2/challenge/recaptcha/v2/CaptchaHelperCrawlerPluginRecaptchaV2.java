497c7dba0dd7783ee97b92807a247b76cc2a7636
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-01-14 11:35:26 +0000

    Fixes Recaptcha Remote Challenges build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32450 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/CaptchaHelperCrawlerPluginRecaptchaV2.java
+++ src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/CaptchaHelperCrawlerPluginRecaptchaV2.java
@@ -2,6 +2,15 @@ package org.jdownloader.captcha.v2.challenge.recaptcha.v2;
 
 import java.io.IOException;
 
+import org.appwork.utils.logging2.LogSource;
+import org.jdownloader.captcha.blacklist.BlacklistEntry;
+import org.jdownloader.captcha.blacklist.BlockAllCrawlerCaptchasEntry;
+import org.jdownloader.captcha.blacklist.BlockCrawlerCaptchasByHost;
+import org.jdownloader.captcha.blacklist.BlockCrawlerCaptchasByPackage;
+import org.jdownloader.captcha.blacklist.CaptchaBlackList;
+import org.jdownloader.captcha.v2.ChallengeResponseController;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.RecaptchaV2Challenge.Recaptcha2FallbackChallenge;
+
 import jd.controlling.captcha.SkipException;
 import jd.controlling.downloadcontroller.SingleDownloadController;
 import jd.controlling.linkcollector.LinkCollector;
@@ -12,15 +21,6 @@ import jd.plugins.DecrypterException;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
-
-import org.appwork.utils.logging2.LogSource;
-import org.jdownloader.captcha.blacklist.BlacklistEntry;
-import org.jdownloader.captcha.blacklist.BlockAllCrawlerCaptchasEntry;
-import org.jdownloader.captcha.blacklist.BlockCrawlerCaptchasByHost;
-import org.jdownloader.captcha.blacklist.BlockCrawlerCaptchasByPackage;
-import org.jdownloader.captcha.blacklist.CaptchaBlackList;
-import org.jdownloader.captcha.v2.ChallengeResponseController;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.RecaptchaV2Challenge.Recaptcha2FallbackChallenge;
 
 public class CaptchaHelperCrawlerPluginRecaptchaV2 extends AbstractCaptchaHelperRecaptchaV2<PluginForDecrypt> {
 
@@ -56,7 +56,12 @@ public class CaptchaHelperCrawlerPluginRecaptchaV2 extends AbstractCaptchaHelper
         }
         try {
             ChallengeResponseController.getInstance().handle(c);
+            logger.info("Results: " + c.getResult().size());
+            if (c.getResult().size() > 0) {
+                logger.info("Challenge: " + c.getResult().get(0).getChallenge().getClass());
+            }
             if (c.getResult().size() == 1 && c.getResult().get(0).getChallenge() instanceof Recaptcha2FallbackChallenge) {
+                logger.info("2 Step Recaptcha v2 round #2");
                 final Recaptcha2FallbackChallenge challenge = ((Recaptcha2FallbackChallenge) c.getResult().get(0).getChallenge());
                 try {
                     challenge.reload(2, c.getResult().get(0).getValue());
