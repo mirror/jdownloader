67ff1852a9f68b13b511c5a72c5d7f350e3fbda1
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-01-19 14:29:31 +0000

    PluginJSonUtils: extracted class from K2SApi Adverigo: fixes #76653 build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32511 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/solver/myjd/CaptchaMyJDSolver.java
+++ src/org/jdownloader/captcha/v2/solver/myjd/CaptchaMyJDSolver.java
@@ -4,6 +4,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import jd.controlling.accountchecker.AccountChecker.AccountCheckJob;
+import jd.controlling.accountchecker.AccountCheckerThread;
+import jd.controlling.downloadcontroller.SingleDownloadController;
+import jd.controlling.linkchecker.LinkCheckerThread;
+import jd.controlling.linkcrawler.LinkCrawlerThread;
+import jd.plugins.Account;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForHost;
+
 import org.appwork.storage.config.JsonConfig;
 import org.appwork.utils.Files;
 import org.appwork.utils.IO;
@@ -26,15 +35,6 @@ import org.jdownloader.myjdownloader.client.json.MyCaptchaChallenge;
 import org.jdownloader.myjdownloader.client.json.MyCaptchaChallenge.TYPE;
 import org.jdownloader.myjdownloader.client.json.MyCaptchaSolution;
 import org.jdownloader.settings.staticreferences.CFG_GENERAL;
-
-import jd.controlling.accountchecker.AccountChecker.AccountCheckJob;
-import jd.controlling.accountchecker.AccountCheckerThread;
-import jd.controlling.downloadcontroller.SingleDownloadController;
-import jd.controlling.linkchecker.LinkCheckerThread;
-import jd.controlling.linkcrawler.LinkCrawlerThread;
-import jd.plugins.Account;
-import jd.plugins.Plugin;
-import jd.plugins.PluginForHost;
 
 public class CaptchaMyJDSolver extends CESChallengeSolver<String> {
 
@@ -143,7 +143,6 @@ public class CaptchaMyJDSolver extends CESChallengeSolver<String> {
                     if (validLogins) {
                         if (c instanceof RecaptchaV1CaptchaChallenge) {
                             logger.info("is RC1");
-
                         }
                         return true;
                     }
