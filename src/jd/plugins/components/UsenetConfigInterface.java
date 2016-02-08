d03c6e0f5c66683acd0ded9a81e40b7e3c0db202
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-08 17:20:13 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32722 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 6ac89b0a7027d3d4060fc3110207cddecd3cd2d4

--- src/jd/plugins/components/UsenetConfigInterface.java
+++ src/jd/plugins/components/UsenetConfigInterface.java
@@ -1,11 +1,9 @@
 package jd.plugins.components;
 
 import org.appwork.storage.config.ConfigInterface;
-import org.appwork.storage.config.annotations.AboutConfig;
 
 public interface UsenetConfigInterface extends ConfigInterface {
 
-    @AboutConfig
     UsenetServer getUsenetServer();
 
     void setUsenetServer(UsenetServer server);
