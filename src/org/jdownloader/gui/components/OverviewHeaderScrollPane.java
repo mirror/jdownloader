c91b7bf59e9fc99b14427c1d008c6a2e2e020e01
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-10 15:14:28 +0000

    OverviewHeaderScrollPane: fixed npe for non synthetica lafs build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@32766 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/components/OverviewHeaderScrollPane.java
+++ src/org/jdownloader/gui/components/OverviewHeaderScrollPane.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Insets;
 
 import javax.swing.JComponent;
+import javax.swing.border.Border;
 
 import org.jdownloader.gui.views.components.HeaderScrollPane;
 
@@ -11,27 +12,28 @@ public class OverviewHeaderScrollPane extends HeaderScrollPane {
 
     public OverviewHeaderScrollPane(JComponent overView) {
         super(overView);
-
     }
 
-    // @Override
-    // protected int getHeaderHeight() {
-    // return 20;
-    // }
-
     public Dimension getPreferredSize() {
-        Dimension pref = getViewport().getPreferredSize();
-        Insets insets = getBorder().getBorderInsets(this);
-        Insets viewPortBorder = getViewportBorder().getBorderInsets(getViewport());
-        // no horizontal scrollbar
-        // if we would check the hsb here, we would get bad initial height informations and flickering
-        pref.height += insets.top + insets.bottom + getPrefHeaderHeight() + viewPortBorder.top + viewPortBorder.bottom;
-
+        final Dimension pref = getViewport().getPreferredSize();
+        final Border border = getBorder();
+        if (border != null) {
+            final Insets insets = border.getBorderInsets(this);
+            // no horizontal scrollbar
+            // if we would check the hsb here, we would get bad initial height informations and flickering
+            final Border viewBorder = getViewportBorder();
+            if (viewBorder != null) {
+                final Insets viewPortBorder = viewBorder.getBorderInsets(getViewport());
+                pref.height += insets.top + insets.bottom + getPrefHeaderHeight() + viewPortBorder.top + viewPortBorder.bottom;
+            } else {
+                pref.height += insets.top + insets.bottom + getPrefHeaderHeight();
+            }
+        }
         return pref;
     }
 
     public Dimension getMinimumSize() {
-        Dimension pref = getPreferredSize();
+        final Dimension pref = getPreferredSize();
         pref.width = 0;
         return pref;
     }
