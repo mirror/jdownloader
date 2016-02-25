b2c7b15679c9e390b0cd3496af43cc957e058721
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-25 16:34:59 +0000

    Exashare.com - Adjustment (The file was deleted by) fixes #77563
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33030 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ExaShareCom.java
+++ src/jd/plugins/hoster/ExaShareCom.java
@@ -147,7 +147,7 @@ public class ExaShareCom extends PluginForHost {
         prepBrowser(br);
         setFUID(link);
         getPage(link.getDownloadURL());
-        if (!br.getURL().matches("https?://(www\\.)?exashare\\.com/((vid)?embed\\-)?[a-z0-9]{12}") || new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|class=\"error\\-container\"|File Not Found<|The file you were looking for could not be found)").matches()) {
+        if (!br.getURL().matches("https?://(www\\.)?exashare\\.com/((vid)?embed\\-)?[a-z0-9]{12}") || new Regex(correctedBR, "(No such file|File Not Found<|>The file was (deleted|removed) by|Reason for deletion:\n|class=\"error\\-container\"|The file you were looking for could not be found)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         if (new Regex(correctedBR, MAINTENANCE).matches()) {
@@ -524,13 +524,13 @@ public class ExaShareCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -686,7 +686,7 @@ public class ExaShareCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
@@ -712,7 +712,7 @@ public class ExaShareCom extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -729,7 +729,7 @@ public class ExaShareCom extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      * */
