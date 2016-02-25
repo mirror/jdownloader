9f97c793e5a07cb1956cc094ef051e30d34a4c37
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-25 15:59:46 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33027 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f6369c9a49dd8bacd3a32429ed26b28c569f16b5

--- src/org/jdownloader/startup/Main.java
+++ src/org/jdownloader/startup/Main.java
@@ -112,7 +112,7 @@ public class Main {
 
     public static void checkLanguageSwitch(final String[] args) {
         try {
-            String lng = JSonStorage.restoreFromFile("cfg/language.json", TranslationFactory.getDesiredLanguage());
+            final String lng = JSonStorage.restoreFromFile("cfg/language.json", TranslationFactory.getDesiredLanguage());
             TranslationFactory.setDesiredLanguage(lng);
 
             for (int i = 0; i < args.length; i++) {
