d8ef58e836a4dbdeccaafe485cd897327164aad0
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-25 16:31:05 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33029 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/solver/dbc/test/Exception.java
+++
@@ -1,7 +0,0 @@
-package org.jdownloader.captcha.v2.solver.dbc.test;
-
-abstract public class Exception extends java.lang.Exception {
-    public Exception(String message) {
-        super(message);
-    }
-}
--- src/org/jdownloader/captcha/v2/solver/dbc/test/ServiceOverloadException.java
+++
@@ -1,7 +0,0 @@
-package org.jdownloader.captcha.v2.solver.dbc.test;
-
-public class ServiceOverloadException extends Exception {
-    public ServiceOverloadException(String message) {
-        super(message);
-    }
-}
