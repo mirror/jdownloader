6b12e2a8dd14777845600e4dbb710388ad940d64
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-25 16:31:05 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33029 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 3660036562cc7fbd92e90a3517a87d7bf8e3bfd6

--- src/org/jdownloader/captcha/v2/solver/dbc/test/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package org.jdownloader.captcha.v2.solver.dbc.test;
-
-public class AccessDeniedException extends Exception {
-    public AccessDeniedException(String message) {
-        super(message);
-    }
-}
--- src/org/jdownloader/captcha/v2/solver/dbc/test/InvalidCaptchaException.java
+++
@@ -1,7 +0,0 @@
-package org.jdownloader.captcha.v2.solver.dbc.test;
-
-public class InvalidCaptchaException extends Exception {
-    public InvalidCaptchaException(String message) {
-        super(message);
-    }
-}
