94dd2e1cd81311414e269a18fd3a19d85966a396
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-26 11:37:25 +0000

    reviewed WindowsApplicationAdapter
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33046 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 52477ec673c18c37f4693d150295ee9e1789d02e

--- src/org/jdownloader/crosssystem/windows/WindowsApplicationAdapter.java
+++ src/org/jdownloader/crosssystem/windows/WindowsApplicationAdapter.java
@@ -9,11 +9,20 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.Transparency;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import jd.SecondLevelLaunch;
+import jd.controlling.downloadcontroller.DownloadController;
+import jd.controlling.downloadcontroller.DownloadLinkCandidate;
+import jd.controlling.downloadcontroller.DownloadLinkCandidateResult;
+import jd.controlling.downloadcontroller.DownloadWatchDog;
+import jd.controlling.downloadcontroller.DownloadWatchDogProperty;
+import jd.controlling.downloadcontroller.SingleDownloadController;
+import jd.controlling.downloadcontroller.event.DownloadWatchdogListener;
+import jd.gui.swing.jdgui.JDGui;
+import jd.gui.swing.jdgui.JDownloaderMainFrame;
 
 import org.appwork.storage.config.ValidationException;
 import org.appwork.storage.config.events.GenericConfigEventListener;
@@ -22,226 +31,169 @@ import org.appwork.utils.logging2.extmanager.Log;
 import org.appwork.utils.os.CrossSystem;
 import org.appwork.utils.os.CrossSystem.OperatingSystem;
 import org.jdownloader.controlling.AggregatedNumbers;
-import org.jdownloader.gui.views.downloads.table.DownloadsTable;
 import org.jdownloader.settings.GraphicalUserInterfaceSettings.WindowsTaskBarProgressDisplay;
 import org.jdownloader.settings.staticreferences.CFG_GUI;
-
-import jd.SecondLevelLaunch;
-import jd.controlling.TaskQueue;
-import jd.controlling.downloadcontroller.DownloadLinkCandidate;
-import jd.controlling.downloadcontroller.DownloadLinkCandidateResult;
-import jd.controlling.downloadcontroller.DownloadWatchDog;
-import jd.controlling.downloadcontroller.DownloadWatchDogProperty;
-import jd.controlling.downloadcontroller.SingleDownloadController;
-import jd.controlling.downloadcontroller.event.DownloadWatchdogListener;
-import jd.gui.swing.jdgui.JDGui;
 
 public class WindowsApplicationAdapter {
     private static final WindowsApplicationAdapter INSTANCE = new WindowsApplicationAdapter();
 
-    /**
-     * get the only existing instance of WindowsApplicationAdapter. This is a singleton
-     *
-     * @return
-     */
     public static WindowsApplicationAdapter getInstance() {
         return WindowsApplicationAdapter.INSTANCE;
     }
 
-    /**
-     * Create a new instance of WindowsApplicationAdapter. This is a singleton class. Access the only existing instance by using
-     * {@link #getInstance()}.
-     */
     private WindowsApplicationAdapter() {
-
         initWin7PlusTaskbarExtension();
-
     }
 
     private void initWin7PlusTaskbarExtension() {
-        if (!CrossSystem.getOS().isMinimum(OperatingSystem.WINDOWS_7)) {
-            return;
-        }
-        SecondLevelLaunch.INIT_COMPLETE.executeWhenReached(new Runnable() {
-
-            private DownloadWatchdogListener listener;
-
-            @Override
-            public void run() {
-
-                CFG_GUI.WINDOWS_TASKBAR_PROGRESS_DISPLAY.getEventSender().addListener(new GenericConfigEventListener<Enum>() {
-
-                    @Override
-                    public void onConfigValidatorError(KeyHandler<Enum> keyHandler, Enum invalidValue, ValidationException validateException) {
-                    }
-
-                    @Override
-                    public void onConfigValueModified(KeyHandler<Enum> keyHandler, Enum newValue) {
-                        if (WindowsTaskBarProgressDisplay.NOTHING.equals(newValue)) {
-                            stopDockUpdater();
-
-                        } else {
-                            startDockUpdater();
-                        }
-                    }
-
-                });
-                DownloadWatchDog.getInstance().getEventSender().addListener(listener = new DownloadWatchdogListener() {
-
-                    @Override
-                    public void onDownloadWatchdogStateIsStopping() {
-                        stopDockUpdater();
-                    }
-
-                    @Override
-                    public void onDownloadWatchdogStateIsStopped() {
-                        stopDockUpdater();
-                    }
-
-                    @Override
-                    public void onDownloadWatchdogStateIsRunning() {
-                        startDockUpdater();
-                    }
-
-                    @Override
-                    public void onDownloadWatchdogStateIsPause() {
-                        startDockUpdater();
-                    }
-
-                    @Override
-                    public void onDownloadWatchdogStateIsIdle() {
-                    }
-
-                    @Override
-                    public void onDownloadWatchdogDataUpdate() {
-                    }
-
-                    @Override
-                    public void onDownloadControllerStart(SingleDownloadController downloadController, DownloadLinkCandidate candidate) {
-                    }
-
-                    @Override
-                    public void onDownloadControllerStopped(SingleDownloadController downloadController, DownloadLinkCandidate candidate, DownloadLinkCandidateResult result) {
-                    }
-
-                    @Override
-                    public void onDownloadWatchDogPropertyChange(DownloadWatchDogProperty propertyChange) {
-                    }
-                });
-                DownloadWatchDog.getInstance().notifyCurrentState(listener);
-            }
-
-        });
-    }
-
-    private final Object LOCK = new Object();
-
-    private double lastPercent;
-
-    private ScheduledFuture<?> queueHandler;
-
-    private void startDockUpdater() {
-        synchronized (LOCK) {
-            if (CFG_GUI.CFG.getWindowsTaskbarProgressDisplay() == WindowsTaskBarProgressDisplay.NOTHING) {
-
-                return;
-            }
-            if (queueHandler != null) {
-                return;
-            }
-            queueHandler = TaskQueue.TIMINGQUEUE.scheduleAtFixedRate(new Runnable() {
+        if (CrossSystem.getOS().isMinimum(OperatingSystem.WINDOWS_7)) {
+            SecondLevelLaunch.INIT_COMPLETE.executeWhenReached(new Runnable() {
 
                 @Override
                 public void run() {
+                    CFG_GUI.WINDOWS_TASKBAR_PROGRESS_DISPLAY.getEventSender().addListener(new GenericConfigEventListener<Enum>() {
 
-                    try {
-                        final AggregatedNumbers aggn = new AggregatedNumbers(DownloadsTable.getInstance().getSelectionInfo(false, false));
-                        double percent = 0;
-                        if (aggn.getTotalBytes() > 0) {
-                            percent = ((double) aggn.getLoadedBytes()) / aggn.getTotalBytes();
+                        @Override
+                        public void onConfigValidatorError(KeyHandler<Enum> keyHandler, Enum invalidValue, ValidationException validateException) {
                         }
-                        System.out.println(percent);
-                        if (lastPercent == percent) {
-                            // no changes
-                            return;
-                        }
-                        lastPercent = percent;
-                        if (CFG_GUI.CFG.getWindowsTaskbarProgressDisplay() == WindowsTaskBarProgressDisplay.TOTAL_PROGRESS_AND_CONNECTIONS) {
-                            BufferedImage image = createImage(DownloadWatchDog.getInstance().getActiveDownloads());
-                            ByteArrayOutputStream baos;
-                            Win7TaskBar.writeTransparentIcoImageWithSanselan(image, baos = new ByteArrayOutputStream());
-                            Object icon = Win7TaskBar.createIcon(baos.toByteArray());
 
-                            if (icon != null) {
-                                Win7TaskBar.setOverlayIcon(JDGui.getInstance().getMainFrame(), icon, true);
+                        @Override
+                        public void onConfigValueModified(KeyHandler<Enum> keyHandler, Enum newValue) {
+                            if (WindowsTaskBarProgressDisplay.NOTHING.equals(newValue)) {
+                                stopDockUpdater();
+                            } else {
+                                startDockUpdater();
                             }
                         }
-                        Win7TaskBar.setProgress(JDGui.getInstance().getMainFrame(), lastPercent, true);
+                    });
+                    final DownloadWatchdogListener listener;
+                    DownloadWatchDog.getInstance().getEventSender().addListener(listener = new DownloadWatchdogListener() {
+
+                        @Override
+                        public void onDownloadWatchdogStateIsStopping() {
+                            stopDockUpdater();
+                        }
+
+                        @Override
+                        public void onDownloadWatchdogStateIsStopped() {
+                            stopDockUpdater();
+                        }
+
+                        @Override
+                        public void onDownloadWatchdogStateIsRunning() {
+                            startDockUpdater();
+                        }
+
+                        @Override
+                        public void onDownloadWatchdogStateIsPause() {
+                            startDockUpdater();
+                        }
+
+                        @Override
+                        public void onDownloadWatchdogStateIsIdle() {
+                        }
+
+                        @Override
+                        public void onDownloadWatchdogDataUpdate() {
+                        }
+
+                        @Override
+                        public void onDownloadControllerStart(SingleDownloadController downloadController, DownloadLinkCandidate candidate) {
+                        }
+
+                        @Override
+                        public void onDownloadControllerStopped(SingleDownloadController downloadController, DownloadLinkCandidate candidate, DownloadLinkCandidateResult result) {
+                        }
+
+                        @Override
+                        public void onDownloadWatchDogPropertyChange(DownloadWatchDogProperty propertyChange) {
+                        }
+                    });
+                    DownloadWatchDog.getInstance().notifyCurrentState(listener);
+                }
+            });
+        }
+    }
+
+    private final AtomicReference<Thread> thread = new AtomicReference<Thread>(null);
+
+    private void startDockUpdater() {
+        if (CFG_GUI.CFG.getWindowsTaskbarProgressDisplay() != WindowsTaskBarProgressDisplay.NOTHING) {
+            final Thread thread = new Thread("WindowsTaskbarProgress") {
+                {
+                    setDaemon(true);
+                }
+
+                public void run() {
+                    double lastPercent = -1;
+                    try {
+                        final JDownloaderMainFrame mainFrame = JDGui.getInstance().getMainFrame();
+                        while (WindowsApplicationAdapter.this.thread.get() == Thread.currentThread()) {
+                            final AggregatedNumbers aggn = new AggregatedNumbers(DownloadController.getInstance().getSelectionInfo());
+                            final double percent;
+                            if (aggn.getTotalBytes() > 0) {
+                                percent = ((double) aggn.getLoadedBytes()) / aggn.getTotalBytes();
+                            } else {
+                                percent = 0;
+                            }
+                            if (lastPercent != percent) {
+                                lastPercent = percent;
+                                if (CFG_GUI.CFG.getWindowsTaskbarProgressDisplay() == WindowsTaskBarProgressDisplay.TOTAL_PROGRESS_AND_CONNECTIONS) {
+                                    final BufferedImage image = createImage(DownloadWatchDog.getInstance().getActiveDownloads());
+                                    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                                    Win7TaskBar.writeTransparentIcoImageWithSanselan(image, baos);
+                                    final Object icon = Win7TaskBar.createIcon(baos.toByteArray());
+                                    if (icon != null) {
+                                        Win7TaskBar.setOverlayIcon(mainFrame, icon, true);
+                                    }
+                                }
+                                Win7TaskBar.setProgress(mainFrame, lastPercent, true);
+                            } else {
+                                Thread.sleep(1000);
+                            }
+                        }
                     } catch (Throwable er) {
                         Log.log(er);
                     } finally {
-                        synchronized (LOCK) {
-                            if (queueHandler == null) {
-                                // ensure reset
-                                reset();
-                                queueHandler.cancel(true);
-                            }
+                        if (WindowsApplicationAdapter.this.thread.compareAndSet(Thread.currentThread(), null)) {
+                            reset();
                         }
                     }
                 }
-            }, 0, 1, TimeUnit.SECONDS);
-
+            };
+            this.thread.set(thread);
+            thread.start();
         }
     }
 
     protected BufferedImage createImage(int speed) {
-        String speedString = (speed) + "";
-
-        Color fg = Color.WHITE;
-        Color bg = Color.GREEN.darker();
-
-        Font font = new Font("Arial", Font.BOLD, 24);
-        Canvas c = new Canvas();
-        FontMetrics fm = c.getFontMetrics(font);
+        final String speedString = Integer.toString(speed);
+        final Color fg = Color.WHITE;
+        final Color bg = Color.GREEN.darker();
+        final Font font = new Font("Arial", Font.BOLD, 24);
+        final Canvas c = new Canvas();
+        final FontMetrics fm = c.getFontMetrics(font);
         int stringwidth = Math.max(fm.getAscent(), fm.stringWidth(speedString)) + 6;
-
-        int w;
-        // GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        // GraphicsDevice gd = ge.getDefaultScreenDevice();
-
-        int h;
-        // GraphicsConfiguration gc = gd.getDefaultConfiguration();
-        // final BufferedImage image = gc.createCompatibleImage(w, h, Transparency.BITMASK);
-        final BufferedImage image = new BufferedImage(w = stringwidth, h = stringwidth, Transparency.TRANSLUCENT);
+        final int w = stringwidth;
+        final int h = stringwidth;
+        final BufferedImage image = new BufferedImage(w, h, Transparency.TRANSLUCENT);
         final Graphics2D g = image.createGraphics();
-
-        // paint
-        // Graphics2D g = image.createGraphics();
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setFont(font);
-        RoundRectangle2D roundedRectangle = new RoundRectangle2D.Float(0, 0, w - 1, h - 1, 16, 16);
         g.setColor(bg);
         g.fillOval(0, 0, w, h);
         g.setColor(Color.WHITE);
         g.setStroke(new BasicStroke(2));
         g.drawOval(0, 0, w - 1, h - 1);
         g.setColor(fg);
-        Rectangle2D bounds = g.getFontMetrics().getStringBounds(speedString, g);
+        final Rectangle2D bounds = g.getFontMetrics().getStringBounds(speedString, g);
         g.drawString(speedString, (int) (w - bounds.getWidth()) / 2, (int) (-bounds.getY() + (h - bounds.getHeight()) / 2));
-
         g.dispose();
         return image;
     }
 
     private void stopDockUpdater() {
-        synchronized (LOCK) {
-
-            if (queueHandler != null) {
-
-                queueHandler = null;
-            }
-
-        }
+        this.thread.set(null);
     }
 
     private void reset() {
