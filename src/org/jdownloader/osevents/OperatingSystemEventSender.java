4b29555f01260b2f821634b34b53ab94f6738998
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-26 16:43:00 +0000

    SignalEventSource: -added HUP support -added setIgnore(Signal,boolean) -synch access to hashset/map
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33053 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9a8ec4afb2662d42b33bead9a928ad377728e4ff

--- src/org/jdownloader/osevents/OperatingSystemEventSender.java
+++ src/org/jdownloader/osevents/OperatingSystemEventSender.java
@@ -2,7 +2,6 @@ package org.jdownloader.osevents;
 
 import org.appwork.shutdown.ShutdownController;
 import org.appwork.utils.event.Eventsender;
-import org.jdownloader.logging.LogController;
 import org.jdownloader.osevents.multios.SignalEventSource;
 import org.jdownloader.updatev2.ForcedShutdown;
 
@@ -11,23 +10,45 @@ public class OperatingSystemEventSender extends Eventsender<OperatingSystemListe
 
     /**
      * get the only existing instance of OperatingSystemEventSender. This is a singleton
-     * 
+     *
      * @return
      */
     public static OperatingSystemEventSender getInstance() {
         return OperatingSystemEventSender.INSTANCE;
     }
 
+    private final SignalEventSource signalEventSource;
+
     /**
      * Create a new instance of OperatingSystemEventSender. This is a singleton class. Access the only existing instance by using
      * {@link #getInstance()}.
      */
     private OperatingSystemEventSender() {
+        SignalEventSource signalEventSource = null;
+        try {
+            signalEventSource = new SignalEventSource() {
+
+                @Override
+                public void onSignal(String name, int number) {
+                    if ("HUP".equals(name)) {
+                        fireEvent(new OperatingSystemEvent(OperatingSystemEvent.Type.SIGNAL_HUP, name, number));
+                        onOperatingSystemTerm();
+                    } else if ("TERM".equals(name) || "INT".equals(name)) {
+                        fireEvent(new OperatingSystemEvent(OperatingSystemEvent.Type.SIGNAL_TERM, name, number));
+                        onOperatingSystemTerm();
+                    }
+                }
+            };
+        } catch (final Throwable e) {
+            org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().log(e);
+        }
+        this.signalEventSource = signalEventSource;
     }
 
     @Override
     protected void fireEvent(OperatingSystemListener listener, OperatingSystemEvent event) {
         switch (event.getType()) {
+        case SIGNAL_HUP:
         case SIGNAL_TERM:
             listener.onOperatingSystemTerm();
             break;
@@ -36,21 +57,11 @@ public class OperatingSystemEventSender extends Eventsender<OperatingSystemListe
         }
     }
 
-    public void init() {
-        try {
-            new SignalEventSource() {
-
-                @Override
-                public void onSignal(String name, int number) {
-                    if ("TERM".equals(name) || "INT".equals(name)) {
-                        fireEvent(new OperatingSystemEvent(OperatingSystemEvent.Type.SIGNAL_TERM, name, number));
-                        onOperatingSystemTerm();
-                    }
-                }
-            }.init();
-        } catch (final Throwable e) {
-             org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().log(e);
+    public boolean setIgnoreSignal(final String signal, boolean ignore) {
+        if (signalEventSource != null) {
+            return signalEventSource.setIgnore(signal, ignore);
         }
+        return false;
     }
 
     @Override
