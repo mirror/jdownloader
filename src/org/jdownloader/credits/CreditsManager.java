e7109baae0df73ef07490f2c22bac1ad9f55365a
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-29 16:57:56 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33106 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: a13f28dc6553523ff38d31a6995a0a09086926b1

--- src/org/jdownloader/credits/CreditsManager.java
+++ src/org/jdownloader/credits/CreditsManager.java
@@ -3,23 +3,21 @@ package org.jdownloader.credits;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jdownloader.gui.translate._GUI;
-
 public class CreditsManager {
     private ArrayList<Credit> list;
 
     private CreditsManager() {
         list = new ArrayList<Credit>();
-        add(new Credit("RTMPDump", _GUI.T.credits_rtmpdump_description(), "http://rtmpdump.mplayerhq.hu", CouplingType.EXECUTABLE, License.GPL_V2));
-        add(new Credit("Cling", _GUI.T.credits_cling_description(), "http://4thline.org/projects/cling/", CouplingType.LIBRARY, License.LGPL_V2_OR_LATER, License.CDDL));
-        add(new Credit("7zipJBindings", _GUI.T.credits_7zipjbindings_description(), "http://sevenzipjbind.sourceforge.net/", CouplingType.LIBRARY, License.LGPL_V2_OR_LATER, new License("unRAR restriction", null, "licenses/cling.license")));
-        add(new Credit("Synthetica", _GUI.T.credits_synthetica_description(), "http://www.javasoft.de/synthetica/", CouplingType.INTERFACE, new License("Paid License", "Licensee AppWork GmbH; LRN (#112044); Enterprise License", "licenses/synthetica.license")));
-        add(new Credit("Icons8 IconSet", _GUI.T.credits_icons8_description(), "http://www.icons8.com", CouplingType.ICONS, License.CC_BY_ND_3, new License("Licensee AppWork GmbH", "Licensee AppWork GmbH; Captain Vector Plan", "licenses/Icons8_Paid_License.pdf")));
-        add(new Credit("Tango IconSet", _GUI.T.credits_tango_description(), "http://tango.freedesktop.org/Tango_Icon_Library", CouplingType.ICONS, License.PUBLIC_DOMAIN));
-        add(new Credit("FatCow IconSet", _GUI.T.credits_fatcow_description(), "http://www.fatcow.com/free-icons", CouplingType.ICONS, License.CC_BY_US_3));
-        add(new Credit("Picol IconSet", _GUI.T.credits_picol_description(), "http://www.picol.org/icon_library.php", CouplingType.ICONS, License.CC_BY_SA_3));
-        add(new Credit("Aha Soft Application IconSet", _GUI.T.credits_aha_description(), "http://www.small-icons.com/packs/24x24-free-application-icons.htm", CouplingType.ICONS, License.CC_BY_SA_3));
-        add(new Credit("Oxygen Icon Set", _GUI.T.credits_oxygen_description(), "http://www.oxygen-icons.org/", CouplingType.ICONS, License.LGPL_V3));
+        add(new Credit("RTMPDump", "© 2009 Andrej Stepanchuk;Copyright (C) 2010-2011 Howard Chu", "Protocol plugin for RTMP streams", "http://rtmpdump.mplayerhq.hu", CouplingType.EXECUTABLE, License.GPL_V2));
+        add(new Credit("Cling", "© 2015, 4th Line GmbH, Switzerland", "Universal Plug and Play library", "http://4thline.org/projects/cling/", CouplingType.LIBRARY, License.LGPL_V2_OR_LATER, License.CDDL));
+        add(new Credit("7zipJBindings", "Boris Brodski", "A java wrapper for 7-Zip C++ library", "http://sevenzipjbind.sourceforge.net/", CouplingType.LIBRARY, License.LGPL_V2_OR_LATER, new License("unRAR restriction", null, "licenses/cling.license")));
+        add(new Credit("Synthetica", "Jyloo Software GmbH", "Look and Feel library", "http://www.javasoft.de/synthetica/", CouplingType.INTERFACE, new License("Paid License", "Licensee AppWork GmbH; LRN (#112044); Enterprise License", "licenses/synthetica.license")));
+        add(new Credit("Icons8 IconSet", "© 2016 VisualPharm LLC", "Several Icons and Images", "http://www.icons8.com", CouplingType.ICONS, License.CC_BY_ND_3, new License("Licensee AppWork GmbH", "Licensee AppWork GmbH; Captain Vector Plan", "licenses/Icons8_Paid_License.pdf")));
+        add(new Credit("Tango IconSet", "freedesktop.org", "Several Icons and Images", "http://tango.freedesktop.org/Tango_Icon_Library", CouplingType.ICONS, License.PUBLIC_DOMAIN));
+        add(new Credit("Picol IconSet", "Melih Bilgil", "Several Icons and Images", "http://www.picol.org/icon_library.php", CouplingType.ICONS, License.CC_BY_SA_3));
+        add(new Credit("Aha Soft Application IconSet", "© 2005-2016 Aha-Soft", "Several Icons and Images", "http://www.small-icons.com/packs/24x24-free-application-icons.htm", CouplingType.ICONS, License.CC_BY_SA_3));
+        add(new Credit("Java D-Bus", "Matthew Johnson & Team", "Desktop-Bus IPC Framework", "https://www.freedesktop.org/wiki/Software/DBusBindings/", CouplingType.LIBRARY, License.AFL_V2_1, License.LGPL_V2));
+        add(new Credit("Base64", "© 2004, Mikael Grev, MiG InfoCom AB", "Base64 En/Decoder", "base64@miginfocom.com", CouplingType.LIBRARY, new License("BSD", "BSD License", "licenses/Base64.license")));
 
     }
 
