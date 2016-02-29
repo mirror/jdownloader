09e71ca73b21314945c8d75bbb87d8238b10df36
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-29 15:55:14 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33105 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/reconnect/pluginsinc/speedporthybrid/SpeedPortHybrid.java
+++ src/jd/controlling/reconnect/pluginsinc/speedporthybrid/SpeedPortHybrid.java
@@ -1,6 +1,7 @@
 package jd.controlling.reconnect.pluginsinc.speedporthybrid;
 
 import java.awt.Component;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -20,10 +21,12 @@ import javax.swing.JPanel;
 import org.appwork.storage.config.JsonConfig;
 import org.appwork.swing.components.ExtPasswordField;
 import org.appwork.swing.components.ExtTextField;
+import org.appwork.utils.Application;
 import org.appwork.utils.Hash;
 import org.appwork.utils.Regex;
 import org.appwork.utils.StringUtils;
 import org.appwork.utils.formatter.HexFormatter;
+import org.appwork.utils.logging2.LoggerInitUtils;
 import org.appwork.utils.logging2.extmanager.Log;
 import org.appwork.utils.net.httpconnection.HTTPProxy;
 import org.appwork.utils.net.httpconnection.HTTPProxy.TYPE;
@@ -50,7 +53,7 @@ import net.miginfocom.swing.MigLayout;
 /**
  * Plugin to use an extern tool for reconnection
  */
-public class SpeedPortHybrid extends RouterPlugin {
+public class SpeedPortHybrid extends RouterPlugin implements IPCheckProvider {
 
     public static final String             ID = "SpeedPortHybrid";
 
@@ -72,6 +75,8 @@ public class SpeedPortHybrid extends RouterPlugin {
 
     private String                         challengev;
 
+    private String                         onlineStatus;
+
     private String PBKDF2Key(String password, String salt) throws Exception {
 
         final PBEKeySpec spec = new PBEKeySpec(Hash.getSHA256(password).toCharArray(), salt.getBytes("UTF-8"), 1000, 16 * 8);
@@ -81,60 +86,33 @@ public class SpeedPortHybrid extends RouterPlugin {
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, IllegalStateException, InvalidCipherTextException {
-        // // Encrypt
-        // var challengev = getCookie('challengev');
-        // var iv = challengev.substr(16, 16);
-        // var adata = challengev.substr(32, 16);
-        //
-        //
-        // var derivedk = getCookie("derivedk");
-        // var c = new sjcl.cipher.aes(sjcl.codec.hex.toBits(derivedk));
-        //
-        //
-        // var pt = $.param(data);
-        // var ct = sjcl.mode.ccm.encrypt(c, sjcl.codec.utf8String.toBits(pt), sjcl.codec.hex.toBits(iv), sjcl.codec.hex.toBits(adata));
-        // ct = sjcl.codec.hex.fromBits(ct);
-
-        String challengeev = "c5D5f37d0EC075dd1793D1eeB01fEc05eA2e4a66BF78908acDDA";
-        byte[] iv = HexFormatter.hexToByteArray(challengeev.substring(16, 32));
-        byte[] adata = HexFormatter.hexToByteArray(challengeev.substring(32, 32 + 16));
-        byte[] derivedk = HexFormatter.hexToByteArray("b5b7e46f30820543f448eece4d20da1a");
-        String pt = "req_connect=disabled&csrf_token=5TvISVht8aUo%2BZXeJv3qjXi818GAWpv";
-
-        CCMBlockCipher chipher = new CCMBlockCipher(new AESFastEngine());
-        AEADParameters params = new AEADParameters(new KeyParameter(derivedk), 64, iv);
-        chipher.init(true, params);
-        byte[] enc = pt.getBytes("UTF-8");
-        byte[] tmp = new byte[enc.length + adata.length];
-        chipher.processAADBytes(adata, 0, adata.length);
-        int len = chipher.processBytes(enc, 0, enc.length, tmp, 0);
-        len += chipher.doFinal(tmp, len);
-        String result = HexFormatter.byteArrayToHex(tmp);
-        System.out.println("77e8cfe400a2e4896ee25d2f3177a474b43e67d55268662ffa8ec2c5225934f8a6df9b11641f0c0a8aec76509a7409c6fff6ff6369a60ae302eec9814f66de92dad0bc5eec71b40f8f".equals(result));
+        LoggerInitUtils.disableFileOut();
+        LoggerInitUtils.disableConsoleOut();
+        Application.setApplication(".appwork");
+        encryptTest();
 
         decrpyttest();
     }
 
-    private static void decrpyttest() throws IllegalStateException, InvalidCipherTextException {
-        // decrypt
-        String challengeev = "CD39f2B7e910aFe9FFA1A2F18DFFD28fB373dbAaaFa444DaBA3A";
-        String ivs;
-        byte[] iv = HexFormatter.hexToByteArray(ivs = challengeev.substring(16, 16 + 16));
-        String ads;
-        byte[] adata = HexFormatter.hexToByteArray(ads = challengeev.substring(32, 32 + 16));
-        byte[] derivedk = HexFormatter.hexToByteArray("595578fe25bca7f227bb2c84f3293fb6");
-        byte[] data = HexFormatter.hexToByteArray(
-                "47E742A51B14BA6A7DE0772B12C5ED2CBA6C82E3A68FE1B77777D7A2B1437592051DE2D1E53F3A79E8F00667AAB8C6E1CDF2242127C9D4210735997247DC6009FEA4D87F79161C69309585B80CEDED46B80B57A0DCF27036B3F38982AB1D4B8052841598741E494B412FF50098A22161CF3AA3E2BC56E49CDC7135D1E9276DB299EE9BE9BDE205C777FDA6C7EAF7652C6FDADA55892AA088FB017313AA6F1A9707530826389B41F5F50ED92D767423C1A7DDD88434FF372E20FF49B755359359B570CD0753FCB0CEA1B496308B9D49A442BF08A7949BE75B1AB188552B8FC46ADFDDBED4A9D0F9F12902CE9BD9BADA6448654296D65DB691562370BB979F3E844E2F1F136A4AA0CE9924CED5B9C8E25EFB85FE2ED41D275DE6418BDB747D8256AEE3642073958387B6EFB038E9EE994A58085560DE4353DF9C9406A89739CCF58462A96E627BF73C9AED3776873A2D970EF938B1CFDC9195F0C1FC8EE033E47351A6D543F0CFE89F9AD8211BA45811F03CFAB932DEA540C7C06DC92EE7BE317A7647A35904151F4B5DD86FB10F6822E0B696A823BB1B91E9AC201E78C5E2CEB35FF31AB1B8E0C0B0459E5DA85E9DB79EF4E5CD77640111249497BE11A7F2AAA410F053D0F2CEBBBA2FBF31512EE7195D081919FDBC260436A9E7241E40D41589A25FB91F214C36DC00DFF14EE012BA0245");
-        AEADParameters params = new AEADParameters(new KeyParameter(derivedk), 64, iv);
-        CCMBlockCipher dc = new CCMBlockCipher(new AESFastEngine());
-        dc.init(false, params);
+    private static void encryptTest() throws UnsupportedEncodingException, InvalidCipherTextException {
 
-        byte[] enc = data;
-        byte[] tmp = new byte[enc.length + adata.length];
-        dc.processAADBytes(adata, 0, adata.length);
-        int len = dc.processBytes(enc, 0, enc.length, tmp, 0);
-        len += dc.doFinal(tmp, len);
-        System.out.println("Decrypted: " + new String(tmp, 0, len));
+        SpeedPortHybrid inst = new SpeedPortHybrid();
+        inst.challengev = "c5D5f37d0EC075dd1793D1eeB01fEc05eA2e4a66BF78908acDDA";
+        inst.derivedk = "b5b7e46f30820543f448eece4d20da1a";
+        String result = inst.encrypt("req_connect=disabled&csrf_token=5TvISVht8aUo%2BZXeJv3qjXi818GAWpv");
+
+        System.out.println("ENcrypting Works: " + "77e8cfe400a2e4896ee25d2f3177a474b43e67d55268662ffa8ec2c5225934f8a6df9b11641f0c0a8aec76509a7409c6fff6ff6369a60ae302eec9814f66de92dad0bc5eec71b40f8f".equals(result));
+    }
+
+    private static void decrpyttest() throws IllegalStateException, InvalidCipherTextException, UnsupportedEncodingException {
+        // decrypt
+        SpeedPortHybrid inst = new SpeedPortHybrid();
+        inst.challengev = "CD39f2B7e910aFe9FFA1A2F18DFFD28fB373dbAaaFa444DaBA3A";
+        inst.derivedk = "595578fe25bca7f227bb2c84f3293fb6";
+        String result = inst.decrypt(
+                "47E742A51B14BA6A7DE0772B12C5ED2CBA6C82E3A68FE1B77777D7A2B1437592051DE2D1E53F3A79E8F00667AAB8C6E1CDF2242127C9D4210735997247DC6009FEA4D87F79161C69309585B80CEDED46B80B57A0DCF27036B3F38982AB1D4B8052841598741E494B412FF50098A22161CF3AA3E2BC56E49CDC7135D1E9276DB299EE9BE9BDE205C777FDA6C7EAF7652C6FDADA55892AA088FB017313AA6F1A9707530826389B41F5F50ED92D767423C1A7DDD88434FF372E20FF49B755359359B570CD0753FCB0CEA1B496308B9D49A442BF08A7949BE75B1AB188552B8FC46ADFDDBED4A9D0F9F12902CE9BD9BADA6448654296D65DB691562370BB979F3E844E2F1F136A4AA0CE9924CED5B9C8E25EFB85FE2ED41D275DE6418BDB747D8256AEE3642073958387B6EFB038E9EE994A58085560DE4353DF9C9406A89739CCF58462A96E627BF73C9AED3776873A2D970EF938B1CFDC9195F0C1FC8EE033E47351A6D543F0CFE89F9AD8211BA45811F03CFAB932DEA540C7C06DC92EE7BE317A7647A35904151F4B5DD86FB10F6822E0B696A823BB1B91E9AC201E78C5E2CEB35FF31AB1B8E0C0B0459E5DA85E9DB79EF4E5CD77640111249497BE11A7F2AAA410F053D0F2CEBBBA2FBF31512EE7195D081919FDBC260436A9E7241E40D41589A25FB91F214C36DC00DFF14EE012BA0245");
+
+        System.out.println("Decryption Works: " + (result.contains("onlinestatus") && result.length() == 489));
     }
 
     public String encrypt(String pt) throws UnsupportedEncodingException, IllegalStateException, InvalidCipherTextException {
@@ -169,6 +147,7 @@ public class SpeedPortHybrid extends RouterPlugin {
         int len = dc.processBytes(enc, 0, enc.length, tmp, 0);
         len += dc.doFinal(tmp, len);
         String ret = new String(tmp, 0, len, "UTF-8");
+        Log.info(ret);
         updateCSRF(ret);
         return ret;
     }
@@ -177,40 +156,39 @@ public class SpeedPortHybrid extends RouterPlugin {
         return "_time=" + System.currentTimeMillis() + "&_rand=" + ((int) (Math.random() * 1000));
     }
 
+    @Override
+    public int getIpCheckInterval() {
+        return 1000;
+    }
+
+    @Override
+    public IP getExternalIP() throws IPCheckException {
+        synchronized (SpeedPortHybrid.this) {
+            try {
+
+                ensureSession();
+
+                String crypted = decrypt(br.getPage("http://" + config.getRouterIP() + "/data/INetIP.json?" + getTimeParams() + "&" + getTimeParams()));
+
+                Log.info("Decrypted IP: " + crypted);
+                onlineStatus = new Regex(crypted, "\"varid\"\\s*:\\s*\"onlinestatus\",\\s*\"varvalue\"\\s*:\\s*\"([^\"]+)").getMatch(0);
+                String externalIP = new Regex(crypted, "\"varid\"\\s*:\\s*\"public_ip_v4\",\\s*\"varvalue\"\\s*:\\s*\"([^\"]+)").getMatch(0);
+                if (externalIP != null) {
+                    return IP.getInstance(externalIP);
+                }
+            } catch (Throwable e) {
+                Log.log(e);
+
+            }
+            throw new InvalidProviderException("Unknown UPNP Response Error");
+        }
+    }
+
     public SpeedPortHybrid() {
         super();
         config = JsonConfig.create(SpeedPortHybridReconnectConfig.class);
         icon = new AbstractIcon(IconKey.ICON_RECONNECT, 16);
-        setIPCheckProvider(new IPCheckProvider() {
-
-            @Override
-            public int getIpCheckInterval() {
-                return 1000;
-            }
-
-            @Override
-            public IP getExternalIP() throws IPCheckException {
-                synchronized (SpeedPortHybrid.this) {
-                    try {
-
-                        ensureSession();
-
-                        String crypted = decrypt(br.getPage("http://" + config.getRouterIP() + "/data/INetIP.json?" + getTimeParams() + "&" + getTimeParams()));
-
-                        Log.info("Decrypted IP: " + crypted);
-
-                        String externalIP = new Regex(crypted, "\"varid\"\\s*:\\s*\"public_ip_v4\",\\s*\"varvalue\"\\s*:\\s*\"([^\"]+)").getMatch(0);
-                        if (externalIP != null) {
-                            return IP.getInstance(externalIP);
-                        }
-                    } catch (Throwable e) {
-                        Log.log(e);
-
-                    }
-                    throw new InvalidProviderException("Unknown UPNP Response Error");
-                }
-            }
-        });
+        setIPCheckProvider(this);
         invoker = new ReconnectInvoker(this) {
             private String derivedk;
             private String challengev;
@@ -228,43 +206,46 @@ public class SpeedPortHybrid extends RouterPlugin {
 
                         ensureSession();
 
-                        String crypted = decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/Connect.json?lang=de", encrypt("req_connect=disabled&csrf_token=" + csrf)));
-                        updateCSRF(crypted);
-                        // req_connect=online
-                        // req_connect=disabled
-                        // lte_reconn=1
-                        Log.info("Decrypted: " + new String(crypted));
-
-                        crypted = decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/modules.json?lang=de", encrypt("lte_reconn=1&csrf_token=" + csrf)));
-
-                        // req_connect=online
-                        // req_connect=disabled
-                        // lte_reconn=1
-                        Log.info("Decrypted: " + new String(crypted));
-
-                        for (int i = 0; i < 10; i++) {
-                            Thread.sleep(3000);
-                            String dec = decrypt(br.getPage("http://192.168.2.1/data/Connect.json?_time=" + System.currentTimeMillis() + "&_rand=" + ((int) (Math.random() * 1000))));
-                            updateCSRF(crypted);
-                            Log.info(dec);
+                        // http://192.168.2.1/html/content/internet/connection.html?lang=de
+                        Log.info("CurrentStatus " + onlineStatus);
+                        boolean changeConnectionOnline = false;
+                        boolean reconnectConnection_Lte = false;
+                        if (StringUtils.isEmpty(onlineStatus) || "disabled".equalsIgnoreCase(onlineStatus)) {
+                            changeConnectionOnline = true;
+                        } else if ("online".equalsIgnoreCase(onlineStatus)) {
+                            reconnectConnection_Lte = true;
                         }
 
-                        crypted = decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/Connect.json?lang=de", encrypt("lte_reconn=online&csrf_token=" + csrf)));
+                        if (changeConnectionOnline) {
+                            decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/Connect.json?lang=de", encrypt("lte_reconn=online&csrf_token=" + csrf)));
 
-                        // req_connect=online
-                        // req_connect=disabled
-                        // lte_reconn=1
-                        Log.info("Decrypted: " + new String(crypted));
-                        /*
-                         * var challengev = getCookie('challengev'); var iv = challengev.substr(16, 16); var adata = challengev.substr(32,
-                         * 16);
-                         *
-                         * var derivedk = getCookie("derivedk"); var c = new sjcl.cipher.aes(sjcl.codec.hex.toBits(derivedk));
-                         *
-                         * var pt = sjcl.mode.ccm.decrypt(c, sjcl.codec.hex.toBits(data), sjcl.codec.hex.toBits(iv),
-                         * sjcl.codec.hex.toBits(adata)); pt = sjcl.codec.utf8String.fromBits(pt); return pt;
-                         */
+                        } else {
+                            decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/Connect.json?lang=de", encrypt("req_connect=disabled&csrf_token=" + csrf)));
 
+                            // req_connect=online
+                            // req_connect=disabled
+                            // lte_reconn=1
+
+                            decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/modules.json?lang=de", encrypt("lte_reconn=1&csrf_token=" + csrf)));
+
+                            for (int i = 0; i < 10; i++) {
+                                Thread.sleep(3000);
+                                decrypt(br.getPage("http://192.168.2.1/data/Connect.json?_time=" + System.currentTimeMillis() + "&_rand=" + ((int) (Math.random() * 1000))));
+
+                            }
+
+                            decrypt(br.postPageRaw("http://" + config.getRouterIP() + "/data/Connect.json?lang=de", encrypt("lte_reconn=online&csrf_token=" + csrf)));
+
+                            /*
+                             * var challengev = getCookie('challengev'); var iv = challengev.substr(16, 16); var adata =
+                             * challengev.substr(32, 16);
+                             *
+                             * var derivedk = getCookie("derivedk"); var c = new sjcl.cipher.aes(sjcl.codec.hex.toBits(derivedk));
+                             *
+                             * var pt = sjcl.mode.ccm.decrypt(c, sjcl.codec.hex.toBits(data), sjcl.codec.hex.toBits(iv),
+                             * sjcl.codec.hex.toBits(adata)); pt = sjcl.codec.utf8String.fromBits(pt); return pt;
+                             */
+                        }
                     } catch (Throwable e) {
                         throw new ReconnectException(e);
                     }
@@ -277,7 +258,10 @@ public class SpeedPortHybrid extends RouterPlugin {
     private void updateCSRF(String crypted) {
         String newCsrf = new Regex(crypted, "\"vartype\"\\s*:\\s*\"value\"\\s*,\\s*\"varid\"\\s*:\\s*\"csrf_token\"\\s*,\\s*\"varvalue\"\\s*:\\s*\"([^\"]+)").getMatch(0);
         if (StringUtils.isNotEmpty(newCsrf)) {
+            Log.info("New CSRF: " + newCsrf);
             csrf = newCsrf;
+        } else {
+            Log.info("No new CSRF");
         }
     }
 
@@ -294,8 +278,19 @@ public class SpeedPortHybrid extends RouterPlugin {
         br.postPage("http://" + config.getRouterIP() + "/data/Login.json?lang=de", new QueryInfo().append("csrf_token", "nulltoken", true).append("showpw", "0", true).append("password", Hash.getSHA256(challengev + ":" + config.getPassword()), true));
 
         br.setCookie("http://" + config.getRouterIP(), "derivedk", derivedk = PBKDF2Key(config.getPassword(), challengev.substring(0, 16)));
-        br.getPage("http://" + config.getRouterIP() + "/html/content/internet/connection.html?lang=de");
-        csrf = br.getRegex("csrf_token\\s*=\\s*\"([^\"]+)").getMatch(0);
+        getPage("/html/content/internet/connection.html?lang=de");
+
+    }
+
+    private void getPage(String string) throws IOException {
+        br.getPage("http://" + config.getRouterIP() + string);
+        String newcsrf = br.getRegex("csrf_token\\s*=\\s*\"([^\"]+)").getMatch(0);
+        if (StringUtils.isNotEmpty(newcsrf)) {
+            Log.info("New CSRF: " + newcsrf);
+            csrf = newcsrf;
+        } else {
+            Log.info("No new CSRF");
+        }
     }
 
     @Override
