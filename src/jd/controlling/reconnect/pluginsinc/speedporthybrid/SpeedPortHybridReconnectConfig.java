e88d05f0c928af980c8d495a314754edefef4ceb
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-02-29 10:42:12 +0000

    build>full testbuild Speedport Hybrid PLugin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33092 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/reconnect/pluginsinc/speedporthybrid/SpeedPortHybridReconnectConfig.java
+++ src/jd/controlling/reconnect/pluginsinc/speedporthybrid/SpeedPortHybridReconnectConfig.java
@@ -3,8 +3,9 @@ package jd.controlling.reconnect.pluginsinc.speedporthybrid;
 import org.appwork.storage.config.ConfigInterface;
 import org.appwork.storage.config.annotations.CryptedStorage;
 
+@CryptedStorage(key = { 1, 6, 4, 5, 2, 7, 4, 3, 12, 61, 14, 75, -2, -7, -44, 33 })
 public interface SpeedPortHybridReconnectConfig extends ConfigInterface {
-    @CryptedStorage(key = { 1, 6, 4, 5, 2, 7, 4, 3, 12, 61, 14, 75, -2, -7, -44, 33 })
+
     public String getPassword();
 
     public void setPassword(String pw);
