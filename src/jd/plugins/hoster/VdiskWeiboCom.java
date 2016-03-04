32fef2c0ef59313946e16d0062936d852b0900c0
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-03-04 18:09:47 +0000

    -added support for nexusmodscom closes #77781 -added support for vdiskweibocom closes #77767
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33184 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d994afd61d44ca01d9f2b2a3e8cb23bdd2103e38

---
+++ src/jd/plugins/hoster/VdiskWeiboCom.java
@@ -0,0 +1,178 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Random;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+import org.appwork.utils.formatter.SizeFormatter;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "vdisk.weibo.com" }, urls = { "http://(?:www\\.)?vdisk\\.weibo\\.com/s/[A-Za-z0-9]+" }, flags = { 0 })
+public class VdiskWeiboCom extends PluginForHost {
+
+    public VdiskWeiboCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "http://vdisk.weibo.com/useHelpTip?log_target=navigation_vdisk";
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME       = true;
+    private static final int     FREE_MAXCHUNKS    = 0;
+    private static final int     FREE_MAXDOWNLOADS = 20;
+
+    private String               dllink            = null;
+
+    // private static final boolean ACCOUNT_FREE_RESUME = true;
+    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
+    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
+    //
+    // /* don't touch the following! */
+    // private static AtomicInteger maxPrem = new AtomicInteger(1);
+
+    @SuppressWarnings({ "deprecation", "unchecked", "rawtypes" })
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        dllink = null;
+        this.setBrowserExclusive();
+        final String fid = new Regex(link.getDownloadURL(), "([A-Za-z0-9]+)$").getMatch(0);
+        link.setLinkID(fid);
+        br.getPage(link.getDownloadURL());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        String md5 = null, sha1 = null;
+        final String json = this.br.getRegex("fileDown\\.init\\((.*?)\\);").getMatch(0);
+        String filename = br.getRegex("id=\"page_down_filename\" value=\"([^<>\"]*?)\"").getMatch(0);
+        String filesize = br.getRegex("class=\"btn_vdisk_size\">([^<>\"]*?)<").getMatch(0);
+        if (filesize != null) {
+            /* Fix html filesize */
+            filesize += "b";
+        }
+        if (json != null) {
+            try {
+                final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) jd.plugins.hoster.DummyScriptEnginePlugin.jsonToJavaObject(json);
+                final ArrayList<Object> ressourcelist = (ArrayList) entries.get("download_list");
+                final int listsize = ressourcelist.size();
+                if (ressourcelist != null && listsize > 0) {
+                    /* Randomply chose downloadlink / mirror */
+                    final int random = new Random().nextInt(listsize - 1);
+                    this.dllink = (String) ressourcelist.get(random);
+                }
+                md5 = (String) entries.get("md5");
+                sha1 = (String) entries.get("sha1");
+                if (filesize == null) {
+                    filesize = (String) entries.get("size");
+                }
+                if (filename == null) {
+                    filename = (String) entries.get("name");
+                }
+            } catch (final Throwable e) {
+            }
+        }
+        if (filename == null) {
+            filename = fid;
+        }
+        filename = Encoding.htmlDecode(filename).trim();
+        link.setName(filename);
+        if (filesize != null) {
+            link.setDownloadSize(SizeFormatter.getSize(filesize));
+        }
+        if (md5 != null) {
+            link.setMD5Hash(md5);
+        }
+        if (sha1 != null) {
+            link.setSha1Hash(sha1);
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        doFree(downloadLink, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink downloadLink, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        if (dllink == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, resumable, maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        downloadLink.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
+        dl.startDownload();
+    }
+
+    // private String checkDirectLink(final DownloadLink downloadLink, final String property) {
+    // String dllink = downloadLink.getStringProperty(property);
+    // if (dllink != null) {
+    // URLConnectionAdapter con = null;
+    // try {
+    // final Browser br2 = br.cloneBrowser();
+    // br2.setFollowRedirects(true);
+    // con = br2.openHeadConnection(dllink);
+    // if (con.getContentType().contains("html") || con.getLongContentLength() == -1) {
+    // downloadLink.setProperty(property, Property.NULL);
+    // dllink = null;
+    // }
+    // } catch (final Exception e) {
+    // downloadLink.setProperty(property, Property.NULL);
+    // dllink = null;
+    // } finally {
+    // try {
+    // con.disconnect();
+    // } catch (final Throwable e) {
+    // }
+    // }
+    // }
+    // return dllink;
+    // }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
