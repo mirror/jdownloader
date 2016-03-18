85440143bd2b7514d428326874472e3289f83bb3
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-03-18 16:29:11 +0000

    Vidzi.tv - Workaround fixes #78047
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33318 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2d32afece857a46a6f07e8ba4075f97b6b06df15

--- src/jd/plugins/hoster/VidziTv.java
+++ src/jd/plugins/hoster/VidziTv.java
@@ -42,13 +42,13 @@ import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
 
 import org.appwork.utils.formatter.SizeFormatter;
 import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "vidzi.tv" }, urls = { "https?://(www\\.)?vidzidecrypted\\.tv/((vid)?embed\\-)?[a-z0-9]{12}" }, flags = { 0 })
 public class VidziTv extends PluginForHost {
@@ -133,6 +133,9 @@ public class VidziTv extends PluginForHost {
         prepBrowser(br);
         setFUID(link);
         getPage(link.getDownloadURL());
+        if (br.getURL().equals("http://vidzi.tv")) {
+            getPage(link.getDownloadURL() + ".html"); // Needed by http://vidzi.tv/y2gcydvbo4dl.html
+        }
         if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:|The file was deleted by administration)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
@@ -488,13 +491,13 @@ public class VidziTv extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -632,7 +635,7 @@ public class VidziTv extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
@@ -658,7 +661,7 @@ public class VidziTv extends PluginForHost {
 
     /**
      * Validates string to series of conditions, null, whitespace, or "". This saves effort factor within if/for/while statements
-     *
+     * 
      * @param s
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
@@ -675,7 +678,7 @@ public class VidziTv extends PluginForHost {
     /**
      * This fixes filenames from all xfs modules: file hoster, audio/video streaming (including transcoded video), or blocked link checking
      * which is based on fuid.
-     *
+     * 
      * @version 0.2
      * @author raztoki
      */
@@ -864,7 +867,7 @@ public class VidziTv extends PluginForHost {
     /**
      * Is intended to handle out of date errors which might occur seldom by re-tring a couple of times before throwing the out of date
      * error.
-     *
+     * 
      * @param dl
      *            : The DownloadLink
      * @param error
