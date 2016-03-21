f439a993826358fbaf5570dd46876a945c47f835
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-03-21 14:58:44 +0000

    Added: geetest captcha, requires browser solver. - refs #76374
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33332 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9277595d17dee14d6148a9c89bb1903738dadc44

---
+++ src/org/jdownloader/captcha/v2/challenge/geetest/AbstractCaptchaHelperGeeTest.java
@@ -0,0 +1,92 @@
+package org.jdownloader.captcha.v2.challenge.geetest;
+
+import org.appwork.utils.Regex;
+import org.appwork.utils.logging2.LogInterface;
+import org.jdownloader.logging.LogController;
+
+import jd.http.Browser;
+import jd.parser.html.Form;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+
+public abstract class AbstractCaptchaHelperGeeTest<T extends Plugin> {
+    protected T            plugin;
+    protected LogInterface logger;
+    protected Browser      br;
+    protected String       siteKey;
+
+    public AbstractCaptchaHelperGeeTest(T plugin, Browser br, String siteKey) {
+        this.plugin = plugin;
+        this.br = br;
+        logger = plugin.getLogger();
+        if (logger == null) {
+            logger = LogController.getInstance().getLogger(getClass().getSimpleName());
+        }
+        this.siteKey = siteKey;
+    }
+
+    public T getPlugin() {
+        return plugin;
+    }
+
+    /**
+     *
+     *
+     * @author raztoki
+     * @since JD2
+     * @return
+     */
+    protected String getGeeTestApiKey() {
+        return getGeeTestApiKey(br != null ? br.toString() : null);
+    }
+
+    /**
+     * will auto find api key, based on google default &lt;div&gt;, @Override to make customised finder.
+     *
+     * @author raztoki
+     * @since JD2
+     * @return
+     */
+    protected static String getGeeTestApiKey(final String source) {
+        if (source == null) {
+            return null;
+        }
+        // lets look for default
+        final String[] scripts = new Regex(source, "<script[^>]*>.*?</script>").getColumn(-1);
+        if (scripts != null) {
+            for (final String script : scripts) {
+                final String apiKey = new Regex(script, "src=('|\")https?://api\\.geetest\\.com/[^\\s]*(?:\\?|&)gt=([a-f0-9]{32})[^\\s]*\\1").getMatch(1);
+                if (apiKey != null) {
+                    return apiKey;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean containsGeeTest(final Browser br) throws PluginException {
+        if (br == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String geeTestApiKey = getGeeTestApiKey(br.toString());
+        return geeTestApiKey != null ? true : false;
+    }
+
+    public static boolean containsGeeTest(final Form form) throws PluginException {
+        if (form == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String geeTestApiKey = getGeeTestApiKey(form.getHtmlCode());
+        return geeTestApiKey != null ? true : false;
+    }
+
+    public static boolean containsGeeTest(final String string) throws PluginException {
+        if (string == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String geeTestApiKey = getGeeTestApiKey(string);
+        return geeTestApiKey != null ? true : false;
+    }
+
+}
---
+++ src/org/jdownloader/captcha/v2/challenge/geetest/CaptchaHelperHostPluginGeeTest.java
@@ -0,0 +1,192 @@
+package org.jdownloader.captcha.v2.challenge.geetest;
+
+import java.awt.Rectangle;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.logging2.LogSource;
+import org.appwork.utils.swing.dialog.Dialog;
+import org.jdownloader.captcha.blacklist.BlacklistEntry;
+import org.jdownloader.captcha.blacklist.BlockAllDownloadCaptchasEntry;
+import org.jdownloader.captcha.blacklist.BlockDownloadCaptchasByHost;
+import org.jdownloader.captcha.blacklist.BlockDownloadCaptchasByLink;
+import org.jdownloader.captcha.blacklist.BlockDownloadCaptchasByPackage;
+import org.jdownloader.captcha.blacklist.CaptchaBlackList;
+import org.jdownloader.captcha.v2.Challenge;
+import org.jdownloader.captcha.v2.ChallengeResponseController;
+import org.jdownloader.captcha.v2.ChallengeSolver;
+import org.jdownloader.captcha.v2.solver.browser.BrowserViewport;
+import org.jdownloader.captcha.v2.solver.browser.BrowserWindow;
+import org.jdownloader.captcha.v2.solverjob.SolverJob;
+import org.jdownloader.gui.helpdialogs.HelpDialog;
+import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.images.NewTheme;
+import org.jdownloader.plugins.CaptchaStepProgress;
+import org.jdownloader.settings.staticreferences.CFG_GUI;
+
+import jd.controlling.accountchecker.AccountCheckerThread;
+import jd.controlling.captcha.SkipException;
+import jd.controlling.captcha.SkipRequest;
+import jd.controlling.downloadcontroller.DownloadWatchDog;
+import jd.controlling.downloadcontroller.SingleDownloadController;
+import jd.controlling.linkcrawler.LinkCrawlerThread;
+import jd.http.Browser;
+import jd.plugins.CaptchaException;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+public class CaptchaHelperHostPluginGeeTest extends AbstractCaptchaHelperGeeTest<PluginForHost> {
+
+    public CaptchaHelperHostPluginGeeTest(PluginForHost plugin, Browser br, String siteKey) {
+        super(plugin, br, siteKey);
+
+    }
+
+    public CaptchaHelperHostPluginGeeTest(PluginForHost plugin, Browser br) {
+        this(plugin, br, null);
+
+    }
+
+    public String getToken() throws PluginException, InterruptedException {
+
+        if (Thread.currentThread() instanceof LinkCrawlerThread) {
+            logger.severe("PluginForHost.getCaptchaCode inside LinkCrawlerThread!?");
+        }
+        String apiKey = siteKey;
+        if (apiKey == null) {
+            apiKey = getGeeTestApiKey();
+            if (apiKey == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "GeeTest API Key can not be found");
+            }
+        }
+        final DownloadLink link = getPlugin().getDownloadLink();
+        final CaptchaStepProgress progress = new CaptchaStepProgress(0, 1, null);
+        progress.setProgressSource(this);
+        progress.setDisplayInProgressColumnEnabled(false);
+        try {
+            link.addPluginProgress(progress);
+            final boolean insideAccountChecker = Thread.currentThread() instanceof AccountCheckerThread;
+            final GeeTestChallenge c = new GeeTestChallenge(apiKey, getPlugin()) {
+
+                @Override
+                public boolean canBeSkippedBy(SkipRequest skipRequest, ChallengeSolver<?> solver, Challenge<?> challenge) {
+                    if (insideAccountChecker) {
+                        /* we don't want to skip login captcha inside fetchAccountInfo(Thread is AccountCheckerThread) */
+                        return false;
+                    }
+                    final Plugin challengePlugin = challenge.getPlugin();
+                    if (challengePlugin != null && !(challengePlugin instanceof PluginForHost)) {
+                        /* we only want block PluginForHost captcha here */
+                        return false;
+                    }
+                    switch (skipRequest) {
+                    case BLOCK_ALL_CAPTCHAS:
+                        /* user wants to block all captchas (current session) */
+                        return true;
+                    case BLOCK_HOSTER:
+                        /* user wants to block captchas from specific hoster */
+                        return StringUtils.equals(link.getHost(), challenge.getHost());
+                    case BLOCK_PACKAGE:
+                        /* user wants to block captchas from current FilePackage */
+                        final DownloadLink lLink = challenge.getDownloadLink();
+                        if (lLink == null || lLink.getDefaultPlugin() == null) {
+                            return false;
+                        }
+                        return link.getFilePackage() == lLink.getFilePackage();
+                    default:
+                        return false;
+                    }
+                }
+
+                @Override
+                public BrowserViewport getBrowserViewport(BrowserWindow screenResource, Rectangle elementBounds) {
+                    return null;
+                }
+
+            };
+            c.setTimeout(getPlugin().getCaptchaTimeout());
+            if (insideAccountChecker || FilePackage.isDefaultFilePackage(link.getFilePackage())) {
+                /**
+                 * account login -> do not use antiCaptcha services
+                 */
+                c.setAccountLogin(true);
+            } else {
+                final SingleDownloadController controller = link.getDownloadLinkController();
+                if (controller != null) {
+                    getPlugin().setHasCaptcha(link, controller.getAccount(), true);
+                }
+            }
+            getPlugin().invalidateLastChallengeResponse();
+            final BlacklistEntry blackListEntry = CaptchaBlackList.getInstance().matches(c);
+            if (blackListEntry != null) {
+                logger.warning("Cancel. Blacklist Matching");
+                throw new CaptchaException(blackListEntry);
+            }
+            final SolverJob<String> job = ChallengeResponseController.getInstance().handle(c);
+            if (!c.isSolved()) {
+                throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            }
+            if (!c.isCaptchaResponseValid()) {
+                throw new PluginException(LinkStatus.ERROR_CAPTCHA, "Captcha reponse value did not validate!");
+            }
+            return c.getResult().getValue();
+        } catch (InterruptedException e) {
+            LogSource.exception(logger, e);
+            throw e;
+        } catch (SkipException e) {
+            LogSource.exception(logger, e);
+            if (link != null) {
+                switch (e.getSkipRequest()) {
+                case BLOCK_ALL_CAPTCHAS:
+                    CaptchaBlackList.getInstance().add(new BlockAllDownloadCaptchasEntry());
+
+                    if (CFG_GUI.HELP_DIALOGS_ENABLED.isEnabled()) {
+                        HelpDialog.show(false, true, HelpDialog.getMouseLocation(), "SKIPPEDHOSTER", Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_title(), _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_msg(), NewTheme.I().getIcon("skipped", 32));
+                    }
+                    break;
+                case BLOCK_HOSTER:
+                    CaptchaBlackList.getInstance().add(new BlockDownloadCaptchasByHost(link.getHost()));
+                    if (CFG_GUI.HELP_DIALOGS_ENABLED.isEnabled()) {
+                        HelpDialog.show(false, true, HelpDialog.getMouseLocation(), "SKIPPEDHOSTER", Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_title(), _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_msg(), NewTheme.I().getIcon("skipped", 32));
+                    }
+                    break;
+
+                case BLOCK_PACKAGE:
+                    CaptchaBlackList.getInstance().add(new BlockDownloadCaptchasByPackage(link.getParentNode()));
+                    if (CFG_GUI.HELP_DIALOGS_ENABLED.isEnabled()) {
+                        HelpDialog.show(false, true, HelpDialog.getMouseLocation(), "SKIPPEDHOSTER", Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_title(), _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_msg(), NewTheme.I().getIcon("skipped", 32));
+                    }
+                    break;
+                case SINGLE:
+                    CaptchaBlackList.getInstance().add(new BlockDownloadCaptchasByLink(link));
+                    if (CFG_GUI.HELP_DIALOGS_ENABLED.isEnabled()) {
+                        HelpDialog.show(false, true, HelpDialog.getMouseLocation(), "SKIPPEDHOSTER", Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_title(), _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_msg(), NewTheme.I().getIcon("skipped", 32));
+                    }
+                    break;
+                case TIMEOUT:
+                    // if (JsonConfig.create(CaptchaSettings.class).isSkipDownloadLinkOnCaptchaTimeoutEnabled()) {
+                    CaptchaBlackList.getInstance().add(new BlockDownloadCaptchasByLink(link));
+                    if (CFG_GUI.HELP_DIALOGS_ENABLED.isEnabled()) {
+                        HelpDialog.show(false, true, HelpDialog.getMouseLocation(), "SKIPPEDHOSTER", Dialog.STYLE_SHOW_DO_NOT_DISPLAY_AGAIN, _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_title(), _GUI.T.ChallengeDialogHandler_viaGUI_skipped_help_msg(), NewTheme.I().getIcon("skipped", 32));
+                    }
+                    // }
+                    break;
+                case REFRESH:
+                    break;
+                case STOP_CURRENT_ACTION:
+                    if (Thread.currentThread() instanceof SingleDownloadController) {
+                        DownloadWatchDog.getInstance().stopDownloads();
+                    }
+                    break;
+                }
+            }
+            throw new CaptchaException(e.getSkipRequest());
+        } finally {
+            link.removePluginProgress(progress);
+        }
+    }
+
+}
