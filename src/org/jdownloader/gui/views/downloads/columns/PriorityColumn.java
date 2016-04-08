831c5ae7f00e8a85062c04ea00d28d8b5b3dbdf7
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-04-08 13:20:13 +0000

    fixed java compatibility issues
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33485 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1d0843e04c5c1e0f09c4bf801fd64b5acd140827

--- src/org/jdownloader/gui/views/downloads/columns/PriorityColumn.java
+++ src/org/jdownloader/gui/views/downloads/columns/PriorityColumn.java
@@ -15,6 +15,13 @@ import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.JTableHeader;
 
+import jd.controlling.TaskQueue;
+import jd.controlling.linkcrawler.CrawledLink;
+import jd.controlling.linkcrawler.CrawledPackage;
+import jd.controlling.packagecontroller.AbstractNode;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
 import org.appwork.swing.action.BasicAction;
 import org.appwork.swing.components.CheckBoxIcon;
 import org.appwork.swing.exttable.ExtColumn;
@@ -30,13 +37,6 @@ import org.jdownloader.gui.translate._GUI;
 import org.jdownloader.images.AbstractIcon;
 import org.jdownloader.images.BadgeIcon;
 import org.jdownloader.images.NewTheme;
-
-import jd.controlling.TaskQueue;
-import jd.controlling.linkcrawler.CrawledLink;
-import jd.controlling.linkcrawler.CrawledPackage;
-import jd.controlling.packagecontroller.AbstractNode;
-import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
 
 public class PriorityColumn extends ExtComboColumn<AbstractNode, Priority> {
 
@@ -138,7 +138,7 @@ public class PriorityColumn extends ExtComboColumn<AbstractNode, Priority> {
         return 35;
     }
 
-    protected AbstractButton getPopupElement(final Priority o, final boolean selected, AbstractNode value) {
+    protected AbstractButton getPopupElement(final Priority o, final boolean selected, final AbstractNode value) {
         return new JMenuItem(new BasicAction(o.toString()) {
             {
 
