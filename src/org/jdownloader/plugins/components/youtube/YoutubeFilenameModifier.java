93ce1653173e542a718f479262790d7362b5445c
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-04-14 07:30:51 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33515 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/plugins/components/youtube/YoutubeFilenameModifier.java
+++
@@ -1,9 +0,0 @@
-package org.jdownloader.plugins.components.youtube;
-
-import jd.plugins.DownloadLink;
-
-public interface YoutubeFilenameModifier {
-
-    String run(String formattedFilename, DownloadLink link);
-
-}
