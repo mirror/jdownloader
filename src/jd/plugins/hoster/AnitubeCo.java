7072a5b54b342af9e2964a5a838f06a096f85c8b
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-04-22 06:51:12 +0000

    FixeD: anitube.se, change default domain, checked plugin and fixed - re: testclass
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33579 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 766d2008e5cbde65370bc50b9fe231c0a5657869

--- src/jd/plugins/hoster/AnitubeCo.java
+++ src/jd/plugins/hoster/AnitubeCo.java
@@ -19,6 +19,7 @@ package jd.plugins.hoster;
 import java.io.IOException;
 
 import jd.PluginWrapper;
+import jd.http.Browser;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.DownloadLink;
@@ -28,8 +29,10 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "anitube.co" }, urls = { "http://(www\\.)?anitube\\.(co|tv|com\\.br|jp|se)/video/\\d+" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "anitube.se" }, urls = { "http://(www\\.)?anitube\\.(co|tv|com\\.br|jp|se)/video/\\d+" }, flags = { 0 })
 public class AnitubeCo extends PluginForHost {
+
+    // note: .co, .tv, .com.br, .jp don't respond only .se -raztoki20160422
 
     private String dllink = null;
 
@@ -39,7 +42,7 @@ public class AnitubeCo extends PluginForHost {
 
     @Override
     public String getAGBLink() {
-        return "http://www.anitube.co/terms";
+        return "http://www.anitube.se/terms";
     }
 
     @Override
@@ -54,29 +57,44 @@ public class AnitubeCo extends PluginForHost {
         br.setFollowRedirects(true);
         br.getPage(downloadLink.getDownloadURL());
         // provider blocks some subnets on http gateway, unknown what ranges.
-        if (br.containsHTML(">403 Forbidden<") && br.containsHTML(">nginx/[\\d+\\.]+<")) throw new PluginException(LinkStatus.ERROR_FATAL, "IP Blocked: Provider prevents access based on IP address.");
-        if (br.containsHTML("Unfortunately it\\'s impossible to access the site from your current geographic location")) return AvailableStatus.UNCHECKABLE;
-        if (br.getURL().contains("error.php?type=video_missing")) throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        if (br.containsHTML(">403 Forbidden<") && br.containsHTML(">nginx/[\\d+\\.]+<")) {
+            throw new PluginException(LinkStatus.ERROR_FATAL, "IP Blocked: Provider prevents access based on IP address.");
+        }
+        if (br.containsHTML("Unfortunately it's impossible to access the site from your current geographic location")) {
+            return AvailableStatus.UNCHECKABLE;
+        }
+        if (br.getURL().contains("error.php?type=video_missing")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
         String filename = br.getRegex("<title>([^<>\"]*?)\\- AniTube\\! Animes Online</title>").getMatch(0);
-        if (filename == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        String[] matches = br.getRegex("(http://(?:www\\.)?anitube\\.[^ \"']+/config\\.php\\?key=[0-9a-f]+)'").getColumn(0);
-        // final Regex match =
-        // br.getRegex("(http://(?:www\\.)?anitube\\.[^/]+)/[^ \"']+/config\\.php\\?key=([0-9a-f]+)'");
-        // http://www.anitube.jp/nuevo/config.php?key=0f114c048f4a91d70108
-        // if (match.count() > 0) {
+        if (filename == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String[] matches = br.getRegex("((?:https?:)?//(?:www\\.)?anitube\\.[^ \"']+/config\\.php\\?key=[0-9a-f]+)").getColumn(0);
+        if (matches == null || matches.length == 0) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
         for (String match : matches) {
-            br.getHeaders().put("Referer", "http://www.anitube.se/nuevo/playerv7.swf");
+            final Browser br = this.br.cloneBrowser();
             br.getPage(match);
-            dllink = br.getRegex("<file>\\s*(http://[^<]+\\d+\\.flv)\\s*</file>").getMatch(0);
-            if (dllink == null) dllink = br.getRegex("<html5>(http[^<]+)</html5>").getMatch(0);
-            if (dllink == null) throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            final String[] qualities = { "720p HD", "360p SD", "480" };
+            for (final String quality : qualities) {
+                dllink = br.getRegex("file\\s*:\\s*\"(https?://[^<>\"]*?)\",\\s*label:\\s*\"" + quality + "\"").getMatch(0);
+                if (dllink != null) {
+                    break;
+                }
+            }
+            if (dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
             URLConnectionAdapter con = null;
             try {
                 con = br.openGetConnection(dllink);
                 if (!con.getContentType().contains("html")) {
                     downloadLink.setDownloadSize(con.getLongContentLength());
                     filename = filename.trim();
-                    downloadLink.setFinalFileName(Encoding.htmlDecode(filename) + dllink.substring(dllink.lastIndexOf(".")));
+                    downloadLink.setFinalFileName(Encoding.htmlDecode(filename) + getFileNameExtensionFromString(dllink, ".mp4"));
+                    break;
                 } else {
                     throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
                 }
@@ -93,7 +111,9 @@ public class AnitubeCo extends PluginForHost {
     @Override
     public void handleFree(final DownloadLink downloadLink) throws Exception {
         requestFileInformation(downloadLink);
-        if (br.containsHTML("Unfortunately it\\'s impossible to access the site from your current geographic location")) throw new PluginException(LinkStatus.ERROR_FATAL, "Your country is blocked!");
+        if (br.containsHTML("Unfortunately it's impossible to access the site from your current geographic location")) {
+            throw new PluginException(LinkStatus.ERROR_FATAL, "Your country is blocked!");
+        }
         dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
         if (dl.getConnection().getContentType().contains("html")) {
             dl.getConnection().disconnect();
