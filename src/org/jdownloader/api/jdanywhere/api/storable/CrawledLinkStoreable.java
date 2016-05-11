00174506725b37c255d87de304a767eb650fd811
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-05-11 13:23:41 +0000

    fixes #78790
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33752 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/jdanywhere/api/storable/CrawledLinkStoreable.java
+++ src/org/jdownloader/api/jdanywhere/api/storable/CrawledLinkStoreable.java
@@ -7,89 +7,120 @@ import org.jdownloader.extensions.ExtensionController;
 import org.jdownloader.extensions.extraction.Archive;
 import org.jdownloader.extensions.extraction.ExtractionExtension;
 import org.jdownloader.extensions.extraction.bindings.crawledlink.CrawledLinkFactory;
+import org.jdownloader.gui.views.components.packagetable.LinkTreeUtils;
 
 public class CrawledLinkStoreable implements Storable {
 
     public long getId() {
-        if (link == null) return 0;
+        if (link == null) {
+            return 0;
+        }
         return link.getDownloadLink().getUniqueID().getID();
     }
 
     public String getName() {
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         return link.getName();
     }
 
     public String getHost() {
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         return link.getHost();
     }
 
     public String getOnlinestatus() {
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         return "";
     }
 
     public long getSize() {
-        if (link == null) return -1l;
+        if (link == null) {
+            return -1l;
+        }
         return link.getDownloadLink().getView().getBytesTotalEstimated();
     }
 
     public boolean isEnabled() {
-        if (link == null) return true;
+        if (link == null) {
+            return true;
+        }
         return link.isEnabled();
     }
 
     public long getAdded() {
-        if (link == null) return -1l;
+        if (link == null) {
+            return -1l;
+        }
         return link.getCreated();
     }
 
     public long getFinished() {
-        if (link == null) return -1l;
+        if (link == null) {
+            return -1l;
+        }
         return link.getFinishedDate();
     }
 
     public int getPriority() {
-        if (link == null) return 0;
+        if (link == null) {
+            return 0;
+        }
         return link.getPriority().getId();
     }
 
     public int getChunks() {
-        if (link == null) return 0;
+        if (link == null) {
+            return 0;
+        }
         return link.getChunks();
     }
 
     public String getBrowserurl() {
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         return link.getURL();
     }
 
     public String getPassword() {
 
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         String returnValue = "";
         ExtractionExtension extractor = (ExtractionExtension) ExtensionController.getInstance().getExtension(ExtractionExtension.class)._getExtension();
-        CrawledLinkFactory clf = new CrawledLinkFactory(((CrawledLink) link));
+        CrawledLinkFactory clf = new CrawledLinkFactory((link));
         Archive archive = extractor.getArchiveByFactory(clf);
         if (archive != null && archive.getSettings() != null && archive.getSettings().getPasswords() != null) {
             for (String pwd : archive.getSettings().getPasswords()) {
-                if (returnValue != "") returnValue += "/";
+                if (returnValue != "") {
+                    returnValue += "/";
+                }
                 returnValue += pwd;
             }
         }
         return returnValue;// getParentNode().getDownloadPassword(); //
-                           // getDownloadLink().getDownloadPassword();
+        // getDownloadLink().getDownloadPassword();
     }
 
     public String getDirectory() {
-        if (link == null) return null;
-        return link.getParentNode().getDownloadFolder(); // getDownloadLink().getFileOutput();
+        if (link == null) {
+            return null;
+        }
+        return LinkTreeUtils.getDownloadDirectory(link).getAbsolutePath(); // getDownloadLink().getFileOutput();
         // return null;
     }
 
     public String getComment() {
-        if (link == null) return null;
+        if (link == null) {
+            return null;
+        }
         return link.getDownloadLink().getComment();
     }
 
