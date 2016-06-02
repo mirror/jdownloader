91f7dc4c47e71f4b040c9c3c32449ba5c043e326
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-06-02 07:24:29 +0000

    fixes #79276
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33908 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 921afc6568134b85b29f3c12a2144618cb999719

--- src/jd/plugins/hoster/UlozTo.java
+++ src/jd/plugins/hoster/UlozTo.java
@@ -179,10 +179,7 @@ public class UlozTo extends PluginForHost {
     }
 
     private String getFilename() {
-        String filename = br.getRegex("<title>([^<>/]*?) \\|").getMatch(0);
-        if (filename == null) {
-            filename = br.getRegex("<title>(.*?) \\|").getMatch(0);
-        }
+        final String filename = br.getRegex("<title>\\s*([^<>/]*?)\\s*(\\|\\s*PORNfile.cz\\s*)?</title>").getMatch(0);
         return filename;
     }
 
@@ -451,9 +448,9 @@ public class UlozTo extends PluginForHost {
                     /*
                      * total bullshit, logs show user has 77.24622536 GB in login check just before given case of this. see log: Link;
                      * 1800542995541.log; 2422576; jdlog://1800542995541
-                     *
+                     * 
                      * @search --ID:1215TS:1456220707529-23.2.16 10:45:07 - [jd.http.Browser(openRequestConnection)] ->
-                     *
+                     * 
                      * I suspect that its caused by the predownload password? or referer? -raztoki20160304
                      */
                     // logger.info("No traffic available!");
