3a46b2e526c8af3a07e6286c3a442bba970b180f
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-06-02 15:52:54 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33911 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 161787b33e073cc3cebe528dd8f067eab955c3cc

--- src/org/jdownloader/scripting/envjs/EnvJSBrowser.java
+++ src/org/jdownloader/scripting/envjs/EnvJSBrowser.java
@@ -17,6 +17,16 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import javax.swing.SwingUtilities;
 
+import org.appwork.exceptions.WTFException;
+import org.appwork.net.protocol.http.HTTPConstants.ResponseCode;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.IO;
+import org.appwork.utils.logging2.LogSource;
+import org.jdownloader.logging.LogController;
+import org.jdownloader.scripting.ContextCallback;
+import org.jdownloader.scripting.JSHtmlUnitPermissionRestricter;
+
 import jd.http.Browser;
 import jd.http.Cookies;
 import jd.http.Request;
@@ -44,16 +54,6 @@ import net.sourceforge.htmlunit.corejs.javascript.tools.debugger.Main;
 import net.sourceforge.htmlunit.corejs.javascript.tools.debugger.SourceProvider;
 import net.sourceforge.htmlunit.corejs.javascript.tools.shell.Global;
 
-import org.appwork.exceptions.WTFException;
-import org.appwork.net.protocol.http.HTTPConstants.ResponseCode;
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.IO;
-import org.appwork.utils.logging2.LogSource;
-import org.jdownloader.logging.LogController;
-import org.jdownloader.scripting.ContextCallback;
-import org.jdownloader.scripting.JSHtmlUnitPermissionRestricter;
-
 public class EnvJSBrowser implements ContextCallback {
     static {
         try {
@@ -63,6 +63,7 @@ public class EnvJSBrowser implements ContextCallback {
             // already set globally
         }
     }
+
     // private Context cx;
     private Global                                                      scope;
     private long                                                        id;
@@ -451,8 +452,8 @@ public class EnvJSBrowser implements ContextCallback {
 
                 /**
                  * Returns the name of the function corresponding to this frame, if it is a function and it has a name. If the function does
-                 * not have a name, this method will try to return the name under which it was referenced. See <a
-                 * href="http://www.digital-web.com/articles/scope_in_javascript/">this page</a> for a good explanation of how the
+                 * not have a name, this method will try to return the name under which it was referenced. See
+                 * <a href="http://www.digital-web.com/articles/scope_in_javascript/">this page</a> for a good explanation of how the
                  * <tt>thisObj</tt> plays into this guess.
                  *
                  * @param thisObj
@@ -661,6 +662,7 @@ public class EnvJSBrowser implements ContextCallback {
                 preloadClasses += "load=" + c.getName() + ";\r\n";
             }
             preloadClasses += "delete load;";
+
             evalTrusted(preloadClasses);
 
             String initSource = readRequire("envjs/init");
@@ -697,6 +699,8 @@ public class EnvJSBrowser implements ContextCallback {
                     }
                 }
             }
+            eval("delete spawn;");
+            eval("delete window.spawn;");
             // if (getDebugLevel() == DebugLevel.DEBUG) {
             // cx.setDebugger(new JsDebugger(), "debugger");ou
             // }
