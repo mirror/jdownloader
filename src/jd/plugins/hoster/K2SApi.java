205c15e816a71e869897df483e7abd06631ccc37
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-06-03 13:35:38 +0000

    Fixed: backin.net, recaptchav2 support - re: IRC Fixed: K2SApi, 503 upgrade (forgot about that class) - re: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33914 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/K2SApi.java
+++ src/jd/plugins/hoster/K2SApi.java
@@ -1579,7 +1579,7 @@ public abstract class K2SApi extends PluginForHost {
                     throw new PluginException(LinkStatus.ERROR_FATAL, message);
                 } else if (responseCode == 503 && cloudflare != null) {
                     // 503 response code with javascript math section
-                    final String[] line1 = ibr.getRegex("var t,r,a,f, (\\w+)=\\{\"(\\w+)\":([^\\}]+)").getRow(0);
+                    final String[] line1 = ibr.getRegex("var (?:t,r,a,f,|s,t,o,[a-z,]+) (\\w+)=\\{\"(\\w+)\":([^\\}]+)").getRow(0);
                     String line2 = ibr.getRegex("(\\;" + line1[0] + "." + line1[1] + ".*?t\\.length\\;)").getMatch(0);
                     StringBuilder sb = new StringBuilder();
                     sb.append("var a={};\r\nvar t=\"" + Browser.getHost(ibr.getURL(), true) + "\";\r\n");
