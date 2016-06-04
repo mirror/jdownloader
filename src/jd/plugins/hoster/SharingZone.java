c7e83deadc10479244bcd10e124e91d5b3206503
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-06-04 01:23:47 +0000

    Fixed: sharing.zone, linkchecking - re: https://board.jdownloader.org/showpost.php?p=372102&postcount=4
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@33915 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 78ab394e857d012872db7f30b41c45eb2021523f

--- src/jd/plugins/hoster/SharingZone.java
+++ src/jd/plugins/hoster/SharingZone.java
@@ -22,6 +22,11 @@ import java.util.Locale;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
+
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.http.Browser;
@@ -29,6 +34,7 @@ import jd.http.Cookies;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
+import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
 import jd.plugins.AccountInfo;
@@ -41,11 +47,6 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.plugins.components.UserAgents;
-
-import org.appwork.utils.formatter.SizeFormatter;
-import org.appwork.utils.formatter.TimeFormatter;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "sharing.zone" }, urls = { "https?://(?:www\\.)?sharing\\.zone/[A-Za-z0-9]+" }, flags = { 2 })
 public class SharingZone extends PluginForHost {
@@ -80,7 +81,7 @@ public class SharingZone extends PluginForHost {
     private static final boolean           supportshttps                                = true;
     private static final boolean           supportshttps_FORCED                         = false;
     /* In case there is no information when accessing the main link */
-    private static final boolean           available_CHECK_OVER_INFO_PAGE               = true;
+    private static final boolean           available_CHECK_OVER_INFO_PAGE               = false;
     private static final boolean           useOldLoginMethod                            = false;
     private static final boolean           enable_RANDOM_UA                             = false;
     /* Known errors */
@@ -172,7 +173,15 @@ public class SharingZone extends PluginForHost {
                 return AvailableStatus.TRUE;
             }
             handleErrors();
-            if (br.getURL().contains("/error." + type) || br.getURL().contains("/index." + type) || (!br.containsHTML("class=\"downloadPageTable(V2)?\"") && !br.containsHTML("class=\"download\\-timer\"")) || br.getHttpConnection().getResponseCode() == 404) {
+            final Form dl = br.getFormbyActionRegex(".+\\?pt=$");
+            if (dl != null) {
+                // images can be behind captcha (recaptchav2) filename is within the url, filesize not possible until after captcha
+                filename = new Regex(dl.getAction(), "/([^/]+)\\?pt=$").getMatch(0);
+                if (filename != null) {
+                    link.setName(Encoding.htmlDecode(filename));
+                    return AvailableStatus.TRUE;
+                }
+            } else if (br.getURL().contains("/error." + type) || br.getURL().contains("/index." + type) || (!br.containsHTML("class=\"downloadPageTable(V2)?\"") && !br.containsHTML("class=\"download\\-timer\"")) || br.getHttpConnection().getResponseCode() == 404) {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
             final Regex fInfo = br.getRegex("<strong>([^<>\"]*?) \\((\\d+(?:,\\d+)?(?:\\.\\d+)? (?:KB|MB|GB))\\)<");
@@ -460,7 +469,7 @@ public class SharingZone extends PluginForHost {
      *            Imported String to match against.
      * @return <b>true</b> on valid rule match. <b>false</b> on invalid rule match.
      * @author raztoki
-     * */
+     */
     private boolean inValidate(final String s) {
         if (s == null || s != null && (s.matches("[\r\n\t ]+") || s.equals(""))) {
             return true;
