bf8242ee501be20f0b19903360f99c270fe781b8
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-06-25 05:33:40 +0000

    Mangahost.com - Support /n.n, https://board.jdownloader.org/showthread.php?t=69614&post#15
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34113 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: bf776cba37a394f162c3fe43c0bb7ac14e54b2d6

--- src/jd/plugins/decrypter/MangahostCom.java
+++ src/jd/plugins/decrypter/MangahostCom.java
@@ -27,7 +27,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangahost.com" }, urls = { "http://(?:www\\.)?br\\.mangahost\\.com/manga/[^/]+/\\d+" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangahost.com" }, urls = { "http://(?:www\\.)?br\\.mangahost\\.com/manga/[^/]+/\\d+(\\.\\d+)?" }, flags = { 0 })
 public class MangahostCom extends PluginForDecrypt {
 
     public MangahostCom(PluginWrapper wrapper) {
