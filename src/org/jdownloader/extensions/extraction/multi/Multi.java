ba066835c131ffdaa9d70eced0478e2290084f59
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-01 14:42:10 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34171 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/extraction/multi/Multi.java
+++ src/org/jdownloader/extensions/extraction/multi/Multi.java
@@ -606,12 +606,6 @@ public class Multi extends IExtraction {
         archive.setExitCode(ExtractionControllerConstants.EXIT_CODE_SUCCESS);
     }
 
-    /**
-     * Checks if the file should be upacked.
-     *
-     * @param file
-     * @return
-     */
     private boolean filter(final String file) {
         final String check = "/".concat(file);
         for (final Pattern regex : filter) {
@@ -1111,25 +1105,25 @@ public class Multi extends IExtraction {
                     if (signatureString.length() >= 24) {
                         /*
                          * 0x0001 Volume attribute (archive volume)
-                         *
+                         * 
                          * 0x0002 Archive comment present RAR 3.x uses the separate comment block and does not set this flag.
-                         *
+                         * 
                          * 0x0004 Archive lock attribute
-                         *
+                         * 
                          * 0x0008 Solid attribute (solid archive)
-                         *
+                         * 
                          * 0x0010 New volume naming scheme ('volname.partN.rar')
-                         *
+                         * 
                          * 0x0020 Authenticity information present RAR 3.x does not set this flag.
-                         *
+                         * 
                          * 0x0040 Recovery record present
-                         *
+                         * 
                          * 0x0080 Block headers are encrypted
                          */
                         final String headerBitFlags1 = "" + signatureString.charAt(20) + signatureString.charAt(21);
                         /*
                          * 0x0100 FIRST Volume
-                         *
+                         * 
                          * 0x0200 EncryptedVerion
                          */
                         final String headerBitFlags2 = "" + signatureString.charAt(22) + signatureString.charAt(23);
