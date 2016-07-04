0f0e44ed8c7c99658902cff3e71ce4362c9ab950
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-04 13:58:25 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34195 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/laf/jddefault/JDDefaultLookAndFeel.java
+++ src/org/jdownloader/gui/laf/jddefault/JDDefaultLookAndFeel.java
@@ -1,10 +1,17 @@
 package org.jdownloader.gui.laf.jddefault;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.text.ParseException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.Icon;
 import javax.swing.JComponent;
 
+import org.appwork.utils.Application;
+import org.appwork.utils.logging2.extmanager.LoggerFactory;
 import org.jdownloader.images.NewTheme;
 
 import de.javasoft.plaf.synthetica.SyntheticaLookAndFeel;
@@ -13,12 +20,15 @@ import de.javasoft.util.OS;
 public class JDDefaultLookAndFeel extends SyntheticaLookAndFeel {
 
     private static final long serialVersionUID = -6728461590292436336L;
+    private boolean           initDone         = false;
 
     // Construtor
     public JDDefaultLookAndFeel() throws java.text.ParseException {
         // load synth.xml from custom package
         super("/org/jdownloader/gui/laf/jddefault/synth.xml");
         OS.getCurrentOS();
+        initDone = true;
+
     }
 
     // return an unique LAF id
@@ -27,6 +37,34 @@ public class JDDefaultLookAndFeel extends SyntheticaLookAndFeel {
     }
 
     private final AtomicBoolean newThemeAvailable = new AtomicBoolean(false);
+
+    @Override
+    public void load(InputStream input, Class<?> resourceBase) throws ParseException {
+        FileInputStream fis = null;
+        if (!initDone) {
+            try {
+                File custom = Application.getResource("cfg/laf/" + JDDefaultLookAndFeel.class.getSimpleName() + ".xml");
+                LoggerFactory.getDefaultLogger().info("Custom LAF XML: " + custom);
+                if (custom.exists()) {
+
+                    super.load(new BufferedInputStream(fis = new FileInputStream(custom)), resourceBase);
+                    input.close();
+                    return;
+                }
+            } catch (Throwable e) {
+                LoggerFactory.getDefaultLogger().log(e);
+            } finally {
+                try {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                } catch (Throwable e) {
+                    LoggerFactory.getDefaultLogger().log(e);
+                }
+            }
+        }
+        super.load(input, resourceBase);
+    }
 
     public Icon getDisabledIcon(JComponent component, Icon icon) {
         if (!newThemeAvailable.get()) {
