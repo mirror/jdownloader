2675a55ce543bd212759fe895d686e531200e7a8
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-05 14:58:28 +0000

    Fixes https://board.jdownloader.org/showthread.php?t=69927
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34216 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/notify/gui/BubbleNotifyConfig.java
+++ src/org/jdownloader/gui/notify/gui/BubbleNotifyConfig.java
@@ -116,18 +116,12 @@ public interface BubbleNotifyConfig extends ConfigInterface {
     public void setBubbleNotifyEnabledDuringSilentMode(boolean b);
 
     public static enum BubbleNotifyEnabledState {
-        @EnumLabel("Always")
-        ALWAYS,
-        @EnumLabel("Never")
-        NEVER,
-        @EnumLabel("Only if JDownloader is not the active window")
-        JD_NOT_ACTIVE,
-        @EnumLabel("Only if JDownloader is minimized to tray or taskbar")
-        TRAY_OR_TASKBAR,
-        @EnumLabel("Only if JDownloader is minimized to tray")
-        TRAY,
-        @EnumLabel("Only if JDownloader is minimized to taskbar")
-        TASKBAR
+        @EnumLabel("Always") ALWAYS,
+        @EnumLabel("Never") NEVER,
+        @EnumLabel("Only if JDownloader is not the active window") JD_NOT_ACTIVE,
+        @EnumLabel("Only if JDownloader is minimized to tray or taskbar") TRAY_OR_TASKBAR,
+        @EnumLabel("Only if JDownloader is minimized to tray") TRAY,
+        @EnumLabel("Only if JDownloader is minimized to taskbar") TASKBAR
 
     }
 
@@ -265,4 +259,10 @@ public interface BubbleNotifyConfig extends ConfigInterface {
     public boolean isBubbleAnchorRelativeToWindow();
 
     public void setBubbleAnchorRelativeToWindow(boolean b);
+
+    @AboutConfig()
+    @DefaultBooleanValue(true)
+    public boolean isCaptchaExchangeSolverBubbleImageVisible();
+
+    public void setCaptchaExchangeSolverBubbleImageVisible(boolean b);
 }
