428f5148f1acededb29feef80b1a078d92f76ab7
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-09 00:32:00 +0000

    Added: safelinking.net, new unique id (hex{10})
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34269 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/SflnkgNt.java
+++ src/jd/plugins/decrypter/SflnkgNt.java
@@ -56,7 +56,7 @@ public class SflnkgNt extends abstractSafeLinking {
                 a[i] = "https?://sflk\\.in/[a-zA-Z0-9]{10}";
             } else {
                 // https is only supported on there safelinking.net domain, once again auto correct within correctLink call to supportsHTTPS
-                a[i] = "https?://(?:www\\.)?" + Pattern.quote(domain) + "/(?:(?:p|d(?:/com)?)/[a-zA-Z0-9]{10}|(?:d/)?" + regexBase58() + ")";
+                a[i] = "https?://(?:www\\.)?" + Pattern.quote(domain) + "/(?:(?:p|d(?:/com)?)/[a-zA-Z0-9]{10}|[a-f0-9]{10}|(?:d/)?" + regexBase58() + ")";
             }
             i++;
         }
