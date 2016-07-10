ebc7976821c5a3838f0ba9db7fe023a92b4b555e
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-10 14:05:44 +0000

    Uplds.com - Adjustment fixes #79765
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34283 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7cbf45448079c699a76f5d31e521c07a5a093851

--- src/jd/plugins/decrypter/UpldsCom.java
+++ src/jd/plugins/decrypter/UpldsCom.java
@@ -48,7 +48,7 @@ public class UpldsCom extends PluginForDecrypt {
             return null;
         }
         br.submitForm(decryptform);
-        final String finallink = br.getRegex("This direct link will be available for your IP .*?<a href=\"(http[^<>\"]*?)\"").getMatch(0);
+        final String finallink = br.getRegex("<span.*?>\\s<a href=\"(http[^<>\"]*?)\"").getMatch(0);
         if (finallink == null) {
             logger.warning("Decrypter broken for link: " + parameter);
             return null;
