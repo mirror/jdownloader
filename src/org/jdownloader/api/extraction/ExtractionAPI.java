b1ca5ac83190156afc96d3e2c32c15d02083c346
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-19 10:25:04 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34393 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: fc913fc1f2a2bba8c06fe58574b3cad7bad2d0b8

--- src/org/jdownloader/api/extraction/ExtractionAPI.java
+++ src/org/jdownloader/api/extraction/ExtractionAPI.java
@@ -4,24 +4,29 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.appwork.remoteapi.RemoteAPIInterface;
+import org.appwork.remoteapi.annotations.APIParameterNames;
 import org.appwork.remoteapi.annotations.ApiNamespace;
 import org.appwork.remoteapi.exceptions.BadParameterException;
 
 @ApiNamespace("extraction")
 public interface ExtractionAPI extends RemoteAPIInterface {
-
+    @APIParameterNames({ "password" })
     public void addArchivePassword(String password);
 
+    @APIParameterNames({ "linkIds", "packageIds" })
     public HashMap<String, Boolean> startExtractionNow(final long[] linkIds, final long[] packageIds);
 
+    @APIParameterNames({ "linkIds", "packageIds" })
     public List<ArchiveStatusStorable> getArchiveInfo(final long[] linkIds, final long[] packageIds);
 
     public List<ArchiveStatusStorable> getQueue();
 
-    public Boolean cancelExtraction(long archiveId);
+    @APIParameterNames({ "controllerID" })
+    public boolean cancelExtraction(long controllerID);
 
+    @APIParameterNames({ "archiveIds" })
     public List<ArchiveSettingsAPIStorable> getArchiveSettings(String[] archiveIds) throws BadParameterException;
 
-    public Boolean setArchiveSettings(String archiveId, ArchiveSettingsAPIStorable archiveSettings) throws BadParameterException;
-
+    @APIParameterNames({ "archiveId", "archiveSettings" })
+    public boolean setArchiveSettings(String archiveId, ArchiveSettingsAPIStorable archiveSettings) throws BadParameterException;
 }
