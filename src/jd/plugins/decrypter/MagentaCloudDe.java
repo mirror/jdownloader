98862d94f0d9e328b892c25d1178001f13c4bdd4
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-19 15:48:28 +0000

    MagentaCloudDe: fixes #78548
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34403 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/MagentaCloudDe.java
@@ -0,0 +1,100 @@
+package jd.plugins.decrypter;
+
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.parser.UrlQuery;
+
+@DecrypterPlugin(revision = "$Revision: 33293 $", interfaceVersion = 3, names = { "magentacloud.de" }, urls = { "https?://(www\\.)?magentacloud\\.de/(share|lnk)/[a-z0-9\\-]+" }, flags = { 32 })
+public class MagentaCloudDe extends PluginForDecrypt {
+
+    public MagentaCloudDe(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final String param = parameter.getCryptedUrl();
+        final String paramID = new Regex(param, "(?:share|lnk)/([a-z0-9\\-]+)").getMatch(0);
+        if (param.contains("/lnk/")) {
+            br.getPage("https://www.magentacloud.de/api/sharelink/info?id=" + paramID);
+            Map<String, Object> info = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP, null);
+            if (paramID.equals(info.get("id")) && !info.containsKey("code")) {
+                br.postPage("https://www.magentacloud.de/api/sharelink/info", UrlQuery.parse("id=" + Encoding.urlEncode(paramID) + "&fields=name%2Ctype%2Csize"));
+                info = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP, null);
+                final String type = (String) info.get("type");
+                if ("file".equals(type)) {
+                    final String name = (String) info.get("name");
+                    final Number size = (Number) info.get("size");
+                    final DownloadLink link = createDownloadlink("directhttp://https://www.magentacloud.de/api/sharelink/download?id=" + paramID);
+                    if (name != null) {
+                        link.setFinalFileName(URLDecoder.decode(name, "UTF-8"));
+                    }
+                    if (size != null) {
+                        link.setVerifiedFileSize(size.longValue());
+                    }
+                    link.setAvailable(true);
+                    link.setLinkID("magentacloud.de://" + paramID);
+                    link.setContentUrl(param);
+                    ret.add(link);
+                }
+            }
+        } else {
+            br.getPage("https://www.magentacloud.de/api/share/info?id=" + paramID);
+            Map<String, Object> info = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP, null);
+            if (!info.containsKey("code")) {
+                br.postPage("https://www.magentacloud.de/api/share/token", UrlQuery.parse("id=" + Encoding.urlEncode(paramID)));
+                info = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP, null);
+                final String root_name = (String) info.get("root_name");
+                final String token_type = (String) info.get("token_type");
+                final String access_token = (String) info.get("access_token");
+                if ("Bearer".equals(token_type) && access_token != null) {
+                    br.setHeader("Authorization", token_type + " " + Encoding.Base64Encode(access_token));
+                    br.getPage("https://www.magentacloud.de/api/dir?path=/&fields=id%2Cpath%2Cwritable%2Cmembers.id%2Cmembers.parent_id%2Cmembers.name%2Cmembers.mtime%2Cmembers.path%2Cmembers.readable%2Cmembers.writable%2Cmembers.type%2Cmembers.image.width%2Cmembers.image.height%2Cmembers.size&members=all&limit=0%2C5000");
+                    info = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP, null);
+                    for (final Map<String, Object> member : (List<Map<String, Object>>) info.get("members")) {
+                        final String type = (String) member.get("type");
+                        if ("file".equals(type)) {
+                            final String name = (String) member.get("name");
+                            final String id = (String) member.get("id");
+                            final Number size = (Number) member.get("size");
+                            final DownloadLink link = createDownloadlink("directhttp://https://www.magentacloud.de/api/file?attachment=true&pid=" + id + "&access_token=" + access_token);
+                            if (name != null) {
+                                link.setFinalFileName(URLDecoder.decode(name, "UTF-8"));
+                            }
+                            if (size != null) {
+                                link.setVerifiedFileSize(size.longValue());
+                            }
+                            link.setAvailable(true);
+                            link.setLinkID("magentacloud.de://" + id);
+                            link.setContainerUrl(param);
+                            ret.add(link);
+                        }
+                    }
+                }
+                if (ret.size() > 1 && root_name != null) {
+                    final FilePackage fp = FilePackage.getInstance();
+                    fp.setName(URLDecoder.decode(root_name, "UTF-8"));
+                    fp.addLinks(ret);
+                }
+            }
+        }
+        return ret;
+    }
+
+}
