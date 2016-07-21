174d92f11a70993d2384b890762477ac92a508f5
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-07-21 09:53:32 +0000

    TorXPw: added premium support, fixes thread 68095
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34419 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/TorXPW.java
+++ src/jd/plugins/decrypter/TorXPW.java
@@ -8,12 +8,12 @@ import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
-import jd.plugins.PluginForDecrypt;
 
+import org.appwork.utils.StringUtils;
 import org.appwork.utils.formatter.SizeFormatter;
 
-@DecrypterPlugin(revision = "$Revision: 21898 $", interfaceVersion = 2, names = { "torx.pw" }, urls = { "https?://(www\\.)?torx\\.pw/\\?download=[a-zA-Z0-9]+" }, flags = { 0 })
-public class TorXPW extends PluginForDecrypt {
+@DecrypterPlugin(revision = "$Revision: 21898 $", interfaceVersion = 2, names = { "torx.pw", "premium.torx.pw" }, urls = { "https?://(www\\.)?torx\\.pw/\\?download=[a-zA-Z0-9]+", "https?://premium\\.torx\\.pw/\\?(file=[a-zA-Z0-9\\=]+&x=[a-zA-Z0-9\\=]+&username=.*?&password=.+|username=.*?&password=.+&download=[a-zA-Z0-9\\=]+)" }, flags = { 0 })
+public class TorXPW extends antiDDoSForDecrypt {
 
     public TorXPW(PluginWrapper wrapper) {
         super(wrapper);
@@ -22,24 +22,51 @@ public class TorXPW extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        br.setFollowRedirects(true);
-        br.getPage(parameter);
-        final String title = br.getRegex("<center><h3><b>(.*?)</b").getMatch(0);
-        final String[][] urls = br.getRegex("href=\"(https?://[a-f0-9]+\\.torx\\.pw/\\?file=.*?)\">(.*?)</a>\\s*-\\s*([0-9\\.]+)\\s*<b>\\s*([TGKMB]+)").getMatches();
-        if (urls != null) {
-            for (final String url[] : urls) {
-                final DownloadLink downloadLink = createDownloadlink("directhttp://" + url[0]);
-                downloadLink.setAvailable(true);
-                downloadLink.setFinalFileName(url[1]);
-                final String size = url[2] + " " + url[3];
-                downloadLink.setDownloadSize(SizeFormatter.getSize(size));
+        if ("premium.torx.pw".equals(getHost())) {
+            if (StringUtils.containsIgnoreCase(parameter, "torx.pw/?file")) {
+                final DownloadLink downloadLink = createDownloadlink("directhttp://" + parameter);
                 decryptedLinks.add(downloadLink);
+            } else {
+                br.setFollowRedirects(true);
+                getPage(parameter);
+                final String title = br.getRegex("<center><h3><b>(.*?)</b").getMatch(0);
+                final String[][] urls = br.getRegex("href=\"(https?://premium\\.torx\\.pw/\\?file=.*?)\">(.*?)</a>\\s*-\\s*([0-9\\.]+)\\s*<b>\\s*([TGKMB]+)").getMatches();
+                if (urls != null) {
+                    for (final String url[] : urls) {
+                        final DownloadLink downloadLink = createDownloadlink("directhttp://" + url[0]);
+                        downloadLink.setAvailable(true);
+                        downloadLink.setFinalFileName(url[1]);
+                        final String size = url[2] + " " + url[3];
+                        downloadLink.setDownloadSize(SizeFormatter.getSize(size));
+                        decryptedLinks.add(downloadLink);
+                    }
+                }
+                if (title != null && decryptedLinks.size() > 0) {
+                    final FilePackage fp = FilePackage.getInstance();
+                    fp.setName(title);
+                    fp.addLinks(decryptedLinks);
+                }
             }
-        }
-        if (title != null && decryptedLinks.size() > 0) {
-            final FilePackage fp = FilePackage.getInstance();
-            fp.setName(title);
-            fp.addLinks(decryptedLinks);
+        } else {
+            br.setFollowRedirects(true);
+            getPage(parameter);
+            final String title = br.getRegex("<center><h3><b>(.*?)</b").getMatch(0);
+            final String[][] urls = br.getRegex("href=\"(https?://[a-f0-9]+\\.torx\\.pw/\\?file=.*?)\">(.*?)</a>\\s*-\\s*([0-9\\.]+)\\s*<b>\\s*([TGKMB]+)").getMatches();
+            if (urls != null) {
+                for (final String url[] : urls) {
+                    final DownloadLink downloadLink = createDownloadlink("directhttp://" + url[0]);
+                    downloadLink.setAvailable(true);
+                    downloadLink.setFinalFileName(url[1]);
+                    final String size = url[2] + " " + url[3];
+                    downloadLink.setDownloadSize(SizeFormatter.getSize(size));
+                    decryptedLinks.add(downloadLink);
+                }
+            }
+            if (title != null && decryptedLinks.size() > 0) {
+                final FilePackage fp = FilePackage.getInstance();
+                fp.setName(title);
+                fp.addLinks(decryptedLinks);
+            }
         }
         return decryptedLinks;
     }
