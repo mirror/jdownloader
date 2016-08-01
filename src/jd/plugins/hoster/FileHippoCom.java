e24d27b92740290d97924de4e460050cdbc599f2
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-01 13:03:03 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34496 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 2d5d149114061a34cbf85e56fc849d20dd2b94a5

--- src/jd/plugins/hoster/FileHippoCom.java
+++ src/jd/plugins/hoster/FileHippoCom.java
@@ -30,7 +30,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filehippo.com" }, urls = { "http://(?:www\\.)?filehippo\\.com(?/:(?:es|en|pl|jp|de))?/download_[^<>/\"]+(?:(?:/tech)?/\\d+/)?" }, flags = { 0 })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filehippo.com" }, urls = { "http://(?:www\\.)?filehippo\\.com(?:/(?:es|en|pl|jp|de))?/download_[^<>/\"]+(?:(?:/tech)?/\\d+/)?" }, flags = { 0 })
 public class FileHippoCom extends PluginForHost {
 
     private static final String FILENOTFOUND = "(<h1>404 Error</h1>|<b>Sorry the page you requested could not be found|Sorry an error occurred processing your request)";
