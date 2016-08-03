671c6fb904e06796edfb47e8b161928ffd8e86be
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-03 09:41:34 +0000

    SolidFilescom: -fixed regression -fixes thread 70301
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34531 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 55653b3bb59752e7b08eea900d8fb70592ff1782

--- src/jd/plugins/decrypter/SolidFilesComFolder.java
+++ src/jd/plugins/decrypter/SolidFilesComFolder.java
@@ -33,7 +33,7 @@ import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.utils.JDUtilities;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "solidfiles.com" }, urls = { "https?://(?:www\\.)?solidfiles\\.com/(?:folder|v)/[a-z0-9]+/?" }, flags = { 0 })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "solidfiles.com" }, urls = { "https?://(?:www\\.)?solidfiles\\.com/(?:folder)/[a-z0-9]+/?" }, flags = { 0 })
 public class SolidFilesComFolder extends PluginForDecrypt {
 
     public SolidFilesComFolder(PluginWrapper wrapper) {
