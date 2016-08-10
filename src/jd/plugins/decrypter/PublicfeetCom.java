089f96393b43299ef0a5d54fbf338f815fb1cc55
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-10 17:38:52 +0000

    -added support for ziifilecom closes #80082 -some work on rtvees refs #80108 -added decrypter for publicfeetcom closes #80121 -imgurcom: added prefermp4 setting for gif downloads fixes #80148 -raiit: some work towards quality selection refs #79535 -tutv: added support for password protected (private) videos fixes #80146 -ServePornCom: added domain lanpornocom closes #79681
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34613 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1a37ad5a7657293a1dce5a5fa6ef9c6a572c7323

---
+++ src/jd/plugins/decrypter/PublicfeetCom.java
@@ -0,0 +1,66 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "publicfeet.com" }, urls = { "https?://(?:www\\.)?publicfeet\\.com/posts/\\d+" }, flags = { 0 })
+public class PublicfeetCom extends PluginForDecrypt {
+
+    public PublicfeetCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.getPage(parameter);
+        if (br.getHttpConnection().getResponseCode() == 404 || this.br.containsHTML(">No such post|This room does not exist")) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
+            return decryptedLinks;
+        }
+        String fpName = br.getRegex("<TITLE>([^<>\"]+)</TITLE>").getMatch(0);
+        final String[] links = br.getRegex("\"anum\":\"(\\d+)\"").getColumn(0);
+        if (links == null || links.length == 0) {
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
+        }
+        for (final String singleLink : links) {
+            final DownloadLink dl = createDownloadlink("http://www.publicfeet.com/photos/" + singleLink + ".jpg");
+            dl.setAvailable(true);
+            decryptedLinks.add(dl);
+        }
+
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+
+        return decryptedLinks;
+    }
+
+}
