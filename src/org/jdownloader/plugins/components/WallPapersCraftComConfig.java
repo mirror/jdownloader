ac2d1621a0d7ee8fff39e0e57279b79f3c512671
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-17 14:32:52 +0000

    WallPapersCraftCom: added decrypter for this page
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34679 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7b973fa5bfebbb88638fcd2a89025e1d688066e0

---
+++ src/org/jdownloader/plugins/components/WallPapersCraftComConfig.java
@@ -0,0 +1,25 @@
+package org.jdownloader.plugins.components;
+
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "wallpaperscraft.com", type = Type.CRAWLER)
+public interface WallPapersCraftComConfig extends PluginConfigInterface {
+    public static class TRANSLATION {
+
+        public String getUseSSLForDownload_label() {
+            return "Prefer original resolution?";
+        }
+    }
+
+    public static final TRANSLATION TRANSLATION = new TRANSLATION();
+
+    @AboutConfig
+    @DefaultBooleanValue(true)
+    boolean isPreferOriginalResolution();
+
+    void setPreferOriginalResolution(boolean b);
+}
