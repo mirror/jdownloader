1a6aa4dbc5375881b5990b2083fa524034a9de51
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-17 14:04:41 +0000

    EventSandbox: -we need a dummy event so ScriptThread can load all possible classes,@mpgai
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34678 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f0343887689ee107ecb44fcdbcd77a604242dd46

--- src/org/jdownloader/extensions/eventscripter/sandboxobjects/EventSandbox.java
+++ src/org/jdownloader/extensions/eventscripter/sandboxobjects/EventSandbox.java
@@ -1,5 +1,10 @@
 package org.jdownloader.extensions.eventscripter.sandboxobjects;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 import org.appwork.remoteapi.events.EventObject;
 import org.jdownloader.extensions.eventscripter.ScriptAPI;
 
@@ -24,13 +29,27 @@ public class EventSandbox {
     private String publisher;
 
     public EventSandbox(EventObject event) {
-
         this.id = event.getEventid();
         this.data = event.getEventdata();
         this.publisher = event.getPublisher().getPublisherName();
     }
 
     public EventSandbox() {
+        // we need this so getTestProperties has some dummy data and ScriptThread can preinit the needed classes
+        this.id = "test";
+        final Map<String, Object> data = new HashMap<String, Object>();
+        data.put("int", new Integer(1));
+        data.put("long", new Long(1));
+        data.put("string", "test");
+        data.put("boolean", Boolean.TRUE);
+        data.put("double", new Double(1));
+        data.put("float", new Float(1));
+        data.put("list", new ArrayList<Object>(0));
+        data.put("set", new HashSet<Object>(0));
+        data.put("array", new Object[0]);
+        data.put("map", new HashMap<String, Object>());
+        this.data = data;
+        this.publisher = "test";
     }
 
 }
