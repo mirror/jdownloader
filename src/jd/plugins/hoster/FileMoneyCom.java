6f7e75d894aa5caa2731b7e154d63829bb255b43
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-08-29 07:04:23 +0000

    Filemoney.com - Wait tine regex, https://board.jdownloader.org/showthread.php?t=70611
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34782 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FileMoneyCom.java
+++ src/jd/plugins/hoster/FileMoneyCom.java
@@ -54,7 +54,7 @@ import org.appwork.utils.formatter.TimeFormatter;
 import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
 import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filemoney.com" }, urls = { "https?://(www\\.)?filemoney\\.com/(vidembed\\-)?[a-z0-9]{12}" }) 
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filemoney.com" }, urls = { "https?://(www\\.)?filemoney\\.com/(vidembed\\-)?[a-z0-9]{12}" })
 public class FileMoneyCom extends PluginForHost {
 
     private String               correctedBR                  = "";
@@ -432,13 +432,13 @@ public class FileMoneyCom extends PluginForHost {
     /**
      * Prevents more than one free download from starting at a given time. One step prior to dl.startDownload(), it adds a slot to maxFree
      * which allows the next singleton download to start, or at least try.
-     *
+     * 
      * This is needed because xfileshare(website) only throws errors after a final dllink starts transferring or at a given step within pre
      * download sequence. But this template(XfileSharingProBasic) allows multiple slots(when available) to commence the download sequence,
      * this.setstartintival does not resolve this issue. Which results in x(20) captcha events all at once and only allows one download to
      * start. This prevents wasting peoples time and effort on captcha solving and|or wasting captcha trading credits. Users will experience
      * minimal harm to downloading as slots are freed up soon as current download begins.
-     *
+     * 
      * @param controlFree
      *            (+1|-1)
      */
@@ -562,7 +562,7 @@ public class FileMoneyCom extends PluginForHost {
     private void waitTime(long timeBefore, final DownloadLink downloadLink) throws PluginException {
         int passedTime = (int) ((System.currentTimeMillis() - timeBefore) / 1000) - 1;
         /** Ticket Time */
-        final String ttt = new Regex(correctedBR, ">Wait <span id=\"[a-z0-9]+\">(\\d+)</span>").getMatch(0);
+        final String ttt = new Regex(correctedBR, ">Wait(?: |<br/>)<[^<>]+>(\\d+)</span>").getMatch(0);
         if (ttt != null) {
             int tt = Integer.parseInt(ttt);
             tt -= passedTime;
@@ -576,7 +576,7 @@ public class FileMoneyCom extends PluginForHost {
     // TODO: remove this when v2 becomes stable. use br.getFormbyKey(String key, String value)
     /**
      * Returns the first form that has a 'key' that equals 'value'.
-     *
+     * 
      * @param key
      * @param value
      * @return
