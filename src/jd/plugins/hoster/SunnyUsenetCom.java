c699fb3a4a5e6950798ec8512b18fd06221bb080
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-09-02 12:33:26 +0000

    SunnyUsenetCom: -added support for this usenet provider
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34821 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/SunnyUsenetCom.java
@@ -0,0 +1,128 @@
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import jd.PluginWrapper;
+import jd.http.Cookies;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.plugins.components.usenet.UsenetAccountConfigInterface;
+import org.jdownloader.plugins.components.usenet.UsenetServer;
+
+@HostPlugin(revision = "$Revision: 31032 $", interfaceVersion = 3, names = { "sunnyusenet.com" }, urls = { "" })
+public class SunnyUsenetCom extends UseNet {
+
+    public SunnyUsenetCom(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium("https://www.sunnyusenet.com/en/packages");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.sunnyusenet.com/en/terms-and-conditions";
+    }
+
+    public static interface SunnyUsenetComConfigInterface extends UsenetAccountConfigInterface {
+    };
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        setBrowserExclusive();
+        final AccountInfo ai = new AccountInfo();
+        br.setFollowRedirects(true);
+        final Cookies cookies = account.loadCookies("");
+        try {
+            Form login = null;
+            if (cookies != null) {
+                br.setCookies(getHost(), cookies);
+                br.getPage("https://www.sunnyusenet.com/en");
+                br.getPage("https://members.sunnyusenet.com/en/index");
+                login = br.getFormbyKey("token");
+                if (login != null && (login.containsHTML("username") || login.containsHTML("password"))) {
+                    br.getCookies(getHost()).clear();
+                } else if (br.getCookie(getHost(), "SUN-SESS") == null) {
+                    br.getCookies(getHost()).clear();
+                }
+            }
+            if (br.getCookie(getHost(), "SUN-SESS") == null) {
+                account.clearCookies("");
+                final String userName = account.getUser();
+                if (userName == null || !userName.matches("^.+?@.+?\\.[^\\.]+")) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "Please enter your e-mail/password for sunnyusenet.com website!", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                br.getPage("https://www.sunnyusenet.com/en");
+                br.getPage("https://members.sunnyusenet.com/en/index");
+                login = br.getFormbyKey("token");
+                login.put("username", Encoding.urlEncode(userName));
+                login.put("password", Encoding.urlEncode(account.getPass()));
+                br.submitForm(login);
+                login = br.getFormbyKey("token");
+                if (login != null && (login.containsHTML("username") || login.containsHTML("password"))) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                } else if (br.getCookie(getHost(), "SUN-SESS") == null) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+            }
+            account.saveCookies(br.getCookies(getHost()), "");
+            final String yourPackage = br.getRegex("Your package:\\s*<span.*?>\\s*(Sunny.*?)\\s*<").getMatch(0);
+            final String expireDate = br.getRegex("Expiration date:\\s*</b>\\s*(\\d+-\\d+-\\d+)\\s*<").getMatch(0);
+            final String connections = br.getRegex("Connections:\\s*</b>\\s*(\\d+)\\s*<").getMatch(0);
+            final int packageConnections;
+            if (StringUtils.containsIgnoreCase(yourPackage, "UNL")) {
+                packageConnections = 20;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "90")) {
+                packageConnections = 15;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "60")) {
+                packageConnections = 15;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "30")) {
+                packageConnections = 10;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "20")) {
+                packageConnections = 10;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "10")) {
+                packageConnections = 5;
+            } else if (StringUtils.containsIgnoreCase(yourPackage, "5")) {
+                packageConnections = 5;
+            } else {
+                packageConnections = 1;
+            }
+            if (connections != null) {
+                account.setMaxSimultanDownloads(Math.min(Integer.parseInt(connections), packageConnections));
+            } else {
+                account.setMaxSimultanDownloads(packageConnections);
+            }
+            ai.setStatus("Your package: " + yourPackage);
+            if (expireDate != null) {
+                ai.setValidUntil(TimeFormatter.getMilliSeconds(expireDate, "yyyy'-'MM'-'dd", Locale.ENGLISH) + (24 * 60 * 60 * 1000l));
+            } else {
+                ai.setExpired(true);
+            }
+        } catch (final PluginException e) {
+            if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                account.clearCookies("");
+            }
+            throw e;
+        }
+        ai.setProperty("multiHostSupport", Arrays.asList(new String[] { "usenet" }));
+        account.setProperty(Account.PROPERTY_REFRESH_TIMEOUT, 5 * 60 * 60 * 1000l);
+        return ai;
+    }
+
+    @Override
+    public List<UsenetServer> getAvailableUsenetServer() {
+        final List<UsenetServer> ret = new ArrayList<UsenetServer>();
+        ret.addAll(UsenetServer.createServerList("news.sunnyusenet.com", false, 119, 80));
+        ret.addAll(UsenetServer.createServerList("news.sunnyusenet.com", true, 563, 443));
+        return ret;
+    }
+}
