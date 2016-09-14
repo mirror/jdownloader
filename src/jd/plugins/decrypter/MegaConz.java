42b67327014833a842ba27ef959f35e4754a5a5e
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-09-14 16:28:54 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34914 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MegaConz.java
+++ src/jd/plugins/decrypter/MegaConz.java
@@ -38,7 +38,7 @@ public class MegaConz extends PluginForDecrypt {
         super(wrapper);
     }
 
-    public static class MegaFolder {
+    private class MegaFolder {
 
         public String  parent;
         public String  name;
@@ -86,19 +86,19 @@ public class MegaConz extends PluginForDecrypt {
         final String nodes[] = br.getRegex("\\{\\s*?(\"h\".*?)\\}").getColumn(0);
         /*
          * p = parent node (ID)
-         *
+         * 
          * s = size
-         *
+         * 
          * t = type (0=file, 1=folder, 2=root, 3=inbox, 4=trash
-         *
+         * 
          * ts = timestamp
-         *
+         * 
          * h = node (ID)
-         *
+         * 
          * u = owner
-         *
+         * 
          * a = attribute (contains name)
-         *
+         * 
          * k = node key
          */
         final HashMap<String, MegaFolder> folders = new HashMap<String, MegaFolder>();
