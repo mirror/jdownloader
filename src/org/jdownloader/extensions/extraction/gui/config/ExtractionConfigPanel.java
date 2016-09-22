e276fc0925b05f5bcc392c35d91dff2be73ea336
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-09-22 15:47:54 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34996 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 75e88e71dc771fb16ef82fb595eb291e48d37fef

--- src/org/jdownloader/extensions/extraction/gui/config/ExtractionConfigPanel.java
+++ src/org/jdownloader/extensions/extraction/gui/config/ExtractionConfigPanel.java
@@ -67,8 +67,7 @@ public class ExtractionConfigPanel extends ExtensionConfigPanel<ExtractionExtens
             final Method method = SevenZip.class.getMethod("getSevenZipJBindingVersion");
             return (String) method.invoke(null, new Object[0]);
         } catch (Throwable e) {
-            e.printStackTrace();
-            return "4.65 " + e.getMessage();
+            return "4.65";
         }
     }
 
