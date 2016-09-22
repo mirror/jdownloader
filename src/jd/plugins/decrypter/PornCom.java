b0f607b60c291b7b7198778f24b84421487da396
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-09-22 03:10:46 +0000

    Update revision property
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@34987 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c7559f4caff78a4f1277ab99819b9a55179593f7

--- src/jd/plugins/decrypter/PornCom.java
+++ src/jd/plugins/decrypter/PornCom.java
@@ -21,7 +21,7 @@ import jd.plugins.PluginForDecrypt;
 import org.appwork.utils.Files;
 import org.appwork.utils.logging2.LogSource;
 
-@DecrypterPlugin(revision = "$Revision: 31682 $", interfaceVersion = 3, names = { "porn.com" }, urls = { "http://(www\\.)?porn\\.com/videos/[^<>\"/]+-\\d+(\\.html)?" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "porn.com" }, urls = { "http://(www\\.)?porn\\.com/videos/[^<>\"/]+-\\d+(\\.html)?" })
 public class PornCom extends PluginForDecrypt {
 
     public PornCom(PluginWrapper wrapper) {
