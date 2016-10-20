cf3180639be02dc0445c4cfbe32def7388bbdfe6
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-10-20 14:38:44 +0000

    ScriptEnvironment: -added BrowserSandBox BrowserSandBox: -added Browser support for Eventscripter -re mpgai
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35193 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/extensions/eventscripter/sandboxobjects/BrowserSandBox.java
@@ -0,0 +1,60 @@
+package org.jdownloader.extensions.eventscripter.sandboxobjects;
+
+import java.io.IOException;
+
+import jd.http.Browser;
+
+import org.jdownloader.extensions.eventscripter.EnvironmentException;
+
+public class BrowserSandBox {
+
+    private final Browser br;
+
+    public BrowserSandBox() {
+        this.br = new Browser();
+    }
+
+    private BrowserSandBox(Browser br) {
+        if (br != null) {
+            this.br = br;
+        } else {
+            this.br = new Browser();
+        }
+    }
+
+    public String getPage(final String url) throws EnvironmentException {
+        try {
+            return br.getPage(url);
+        } catch (IOException e) {
+            throw new EnvironmentException(e);
+        }
+    }
+
+    public BrowserSandBox cloneBrowser() {
+        return new BrowserSandBox(br.cloneBrowser());
+    }
+
+    public String postPage(final String url, final String postData) throws EnvironmentException {
+        try {
+            return br.postPage(url, postData);
+        } catch (IOException e) {
+            throw new EnvironmentException(e);
+        }
+    }
+
+    public String getURL() {
+        return br.getURL();
+    }
+
+    public String getHTML() {
+        return br.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        final Browser br = this.br;
+        if (br != null) {
+            br.disconnect();
+        }
+    }
+}
