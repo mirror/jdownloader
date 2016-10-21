291e15400b3ff38427a8746f3252dc1e8f5c0989
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-10-21 14:11:35 +0000

    XArt: -refactored to use PluginConfigInterface -added best videos/best images settings -updated support to refresh expired links -updated login to check if cookies are still valid -refactored login to use customized PostRequest -fixes thread 71217
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35206 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: baad394659a1ce4837c64604d0a839ef53c13dd3

--- src/jd/plugins/decrypter/XArt.java
+++ src/jd/plugins/decrypter/XArt.java
@@ -17,27 +17,37 @@
 package jd.plugins.decrypter;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 import jd.PluginWrapper;
-import jd.config.SubConfiguration;
 import jd.controlling.AccountController;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
+import jd.plugins.hoster.XArtCom;
+import jd.plugins.hoster.XArtCom.XArtConfigInterface;
 import jd.utils.JDUtilities;
 
+import org.appwork.utils.Files;
 import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.plugins.config.PluginJsonConfig;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "x-art.com" }, urls = { "^https?://(www\\.)?x-art\\.com/(members/)?.+" })
 public class XArt extends PluginForDecrypt {
@@ -46,26 +56,26 @@ public class XArt extends PluginForDecrypt {
         super(wrapper);
     }
 
-    private SubConfiguration cfg = null;
-
-    @Override
-    protected DownloadLink createDownloadlink(String link) {
-        final String original_url = link;
-        link = link.replaceFirst("x-art\\.com", "x-artdecrypted.com");
-        final DownloadLink ret = super.createDownloadlink(link);
-        ret.setContentUrl(original_url);
+    private static DownloadLink createDownloadlink(final Plugin plugin, final String contentURL, String url) throws MalformedURLException {
+        final DownloadLink ret = new DownloadLink(null, null, plugin.getHost(), url.replaceFirst("x-art\\.com", "x-artdecrypted.com"), true);
+        ret.setContentUrl(contentURL);
+        ret.setProperty("pageURL", contentURL);
+        ret.setFinalFileName(Plugin.getFileNameFromURL(new URL(url)));
         return ret;
     }
 
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
-        cfg = SubConfiguration.getConfig(this.getHost());
-        ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>() {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
+
             public boolean add(final DownloadLink e) {
                 try {
                     distribute(e);
                 } catch (Throwable ee) {
-
                 }
                 return super.add(e);
             };
@@ -79,20 +89,19 @@ public class XArt extends PluginForDecrypt {
             }
         };
         String url = parameter.getCryptedUrl();
-
         if (!url.contains("/members/") && !url.contains("/download/")) {
             url = url.replaceAll("x-art\\.com\\/", "x-art.com/members/");
         }
         if (url.matches("(https?://([^\r\n\t\"']+\\.)?x-art\\.com/[^\r\n\t\"']+\\.(mp4|wmv|mov|zip)[^\r\n\t\"']*)")) {
-            ret.add(createDownloadlink(url));
+            ret.add(createDownloadlink(this, url, url));
             return ret;
         }
         boolean prem = false;
-        final ArrayList<Account> accounts = AccountController.getInstance().getAllAccounts("x-art.com");
+        final ArrayList<Account> accounts = AccountController.getInstance().getValidAccounts(getHost());
         if (accounts != null && accounts.size() != 0) {
-            Iterator<Account> it = accounts.iterator();
+            final Iterator<Account> it = accounts.iterator();
             while (it.hasNext()) {
-                Account n = it.next();
+                final Account n = it.next();
                 if (n.isEnabled() && n.isValid()) {
                     prem = this.login(n);
                     break;
@@ -104,56 +113,81 @@ public class XArt extends PluginForDecrypt {
             return ret;
         }
         if (url.matches("^https?://(www\\.)?x-art\\.com/(members/)?(videos|galleries)/[a-zA0-9\\-\\_]+/?$")) {
-            parseUrl(ret, url);
+            parseUrl(br, this, ret, url, false);
         } else {
             br.getPage(url);
-            String[] sub = br.getRegex("(https?://(www\\.)?x-art\\.com/(members/)?(videos|galleries)/[a-zA0-9\\-\\_]+/?)").getColumn(0);
-            HashSet<String> dupe = new HashSet<String>();
-            for (String s : sub) {
+            final String[] subs = br.getRegex("(https?://(www\\.)?x-art\\.com/(members/)?(videos|galleries)/[a-zA0-9\\-\\_]+/?)").getColumn(0);
+            final HashSet<String> dupe = new HashSet<String>();
+            for (final String sub : subs) {
                 try {
-                    if (isAbort()) {
-                        return ret;
+                    if (!isAbort()) {
+                        if (dupe.add(sub)) {
+                            parseUrl(br, this, ret, sub, false);
+                        }
                     }
                 } catch (Throwable ee) {
-
-                }
-
-                if (dupe.add(s)) {
-                    parseUrl(ret, s);
                 }
             }
-
         }
         return ret;
     }
 
-    public void parseUrl(final ArrayList<DownloadLink> ret, final String url) throws IOException {
+    public static void parseUrl(final Browser br, final Plugin plugin, final ArrayList<DownloadLink> ret, final String url, boolean returnAll) throws IOException {
         br.setFollowRedirects(true);
-
         br.getPage(url);
+        final XArtConfigInterface cfg = PluginJsonConfig.get(XArtCom.XArtConfigInterface.class);
         String title = br.getRegex("<div class=\"small-12 medium-12 large-12 columns\">\\s*<h1>([a-zA-Z0-9\\_\\-\\ ]*)<\\/h1>").getMatch(0);
         if (url.matches("^https?://(www\\.)?x-art\\.com/(members/)?galleries/[a-zA0-9\\-\\_]+/?$")) {
-            ArrayList<DownloadLink> dlinks = new ArrayList<DownloadLink>();
-            String links[] = br.getRegex("href=\"([a-zA-Z0-9\\_\\-]*\\.(mp4|wmv|mov|zip))\"").getColumn(0);
-            String lnks[] = br.getRegex("href=(\"|')(https?://([^\r\n\t\"']+\\.)?x-art\\.com/[^\r\n\t\"']+\\.(mp4|wmv|mov|zip)[^\r\n\t\"']*)").getColumn(1);
+            final String fid = new Regex(url, "/galleries/([^/]+)").getMatch(0);
+            final FilePackage fp;
+            if (title != null) {
+                fp = FilePackage.getInstance();
+                fp.setName(title);
+            } else {
+                fp = null;
+            }
+            final String links[] = br.getRegex("href=\"([a-zA-Z0-9\\_\\-]*\\.(mp4|wmv|mov|zip))\"").getColumn(0);
+            final String lnks[] = br.getRegex("href=(\"|')(https?://([^\r\n\t\"']+\\.)?x-art\\.com/[^\r\n\t\"']+\\.(mp4|wmv|mov|zip)[^\r\n\t\"']*)").getColumn(1);
             if (links != null) {
                 for (String link : links) {
-                    DownloadLink dl = createDownloadlink(br.getURL(link).toString());
-                    dlinks.add(dl);
+                    final DownloadLink dl = createDownloadlink(plugin, url, br.getURL(link).toString());
+                    dl.setAvailable(true);
+                    if (fp != null) {
+                        fp.add(dl);
+                    }
+                    ret.add(dl);
                 }
             }
             if (lnks != null) {
+                final ArrayList<DownloadLink> results = new ArrayList<DownloadLink>();
+                final HashMap<String, DownloadLink> qualities = new HashMap<String, DownloadLink>();
                 for (final String link : lnks) {
                     final String quality_url = new Regex(link, "([A-Za-z]+)\\.zip").getMatch(0);
-                    if (quality_url != null && !cfg.getBooleanProperty("GRAB_" + quality_url, true)) {
-                        /* Skip unwanted content */
-                        continue;
-                    }
-                    final DownloadLink dl = createDownloadlink((link));
-                    dlinks.add(dl);
+                    final DownloadLink dl = createDownloadlink(plugin, url, link);
+                    dl.setAvailable(true);
+                    dl.setProperty("imageID", fid);
+                    dl.setProperty("quality", quality_url);
+                    dl.setLinkID(plugin.getHost() + "://zip/" + fid + "/" + quality_url);
+                    qualities.put(quality_url, dl);
                 }
+                final boolean allQualities = returnAll || (!cfg.isGrab1200pImagesVersionEnabled() && !cfg.isGrab2000pImagesVersionEnabled() && !cfg.isGrab4000pImagesVersionEnabled());
+                final boolean bestOnly = !returnAll && cfg.isGrabBestImagesVersionEnabled();
+
+                if ((allQualities || cfg.isGrab4000pImagesVersionEnabled()) && qualities.containsKey("lrg")) {
+                    results.add(qualities.get("lrg"));
+                }
+                if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab2000pImagesVersionEnabled()) && qualities.containsKey("med")) {
+                    results.add(qualities.get("med"));
+                }
+                if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab1200pImagesVersionEnabled()) && qualities.containsKey("sml")) {
+                    results.add(qualities.get("sml"));
+                }
+
+                if (fp != null) {
+                    fp.addLinks(results);
+                }
+                ret.addAll(results);
             }
-            ret.addAll(dlinks);
         } else {
             /* Decrypt video */
             final String fid = new Regex(url, "/videos/([^/]+)").getMatch(0);
@@ -164,35 +198,65 @@ public class XArt extends PluginForDecrypt {
                 /* Fallback */
                 title = fid;
             }
-            final String htmldownload = this.br.getRegex("<ul[^>]*?id=\"drop\\-download\"[^>]*?>(.*?)</ul>").getMatch(0);
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(title);
+            final String htmldownload = br.getRegex("<ul[^>]*?id=\"drop\\-download\"[^>]*?>(.*?)</ul>").getMatch(0);
             final String[] dlinfo = htmldownload.split("</li>");
+            final HashMap<String, List<DownloadLink>> qualities = new HashMap<String, List<DownloadLink>>();
+            final ArrayList<DownloadLink> results = new ArrayList<DownloadLink>();
             for (final String video : dlinfo) {
                 final String dlurl = new Regex(video, "(/download/[^<>\"]+)\"").getMatch(0);
                 final String filesize = new Regex(video, "\\((\\d+(?:\\.\\d+)? [A-Za-z]{2,5})\\)").getMatch(0);
-                final String quality_url = dlurl != null ? new Regex(dlurl, "([A-Za-z0-9]+)\\.mp4").getMatch(0) : null;
+                final String quality_url = dlurl != null ? new Regex(dlurl, "([A-Za-z0-9]+)\\.(mp4|mov|wmv)").getMatch(0) : null;
                 if (dlurl == null || quality_url == null) {
                     continue;
                 }
-                if (!cfg.getBooleanProperty("GRAB_" + quality_url, true)) {
-                    /* Skip unwanted content */
-                    continue;
-                }
-                final DownloadLink dl = this.createDownloadlink(br.getURL(dlurl).toString());
+                final URL finalURL = br.getURL(dlurl);
+                final DownloadLink dl = createDownloadlink(plugin, url, finalURL.toString());
+                dl.setAvailable(true);
                 if (filesize != null) {
                     dl.setDownloadSize(SizeFormatter.getSize(filesize));
-                    dl.setAvailable(true);
                 }
-                dl.setName(title + "_" + quality_url + ".mp4");
-                dl.setProperty("fid", fid);
+                String ext = Files.getExtension(finalURL.getPath());
+                if (ext == null) {
+                    ext = "mp4";
+                }
+                dl.setFinalFileName(title + "_" + quality_url + "." + ext);
+                dl.setProperty("videoID", fid);
+                dl.setProperty("ext", ext);
                 dl.setProperty("quality", quality_url);
-                ret.add(dl);
+                dl.setLinkID(plugin.getHost() + "://" + ext + "/" + fid + "/" + quality_url);
+                List<DownloadLink> list = qualities.get(quality_url.toLowerCase(Locale.ENGLISH));
+                if (list == null) {
+                    list = new ArrayList<DownloadLink>();
+                    qualities.put(quality_url.toLowerCase(Locale.ENGLISH), list);
+                }
+                list.add(dl);
             }
+            final boolean allQualities = returnAll || (!cfg.isGrab4KVideoEnabled() && !cfg.isGrab1080pVideoEnabled() && !cfg.isGrab720pVideoEnabled() && !cfg.isGrab540pVideoEnabled() && !cfg.isGrab360pVideoEnabled());
+            final boolean bestOnly = !returnAll && cfg.isGrabBestVideoVersionEnabled();
+
+            if ((allQualities || cfg.isGrab4KVideoEnabled()) && qualities.containsKey("4k")) {
+                results.addAll(qualities.get("4k"));
+            }
+            if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab1080pVideoEnabled()) && qualities.containsKey("1080")) {
+                results.addAll(qualities.get("1080"));
+            }
+            if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab720pVideoEnabled()) && qualities.containsKey("720")) {
+                results.addAll(qualities.get("720"));
+            }
+            if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab540pVideoEnabled()) && qualities.containsKey("540")) {
+                results.addAll(qualities.get("540"));
+            }
+            if ((!bestOnly || results.isEmpty()) && (allQualities || cfg.isGrab540pVideoEnabled()) && qualities.containsKey("360")) {
+                results.addAll(qualities.get("360"));
+            }
+            if (fp != null) {
+                fp.addLinks(results);
+            }
+            ret.addAll(results);
         }
-        if (title != null) {
-            FilePackage fp = FilePackage.getInstance();
-            fp.setName(title);
-            fp.addLinks(ret);
-        }
+
     }
 
     private boolean login(final Account account) throws Exception {
@@ -205,15 +269,11 @@ public class XArt extends PluginForDecrypt {
                 return false;
             }
         } catch (final PluginException e) {
+            logger.log(e);
             account.setValid(false);
             return false;
         }
         return true;
-    }
-
-    /* NO OVERRIDE!! */
-    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
-        return false;
     }
 
 }
