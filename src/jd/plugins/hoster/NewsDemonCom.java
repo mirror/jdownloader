029a74a86c39781c55a1626af165c341d09c439e
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-10-28 11:42:49 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35254 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d0a5a065b1fe5f1588c93c7af30b4ddfc74d8c38

--- src/jd/plugins/hoster/NewsDemonCom.java
+++ src/jd/plugins/hoster/NewsDemonCom.java
@@ -114,6 +114,7 @@ public class NewsDemonCom extends UseNet {
             }
             throw e;
         }
+        account.setProperty(Account.PROPERTY_REFRESH_TIMEOUT, 2 * 60 * 60 * 1000l);
         ai.setProperty("multiHostSupport", Arrays.asList(new String[] { "usenet" }));
         return ai;
     }
