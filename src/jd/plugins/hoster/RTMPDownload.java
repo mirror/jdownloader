d38990e2809ed8bddb649eaf0a00586baf611d93
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-11-22 08:43:32 +0000

    RTMPDownload: -updated to auto resume for isReadPacketError/isTimeoutError -fixes thread 71633 RtmpDump: -added method isReadPacketError/isTimeoutError -updated error handling
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35478 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4698561c7ab8667094adcacb30890b6cd607b5d5

--- src/jd/plugins/hoster/RTMPDownload.java
+++ src/jd/plugins/hoster/RTMPDownload.java
@@ -7,6 +7,7 @@ import jd.http.Request;
 import jd.network.rtmp.RtmpDump;
 import jd.network.rtmp.url.RtmpUrlConnection;
 import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.components.RTMPStreamHandler;
@@ -27,9 +28,9 @@ public class RTMPDownload extends RAFDownload {
 
     private final URL                 url;
     // don't name it plugin!
-    protected PluginForHost           plg;
+    protected final PluginForHost     plg;
     // don't name it downloadLink
-    protected DownloadLink            dLink;
+    protected final DownloadLink      dLink;
 
     public RTMPDownload(final PluginForHost plugin, final DownloadLink downloadLink, final String rtmpURL) throws IOException, PluginException {
         super(plugin, downloadLink, null);
@@ -57,7 +58,24 @@ public class RTMPDownload extends RAFDownload {
         }
         finalfilename = finalfilename.replace("⁄", "_");
         this.plg.getDownloadLink().setFinalFileName(finalfilename);
-        return rtmpDump().start(rtmpConnection);
+        long before = 0;
+        while (!externalDownloadStop()) {
+            before = Math.max(before, downloadable.getDownloadBytesLoaded());
+            final RtmpDump rtmpDump = rtmpDump();
+            try {
+                return rtmpDump.start(rtmpConnection);
+            } catch (PluginException e) {
+                logger.log(e);
+                if (e.getLinkStatus() == LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE && (rtmpDump.isReadPacketError(e.getMessage()) || rtmpDump.isTimeoutError(e.getMessage()))) {
+                    if (downloadable.getDownloadBytesLoaded() > before && rtmpConnection.isResume()) {
+                        Thread.sleep(1000);
+                        continue;
+                    }
+                }
+                throw e;
+            }
+        }
+        return false;
     }
 
     public String getRtmpDumpChecksum() throws Exception {
