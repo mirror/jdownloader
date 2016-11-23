7a59cee34ee0cab8199e84cd0c94862803d80621
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-11-23 15:12:21 +0000

    HlsContainer: -updated getHlsQualities -fixes thread 71644
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35496 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1bc9472c29829ec3cc56e75592bea0d88cae2197

--- src/org/jdownloader/plugins/components/hls/HlsContainer.java
+++ src/org/jdownloader/plugins/components/hls/HlsContainer.java
@@ -6,6 +6,7 @@ import java.util.List;
 import jd.http.Browser;
 
 import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
 
 public class HlsContainer {
 
@@ -27,63 +28,52 @@ public class HlsContainer {
 
     public static List<HlsContainer> getHlsQualities(final Browser br) throws Exception {
         final ArrayList<HlsContainer> hlsqualities = new ArrayList<HlsContainer>();
-        final String[] medias = br.getRegex("#EXT-X-STREAM-INF([^\r\n]+[\r\n]+[^\r\n]+)").getColumn(-1);
-        if (medias == null) {
+        final String[][] streams = br.getRegex("#EXT-X-STREAM-INF:?([^\r\n]+)[\r\n]+([^\r\n]+)").getMatches();
+        if (streams == null) {
             return null;
         }
-        for (final String media : medias) {
-            // name = quality
-            // final String quality = new Regex(media, "NAME=\"(.*?)\"").getMatch(0);
-            String hlsurl = null;
-            final String bandwidth = new Regex(media, "BANDWIDTH=(\\d+)").getMatch(0);
-            final String resolution = new Regex(media, "RESOLUTION=(\\d+x\\d+)").getMatch(0);
-            final String codecs = new Regex(media, "CODECS=\"([^<>\"]+)\"").getMatch(0);
-            final String[] lines = Regex.getLines(media);
-            for (final String line : lines) {
-                if (line.contains(".m3u8")) {
-                    hlsurl = line;
-                    break;
+        for (final String stream[] : streams) {
+            if (StringUtils.isNotEmpty(stream[1])) {
+                final String streamInfo = stream[0];
+                // name = quality
+                // final String quality = new Regex(media, "NAME=\"(.*?)\"").getMatch(0);
+                final String bandwidth = new Regex(streamInfo, "BANDWIDTH=(\\d+)").getMatch(0);
+                final String resolution = new Regex(streamInfo, "RESOLUTION=(\\d+x\\d+)").getMatch(0);
+                final String codecs = new Regex(streamInfo, "CODECS=\"([^<>\"]+)\"").getMatch(0);
+                final String url = br.getURL(stream[1]).toString();
+                final HlsContainer hls = new HlsContainer();
+                if (bandwidth != null) {
+                    hls.bandwidth = Long.parseLong(bandwidth);
+                } else {
+                    hls.bandwidth = -1;
                 }
+                if (codecs != null) {
+                    hls.codecs = codecs.trim();
+                }
+                hls.downloadurl = url;
+                if (resolution != null) {
+                    final String[] resolution_info = resolution.split("x");
+                    final String width = resolution_info[0];
+                    final String height = resolution_info[1];
+                    hls.width = Integer.parseInt(width);
+                    hls.height = Integer.parseInt(height);
+                }
+                hlsqualities.add(hls);
             }
-            if (bandwidth == null || hlsurl == null) {
-                continue;
-            }
-            hlsurl = hlsurl.trim();
-            if (!hlsurl.startsWith("http")) {
-                hlsurl = br.getBaseURL() + hlsurl;
-            }
-            final HlsContainer hls = new HlsContainer();
-            hls.bandwidth = Long.parseLong(bandwidth);
-            hls.codecs = codecs;
-            hls.downloadurl = hlsurl;
-            /* TODO: Add audio */
-            hls.type = "video";
-
-            if (resolution != null) {
-                final String[] resolution_info = resolution.split("x");
-                final String width = resolution_info[0];
-                final String height = resolution_info[1];
-                hls.width = Integer.parseInt(width);
-                hls.height = Integer.parseInt(height);
-            }
-
-            hlsqualities.add(hls);
         }
         return hlsqualities;
     }
 
-    public static final String ext = ".mp4";
-    public String              codecs;
-    public String              downloadurl;
+    public String codecs;
+    public String downloadurl;
 
     public String getDownloadurl() {
         return downloadurl;
     }
 
-    public String type;
-    public int    width;
-    public int    height;
-    public long   bandwidth;
+    public int  width     = -1;
+    public int  height    = -1;
+    public long bandwidth = -1;
 
     public String getResolution() {
         return width + "x" + height;
@@ -94,15 +84,24 @@ public class HlsContainer {
 
     @Override
     public String toString() {
-        return codecs + "_" + width + "x" + height;
+        return getStandardFilename();
     }
 
     public String getStandardFilename() {
-        String filename = width + "x" + height;
+        String filename = "";
+        if (width != -1 && height != -1) {
+            filename += getResolution();
+        }
         if (codecs != null) {
             filename += "_" + codecs;
         }
-        filename += ext;
+        if (StringUtils.equalsIgnoreCase(codecs, "mp4a.40.34")) {
+            filename += ".mp3";
+        } else if (StringUtils.equalsIgnoreCase(codecs, "mp4a.40.5") || StringUtils.equalsIgnoreCase(codecs, "mp4a.40.2")) {
+            filename += ".aac";
+        } else {
+            filename += ".mp4";
+        }
         return filename;
     }
 
