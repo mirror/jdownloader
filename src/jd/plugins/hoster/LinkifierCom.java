4aca2347b3f3ccd2abfd44041ded890c014fdb0a
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-11-24 11:01:11 +0000

    Linkifier:add support for this multihoster
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35515 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4ccc7a4faf4c7a69569c74616df07f8f4a9df6b6

---
+++ src/jd/plugins/hoster/LinkifierCom.java
@@ -0,0 +1,144 @@
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jd.PluginWrapper;
+import jd.http.requests.PostRequest;
+import jd.plugins.Account;
+import jd.plugins.AccountInfo;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Hash;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.controller.host.LazyHostPlugin.FEATURE;
+
+@HostPlugin(revision = "$Revision: 35512 $", interfaceVersion = 3, names = { "linkifier.com" }, urls = { "" })
+public class LinkifierCom extends PluginForHost {
+
+    private static final String API_KEY = "d046c4309bb7cabd19f49118a2ab25e0";
+
+    public LinkifierCom(PluginWrapper wrapper) {
+        super(wrapper);
+        enablePremium("https://www.linkifier.com");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.linkifier.com/terms-of-use/";
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        final String username = account.getUser();
+        if (username == null || !username.matches("^.+?@.+?\\.[^\\.]+")) {
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, "Please use your email address to login", PluginException.VALUE_ID_PREMIUM_DISABLE);
+        }
+        final AccountInfo ai = new AccountInfo();
+        final HashMap<String, Object> userJson = new HashMap<String, Object>();
+        userJson.put("login", username);
+        userJson.put("md5Pass", Hash.getMD5(account.getPass()));
+        userJson.put("apiKey", API_KEY);
+        final PostRequest userRequest = new PostRequest("https://api.linkifier.com/downloadapi.svc/user");
+        userRequest.setContentType("application/json; charset=utf-8");
+        userRequest.setPostBytes(JSonStorage.serializeToJsonByteArray(userJson));
+        final HashMap<String, Object> userResponse = JSonStorage.restoreFromString(br.getPage(userRequest), TypeRef.HASHMAP);
+        if (Boolean.TRUE.equals(userResponse.get("isActive")) && !Boolean.TRUE.equals(userResponse.get("hasErrors"))) {
+            if ("unlimited".equalsIgnoreCase(String.valueOf(userResponse.get("extraTraffic")))) {
+                ai.setUnlimitedTraffic();
+            }
+            final Number expiryDate = (Number) userResponse.get("expirydate");
+            if (expiryDate != null) {
+                ai.setValidUntil(expiryDate.longValue());
+                if (!ai.isExpired()) {
+                    final PostRequest hosterRequest = new PostRequest("https://api.linkifier.com/DownloadAPI.svc/hosters");
+                    hosterRequest.setContentType("application/json; charset=utf-8");
+                    hosterRequest.setPostBytes(JSonStorage.serializeToJsonByteArray(userJson));
+                    final HashMap<String, Object> hosterResponse = JSonStorage.restoreFromString(br.getPage(hosterRequest), TypeRef.HASHMAP);
+                    final List<Map<String, Object>> hosters = (List<Map<String, Object>>) hosterResponse.get("hosters");
+                    if (hosters != null) {
+                        final List<String> supportedHosts = new ArrayList<String>();
+                        for (Map<String, Object> host : hosters) {
+                            final String hostername = host.get("hostername") != null ? String.valueOf(host.get("hostername")) : null;
+                            if (Boolean.TRUE.equals(host.get("isActive")) && StringUtils.isNotEmpty(hostername)) {
+                                supportedHosts.add(hostername);
+                            }
+                        }
+                        ai.setMultiHostSupport(this, supportedHosts);
+                    }
+                    return ai;
+                }
+            }
+        }
+        final String errorMsg = userResponse.get("ErrorMSG") != null ? String.valueOf(userResponse.get("ErrorMSG")) : null;
+        if (StringUtils.isNotEmpty(errorMsg)) {
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, errorMsg, PluginException.VALUE_ID_PREMIUM_DISABLE);
+        }
+        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink parameter) throws Exception {
+        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+    }
+
+    @Override
+    public void handleMultiHost(DownloadLink downloadLink, Account account) throws Exception {
+        final HashMap<String, Object> downloadJson = new HashMap<String, Object>();
+        downloadJson.put("login", account.getUser());
+        downloadJson.put("md5Pass", Hash.getMD5(account.getPass()));
+        downloadJson.put("apiKey", API_KEY);
+        downloadJson.put("url", downloadLink.getDefaultPlugin().buildExternalDownloadURL(downloadLink, this));
+        final PostRequest downloadRequest = new PostRequest("https://api.linkifier.com/downloadapi.svc/download");
+        downloadRequest.setContentType("application/json; charset=utf-8");
+        downloadRequest.setPostBytes(JSonStorage.serializeToJsonByteArray(downloadJson));
+        final HashMap<String, Object> downloadResponse = JSonStorage.restoreFromString(br.getPage(downloadRequest), TypeRef.HASHMAP);
+        if (Boolean.FALSE.equals(downloadResponse.get("hasErrors"))) {
+            final String url = downloadResponse.get("url") != null ? String.valueOf(downloadResponse.get("url")) : null;
+            if (StringUtils.isEmpty(url)) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, url, true, 0);
+            if (StringUtils.containsIgnoreCase(dl.getConnection().getContentType(), "json") || StringUtils.containsIgnoreCase(dl.getConnection().getContentType(), "text")) {
+                br.followConnection();
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            if (!dl.getConnection().isContentDisposition()) {
+                dl.getConnection().disconnect();
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dl.startDownload();
+            return;
+        }
+        final String errorMsg = downloadResponse.get("ErrorMSG") != null ? String.valueOf(downloadResponse.get("ErrorMSG")) : null;
+        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, errorMsg);
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+    @Override
+    public FEATURE[] getFeatures() {
+        return new FEATURE[] { FEATURE.MULTIHOST };
+    }
+
+}
