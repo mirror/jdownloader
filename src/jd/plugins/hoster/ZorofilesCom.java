7fc3cf16882aa45fbf63e456508aee93c40f1fbe
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-12-02 06:25:41 +0000

    Zorofiles.com - Adjustment fixes #81963
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35599 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ZorofilesCom.java
+++ src/jd/plugins/hoster/ZorofilesCom.java
@@ -26,10 +26,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
-import org.appwork.utils.formatter.SizeFormatter;
-import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
-
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.http.Browser;
@@ -50,6 +46,10 @@ import jd.plugins.PluginForHost;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.utils.JDUtilities;
 import jd.utils.locale.JDL;
+
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "zorofiles.com" }, urls = { "https?://(www\\.)?zorofiles\\.com/(embed\\-)?[a-z0-9]{12}" })
 public class ZorofilesCom extends PluginForHost {
@@ -177,7 +177,7 @@ public class ZorofilesCom extends PluginForHost {
         altbr = br.cloneBrowser();
         setFUID(link);
         getPage(link.getDownloadURL());
-        if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
+        if (new Regex(correctedBR, "(File Not Found|No such file|>Oops! Something went wrong|Reason for deletion:\n|>The file expired)|>The file was removed").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         if (new Regex(correctedBR, HTML_MAINTENANCE_MODE).matches()) {
