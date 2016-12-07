3c6b54fbf6ebd4e809f6ab7bf73aaa1ec3e57ca3
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-12-07 13:04:11 +0000

    Prefiles.com - Adjustment fixes #82065
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35623 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PreFilesCom.java
+++ src/jd/plugins/hoster/PreFilesCom.java
@@ -137,7 +137,7 @@ public class PreFilesCom extends antiDDoSForHost {
         this.setBrowserExclusive();
         br.setFollowRedirects(false);
         getPage(link.getDownloadURL());
-        if (new Regex(correctedBR, ">The file you were looking for could not be found").matches()) {
+        if (new Regex(correctedBR, ">(Sorry! )?The file you were looking for could not be found").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         if (correctedBR.contains(MAINTENANCE)) {
