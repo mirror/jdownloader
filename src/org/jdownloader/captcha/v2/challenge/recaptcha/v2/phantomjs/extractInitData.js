f3ef931a6cd2a4c497269016911b78e37e70b0c1
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-12-12 08:42:01 +0000

    Should catch unsupported or unknown challenges
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35655 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5055261bf347f6ba095c8b2a3374d95a7a11ce5d

--- src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/phantomjs/extractInitData.js
+++ src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/phantomjs/extractInitData.js
@@ -1,41 +1,53 @@
 (function() {
-	var initData = eval(initScript);
-	initData = eval(initData);
-	if("multicaptcha"==initData[5][5]){
-		ret= {};
-		ret.x=initData[5][4][5][0][0][4];
-		ret.y=initData[5][4][5][0][0][3];
-		ret.challengeType=initData[5][5];
+	try {
+		var initData = eval(initScript);
+		initData = eval(initData);
+		if ("multicaptcha" == initData[5][5]) {
+			ret = {};
+			ret.x = initData[5][4][5][0][0][4];
+			ret.y = initData[5][4][5][0][0][3];
+			ret.challengeType = initData[5][5];
+			return ret;
+		} else if ("nocaptcha" == initData[5][5]) {
+			ret = {};
+			ret.x = -1;
+			ret.y = -1;
+			ret.challengeType = initData[5][5];
+			return ret;
+		}
+		ret = {};
+		ret.contentType = initData[5][4][1][0];
+		ret.x = initData[5][4][1][4];
+		ret.y = initData[5][4][1][3];
+		ret.challengeType = initData[5][5];
+		if (typeof (initData[5][4][1][1]) !== 'undefined') {
+			ret.explainUrl = [ 5 ][4][1][1];
+		}
+
+		return ret;
+	} catch (err) {
+		ret = {};
+		ret.x = -1;
+		ret.y = -1;
+		ret.challengeType = "unknown";
 		return ret;
 	}
-	ret= {};
-	ret.contentType=initData[5][4][1][0];
-	ret.x=initData[5][4][1][4];
-	ret.y=initData[5][4][1][3];
-	ret.challengeType=initData[5][5];
-	if(	typeof(initData[5][4][1][1]) !== 'undefined' ){
-		ret.explainUrl=[5][4][1][1];
-	}
-
-
-	return ret;
-	
-//	 var Ko = function(a) {
-//	        switch (a) {
-//	        case "default":
-//	            return new mo;
-//	        case "nocaptcha":
-//	            return new Eo;
-//	        case "imageselect":
-//	            return new so;
-//	        case "tileselect":
-//	            return new so;
-//	        case "dynamic":
-//	            return new xo;
-//	        case "audio":
-//	            return new go;
-//	        case "text":
-//	            return new Jo
-//	        }
-//	    }
+	// var Ko = function(a) {
+	// switch (a) {
+	// case "default":
+	// return new mo;
+	// case "nocaptcha":
+	// return new Eo;
+	// case "imageselect":
+	// return new so;
+	// case "tileselect":
+	// return new so;
+	// case "dynamic":
+	// return new xo;
+	// case "audio":
+	// return new go;
+	// case "text":
+	// return new Jo
+	// }
+	// }
 })()
