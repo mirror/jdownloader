60e07205e88633432b334442ac152c7311c62f74
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2016-12-22 16:28:05 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35747 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: ad9afa98dbbd89e5ec5d51ac8f151d7e6ffd083e

--- src/jd/gui/swing/jdgui/DonateTabHeader.java
+++ src/jd/gui/swing/jdgui/DonateTabHeader.java
@@ -4,58 +4,51 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
+import javax.swing.Icon;
 import javax.swing.Timer;
-
-import org.appwork.uio.ConfirmDialogInterface;
-import org.appwork.uio.UIOManager;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.images.IconIO;
-import org.appwork.utils.swing.dialog.ConfirmDialog;
-import org.jdownloader.gui.IconKey;
-import org.jdownloader.gui.mainmenu.DonateAction;
-import org.jdownloader.gui.translate._GUI;
-import org.jdownloader.images.AbstractIcon;
-import org.jdownloader.settings.staticreferences.CFG_GUI;
 
 import jd.gui.swing.jdgui.interfaces.JDMouseAdapter;
 import jd.gui.swing.jdgui.interfaces.View;
 import jd.gui.swing.jdgui.maintab.TabHeader;
 
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.images.IconIO;
+import org.jdownloader.gui.mainmenu.DonateAction;
+import org.jdownloader.settings.staticreferences.CFG_GUI;
+
 public class DonateTabHeader extends TabHeader implements PromotionTabHeader {
 
-    private static final String ELV_CC = "elv&cc";
-    private Timer               blinker;
+    private static final String NOTIFY_ID = "xmas.22.12.2016.1";
 
     public DonateTabHeader(final View view) {
         super(view);
-
         if (doFlash()) {
-            blinker = new Timer(1500, new ActionListener() {
-                private int i = 0;
+            final Timer blinker = new Timer(1500, new ActionListener() {
+                private int        i               = 0;
+                private final Icon iconNormal      = view.getIcon();
+                private final Icon iconTransparent = IconIO.getTransparentIcon(IconIO.toBufferedImage(iconNormal), 0.5f);
 
                 @Override
-                public void actionPerformed(ActionEvent e) {
-
+                public void actionPerformed(final ActionEvent e) {
                     if (!doFlash()) {
-                        labelIcon.setIcon(view.getIcon());
-                        blinker.stop();
-                        return;
-                    }
-                    if (i++ % 2 == 0) {
-                        labelIcon.setIcon(view.getIcon());
+                        labelIcon.setIcon(iconNormal);
+                        ((Timer) e.getSource()).stop();
                     } else {
-                        labelIcon.setIcon(IconIO.getTransparentIcon(IconIO.toBufferedImage(view.getIcon()), 0.5f));
+                        if (i++ % 2 == 0) {
+                            labelIcon.setIcon(iconNormal);
+                        } else {
+                            labelIcon.setIcon(iconTransparent);
+                        }
                     }
                 }
             });
             blinker.setRepeats(true);
             blinker.start();
         }
-
     }
 
     private boolean doFlash() {
-        return !StringUtils.equalsIgnoreCase(CFG_GUI.CFG.getDonationNotifyID(), ELV_CC);
+        return !StringUtils.equalsIgnoreCase(CFG_GUI.CFG.getDonationNotifyID(), NOTIFY_ID);
     }
 
     @Override
@@ -89,11 +82,7 @@ public class DonateTabHeader extends TabHeader implements PromotionTabHeader {
             @Override
             public void mousePressed(MouseEvent e) {
                 setShown();
-                if (doFlash()) {
-                    ConfirmDialog d = new ConfirmDialog(UIOManager.BUTTONS_HIDE_CANCEL, _GUI.T.donation_news_title(), _GUI.T.donation_news(), new AbstractIcon(IconKey.ICON_BOTTY_ROBOT_INFO, -1), _GUI.T.lit_continue(), null);
-                    UIOManager.I().show(ConfirmDialogInterface.class, d);
-                }
-                CFG_GUI.CFG.setDonationNotifyID(ELV_CC);
+                CFG_GUI.CFG.setDonationNotifyID(NOTIFY_ID);
                 new DonateAction().actionPerformed(null);
             }
 
