b6de79c55bfeaedea071ced1358f2e44bceb89de
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-01-03 11:31:31 +0000

    Added: pastehere.xyz, decrypter support - closes #82453
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35780 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5b89a759d972c32d48ecf8c8e9269613a0408991

---
+++ src/jd/plugins/decrypter/PasteHereXyz.java
@@ -0,0 +1,84 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.jdownloader.controlling.PasswordUtils;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.parser.html.HTMLParser;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+/**
+ *
+ * @version raz_Template-pastebin-201508200000
+ * @author raztoki
+ */
+@DecrypterPlugin(revision = "$Revision: 20515 $", interfaceVersion = 3, names = { "pastehere.xyz" }, urls = { "https?://(?:www\\.)?pastehere\\.xyz/\\d+/?" })
+public class PasteHereXyz extends PluginForDecrypt {
+
+    public PasteHereXyz(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.setFollowRedirects(true);
+        br.getPage(parameter);
+        /* Error handling */
+        if (br.containsHTML("<strong>Alert!</strong>\\s*Paste not found") || br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
+            return decryptedLinks;
+        }
+        String plaintxt = br.getRegex("<div[^>]+id=\"p_data\"[^>]*>(.*?)\\s*</div>\\s*</div>").getMatch(0);
+        if (plaintxt == null) {
+            logger.info("Could not find 'plaintxt' : " + parameter + ", using full browser instead.");
+            plaintxt = br.toString();
+            // return decryptedLinks;
+        }
+        final Set<String> pws = PasswordUtils.getPasswords(plaintxt);
+        final String[] links = HTMLParser.getHttpLinks(plaintxt, "");
+        if (links == null || links.length == 0) {
+            logger.info("Found no links[] from 'plaintxt' : " + parameter);
+            return decryptedLinks;
+        }
+        /* avoid recursion */
+        for (final String link : links) {
+            if (!this.canHandle(link)) {
+                final DownloadLink dl = createDownloadlink(link);
+                if (pws != null && pws.size() > 0) {
+                    dl.setSourcePluginPasswordList(new ArrayList<String>(pws));
+                }
+                decryptedLinks.add(dl);
+            }
+        }
+        return decryptedLinks;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+}
