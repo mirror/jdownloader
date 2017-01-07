42b4924f3267c21d0c75cfb450baeae35778c0cc
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-01-07 02:58:37 +0000

    Xtube.com - Adjustment fixes #82502
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35807 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/XTubeCom.java
+++ src/jd/plugins/hoster/XTubeCom.java
@@ -33,7 +33,7 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.utils.locale.JDL;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xtube.com" }, urls = { "http://(www\\.)?xtube\\.com/(video-watch/|(watch|play_re)\\.php\\?v=)[A-Za-z0-9_\\-]+" }) 
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xtube.com" }, urls = { "http://(www\\.)?xtube\\.com/(video-watch/|(watch|play_re)\\.php\\?v=)[A-Za-z0-9_\\-]+" })
 public class XTubeCom extends PluginForHost {
 
     private String              DLLINK   = null;
@@ -90,7 +90,7 @@ public class XTubeCom extends PluginForHost {
         br.setCookie(MAINPAGE, "cookie_warning", "S");
         br.setFollowRedirects(true);
         br.getPage(downloadLink.getDownloadURL());
-        if (br.getURL().contains("msg=Invalid+Video+ID") || br.containsHTML(">Video not available<|>This video has been removed from XTube") || this.br.getHttpConnection().getResponseCode() == 404) {
+        if (br.getURL().contains("msg=Invalid+Video+ID") || br.containsHTML(">Video not available<|img/removed_video|>This video has been removed from XTube") || this.br.getHttpConnection().getResponseCode() == 404) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         String filename = null;
