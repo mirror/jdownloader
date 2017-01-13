c9e29ec0faea34eba02ac5c9deb14309fd80fe14
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-01-13 23:52:18 +0000

    Added: nhentai.net, basic decrypter - closes #79934
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35862 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/NhentaiNet.java
@@ -0,0 +1,82 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jd.plugins.decrypter;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.http.Request;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+/**
+ *
+ * @author raztoki
+ *
+ */
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "nhentai.net" }, urls = { "https?://(?:www\\.)?nhentai\\.net/g/(\\d+)/" })
+public class NhentaiNet extends antiDDoSForDecrypt {
+
+    public NhentaiNet(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        getPage(parameter);
+
+        final String fpName = new Regex(parameter, this.getSupportedLinks()).getMatch(0) + " - nhentai gallery";
+
+        // images
+        final String[] imgs = br.getRegex("<img is=\"lazyload-image\" class=\"lazyload\"[^>]+data-src=\"(.*?)\"").getColumn(0);
+        if (imgs == null || imgs.length == 0) {
+            throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
+        }
+        final int numberOfPages = imgs.length;
+        final DecimalFormat df = numberOfPages > 999 ? new DecimalFormat("0000") : numberOfPages > 99 ? new DecimalFormat("000") : new DecimalFormat("00");
+
+        int i = 0;
+        for (final String img : imgs) {
+            final String link = Request.getLocation(img.replace("//t.", "//i.").replaceFirst("/(\\d+)t(\\.[a-z0-9]+)$", "/$1$2"), br.getRequest());
+            final DownloadLink dl = createDownloadlink("directhttp://" + link);
+            dl.setFinalFileName(df.format(++i) + getFileNameExtensionFromString(img, ".jpg"));
+            dl.setAvailable(true);
+            decryptedLinks.add(dl);
+        }
+
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(Encoding.htmlDecode(fpName.trim()));
+        fp.addLinks(decryptedLinks);
+        return decryptedLinks;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+}
