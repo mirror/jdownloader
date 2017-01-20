7411c7df38a0017c0a18e647b60ce37ca0ae9a36
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-01-20 18:24:38 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35899 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/YourPornSexy.java
@@ -0,0 +1,77 @@
+package jd.plugins.hoster;
+
+import jd.PluginWrapper;
+import jd.http.URLConnectionAdapter;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+import org.appwork.utils.StringUtils;
+
+@HostPlugin(revision = "$Revision: 34675 $", interfaceVersion = 2, names = { "yourporn.sexy" }, urls = { "https?://(www\\.)?yourporn\\.sexy/post/[a-fA-F0-9]{13}\\.html" })
+public class YourPornSexy extends PluginForHost {
+
+    public YourPornSexy(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return null;
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(DownloadLink link) throws Exception {
+        br.setFollowRedirects(true);
+        br.getPage(link.getDownloadURL());
+        final String title = br.getRegex("<div\\s*class='post_text'>\\s*<span>(.*?)<").getMatch(0);
+        if (title == null) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String mp4 = br.getRegex("<video id='.*?'\\s*src=(\"|')(https?://.*?\\.mp4)(\"|')").getMatch(1);
+        if (mp4 == null) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        link.setFinalFileName(title.trim() + ".mp4");
+        if (link.getVerifiedFileSize() == -1) {
+            final URLConnectionAdapter con = br.cloneBrowser().openHeadConnection(mp4);
+            try {
+                if (con.isOK() && StringUtils.containsIgnoreCase(con.getContentType(), "video")) {
+                    link.setVerifiedFileSize(con.getCompleteContentLength());
+                } else {
+                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                }
+            } finally {
+                con.disconnect();
+            }
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        final String mp4 = br.getRegex("<video id='.*?'\\s*src=(\"|')(https?://.*?\\.mp4)(\"|')").getMatch(1);
+        if (mp4 == null) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, mp4, true, 0);
+        if (dl.getConnection().getContentType().contains("html")) {
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+
+}
