bdeaafd9637c23ebbc0fba5108e76465d5f9492c
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-01-26 00:54:19 +0000

    Added: xvideos.com, https support - re: XOL-375-80173
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@35929 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 69b0f94173f5244c3bf3f7d81e2ca6d5cc38ccc3

--- src/jd/plugins/hoster/XvideosCom.java
+++ src/jd/plugins/hoster/XvideosCom.java
@@ -31,7 +31,7 @@ import jd.plugins.PluginForHost;
 import jd.utils.locale.JDL;
 
 //xvideos.com by pspzockerscene
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xvideos.com" }, urls = { "http://((www\\.)?xvideos\\.com/video[0-9]+/|\\w+\\.xvideos\\.com/embedframe/\\d+|(www\\.)?xvideos\\.com/[a-z0-9\\-]+/upload/[a-z0-9\\-]+/\\d+|(www\\.)?xvideos\\.com/prof\\-video\\-click/pornstar/[a-z0-9\\-]+/\\d+)" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "xvideos.com" }, urls = { "https?://((www\\.)?xvideos\\.com/video[0-9]+/|\\w+\\.xvideos\\.com/embedframe/\\d+|(www\\.)?xvideos\\.com/[a-z0-9\\-]+/upload/[a-z0-9\\-]+/\\d+|(www\\.)?xvideos\\.com/prof\\-video\\-click/pornstar/[a-z0-9\\-]+/\\d+)" })
 public class XvideosCom extends PluginForHost {
 
     public XvideosCom(PluginWrapper wrapper) {
@@ -71,16 +71,16 @@ public class XvideosCom extends PluginForHost {
         return -1;
     }
 
-    private static final String type_normal  = "http://(www\\.)?xvideos\\.com/video[0-9]+/";
-    private static final String type_embed   = "http://\\w+\\.xvideos\\.com/embedframe/\\d+";
-    private static final String type_special = "http://(www\\.)?xvideos\\.com/([a-z0-9\\-]+/upload/[a-z0-9\\-]+/\\d+|prof\\-video\\-click/pornstar/[a-z0-9\\-]+/\\d+)";
+    private static final String type_normal  = "https?://(www\\.)?xvideos\\.com/video[0-9]+/";
+    private static final String type_embed   = "https?://\\w+\\.xvideos\\.com/embedframe/\\d+";
+    private static final String type_special = "https?://(www\\.)?xvideos\\.com/([a-z0-9\\-]+/upload/[a-z0-9\\-]+/\\d+|prof\\-video\\-click/pornstar/[a-z0-9\\-]+/\\d+)";
     private static final String NOCHUNKS     = "NOCHUNKS";
     private String              dllink       = null;
 
     @SuppressWarnings("deprecation")
     public void correctDownloadLink(final DownloadLink link) {
         if (link.getDownloadURL().matches(type_embed) || link.getDownloadURL().matches(type_special)) {
-            link.setUrlDownload("http://www.xvideos.com/video" + new Regex(link.getDownloadURL(), "(\\d+)$").getMatch(0) + "/");
+            link.setUrlDownload(new Regex(link.getDownloadURL(), "https?://[^/]+").getMatch(-1) + "/video" + new Regex(link.getDownloadURL(), "(\\d+)$").getMatch(0) + "/");
             link.setContentUrl(link.getDownloadURL());
         }
     }
