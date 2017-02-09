4e75b77df88e093e8b99d455b80fcc39a6508bde
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-09 09:58:36 +0000

    Vidtodo.com - Adjustment fixes #82867
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36089 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/VidtodoCom.java
+++ src/jd/plugins/hoster/VidtodoCom.java
@@ -188,7 +188,7 @@ public class VidtodoCom extends antiDDoSForHost {
         correctDownloadLink(link);
         setFUID(link);
         getPage(link.getDownloadURL());
-        if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
+        if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was (deleted|removed) by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
 
