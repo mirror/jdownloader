fae0f253c831c96c09f8ae1472b0161d44ef48b2
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-13 17:55:19 +0000

    refs #82900 build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36124 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/solver/service/AbstractSolverService.java
+++ src/org/jdownloader/captcha/v2/solver/service/AbstractSolverService.java
@@ -117,22 +117,21 @@ public abstract class AbstractSolverService implements SolverService {
     public static ArrayList<SolverService> validateWaittimeQueue(SolverService start, SolverService check) {
         if (start == null || check == null) {
             return null;
+        } else {
+            return validateWaittimeQueue(start, check, new ArrayList<SolverService>(), new HashSet<SolverService>());
         }
-        return validateWaittimeQueue(start, check, new ArrayList<SolverService>(), new HashSet<SolverService>());
     }
 
     private static final Object LOCK = new Object();
 
     private static ArrayList<SolverService> validateWaittimeQueue(SolverService start, SolverService check, ArrayList<SolverService> arrayList, HashSet<SolverService> dupe) {
         synchronized (LOCK) {
-
             if (arrayList == null) {
                 arrayList = new ArrayList<SolverService>();
             }
             if (dupe == null) {
                 dupe = new HashSet<SolverService>();
             }
-
             //
             // System.out.println("Start: " + start.getName());
             // System.out.println("Check: " + check.getName());
@@ -144,14 +143,13 @@ public abstract class AbstractSolverService implements SolverService {
             // System.out.println("Added " + check.getName());
             arrayList.add(check);
             if (!dupe.add(check)) {
-
                 // System.out.println("Dupe found " + check.getName());
                 return arrayList;
             }
             // System.out.println(check.getName() + ": " + JSonStorage.serializeToJson(check.getWaitForMapCopy()));
-            for (Entry<String, Integer> es : check.getWaitForMapCopy().entrySet()) {
+            for (final Entry<String, Integer> es : check.getWaitForMapCopy().entrySet()) {
                 final SolverService service = ChallengeResponseController.getInstance().getServiceByID(es.getKey());
-                if (service != null && es.getValue() != null && es.getValue().intValue() > 0 && service.isEnabled()) {
+                if (service != null && service.isEnabled() && es.getValue() != null && es.getValue().intValue() > 0) {
                     // System.out.println(check.getName() + " waits for " + service.getName() + " : " + es.getValue().intValue());
                     final ArrayList<SolverService> ret = validateWaittimeQueue(start, service, new ArrayList<SolverService>(arrayList), new HashSet<SolverService>(dupe));
                     if (ret != null) {
