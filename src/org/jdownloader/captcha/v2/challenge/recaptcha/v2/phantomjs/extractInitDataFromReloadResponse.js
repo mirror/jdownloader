e39a8e67406e24f8eb564c5417bc8573d2852c27
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-13 11:49:58 +0000

    fixes #82894 RC2
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36116 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/phantomjs/extractInitDataFromReloadResponse.js
+++ src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/phantomjs/extractInitDataFromReloadResponse.js
@@ -1,16 +1,63 @@
 (function() {
-	var initData = reloadResponse;
+	try {
+		var initData = reloadResponse;
+		if ("multicaptcha" == initData[5]) {
+			console.log("TYPE multicaptcha");
+			ret = {};
+			ret.x = initData[4][5][0][0][4];
+			ret.y = initData[4][5][0][0][3];
+			ret.challengeType = initData[5];
+			return ret;
+		} else if ("nocaptcha" == initData[5]) {
+			console.log("TYPE NOCAPTCHA");
+			ret = {};
+			ret.x = -1;
+			ret.y = -1;
+			ret.challengeType = initData[5];
+			return ret;
+		}
+		console.log("TYPE "+initData[4][1][0]);
+		ret = {};
+		ret.contentType = initData[4][1][0];
+		ret.x = initData[4][1][4];
+		ret.y = initData[4][1][3];
+		ret.challengeType = initData[5];
+		
+		if (typeof (initData[4][1][1]) !== 'undefined') {
+			ret.explainUrl = [4][1][1];
+		}
 
-	ret= {};
-	ret.contentType=initData[4][1][0];
-	ret.x=initData[4][1][4];
-	ret.y=initData[4][1][3];
-	ret.challengeType=initData[5];
-	if(	typeof(initData[4][1][1]) !== 'undefined' ){
-		ret.explainUrl=[4][1][1];
+		return ret;
+	} catch (err) {
+		ret = {};
+		ret.x = -1;
+		ret.y = -1;
+		ret.challengeType = "unknown";
+		return ret;
 	}
-
-
-	return ret;
-	
+//	  var rq = function(a) {
+//			debugger;
+//	        switch (a) {
+//	            case "default":
+//	                return new Vp;
+//	            case "nocaptcha":
+//	                return new aq;
+//	            case "imageselect":
+//	                return new Wp;
+//	            case "tileselect":
+//	                return new Wp("tileselect");
+//	            case "dynamic":
+//	                return new wq;
+//	            case "audio":
+//	                return new Np;
+//	            case "text":
+//	                return new cq;
+//	            case "multicaptcha":
+//	                return new tq;
+//	            case "canvas":
+//	                return new hq;
+//	            case "coref":
+//	                return new Qp
+//	        }
+//	    };
 })()
