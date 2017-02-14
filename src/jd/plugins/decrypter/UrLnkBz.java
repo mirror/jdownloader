10929b75970147d6f38f65d14115320a754c3339
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-14 03:02:43 +0000

    Reviewed: urlink.biz, - Added more domains - re: UGY-500-63504 - reviewed psp code, changed it basically back to what it was. - implemented and tested proper KeyCaptcha retry code
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36125 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/UrLnkBz.java
+++ src/jd/plugins/decrypter/UrLnkBz.java
@@ -18,29 +18,41 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.appwork.utils.StringUtils;
+import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.nutils.encoding.Encoding;
 import jd.parser.html.Form;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 
-import org.appwork.utils.StringUtils;
-import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
-
 /**
  * @author raztoki
  * @author psp
+ *
+ *         Name: urlink.biz Address: 37.48.120.85<br/>
+ *         Name: ur-link.biz Address: 37.48.120.85<br/>
+ *         Name: url-ink.biz Address: 37.48.120.85 Name: url-ink.com Address: 37.48.120.85<br/>
+ *         Name: urli-nk.com Address: 37.48.120.85<br/>
+ *
  */
-@DecrypterPlugin(revision = "$Revision: 25143 $", interfaceVersion = 2, names = { "urlink.biz" }, urls = { "https?://(?:www\\.)?(?:urlink\\.biz|url\\-ink\\.biz|urli\\-nk\\.com)/[A-Za-z0-9]{4,6}" })
+@DecrypterPlugin(revision = "$Revision: 25143 $", interfaceVersion = 2, names = { "urlink.biz" }, urls = { "https?://(?:www\\.)?(?:urlink\\.biz|ur-link\\.biz|url-ink\\.biz|url-ink\\.com|urli-nk\\.com)/[A-Za-z0-9]{4,6}" })
 public class UrLnkBz extends PluginForDecrypt {
 
     public UrLnkBz(PluginWrapper wrapper) {
         super(wrapper);
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return new String[] { "urlink.biz", "ur-link.biz", "url-ink.biz", "url-ink.com", "urli-nk.com" };
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
@@ -52,14 +64,24 @@ public class UrLnkBz extends PluginForDecrypt {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
+        // keycaptcha
         int counter = 0;
-        do {
-            /* 2017-02-10: A single URL can actually require 5 captchas (same via browser)! */
+        final int retryCount = 4;
+        String dl = null;
+        while (dl == null && counter++ <= retryCount) {
             final Form captchaForm = br.getForm(0);
             if (captchaForm != null && captchaForm.containsHTML("capcode")) {
-                final String result = handleCaptchaChallenge(new KeyCaptcha(this, br, createDownloadlink(parameter)).createChallenge(this));
+                String result = null;
+                try {
+                    result = handleCaptchaChallenge(new KeyCaptcha(this, br, createDownloadlink(parameter)).createChallenge(this));
+                } catch (final DecrypterException d) {
+                    if ("Wrong Captcha".equals(d.getMessage())) {
+                        continue;
+                    }
+                    throw d;
+                }
                 if (StringUtils.isEmpty(result)) {
-                    throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+                    throw new DecrypterException(DecrypterException.CAPTCHA);
                 }
                 if ("CANCEL".equals(result)) {
                     throw new PluginException(LinkStatus.ERROR_FATAL);
@@ -67,12 +89,17 @@ public class UrLnkBz extends PluginForDecrypt {
                 captchaForm.put("capcode", Encoding.urlEncode(result));
                 br.submitForm(captchaForm);
                 // captcha form and inputfield is shown even when the correct response is given.
-            } else {
-                break;
             }
-            counter++;
-        } while (counter <= 10);
-        final String dl = br.getRegex("document\\.location\\.href\\s*=\\s*(\"|')(.*?)\\1").getMatch(1);
+            dl = br.getRegex("document\\.location\\.href\\s*=\\s*(\"|')(.*?)\\1").getMatch(1);
+        }
+        if (dl == null) {
+            if (counter >= retryCount) {
+                logger.info("Exceeded counter");
+                throw new DecrypterException(DecrypterException.CAPTCHA);
+            }
+            logger.info("Can't find 'dl'");
+            throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
+        }
         if (dl != null) {
             decryptedLinks.add(createDownloadlink(dl));
         }
