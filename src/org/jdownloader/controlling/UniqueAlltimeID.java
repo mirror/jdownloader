c68006b41897c61f5884bb150b03af157a60c958
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-14 16:22:07 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36134 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/controlling/UniqueAlltimeID.java
+++ src/org/jdownloader/controlling/UniqueAlltimeID.java
@@ -2,86 +2,15 @@ package org.jdownloader.controlling;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicLong;
 
-public class UniqueAlltimeID {
-    private static final AtomicLong ID = new AtomicLong(System.currentTimeMillis());
-    private long                    id;
+public class UniqueAlltimeID extends org.appwork.utils.UniqueAlltimeID {
 
     public UniqueAlltimeID() {
-        this.id = createUniqueAlltimeID();
-    }
-
-    private static long createUniqueAlltimeID() {
-        long id = -1;
-        while (true) {
-            final long lastID = ID.get();
-            id = System.currentTimeMillis();
-            if (id < lastID) {
-                /* WTF?! timestamp is smaller as previous timestamp */
-                id = lastID + 1;
-            } else if (id == lastID) {
-                /* same timestamp, increase by 1 */
-                id = id + 1;
-            }
-            if (ID.compareAndSet(lastID, id)) {
-                return id;
-            }
-        }
+        super();
     }
 
     public UniqueAlltimeID(long id2) {
-        this.id = id2;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-        if (this == o) {
-            return true;
-        }
-        if (o instanceof UniqueAlltimeID) {
-            if (((UniqueAlltimeID) o).id == id) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public long getID() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return Long.toString(id);
-    }
-
-    /**
-     * WARNING: by manually changing the ID you can break unique state of this Instance!
-     *
-     * @param ID
-     */
-    public void setID(long ID) {
-        this.id = ID;
-    }
-
-    /**
-     * WARNING: by manually refreshing the ID you can break unique state of this Instance!
-     */
-    public void refresh() {
-        this.setID(createUniqueAlltimeID());
-    }
-
-    public static String create() {
-        return Long.toString(createUniqueAlltimeID());
+        super(id2);
     }
 
     public static Set<UniqueAlltimeID> createSet(long... ids) {
