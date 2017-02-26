42eafaa88a9944685a98d0218b392ead40f3ecd1
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-02-26 03:51:55 +0000

    Imgrock.net - Max. Chunks = 1 fixes #83000
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36219 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ImgrockNet.java
+++ src/jd/plugins/hoster/ImgrockNet.java
@@ -87,10 +87,10 @@ public class ImgrockNet extends antiDDoSForHost {
     private static final int               WAITSECONDSFORCED               = 5;
     /* Connection stuff */
     private static final boolean           FREE_RESUME                     = true;
-    private static final int               FREE_MAXCHUNKS                  = 0;
+    private static final int               FREE_MAXCHUNKS                  = 1;
     private static final int               FREE_MAXDOWNLOADS               = 20;
     private static final boolean           ACCOUNT_FREE_RESUME             = true;
-    private static final int               ACCOUNT_FREE_MAXCHUNKS          = 0;
+    private static final int               ACCOUNT_FREE_MAXCHUNKS          = 1;
     private static final int               ACCOUNT_FREE_MAXDOWNLOADS       = 20;
     private static final boolean           ACCOUNT_PREMIUM_RESUME          = true;
     private static final int               ACCOUNT_PREMIUM_MAXCHUNKS       = 0;
