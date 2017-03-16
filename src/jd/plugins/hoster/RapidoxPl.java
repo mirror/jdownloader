c4b3c98e4286799e558891e5b209a19ef1ef8913
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-03-16 10:56:07 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36368 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/RapidoxPl.java
+++ src/jd/plugins/hoster/RapidoxPl.java
@@ -223,7 +223,7 @@ public class RapidoxPl extends PluginForHost {
                 handleErrorRetries("unknowndlerror", 5, 2 * 60 * 1000l);
             }
             this.dl.startDownload();
-        } catch (final Throwable e) {
+        } catch (final Exception e) {
             link.setProperty(NICE_HOSTproperty + "directlink", Property.NULL);
             throw e;
         }
