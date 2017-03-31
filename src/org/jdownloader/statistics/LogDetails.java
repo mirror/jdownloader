f95c62f8c682b1db1e7b09490047b136d9466cd7
coalado <coalado@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-03-31 13:47:57 +0000

    Added Option to request logs based on candidate Added UpdateSys Revisions to stats call
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36484 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/statistics/LogDetails.java
+++ src/org/jdownloader/statistics/LogDetails.java
@@ -3,9 +3,35 @@ package org.jdownloader.statistics;
 import org.appwork.storage.Storable;
 
 public class LogDetails implements Storable {
-
     private String logID;
     private String id;
+    private String cls;
+    private String host;
+    private String type;
+
+    public String getCls() {
+        return cls;
+    }
+
+    public void setCls(String cls) {
+        this.cls = cls;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public String getId() {
         return id;
@@ -24,12 +50,13 @@ public class LogDetails implements Storable {
     }
 
     public LogDetails(/* storable */) {
-
     }
 
-    public LogDetails(String logID, String errorID) {
+    public LogDetails(String logID, String errorID, String cls, String host, String type) {
         this.logID = logID;
         this.id = errorID;
+        this.cls = cls;
+        this.host = host;
+        this.type = type;
     }
-
 }
