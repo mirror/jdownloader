44d25bcc512a9f47aa01a180ccb6b9ffec5f3789
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-04-20 13:46:38 +0000

    Added: sharelinked.net, decrypter support - re: IRC Consolidated: rawfile.co, to sharelinkedcom class
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36635 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 63d9d8b10ae79dd9d860a4da18bc948206bb62cd

--- src/jd/plugins/decrypter/SharelinkedCom.java
+++ src/jd/plugins/decrypter/SharelinkedCom.java
@@ -1,5 +1,5 @@
 //jDownloader - Downloadmanager
-//Copyright (C) 2009  JD-Team support@jdownloader.org
+//Copyright (C) 2017  JD-Team support@jdownloader.org
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -17,20 +17,31 @@
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
-import jd.nutils.encoding.Encoding;
+import jd.http.Request;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
-import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "sharelinked.com" }, urls = { "https?://(?:www\\.)?sharelinked\\.com/\\?p=\\d+" })
-public class SharelinkedCom extends PluginForDecrypt {
+/**
+ * NOTE: sharelinked.com and rawfile.co are run by the same people. <br/>
+ * NOTE: sharelinked.net, maybe not... ?<br/>
+ * NOTE: domains can not be switched
+ *
+ * @author psp
+ * @author raztoki
+ *
+ */
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "rawfile.co", "sharelinked.com", "sharelinked.net" }, urls = { "https?://(?:www\\.)?rawfile\\.co/\\?p=(\\d+)", "https?://(?:www\\.)?sharelinked\\.com/\\?p=(\\d+)", "https?://(?:www\\.)?sharelinked\\.net/\\?p=(\\d+)" })
+public class SharelinkedCom extends antiDDoSForDecrypt {
 
     public SharelinkedCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -39,33 +50,26 @@ public class SharelinkedCom extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        final String fid = new Regex(parameter, "(\\d+)$").getMatch(0);
-        br.getPage("http://sharelinked.com/wp-admin/admin-ajax.php?post=" + fid + "&action=get_content");
+        final String fid = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        getPage(Request.getLocation("/wp-admin/admin-ajax.php?post=" + fid + "&action=get_content", br.createGetRequest(parameter)));
         if (br.getHttpConnection().getResponseCode() == 404 || this.br.toString().length() <= 10) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
-        String fpName = null;
-        final String[] links = br.getRegex("href=\"(https?://(?!sharelinked\\.com/)[^<>\"]+|https?://sharelinked\\.com/\\?across[^\"]+)\"").getColumn(0);
+        final String[] links = br.getRegex("href=(\"|')(https?://(?!" + Pattern.quote(getHost()) + "/)[^<>\"]+|https?://" + Pattern.quote(getHost()) + "/\\?across[^\"]+)\\1").getColumn(1);
         if (links == null || links.length == 0) {
             logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+            throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
         }
         for (String singleLink : links) {
-            if (singleLink.contains("sharelinked.com/?across")) {
+            if (singleLink.contains(getHost() + "/?across")) {
                 // single link
                 final Browser br2 = br.cloneBrowser();
                 br2.setFollowRedirects(false);
-                br2.getPage(singleLink);
+                getPage(br2, singleLink);
                 singleLink = br2.getRedirectLocation();
             }
             decryptedLinks.add(createDownloadlink(singleLink));
-        }
-
-        if (fpName != null) {
-            final FilePackage fp = FilePackage.getInstance();
-            fp.setName(Encoding.htmlDecode(fpName.trim()));
-            fp.addLinks(decryptedLinks);
         }
 
         return decryptedLinks;
