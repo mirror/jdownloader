697721330a371b479e1a005dc6d02ed5453156f9
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-05-18 13:02:15 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36862 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/DeprecatedRemoteAPIRequest.java
+++ src/org/jdownloader/api/DeprecatedRemoteAPIRequest.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 
 import org.appwork.remoteapi.InterfaceHandler;
 import org.appwork.remoteapi.RemoteAPIRequest;
-import org.appwork.remoteapi.exceptions.ApiCommandNotAvailable;
+import org.appwork.remoteapi.exceptions.BasicRemoteAPIException;
 import org.appwork.utils.net.httpserver.requests.HttpRequest;
 
 public class DeprecatedRemoteAPIRequest extends RemoteAPIRequest {
-
-    public DeprecatedRemoteAPIRequest(InterfaceHandler<?> iface, String methodName, String[] parameters, DeprecatedAPIRequestInterface request, String callback) throws IOException, ApiCommandNotAvailable {
+    public DeprecatedRemoteAPIRequest(InterfaceHandler<?> iface, String methodName, String[] parameters, DeprecatedAPIRequestInterface request, String callback) throws IOException, BasicRemoteAPIException {
         super(iface, methodName, parameters, (HttpRequest) request, callback);
     }
 
--- src/org/jdownloader/api/MyJDRemoteAPIRequest.java
+++ src/org/jdownloader/api/MyJDRemoteAPIRequest.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 
 import org.appwork.remoteapi.InterfaceHandler;
 import org.appwork.remoteapi.RemoteAPIRequest;
-import org.appwork.remoteapi.exceptions.ApiCommandNotAvailable;
+import org.appwork.remoteapi.exceptions.BasicRemoteAPIException;
 import org.appwork.utils.net.httpserver.requests.HttpRequest;
 import org.jdownloader.api.myjdownloader.MyJDownloaderRequestInterface;
 
 public class MyJDRemoteAPIRequest extends RemoteAPIRequest {
-
-    public MyJDRemoteAPIRequest(InterfaceHandler<?> iface, String methodName, String[] parameters, MyJDownloaderRequestInterface request) throws IOException, ApiCommandNotAvailable {
+    public MyJDRemoteAPIRequest(InterfaceHandler<?> iface, String methodName, String[] parameters, MyJDownloaderRequestInterface request) throws IOException, BasicRemoteAPIException {
         super(iface, methodName, parameters, (HttpRequest) request, request.getJqueryCallback());
     }
 
