3b188599b6d9e3ed144b005654689a6efcb57f99
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-06-07 10:03:10 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36979 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/UJCECheck.java
+++ src/jd/UJCECheck.java
@@ -1,23 +1,17 @@
 package jd;
 
-import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.security.Permission;
 import java.security.PermissionCollection;
+import java.security.Security;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.crypto.Cipher;
 
-import org.appwork.shutdown.ShutdownController;
-import org.appwork.shutdown.ShutdownEvent;
-import org.appwork.shutdown.ShutdownRequest;
-import org.appwork.utils.Application;
-import org.appwork.utils.IO;
-
 public class UJCECheck {
-
+    // Version 07-06-2017 12:00
     private static Boolean isRestrictedCryptography() {
         try {
             final int strength = Cipher.getMaxAllowedKeyLength("AES");
@@ -26,7 +20,6 @@ public class UJCECheck {
             } else {
                 return true;
             }
-
         } catch (Throwable e) {
             return null;
         }
@@ -51,38 +44,72 @@ public class UJCECheck {
                 if (true) {
                     removeCryptographyRestrictions();
                 } else {
-                    final String javaHome = System.getProperty("java.home", null);
-                    final File securityFolder = new File(javaHome, "lib" + File.separatorChar + "security");
-                    if (securityFolder.exists() && securityFolder.isDirectory()) {
-                        final long javaVersion = Application.getJavaVersion();
-                        final File uJCE;
-                        if (javaVersion >= Application.JAVA18) {
-                            uJCE = Application.getResource("security/ujce8");
-                        } else if (javaVersion >= Application.JAVA17) {
-                            uJCE = Application.getResource("security/ujce7");
-                        } else if (javaVersion >= Application.JAVA16) {
-                            uJCE = Application.getResource("security/ujce6");
-                        } else {
-                            return;
-                        }
-                        if (uJCE.exists() && uJCE.isDirectory()) {
-                            ShutdownController.getInstance().addShutdownEvent(new ShutdownEvent() {
-
-                                @Override
-                                public void onShutdown(ShutdownRequest shutdownRequest) {
-                                    try {
-                                        IO.copyFolderRecursive(uJCE, securityFolder, true);
-                                    } catch (final Throwable e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-
-                        }
-                    }
+                    // final String javaHome = System.getProperty("java.home", null);
+                    // final File securityFolder = new File(javaHome, "lib" + File.separatorChar + "security");
+                    // if (securityFolder.exists() && securityFolder.isDirectory()) {
+                    // final long javaVersion = Application.getJavaVersion();
+                    // final File uJCE;
+                    // if (javaVersion >= Application.JAVA19) {
+                    // try {
+                    // // JDK1.9
+                    // Security.setProperty("crypto.policy", "unlimited");
+                    // } catch (final Throwable e) {
+                    // }
+                    // uJCE = null;
+                    // } else if (javaVersion >= Application.JAVA18) {
+                    // uJCE = Application.getResource("security/ujce8");
+                    // } else if (javaVersion >= Application.JAVA17) {
+                    // uJCE = Application.getResource("security/ujce7");
+                    // } else if (javaVersion >= Application.JAVA16) {
+                    // uJCE = Application.getResource("security/ujce6");
+                    // } else {
+                    // return;
+                    // }
+                    // if (uJCE != null && uJCE.exists() && uJCE.isDirectory()) {
+                    // ShutdownController.getInstance().addShutdownEvent(new ShutdownEvent() {
+                    // @Override
+                    // public void onShutdown(ShutdownRequest shutdownRequest) {
+                    // try {
+                    // IO.copyFolderRecursive(uJCE, securityFolder, true);
+                    // } catch (final Throwable e) {
+                    // e.printStackTrace();
+                    // }
+                    // }
+                    // });
+                    // }
+                    // }
                 }
             }
         }
+    }
+
+    private static void modifyRestricted(Field isRestrictedField) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        /**
+         * JceSecurity.isRestricted = false;
+         */
+        if (Modifier.isFinal(isRestrictedField.getModifiers())) {
+            // >=1.8U102
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+            isRestrictedField.setAccessible(true);
+            isRestrictedField.set(null, false);
+            isRestrictedField.setAccessible(false);
+        } else {
+            isRestrictedField.setAccessible(true);
+            isRestrictedField.set(null, false);
+            isRestrictedField.setAccessible(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        removeCryptographyRestrictions();
+        if (isRestrictedCryptography()) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Sad world :(");
+        }
+        // System.out.println("JavaVersion:" + Application.getJavaVersion());
     }
 
     /**
@@ -96,42 +123,30 @@ public class UJCECheck {
             return;
         }
         try {
+            // JDK1.9
+            Security.setProperty("crypto.policy", "unlimited");
+        } catch (final Throwable e) {
+        }
+        try {
             try {
                 final Class<?> jceSecurity = Class.forName("javax.crypto.JceSecurity");
                 final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
                 final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
-
                 final Field isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
                 final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
                 final Field perms = cryptoPermissions.getDeclaredField("perms");
                 final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
-
-                /**
-                 * JceSecurity.isRestricted = false;
-                 */
-                isRestrictedField.setAccessible(true);
-                if (Modifier.isFinal(isRestrictedField.getModifiers())) {
-                    // >=1.8U102
-                    Field modifiers = Field.class.getDeclaredField("modifiers");
-                    modifiers.setAccessible(true);
-                    modifiers.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
-                }
-                isRestrictedField.set(null, false);
-
+                modifyRestricted(isRestrictedField);
                 defaultPolicyField.setAccessible(true);
                 final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
-
                 /**
                  * JceSecurity.defaultPolicy.perms.clear();
                  */
-
                 perms.setAccessible(true);
                 ((Map<?, ?>) perms.get(defaultPolicy)).clear();
-
                 /**
                  * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
                  */
-
                 instance.setAccessible(true);
                 defaultPolicy.add((Permission) instance.get(null));
             } catch (final ClassNotFoundException e) {
@@ -140,33 +155,35 @@ public class UJCECheck {
                     final Class<?> jceSecurity = Class.forName("javax.crypto.SunJCE_b");
                     final Class<?> cryptoPermissions = Class.forName("javax.crypto.SunJCE_d");
                     final Class<?> cryptoAllPermission = Class.forName("javax.crypto.SunJCE_k");
-
                     final Field defaultPolicyField = jceSecurity.getDeclaredField("c");
                     final Field perms = cryptoPermissions.getDeclaredField("a");
                     final Field isRestrictedField = jceSecurity.getDeclaredField("g");
                     final Field instance = cryptoAllPermission.getDeclaredField("b");
-
-                    /**
-                     * JceSecurity.isRestricted = false;
-                     */
-                    isRestrictedField.setAccessible(true);
-                    if (Modifier.isFinal(isRestrictedField.getModifiers())) {
-                        // >=1.8U102
-                        Field modifiers = Field.class.getDeclaredField("modifiers");
-                        modifiers.setAccessible(true);
-                        modifiers.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+                    if (Boolean.TRUE.equals(isRestrictedField.get(null))) {
+                        /**
+                         * JceSecurity.isRestricted = false;
+                         */
+                        if (Modifier.isFinal(isRestrictedField.getModifiers())) {
+                            // >=1.8U102
+                            Field modifiers = Field.class.getDeclaredField("modifiers");
+                            modifiers.setAccessible(true);
+                            modifiers.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+                            isRestrictedField.setAccessible(true);
+                            isRestrictedField.set(null, false);
+                            isRestrictedField.setAccessible(false);
+                        } else {
+                            isRestrictedField.setAccessible(true);
+                            isRestrictedField.set(null, false);
+                            isRestrictedField.setAccessible(false);
+                        }
                     }
-                    isRestrictedField.set(null, false);
-
                     defaultPolicyField.setAccessible(true);
                     final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
-
                     /**
                      * JceSecurity.defaultPolicy.perms.clear();
                      */
                     perms.setAccessible(true);
                     ((Map<?, ?>) perms.get(defaultPolicy)).clear();
-
                     /**
                      * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
                      */
@@ -182,5 +199,4 @@ public class UJCECheck {
             System.out.println("Failed to remove cryptography restrictions:" + e);
         }
     }
-
 }
