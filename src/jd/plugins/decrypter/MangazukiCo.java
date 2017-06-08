e061aae2277639f39ccbf7ac2b06d89ec28027ee
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-06-08 10:05:11 +0000

    MangazukiCo: -fixes thread 73846
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@36984 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MangazukiCo.java
+++ src/jd/plugins/decrypter/MangazukiCo.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
@@ -29,15 +28,13 @@ import jd.plugins.FilePackage;
 
 import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangazuki.co" }, urls = { "https?://(?:www\\.)?mangazuki\\.co/read/[^/]+/\\d+(?:\\.\\d+)?" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangazuki.co" }, urls = { "https?://(?:www\\.|raws\\.)?mangazuki\\.co/read/[^/]+/\\d+(?:\\.\\d+)?" })
 public class MangazukiCo extends antiDDoSForDecrypt {
-
     public MangazukiCo(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     /* Tags: MangaPictureCrawler */
-
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
@@ -51,29 +48,24 @@ public class MangazukiCo extends antiDDoSForDecrypt {
         final String url_name = urlinfo.getMatch(0);
         final String url_chapter = urlinfo.getMatch(1);
         String ext = null;
-
         final FilePackage fp = FilePackage.getInstance();
         fp.setName(url_chapter + "_" + url_name);
-
         final String[] images = this.br.getRegex("<img src=\"http[^<>\"]+\" data\\-src=\"(https?://[^<>\"]+)\"").getColumn(0);
         if (images == null || images.length == 0) {
             return null;
         }
         final int padLength = getPadLength(images.length);
         int page = 0;
-
         for (final String url_image : images) {
             if (this.isAbort()) {
                 return decryptedLinks;
             }
             final String page_formatted = String.format(Locale.US, "%0" + padLength + "d", page);
-
             if (ext == null) {
                 /* No general extension given? Get it from inside the URL. */
                 ext = getFileNameExtensionFromURL(url_image, ".jpg");
             }
             final String filename = url_chapter + "_" + url_name + "_" + page_formatted + ext;
-
             final DownloadLink dl = this.createDownloadlink(url_image);
             dl._setFilePackage(fp);
             dl.setFinalFileName(filename);
@@ -84,7 +76,6 @@ public class MangazukiCo extends antiDDoSForDecrypt {
             distribute(dl);
             page++;
         }
-
         return decryptedLinks;
     }
 
@@ -107,5 +98,4 @@ public class MangazukiCo extends antiDDoSForDecrypt {
             return 8;
         }
     }
-
 }
