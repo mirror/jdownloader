37309b7691985c3bbac7a247436e2679f01de85c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-06-23 16:44:41 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37080 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/api/system/SystemAPIImpl17.java
+++ src/org/jdownloader/api/system/SystemAPIImpl17.java
@@ -20,7 +20,6 @@ import org.appwork.utils.os.CrossSystem;
 import org.jdownloader.myjdownloader.client.bindings.StorageInformationStorable;
 
 public class SystemAPIImpl17 {
-
     public static List<StorageInformationStorable> getStorageInfos(final String path) {
         final List<StorageInformationStorable> ret = new ArrayList<StorageInformationStorable>();
         final List<String> filterList;
@@ -60,7 +59,7 @@ public class SystemAPIImpl17 {
                 if (!customPath && (store.isReadOnly() || filterList.contains(store.type()))) {
                     continue;
                 }
-                storage.setPath(root.toString());
+                storage.setPath(root.toString() + "'Type:" + store.type() + "'");
                 storage.setSize(store.getTotalSpace());
                 storage.setFree(store.getUsableSpace());
             } catch (final IOException e) {
@@ -73,5 +72,4 @@ public class SystemAPIImpl17 {
         }
         return ret;
     }
-
 }
