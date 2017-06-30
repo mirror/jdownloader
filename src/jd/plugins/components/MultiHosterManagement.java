f2faa3e06b6cef7ada175909c2910886a1b85d71
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-06-30 05:57:03 +0000

    Fixed: foxleech.com, - closes #83778 - non api mode fixed (but disabled) - implemented api mode, enabled by default. Implementation: MultiHosterManagement.handleErrorGeneric, for unknown or even known errors with specified retry count. This has small grace period before throwing exception to prevent hammering. This is on the assumption that same download candidate will be triggered after retry.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37111 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/components/MultiHosterManagement.java
+++ src/jd/plugins/components/MultiHosterManagement.java
@@ -2,6 +2,7 @@ package jd.plugins.components;
 
 import java.util.HashMap;
 
+import jd.config.Property;
 import jd.plugins.Account;
 import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
@@ -18,14 +19,18 @@ import jd.plugins.PluginException;
 public class MultiHosterManagement {
 
     private String getHost;
+    private String niceHost;
+    private String hsFailed;
 
     public MultiHosterManagement(final String getHost) {
         this.getHost = getHost;
+        this.niceHost = getHost.replaceAll("https?://|\\.|\\-", "");
+        this.hsFailed = niceHost + "_failedtimes_";
     }
 
     private final HashMap<Object, HashMap<String, UnavailableHost>> db = new HashMap<Object, HashMap<String, UnavailableHost>>();
 
-    public void putError(final Object account, final DownloadLink dl, final Long timeout, final String reason) throws PluginException {
+    public void putError(final Object account, final DownloadLink downloadLink, final Long timeout, final String reason) throws PluginException {
         synchronized (db) {
             // null(multihosterwide) && AccountType && Account
             final UnavailableHost nue = new UnavailableHost(System.currentTimeMillis() + timeout, reason);
@@ -34,37 +39,38 @@ public class MultiHosterManagement {
                 unavailableMap = new HashMap<String, UnavailableHost>();
                 db.put(account, unavailableMap);
             }
-            unavailableMap.put(dl.getHost(), nue);
+            unavailableMap.put(downloadLink.getHost(), nue);
         }
-        throw new PluginException(LinkStatus.ERROR_RETRY);
+        throw new PluginException(LinkStatus.ERROR_RETRY, reason);
     }
 
     /**
-     * intended purpose is predownload. CanHandle can't deal with exceptions at this time. Later to be called within canHandle
+     * Intended purpose is predownload. CanHandle can't deal with exceptions at this time. Later to be called within canHandle
      *
      * @param account
-     * @param dl
+     * @param downloadLink
      * @throws PluginException
+     * @throws InterruptedException
      */
-    public void runCheck(final Account account, final DownloadLink dl) throws PluginException {
+    public void runCheck(final Account account, final DownloadLink downloadLink) throws PluginException, InterruptedException {
         synchronized (db) {
             // check for null(multihosterwide) first, AccountTypes(specific to this account type) second, and Account (specific to this
             // account) last!
             final Object[] acc = new Object[] { null, account.getType(), account };
             for (final Object ob : acc) {
                 final HashMap<String, UnavailableHost> unavailableMap = db.get(ob);
-                final UnavailableHost nue = unavailableMap != null ? (UnavailableHost) unavailableMap.get(dl.getHost()) : null;
+                final UnavailableHost nue = unavailableMap != null ? (UnavailableHost) unavailableMap.get(downloadLink.getHost()) : null;
                 if (nue != null) {
                     final Long lastUnavailable = nue.getErrorTimeout();
                     final String errorReason = nue.getErrorReason();
                     if (lastUnavailable == null) {
                         // never can download from
-                        throw new PluginException(LinkStatus.ERROR_FATAL, "Not possible to download from " + dl.getHost());
+                        throw new PluginException(LinkStatus.ERROR_FATAL, "Not possible to download from " + downloadLink.getHost());
                     } else if (System.currentTimeMillis() < lastUnavailable) {
                         final long wait = lastUnavailable - System.currentTimeMillis();
                         throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Temporarily unavailable by this MultiHoster Provider: " + errorReason != null ? errorReason : "via " + getHost, wait);
                     } else {
-                        unavailableMap.remove(dl.getHost());
+                        unavailableMap.remove(downloadLink.getHost());
                         if (unavailableMap.size() == 0) {
                             db.remove(acc);
                         }
@@ -73,5 +79,44 @@ public class MultiHosterManagement {
             }
         }
     }
+
+    /**
+     * Intended to handle errors which warrant a series of retries before 'putError' called.
+     *
+     * @param downloadLink
+     *            : DownloadLink
+     * @param error
+     *            : name of the error
+     * @param maxRetries
+     *            : Max retries
+     * @throws InterruptedException
+     */
+    public void handleErrorGeneric(final Account account, final DownloadLink downloadLink, final String error, final int maxRetries) throws PluginException, InterruptedException {
+        final String errorID = hsFailed + error;
+        int timesFailed = downloadLink.getIntegerProperty(errorID, 0);
+        if (timesFailed <= maxRetries) {
+            timesFailed++;
+            downloadLink.setProperty(errorID, timesFailed);
+            // we should apply some grace period here since JD2 core does not apply a wait time parameter with the retry exception.
+            long waitPeriod = 5 * timesFailed;
+            do {
+                downloadLink.getLinkStatus().setStatusText("Small wait: " + waitPeriod + " secs");
+                Thread.sleep(1000l);
+            } while (--waitPeriod > 0);
+            downloadLink.getLinkStatus().setStatusText("");
+            throw new PluginException(LinkStatus.ERROR_RETRY, error);
+        } else {
+            downloadLink.setProperty(errorID, Property.NULL);
+            // default of 1 hour wait.
+            this.putError(account, downloadLink, 1 * 60 * 60 * 1000l, "Excausted retry count");
+        }
+    }
+
+    /***
+     * PREMISE
+     *
+     * JD retry does not wait between retries.. this is flawed logic. ensure a small retry is then done within runcheck
+     *
+     */
 
 }
