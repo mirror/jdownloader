affb8fe37972dab7f04613d0a29cc76e70e66ba9
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-05 14:53:56 +0000

    Added: 4tube.com, https support - closes #83617
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37181 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 05a55a975e9d13450dda4495309f591d3e7c4846

--- src/jd/plugins/hoster/FourTubeCom.java
+++ src/jd/plugins/hoster/FourTubeCom.java
@@ -19,6 +19,7 @@ import org.jdownloader.plugins.components.antiDDoSForHost;
 
 import jd.PluginWrapper;
 import jd.http.Browser;
+import jd.http.Request;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -28,8 +29,9 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "4tube.com" }, urls = { "http://(?:www\\.)?4tube\\.com/(?:embed|videos)/(\\d+)/?([\\w-]+)?" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "4tube.com" }, urls = { "https?://(?:www\\.)?4tube\\.com/(?:embed|videos)/(\\d+)/?([\\w-]+)?" })
 public class FourTubeCom extends antiDDoSForHost {
+
     /* DEV NOTES */
     /* Porn_plugin */
     /* tags: fux.com, porntube.com, 4tube.com, pornerbros.com */
@@ -48,7 +50,7 @@ public class FourTubeCom extends antiDDoSForHost {
         return -1;
     }
 
-    private String               DLLINK  = null;
+    private String               dllink  = null;
     private URLConnectionAdapter con     = null;
     private String               uid     = null;
     private boolean              isEmbed = false;
@@ -56,7 +58,7 @@ public class FourTubeCom extends antiDDoSForHost {
     @Override
     public AvailableStatus requestFileInformation(DownloadLink downloadLink) throws Exception {
         setBrowserExclusive();
-        String dllink = downloadLink.getDownloadURL();
+        dllink = downloadLink.getDownloadURL();
         uid = new Regex(dllink, this.getSupportedLinks()).getMatch(0);
         isEmbed = dllink.contains("/embed/");
         br.getHeaders().put("Accept-Language", "en-gb, en;q=0.8");
@@ -82,7 +84,7 @@ public class FourTubeCom extends antiDDoSForHost {
         // seems to be listed in order highest quality to lowest. 20130513
         getDllink();
         String ext = "mp4";
-        if (DLLINK.contains(".flv")) {
+        if (dllink.contains(".flv")) {
             ext = "flv";
         }
         filename = filename.endsWith(".") ? filename + ext : filename + "." + ext;
@@ -133,7 +135,7 @@ public class FourTubeCom extends antiDDoSForHost {
         if (finallink == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        DLLINK = finallink;
+        dllink = finallink;
     }
 
     private String checkDirectLink(String directlink) {
@@ -195,12 +197,12 @@ public class FourTubeCom extends antiDDoSForHost {
                 rtmp.setPlayPath(playpath);
                 rtmp.setApp(app);
                 rtmp.setUrl(host + app);
-                rtmp.setSwfUrl("http://www.4tube.com/player2.swf");
+                rtmp.setSwfUrl(Request.getLocation("//www.4tube.com/player2.swf", br.getRequest()));
                 ((RTMPDownload) dl).startDownload();
                 return;
             }
         } else {
-            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, DLLINK, true, 0);
+            dl = new jd.plugins.BrowserAdapter().openDownload(br, downloadLink, dllink, true, 0);
             if (dl.getConnection().getContentType().contains("html")) {
                 br.followConnection();
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
