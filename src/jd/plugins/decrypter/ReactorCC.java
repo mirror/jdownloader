42b6bb04dba72bafe29b72099b9727f7c16b0353
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-05 16:24:39 +0000

    Fixed: reactor.cc, error handling for unsafe content - closes #83771
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37187 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 94a0779ff716e6d939a08e5ceec9c1db8263eb2c

--- src/jd/plugins/decrypter/ReactorCC.java
+++ src/jd/plugins/decrypter/ReactorCC.java
@@ -10,8 +10,13 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "reactor.cc" }, urls = { "https?://([^/]*?)?reactor\\.cc/post/\\d+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "reactor.cc" }, urls = { "https?://(?:joy)?reactor\\.cc/post/\\d+" })
 public class ReactorCC extends PluginForDecrypt {
+
+    @Override
+    public String[] siteSupportedNames() {
+        return new String[] { "reactor.cc", "joyreactor.cc" };
+    }
 
     public ReactorCC(PluginWrapper wrapper) {
         super(wrapper);
@@ -19,13 +24,19 @@ public class ReactorCC extends PluginForDecrypt {
 
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> ret = null;
         br.setFollowRedirects(true);
         br.getPage(parameter.getCryptedUrl());
         final String reactor[] = br.getRegex("(https?://[^/]*?reactor\\.cc/pics/post/full/.*?(jpe?g|png|gif))").getColumn(0);
         if (reactor == null || reactor.length == 0) {
-            return null;
+            // Unsafe content - only for registered users (google translate)
+            if (br.containsHTML("Небезопасный контент - только для зарегистрированных пользователей|joyreactor\\.cc/images/unsafe_ru.gif")) {
+                ret = new ArrayList<DownloadLink>();
+                ret.add(createOfflinelink(parameter.getCryptedUrl(), "Unsafe content - only for registered users"));
+            }
+            return ret;
         }
-        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        ret = new ArrayList<DownloadLink>();
         final HashSet<String> dups = new HashSet<String>();
         for (String url : reactor) {
             if (dups.add(url)) {
