f6dde6e6eeef05ab142523c51f5482f1707dafb4
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-07 14:50:08 +0000

    To be used by decypters within other methods
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37217 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/AbortException.java
@@ -0,0 +1,10 @@
+package jd.plugins.decrypter;
+
+public class AbortException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3092315894696695708L;
+
+}
