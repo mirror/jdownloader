85f5af6381ec0255a97e022433bf871ad6efc69c
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-09 01:50:55 +0000

    Added: InfoBar, to reduce overall dialog size!
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37222 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/extensions/infobar/CFG_INFOBAR.java
+++ src/org/jdownloader/extensions/infobar/CFG_INFOBAR.java
@@ -8,27 +8,32 @@ import org.appwork.storage.config.handler.StorageHandler;
 import org.appwork.utils.Application;
 
 public class CFG_INFOBAR {
+
     public static void main(String[] args) {
         ConfigUtils.printStaticMappings(InfoBarConfig.class, "Application.getResource(\"cfg/\" + " + InfoBarExtension.class.getSimpleName() + ".class.getName())");
     }
 
     // Static Mappings for interface org.jdownloader.extensions.infobar.InfoBarConfig
-    public static final InfoBarConfig                 CFG                   = JsonConfig.create(Application.getResource("cfg/" + InfoBarExtension.class.getName()), InfoBarConfig.class);
-    public static final StorageHandler<InfoBarConfig> SH                    = (StorageHandler<InfoBarConfig>) CFG._getStorageHandler();
+    public static final InfoBarConfig                 CFG                        = JsonConfig.create(Application.getResource("cfg/" + InfoBarExtension.class.getName()), InfoBarConfig.class);
+    public static final StorageHandler<InfoBarConfig> SH                         = (StorageHandler<InfoBarConfig>) CFG._getStorageHandler();
     // let's do this mapping here. If we map all methods to static handlers, access is faster, and we get an error on init if mappings are
     // wrong.
 
-    public static final BooleanKeyHandler             WINDOW_VISIBLE        = SH.getKeyHandler("WindowVisible", BooleanKeyHandler.class);
+    public static final BooleanKeyHandler             WINDOW_VISIBLE             = SH.getKeyHandler("WindowVisible", BooleanKeyHandler.class);
 
-    public static final BooleanKeyHandler             DRAG_AND_DROP_ENABLED = SH.getKeyHandler("DragAndDropEnabled", BooleanKeyHandler.class);
+    public static final BooleanKeyHandler             DRAG_AND_DROP_ENABLED      = SH.getKeyHandler("DragAndDropEnabled", BooleanKeyHandler.class);
 
-    public static final BooleanKeyHandler             FRESH_INSTALL         = SH.getKeyHandler("FreshInstall", BooleanKeyHandler.class);
+    public static final BooleanKeyHandler             FRESH_INSTALL              = SH.getKeyHandler("FreshInstall", BooleanKeyHandler.class);
 
     /**
      * Java 1.7 or higher required!
      **/
-    public static final IntegerKeyHandler             TRANSPARENCY          = SH.getKeyHandler("Transparency", IntegerKeyHandler.class);
+    public static final IntegerKeyHandler             TRANSPARENCY               = SH.getKeyHandler("Transparency", IntegerKeyHandler.class);
 
-    public static final BooleanKeyHandler             ENABLED               = SH.getKeyHandler("Enabled", BooleanKeyHandler.class);
+    public static final BooleanKeyHandler             ENABLED                    = SH.getKeyHandler("Enabled", BooleanKeyHandler.class);
+
+    public static final BooleanKeyHandler             DRAGNDROP_ICON_DISPLAYED     = SH.getKeyHandler("DragNDropIconDisplayed", BooleanKeyHandler.class);
+
+    public static final BooleanKeyHandler             LINKGRABBER_BUTTON_DISPLAYED = SH.getKeyHandler("LinkgrabberButtonDisplayed", BooleanKeyHandler.class);
 
 }
