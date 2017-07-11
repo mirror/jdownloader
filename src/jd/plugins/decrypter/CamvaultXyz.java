2fc33450d0249f40e12094eed8a2f84367db2bc1
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-11 04:30:06 +0000

    Reviewed: camvault.xyz, - removed this.br nuking - clone browser required! - cloudflare support added - removed unused code
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37250 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: a0c99bf2f6ec0ec8c5d94cee1b83a823cac2f078

--- src/jd/plugins/decrypter/CamvaultXyz.java
+++ src/jd/plugins/decrypter/CamvaultXyz.java
@@ -18,21 +18,21 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.appwork.utils.StringUtils;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
-import jd.plugins.PluginForDecrypt;
 import jd.plugins.components.PluginJSonUtils;
 
-import org.appwork.utils.StringUtils;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
-
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "camvault.xyz" }, urls = { "https?://(?:www\\.)?camvault\\.xyz/download/[A-Za-z0-9\\-_]+\\-\\d+\\.html" })
-public class CamvaultXyz extends PluginForDecrypt {
+public class CamvaultXyz extends antiDDoSForDecrypt {
 
     public CamvaultXyz(PluginWrapper wrapper) {
         super(wrapper);
@@ -41,12 +41,11 @@ public class CamvaultXyz extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        br.getPage(parameter);
+        getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 404) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
-        String fpName = null;
         final String[] videoTokens = br.getRegex("name=\"videoToken\" type=\"hidden\" value=\"([^<>\"]+)\"").getColumn(0);
         if (videoTokens == null || videoTokens.length == 0) {
             logger.warning("Decrypter broken for link: " + parameter);
@@ -58,26 +57,20 @@ public class CamvaultXyz extends PluginForDecrypt {
             if (this.isAbort()) {
                 return decryptedLinks;
             }
-            br.postPage("/gallery/megadownload", "captcha=&token=" + Encoding.urlEncode(videoToken));
-            final String reCaptchaSiteKey = PluginJSonUtils.getJsonValue(this.br, "sitekey");
+            final Browser br = this.br.cloneBrowser();
+            postPage(br, "/gallery/megadownload", "captcha=&token=" + Encoding.urlEncode(videoToken));
+            final String reCaptchaSiteKey = PluginJSonUtils.getJsonValue(br, "sitekey");
             if (!StringUtils.isEmpty(reCaptchaSiteKey)) {
                 /* Usually a reCaptchaV2 is required! */
                 final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br, reCaptchaSiteKey).getToken();
-                br.postPage("/gallery/megadownload", "captcha=" + Encoding.urlEncode(recaptchaV2Response) + "&token=" + Encoding.urlEncode(videoToken));
+                postPage(br, "/gallery/megadownload", "captcha=" + Encoding.urlEncode(recaptchaV2Response) + "&token=" + Encoding.urlEncode(videoToken));
             }
-            br.getRequest().setHtmlCode(Encoding.unicodeDecode(this.br.toString()));
-            final String[] dllinks = this.br.getRegex("download\\-link\"><a href=\"(https[^<>\"]+)\"").getColumn(0);
+            br.getRequest().setHtmlCode(Encoding.unicodeDecode(br.toString()));
+            final String[] dllinks = br.getRegex("download-link\"><a href=\"(https?[^<>\"]+)\"").getColumn(0);
             for (final String dllink : dllinks) {
                 decryptedLinks.add(createDownloadlink(dllink));
             }
         }
-
-        if (fpName != null) {
-            final FilePackage fp = FilePackage.getInstance();
-            fp.setName(Encoding.htmlDecode(fpName.trim()));
-            fp.addLinks(decryptedLinks);
-        }
-
         return decryptedLinks;
     }
 
