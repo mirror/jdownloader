e9b1e461741d30f0ec50c52f9c06edacab8b3fcc
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-16 15:09:17 +0000

    Improved: AbstractCaptchaHelperRecaptchaV2.getSiteKey(String), regex
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37317 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/AbstractCaptchaHelperRecaptchaV2.java
+++ src/org/jdownloader/captcha/v2/challenge/recaptcha/v2/AbstractCaptchaHelperRecaptchaV2.java
@@ -1,14 +1,15 @@
 package org.jdownloader.captcha.v2.challenge.recaptcha.v2;
 
-import jd.http.Browser;
-import jd.plugins.Plugin;
-
 import org.appwork.utils.Regex;
 import org.appwork.utils.StringUtils;
 import org.appwork.utils.logging2.LogInterface;
 import org.jdownloader.logging.LogController;
 
+import jd.http.Browser;
+import jd.plugins.Plugin;
+
 public abstract class AbstractCaptchaHelperRecaptchaV2<T extends Plugin> {
+
     protected T            plugin;
     protected LogInterface logger;
     protected Browser      br;
@@ -89,6 +90,8 @@ public abstract class AbstractCaptchaHelperRecaptchaV2<T extends Plugin> {
         return getSiteKey(br != null ? br.toString() : null);
     }
 
+    private final static String apiKeyRegex = "[\\w-]+";
+
     /**
      * will auto find api key, based on google default &lt;div&gt;, @Override to make customised finder.
      *
@@ -103,42 +106,50 @@ public abstract class AbstractCaptchaHelperRecaptchaV2<T extends Plugin> {
         if (source == null) {
             return null;
         }
-        // lets look for default
-        final String[] divs = new Regex(source, "<div(?:[^>]*>.*?</div>|[^>]*\\s*/\\s*>)").getColumn(-1);
-        if (divs != null) {
-            for (final String div : divs) {
-                if (new Regex(div, "class=('|\")[^'\"]*g-recaptcha(\\1|\\s+)").matches()) {
-                    siteKey = new Regex(div, "data-sitekey=('|\")\\s*([\\w-]+)\\s*\\1").getMatch(1);
+        {
+            // lets look for defaults
+            final String[] divs = new Regex(source, "<\\s*(div|button)(?:[^>]*>.*?</\\1>|[^>]*\\s*/\\s*>)").getColumn(-1);
+            if (divs != null) {
+                for (final String div : divs) {
+                    if (new Regex(div, "class=('|\")(?:.*?\\s+)?g-recaptcha(\\1|\\s+)").matches()) {
+                        siteKey = new Regex(div, "data-sitekey=('|\")\\s*(" + apiKeyRegex + ")\\s*\\1").getMatch(1);
+                        if (siteKey != null) {
+                            return siteKey;
+                        }
+                    }
+                }
+            }
+        }
+        {
+            // can also be within <script> (for example cloudflare)
+            final String[] scripts = new Regex(source, "<\\s*script\\s+(?:.*?<\\s*/\\s*script\\s*>|[^>]+\\s*/\\s*>)").getColumn(-1);
+            if (scripts != null) {
+                for (final String script : scripts) {
+                    siteKey = new Regex(script, "data-sitekey=('|\")\\s*(" + apiKeyRegex + ")\\s*\\1").getMatch(1);
                     if (siteKey != null) {
                         return siteKey;
                     }
                 }
             }
         }
-        // can also be within <script> (for example cloudflare)
-        final String[] scripts = new Regex(source, "<\\s*script\\s+(?:.*?<\\s*/\\s*script\\s*>|[^>]+\\s*/\\s*>)").getColumn(-1);
-        if (scripts != null) {
-            for (final String script : scripts) {
-                siteKey = new Regex(script, "data-sitekey=('|\")\\s*([\\w-]+)\\s*\\1").getMatch(1);
-                if (siteKey != null) {
-                    return siteKey;
+        {
+            // within iframe
+            final String[] iframes = new Regex(source, "<\\s*iframe\\s+(?:.*?<\\s*/\\s*iframe\\s*>|[^>]+\\s*/\\s*>)").getColumn(-1);
+            if (iframes != null) {
+                for (final String iframe : iframes) {
+                    siteKey = new Regex(iframe, "google\\.com/recaptcha/api/fallback\\?k=(" + apiKeyRegex + ")").getMatch(0);
+                    if (siteKey != null) {
+                        return siteKey;
+                    }
                 }
             }
         }
-        // within iframe
-        final String[] iframes = new Regex(source, "<\\s*iframe\\s+(?:.*?<\\s*/\\s*iframe\\s*>|[^>]+\\s*/\\s*>)").getColumn(-1);
-        if (iframes != null) {
-            for (final String iframe : iframes) {
-                siteKey = new Regex(iframe, "google\\.com/recaptcha/api/fallback\\?k=([\\w-]+)").getMatch(0);
-                if (siteKey != null) {
-                    return siteKey;
-                }
+        {
+            // json values in script or json
+            final String jssource = new Regex(source, "recaptcha\\.render\\s*\\(.*?,\\s*\\{(.*?)\\}\\);").getMatch(0);
+            if (jssource != null) {
+                siteKey = new Regex(jssource, "('|\"|)sitekey\\1\\s*:\\s*('|\"|)\\s*(" + apiKeyRegex + ")\\s*\\2").getMatch(2);
             }
-        }
-        // json values in script or json
-        final String jssource = new Regex(source, "recaptcha\\.render\\s*\\(.*?,\\s*\\{(.*?)\\}\\);").getMatch(0);
-        if (jssource != null) {
-            siteKey = new Regex(jssource, "('|\"|)sitekey\\1\\s*:\\s*('|\"|)\\s*([\\w-]+)\\s*\\2").getMatch(2);
         }
         return siteKey;
     }
