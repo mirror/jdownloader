2901eb7d0ac5c7f9a7875ba0b41b2273f8ebfc7f
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-18 18:28:02 +0000

    *Plugins: Fixes/changes* -highwayme: added support for more onlinetv linktypes RE: me -UnknownPornScript5: moved porndoecom into its' own class fixes #83690 -VidEarnDecrypter: RegEx/errorhandling fixes #83924 -Twitter: added errorhandling for possible server error or block refs #83922
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37345 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1c8bdb67deada368f2de5e02920936d2b3703bec

--- src/jd/plugins/decrypter/VidEarnDecrypter.java
+++ src/jd/plugins/decrypter/VidEarnDecrypter.java
@@ -13,12 +13,9 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
-
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
@@ -29,9 +26,10 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornxs.com" }, urls = { "http://(www\\.)?(pornxs\\.com/(video\\.php\\?id=|[a-z0-9\\-]+/$|playlists/\\d+[-\\w]+/)|(?:embed\\.pornxs\\.com/embed\\.php\\?id=)\\d+([a-z0-9\\-]+\\.html)?)" })
-public class VidEarnDecrypter extends antiDDoSForDecrypt {
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornxs.com" }, urls = { "https?://(www\\.)?(pornxs\\.com/(video\\.php\\?id=|[a-z0-9\\-]+/$|playlists/\\d+[-\\w]+/)|(?:embed\\.pornxs\\.com/embed\\.php\\?id=)\\d+([a-z0-9\\-]+\\.html)?)" })
+public class VidEarnDecrypter extends antiDDoSForDecrypt {
     public VidEarnDecrypter(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -54,13 +52,13 @@ public class VidEarnDecrypter extends antiDDoSForDecrypt {
             return decryptedLinks;
         }
         String fpName = null;
-        if (br.getHttpConnection().getResponseCode() == 404 || (false && !br.containsHTML("id=\"video\\-player\""))) {
+        if (br.getHttpConnection().getResponseCode() == 404) {
             mainlink.setAvailable(false);
             mainlink.setProperty("offline", true);
         } else {
             fpName = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
             if (fpName == null) {
-                return null;
+                fpName = parameter;
             }
             fpName = Encoding.htmlDecode(fpName);
             fpName = fpName.trim();
@@ -73,11 +71,14 @@ public class VidEarnDecrypter extends antiDDoSForDecrypt {
                 xdl.setProperty("videarnname", fpName);
                 decryptedLinks.add(xdl);
             }
-
             mainlink.setName(fpName + ".mp4");
             mainlink.setAvailable(true);
         }
-
+        if (!br.containsHTML("id=\"video\\-player\"")) {
+            /* Check here to prevent faulty offline URLs. */
+            mainlink.setAvailable(false);
+            mainlink.setProperty("offline", true);
+        }
         decryptedLinks.add(mainlink);
         if (fpName != null) {
             final FilePackage fp = FilePackage.getInstance();
@@ -91,5 +92,4 @@ public class VidEarnDecrypter extends antiDDoSForDecrypt {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
