873b383c9301d8a978bbff7d509d868e3f7be03a
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-07-29 13:04:31 +0000

    Reviewed: GoogleVideoRefresh, - has nothing todo with google - cross browser isn't required. preferable that a new browser is set(to this) once within method. Reviewed: kissanime.to, - flawed logics with refresh, -- original plugin methods call this.br, which where not related to refreshed browser, also this created NPE -- no reference to the mirror it used (as it was never stored), subsequent caused failures - store the mirror id within feedback in source_url - ep base (without mirror id), is no longer entry 0. - embeded/iframed links are now also obstructed Added: kissanime.to, hoster for internal server (todo: need to check this approach is ok) - re: https://board.jdownloader.org/showthread.php?p=404620#post404620 Reviewed: video.google.com, - refresh link should be saved for retry, vs using original - removed code not used within refresh method
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37459 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/RefreshSessionLink.java
@@ -0,0 +1,9 @@
+package org.jdownloader.plugins.components;
+
+import jd.plugins.DownloadLink;
+
+public interface RefreshSessionLink {
+
+    public String refreshVideoDirectUrl(final DownloadLink dl) throws Exception;
+
+}
--- src/org/jdownloader/plugins/components/google/GoogleVideoRefresh.java
+++
@@ -1,8 +0,0 @@
-package org.jdownloader.plugins.components.google;
-
-import jd.http.Browser;
-import jd.plugins.DownloadLink;
-
-public interface GoogleVideoRefresh {
-    public String refreshVideoDirectUrl(final DownloadLink dl, final Browser br) throws Exception;
-}
