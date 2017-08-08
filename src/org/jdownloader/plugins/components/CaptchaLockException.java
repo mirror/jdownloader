fb72bc48eae5eb79d1df32720ba9e0582f4ff62b
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-08-08 12:44:46 +0000

    Reviewed: antiDDoSFor(Host|Decrypt), - prevent multiple captcha events from been stacked/queued - prevent post lock flooding
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37560 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/CaptchaLockException.java
@@ -0,0 +1,10 @@
+package org.jdownloader.plugins.components;
+
+public class CaptchaLockException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6684193398458411608L;
+
+}
