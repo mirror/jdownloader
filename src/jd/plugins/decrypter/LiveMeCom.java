15e268c72d9a5d88b8bf49036993568a2f8110c3
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-08-14 13:12:19 +0000

    Added: liveme.com, more url structures gained from home page and our forum - re: https://board.jdownloader.org/showthread.php?p=405712#post405712
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37603 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 6b17deac3f9d5c78a6ccd4dba26e9179deb262a7

--- src/jd/plugins/decrypter/LiveMeCom.java
+++ src/jd/plugins/decrypter/LiveMeCom.java
@@ -3,6 +3,11 @@ package jd.plugins.decrypter;
 import java.util.ArrayList;
 import java.util.Map;
 
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.plugins.CryptedLink;
@@ -10,12 +15,7 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
-
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "liveme.com" }, urls = { "http://(www\\.)?liveme\\.com/media/play/\\?videoid=\\d+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "liveme.com" }, urls = { "http://(?:www\\.)?liveme\\.com/(?:media/play/\\?videoid=\\d+|media/liveshort/dist/\\?videoid=\\d+&.*?|live\\.html\\?videoid=\\d+.*?)" })
 public class LiveMeCom extends PluginForDecrypt {
 
     public LiveMeCom(PluginWrapper wrapper) {
@@ -23,9 +23,10 @@ public class LiveMeCom extends PluginForDecrypt {
     }
 
     @Override
-    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, final ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        final String videoid = new Regex(parameter.getCryptedUrl(), "(\\d+)$").getMatch(0);
+        final String parameter = param.toString();
+        final String videoid = getVideoID(parameter);
         br.setFollowRedirects(true);
         br.getPage("https://live.ksmobile.net/live/queryinfo?videoid=" + videoid);
         final Map<String, Object> response = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
@@ -36,15 +37,22 @@ public class LiveMeCom extends PluginForDecrypt {
         final DownloadLink link;
         if (StringUtils.endsWithCaseInsensitive(url, "m3u8")) {
             link = createDownloadlink("m3u8" + url.substring(4));
-        } else {
+        } else if (StringUtils.isNotEmpty(url)) {
             link = createDownloadlink(url);
+        } else {
+            return ret;
         }
         if (title != null) {
             link.setFinalFileName(title + ".mp4");
         }
-        link.setContentUrl(parameter.getCryptedUrl());
+        link.setContentUrl(param.getCryptedUrl());
         ret.add(link);
         return ret;
+    }
+
+    private String getVideoID(String parameter) {
+        final String result = new Regex(parameter, "[&?]videoid=(\\d+)").getMatch(0);
+        return result;
     }
 
 }
