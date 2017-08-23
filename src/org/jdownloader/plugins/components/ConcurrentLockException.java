17d95d3c483695787a39136d2ffcbe2fb8cf1251
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-08-23 14:19:58 +0000

    Review/Refactored/Optimise: antiDDoSForX, - restructured so form finding isn't needed 100% of the time, only for the select codes that require it. Form parsing is slow. - identified possible bug with open connection and cloudflare response code 200, as it wasn't supported. Currently all codes are supported in open connection now. - identified another response code 424
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37677 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/ConcurrentLockException.java
@@ -0,0 +1,15 @@
+package org.jdownloader.plugins.components;
+
+/**
+ * Concurrent access not possible, Do not pass Go!, Do not collect 200 dollars.
+ *
+ * @author raztoki
+ *
+ */
+public class ConcurrentLockException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6684193398458411608L;
+}
