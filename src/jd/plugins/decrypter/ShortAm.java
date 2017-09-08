dc8ba2c4412fb4d5cddfb36d61cbe02fd7569ad5
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-09-08 05:20:02 +0000

    Fixed: short.am, captcha was done but not submitted o_O - re: https://board.jdownloader.org/showthread.php?p=407489#post407489 Added: mitly.us, decrypter support - re: https://board.jdownloader.org/showthread.php?p=407489#post407489
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37768 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: daa443da5b42edff102240f800c0aa597e2d7d04

--- src/jd/plugins/decrypter/ShortAm.java
+++ src/jd/plugins/decrypter/ShortAm.java
@@ -18,51 +18,56 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.parser.html.Form;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.PluginForDecrypt;
-
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "short.am" }, urls = { "https?://(?:www\\.)?short\\.am/[A-Za-z0-9]+" })
-public class ShortAm extends PluginForDecrypt {
+public class ShortAm extends antiDDoSForDecrypt {
 
     public ShortAm(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        br = new Browser();
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        this.br.setFollowRedirects(false);
-        br.getPage(parameter);
+        br.setFollowRedirects(false);
+        getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 404) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
-
-        Form continueform = this.br.getFormbyKey("_token");
+        Form continueform = br.getFormbyKey("_token");
         if (continueform == null) {
-            continueform = this.br.getForm(0);
+            continueform = br.getForm(0);
+            if (continueform == null) {
+                throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
+            }
         }
-
-        if (continueform == null) {
-            return null;
-        }
-
         final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br).getToken();
         continueform.put("g-recaptcha-response", Encoding.urlEncode(recaptchaV2Response));
-
-        this.br.postPage(this.br.getURL(), "image=Continue");
-        final String finallink = this.br.getRegex("window\\.location\\.replace\\(\\'(http[^<>\"\\']+)\\'\\)").getMatch(0);
+        submitForm(continueform);
+        continueform = br.getForm(0);
+        if (continueform == null) {
+            throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
+        }
+        sleep(6123l, param);
+        submitForm(continueform);
+        final String finallink = br.getRegex("window\\.location\\.replace\\('(http[^<>\"\\']+)'\\)").getMatch(0);
         if (finallink == null) {
             /* Basically via browser there just isn't any redirect present if a user uses an invalid url. */
-            if (this.br.containsHTML("Please wait \\d+s")) {
+            if (br.containsHTML("Please wait \\d+s")) {
                 decryptedLinks.add(this.createOfflinelink(parameter));
                 return decryptedLinks;
             }
