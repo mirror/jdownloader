5ed78d02056044066a52f591bb6523b2f0e03fae
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-09-20 13:52:12 +0000

    Fixed: kissanime.to, - closes #84367
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37837 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 28f3b6bf86800054ad172854e8483d7a17f1c922

--- src/jd/plugins/decrypter/KisAmeCm.java
+++ src/jd/plugins/decrypter/KisAmeCm.java
@@ -26,6 +26,19 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.imageio.ImageIO;
 
+import org.appwork.utils.Hash;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.images.IconIO;
+import org.jdownloader.captcha.v2.challenge.clickcaptcha.ClickedPoint;
+import org.jdownloader.captcha.v2.challenge.multiclickcaptcha.MultiClickedPoint;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.captcha.v2.challenge.sweetcaptcha.CaptchaHelperCrawlerPluginSweetCaptcha;
+import org.jdownloader.plugins.components.RefreshSessionLink;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+import org.jdownloader.plugins.components.config.KissanimeToConfig;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginJsonConfig;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
@@ -45,19 +58,6 @@ import jd.plugins.PluginException;
 import jd.utils.JDHexUtils;
 import jd.utils.RazStringBuilder;
 
-import org.appwork.utils.Hash;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.images.IconIO;
-import org.jdownloader.captcha.v2.challenge.clickcaptcha.ClickedPoint;
-import org.jdownloader.captcha.v2.challenge.multiclickcaptcha.MultiClickedPoint;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
-import org.jdownloader.captcha.v2.challenge.sweetcaptcha.CaptchaHelperCrawlerPluginSweetCaptcha;
-import org.jdownloader.plugins.components.RefreshSessionLink;
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-import org.jdownloader.plugins.components.config.KissanimeToConfig;
-import org.jdownloader.plugins.config.PluginConfigInterface;
-import org.jdownloader.plugins.config.PluginJsonConfig;
-
 /**
  *
  *
@@ -65,6 +65,7 @@ import org.jdownloader.plugins.config.PluginJsonConfig;
  */
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "kissanime.to", "kissasian.com", "kisscartoon.me", "kissmanga.com" }, urls = { "https?://(?:www\\.)?kissanime\\.(?:com|to|ru)/anime/[a-zA-Z0-9\\-\\_]+/[a-zA-Z0-9\\-\\_]+(?:\\?id=\\d+)?", "http://kissasian\\.(?:com|ch)/[^/]+/[A-Za-z0-9\\-]+/[^/]+(?:\\?id=\\d+)?", "https?://(?:kisscartoon\\.(?:me|io)|kimcartoon\\.me)/[^/]+/[A-Za-z0-9\\-]+/[^/]+(?:\\?id=\\d+)?", "https?://(?:www\\.)?kissmanga\\.com/Manga/.+\\?id=\\d+" })
 public class KisAmeCm extends antiDDoSForDecrypt implements RefreshSessionLink {
+
     public KisAmeCm(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -80,6 +81,7 @@ public class KisAmeCm extends antiDDoSForDecrypt implements RefreshSessionLink {
         KISS_CARTOON,
         KISS_MANGA,
         KISS_UNKNOWN;
+
         private static HostType parse(final String link) {
             if (StringUtils.containsIgnoreCase(link, "kissanime")) {
                 return KISS_ANIME;
@@ -447,7 +449,7 @@ public class KisAmeCm extends antiDDoSForDecrypt implements RefreshSessionLink {
                         continue;
                     }
                     break;
-                } else if (ruh.containsHTML("/Special/CapImg\\?")) {
+                } else if (ruh.containsHTML("/Special/CapImg[\\?/]")) {
                     /**
                      * select an image from selection that best fits provided phrase
                      *
@@ -458,7 +460,7 @@ public class KisAmeCm extends antiDDoSForDecrypt implements RefreshSessionLink {
                      */
                     final String phraseSingle = ruh.getRegex("Choose the most suitable image for: <span style=\".*?\">(.*?)</span></p>").getMatch(0);
                     final String[] phraseMulti = ruh.getRegex("(Choose the)\\s*<b>(\\w+)</b>\\s*(suitable images for\\s*:)\\s*<span style=\"[^\"]*\">\\s*([^<>]+)\\s*</span>(?:(\\s*and\\s*)<span style=\"[^\"]*\">\\s*([^<>]+)\\s*</span>)*").getRow(0);
-                    final String[] captchaImages = ruh.getRegex("('|\")(/Special/CapImg\\?f=.*?)\\1").getColumn(1);
+                    final String[] captchaImages = ruh.getRegex("('|\")(/Special/CapImg.*?)\\1").getColumn(1);
                     if (captchaImages == null || captchaImages.length == 0) {
                         throw new DecrypterException(DecrypterException.PLUGIN_DEFECT);
                     }
