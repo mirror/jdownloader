0239ca0cef2375a19d39fc29d9d789200396e950
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-09-26 11:12:55 +0000

    fixes thread 75016
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37890 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3e1785dccc1cee1061bd2fb80cf100bfbe4fac7c

--- src/jd/plugins/hoster/GooglePhotos.java
+++ src/jd/plugins/hoster/GooglePhotos.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.hoster;
 
 import java.io.IOException;
@@ -22,6 +21,7 @@ import jd.PluginWrapper;
 import jd.config.ConfigContainer;
 import jd.config.ConfigEntry;
 import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
@@ -31,9 +31,8 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.utils.locale.JDL;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "photos.google.com" }, urls = { "https?://photos\\.google\\.com/share/[A-Za-z0-9\\-_]+/photo/[A-Za-z0-9\\-_]+\\?key=[A-Za-z0-9\\-_]+" }) 
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "photos.google.com" }, urls = { "https?://photos\\.google\\.com/share/[A-Za-z0-9\\-_]+/photo/[A-Za-z0-9\\-_]+\\?key=[A-Za-z0-9\\-_]+" })
 public class GooglePhotos extends PluginForHost {
-
     public GooglePhotos(PluginWrapper wrapper) {
         super(wrapper);
         setConfigElements();
@@ -43,15 +42,12 @@ public class GooglePhotos extends PluginForHost {
     // Tags: Google Service
     // protocol: https
     // other:
-
     /** Settings stuff */
     public static final String FAST_LINKCHECK    = "FAST_LINKCHECK";
-
     /* Connection stuff */
     private boolean            free_resume       = true;
     private int                free_maxchunks    = 0;
     private final int          free_maxdownloads = -1;
-
     private String             dllink            = null;
     private boolean            serverissue       = false;
 
@@ -68,7 +64,6 @@ public class GooglePhotos extends PluginForHost {
         serverissue = false;
         free_resume = true;
         free_maxchunks = 0;
-
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
         final String url_title = new Regex(link.getDownloadURL(), "/photo/([A-Za-z0-9\\-_]+)").getMatch(0).replace("-", " ");
@@ -82,68 +77,85 @@ public class GooglePhotos extends PluginForHost {
         long filesize = 0;
         String filename = null;
         URLConnectionAdapter con = null;
-        if (this.br.containsHTML("data\\-isvideo=\"true\"")) {
-            try {
-                /*
-                 * Special for videos to find the filename. If we access the 'wrong' videourl we'll get a .gifv filename - if we access the
-                 * 'right' videourl we won't get a filename at all!
-                 */
-                isvideo = true;
-                /* Try to get downloadlink */
-                dllink = br.getRegex("(https?://video\\.googleusercontent\\.com/[A-Za-z0-9\\-_]+)").getMatch(0);
-                if (dllink == null) {
-                    /* Failed? Try to get streamlink! */
-                    dllink = br.getRegex("data\\-media\\-key=\"[^<>\"]+\"data\\-url=\"(https[^<>\"]+)\"[^@]+data\\-isvideo=\"true\"").getMatch(0);
-                    isVideoStreamDwnload = true;
-                }
-                if (dllink != null) {
-                    con = br.openHeadConnection(dllink);
-                    filename = getFileNameFromHeader(con);
-                    if (isVideoStreamDwnload) {
-                        filename = this.removeDoubleExtensions(filename, "mp4");
-                        /* =m22?cpn=blablabla&c=WEB&cver=1.20160414 */
-                        /* Correct videourl */
-                        dllink += "=m22?c=WEB";
-                    } else {
-                        /* Download of original file --> No workaround needed and filesize & final filename is known! */
-                        filesize = con.getLongContentLength();
-                        free_maxchunks = 1;
-                        free_resume = false;
-                    }
-                }
-            } catch (final Throwable e) {
-            }
-        } else {
-            /* For photos */
-            dllink = br.getRegex("2\\][\t\n\r ]*?,\"(https?://[^<>\"]*?)\"").getMatch(0);
-        }
-        if (dllink != null && (filesize == 0 || filename == null)) {
-            con = br.openHeadConnection(dllink);
-            if (!con.getContentType().contains("html")) {
-                filesize = con.getLongContentLength();
-                if (filename == null) {
-                    filename = getFileNameFromHeader(con);
-                }
-                if (filename == null) {
-                    filename = url_title;
-                    if (isVideoStreamDwnload) {
-                        filename += ".mp4";
-                    } else if (!isvideo) {
-                        filename += ".jpg";
-                    } else {
-                        /* Video download with unknown extension. */
-                    }
-                } else if (isVideoStreamDwnload && !filename.endsWith(".mp4")) {
-                    filename = url_title + ".mp4";
-                }
-                link.setProperty("directlink", dllink);
-            } else {
-                serverissue = true;
-            }
-        }
         try {
-            con.disconnect();
-        } catch (final Throwable e) {
+            if (this.br.containsHTML("data\\-isvideo=\"true\"")) {
+                try {
+                    /*
+                     * Special for videos to find the filename. If we access the 'wrong' videourl we'll get a .gifv filename - if we access
+                     * the 'right' videourl we won't get a filename at all!
+                     */
+                    isvideo = true;
+                    /* Try to get downloadlink */
+                    dllink = br.getRegex("(https?://video\\.googleusercontent\\.com/[A-Za-z0-9\\-_]+)").getMatch(0);
+                    if (dllink == null) {
+                        dllink = br.getRegex("(https?://video-downloads\\.googleusercontent\\.com/[A-Za-z0-9\\-_]+)").getMatch(0);
+                    }
+                    if (dllink == null) {
+                        dllink = br.getRegex("u003d(https%3A%2F%2F.*?mp4)[^\",]*?(hd720|medium|small)(\"|,)").getMatch(0);
+                        if (dllink != null) {
+                            dllink = Encoding.urlDecode(Encoding.unicodeDecode(dllink), false);
+                        } else {
+                            /* Failed? Try to get streamlink! */
+                            dllink = br.getRegex("data\\-media\\-key=\"[^<>\"]+\"data\\-url=\"(https[^<>\"]+)\"[^@]+data\\-isvideo=\"true\"").getMatch(0);
+                        }
+                        isVideoStreamDwnload = dllink != null;
+                    }
+                    if (dllink != null) {
+                        con = br.openHeadConnection(dllink);
+                        filename = getFileNameFromHeader(con);
+                        if (isVideoStreamDwnload) {
+                            filename = this.removeDoubleExtensions(filename, "mp4");
+                            /* =m22?cpn=blablabla&c=WEB&cver=1.20160414 */
+                            /* Correct videourl */
+                            if (!dllink.contains("=m22")) {
+                                dllink += "=m22?c=WEB";
+                            }
+                        } else {
+                            /* Download of original file --> No workaround needed and filesize & final filename is known! */
+                            filesize = con.getLongContentLength();
+                            free_maxchunks = 1;
+                            free_resume = false;
+                        }
+                    }
+                } catch (final Throwable e) {
+                }
+            } else {
+                /* For photos */
+                dllink = br.getRegex("2\\][\t\n\r ]*?,\"(https?://[^<>\"]*?)\"").getMatch(0);
+            }
+            if (dllink != null && (filesize == 0 || filename == null)) {
+                if (con == null) {
+                    con = br.openHeadConnection(dllink);
+                }
+                if (!con.getContentType().contains("html")) {
+                    filesize = con.getLongContentLength();
+                    if (filename == null) {
+                        filename = getFileNameFromHeader(con);
+                    }
+                    if (filename == null) {
+                        filename = url_title;
+                        if (isVideoStreamDwnload) {
+                            filename += ".mp4";
+                        } else if (!isvideo) {
+                            filename += ".jpg";
+                        } else {
+                            /* Video download with unknown extension. */
+                        }
+                    } else if (isVideoStreamDwnload && !filename.endsWith(".mp4")) {
+                        filename = url_title + ".mp4";
+                    }
+                    link.setProperty("directlink", dllink);
+                } else {
+                    serverissue = true;
+                }
+            }
+        } finally {
+            if (con != null) {
+                try {
+                    con.disconnect();
+                } catch (final Throwable e) {
+                }
+            }
         }
         link.setFinalFileName(filename);
         link.setDownloadSize(filesize);
