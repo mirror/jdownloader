9b489af705778d9bce831fbeb323d0b136ce7eb0
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-09-27 14:10:54 +0000

    *Plugins: Fixes/changes* - Save.tv: Fixed small settings bug, removed 'BETA features' setting for now as it was never used - UnknownPornScript9: added this main host class and merged: winporncom, proporncom, vivatubecom, tubeoncom, viptubecom, iceporncom, nuvidcom -> This also fixes: proporncom - XFSv2: fixed small RegEx indexoutofbounds bug introduced due to 'DOMAINS' change
    *Plugins: New plugins*
    - UnknownPornScript9: hd21com closes #81411
    - xubstercom closes #83829
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37925 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/components/SiteType.java
+++ src/jd/plugins/components/SiteType.java
@@ -7,21 +7,17 @@ package jd.plugins.components;
  *
  */
 public class SiteType {
-
     public static enum SiteTemplate {
-
         /**
          * Sold on: <a href="http://www.digitaldutch.com/arles/">digitaldutch.com</a><br />
          * examples <a href="http:/palcomix.com/">palcomix.com</a>
          */
         ArlesImageWebPageCreator,
-
         /**
          * File host script first used for: <a href="http://www.chomikuj.pl/">chomikuj.pl</a><br />
          * examples <a href="http:/diskokosmiko.mx/">diskokosmiko.mx</a>
          */
         ChomikujPlScript,
-
         /**
          * File host script: <a href="http://www.daddyscripts.com/products.php?name=daddys_file_host">Daddy's File Host</a><br />
          * example: <a href="http://dosya.tc/">dosya.tc</a>
@@ -33,19 +29,16 @@ public class SiteType {
          * example: <a href="http://protect-link.org/">protect-link.org</a>
          */
         DaddyScripts_DaddysLinkProtector,
-
         /**
          * Open source image board: <a href="https://github.com/r888888888/danbooru">danbooru github project</a><br />
          * example: <a href="http://danbooru.donmai.us/">danbooru.donmai.us</a>
          */
         Danbooru,
-
         /**
          * Porn website/script: <a href="http://www.evilangel.com/">evilangel.com</a><br />
          * example: <a href="http://analacrobats.com/">analacrobats.com</a>
          */
         EvilAngelNetwork,
-
         /**
          * <a href="http://gempixel.com/project/premium-url-shortener/">Premium URL Shortener</a><br />
          * sold on <a href="http://codecanyon.net/item/premium-url-shortener/3688135">codecanyon.net</a><br />
@@ -53,14 +46,12 @@ public class SiteType {
          *
          */
         GemPixel_PremiumURLShortener,
-
         /**
          * Script used by some video/porn hosting sites.<br />
          * Can be bought e.g. from here: <a href="http://www.kernel-video-sharing.com/en/"</a> >kernel-video-sharing.com</a>.<br />
          * Demo: <a href="http://www.kvs-demo.com/">kvs-demo.com</a>
          */
         KernelVideoSharing,
-
         /**
          * Script used by some image hosting sites e.g.: <a href="http:/damimage.com/">damimage.com</a>. <br />
          * Can be bought e.g. from here:
@@ -78,7 +69,6 @@ public class SiteType {
          * Can be bought e.g. from here: <a href="http://de.corp.kaltura.com/" >http://de.corp.kaltura.com/</a>.<br />
          */
         KalturaVideoPlatform,
-
         /**
          * Should cover all given templates.<br />
          * <a href="http://sibsoft.net/xfilesharing.html">XFileSharing<a><br />
@@ -87,46 +77,38 @@ public class SiteType {
          * <a href="http://sibsoft.net/ximagesharing.html">XImageSharing<a><br />
          */
         SibSoft_XFileShare,
-
         /**
          * <a href="http://sibsoft.net/xlinker.html">XLinker</a>
          */
         SibSoft_XLinker,
-
         /**
          * <a href="https://mfscripts.com/yetishare/overview.html">YetiShare - PHP File Hosting Site Script</a>
          */
         MFScripts_YetiShare,
-
         /**
          * <a href="https://mfscripts.com/wurlie/overview.html">Wurlie - PHP Short Url Script</a>
          */
         MFScripts_Wurlie,
-
         /**
          * <a href="http://ouo.io/">ouo.io</a> linkcryptor script, usually with reCaptchaV2 as protection. <a
          */
         OuoIoCryptor,
-
         /**
          * <a href="http://www.hostedtube.com/">hosted tube</a> porn script/template provided by
          * <a href="http://pimproll.com/">pimproll.com</a>
          */
         PimpRoll_HostedTube,
-
         /**
          * <a href="http://www.brazzers.com/">brazzers.com</a> porn script/template used for a lot of porn paysites.<br />
          * Example#2: <a href="http://babes.com/">babes.com</a><br />
          * They often modify it heavily which is why we usually have a separate plugin for each website.<br />
          */
         PornPortal,
-
         /**
          * MultiUpload script by unknown, called Qooy Mirrors (taken from paypal description) <a href="http://qooy.com/sale.php">Qooy
          * Mirrors</a>
          */
         Qooy_Mirrors,
-
         /**
          * Script used by some chinese file-hosting sites. <a href="http:/sx566.com/">sx566.com</a>. Not sure what to call this script.
          *
@@ -134,17 +116,14 @@ public class SiteType {
          * google
          */
         Unknown_ChineseFileHosting,
-
         /**
          * Script used by some multihosters e.g. <a href="http://www.premiumax.net/">premiumax.net</a>. Not sure what to call this script.
          */
         Unknown_MultihosterScript,
-
         /**
          * the template that supports mirror stack type sites.. Not sure what to call this script.
          */
         Unknown_MirrorStack,
-
         /**
          * <b>Minimum requirements:</b> Usage of "flowplayer" <br />
          * <b>Additional requirements to be added to main class:</b> NO account support, offline via http code 404<br />
@@ -154,7 +133,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript1,
-
         /**
          * <b>Minimum requirements:</b> Website has to belong to that specified porn network/company <br />
          * <b>Additional requirements to be added to main class:</b> NO account support<br />
@@ -164,7 +142,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript2,
-
         /**
          * <b>Minimum requirements:</b> Website has to fit script <br />
          * <b>Additional requirements to be added to main class:</b> NO account support<br />
@@ -174,7 +151,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript3,
-
         /**
          * <b>Minimum requirements:</b> Website has to fit script <br />
          * <b>Additional requirements to be added to main class:</b> NO account support<br />
@@ -184,7 +160,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> <a href="http:/eroxia.com/">eroxia.com</a><br />
          */
         UnknownPornScript4,
-
         /**
          * <b>Minimum requirements:</b> Usage of "jwplayer" <br />
          * <b>Additional requirements to be added to main class:</b> Usage of "jwplayer" - one of the 2 basic jwplayer js/html sources, NO
@@ -195,7 +170,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript5,
-
         /**
          * <b>Minimum requirements:</b> Website has to fit script <br />
          * <b>Additional requirements to be added to main class:</b> NO account support<br />
@@ -205,7 +179,6 @@ public class SiteType {
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript6,
-
         /**
          * <b>Minimum requirements:</b> Website has to fit script <br />
          * <b>Additional requirements to be added to main class:</b> -<br />
@@ -226,54 +199,48 @@ public class SiteType {
         UnknownPornScript8,
         /**
          * <b>Minimum requirements:</b> Website has to fit script <br />
-         * <b>Main host class:</b> There is no main class<br />
-         * <b>Example that suits :</b> <a href="http:/nuvid.com/">nuvid.com</a><br />
+         * <b>Main host class:</b> UnknownPornScript9<br />
+         * <b>Examples that suit :</b> <a href="http:/nuvid.com/">nuvid.com</a><br />
+         * , <a href="http:/hd21.com/">hd21.com</a><br />
+         * <b>Example that does NOT suit main host class [needs separate class]:</b> <a href="http:/drtuber.com/">drtuber.com</a><br />
+         * <br />
          * <b>Example that needs a decrypter class :</b> -<br />
          */
         UnknownPornScript9,
-
         /**
          * Script used by some video hosting sites. <a href="http://cloudy.ec/">cloudy.ec</a>. Not sure what to call this script.
          */
         Unknown_VideoHosting,
-
         /**
          * Turbobit hosted sites. <a href="http://turbobit.net/">turbobit.net</a>
          */
         Turbobit_Turbobit,
-
         /**
          * linkbucks hosted sites. <a href="http://linkbucks.com/">linkbucks.com</a>
          */
         Linkbucks_Linkbucks,
-
         /**
          * safelinking hosted sites <a href="http://safelinking.net/">safelinking.net</a>
          */
         SafeLinking_SafeLinking,
-
         /**
          * Old link crypt script e.g. <a href="http://save-link.info">save-link.info</a> <br />
          * <b>Main decrypter class:</b> Zro10BasicDecrypt. <br />
          */
         Zero10BasicDecrypt,
-
         /**
          * <a href="https://shorte.st/">shorte.st</a> plugin, used to detect new/old domains that are using their service.
          */
         ShorteSt_ShorteSt,
-
         /**
          * <a href="https://adf.ly/">adf.ly</a> plugin, used to detect new/old domains that are using their service.
          */
         AdfLy_AdfLy,
-
         /**
          * URL shortening service provider. <br />
          * <a href="http://click.tf/services.html">A list of all supported domains</a>
          */
         URLShortnerLLP_URLShortner,
-
         /**
          * URL shortening or monetising platform by <a href="https://www.mightyscripts.com/">mightyscripts.com</a><br />
          * <a href="https://www.mightyscripts.com/downloads/adlinkfly-monetized-url-shortener/">AdLinkFly – Monetized URL
@@ -284,7 +251,6 @@ public class SiteType {
          */
         MightyScript_AdLinkFly,
         MightyScript_MightyURLShortener,
-
         /**
          * <a href="https://www.abdulibrahim.com/url-shortener-with-ads-and-powerful-admin-panel/">URL Shortener with Ads and Powerful Admin
          * Panel</a><br />
@@ -297,14 +263,12 @@ public class SiteType {
          * bottom bottom 0.8/9th screen ( copy right centred)<br />
          */
         AbdulIbrahim_URLShortener,
-
         /**
          * <a href="https://codecanyon.net/item/url-shortener-script-with-statistics/4461940">URL Shortener Script with Statistics @
          * codecanyon.net market place</a> & <a href="https://codepulsar.com/short1/index.php?a=short">demo site</a> <br />
          *
          */
         PricopAlexandru_URLShorterner1,
-
         /**
          * <a href="https://codecanyon.net/item/advanced-php-url-shortener/3450738">Advanced PHP URL Shortener @ codecanyon.net market
          * place</a> & <a href="https://codepulsar.com/short2/index.php?a=short">demo site</a> <br/>
@@ -315,17 +279,13 @@ public class SiteType {
          * chain link image top left in title bar next to site name.
          */
         PricopAlexandru_URLShorterner2,
-
         /**
          * do not use, raztoki will set as required.
          */
         NotApplicable,
-
         /**
          * typically word press sites have common traits like file paths(css/plugins/js/etc).
          */
         WordPress_Wordpress;
-
     }
-
 }
---
+++ src/jd/plugins/hoster/UnknownPornScript9.java
@@ -0,0 +1,167 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.Map;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
+import jd.PluginWrapper;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.components.PluginJSonUtils;
+import jd.plugins.components.SiteType.SiteTemplate;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "winporn.com", "proporn.com", "vivatube.com", "tubeon.com", "viptube.com", "hd21.com", "iceporn.com", "nuvid.com" }, urls = { "https?://(?:www\\.)?winporn\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?proporn\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?vivatube\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?tubeon\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?viptube\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?hd21\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?iceporn\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?", "https?://(?:www\\.)?nuvid\\.com/(?:[a-z]{2}/)?video/\\d+(?:/[a-z0-9\\-]+)?" })
+public class UnknownPornScript9 extends PluginForHost {
+    public UnknownPornScript9(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+    /* Similar sites but they use a different 'player_config' URL: drtuber.com, viptube.com */
+
+    /* Connection stuff */
+    private static final boolean free_resume       = true;
+    private static final int     free_maxchunks    = 0;
+    private static final int     free_maxdownloads = -1;
+    private String               dllink            = null;
+    private boolean              server_issues     = false;
+
+    @Override
+    public String getAGBLink() {
+        return "http://www.winporn.com/static/terms";
+    }
+
+    @SuppressWarnings({ "deprecation", "unchecked" })
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
+        dllink = null;
+        server_issues = false;
+        downloadLink.setMimeHint(CompiledFiletypeFilter.VideoExtensions.MP4);
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(downloadLink.getDownloadURL());
+        if (br.containsHTML("class=\"notifications__item notifications__item-error\"") || br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String url_filename = new Regex(downloadLink.getDownloadURL(), "([a-z0-9\\-]+)$").getMatch(0);
+        /* Access player json */
+        final String videoid = PluginJSonUtils.getJson(br, "vid");
+        if (StringUtils.isEmpty(videoid)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+        String config_url = br.getRegex("config_url\\s*:\\s*'(.*?)'").getMatch(0);
+        String embed = br.getRegex("embed\\s*:\\s*(\\d+)").getMatch(0);
+        if (config_url == null) {
+            /* Fallback */
+            config_url = "/player_config_json/";
+        }
+        if (embed == null) {
+            embed = "0";
+        }
+        br.getPage(String.format("%s?vid=%s&aid=&domain_id=&embed=%s&ref=&check_speed=0", config_url, videoid, embed));
+        final Map<String, Object> map = (Map<String, Object>) JavaScriptEngineFactory.jsonToJavaObject(br.toString());
+        // final long has_hq = JavaScriptEngineFactory.toLong(map.get("has_hq"), 1);
+        /* Most reliable way to find filename */
+        String filename = (String) map.get("title");
+        if (filename == null) {
+            filename = url_filename;
+        }
+        /* Prefer hq */
+        dllink = (String) JavaScriptEngineFactory.walkJson(map, "files/hq");
+        if (dllink == null) {
+            dllink = (String) JavaScriptEngineFactory.walkJson(map, "files/lq");
+        }
+        filename = filename.trim();
+        final String ext = getFileNameExtensionFromString(dllink, ".mp4");
+        downloadLink.setFinalFileName(Encoding.htmlDecode(filename) + ext);
+        URLConnectionAdapter con = null;
+        try {
+            con = br.openHeadConnection(dllink);
+            if (con.getResponseCode() == 404) {
+                /*
+                 * Small workaround for buggy servers that redirect and fail if the Referer is wrong then. Examples: hdzog.com
+                 */
+                final String redirect_url = con.getRequest().getUrl();
+                con = br.openHeadConnection(redirect_url);
+            }
+            if (!con.getContentType().contains("html")) {
+                downloadLink.setDownloadSize(con.getLongContentLength());
+            } else {
+                server_issues = true;
+            }
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Throwable e) {
+            }
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        if (server_issues) {
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Unknown server error", 10 * 60 * 1000l);
+        } else if (StringUtils.isEmpty(dllink)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, free_resume, free_maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            }
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return free_maxdownloads;
+    }
+
+    @Override
+    public SiteTemplate siteTemplateType() {
+        return SiteTemplate.UnknownPornScript9;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
