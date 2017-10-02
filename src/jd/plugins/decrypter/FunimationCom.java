835ebe619b83b95fa8770be54bebba7f49db9d32
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-10-02 12:01:11 +0000

    Reviewed: funimation.com, - removed this. nuking. - setBrowser isn't needed in host plugin, old stable is no more - all decrypter calls should call hoster plugin antiddos. they use Incapsula - file package only called within single method, no real need it in this - add download link to file package, should use _setFilePackage - set browser in decrypter, then methods don't require browser reference - hoster requestfileinfo still needs looking at. method has no purpose. - checkffmpeg really should be called before download
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@37964 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: ba20c31f6de59bda745e874201943e27bae96f78

--- src/jd/plugins/decrypter/FunimationCom.java
+++ src/jd/plugins/decrypter/FunimationCom.java
@@ -1,13 +1,12 @@
 package jd.plugins.decrypter;
 
-import java.io.IOException;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-
-import org.jdownloader.plugins.components.hls.HlsContainer;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 import jd.PluginWrapper;
 import jd.controlling.AccountController;
@@ -26,11 +25,11 @@ import jd.utils.JDUtilities;
 
 @DecrypterPlugin(revision = "$Revision: 37916 $", interfaceVersion = 3, names = { "funimation.com" }, urls = { "http(?:s)://www.funimation.com/(?:shows/)[-0-9a-zA-Z]+/[-0-9a-zA-Z]+/.*" })
 public class FunimationCom extends PluginForDecrypt {
+
     static private final String PLAYER_URL         = "https://www.funimation.com/player/";
     static private final String SHOWEXPERIENCE_API = "https://www.funimation.com/api/showexperience/";
     static private final String EXPERIENCE_API     = "https://www.funimation.com/api/experience/";
     private final char          SEPARATOR          = '-';
-    final FilePackage           filePackage        = FilePackage.getInstance();
 
     @SuppressWarnings("deprecation")
     public FunimationCom(final PluginWrapper wrapper) {
@@ -48,126 +47,119 @@ public class FunimationCom extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(final CryptedLink cryptedLink, final ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        try {
-            // Attempt to login
-            this.setBrowserExclusive();
-            loadPlugin();
-            final Account account = AccountController.getInstance().getValidAccount(plugin);
-            if (account != null) {
-                try {
-                    ((jd.plugins.hoster.FunimationCom) plugin).login(account, this.br, false);
-                } catch (final Throwable e) {
-                    throw e;
-                }
+        // Attempt to login
+        this.setBrowserExclusive();
+        loadPlugin();
+        ((jd.plugins.hoster.FunimationCom) plugin).setBrowser(br);
+        final Account account = AccountController.getInstance().getValidAccount(plugin);
+        if (account != null) {
+            try {
+                ((jd.plugins.hoster.FunimationCom) plugin).login(account, false);
+            } catch (final Throwable e) {
+                throw e;
             }
-            // set utf-8
-            this.br.setCustomCharset("utf-8");
-            // Load the linked page
-            this.br.setFollowRedirects(true);
-            br.getPage(cryptedLink.getCryptedUrl());
-            final String video_id = this.br.getRegex("id: \'(\\d+)\'").getMatch(0);
-            final String show_name = new Regex(cryptedLink.getCryptedUrl(), "http(?:s)://www.funimation.com/(?:shows/)([-0-9a-zA-Z]+)/").getMatch(0);
-            final String episode_name = new Regex(cryptedLink.getCryptedUrl(), "http(?:s)://www.funimation.com/(?:shows/)[-0-9a-zA-Z]+/([-0-9a-zA-Z]+)/").getMatch(0);
-            final String episode_number = this.br.getRegex("episodeNum: (\\d+),").getMatch(0);
-            final String season_number = this.br.getRegex("seasonNum: (\\d+),").getMatch(0);
-            final String alpha = this.br.getRegex("alpha: \"(\\w+)\",").getMatch(0);
-            final String language = this.br.getRegex("KANE_customdimensions.showLanguage\\s+=\\s+'(\\w+)';").getMatch(0);
-            final String title = show_name + "-" + episode_number + "-" + episode_name + "-" + language;
-            filePackage.setName(title);
-            final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            SecureRandom rnd = new SecureRandom();
-            StringBuilder sb = new StringBuilder(8);
-            for (int i = 0; i < 8; i++) {
-                sb.append(AB.charAt(rnd.nextInt(AB.length())));
-            }
-            final String random = sb.toString();
-            br.getPage(SHOWEXPERIENCE_API + video_id + "/?pinst_id=" + random);
-            String json_source = br.toString();
-            if (json_source.contains("\"errors\"") || json_source.contains("Invalid")) {
-                decryptedLinks.add(createOfflinelink(cryptedLink.getCryptedUrl(), "Unable to grab video links"));
-                return decryptedLinks;
-            }
-            json_source = Encoding.htmlDecode(json_source);
-            LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(json_source);
-            final ArrayList<Object> items_array = (ArrayList<Object>) entries.get("items");
-            for (Object i : items_array) {
-                entries = (LinkedHashMap<String, Object>) i;
-                final String src = entries.get("src").toString();
-                String filename = null;
-                if (src.contains(".mp4")) {
-                    filename = title + "-mobile.mp4";
-                    final DownloadLink dl = this.createDownloadlink(src);
-                    dl._setFilePackage(filePackage);
+        }
+        // set utf-8
+        br.setCustomCharset("utf-8");
+        // Load the linked page
+        br.setFollowRedirects(true);
+        ((jd.plugins.hoster.FunimationCom) plugin).getPage(br, cryptedLink.getCryptedUrl());
+        final String video_id = br.getRegex("id: \'(\\d+)\'").getMatch(0);
+        final String show_name = new Regex(cryptedLink.getCryptedUrl(), "http(?:s)://www.funimation.com/(?:shows/)([-0-9a-zA-Z]+)/").getMatch(0);
+        final String episode_name = new Regex(cryptedLink.getCryptedUrl(), "http(?:s)://www.funimation.com/(?:shows/)[-0-9a-zA-Z]+/([-0-9a-zA-Z]+)/").getMatch(0);
+        final String episode_number = br.getRegex("episodeNum: (\\d+),").getMatch(0);
+        final String season_number = br.getRegex("seasonNum: (\\d+),").getMatch(0);
+        final String alpha = br.getRegex("alpha: \"(\\w+)\",").getMatch(0);
+        final String language = br.getRegex("KANE_customdimensions.showLanguage\\s+=\\s+'(\\w+)';").getMatch(0);
+        final String title = show_name + "-" + episode_number + "-" + episode_name + "-" + language;
+        final FilePackage filePackage = FilePackage.getInstance();
+        filePackage.setName(title);
+        final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+        StringBuilder sb = new StringBuilder(8);
+        for (int i = 0; i < 8; i++) {
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        }
+        final String random = sb.toString();
+        ((jd.plugins.hoster.FunimationCom) plugin).getPage(br, SHOWEXPERIENCE_API + video_id + "/?pinst_id=" + random);
+        String json_source = br.toString();
+        if (json_source.contains("\"errors\"") || json_source.contains("Invalid")) {
+            decryptedLinks.add(createOfflinelink(cryptedLink.getCryptedUrl(), "Unable to grab video links"));
+            return decryptedLinks;
+        }
+        json_source = Encoding.htmlDecode(json_source);
+        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(json_source);
+        final ArrayList<Object> items_array = (ArrayList<Object>) entries.get("items");
+        for (Object i : items_array) {
+            entries = (LinkedHashMap<String, Object>) i;
+            final String src = entries.get("src").toString();
+            String filename = null;
+            if (src.contains(".mp4")) {
+                filename = title + "-mobile.mp4";
+                final DownloadLink dl = createDownloadlink(src);
+                filePackage.add(dl);
+                dl.setFinalFileName(filename);
+                dl.setContentUrl(cryptedLink.getCryptedUrl());
+                dl.setLinkID(filename);
+                dl.setAvailable(true);
+                decryptedLinks.add(dl);
+            } else {
+                final Browser br = this.br.cloneBrowser();
+                ((jd.plugins.hoster.FunimationCom) plugin).getPage(br, src);
+                List<HlsContainer> qualities = HlsContainer.getHlsQualities(br);
+                for (final HlsContainer h : qualities) {
+                    String quality = h.getResolution();
+                    filename = title + "-" + quality + h.getFileExtension();
+                    final DownloadLink dl = createDownloadlink(h.getDownloadurl());
+                    filePackage.add(dl);
                     dl.setFinalFileName(filename);
                     dl.setContentUrl(cryptedLink.getCryptedUrl());
                     dl.setLinkID(filename);
                     dl.setAvailable(true);
                     decryptedLinks.add(dl);
-                } else {
-                    br.getPage(src);
-                    List<HlsContainer> qualities = HlsContainer.getHlsQualities(br);
-                    for (final HlsContainer h : qualities) {
-                        String quality = h.getResolution();
-                        filename = title + "-" + quality + h.getFileExtension();
-                        final DownloadLink dl = this.createDownloadlink(h.getDownloadurl());
-                        dl._setFilePackage(filePackage);
-                        dl.setFinalFileName(filename);
-                        dl.setContentUrl(cryptedLink.getCryptedUrl());
-                        dl.setLinkID(filename);
-                        dl.setAvailable(true);
-                        decryptedLinks.add(dl);
-                    }
                 }
             }
-            // Get subtitles if available
-            br.getPage(EXPERIENCE_API + video_id);
-            json_source = br.toString();
-            json_source = Encoding.htmlDecode(json_source);
-            if (json_source.contains("Sorry, but the page you're looking for doesn't exist.")) {
-                decryptedLinks.add(createOfflinelink(cryptedLink.getCryptedUrl(), "Unable to grab subtitle links"));
-                return decryptedLinks;
-            }
-            entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(json_source);
-            final ArrayList<Object> seasons_array = (ArrayList<Object>) entries.get("seasons");
-            for (Object s : seasons_array) {
-                entries = (LinkedHashMap<String, Object>) s;
-                String season = entries.get("seasonId").toString();
-                if (season_number.equals(season)) {
-                    final ArrayList<Object> episodes_array = (ArrayList<Object>) entries.get("episodes");
-                    for (Object e : episodes_array) {
-                        entries = (LinkedHashMap<String, Object>) e;
-                        String episode = entries.get("episodeId").toString();
-                        if (episode_number.equals(episode)) {
-                            entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.walkJson(entries, "languages/" + language + "/alpha/" + alpha);
-                            final ArrayList<Object> sources_array = (ArrayList<Object>) entries.get("sources");
-                            // Only get the first array as the sources are the same between both arrays
-                            entries = (LinkedHashMap<String, Object>) sources_array.get(0);
-                            final ArrayList<Object> text_tracks_array = (ArrayList<Object>) entries.get("textTracks");
-                            for (Object t : text_tracks_array) {
-                                entries = (LinkedHashMap<String, Object>) t;
-                                String src = entries.get("src").toString();
-                                if (src.contains(".srt")) {
-                                    String filename = title + ".srt";
-                                    final DownloadLink dl = this.createDownloadlink(src);
-                                    dl._setFilePackage(filePackage);
-                                    dl.setFinalFileName(filename);
-                                    dl.setContentUrl(cryptedLink.getCryptedUrl());
-                                    dl.setLinkID(filename);
-                                    dl.setAvailable(true);
-                                    decryptedLinks.add(dl);
-                                }
+        }
+        // Get subtitles if available
+        ((jd.plugins.hoster.FunimationCom) plugin).getPage(br, EXPERIENCE_API + video_id);
+        json_source = br.toString();
+        json_source = Encoding.htmlDecode(json_source);
+        if (json_source.contains("Sorry, but the page you're looking for doesn't exist.")) {
+            decryptedLinks.add(createOfflinelink(cryptedLink.getCryptedUrl(), "Unable to grab subtitle links"));
+            return decryptedLinks;
+        }
+        entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(json_source);
+        final ArrayList<Object> seasons_array = (ArrayList<Object>) entries.get("seasons");
+        for (Object s : seasons_array) {
+            entries = (LinkedHashMap<String, Object>) s;
+            String season = entries.get("seasonId").toString();
+            if (season_number.equals(season)) {
+                final ArrayList<Object> episodes_array = (ArrayList<Object>) entries.get("episodes");
+                for (Object e : episodes_array) {
+                    entries = (LinkedHashMap<String, Object>) e;
+                    String episode = entries.get("episodeId").toString();
+                    if (episode_number.equals(episode)) {
+                        entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.walkJson(entries, "languages/" + language + "/alpha/" + alpha);
+                        final ArrayList<Object> sources_array = (ArrayList<Object>) entries.get("sources");
+                        // Only get the first array as the sources are the same between both arrays
+                        entries = (LinkedHashMap<String, Object>) sources_array.get(0);
+                        final ArrayList<Object> text_tracks_array = (ArrayList<Object>) entries.get("textTracks");
+                        for (Object t : text_tracks_array) {
+                            entries = (LinkedHashMap<String, Object>) t;
+                            String src = entries.get("src").toString();
+                            if (src.contains(".srt")) {
+                                String filename = title + ".srt";
+                                final DownloadLink dl = createDownloadlink(src);
+                                filePackage.add(dl);
+                                dl.setFinalFileName(filename);
+                                dl.setContentUrl(cryptedLink.getCryptedUrl());
+                                dl.setLinkID(filename);
+                                dl.setAvailable(true);
+                                decryptedLinks.add(dl);
                             }
                         }
                     }
                 }
-            }
-        } catch (final IOException e) {
-            this.logger.log(e);
-            return null;
-        } finally {
-            try {
-                this.br.getHttpConnection().disconnect();
-            } catch (final Throwable e) {
             }
         }
         return decryptedLinks;
