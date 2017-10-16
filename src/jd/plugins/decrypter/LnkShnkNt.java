382e158f5025e2f30ac1c00106e062a4646a74c0
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-10-16 07:36:02 +0000

    Fixed: linkshrink.net, minor changes - closes #84499
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38037 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f53bfb5c213e54b85464e48303e257e2ab586ac5

--- src/jd/plugins/decrypter/LnkShnkNt.java
+++ src/jd/plugins/decrypter/LnkShnkNt.java
@@ -16,10 +16,12 @@
 
 package jd.plugins.decrypter;
 
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import org.appwork.utils.StringUtils;
+
 import java.io.File;
 import java.util.ArrayList;
-
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
@@ -47,8 +49,12 @@ public class LnkShnkNt extends antiDDoSForDecrypt {
     }
 
     private static final String type_direct  = "https?://(www\\.)?linkshrink\\.net/[A-Za-z0-9]{4}=https?://.+";
-
     private static final String type_invalid = "https?://(www\\.)?linkshrink\\.net/(report|login)";
+
+    @Override
+    protected boolean useRUA() {
+        return true;
+    }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         br = new Browser();
@@ -65,65 +71,77 @@ public class LnkShnkNt extends antiDDoSForDecrypt {
             return decryptedLinks;
         }
         br.setCookie(getHost(), "s32", "1");
-        br.setCookie(getHost(), "AABE2", "1");
+        String link;
         getPage(parameter);
-        if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 404 || (br.getRedirectLocation() != null && br.getRedirectLocation().matches(type_invalid)) || br.containsHTML(">Link does not exist")) {
-            decryptedLinks.add(createOfflinelink(parameter));
-            return decryptedLinks;
-        } else if (this.br.getRedirectLocation() == null && this.br.toString().length() < 100) {
-            decryptedLinks.add(createOfflinelink(parameter));
-            return decryptedLinks;
-        }
-        String link = br.getRedirectLocation();
-        if (link != null && link.contains("linkshrink.net/")) {
-            getPage(link);
-            link = null;
-        }
-        if (link == null) {
-            if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
-                /* This part was coded blindly! */
-                for (int i = 0; i <= 3; i++) {
-
-                    final org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia sm = new org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia(br);
-                    File cf = null;
-                    try {
-                        cf = sm.downloadCaptcha(getLocalCaptchaFile());
-                    } catch (final Exception e) {
-                        if (org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia.FAIL_CAUSE_CKEY_MISSING.equals(e.getMessage())) {
-                            throw new PluginException(LinkStatus.ERROR_FATAL, "Host side solvemedia.com captcha error - please contact the " + this.getHost() + " support");
-                        }
-                        throw e;
-                    }
-                    final String code = getCaptchaCode("solvemedia", cf, param);
-                    final String chid = sm.getChallenge(code);
-                    postPage(br.getURL(), "adcopy_response=manual_challenge&adcopy_challenge=" + Encoding.urlEncode(chid));
-                    if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
-                        continue;
-                    }
-                    break;
-                }
-                if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
-                    throw new DecrypterException(DecrypterException.CAPTCHA);
-                }
+        while (true) {
+            if (br.getHttpConnection() != null && br.getHttpConnection().getResponseCode() == 404 || (br.getRedirectLocation() != null && br.getRedirectLocation().matches(type_invalid)) || br.containsHTML(">Link does not exist")) {
+                decryptedLinks.add(createOfflinelink(parameter));
+                return decryptedLinks;
+            } else if (br.getRedirectLocation() == null && br.toString().length() < 100) {
+                decryptedLinks.add(createOfflinelink(parameter));
+                return decryptedLinks;
             }
-            String continu = br.getRegex("href=(\"|')([^\r\n]+)\\1 (?:onClick)?[^>]*>Continue").getMatch(1);
-            if ("#".equals(continu)) {
-                // new method 20170122
-                continu = br.getRegex("<script>\\s*g\\.href\\s*=\\s*\"(https?://linkshrink.net/[a-zA-Z0-9]+)\";</script>").getMatch(0);
-                if (continu == null) {
-                    // new method 20170127
-                    continu = br.getRegex("\\.href\\s*=\\s*\\w+\\(\"([a-zA-Z0-9_/+=\\-%]+)\"\\)</script>").getMatch(0);
-                    if (continu != null) {
-                        continu = "/" + Encoding.Base64Decode(continu);
-                    }
-
-                }
-            }
-            if (continu == null) {
-                return null;
-            }
-            getPage(continu);
             link = br.getRedirectLocation();
+            if (link != null && link.contains("linkshrink.net/")) {
+                getPage(link);
+                link = null;
+            }
+            if (link == null) {
+                if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
+                    /* This part was coded blindly! */
+                    for (int i = 0; i <= 3; i++) {
+
+                        final org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia sm = new org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia(br);
+                        File cf = null;
+                        try {
+                            cf = sm.downloadCaptcha(getLocalCaptchaFile());
+                        } catch (final Exception e) {
+                            if (org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia.FAIL_CAUSE_CKEY_MISSING.equals(e.getMessage())) {
+                                throw new PluginException(LinkStatus.ERROR_FATAL, "Host side solvemedia.com captcha error - please contact the " + this.getHost() + " support");
+                            }
+                            throw e;
+                        }
+                        final String code = getCaptchaCode("solvemedia", cf, param);
+                        final String chid = sm.getChallenge(code);
+                        postPage(br.getURL(), "adcopy_response=manual_challenge&adcopy_challenge=" + Encoding.urlEncode(chid));
+                        if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
+                            continue;
+                        }
+                        break;
+                    }
+                    if (br.containsHTML("api\\.solvemedia\\.com/papi")) {
+                        throw new DecrypterException(DecrypterException.CAPTCHA);
+                    }
+                }
+                String continu = br.getRegex("href=(\"|')([^\r\n]+)\\1 (?:onClick)?[^>]*>Continue").getMatch(1);
+                if ("#".equals(continu)) {
+                    // new method 20170122
+                    continu = br.getRegex("<script>\\s*g\\.href\\s*=\\s*\"(https?://linkshrink.net/[a-zA-Z0-9]+)\";</script>").getMatch(0);
+                    if (continu == null) {
+                        // new method 20170127
+                        continu = br.getRegex("\\.href\\s*=\\s*\\w+\\(\"([a-zA-Z0-9_/+=\\-%]+)\"\\)</script>").getMatch(0);
+                        if (continu == null) {
+                            // same shit different js/placement 201710xx
+                            continu = br.getRegex("\\.onclick\\s*=\\s*function\\(\\)\\s*\\{\\s*window\\.open\\(revC\\(\"(.*?)\"\\)").getMatch(0);
+                        }
+                        if (continu != null) {
+                            continu = "/" + Encoding.Base64Decode(continu);
+                        }
+                    }
+                }
+                if (continu == null) {
+                    return null;
+                }
+                final String ref = br.getURL();
+                getPage(continu);
+                if (StringUtils.contains(br.getRedirectLocation(), "linkshrink.net/")) {
+                    // referer bug, or not so bug, since we are not following redirects browser treats it as standard request.
+                    br.getHeaders().put("Referer", ref);
+                    getPage(br.getRedirectLocation());
+                    continue;
+                }
+            }
+            break;
         }
         if (link != null) {
             decryptedLinks.add(createDownloadlink(link));
