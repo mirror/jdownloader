6482eab32f5d5da4126bb74d5046ad317c2f9a20
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-10-21 23:37:00 +0000

    Added: liens-telechargement.com, decrypter support - re: https://board.jdownloader.org/showthread.php?p=410414#post410414 & https://board.jdownloader.org/showthread.php?p=410415#post410415
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38073 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/DlPrteCom.java
+++ src/jd/plugins/decrypter/DlPrteCom.java
@@ -15,6 +15,8 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.decrypter;
 
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -27,18 +29,17 @@ import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
 /**
  *
  * @version raz_Template
  * @author raztoki
  */
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "dl-protecte.com" }, urls = { "https?://(?:www\\.)?(?:dl-protecte\\.(?:com|org)|protect-lien\\.com|protect-zt\\.com|protecte-link\\.com)/\\S+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "dl-protecte.com" }, urls = { "https?://(?:www\\.)?(?:dl-protecte\\.(?:com|org)|protect-lien\\.com|protect-zt\\.com|protecte-link\\.com|liens-telechargement\\.com)/\\S+" })
 public class DlPrteCom extends antiDDoSForDecrypt {
+
     @Override
     public String[] siteSupportedNames() {
-        return new String[] { "dl-protecte.com", "dl-protecte.org", "protect-lien.com", "protect-zt.com", "protecte-link.com" };
+        return new String[] { "dl-protecte.com", "dl-protecte.org", "protect-lien.com", "protect-zt.com", "protecte-link.com", "liens-telechargement.com" };
     }
 
     public DlPrteCom(PluginWrapper wrapper) {
@@ -51,7 +52,7 @@ public class DlPrteCom extends antiDDoSForDecrypt {
         br.setFollowRedirects(true);
         getPage(parameter);
         /* Error handling */
-        if (br.containsHTML("Page Not Found") || br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404) {
+        if (br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404 || br.containsHTML("Page Not Found")) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
