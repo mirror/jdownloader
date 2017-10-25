9131e034bf5caea45d6335859097510bd32ee372
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-10-25 13:09:19 +0000

    FilestoreTo: -added premium support -fixes #84404
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38096 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/FilestoreTo.java
+++ src/jd/plugins/hoster/FilestoreTo.java
@@ -15,14 +15,19 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
+import java.util.Locale;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
 import jd.http.Browser;
+import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -34,6 +39,7 @@ import jd.plugins.components.UserAgents.BrowserName;
 
 import org.appwork.utils.StringUtils;
 import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "filestore.to" }, urls = { "http://(www\\.)?filestore\\.to/\\?d=[A-Z0-9]+" })
 public class FilestoreTo extends PluginForHost {
@@ -42,6 +48,94 @@ public class FilestoreTo extends PluginForHost {
     public FilestoreTo(final PluginWrapper wrapper) {
         super(wrapper);
         setStartIntervall(2000l);
+        enablePremium("http://filestore.to/premium");
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(Account account) throws Exception {
+        AccountInfo ai = new AccountInfo();
+        login(ai, account);
+        if (!StringUtils.endsWithCaseInsensitive(br.getURL(), "/konto")) {
+            br.getPage("/konto");
+        }
+        final String validUntilString = br.getRegex("Premium-Status\\s*</small>\\s*<div class=\"value text-success\">\\s*(.*?)\\s*Uhr").getMatch(0);
+        if (validUntilString != null) {
+            final long until = TimeFormatter.getMilliSeconds(validUntilString, "dd'.'MM'.'yyyy' - 'HH':'mm", Locale.ENGLISH);
+            ai.setValidUntil(until);
+            if (!ai.isExpired()) {
+                ai.setStatus("Premium");
+                account.setType(AccountType.PREMIUM);
+                account.setMaxSimultanDownloads(20);
+                account.setConcurrentUsePossible(true);
+                return ai;
+            }
+        }
+        ai.setStatus("Free");
+        account.setType(AccountType.FREE);
+        account.setMaxSimultanDownloads(1);
+        account.setConcurrentUsePossible(false);
+        return ai;
+    }
+
+    private boolean isCookieSet(Browser br, String key) {
+        final String value = br.getCookie(getHost(), key);
+        return StringUtils.isNotEmpty(value) && !StringUtils.equalsIgnoreCase(value, "deleted");
+    }
+
+    private AccountInfo login(final AccountInfo ai, final Account account) throws Exception {
+        synchronized (account) {
+            final Cookies cookies = account.loadCookies("");
+            try {
+                if (cookies != null) {
+                    br.setCookies(getHost(), cookies);
+                    br.getPage("http://filestore.to/konto");
+                }
+                if (!isCookieSet(br, "login")) {
+                    account.clearCookies("");
+                    br.getPage("http://filestore.to/login");
+                    final Form form = br.getFormbyKey("Email");
+                    form.put("EMail", Encoding.urlEncode(account.getUser()));
+                    form.put("Password", Encoding.urlEncode(account.getPass()));
+                    br.submitForm(form);
+                    br.followRedirect();
+                    if (!isCookieSet(br, "login")) {
+                        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                    }
+                }
+                account.saveCookies(br.getCookies(getHost()), "");
+                return ai;
+            } catch (final PluginException e) {
+                if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                    account.clearCookies("");
+                }
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    public void handlePremium(DownloadLink link, Account account) throws Exception {
+        requestFileInformation(link);
+        login(null, account);
+        boolean ok = false;
+        for (int i = 1; i < 3; i++) {
+            try {
+                br.getPage(link.getDownloadURL());
+                ok = true;
+                break;
+            } catch (final Exception e) {
+                continue;
+            }
+        }
+        if (ok) {
+            if (AccountType.FREE.equals(account.getType())) {
+                download(link, account, true, 1);
+            } else {
+                download(link, account, true, 0);
+            }
+        } else {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
     }
 
     @Override
@@ -115,10 +209,7 @@ public class FilestoreTo extends PluginForHost {
         throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
     }
 
-    @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception {
-        requestFileInformation(downloadLink);
-        // lets check they at times have issues
+    private void download(final DownloadLink downloadLink, final Account account, final boolean resume, int maxChunks) throws Exception {
         if (br.containsHTML(Pattern.quote(">Der Download ist nicht bereit !</span><br />Die Datei wird noch auf die Server verteilt.<br />Bitte versuche es in ein paar Minuten erneut.<"))) {
             throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, 20 * 60 * 1000l);
         }
@@ -131,14 +222,19 @@ public class FilestoreTo extends PluginForHost {
         f = br.getFormByRegex(">Download starten</button>");
         if (f != null) {
             // not enforced
-            processWait();
+            if (account == null || AccountType.FREE.equals(account.getType())) {
+                processWait();
+            }
             br.submitForm(f);
         }
         final String dllink = getDllink();
         if (StringUtils.isEmpty(dllink)) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        dl = new jd.plugins.BrowserAdapter().openDownload(br, downloadLink, dllink, true, 1);
+        if (resume == false) {
+            maxChunks = 1;
+        }
+        dl = new jd.plugins.BrowserAdapter().openDownload(br, downloadLink, dllink, resume, maxChunks);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
             if (br.containsHTML("Derzeit haben wir Serverprobleme und arbeiten daran\\. Bitte nochmal versuchen\\.")) {
@@ -150,6 +246,12 @@ public class FilestoreTo extends PluginForHost {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception {
+        requestFileInformation(downloadLink);
+        download(downloadLink, null, true, 1);
     }
 
     private void processWait() throws PluginException {
