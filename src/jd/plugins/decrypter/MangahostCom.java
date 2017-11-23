a11f095ce662b09510823bd824bf8013c767f61c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-11-23 18:38:17 +0000

    fixes ticket 74922
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38284 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MangahostCom.java
+++ src/jd/plugins/decrypter/MangahostCom.java
@@ -15,9 +15,6 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.decrypter;
 
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
@@ -29,16 +26,18 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangahost.com" }, urls = { "https?://(?:www\\.)?(?:br\\.)?(mangahost\\.(com|net|me|org)|yesmangas\\.net|mangashost\\.net|mangashost\\.com)/manga/[^/]+/([^\\s]*\\d+(\\.\\d+|[a-z])?|one-shot)" })
-public class MangahostCom extends antiDDoSForDecrypt {
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mangahost.com" }, urls = { "https?://(?:www\\.)?(?:br\\.)?(mangahost\\.(com|net|me|org)|yesmangas\\.net|mangashost\\.net|mangashost\\.com|mangahosts\\.com)/manga/[^/]+/([^\\s]*\\d+(\\.\\d+|[a-z])?|one-shot)" })
+public class MangahostCom extends antiDDoSForDecrypt {
     public MangahostCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     @Override
     public String[] siteSupportedNames() {
-        return new String[] { "mangahost.com", "mangahost.net", "mangahost.me", "mangahost.org", "yesmangas.net", "mangashost.net", "mangashost.com" };
+        return new String[] { "mangahosts.com", "mangahost.com", "mangahost.net", "mangahost.me", "mangahost.org", "yesmangas.net", "mangashost.net", "mangashost.com" };
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
