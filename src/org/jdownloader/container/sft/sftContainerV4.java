539245f153d458f672d79c1652b75cc6f68945ec
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-11-27 12:48:10 +0000

    fixes thread 75799
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38301 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: cc1b3f2f540f837340a626eda85bb1cecc83a6b5

--- src/org/jdownloader/container/sft/sftContainerV4.java
+++ src/org/jdownloader/container/sft/sftContainerV4.java
@@ -8,10 +8,9 @@ import java.util.Arrays;
 
 import javax.xml.bind.DatatypeConverter;
 
-import sun.misc.BASE64Decoder;
+import org.appwork.utils.encoding.Base64;
 
 public class sftContainerV4 extends sftContainer {
-
     public static String            MAGIC_SHA512_CALL = "callstackapi";
     public static String            MAGIC_SHA512_RELO = "SFT Loader Reloaded";
     protected final DelphiFormEntry sft_root;
@@ -21,58 +20,50 @@ public class sftContainerV4 extends sftContainer {
 
     public sftContainerV4(DelphiFormBinLoader dfm) throws IOException, NoSuchAlgorithmException {
         super(dfm);
-
         sft_root = dfm.getRoot().find(NODE_ITEMS).get(0);
         DelphiFormEntry node;
-
         MessageDigest sh512 = MessageDigest.getInstance(CRYPT_SHA512);
         byte[] sha512_callkey = sh512.digest(MAGIC_SHA512_CALL.getBytes());
         byte[] sha512_reloadedkey = sh512.digest(MAGIC_SHA512_RELO.getBytes());
         byte[] eHeader = null;
-
-        BASE64Decoder base64 = new BASE64Decoder();
-
         RC4 rc4 = new RC4(sha512_callkey);
-        eHeader = base64.decodeBuffer(sft_root.find(NODE_HEADER).getValue());
+        eHeader = Base64.decode(sft_root.find(NODE_HEADER).getValue());
         rc4.encode(eHeader);
         /* CHECK: we should always use new String (bytes,charset) to avoid issues with system charset and utf-8 */
         this.strHeader = new String(eHeader);
-
         // decode header information
         rc4.init(sha512_reloadedkey);
         node = sft_root.find(NODE_DESCRIPT);
         if (node != null) {
-
-            byte[] eValue = base64.decodeBuffer(node.getValue());
+            byte[] eValue = Base64.decode(node.getValue());
             rc4.encode(eValue);
             this.strDescription = new String(eValue, "ISO-8859-1");
         }
-
         node = sft_root.find(NODE_UPLOADER);
         if (node != null) {
-            byte[] eValue = base64.decodeBuffer(node.getValue());
+            byte[] eValue = Base64.decode(node.getValue());
             rc4.encode(eValue);
             this.strUploader = new String(eValue, "ISO-8859-1");
         }
-
         node = sft_root.find(NODE_COMMENT);
         if (node != null) {
-            byte[] eValue = base64.decodeBuffer(node.getValue());
+            byte[] eValue = Base64.decode(node.getValue());
             rc4.encode(eValue);
             this.strComment = new String(eValue, "ISO-8859-1");
         }
-
         // check if file is valid and check if password needed
         if (this.strHeader.substring(0, 3).equals("SFT")) {
             this.passwordNeeded = this.strHeader.charAt(3) == '1';
             int pos = this.strHeader.lastIndexOf('#') + 1;
             this.strHeader = this.strHeader.substring(pos);
-
             if (!this.passwordNeeded) {
-                if (!this.setPassword(null)) throw new UnsupportedOperationException("decrypt error");
+                if (!this.setPassword(null)) {
+                    throw new UnsupportedOperationException("decrypt error");
+                }
             }
-        } else
+        } else {
             throw new UnsupportedOperationException("decrypt error");
+        }
     }
 
     @Override
@@ -82,19 +73,15 @@ public class sftContainerV4 extends sftContainer {
             byte[] pass = ("callstackapi" + (cs == null ? "" : new String(cs))).getBytes("UTF-8");
             byte[] sha512_pass = MessageDigest.getInstance(CRYPT_SHA512).digest(pass);
             sha512_pass = Arrays.copyOf(sha512_pass, sha512_pass.length / 2);
-
             // YurryXOR
-            for (int j = 0; j < sha512_pass.length; j++)
+            for (int j = 0; j < sha512_pass.length; j++) {
                 sha512_pass[j] ^= pass[j % pass.length];
-
+            }
             RCx rcx = new RCx(sha512_pass);
             rcx.decode(checkHeader);
-
             checkHeader = Arrays.copyOfRange(checkHeader, checkHeader.length - 41, checkHeader.length);
-
             if (new String(checkHeader).equals(MAGIC_SERIAL)) {
                 DelphiFormEntry node = sft_root.find("FTPDownloads");
-
                 linkList = new ArrayList<String>();
                 for (int i = 0; i < node.getChildLength(); i++) {
                     byte[] bHost = null;
@@ -102,32 +89,36 @@ public class sftContainerV4 extends sftContainer {
                     byte[] bPassword = null;
                     byte[] bDirname = null;
                     byte[] bFilename = null;
-
                     DelphiFormEntry eHost = node.get(i);
                     DelphiFormEntry eDir = eHost.find(NODE_DIRS).get(0);
                     DelphiFormEntry eFile = eDir.find(NODE_FILES).get(0);
-
                     DelphiFormEntry neHost = eHost.find(NODE_HOST);
                     DelphiFormEntry neUsername = eHost.find(NODE_USERNAME);
                     DelphiFormEntry nePassword = eHost.find(NODE_PASSWORD);
                     DelphiFormEntry neDirname = eDir.find(NODE_DIRNAME);
                     DelphiFormEntry neFilename = eFile.find(NODE_FILENAME);
-
                     String Host = neHost == null ? null : neHost.getValue();
                     String Username = neUsername == null ? null : neUsername.getValue();
                     String Password = nePassword == null ? null : nePassword.getValue();
                     String Dirname = neDirname == null ? null : neDirname.getValue();
                     String Filename = neFilename == null ? null : neFilename.getValue();
-
-                    if (Host != null) bHost = DatatypeConverter.parseHexBinary(Host);
-                    if (Username != null) bUsername = DatatypeConverter.parseHexBinary(Username);
-                    if (Password != null) bPassword = DatatypeConverter.parseHexBinary(Password);
-                    if (Dirname != null) bDirname = DatatypeConverter.parseHexBinary(Dirname);
-                    if (Filename != null) bFilename = DatatypeConverter.parseHexBinary(Filename);
-
+                    if (Host != null) {
+                        bHost = DatatypeConverter.parseHexBinary(Host);
+                    }
+                    if (Username != null) {
+                        bUsername = DatatypeConverter.parseHexBinary(Username);
+                    }
+                    if (Password != null) {
+                        bPassword = DatatypeConverter.parseHexBinary(Password);
+                    }
+                    if (Dirname != null) {
+                        bDirname = DatatypeConverter.parseHexBinary(Dirname);
+                    }
+                    if (Filename != null) {
+                        bFilename = DatatypeConverter.parseHexBinary(Filename);
+                    }
                     sha512_pass[0] ^= (byte) i;
                     rcx.init(sha512_pass);
-
                     if (bHost != null) {
                         rcx.reset();
                         rcx.decode(bHost);
@@ -158,14 +149,13 @@ public class sftContainerV4 extends sftContainer {
                     String link = buildFTPLink(hosts[0].getBytes("UTF-8"), (short) (Integer.parseInt(hosts[1]) & 0xFFFF), bUsername, bPassword, bDirname, bFilename);
                     linkList.add(link);
                 }
-
                 decryptedState = true;
-            } else
+            } else {
                 decryptedState = false;
+            }
         } catch (Exception e) {
             decryptedState = false;
         }
-
         return decryptedState;
     }
 
@@ -176,9 +166,10 @@ public class sftContainerV4 extends sftContainer {
 
     @Override
     public ArrayList<String> getFormatedLinks() {
-        if (decryptedState)
+        if (decryptedState) {
             return linkList;
-        else
+        } else {
             return null;
+        }
     }
 }
--- src/org/jdownloader/container/sft/sftContainerV8.java
+++ src/org/jdownloader/container/sft/sftContainerV8.java
@@ -10,13 +10,11 @@ import java.util.regex.Pattern;
 
 import javax.xml.bind.DatatypeConverter;
 
+import org.appwork.utils.encoding.Base64;
 import org.bouncycastle.crypto.engines.BlowfishEngine;
 import org.bouncycastle.crypto.params.KeyParameter;
 
-import sun.misc.BASE64Decoder;
-
 public class sftContainerV8 extends sftContainer {
-
     public static final byte[]      SENSITIVPASSWORD = "memory".getBytes();
     protected byte[]                bHeader;
     protected boolean               decryptedState;
@@ -26,14 +24,10 @@ public class sftContainerV8 extends sftContainer {
 
     public sftContainerV8(DelphiFormBinLoader dfm, byte[] magicDecryptionKey) throws IOException, NoSuchAlgorithmException {
         super(dfm);
-
         sft_root = dfm.getRoot().find(NODE_ITEMS).get(0);
-
-        byte[] eHeader = new BASE64Decoder().decodeBuffer(sft_root.find(NODE_HEADER).getValue());
-
+        byte[] eHeader = Base64.decode(sft_root.find(NODE_HEADER).getValue());
         RC4 rc = new RC4(Arrays.copyOf(magicDecryptionKey, 128));
         rc.encode(eHeader);
-
         this.bHeader = eHeader;
         this.magicDecryptionKey = magicDecryptionKey;
         this.setPassword(null);
@@ -45,22 +39,22 @@ public class sftContainerV8 extends sftContainer {
         try {
             this.decryptedState = false;
             byte[] password;
-
             if (cs == null) {
                 password = new byte[MAGIC_SN.length];
-                for (int i = 0; i < password.length; ++i)
+                for (int i = 0; i < password.length; ++i) {
                     password[i] = (byte) MAGIC_SN[i];
+                }
             } else {
                 password = new byte[MAGIC_SN.length < cs.length ? cs.length : MAGIC_SN.length];
                 int i;
-                for (i = 0; i < cs.length; ++i)
+                for (i = 0; i < cs.length; ++i) {
                     password[i] = (byte) cs[i];
-                for (; i < password.length; ++i)
+                }
+                for (; i < password.length; ++i) {
                     password[i] = (byte) MAGIC_SN[i];
+                }
             }
-
             byte[] headerCheck = new byte[bHeader.length];
-
             headerCheck[0] = bHeader[0];
             for (int i = 1; i < headerCheck.length; ++i) {
                 byte xor = password[i];
@@ -68,57 +62,48 @@ public class sftContainerV8 extends sftContainer {
                 xor ^= bHeader[i - 1];
                 headerCheck[i] = xor;
             }
-
             Pattern decryptPattern = Pattern.compile("^SFT#(\\d)#(\\d+)#SFT##$");
             /* CHECK: we should always use new String (bytes,charset) to avoid issues with system charset and utf-8 */
             Matcher decryptMatcher = decryptPattern.matcher(new String(headerCheck));
-
             if (decryptMatcher.matches()) {
                 int passwordNeeded = Integer.parseInt(decryptMatcher.group(1));
-
                 this.passwordNeeded = passwordNeeded > 0;
-
-                BASE64Decoder base64 = new BASE64Decoder();
                 byte[] sha256 = MessageDigest.getInstance(CRYPT_SHA256).digest(password);
                 RC4 rc = new RC4(sha256);
                 DelphiFormEntry node;
-
                 node = sft_root.find(NODE_DESCRIPT);
                 if (node != null) {
-                    byte[] bValue = base64.decodeBuffer(node.getValue());
+                    byte[] bValue = Base64.decode(node.getValue());
                     rc.encode(bValue);
                     this.strDescription = new String(bValue, "ISO-8859-1");
                 }
-
                 node = sft_root.find(NODE_UPLOADER);
                 if (node != null) {
-                    byte[] bValue = base64.decodeBuffer(node.getValue());
+                    byte[] bValue = Base64.decode(node.getValue());
                     rc.encode(bValue);
                     this.strUploader = new String(bValue, "ISO-8859-1");
                 }
-
                 node = sft_root.find(NODE_COMMENT);
                 if (node != null) {
-                    byte[] bValue = base64.decodeBuffer(node.getValue());
+                    byte[] bValue = Base64.decode(node.getValue());
                     rc.encode(bValue);
                     this.strComment = new String(bValue, "ISO-8859-1");
                 }
-
                 byte[] magicStringCopy = null;
                 if (passwordNeeded > 0) {
                     magicStringCopy = new byte[cs.length];
-                    for (int i = 0; i < magicStringCopy.length; ++i)
+                    for (int i = 0; i < magicStringCopy.length; ++i) {
                         magicStringCopy[i] = (byte) cs[i];
-                } else
+                    }
+                } else {
                     magicStringCopy = Arrays.copyOfRange(sftBinary.MAGIC, 1, sftBinary.MAGIC.length);
-
+                }
                 ArrayList<byte[]> HostList = new ArrayList<byte[]>();
                 ArrayList<byte[]> UsernameList = new ArrayList<byte[]>();
                 ArrayList<byte[]> PasswordList = new ArrayList<byte[]>();
                 ArrayList<byte[]> DirList = new ArrayList<byte[]>();
                 ArrayList<byte[]> FileList = new ArrayList<byte[]>();
                 linkList = new ArrayList<String>();
-
                 node = sft_root.find(NODE_FTPDOWNLOAD);
                 for (int i = node.getChildLength() - 1; i >= 0; i--) {
                     byte[] bHost = null;
@@ -127,36 +112,40 @@ public class sftContainerV8 extends sftContainer {
                     byte[] bPassword = null;
                     byte[] bDirname = null;
                     byte[] bFilename = null;
-
                     {
                         DelphiFormEntry eHost = node.get(i);
                         DelphiFormEntry eDir = eHost.find(NODE_DIRS).get(0);
                         DelphiFormEntry eFile = eDir.find(NODE_FILES).get(0);
-
                         DelphiFormEntry neHost = eHost.find(NODE_HOST);
                         // DelphiFormEntry nePort = eHost.find("Port");
                         DelphiFormEntry neUsername = eHost.find(NODE_USERNAME);
                         DelphiFormEntry nePassword = eHost.find(NODE_PASSWORD);
                         DelphiFormEntry neDirname = eDir.find(NODE_DIRNAME);
                         DelphiFormEntry neFilename = eFile.find(NODE_FILENAME);
-
                         String Host = neHost == null ? null : neHost.getValue();
                         // String Port = nePort == null ? null : nePort.getValue();
                         String Username = neUsername == null ? null : neUsername.getValue();
                         String Password = nePassword == null ? null : nePassword.getValue();
                         String Dirname = neDirname == null ? null : neDirname.getValue();
                         String Filename = neFilename == null ? null : neFilename.getValue();
-
-                        if (Host != null) bHost = DatatypeConverter.parseHexBinary(Host);
+                        if (Host != null) {
+                            bHost = DatatypeConverter.parseHexBinary(Host);
+                        }
                         // if (Port != null) bPort = Port.getBytes();
-                        if (Username != null) bUsername = DatatypeConverter.parseHexBinary(Username);
-                        if (Password != null) bPassword = DatatypeConverter.parseHexBinary(Password);
-                        if (Dirname != null) bDirname = DatatypeConverter.parseHexBinary(Dirname);
-                        if (Filename != null) bFilename = Filename.getBytes("UTF-8");
+                        if (Username != null) {
+                            bUsername = DatatypeConverter.parseHexBinary(Username);
+                        }
+                        if (Password != null) {
+                            bPassword = DatatypeConverter.parseHexBinary(Password);
+                        }
+                        if (Dirname != null) {
+                            bDirname = DatatypeConverter.parseHexBinary(Dirname);
+                        }
+                        if (Filename != null) {
+                            bFilename = Filename.getBytes("UTF-8");
+                        }
                     }
-
                     RCx rcx = new RCx(null);
-
                     rcx.init2(magicDecryptionKey);
                     rcx.encode2(bHost);
                     rcx.init2(magicDecryptionKey);
@@ -165,53 +154,71 @@ public class sftContainerV8 extends sftContainer {
                     rcx.encode2(bPassword);
                     rcx.init2(magicDecryptionKey);
                     rcx.encode2(bDirname);
-
                     HostList.add(bHost);
                     UsernameList.add(bUsername);
                     PasswordList.add(bPassword);
                     DirList.add(bDirname);
                     FileList.add(bFilename);
-
-                    if (bHost != null) manipulateMagicString(magicStringCopy, bHost[bHost.length - 1]);
-                    if (bUsername != null) manipulateMagicString(magicStringCopy, bUsername[bUsername.length - 1]);
-                    if (bPassword != null) manipulateMagicString(magicStringCopy, bPassword[bPassword.length - 1]);
-                    if (bDirname != null) manipulateMagicString(magicStringCopy, bDirname[bDirname.length - 1]);
+                    if (bHost != null) {
+                        manipulateMagicString(magicStringCopy, bHost[bHost.length - 1]);
+                    }
+                    if (bUsername != null) {
+                        manipulateMagicString(magicStringCopy, bUsername[bUsername.length - 1]);
+                    }
+                    if (bPassword != null) {
+                        manipulateMagicString(magicStringCopy, bPassword[bPassword.length - 1]);
+                    }
+                    if (bDirname != null) {
+                        manipulateMagicString(magicStringCopy, bDirname[bDirname.length - 1]);
+                    }
                 }
-
                 for (int i = 0; i < node.getChildLength(); ++i) {
                     byte[] bHost = HostList.get(i);
                     byte[] bUsername = UsernameList.get(i);
                     byte[] bPassword = PasswordList.get(i);
                     byte[] bDirname = DirList.get(i);
                     byte[] bFilename = FileList.get(i);
-
                     decodeFilename(magicStringCopy, bFilename);
                     if (bDirname != null) {
-                        if (!toBase64String(magicStringCopy, bDirname)) return false;
+                        if (!toBase64String(magicStringCopy, bDirname)) {
+                            return false;
+                        }
                     }
                     if (bPassword != null) {
-                        if (!toBase64String(magicStringCopy, bPassword)) return false;
+                        if (!toBase64String(magicStringCopy, bPassword)) {
+                            return false;
+                        }
                     }
                     if (bUsername != null) {
-                        if (!toBase64String(magicStringCopy, bUsername)) return false;
+                        if (!toBase64String(magicStringCopy, bUsername)) {
+                            return false;
+                        }
                     }
                     if (bHost != null) {
-                        if (!toBase64String(magicStringCopy, bHost)) return false;
+                        if (!toBase64String(magicStringCopy, bHost)) {
+                            return false;
+                        }
                     }
-
-                    if (bHost != null) bHost = decodeCoreInformation(bHost);
-                    if (bUsername != null) bUsername = decodeCoreInformation(bUsername);
-                    if (bPassword != null) bPassword = decodeCoreInformation(bPassword);
-                    if (bDirname != null) bDirname = decodeCoreInformation(bDirname);
-
+                    if (bHost != null) {
+                        bHost = decodeCoreInformation(bHost);
+                    }
+                    if (bUsername != null) {
+                        bUsername = decodeCoreInformation(bUsername);
+                    }
+                    if (bPassword != null) {
+                        bPassword = decodeCoreInformation(bPassword);
+                    }
+                    if (bDirname != null) {
+                        bDirname = decodeCoreInformation(bDirname);
+                    }
                     String link = buildFTPLink(bHost, (short) 0, bUsername, bPassword, bDirname, bFilename);
                     linkList.add(link);
                 }
-
                 this.decryptedState = true;
                 return true;
-            } else
+            } else {
                 this.passwordNeeded = true;
+            }
         } catch (Exception e) {
             e.printStackTrace();
             this.decryptedState = false;
@@ -220,30 +227,24 @@ public class sftContainerV8 extends sftContainer {
     }
 
     private byte[] decodeCoreInformation(byte[] encrypted) throws NoSuchAlgorithmException, IOException, Exception {
-        BASE64Decoder base64 = new BASE64Decoder();
         /* CHECK: we should always use new String (bytes,charset) to avoid issues with system charset and utf-8 */
         byte[] sha256 = MessageDigest.getInstance(CRYPT_SHA256).digest(SENSITIVPASSWORD);
-        byte[] data = base64.decodeBuffer(new String(encrypted));
-
+        byte[] data = Base64.decode(new String(encrypted));
         byte[] IV = new byte[8];
-
         BlowfishEngine cipher = new BlowfishEngine();
         cipher.init(true, new KeyParameter(sha256));
         cipher.processBlock(IV, 0, IV, 0);
-
         for (int i = 0; i < data.length; ++i) {
             byte[] BlowFishRound = new byte[IV.length];
             cipher.init(true, new KeyParameter(sha256));
             cipher.processBlock(IV, 0, BlowFishRound, 0);
-
             byte temp = data[i];
             data[i] ^= BlowFishRound[0];
-
-            for (int q = 1; q < 8; ++q)
+            for (int q = 1; q < 8; ++q) {
                 IV[q - 1] = IV[q];
+            }
             IV[7] = temp;
         }
-
         return data;
     }
 
@@ -251,41 +252,39 @@ public class sftContainerV8 extends sftContainer {
         for (int i = 0; i < data.length; ++i) {
             char chr = (char) data[i];
             boolean valid = (chr >= 'A' && chr <= 'Z') || (chr >= 'a' && chr <= 'z') || (chr >= '0' && chr <= '9') || (chr == '+') || (chr == '/') || (chr == '=');
-            if (!valid) return false;
+            if (!valid) {
+                return false;
+            }
         }
         return true;
     }
 
     private boolean toBase64String(byte[] magicString, byte[] encrypted) {
         int magicLen = magicString.length;
-
         byte a = encrypted[encrypted.length - 1];
         magicString[a % magicLen] ^= a;
-
         for (int i = 1; i < encrypted.length; i++) {
             a = magicString[i % magicString.length];
             a |= sftBinary.MAGIC[(i % 0xFF) + 1]; // FIX
             encrypted[i - 1] ^= a;
         }
-
-        if (!isBase64(encrypted)) return false;
-
+        if (!isBase64(encrypted)) {
+            return false;
+        }
         return true;
     }
 
     private void decodeFilename(byte[] key, byte[] filename) {
         int keyLength = key.length;
-
         for (int i = 1; i <= filename.length; i++) {
             byte a = key[i % keyLength];
             byte b = sftBinary.MAGIC[(i % 0xFF) + 1];
-
             filename[i - 1] = (byte) ((a | b) ^ filename[i - 1]);
         }
     }
 
     private void manipulateMagicString(byte[] magicStringCopy, byte lastByte) {
-        int p = ((int) lastByte & 0xFF) % magicStringCopy.length;
+        int p = (lastByte & 0xFF) % magicStringCopy.length;
         magicStringCopy[p] ^= lastByte;
     }
 
