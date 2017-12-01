42c7f98c04e1e1456eaa52e23eeb7466f4cca33a
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-01 11:15:14 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38341 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/SrBoxLife.java
+++ src/jd/plugins/decrypter/SrBoxLife.java
@@ -32,14 +32,13 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "israbox.life" }, urls = { "https?://[\\w\\.]*(?:israbox\\.(?:com|co|net|org|info|me|download|eu|be|club|life|pw)|isbox\\.net)/[0-9]+-.*?\\.html" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "israbox.life" }, urls = { "https?://[\\w\\.]*(?:israbox\\.(?:com|co|net|org|info|me|download|eu|be|club|life|pw|io|one)|isbox\\.net)/[0-9]+-.*?\\.html" })
 public class SrBoxLife extends PluginForDecrypt {
-
     public SrBoxLife(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private final String base = "(?i)https?://[\\w\\.]*(?:israbox\\.(?:com|co|net|org|info|me|download|eu|be|club|life|pw)|isbox\\.net)/";
+    private final String base = "(?i)https?://[\\w\\.]*(?:israbox\\.(?:com|co|net|org|info|me|download|eu|be|club|life|pw|io|one)|isbox\\.net)/";
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
@@ -54,7 +53,6 @@ public class SrBoxLife extends PluginForDecrypt {
             br.setFollowRedirects(true);
             br.getPage(redirect);
         }
-
         String fpName = br.getRegex("<h1 itemprop=\"name\">(.*?)</h1>").getMatch(0);
         if (fpName == null) {
             fpName = br.getRegex("<h1><a href[^>]+>(.*?)</a></h1>").getMatch(0);
@@ -73,13 +71,11 @@ public class SrBoxLife extends PluginForDecrypt {
         fpName = Encoding.htmlDecode(fpName);
         fpName = RemoveCharacter(fpName);
         fpName = CapitalLetterForEachWords(fpName);
-
         // Creation of the array of link that is supported by all plug-in
         String[] links = br.getRegex("<a href=\"((?!javascript.+)[^\"]+)\"").getColumn(0);
         if (links == null || links.length == 0) {
             return null;
         }
-
         // Added links
         for (String redirectlink : links) {
             // prevent null in decryptedLinks
@@ -91,10 +87,8 @@ public class SrBoxLife extends PluginForDecrypt {
                 decryptedLinks.add(dl);
             }
         }
-
         // Some link can be crypted in this site, see if it is the case
         String[] linksCrypted = br.getRegex("\"(" + base + "engine/go\\.php\\?url=.*?)\"").getColumn(0);
-
         // Added crypted links
         for (String redirectlink : linksCrypted) {
             final Browser br2 = br.cloneBrowser();
@@ -104,17 +98,14 @@ public class SrBoxLife extends PluginForDecrypt {
                 decryptedLinks.add(createDownloadlink(finallink));
             }
         }
-
         /*
          * Array of image to download the cover (It can be usable if the user want to create a subfolder with the name of the package
          * because the folder is immediately created because it will download the cover in it
          */
         String[] TabImage2 = br.getRegex("<img src=\"(https?://[\\w\\.]*?lectro\\.ws)/*?/uploads/posts/(.*?)\"").getColumn(1);
         String[] TabImage1 = br.getRegex("<img src=\"" + base + "uploads/(.*?)\"").getColumn(0);
-
         // Number of pictures
         int iImage = (TabImage1 != null ? TabImage1.length : 0) + (TabImage2 != null ? TabImage2.length : 0);
-
         // Array for added Images as DownloadLink
         DownloadLink[] tabImageLink = new DownloadLink[iImage];
         int iImageFinal = 0;
@@ -124,7 +115,6 @@ public class SrBoxLife extends PluginForDecrypt {
             for (String strImageLink : TabImage1) {
                 if (!strImageLink.toLowerCase().contains("foto")) {
                     strImageLink = Request.getLocation("/uploads/" + strImageLink, br.getRequest());
-
                     if (strImageLink.contains("/thumbs/") || strImageLink.contains("/medium/")) {
                         DownloadLink DLLink = createDownloadlink(strImageLink, false);
                         if (DLLink != null) {
@@ -138,7 +128,6 @@ public class SrBoxLife extends PluginForDecrypt {
                         }
                         strImageLink = strImageLink.replace("thumbs/", "").replace("medium/", "");
                     }
-
                     final DownloadLink DLLink = createDownloadlink(strImageLink, false);
                     if (DLLink != null) {
                         iImageIndex++;
@@ -153,11 +142,9 @@ public class SrBoxLife extends PluginForDecrypt {
                 iImageNet++;
             }
         }
-
         if (TabImage2 != null) {
             for (String strImageLink : TabImage2) {
                 strImageLink = "http://www.lectro.ws/uploads/posts/" + strImageLink;
-
                 if (strImageLink.contains("/thumbs/") || strImageLink.contains("/medium/")) {
                     DownloadLink DLLink = createDownloadlink(strImageLink, false);
                     if (DLLink != null) {
@@ -167,16 +154,13 @@ public class SrBoxLife extends PluginForDecrypt {
                     strImageLink = strImageLink.replace("thumbs/", "");
                     strImageLink = strImageLink.replace("medium/", "");
                 }
-
                 DownloadLink DLLink = createDownloadlink(strImageLink, false);
                 if (DLLink != null) {
                     tabImageLink[iImageIndex++] = DLLink;
                     iImageFinal++;
-
                 }
             }
         }
-
         if (tabImageLink != null) {
             iImageIndex = 1;
             for (DownloadLink DLLink : tabImageLink) {
@@ -186,11 +170,9 @@ public class SrBoxLife extends PluginForDecrypt {
                     if (iIndex > -1) {
                         strExtension = DLLink.getPluginPatternMatcher().substring(iIndex);
                     }
-
                     if (strExtension != "") {
                         if (fpName != null) {
                             String strName = fpName;
-
                             // Delete the end of the filename only if it represents the year in parenthesis -- why??? raztoki20160115
                             strName = ReplacePattern(strName, "\\([0-9]{4}\\)$").trim();
                             if (iImageFinal > 1) {
@@ -204,7 +186,6 @@ public class SrBoxLife extends PluginForDecrypt {
                 }
             }
         }
-
         // Add all link in a package
         if (fpName != null) {
             FilePackage fp = FilePackage.getInstance();
@@ -223,43 +204,33 @@ public class SrBoxLife extends PluginForDecrypt {
         if (!link.startsWith("http")) {
             return null;
         }
-
         if (link.contains("?media=")) {
             return null;
         }
-
         if (link.matches("^" + base + "uploads/img/.+$")) {
             return null;
         }
-
         if (link.toLowerCase().endsWith("_04c.png") || link.toLowerCase().endsWith("_kap-download.jpg") || link.toLowerCase().endsWith("_download-button.png") || link.toLowerCase().endsWith("_download.png")) {
             return null;
         }
-
         if (link.toLowerCase().contains("1418317446_53af83b0c0ff48717413b401bdc95723") || link.toLowerCase().contains("qm9ccs3xn4c9le2paels")) {
             return null;
         }
-
         if (link.toLowerCase().contains("discogs") || link.toLowerCase().contains("tracklist") || link.toLowerCase().contains("all-music") || link.toLowerCase().contains("junodownload")) {
             return null;
         }
-
         if (link.contains("signup?")) {
             return null;
         }
-
         if (link.startsWith("http://ax-d.pixfuture.net") || link.startsWith("http://www.friendlyduckaffiliates.com/")) {
             return null;
         }
-
         if (link.startsWith("http://www.facebook") || link.startsWith("https://twitter.com") || link.startsWith("https://plus.google.com")) {
             return null;
         }
-
         if (bVerify && link.matches("^" + base + ".*?")) {
             return null;
         }
-
         return super.createDownloadlink(link);
     }
 
@@ -278,46 +249,34 @@ public class SrBoxLife extends PluginForDecrypt {
         strName = strName.replace("/", "&");
         strName = strName.replace("\\", "&");
         strName = strName.replace("’", "'");
-
         strName = strName.replace("&nbsp;", " ");
         if (strName.startsWith("VA - ")) {
             strName = strName.replace("VA - ", "");
         }
         strName = strName.replace("Various Artists - ", "");
         strName = strName.replace("Vinyl", "");
-
         // Remove the exact words mp3, flac, lossless and ape
         strName = ReplacePattern(strName, "(?<!\\p{L})mp3(?!\\p{L})|(?<!\\p{L})ape(?!\\p{L})|(?<!\\p{L})flac(?!\\p{L})|(?<!\\p{L})lossless(?!\\p{L})|(?<!\\p{L})lossles(?!\\p{L})|(?<!\\p{L})alac(?!\\p{L})");
-
         // Remove the box set information
         strName = ReplacePattern(strName, "[0-9]+ *albums");
         strName = ReplacePattern(strName, "- [0-9]+ *CD Box Set");
         strName = ReplacePattern(strName, "Box Set");
-
         // Remove the HDTracks information (Take care of the case)
         strName = ReplacePattern(strName, "HDTracks");
-
         // Remove the vinyl digitizing information
         strName = ReplacePattern(strName, "Vinyl Digitizing");
-
         // Remove the number of CD
         strName = ReplacePattern(strName, "[0-9]+ *CD");
-
         // Replace Vol ou Vol. with 0
         strName = ReplacePattern(strName, "Vol(\\. |\\.| )", "0");
-
         // Replace the [ ] with nothing because the string in this bracket has been removed
         strName = ReplacePattern(strName, "\\[ *\\]");
-
         // Replace the ( ) with nothing because the string in this parenthesis has been removed
         strName = ReplacePattern(strName, "^\\[.*\\] ");
-
         // Replace the ( ) with nothing because the string in this parenthesis has been removed
         strName = ReplacePattern(strName, "\\( *\\)");
-
         // Replace several space characters in one
         strName = ReplacePattern(strName, "\\s{2,}", " ");
-
         // Remove the encoding information
         strName = ReplacePattern(strName, "[0-9]+ Kbps itunes");
         strName = ReplacePattern(strName, "[0-9]+ Kbps *&");
@@ -327,22 +286,18 @@ public class SrBoxLife extends PluginForDecrypt {
         strName = ReplacePattern(strName, "320 *&");
         strName = ReplacePattern(strName, "& 320");
         strName = ReplacePattern(strName, "\\s{2,}");
-
         strName = strName.trim();
         if (strName.endsWith("320")) {
             strName = strName.substring(0, strName.length() - 3);
         }
-
         // Add a dash to collection
         int iPositionDash = -1;
         String strPattern = "Collection \\([0-9]{4}[-][0-9]{4}\\)";
-
         Pattern pattern = Pattern.compile(strPattern, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(strName);
         if (matcher.find()) {
             iPositionDash = strName.indexOf("Collection");
         }
-
         // Add a dash to discography
         strPattern = "Discography \\([0-9]{4}[-][0-9]{4}\\)";
         pattern = Pattern.compile(strPattern, Pattern.CASE_INSENSITIVE);
@@ -350,21 +305,16 @@ public class SrBoxLife extends PluginForDecrypt {
         if (matcher.matches()) {
             iPositionDash = strName.indexOf("Discography");
         }
-
         if (iPositionDash != -1 && (iPositionDash > 2 && strName.substring(iPositionDash - 2, iPositionDash) != "- ")) {
             strName = strName.substring(0, iPositionDash).trim() + " - " + strName.substring(iPositionDash, strName.length()).trim();
         }
-
         // Replace brackets with parenthesis
         strName = strName.replace("[", "(");
         strName = strName.replace("]", ")");
-
         // Add a space between two parenthesis
         strName = strName.replace(")(", ") (");
-
         // remove trailing ,
         strName = strName.replaceFirst(",$", "");
-
         strName = strName.trim();
         if (strName.endsWith("&") || strName.endsWith("+")) {
             strName = strName.substring(0, strName.length() - 1);
@@ -375,7 +325,6 @@ public class SrBoxLife extends PluginForDecrypt {
         if (strName.endsWith("( & )")) {
             strName = strName.substring(0, strName.length() - 5);
         }
-
         return strName.trim();
     }
 
@@ -390,16 +339,13 @@ public class SrBoxLife extends PluginForDecrypt {
         if (strText == null || strText == "") {
             return "";
         }
-
         String strReturn = "";
         try {
             String strTemp = "";
             String strFirstChar = "";
-
             List<String> CapitalFirstCharacterException = new ArrayList<String>();
             CapitalFirstCharacterException.add("(");
             CapitalFirstCharacterException.add("[");
-
             List<String> CapitalCharacterInException = new ArrayList<String>();
             CapitalCharacterInException.add("-");
             CapitalCharacterInException.add(".");
@@ -409,7 +355,6 @@ public class SrBoxLife extends PluginForDecrypt {
             CapitalCharacterInException.add("\\");
             CapitalCharacterInException.add("(");
             CapitalCharacterInException.add("[");
-
             String[] strAllWord = strText.split(" ");
             for (String strWord : strAllWord) {
                 if (strWord.length() > 0) {
@@ -426,7 +371,6 @@ public class SrBoxLife extends PluginForDecrypt {
                     } else {
                         strCharacter = strFirstChar.toUpperCase();
                     }
-
                     String strCharacterIn = "";
                     strTemp = "";
                     for (String strInException : CapitalCharacterInException) {
@@ -452,7 +396,6 @@ public class SrBoxLife extends PluginForDecrypt {
                                     } else {
                                         strCharacterIn = strFirstChar.toUpperCase();
                                     }
-
                                     strTemp += strCharacterIn + strRemain;
                                     boolean bSpaceAtTheEnd = strTemp.endsWith(" ");
                                     strTemp = strTemp.trim();
@@ -515,5 +458,4 @@ public class SrBoxLife extends PluginForDecrypt {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
