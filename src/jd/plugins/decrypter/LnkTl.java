594bc9adce500ee905108d76863e64a2fba2814c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-08 08:10:25 +0000

    refs #84842
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38379 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/LnkTl.java
+++ src/jd/plugins/decrypter/LnkTl.java
@@ -13,13 +13,10 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
@@ -31,9 +28,10 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.components.PluginJSonUtils;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "link.tl" }, urls = { "http://(www\\.)?link\\.tl/(?!advertising)[A-Za-z0-9\\-]+" })
-public class LnkTl extends antiDDoSForDecrypt {
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "link.tl" }, urls = { "https?://(www\\.)?link\\.tl/(?!advertising)[A-Za-z0-9\\-]+" })
+public class LnkTl extends antiDDoSForDecrypt {
     public LnkTl(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -59,18 +57,16 @@ public class LnkTl extends antiDDoSForDecrypt {
         final String parameter = param.toString();
         br.setFollowRedirects(false);
         getPage(parameter);
-
         /* Check for direct redirect */
         String redirect = br.getRedirectLocation();
         if (redirect == null) {
-            redirect = br.getRegex("top\\.location\\.href = \"(http[^<>\"]*?)\"").getMatch(0);
+            redirect = br.getRegex("top\\.location\\.href = \"(https?[^<>\"]*?)\"").getMatch(0);
         }
         if (redirect != null && !redirect.contains("link.tl/")) {
             decryptedLinks.add(createDownloadlink(redirect));
             return decryptedLinks;
         }
-
-        if (br.getURL().equals("http://link.tl/") || br.containsHTML("top\\.location\\.href = \"http://link\\.tl/\"") || br.containsHTML(">404 Not Found<") || br.containsHTML(">Sorry the page you are looking for does not exist|>Üzgünüz, ulaşmaya çalışmış olduğunuz kısaltma sistemde yer almamaktadır\\.<")) {
+        if (br.getURL().equals("http://link.tl/") || br.getURL().equals("https://link.tl/") || br.containsHTML("top\\.location\\.href = \"https?://link\\.tl/\"") || br.containsHTML(">404 Not Found<") || br.containsHTML(">Sorry the page you are looking for does not exist|>Üzgünüz, ulaşmaya çalışmış olduğunuz kısaltma sistemde yer almamaktadır\\.<")) {
             logger.info("Link offline: " + parameter);
             return decryptedLinks;
         }
@@ -104,16 +100,13 @@ public class LnkTl extends antiDDoSForDecrypt {
         data.put(Encoding.urlEncode("args[oid]"), matches[2]);
         ajaxPostPage(post[0], data);
         ajaxPostPage(post[0], data);
-
         // waittime is 5 seconds. but somehow this often results in an error.
         // we use 5.5 seconds to avoid them
         sleep(5500, param);
-
         // second
         data.put("opt", "make_log");
         data.put(Encoding.urlEncode("args[aid]"), matches[0]);
         ajaxPostPage(post[post.length - 1], data);
-
         String url = PluginJSonUtils.getJsonValue(ajax, "url");
         if (url == null) {
             // maybe we have to wait even longer?
@@ -134,7 +127,6 @@ public class LnkTl extends antiDDoSForDecrypt {
                 }
             }
         }
-
         decryptedLinks.add(createDownloadlink(url));
         return decryptedLinks;
     }
@@ -146,22 +138,18 @@ public class LnkTl extends antiDDoSForDecrypt {
      */
     protected String decodeDownloadLink(final String s) {
         String decoded = null;
-
         try {
             Regex params = new Regex(s, "'(.*?[^\\\\])',(\\d+),(\\d+),'(.*?)'");
-
             String p = params.getMatch(0).replaceAll("\\\\", "");
             int a = Integer.parseInt(params.getMatch(1));
             int c = Integer.parseInt(params.getMatch(2));
             String[] k = params.getMatch(3).split("\\|");
-
             while (c != 0) {
                 c--;
                 if (k[c].length() != 0) {
                     p = p.replaceAll("\\b" + Integer.toString(c, a) + "\\b", k[c]);
                 }
             }
-
             decoded = p;
         } catch (Exception e) {
         }
@@ -172,5 +160,4 @@ public class LnkTl extends antiDDoSForDecrypt {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
