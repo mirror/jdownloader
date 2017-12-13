2f55e75a1b964c423f825356ebdde73f543dc075
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-13 08:50:52 +0000

    Pornve.com - Adjustment (filename) fixes #84241
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38399 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PornveCom.java
+++ src/jd/plugins/hoster/PornveCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.hoster;
 
 import java.io.File;
@@ -55,11 +54,9 @@ import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "pornve.com" }, urls = { "https?://(?:www\\.)?pornve\\.com/(?:embed\\-)?[a-z0-9]{12}" })
 public class PornveCom extends PluginForHost {
-
     /* Some HTML code to identify different (error) states */
     private static final String            HTML_PASSWORDPROTECTED             = "<br><b>Passwor(d|t):</b> <input";
     private static final String            HTML_MAINTENANCE_MODE              = ">This server is in maintenance mode";
-
     /* Here comes our XFS-configuration */
     /* primary website url, take note of redirects */
     private static final String            COOKIE_HOST                        = "http://pornve.com";
@@ -67,10 +64,8 @@ public class PornveCom extends PluginForHost {
     private static final String            NICE_HOSTproperty                  = COOKIE_HOST.replaceAll("(https://|http://|\\.|\\-)", "");
     /* domain names used within download links */
     private static final String            DOMAINS                            = "(pornve\\.com)";
-
     /* Errormessages inside URLs */
     private static final String            URL_ERROR_PREMIUMONLY              = "/?op=login&redirect=";
-
     /* All kinds of XFS-plugin-configuration settings - be sure to configure this correctly when developing new XFS plugins! */
     /*
      * If activated, filename can be null - fuid will be used instead then. Also the code will check for imagehosts-continue-POST-forms and
@@ -87,7 +82,6 @@ public class PornveCom extends PluginForHost {
      * will check for videohoster "next" Download/Ad- Form.
      */
     private final boolean                  IMAGEHOSTER                        = false;
-
     private final boolean                  SUPPORTS_HTTPS                     = false;
     private final boolean                  SUPPORTS_HTTPS_FORCED              = false;
     private final boolean                  SUPPORTS_AVAILABLECHECK_ALT        = false;
@@ -99,33 +93,27 @@ public class PornveCom extends PluginForHost {
      * contains misleading information such as fake texts.
      */
     private final boolean                  ENABLE_HTML_FILESIZE_CHECK         = false;
-
     /* Pre-Download waittime stuff */
     private final boolean                  WAITFORCED                         = false;
     private final int                      WAITSECONDSMIN                     = 3;
     private final int                      WAITSECONDSMAX                     = 100;
     private final int                      WAITSECONDSFORCED                  = 5;
-
     /* Supported linktypes */
     private final String                   TYPE_EMBED                         = "https?://[A-Za-z0-9\\-\\.]+/embed\\-[a-z0-9]{12}";
     private final String                   TYPE_NORMAL                        = "https?://[A-Za-z0-9\\-\\.]+/[a-z0-9]{12}";
-
     /* Texts displayed to the user in some errorcases */
     private final String                   USERTEXT_ALLWAIT_SHORT             = "Waiting till new downloads can be started";
     private final String                   USERTEXT_MAINTENANCE               = "This server is under maintenance";
     private final String                   USERTEXT_PREMIUMONLY_LINKCHECK     = "Only downloadable via premium or registered";
-
     /* Properties */
     private final String                   PROPERTY_DLLINK_FREE               = "freelink";
     private final String                   PROPERTY_DLLINK_ACCOUNT_FREE       = "freelink2";
     private final String                   PROPERTY_DLLINK_ACCOUNT_PREMIUM    = "premlink";
     private final String                   PROPERTY_PASS                      = "pass";
-
     /* Used variables */
     private String                         correctedBR                        = "";
     private String                         fuid                               = null;
     private String                         passCode                           = null;
-
     private static AtomicReference<String> agent                              = new AtomicReference<String>(null);
     /* note: CAN NOT be negative or zero! (ie. -1 or 0) Otherwise math sections fail. .:. use [1-20] */
     private static AtomicInteger           totalMaxSimultanFreeDownload       = new AtomicInteger(1);
@@ -143,7 +131,6 @@ public class PornveCom extends PluginForHost {
      * captchatype: null<br />
      * other:<br />
      */
-
     @SuppressWarnings({ "deprecation" })
     @Override
     public void correctDownloadLink(final DownloadLink link) {
@@ -182,9 +169,7 @@ public class PornveCom extends PluginForHost {
         if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-
         altbr = this.br.cloneBrowser();
-
         if (new Regex(correctedBR, HTML_MAINTENANCE_MODE).matches()) {
             /* In maintenance mode this sometimes is a way to find filenames! */
             if (SUPPORTS_AVAILABLECHECK_ABUSE) {
@@ -232,9 +217,7 @@ public class PornveCom extends PluginForHost {
             logger.warning("Alternative linkcheck failed!");
             return AvailableStatus.UNCHECKABLE;
         }
-
         scanInfo(fileInfo);
-
         /* Filename abbreviated over x chars long --> Use getFnameViaAbuseLink as a workaround to find the full-length filename! */
         if (!inValidate(fileInfo[0]) && fileInfo[0].trim().endsWith("&#133;") && SUPPORTS_AVAILABLECHECK_ABUSE) {
             logger.warning("filename length is larrrge");
@@ -244,7 +227,6 @@ public class PornveCom extends PluginForHost {
             logger.info("Failed to find filename, trying getFnameViaAbuseLink");
             fileInfo[0] = this.getFnameViaAbuseLink(altbr, link);
         }
-
         if (inValidate(fileInfo[0]) && IMAGEHOSTER) {
             /*
              * Imagehosts often do not show any filenames, at least not on the first page plus they often have their abuse-url disabled. Add
@@ -293,14 +275,13 @@ public class PornveCom extends PluginForHost {
     private String[] scanInfo(final String[] fileInfo) {
         final String sharebox0 = "copy\\(this\\);.+>(.+) - ([\\d\\.]+ (?:B|KB|MB|GB))</a></textarea>[\r\n\t ]+</div>";
         final String sharebox1 = "copy\\(this\\);.+\\](.+) - ([\\d\\.]+ (?:B|KB|MB|GB))\\[/URL\\]";
-
         /* standard traits from base page */
         if (inValidate(fileInfo[0])) {
             fileInfo[0] = new Regex(correctedBR, "You have requested.*?https?://(www\\.)?" + DOMAINS + "/" + fuid + "/(.*?)</font>").getMatch(2);
             if (inValidate(fileInfo[0])) {
                 fileInfo[0] = new Regex(correctedBR, "fname\"( type=\"hidden\")? value=\"(.*?)\"").getMatch(1);
                 if (inValidate(fileInfo[0])) {
-                    fileInfo[0] = new Regex(correctedBR, "<h2>Download File(.*?)</h2>").getMatch(0);
+                    fileInfo[0] = new Regex(correctedBR, "<h2>(.*?)</h2>").getMatch(0);
                     /* traits from download1 page below */
                     if (inValidate(fileInfo[0])) {
                         fileInfo[0] = new Regex(correctedBR, "Filename:? ?(<[^>]+> ?)+?([^<>\"\\']+)").getMatch(1);
@@ -321,6 +302,9 @@ public class PornveCom extends PluginForHost {
         }
         if (inValidate(fileInfo[0])) {
             fileInfo[0] = new Regex(correctedBR, "class=\"dfilename\">([^<>\"]*?)<").getMatch(0);
+        }
+        if (inValidate(fileInfo[0])) {
+            fileInfo[0] = new Regex(correctedBR, "<Title>(?:Watch )?([^<>\"]*?)(?: - PornVe.com)?</Title>").getMatch(0);
         }
         if (ENABLE_HTML_FILESIZE_CHECK) {
             if (inValidate(fileInfo[1])) {
@@ -619,7 +603,6 @@ public class PornveCom extends PluginForHost {
                     dlForm.put("g-recaptcha-response", Encoding.urlEncode(recaptchaV2Response));
                 } else if (br.containsHTML("solvemedia\\.com/papi/")) {
                     logger.info("Detected captcha method \"solvemedia\" for this host");
-
                     final org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia sm = new org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia(br);
                     File cf = null;
                     try {
@@ -780,14 +763,11 @@ public class PornveCom extends PluginForHost {
     private void correctBR() throws NumberFormatException, PluginException {
         correctedBR = br.toString();
         ArrayList<String> regexStuff = new ArrayList<String>();
-
         // remove custom rules first!!! As html can change because of generic cleanup rules.
-
         /* generic cleanup */
         regexStuff.add("<\\!(\\-\\-.*?\\-\\-)>");
         regexStuff.add("(display: ?none;\">.*?</div>)");
         regexStuff.add("(visibility:hidden>.*?<)");
-
         for (String aRegex : regexStuff) {
             String results[] = new Regex(correctedBR, aRegex).getColumn(0);
             if (results != null) {
@@ -873,26 +853,21 @@ public class PornveCom extends PluginForHost {
 
     private String decodeDownloadLink(final String s) {
         String decoded = null;
-
         try {
             Regex params = new Regex(s, "\\'(.*?[^\\\\])\\',(\\d+),(\\d+),\\'(.*?)\\'");
-
             String p = params.getMatch(0).replaceAll("\\\\", "");
             int a = Integer.parseInt(params.getMatch(1));
             int c = Integer.parseInt(params.getMatch(2));
             String[] k = params.getMatch(3).split("\\|");
-
             while (c != 0) {
                 c--;
                 if (k[c].length() != 0) {
                     p = p.replaceAll("\\b" + Integer.toString(c, a) + "\\b", k[c]);
                 }
             }
-
             decoded = p;
         } catch (Exception e) {
         }
-
         String finallink = null;
         if (decoded != null) {
             /* Open regex is possible because in the unpacked JS there are usually only 1 links */
@@ -938,7 +913,6 @@ public class PornveCom extends PluginForHost {
     // counter++;
     // }
     // }
-
     private String correctProtocol(String url) {
         if (SUPPORTS_HTTPS && SUPPORTS_HTTPS_FORCED) {
             url = url.replaceFirst("http://", "https://");
@@ -995,7 +969,6 @@ public class PornveCom extends PluginForHost {
             }
             wait = i;
         }
-
         wait -= passedTime;
         if (wait > 0) {
             logger.info("Waiting waittime: " + wait);
@@ -1407,7 +1380,6 @@ public class PornveCom extends PluginForHost {
     // dl.startDownload();
     // }
     // }
-
     @Override
     public void reset() {
     }
@@ -1420,5 +1392,4 @@ public class PornveCom extends PluginForHost {
     public SiteTemplate siteTemplateType() {
         return SiteTemplate.SibSoft_XFileShare;
     }
-
 }
