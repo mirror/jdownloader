a87613cf33d2711b17f49eff834e7db12278f74b
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-16 12:11:30 +0000

    Realitykings.com - Enforce https fixes #84868
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38416 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2888006d2f7e8a984861213a578d59632141d181

--- src/jd/plugins/decrypter/RealityKingsCom.java
+++ src/jd/plugins/decrypter/RealityKingsCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
@@ -44,7 +43,6 @@ import org.appwork.utils.formatter.SizeFormatter;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "realitykings.com" }, urls = { "https?://(?:new\\.)?members\\.realitykings\\.com/video/(?:(?:full|watch)/\\d+(?:/[a-z0-9\\-_]+/?)?|pics/\\d+(?:/[a-z0-9\\-_]+/?)?)|https?://(?:new\\.)?members\\.realitykings\\.com/(?:videos/\\?models=\\d+|model/view/\\d+/[a-z0-9\\-_]+/?)|https?://(?:www\\.)?realitykings\\.com/tour/video/watch/\\d+/(?:[a-z0-9\\-_]+/?)?" })
 public class RealityKingsCom extends PluginForDecrypt {
-
     public RealityKingsCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -52,12 +50,9 @@ public class RealityKingsCom extends PluginForDecrypt {
     private static final String TYPE_VIDEO            = "https?://(?:new\\.)?members\\.realitykings\\.com/video/(?:full|watch)/\\d+(?:/[a-z0-9\\-_]+/?)?";
     private static final String TYPE_PHOTO            = "https?://(?:new\\.)?members\\.realitykings\\.com/video/pics/\\d+(?:/[a-z0-9\\-_]+/?)?";
     private static final String TYPE_MEMBER           = "https?://(?:new\\.)?members\\.realitykings\\.com/(?:videos/\\?models=\\d+|model/view/\\d+/[a-z0-9\\-_]+/?)";
-
     private static final String TYPE_VIDEO_FREE       = ".+/tour/video/watch/.+";
-
     public static String        DOMAIN_BASE           = "realitykings.com";
     public static String        DOMAIN_PREFIX_PREMIUM = "new.members.";
-
     private final List<String>  all_known_qualities   = Arrays.asList("GRAB_1080", "GRAB_3000", "GRAB_1500", "GRAB_800", "GRAB_mp4v_480", "GRAB_mp4v_320", "GRAB_3gp");
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
@@ -74,7 +69,7 @@ public class RealityKingsCom extends PluginForDecrypt {
         }
         if (parameter.matches(TYPE_MEMBER)) {
             fid = new Regex(parameter, ".+realitykings\\.com/(?:videos/\\?models=|model/view/)(\\d+)").getMatch(0);
-            br.getPage("http://" + DOMAIN_PREFIX_PREMIUM + DOMAIN_BASE + "/videos/?models=" + fid);
+            br.getPage("https://" + DOMAIN_PREFIX_PREMIUM + DOMAIN_BASE + "/videos/?models=" + fid);
             if (isOffline(this.br)) {
                 final DownloadLink offline = this.createOfflinelink(parameter);
                 decryptedLinks.add(offline);
@@ -106,7 +101,6 @@ public class RealityKingsCom extends PluginForDecrypt {
                 final String format_filename = "%s_%s%s";
                 final String htmldownload = this.br.getRegex("class=\"fa fa\\-download\"></i>[^<>]*?</div>(.*?)</div>").getMatch(0);
                 final String[] dlinfo = htmldownload != null ? htmldownload.split("</a>") : null;
-
                 if (dlinfo != null) {
                     List<String> all_selected_qualities = new ArrayList<String>();
                     for (final String possibleQuality : all_known_qualities) {
@@ -118,7 +112,6 @@ public class RealityKingsCom extends PluginForDecrypt {
                         logger.info("User selected nothing --> Grabbing ALL qualities");
                         all_selected_qualities = all_known_qualities;
                     }
-
                     for (final String video : dlinfo) {
                         final String dlurl = new Regex(video, "\"(/[^<>\"]*?download/[^<>\"]+/)\"").getMatch(0);
                         final String quality = new Regex(video, "<span>([^<>\"]+)</span>").getMatch(0);
@@ -149,7 +142,6 @@ public class RealityKingsCom extends PluginForDecrypt {
                         decryptedLinks.add(dl);
                     }
                 }
-
                 if (decryptedLinks.size() == 0) {
                     /* This should only happen with free account mode or no account at all (trailer download). */
                     logger.info("No official downloads available --> Downloading streams (adding all regardless of quality selection)");
@@ -157,20 +149,16 @@ public class RealityKingsCom extends PluginForDecrypt {
                     if (json == null) {
                         return null;
                     }
-
                     final LinkedHashMap<String, Object> entries = jd.plugins.decrypter.BrazzersCom.getVideoMapHttpStreams(json);
-
                     int bitrate_max = 0;
                     int bitrate_temp = 0;
                     DownloadLink downloadlink_quality_BEST = null;
-
                     final Iterator<Entry<String, Object>> it = entries.entrySet().iterator();
                     final String ext = ".mp4";
                     while (it.hasNext()) {
                         final Entry<String, Object> entry = it.next();
                         final String quality_key = entry.getKey();
                         final String quality_url = (String) entry.getValue();
-
                         final DownloadLink dl = this.createDownloadlink(quality_url.replaceAll("https?://", host_decrypted));
                         dl.setName(String.format(format_filename, title, quality_key, ext));
                         dl.setProperty("fid", fid);
@@ -179,7 +167,6 @@ public class RealityKingsCom extends PluginForDecrypt {
                             dl.setProperty("free_downloadable", true);
                         }
                         decryptedLinks.add(dl);
-
                         if (quality_key.matches("\\d+")) {
                             bitrate_temp = Integer.parseInt(quality_key);
                             if (bitrate_temp > bitrate_max) {
@@ -187,18 +174,14 @@ public class RealityKingsCom extends PluginForDecrypt {
                                 downloadlink_quality_BEST = dl;
                             }
                         }
-
                     }
-
                     /* Not logged in --> Usually only trailer / MOCH download --> Add BEST only */
                     if (!loggedin && downloadlink_quality_BEST != null) {
                         logger.info("Trailer download: Adding BEST quality only");
                         decryptedLinks.clear();
                         decryptedLinks.add(downloadlink_quality_BEST);
                     }
-
                 }
-
             } else if (parameter.matches(TYPE_PHOTO)) {
                 br.getPage(parameter);
                 if (isOffline(this.br)) {
@@ -230,7 +213,6 @@ public class RealityKingsCom extends PluginForDecrypt {
             fp.setName(title);
             fp.addLinks(decryptedLinks);
         }
-
         return decryptedLinks;
     }
 
@@ -259,7 +241,6 @@ public class RealityKingsCom extends PluginForDecrypt {
         try {
             ((jd.plugins.hoster.RealityKingsCom) hostPlugin).login(this.br, aa, force);
         } catch (final PluginException e) {
-
             aa.setValid(false);
             return false;
         }
@@ -284,7 +265,7 @@ public class RealityKingsCom extends PluginForDecrypt {
     }
 
     public static String getProtocol() {
-        return "http://";
+        return "https://";
     }
 
     public static boolean isOffline(final Browser br) {
@@ -300,5 +281,4 @@ public class RealityKingsCom extends PluginForDecrypt {
     public SiteTemplate siteTemplateType() {
         return SiteTemplate.PornPortal;
     }
-
 }
