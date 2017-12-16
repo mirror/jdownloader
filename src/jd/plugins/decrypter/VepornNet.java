5ef0296bf6ba5549e939bfd63fe63c6b95cebfb4
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-16 09:04:34 +0000

    Veporn.net - Error handling fixes #84355
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38413 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f9955e7096f56120b1a5b4911bdeec7f9efbe769

--- src/jd/plugins/decrypter/VepornNet.java
+++ src/jd/plugins/decrypter/VepornNet.java
@@ -17,16 +17,17 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "veporn.net" }, urls = { "https?://(?:www\\.)?ve(?:-)?porn\\.net/video/[A-Za-z0-9\\-_]+" })
 public class VepornNet extends antiDDoSForDecrypt {
@@ -46,8 +47,7 @@ public class VepornNet extends antiDDoSForDecrypt {
         String fpName = br.getRegex("(rhgd)").getMatch(0);
         final String[] links = br.getRegex("comment\\((\\d+)\\)").getColumn(0);
         if (links == null || links.length == 0) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+            throw new DecrypterException("Decrypter broken for link: " + parameter);
         }
         br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
         int counter = 1;
@@ -57,6 +57,15 @@ public class VepornNet extends antiDDoSForDecrypt {
             }
             final Browser br = this.br.cloneBrowser();
             getPage(br, "http://www.ve-porn.net/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
+            if (br.containsHTML(">Site is too crowded<")) {
+                for (int i = 1; i <= 3; i++) {
+                    sleep(i * 3 * 1001l, param);
+                    getPage(br, "http://www.ve-porn.net/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
+                    if (!br.containsHTML(">Site is too crowded<")) {
+                        break;
+                    }
+                }
+            }
             String finallink = br.getRegex("iframe src='(http[^<>']+)'").getMatch(0);
             if (finallink == null) {
                 finallink = br.getRegex("iframe src=\"(http[^<>\"]+)\"").getMatch(0);
