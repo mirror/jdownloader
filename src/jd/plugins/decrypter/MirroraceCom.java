338304e8eb7b29fb39d605dc9ddeecf93a70ecc1
raztoki <raztoki@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2017-12-27 12:00:47 +0000

    Reviewed: mirrorace.com, - re: IRC - cloudflare support - removed unnecessary escaping - removed this.br nuking - recaptcha2 tidy up, (copy paste code it seems) retry loops are not required. valid response tokens are returned, or exceptions are thrown at the recaptchav2 backend. retry of images etc are done within user interface (browser/my.jd app).
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38457 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 42a30c4ebea67f8c2988cedbfad69c8a91b24145

--- src/jd/plugins/decrypter/MirroraceCom.java
+++ src/jd/plugins/decrypter/MirroraceCom.java
@@ -15,6 +15,9 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.decrypter;
 
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import java.util.ArrayList;
 
 import jd.PluginWrapper;
@@ -23,76 +26,50 @@ import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.parser.html.Form;
 import jd.plugins.CryptedLink;
-import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
-import jd.plugins.LinkStatus;
-import jd.plugins.PluginException;
-import jd.plugins.PluginForDecrypt;
-
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "mirrorace.com" }, urls = { "https?://(?:www\\.)?mirrorace.com/m/[A-Za-z0-9]+" })
-public class MirroraceCom extends PluginForDecrypt {
+public class MirroraceCom extends antiDDoSForDecrypt {
+
     public MirroraceCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         br.setFollowRedirects(true);
-        br.getPage(parameter);
+        getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 404) {
-            decryptedLinks.add(createOfflinelink(parameter));
+            decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
         final String fpName = br.getRegex("<title>\\s*(?:Download)?\\s*([^<]*?)\\s*(?:-\\s*MirrorAce)?\\s*</title>").getMatch(0);
         final String[] links = br.getRegex("\"(https?://mirrorace\\.com/m/[A-Za-z0-9]+/\\d+\\?t=[^<>\"]*?)\"").getColumn(0);
         if (links == null || links.length == 0) {
-            throw new DecrypterException("Decrypter broken for link: " + parameter);
+            logger.warning("Decrypter broken for link: " + parameter);
+            return null;
         }
-        logger.info("Links found: " + links.length + " for " + parameter);
         for (final String singleLink : links) {
-            // logger.info("singleLink: " + singleLink);
             if (this.isAbort()) {
                 return decryptedLinks;
             }
-            br.getPage(singleLink);
-            // Sometimes we get g-recaptcha here
-            int counter = -1;
-            int retry = 9;
-            while (counter++ < retry && br.containsHTML("g-recaptcha")) {
-                Form captchaForm = null;
-                final Form[] allForms = br.getForms();
-                if (allForms != null && allForms.length != 0) {
-                    for (final Form aForm : allForms) {
-                        if (aForm.containsHTML("captcha")) {
-                            captchaForm = aForm;
-                            break;
-                        }
-                    }
-                }
-                final String captcha = captchaForm != null ? captchaForm.getRegex("(/captcha/[^<>\"]*?)\"").getMatch(0) : null;
-                if (captchaForm != null && captchaForm.containsHTML("=\"g-recaptcha\"")) {
+            final Browser br = this.br.cloneBrowser();
+            getPage(br, singleLink);
+            {
+                // sometimes a captcha event can happen here
+                final Form captchaForm = br.getFormByRegex("class=\"g-recaptcha\"");
+                if (captchaForm != null) {
                     final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br).getToken();
                     captchaForm.put("g-recaptcha-response", Encoding.urlEncode(recaptchaV2Response));
                     submitForm(captchaForm);
-                } else {
-                    logger.info("Captcha is detected but not g-recaptcha");
-                    throw new DecrypterException("Decrypter broken for link: " + parameter);
                 }
             }
-            if (counter == retry && br.containsHTML("g-recaptcha")) {
-                throw new DecrypterException(DecrypterException.CAPTCHA);
-            }
-            final String finallink = br.getRegex("<a class=\"uk\\-button uk\\-button\\-primary\" href=\"(http[^<>\"]+)").getMatch(0);
-            logger.info("\r\nfinallink: " + finallink + "\n for singleLink: " + singleLink);
+            final String finallink = br.getRegex("<a class=\"uk-button uk-button-primary\" href=\"(http[^<>\"]+)").getMatch(0);
             if (finallink == null) {
-                // return null;
-                // throw new DecrypterException("Decrypter broken for link: " + parameter);
-                continue;
+                return null;
             }
             final DownloadLink dl;
             if (finallink.contains("mirrorace.com")) {
@@ -100,7 +77,6 @@ public class MirroraceCom extends PluginForDecrypt {
                 brc.setFollowRedirects(false);
                 brc.getPage(finallink);
                 if (brc.getRedirectLocation() != null) {
-                    logger.info("External link: " + brc.getRedirectLocation());
                     dl = createDownloadlink(brc.getRedirectLocation());
                 } else {
                     continue;
@@ -117,18 +93,5 @@ public class MirroraceCom extends PluginForDecrypt {
             fp.addLinks(decryptedLinks);
         }
         return decryptedLinks;
-    }
-
-    private final void submitForm(final Form form) throws Exception {
-        if (form == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        br.submitForm(form);
-        // cleanUpHTML();
-    }
-
-    /* NO OVERRIDE!! */
-    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
-        return true;
     }
 }
