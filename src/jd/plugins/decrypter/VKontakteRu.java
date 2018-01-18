6aba56c6d62d120ae0e30cdb9bf60b9521534457
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-01-18 16:52:34 +0000

    VK: -fixes post 414906
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38532 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 39c722f1639146366723f4062b2455a6ad5a60b5

--- src/jd/plugins/decrypter/VKontakteRu.java
+++ src/jd/plugins/decrypter/VKontakteRu.java
@@ -25,11 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.formatter.SizeFormatter;
-import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.config.SubConfiguration;
@@ -53,6 +48,11 @@ import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.plugins.components.PluginJSonUtils;
 import jd.utils.JDUtilities;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "vkontakte.ru" }, urls = { "https?://(?:www\\.|m\\.|new\\.)?(?:vk\\.com|vkontakte\\.ru|vkontakte\\.com)/(?!doc[\\d\\-]+_[\\d\\-]+|picturelink|audiolink|videolink)[a-z0-9_/=\\.\\-\\?&%]+" })
 public class VKontakteRu extends PluginForDecrypt {
@@ -1402,6 +1402,7 @@ public class VKontakteRu extends PluginForDecrypt {
 
     /** Using Website */
     private void decryptWall_Website() throws Exception {
+        this.getPageSafe(this.CRYPTEDLINK_FUNCTIONAL);
         long total_numberof_entries;
         final String ownerID = new Regex(this.CRYPTEDLINK_FUNCTIONAL, "vk\\.com/wall((\\-)?\\d+)").getMatch(0);
         final FilePackage fp = FilePackage.getInstance();
