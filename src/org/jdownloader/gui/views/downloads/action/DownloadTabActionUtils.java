dce77b037b9940968bd7a7c178310f2278d87149
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-01-25 10:03:17 +0000

    fixes thread 76466
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38576 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/views/downloads/action/DownloadTabActionUtils.java
+++ src/org/jdownloader/gui/views/downloads/action/DownloadTabActionUtils.java
@@ -1,19 +1,5 @@
 package org.jdownloader.gui.views.downloads.action;
 
-import org.appwork.uio.CloseReason;
-import org.appwork.utils.formatter.SizeFormatter;
-import org.appwork.utils.swing.EDTHelper;
-import org.appwork.utils.swing.IconDialog;
-import org.jdownloader.controlling.DownloadLinkAggregator;
-import org.jdownloader.controlling.FileCreationManager.DeleteOption;
-import org.jdownloader.gui.IconKey;
-import org.jdownloader.gui.translate._GUI;
-import org.jdownloader.gui.views.SelectionInfo;
-import org.jdownloader.images.AbstractIcon;
-import org.jdownloader.settings.GraphicalUserInterfaceSettings.DeleteFileOptions;
-import org.jdownloader.settings.staticreferences.CFG_GUI;
-import org.jdownloader.utils.JDFileUtils;
-
 import jd.controlling.downloadcontroller.DownloadController;
 import jd.controlling.downloadcontroller.DownloadWatchDog;
 import jd.gui.swing.jdgui.JDGui;
@@ -21,60 +7,79 @@ import jd.gui.swing.jdgui.WarnLevel;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
+import org.appwork.uio.CloseReason;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.swing.EDTHelper;
+import org.jdownloader.controlling.DownloadLinkAggregator;
+import org.jdownloader.controlling.FileCreationManager.DeleteOption;
+import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.gui.views.SelectionInfo;
+import org.jdownloader.settings.GraphicalUserInterfaceSettings.DeleteFileOptions;
+import org.jdownloader.settings.staticreferences.CFG_GUI;
+import org.jdownloader.utils.JDFileUtils;
+
 public class DownloadTabActionUtils {
-
     public static void deleteLinksRequest(final SelectionInfo<FilePackage, DownloadLink> si, final String msg, final DeleteFileOptions mode, final boolean byPassDialog) {
-
         final DownloadLinkAggregator agg = new DownloadLinkAggregator();
         agg.setMirrorHandlingEnabled(false);
         agg.setLocalFileUsageEnabled(true);
         agg.update(si.getChildren());
-
-        new EDTHelper<Void>() {
-
-            @Override
-            public Void edtRun() {
-                if (agg.getTotalCount() == 0) {
-                    new IconDialog(0, _GUI.T.lit_ups_something_is_wrong(), _GUI.T.DownloadController_deleteLinksRequest_nolinks(), new AbstractIcon(IconKey.ICON_BOTTY_ROBOT_INFO, 256), null).show();
-                    return null;
-                }
-                WarnLevel level = WarnLevel.LOW;
-                switch (mode) {
-                case REMOVE_LINKS_AND_DELETE_FILES:
-                    if (agg.getBytesLoaded() > 0) {
-                        level = WarnLevel.SEVERE;
+        if (agg.getTotalCount() > 0) {
+            new EDTHelper<Void>() {
+                @Override
+                public Void edtRun() {
+                    WarnLevel level = WarnLevel.LOW;
+                    switch (mode) {
+                    case REMOVE_LINKS_AND_DELETE_FILES:
+                        if (agg.getBytesLoaded() > 0) {
+                            level = WarnLevel.SEVERE;
+                        } else {
+                            level = WarnLevel.NORMAL;
+                        }
+                        break;
+                    case REMOVE_LINKS_AND_RECYCLE_FILES:
+                        if (agg.getBytesLoaded() > 0) {
+                            level = WarnLevel.SEVERE;
+                        } else if (agg.getFinishedCount() != agg.getTotalCount()) {
+                            level = WarnLevel.NORMAL;
+                        }
+                        break;
+                    case REMOVE_LINKS_ONLY:
+                        if (agg.getBytesLoaded() > 0) {
+                            level = WarnLevel.SEVERE;
+                        } else if (agg.getFinishedCount() != agg.getTotalCount()) {
+                            level = WarnLevel.NORMAL;
+                        }
+                        break;
+                    }
+                    final boolean finalByPassDialog;
+                    if (!JDGui.bugme(level)) {
+                        finalByPassDialog = true;
                     } else {
-                        level = WarnLevel.NORMAL;
+                        finalByPassDialog = byPassDialog;
                     }
-                    break;
-                case REMOVE_LINKS_AND_RECYCLE_FILES:
-                    if (agg.getBytesLoaded() > 0) {
-                        level = WarnLevel.SEVERE;
-                    } else if (agg.getFinishedCount() != agg.getTotalCount()) {
-                        level = WarnLevel.NORMAL;
-                    }
-                    break;
-                case REMOVE_LINKS_ONLY:
-                    if (agg.getBytesLoaded() > 0) {
-                        level = WarnLevel.SEVERE;
-                    } else if (agg.getFinishedCount() != agg.getTotalCount()) {
-                        level = WarnLevel.NORMAL;
-                    }
-                    break;
-                }
-                final boolean finalByPassDialog;
-                if (!JDGui.bugme(level)) {
-                    finalByPassDialog = true;
-                } else {
-                    finalByPassDialog = byPassDialog;
-                }
-                if (!finalByPassDialog && !CFG_GUI.CFG.isBypassAllRlyDeleteDialogsEnabled()) {
-                    final ConfirmDeleteLinksDialog dialog = new ConfirmDeleteLinksDialog(msg + "\r\n" + _GUI.T.DeleteSelectionAction_actionPerformed_affected2(agg.getTotalCount(), SizeFormatter.formatBytes(agg.getBytesLoaded()), DownloadController.getInstance().getChildrenCount() - agg.getTotalCount(), agg.getLocalFileCount()), agg.getBytesLoaded());
-                    dialog.setRecycleSupported(JDFileUtils.isTrashSupported());
-                    dialog.setMode(mode);
-                    dialog.show();
-                    if (dialog.getCloseReason() == CloseReason.OK) {
-                        switch (dialog.getMode()) {
+                    if (!finalByPassDialog && !CFG_GUI.CFG.isBypassAllRlyDeleteDialogsEnabled()) {
+                        final ConfirmDeleteLinksDialog dialog = new ConfirmDeleteLinksDialog(msg + "\r\n" + _GUI.T.DeleteSelectionAction_actionPerformed_affected2(agg.getTotalCount(), SizeFormatter.formatBytes(agg.getBytesLoaded()), DownloadController.getInstance().getChildrenCount() - agg.getTotalCount(), agg.getLocalFileCount()), agg.getBytesLoaded());
+                        dialog.setRecycleSupported(JDFileUtils.isTrashSupported());
+                        dialog.setMode(mode);
+                        dialog.show();
+                        if (dialog.getCloseReason() == CloseReason.OK) {
+                            switch (dialog.getMode()) {
+                            case REMOVE_LINKS_ONLY:
+                                DownloadController.getInstance().removeChildren(si.getChildren());
+                                break;
+                            case REMOVE_LINKS_AND_DELETE_FILES:
+                                DownloadController.getInstance().removeChildren(si.getChildren());
+                                DownloadWatchDog.getInstance().delete(si.getChildren(), DeleteOption.NULL);
+                                break;
+                            case REMOVE_LINKS_AND_RECYCLE_FILES:
+                                DownloadController.getInstance().removeChildren(si.getChildren());
+                                DownloadWatchDog.getInstance().delete(si.getChildren(), DeleteOption.RECYCLE);
+                                break;
+                            }
+                        }
+                    } else {
+                        switch (mode) {
                         case REMOVE_LINKS_ONLY:
                             DownloadController.getInstance().removeChildren(si.getChildren());
                             break;
@@ -84,28 +89,13 @@ public class DownloadTabActionUtils {
                             break;
                         case REMOVE_LINKS_AND_RECYCLE_FILES:
                             DownloadController.getInstance().removeChildren(si.getChildren());
-                            DownloadWatchDog.getInstance().delete(si.getChildren(), DeleteOption.RECYCLE);
+                            DownloadWatchDog.getInstance().delete(si.getChildren(), JDFileUtils.isTrashSupported() ? DeleteOption.RECYCLE : DeleteOption.NULL);
                             break;
                         }
                     }
-                } else {
-                    switch (mode) {
-                    case REMOVE_LINKS_ONLY:
-                        DownloadController.getInstance().removeChildren(si.getChildren());
-                        break;
-                    case REMOVE_LINKS_AND_DELETE_FILES:
-                        DownloadController.getInstance().removeChildren(si.getChildren());
-                        DownloadWatchDog.getInstance().delete(si.getChildren(), DeleteOption.NULL);
-                        break;
-                    case REMOVE_LINKS_AND_RECYCLE_FILES:
-                        DownloadController.getInstance().removeChildren(si.getChildren());
-                        DownloadWatchDog.getInstance().delete(si.getChildren(), JDFileUtils.isTrashSupported() ? DeleteOption.RECYCLE : DeleteOption.NULL);
-                        break;
-                    }
+                    return null;
                 }
-                return null;
-            }
-
-        }.start(true);
+            }.start(true);
+        }
     }
 }
