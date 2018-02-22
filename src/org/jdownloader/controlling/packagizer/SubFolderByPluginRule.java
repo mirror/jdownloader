dd2996288cee21faae3311908cb3d88c725120d2
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-02-22 18:49:22 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38728 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/controlling/packagizer/SubFolderByPluginRule.java
+++ src/org/jdownloader/controlling/packagizer/SubFolderByPluginRule.java
@@ -15,7 +15,7 @@ public class SubFolderByPluginRule extends PackagizerRule {
 
     public void init() {
         setMatchAlwaysFilter(new BooleanFilter(true));
-        setDownloadDestination("<jd:append><jd:" + DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH + ">");
+        setDownloadDestination("<jd:" + DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH + ">");
         setIconKey(IconKey.ICON_FOLDER);
         setName(_JDT.T.PackagizerSettings_folderbyplugin_rule_name2());
         setEnabled(true);
