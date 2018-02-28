db75833b53b7f88980a7ddc249eaf8e7c6bd7059
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-02-28 13:56:13 +0000

    fixes thread 76857
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38766 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/VolaFileOrg.java
+++ src/jd/plugins/decrypter/VolaFileOrg.java
@@ -33,12 +33,12 @@ public class VolaFileOrg extends PluginForDecrypt {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         br.getPage(parameter.getCryptedUrl().replace(".io/", ".org/"));
         br.followRedirect();
+        br.setCookie(getHost(), "allow-download", "1");
         final String checksum = br.getRegex("\"checksum2\"\\s*:\\s*\"(.*?)\"").getMatch(0);
         final String room = new Regex(parameter.getCryptedUrl(), "/r/([A-Za-z0-9\\-_]+)").getMatch(0);
-        final WebSocketClient wsc = new WebSocketClient(br, new URL("https://volafile.org/api/?EIO=3&transport=websocket"));
+        final WebSocketClient wsc = new WebSocketClient(br, new URL("https://volafile.org/api/?room=" + URLEncoder.encode(room, "UTF-8") + "&cs=" + URLEncoder.encode(checksum, "UTF-8") + "&nick=Alke&EIO=3&transport=websocket"));
         try {
             wsc.connect();
-            wsc.writeFrame(wsc.buildUTF8TextFrame("4[-1,[[0,[\"subscribe\",{\"room\":\"" + room + "\",\"checksum\":\"" + checksum + "\",\"checksum2\":\"" + checksum + "\",\"nick\":\"Alke\"}]],0]]"));// subscribe
             wsc.readNextFrame();// sid
             wsc.readNextFrame();// session
             final ReadWebSocketFrame frame = wsc.readNextFrame();// subscription
@@ -46,7 +46,7 @@ public class VolaFileOrg extends PluginForDecrypt {
                 String string = new String(frame.getPayload(), "UTF-8");
                 string = string.replaceFirst("^\\d+", "");
                 final List<Object> list = JSonStorage.restoreFromString(string, TypeRef.LIST);
-                final List<Object> files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(4)).get(0)).get(1)).get(1)).get("files");
+                final List<Object> files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(6)).get(0)).get(1)).get(1)).get("files");
                 for (final Object file : files) {
                     final List<Object> fileInfo = (List<Object>) file;
                     final String fileName = String.valueOf(fileInfo.get(1));
