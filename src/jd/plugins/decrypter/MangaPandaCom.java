421d4c009e257b2d1d5851450838437fad6fb57b
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-03-03 10:57:20 +0000

    Mangapanda.com - https, https://board.jdownloader.org/showthread.php?t=76896
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38811 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MangaPandaCom.java
+++ src/jd/plugins/decrypter/MangaPandaCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.text.DecimalFormat;
@@ -24,14 +23,14 @@ import jd.controlling.ProgressController;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangapanda.com" }, urls = { "http://(www\\.)?mangapanda\\.com/([a-z0-9\\-]+/\\d+|\\d+\\-\\d+\\-1/[a-z0-9\\-]+/chapter\\-\\d+\\.html)" }) 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangapanda.com" }, urls = { "https?://(www\\.)?mangapanda\\.com/([a-z0-9\\-]+/\\d+|\\d+\\-\\d+\\-1/[a-z0-9\\-]+/chapter\\-\\d+\\.html)" })
 public class MangaPandaCom extends PluginForDecrypt {
-
     public MangaPandaCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -45,35 +44,31 @@ public class MangaPandaCom extends PluginForDecrypt {
         if (br.getRequest().getContentLength() == 0) {
             return decryptedLinks;
         }
-
         if (br.containsHTML("is not released yet")) {
             logger.info("No downloadable content available for link: " + parameter);
             return decryptedLinks;
         }
-
         final String maxPage = br.getRegex("</select>\\s*of\\s*(\\d+)</div>").getMatch(0);
         String fpName = br.getRegex("<h1>([^<>\"]*?)</h1>").getMatch(0);
         if (fpName == null || maxPage == null) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+            throw new DecrypterException("Decrypter broken for link: " + parameter);
         }
-        final Regex splitLink = new Regex(parameter, "(http://(www\\.)?.*?/\\d+\\-\\d+\\-)1(/.+)");
+        final Regex splitLink = new Regex(parameter, "(https?://(www\\.)?.*?/\\d+\\-\\d+\\-)1(/.+)");
         final String part1 = splitLink.getMatch(0);
         final String part2 = splitLink.getMatch(2);
         fpName = Encoding.htmlDecode(fpName.trim());
         final DecimalFormat df = new DecimalFormat("0000");
         for (int i = 1; i <= Integer.parseInt(maxPage); i++) {
             if (i > 1) {
-                if (parameter.matches("http://(www\\.)?.*?/\\d+\\-\\d+\\-1/[a-z0-9\\-]+/chapter\\-\\d+\\.html")) {
+                if (parameter.matches("https?://(www\\.)?.*?/\\d+\\-\\d+\\-1/[a-z0-9\\-]+/chapter\\-\\d+\\.html")) {
                     br.getPage(part1 + i + part2);
                 } else {
                     br.getPage(parameter + "/" + i);
                 }
             }
-            final String finallink = br.getRegex("\"(http://i\\d+\\.(p\\.)?(mangapanda\\.com|mangacdn\\.com)/[a-z0-9\\-]+/\\d+/[^<>\"]*?)\"").getMatch(0);
+            final String finallink = br.getRegex("\"(https://i\\d+\\.(p\\.)?(mangapanda\\.com|mangacdn\\.com)/[a-z0-9\\-]+/\\d+/[^<>\"]*?)\"").getMatch(0);
             if (finallink == null) {
-                logger.warning("Decrypter broken for link: " + parameter);
-                return null;
+                throw new DecrypterException("Decrypter broken for link: " + parameter);
             }
             final DownloadLink dl = createDownloadlink("directhttp://" + finallink);
             dl.setFinalFileName(fpName + "_" + df.format(i) + finallink.substring(finallink.lastIndexOf(".")));
@@ -90,5 +85,4 @@ public class MangaPandaCom extends PluginForDecrypt {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
