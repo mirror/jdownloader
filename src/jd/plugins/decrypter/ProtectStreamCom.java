f93670bb896563daee61b7987592ce3d2429bd33
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-03-05 08:24:55 +0000

    fixes thread 76928
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38825 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 25199cb41aeb7fea11df44e522325c5d8018b110

--- src/jd/plugins/decrypter/ProtectStreamCom.java
+++ src/jd/plugins/decrypter/ProtectStreamCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
@@ -27,9 +26,8 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "protect-stream.com" }, urls = { "http://(www\\.)?protect\\-stream\\.com/PS_DL_[A-Za-z0-9\\-_]+" }) 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "protect-stream.com" }, urls = { "https?://(www\\.)?protect\\-stream\\.com/PS_(DL|SM)_[A-Za-z0-9\\-_]+" })
 public class ProtectStreamCom extends PluginForDecrypt {
-
     public ProtectStreamCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -37,25 +35,33 @@ public class ProtectStreamCom extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        final String prtcid = new Regex(parameter, "protect\\-stream\\.com/PS_DL_([A-Za-z0-9\\-_]+)").getMatch(0);
-        // br.getPage(parameter);
-        // br.getPage("http://www.protect-stream.com/frame.php?u=" + prtcid);
-        br.getPage("http://www.protect-stream.com/w.php?u=" + prtcid);
-        final String cheap = br.getRegex("var k=\"([^<>\"]*?)\";").getMatch(0);
-        if (cheap == null) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+        final String prtcid = new Regex(parameter, "protect\\-stream\\.com/PS_(?:DL|SM)_([A-Za-z0-9\\-_]+)").getMatch(0);
+        if (parameter.contains("PS_SM")) {
+            br.getPage(parameter);
+            final String cheap = br.getRegex("var k=\"([^<>\"]*?)\";").getMatch(0);
+            if (cheap == null) {
+                logger.warning("Decrypter broken for link: " + parameter);
+                return null;
+            }
+            this.sleep(10 * 1000l, param);
+            br.postPage("http://www.protect-stream.com/secur2_sm.php", "k=" + Encoding.urlEncode(cheap));
+        } else {
+            // br.getPage(parameter);
+            // br.getPage("http://www.protect-stream.com/frame.php?u=" + prtcid);
+            br.getPage("http://www.protect-stream.com/w.php?u=" + prtcid);
+            final String cheap = br.getRegex("var k=\"([^<>\"]*?)\";").getMatch(0);
+            if (cheap == null) {
+                logger.warning("Decrypter broken for link: " + parameter);
+                return null;
+            }
+            br.postPage("http://www.protect-stream.com/secur.php", "k=" + Encoding.urlEncode(cheap));
         }
-        this.sleep(10 * 1000l, param);
-        br.postPage("http://www.protect-stream.com/secur.php", "k=" + Encoding.urlEncode(cheap));
-        final String finallink = br.getRegex("href=\"(http[^<>\"]*?)\"").getMatch(0);
+        final String finallink = br.getRegex("(?:href|src)\\s*=\\s*\"(http[^<>\"]*?)\"").getMatch(0);
         if (finallink == null) {
             logger.warning("Decrypter broken for link: " + parameter);
             return null;
         }
         decryptedLinks.add(createDownloadlink(finallink));
-
         return decryptedLinks;
     }
-
 }
