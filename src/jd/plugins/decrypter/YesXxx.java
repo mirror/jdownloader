2f14dd914800a0eb9708dfa92209d79f21e1ea27
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-03-12 10:32:40 +0000

    Yes.xxx - Decrypter fixes #85268
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38907 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/YesXxx.java
@@ -0,0 +1,55 @@
+//    jDownloader - Downloadmanager
+//    Copyright (C) 2012  JD-Team support@jdownloader.org
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "yes.xxx" }, urls = { "https?://(www\\.)?yes\\.xxx/\\?v=[A-Z0-9]+" })
+public class YesXxx extends PluginForDecrypt {
+    public YesXxx(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.getPage(parameter);
+        String rdl = br.getRedirectLocation();
+        if (rdl == null) {
+            rdl = br.getRegex("<iframe frameborder=\"0\"\\s*src=\"(.*?)\"").getMatch(0);
+        }
+        if (rdl != null) {
+            decryptedLinks.add(createDownloadlink(rdl));
+        } else {
+            // decryptedLinks.add(createDownloadlink(parameter));
+            throw new DecrypterException("Decrypter broken for link: " + parameter);
+        }
+        return decryptedLinks;
+    }
+
+    /* NO OVERRIDE!! */
+    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
+        return false;
+    }
+}
