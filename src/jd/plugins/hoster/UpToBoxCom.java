eaa70b54fd93e6eb7cbeb3705fed158f23def18f
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-03-24 13:50:40 +0000

    Uptobox.com - Max chunks = 1, https://board.jdownloader.org/showthread.php?t=77105
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@38996 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/UpToBoxCom.java
+++ src/jd/plugins/hoster/UpToBoxCom.java
@@ -72,10 +72,10 @@ public class UpToBoxCom extends antiDDoSForHost {
     private static final String  PREMIUMONLY2                 = JDL.L("hoster.xfilesharingprobasic.errors.premiumonly2", "Only downloadable via premium or registered");
     /* Connection stuff */
     private static final boolean FREE_RESUME                  = true;
-    private static final int     FREE_MAXCHUNKS               = -2;
+    private static final int     FREE_MAXCHUNKS               = -1;
     private static final int     FREE_MAXDOWNLOADS            = 5;
     private static final boolean ACCOUNT_FREE_RESUME          = true;
-    private static final int     ACCOUNT_FREE_MAXCHUNKS       = -2;
+    private static final int     ACCOUNT_FREE_MAXCHUNKS       = -1;
     private static final int     ACCOUNT_FREE_MAXDOWNLOADS    = 5;
     private static final boolean ACCOUNT_PREMIUM_RESUME       = true;
     private static final int     ACCOUNT_PREMIUM_MAXCHUNKS    = 0;
