20b27fcd11bcb02499a278e33095d9a6e9b9286a
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-03-31 08:57:47 +0000

    Depfile.us - Folder, https://board.jdownloader.org/showthread.php?t=77174
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39034 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/DepfileComDecrypter.java
+++ src/jd/plugins/decrypter/DepfileComDecrypter.java
@@ -18,16 +18,17 @@ package jd.plugins.decrypter;
 import java.net.URL;
 import java.util.ArrayList;
 
-import org.appwork.utils.formatter.SizeFormatter;
-
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
+
+import org.appwork.utils.formatter.SizeFormatter;
 
 //This decrypter is there to seperate folder- and hosterlinks as hosterlinks look the same as folderlinks
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "depfile.com" }, urls = { "https?://(www\\.)?(i\\-filez\\.com|d[ei]pfile\\.com|depfile\\.us)/(downloads/i/\\d+/f/[^\"\\']+|(?!downloads)[a-zA-Z0-9]+)" })
@@ -45,47 +46,59 @@ public class DepfileComDecrypter extends PluginForDecrypt {
             logger.info("Link invalid: " + parameter);
             return decryptedLinks;
         }
+        final String host = new Regex(parameter, "(https?://[^/]+)/").getMatch(0);
         final String folder_id = new Regex(parameter, "([A-Za-z0-9]+)$").getMatch(0);
         jd.plugins.hoster.DepfileCom.prepBrowser(this.br);
         br.setFollowRedirects(true);
         br.getPage(parameter);
-        // mass adding links from folders can cause exceptions and high server loads. when possible best practice to set all info, a full
-        // linkcheck happens prior to download which can correct any false positives by doing the following..
-        final String[] links = br.getRegex("<tr><td[^>]+><input[^>]+>.*?</td></tr>").getColumn(-1);
-        if (links != null && links.length != 0) {
-            // folder link
-            for (String link : links) {
-                final String l = new Regex(link, "https?://(?:d[ei]pfile\\.com|depfile\\.us)/[A-Za-z0-9]+|https?://(www\\.)?(?:d[ei]pfile\\.com|depfile\\.us)/[a-zA-Z0-9]{8}\\?cid=[a-z0-9]{32}").getMatch(-1);
-                final String f = new Regex(link, "title=(\"|')(.*?)\\1").getMatch(1);
-                final String s = new Regex(link, ">(\\d+(\\.\\d+)?\\s*[a-z]{2})<").getMatch(0);
-                final boolean folder = new Regex(link, "<input type='text' class='view state_0 folder'").matches();
-                if (l != null && !l.contains(folder_id) && !new URL(l).getPath().equals("/downloads")) {
-                    final DownloadLink d = createDownloadlink(l);
-                    if (f != null) {
-                        d.setName(f);
+        String nops = br.getRegex("current.>1/(\\d+)<").getMatch(0);
+        int nps = 1;
+        if (nops != null) {
+            logger.info("Debug info: Number of pages: " + nops);
+            nps = Integer.valueOf(nops);
+        }
+        for (int i = 2; i <= nps + 1; i++) {
+            // mass adding links from folders can cause exceptions and high server loads. when possible best practice to set all info, a
+            // full
+            // linkcheck happens prior to download which can correct any false positives by doing the following..
+            final String[] links = br.getRegex("<tr>\\s*<td[^>]+><input[^>]+>.*?</td>\\s*</tr>").getColumn(-1);
+            if (links != null && links.length != 0) {
+                // folder link
+                for (String link : links) {
+                    final String l = host + new Regex(link, "window.open\\('(.*?)'\\)").getMatch(0);
+                    final String f = new Regex(link, "title=(\"|')(.*?)\\1").getMatch(1);
+                    final String s = new Regex(link, ";(\\d+(\\.\\d+)?\\s*[a-zA-Z]{2})<").getMatch(0);
+                    final boolean folder = new Regex(link, "<input type='text' class='view state_0 folder'").matches();
+                    if (l != null && !l.contains(folder_id) && !new URL(l).getPath().equals("/downloads")) {
+                        final DownloadLink d = createDownloadlink(l);
+                        if (f != null) {
+                            d.setName(f);
+                        }
+                        if (s != null) {
+                            d.setDownloadSize(SizeFormatter.getSize(s));
+                        }
+                        if (!folder) {
+                            // cant set true as we need to re-enter.
+                            d.setAvailable(true);
+                        }
+                        decryptedLinks.add(d);
+                        distribute(d);
                     }
-                    if (s != null) {
-                        d.setDownloadSize(SizeFormatter.getSize(s));
+                    final String fpName = br.getRegex("<th>Folder name:</th>\\s*<td>(.*?)</td>").getMatch(0);
+                    if (fpName != null) {
+                        FilePackage fp = FilePackage.getInstance();
+                        fp.setName(fpName.trim());
+                        fp.addLinks(decryptedLinks);
                     }
-                    if (!folder) {
-                        // cant set true as we need to re-enter.
-                        d.setAvailable(true);
-                    }
-                    decryptedLinks.add(d);
                 }
             }
-            final String fpName = br.getRegex("<th>Folder name:</th>\\s*<td>(.*?)</td>").getMatch(0);
-            if (fpName != null) {
-                FilePackage fp = FilePackage.getInstance();
-                fp.setName(fpName.trim());
-                fp.addLinks(decryptedLinks);
+            if (i < nps + 1) {
+                br.getPage(parameter + "?page=" + i);
             }
-            return decryptedLinks;
         }
         // when 'folder link' above has failed...
-        if (br.containsHTML(">Description of the downloaded folder")) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+        if (decryptedLinks.isEmpty() && br.containsHTML(">Description of the downloaded folder")) {
+            throw new DecrypterException("Decrypter broken for link: " + parameter);
         }
         // single link
         if (decryptedLinks.isEmpty()) {
