b7e3e72318aaee5df52c5d315955b1d7cf717f18
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-04-19 15:44:09 +0000

    Perfectgirls.net PornEmbedParser fixes #85413
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39160 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: b7cecaecec9d788d71a8563194660f0f9bf2ae93

--- src/jd/plugins/decrypter/PornEmbedParser.java
+++ src/jd/plugins/decrypter/PornEmbedParser.java
@@ -981,7 +981,7 @@ public abstract class PornEmbedParser extends antiDDoSForDecrypt {
             externID = br.getRegex("(https://cdn[^/]+.tnaflix.com/[^\"]+)\"").getMatch(0);
         }
         if (externID == null) { // <source src="https://smog-02.tnaflix.com/15/15845fb87f8c5eae3cbf/.+
-            externID = br.getRegex("<source src=\"([^\"]+(cdn|smog)[^\"]+)\"").getMatch(0);
+            externID = br.getRegex("<source src=\"([^\"]+(cdn(?!\\.perfectgirls)|smog)[^\"]+)\"").getMatch(0);
         }
         if (externID != null) {
             final DownloadLink dl = createDownloadlink(externID);
