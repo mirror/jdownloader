418e86f84044605c9875b2476802ad1b3db3dca4
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-04-27 07:08:32 +0000

    Upload4earn.com - Support .org, https://board.jdownloader.org/showthread.php?t=77378
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39196 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/Upload4earnCom.java
+++ src/jd/plugins/hoster/Upload4earnCom.java
@@ -62,7 +62,7 @@ import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
 import org.jdownloader.plugins.components.antiDDoSForHost;
 import org.jdownloader.scripting.JavaScriptEngineFactory;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "upload4earn.com" }, urls = { "https?://(?:www\\.)?upload4earn\\.(com|net)/(?:embed\\-)?[a-z0-9]{12}" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "upload4earn.com" }, urls = { "https?://(?:www\\.)?upload4earn\\.(com|net|org)/(?:embed\\-)?[a-z0-9]{12}" })
 public class Upload4earnCom extends antiDDoSForHost {
     /* Some HTML code to identify different (error) states */
     private static final String  HTML_PASSWORDPROTECTED             = "<br><b>Passwor(d|t):</b> <input";
@@ -70,10 +70,10 @@ public class Upload4earnCom extends antiDDoSForHost {
     /* Here comes our XFS-configuration */
     private final boolean        SUPPORTS_HTTPS                     = true;
     /* primary website url, take note of redirects */
-    private final String         COOKIE_HOST                        = "http://upload4earn.net".replaceFirst("https?://", SUPPORTS_HTTPS ? "https://" : "http://");
+    private final String         COOKIE_HOST                        = "http://upload4earn.org".replaceFirst("https?://", SUPPORTS_HTTPS ? "https://" : "http://");
     private final String         NICE_HOSTproperty                  = COOKIE_HOST.replaceAll("(https://|http://|\\.|\\-)", "");
     /* domain names used within download links */
-    private final static String  DOMAINS                            = "(?:upload4earn\\.(?:com|net))";
+    private final static String  DOMAINS                            = "(?:upload4earn\\.(?:com|net|org))";
     private final static String  dllinkRegexFile                    = "https?://(?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|(?:[\\w\\-\\.]+\\.)?%s)(?::\\d{1,4})?/(?:files|d|cgi\\-bin/dl\\.cgi)/(?:\\d+/)?[a-z0-9]+/[^<>\"/]*?";
     private final static String  dllinkRegexImage                   = "https?://(?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|(?:[\\w\\-\\.]+\\.)?%s)(?:/img/\\d+/[^<>\"'\\[\\]]+|/img/[a-z0-9]+/[^<>\"'\\[\\]]+|/img/[^<>\"'\\[\\]]+|/i/\\d+/[^<>\"'\\[\\]]+|/i/\\d+/[^<>\"'\\[\\]]+(?!_t\\.[A-Za-z]{3,4}))";
     /* Errormessages inside URLs */
