47516983927e586af012c47c2c150b8c7233b49d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-04-30 09:38:28 +0000

    WindowsMouseSpeedWorkaround: -fixes #85445 -refs thread 77482
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39212 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/jna/windows/User32.java
+++ src/org/jdownloader/jna/windows/User32.java
@@ -18,8 +18,8 @@ public interface User32 extends com.sun.jna.platform.win32.User32 {
      * @return
      */
     boolean CloseWindow(HWND hWnd);
-    // https://msdn.microsoft.com/de-de/library/windows/desktop/ms633548(v=vs.85).aspx
 
+    // https://msdn.microsoft.com/de-de/library/windows/desktop/ms633548(v=vs.85).aspx
     // Displays the window in its current size and position. This value is similar to SW_SHOW, except that the window is not activated.
     public static final int SHOW_SW_SHOWNA            = 8;
     // Maximizes the specified window.
@@ -44,4 +44,7 @@ public interface User32 extends com.sun.jna.platform.win32.User32 {
     boolean ShowWindow(HWND hWnd, int state);
 
     void AnimateWindow(HWND hwnd, long time, long animation);
+
+    boolean SystemParametersInfo(int uiAction, int uiParam, Object pvParam, // Pointer or int
+            int fWinIni);
 }
