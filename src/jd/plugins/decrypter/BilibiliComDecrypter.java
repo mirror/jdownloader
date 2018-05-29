d6eeb3159e6b8a681fed5b3d8b97c4baa316d432
ZT333 <ZT333@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-05-29 11:10:05 +0000

    bilibili.com - implemented bangumi
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39330 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: ac3fde680627edc911a06c9389f9d12ed880ce76

--- src/jd/plugins/decrypter/BilibiliComDecrypter.java
+++ src/jd/plugins/decrypter/BilibiliComDecrypter.java
@@ -26,30 +26,43 @@ import org.jdownloader.scripting.JavaScriptEngineFactory;
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.nutils.JDHash;
+import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
+import jd.plugins.components.PluginJSonUtils;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "bilibili.com" }, urls = { "https?://(?:www\\.)?bilibili\\.com/(?:mobile/)?video/av\\d+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "bilibili.com" }, urls = { "https?://(?:www\\.)?bilibili\\.com/(?:video/av\\d+(?:/\\?p=\\d+)?|bangumi/(?:play|media)/.+)" })
 public class BilibiliComDecrypter extends PluginForDecrypt {
     public BilibiliComDecrypter(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final long   SUCCESS_OK        = 200l;
-    private static final String API_URL           = "//interface.bilibili.com/v2/playurl?";
-    private static final String API_QUERY_FORMAT1 = "appkey=%s&cid=%s&otype=json";
-    private static final String API_QUERY_FORMAT2 = "%s&qn=%s&quality=%s&type=";
-    private static final String API_QUERY_FORMAT3 = "%s%s&sign=%s";
-    private static final String APP_KEY           = "84956560bc028eb7";
-    private static final String SEC_KEY           = "94aba54af9065f71de72f5508f1cd42e";
+    private static final long   SUCCESS_OK                = 200l;
+    private static final String API_URL                   = "//interface.bilibili.com/v2/playurl?";
+    private static final String API_QUERY_FORMAT1         = "appkey=%s&cid=%s&otype=json";
+    private static final String API_QUERY_FORMAT2         = "%s&qn=%s&quality=%s&type=";
+    private static final String API_QUERY_FORMAT3         = "%s%s&sign=%s";
+    private static final String BANGUMI_API_URL           = "//bangumi.bilibili.com/player/web_api/v2/playurl?";
+    private static final String BANGUMI_API_QUERY_FORMAT1 = "appkey=%s&cid=%s&module=bangumi&otype=json";
+    private static final String BANGUMI_API_QUERY_FORMAT2 = "%s&qn=%s&quality=%s&season_type=%s&type=";
+    private static final String BANGUMI_API_QUERY_FORMAT3 = API_QUERY_FORMAT3;
+    private static final String BANGUMI_API_GEO_CHECK     = "//bangumi.bilibili.com/view/web_api/season/user/status?season_id=%s&season_type=%s";
+    //
+    private static final String APP_KEY                   = "84956560bc028eb7";
+    private static final String SEC_KEY                   = "94aba54af9065f71de72f5508f1cd42e";
 
     @SuppressWarnings("unchecked")
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         br.getPage(parameter);
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
+            return decryptedLinks;
+        }
         final String jsonString = br.getRegex("window\\.__INITIAL_STATE__=(\\{.+?\\});").getMatch(0);
         Map<String, Object> entries = null;
         long errCode = SUCCESS_OK;
@@ -58,57 +71,168 @@ public class BilibiliComDecrypter extends PluginForDecrypt {
             errCode = JavaScriptEngineFactory.toLong(JavaScriptEngineFactory.walkJson(entries, "error/code"), SUCCESS_OK);
         }
         if (errCode == 404 || entries == null) {
-            decryptedLinks.add(this.createOfflinelink(parameter));
+            decryptedLinks.add(createOfflinelink(parameter));
             return decryptedLinks;
         }
         String aid = (String) entries.get("aid");
-        if (parameter.contains("/bangumi/")) {
-        } else {
+        if (!parameter.contains("/bangumi/")) {
             String title = (String) JavaScriptEngineFactory.walkJson(entries, "videoData/title");
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(encodeUnicode(title.trim()));
             List<Map<String, Object>> pages = (List<Map<String, Object>>) JavaScriptEngineFactory.walkJson(entries, "videoData/pages");
-            int pageCnt = 1;
-            for (Map<String, Object> p : pages) {
-                // default
-                String cid = String.valueOf(p.get("cid"));
-                String part = (String) p.get("part");
-                final String query1 = String.format(API_QUERY_FORMAT1, APP_KEY, cid);
-                String query2 = String.format(API_QUERY_FORMAT2, query1, "0", "0");
-                String url = String.format(API_QUERY_FORMAT3, API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
-                br.getPage(url);
-                // best quality
-                Map<String, Object> entries2 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
-                List<Object> acceptQualityList = (List<Object>) entries2.get("accept_quality");
-                String acceptQualityMax = String.valueOf(acceptQualityList.get(0));
-                query2 = String.format(API_QUERY_FORMAT2, query1, acceptQualityMax, acceptQualityMax);
-                url = String.format(API_QUERY_FORMAT3, API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
-                br.getPage(url);
-                // dllink
-                Map<String, Object> entries3 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
-                String ext = (String) entries3.get("format");
-                List<Map<String, Object>> durls = (List<Map<String, Object>>) entries3.get("durl");
-                int partCnt = 1;
-                for (Map<String, Object> durl : durls) {
-                    // There are multiple possibilities, in that case need to join with ffmpeg.
-                    DownloadLink dl = createDownloadlink("https://bilibilidecrypted.com/" + System.currentTimeMillis() + new Random().nextInt(10000));
-                    dl.setProperty("mainLink", durl.get("url"));
-                    StringBuilder fileName = new StringBuilder(title);
-                    if (StringUtils.isNotEmpty(part)) {
-                        fileName.append(String.format(" P%d %s", pageCnt, part));
-                    }
-                    if (durls.size() > 2) {
-                        fileName.append(".part");
-                        fileName.append(String.valueOf(partCnt));
-                        partCnt++;
-                    }
-                    fileName.append(".");
-                    fileName.append(ext);
-                    dl.setFinalFileName(fileName.toString());
-                    dl.setDownloadSize(JavaScriptEngineFactory.toLong(durl.get("size"), -1));
-                    decryptedLinks.add(dl);
+            String pageAssignment = new Regex(parameter, "/\\?p=(\\d+)").getMatch(0);
+            if (pageAssignment != null && pages.size() > 1) {
+                createDLLink(fp, decryptedLinks, pages.get(Integer.parseInt(pageAssignment) - 1), title, pages.size());
+            } else {
+                for (Map<String, Object> p : pages) {
+                    createDLLink(fp, decryptedLinks, p, title, pages.size());
                 }
-                pageCnt++;
+            }
+        } else {
+            if (parameter.contains("/media/")) {
+                String seasonId = String.valueOf(JavaScriptEngineFactory.walkJson(entries, "mediaInfo/param/season_id"));
+                String seasonType = String.valueOf(JavaScriptEngineFactory.walkJson(entries, "mediaInfo/param/season_type"));
+                if (!checkGeoBangumi(seasonId, seasonType)) {
+                    logger.info("GEO-blocked");
+                    decryptedLinks.add(createOfflinelink(parameter));
+                } else {
+                    String mt = (String) JavaScriptEngineFactory.walkJson(entries, "mediaInfo/chn_name");
+                    FilePackage fp = FilePackage.getInstance();
+                    fp.setName(encodeUnicode(mt.trim()));
+                    List<Map<String, Object>> episodes = (List<Map<String, Object>>) JavaScriptEngineFactory.walkJson(entries, "mediaInfo/episodes");
+                    for (Map<String, Object> ep : episodes) {
+                        String ei = (String) ep.get("index");
+                        String et = (String) ep.get("index_title");
+                        String title = String.format("%s EP%s %s", mt, ei, et);
+                        String cid = String.valueOf(ep.get("cid"));
+                        createDLLinkBangumi(fp, decryptedLinks, title, seasonType, cid);
+                    }
+                }
+            } else {
+                String seasonId = String.valueOf(JavaScriptEngineFactory.walkJson(entries, "mediaInfo/season_id"));
+                String seasonType = String.valueOf(JavaScriptEngineFactory.walkJson(entries, "mediaInfo/season_type"));
+                if (!checkGeoBangumi(seasonId, seasonType)) {
+                    logger.info("GEO-blocked");
+                    decryptedLinks.add(createOfflinelink(parameter));
+                } else {
+                    String mt = (String) JavaScriptEngineFactory.walkJson(entries, "mediaInfo/title");
+                    String ei = (String) JavaScriptEngineFactory.walkJson(entries, "epInfo/index");
+                    String et = (String) JavaScriptEngineFactory.walkJson(entries, "epInfo/index_title");
+                    String title = String.format("%s EP%s %s", mt, ei, et);
+                    String cid = String.valueOf(JavaScriptEngineFactory.walkJson(entries, "epInfo/cid"));
+                    FilePackage fp = FilePackage.getInstance();
+                    fp.setName(encodeUnicode(title.trim()));
+                    createDLLinkBangumi(fp, decryptedLinks, title, seasonType, cid);
+                }
             }
         }
         return decryptedLinks;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void createDLLink(final FilePackage fp, final ArrayList<DownloadLink> decryptedLinks, final Map<String, Object> page, final String title, final int pageSize) throws Exception {
+        // default
+        String cid = String.valueOf(page.get("cid"));
+        String pageNo = String.valueOf(page.get("page"));
+        String part = (String) page.get("part");
+        final String query1 = String.format(API_QUERY_FORMAT1, APP_KEY, cid);
+        String query2 = String.format(API_QUERY_FORMAT2, query1, "0", "0");
+        String url = String.format(API_QUERY_FORMAT3, API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
+        br.getPage(url);
+        // best quality
+        Map<String, Object> entries2 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        List<Object> acceptQualityList = (List<Object>) entries2.get("accept_quality");
+        String acceptQualityMax = String.valueOf(acceptQualityList.get(0));
+        query2 = String.format(API_QUERY_FORMAT2, query1, acceptQualityMax, acceptQualityMax);
+        url = String.format(API_QUERY_FORMAT3, API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
+        br.getPage(url);
+        // dllink
+        Map<String, Object> entries3 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        String ext = "flv"; // (String) entries3.get("format");
+        List<Map<String, Object>> durls = (List<Map<String, Object>>) entries3.get("durl");
+        for (Map<String, Object> durl : durls) {
+            // There are multiple possibilities, in that case need to join with ffmpeg.
+            DownloadLink dl = createDownloadlink("https://bilibilidecrypted.com/" + System.currentTimeMillis() + new Random().nextInt(10000));
+            dl.setProperty("mainLink", durl.get("url"));
+            StringBuilder fileName = new StringBuilder(title);
+            if (StringUtils.isNotEmpty(part) && pageSize > 1) {
+                fileName.append(String.format(" P%s %s", pageNo, part));
+            }
+            if (durls.size() > 1) {
+                fileName.append(".part");
+                fileName.append(String.valueOf(durl.get("order")));
+            }
+            fileName.append(".");
+            fileName.append(ext);
+            dl.setFinalFileName(encodeUnicode(fileName.toString()));
+            dl.setDownloadSize(JavaScriptEngineFactory.toLong(durl.get("size"), -1));
+            fp.add(dl);
+            decryptedLinks.add(dl);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void createDLLinkBangumi(final FilePackage fp, final ArrayList<DownloadLink> decryptedLinks, final String title, final String seasonType, final String cid) throws Exception {
+        // default
+        final String query1 = String.format(BANGUMI_API_QUERY_FORMAT1, APP_KEY, cid);
+        String query2 = String.format(BANGUMI_API_QUERY_FORMAT2, query1, "0", "0", seasonType);
+        String url = String.format(BANGUMI_API_QUERY_FORMAT3, BANGUMI_API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
+        br.getPage(url);
+        // best quality
+        Map<String, Object> entries2 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        List<Object> acceptQualityList = (List<Object>) entries2.get("accept_quality");
+        String acceptQualityMax = String.valueOf(acceptQualityList.get(0));
+        query2 = String.format(BANGUMI_API_QUERY_FORMAT2, query1, acceptQualityMax, acceptQualityMax, seasonType);
+        url = String.format(BANGUMI_API_QUERY_FORMAT3, BANGUMI_API_URL, query2, JDHash.getMD5(query2 + SEC_KEY));
+        br.getPage(url);
+        // dllink
+        Map<String, Object> entries3 = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        String ext = "flv"; // (String) entries3.get("format");
+        List<Map<String, Object>> durls = (List<Map<String, Object>>) entries3.get("durl");
+        for (Map<String, Object> durl : durls) {
+            // There are multiple possibilities, in that case need to join with ffmpeg.
+            DownloadLink dl = createDownloadlink("https://bilibilidecrypted.com/" + System.currentTimeMillis() + new Random().nextInt(10000));
+            dl.setProperty("mainLink", durl.get("url"));
+            StringBuilder fileName = new StringBuilder(title);
+            if (durls.size() > 1) {
+                fileName.append(".part");
+                fileName.append(String.valueOf(durl.get("order")));
+            }
+            fileName.append(".");
+            fileName.append(ext);
+            dl.setFinalFileName(encodeUnicode(fileName.toString()));
+            dl.setDownloadSize(JavaScriptEngineFactory.toLong(durl.get("size"), -1));
+            fp.add(dl);
+            decryptedLinks.add(dl);
+        }
+    }
+
+    private boolean checkGeoBangumi(String seasonId, String seasonType) throws Exception {
+        boolean result = false;
+        br.getPage(String.format(BANGUMI_API_GEO_CHECK, seasonId, seasonType));
+        String areaLimit = PluginJSonUtils.getJsonValue(br, "area_limit");
+        if ("0".equals(areaLimit)) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public String encodeUnicode(final String input) {
+        if (input != null) {
+            String output = input;
+            output = output.replace(":", "：");
+            output = output.replace("|", "｜");
+            output = output.replace("<", "＜");
+            output = output.replace(">", "＞");
+            output = output.replace("/", "⁄");
+            output = output.replace("\\", "∖");
+            output = output.replace("*", "＊");
+            output = output.replace("?", "？");
+            output = output.replace("!", "！");
+            output = output.replace("\"", "”");
+            return output;
+        }
+        return null;
     }
 }
