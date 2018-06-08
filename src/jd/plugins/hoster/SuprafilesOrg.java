9142eb66bf81317400a3b5393ed920dbaefd6002
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-06-08 13:47:22 +0000

    refs #85590
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39380 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7c6508d81bc10ae2b7a23e5cf2a5a38e27008951

--- src/jd/plugins/hoster/SuprafilesOrg.java
+++ src/jd/plugins/hoster/SuprafilesOrg.java
@@ -196,7 +196,7 @@ public class SuprafilesOrg extends antiDDoSForHost {
         getPage(link.getPluginPatternMatcher());
         setFUID(link);
         if (new Regex(correctedBR, "(No such file|>File Not Found<|>The file was removed by|Reason for deletion:\n|File Not Found|>The file expired)").matches()) {
-            if (!br.containsHTML("hidden\"\\s*>\\s*File Not Found\\s*</font>") && !br.containsHTML("hidden\"\\s*>\\s*No such file\\s*</font>")) {
+            if (!br.containsHTML("hidden\"\\s*>\\s*File Not Found\\s*</font>") && !br.containsHTML("hidden\"\\s*>\\s*No such file\\s*</font>") && !br.containsHTML("hidden\"\\s*>\\s*The file was removed by\\s*</font>")) {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
         }
