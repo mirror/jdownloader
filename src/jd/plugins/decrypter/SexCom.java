6869621fa048a67524803f37d8da0b742643c197
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-06-15 10:42:12 +0000

    Sex.com - Picture link fixes #85599
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39401 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5ea06438a143167b220ca878eb530fd307585ade

--- src/jd/plugins/decrypter/SexCom.java
+++ src/jd/plugins/decrypter/SexCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.net.URL;
@@ -32,17 +31,14 @@ import org.appwork.utils.Files;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "sex.com" }, urls = { "https?://(?:www\\.)?sex\\.com/(?:pin/\\d+|picture/\\d+|video/\\d+|galleries/[a-z0-9\\-_]+/\\d+|link/out\\?id=\\d+)" })
 public class SexCom extends PornEmbedParser {
-
     public SexCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     /* DEV NOTES */
     /* Porn_plugin */
-
     private static final String TYPE_VIDEO           = "https?://(www\\.)?sex\\.com/video/\\d+.*?";
     private static final String TYPE_EXTERN_REDIRECT = "https?://(?:www\\.)?sex\\.com/link/out\\?id=\\d+";
-
     ArrayList<DownloadLink>     decryptedLinks       = new ArrayList<DownloadLink>();
     private String              PARAMETER            = null;
 
@@ -149,6 +145,9 @@ public class SexCom extends PornEmbedParser {
             return;
         }
         externID = br.getRegex("\"(/link/out\\?id=\\d+)\" data\\-hostname").getMatch(0);
+        if (externID == null) {
+            externID = br.getRegex("href=\"([^<>\"]+)\" data-rel=\"source\"").getMatch(0); // Picture
+        }
         if (externID != null) {
             decryptedLinks.add(this.createDownloadlink(br.getURL(externID).toString()));
             return;
@@ -160,5 +159,4 @@ public class SexCom extends PornEmbedParser {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
