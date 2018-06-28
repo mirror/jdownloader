c039b612a4dbbf3210212bbf8f94695e31b1f6bc
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-06-28 13:43:34 +0000

    Wait time, Ref.: #85590
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39449 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: fa3ba694a783e3d73b5e7ff59840df07f44abbd1

--- src/jd/plugins/hoster/SuprafilesOrg.java
+++ src/jd/plugins/hoster/SuprafilesOrg.java
@@ -1189,7 +1189,7 @@ public class SuprafilesOrg extends antiDDoSForHost {
         }
         /** Wait time reconnect handling */
         // if (new Regex(correctedBR, "(You have reached the download(\\-| )limit|You have to wait)").matches()) {
-        if (new Regex(correctedBR, "(font-size:15px;\">You have to wait)").matches()) {
+        if (!new Regex(correctedBR, "(visibility:hidden[^<>]+>You have to wait)").matches()) {
             /* adjust this regex to catch the wait time string for COOKIE_HOST */
             String wait = new Regex(correctedBR, "((You have reached the download(\\-| )limit|You have to wait)[^<>]+)").getMatch(0);
             String tmphrs = new Regex(wait, "\\s+(\\d+)\\s+hours?").getMatch(0);
@@ -1225,7 +1225,7 @@ public class SuprafilesOrg extends antiDDoSForHost {
                 if (waittime < 180000) {
                     throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, JDL.L("plugins.hoster.xfilesharingprobasic.allwait", USERTEXT_ALLWAIT_SHORT), waittime);
                 }
-                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, null, waittime);
+                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "JD is blocked", waittime);
             }
         }
         if (correctedBR.contains("You're using all download slots for IP")) {
