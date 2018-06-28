726650c46aed0de332b686cc4ac9e5750e948398
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-06-28 06:24:31 +0000

    Wait time, Ref.: #85590
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39446 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/SuprafilesOrg.java
+++ src/jd/plugins/hoster/SuprafilesOrg.java
@@ -1189,7 +1189,7 @@ public class SuprafilesOrg extends antiDDoSForHost {
         }
         /** Wait time reconnect handling */
         // if (new Regex(correctedBR, "(You have reached the download(\\-| )limit|You have to wait)").matches()) {
-        if (new Regex(correctedBR, "(font-size[^<>]*?>You don't have to wait)").matches()) {
+        if (new Regex(correctedBR, "(font-size:15px;\">You have to wait)").matches()) {
             /* adjust this regex to catch the wait time string for COOKIE_HOST */
             String wait = new Regex(correctedBR, "((You have reached the download(\\-| )limit|You have to wait)[^<>]+)").getMatch(0);
             String tmphrs = new Regex(wait, "\\s+(\\d+)\\s+hours?").getMatch(0);
