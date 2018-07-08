99b7585fb47548986391d6bcf27c7cb16e01d438
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-07-08 10:45:56 +0000

    Pornktube.com - dllink fixes #85665
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39483 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9d57067ddb75d76b1544bc5fc246f2fedee85a1a

--- src/jd/plugins/hoster/PornktubeCom.java
+++ src/jd/plugins/hoster/PornktubeCom.java
@@ -121,7 +121,10 @@ public class PornktubeCom extends PluginForHost {
                 for (final String item[] : items) {
                     logger.info("Debug info: Preferred_format: " + Preferred_format + ", checking format: " + item[1]);
                     final String range = new Regex(item[4], "(\\d+)\\d{3}").getMatch(0) + "000";
-                    dllink = "http://s" + item[7] + ".cdna.tv/cvideo/" + item[5] + "/" + item[6] + "/" + range + "/" + item[4] + "/" + item[4] + "_" + item[1] + ".mp4";
+                    dllink = "http://s" + item[7] + ".cdna.tv/lhvideo/" + item[5] + "/" + item[6] + "/" + range + "/" + item[4] + "/" + item[4] + "_" + item[1] + ".mp4" + "/" + item[4] + "_" + item[1] + ".mp4";
+                    if (item[1].equals("720p")) {
+                        dllink = "http://s" + item[7] + ".cdna.tv/lhvideo/" + item[5] + "/" + item[6] + "/" + range + "/" + item[4] + "/" + item[4] + ".mp4" + "/" + item[4] + ".mp4";
+                    }
                     logger.info("Debug info: checking dllink: " + dllink);
                     checkDllink(link);
                     if (dllink == null) {
