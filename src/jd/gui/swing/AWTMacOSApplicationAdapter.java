a2ff42e24369eb1ed200bfe3ad36e528f7be51c3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-09-10 15:14:39 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39766 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: dd468b36e950b57fd2d7f37afb87b41404567ceb

--- src/jd/gui/swing/AWTMacOSApplicationAdapter.java
+++ src/jd/gui/swing/AWTMacOSApplicationAdapter.java
@@ -45,20 +45,18 @@ public class AWTMacOSApplicationAdapter {
                 final Object openURIHandler = java.lang.reflect.Proxy.newProxyInstance(openURIHandlerInterface.getClassLoader(), new Class[] { openURIHandlerInterface }, new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        if ("openURI​".equals(method.getName())) {
-                            appReOpened();
-                            final Object openURIEvent​ = args[0];
-                            final Method getURI​ = openURIEvent​.getClass().getMethod("getURI​", new Class[0]);
-                            final URI uri = (URI) getURI​.invoke(openURIEvent​, new Object[0]);
-                            final String links = uri.toString();
-                            SecondLevelLaunch.GUI_COMPLETE.executeWhenReached(new Runnable() {
-                                @Override
-                                public void run() {
-                                    org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Distribute links: " + links);
-                                    LinkCollector.getInstance().addCrawlerJob(new LinkCollectingJob(LinkOrigin.MAC_DOCK.getLinkOriginDetails(), links));
-                                }
-                            });
-                        }
+                        appReOpened();
+                        final Object openURIEvent​ = args[0];
+                        final Method getURI​ = openURIEvent​.getClass().getMethod("getURI​", new Class[0]);
+                        final URI uri = (URI) getURI​.invoke(openURIEvent​, new Object[0]);
+                        final String links = uri.toString();
+                        SecondLevelLaunch.GUI_COMPLETE.executeWhenReached(new Runnable() {
+                            @Override
+                            public void run() {
+                                org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Distribute links: " + links);
+                                LinkCollector.getInstance().addCrawlerJob(new LinkCollectingJob(LinkOrigin.MAC_DOCK.getLinkOriginDetails(), links));
+                            }
+                        });
                         return null;
                     }
                 });
@@ -79,28 +77,26 @@ public class AWTMacOSApplicationAdapter {
                 final Object openFileHandler = java.lang.reflect.Proxy.newProxyInstance(openFilesHandlerInterface.getClassLoader(), new Class[] { openFilesHandlerInterface }, new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        if ("openFiles​".equals(method.getName())) {
-                            appReOpened();
-                            final Object openFilesEvent = args[0];
-                            Method getFiles = openFilesEvent.getClass().getMethod("getFiles", new Class[0]);
-                            final List<File> files = (List<File>) getFiles.invoke(openFilesEvent, new Object[0]);
-                            org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Handle open files from Dock " + files.toString());
-                            final StringBuilder sb = new StringBuilder();
-                            for (final File file : files) {
-                                if (sb.length() > 0) {
-                                    sb.append("\r\n");
-                                }
-                                sb.append(file.toURI().toString());
+                        appReOpened();
+                        final Object openFilesEvent = args[0];
+                        Method getFiles = openFilesEvent.getClass().getMethod("getFiles", new Class[0]);
+                        final List<File> files = (List<File>) getFiles.invoke(openFilesEvent, new Object[0]);
+                        org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Handle open files from Dock " + files.toString());
+                        final StringBuilder sb = new StringBuilder();
+                        for (final File file : files) {
+                            if (sb.length() > 0) {
+                                sb.append("\r\n");
                             }
-                            final String links = sb.toString();
-                            SecondLevelLaunch.GUI_COMPLETE.executeWhenReached(new Runnable() {
-                                @Override
-                                public void run() {
-                                    org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Distribute links: " + links);
-                                    LinkCollector.getInstance().addCrawlerJob(new LinkCollectingJob(LinkOrigin.MAC_DOCK.getLinkOriginDetails(), links));
-                                }
-                            });
+                            sb.append(file.toURI().toString());
                         }
+                        final String links = sb.toString();
+                        SecondLevelLaunch.GUI_COMPLETE.executeWhenReached(new Runnable() {
+                            @Override
+                            public void run() {
+                                org.appwork.utils.logging2.extmanager.LoggerFactory.getDefaultLogger().info("Distribute links: " + links);
+                                LinkCollector.getInstance().addCrawlerJob(new LinkCollectingJob(LinkOrigin.MAC_DOCK.getLinkOriginDetails(), links));
+                            }
+                        });
                         return null;
                     }
                 });
@@ -121,16 +117,21 @@ public class AWTMacOSApplicationAdapter {
                 final Object preferencesHandler = java.lang.reflect.Proxy.newProxyInstance(preferencesHandlerInterface.getClassLoader(), new Class[] { preferencesHandlerInterface }, new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        System.out.println(method.getName());
-                        if ("handlePreferences​".equals(method.getName())) {
-                            try {
-                                appReOpened();
-                                JsonConfig.create(GraphicalUserInterfaceSettings.class).setConfigViewVisible(true);
-                                JDGui.getInstance().setContent(ConfigurationView.getInstance(), true);
-                            } catch (Throwable ignore) {
-                                ignore.printStackTrace();
+                        new EDTRunner() {
+                            @Override
+                            protected void runInEDT() {
+                                try {
+                                    System.out.println(1);
+                                    appReOpened();
+                                    System.out.println(2);
+                                    JsonConfig.create(GraphicalUserInterfaceSettings.class).setConfigViewVisible(true);
+                                    System.out.println(3);
+                                    JDGui.getInstance().setContent(ConfigurationView.getInstance(), true);
+                                } catch (Throwable ignore) {
+                                    ignore.printStackTrace();
+                                }
                             }
-                        }
+                        };
                         return null;
                     }
                 });
@@ -167,11 +168,9 @@ public class AWTMacOSApplicationAdapter {
                 final Object aboutHandler = java.lang.reflect.Proxy.newProxyInstance(aboutHandlerInterface.getClassLoader(), new Class[] { aboutHandlerInterface }, new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        if ("handleAbout".equals(method.getName())) {
-                            try {
-                                Dialog.getInstance().showDialog(new AboutDialog());
-                            } catch (DialogNoAnswerException e1) {
-                            }
+                        try {
+                            Dialog.getInstance().showDialog(new AboutDialog());
+                        } catch (DialogNoAnswerException e1) {
                         }
                         return null;
                     }
