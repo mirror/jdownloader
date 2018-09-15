e65866a5e6648d0bed2f41698fabf06ac87a9432
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-09-15 10:04:18 +0000

    Opendrive.com - Offline fixes #86034
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39796 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/OpenDriveComDecrypter.java
+++ src/jd/plugins/decrypter/OpenDriveComDecrypter.java
@@ -18,6 +18,9 @@ package jd.plugins.decrypter;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+import org.appwork.utils.StringUtils;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.nutils.encoding.Encoding;
@@ -30,9 +33,6 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.components.PluginJSonUtils;
-
-import org.appwork.utils.StringUtils;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "opendrive.com" }, urls = { "https?://(?:www\\.)?opendrive\\.com/folders\\?[A-Za-z0-9]+|https?://od\\.lk/(?:fl|s)/[A-Za-z0-9]+(?:\\?folderpath=[a-zA-Z0-9_/\\+\\=\\-%]+)?" })
 public class OpenDriveComDecrypter extends PluginForDecrypt {
@@ -61,8 +61,8 @@ public class OpenDriveComDecrypter extends PluginForDecrypt {
         br.getHeaders().put("X-Ajax-CSRF-Token", csrftoken);
         br.postPage("https://od.lk/ajax", "action=files.load-folder-content&folder_id=" + folderid + "&with_breadcrumbs=1&last_request_time=0&public=1&offset=0&order_by=name&order_type=asc");
         final String error = PluginJSonUtils.getJson(br, "error");
-        if (br.getHttpConnection().getResponseCode() == 400 || br.getHttpConnection().getResponseCode() == 404 || error != null) {
-            final DownloadLink offline = this.createOfflinelink(parameter);
+        if (br.getHttpConnection().getResponseCode() == 400 || br.getHttpConnection().getResponseCode() == 404 || error != null || br.containsHTML("400")) {
+            final DownloadLink offline = createOfflinelink(parameter);
             offline.setFinalFileName("folder_offline_" + folderid);
             decryptedLinks.add(offline);
             return decryptedLinks;
