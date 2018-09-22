ff5419677c824168d01e01f3cc52a1b795fe80c0
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-09-22 17:49:33 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39847 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: dc7988111cf01482eadc92b02f21422e7368d238

--- src/jd/plugins/decrypter/WieMangaCom.java
+++ src/jd/plugins/decrypter/WieMangaCom.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 import org.appwork.utils.Regex;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
@@ -14,10 +15,9 @@ import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
 import jd.plugins.PluginException;
-import jd.plugins.PluginForDecrypt;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "wiemanga.com" }, urls = { "https?://(www\\.)?wiemanga\\.com/chapter/[^/]*(Kapitel|Prolog|Chapter)\\d+[^/]*/\\d+" })
-public class WieMangaCom extends PluginForDecrypt {
+public class WieMangaCom extends antiDDoSForDecrypt {
     public WieMangaCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -25,14 +25,14 @@ public class WieMangaCom extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        br.getPage(parameter.getCryptedUrl());
-        final String manga = br.getRegex("href\\s*=\\s*\"https?://(?:www.)?wiemanga\\.com/manga/.*?\\.html\".*?>\\s*(.*?)\\s*<").getMatch(0);
-        final String chapter = br.getRegex("href\\s*=\\s*\"https?://(?:www.)?wiemanga\\.com/chapter/.*?>[^<]*(?:Kapitel|Chapter) (\\d+)").getMatch(0);
+        getPage(br, parameter.getCryptedUrl());
+        final String manga = br.getRegex("(?i)href\\s*=\\s*\"https?://(?:www.)?wiemanga\\.com/manga/.*?\\.html\".*?>\\s*(.*?)\\s*<").getMatch(0);
+        final String chapter = br.getRegex("(?i)href\\s*=\\s*\"https?://(?:www.)?wiemanga\\.com/chapter/.*?>[^<]*(?:Kapitel|Chapter) (\\d+)").getMatch(0);
         final String title = manga + "-Kapitel_" + chapter;
         final FilePackage fp = FilePackage.getInstance();
         fp.setName(title);
         final String id = new Regex(parameter.getCryptedUrl(), "/(\\d+)$").getMatch(0);
-        final String sites[][] = br.getRegex("value\\s*=\\s*\"(https?://(?:www\\.)?wiemanga\\.com/chapter/[^/]*/" + id + "-\\d+.html)\"\\s*(selected)?\\s*>\\s*(.*?)\\s*<").getMatches();
+        final String sites[][] = br.getRegex("(?i)value\\s*=\\s*\"(https?://(?:www\\.)?wiemanga\\.com/chapter/[^/]*/" + id + "-\\d+.html)\"\\s*(selected)?\\s*>\\s*(.*?)\\s*<").getMatches();
         final HashSet<String> dups = new HashSet<String>();
         for (String site[] : sites) {
             if (isAbort()) {
@@ -42,9 +42,9 @@ public class WieMangaCom extends PluginForDecrypt {
                 continue;
             }
             if (!"selected".equals(site[1])) {
-                br.getPage(site[0]);
+                getPage(br, site[0]);
             }
-            final String image = br.getRegex("img\\s*id='comicpic'.*?src=\"(https?://[^'\"]*?\\.(?:jpe?g|png))\"").getMatch(0);
+            final String image = br.getRegex("(?i)img\\s*id='comicpic'.*?src=\"(https?://[^'\"]*?\\.(?:jpe?g|png))\"").getMatch(0);
             if (image == null) {
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
