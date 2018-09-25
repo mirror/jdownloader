eba44ad2c01e2cde3ed7514583387e3f13c1ad8d
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-09-25 07:56:49 +0000

    Pornhub.com - Handle /users/ fixes #85995
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39852 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2f44c5f5c4f718a884bc3ecf093ff0d6af20558c

--- src/jd/plugins/decrypter/PornHubCom.java
+++ src/jd/plugins/decrypter/PornHubCom.java
@@ -22,6 +22,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+
 import jd.PluginWrapper;
 import jd.config.SubConfiguration;
 import jd.controlling.AccountController;
@@ -38,10 +42,6 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
-
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "pornhub.com" }, urls = { "https?://(?:www\\.|[a-z]{2}\\.)?pornhub(?:premium)?\\.com/(?:.*\\?viewkey=[a-z0-9]+|embed/[a-z0-9]+|embed_player\\.php\\?id=\\d+|pornstar/[^/]+(?:/gifs(/public|/video|/from_videos)?|/videos(/upload)?)?|users/[^/]+(?:/gifs(/public|/video|/from_videos)?|/videos(/public)?)?|model/[^/]+(?:/gifs(/public|/video|/from_videos)?|/videos)?|playlist/\\d+)" })
 public class PornHubCom extends PluginForDecrypt {
@@ -128,6 +128,7 @@ public class PornHubCom extends PluginForDecrypt {
             return true;
         }
         FilePackage fp = null;
+        // TODO: better check for user/model/pornstar and handle all possible cases
         if (parameter.matches("(?i).*/pornstar/[^/]+/videos/upload")) {
             jd.plugins.hoster.PornHubCom.getPage(br, parameter);
             final String user = getUser(br);
@@ -149,12 +150,17 @@ public class PornHubCom extends PluginForDecrypt {
                 fp = FilePackage.getInstance();
                 fp.setName(user + " - Upload Videos");
             }
-        } else {
-            // TODO: better check for user/model/pornstar and handle all possible cases
-            jd.plugins.hoster.PornHubCom.getPage(br, parameter + "/videos");
+        } else if (parameter.matches("(?i).*/users/[^/]*")) {
+            jd.plugins.hoster.PornHubCom.getPage(br, new Regex(parameter, "(.*/users/[^/]*)").getMatch(0) + "/videos/public");
+            final String user = getUser(br);
+            // logger.info("br.getURL(): " + br.getURL()); // TODO: Handle /users/ that is switched to /model/
+            if (user != null) {
+                fp = FilePackage.getInstance();
+                fp.setName(user + "'s Public Videos");
+            }
         }
         if (br.containsHTML(">There are no videos...<")) {
-            // decryptedLinks.add(createOfflinelink(parameter));
+            decryptedLinks.add(createOfflinelink(parameter));
             return true;
         }
         // final String username = new Regex(parameter, "users/([^/]+)/").getMatch(0);
