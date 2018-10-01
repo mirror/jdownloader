6728aaa251afa0366cd8afbf2703c58ec240564a
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-10-01 15:20:58 +0000

    SexyHubCom: -updated login method AbstractChan: -refactored chan decrypter to extend AbstractChan
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@39889 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 11056b385007fb4ac1cb1028860a4256bfe48c47

---
+++ src/jd/plugins/decrypter/AbstractChan.java
@@ -0,0 +1,93 @@
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+import org.appwork.utils.Regex;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+public abstract class AbstractChan extends antiDDoSForDecrypt {
+    public AbstractChan(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final boolean singlePage;
+        if (parameter.getCryptedUrl().matches("(?i).+/res/\\d+\\.html$")) {
+            getPage(br, parameter.getCryptedUrl());
+            singlePage = true;
+        } else if (parameter.getCryptedUrl().matches("(?i).+/\\d+\\.html$")) {
+            getPage(br, parameter.getCryptedUrl().replaceAll("(?i)/\\d+\\.html$", "/index.html"));
+            singlePage = false;
+        } else {
+            getPage(br, parameter.getCryptedUrl());
+            singlePage = false;
+        }
+        final String title = br.getRegex("<header>\\s*<h\\d+>\\s*(.*?)</h").getMatch(0);
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(title);
+        final Set<String> pages = new HashSet<String>();
+        while (!isAbort()) {
+            final String files[][] = br.getRegex("<div class=\"(?:file|[^\"]*postContainer[^\"]*)\"[^>]*>(.*?)</div").getMatches();
+            if (files != null) {
+                for (String file[] : files) {
+                    final String href = new Regex(file[0], "href\\s*=\\s*\"([^\"]*/src/\\d+(-\\d+)?\\.[^\"]*)\"").getMatch(0);
+                    if (href == null) {
+                        if (file[0].matches("(?i).+post-image\\s*deleted.+")) {
+                            continue;
+                        } else if (file[0].matches("(?i).+jumpHandle.+")) {
+                            continue;
+                        } else {
+                            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                        }
+                    }
+                    String postfilename = new Regex(file[0], "\"postfilename\"\\s*>\\s*(.*?)\\s*<").getMatch(0);
+                    if (postfilename == null) {
+                        postfilename = new Regex(file[0], "class\\s*=\\s*\"post-filename\".*?download\\s*=\\s*\"(.*?)\"").getMatch(0);
+                    }
+                    final String size = new Regex(file[0], ">\\s*\\(\\s*([0-9\\.]+\\s*[KGTBM]{1,2})").getMatch(0);
+                    final DownloadLink downloadLink = createDownloadlink("directhttp://" + br.getURL(href).toString());
+                    final String uploadID = new Regex(href, ".+/(.*?)\\.[^/]*$").getMatch(0);
+                    if (size != null) {
+                        downloadLink.setDownloadSize(SizeFormatter.getSize(size));
+                    }
+                    if (postfilename != null) {
+                        downloadLink.setFinalFileName(uploadID + "_" + Encoding.htmlOnlyDecode(postfilename));
+                    }
+                    downloadLink.setAvailable(true);
+                    if (!singlePage) {
+                        downloadLink.setContainerUrl(br.getURL());
+                    }
+                    fp.add(downloadLink);
+                    distribute(downloadLink);
+                    ret.add(downloadLink);
+                }
+            }
+            if (!singlePage) {
+                final Form next = br.getFormBySubmitvalue("Next");
+                if (next != null && next.getAction() != null && pages.add(next.getAction())) {
+                    submitForm(next);
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        return ret;
+    }
+}
