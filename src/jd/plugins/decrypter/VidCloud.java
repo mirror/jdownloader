57b5cb7fec41f1f6a55daa7abfa1d616d0d9bf3e
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-10-24 00:54:47 +0000

    MoviesFun & VidCloud: -Added initial rudimentary support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40000 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/VidCloud.java
@@ -0,0 +1,58 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.appwork.utils.Regex;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision: 40000 $", interfaceVersion = 2, names = { "vidcloud.xyz" }, urls = { "https?://(play\\.)?vidcloud\\.xyz/(watch|stream).+" })
+public class VidCloud extends PluginForDecrypt {
+    public VidCloud(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        String page = br.getPage(parameter);
+        String[][] playlistEmbeds = br.getRegex("playcdn\\.vidcloud\\.xyz/stream/[^\"]+").getMatches();
+        for (String[] playlistEmbed : playlistEmbeds) {
+            final Browser brPlaylist = br.cloneBrowser();
+            brPlaylist.setFollowRedirects(true);
+            playlistEmbed[0] = "https://" + playlistEmbed[0];
+            decryptedLinks.add(createDownloadlink(Encoding.htmlOnlyDecode(playlistEmbed[0])));
+            brPlaylist.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+            brPlaylist.getHeaders().put("Referer", parameter);
+            String playlist = brPlaylist.getPage(playlistEmbed[0]);
+            String[][] playlistItems = new Regex(playlist, "https?://(?:[^\\.]+\\.)?vidcloud\\.xyz/[a-zA-Z0-9/\\.?=]+").getMatches();
+            for (String[] playlistItem : playlistItems) {
+                decryptedLinks.add(createDownloadlink(Encoding.htmlOnlyDecode(playlistItem[0])));
+            }
+        }
+        return decryptedLinks;
+    }
+}
