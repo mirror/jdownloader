a49831a5943c4852ccf8420f75f634b46d848095
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-10-28 12:33:11 +0000

    Cloudyfiles.me -> cfiles.net fixes #86234
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40018 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 783fb68d6a496fde396188a2c51daefe802d2104

--- src/jd/plugins/hoster/CloudyfilesCom.java
+++ src/jd/plugins/hoster/CloudyfilesCom.java
@@ -26,6 +26,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.http.Browser;
@@ -49,15 +57,7 @@ import jd.plugins.components.SiteType.SiteTemplate;
 import jd.plugins.components.UserAgents;
 import jd.utils.locale.JDL;
 
-import org.appwork.utils.formatter.SizeFormatter;
-import org.appwork.utils.formatter.TimeFormatter;
-import org.jdownloader.captcha.v2.challenge.keycaptcha.KeyCaptcha;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
-import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "cloudyfiles.me" }, urls = { "https?://(?:www\\.)?cloudyfiles\\.(?:com|co|org|me)/(?:embed\\-)?[a-z0-9]{12}" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "cloudyfiles.me" }, urls = { "https?://(?:www\\.)?c(?:loudy)?files\\.(?:com|co|org|me|net)/(?:embed\\-)?[a-z0-9]{12}" })
 public class CloudyfilesCom extends PluginForHost {
     /* Some HTML code to identify different (error) states */
     private static final String            HTML_PASSWORDPROTECTED             = "<br><b>Passwor(d|t):</b> <input";
@@ -65,11 +65,11 @@ public class CloudyfilesCom extends PluginForHost {
     /* Here comes our XFS-configuration */
     private final boolean                  SUPPORTS_HTTPS                     = false;
     /* primary website url, take note of redirects */
-    private final String                   primaryDomain                      = "cloudyfiles.me";
+    private final String                   primaryDomain                      = "cfiles.net";
     private final String                   COOKIE_HOST                        = (SUPPORTS_HTTPS ? "https://" : "http://") + primaryDomain;
     private final String                   NICE_HOSTproperty                  = primaryDomain.replaceAll("(\\.|\\-)", "");
     /* domain names used within download links */
-    private static final String            DOMAINS                            = "(cloudyfiles\\.(?:co|com|org|me))";
+    private static final String            DOMAINS                            = "(c(?:loudy)?files\\.(?:co|com|org|me|net))";
     /* Errormessages inside URLs */
     private static final String            URL_ERROR_PREMIUMONLY              = "/?op=login&redirect=";
     /* All kinds of XFS-plugin-configuration settings - be sure to configure this correctly when developing new XFS plugins! */
@@ -152,7 +152,7 @@ public class CloudyfilesCom extends PluginForHost {
 
     @Override
     public String[] siteSupportedNames() {
-        return new String[] { "cloudyfiles.co", "cloudyfiles.com", "cloudyfiles.org", "cloudyfiles.me" };
+        return new String[] { "cloudyfiles.co", "cloudyfiles.com", "cloudyfiles.org", "cloudyfiles.me", "cfiles.net" };
     }
 
     @Override
