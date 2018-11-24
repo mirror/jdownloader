7e0394ccba427fcd757e8cf38acc877e009bb1b8
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-11-24 22:11:52 +0000

    S.to -Adapted handling to the new structure and redirect procedure.
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40121 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/SerienStreamTo.java
+++ src/jd/plugins/decrypter/SerienStreamTo.java
@@ -17,16 +17,21 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.appwork.utils.Regex;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision: 39902 $", interfaceVersion = 3, names = { "s.to" }, urls = { "https?://(?:www\\.)?s\\.to/[^/]+/.*" })
+@DecrypterPlugin(revision = "$Revision: 40121 $", interfaceVersion = 3, names = { "s.to" }, urls = { "https?://(?:www\\.)?s\\.to/[^/]+/.*" })
 public class SerienStreamTo extends PluginForDecrypt {
     @SuppressWarnings("deprecation")
     public SerienStreamTo(final PluginWrapper wrapper) {
@@ -41,14 +46,36 @@ public class SerienStreamTo extends PluginForDecrypt {
         final String page = br.getPage(parameter);
         final String[][] titleDetail = br.getRegex("<meta property=\"og:title\" content=\"(Episode \\d+\\s|Staffel \\d+\\s|von+\\s)+([^\"]+)\"/>").getMatches();
         final String title = (titleDetail.length > 0) ? (titleDetail[0][titleDetail[0].length - 1]) : null;
-        String[][] episodeLinks = br.getRegex("<a itemprop=\"url\"[^>]+href=\"([^\"]+)\"[^>]*>").getMatches();
-        for (String[] episodeLink : episodeLinks) {
-            String episodeURL = "https://s.to" + episodeLink[0];
-            final Browser brLink = br.cloneBrowser();
-            brLink.setFollowRedirects(true);
-            brLink.getPage(episodeURL);
-            String targetURL = brLink.getURL();
-            decryptedLinks.add(createDownloadlink(targetURL));
+        final String itemTitle = new Regex(parameter, "https?://(?:www\\.)?s\\.to/[^/]+/[^/]+/(.*)").getMatch(0);
+        // If we're on a show site, add the seasons, if we're on a season page, add the episodes and so on ...
+        String[][] itemLinks = br.getRegex("href=\"([^\"]+" + Regex.escape(itemTitle) + "[^\"]+)\"").getMatches();
+        for (String[] itemLink : itemLinks) {
+            decryptedLinks.add(createDownloadlink(br.getURL(Encoding.htmlDecode(itemLink[0])).toString()));
+        }
+        // Videos are on external sites (not in embeds), so harvest those if we can get our hands on them.
+        String[][] videoLinks = br.getRegex("itemprop=\"url\" href=\"([^\"]+redirect[^\"]+)\" target=\"_blank\"").getMatches();
+        for (String[] videoLink : videoLinks) {
+            final Browser br2 = br.cloneBrowser();
+            String videoURL = br.getURL(Encoding.htmlDecode(videoLink[0])).toString();
+            br2.setFollowRedirects(true);
+            String redirectPage = br2.getPage(videoURL);
+            if (br2.getRedirectLocation() != null) {
+                videoURL = br2.getRedirectLocation();
+            } else if (br2.containsHTML("grecaptcha")) {
+                Form captcha = br2.getForm(0);
+                String sitekey = new Regex(redirectPage, "grecaptcha.execute\\('([^']+)'").getMatch(0);
+                String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br2, sitekey) {
+                    @Override
+                    public TYPE getType() {
+                        return TYPE.INVISIBLE;
+                    }
+                }.getToken();
+                captcha.put("original", "");
+                captcha.put("token", Encoding.urlEncode(recaptchaV2Response));
+                redirectPage = br2.submitForm(captcha);
+                videoURL = br2.getURL().toString();
+            }
+            decryptedLinks.add(createDownloadlink(videoURL));
         }
         if (title != null) {
             final FilePackage filePackage = FilePackage.getInstance();
