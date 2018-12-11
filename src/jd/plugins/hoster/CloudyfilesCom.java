407f3bba6983c6b213f58d33abc7731820e47702
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2018-12-11 07:02:17 +0000

    Cloudyfiles.com https://board.jdownloader.org/showthread.php?t=77853
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40163 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f996c229c3c3facdeb183429cb52c7fd45949c79

--- src/jd/plugins/hoster/CloudyfilesCom.java
+++ src/jd/plugins/hoster/CloudyfilesCom.java
@@ -57,7 +57,7 @@ import jd.plugins.components.SiteType.SiteTemplate;
 import jd.plugins.components.UserAgents;
 import jd.utils.locale.JDL;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "cloudyfiles.me" }, urls = { "https?://(?:www\\.)?(?:c(?:loudy)?files\\.(?:com|co|org|me|net)|businessnewslive2018.online)/(?:embed\\-)?[a-z0-9]{12}" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "cloudyfiles.me" }, urls = { "https?://(?:www\\.)?(?:c(?:loudy)?files\\.(?:com|co|org|me|net)|businessnews(?:live2018|current).online)/(?:embed\\-)?[a-z0-9]{12}" })
 public class CloudyfilesCom extends PluginForHost {
     /* Some HTML code to identify different (error) states */
     private static final String            HTML_PASSWORDPROTECTED             = "<br><b>Passwor(d|t):</b> <input";
@@ -65,11 +65,11 @@ public class CloudyfilesCom extends PluginForHost {
     /* Here comes our XFS-configuration */
     private final boolean                  SUPPORTS_HTTPS                     = false;
     /* primary website url, take note of redirects */
-    private final String                   primaryDomain                      = "businessnewslive2018.online";
+    private final String                   primaryDomain                      = "businessnewscurrent.online";
     private final String                   COOKIE_HOST                        = (SUPPORTS_HTTPS ? "https://" : "http://") + primaryDomain;
     private final String                   NICE_HOSTproperty                  = primaryDomain.replaceAll("(\\.|\\-)", "");
     /* domain names used within download links */
-    private static final String            DOMAINS                            = "(c(?:loudy)?files\\.(?:co|com|org|me|net)|businessnewslive2018.online)";
+    private static final String            DOMAINS                            = "(c(?:loudy)?files\\.(?:co|com|org|me|net)|businessnewscurrent.online)";
     /* Errormessages inside URLs */
     private static final String            URL_ERROR_PREMIUMONLY              = "/?op=login&redirect=";
     /* All kinds of XFS-plugin-configuration settings - be sure to configure this correctly when developing new XFS plugins! */
@@ -152,7 +152,7 @@ public class CloudyfilesCom extends PluginForHost {
 
     @Override
     public String[] siteSupportedNames() {
-        return new String[] { "cloudyfiles.co", "cloudyfiles.com", "cloudyfiles.org", "cloudyfiles.me", "cfiles.net", "businessnewslive2018.online" };
+        return new String[] { "cloudyfiles.co", "cloudyfiles.com", "cloudyfiles.org", "cloudyfiles.me", "cfiles.net", "businessnewslive2018.online", "businessnewscurrent.online" };
     }
 
     @Override
@@ -349,7 +349,7 @@ public class CloudyfilesCom extends PluginForHost {
                             }
                             // generic failover#2
                             if (inValidate(fileInfo[1])) {
-                                fileInfo[1] = new Regex(correctedBR, "(\\d+(?:\\.\\d+)? ?(?:B(?:ytes?)?))").getMatch(0);
+                                // fileInfo[1] = new Regex(correctedBR, "(\\d+(?:\\.\\d+)? ?(?:B(?:ytes?)?))").getMatch(0);
                             }
                         }
                     }
@@ -857,6 +857,9 @@ public class CloudyfilesCom extends PluginForHost {
             }
             if (inValidate(dllink)) {
                 dllink = new Regex(correctedBR, "file:[\t\n\r ]*?\"(http[^<>\"]*?\\.(?:mp4|flv))\"").getMatch(0);
+            }
+            if (inValidate(dllink)) {
+                dllink = new Regex(correctedBR, "direct_link[^<>]*?>\\s*<a href=\"(http[^<>\"]*?\\.(?:mp4|mkv))\"").getMatch(0);
             }
         }
         if (dllink == null && IMAGEHOSTER) {
