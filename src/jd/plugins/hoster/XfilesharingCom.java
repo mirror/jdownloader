f591e3465ab071aa4fd736c170113c7823d9d106
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-05-22 16:36:14 +0000

    *General* XFileSharingProBasic: - improved login - added experimental code for app-login
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [XFS] xfilesharingcom: added support for official XFS DEMO website
    - [XFS] xvideosharingcom: minor changes
    
    RIP
    - [XFS] xfilesharing.us
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40669 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2f07de1450dbfafc9de3550dc5cf1457ae7d021e

--- src/jd/plugins/hoster/XfilesharingCom.java
+++ src/jd/plugins/hoster/XfilesharingCom.java
@@ -28,52 +28,32 @@ import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
-public class XvideosharingCom extends XFileSharingProBasic {
-    public XvideosharingCom(final PluginWrapper wrapper) {
+public class XfilesharingCom extends XFileSharingProBasic {
+    public XfilesharingCom(final PluginWrapper wrapper) {
         super(wrapper);
-        // this.enablePremium(super.getPremiumLink());
+        this.enablePremium(super.getPurchasePremiumURL());
     }
 
     /**
      * DEV NOTES XfileSharingProBasic Version SEE SUPER-CLASS<br />
      * mods: See overridden functions<br />
      * limit-info:<br />
-     * captchatype-info: 2019-02-08: null<br />
-     * other:<br />
+     * captchatype-info: null<br />
+     * other: This is an official XFS DEMO website! Testlogindata: admin:admin <br />
      */
-    private static String[] domains = new String[] { "xvideosharing.com" };
-
-    @Override
-    public boolean supports_availablecheck_alt() {
-        return false;
-    }
-
-    @Override
-    public boolean supports_https() {
-        return false;
-    }
-
-    @Override
-    public boolean supports_availablecheck_filesize_html() {
-        return false;
-    }
-
-    @Override
-    public boolean isVideohoster_enforce_video_filename() {
-        return true;
-    }
+    private static String[] domains = new String[] { "xfilesharing.com" };
 
     @Override
     public boolean isResumeable(final DownloadLink link, final Account account) {
         if (account != null && account.getType() == AccountType.FREE) {
             /* Free Account */
-            return true;
+            return false;
         } else if (account != null && account.getType() == AccountType.PREMIUM) {
             /* Premium account */
-            return true;
+            return false;
         } else {
             /* Free(anonymous) and unknown account type */
-            return true;
+            return false;
         }
     }
 
@@ -81,13 +61,13 @@ public class XvideosharingCom extends XFileSharingProBasic {
     public int getMaxChunks(final Account account) {
         if (account != null && account.getType() == AccountType.FREE) {
             /* Free Account */
-            return -2;
+            return 1;
         } else if (account != null && account.getType() == AccountType.PREMIUM) {
             /* Premium account */
-            return -2;
+            return 1;
         } else {
             /* Free(anonymous) and unknown account type */
-            return -2;
+            return 1;
         }
     }
 
@@ -107,18 +87,43 @@ public class XvideosharingCom extends XFileSharingProBasic {
     }
 
     @Override
-    public boolean supports_availablecheck_filesize_via_embedded_video() {
-        return super.supports_availablecheck_filesize_via_embedded_video();
+    public boolean supports_https() {
+        return super.supports_https();
     }
 
-    /** 2019-02-08: Special */
     @Override
-    public String getDllink(DownloadLink link, Account account) {
-        String dllink = super.getDllink(link, account);
-        // if (StringUtils.isEmpty(dllink)) {
-        // dllink = new Regex(super.correctedBR, "\"(https?://[^\"]+v\\.mp4)\"").getMatch(0);
-        // }
-        return dllink;
+    public boolean supports_precise_expire_date() {
+        return super.supports_precise_expire_date();
+    }
+
+    @Override
+    public boolean isVideohosterEmbed() {
+        return super.isVideohosterEmbed();
+    }
+
+    @Override
+    public boolean isVideohoster_enforce_video_filename() {
+        return super.isVideohoster_enforce_video_filename();
+    }
+
+    @Override
+    public boolean supports_availablecheck_alt() {
+        return super.supports_availablecheck_alt();
+    }
+
+    @Override
+    public boolean supports_availablecheck_filename_abuse() {
+        return super.supports_availablecheck_filename_abuse();
+    }
+
+    @Override
+    public boolean supports_availablecheck_filesize_html() {
+        return super.supports_availablecheck_filesize_html();
+    }
+
+    @Override
+    public boolean supports_availablecheck_filesize_via_embedded_video() {
+        return super.supports_availablecheck_filesize_via_embedded_video();
     }
 
     public static String[] getAnnotationNames() {
