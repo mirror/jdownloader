c10b164b4628bde8dc4790f283ff4e90b23eab66
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-06-04 18:08:35 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40745 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/AccountController.java
+++ src/jd/controlling/AccountController.java
@@ -78,11 +78,11 @@ public class AccountController implements AccountControllerListener, AccountProp
     private final HashMap<String, List<Account>>                                 MULTIHOSTER_ACCOUNTS;
     private static AccountController                                             INSTANCE         = new AccountController();
     private final Eventsender<AccountControllerListener, AccountControllerEvent> broadcaster      = new Eventsender<AccountControllerListener, AccountControllerEvent>() {
-                                                                                                      @Override
-                                                                                                      protected void fireEvent(final AccountControllerListener listener, final AccountControllerEvent event) {
-                                                                                                          listener.onAccountControllerEvent(event);
-                                                                                                      }
-                                                                                                  };
+        @Override
+        protected void fireEvent(final AccountControllerListener listener, final AccountControllerEvent event) {
+            listener.onAccountControllerEvent(event);
+        }
+    };
 
     public Eventsender<AccountControllerListener, AccountControllerEvent> getEventSender() {
         return broadcaster;
