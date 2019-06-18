191acda16a293a5c5535bf6a0ef4b73c3a21da97
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-06-18 16:20:24 +0000

    fixes #86268 fixes #84070 build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40778 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1f34431a1a075ff4ec3bd951b51dd6ba61c75848

---
+++ src/jd/plugins/decrypter/MagentaMusik360.java
@@ -0,0 +1,81 @@
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.config.MagentaMusik360Config;
+import org.jdownloader.plugins.config.PluginJsonConfig;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
+@DecrypterPlugin(revision = "$Revision: 36558 $", interfaceVersion = 3, names = { "magenta-musik-360.de" }, urls = { "https?://(www\\.)?magenta-musik-360\\.de/[a-zA-Z0-9\\-]+-\\d+" })
+public class MagentaMusik360 extends PluginForDecrypt {
+    public MagentaMusik360(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public Class<? extends MagentaMusik360Config> getConfigInterface() {
+        return MagentaMusik360Config.class;
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        br.setFollowRedirects(true);
+        br.getPage(parameter.getCryptedUrl());
+        final String itemId = new Regex(parameter.getCryptedUrl(), "-(\\d+)$").getMatch(0);
+        final String playerVersion = "58935";
+        br.getPage("https://wcss.t-online.de/cvss/magentamusic/vodplayer/v3/player/" + playerVersion + "/" + itemId + "/Main%20Movie?referrer=https%3A%2F%2Fwcss.t-online.de%2Fcvss%2Fmagentamusic%2Fvodplayer%2Fv3%2Fdetails%2F" + playerVersion + "%2F" + itemId + "%3F%24whiteLabelId%3DMM3&%24whiteLabelId=MM3&_c=aHR0cHM6d3d3Lm1hZ2VudGEtbXVzaWstMzYwLmRl");
+        final Map<String, Object> map = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+        final Map<String, Object> content = (Map<String, Object>) map.get("content");
+        final Map<String, Object> feature = (Map<String, Object>) content.get("feature");
+        final Map<String, Object> metadata = (Map<String, Object>) feature.get("metadata");
+        final String title = (String) metadata.get("title");
+        final String originalTitle = (String) metadata.get("originalTitle");
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final List<Map<String, Object>> representations = (List<Map<String, Object>>) feature.get("representations");
+        for (final Map<String, Object> representation : representations) {
+            final String type = (String) representation.get("type");
+            if (StringUtils.equalsIgnoreCase("HlsStreaming", type)) {
+                final String quality = (String) representation.get("quality");
+                if (StringUtils.equalsIgnoreCase("VR", quality) && !PluginJsonConfig.get(MagentaMusik360Config.class).isCrawlVR()) {
+                    continue;
+                }
+                final String href = (String) JavaScriptEngineFactory.walkJson(representation, "contentPackages/{0}/media/href");
+                if (href == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                final Browser br2 = br.cloneBrowser();
+                br2.getPage(href);
+                final String src = br2.getRegex("src\\s*=\\s*\"(https?://.*?m3u8(\\?yospace=true)?)\"").getMatch(0);
+                if (src == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                br2.getPage(src);
+                final ArrayList<DownloadLink> found = GenericM3u8Decrypter.parseM3U8(this, href, br2, null, null, null, title + "-" + originalTitle + "_" + quality);
+                ret.addAll(found);
+            }
+        }
+        if (ret.size() > 0) {
+            FilePackage fp = FilePackage.getInstance();
+            fp.setName(title + "-" + originalTitle);
+            fp.addLinks(ret);
+        }
+        return ret;
+    }
+}
