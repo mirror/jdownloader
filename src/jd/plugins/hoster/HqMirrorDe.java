b57c34cccabd3490946544670d4cd0ef06ef98f0
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-06-27 13:42:22 +0000

    *Plugins: Fixes/Changes/Maintenance* - hqmirrorde: added crawler, fixed host plugin, added account support fixes #87154
    *Plugins: New plugins or new websites that belong to existing scripts*
    - nowoscimuzyczneeu & krakenfilescom closes #87162
    
    RIP
    - [XFS] axfiles.net fixes #87135
    - [XFS] cloudupload.co fixes #87136
    - ezfiles.net fixes #87137
    - [YetiShare] stream.moe fixes #87139
    - [XFS] top4upload.com fixes #87140
    - [XFS] upload2win.com fixes #87141
    - [XFS] 1cloud2.net fixes #87142
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40814 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: fb6763135e1c2bd0141eb92a222b115ca6a501f4

--- src/jd/plugins/hoster/HqMirrorDe.java
+++ src/jd/plugins/hoster/HqMirrorDe.java
@@ -13,13 +13,24 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.hoster;
 
 import java.io.IOException;
+import java.util.Locale;
+
+import org.appwork.utils.formatter.TimeFormatter;
 
 import jd.PluginWrapper;
+import jd.config.Property;
+import jd.http.Browser;
+import jd.http.Cookies;
+import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.AccountInfo;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -27,13 +38,11 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-import org.appwork.utils.formatter.SizeFormatter;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hq-mirror.de" }, urls = { "http://(www\\.)?hq\\-mirror\\.de/film/\\d+" }) 
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hq-mirror.de" }, urls = { "https?://(?:www\\.)?hq\\-mirror\\.de/tvprogram/show/(\\d+)#collapse\\-link\\-copy\\-(\\d+)" })
 public class HqMirrorDe extends PluginForHost {
-
     public HqMirrorDe(PluginWrapper wrapper) {
         super(wrapper);
+        this.enablePremium("https://www.hq-mirror.de/payment/prepare");
     }
 
     @Override
@@ -41,43 +50,183 @@ public class HqMirrorDe extends PluginForHost {
         return "http://www.hq-mirror.de/index/impressum";
     }
 
-    @SuppressWarnings("deprecation")
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String linkid = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1);
+        if (linkid != null) {
+            return linkid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    /* Connection stuff */
+    private final boolean FREE_RESUME                  = true;
+    private final int     FREE_MAXCHUNKS               = -2;
+    /* Only 1 according to website but I was not able to verify this! */
+    private final int     FREE_MAXDOWNLOADS            = 3;
+    private final boolean ACCOUNT_FREE_RESUME          = true;
+    private final int     ACCOUNT_FREE_MAXCHUNKS       = -2;
+    private final int     ACCOUNT_FREE_MAXDOWNLOADS    = 3;
+    private final boolean ACCOUNT_PREMIUM_RESUME       = true;
+    private final int     ACCOUNT_PREMIUM_MAXCHUNKS    = -2;
+    /* Unlimited according to website but I was not able to verify this! */
+    private final int     ACCOUNT_PREMIUM_MAXDOWNLOADS = 3;
+
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        br.getPage(link.getDownloadURL());
-        if (br.containsHTML("class=\"alert alert-danger\"|Sendung nicht gefunden") || br.getHttpConnection().getResponseCode() == 404) {
+        br.getPage(link.getPluginPatternMatcher());
+        if (jd.plugins.decrypter.HqMirrorDe.isOffline(this.br)) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        String filename = br.getRegex("title\" content=\"([^<>\"]*?)\"").getMatch(0);
-        final String filesize = br.getRegex("<b>HQ</b> \\(([^<>\"]*?)\\)").getMatch(0);
-        if (filename == null || filesize == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        filename = Encoding.htmlDecode(filename).trim();
-        if (!filename.endsWith(".otrkey")) {
-            filename += ".otrkey";
-        }
-        link.setName(filename);
-        link.setDownloadSize(SizeFormatter.getSize(filesize));
         return AvailableStatus.TRUE;
     }
 
     @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
-        requestFileInformation(downloadLink);
-        String dllink = br.getRegex("\"(/file/download/key/[^<>\"]*?)\"").getMatch(0);
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        doFree(link, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink link, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        String dllink = checkDirectLink(link, directlinkproperty);
         if (dllink == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            requestFileInformation(link);
+            final String linkid = getLinkID(link);
+            final String[] htmls = jd.plugins.decrypter.HqMirrorDe.getHtmls(this.br);
+            String targetHTML = null;
+            for (final String html : htmls) {
+                if (html.contains(linkid)) {
+                    targetHTML = html;
+                    break;
+                }
+            }
+            /* For free accounts/without account, it will be '/download/guest/bla' */
+            dllink = new Regex(targetHTML, "(/download/[^\"]+)").getMatch(0);
+            if (dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dllink = "https://www.hq-mirror.de" + Encoding.htmlDecode(dllink.trim());
         }
-        dllink = "http://www.hq-mirror.de" + Encoding.htmlDecode(dllink.trim());
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resumable, maxchunks);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Unknown server error");
         }
+        link.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
         dl.startDownload();
+    }
+
+    private String checkDirectLink(final DownloadLink downloadLink, final String property) {
+        String dllink = downloadLink.getStringProperty(property);
+        if (dllink != null) {
+            URLConnectionAdapter con = null;
+            try {
+                final Browser br2 = br.cloneBrowser();
+                con = br2.openHeadConnection(dllink);
+                if (con.getContentType().contains("html") || con.getLongContentLength() == -1) {
+                    downloadLink.setProperty(property, Property.NULL);
+                    dllink = null;
+                }
+            } catch (final Exception e) {
+                downloadLink.setProperty(property, Property.NULL);
+                dllink = null;
+            } finally {
+                try {
+                    con.disconnect();
+                } catch (final Throwable e) {
+                }
+            }
+        }
+        return dllink;
+    }
+
+    private static Object LOCK = new Object();
+
+    private void login(final Account account, final boolean force) throws Exception {
+        synchronized (LOCK) {
+            try {
+                br.setFollowRedirects(true);
+                br.setCookiesExclusive(true);
+                final Cookies cookies = account.loadCookies("");
+                if (cookies != null && !force) {
+                    this.br.setCookies(this.getHost(), cookies);
+                    br.getPage("https://www." + account.getHoster() + "/");
+                    if (this.isLoggedin()) {
+                        return;
+                    }
+                }
+                br.getPage("https://www." + account.getHoster() + "/auth/login");
+                final Form loginform = br.getFormbyActionRegex(".*?auth/login.*?");
+                if (loginform == null) {
+                }
+                loginform.put("email", Encoding.urlEncode(account.getUser()));
+                loginform.put("password", Encoding.urlEncode(account.getPass()));
+                loginform.put("remember", "on");
+                br.submitForm(loginform);
+                if (!isLoggedin()) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                account.saveCookies(this.br.getCookies(this.getHost()), "");
+            } catch (final PluginException e) {
+                account.clearCookies("");
+                throw e;
+            }
+        }
+    }
+
+    private boolean isLoggedin() {
+        return br.containsHTML("/auth/logout");
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        final AccountInfo ai = new AccountInfo();
+        try {
+            login(account, true);
+        } catch (final PluginException e) {
+            throw e;
+        }
+        ai.setUnlimitedTraffic();
+        final String expire = br.getRegex("Premium Status: gültig bis (\\d{2}\\.\\d{2}\\.\\d{4})").getMatch(0);
+        if (expire == null) {
+            account.setType(AccountType.FREE);
+            /* free accounts can still have captcha */
+            account.setMaxSimultanDownloads(ACCOUNT_FREE_MAXDOWNLOADS);
+            account.setConcurrentUsePossible(false);
+            ai.setStatus("Registered (free) user");
+        } else {
+            ai.setValidUntil(TimeFormatter.getMilliSeconds(expire, "dd.MM.yyyy", Locale.GERMANY));
+            account.setType(AccountType.PREMIUM);
+            account.setMaxSimultanDownloads(ACCOUNT_PREMIUM_MAXDOWNLOADS);
+            account.setConcurrentUsePossible(true);
+            ai.setStatus("Premium account");
+        }
+        ai.setUnlimitedTraffic();
+        return ai;
+    }
+
+    @Override
+    public void handlePremium(final DownloadLink link, final Account account) throws Exception {
+        login(account, false);
+        requestFileInformation(link);
+        if (account.getType() == AccountType.FREE) {
+            doFree(link, ACCOUNT_FREE_RESUME, ACCOUNT_FREE_MAXCHUNKS, "account_free_directlink");
+        } else {
+            doFree(link, ACCOUNT_PREMIUM_RESUME, ACCOUNT_PREMIUM_MAXCHUNKS, "premium_directlink");
+        }
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return ACCOUNT_FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        /* 2019-06-27: There are no captchas at all! */
+        return false;
     }
 
     @Override
@@ -86,11 +235,10 @@ public class HqMirrorDe extends PluginForHost {
 
     @Override
     public int getMaxSimultanFreeDownloadNum() {
-        return -1;
+        return FREE_MAXDOWNLOADS;
     }
 
     @Override
     public void resetDownloadlink(final DownloadLink link) {
     }
-
 }
---
+++ src/jd/plugins/hoster/KrakenfilesCom.java
@@ -0,0 +1,185 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+
+import jd.PluginWrapper;
+import jd.config.Property;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.Account.AccountType;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.components.PluginJSonUtils;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "krakenfiles.com" }, urls = { "https?://(?:www\\.)?krakenfiles\\.com/view/([a-z0-9]+)/file\\.html" })
+public class KrakenfilesCom extends PluginForHost {
+    public KrakenfilesCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://krakenfiles.com/";
+    }
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String linkid = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+        if (linkid != null) {
+            return linkid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME       = true;
+    private static final int     FREE_MAXCHUNKS    = 0;
+    private static final int     FREE_MAXDOWNLOADS = 20;
+
+    // private static final boolean ACCOUNT_FREE_RESUME = true;
+    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
+    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
+    //
+    // /* don't touch the following! */
+    // private static AtomicInteger maxPrem = new AtomicInteger(1);
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        link.setMimeHint(CompiledFiletypeFilter.AudioExtensions.MP3);
+        link.setName(this.getLinkID(link));
+        this.setBrowserExclusive();
+        /* This json is part of their embed functions :) */
+        br.getPage("https://" + this.getHost() + "/json/" + this.getLinkID(link));
+        final String hash = PluginJSonUtils.getJson(br, "hash");
+        if (br.getHttpConnection().getResponseCode() == 404 || StringUtils.isEmpty(hash) || !StringUtils.equalsIgnoreCase(hash, this.getLinkID(link))) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        String filename = PluginJSonUtils.getJson(br, "title");
+        if (StringUtils.isEmpty(filename)) {
+            filename = this.getLinkID(link);
+        }
+        String filesize = PluginJSonUtils.getJson(br, "size");
+        filename = Encoding.htmlDecode(filename).trim();
+        link.setName(filename);
+        if (filesize != null) {
+            link.setDownloadSize(SizeFormatter.getSize(filesize));
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
+        requestFileInformation(downloadLink);
+        doFree(downloadLink, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink link, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        String dllink = checkDirectLink(link, directlinkproperty);
+        if (dllink == null) {
+            br.getPage(link.getPluginPatternMatcher());
+            final Form dlform = br.getFormbyActionRegex(".*?download/.*?");
+            if (dlform == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            br.getHeaders().put("hash", this.getLinkID(link));
+            br.submitForm(dlform);
+            dllink = PluginJSonUtils.getJson(br, "url");
+            if (StringUtils.isEmpty(dllink) && dllink.startsWith("http")) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resumable, maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            }
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        link.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
+        dl.startDownload();
+    }
+
+    private String checkDirectLink(final DownloadLink downloadLink, final String property) {
+        String dllink = downloadLink.getStringProperty(property);
+        if (dllink != null) {
+            URLConnectionAdapter con = null;
+            try {
+                final Browser br2 = br.cloneBrowser();
+                br2.setFollowRedirects(true);
+                con = br2.openHeadConnection(dllink);
+                if (con.getContentType().contains("html") || con.getLongContentLength() == -1) {
+                    downloadLink.setProperty(property, Property.NULL);
+                    dllink = null;
+                }
+            } catch (final Exception e) {
+                downloadLink.setProperty(property, Property.NULL);
+                dllink = null;
+            } finally {
+                try {
+                    con.disconnect();
+                } catch (final Throwable e) {
+                }
+            }
+        }
+        return dllink;
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        if (acc == null) {
+            /* no account, yes we can expect captcha */
+            return true;
+        }
+        if (acc.getType() == AccountType.FREE) {
+            /* Free accounts can have captchas */
+            return true;
+        }
+        /* Premium accounts do not have captchas */
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
