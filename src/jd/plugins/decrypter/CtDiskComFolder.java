e0bf44f655f5972d9c4cfaad53a0fb6db2cd6b95
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-07-05 11:19:41 +0000

    *Plugins: Fixes/Changes/Maintenance* - CtDiskCom: fixed availablecheck and free mode fixes #61568 - CtDiskComFolder: fixed and dropped support for old URLs for now as I was unable to find any working old URLs fixes #78449 fixes #86580
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40865 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/CtDiskComFolder.java
+++ src/jd/plugins/decrypter/CtDiskComFolder.java
@@ -13,41 +13,74 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Random;
+import java.util.regex.Pattern;
+
+import org.appwork.utils.StringUtils;
 import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
-import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
-import jd.plugins.LinkStatus;
-import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
+import jd.plugins.components.PluginJSonUtils;
 import jd.utils.JDUtilities;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "ctdisk.com" }, urls = { "https?://(www\\.)?((ctdisk|400gb|pipipan|t00y)\\.com|bego\\.cc)/u/\\d{6,7}(/\\d{6,7})?" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class CtDiskComFolder extends PluginForDecrypt {
-
     // DEV NOTES
     // protocol: no https.
-    // t00y doesn't seem to work as alias but ill add it anyway.
-
-    private static final String domains = "((ctdisk|400gb|pipipan|t00y)\\.com|bego\\.cc)";
     // user unique id
-    private String              uuid    = null;
+    private String        uuid  = null;
     // folder unique id
-    private String              fuid    = null;
-    private static String       agent   = null;
-    private static Object       LOCK    = new Object();
+    private String        fuid  = null;
+    private static String agent = null;
+    private static Object LOCK  = new Object();
+
+    public static String[] getAnnotationNames() {
+        return jd.plugins.hoster.CtDiskCom.domains;
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return jd.plugins.hoster.CtDiskCom.domains;
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (int i = 0; i < jd.plugins.hoster.CtDiskCom.domains.length; i++) {
+            if (i == 0) {
+                /* Match all URLs on first (=current) domain */
+                ret.add("https?://[A-Za-z0-9]+\\." + getHostsPatternPart() + "/dir/.+");
+            } else {
+                ret.add("");
+            }
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /** Returns '(?:domain1|domain2)' */
+    public static String getHostsPatternPart() {
+        final StringBuilder pattern = new StringBuilder();
+        pattern.append("(?:");
+        for (final String name : jd.plugins.hoster.CtDiskCom.domains) {
+            pattern.append((pattern.length() > 0 ? "|" : "") + Pattern.quote(name));
+        }
+        pattern.append(")");
+        return pattern.toString();
+    }
 
     public CtDiskComFolder(PluginWrapper wrapper) {
         super(wrapper);
@@ -69,35 +102,38 @@ public class CtDiskComFolder extends PluginForDecrypt {
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString().replace("https://", "http://").replaceAll(domains + "/", "bego.cc/");
+        final String host_current = Browser.getHost(param.getCryptedUrl());
+        final String host_new = jd.plugins.hoster.CtDiskCom.correctHost(host_current);
+        String parameter = param.toString().replace(host_current + "/", host_new + "/");
         prepBrowser(br);
         // lock to one thread!
         synchronized (LOCK) {
             getPage(br, parameter);
-            uuid = new Regex(parameter, "/u/(\\d+)").getMatch(0);
-            fuid = new Regex(parameter, "/u/\\d+/(\\d+)").getMatch(0);
+            final boolean accessDenied = br.containsHTML("主页分享功能已经关闭，请直接分享文件或文件夹");
+            if (br.getHttpConnection().getResponseCode() == 404 || accessDenied || br.containsHTML("(Due to the limitaion of local laws, this url has been disabled!<|该用户还未打开完全共享\\。|您目前无法访问他的资源列表\\。)")) {
+                decryptedLinks.add(this.createOfflinelink(parameter));
+                return decryptedLinks;
+            }
+            uuid = jd.plugins.hoster.CtDiskCom.getUserID(parameter);
+            if (uuid == null) {
+                logger.warning("Failed to find userid");
+                return null;
+            }
             if (fuid == null) {
                 fuid = "0";
             }
-            if (br.containsHTML("(Due to the limitaion of local laws, this url has been disabled!<|该用户还未打开完全共享\\。|您目前无法访问他的资源列表\\。)")) {
-                logger.info("Invalid URL: " + parameter);
-                return decryptedLinks;
-            }
-
-            String fpName = null;
-            if (!"0".equals(fuid)) {
-                // covers sub directories. /u/uuid/fuid/
-                fpName = br.getRegex("href=\"/u/" + uuid + "/" + fuid + "\">(.*?)</a>").getMatch(0);
-                // fail over
-                if (fpName == null && uuid != null) {
-                    fpName = "User " + uuid + " - Sub Directory " + fuid;
-                }
-            }
+            String fpName = uuid;
+            // if (!"0".equals(fuid)) {
+            // // covers sub directories. /u/uuid/fuid/
+            // fpName = br.getRegex("href=\"/u/" + uuid + "/" + fuid + "\">(.*?)</a>").getMatch(0);
+            // // fail over
+            // if (fpName == null && uuid != null) {
+            // fpName = "User " + uuid + " - Sub Directory " + fuid;
+            // }
+            // }
             // covers base /u/\d+ directories,
             // no fpName for these as results of base directory returns subdirectories.
-
             parsePage(decryptedLinks, parameter);
-
             if (fpName != null) {
                 FilePackage fp = FilePackage.getInstance();
                 fp.setName(fpName.trim());
@@ -116,50 +152,64 @@ public class CtDiskComFolder extends PluginForDecrypt {
 
     private void parsePage(ArrayList<DownloadLink> ret, String parameter) throws Exception {
         // "/iajax_guest.php?item=file_act&action=file_list&folder_id=0&uid=1942919&task=file_list&t=1420817115&k=40d90e63574e9dce0af62dfb94aafdf7"
-        String ajaxSource = br.getRegex("\"sAjaxSource\": \"(/iajax_guest\\.php\\?item=file_act&action=file_list&folder_id=" + fuid + "&uid=" + uuid + "&task=file_list&t=\\d+&k=[a-f0-9]{32})\"").getMatch(0);
-        if (ajaxSource == null) {
+        String ajaxSource = PluginJSonUtils.getJson(br, "sAjaxSource");
+        if (StringUtils.isEmpty(ajaxSource)) {
             logger.warning("Can not find 'ajax source' : " + parameter);
             return;
         }
         Browser ajax = br.cloneBrowser();
         prepAjax(ajax);
         getPage(ajax, ajaxSource);
-        ajax.getHttpConnection().getRequest().setHtmlCode(ajax.toString().replaceAll("\\\\/", "/").replaceAll("\\\\\"", "\""));
-        final String[][] results = ajax.getRegex("(href=\"([^\"]+/file/\\d+)\">(.*?)</a>(\\\\t){1,}\",\"([\\d\\.]+ [KMGT]?B)\")").getMatches();
-        // export folders back into decrypter again.
-        final String[] folders = ajax.getRegex("<a href=\"(/u/" + uuid + "/\\d+)\">").getColumn(0);
-        if ((folders == null || folders.length == 0) && (results == null || results.length == 0)) {
-            if (ajax.containsHTML("\"iTotalRecords\":\"0\"")) {
-                logger.info("Link offline (empty): " + parameter);
-                return;
-            }
-            ret = null;
+        // ajax.getHttpConnection().getRequest().setHtmlCode(ajax.toString().replaceAll("\\\\/", "/").replaceAll("\\\\\"", "\""));
+        final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(ajax.toString());
+        final long totalCount = JavaScriptEngineFactory.toLong(entries.get("iTotalRecords"), 0);
+        if (totalCount == 0) {
+            ret.add(this.createOfflinelink(parameter));
             return;
         }
-        if (results != null && results.length != 0) {
-            for (String[] args : results) {
-                DownloadLink dl = createDownloadlink(args[1]);
-                if (args[1] != null) {
-                    dl.setName(Encoding.unicodeDecode(args[2]));
-                    if (args[4] != null) {
-                        dl.setDownloadSize(SizeFormatter.getSize(args[4]));
-                    }
-                    dl.setAvailable(true);
-                }
-                ret.add(dl);
+        final ArrayList<Object> ressourcelist = (ArrayList<Object>) entries.get("aaData");
+        ArrayList<Object> fileinfo = (ArrayList<Object>) entries.get("aaData");
+        for (final Object fileO : ressourcelist) {
+            fileinfo = (ArrayList<Object>) fileO;
+            final String fileIDhtml = (String) fileinfo.get(0);
+            final String filehtml = (String) fileinfo.get(1);
+            final String filesize = (String) fileinfo.get(2);
+            final String fileID = new Regex(fileIDhtml, "value=\"(\\d+)\"").getMatch(0);
+            // String url = new Regex(filehtml, "href=\"(/[^<>\"]+)").getMatch(0);
+            if (StringUtils.isEmpty(fileID)) {
+                /* Skip invalid items */
+                continue;
             }
+            /* Build url */
+            String url = "https://" + br.getHost(true) + "/fs/" + this.uuid + "-" + fileID;
+            final String filename = new Regex(filehtml, ">([^<>\"]+)</a>").getMatch(0);
+            final DownloadLink dl = this.createDownloadlink(url);
+            if (filename != null) {
+                dl.setName(filename);
+            }
+            if (filesize != null) {
+                dl.setDownloadSize(SizeFormatter.getSize(filesize));
+            }
+            dl.setAvailable(true);
+            ret.add(dl);
         }
-        if (folders != null && folders.length != 0) {
-            final String host = new Regex(br.getURL(), "(https?://(www\\.)?" + domains + ")").getMatch(0);
-            if (host == null) {
-                logger.info("Could not determine Host :: " + parameter);
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            }
-            for (String folder : folders) {
-                ret.add(createDownloadlink(host + folder));
-            }
-        }
-
+        // export folders back into decrypter again.
+        // final String[] folders = ajax.getRegex("<a href=\"(/u/" + uuid + "/\\d+)\">").getColumn(0);
+        // if ((folders == null || folders.length == 0)) {
+        // ret = null;
+        // return;
+        // }
+        // if (folders != null && folders.length != 0) {
+        /** TODO: 2019-07-05: Re-add subfolder support */
+        // final String host = new Regex(br.getURL(), "(https?://(www\\.)?" + domains + ")").getMatch(0);
+        // if (host == null) {
+        // logger.info("Could not determine Host :: " + parameter);
+        // throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        // }
+        // for (String folder : folders) {
+        // ret.add(createDownloadlink(host + folder));
+        // }
+        // }
     }
 
     /**
@@ -203,5 +253,4 @@ public class CtDiskComFolder extends PluginForDecrypt {
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
