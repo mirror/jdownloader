fdc5181b87aaf2349763bccd89df3b69c22fb466
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-07-08 00:37:44 +0000

    sketchfab.com -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40882 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4518521bf402f2f3ff78f8d9790fe80e14152807

---
+++ src/jd/plugins/decrypter/Sketchfab.java
@@ -0,0 +1,58 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision: 40882 $", interfaceVersion = 2, names = { "sketchfab.com" }, urls = { "https?://(www\\.)?sketchfab\\.com/3d-models/.*" })
+public class Sketchfab extends antiDDoSForDecrypt {
+    public Sketchfab(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        String page = br.toString();
+        String fpName = null;
+        if (br.containsHTML("<div class=\"viewer-viewport\">")) {
+            fpName = br.getRegex("class=\"model-name__label\">([^<]+)</span").getMatch(0);
+            String[][] links = br.getRegex("(http[^#;]+ile.osgjs.gz)").getMatches();
+            if (links != null && links.length > 0) {
+                String decodedLink = br.getURL(Encoding.htmlDecode(links[0][0])).toString().replace("file.osgjs.gz", "model_file.bin.gz");
+                decryptedLinks.add(createDownloadlink(decodedLink));
+            }
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
