e7e4e7b5f4f024cc543c328f9a03c3d6de9eea78
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-07-10 14:44:21 +0000

    TheartistunionCom: -fixed getLinkID. @pspzocker: this method must return sort of unique identifier! -updated requestFileInformation to also check filesize -caching url isn't required -refs thread 80876
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40899 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/TheartistunionCom.java
+++ src/jd/plugins/hoster/TheartistunionCom.java
@@ -15,27 +15,22 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
-import java.io.IOException;
-
 import jd.PluginWrapper;
-import jd.config.Property;
-import jd.http.Browser;
 import jd.http.URLConnectionAdapter;
-import jd.parser.Regex;
-import jd.plugins.Account.AccountType;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
-import jd.plugins.PluginForHost;
 import jd.plugins.components.PluginJSonUtils;
 
+import org.appwork.utils.Regex;
 import org.appwork.utils.StringUtils;
 import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.plugins.components.antiDDoSForHost;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "theartistunion.com" }, urls = { "https?://(?:www\\.)?theartistunion\\.com/tracks/([A-Za-z0-9]+)" })
-public class TheartistunionCom extends PluginForHost {
+public class TheartistunionCom extends antiDDoSForHost {
     public TheartistunionCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -45,35 +40,21 @@ public class TheartistunionCom extends PluginForHost {
         return "https://theartistunion.com/";
     }
 
-    @Override
-    public String getLinkID(final DownloadLink link) {
-        final String linkid = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
-        if (linkid != null) {
-            return linkid;
-        } else {
-            return super.getLinkID(link);
-        }
-    }
-
     /* Connection stuff */
     private static final boolean FREE_RESUME       = true;
     private static final int     FREE_MAXCHUNKS    = 1;
     private static final int     FREE_MAXDOWNLOADS = 20;
+    private String               url               = null;
 
-    // private static final boolean ACCOUNT_FREE_RESUME = true;
-    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
-    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
-    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
-    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
-    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
-    //
-    // /* don't touch the following! */
-    // private static AtomicInteger maxPrem = new AtomicInteger(1);
+    private String getTrackID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
     @Override
-    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
         link.setMimeHint(CompiledFiletypeFilter.AudioExtensions.MP3);
         this.setBrowserExclusive();
-        br.getPage("https://" + this.getHost() + "/api/v3/tracks/" + this.getLinkID(link) + ".json");
+        getPage("https://" + this.getHost() + "/api/v3/tracks/" + this.getTrackID(link) + ".json");
         if (br.getHttpConnection().getResponseCode() == 404) {
             /* 2019-07-04: {"error":"Could not find track"} */
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
@@ -87,7 +68,43 @@ public class TheartistunionCom extends PluginForHost {
         } else {
             link.setName(this.getLinkID(link) + ".mp3");
         }
-        return AvailableStatus.TRUE;
+        final String audio_source = PluginJSonUtils.getJson(br, "audio_source");
+        if (StringUtils.isEmpty(audio_source)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        } else {
+            this.url = audio_source;
+            if (StringUtils.containsIgnoreCase(audio_source, "/stream_files/")) {
+                logger.info("Trying to find better quality download");
+                final String dllink_better_quality = audio_source.replace("/stream_files/", "/original_files/");
+                final URLConnectionAdapter con = br.openGetConnection(dllink_better_quality);
+                try {
+                    if (con.getContentType().contains("text") || !con.isOK() || con.getLongContentLength() == -1) {
+                        logger.info("No better quality download found");
+                    } else {
+                        logger.info("Found better quality download");
+                        if (con.getCompleteContentLength() > 0) {
+                            link.setVerifiedFileSize(con.getCompleteContentLength());
+                        }
+                        url = dllink_better_quality;
+                    }
+                } finally {
+                    con.disconnect();
+                }
+            } else {
+                logger.info("No better quality download possible");
+            }
+            if (link.getVerifiedFileSize() == -1) {
+                final URLConnectionAdapter con = br.openGetConnection(url);
+                try {
+                    if (!con.getContentType().contains("text") && con.isOK() && con.getLongContentLength() > 0) {
+                        link.setVerifiedFileSize(con.getCompleteContentLength());
+                    }
+                } finally {
+                    con.disconnect();
+                }
+            }
+            return AvailableStatus.TRUE;
+        }
     }
 
     @Override
@@ -97,31 +114,10 @@ public class TheartistunionCom extends PluginForHost {
     }
 
     private void doFree(final DownloadLink downloadLink, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
-        String dllink = checkDirectLink(downloadLink, directlinkproperty);
-        if (dllink == null) {
-            dllink = PluginJSonUtils.getJson(br, "audio_source");
-            if (StringUtils.isEmpty(dllink)) {
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            }
+        if (url == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        if (StringUtils.containsIgnoreCase(dllink, "/stream_files/")) {
-            logger.info("Trying to find better quality download");
-            final String dllink_better_quality = dllink.replace("/stream_files/", "/original_files/");
-            final URLConnectionAdapter con = br.openGetConnection(dllink_better_quality);
-            try {
-                if (con.getContentType().contains("text") || !con.isOK() || con.getLongContentLength() == -1) {
-                    logger.info("No better quality download found");
-                } else {
-                    logger.info("Found better quality download");
-                    dllink = dllink_better_quality;
-                }
-            } finally {
-                con.disconnect();
-            }
-        } else {
-            logger.info("No better quality download possible");
-        }
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, resumable, maxchunks);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, url, resumable, maxchunks);
         if (dl.getConnection().getContentType().contains("html")) {
             br.followConnection(true);
             if (dl.getConnection().getResponseCode() == 403) {
@@ -133,46 +129,6 @@ public class TheartistunionCom extends PluginForHost {
         }
         downloadLink.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
         dl.startDownload();
-    }
-
-    private String checkDirectLink(final DownloadLink downloadLink, final String property) {
-        String dllink = downloadLink.getStringProperty(property);
-        if (dllink != null) {
-            URLConnectionAdapter con = null;
-            try {
-                final Browser br2 = br.cloneBrowser();
-                br2.setFollowRedirects(true);
-                con = br2.openHeadConnection(dllink);
-                if (con.getContentType().contains("text") || !con.isOK() || con.getLongContentLength() == -1) {
-                    downloadLink.setProperty(property, Property.NULL);
-                    dllink = null;
-                }
-            } catch (final Exception e) {
-                logger.log(e);
-                downloadLink.setProperty(property, Property.NULL);
-                dllink = null;
-            } finally {
-                try {
-                    con.disconnect();
-                } catch (final Throwable e) {
-                }
-            }
-        }
-        return dllink;
-    }
-
-    @Override
-    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
-        if (acc == null) {
-            /* no account, yes we can expect captcha */
-            return true;
-        }
-        if (acc.getType() == AccountType.FREE) {
-            /* Free accounts can have captchas */
-            return true;
-        }
-        /* Premium accounts do not have captchas */
-        return false;
     }
 
     @Override
