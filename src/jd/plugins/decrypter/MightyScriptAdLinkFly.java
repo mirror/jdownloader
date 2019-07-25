5e399a1e448b8d5538b05778b08e5c4df28b6e89
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-07-25 00:46:48 +0000

    *Plugins: Fixes/Changes/Maintenance* - superloadcz: added workaround for issues when re-using directurls - we should make a template which works for superload.cz and stahomat.cz as APIs are exactly the same RE forum 81138 - panbaiducom: improved crawler, fixed contentURLs
    *Plugins: New plugins or new websites that belong to existing scripts*
    - MightyScriptAdLinkFly: 2clickus & 2ckcc & earticalexyz closes #87254
    - MightyScriptAdLinkFly: 2shortencom & nashronline & almrfcom closes #87255
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@40997 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MightyScriptAdLinkFly.java
+++ src/jd/plugins/decrypter/MightyScriptAdLinkFly.java
@@ -21,6 +21,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import org.appwork.utils.StringUtils;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
@@ -35,10 +39,6 @@ import jd.plugins.PluginException;
 import jd.plugins.components.PluginJSonUtils;
 import jd.plugins.components.SiteType.SiteTemplate;
 
-import org.appwork.utils.StringUtils;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
 /**
  *
  * @author raztoki
@@ -52,6 +52,10 @@ import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 public class MightyScriptAdLinkFly extends antiDDoSForDecrypt {
     private static final String[]     domains                    = { "arabtvlink.com", "medche.com", "go-urls.tk", "btc4link.com", "topklink.com", "shorts-link.com", "easy4earn.com", "linkat4all.com", "linkdrop.net", "shrtz.me", "ctkings.com", "linksad.net", "paylink.pro", "123link.pro", "donia2link.com", "cutpaid.com", "shortadz.org", "itiurl.co", "shortli.net", "cutearn.ca", "icutit.ca", "cut-one.com", "cll.press", "link-zero.com", "linktor.io", "cash4url.com", "cashat.net", "123short.com", "skip-url.me", "msms4.com", "empireshort.com", "loadurl.com", "shortmony.me", "geistlink.com", "cutt.us.com", "arabdollar.com", "shortenow.com", "kingurl.net", "best3link.com", "solo-link.com", "best5link.com", "lkky.co", "win4cut.com", "coinlink.co", "adlink.guru", "short.es", "tmearn.com", "ibly.co", "urle.co", "mitly.us", "zlshorte.net", "igram.im", "gram.im", "bit-url.com", "adbilty.me",
             "linclik.com", "oke.io", "vivads.net", "pnd.tl", "met.bz", "urlcloud.us", "clik.pw",
+            /** 2019-07-24: 2click.us domains */
+            "2click.us", "2ck.cc", "earticale.xyz",
+            /** 2019-07-24: 2shorten.com domains */
+            "2shorten.com", "nashr.online", "almrf.com",
             /** shrinkearn.com domains */
             "shrinkearn.com", "wishes2.com", "shortearn.eu",
             /** clk.sh domains */
