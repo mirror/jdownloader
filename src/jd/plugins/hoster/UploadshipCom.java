9a635555a4e48c8745a644e4ff3ce6b7892a4bb1
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-07-31 17:10:55 +0000

    *General* YetiShareCore: - added supports_availablecheck_filesize_html - added support for solvemedia login-captcha on old login method (very rare case) - set correct linkid
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [YetiShare] +account uploadshipcom RE forum 81190
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41015 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: e10a94e7c1563b2c6e72414b41d475d67777d628

---
+++ src/jd/plugins/hoster/UploadshipCom.java
@@ -0,0 +1,140 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2016  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.YetiShareCore;
+
+import jd.PluginWrapper;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+import jd.plugins.PluginException;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
+public class UploadshipCom extends YetiShareCore {
+    public UploadshipCom(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium(getPurchasePremiumURL());
+    }
+
+    /**
+     * DEV NOTES YetiShare<br />
+     ****************************
+     * mods: See overridden functions<br />
+     * limit-info: 2019-07-31: Premium untested, set FREE account limits <br />
+     * captchatype-info: 2019-07-31: solvemedia<br />
+     * other: <br />
+     */
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "uploadship.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String[]> pluginDomains = getPluginDomains();
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + YetiShareCore.getDefaultAnnotationPatternPart());
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return true;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return true;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return true;
+        }
+    }
+
+    public int getMaxChunks(final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return 1;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return 1;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return 1;
+        }
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return 1;
+    }
+
+    public int getMaxSimultaneousFreeAccountDownloads() {
+        return 1;
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return 1;
+    }
+
+    @Override
+    public boolean supports_availablecheck_over_info_page() {
+        /* 2019-07-31: Special */
+        return false;
+    }
+
+    @Override
+    protected boolean supports_availablecheck_filesize_html() {
+        /* 2019-07-31: Special */
+        return false;
+    }
+
+    @Override
+    protected boolean isOfflineWebsite(final DownloadLink link, final boolean checkErrors) throws PluginException {
+        return br.getHttpConnection().getResponseCode() == 404;
+    }
+
+    @Override
+    public String[] scanInfo(final String[] fileInfo) {
+        /* 2019-07-31: Special */
+        fileInfo[0] = br.getRegex("<h4>\\s*?Download File\\s*?:\\s*?([^<>\"]+)</h4>").getMatch(0);
+        fileInfo[1] = br.getRegex("/" + this.getFUIDFromURL(this.getDownloadLink()) + "</font> \\- \\[ ([^<>\"\\]]+) \\][^<]+</small>").getMatch(0);
+        if (StringUtils.isEmpty(fileInfo[0]) || StringUtils.isEmpty(fileInfo[1])) {
+            /* Use default handling as fallback */
+            super.scanInfo(fileInfo);
+        }
+        return fileInfo;
+    }
+}
