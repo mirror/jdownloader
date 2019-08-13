4bb6fdbc6075181afa6154e4613694ced1593db7
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-13 12:59:03 +0000

    *Plugins: Fixes/Changes/Maintenance* - vizcom: re-use cookies, improved account info, can now also display GEO-blocked state and expiredate refs #87270
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41089 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2efa7db52c736fe851f9bb66997034999fcbe53b

--- src/jd/plugins/hoster/VizCom.java
+++ src/jd/plugins/hoster/VizCom.java
@@ -15,8 +15,11 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
+import java.util.Locale;
+
+import org.appwork.utils.formatter.TimeFormatter;
+
 import jd.PluginWrapper;
-import jd.controlling.AccountController;
 import jd.http.Browser;
 import jd.http.Cookies;
 import jd.http.URLConnectionAdapter;
@@ -24,6 +27,7 @@ import jd.nutils.encoding.Encoding;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
 import jd.plugins.AccountInfo;
+import jd.plugins.AccountRequiredException;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -44,39 +48,44 @@ public class VizCom extends PluginForHost {
     // protocol: https
     // other:
     /* Extension which will be used if no correct extension is found */
-    private static final String default_extension            = ".jpg";
+    private static final String default_extension    = ".jpg";
     /* Connection stuff */
-    private final boolean       FREE_RESUME                  = false;
-    private final int           FREE_MAXCHUNKS               = 1;
-    private final int           FREE_MAXDOWNLOADS            = 20;
-    private final boolean       ACCOUNT_FREE_RESUME          = false;
-    private final int           ACCOUNT_FREE_MAXCHUNKS       = 1;
-    private final int           ACCOUNT_FREE_MAXDOWNLOADS    = 20;
-    private final boolean       ACCOUNT_PREMIUM_RESUME       = false;
-    private final int           ACCOUNT_PREMIUM_MAXCHUNKS    = 1;
-    private final int           ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
-    private String              dllink                       = null;
-    private boolean             server_issues                = false;
-    private boolean             premiumonly                  = false;
+    private final boolean       FREE_RESUME          = false;
+    private final int           FREE_MAXCHUNKS       = 1;
+    private final int           FREE_MAXDOWNLOADS    = 20;
+    private final boolean       ACCOUNT_RESUME       = false;
+    private final int           ACCOUNT_MAXCHUNKS    = 1;
+    private final int           ACCOUNT_MAXDOWNLOADS = 20;
+    private String              dllink               = null;
+    private boolean             server_issues        = false;
+    private boolean             premiumonly          = false;
 
     @Override
     public String getAGBLink() {
         return "https://www.viz.com/terms";
     }
 
-    @SuppressWarnings("deprecation")
+    private Browser prepBR(final Browser br) {
+        br.setAllowedResponseCodes(new int[] { 500 });
+        br.setCookie(this.getHost(), "curtain_seen", "true");
+        return br;
+    }
+
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        return requestFileInformation(link, null);
+    }
+
+    public AvailableStatus requestFileInformation(final DownloadLink link, final Account account) throws Exception {
         dllink = null;
         server_issues = false;
         premiumonly = false;
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        final Account aa = AccountController.getInstance().getValidAccount(this);
-        if (aa != null) {
-            this.login(aa, false);
+        if (account != null) {
+            this.login(account, false);
         }
-        final String[] ids = link.getDownloadURL().replace("http://vizdecrypted/", "").split("_");
+        final String[] ids = link.getPluginPatternMatcher().replace("http://vizdecrypted/", "").split("_");
         final String manga_id = ids[0];
         final String page = ids[1];
         final String page_for_url = ids[2];
@@ -113,15 +122,13 @@ public class VizCom extends PluginForHost {
         if (!filename.endsWith(ext)) {
             filename += ext;
         }
-        dllink = Encoding.htmlDecode(dllink);
         link.setName(filename);
-        final Browser br2 = br.cloneBrowser();
-        // In case the link redirects to the finallink
         if (dllink != null) {
-            br2.setFollowRedirects(true);
+            dllink = Encoding.htmlDecode(dllink);
+            br.setFollowRedirects(true);
             URLConnectionAdapter con = null;
             try {
-                con = br2.openHeadConnection(dllink);
+                con = br.openHeadConnection(dllink);
                 if (!con.getContentType().contains("html")) {
                     link.setDownloadSize(con.getLongContentLength());
                     link.setProperty("directlink", dllink);
@@ -148,7 +155,8 @@ public class VizCom extends PluginForHost {
         if (server_issues) {
             throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Unknown server error", 10 * 60 * 1000l);
         } else if (premiumonly) {
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_ONLY);
+            logger.info("Account required OR GEO-blocked OR not downloadable with current account (free account but premium required?) OR item needs to be purchased separately");
+            throw new AccountRequiredException();
         }
         if (dllink == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
@@ -175,72 +183,99 @@ public class VizCom extends PluginForHost {
         return FREE_MAXDOWNLOADS;
     }
 
-    private static Object LOCK = new Object();
-
+    /**
+     * 2019-08-13: This host GEO-blocks a lot of content for outside US users. Login however should always work fine! There is a page which
+     * shows additional account information/purchases which is not accessible when GEO-blocked:
+     * https://www.viz.com/account/manage_membership
+     */
     private void login(final Account account, final boolean force) throws Exception {
-        synchronized (LOCK) {
+        synchronized (account) {
             try {
                 br.setFollowRedirects(true);
                 br.setCookiesExclusive(true);
+                prepBR(this.br);
                 final Cookies cookies = account.loadCookies("");
-                if (cookies != null && !force) {
+                boolean isLoggedin = false;
+                if (cookies != null) {
                     this.br.setCookies(this.getHost(), cookies);
-                    return;
+                    br.getPage("https://www." + this.getHost() + "/account");
+                    isLoggedin = this.isLoggedin();
                 }
-                br.setAllowedResponseCodes(500);
-                br.setCookie(account.getHoster(), "curtain_seen", "true");
-                br.getPage("https://www.viz.com/");
-                br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-                // Grab the now required authenticity_token value from refresh_login_links and use it to properly log in.
-                br.getPage("https://www.viz.com/account/refresh_login_links");
-                final String authenticity_token = br.getRegex("var AUTH_TOKEN\\s*=\\s*\"([^\\\"]*)\";").getMatch(0);
-                br.postPage("https://www." + account.getHoster() + "/account/try_login", "login=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()) + "&authenticity_token=" + Encoding.urlEncode(authenticity_token) + "&rem_user=1");
-                final String okay = PluginJSonUtils.getJsonValue(this.br, "ok");
-                if (this.br.getHttpConnection().getResponseCode() != 200 || !"1".equals(okay)) {
-                    if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
-                        throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nUngültiger Benutzername oder ungültiges Passwort!\r\nSchnellhilfe: \r\nDu bist dir sicher, dass dein eingegebener Benutzername und Passwort stimmen?\r\nFalls dein Passwort Sonderzeichen enthält, ändere es und versuche es erneut!", PluginException.VALUE_ID_PREMIUM_DISABLE);
-                    } else {
-                        throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nInvalid username/password!\r\nQuick help:\r\nYou're sure that the username and password you entered are correct?\r\nIf your password contains special characters, change it (remove them) and try again!", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                if (!isLoggedin) {
+                    br.getPage("https://www." + this.getHost() + "/account");
+                    br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+                    // Grab the now required authenticity_token value from refresh_login_links and use it to properly log in.
+                    br.getPage("/account/refresh_login_links");
+                    final String authenticity_token = br.getRegex("var AUTH_TOKEN\\s*?=\\s*?\"([^\\\"]*)\";").getMatch(0);
+                    br.postPage("/account/try_login", "login=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()) + "&authenticity_token=" + Encoding.urlEncode(authenticity_token) + "&rem_user=1");
+                    final String okay = PluginJSonUtils.getJsonValue(this.br, "ok");
+                    if (this.br.getHttpConnection().getResponseCode() != 200 || !"1".equals(okay)) {
+                        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
                     }
                 }
                 account.saveCookies(this.br.getCookies(this.getHost()), "");
             } catch (final PluginException e) {
-                account.clearCookies("");
+                if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                    account.clearCookies("");
+                }
                 throw e;
             }
         }
     }
 
-    @SuppressWarnings("deprecation")
+    private boolean isLoggedin() {
+        return br.getCookie(br.getHost(), "", Cookies.NOTDELETEDPATTERN) != null || br.containsHTML("id=\"account_date_day\"");
+    }
+
     @Override
     public AccountInfo fetchAccountInfo(final Account account) throws Exception {
         final AccountInfo ai = new AccountInfo();
         try {
             login(account, true);
         } catch (PluginException e) {
-            account.setValid(false);
             throw e;
         }
         /*
          * 2017-01-25: Treat all as free accounts - accounts do not have an exact status - users can buy single items which are then
          * unlocked to view/download in their account.
          */
-        account.setType(AccountType.FREE);
-        account.setMaxSimultanDownloads(ACCOUNT_PREMIUM_MAXDOWNLOADS);
-        ai.setStatus("Registered (free) user");
-        account.setValid(true);
+        account.setMaxSimultanDownloads(ACCOUNT_MAXDOWNLOADS);
+        String accountStatus = null;
+        try {
+            br.getPage("/account/manage_membership");
+            String expireStr = br.getRegex(">Member until ([A-Za-z]+  \\d{1,2}, \\d{4})</a>").getMatch(0);
+            if (br.containsHTML(">CONTENT NOT AVAILABLE<|>\\s*?Unfortunately this content is not available in your location")) {
+                /* GEO-blocked - basically we cannot find out the real account type in this state */
+                accountStatus = "Registered (free) user [GEO-blocked(only US IPs are allowed)]";
+                account.setType(AccountType.FREE);
+            } else {
+                /* NOT GEO-blocked */
+                if (expireStr == null) {
+                    accountStatus = "Registered (free) user";
+                    account.setType(AccountType.FREE);
+                } else {
+                    expireStr = expireStr.replace(" ", "");
+                    ai.setValidUntil(TimeFormatter.getMilliSeconds(expireStr, "MMMdd','yyyy", Locale.ENGLISH), br);
+                    accountStatus = "Premium user";
+                    account.setType(AccountType.PREMIUM);
+                }
+                accountStatus += " [NOT GEO-blocked]";
+            }
+        } catch (final Throwable e) {
+        }
+        ai.setStatus(accountStatus);
         return ai;
     }
 
     @Override
     public void handlePremium(final DownloadLink link, final Account account) throws Exception {
-        requestFileInformation(link);
-        doFree(link, ACCOUNT_FREE_RESUME, ACCOUNT_FREE_MAXCHUNKS, "account_free_directlink");
+        requestFileInformation(link, account);
+        doFree(link, ACCOUNT_RESUME, ACCOUNT_MAXCHUNKS, "account_free_directlink");
     }
 
     @Override
     public int getMaxSimultanPremiumDownloadNum() {
-        return ACCOUNT_FREE_MAXDOWNLOADS;
+        return ACCOUNT_MAXDOWNLOADS;
     }
 
     @Override
