a1b17dad873ec1221e739d971391e2ad8e8587f6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-14 12:19:08 +0000

    *Plugins: Fixes/Changes/Maintenance* - goloadycom: added folder crawler refs #87201
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [XFS] +account firefilesorg RE: IRC
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41106 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FirefilesOrg.java
@@ -0,0 +1,105 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdownloader.plugins.components.XFileSharingProBasic;
+
+import jd.PluginWrapper;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class FirefilesOrg extends XFileSharingProBasic {
+    public FirefilesOrg(final PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium(super.getPurchasePremiumURL());
+    }
+
+    /**
+     * DEV NOTES XfileSharingProBasic Version SEE SUPER-CLASS<br />
+     * mods: See overridden functions<br />
+     * limit-info: no limits at all <br />
+     * captchatype-info: null<br />
+     * other:<br />
+     */
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "firefiles.org" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return XFileSharingProBasic.buildAnnotationUrls(getPluginDomains());
+    }
+
+    @Override
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return true;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return true;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return true;
+        }
+    }
+
+    @Override
+    public int getMaxChunks(final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return 0;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return 0;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return 0;
+        }
+    }
+
+    @Override
+    public int getMaxSimultaneousFreeAnonymousDownloads() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultaneousFreeAccountDownloads() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
+    }
+}
