f635aa6f74cec7f76edf3320c1bd81b506cde974
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-21 15:17:19 +0000

    *Plugins: Fixes/Changes/Maintenance* - ParteeeyDe: dramatically improved speed - pornhubcom: improved errorhandling #87362
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41145 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 29e458f377e7f3239ce8e9455937574c0da56649

--- src/jd/plugins/hoster/ParteeeyDe.java
+++ src/jd/plugins/hoster/ParteeeyDe.java
@@ -62,9 +62,12 @@ public class ParteeeyDe extends PluginForHost {
         link.setContentUrl(String.format("https://www.parteeey.de/galerie/datei?p=%s", getFID(link)));
     }
 
-    @SuppressWarnings({ "deprecation", "unchecked" })
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        return requestFileInformation(link, false);
+    }
+
+    public AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
         dllink = null;
         server_issues = false;
         final String fid = getFID(link);
@@ -76,10 +79,10 @@ public class ParteeeyDe extends PluginForHost {
             link.getLinkStatus().setStatusText("Account needed for linkcheck- and download");
             return AvailableStatus.UNCHECKABLE;
         }
-        login(this.br, aa);
         String url_thumb = link.getStringProperty("thumburl", null);
         String filename = null;
-        String filename_decrypter = link.getStringProperty("decrypterfilename", null);
+        final String filename_decrypter = link.getStringProperty("decrypterfilename", null);
+        final String galleryid = link.getStringProperty("galleryid", null);
         /* 2017-10-06: Official downloads are disabled (seems like that requires special permissions on users' accounts from now on) */
         final boolean downloadAllowed = false;
         if (downloadAllowed) {
@@ -87,38 +90,52 @@ public class ParteeeyDe extends PluginForHost {
              * 2016-08-21: Prefer download via official download function as this gives us the highest quality possible - usually a bit(some
              * KBs) better than the image displayed in the gallery via browser.
              */
+            /* Login with validating cookies */
+            login(this.br, aa, true);
             dllink = "https://www." + this.getHost() + "/galerie/datei/herunterladen/" + fid;
         } else {
-            final boolean grabAjax = false;
-            if (grabAjax) {
-                /* 2016-08-21: Set width & height to higher values so that we get the max quality possible. */
-                this.br.postPage("https://www." + this.getHost() + "/Ajax/mulFileInfo", "filId=" + fid + "&width=5000&height=5000&filIdPrevious=&filIdNext=");
-                try {
-                    final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaObject(br.toString());
-                    dllink = (String) entries.get("path");
-                } catch (final Throwable e) {
-                }
-            } else {
-                /*
-                 * 2019-08-21: These images seem to have a slightly better quality than via ajax request (also the downloadlink extracted
-                 * via ajax request will contain the String '/tumbnail/'.)
-                 */
-                br.getPage("https://www.parteeey.de/galerie/datei?p=" + fid);
-                dllink = br.getRegex("<meta property=\"og:image\" content=\"(https?://[^\"]+)\">").getMatch(0);
+            final String original_filename_in_thumbnail = new Regex(url_thumb, "/[^/]*(" + fid + ".+)").getMatch(0);
+            if (original_filename_in_thumbnail != null && galleryid != null) {
+                /* 2019-08-21: Best way to generate downloadlinks - and we do not even have to login to download them! */
+                dllink = String.format("https://www.parteeey.de/files/mul/galleries/%s/%s", galleryid, original_filename_in_thumbnail);
             }
-            if (!StringUtils.isEmpty(dllink)) {
-                if (url_thumb != null && !url_thumb.startsWith("http")) {
-                    url_thumb = "https://www." + this.getHost() + "/" + url_thumb;
+            if (StringUtils.isEmpty(dllink)) {
+                final boolean grabAjax = false;
+                if (grabAjax) {
+                    /* 2016-08-21: Set width & height to higher values so that we get the max quality possible. */
+                    /* Login with validating cookies */
+                    login(this.br, aa, true);
+                    this.br.postPage("https://www." + this.getHost() + "/Ajax/mulFileInfo", "filId=" + fid + "&width=5000&height=5000&filIdPrevious=&filIdNext=");
+                    try {
+                        final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaObject(br.toString());
+                        dllink = (String) entries.get("path");
+                    } catch (final Throwable e) {
+                    }
+                } else {
+                    /*
+                     * 2019-08-21: These images seem to have a slightly better quality than via ajax request (also the downloadlink
+                     * extracted via ajax request will contain the String '/tumbnail/'.)
+                     */
+                    /* Login WITHOUT validating cookies - this speeds up the downloading process! */
+                    final boolean verifiedLogin = login(this.br, aa, false);
+                    br.getPage("https://www.parteeey.de/galerie/datei?p=" + fid);
+                    if (!verifiedLogin && !isLoggedin(br)) {
+                        logger.info("Login cookies did not work - trying again!");
+                        login(this.br, aa, true);
+                        br.getPage("/galerie/datei?p=" + fid);
+                    }
+                    if (br.getHttpConnection().getResponseCode() == 404) {
+                        /* Offline - very very rare case! */
+                        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                    }
+                    dllink = br.getRegex("<meta property=\"og:image\" content=\"(https?://[^\"]+)\">").getMatch(0);
                 }
-                /* 2019-08-21: Only prefer thumbnail download if it is available in higher quality than 'original'. */
-                // dllink = url_thumb;
-            } else {
-                if (!dllink.startsWith("http") && !dllink.startsWith("/")) {
+                if (dllink != null && !dllink.startsWith("http") && !dllink.startsWith("/")) {
                     dllink = "https://www." + this.getHost() + "/" + dllink;
                 }
             }
         }
-        if (!StringUtils.isEmpty(dllink)) {
+        if (!StringUtils.isEmpty(dllink) && !isDownload) {
             final String url_filename = getFilenameFromDirecturl(dllink);
             if (url_filename != null) {
                 filename = url_filename;
@@ -156,7 +173,7 @@ public class ParteeeyDe extends PluginForHost {
 
     @Override
     public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
-        requestFileInformation(downloadLink);
+        requestFileInformation(downloadLink, true);
         /* Account required to download. */
         throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_ONLY);
     }
@@ -185,23 +202,28 @@ public class ParteeeyDe extends PluginForHost {
         return FREE_MAXDOWNLOADS;
     }
 
-    public static void login(Browser br, final Account account) throws Exception {
+    public static boolean login(final Browser br, final Account account, final boolean validateCookies) throws Exception {
+        boolean loggedIN = false;
         synchronized (account) {
             try {
                 prepBR(br);
                 br.setFollowRedirects(true);
                 br.setCookiesExclusive(true);
                 final Cookies cookies = account.loadCookies("");
-                boolean loggedIN = false;
                 if (cookies != null) {
                     br.setCookies(account.getHoster(), cookies);
+                    if (System.currentTimeMillis() - account.getCookiesTimeStamp("") <= trust_cookie_age && !validateCookies) {
+                        /* We trust these cookies as they're not that old --> Do not check them */
+                        return false;
+                    }
                     br.getPage("https://www." + account.getHoster());
                     loggedIN = isLoggedin(br);
                 }
                 if (!loggedIN) {
                     /* Perform full login */
                     br.postPage("https://www." + account.getHoster() + "/login", "loginData%5BauthsysAuthProvider%5D%5BrememberLogin%5D=on&sent=true&url=%2F&usedProvider=authsysAuthProvider&loginData%5BauthsysAuthProvider%5D%5Busername%5D=" + Encoding.urlEncode(account.getUser()) + "&loginData%5BauthsysAuthProvider%5D%5Bpassword%5D=" + Encoding.urlEncode(account.getPass()));
-                    if (br.containsHTML("Ihre Login\\-Daten sind ungültig") || !isLoggedin(br)) {
+                    loggedIN = isLoggedin(br);
+                    if (br.containsHTML("Ihre Login\\-Daten sind ungültig") || !loggedIN) {
                         throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
                     }
                 }
@@ -211,17 +233,18 @@ public class ParteeeyDe extends PluginForHost {
                 throw e;
             }
         }
+        return loggedIN;
     }
 
     public static boolean isLoggedin(final Browser br) {
-        return br.containsHTML("/logout\"");
+        return br.containsHTML("/logout\"") && br.getCookie("parteeey.de", "login-token", Cookies.NOTDELETEDPATTERN) != null;
     }
 
     @Override
     public AccountInfo fetchAccountInfo(final Account account) throws Exception {
         final AccountInfo ai = new AccountInfo();
         try {
-            login(this.br, account);
+            login(this.br, account, true);
         } catch (PluginException e) {
             throw e;
         }
@@ -234,7 +257,7 @@ public class ParteeeyDe extends PluginForHost {
 
     @Override
     public void handlePremium(final DownloadLink link, final Account account) throws Exception {
-        requestFileInformation(link);
+        requestFileInformation(link, true);
         doFree(link);
     }
 
