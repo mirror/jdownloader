682d89853cb5d3d8b2dbef4146679e86daf6445f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-23 12:57:12 +0000

    ThingiverseCom: - refs #87338
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41150 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/ThingiverseCom.java
+++ src/jd/plugins/decrypter/ThingiverseCom.java
@@ -2,20 +2,21 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
-import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 
-@DecrypterPlugin(revision = "$Revision: 41147 $", interfaceVersion = 2, names = { "thingiverse.com" }, urls = { "https?://(www\\.)?thingiverse\\.com/.*" })
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision: 41147 $", interfaceVersion = 2, names = { "thingiverse.com" }, urls = { "https?://(www\\.)?thingiverse\\.com/(thing:\\d+|make:\\d+|[^/]+/(about|designs|collections(/[^/]+)?|makes|likes))" })
 public class ThingiverseCom extends antiDDoSForDecrypt {
     public ThingiverseCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -23,38 +24,40 @@ public class ThingiverseCom extends antiDDoSForDecrypt {
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString().replaceAll("/about($|\\?.*)", "/designs");
-        if (StringUtils.containsIgnoreCase(parameter, "/groups/")) {
-            parameter = param.toString().replaceAll("/(forums|members|about)($|\\?.*)", "/designs");
-        }
         br.setFollowRedirects(true);
-        getPage(parameter);
+        getPage(param.getCryptedUrl());
         String fpName = br.getRegex("<title>\\s*([^<]+?)\\s*-\\s*Thingiverse").getMatch(0);
-        if (parameter.contains("thing:")) {
-            final String fid = new Regex(parameter, "(\\d+).*").getMatch(0);
-            final DownloadLink link = createDownloadlink("directhttp://https://www.thingiverse.com/thing:" + fid + "/zip");
+        if (new Regex(br.getURL(), "/[^/]+/(about|designs|collections(/[^/]+)?|makes|likes)").matches()) {
+            // TODO add support for user-abouts/designs/collections/makes/likes, collections may have individual ones
+            // requires post requests to ajax api (pagination)
+            // please don't use wide/open pattern but try to make it as fine as possible
+            // TODO: /groups/XY , /groups/XY/things , /groups/XY/about
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        } else if (StringUtils.containsIgnoreCase(br.getURL(), "/thing:")) {
+            // a thing
+            final String thingID = new Regex(br.getURL(), "thing:(\\d+).*").getMatch(0);
+            final DownloadLink link = createDownloadlink("directhttp://https://www.thingiverse.com/thing:" + thingID + "/zip");
             if (fpName != null) {
                 fpName = Encoding.htmlOnlyDecode(fpName);
                 link.setFinalFileName(fpName + ".zip");
             }
             decryptedLinks.add(link);
-        } else if (parameter.contains("make:")) {
-            String link = br.getRegex("href=\"(/\\w+:\\d+)\" class=\"card-img-holder\"").getMatch(0);
-            link = br.getURL(Encoding.htmlOnlyDecode(link)).toString();
-            decryptedLinks.add(createDownloadlink(link));
-        } else if (br.containsHTML("<div class=\"items-page result-page")) {
-            final String[] links = br.getRegex("href=\"(/\\w+:\\d+)\" class=\"card-img-holder\"").getColumn(0);
-            if (links != null && links.length > 0) {
-                for (String link : links) {
-                    link = br.getURL(Encoding.htmlOnlyDecode(link)).toString();
-                    decryptedLinks.add(createDownloadlink(link));
-                }
+        } else if (StringUtils.containsIgnoreCase(br.getURL(), "/make:")) {
+            // a make
+            final String thingID = br.getRegex("href=\"/thing:(\\d+)\"\\s*class=\"card-img-holder\"").getMatch(0);
+            if (thingID != null) {
+                final DownloadLink thing = createDownloadlink("https://www.thingiverse.com/thing:" + thingID);
+                decryptedLinks.add(thing);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
-            if (fpName != null) {
-                final FilePackage fp = FilePackage.getInstance();
-                fp.setName(Encoding.htmlDecode(fpName.trim()));
-                fp.addLinks(decryptedLinks);
-            }
+        } else {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
         }
         return decryptedLinks;
     }
