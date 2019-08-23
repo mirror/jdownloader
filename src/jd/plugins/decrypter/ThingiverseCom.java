7f8b89945bd3e392bae71f287ab2c247212f0708
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-23 10:47:40 +0000

    ThingiverseCom: -Added support for result lists
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41149 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: bac201aac0beb35f4fd5e30add66f40b3c45104d

--- src/jd/plugins/decrypter/ThingiverseCom.java
+++ src/jd/plugins/decrypter/ThingiverseCom.java
@@ -2,17 +2,20 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 
-import org.appwork.utils.Regex;
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
-@DecrypterPlugin(revision = "$Revision: 41147 $", interfaceVersion = 2, names = { "thingiverse.com" }, urls = { "https?://(www\\.)?thingiverse\\.com/thing:\\d+" })
+@DecrypterPlugin(revision = "$Revision: 41147 $", interfaceVersion = 2, names = { "thingiverse.com" }, urls = { "https?://(www\\.)?thingiverse\\.com/.*" })
 public class ThingiverseCom extends antiDDoSForDecrypt {
     public ThingiverseCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -20,17 +23,39 @@ public class ThingiverseCom extends antiDDoSForDecrypt {
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString();
+        String parameter = param.toString().replaceAll("/about($|\\?.*)", "/designs");
+        if (StringUtils.containsIgnoreCase(parameter, "/groups/")) {
+            parameter = param.toString().replaceAll("/(forums|members|about)($|\\?.*)", "/designs");
+        }
         br.setFollowRedirects(true);
         getPage(parameter);
         String fpName = br.getRegex("<title>\\s*([^<]+?)\\s*-\\s*Thingiverse").getMatch(0);
-        final String fid = new Regex(parameter, "thing:(\\d+).*").getMatch(0);
-        final DownloadLink link = createDownloadlink("directhttp://https://www.thingiverse.com/thing:" + fid + "/zip");
-        if (fpName != null) {
-            fpName = Encoding.htmlOnlyDecode(fpName);
-            link.setFinalFileName(fpName + ".zip");
+        if (parameter.contains("thing:")) {
+            final String fid = new Regex(parameter, "(\\d+).*").getMatch(0);
+            final DownloadLink link = createDownloadlink("directhttp://https://www.thingiverse.com/thing:" + fid + "/zip");
+            if (fpName != null) {
+                fpName = Encoding.htmlOnlyDecode(fpName);
+                link.setFinalFileName(fpName + ".zip");
+            }
+            decryptedLinks.add(link);
+        } else if (parameter.contains("make:")) {
+            String link = br.getRegex("href=\"(/\\w+:\\d+)\" class=\"card-img-holder\"").getMatch(0);
+            link = br.getURL(Encoding.htmlOnlyDecode(link)).toString();
+            decryptedLinks.add(createDownloadlink(link));
+        } else if (br.containsHTML("<div class=\"items-page result-page")) {
+            final String[] links = br.getRegex("href=\"(/\\w+:\\d+)\" class=\"card-img-holder\"").getColumn(0);
+            if (links != null && links.length > 0) {
+                for (String link : links) {
+                    link = br.getURL(Encoding.htmlOnlyDecode(link)).toString();
+                    decryptedLinks.add(createDownloadlink(link));
+                }
+            }
+            if (fpName != null) {
+                final FilePackage fp = FilePackage.getInstance();
+                fp.setName(Encoding.htmlDecode(fpName.trim()));
+                fp.addLinks(decryptedLinks);
+            }
         }
-        decryptedLinks.add(link);
         return decryptedLinks;
     }
 }
