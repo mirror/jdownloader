90a90e70bb0b2196f5bd1ff1d5540ea916bad89e
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-08-29 01:44:32 +0000

    TopDocumentaryFilms.com -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41181 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: b34cc45bf31ce683443b2ac56126705b82d726d3

---
+++ src/jd/plugins/decrypter/TopDocumentaryFilmsCom.java
@@ -0,0 +1,54 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision: 41180 $", interfaceVersion = 2, names = { "topdocumentaryfilms.com" }, urls = { "https?://(www\\.)?topdocumentaryfilms\\.com/[^/]+/" })
+public class TopDocumentaryFilmsCom extends antiDDoSForDecrypt {
+    public TopDocumentaryFilmsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        String page = br.toString();
+        String fpName = br.getRegex("<title>\\s*([^<]+)\\s*- Top Documentary Films").getMatch(0);
+        String link = br.getRegex("itemprop=\"embedUrl\"\\s+content=\"([^\"]+)\"").getMatch(0);
+        if (link != null && link.length() > 0) {
+            decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
