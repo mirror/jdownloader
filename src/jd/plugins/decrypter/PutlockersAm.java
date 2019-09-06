59773a2ca813c53a589a35a5043a276ae5c4309f
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-09-06 11:01:07 +0000

    vidcloud.icu -Added initial support putlockers.am -Now also able to handle updated version of the same CMS on putlockerhd.io fembed.com -Added support for alias gcloud.live
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41221 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1b4317c907fc89b51b46945ef0366de72151efe2

--- src/jd/plugins/decrypter/PutlockersAm.java
+++ src/jd/plugins/decrypter/PutlockersAm.java
@@ -28,7 +28,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision: 40114 $", interfaceVersion = 3, names = { "putlockers.am" }, urls = { "https?://(www[0-9]*\\.)?putlockers\\.am/watch/.*" })
+@DecrypterPlugin(revision = "$Revision: 41221 $", interfaceVersion = 3, names = { "putlockers.am" }, urls = { "https?://(www\\d*\\.)?(putlockers\\.am|putlockerhd\\.io)/(watch/|film/|streaming\\.php\\?|load\\.php\\?).+" })
 public class PutlockersAm extends PluginForDecrypt {
     public PutlockersAm(PluginWrapper wrapper) {
         super(wrapper);
@@ -41,10 +41,18 @@ public class PutlockersAm extends PluginForDecrypt {
         br.setFollowRedirects(true);
         br.getPage(parameter);
         String fpName = br.getRegex("<meta name=\"description\" content=\"(?:Watch )([^\"]+) Online \\|").getMatch(0);
-        String linkBlock = br.getRegex(" <div id=\"cont_player\">(.*)<div class=\"movies-list-wrap mlw-related\">").getMatch(0).toString();
-        String[][] links = new Regex(linkBlock, "href=\"([^\"]+)\"").getMatches();
-        for (String[] link : links) {
-            decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link[0])));
+        String linkBlock = br.getRegex("(<div id=\"list-eps\"[^>]*>[^$]+)<div class=\"(?:mvi-content|movies-list-wrap mlw-related)\"").getMatch(0);
+        if (linkBlock != null) {
+            String[] links = new Regex(linkBlock, "(?:href|player-data)=\"([^\"]+)\"").getColumn(0);
+            System.out.println(String.join("~~~", links));
+            for (String link : links) {
+                if (!link.contains("addthis.com") && !link.contains("disqus.com")) {
+                    if (link.startsWith("//")) {
+                        link = link.replace("//", "");
+                    }
+                    decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+                }
+            }
         }
         String iframe64 = br.getRegex("document\\.write\\(Base64\\.decode\\(\"([^\"]+)\"").getMatch(0);
         if (iframe64 != null) {
@@ -53,11 +61,12 @@ public class PutlockersAm extends PluginForDecrypt {
             if (iframeURL != null) {
                 decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(iframeURL)));
             }
-        } else {
-            String mediablock = br.getRegex("<div id=\"media-player\">(.*)</div>.*<div class=\"mvi-content\">").getMatch(0);
-            String mediaLinkURL = new Regex(mediablock, "href=\"([^\"]+)\"").getMatch(0);
-            if (mediaLinkURL != null) {
-                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(mediaLinkURL)));
+        }
+        String mediablock = br.getRegex("<div id=\"media-player\"[^>]+>\\s*([^$]*)\\s*<div id=\"(?:bar-player|list-eps)\"").getMatch(0);
+        if (mediablock != null) {
+            String[] mediaLinkURLs = new Regex(linkBlock, "(?:href|src|player-data)=\"([^\"]+)\"").getColumn(0);
+            if (mediaLinkURLs != null) {
+                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(mediaLinkURLs[0])));
             }
         }
         if (fpName != null) {
