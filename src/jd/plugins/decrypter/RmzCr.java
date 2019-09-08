3cb5b2ccc388d7707f462c72cff3a923ab246bd9
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-09-08 23:00:57 +0000

    rmz.cr -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41241 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/RmzCr.java
@@ -0,0 +1,64 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.appwork.utils.Regex;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision: 41221 $", interfaceVersion = 3, names = { "rmz.cr" }, urls = { "https?://(www\\.)?rmz\\.cr/(?:release/)?[^/]+" })
+public class RmzCr extends antiDDoSForDecrypt {
+    public RmzCr(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        String fpName = br.getRegex("<title>RapidMoviez\\s+-\\s+([^<]+)</title>").getMatch(0);
+        String linkBlock = br.getRegex("(<div id=\"(?:title_release_after_download_title|title_release_after_imdb)\">[^$]+<div id=\"title_release_after_links\">)").getMatch(0);
+        if (linkBlock != null) {
+            String[] links = new Regex(linkBlock, "href=\"([^\"]+)\"").getColumn(0);
+            if (links != null && links.length > 0) {
+                for (String link : links) {
+                    if (link.startsWith("/")) {
+                        link = br.getURL(link).toString();
+                    }
+                    decryptedLinks.add(createDownloadlink(link));
+                }
+            }
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.setProperty("ALLOW_MERGE", true);
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
