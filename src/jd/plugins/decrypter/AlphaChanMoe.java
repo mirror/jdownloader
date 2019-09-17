b7779582f86371ca050b174c05bb3463d0eec2b0
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-09-17 11:42:41 +0000

    fenixfansub.com -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41272 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2d4c76694636bda5b50423771fe12b5c94747e48

---
+++ src/jd/plugins/decrypter/AlphaChanMoe.java
@@ -0,0 +1,53 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+@DecrypterPlugin(revision = "$Revision: svn:revision $", interfaceVersion = 3, names = { "alphachan.moe" }, urls = { "https?://(?:www\\.)?alphachan\\.moe/(?:play2|player)/.+" })
+public class AlphaChanMoe extends antiDDoSForDecrypt {
+    public AlphaChanMoe(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        if (parameter.contains("/player/link/")) {
+            decryptedLinks.add(createDownloadlink("directhttp://" + parameter));
+            return decryptedLinks;
+        }
+        getPage(parameter);
+        String[] links = br.getRegex("<source[^>]+src=\"([^\"]+)\"").getColumn(0);
+        if (links != null && links.length > 0) {
+            for (String link : links) {
+                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+            }
+        }
+        return decryptedLinks;
+    }
+}
---
+++ src/jd/plugins/decrypter/FenixFansub.java
@@ -0,0 +1,58 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision: svn:revision $", interfaceVersion = 3, names = { "fenixfansub.com" }, urls = { "https?://(?:www\\.)?fenixfansub\\.com/animes/[^/]+/?" })
+public class FenixFansub extends antiDDoSForDecrypt {
+    public FenixFansub(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        final String fpName = br.getRegex("<title>\\s*([^<]+)\\s*\\|\\s*Fênix Fansub").getMatch(0);
+        String[] links = br.getRegex("<a[^>]+data-src=\"([^\"]+)\"").getColumn(0);
+        if (links != null && links.length > 0) {
+            for (String link : links) {
+                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+            }
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.setProperty("ALLOW_MERGE", true);
+            fp.setProperty("Referer", parameter);
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
