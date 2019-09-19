88fbefa64db57cb56db955622a5205156bde21ac
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-09-19 14:51:43 +0000

    porncomix.one -Support for alternate domain and styling fixes #86164
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41297 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/PornComixInfo.java
+++ src/jd/plugins/decrypter/PornComixInfo.java
@@ -10,22 +10,28 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "porncomix.info " }, urls = { "https?://(?:www\\.)?(?:porncomix\\.info|bestporncomix\\.com/gallery)/[a-zA-Z0-9\\-_]+/" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "porncomix.info " }, urls = { "https?://(?:www\\.)?(?:porncomix\\.info|(?:bestporncomix\\.com|porncomix\\.one)/gallery)/[a-zA-Z0-9\\-_]+/?" })
 public class PornComixInfo extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         br.setFollowRedirects(true);
         br.getPage(parameter.getCryptedUrl());
-        final String postTitle = br.getRegex("class=\"post-title\"\\s*>\\s*(.*?)\\s*</").getMatch(0);
-        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String postTitle = br.getRegex("class=\"post-title\"\\s*>\\s*(.*?)\\s*</").getMatch(0);
+        if (postTitle == null || postTitle.length() == 0) {
+            postTitle = br.getRegex("<h1\\s+class\\s*=\\s*\"post-title[^\"]*\"><a[^>]*>\\s*([^<]+)\\s*</a></h1>").getMatch(0);
+        }
         if (postTitle != null) {
             FilePackage fp = FilePackage.getInstance();
             fp.setName(Encoding.htmlDecode(postTitle));
-            final String[] images = br.getRegex("class='gallery-icon\\s*(?:portrait|landscape)'\\s*>\\s*<a.*?href='.*?'\\s*>\\s*<img[^>]+src=\"(https?://(?:www\\.)?\\w+\\.\\w+/.*?(jpe?g|gif|png))\"").getColumn(0);
+            String[] images = br.getRegex("class='gallery-icon\\s*(?:portrait|landscape)'\\s*>\\s*<a.*?href='.*?'\\s*>\\s*<img[^>]+src=\"(https?://(?:www\\.)?\\w+\\.\\w+/.*?(jpe?g|gif|png))\"").getColumn(0);
+            if (images == null || images.length == 0) {
+                images = br.getRegex("<img[^>]+data-jg-srcset\\s*=\\s*\"([^\",\\s]+)").getColumn(0);
+            }
             if (images != null) {
                 for (final String image : images) {
                     if (!isAbort()) {
-                        final String url = image.replaceFirst("(-\\d+x\\d+)\\.(jpe?g|gif|png)$", ".$2");
+                        final String url = Encoding.htmlDecode(image).replaceFirst("(-\\d+x\\d+)\\.(jpe?g|gif|png)$", ".$2");
                         final DownloadLink link = createDownloadlink(url);
                         link.setAvailable(true);
                         link.setContainerUrl(parameter.getCryptedUrl());
