e72c8b8924b83dce6f9f6e7966bac37f396f6136
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-09-28 11:18:15 +0000

    pornze.com -Added initial support, fixes #87478
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41347 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1a00205b30caff4c7ba332eabb4a5c865a8de7fa

---
+++ src/jd/plugins/decrypter/PornZeCom.java
@@ -0,0 +1,57 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "pornze.com" }, urls = { "https?://(www\\.)?pornze\\.com/[^/]+/?" })
+public class PornZeCom extends antiDDoSForDecrypt {
+    public PornZeCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        String fpName = null;
+        fpName = br.getRegex("<meta\\s*property\\s*=\\s*\"og:title\"[^>]+content\\s*=\\s*\"([^\"]+)\"[^>]+>").getMatch(0);
+        String[] links = br.getRegex("<iframe[^>]+src\\s*=\\s*\"([^\"]+)\"[^>]+class\\s*=\\s*\"[^\"]*wps-player[^\"]*\"[^>]*>").getColumn(0);
+        if (links != null) {
+            for (String link : links) {
+                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+            }
+        }
+        if (StringUtils.isNotEmpty(fpName)) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
