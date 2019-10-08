f30e004e897de4b4963ded25e485a9e6955d270a
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-10-08 18:17:42 +0000

    zipcomic.com -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41394 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/ZipComic.java
@@ -0,0 +1,60 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "zipcomic.com" }, urls = { "https?://(www\\.)?zipcomic\\.com/[^/]+/?" })
+public class ZipComic extends antiDDoSForDecrypt {
+    public ZipComic(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        br.setFollowRedirects(true);
+        getPage(parameter);
+        final String fpName = br.getRegex("<title>\\s*Read\\s+online,\\s+Download\\s+zip\\s+([^>]+)\\s+comic").getMatch(0);
+        final String[] links = br.getRegex("<a[^>]+href\\s*=\\s*[\"']*([^\"'>]+)[\"']*[^>]*><i[^>]+class\\s*=\\s*\"fa fa-download\"[^>]*>").getColumn(0);
+        if (links != null && links.length > 0) {
+            for (String link : links) {
+                if (new Regex(link, "^/[^/]").matches()) {
+                    link = "directhttp://" + br.getURL(link).toString();
+                }
+                decryptedLinks.add(createDownloadlink(Encoding.htmlDecode(link)));
+            }
+        }
+        if (StringUtils.isNotEmpty(fpName)) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
