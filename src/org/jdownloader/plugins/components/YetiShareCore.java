9516aef21144c655ece833fee80e73b872dba56c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-10-14 17:38:43 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41420 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/plugins/components/YetiShareCore.java
+++ src/org/jdownloader/plugins/components/YetiShareCore.java
@@ -24,11 +24,6 @@ import java.util.Locale;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Pattern;
 
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.formatter.SizeFormatter;
-import org.appwork.utils.formatter.TimeFormatter;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
-
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.http.Browser;
@@ -51,6 +46,11 @@ import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.plugins.components.UserAgents;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class YetiShareCore extends antiDDoSForHost {
@@ -200,8 +200,8 @@ public class YetiShareCore extends antiDDoSForHost {
 
     /**
      * @return true: Implies that website will show filename & filesize via website.tld/<fuid>~i <br />
-     *         Most YetiShare websites support this kind of linkcheck! </br>
-     *         false: Implies that website does NOT show filename & filesize via website.tld/<fuid>~i. <br />
+     *         Most YetiShare websites support this kind of linkcheck! </br> false: Implies that website does NOT show filename & filesize
+     *         via website.tld/<fuid>~i. <br />
      *         default: true
      */
     public boolean supports_availablecheck_over_info_page() {
@@ -252,9 +252,7 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * Enforces old, non-ajax login-method. </br>
-     * This is only rarely needed (e.g. badshare.io). </br>
-     * default = false
+     * Enforces old, non-ajax login-method. </br> This is only rarely needed (e.g. badshare.io). </br> default = false
      */
     protected boolean enforce_old_login_method() {
         return false;
@@ -680,17 +678,20 @@ public class YetiShareCore extends antiDDoSForHost {
 
     /** If overridden, make sure to make isDownloadlink compatible as well! */
     protected String getDllink(final Browser br) {
-        String dllink = br.getRegex("\"(https?://[A-Za-z0-9\\.\\-]+\\.[^/]+/[^<>\"]*?(?:\\?|\\&)download_token=[A-Za-z0-9]+[^<>\"]*?)\"").getMatch(0);
-        return dllink;
+        final String ret = br.getRegex("\"(https?://[A-Za-z0-9\\.\\-]+\\.[^/]+/[^<>\"]*?(?:\\?|\\&)download_token=[A-Za-z0-9]+[^<>\"]*?)\"").getMatch(0);
+        if (isDownloadlink(ret)) {
+            return ret;
+        } else if (ret != null) {
+            logger.info("isDownloadlink false:" + ret);
+            return null;
+        } else {
+            return null;
+        }
     }
 
     public boolean isDownloadlink(final String url) {
-        if (url == null) {
-            return false;
-        } else {
-            final boolean isdownloadlink = url.contains("download_token=");
-            return isdownloadlink;
-        }
+        final boolean ret = url != null && url.contains("download_token=");
+        return ret;
     }
 
     /** Returns unique id from inside URL - usually with this pattern: [A-Za-z0-9]+ */
