addbc32af3595b4593ef1c8ffa7cace706578623
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-10-17 16:23:10 +0000

    DouploadsNet: -added new domain -fixes thread 81894
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41438 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/DouploadsNet.java
+++ src/jd/plugins/hoster/DouploadsNet.java
@@ -43,7 +43,7 @@ public class DouploadsNet extends XFileSharingProBasic {
     private static List<String[]> getPluginDomains() {
         final List<String[]> ret = new ArrayList<String[]>();
         // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
-        ret.add(new String[] { "douploads.net", "douploads.com" });
+        ret.add(new String[] { "douploads.net", "douploads.com", "douploads.me" });
         return ret;
     }
 
