e7f8b7d966871663e7068afa6e79fed611b14aae
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-10-19 13:04:15 +0000

    *Plugins: Fixes/Changes/Maintenance* - megaupload2com: merged with dropmegacom fixes #87542
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [UnknownHostingScriptCore] shareonlineis RE: IRC
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41445 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ShareonlineIs.java
+++ src/jd/plugins/hoster/ShareonlineIs.java
@@ -1,5 +1,5 @@
 //jDownloader - Downloadmanager
-//Copyright (C) 2016  JD-Team support@jdownloader.org
+//Copyright (C) 2010  JD-Team support@jdownloader.org
 //
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@ package jd.plugins.hoster;
 
 import java.util.regex.Pattern;
 
-import org.jdownloader.plugins.components.YetiShareCore;
+import org.jdownloader.plugins.components.UnknownHostingScriptCore;
 
 import jd.PluginWrapper;
 import jd.plugins.Account;
@@ -25,33 +25,84 @@ import jd.plugins.Account.AccountType;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
-public class Megaupload2Com extends YetiShareCore {
-    public Megaupload2Com(PluginWrapper wrapper) {
+@HostPlugin(revision = "$Revision $", interfaceVersion = 2, names = {}, urls = {})
+public class ShareonlineIs extends UnknownHostingScriptCore {
+    public ShareonlineIs(PluginWrapper wrapper) {
         super(wrapper);
-        this.enablePremium(getPurchasePremiumURL());
+        this.enablePremium(super.getPurchasePremiumURL());
     }
 
     /**
-     * DEV NOTES YetiShare<br />
-     ****************************
      * mods: See overridden functions<br />
-     * limit-info: 2019-04-27: no limits at all <br />
-     * captchatype-info: 2019-04-27: null<br />
-     * other: <br />
+     * limit-info:<br />
+     * captchatype-info: null<br />
+     * other:<br />
      */
     /* 1st domain = current domain! */
-    public static String[] domains = new String[] { "megaupload2.com" };
+    public static String[] domains = new String[] { "share-online.is" };
+
+    @Override
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return false;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return false;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return false;
+        }
+    }
+
+    @Override
+    public int getMaxChunks(final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return 1;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return 1;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return 1;
+        }
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultaneousFreeAccountDownloads() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public boolean supports_availablecheck_via_api() {
+        return super.supports_availablecheck_via_api();
+    }
 
     public static String[] getAnnotationNames() {
         return new String[] { domains[0] };
     }
 
     /**
-     * returns the annotation pattern array: 'https?://(?:www\\.)?(?:domain1|domain2)/[A-Za-z0-9]+(?:/[^/<>]+)?'
+     * returns the annotation pattern array: 'https?://(?:www\\.)?(?:domain1|domain2)/[A-Za-z0-9]+'
+     *
+     */
+    /**
+     * returns the annotation pattern array: 'https?://(?:www\\.)?(?:domain1|domain2)/[A-Za-z0-9]+(?:/[^/]+)?'
      *
      */
     public static String[] getAnnotationUrls() {
+        // construct pattern
         final String host = getHostsPattern();
         return new String[] { host + "/[A-Za-z0-9]+(?:/[^/<>]+)?" };
     }
@@ -75,46 +126,4 @@ public class Megaupload2Com extends YetiShareCore {
     public String[] siteSupportedNames() {
         return domains;
     }
-
-    @Override
-    public boolean isResumeable(final DownloadLink link, final Account account) {
-        if (account != null && account.getType() == AccountType.FREE) {
-            /* Free Account */
-            return true;
-        } else if (account != null && account.getType() == AccountType.PREMIUM) {
-            /* Premium account */
-            return true;
-        } else {
-            /* Free(anonymous) and unknown account type */
-            return true;
-        }
-    }
-
-    public int getMaxChunks(final Account account) {
-        if (account != null && account.getType() == AccountType.FREE) {
-            /* Free Account */
-            return 0;
-        } else if (account != null && account.getType() == AccountType.PREMIUM) {
-            /* Premium account */
-            return 0;
-        } else {
-            /* Free(anonymous) and unknown account type */
-            return 0;
-        }
-    }
-
-    @Override
-    public int getMaxSimultanFreeDownloadNum() {
-        return -1;
-    }
-
-    public int getMaxSimultaneousFreeAccountDownloads() {
-        return -1;
-    }
-
-    @Override
-    public int getMaxSimultanPremiumDownloadNum() {
-        return -1;
-    }
-
-    }
+}
