28378317d784e12ddf4cc965a8f5424ac7db47ce
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2019-10-28 14:14:19 +0000

    4downfiles.co Domains, https://board.jdownloader.org/showthread.php?t=81839
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41478 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: bab10b88e8b792524e291aafaad5801b74e55850

--- src/jd/plugins/hoster/FourDownFilesOrg.java
+++ src/jd/plugins/hoster/FourDownFilesOrg.java
@@ -43,7 +43,7 @@ public class FourDownFilesOrg extends XFileSharingProBasic {
     public static List<String[]> getPluginDomains() {
         final List<String[]> ret = new ArrayList<String[]>();
         // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
-        ret.add(new String[] { "4downfile.org", "4downfiles.org", "4downfiles.com", "4downfiles.net" });
+        ret.add(new String[] { "4downfile.org", "4downfiles.org", "4downfiles.com", "4downfiles.co", "4downfiles.net" });
         return ret;
     }
 
