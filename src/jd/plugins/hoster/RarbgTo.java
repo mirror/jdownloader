c175ac9907eb9d0aade7c9e186fdcdab32111a0b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-01-03 23:50:10 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - rbgto RE: IRC
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41657 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 52e93bdae61dc59a4abddd0b6000d17566dd1319

---
+++ src/jd/plugins/hoster/RarbgTo.java
@@ -0,0 +1,230 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForHost;
+
+import jd.PluginWrapper;
+import jd.config.Property;
+import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.Account.AccountType;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class RarbgTo extends antiDDoSForHost {
+    public RarbgTo(PluginWrapper wrapper) {
+        super(wrapper);
+        /* 2019-01-04: Try to avoid triggering their anti-spam measures! */
+        this.setStartIntervall(10 * 1000l);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://rarbg.to/";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "rarbg.to" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/torrent/([a-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME       = false;
+    private static final int     FREE_MAXCHUNKS    = 1;
+    private static final int     FREE_MAXDOWNLOADS = 1;
+
+    // private static final boolean ACCOUNT_FREE_RESUME = true;
+    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
+    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
+    //
+    // /* don't touch the following! */
+    // private static AtomicInteger maxPrem = new AtomicInteger(1);
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        return requestFileInformation(link, false);
+    }
+
+    private AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
+        br.setFollowRedirects(true);
+        if (!isDownload) {
+            /* Avoid triggering their spam protection! Also all added URLs are usually online! */
+            link.setName(this.getFID(link) + ".torrent");
+            return AvailableStatus.TRUE;
+        }
+        getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404 || br.containsHTML(">\\s*No such torrent")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br.getURL().contains("threat_defence.php")) {
+            /* 2019-01-04: TODO: Maybe add support for this (redirect + simple captcha) */
+            // final String js_sk = br.getRegex("var value_sk\\s*=\\s*'([a-z0-9]+)';").getMatch(0);
+            // final String js_c = br.getRegex("var value_c\\s*=\\s*'([0-9]+)';").getMatch(0);
+            // final String js_i = br.getRegex("var value_i\\s*=\\s*'([0-9]+)';").getMatch(0);
+            // if (js_sk != null) {
+            // br.setCookie(br.getURL(), "sk", js_sk);
+            // }
+            // final String continueURL = br.getRegex("(/threat_defence\\.php\\?defence=nojc\\&r=\\d+)").getMatch(0);
+            // if (continueURL != null) {
+            // this.getPage(continueURL);
+            // }
+            throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "Anti-spam was triggered", 30 * 60 * 1000l);
+        }
+        boolean setFinalFilename = true;
+        String filename = br.getRegex("\\&f=([^<>\"]+)").getMatch(0);
+        if (StringUtils.isEmpty(filename)) {
+            /* Fallback */
+            filename = this.getFID(link);
+            setFinalFilename = false;
+        }
+        if (!filename.endsWith(".torrent")) {
+            filename += ".torrent";
+        }
+        filename = Encoding.htmlDecode(filename).trim();
+        if (setFinalFilename) {
+            link.setFinalFileName(filename);
+        } else {
+            link.setName(filename);
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link, true);
+        doFree(link, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink link, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        String dllink = checkDirectLink(link, directlinkproperty);
+        if (dllink == null) {
+            dllink = br.getRegex("(/download\\.php[^<>\"]+)").getMatch(0);
+            if (StringUtils.isEmpty(dllink)) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resumable, maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            }
+            br.followConnection();
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        link.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
+        dl.startDownload();
+    }
+
+    private String checkDirectLink(final DownloadLink downloadLink, final String property) {
+        String dllink = downloadLink.getStringProperty(property);
+        if (dllink != null) {
+            URLConnectionAdapter con = null;
+            try {
+                final Browser br2 = br.cloneBrowser();
+                br2.setFollowRedirects(true);
+                con = br2.openHeadConnection(dllink);
+                if (con.getContentType().contains("text") || !con.isOK() || con.getLongContentLength() == -1) {
+                    downloadLink.setProperty(property, Property.NULL);
+                    dllink = null;
+                }
+            } catch (final Exception e) {
+                logger.log(e);
+                downloadLink.setProperty(property, Property.NULL);
+                dllink = null;
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
+        }
+        return dllink;
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        if (acc == null) {
+            /* no account, yes we can expect captcha */
+            return true;
+        }
+        if (acc.getType() == AccountType.FREE) {
+            /* Free accounts can have captchas */
+            return true;
+        }
+        /* Premium accounts do not have captchas */
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
