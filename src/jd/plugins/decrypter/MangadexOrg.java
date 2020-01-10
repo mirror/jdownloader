21e6b1844a3b45d2d6e6b26895d39e2037c13a09
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-01-10 06:34:57 +0000

    Mangadex.org - Support .cc, https://board.jdownloader.org/showthread.php?t=82722
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41671 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MangadexOrg.java
+++ src/jd/plugins/decrypter/MangadexOrg.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.plugins.CryptedLink;
@@ -14,13 +20,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangadex.org" }, urls = { "https?://(www\\.)?mangadex\\.org/chapter/\\d+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangadex.org" }, urls = { "https?://(www\\.)?mangadex\\.(org|cc)/chapter/\\d+" })
 public class MangadexOrg extends antiDDoSForDecrypt {
     public MangadexOrg(PluginWrapper wrapper) {
         super(wrapper);
@@ -30,7 +30,7 @@ public class MangadexOrg extends antiDDoSForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         final String chapterID = new Regex(parameter, "/chapter/(\\d+)").getMatch(0);
-        getPage("https://mangadex.org/api/?id=" + chapterID + "&type=chapter");
+        getPage("https://mangadex.cc/api/?id=" + chapterID + "&type=chapter");
         final Map<String, Object> map = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
         final String hash = (String) map.get("hash");
         final String server = (String) map.get("server");
@@ -43,7 +43,7 @@ public class MangadexOrg extends antiDDoSForDecrypt {
         final StringBuilder sb = new StringBuilder();
         final String mangaTitle;
         if (StringUtils.isNotEmpty(mangaID)) {
-            getPage("https://mangadex.org/api/?id=" + mangaID + "&type=manga");
+            getPage("https://mangadex.cc/api/?id=" + mangaID + "&type=manga");
             final Map<String, Object> manga = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
             mangaTitle = (String) ((Map<String, Object>) manga.get("manga")).get("title");
             if (StringUtils.isNotEmpty(mangaTitle)) {
