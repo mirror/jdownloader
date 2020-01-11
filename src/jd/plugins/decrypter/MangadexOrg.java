59f3967bfcd28db08304fa5ce3eb068ef0f14101
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-01-11 03:30:08 +0000

    mangadex.org -Added basic support for title overview pages (only had chapter support before).
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41672 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MangadexOrg.java
+++ src/jd/plugins/decrypter/MangadexOrg.java
@@ -20,7 +20,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangadex.org" }, urls = { "https?://(www\\.)?mangadex\\.(org|cc)/chapter/\\d+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mangadex.org" }, urls = { "https?://(www\\.)?mangadex\\.cc/(title|chapter)/\\d+" })
 public class MangadexOrg extends antiDDoSForDecrypt {
     public MangadexOrg(PluginWrapper wrapper) {
         super(wrapper);
@@ -28,82 +28,97 @@ public class MangadexOrg extends antiDDoSForDecrypt {
 
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        br.setFollowRedirects(true);
+        getPage(parameter.getCryptedUrl());
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        final String chapterID = new Regex(parameter, "/chapter/(\\d+)").getMatch(0);
-        getPage("https://mangadex.cc/api/?id=" + chapterID + "&type=chapter");
-        final Map<String, Object> map = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
-        final String hash = (String) map.get("hash");
-        final String server = (String) map.get("server");
-        if (StringUtils.isEmpty(server)) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        } else if (StringUtils.isEmpty(hash)) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        final String mangaID = map.get("manga_id") != null ? String.valueOf(map.get("manga_id")) : null;
-        final StringBuilder sb = new StringBuilder();
-        final String mangaTitle;
-        if (StringUtils.isNotEmpty(mangaID)) {
-            getPage("https://mangadex.cc/api/?id=" + mangaID + "&type=manga");
-            final Map<String, Object> manga = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
-            mangaTitle = (String) ((Map<String, Object>) manga.get("manga")).get("title");
-            if (StringUtils.isNotEmpty(mangaTitle)) {
-                sb.append(mangaTitle);
-            }
-        } else {
-            mangaTitle = null;
-        }
-        final String volume = (String) map.get("volume");
-        final String chapter = (String) map.get("chapter");
-        String title = (String) map.get("title");
-        final FilePackage fp = FilePackage.getInstance();
-        if (StringUtils.isNotEmpty(volume)) {
-            if (sb.length() > 0) {
-                sb.append("-");
-            }
-            sb.append("Vol_");
-            sb.append(volume);
-        }
-        if (StringUtils.isNotEmpty(chapter)) {
-            if (sb.length() > 0) {
-                sb.append("-");
-            }
-            sb.append("Ch_");
-            sb.append(chapter);
-        }
-        if (StringUtils.isNotEmpty(title)) {
-            if (sb.length() > 0) {
-                sb.append("-");
-            }
-            sb.append(title);
-        } else if (StringUtils.isNotEmpty(mangaTitle)) {
-            title = mangaTitle;
-        } else {
-            title = "";
-        }
-        fp.setName(sb.toString());
-        final List<Object> page_array = (List<Object>) map.get("page_array");
-        if (page_array == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        int pageIndex = 0;
-        for (Object page : page_array) {
-            pageIndex++;
-            if (page == null) {
-                continue;
-            } else if (page instanceof String) {
-                final String url = br.getURL(server + hash + "/" + page).toString();
-                final DownloadLink link = createDownloadlink("directhttp://" + url);
-                link.setProperty("refURL", parameter.getCryptedUrl());
-                link.setAvailable(true);
-                if (title != null) {
-                    link.setFinalFileName(title + "-Page_" + pageIndex + Plugin.getFileNameExtensionFromURL(url));
+        if (StringUtils.containsIgnoreCase(parameter.getCryptedUrl(), "/title/")) {
+            String[] chapters = br.getRegex("<a[^>]+href\\s*=\\s*[\"']([^\"']*/chapter/[^\"'/]*)").getColumn(0);
+            if (chapters != null && chapters.length > 0) {
+                for (String chapter : chapters) {
+                    chapter = chapter.trim().replaceAll("^//", "https://");
+                    if (chapter.startsWith("/") || !chapter.startsWith("http")) {
+                        chapter = br.getURL(chapter).toString();
+                    }
+                    ret.add(createDownloadlink(chapter));
                 }
-                link.setContentUrl(parameter.getCryptedUrl() + "/" + pageIndex);
-                link.setLinkID(getHost() + "://" + chapterID + "/" + page);
-                fp.add(link);
-                distribute(link);
-            } else {
+            }
+        } else if (StringUtils.containsIgnoreCase(parameter.getCryptedUrl(), "/chapter/")) {
+            final String chapterID = new Regex(parameter, "/chapter/(\\d+)").getMatch(0);
+            getPage("/api/?id=" + chapterID + "&type=chapter");
+            final Map<String, Object> map = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+            final String hash = (String) map.get("hash");
+            final String server = (String) map.get("server");
+            if (StringUtils.isEmpty(server)) {
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            } else if (StringUtils.isEmpty(hash)) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            final String mangaID = map.get("manga_id") != null ? String.valueOf(map.get("manga_id")) : null;
+            final StringBuilder sb = new StringBuilder();
+            final String mangaTitle;
+            if (StringUtils.isNotEmpty(mangaID)) {
+                getPage("/api/?id=" + mangaID + "&type=manga");
+                final Map<String, Object> manga = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+                mangaTitle = (String) ((Map<String, Object>) manga.get("manga")).get("title");
+                if (StringUtils.isNotEmpty(mangaTitle)) {
+                    sb.append(mangaTitle);
+                }
+            } else {
+                mangaTitle = null;
+            }
+            final String volume = (String) map.get("volume");
+            final String chapter = (String) map.get("chapter");
+            String title = (String) map.get("title");
+            final FilePackage fp = FilePackage.getInstance();
+            if (StringUtils.isNotEmpty(volume)) {
+                if (sb.length() > 0) {
+                    sb.append("-");
+                }
+                sb.append("Vol_");
+                sb.append(volume);
+            }
+            if (StringUtils.isNotEmpty(chapter)) {
+                if (sb.length() > 0) {
+                    sb.append("-");
+                }
+                sb.append("Ch_");
+                sb.append(chapter);
+            }
+            if (StringUtils.isNotEmpty(title)) {
+                if (sb.length() > 0) {
+                    sb.append("-");
+                }
+                sb.append(title);
+            } else if (StringUtils.isNotEmpty(mangaTitle)) {
+                title = mangaTitle;
+            } else {
+                title = "";
+            }
+            fp.setName(sb.toString());
+            final List<Object> page_array = (List<Object>) map.get("page_array");
+            if (page_array == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            int pageIndex = 0;
+            for (Object page : page_array) {
+                pageIndex++;
+                if (page == null) {
+                    continue;
+                } else if (page instanceof String) {
+                    final String url = br.getURL(server + hash + "/" + page).toString();
+                    final DownloadLink link = createDownloadlink("directhttp://" + url);
+                    link.setProperty("refURL", parameter.getCryptedUrl());
+                    link.setAvailable(true);
+                    if (title != null) {
+                        link.setFinalFileName(title + "-Page_" + pageIndex + Plugin.getFileNameExtensionFromURL(url));
+                    }
+                    link.setContentUrl(parameter.getCryptedUrl() + "/" + pageIndex);
+                    link.setLinkID(getHost() + "://" + chapterID + "/" + page);
+                    fp.add(link);
+                    distribute(link);
+                } else {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
             }
         }
         return ret;
