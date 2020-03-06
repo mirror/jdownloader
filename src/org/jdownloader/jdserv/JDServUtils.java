607c54c5c54f82360280d8a416d465be47ad2904
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-03-06 17:28:41 +0000

    JDServUtils: -use FormData.FILE for upload to avoid OutOfMemoryException for large logs -fixed wrong exception handling -Browser is now set to Verbose mode build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41853 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/jdserv/JDServUtils.java
+++ src/org/jdownloader/jdserv/JDServUtils.java
@@ -12,7 +12,6 @@ import jd.nutils.encoding.Encoding;
 
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
-import org.appwork.utils.IO;
 
 public class JDServUtils {
     // private static String BASE = "http://nas:81/thomas/fcgi/"
@@ -22,29 +21,29 @@ public class JDServUtils {
         return uploadLog(zip, "", id);
     }
 
-    public static String uploadLog(File zip, String name, String id) throws IOException { //
-        return upload(IO.readFile(zip), name, id);
-    }
-
-    public static String upload(byte[] bytes, String name, String id) {
+    public static String uploadLog(final File zip, String name, String id) throws IOException { //
         final Browser br = new Browser();
-        String url = BASE + "upload";
+        br.setVerbose(true);
+        final String url = BASE + "upload";
         br.setProxySelector(ProxyController.getInstance());
         URLConnectionAdapter con = null;
         try {
             final PostFormDataRequest req = br.createPostFormDataRequest(url);
-            req.addFormData(new FormData("0", "Data", bytes));
+            req.addFormData(new FormData("0", "Data", zip));
             req.addFormData(new FormData("1", Encoding.urlEncode(JSonStorage.serializeToJson(name))));
             req.addFormData(new FormData("2", Encoding.urlEncode(JSonStorage.serializeToJson(id))));
             con = br.openRequestConnection(req);
             final String response = br.loadConnection(con).getHtmlCode();
             final String string = JSonStorage.restoreFromString(response, TypeRef.STRING);
             return string;
+        } catch (final IOException e) {
+            throw e;
         } catch (final Exception e) {
             if (e instanceof RuntimeException) {
                 throw (RuntimeException) e;
+            } else {
+                throw new IOException(e);
             }
-            throw new RuntimeException(e);
         } finally {
             if (con != null) {
                 con.disconnect();
