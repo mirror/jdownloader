6628dacdb9ab4c615ed952b5a5d31595d1eb6a39
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-03-09 17:08:28 +0000

    *Plugins: Fixes/Changes/Maintenance* - vikicom: fixed and improved errorhandling fixes #84311
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41859 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/VikiCom.java
+++ src/jd/plugins/hoster/VikiCom.java
@@ -17,11 +17,18 @@ package jd.plugins.hoster;
 
 import java.util.LinkedHashMap;
 
+import org.appwork.utils.StringUtils;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.macs.HMac;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.http.Browser;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
+import jd.plugins.AccountRequiredException;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
@@ -30,11 +37,6 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.utils.JDHexUtils;
 
-import org.bouncycastle.crypto.digests.SHA1Digest;
-import org.bouncycastle.crypto.macs.HMac;
-import org.bouncycastle.crypto.params.KeyParameter;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "viki.com" }, urls = { "https?://(www\\.)?viki\\.(com|mx|jp)/videos/\\d+v" })
 public class VikiCom extends PluginForHost {
     public VikiCom(PluginWrapper wrapper) {
@@ -42,16 +44,19 @@ public class VikiCom extends PluginForHost {
     }
 
     /* Extension which will be used if no correct extension is found */
-    private static final String  default_Extension = ".mp4";
+    private static final String  default_Extension       = ".mp4";
     /* Connection stuff */
-    private static final boolean free_resume       = true;
-    private static final int     free_maxchunks    = 0;
-    private static final int     free_maxdownloads = -1;
-    private String               dllink            = null;
-    private boolean              server_issues     = false;
-    private boolean              geoblocked        = false;
-    private static final String  APP_ID            = "100005a";
-    private static final String  APP_SECRET        = "MM_d*yP@`&1@]@!AVrXf_o-HVEnoTnm$O-ti4[G~$JDI/Dc-&piU&z&5.;:}95=Iad";
+    private static final boolean free_resume             = true;
+    private static final int     free_maxchunks          = 0;
+    private static final int     free_maxdownloads       = -1;
+    private String               dllink                  = null;
+    private boolean              server_issues           = false;
+    private boolean              blocking_geoblocked     = false;
+    private boolean              blocking_paywall        = false;
+    private boolean              blocking_notyetreleased = false;
+    private static final String  APP_ID                  = "100005a";
+    private static final String  APP_SECRET              = "MM_d*yP@`&1@]@!AVrXf_o-HVEnoTnm$O-ti4[G~$JDI/Dc-&piU&z&5.;:}95=Iad";
+    private static final String  API_BASE                = "https://www.viki.com/api";
 
     @Override
     public String getAGBLink() {
@@ -63,6 +68,11 @@ public class VikiCom extends PluginForHost {
         link.setUrlDownload("https://www.viki.com/videos/" + getVID(link));
     }
 
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        return requestFileInformation(link, false);
+    }
+
     /** Thanks for the html5 idea guys: https://github.com/rg3/youtube-dl/blob/master/youtube_dl/extractor/viki.py */
     /**
      * To get more/better qualities and streaming types (protocols) we'd have to use the API: http://dev.viki.com/. Unfortunately the call
@@ -72,55 +82,50 @@ public class VikiCom extends PluginForHost {
      *
      * @throws Exception
      */
-    @SuppressWarnings("deprecation")
-    @Override
-    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
-        geoblocked = false;
-        final String vid = getVID(downloadLink);
-        downloadLink.setName(vid);
+    public AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
+        blocking_geoblocked = false;
+        final String vid = getVID(link);
+        link.setName(vid);
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        br.setAllowedResponseCodes(410);
-        br.getPage(downloadLink.getDownloadURL());
+        br.setAllowedResponseCodes(new int[] { 410 });
+        br.getPage(API_BASE + "/videos/" + this.getVID(link));
         if (br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 410) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        String filename = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
-        if (filename == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        entries = (LinkedHashMap<String, Object>) entries.get("video");
+        // final ArrayList<Object> ressourcelist = (ArrayList<Object>) entries.get("");
+        String filename = (String) JavaScriptEngineFactory.walkJson(entries, "container/i18n_title");
+        blocking_geoblocked = ((Boolean) JavaScriptEngineFactory.walkJson(entries, "blocking/geo")).booleanValue();
+        blocking_paywall = ((Boolean) JavaScriptEngineFactory.walkJson(entries, "blocking/paywall")).booleanValue();
+        blocking_notyetreleased = ((Boolean) JavaScriptEngineFactory.walkJson(entries, "blocking/upcoming")).booleanValue();
+        if (!StringUtils.isEmpty(filename)) {
+            /* 2020-03-09: Prevent duplicated filenames */
+            filename = this.getVID(link) + "_" + filename;
+        } else {
+            /* Fallback */
+            filename = this.getVID(link);
         }
         filename = Encoding.htmlDecode(filename);
         filename = filename.trim();
         filename = encodeUnicode(filename);
-        LinkedHashMap<String, Object> entries = null;
-        if (br.containsHTML("\"geo\":true")) {
-            geoblocked = true;
-        } else {
-            if (br.containsHTML("Viki Pass does not apply on these Channels.")) {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            }
-            br.getPage("https://www.viki.com/player5_fragment/" + vid + "?action=show&controller=videos");
-            if (br.containsHTML("this content is not available")) {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            }
-            final String videoJson = br.getRegex("var video =(\\{.+?\\});").getMatch(0);
-            if (videoJson != null) {
-                entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(videoJson);
-                geoblocked = ((Boolean) JavaScriptEngineFactory.walkJson(entries, "blocking/geo")).booleanValue();
-            }
-        }
-        if (geoblocked) {
-            downloadLink.getLinkStatus().setStatusText("Not available in your country (geo block)");
-            downloadLink.setName("Geo blocked! " + filename + default_Extension);
+        if (blocking_geoblocked) {
+            link.setName("GEO_BLOCKED_" + filename + default_Extension);
+            return AvailableStatus.TRUE;
+        } else if (blocking_paywall) {
+            link.setName("PAYWALL_" + filename + default_Extension);
+            return AvailableStatus.TRUE;
+        } else if (blocking_notyetreleased) {
+            link.setName("NOT_YET_RELEASED" + filename + default_Extension);
             return AvailableStatus.TRUE;
         }
+        br.getPage("https://www.viki.com/player5_fragment/" + vid + "?action=show&controller=videos");
         server_issues = this.br.containsHTML("Video playback for this video is not supported by your browser");
         /* Should never happen */
-        if (br.containsHTML("Video is Unavailable")) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        } else if (server_issues) {
-            downloadLink.getLinkStatus().setStatusText("Linktype not yet supported");
-            downloadLink.setName(filename + default_Extension);
+        if (server_issues) {
+            link.getLinkStatus().setStatusText("Linktype not yet supported");
+            link.setName(filename + default_Extension);
             return AvailableStatus.TRUE;
         }
         String idpart = this.br.getMatch("oster=\"https?://[^/]+/videos/\\d+v/[^_]+_(\\d+)_");
@@ -137,7 +142,7 @@ public class VikiCom extends PluginForHost {
             cbr.getPage(apiUrl);
             LinkedHashMap<String, Object> jsonEntries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(cbr.toString());
             String url = null;
-            for (String quality : new String[] { "480p", "380p", "240p" }) {
+            for (String quality : new String[] { "1080p", "720p", "480p", "380p", "240p" }) {
                 url = (String) JavaScriptEngineFactory.walkJson(jsonEntries, quality + "/https/url");
                 if (url == null) {
                     url = (String) JavaScriptEngineFactory.walkJson(jsonEntries, quality + "/http/url");
@@ -178,30 +183,32 @@ public class VikiCom extends PluginForHost {
         }
         if (dllink != null) {
             dllink = Encoding.htmlDecode(dllink);
-            downloadLink.setFinalFileName(filename);
-            final Browser br2 = br.cloneBrowser();
-            // In case the link redirects to the finallink
-            br2.setFollowRedirects(true);
-            URLConnectionAdapter con = null;
-            try {
-                con = br2.openHeadConnection(dllink);
-                if (con.isOK() && !con.getContentType().contains("html")) {
-                    if (con.getCompleteContentLength() > 0) {
-                        downloadLink.setDownloadSize(con.getCompleteContentLength());
-                    }
-                    downloadLink.setProperty("directlink", dllink);
-                } else {
-                    server_issues = true;
-                }
-            } finally {
+            link.setFinalFileName(filename);
+            if (!isDownload) {
+                final Browser br2 = br.cloneBrowser();
+                // In case the link redirects to the finallink
+                br2.setFollowRedirects(true);
+                URLConnectionAdapter con = null;
                 try {
-                    con.disconnect();
-                } catch (final Throwable e) {
+                    con = br2.openHeadConnection(dllink);
+                    if (con.isOK() && !con.getContentType().contains("html")) {
+                        if (con.getCompleteContentLength() > 0) {
+                            link.setDownloadSize(con.getCompleteContentLength());
+                        }
+                        link.setProperty("directlink", dllink);
+                    } else {
+                        server_issues = true;
+                    }
+                } finally {
+                    try {
+                        con.disconnect();
+                    } catch (final Throwable e) {
+                    }
                 }
             }
         } else {
             /* We cannot be sure whether we have the correct extension or not! */
-            downloadLink.setName(filename);
+            link.setName(filename);
         }
         return AvailableStatus.TRUE;
     }
@@ -217,16 +224,16 @@ public class VikiCom extends PluginForHost {
     }
 
     @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception {
-        requestFileInformation(downloadLink);
-        if (geoblocked) {
-            throw new PluginException(LinkStatus.ERROR_FATAL, "Not available in your country (geo blocked)");
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link, true);
+        if (blocking_geoblocked || blocking_paywall || blocking_notyetreleased) {
+            throw new AccountRequiredException();
         } else if (server_issues) {
             throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error");
         } else if (dllink == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, free_resume, free_maxchunks);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, free_resume, free_maxchunks);
         if (dl.getConnection().getContentType().contains("html")) {
             if (dl.getConnection().getResponseCode() == 403) {
                 throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
