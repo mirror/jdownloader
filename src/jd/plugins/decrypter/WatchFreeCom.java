84bc5f71cd2e50e695b1e04d890b17cae546247b
tony2long <tony2long@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-03-11 09:51:20 +0000

    WatchFreeCom decrypter
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41871 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/WatchFreeCom.java
+++ src/jd/plugins/decrypter/WatchFreeCom.java
@@ -13,7 +13,6 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
@@ -23,12 +22,13 @@ import jd.controlling.ProgressController;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "watchfree.to" }, urls = { "http://(www\\.)?watchfree\\.(to|com)/.+\\.html" })
-public class WatchFreeTo extends PluginForDecrypt {
-
-    public WatchFreeTo(PluginWrapper wrapper) {
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "watchfree.com" }, urls = { "https?://(www\\.)?watchfree\\.com/video/.+\\.html" })
+public class WatchFreeCom extends PluginForDecrypt {
+    public WatchFreeCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
@@ -36,19 +36,26 @@ public class WatchFreeTo extends PluginForDecrypt {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         br.getPage(parameter);
-        if (br.containsHTML(">Um, Where did the page go|You either took a wrong turn or the site is screwed")) {
-            logger.info("Link offline: " + parameter);
+        if (br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404 || br.containsHTML("404 Not Found<|Page not found")) {
+            decryptedLinks.add(createOfflinelink(parameter));
             return decryptedLinks;
         }
-        if (br.containsHTML("var locations")) {
-            final String locations = br.getRegex("var locations = \\[\"([^<>\"]*?)\"").getMatch(0);
-            final String finallink = locations.replace("\\/", "/");
-            if (finallink == null) {
-                logger.warning("Decrypter broken 1 for link: " + parameter);
-                return null;
+        String iframe = br.getRegex("iframe src=(?:'|\")([^'\"]*?)(?:'|\")").getMatch(0);
+        logger.info("iframe: " + iframe);
+        if (iframe == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        if (iframe != null) {
+            if (iframe.contains("watchfree")) {
+                br.setFollowRedirects(true);
+                br.getPage(iframe);
+                iframe = br.getRegex("iframe.*?src=(?:'|\")([^'\"]*?)(?:'|\")").getMatch(0);
+                logger.info("iframe: " + iframe);
+                if (iframe != null && iframe.contains(".spankmasters.com")) {
+                    iframe = iframe.replace(".spankmasters.com", "");
+                }
             }
-            logger.info("Finallink: " + finallink);
-            decryptedLinks.add(createDownloadlink(finallink));
+            decryptedLinks.add(createDownloadlink(iframe));
         }
         return decryptedLinks;
     }
