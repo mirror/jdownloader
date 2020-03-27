b39f56961de0a5e08c36535b7930c55ccf34327d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-03-27 15:59:04 +0000

    FakeHub: -refs #87191
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41954 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7c65996d5e5c12ab5a9dc2e52ee27d34512aca06

--- src/jd/plugins/decrypter/FakehubCom.java
+++ src/jd/plugins/decrypter/FakehubCom.java
@@ -17,36 +17,37 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
-import org.appwork.utils.formatter.SizeFormatter;
-
 import jd.PluginWrapper;
 import jd.config.SubConfiguration;
 import jd.controlling.AccountController;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
+import jd.http.requests.GetRequest;
 import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.utils.JDUtilities;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "fakehub.com" }, urls = { "https?://ma\\.fakehub\\.com/(?:watch/\\d+(?:/[a-z0-9\\-_]+/?)?|pics/\\d+(?:/[a-z0-9\\-_]+/?)?|model/\\d+(?:/[a-z0-9\\-_]+/?)?)" })
+import org.appwork.utils.formatter.SizeFormatter;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "fakehub.com" }, urls = { "https?://(?:new\\.|site-)?ma\\.fakehub\\.com/(?:(watch|scene)/\\d+(?:/[a-z0-9\\-_]+/?)?|pics/\\d+(?:/[a-z0-9\\-_]+/?)?|model/\\d+(?:/[a-z0-9\\-_]+/?)?)" })
 public class FakehubCom extends PluginForDecrypt {
     public FakehubCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    private static final String TYPE_VIDEO            = "https?://(?:new\\.)?ma\\.fakehub\\.com/watch/\\d+(?:/[a-z0-9\\-_]+/?)?";
-    private static final String TYPE_PHOTO            = "https?://(?:new\\.)?ma\\.fakehub\\.com/pics/\\d+(?:/[a-z0-9\\-_]+/?)?";
-    private static final String TYPE_MEMBER           = "https?://(?:new\\.)?ma\\.fakehub\\.com/model/\\d+(?:/[a-z0-9\\-_]+/?)?";
+    private static final String TYPE_VIDEO            = "https?://(?:new\\.|site-)?ma\\.fakehub\\.com/(watch|scene)/(\\d+)(?:/[a-z0-9\\-_]+/?)?";
+    private static final String TYPE_PHOTO            = "https?://(?:new\\.|site-)?ma\\.fakehub\\.com/pics/(\\d+)(?:/[a-z0-9\\-_]+/?)?";
+    private static final String TYPE_MEMBER           = "https?://(?:new\\.|site-)?ma\\.fakehub\\.com/model/(\\d+)(?:/[a-z0-9\\-_]+/?)?";
     public static String        DOMAIN_BASE           = "fakehub.com";
-    public static String        DOMAIN_PREFIX_PREMIUM = "ma.";
+    public static String        DOMAIN_PREFIX_PREMIUM = "site-ma.";
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
@@ -54,25 +55,19 @@ public class FakehubCom extends PluginForDecrypt {
         final String fid = new Regex(parameter, "/(\\d+)/").getMatch(0);
         final SubConfiguration cfg = SubConfiguration.getConfig(this.getHost());
         // Login if possible
-        if (!getUserLogin(false)) {
-            logger.info("No account present --> Cannot decrypt anything!");
-            return decryptedLinks;
-        }
+        getUserLogin(false);
         br.getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 404) {
             final DownloadLink offline = this.createOfflinelink(parameter);
             decryptedLinks.add(offline);
             return decryptedLinks;
         }
-        String title = br.getRegex("<title>([^<>\"]*?) \\- Fakehub\\.com :: </title>").getMatch(0);
-        if (title == null) {
-            title = br.getRegex("<h1[^>]*?>([^<>\"]+)</h1>").getMatch(0);
-        }
-        if (title == null) {
-            /* Fallback to id from inside url */
-            title = fid;
-        }
         if (parameter.matches(TYPE_VIDEO)) {
+            final String type = new Regex(parameter, TYPE_VIDEO).getMatch(0);
+            final String id = new Regex(parameter, TYPE_VIDEO).getMatch(1);
+            final GetRequest get = br.createGetRequest("https://site-api.project1service.com/v2/releases/" + id);
+            get.getHeaders().put("Instance", br.getCookie(getHost(), "instance_token"));
+            br.getPage(get);
             final String base_url = new Regex(this.br.getURL(), "(https?://[^/]+)/").getMatch(0);
             final String htmldownload = this.br.getRegex("<ul id=\"video\\-download\\-format\">(.*?)</ul>").getMatch(0);
             final String[] dlinfo = htmldownload.split("</li>");
@@ -94,7 +89,7 @@ public class FakehubCom extends PluginForDecrypt {
                     dl.setDownloadSize(SizeFormatter.getSize(filesize));
                     dl.setAvailable(true);
                 }
-                dl.setName(title + "_" + quality + ext);
+                dl.setName("_" + quality + ext);
                 dl.setProperty("fid", fid);
                 dl.setProperty("quality", quality);
                 decryptedLinks.add(dl);
@@ -111,7 +106,7 @@ public class FakehubCom extends PluginForDecrypt {
                 final String number_formatted = new Regex(finallink, "(\\d+)\\.jpg").getMatch(0);
                 finallink = finallink.replaceAll("https?://", "http://fakehubdecrypted");
                 final DownloadLink dl = this.createDownloadlink(finallink);
-                dl.setFinalFileName(title + "_" + number_formatted + ".jpg");
+                dl.setFinalFileName(number_formatted + ".jpg");
                 dl.setAvailable(true);
                 dl.setProperty("fid", fid);
                 dl.setProperty("picnumber_formatted", number_formatted);
@@ -122,9 +117,6 @@ public class FakehubCom extends PluginForDecrypt {
             logger.warning("Unsupported linktype");
             return decryptedLinks;
         }
-        final FilePackage fp = FilePackage.getInstance();
-        fp.setName(title);
-        fp.addLinks(decryptedLinks);
         return decryptedLinks;
     }
 
@@ -135,20 +127,18 @@ public class FakehubCom extends PluginForDecrypt {
         return false;
     }
 
-    private boolean getUserLogin(final boolean force) throws Exception {
+    private void getUserLogin(final boolean force) throws Exception {
         final PluginForHost hostPlugin = JDUtilities.getPluginForHost(this.getHost());
         final Account aa = AccountController.getInstance().getValidAccount(hostPlugin);
-        if (aa == null) {
-            logger.warning("There is no account available, stopping...");
-            return false;
+        if (aa != null) {
+            try {
+                ((jd.plugins.hoster.FakehubCom) hostPlugin).login(this.br, aa, force);
+                return;
+            } catch (final PluginException e) {
+                handleAccountException(aa, e);
+            }
         }
-        try {
-            ((jd.plugins.hoster.FakehubCom) hostPlugin).login(this.br, aa, force);
-        } catch (final PluginException e) {
-            aa.setValid(false);
-            return false;
-        }
-        return true;
+        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_ONLY);
     }
 
     public static String[] getPictureArray(final Browser br) {
@@ -174,11 +164,6 @@ public class FakehubCom extends PluginForDecrypt {
 
     public static boolean isOffline(final Browser br) {
         return br.getHttpConnection().getResponseCode() == 404;
-    }
-
-    /* NO OVERRIDE!! */
-    public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
-        return false;
     }
 
     @Override
