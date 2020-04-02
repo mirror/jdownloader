2defc090ebeda77211b8ce3ec7b98d475eb7d705
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-04-02 10:01:48 +0000

    *Plugins: Fixes/Changes/Maintenance* - eightmusescom: added support for another linktype RE: forum 83431
    *Plugins: New plugins or new websites that belong to existing scripts*
    - kamababacom RE forum 74354
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@41990 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: bc3a2af6feab652beb80824ed54b2300913ccce1

--- src/jd/plugins/hoster/EightMusesCom.java
+++ src/jd/plugins/hoster/EightMusesCom.java
@@ -23,7 +23,6 @@ import org.jdownloader.plugins.components.antiDDoSForHost;
 
 import jd.PluginWrapper;
 import jd.http.Browser;
-import jd.http.Browser.BrowserException;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -31,9 +30,10 @@ import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
 import jd.plugins.PluginException;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "8muses.com" }, urls = { "https?://(?:www\\.)?8muses\\.com/(comics/)?picture/([^/]+/){1,}\\d+" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "8muses.com" }, urls = { "https?://(?:www\\.)?8muses\\.com/(?:(comics/)?picture/([^/]+/){1,}\\d+|forum/(?:data/)?attachments/.+)" })
 public class EightMusesCom extends antiDDoSForHost {
     public EightMusesCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -43,113 +43,123 @@ public class EightMusesCom extends antiDDoSForHost {
     // Tags:
     // protocol: no https
     // other:
-    private String dllink = null;
+    private String              dllink      = null;
+    private static final String TYPE_DIRECT = ".+8muses\\.com/forum/.+";
 
     @Override
     public String getAGBLink() {
         return "http://www.8muses.com/";
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws Exception {
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        return requestFileInformation(link, false);
+    }
+
+    public AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
         dllink = null;
         br = new Browser();
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        getPage(downloadLink.getDownloadURL());
-        if (br.getHttpConnection().getResponseCode() == 404 || br.containsHTML("<b>Notice</b>:")) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        }
-        String filename = new Regex(downloadLink.getDownloadURL(), "8muses\\.com/(?:[^/]*/)?picture/(?:\\d+\\-)?(.+)").getMatch(0);
-        filename = filename.replace("/", "_");
-        final String ractive_public = n(br.getRegex("<script id=\"ractive-public\" type=\"text/plain\">\\s*(.*?)\\s*<").getMatch(0));
-        final String imageDir = br.getRegex("imageDir\" value=\"(/data/.{2}/)\"").getMatch(0);
-        final String imageName = br.getRegex("imageName\" value=\"([^<>\"]*?)\"").getMatch(0);
-        final String imageHost = br.getRegex("imageHost\" value=\"([^<>\"]*?)\"").getMatch(0);
-        if (imageDir != null && imageName != null) {
-            dllink = imageDir + imageName;
-        } else if (imageHost != null && imageName != null) {
-            dllink = imageHost + "/image/fl/" + imageName;
-        } else if (imageName != null) {
-            /* 2018-02-09 */
-            dllink = "https://www.8muses.com/image/fl/" + imageName;
-        } else if (ractive_public != null) {
-            final String image = new Regex(ractive_public, "\"picture\"\\s*:\\s*\\{.*?\"public.*?\"\\s*:\\s*\"(.*?)\"").getMatch(0);
-            if (image != null) {
-                dllink = "https://www.8muses.com/image/fl/" + image + ".jpg";
-            }
-        }
-        if (ractive_public.contains("\"pictures\":[]")) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        }
-        if (filename == null || dllink == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        filename = Encoding.htmlDecode(filename);
-        filename = filename.trim();
-        filename = encodeUnicode(filename);
-        String ext = getFileNameExtensionFromString(dllink);
-        /* Make sure that we get a correct extension */
-        if (ext == null || !ext.matches("\\.[A-Za-z0-9]{3,5}")) {
-            ext = ".jpg";
-        }
-        if (!filename.endsWith(ext)) {
-            filename += ext;
-        }
-        downloadLink.setFinalFileName(filename);
-        final Browser br2 = br.cloneBrowser();
-        // In case the link redirects to the finallink
-        br2.setFollowRedirects(true);
-        URLConnectionAdapter con = null;
-        try {
-            try {
-                con = br2.openHeadConnection(dllink);
-            } catch (final BrowserException e) {
+        String filename = null;
+        if (link.getPluginPatternMatcher().matches(TYPE_DIRECT)) {
+            dllink = link.getPluginPatternMatcher();
+        } else {
+            getPage(link.getPluginPatternMatcher());
+            if (br.getHttpConnection().getResponseCode() == 404 || br.containsHTML("<b>Notice</b>:")) {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
-            if (!con.getContentType().contains("html")) {
-                if (con.getHeaderField("cf-bgj") != null && !downloadLink.hasProperty(BYPASS_CLOUDFLARE_BGJ)) {
-                    downloadLink.setProperty(BYPASS_CLOUDFLARE_BGJ, Boolean.TRUE);
+            filename = new Regex(link.getPluginPatternMatcher(), "8muses\\.com/(?:[^/]*/)?picture/(?:\\d+\\-)?(.+)").getMatch(0);
+            filename = filename.replace("/", "_");
+            final String ractive_public = n(br.getRegex("<script id=\"ractive-public\" type=\"text/plain\">\\s*(.*?)\\s*<").getMatch(0));
+            final String imageDir = br.getRegex("imageDir\" value=\"(/data/.{2}/)\"").getMatch(0);
+            final String imageName = br.getRegex("imageName\" value=\"([^<>\"]*?)\"").getMatch(0);
+            final String imageHost = br.getRegex("imageHost\" value=\"([^<>\"]*?)\"").getMatch(0);
+            if (imageDir != null && imageName != null) {
+                dllink = imageDir + imageName;
+            } else if (imageHost != null && imageName != null) {
+                dllink = imageHost + "/image/fl/" + imageName;
+            } else if (imageName != null) {
+                /* 2018-02-09 */
+                dllink = "https://www.8muses.com/image/fl/" + imageName;
+            } else if (ractive_public != null) {
+                final String image = new Regex(ractive_public, "\"picture\"\\s*:\\s*\\{.*?\"public.*?\"\\s*:\\s*\"(.*?)\"").getMatch(0);
+                if (image != null) {
+                    dllink = "https://www.8muses.com/image/fl/" + image + ".jpg";
                 }
-                downloadLink.setDownloadSize(con.getLongContentLength());
-            } else {
+            }
+            if (ractive_public.contains("\"pictures\":[]")) {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
-            downloadLink.setProperty("directlink", dllink);
-            return AvailableStatus.TRUE;
-        } finally {
+            if (filename == null || dllink == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            filename = Encoding.htmlDecode(filename);
+            filename = filename.trim();
+            filename = encodeUnicode(filename);
+            String ext = getFileNameExtensionFromString(dllink);
+            /* Make sure that we get a correct extension */
+            if (ext == null || !ext.matches("\\.[A-Za-z0-9]{3,5}")) {
+                ext = ".jpg";
+            }
+            if (!filename.endsWith(ext)) {
+                filename += ext;
+            }
+            link.setFinalFileName(filename);
+        }
+        br.setFollowRedirects(true);
+        if (dllink != null && !isDownload) {
+            URLConnectionAdapter con = null;
             try {
-                con.disconnect();
-            } catch (final Throwable e) {
+                con = br.openHeadConnection(dllink);
+                if (!con.getContentType().contains("html")) {
+                    if (con.getHeaderField("cf-bgj") != null && !link.hasProperty(BYPASS_CLOUDFLARE_BGJ)) {
+                        link.setProperty(BYPASS_CLOUDFLARE_BGJ, Boolean.TRUE);
+                    }
+                    link.setDownloadSize(con.getLongContentLength());
+                    if (filename == null) {
+                        link.setFinalFileName(Plugin.getFileNameFromHeader(con));
+                    }
+                } else if (con.getResponseCode() == 404) {
+                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                } else {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error");
+                }
+                link.setProperty("directlink", dllink);
+            } finally {
+                try {
+                    con.disconnect();
+                } catch (final Throwable e) {
+                }
             }
         }
+        return AvailableStatus.TRUE;
     }
 
     public static final String BYPASS_CLOUDFLARE_BGJ = "bpCfBgj";
 
     @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception {
-        requestFileInformation(downloadLink);
-        if (downloadLink.getProperty(BYPASS_CLOUDFLARE_BGJ) != null) {
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link, true);
+        if (link.getProperty(BYPASS_CLOUDFLARE_BGJ) != null) {
             logger.info("Apply Cloudflare BGJ bypass");
             dllink = URLHelper.parseLocation(br.getURL(dllink), "&bpcfbgj=" + System.nanoTime());
-            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, false, 1);
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, false, 1);
         } else {
-            dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, true, 0);
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, true, 0);
         }
         if (dl.getConnection().getContentType().contains("html")) {
             if (dl.getConnection().getResponseCode() == 403) {
                 throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
             } else if (dl.getConnection().getResponseCode() == 404) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
             br.followConnection();
             try {
                 dl.getConnection().disconnect();
             } catch (final Throwable e) {
             }
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error");
         }
         dl.startDownload();
     }
