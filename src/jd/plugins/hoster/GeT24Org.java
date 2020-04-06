52c2102adf1b5f0c7f0d32340eb1c8f0944aca78
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-04-06 10:59:13 +0000

    *Plugins: Fixes/Changes/Maintenance* - multihoster get24.org: improved errorhandling, fixed wrong errorhandling, added storing- and re-using of generated directurls added some comments, cannot test anything as there is not official test account available RE: me
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42011 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5bede0602ec6f517804693f5f7f1c58d4645b000

--- src/jd/plugins/hoster/GeT24Org.java
+++ src/jd/plugins/hoster/GeT24Org.java
@@ -1,15 +1,17 @@
 package jd.plugins.hoster;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
 
+import org.appwork.utils.StringUtils;
 import org.appwork.utils.formatter.TimeFormatter;
 import org.jdownloader.plugins.controller.host.LazyHostPlugin.FEATURE;
 import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 import jd.PluginWrapper;
+import jd.config.Property;
 import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
 import jd.nutils.JDHash;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.Account;
@@ -25,11 +27,8 @@ import jd.plugins.components.PluginJSonUtils;
 
 @HostPlugin(revision = "$Revision: 41665 $", interfaceVersion = 3, names = { "get24.org" }, urls = { "REGEX_NOT_POSSIBLE_RANDOM-asdfasdfsadfsdgfd32423" })
 public class GeT24Org extends PluginForHost {
-    private static HashMap<Account, HashMap<String, Long>> hostUnavailableMap = new HashMap<Account, HashMap<String, Long>>();
-    private static final String                            NICE_HOST          = "get24.org";
-    private static final String                            NICE_HOSTproperty  = "get24org";
-    private static final String                            VERSION            = "0.0.1";
-    private static final Integer                           MAXSIM             = 3;
+    private static final String  VERSION = "0.0.1";
+    private static final Integer MAXSIM  = 3;
 
     public GeT24Org(PluginWrapper wrapper) {
         super(wrapper);
@@ -83,52 +82,100 @@ public class GeT24Org extends PluginForHost {
 
     public void handleMultiHost(final DownloadLink link, final Account account) throws Exception {
         this.br = newBrowser();
-        String post_data = "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()) + "&link=" + Encoding.urlEncode(link.getDownloadURL());
-        // "&url=" + Encoding.urlEncode(link.getDownloadURL());
-        String response = br.postPage("https://get24.org/api/debrid/geturl", post_data); // security
-        if (!Boolean.parseBoolean(PluginJSonUtils.getJson(response, "ok"))) {
-            String reason = PluginJSonUtils.getJson(response, "reason");
-            if (reason.equals("invalid credentials")) {
-                throw new PluginException(LinkStatus.ERROR_FATAL, "Wrong login or password");
-            } else if (reason.equals("user not activated")) {
-                throw new PluginException(LinkStatus.ERROR_FATAL, "Email not activated");
-            } else if (reason.equals("premium required")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "premium required");
-            } else if (reason.equals("premiumX required")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "premiumX required");
-            } else if (reason.equals("no transfer")) {
-                throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, "daily limit exceeded");
-            } else if (reason.equals("host daily limit exceeded")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "daily limit exceeded for this host"); // TODO: allow launching other
-                                                                                                           // links/hosts
-            } else if (reason.equals("file removed")) {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            } else if (reason.equals("host not supported")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "host not supported");
-            } else if (reason.equals("host disabled")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "host disabled");
-            } else if (reason.equals("temporary error")) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "temporary error", 60 * 60 * 1000l); // we can try
-                                                                                                                         // another links or
-                                                                                                                         // hosts probably
-            } else if (reason.equals("unknown error")) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "unknown error", 60 * 60 * 1000l); // we can try another
-                                                                                                                       // hosts or maybe
-                                                                                                                       // even links
-            } else {
-                throw new PluginException(LinkStatus.ERROR_RETRY, "unknown error");
+        final String directurlproperty = this.getHost() + "directurl";
+        String directurl = checkDirectLink(link, directurlproperty);
+        if (directurl == null) {
+            String post_data = "email=" + Encoding.urlEncode(account.getUser()) + "&passwd_sha256=" + JDHash.getSHA256(account.getPass()) + "&link=" + Encoding.urlEncode(link.getDownloadURL());
+            // "&url=" + Encoding.urlEncode(link.getDownloadURL());
+            String response = br.postPage("https://get24.org/api/debrid/geturl", post_data); // security
+            if (!Boolean.parseBoolean(PluginJSonUtils.getJson(response, "ok"))) {
+                String reason = PluginJSonUtils.getJson(response, "reason");
+                if (reason.equals("invalid credentials")) {
+                    /* Permanently disable account */
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "Wrong login or password");
+                } else if (reason.equals("user not activated")) {
+                    /* Permanently disable account */
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "Email not activated");
+                } else if (reason.equals("premium required")) {
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "premium required", 5 * 60 * 1000l);
+                } else if (reason.equals("premiumX required")) {
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "premiumX required", 5 * 60 * 1000l);
+                } else if (reason.equals("no transfer")) {
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "daily limit exceeded", 5 * 60 * 1000l);
+                } else if (reason.equals("host daily limit exceeded")) {
+                    // TODO: allow launching other
+                    // links/hosts
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "host daily limit exceeded", 5 * 60 * 1000l);
+                } else if (reason.equals("file removed")) {
+                    /*
+                     * NEVER trust multihoster offline status --> Try next download candidate instead. If the URL really is offline, the
+                     * real plugin should find the correct status via the next availablecheck!
+                     */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "File offline according to multihoster", 5 * 60 * 1000l);
+                } else if (reason.equals("host not supported")) {
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "host not supported", 5 * 60 * 1000l);
+                } else if (reason.equals("host disabled")) {
+                    /* Skip to next download candidate */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "host disabled", 5 * 60 * 1000l);
+                } else if (reason.equals("temporary error")) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "temporary error", 5 * 60 * 1000l); // we can try
+                                                                                                                            // another links
+                                                                                                                            // or
+                                                                                                                            // hosts
+                                                                                                                            // probably
+                } else if (reason.equals("unknown error")) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "unknown error", 5 * 60 * 1000l); // we can try
+                                                                                                                          // another
+                                                                                                                          // hosts or maybe
+                                                                                                                          // even links
+                } else {
+                    throw new PluginException(LinkStatus.ERROR_RETRY, "unknown error");
+                }
+            }
+            directurl = PluginJSonUtils.getJson(response, "url");
+            if (StringUtils.isEmpty(directurl)) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "unknown error failed to find final downloadurl", 5 * 60 * 1000l);
             }
         }
-        String url = PluginJSonUtils.getJson(response, "url");
         // TODO: resume support
-        // but jdownloader is not saving final link and allways asks for new so it has to be implemented server side first to not waste traffic
-        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, url, false, 1);
+        dl = jd.plugins.BrowserAdapter.openDownload(this.br, link, directurl, false, 1);
+        link.setProperty(this.getHost() + "directurl", directurl);
         // TODO: validate status code
         dl.startDownload();
         // link_hash
         // url
         // filename
         // filesize
+    }
+
+    private String checkDirectLink(final DownloadLink link, final String property) {
+        String dllink = link.getStringProperty(property);
+        if (dllink != null) {
+            URLConnectionAdapter con = null;
+            try {
+                final Browser br2 = br.cloneBrowser();
+                br2.setFollowRedirects(true);
+                con = br2.openHeadConnection(dllink);
+                if (con.getContentType().contains("text") || !con.isOK() || con.getLongContentLength() == -1) {
+                    link.setProperty(property, Property.NULL);
+                    dllink = null;
+                }
+            } catch (final Exception e) {
+                logger.log(e);
+                link.setProperty(property, Property.NULL);
+                dllink = null;
+            } finally {
+                if (con != null) {
+                    con.disconnect();
+                }
+            }
+        }
+        return dllink;
     }
 
     @Override
