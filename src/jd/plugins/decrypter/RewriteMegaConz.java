5e5c58b60d59d48bab32f194524ea86f9bac6f76
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-04-15 13:45:51 +0000

    RewriteMegaConz: -added subfolder support -fixes #87913 -refs thread 83745
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42075 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: afb7152cc14e80ad9ea65e9b0caedcddef72f344

--- src/jd/plugins/decrypter/RewriteMegaConz.java
+++ src/jd/plugins/decrypter/RewriteMegaConz.java
@@ -12,7 +12,7 @@ import jd.plugins.PluginForDecrypt;
 
 import org.appwork.utils.StringUtils;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mega.co.nz" }, urls = { "https?://(www\\.)?mega\\.(co\\.)?nz/(file|folder)/[a-zA-Z0-9]+(#|%23)[a-zA-Z0-9_,\\-%]{16,}" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mega.co.nz" }, urls = { "https?://(www\\.)?mega\\.(co\\.)?nz/(file|folder)/[a-zA-Z0-9]+(#|%23)[a-zA-Z0-9_,\\-%]{16,}(/folder/[a-zA-Z0-9]+)?" })
 public class RewriteMegaConz extends PluginForDecrypt {
     public RewriteMegaConz(PluginWrapper wrapper) {
         super(wrapper);
@@ -22,12 +22,16 @@ public class RewriteMegaConz extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         // TODO: update pattern in hoster/decrypter plugin to support this new url format
-        final String parts[] = new Regex(parameter.getCryptedUrl(), "(file|folder)/([a-zA-Z0-9]+)(?:#|%23)([a-zA-Z0-9_,\\-%]{16,})").getRow(0);
+        final String parts[] = new Regex(parameter.getCryptedUrl(), "(file|folder)/([a-zA-Z0-9]+)(?:#|%23)([a-zA-Z0-9_,\\-%]{16,})(/folder/([a-zA-Z0-9]+))?").getRow(0);
         final DownloadLink link;
         if (StringUtils.equals("file", parts[0])) {
             link = createDownloadlink("https://mega.nz/#!" + parts[1] + "!" + parts[2]);
         } else {
-            link = createDownloadlink("https://mega.nz/#F!" + parts[1] + "!" + parts[2]);
+            if (parts.length == 5 && StringUtils.isNotEmpty(parts[4])) {
+                link = createDownloadlink("https://mega.nz/#F!" + parts[1] + "!" + parts[2] + "!" + parts[4]);
+            } else {
+                link = createDownloadlink("https://mega.nz/#F!" + parts[1] + "!" + parts[2]);
+            }
         }
         decryptedLinks.add(link);
         return decryptedLinks;
