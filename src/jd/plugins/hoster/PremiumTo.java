f49fb045e918b52fed0c793a7d7e290b4eb2f688
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-04-22 18:14:50 +0000

    - multihoster premium.to: higher read timeout RE: admin chunkload test
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42125 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 11f4e2412190fe04e36e4184144cd08027814e6c

--- src/jd/plugins/hoster/PremiumTo.java
+++ src/jd/plugins/hoster/PremiumTo.java
@@ -182,7 +182,7 @@ public class PremiumTo extends UseNet {
     }
 
     private int getReadTimeout() {
-        return 90 * 1000;
+        return 180 * 1000;
     }
 
     private int getConnectTimeout() {
