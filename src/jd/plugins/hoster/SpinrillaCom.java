41cd4834e9e816e3d56dcac78a77bce862c734a3
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-05-07 13:16:32 +0000

    *Plugins: Fixes/Changes/Maintenance* - spinrillacom: fixed wrong filenames fixes #88200 - stileprojectcom: fixed bad linkid and fixed download fixes #87775
    *Plugins: New plugins or new websites that belong to existing scripts*
    - added support for multihoster mydebrid.com refs #88026
    
    *Plugins: RIP*
    - [usenet] ssl-news.info fixes #85908
    - qq.com: RIP but do not add domain to offline so that deepparser can be used for their current page fixes #84232 fixes #84231
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42235 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 530cbd56bd1a718bcebbd0a63d0940fa5384c354

--- src/jd/plugins/hoster/SpinrillaCom.java
+++ src/jd/plugins/hoster/SpinrillaCom.java
@@ -13,11 +13,12 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.hoster;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
 
 import jd.PluginWrapper;
 import jd.config.Property;
@@ -33,11 +34,8 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-import org.jdownloader.captcha.v2.challenge.recaptcha.v1.Recaptcha;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "spinrilla.com" }, urls = { "https?://(www\\.)?spinrilla\\.com/(?:mixtapes|songs)/[a-z0-9\\-]+" }) 
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "spinrilla.com" }, urls = { "https?://(?:www\\.)?spinrilla\\.com/(?:mixtapes|songs)/(\\d+)-([a-z0-9\\-]+)" })
 public class SpinrillaCom extends PluginForHost {
-
     public SpinrillaCom(PluginWrapper wrapper) {
         super(wrapper);
     }
@@ -49,7 +47,6 @@ public class SpinrillaCom extends PluginForHost {
 
     private static final String TYPE_MIXTAPES = "https?://(www\\.)?spinrilla\\.com/mixtapes/[a-z0-9\\-]+";
     private static final String TYPE_SONGS    = "https?://(www\\.)?spinrilla\\.com/songs/[a-z0-9\\-]+";
-
     private boolean             skipCaptcha   = true;
 
     @SuppressWarnings("deprecation")
@@ -61,13 +58,8 @@ public class SpinrillaCom extends PluginForHost {
         if (br.getHttpConnection().getResponseCode() == 404) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        String filename = br.getRegex("<title>([^<>\"]*?) \\| Spinrilla</title>").getMatch(0);
-        if (filename == null) {
-            filename = br.getRegex("content=\\'([^<>\"]*?)\\' name=\\'twitter:title\\'").getMatch(0);
-        }
-        if (filename == null) {
-            filename = link.getDownloadURL().substring(link.getDownloadURL().lastIndexOf("/"));
-        }
+        /* 2020-05-07: Use part of URL as filename */
+        String filename = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1).replace("-", " ");
         if (filename == null) {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
@@ -112,7 +104,6 @@ public class SpinrillaCom extends PluginForHost {
                         timeUntilRelease = 1 * 60 * 60 * 1000l;
                     }
                     throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "This mixtape is not yet released", timeUntilRelease);
-
                 }
                 br.setFollowRedirects(false);
                 final String baseurl = br.getURL();
@@ -200,5 +191,4 @@ public class SpinrillaCom extends PluginForHost {
     @Override
     public void resetDownloadlink(final DownloadLink link) {
     }
-
 }
