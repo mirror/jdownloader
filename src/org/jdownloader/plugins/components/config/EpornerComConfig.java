d0dcd74654f4daa286816529b9b6f8e05efc8729
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-05-23 17:35:13 +0000

    *Plugins: Fixes/Changes/Maintenance* - [XFS] deimosclick: updated free mode connection limits - epornercom: updated quality selection refs #88334
    *Plugins: New plugins or new websites that belong to existing scripts*
    - DeimosClickCrawler: special crawler for strange short-URLs from XFS filehost deimosclick
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42354 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/config/EpornerComConfig.java
@@ -0,0 +1,66 @@
+package org.jdownloader.plugins.components.config;
+
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultEnumValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.appwork.storage.config.annotations.LabelInterface;
+import org.jdownloader.plugins.config.Order;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "eporner.com", type = Type.HOSTER)
+public interface EpornerComConfig extends PluginConfigInterface {
+    public static enum PreferredStreamQuality implements LabelInterface {
+        BEST {
+            @Override
+            public String getLabel() {
+                return "Best";
+            }
+        },
+        Q240P {
+            @Override
+            public String getLabel() {
+                return "240p";
+            }
+        },
+        Q360P {
+            @Override
+            public String getLabel() {
+                return "360p";
+            }
+        },
+        Q480P {
+            @Override
+            public String getLabel() {
+                return "480p";
+            }
+        },
+        Q720P {
+            @Override
+            public String getLabel() {
+                return "720p";
+            }
+        },
+        Q1080P {
+            @Override
+            public String getLabel() {
+                return "1080p";
+            }
+        },
+        Q2160P {
+            @Override
+            public String getLabel() {
+                return "2160p (4k)";
+            }
+        };
+    }
+
+    @AboutConfig
+    @DefaultEnumValue("BEST")
+    @DescriptionForConfigEntry("If your preferred stream quality is not found, best quality will be downloaded instead.")
+    @Order(100)
+    PreferredStreamQuality getPreferredStreamQuality();
+
+    void setPreferredStreamQuality(PreferredStreamQuality quality);
+}
