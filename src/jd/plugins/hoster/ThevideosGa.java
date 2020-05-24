8b7cb501472a58468aa9d4189d976a1a33fcbb01
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-05-24 10:40:05 +0000

    *General* UnknownVideohostingCore: - improved dialog handling - Fixed check for offline
    *Plugins: Fixes/Changes/Maintenance*
    - ThevideosGa: separated from UnknownVideohostingCore and moved into completely separate class and improved linkcheck speed
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42357 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/ThevideosGa.java
+++ src/jd/plugins/hoster/ThevideosGa.java
@@ -15,86 +15,138 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.jdownloader.plugins.components.UnknownVideohostingCore;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.plugins.components.antiDDoSForHost;
 
 import jd.PluginWrapper;
+import jd.http.URLConnectionAdapter;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
 import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
-public class ThevideosGa extends UnknownVideohostingCore {
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "thevideos.ga" }, urls = { "https?://(?:www\\.)?thevideos\\.ga/embed-([a-f0-9]{32})\\.html" })
+public class ThevideosGa extends antiDDoSForHost {
     public ThevideosGa(PluginWrapper wrapper) {
         super(wrapper);
     }
+    /* DEV NOTES */
+    // Tags: vev.io, UnknownVideohostingCore
+    // other: base is vev.io but it is impossible to use the normal vev.io website/API for their URLs. Handles special embed URLs receives
+    // mostly from crawler KinoxTo.java
 
-    public static List<String[]> getPluginDomains() {
-        final List<String[]> ret = new ArrayList<String[]>();
-        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
-        ret.add(new String[] { "thevideos.ga" });// standalone domain/site, only hosting embedded content
-        return ret;
+    /* Connection stuff */
+    private static final boolean free_resume       = true;
+    private static final int     free_maxchunks    = 0;
+    private static final int     free_maxdownloads = -1;
+    private String               dllink            = null;
+    private boolean              server_issues     = false;
+
+    @Override
+    public String getAGBLink() {
+        return "https://thevideos.ga/";
     }
 
     @Override
-    public void correctDownloadLink(DownloadLink link) {
-        // do not correct URLs from thevideos.ga!
+    public String getLinkID(final DownloadLink link) {
+        final String linkid = getFID(link);
+        if (linkid != null) {
+            return this.getHost() + "://" + linkid;
+        } else {
+            return super.getLinkID(link);
+        }
     }
 
-    public static String[] getAnnotationNames() {
-        return buildAnnotationNames(getPluginDomains());
-    }
-
-    @Override
-    public String[] siteSupportedNames() {
-        return buildSupportedNames(getPluginDomains());
-    }
-
-    public static String[] getAnnotationUrls() {
-        return UnknownVideohostingCore.buildAnnotationUrls(getPluginDomains());
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
     }
 
     @Override
-    public boolean check_filesize_via_directurl() {
-        return true;
-    }
-
-    @Override
-    protected boolean allowPairingLinkcheck() {
-        /* 2020-05-23: Not possible as their embedded videos cannot be accessed via their API: https://vev.io/api#startcontent */
-        return false;
-    }
-
-    @Override
-    protected String getDllink(final DownloadLink link, final boolean isDownload) throws IOException, PluginException, InterruptedException {
-        br.setCurrentURL("https://" + this.getHost() + "/" + this.getFID(link));
-        final String url = "https://" + this.getHost() + "/stream" + this.getFID(link) + ".mp4";
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        link.setMimeHint(CompiledFiletypeFilter.VideoExtensions.MP4);
+        dllink = null;
+        server_issues = false;
+        this.setBrowserExclusive();
+        /* No filename available --> Check if it maybe has been set in crawler before */
+        String filename = link.getStringProperty("fallback_filename");
+        if (filename == null) {
+            /* Last chance fallback */
+            filename = this.getFID(link);
+        }
+        final String ext = ".mp4";
+        filename = Encoding.htmlDecode(filename).trim();
+        if (!filename.endsWith(ext)) {
+            filename += ext;
+        }
+        link.setFinalFileName(filename);
+        dllink = String.format("https://thevideos.ga/stream%s.mp4", this.getFID(link));
+        br.setAllowedResponseCodes(new int[] { 502 });
         br.setFollowRedirects(false);
-        br.getPage(url);
-        if (br.getHttpConnection().getResponseCode() == 404) {
+        br.getPage(dllink);
+        /* 2020-05-24: 502 is typical for timed-out URLs here */
+        if (br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 502) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        return br.getRedirectLocation();
+        dllink = br.getRedirectLocation();
+        if (dllink == null) {
+            logger.info("No redirect found --> Content is probably offline");
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        URLConnectionAdapter con = null;
+        try {
+            con = openAntiDDoSRequestConnection(br, br.createHeadRequest(dllink));
+            if (con.getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            } else if (con.getCompleteContentLength() < 1000) {
+                /* 2020-05-24 */
+                logger.info("File is very small --> Must be offline");
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            } else if (!con.getContentType().contains("html")) {
+                link.setDownloadSize(con.getLongContentLength());
+            } else {
+                server_issues = true;
+            }
+        } finally {
+            try {
+                con.disconnect();
+            } catch (final Throwable e) {
+            }
+        }
+        return AvailableStatus.TRUE;
     }
 
     @Override
-    protected String getReCaptchaKey() {
-        return null;
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        if (server_issues) {
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Unknown server error", 10 * 60 * 1000l);
+        } else if (StringUtils.isEmpty(dllink)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, free_resume, free_maxchunks);
+        if (dl.getConnection().getContentType().contains("html")) {
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            }
+            br.followConnection();
+            try {
+                dl.getConnection().disconnect();
+            } catch (final Throwable e) {
+            }
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error");
+        }
+        dl.startDownload();
     }
 
     @Override
-    protected String getReCaptchaKeyPairing() {
-        return null;
-    }
-
-    @Override
-    protected boolean usePairingMode() {
-        /* 2020-05-23: Special */
-        return false;
+    public int getMaxSimultanFreeDownloadNum() {
+        return free_maxdownloads;
     }
 
     @Override
