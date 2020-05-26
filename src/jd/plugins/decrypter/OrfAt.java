4e2406219f164fb9c7fde20bef1c2012dc7d5c86
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-05-26 12:19:37 +0000

    OrfAt: -replaces old Fm4/Oe1OrfAt plugins -fixes thread 84511
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42369 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/OrfAt.java
+++ src/jd/plugins/decrypter/OrfAt.java
@@ -11,23 +11,29 @@ import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
 import org.appwork.utils.Regex;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "oe1.orf.at" }, urls = { "https?://oe1\\.orf\\.at/(player|programm)/\\d+/\\d+" })
-public class Oe1OrfAt extends PluginForDecrypt {
-    public Oe1OrfAt(PluginWrapper wrapper) {
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "orf.at" }, urls = { "https?://(oe1|oe3|fm4)\\.orf\\.at/(player|programm)/\\d+/[a-zA-Z0-9]+" })
+public class OrfAt extends PluginForDecrypt {
+    public OrfAt(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
-        final String broadCastID = new Regex(parameter.getCryptedUrl(), "/(?:player|programm)/\\d+/(\\d+)").getMatch(0);
+        final String broadCastID = new Regex(parameter.getCryptedUrl(), "/(?:player|programm)/\\d+/([a-zA-Z0-9]+)").getMatch(0);
         final String broadCastKey = new Regex(parameter.getCryptedUrl(), "/(?:player|programm)/(\\d+)/\\d+").getMatch(0);
-        br.getPage("https://audioapi.orf.at/oe1/api/json/current/broadcast/" + broadCastID + "/" + broadCastKey + "?_s=" + System.currentTimeMillis());
+        final String domainID = new Regex(parameter.getCryptedUrl(), "https?://(oe1|oe3|fm4)\\.orf\\.at").getMatch(0);
+        if (domainID == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getPage("https://audioapi.orf.at/" + domainID + "/api/json/current/broadcast/" + broadCastID + "/" + broadCastKey + "?_s=" + System.currentTimeMillis());
         final Map<String, Object> response = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
         final String broadCastDay = response.get("broadcastDay").toString();
         final String title = (String) response.get("title");
@@ -42,7 +48,7 @@ public class Oe1OrfAt extends PluginForDecrypt {
             if (loopStreamId == null) {
                 continue;
             }
-            final DownloadLink link = createDownloadlink("directhttp://http://loopstream01.apa.at/?channel=oe1&shoutcast=0&player=oe1_v1&referer=oe1.orf.at&_=" + System.currentTimeMillis() + "&userid=" + userid + "&id=" + loopStreamId);
+            final DownloadLink link = createDownloadlink("directhttp://http://loopstream01.apa.at/?channel=" + domainID + "&shoutcast=0&player=" + domainID + "_v1&referer=" + domainID + ".orf.at&_=" + System.currentTimeMillis() + "&userid=" + userid + "&id=" + loopStreamId);
             if (streams.size() > 1) {
                 link.setFinalFileName(title + "_" + broadCastDay + "_" + (++index) + ".mp3");
             } else {
@@ -50,7 +56,7 @@ public class Oe1OrfAt extends PluginForDecrypt {
             }
             link.setProperty("requestType", "GET");
             link.setAvailable(true);
-            link.setLinkID(getHost() + "://" + broadCastID + "/" + broadCastKey + "/" + index);
+            link.setLinkID(domainID + ".orf.at://" + broadCastID + "/" + broadCastKey + "/" + index);
             ret.add(link);
             fp.add(link);
         }
