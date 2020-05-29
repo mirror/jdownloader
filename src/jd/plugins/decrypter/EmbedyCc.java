18784697db4d26b065d2ce1b9749ca80df9c9e8c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-05-29 13:55:51 +0000

    *Plugins: Fixes/Changes/Maintenance* - embedycc: improved crawler refs #86124
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42411 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1f58f55fd143e2af0e863bf19b2050816c92e788

--- src/jd/plugins/decrypter/EmbedyCc.java
+++ src/jd/plugins/decrypter/EmbedyCc.java
@@ -20,6 +20,9 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
+import org.appwork.utils.parser.UrlQuery;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.nutils.encoding.Encoding;
@@ -30,9 +33,7 @@ import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "embedy.cc" }, urls = { "https?://(?:www\\.)?embedy\\.cc/movies/[A-Za-z0-9=]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "embedy.cc" }, urls = { "https?://(?:www\\.)?embedy\\.cc/movies/([A-Za-z0-9=]+)" })
 public class EmbedyCc extends PluginForDecrypt {
     public EmbedyCc(PluginWrapper wrapper) {
         super(wrapper);
@@ -41,7 +42,7 @@ public class EmbedyCc extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        final String linkid = new Regex(parameter, "/movies/(.+)").getMatch(0);
+        final String linkid = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
         this.br.setFollowRedirects(true);
         br.getPage(parameter);
         if (br.getHttpConnection().getResponseCode() == 404 || !this.br.getURL().contains("/movies/")) {
@@ -50,36 +51,64 @@ public class EmbedyCc extends PluginForDecrypt {
         }
         String fpName = br.getRegex("property=\"og:title\" content=\"([^<>\"]+)\"").getMatch(0);
         if (fpName == null) {
+            /* Fallback */
             fpName = linkid;
         }
         this.br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        this.br.postPage("https://" + this.getHost() + "/video.get/", "video=" + Encoding.urlEncode(linkid));
-        if (br.getHttpConnection().getResponseCode() == 404) {
-            decryptedLinks.add(this.createOfflinelink(parameter));
-            return decryptedLinks;
-        }
-        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
-        entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.walkJson(entries, "response/1/files");
-        final Iterator<Entry<String, Object>> it = entries.entrySet().iterator();
-        while (it.hasNext()) {
-            final Entry<String, Object> ipentry = it.next();
-            final String quality = ipentry.getKey();
-            final String url = (String) ipentry.getValue();
-            if (url == null || !url.startsWith("http") || quality == null) {
-                continue;
-            }
-            final DownloadLink dl;
-            if (url.contains(this.getHost())) {
-                dl = createDownloadlink("directhttp://" + url);
-                /* Hoster by embedy.cc and/or vk.com[vk.me] */
-                dl.setFinalFileName(fpName + "_" + quality + ".mp4");
+        /* 2020-05-29: New */
+        final boolean forceOfficialDownload = true;
+        if (forceOfficialDownload) {
+            br.postPage("https://embedy.cc/down/", "id=" + Encoding.urlEncode(linkid));
+            final String[] qualities = br.getRegex("(\\d+p)").getColumn(0);
+            final String[] urls = br.getRegex("(https://[^/]+/download/[^<>\"]+)\"").getColumn(0);
+            for (final String url : urls) {
+                final DownloadLink dl = createDownloadlink("directhttp://" + url);
+                final UrlQuery query = new UrlQuery().parse(url);
+                final String fname_crypted = query.get("title");
+                if (fname_crypted != null) {
+                    String fname_decrypted = Encoding.Base64Decode(fname_crypted);
+                    if (!fname_decrypted.endsWith(".mp4")) {
+                        fname_decrypted += ".mp4";
+                    }
+                    dl.setFinalFileName(fname_decrypted);
+                }
                 dl.setAvailable(true);
-                dl.setProperty("refURL", parameter);
-                dl.setProperty("requestType", "GET");
-            } else {
-                dl = createDownloadlink(url);
+                /* Hoster by embedy.cc and/or vk.com[vk.me] */
+                // dl.setFinalFileName(fpName + "_" + quality + ".mp4");
+                // dl.setAvailable(true);
+                // dl.setProperty("refURL", parameter);
+                // dl.setProperty("requestType", "GET");
+                decryptedLinks.add(dl);
             }
-            decryptedLinks.add(dl);
+        } else {
+            this.br.postPage("https://" + this.getHost() + "/video.get/", "video=" + Encoding.urlEncode(linkid));
+            if (br.getHttpConnection().getResponseCode() == 404) {
+                decryptedLinks.add(this.createOfflinelink(parameter));
+                return decryptedLinks;
+            }
+            LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+            entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.walkJson(entries, "response/1/files");
+            final Iterator<Entry<String, Object>> it = entries.entrySet().iterator();
+            while (it.hasNext()) {
+                final Entry<String, Object> ipentry = it.next();
+                final String quality = ipentry.getKey();
+                final String url = (String) ipentry.getValue();
+                if (url == null || !url.startsWith("http") || quality == null) {
+                    continue;
+                }
+                final DownloadLink dl;
+                if (url.contains(this.getHost())) {
+                    dl = createDownloadlink("directhttp://" + url);
+                    /* Hoster by embedy.cc and/or vk.com[vk.me] */
+                    dl.setFinalFileName(fpName + "_" + quality + ".mp4");
+                    dl.setAvailable(true);
+                    dl.setProperty("refURL", parameter);
+                    dl.setProperty("requestType", "GET");
+                } else {
+                    dl = createDownloadlink(url);
+                }
+                decryptedLinks.add(dl);
+            }
         }
         final FilePackage fp = FilePackage.getInstance();
         fp.setName(Encoding.htmlDecode(fpName.trim()));
