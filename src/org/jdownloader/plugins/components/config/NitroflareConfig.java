21f187d6385a74bcab124e4e45d0ecaeb8c1923c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-06-24 12:25:32 +0000

    *Plugins: Fixes/Changes/Maintenance* - nitroflarecom: implemented account API, moved to new plugin settings and added API setting refs #88302
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42519 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/config/NitroflareConfig.java
@@ -0,0 +1,39 @@
+package org.jdownloader.plugins.components.config;
+
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.jdownloader.plugins.config.Order;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.TakeValueFromSubconfig;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "nitroflare.com", type = Type.HOSTER)
+public interface NitroflareConfig extends PluginConfigInterface {
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @TakeValueFromSubconfig("allowMultipleFreeDownloads")
+    @DescriptionForConfigEntry("Allow multiple free downloads?\r\nThis might result in fatal errors!")
+    @Order(10)
+    boolean isAllowMultipleFreeDownloads();
+
+    void setAllowMultipleFreeDownloads(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(true)
+    @TakeValueFromSubconfig("trustAPIPremiumOnly")
+    @DescriptionForConfigEntry("Trust API about Premium Only flag?")
+    @Order(20)
+    boolean isTrustAPIAboutPremiumOnlyFlag();
+
+    void setTrustAPIAboutPremiumOnlyFlag(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("Use API for account check and premium downloading [recommended]?")
+    @Order(30)
+    boolean isUsePremiumAPIEnabled();
+
+    void setUsePremiumAPIEnabled(boolean b);
+}
