efa9b442ece0a1c4232234b1a7cfb28ac71ab197
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-07-22 13:29:13 +0000

    *Plugins: Fixes/Changes/Maintenance* - [XFS] clicknuploadto: updated free- and free account connection limits RE forum 83156 - degoocom: added subfolder supportRE forum 82694
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42630 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/DegooCom.java
+++ src/jd/plugins/decrypter/DegooCom.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
 import org.appwork.utils.StringUtils;
+import org.appwork.utils.parser.UrlQuery;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
@@ -29,9 +30,10 @@ import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "degoo.com" }, urls = { "https?://(?:cloud|app)\\.degoo\\.com/share/([A-Za-z0-9]+)" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "degoo.com" }, urls = { "https?://(?:cloud|app)\\.degoo\\.com/share/([A-Za-z0-9]+)(\\?ID=\\d+)?" })
 public class DegooCom extends PluginForDecrypt {
     public DegooCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -41,41 +43,83 @@ public class DegooCom extends PluginForDecrypt {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         final String folderID = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        final String fileID = UrlQuery.parse(parameter).get("ID");
         br.setAllowedResponseCodes(new int[] { 400 });
-        br.postPageRaw("https://rest-api.degoo.com/shared", String.format("{\"HashValue\":\"%s\",\"Limit\":100,\"FileID\":null,\"JWT\":null}", folderID));
+        final Map<String, Object> params = new HashMap<String, Object>();
+        params.put("HashValue", folderID);
+        params.put("FileID", fileID);
+        params.put("Limit", 100);
+        params.put("JWT", null);
+        br.postPageRaw("https://rest-api.degoo.com/shared", JSonStorage.serializeToJson(params));
         if (br.getHttpConnection().getResponseCode() == 400 || br.getHttpConnection().getResponseCode() == 404) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
+        String path = this.getAdoptedCloudFolderStructure();
+        if (path == null) {
+            path = "";
+        }
         Map<String, Object> entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
         final ArrayList<Object> ressourcelist = (ArrayList<Object>) entries.get("Items");
+        if (ressourcelist.size() == 0) {
+            logger.info("Empty folder");
+            final DownloadLink offline = this.createOfflinelink(parameter);
+            if (!StringUtils.isEmpty(path)) {
+                offline.setName(path);
+            }
+            decryptedLinks.add(offline);
+            return decryptedLinks;
+        }
+        FilePackage fp = null;
+        if (!StringUtils.isEmpty(path)) {
+            fp = FilePackage.getInstance();
+            fp.setProperty("ALLOW_MERGE", true);
+            fp.setName(path);
+        }
         for (final Object ressourceO : ressourcelist) {
             entries = (HashMap<String, Object>) ressourceO;
-            final String filename = (String) entries.get("Name");
+            final String title = (String) entries.get("Name");
             final int filesize = ((Integer) entries.get("Size")).intValue();
             final int categoryID = ((Integer) entries.get("Category")).intValue();
             final String id = Long.toString(((Long) entries.get("ID")).longValue());
-            if (StringUtils.isEmpty(filename) || id.equals("0")) {
+            if (StringUtils.isEmpty(title) || id.equals("0")) {
                 /* Skip invalid items */
                 continue;
             }
-            if (categoryID != 0) {
-                /* 2020-07-21: (Nested) Subfolders are not (yet) supported */
-                continue;
+            if (categoryID == 0) {
+                /* File */
+                final String directurl = (String) entries.get("URL");
+                final String contentURL = "https://cloud.degoo.com/share/" + folderID + "?ID=" + id;
+                final DownloadLink dl = this.createDownloadlink(contentURL);
+                dl.setContentUrl(contentURL);
+                dl.setAvailable(true);
+                dl.setFinalFileName(title);
+                if (!StringUtils.isEmpty(directurl)) {
+                    dl.setProperty("directurl", directurl);
+                }
+                if (filesize > 0) {
+                    dl.setDownloadSize(filesize);
+                }
+                if (!StringUtils.isEmpty(path)) {
+                    dl.setProperty(DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH, path);
+                }
+                if (fp != null) {
+                    dl._setFilePackage(fp);
+                }
+                decryptedLinks.add(dl);
+            } else {
+                /* Folder --> Goes back into crawler */
+                final String contentURL = "https://app.degoo.com/share/" + folderID + "?ID=" + id;
+                final DownloadLink dl = this.createDownloadlink(contentURL);
+                if (!StringUtils.isEmpty(title)) {
+                    if (StringUtils.isEmpty(path)) {
+                        dl.setProperty(DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH, title);
+                    } else {
+                        dl.setProperty(DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH, path + "/" + title);
+                    }
+                }
+                decryptedLinks.add(dl);
             }
-            final String directurl = (String) entries.get("URL");
-            final String contentURL = "https://app.degoo.com/share/" + folderID + "?ID=" + id;
-            final DownloadLink dl = this.createDownloadlink(contentURL);
-            dl.setContentUrl(contentURL);
-            dl.setAvailable(true);
-            dl.setFinalFileName(filename);
-            if (!StringUtils.isEmpty(directurl)) {
-                dl.setProperty("directurl", directurl);
-            }
-            if (filesize > 0) {
-                dl.setDownloadSize(filesize);
-            }
-            decryptedLinks.add(dl);
         }
         return decryptedLinks;
     }
