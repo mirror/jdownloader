ef7b210ceeedfa01550eb332776c6d936db90ce1
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-07-23 12:34:20 +0000

    *Plugins: Fixes/Changes/Maintenance* redditcom: - Added package name - Added url parser for text of posts
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42636 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 0cb793189b2daed8c6cd1c9327357f758e411369

--- src/jd/plugins/decrypter/RedditCom.java
+++ src/jd/plugins/decrypter/RedditCom.java
@@ -15,7 +15,9 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.decrypter;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
 
 import org.appwork.utils.StringUtils;
@@ -28,9 +30,10 @@ import jd.parser.html.HTMLParser;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "reddit.com" }, urls = { "https?://(?:www\\.)?reddit\\.com/r/[^/]+/comments/([a-z0-9]+)/[A-Za-z0-9\\-_]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "reddit.com" }, urls = { "https?://(?:www\\.)?reddit\\.com/r/([^/]+)/comments/([a-z0-9]+)/[A-Za-z0-9\\-_]+" })
 public class RedditCom extends PluginForDecrypt {
     public RedditCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -42,10 +45,22 @@ public class RedditCom extends PluginForDecrypt {
         return 1;
     }
 
+    @Override
+    protected DownloadLink createDownloadlink(final String url) {
+        final DownloadLink dl = super.createDownloadlink(url);
+        if (fp != null) {
+            dl._setFilePackage(fp);
+        }
+        return dl;
+    }
+
+    private FilePackage fp = null;
+
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
-        final String commentID = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        final String subredditTitle = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        final String commentID = new Regex(parameter, this.getSupportedLinks()).getMatch(1);
         // final Account acc = AccountController.getInstance().getValidAccount(this.getHost());
         // if (acc == null) {
         // throw new AccountRequiredException();
@@ -67,11 +82,22 @@ public class RedditCom extends PluginForDecrypt {
         for (final Object postO : ressourcelist) {
             entries = (LinkedHashMap<String, Object>) postO;
             entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.walkJson(entries, "data/children/{0}/data");
-            /* TODO: Add FilePackage handling */
-            // final String title = (String) entries.get("title");
-            /* Look for single URLs e.g. single pictures (e.g. often imgur.com URLs) */
+            final long createdTimestamp = JavaScriptEngineFactory.toLong(entries.get("created"), 0) * 1000;
+            final Date theDate = new Date(createdTimestamp);
+            final String dateFormatted = new SimpleDateFormat("yyy-MM-dd").format(theDate);
+            String title = (String) entries.get("title");
+            if (StringUtils.isEmpty(title)) {
+                /* Fallback */
+                title = commentID;
+            }
+            title = dateFormatted + "_" + subredditTitle + " - " + title;
+            fp = FilePackage.getInstance();
+            fp.setName(title);
+            /* 2020-07-23: TODO: This field might indicate selfhosted content: is_reddit_media_domain */
+            /* Look for single URLs e.g. single pictures (e.g. often imgur.com URLs, can also be selfhosted content) */
             final String externalURL = (String) entries.get("url");
-            if (externalURL != null) {
+            if (!StringUtils.isEmpty(externalURL)) {
+                logger.info("Found external URL");
                 decryptedLinks.add(this.createDownloadlink(externalURL));
             }
             /* Look for embedded content from external sources - the object is always given but can be empty */
@@ -105,22 +131,38 @@ public class RedditCom extends PluginForDecrypt {
                     }
                 }
             }
-            /* Look for selfhosted photo content */
+            /* Look for selfhosted photo content, Only add image if nothing else is found */
             if (decryptedLinks.size() == 0) {
-                /* Only add image if nothing else is found */
-                /* TODO: Add support for multiple images */
-                logger.info("Found nothing --> Looking for image");
-                final String bestImage = (String) JavaScriptEngineFactory.walkJson(entries, "preview/images/{0}/source/url");
-                if (bestImage != null) {
-                    logger.info("Found image");
-                    final DownloadLink dl = this.createDownloadlink("directhttp://" + bestImage);
-                    dl.setAvailable(true);
-                    decryptedLinks.add(dl);
+                final ArrayList<Object> imagesO = (ArrayList<Object>) JavaScriptEngineFactory.walkJson(entries, "preview/images");
+                if (imagesO != null) {
+                    logger.info(String.format("Found %d selfhosted images", imagesO.size()));
+                    for (final Object imageO : imagesO) {
+                        final LinkedHashMap<String, Object> imageInfo = (LinkedHashMap<String, Object>) imageO;
+                        final String bestImage = (String) JavaScriptEngineFactory.walkJson(imageInfo, "source/url");
+                        if (bestImage != null) {
+                            final DownloadLink dl = this.createDownloadlink("directhttp://" + bestImage);
+                            dl.setAvailable(true);
+                            decryptedLinks.add(dl);
+                        }
+                    }
                 } else {
-                    logger.info("Failed to find image");
+                    logger.info("Failed to find selfhosted image(s)");
                 }
             }
-            /* TODO: Look for URLs inside post text as a final fallback - this might possibly also be the field "url_overridden_by_dest". */
+            /* Look for URLs inside post text. selftext is always present but empty when not used. */
+            final String postText = (String) entries.get("selftext");
+            if (!StringUtils.isEmpty(postText)) {
+                logger.info("Looking for URLs in selftext");
+                String[] urls = HTMLParser.getHttpLinks(postText, null);
+                if (urls.length > 0) {
+                    logger.info(String.format("Found %d URLs in selftext", urls.length));
+                    for (final String url : urls) {
+                        decryptedLinks.add(this.createDownloadlink(url));
+                    }
+                } else {
+                    logger.info("Failed to find any URLs in selftext");
+                }
+            }
             /* Only grab first post - stop then! */
             break;
         }
