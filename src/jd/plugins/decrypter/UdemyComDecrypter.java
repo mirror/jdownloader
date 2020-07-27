f23363d0df1583085090c3fc84c05218c8715984
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-07-27 16:01:30 +0000

    *Plugins: Fixes/Changes/Maintenance* - udemycom: added support for "adopted folder structure" RE forum 469393
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42654 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/UdemyComDecrypter.java
+++ src/jd/plugins/decrypter/UdemyComDecrypter.java
@@ -48,7 +48,6 @@ public class UdemyComDecrypter extends PluginForDecrypt {
     @SuppressWarnings("deprecation")
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         final String parameter = param.toString();
-        final String courseName = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
         /* 2020-07-24: Don't do this anymore - always add all items of the course instead! */
         // if (parameter.matches(jd.plugins.hoster.UdemyCom.TYPE_SINGLE_PREMIUM_WEBSITE)) {
         // /* Single links --> Host plugin */
@@ -75,26 +74,44 @@ public class UdemyComDecrypter extends PluginForDecrypt {
             logger.info("Could not find any downloadable content");
             return decryptedLinks;
         }
-        this.br.getPage("https://www.udemy.com/api-2.0/courses/" + course_id + "/subscriber-curriculum-items?fields%5Basset%5D=@min,title,filename,asset_type,external_url,length&fields%5Bchapter%5D=@min,description,object_index,title,sort_order&fields%5Blecture%5D=@min,object_index,asset,supplementary_assets,sort_order,is_published,is_free&fields%5Bquiz%5D=@min,object_index,title,sort_order,is_published&page_size=9999");
+        this.br.getPage("https://www.udemy.com/api-2.0/courses/" + course_id + "/?fields[course]=title,headline,description,prerequisites,objectives,target_audiences,url,is_published,is_approved,is_practice_test_course,content_length_video,instructional_level,locale,content_length_practice_test_questions,content_info,num_subscribers,visible_instructors,is_paid,is_private,is_owner_terms_banned,is_owned_by_instructor_team,image_240x135,instructor_status,is_cpe_compliant,cpe_field_of_study,cpe_program_level,num_cpe_credits&fields[locale]=simple_english_title&fields[user]=url,title,job_title,image_200_H,description,display_name,image_50x50,initials,url_twitter,url_facebook,url_linkedin,url_youtube,url_personal_website&caching_intent=True");
+        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        String courseTitle = (String) entries.get("title");
+        if (StringUtils.isEmpty(courseTitle)) {
+            /* Fallback */
+            courseTitle = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        }
+        this.br.getPage("/api-2.0/courses/" + course_id + "/subscriber-curriculum-items?fields%5Basset%5D=@min,title,filename,asset_type,external_url,length&fields%5Bchapter%5D=@min,description,object_index,title,sort_order&fields%5Blecture%5D=@min,object_index,asset,supplementary_assets,sort_order,is_published,is_free&fields%5Bquiz%5D=@min,object_index,title,sort_order,is_published&page_size=9999");
         if (this.br.getHttpConnection().getResponseCode() == 403) {
             logger.info("User tried to download content which he did not pay for --> Impossible");
             return decryptedLinks;
-        }
-        if (br.getHttpConnection().getResponseCode() == 404) {
+        } else if (br.getHttpConnection().getResponseCode() == 404) {
             decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
-        LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
-        final LinkedHashMap<String, Object> page_info = (LinkedHashMap<String, Object>) entries.get("");
+        entries = (LinkedHashMap<String, Object>) JavaScriptEngineFactory.jsonToJavaMap(br.toString());
         final ArrayList<Object> ressourcelist = (ArrayList<Object>) entries.get("results");
         ArrayList<Object> ressourcelist_2 = null;
-        int position = 1;
+        int positionTOTAL = 1;
+        int positionChapter = 0;
+        final DecimalFormat chapterFormat = new DecimalFormat("000");
+        String sectionTitle = null;
         for (final Object courseo : ressourcelist) {
             entries = (LinkedHashMap<String, Object>) courseo;
             final String lecture_id = Long.toString(JavaScriptEngineFactory.toLong(entries.get("id"), 0));
             final String _class = (String) entries.get("_class");
             final Object supplementary_assets = entries.get("supplementary_assets");
-            if (lecture_id.equals("0") || !_class.equalsIgnoreCase("lecture")) {
+            if (_class.equals("chapter")) {
+                positionChapter += 1;
+                /* 2020-07-27: Single objects of courses and their "headlines" are all mixed into one array lol. */
+                sectionTitle = (String) entries.get("title");
+                if (StringUtils.isEmpty(sectionTitle)) {
+                    /* This should never happen */
+                    sectionTitle = "unknown_" + (positionTOTAL - 1);
+                }
+                sectionTitle = chapterFormat.format(positionChapter) + "_" + sectionTitle;
+                continue;
+            } else if (lecture_id.equals("0") || !_class.equalsIgnoreCase("lecture")) {
                 /* Hm maybe some type we don't support (yet). */
                 continue;
             }
@@ -102,25 +119,24 @@ public class UdemyComDecrypter extends PluginForDecrypt {
             if (entries == null) {
                 continue;
             }
-            decryptAsset(entries, courseName, lecture_id, position);
+            decryptedLinks.add(decryptAsset(entries, courseTitle, sectionTitle, lecture_id, positionTOTAL));
             /* Add file content */
             if (supplementary_assets != null) {
                 ressourcelist_2 = (ArrayList<Object>) supplementary_assets;
                 for (final Object supplementary_asseto : ressourcelist_2) {
                     entries = (LinkedHashMap<String, Object>) supplementary_asseto;
-                    decryptAsset(entries, courseName, lecture_id, position);
+                    decryptedLinks.add(decryptAsset(entries, courseTitle, sectionTitle, lecture_id, positionTOTAL));
                 }
             }
-            position++;
+            positionTOTAL++;
         }
-        final FilePackage fp = FilePackage.getInstance();
-        fp.setName(courseName);
-        fp.addLinks(decryptedLinks);
         return decryptedLinks;
     }
 
     /** Crawls single object (video or document) */
-    private void decryptAsset(final LinkedHashMap<String, Object> entries, final String courseTitle, final String lecture_id, final int position) {
+    private DownloadLink decryptAsset(final LinkedHashMap<String, Object> entries, final String courseTitle, final String chapterTitle, final String lecture_id, final int position) {
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(courseTitle + " - " + chapterTitle);
         final DecimalFormat df = new DecimalFormat("000");
         final String position_formatted = df.format(position);
         String asset_id = Long.toString(JavaScriptEngineFactory.toLong(entries.get("id"), 0));
@@ -128,8 +144,8 @@ public class UdemyComDecrypter extends PluginForDecrypt {
         final String filename = (String) entries.get("filename");
         /* E.g. Video, Article, File */
         final String asset_type = (String) entries.get("asset_type");
-        if (asset_id.equals("0") || asset_type == null || asset_type.equals("")) {
-            return;
+        if (asset_id.equals("0") || StringUtils.isEmpty(asset_type)) {
+            return null;
         }
         String filename_temp;
         if (!StringUtils.isEmpty(filename)) {
@@ -149,7 +165,7 @@ public class UdemyComDecrypter extends PluginForDecrypt {
             /* TODO: Check if normal (e.g. "Video") assets can also contain an "external_url" object. */
             final String external_url = (String) entries.get("external_url");
             if (external_url == null || external_url.equals("")) {
-                return;
+                return null;
             }
             dl = createDownloadlink(external_url);
         } else {
@@ -162,8 +178,14 @@ public class UdemyComDecrypter extends PluginForDecrypt {
             dl.setProperty("lecture_id", lecture_id);
             dl.setProperty("course_id", course_id);
             dl.setProperty("position", position);
-            dl.setLinkID(asset_id);
+            /* Set relative download path - ignore "/" inside these strings otherwise we'll get wrong paths ;) */
+            if (asset_type.equalsIgnoreCase("video")) {
+                dl.setProperty(DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH, courseTitle.replace("/", "_") + "/" + chapterTitle.replace("/", "_"));
+            } else {
+                dl.setProperty(DownloadLink.RELATIVE_DOWNLOAD_FOLDER_PATH, courseTitle.replace("/", "_") + "/" + chapterTitle.replace("/", "_") + "/ressources");
+            }
         }
-        decryptedLinks.add(dl);
+        dl._setFilePackage(fp);
+        return dl;
     }
 }
