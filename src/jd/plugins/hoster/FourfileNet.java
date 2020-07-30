3fb2de556aeff4bed61ff4338a2a11c9421b456a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-07-30 10:40:16 +0000

    *Plugins: Fixes/Changes/Maintenance* - ehentaiorg: allow max. one captcha per login attempt, fixed login captcha handling RE forum 85109 - [XFS] FourfileNet: main domain has changed once again back to their old one RE forum 85108
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42666 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: b67b8f5f545ea6877619d12e52eca6e4a0356299

--- src/jd/plugins/hoster/FourfileNet.java
+++ src/jd/plugins/hoster/FourfileNet.java
@@ -15,7 +15,8 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.appwork.utils.StringUtils;
 import org.jdownloader.plugins.components.XFileSharingProBasic;
@@ -42,7 +43,31 @@ public class FourfileNet extends XFileSharingProBasic {
      * captchatype-info: 2019-02-21: reCaptchaV2<br />
      * other:<br />
      */
-    private static String[] domains = new String[] { "4file.net", "filedwon.info", "filedwon.net", "filedwon.com", "4file.win" };
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "filedwon.info", "4file.net", "filedwon.net", "filedwon.com", "4file.win" });
+        return ret;
+    }
+
+    @Override
+    public String rewriteHost(String host) {
+        /* 2020-07-30: Main domain has changed BACK from 4file.net to filedwon.info. */
+        return this.rewriteHost(getPluginDomains(), host, new String[0]);
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return XFileSharingProBasic.buildAnnotationUrls(getPluginDomains());
+    }
 
     @Override
     public boolean isResumeable(final DownloadLink link, final Account account) {
@@ -113,39 +138,5 @@ public class FourfileNet extends XFileSharingProBasic {
             dllink = new Regex(src, "id=\"direct_link\"[^<>]*?>\\s*<a href=\"(http[^\"]+)\"").getMatch(0);
         }
         return dllink;
-    }
-
-    public static String[] getAnnotationNames() {
-        return new String[] { domains[0] };
-    }
-
-    @Override
-    public String[] siteSupportedNames() {
-        return domains;
-    }
-
-    /**
-     * returns the annotation pattern array: 'https?://(?:www\\.)?(?:domain1|domain2)/(?:embed\\-)?[a-z0-9]{12}'
-     *
-     */
-    public static String[] getAnnotationUrls() {
-        // construct pattern
-        final String host = getHostsPattern();
-        return new String[] { host + "/(?:embed\\-)?[a-z0-9]{12}(?:/[^/]+\\.html)?" };
-    }
-
-    /** returns 'https?://(?:www\\.)?(?:domain1|domain2)' */
-    private static String getHostsPattern() {
-        final String hosts = "https?://(?:www\\.)?" + "(?:" + getHostsPatternPart() + ")";
-        return hosts;
-    }
-
-    /** Returns '(?:domain1|domain2)' */
-    public static String getHostsPatternPart() {
-        final StringBuilder pattern = new StringBuilder();
-        for (final String name : domains) {
-            pattern.append((pattern.length() > 0 ? "|" : "") + Pattern.quote(name));
-        }
-        return pattern.toString();
     }
 }
