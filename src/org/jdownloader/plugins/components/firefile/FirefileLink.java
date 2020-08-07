ea7a185487af3129eb02e8101e4711f8e7177c0e
Niccoo <Niccoo@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-08-07 08:15:54 +0000

    added firefile.cc as hoster
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42692 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d10a4e38eef69dc54d3f90efd55cb3121da5526a

---
+++ src/org/jdownloader/plugins/components/firefile/FirefileLink.java
@@ -0,0 +1,22 @@
+package org.jdownloader.plugins.components.firefile;
+
+public class FirefileLink {
+    private String hash;
+    private String key;
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
