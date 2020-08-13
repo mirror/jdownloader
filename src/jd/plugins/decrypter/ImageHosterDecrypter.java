7cf124796e10b096ef83772e8ba31ed8a5dc2b3b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-08-13 12:54:26 +0000

    *Plugins: Fixes/Changes/Maintenance* - MOCH real-debrid.com: do not display expire date for free accounts and set 0 traffic on free accounts RE forum 85205 - jetloadnet: updated APIkey and improved linkcheck RE forum 85219 - chomikujpl free: prever normal download over stream download RE forum 85218 - vkontakte: added support for videos with only one HLS quality RE forum 30431 refs #85330
    *Plugins: New plugins or new websites that belong to existing scripts*
    - MightyScriptAdLinkFly: ExeIo: added new domain bolssccom RE forum 85220 refs #84841
    
    *Plugins: RIP*
    - timekiller-erotic.com fixes #88687 purposely not adding this to list of offline hosts
    - [ImageHosterDecrypter] picsee.net fixes #88015
    - [KernelVideoSharingCom] updatetube.com fixes #88174 purposely not adding this to list of offline hosts
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42725 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f0538a227a9629f1387a15293ebc641c99cd2e90

--- src/jd/plugins/decrypter/ImageHosterDecrypter.java
+++ src/jd/plugins/decrypter/ImageHosterDecrypter.java
@@ -18,6 +18,8 @@ package jd.plugins.decrypter;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
@@ -30,10 +32,7 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
 
-import org.jdownloader.plugins.components.antiDDoSForDecrypt;
-
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "pic5you.ru", "image2you.ru", "picsee.net", "imagecurl.com", "twitpic.com", "pic4you.ru", "turboimagehost.com", "imagebam.com", "imagebam.com", "freeimagehosting.net", "girlswithmuscle.com" }, urls = { "http://pic5you\\.ru/\\d+/\\d+/", "http://(?:www\\.)?image2you\\.ru/\\d+/\\d+/", "http://(www\\.)?picsee\\.net/\\d{4}-\\d{2}-\\d{2}/.*?\\.html", "http://(?:www\\.)?imagecurl\\.com/viewer\\.php\\?file=[\\w-]+\\.[a-z]{2,4}", "https?://(www\\.)?twitpic\\.com/show/[a-z]+/[a-z0-9]+", "http://(?:www\\.)?pic4you\\.ru/\\d+/\\d+/", "https?://(?:www\\.)?turboimagehost\\.com/p/\\d+/.*?\\.html", "https?://[\\w\\.]*imagebam\\.com/(image|gallery)/[a-z0-9]+", "https?://thumbs\\d+\\.imagebam\\.com/\\d+/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+", "http://[\\w\\.]*?freeimagehosting\\.net/image\\.php\\?.*?\\..{3,4}",
-        "https?://(www.)?girlswithmuscle\\.com/\\d+/?" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "pic5you.ru", "image2you.ru", "imagecurl.com", "twitpic.com", "pic4you.ru", "turboimagehost.com", "imagebam.com", "imagebam.com", "freeimagehosting.net", "girlswithmuscle.com" }, urls = { "http://pic5you\\.ru/\\d+/\\d+/", "http://(?:www\\.)?image2you\\.ru/\\d+/\\d+/", "http://(?:www\\.)?imagecurl\\.com/viewer\\.php\\?file=[\\w-]+\\.[a-z]{2,4}", "https?://(www\\.)?twitpic\\.com/show/[a-z]+/[a-z0-9]+", "http://(?:www\\.)?pic4you\\.ru/\\d+/\\d+/", "https?://(?:www\\.)?turboimagehost\\.com/p/\\d+/.*?\\.html", "https?://[\\w\\.]*imagebam\\.com/(image|gallery)/[a-z0-9]+", "https?://thumbs\\d+\\.imagebam\\.com/\\d+/[a-z0-9]+/[a-z0-9]+/[a-z0-9]+", "http://[\\w\\.]*?freeimagehosting\\.net/image\\.php\\?.*?\\..{3,4}", "https?://(www.)?girlswithmuscle\\.com/\\d+/?" })
 public class ImageHosterDecrypter extends antiDDoSForDecrypt {
     public ImageHosterDecrypter(final PluginWrapper wrapper) {
         super(wrapper);
@@ -174,8 +173,6 @@ public class ImageHosterDecrypter extends antiDDoSForDecrypt {
             if (finallink == null) {
                 finallink = br.getRegex("To view its <a href=\"(http://cdn\\.imagecurl\\.com/images/\\w+\\.[a-z]{2,4})\">true size<").getMatch(0) + finallink;
             }
-        } else if (parameter.contains("picsee.net/")) {
-            finallink = parameter.replace("picsee.net/", "picsee.net/upload/").replace(".html", "");
         } else if (parameter.contains("image2you.ru/")) {
             br.getPage(parameter);
             if (this.br.getHttpConnection().getResponseCode() == 404) {
