5af345cd12e3750c9083a512c91f02d4c6ff8c8d
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-09-14 12:35:52 +0000

    *Plugins: Fixes/Changes/Maintenance* - volafileio: added support for password protected URLs refs #83154 RE forum 85441
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42839 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/VolaFileOrg.java
+++ src/jd/plugins/decrypter/VolaFileOrg.java
@@ -6,6 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.net.websocket.ReadWebSocketFrame;
+import org.appwork.utils.net.websocket.WebSocketFrameHeader;
+import org.appwork.utils.parser.UrlQuery;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.plugins.CryptedLink;
@@ -14,13 +22,8 @@ import jd.plugins.DownloadLink;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
+import jd.plugins.components.PluginJSonUtils;
 import jd.websocket.WebSocketClient;
-
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.Regex;
-import org.appwork.utils.net.websocket.ReadWebSocketFrame;
-import org.appwork.utils.net.websocket.WebSocketFrameHeader;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "volafile.org" }, urls = { "https?://(?:www\\.)?volafile\\.(?:org|io)/r/[A-Za-z0-9\\-_]+" })
 public class VolaFileOrg extends PluginForDecrypt {
@@ -32,48 +35,79 @@ public class VolaFileOrg extends PluginForDecrypt {
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         br.getPage(parameter.getCryptedUrl().replace(".io/", ".org/"));
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            ret.add(this.createOfflinelink(parameter.getCryptedUrl()));
+            return ret;
+        }
         br.followRedirect();
         br.setCookie(getHost(), "allow-download", "1");
-        final String checksum = br.getRegex("\"checksum2\"\\s*:\\s*\"(.*?)\"").getMatch(0);
+        final String checksum = PluginJSonUtils.getJson(br, "checksum2");
         String room = br.getRegex("\"room_id\"\\s*:\\s*\"(.*?)\"").getMatch(0);
         if (room == null) {
             room = new Regex(parameter.getCryptedUrl(), "/r/([A-Za-z0-9\\-_]+)").getMatch(0);
         }
-        final WebSocketClient wsc = new WebSocketClient(br, new URL("https://volafile.org/api/?room=" + URLEncoder.encode(room, "UTF-8") + "&cs=" + URLEncoder.encode(checksum, "UTF-8") + "&nick=Alke&EIO=3&transport=websocket"));
-        try {
-            wsc.connect();
-            ReadWebSocketFrame frame = wsc.readNextFrame();// sid
-            frame = wsc.readNextFrame();// session
-            frame = wsc.readNextFrame();// subscription
-            if (WebSocketFrameHeader.OP_CODE.UTF8_TEXT.equals(frame.getOpCode()) && frame.isFin()) {
-                String string = new String(frame.getPayload(), "UTF-8");
-                string = string.replaceFirst("^\\d+", "");
-                final List<Object> list = JSonStorage.restoreFromString(string, TypeRef.LIST);
-                List<Object> files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(6)).get(0)).get(1)).get(1)).get("files");
-                if (files == null) {
-                    files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(7)).get(0)).get(1)).get(1)).get("files");
-                }
-                if (files == null) {
-                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-                } else {
-                    for (final Object file : files) {
-                        final List<Object> fileInfo = (List<Object>) file;
-                        final String fileName = String.valueOf(fileInfo.get(1));
-                        final Number fileSize = ((Number) fileInfo.get(3));
-                        final DownloadLink downloadLink = createDownloadlink("https://volafile.org/download/" + fileInfo.get(0) + "/" + URLEncoder.encode(fileName, "UTF-8"));
-                        downloadLink.setFinalFileName(fileName);
-                        downloadLink.setVerifiedFileSize(fileSize.longValue());
-                        downloadLink.setAvailable(true);
-                        ret.add(downloadLink);
-                    }
-                }
-            } else {
-                logger.severe("Unsupported:" + frame);
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            }
-        } finally {
-            wsc.close();
+        if (StringUtils.isEmpty(checksum)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
+        final UrlQuery query = new UrlQuery();
+        query.add("room", URLEncoder.encode(room, "UTF-8"));
+        query.add("cs", URLEncoder.encode(checksum, "UTF-8"));
+        query.add("nick", "Alke");
+        query.add("EIO", "3");
+        query.add("transport", "websocket");
+        WebSocketClient wsc = null;
+        String passCode = null;
+        do {
+            try {
+                wsc = new WebSocketClient(br, new URL("https://volafile.org/api/?" + query.toString()));
+                wsc.connect();
+                ReadWebSocketFrame frame = wsc.readNextFrame();// sid
+                frame = wsc.readNextFrame();// session
+                frame = wsc.readNextFrame();// subscription
+                if (WebSocketFrameHeader.OP_CODE.UTF8_TEXT.equals(frame.getOpCode()) && frame.isFin()) {
+                    String string = new String(frame.getPayload(), "UTF-8");
+                    string = string.replaceFirst("^\\d+", "");
+                    if (string.startsWith("[-1") && !string.contains("\"files\"")) {
+                        /* Password protected content */
+                        if (passCode != null) {
+                            /* Wrong password - Don't allow 2nd try. */
+                            throw new PluginException(LinkStatus.ERROR_RETRY, "Wrong password entered");
+                        }
+                        passCode = getUserInput("Password?", parameter);
+                        query.add("password", passCode);
+                        continue;
+                    }
+                    final List<Object> list = JSonStorage.restoreFromString(string, TypeRef.LIST);
+                    List<Object> files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(6)).get(0)).get(1)).get(1)).get("files");
+                    if (files == null) {
+                        files = (List<Object>) ((Map<String, Object>) ((List<Object>) ((List<Object>) ((List<Object>) list.get(7)).get(0)).get(1)).get(1)).get("files");
+                    }
+                    if (files == null) {
+                        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                    } else {
+                        for (final Object file : files) {
+                            final List<Object> fileInfo = (List<Object>) file;
+                            final String fileName = String.valueOf(fileInfo.get(1));
+                            final Number fileSize = ((Number) fileInfo.get(3));
+                            final DownloadLink link = createDownloadlink("https://volafile.org/download/" + fileInfo.get(0) + "/" + URLEncoder.encode(fileName, "UTF-8"));
+                            link.setFinalFileName(fileName);
+                            link.setVerifiedFileSize(fileSize.longValue());
+                            link.setAvailable(true);
+                            if (passCode != null) {
+                                link.setDownloadPassword(passCode);
+                            }
+                            ret.add(link);
+                        }
+                        break;
+                    }
+                } else {
+                    logger.severe("Unsupported:" + frame);
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+            } finally {
+                wsc.close();
+            }
+        } while (!StringUtils.isEmpty(passCode));
         return ret;
     }
 }
