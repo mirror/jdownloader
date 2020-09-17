922535c8e16bad8b39997341ef243ab224e91338
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-09-17 15:39:20 +0000

    *Plugins: Fixes/Changes/Maintenance* - vikicom: added simple preferred quality setting RE forum 85479
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42861 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5ebe43f8bd6cfa58866904f04f4bd318cd8d08ca

---
+++ src/org/jdownloader/plugins/components/config/VikiComConfig.java
@@ -0,0 +1,60 @@
+package org.jdownloader.plugins.components.config;
+
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultEnumValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.appwork.storage.config.annotations.LabelInterface;
+import org.jdownloader.plugins.config.Order;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "viki.com", type = Type.HOSTER)
+public interface VikiComConfig extends PluginConfigInterface {
+    public static enum PreferredStreamQuality implements LabelInterface {
+        BEST {
+            @Override
+            public String getLabel() {
+                return "Best";
+            }
+        },
+        Q240P {
+            @Override
+            public String getLabel() {
+                return "240p";
+            }
+        },
+        Q360P {
+            @Override
+            public String getLabel() {
+                return "360p";
+            }
+        },
+        Q480P {
+            @Override
+            public String getLabel() {
+                return "480p";
+            }
+        },
+        Q720P {
+            @Override
+            public String getLabel() {
+                return "720p";
+            }
+        },
+        Q1080P {
+            @Override
+            public String getLabel() {
+                return "1080p";
+            }
+        };
+    }
+
+    @AboutConfig
+    @DefaultEnumValue("BEST")
+    @DescriptionForConfigEntry("If your preferred stream quality is not found, best quality will be downloaded instead.")
+    @Order(10)
+    PreferredStreamQuality getPreferredStreamQuality();
+
+    void setPreferredStreamQuality(PreferredStreamQuality quality);
+}
