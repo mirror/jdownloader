cb7a44b941b6a3469a7c1343afb346c2c0746c37
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-09-30 12:41:03 +0000

    *Plugins: Fixes/Changes/Maintenance* - fucktubecom: back from the dead (well, probably new project on old known domain) fixes #88771
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42923 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/FucktubeCom.java
@@ -0,0 +1,103 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2017  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.plugins.components.antiDDoSForHost;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "fucktube.com" }, urls = { "https?://(?:www\\.)?fucktube\\.com/([a-z0-9\\-]+)-(\\d+)" })
+public class FucktubeCom extends antiDDoSForHost {
+    public FucktubeCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+    /* DEV NOTES */
+    // Tags: Porn plugin
+    // other:
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.fucktube.com/legal#terms";
+    }
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String linkid = getFID(link);
+        if (linkid != null) {
+            return this.getHost() + "://" + linkid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        link.setMimeHint(CompiledFiletypeFilter.VideoExtensions.MP4);
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String filename = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0).replace("-", " ");
+        link.setFinalFileName(filename + ".mp4");
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        final String hlsMaster = br.getRegex("url:\"(https?://[^\"]+\\.m3u8[^\"]*)\"").getMatch(0);
+        if (hlsMaster == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getPage(hlsMaster);
+        final HlsContainer hlsbest = HlsContainer.findBestVideoByBandwidth(HlsContainer.getHlsQualities(this.br));
+        checkFFmpeg(link, "Download a HLS Stream");
+        dl = new HLSDownloader(link, br, hlsbest.getDownloadurl());
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
