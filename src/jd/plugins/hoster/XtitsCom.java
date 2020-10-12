e399cd94ff3e84e1cc8bb6faa8a52d2c6e5a63b1
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-10-12 23:34:44 +0000

    *Plugins: Fixes/Changes/Maintenance* KernelVideoSharingCom: Started work on KernelVideoSharingComV2 - please keep it in plugins.hoster until it doesn't require frequent changes anymore refs #88825 --> It will soon be less chaotic ;) - [KVS] crockotubecom: upgraded to KVSv2 fixes #88829 - [KVS] xtitscom: upgraded to KVSv2 fixes #88812
    *Plugins: RIP*
    - [XFS] vidtome.co, vidtome.stream, vidto.me, vidto.se fixes #88846
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@42999 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/XtitsCom.java
@@ -0,0 +1,75 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jd.PluginWrapper;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class XtitsCom extends KernelVideoSharingComV2 {
+    public XtitsCom(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "xtits.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?:videos/\\d+/[a-z0-9\\-]+/?|embed/\\d+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    protected AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
+        final String fuid = this.getFUID(link.getPluginPatternMatcher());
+        if (fuid == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String filetitle = getURLFilename(link.getPluginPatternMatcher());
+        link.setFinalFileName(filetitle + ".mp4");
+        final String urlOriginal = link.getPluginPatternMatcher();
+        /* 2020-10-13: Special workaround to prevent serverside issue of random infinite redirects */
+        link.setPluginPatternMatcher("https://www.xtits.com/popup-video/" + fuid + "/");
+        try {
+            return super.requestFileInformation(link, isDownload);
+        } finally {
+            link.setPluginPatternMatcher(urlOriginal);
+        }
+    }
+}
