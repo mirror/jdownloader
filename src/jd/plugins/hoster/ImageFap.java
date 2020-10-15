36416f89159747bbae1274d56852f90733b6e94c
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-10-15 17:00:19 +0000

    DirectHTTP: -updated getExtensionFromMimeType, jpg ImageFap: -fixed filename handling, check/auto add extension via DirectHTTP.getExtensionFromMimeType
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43030 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c4bfea928fa051c820213e64e31ab5bae2a334b4

--- src/jd/plugins/hoster/ImageFap.java
+++ src/jd/plugins/hoster/ImageFap.java
@@ -340,10 +340,9 @@ public class ImageFap extends PluginForHost {
                 }
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
-            if (link.getFinalFileName() == null) {
-                if (!pfilename.endsWith(new Regex(finalLink, "(\\.[A-Za-z0-9]+)($|\\?)").getMatch(0))) {
-                    pfilename += new Regex(finalLink, "(\\.[A-Za-z0-9]+)($|\\?)").getMatch(0);
-                }
+            final String expectedExtension = DirectHTTP.getExtensionFromMimeType(dl.getConnection().getContentType());
+            if ((expectedExtension != null && !StringUtils.endsWithCaseInsensitive(link.getFinalFileName(), expectedExtension))) {
+                pfilename = pfilename + "." + expectedExtension;
                 link.setFinalFileName(pfilename);
             }
         }
@@ -364,7 +363,7 @@ public class ImageFap extends PluginForHost {
             br.getPage(request);
             if (br.getHttpConnection().getResponseCode() == 429) {
                 /*
-                 *
+                 * 
                  * 100 requests per 1 min 200 requests per 5 min 1000 requests per 1 hour
                  */
                 /* 2020-09-22: Most likely they will allow a retry after one hour. */
@@ -461,19 +460,19 @@ public class ImageFap extends PluginForHost {
     }
 
     private HashMap<String, String> phrasesEN = new HashMap<String, String>() {
-        {
-            put("SETTING_FORCE_RECONNECT_ON_RATELIMIT", "Reconnect if rate limit is reached and captcha is required?");
-            put("LABEL_FILENAME", "Define custom filename for pictures:");
-            put("SETTING_TAGS", "Explanation of the available tags:\r\n*username* = Name of the user who posted the content\r\n*title* = Original title of the picture including file extension\r\n*galleryname* = Name of the gallery in which the picture is listed\r\n*orderid* = Position of the picture in a gallery e.g. '0001'");
-        }
-    };
+                                                  {
+                                                      put("SETTING_FORCE_RECONNECT_ON_RATELIMIT", "Reconnect if rate limit is reached and captcha is required?");
+                                                      put("LABEL_FILENAME", "Define custom filename for pictures:");
+                                                      put("SETTING_TAGS", "Explanation of the available tags:\r\n*username* = Name of the user who posted the content\r\n*title* = Original title of the picture including file extension\r\n*galleryname* = Name of the gallery in which the picture is listed\r\n*orderid* = Position of the picture in a gallery e.g. '0001'");
+                                                  }
+                                              };
     private HashMap<String, String> phrasesDE = new HashMap<String, String>() {
-        {
-            put("SETTING_FORCE_RECONNECT_ON_RATELIMIT", "Führe einen Reconnect durch, wenn das Rate-Limit erreicht ist und ein Captcha benötigt wird?");
-            put("LABEL_FILENAME", "Gib das Muster des benutzerdefinierten Dateinamens für Bilder an:");
-            put("SETTING_TAGS", "Erklärung der verfügbaren Tags:\r\n*username* = Name des Benutzers, der den Inhalt veröffentlicht hat \r\n*title* = Originaler Dateiname mitsamt Dateiendung\r\n*galleryname* = Name der Gallerie, in der sich das Bild befand\r\n*orderid* = Position des Bildes in einer Gallerie z.B. '0001'");
-        }
-    };
+                                                  {
+                                                      put("SETTING_FORCE_RECONNECT_ON_RATELIMIT", "Führe einen Reconnect durch, wenn das Rate-Limit erreicht ist und ein Captcha benötigt wird?");
+                                                      put("LABEL_FILENAME", "Gib das Muster des benutzerdefinierten Dateinamens für Bilder an:");
+                                                      put("SETTING_TAGS", "Erklärung der verfügbaren Tags:\r\n*username* = Name des Benutzers, der den Inhalt veröffentlicht hat \r\n*title* = Originaler Dateiname mitsamt Dateiendung\r\n*galleryname* = Name der Gallerie, in der sich das Bild befand\r\n*orderid* = Position des Bildes in einer Gallerie z.B. '0001'");
+                                                  }
+                                              };
 
     /**
      * Returns a German/English translation of a phrase. We don't use the JDownloader translation framework since we need only German and
