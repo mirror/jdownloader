4478e829106f3bb8f640503dc3a96cccf232f15b
dev_man <dev_man@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-10-17 09:51:36 +0000

    added support for galleries of hqsluts.com, sexhd.pics, xxxporn.pics and viewgals.com
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43037 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/HqSlutsCom.java
@@ -0,0 +1,27 @@
+package jd.plugins.decrypter;
+
+import org.appwork.utils.Regex;
+
+import jd.PluginWrapper;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.DecrypterPlugin;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hqsluts.com" }, urls = { "https?://(?:www\\.)?hqsluts\\.com/[^/]+-\\d+" })
+public class HqSlutsCom extends SimpleHtmlBasedGalleryPlugin {
+    public HqSlutsCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    protected String getFilePackageName(String url) {
+        String title = br.getRegex("<title>\\s*([^<>]+?)\\s*</title>").getMatch(0);
+        String uri = new Regex(url, "hqsluts\\.com/(.+)").getMatch(0);
+        String id = new Regex(url, "hqsluts\\.com/[^/]+-(\\d+)").getMatch(0);
+        String name;
+        if (title == null) {
+            name = uri;
+        } else {
+            name = title + " " + id;
+        }
+        return name != null ? Encoding.htmlDecode(name.trim()) : null;
+    }
+}
