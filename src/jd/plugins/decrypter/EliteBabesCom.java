6a05219de93fe1a8fa7d60536af18c356863df36
dev_man <dev_man@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-10-22 18:49:13 +0000

    merged SimpleHtmlBasedGalleriesPlugin into SimpleHtmlBasedGalleryPlugin. further reworks and improvements
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43082 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/EliteBabesCom.java
+++ src/jd/plugins/decrypter/EliteBabesCom.java
@@ -2,6 +2,7 @@ package jd.plugins.decrypter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.appwork.utils.Regex;
 import org.appwork.utils.StringUtils;
@@ -11,14 +12,54 @@ import jd.plugins.DecrypterPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 
-//@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "elitebabes.com" }, urls = { "https?://(?:www\\.)?elitebabes\\.com/model/.+" })
-public class EliteBabesCom extends SimpleHtmlBasedGalleriesPlugin {
+/**
+ * Downloads single galleries and all per model from elitebabes.com. Re-uses functionality from SimpleHtmlBasedGalleryPlugin, but has
+ * specific functionality to get the gallery urls.
+ */
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
+public class EliteBabesCom extends SimpleHtmlBasedGalleryPlugin {
+
+    private static final SiteData SITE_DATA = new SiteData("elitebabes.com", "/(?!model/).+", "/model/.+", null);
 
     public EliteBabesCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    protected String[] getGalleryUrls() throws PluginException {
+    public static List<String[]> getSupportedSites() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        ret.add(new String[] { SITE_DATA.host, SITE_DATA.getUrlRegex() });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] supportedSite : getSupportedSites()) {
+            ret.add(supportedSite[0]);
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return new String[] { getHost() };
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] supportedSite : getSupportedSites()) {
+            ret.add(supportedSite[1]);
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    protected List<SiteData> getSiteData() {
+        ArrayList<SiteData> siteData = new ArrayList<SiteData>();
+        siteData.add(SITE_DATA);
+        return siteData;
+    }
+
+    protected String[] getGalleryUrls(String galleryHrefRegex) throws PluginException {
         String[][] listItems = br.getRegex("<li>(.*?)</li>").getMatches();
         if (listItems.length == 0) {
             return new String[0];
