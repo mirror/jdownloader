6242823a547c2240420dec41ccfddb9fe3d1317e
dev_man <dev_man@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-10-23 10:47:03 +0000

    auto-paging for all galleries per model for EliteBabesCom
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43083 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/EliteBabesCom.java
+++ src/jd/plugins/decrypter/EliteBabesCom.java
@@ -2,6 +2,7 @@ package jd.plugins.decrypter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.appwork.utils.Regex;
@@ -15,11 +16,13 @@ import jd.plugins.PluginException;
 /**
  * Downloads single galleries and all per model from elitebabes.com. Re-uses functionality from SimpleHtmlBasedGalleryPlugin, but has
  * specific functionality to get the gallery urls.
+ * 
+ * It supports auto-paging for all galleries of a model.
  */
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class EliteBabesCom extends SimpleHtmlBasedGalleryPlugin {
 
-    private static final SiteData SITE_DATA = new SiteData("elitebabes.com", "/(?!model/).+", "/model/.+", null);
+    private static final SiteData SITE_DATA = new SiteData("elitebabes.com", "/(?!model/).+", "/model/.+", "[^\"']+");
 
     public EliteBabesCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -39,6 +42,7 @@ public class EliteBabesCom extends SimpleHtmlBasedGalleryPlugin {
         return ret.toArray(new String[0]);
     }
 
+    // TODO REVIEW really necessary?
     @Override
     public String[] siteSupportedNames() {
         return new String[] { getHost() };
@@ -59,26 +63,57 @@ public class EliteBabesCom extends SimpleHtmlBasedGalleryPlugin {
         return siteData;
     }
 
-    protected String[] getGalleryUrls(String galleryHrefRegex) throws PluginException {
+    // first page is "https://www.elitebabes.com/model/alena-i/" (accessible via UI)
+    // next pages are "https://www.elitebabes.com/model/alena-i/mpage/2/" (NOT accessible via UI, only programmatic)
+    protected boolean fetchMoreGalleries() throws IOException {
+        String[][] matches = br.getRegex("href\\s*=\\s*(?:\"|')([^\"']+mpage[^\"']+)(?:\"|')").getMatches();
+        if (matches.length == 0) {
+            // only 1 page, no next pages
+            return false;
+        }
+        ArrayList<String> atLeastPage2Urls = new ArrayList<String>();
+        for (String[] match : matches) {
+            atLeastPage2Urls.add(match[0]);
+        }
+        Collections.sort(atLeastPage2Urls); // ascending, so .../mpage/2/, .../mpage/3/, and so on
+        // find current page
+        String currentUrl = br.getURL();
+        int currentPageIndex = -1; // 1st page, not containing "mpage"
+        for (int i = 0; i < atLeastPage2Urls.size(); i++) {
+            if (currentUrl.equals(atLeastPage2Urls.get(i))) {
+                currentPageIndex = i;
+                break;
+            }
+        }
+        // if next page defined
+        if (currentPageIndex < atLeastPage2Urls.size() - 1) {
+            // navigate to next page
+            br.getPage(atLeastPage2Urls.get(currentPageIndex + 1)); // TODO handle 404 ?
+            return true;
+        }
+        return false;
+    }
+
+    protected ArrayList<String> getCurrentGalleryUrls(String galleryHrefRegex) throws PluginException {
+        ArrayList<String> galleryUrls = new ArrayList<String>();
         String[][] listItems = br.getRegex("<li>(.*?)</li>").getMatches();
         if (listItems.length == 0) {
-            return new String[0];
+            return galleryUrls;
         }
-        ArrayList<String> urls = new ArrayList<String>(listItems.length);
         for (String[] listItem : listItems) {
             try {
                 if (!listItem[0].contains("title")) {
                     continue;
                 }
-                String galleryUrl = new Regex(listItem[0], "href\\s*=\\s*(?:\"|')([^\"']+)(?:\"|')").getMatch(0);
+                String galleryUrl = new Regex(listItem[0], "href\\s*=\\s*(?:\"|')(" + galleryHrefRegex + ")(?:\"|')").getMatch(0);
                 if (StringUtils.isEmpty(galleryUrl)) {
                     throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, "no gallery match found");
                 }
-                urls.add(br.getURL(galleryUrl).toString());
+                galleryUrls.add(br.getURL(galleryUrl).toString());
             } catch (IOException e) {
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT, null, e);
             }
         }
-        return urls.toArray(new String[0]);
+        return galleryUrls;
     }
 }
