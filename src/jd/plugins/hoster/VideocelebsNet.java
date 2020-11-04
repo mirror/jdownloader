e5db3688716a69ca3206ae4c521d0e8b434a2137
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-04 12:48:52 +0000

    *Plugins: Fixes/Changes/Maintenance* KVSv2: refs #88825 - migrated more hosts to KVSv2: videocelebsnet, pornyeahcom, porngemcom, yourlustcom - Fixed regression in embed handling: Sometimes, wrong URLs were detected as "real URL2 and set as pluginPatternMatcher - Fixed bug where sometimes the best quality was not found if it was "crypted"
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43145 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 1655490b310e6ccf5f3cab7d7e4354224611df9e

---
+++ src/jd/plugins/hoster/VideocelebsNet.java
@@ -0,0 +1,59 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2020  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jd.PluginWrapper;
+import jd.plugins.HostPlugin;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class VideocelebsNet extends KernelVideoSharingComV2 {
+    public VideocelebsNet(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /** Add all KVS hosts to this list that fit the main template without the need of ANY changes to this class. */
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "videocelebs.net" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([a-z0-9\\-]+\\.html|embed/\\d+/?)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    protected boolean hasFUIDAtEnd(final String url) {
+        return false;
+    }
+}
