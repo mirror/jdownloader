e96140255b939658654a39b7c2b505da1530ed82
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-06 11:51:04 +0000

    *Plugins: Fixes/Changes/Maintenance* - DiskYandexNetFolder: improved paths for RELATIVE_DOWNLOAD_FOLDER_PATH - play44net: improved errorhandling fixes #86800
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [KVS] KernelVideoSharingComV2HostsDefault: deviantscom closes #88977
    - [XFS] sendcm
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43165 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/KernelVideoSharingComV2HostsDefault.java
+++ src/jd/plugins/hoster/KernelVideoSharingComV2HostsDefault.java
@@ -81,6 +81,7 @@ public class KernelVideoSharingComV2HostsDefault extends KernelVideoSharingComV2
         ret.add(new String[] { "tubepornclassic.com" });
         ret.add(new String[] { "camvideos.org" });
         ret.add(new String[] { "nudogram.com" });
+        ret.add(new String[] { "deviants.com" });
         return ret;
     }
 
