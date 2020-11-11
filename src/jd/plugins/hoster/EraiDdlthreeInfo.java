16939a4dc18407c4f5e27abaa9970ffc02a5391c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-11 15:01:42 +0000

    *Plugins: Fixes/Changes/Maintenance* - MangatownCom: improved offline errorhandling fixes #89018 - [YetiShare] EraiDdlthreeInfo: fixed login and improved availablecheck but website is broken and downloads won't even work via free account RE forum 85439
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43199 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/EraiDdlthreeInfo.java
+++ src/jd/plugins/hoster/EraiDdlthreeInfo.java
@@ -15,16 +15,24 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.appwork.utils.StringUtils;
 import org.jdownloader.plugins.components.YetiShareCore;
 
 import jd.PluginWrapper;
+import jd.http.Cookies;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
+import jd.plugins.AccountRequiredException;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class EraiDdlthreeInfo extends YetiShareCore {
@@ -39,7 +47,7 @@ public class EraiDdlthreeInfo extends YetiShareCore {
      * mods: See overridden functions<br />
      * limit-info 2020-09-14: No limits at all :<br />
      * captchatype-info: 2020-09-14: null<br />
-     * other: <br />
+     * other: 2020-11-11: Website is broken - downloads won't even work when logged in via (Free-)Account <br />
      */
     public static List<String[]> getPluginDomains() {
         final List<String[]> ret = new ArrayList<String[]>();
@@ -115,5 +123,113 @@ public class EraiDdlthreeInfo extends YetiShareCore {
     public boolean requires_WWW() {
         /* 2020-09-14 */
         return false;
+    }
+
+    @Override
+    public String[] scanInfo(final DownloadLink link, final String[] fileInfo) {
+        super.scanInfo(link, fileInfo);
+        if (supports_availablecheck_over_info_page(link)) {
+            if (StringUtils.isEmpty(fileInfo[1])) {
+                fileInfo[1] = br.getRegex(">Filesize:</span>\\s*<span>([^<>\"]+)</span>").getMatch(0);
+            }
+        }
+        return fileInfo;
+    }
+
+    @Override
+    public void checkErrors(final DownloadLink link, final Account account) throws PluginException {
+        super.checkErrors(link, account);
+        if (br.containsHTML(">\\s*Please enter your information to register for an account")) {
+            throw new AccountRequiredException();
+        }
+    }
+
+    @Override
+    protected void loginWebsite(final Account account, boolean force) throws Exception {
+        synchronized (account) {
+            try {
+                br.setCookiesExclusive(true);
+                prepBrowser(this.br, account.getHoster());
+                br.setFollowRedirects(true);
+                final Cookies cookies = account.loadCookies("");
+                if (cookies != null) {
+                    this.br.setCookies(this.getHost(), cookies);
+                    if (System.currentTimeMillis() - account.getCookiesTimeStamp("") <= 300000l && !force) {
+                        /* We trust these cookies as they're not that old --> Do not check them */
+                        return;
+                    }
+                    logger.info("Verifying login-cookies");
+                    getPage(this.getMainPage() + "/account");
+                    if (isLoggedin()) {
+                        logger.info("Successfully logged in via cookies");
+                        account.saveCookies(this.br.getCookies(this.getHost()), "");
+                        return;
+                    } else {
+                        logger.info("Failed to login via cookies");
+                    }
+                }
+                logger.info("Performing full login");
+                getPage(this.getProtocol() + this.getHost() + "/account/login");
+                Form loginform;
+                /* Old login method - rare case! Example: udrop.net --> 2020-08-07: Website is dead */
+                logger.info("Using old login method");
+                loginform = br.getFormbyProperty("id", "form_login");
+                if (loginform == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                loginform.put("username", Encoding.urlEncode(account.getUser()));
+                loginform.put("password", Encoding.urlEncode(account.getPass()));
+                /* 2019-07-31: At the moment only this older login method supports captchas. Examplehost: uploadship.com */
+                if (br.containsHTML("solvemedia\\.com/papi/")) {
+                    /* Handle login-captcha if required */
+                    DownloadLink dlinkbefore = this.getDownloadLink();
+                    try {
+                        final DownloadLink dl_dummy;
+                        if (dlinkbefore != null) {
+                            dl_dummy = dlinkbefore;
+                        } else {
+                            dl_dummy = new DownloadLink(this, "Account", this.getHost(), "https://" + account.getHoster(), true);
+                            this.setDownloadLink(dl_dummy);
+                        }
+                        final org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia sm = new org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia(br);
+                        if (br.containsHTML("api\\-secure\\.solvemedia\\.com/")) {
+                            sm.setSecure(true);
+                        }
+                        File cf = null;
+                        try {
+                            cf = sm.downloadCaptcha(getLocalCaptchaFile());
+                        } catch (final Exception e) {
+                            if (org.jdownloader.captcha.v2.challenge.solvemedia.SolveMedia.FAIL_CAUSE_CKEY_MISSING.equals(e.getMessage())) {
+                                throw new PluginException(LinkStatus.ERROR_FATAL, "Host side solvemedia.com captcha error - please contact the " + this.getHost() + " support");
+                            }
+                            throw e;
+                        }
+                        final String code = getCaptchaCode("solvemedia", cf, dl_dummy);
+                        final String chid = sm.getChallenge(code);
+                        loginform.put("adcopy_challenge", chid);
+                        loginform.put("adcopy_response", "manual_challenge");
+                    } finally {
+                        if (dlinkbefore != null) {
+                            this.setDownloadLink(dlinkbefore);
+                        }
+                    }
+                }
+                submitForm(loginform);
+                if (br.containsHTML(">\\s*Your username and password are invalid<") || !isLoggedin()) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                account.saveCookies(this.br.getCookies(this.getHost()), "");
+            } catch (final PluginException e) {
+                if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                    account.clearCookies("");
+                }
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    protected boolean isLoggedin() {
+        return br.containsHTML("/account/logout\"");
     }
 }
