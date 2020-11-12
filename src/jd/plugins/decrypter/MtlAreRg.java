5c0f34ae07e8fefc5973059ba56a9429297a24ac
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-12 15:29:26 +0000

    *Plugins: Fixes/Changes/Maintenance* - metalarea: fixed old crawler RE forum 85910 - atvat: minor changes RE forum 85642
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43208 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MtlAreRg.java
+++ src/jd/plugins/decrypter/MtlAreRg.java
@@ -13,7 +13,6 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.io.IOException;
@@ -23,20 +22,21 @@ import jd.PluginWrapper;
 import jd.config.Property;
 import jd.controlling.ProgressController;
 import jd.gui.UserIO;
+import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.parser.html.HTMLParser;
+import jd.plugins.AccountRequiredException;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "metalarea.org" }, urls = { "http://[\\w\\.]*?metalarea\\.org/forum/index\\.php\\?showtopic=[0-9]+" }) 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "metalarea.org" }, urls = { "https?://(?:www\\.)?metalarea\\.org/forum/index\\.php\\?showtopic=([0-9]+)" })
 public class MtlAreRg extends PluginForDecrypt {
-
-    private static final String HOST = "http://metalarea.org";
     /* must be static so all plugins share same lock */
-    private static Object       LOCK = new Object();
+    private static Object LOCK = new Object();
 
     public MtlAreRg(PluginWrapper wrapper) {
         super(wrapper);
@@ -45,16 +45,21 @@ public class MtlAreRg extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
         ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        String parameter = param.toString();
+        final String parameter = param.toString();
         br.setCookiesExclusive(false);
-        br.setFollowRedirects(false);
+        br.setFollowRedirects(true);
         if (!getUserLogin(parameter)) {
-            logger.info("No or wrong logindata entered!");
+            logger.info("No- or wrong logindata entered!");
+            throw new AccountRequiredException();
+        } else if (br.getHttpConnection().getResponseCode() == 404) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
             return decryptedLinks;
         }
+        String fpName = br.getRegex("<title>(.*?)</title>").getMatch(0);
         // Filter links in hide(s)
         String pagepieces[] = br.getRegex("<\\!\\-\\-HideBegin\\-\\->(.*?)<\\!\\-\\-HideEnd\\-\\->").getColumn(0);
         if (pagepieces == null || pagepieces.length == 0) {
+            logger.info("Failed to find any HIDE-area");
             return null;
         }
         for (String pagepiece : pagepieces) {
@@ -63,53 +68,73 @@ public class MtlAreRg extends PluginForDecrypt {
                 for (String link : links) {
                     decryptedLinks.add(createDownloadlink(link));
                 }
-
             }
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName.trim()));
+            fp.addLinks(decryptedLinks);
         }
         return decryptedLinks;
-
     }
 
-    private boolean getUserLogin(String url) throws IOException, DecrypterException {
+    private boolean getUserLogin(final String url) throws IOException, DecrypterException {
         synchronized (LOCK) {
-            String username = this.getPluginConfig().getStringProperty("user", null);
-            String password = this.getPluginConfig().getStringProperty("pass", null);
-            for (int i = 0; i < 3; i++) {
-                if (username != null && password != null) {
-                    br.postPage("https://metalarea.org/forum/index.php?act=Login&CODE=01", "UserName=" + Encoding.urlEncode(username) + "&PassWord=" + Encoding.urlEncode(password) + "&CookieDate=1");
-                }
-                if (br.getCookie(HOST, "ma_tr_pass") == null || br.getCookie(HOST, "ma_tr_uid") == null) {
-                    this.getPluginConfig().setProperty("user", Property.NULL);
-                    this.getPluginConfig().setProperty("pass", Property.NULL);
-                    username = UserIO.getInstance().requestInputDialog("Enter Loginname for metalarea.org :");
-                    if (username == null) {
-                        return false;
-                    }
-                    password = UserIO.getInstance().requestInputDialog("Enter password for metalarea.org :");
-                    if (password == null) {
-                        return false;
-                    }
-                    br.postPage("https://metalarea.org/forum/index.php?act=Login&CODE=01", "UserName=" + Encoding.urlEncode(username) + "&PassWord=" + Encoding.urlEncode(password) + "&CookieDate=1");
-                }
-                if (br.getCookie(HOST, "ma_tr_pass") != null || br.getCookie(HOST, "ma_tr_uid") != null) {
-                    this.getPluginConfig().setProperty("user", username);
-                    this.getPluginConfig().setProperty("pass", password);
-                    this.getPluginConfig().save();
-                    br.getPage(url);
+            String logincookie = this.getPluginConfig().getStringProperty("masession_id");
+            if (logincookie != null) {
+                br.setCookie(this.getHost(), "masession_id", logincookie);
+                br.getPage(url);
+                if (this.isLoggedIN()) {
+                    logger.info("Cookie login successful");
                     return true;
                 } else {
-                    username = null;
-                    password = null;
-                    continue;
+                    logger.info("Cookie login failed");
+                    this.br.clearAll();
                 }
             }
+            logger.info("Full login required");
+            String username = this.getPluginConfig().getStringProperty("user", null);
+            String password = this.getPluginConfig().getStringProperty("pass", null);
+            if (username == null || password == null) {
+                username = UserIO.getInstance().requestInputDialog("Enter Loginname for metalarea.org :");
+                if (username == null) {
+                    return false;
+                }
+                password = UserIO.getInstance().requestInputDialog("Enter password for metalarea.org :");
+                if (password == null) {
+                    return false;
+                }
+            }
+            br.getPage("https://" + this.getHost() + "/forum/index.php?act=Login");
+            br.postPage("/forum/index.php?act=Login&CODE=01", "UserName=" + Encoding.urlEncode(username) + "&PassWord=" + Encoding.urlEncode(password) + "&CookieDate=1");
+            logincookie = br.getCookie(br.getHost(), "masession_id", Cookies.NOTDELETEDPATTERN);
+            if (logincookie != null && this.isLoggedIN()) {
+                logger.info("Full login successful");
+                this.getPluginConfig().setProperty("user", username);
+                this.getPluginConfig().setProperty("pass", password);
+                this.getPluginConfig().setProperty("masession_id", password);
+                this.getPluginConfig().save();
+                br.getPage(url);
+                return true;
+            } else {
+                /* Clear logindata so that we can ask user again the next time */
+                logger.info("Full login failed");
+                this.getPluginConfig().setProperty("user", Property.NULL);
+                this.getPluginConfig().setProperty("pass", Property.NULL);
+                this.getPluginConfig().setProperty("masession_id", Property.NULL);
+                this.getPluginConfig().save();
+                return false;
+            }
         }
-        return false;
+    }
+
+    boolean isLoggedIN() {
+        /* Check if user control-panel is visible. */
+        return br.containsHTML("id=\"userlinks\"");
     }
 
     /* NO OVERRIDE!! */
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
