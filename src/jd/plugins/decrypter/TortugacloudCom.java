949612c8a3a864326333841126fe78f85b283a16
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-12 16:36:46 +0000

    *Plugins: Fixes/Changes/Maintenance* - privatehomeclips.com: removed from Offline class so deepdecrypt can work with this domain fixes #86987 - [XFS] voesx: removed commented out lines of code
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [KVS] 3movscom closes #86641
    - crawler for url shortener tgc
    
    *Plugins: RIP*
    - shots.com: plugin has been broken for years & no demand anymore fixes #86105
    - nhaccuatui.com: plugin has been broken for years & no demand anymore fixes #86187
    - bongobd.com: plugin has been broken for years, users are better off using manual streaming download (HLS) fixes #86187
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43211 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/TortugacloudCom.java
@@ -0,0 +1,64 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.StringUtils;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "tortugacloud.com" }, urls = { "https?://(?:www\\.)?tg-c\\.cc/([a-z0-9]+)" })
+public class TortugacloudCom extends PluginForDecrypt {
+    public TortugacloudCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString();
+        final String itemID = new Regex(parameter, this.getSupportedLinks()).getMatch(0);
+        br.getPage("https://tortugacloud.com/api/v1/shorter/" + itemID);
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            decryptedLinks.add(this.createOfflinelink(parameter));
+            return decryptedLinks;
+        }
+        String finallink = null;
+        try {
+            final Map<String, Object> entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+            finallink = (String) entries.get("url");
+        } catch (final Throwable e) {
+            /* Parsing failure --> Offline */
+            decryptedLinks.add(this.createOfflinelink(parameter));
+        }
+        if (StringUtils.isEmpty(finallink)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        decryptedLinks.add(createDownloadlink(finallink));
+        return decryptedLinks;
+    }
+}
--- src/jd/plugins/hoster/VoeSx.java
+++ src/jd/plugins/hoster/VoeSx.java
@@ -50,29 +50,6 @@ public class VoeSx extends XFileSharingProBasic {
         return ret;
     }
 
-    // public static List<String[]> getPluginDomains() {
-    // final List<String[]> ret = new ArrayList<String[]>();
-    // // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
-    // ret.add(new String[] { "imgdew.com" });
-    // return ret;
-    // }
-    //
-    // public static String[] getAnnotationNames() {
-    // return buildAnnotationNames(getPluginDomains());
-    // }
-    //
-    // @Override
-    // public String[] siteSupportedNames() {
-    // return buildSupportedNames(getPluginDomains());
-    // }
-    //
-    // public static String[] getAnnotationUrls() {
-    // return XFileSharingProBasic.buildAnnotationUrls(getPluginDomains());
-    // }
-    // @Override
-    // public String rewriteHost(final String host) {
-    // return this.rewriteHost(getPluginDomains(), host, new String[0]);
-    // }
     public static final String getDefaultAnnotationPatternPartVoeSx() {
         return "/(?:embed-|e/)?[a-z0-9]{12}(?:/[^/]+(?:\\.html)?)?";
     }
