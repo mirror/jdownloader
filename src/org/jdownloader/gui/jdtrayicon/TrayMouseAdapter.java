d610418a770cc4889fbcdeb82bcc28347c054234
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-13 10:48:37 +0000

    TrayExtension: -refactored listener add/removal into start/stopListener methods -added support for indicator icon when clipboard monitoring is disabled -fixes thread 85929
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43219 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/gui/jdtrayicon/TrayMouseAdapter.java
+++ src/org/jdownloader/gui/jdtrayicon/TrayMouseAdapter.java
@@ -13,14 +13,15 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package org.jdownloader.gui.jdtrayicon;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.MouseInfo;
 import java.awt.Point;
@@ -33,49 +34,56 @@ import java.awt.event.MouseMotionListener;
 import java.awt.geom.Area;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
+
 import jd.gui.swing.jdgui.JDGui;
 
+import org.appwork.storage.config.ValidationException;
+import org.appwork.storage.config.events.GenericConfigEventListener;
+import org.appwork.storage.config.handler.KeyHandler;
 import org.appwork.utils.NullsafeAtomicReference;
+import org.appwork.utils.images.IconIO;
 import org.appwork.utils.logging2.LogSource;
 import org.appwork.utils.swing.EDTRunner;
+import org.jdownloader.gui.IconKey;
+import org.jdownloader.images.BadgeIcon;
+import org.jdownloader.images.NewTheme;
+import org.jdownloader.settings.staticreferences.CFG_GUI;
 
 public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
-
-    private TrayExtension                   deligate;
-    private NullsafeAtomicReference<Thread> mouseLocationObserver = new NullsafeAtomicReference<Thread>(null);
-    private Point                           min;
-    private Point                           max;
-    private Dimension                       size;
-    private MouseEvent                      lastEvent;
-    private Component                       dummy;
-    private LogSource                       logger;
-    private TrayIcon                        trayIcon;
-    private ArrayList<Area>                 possibleLocations;
-    private static int                      TOOLTIP_DELAY         = 1000;
+    private TrayExtension                             deligate;
+    private NullsafeAtomicReference<Thread>           mouseLocationObserver = new NullsafeAtomicReference<Thread>(null);
+    private Point                                     min;
+    private Point                                     max;
+    private Dimension                                 size;
+    private MouseEvent                                lastEvent;
+    private Component                                 dummy;
+    private LogSource                                 logger;
+    private final TrayIcon                            trayIcon;
+    private ArrayList<Area>                           possibleLocations;
+    private final Image                               image;
+    private final GenericConfigEventListener<Boolean> clipboardToggle;
+    private static int                                TOOLTIP_DELAY         = 1000;
 
     public TrayMouseAdapter(TrayExtension lightTray, TrayIcon trayIcon) {
         deligate = lightTray;
         this.trayIcon = trayIcon;
+        this.image = trayIcon.getImage();
         dummy = new Component() {
             private static final long serialVersionUID = 1L;
         };
         size = trayIcon.getSize();
         logger = JDGui.getInstance().getLogger();
-
         GraphicsDevice device = null;
-
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice lstGDs[] = ge.getScreenDevices();
-
         ArrayList<GraphicsDevice> lstDevices = new ArrayList<GraphicsDevice>(lstGDs.length);
         possibleLocations = new ArrayList<Area>();
         for (GraphicsDevice gd : lstGDs) {
-
             GraphicsConfiguration gc = gd.getDefaultConfiguration();
             Rectangle screenBounds = gc.getBounds();
             Rectangle safeArea = new Rectangle(screenBounds);
             Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(gd.getDefaultConfiguration());
-
             safeArea.x += insets.left;
             safeArea.y += insets.top;
             safeArea.width -= (insets.left + insets.right);
@@ -83,14 +91,55 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
             Area possibleArea = new Area(screenBounds);
             possibleArea.subtract(new Area(safeArea));
             possibleLocations.add(possibleArea);
-
         }
+        clipboardToggle = new GenericConfigEventListener<Boolean>() {
+            @Override
+            public void onConfigValueModified(KeyHandler<Boolean> keyHandler, Boolean newValue) {
+                TrayMouseAdapter.this.trayIcon.setImage(getCurrentTrayIconImage());
+            }
 
+            @Override
+            public void onConfigValidatorError(KeyHandler<Boolean> keyHandler, Boolean invalidValue, ValidationException validateException) {
+            }
+        };
+    }
+
+    public void startListener() {
+        CFG_GUI.CLIPBOARD_MONITORED.getEventSender().addListener(clipboardToggle, false);
+        CFG_GUI.CLIPBOARD_DISABLED_WARNING_FLASH_ENABLED.getEventSender().addListener(clipboardToggle, false);
+        new EDTRunner() {
+            @Override
+            protected void runInEDT() {
+                trayIcon.addMouseListener(TrayMouseAdapter.this);
+                trayIcon.addMouseMotionListener(TrayMouseAdapter.this);
+                trayIcon.setImage(getCurrentTrayIconImage());
+            }
+        };
+    }
+
+    private Image getCurrentTrayIconImage() {
+        if (org.jdownloader.settings.staticreferences.CFG_GUI.CLIPBOARD_MONITORED.isEnabled() || !CFG_GUI.CLIPBOARD_DISABLED_WARNING_FLASH_ENABLED.isEnabled()) {
+            return image;
+        } else {
+            return IconIO.toImage(new BadgeIcon(new ImageIcon(image), NewTheme.I().getCheckBoxImage(IconKey.ICON_CLIPBOARD, false, Math.max(8, image.getHeight(null) / 2), new Color(0xFF9393)), 4, 2));
+        }
+    }
+
+    public void stopListener() {
+        CFG_GUI.CLIPBOARD_MONITORED.getEventSender().removeListener(clipboardToggle);
+        CFG_GUI.CLIPBOARD_DISABLED_WARNING_FLASH_ENABLED.getEventSender().removeListener(clipboardToggle);
+        new EDTRunner() {
+            @Override
+            protected void runInEDT() {
+                trayIcon.removeMouseListener(TrayMouseAdapter.this);
+                trayIcon.removeMouseMotionListener(TrayMouseAdapter.this);
+                trayIcon.setImage(getCurrentTrayIconImage());
+            }
+        };
     }
 
     public void mouseClicked(MouseEvent e) {
         deligate.mouseClicked(e);
-
     }
 
     public void mouseEntered(MouseEvent e) {
@@ -100,7 +149,6 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
         if (mouseLocationObserver.get() != null) {
             return;
         }
-
         Thread localmouseLocationObserver = new Thread() {
             boolean    mouseStay = false;
             final long enterTime = System.currentTimeMillis();
@@ -125,7 +173,6 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
                                         MouseEvent me;
                                         me = new MouseEvent(dummy, 0, System.currentTimeMillis(), 0, point.x, point.y, 0, false);
                                         me.setSource(TrayMouseAdapter.this);
-
                                         deligate.mouseStay(me);
                                     }
                                 }
@@ -139,7 +186,6 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
                     mouseLocationObserver.compareAndSet(Thread.currentThread(), null);
                 }
             }
-
         };
         if (mouseLocationObserver.compareAndSet(null, localmouseLocationObserver)) {
             localmouseLocationObserver.setDaemon(true);
@@ -157,7 +203,6 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
      */
     private boolean validateLocation(Point point) {
         for (Area a : possibleLocations) {
-
             if (a.contains(point)) {
                 return true;
             }
@@ -197,37 +242,30 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
             return;
         }
         Point point = e.getPoint();
-
         lastEvent = e;
         /**
          * the more the user moves over the tray, the better we know it's location *
          */
-
         if (this.min == null) {
             this.min = new Point(point.x, point.y);
             this.max = new Point(point.x, point.y);
         } else {
             min.x = Math.min(point.x, min.x);
-
             min.y = Math.min(point.y, min.y);
-
             max.x = Math.max(point.x, max.x);
             max.y = Math.max(point.y, max.y);
             // System.out.println(min+" - "+max);
         }
-
         if (mouseLocationObserver.get() == null) {
             this.mouseEntered(e);
         } else {
             deligate.mouseMoved(e);
         }
-
     }
 
     public Point getEstimatedTopLeft() {
         int midx = (max.x + min.x) / 2;
         int midy = (max.y + min.y) / 2;
-
         return new Point(midx - size.width / 2, midy - size.height / 2);
     }
 
@@ -245,7 +283,6 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
         int midy = (max.y + min.y) / 2;
         // int width = Math.min(size.width, max.x - min.x);
         // int height = Math.min(size.height, max.y - min.y);
-
         /* the code above causes flickering of the tooltip because isOver only returns true in inner middle of the trayicon */
         int width = size.width;
         int height = size.height;
@@ -258,9 +295,7 @@ public class TrayMouseAdapter implements MouseListener, MouseMotionListener {
             if (point.y >= miny && point.y <= maxy) {
                 return true;
             }
-
         }
         return false;
     }
-
 }
