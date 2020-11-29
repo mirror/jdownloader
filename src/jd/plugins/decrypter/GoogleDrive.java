88f6e6f07ca0cdb25fea23600afaf801e9fd2c74
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-11-29 19:01:00 +0000

    *Plugins: Fixes/Changes/Maintenance* GDrive: refs #76108 - Improved errorhandling - Better errormessages for "Quota reached" files - Added more supported streaming file-extensions - Attempted to implement captcha but again failed to make it work
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43346 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/GoogleDrive.java
+++ src/jd/plugins/decrypter/GoogleDrive.java
@@ -87,7 +87,7 @@ public class GoogleDrive extends PluginForDecrypt {
             login(this.br, aa);
         } else {
             /* Respect users' plugin settings (e.g. custom User-Agent) */
-            ((jd.plugins.hoster.GoogleDrive) hostPlugin).prepBrowser(br, aa);
+            ((jd.plugins.hoster.GoogleDrive) hostPlugin).prepBrowser(br);
         }
         logger.info("LoggedIn:" + loggedin);
         if (parameter.contains("open?id")) {
