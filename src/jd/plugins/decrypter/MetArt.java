aedcae7822bf7d793d784e96a8d65b2baa061c50
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-12-08 13:37:42 +0000

    *Plugins: Fixes/Changes/Maintenance* - metart: fixed video crawler RE forum 86144
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43416 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 946b9fae4d0de90493b7c291b40ba4be4113609b

--- src/jd/plugins/decrypter/MetArt.java
+++ src/jd/plugins/decrypter/MetArt.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
 import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
 import org.appwork.utils.parser.UrlQuery;
 import org.jdownloader.scripting.JavaScriptEngineFactory;
 
@@ -15,7 +16,6 @@ import jd.PluginWrapper;
 import jd.controlling.AccountController;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
-import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.AccountRequiredException;
@@ -23,16 +23,19 @@ import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "metart.com", "sexart.com" }, urls = { "https?://members\\.(?:met\\-art|metart)\\.com/members/model/[A-Za-z0-9\\-\\_]+/movie/\\d+/[A-Za-z0-9\\-\\_]+/|https://(?:www\\.)?metart\\.com/model/[^/]+/gallery/\\d+/[A-Za-z0-9\\-_]+", "https?://members\\.sexart\\.com/members/model/[A-Za-z0-9\\-\\_]+/movie/\\d+/[A-Za-z0-9\\-\\_]+/|https://(?:www\\.)?sexart\\.com/model/[^/]+/gallery/\\d+/[A-Za-z0-9\\-_]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "metart.com", "sexart.com" }, urls = { "https://(?:www\\.)?metart\\.com/model/[^/]+/(gallery|movie)/\\d+/[A-Za-z0-9\\-_]+", "https://(?:www\\.)?sexart\\.com/model/[^/]+/(gallery|movie)/\\d+/[A-Za-z0-9\\-_]+" })
 public class MetArt extends PluginForDecrypt {
     public MetArt(PluginWrapper wrapper) {
         super(wrapper);
     }
 
     private static final String TYPE_GALLERY = "https://[^/]+/model/([^/]+)/gallery/(\\d+)/([^/]+)";
+    private static final String TYPE_MOVIE   = "https://[^/]+/model/([^/]+)/movie/(\\d+)/([^/]+)";
 
     @Override
     public int getMaxConcurrentProcessingInstances() {
@@ -64,7 +67,7 @@ public class MetArt extends PluginForDecrypt {
         final PluginForHost plg = this.getNewPluginForHostInstance(this.getHost());
         plg.setBrowser(this.br);
         /* TODO: We should be able to login without verifying to speed-up this crawler! */
-        ((jd.plugins.hoster.MetArtCom) plg).login(useAcc, true);
+        ((jd.plugins.hoster.MetArtCom) plg).login(useAcc, false);
         if (param.getCryptedUrl().matches(TYPE_GALLERY)) {
             /* New 2020-12-07 */
             final Regex urlinfo = new Regex(param.getCryptedUrl(), TYPE_GALLERY);
@@ -88,7 +91,7 @@ public class MetArt extends PluginForDecrypt {
                     continue;
                 }
                 final String filenameURL = UrlQuery.parse(url).get("filename");
-                final DownloadLink dl = this.createDownloadlink(url);
+                final DownloadLink dl = new DownloadLink(plg, filenameURL, this.getHost(), url, true);
                 dl.setAvailable(true);
                 if (filenameURL != null) {
                     dl.setName(filenameURL);
@@ -96,21 +99,80 @@ public class MetArt extends PluginForDecrypt {
                 dl._setFilePackage(fp);
                 ret.add(dl);
             }
-        } else {
-            /* Old: Revision 36050 and before */
-            final String links[] = br.getRegex("href=\"(https?://[^/]+/[^<>\"\\']+method=download)\"").getColumn(0);
-            String title = br.getRegex("<title>(.*?)</title").getMatch(0);
-            for (String link : links) {
-                link = Encoding.htmlDecode(link);
-                final DownloadLink dl = createDownloadlink(link.replaceAll("https?://", host_for_url));
+        } else if (param.getCryptedUrl().matches(TYPE_MOVIE)) {
+            /* New 2020-12-08 */
+            final Regex urlinfo = new Regex(param.getCryptedUrl(), TYPE_MOVIE);
+            final String modelname = urlinfo.getMatch(0);
+            final String date = urlinfo.getMatch(1);
+            final String galleryname = urlinfo.getMatch(2);
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(modelname + " - " + date + " - " + galleryname);
+            br.getPage("https://www." + this.getHost() + "/api/movie?name=" + galleryname + "&date=" + date);
+            if (br.getHttpConnection().getResponseCode() == 404) {
+                ret.add(this.createOfflinelink(param.getCryptedUrl()));
+                return ret;
+            }
+            Map<String, Object> entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+            final String title = (String) entries.get("name");
+            final String description = (String) entries.get("description");
+            final String uuid = (String) entries.get("UUID");
+            if (StringUtils.isEmpty(title) || StringUtils.isEmpty(uuid)) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            entries = (Map<String, Object>) entries.get("files");
+            final List<Object> teasersO = (List<Object>) entries.get("teasers");
+            Map<String, Object> sizes = (Map<String, Object>) entries.get("sizes");
+            final List<Object> videoQualitiesO = (List<Object>) sizes.get("videos");
+            for (final Object videoO : videoQualitiesO) {
+                entries = (Map<String, Object>) videoO;
+                final String id = (String) entries.get("id");
+                final String filesizeStr = (String) entries.get("size");
+                if (StringUtils.isEmpty(id) || StringUtils.isEmpty(filesizeStr)) {
+                    /* Skip invalid objects */
+                    continue;
+                }
+                final String ext;
+                if (id.matches("\\d+p")) {
+                    ext = "mp4";
+                } else if (id.equalsIgnoreCase("4k")) {
+                    ext = "mp4";
+                } else {
+                    /* E.g. avi, wmv */
+                    ext = id;
+                }
+                final String url = "https://www.metart.com/api/download-media/" + uuid + "/film/" + id;
+                String filename = modelname + " - " + title;
+                /* Do not e.g. generate filenames like "title_avi.avi" */
+                if (!ext.equals(id)) {
+                    filename += "_" + id;
+                }
+                filename += "." + ext;
+                final DownloadLink dl = new DownloadLink(plg, filename, this.getHost(), url, true);
+                dl.setDownloadSize(SizeFormatter.getSize(filesizeStr));
+                dl.setAvailable(true);
+                // dl.setFinalFileName(modelname + " - " + title + "_" + id + "." + ext);
+                if (!StringUtils.isEmpty(description)) {
+                    dl.setComment(description);
+                }
+                dl._setFilePackage(fp);
                 ret.add(dl);
             }
-            if (title != null) {
-                title = Encoding.htmlDecode(title);
-                final FilePackage fp = FilePackage.getInstance();
-                fp.setName(title);
-                fp.addLinks(ret);
+            if (ret.isEmpty() && !teasersO.isEmpty()) {
+                /* No downloads found -> Fallback to trailer download */
+                final DownloadLink trailer = this.createDownloadlink(br.getURL("/api/m3u8/" + uuid + "/720.m3u8").toString().replaceFirst("https?://", "m3u8s://"));
+                trailer.setAvailable(true);
+                trailer.setFinalFileName(modelname + " - " + title + " - teaser.mp4");
+                trailer._setFilePackage(fp);
+                ret.add(trailer);
+            } else if (ret.isEmpty()) {
+                /* Rare case */
+                logger.info("Failed to find any downloadable content");
+                ret.add(this.createOfflinelink(param.getCryptedUrl()));
+                return ret;
             }
+        } else {
+            /* Unsupported URL */
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         return ret;
     }
