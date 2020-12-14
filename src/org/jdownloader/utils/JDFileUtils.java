6174066531da99bd11f91adcb76de51b2e7a3a06
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2020-12-14 10:57:55 +0000

    refs #89044
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43467 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/utils/JDFileUtils.java
+++ src/org/jdownloader/utils/JDFileUtils.java
@@ -3,52 +3,67 @@ package org.jdownloader.utils;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.appwork.utils.Files;
 import org.appwork.utils.os.CrossSystem;
 
 public class JDFileUtils extends Files {
-
     private static final boolean supported = checkTrashSupported();
 
     private static boolean checkTrashSupported() {
         try {
             if (CrossSystem.isWindows()) {
                 return com.sun.jna.platform.win32.W32FileUtils.getInstance().hasTrash();
-            } else if (CrossSystem.isMac()) {
+            } else if (CrossSystem.isMac() && CrossSystem.getOS().isMaximum(CrossSystem.OperatingSystem.MAC_CATALINA)) {
+                // BigSur causes java.lang.UnsatisfiedLinkError: Unable to load library 'CoreServices':
+                // https://svn.jdownloader.org/issues/89044
                 return com.sun.jna.platform.mac.MacFileUtils.getInstance().hasTrash();
+            } else {
+                return false;
             }
         } catch (final Throwable e) {
             e.printStackTrace();
+            return false;
         }
-        return false;
     }
 
     /**
      * @param f
      */
-
     public static boolean isTrashSupported() {
         return supported;
     }
 
     public static void moveToTrash(File... files) throws IOException {
-        if (isTrashSupported()) {
-            if (CrossSystem.isWindows()) {
-                for (File f : files) {
-                    if (!f.exists()) {
-                        throw new FileNotFoundException(f.getAbsolutePath());
+        try {
+            if (isTrashSupported()) {
+                final List<File> existing = new ArrayList<File>();
+                FileNotFoundException exception = null;
+                for (final File file : files) {
+                    if (!file.exists()) {
+                        if (exception == null) {
+                            exception = new FileNotFoundException(file.getAbsolutePath());
+                        }
+                    } else {
+                        existing.add(file);
                     }
                 }
-                com.sun.jna.platform.win32.W32FileUtils.getInstance().moveToTrash(files);
-            } else if (CrossSystem.isMac()) {
-                for (File f : files) {
-                    if (!f.exists()) {
-                        throw new FileNotFoundException(f.getAbsolutePath());
+                if (existing.size() > 0) {
+                    if (CrossSystem.isWindows()) {
+                        com.sun.jna.platform.win32.W32FileUtils.getInstance().moveToTrash(existing.toArray(new File[0]));
+                    } else if (CrossSystem.isMac()) {
+                        com.sun.jna.platform.mac.MacFileUtils.getInstance().moveToTrash(existing.toArray(new File[0]));
                     }
                 }
-                com.sun.jna.platform.mac.MacFileUtils.getInstance().moveToTrash(files);
+                if (exception != null) {
+                    throw exception;
+                }
             }
+        } catch (final UnsatisfiedLinkError e) {
+            // may cause java.lang.UnsatisfiedLinkError
+            throw new IOException(e);
         }
     }
 }
