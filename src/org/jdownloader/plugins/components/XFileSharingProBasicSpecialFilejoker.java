61a483795da3270dda4334cb6e95dcbc5d0ee9c9
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-01-08 16:34:59 +0000

    XFileSharingProBasic/YetiShareCore: -updated correctDownloadLink and fixed host correction to keep subdomain even if it matches domain -updated setLinkID stuff, replaced by getLinkID method
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43600 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: a928107099d5f4f30f9bfbcfedda6605d6369ace

--- src/org/jdownloader/plugins/components/XFileSharingProBasicSpecialFilejoker.java
+++ src/org/jdownloader/plugins/components/XFileSharingProBasicSpecialFilejoker.java
@@ -5,11 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.formatter.TimeFormatter;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 import jd.PluginWrapper;
 import jd.http.Browser;
 import jd.http.Cookie;
@@ -28,6 +23,11 @@ import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.components.PluginJSonUtils;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
@@ -66,10 +66,10 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
         final String fuid = this.fuid != null ? this.fuid : getFUIDFromURL(link);
         if (fuid != null) {
             /* link cleanup, prefer https if possible */
-            if (link.getPluginPatternMatcher() != null && link.getPluginPatternMatcher().matches("https?://[A-Za-z0-9\\-\\.:]+/embed-[a-z0-9]{12}")) {
+            if (isEmbedURL(link)) {
                 link.setContentUrl(getMainPage() + "/embed-" + fuid + ".html");
             }
-            if (link.getPluginPatternMatcher() != null && link.getPluginPatternMatcher().matches("https?://[A-Za-z0-9\\-\\.:]+/file/[a-z0-9]{12}")) {
+            if (link.getPluginPatternMatcher().matches("https?://[A-Za-z0-9\\-\\.:]+/file/[a-z0-9]{12}")) {
                 // new fileIDs 23.11.2020 --> URLs with this pattern have to stay like this - it is not possible to remove the "/file/"
                 // part.
                 link.setPluginPatternMatcher(getMainPage() + "/file/" + fuid);
@@ -77,7 +77,6 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
                 // old fileIDs, not compatible with new ones
                 link.setPluginPatternMatcher(getMainPage() + "/" + fuid);
             }
-            link.setLinkID(getHost() + "://" + fuid);
         }
     }
 
@@ -178,8 +177,8 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
 
     /**
      * Turns on/off special API for (Free-)Account Login & Download. Keep this activated whenever possible as it will solve a lot of
-     * issues/complicated handling which is required for website login and download! </br>
-     * Sidenote: API Cookies will work fine for the website too so if enabled- and later disabled, login-captchas should still be avoided!
+     * issues/complicated handling which is required for website login and download! </br> Sidenote: API Cookies will work fine for the
+     * website too so if enabled- and later disabled, login-captchas should still be avoided!
      */
     protected boolean useAPIZeusCloudManager(final Account account) {
         return true;
@@ -191,8 +190,7 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
 
     /**
      * API login may avoid the need of login captchas. If enabled, ZeusCloudManagerAPI login will be tried even if API is disabled and
-     * resulting cookies will be used in website mode. Only enable this if tested! </br>
-     * default = false
+     * resulting cookies will be used in website mode. Only enable this if tested! </br> default = false
      */
     protected boolean tryAPILoginInWebsiteMode(final Account account) {
         return false;
@@ -200,9 +198,8 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
 
     /**
      * If enabled [and tryAPILoginInWebsiteMode enabled], API can be used to login- and obtain account information even if API is disabled
-     * and downloads will be executed via website. </br>
-     * If disabled [and tryAPILoginInWebsiteMode enabled], API can be used to login in website mode but account information will be obtained
-     * from website.
+     * and downloads will be executed via website. </br> If disabled [and tryAPILoginInWebsiteMode enabled], API can be used to login in
+     * website mode but account information will be obtained from website.
      */
     protected boolean tryAPILoginInWebsiteMode_get_account_info_from_api(final Account account) {
         return true;
@@ -245,8 +242,7 @@ public class XFileSharingProBasicSpecialFilejoker extends XFileSharingProBasic {
     }
 
     /**
-     * @return true = verified cookies/session </br>
-     *         false = did not verify cookies/session
+     * @return true = verified cookies/session </br> false = did not verify cookies/session
      */
     private final boolean loginAPIZeusCloudManager(final Browser apibr, final Account account, final boolean validateSession) throws Exception {
         synchronized (account) {
