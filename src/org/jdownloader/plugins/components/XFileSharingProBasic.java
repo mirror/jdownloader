f6589dd888376f48a163a3589738ce5b552da92b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-01-12 18:18:16 +0000

    *Plugins: RIP* - [XFS] snowfiles.com refs #87000
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43628 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 222151b914384ef496c19088b0cb6872f02a07e9

--- src/org/jdownloader/plugins/components/XFileSharingProBasic.java
+++ src/org/jdownloader/plugins/components/XFileSharingProBasic.java
@@ -840,7 +840,7 @@ public class XFileSharingProBasic extends antiDDoSForHost {
          */
         final String sharebox0 = "copy\\(this\\);.+>(.+) - ([\\d\\.]+ (?:B|KB|MB|GB))</a></textarea>[\r\n\t ]+</div>";
         final String sharebox1 = "copy\\(this\\);.+\\](.+) - ([\\d\\.]+ (?:B|KB|MB|GB))\\[/URL\\]";
-        /* 2019-05-08: 'Forum Code': Sharebox with filename & filesize (bytes), example: snowfiles.com, brupload.net, qtyfiles.com */
+        /* 2019-05-08: 'Forum Code': Sharebox with filename & filesize (bytes), example: brupload.net, qtyfiles.com */
         final String sharebox2 = "\\[URL=https?://(?:www\\.)?[^/\"]+/" + this.fuid + "[^\\]]*?\\]([^\"/]*?)\\s*\\-\\s*(\\d+)\\[/URL\\]";
         /* First found for pixroute.com URLs */
         final String sharebox2_without_filesize = "\\[URL=https?://(?:www\\.)?[^/\"]+/" + this.fuid + "/([^<>\"/\\]]*?)(?:\\.html)?\\]";
