f8e932fdf00c546f4b46f8a4f9ed35dbb5d8cb6e
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-02-03 12:31:29 +0000

    *Plugins: Fixes/Changes/Maintenance* - turbobit: added new domain turbobbitcom and added setting to define custom preferred domain RE forum 83153 - udmy: bugfix for official downloadurls and implemented additional login via cookies refs #85570
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43730 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 280fb86c5b97d4f815f7b513e635be29e1eeb0e1

--- src/org/jdownloader/plugins/components/config/TurbobitCoreConfigTurbobitNet.java
+++ src/org/jdownloader/plugins/components/config/TurbobitCoreConfigTurbobitNet.java
@@ -2,7 +2,9 @@ package org.jdownloader.plugins.components.config;
 
 import org.appwork.storage.config.annotations.AboutConfig;
 import org.appwork.storage.config.annotations.DefaultEnumValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
 import org.appwork.storage.config.annotations.LabelInterface;
+import org.jdownloader.plugins.config.Order;
 import org.jdownloader.plugins.config.PluginHost;
 import org.jdownloader.plugins.config.Type;
 
@@ -37,7 +39,16 @@ public interface TurbobitCoreConfigTurbobitNet extends TurbobitCoreConfig {
 
     @AboutConfig
     @DefaultEnumValue("DEFAULT")
+    @Order(10)
     PreferredDomain getPreferredDomain();
 
     void setPreferredDomain(PreferredDomain domain);
+
+    @AboutConfig
+    // @DefaultStringValue("")
+    @DescriptionForConfigEntry("Define custom preferred domain. If given this will be preferred over the above selection")
+    @Order(20)
+    String getCustomDomain();
+
+    void setCustomDomain(String str);
 }
