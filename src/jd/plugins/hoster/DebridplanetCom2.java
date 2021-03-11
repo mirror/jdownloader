86d9413cf30c3ae4034c164a4e5d02e199d232e4
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-03-11 14:13:29 +0000

    *General* - YetiShareCore: minor changes - more precise errormessage
    *Plugins: Fixes/Changes/Maintenance*
    - MOCH debridplanet.com: added test-plugin containing implementation of their new API
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43950 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: ccffaf25c791f95d7b65ca666e10114bc7fd292c

---
+++ src/jd/plugins/hoster/DebridplanetCom2.java
@@ -0,0 +1,244 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.jdownloader.plugins.controller.host.LazyHostPlugin.FEATURE;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.nutils.JDHash;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.AccountInfo;
+import jd.plugins.AccountRequiredException;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.components.MultiHosterManagement;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 0, names = { "debridplanet.com" }, urls = { "" })
+public class DebridplanetCom2 extends PluginForHost {
+    private static final String          API_BASE               = "https://debridplanet.com/v1";
+    private static MultiHosterManagement mhm                    = new MultiHosterManagement("debridplanet.com");
+    private static final boolean         resume                 = true;
+    private static final int             maxchunks              = -10;
+    private static final String          PROPERTY_ACCOUNT_TOKEN = "LOGIN_TOKEN";
+
+    @SuppressWarnings("deprecation")
+    public DebridplanetCom2(PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium(API_BASE + "/premium");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return API_BASE + "/tos";
+    }
+
+    private Browser prepBR(final Browser br) {
+        br.setCookiesExclusive(true);
+        br.setFollowRedirects(true);
+        br.getHeaders().put("User-Agent", "JDownloader");
+        return br;
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        return AvailableStatus.UNCHECKABLE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        throw new AccountRequiredException();
+    }
+
+    @Override
+    public void handlePremium(final DownloadLink link, final Account account) throws Exception {
+        throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_ONLY);
+    }
+
+    @Override
+    public FEATURE[] getFeatures() {
+        return new FEATURE[] { FEATURE.MULTIHOST };
+    }
+
+    @Override
+    public void handleMultiHost(final DownloadLink link, final Account account) throws Exception {
+        mhm.runCheck(account, link);
+        this.loginWebsite(account, false);
+        if (!attemptStoredDownloadurlDownload(link)) {
+            final Map<String, Object> postdata = new HashMap<String, Object>();
+            postdata.put("listurl", new ArrayList<String>().add(link.getDefaultPlugin().buildExternalDownloadURL(link, this)));
+            br.postPageRaw(API_BASE + "/gen_link.php", JSonStorage.serializeToJson(postdata));
+            final List<Object> ressourcelist = JSonStorage.restoreFromString(br.toString(), TypeRef.LIST);
+            final Map<String, Object> entries = (Map<String, Object>) ressourcelist.get(0);
+            final String dllink = (String) entries.get("link");
+            if (StringUtils.isEmpty(dllink)) {
+                mhm.handleErrorGeneric(account, link, "dllinknull", 50, 5 * 60 * 1000l);
+            }
+            if (StringUtils.isEmpty(dllink)) {
+                mhm.handleErrorGeneric(account, link, "dllinknull", 50, 5 * 60 * 1000l);
+            }
+            link.setProperty(this.getHost() + "directlink", dllink);
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resume, maxchunks);
+            if (!this.looksLikeDownloadableContent(dl.getConnection())) {
+                try {
+                    br.followConnection(true);
+                } catch (final IOException e) {
+                    logger.log(e);
+                }
+                mhm.handleErrorGeneric(account, link, "unknown_dl_error", 10, 5 * 60 * 1000l);
+            }
+        }
+        this.dl.startDownload();
+    }
+
+    private boolean attemptStoredDownloadurlDownload(final DownloadLink link) throws Exception {
+        final String url = link.getStringProperty(this.getHost() + "directlink");
+        if (url == null) {
+            return false;
+        }
+        try {
+            dl = new jd.plugins.BrowserAdapter().openDownload(br, this.getDownloadLink(), url, resume, maxchunks);
+            if (this.looksLikeDownloadableContent(dl.getConnection())) {
+                return true;
+            } else {
+                dl.getConnection().disconnect();
+                return false;
+            }
+        } catch (final Throwable e) {
+            try {
+                dl.getConnection().disconnect();
+            } catch (final Throwable e2) {
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
+        final AccountInfo ai = new AccountInfo();
+        loginWebsite(account, true);
+        /* TODO */
+        // if (br.getRequest() == null || !br.getURL().contains("/login.php")) {
+        // br.getPage("/account");
+        // }
+        Map<String, Object> entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+        entries = (Map<String, Object>) entries.get("user");
+        final String accountType = (String) entries.get("account_type");
+        ai.setStatus("Premium account");
+        if (accountType.equalsIgnoreCase("free")) {
+            account.setType(AccountType.FREE);
+            /*
+             * 2020-12-08: Free Accounts can download from some hosts (see websites' host list) but we just won't allow free account
+             * downloads at all.
+             */
+            ai.setTrafficLeft(0);
+        } else {
+            /* There are also accounts without expire-date! */
+            final Object expireDateO = entries.get("expire");
+            if (expireDateO != null) {
+                final String expireDate = (String) expireDateO;
+                ai.setValidUntil(TimeFormatter.getMilliSeconds(expireDate, "yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH), this.br);
+            }
+            account.setType(AccountType.PREMIUM);
+            ai.setUnlimitedTraffic();
+        }
+        br.getPage(API_BASE + "/supportedhosts.php");
+        entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+        final ArrayList<String> supportedHosts = new ArrayList<String>();
+        final List<Object> supportedHostsO = (List<Object>) entries.get("supportedhosts");
+        for (final Object supportedHostO : supportedHostsO) {
+            entries = (Map<String, Object>) supportedHostO;
+            final String domain = (String) entries.get("host");
+            final boolean currently_working = ((Boolean) entries.get("currently_working")).booleanValue();
+            if (!currently_working) {
+                logger.info("Skipping offline host: " + domain);
+                continue;
+            }
+            supportedHosts.add(domain);
+        }
+        ai.setMultiHostSupport(this, supportedHosts);
+        account.setConcurrentUsePossible(true);
+        return ai;
+    }
+
+    private void loginWebsite(final Account account, final boolean validateLogins) throws IOException, PluginException, InterruptedException {
+        synchronized (account) {
+            try {
+                prepBR(this.br);
+                if (account.getStringProperty(PROPERTY_ACCOUNT_TOKEN) != null) {
+                    logger.info("Trying to login via token");
+                    br.getHeaders().put("Authorization", "Bearer " + account.getStringProperty(PROPERTY_ACCOUNT_TOKEN));
+                    if (!validateLogins) {
+                        logger.info("Trust token without check");
+                        return;
+                    } else {
+                        logger.info("Validating login token...");
+                        br.getPage(API_BASE + "/login.php");
+                        /* TODO */
+                        // if (this.isLoggedIN()) {
+                        // logger.info("Cookie login successful");
+                        // account.saveCookies(br.getCookies(br.getHost()), "");
+                        // return;
+                        // } else {
+                        // logger.info("Cookie login failed");
+                        // }
+                    }
+                }
+                logger.info("Performing full login");
+                final Map<String, Object> postdata = new HashMap<String, Object>();
+                postdata.put("username", account.getUser());
+                postdata.put("password", JDHash.getSHA256(account.getPass()));
+                br.postPageRaw(API_BASE + "/login.php", JSonStorage.serializeToJson(postdata));
+                final Map<String, Object> entries = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
+                final String token = (String) entries.get("token");
+                if (StringUtils.isEmpty(token)) {
+                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
+                }
+                br.getHeaders().put("Authorization", "Bearer " + token);
+                account.setProperty(PROPERTY_ACCOUNT_TOKEN, token);
+            } catch (PluginException e) {
+                if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
+                    account.clearCookies("");
+                }
+                throw e;
+            }
+        }
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
