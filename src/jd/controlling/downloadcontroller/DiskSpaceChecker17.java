6d4d81c2ede08b326cffb6f8e954aa57d7d03997
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-03-17 15:14:16 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43972 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/downloadcontroller/DiskSpaceChecker17.java
+++ src/jd/controlling/downloadcontroller/DiskSpaceChecker17.java
@@ -1,44 +1,53 @@
 package jd.controlling.downloadcontroller;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.appwork.utils.Files17;
+import org.appwork.utils.StringUtils;
 
 public class DiskSpaceChecker17 extends DiskSpaceChecker {
+    protected static boolean MOUNT_POINT_NOT_FOUND_IN_FSTAB = false;
+
     public DiskSpaceChecker17(DiskSpaceReservation diskSpaceReservation, final Object requestor) {
         super(diskSpaceReservation, requestor);
     }
 
     @Override
     protected void findRoots() {
-        try {
-            final File dest = getDestination();
-            final Path normalRoot = Files17.guessRoot(dest.toPath());
-            if (normalRoot != null) {
-                roots.addIfAbsent(normalRoot.toString());
-                Path currentPath = dest.toPath();
-                Path realPath = null;
-                while (currentPath != null) {
-                    if (Files.exists(currentPath)) {
-                        realPath = currentPath.toRealPath();
-                        break;
-                    } else if (currentPath.equals(normalRoot)) {
-                        break;
-                    } else {
-                        currentPath = currentPath.getParent();
+        if (!MOUNT_POINT_NOT_FOUND_IN_FSTAB) {
+            try {
+                final File dest = getDestination();
+                final Path normalRoot = Files17.guessRoot(dest.toPath());
+                if (normalRoot != null) {
+                    roots.addIfAbsent(normalRoot.toString());
+                    Path currentPath = dest.toPath();
+                    Path realPath = null;
+                    while (currentPath != null) {
+                        if (Files.exists(currentPath)) {
+                            realPath = currentPath.toRealPath();
+                            break;
+                        } else if (currentPath.equals(normalRoot)) {
+                            break;
+                        } else {
+                            currentPath = currentPath.getParent();
+                        }
+                    }
+                    if (realPath != null) {
+                        final Path realRoot = Files17.guessRoot(realPath);
+                        if (realRoot != null) {
+                            roots.addIfAbsent(realRoot.toString());
+                        }
                     }
                 }
-                if (realPath != null) {
-                    final Path realRoot = Files17.guessRoot(realPath);
-                    if (realRoot != null) {
-                        roots.addIfAbsent(realRoot.toString());
-                    }
+            } catch (Throwable e) {
+                if (e instanceof IOException && StringUtils.contains(e.getMessage(), "Mount point not found in fstab")) {
+                    MOUNT_POINT_NOT_FOUND_IN_FSTAB = true;
                 }
+                getLogger().log(e);
             }
-        } catch (Throwable e) {
-            getLogger().log(e);
         }
         if (roots.size() == 0) {
             super.findRoots();
@@ -47,14 +56,16 @@ public class DiskSpaceChecker17 extends DiskSpaceChecker {
 
     @Override
     public long getUsableSpace() {
-        final File dest = getDestination();
-        try {
-            final Long ret = Files17.getUsableSpace(dest.toPath());
-            if (ret != null) {
-                return ret.longValue();
+        if (!MOUNT_POINT_NOT_FOUND_IN_FSTAB) {
+            final File dest = getDestination();
+            try {
+                final Long ret = Files17.getUsableSpace(dest.toPath());
+                if (ret != null) {
+                    return ret.longValue();
+                }
+            } catch (Throwable e) {
+                getLogger().log(e);
             }
-        } catch (Throwable e) {
-            getLogger().log(e);
         }
         return super.getUsableSpace();
     }
