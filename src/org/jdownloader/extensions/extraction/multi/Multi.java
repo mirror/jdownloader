5ca095fffab042409a408d3d51282fe13ad3f2fc
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-03-19 13:26:45 +0000

    Extraction: -added native library for aarch64,armhf,armv7 for musl
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@43984 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 96370df7203784a52e1645e97f6b381e5b4ce312

--- src/org/jdownloader/extensions/extraction/multi/Multi.java
+++ src/org/jdownloader/extensions/extraction/multi/Multi.java
@@ -331,11 +331,18 @@ public class Multi extends IExtraction {
                 switch (arch) {
                 case ARM:
                     if (is64BitJvm) {
+                        if (LibCDetector.isMuslSupported()) {
+                            libIDs.add("Linux-aarch64-musl");
+                        }
                         // new scheme
                         libIDs.add("Linux-arm64");
                         // old scheme
                         libIDs.add("Linux-aarch64");
                     } else {
+                        if (LibCDetector.isMuslSupported()) {
+                            libIDs.add("Linux-armhf-musl");
+                            libIDs.add("Linux-armv7-musl");
+                        }
                         // new scheme
                         libIDs.add("Linux-armv5");
                         if (HardwareType.getHardware() != null && HardwareTypeInterface.ID.QNAP.equals(HardwareType.getHardware().getHardwareType())) {
@@ -1206,25 +1213,25 @@ public class Multi extends IExtraction {
                     if (signatureString.length() >= 24) {
                         /*
                          * 0x0001 Volume attribute (archive volume)
-                         *
+                         * 
                          * 0x0002 Archive comment present RAR 3.x uses the separate comment block and does not set this flag.
-                         *
+                         * 
                          * 0x0004 Archive lock attribute
-                         *
+                         * 
                          * 0x0008 Solid attribute (solid archive)
-                         *
+                         * 
                          * 0x0010 New volume naming scheme ('volname.partN.rar')
-                         *
+                         * 
                          * 0x0020 Authenticity information present RAR 3.x does not set this flag.
-                         *
+                         * 
                          * 0x0040 Recovery record present
-                         *
+                         * 
                          * 0x0080 Block headers are encrypted
                          */
                         final String headerBitFlags1 = "" + signatureString.charAt(20) + signatureString.charAt(21);
                         /*
                          * 0x0100 FIRST Volume
-                         *
+                         * 
                          * 0x0200 EncryptedVerion
                          */
                         // final String headerBitFlags2 = "" + signatureString.charAt(22) + signatureString.charAt(23);
