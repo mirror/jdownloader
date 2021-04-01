8b7790e3bbdda17cec89dcf11fc84b7cf218b48c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-04-01 11:14:43 +0000

    *General* YetiShareCore: refs #89317 - Make sure that we're logged in during account downloading - Added premiumonly errorhandling for new YetiShare websites (new and old is kinda mixed but that's fine I'd like to merge them anyways) - Some work on API mode though I still did not find a single YetiShare website with working or enabled API capabilities
    *Plugins: Fixes/Changes/Maintenance*
    - Yourfreepornus: updated main domain, fixed premiumonly errorhandling RE forum 87111
    
    *Plugins: New plugins or new websites that belong to existing scripts*
    GenericYetiShareFolder:
    - added przeslijcom
    - added support for folder type (pageType) "nonaccountshared"
    - added more meaningful names for empty folders (include path to empty folder in dummy filename)
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44052 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2a1dd7b9d5537889f28c9c0071df53f374b22db5

--- src/jd/plugins/hoster/YourFreePornTv.java
+++ src/jd/plugins/hoster/YourFreePornTv.java
@@ -16,6 +16,8 @@
 package jd.plugins.hoster;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.appwork.utils.StringUtils;
 
@@ -29,11 +31,11 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "yourfreeporn.us" }, urls = { "https?://(?:www\\.)?yourfreeporn\\.(?:us|tv)/video/\\d+(/[a-z0-9\\-_]+)?" })
-public class YourFreePornUs extends PluginForHost {
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
+public class YourFreePornTv extends PluginForHost {
     private String dllink = null;
 
-    public YourFreePornUs(PluginWrapper wrapper) {
+    public YourFreePornTv(PluginWrapper wrapper) {
         super(wrapper);
     }
 
@@ -47,52 +49,96 @@ public class YourFreePornUs extends PluginForHost {
         return 1;
     }
 
-    @SuppressWarnings("deprecation")
-    public void correctDownloadLink(final DownloadLink link) {
-        link.setUrlDownload(link.getDownloadURL().replace("yourfreeporn.us/", "yourfreeporn.tv/"));
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "yourfreeporn.tv", "yourfreeporn.us" });
+        return ret;
     }
 
-    private static final String HTML_LIMITREACHED = ">You have reached your free daily limit";
-    private static final String HTML_PREMIUMONLY  = "class=\"goPremiumPitch\"";
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/video/(\\d+)(/([a-z0-9\\-_]+))?");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    @Override
+    public String rewriteHost(final String host) {
+        /* 2021-04-01: change main domain has changed from yourfreeporn.us -> yourfreeporn.tv */
+        return this.rewriteHost(getPluginDomains(), host);
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    private static final String HTML_LIMITREACHED = ">\\s*You have reached your free daily limit";
+    private static final String HTML_PREMIUMONLY  = "class=\"goPremiumPitch[^\"]*\"";
 
     @SuppressWarnings("deprecation")
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        br.postPage(link.getDownloadURL(), "language=en_US");
-        if (br.getHttpConnection().getResponseCode() == 404 || br.getURL().contains("/error/video_missing") || br.containsHTML(">This video cannot be found")) {
+        // br.postPage(link.getDownloadURL(), "language=en_US");
+        /* 2021-04-01 */
+        br.getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404 || br.containsHTML(">\\s*This video cannot be found") || !this.br.getURL().contains(this.getFID(link))) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         String filename = null;
         /**
          * Limit reached? We don't care, we can get the filename from the url and still start the download
          */
+        String filenameURL = new Regex(link.getPluginPatternMatcher(), "/video/\\d+/([a-z0-9\\-_]+)").getMatch(0);
+        if (filenameURL != null) {
+            filenameURL = filenameURL.replace("-", " ").trim();
+        }
+        filename = filenameURL;
         if (br.containsHTML(HTML_PREMIUMONLY) || br.containsHTML(HTML_LIMITREACHED)) {
             if (br.containsHTML(HTML_PREMIUMONLY)) {
                 link.getLinkStatus().setStatusText("This file can only be downloaded by premium users");
             }
-            filename = new Regex(link.getDownloadURL(), "/video/\\d+/([a-z0-9\\-_]+)").getMatch(0);
-            if (filename == null) {
-                filename = new Regex(link.getDownloadURL(), "/video/(\\d+)").getMatch(0);
-            }
         } else {
-            final String ogTitle = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
-            final String header = br.getRegex("class\\s*=\\s*\"page_title\"\\s*>\\s*(.*?)\\s*</h").getMatch(0);
-            if (StringUtils.isAllNotEmpty(header, ogTitle)) {
-                if (ogTitle.length() < header.length()) {
-                    filename = ogTitle;
-                } else {
+            if (filename == null) {
+                final String ogTitle = br.getRegex("property=\"og:title\" content=\"([^<>\"]*?)\"").getMatch(0);
+                final String header = br.getRegex("class\\s*=\\s*\"page_title\"\\s*>\\s*(.*?)\\s*</h").getMatch(0);
+                if (StringUtils.isAllNotEmpty(header, ogTitle)) {
+                    if (ogTitle.length() < header.length()) {
+                        filename = ogTitle;
+                    } else {
+                        filename = header;
+                    }
+                } else if (StringUtils.isNotEmpty(header)) {
                     filename = header;
+                } else {
+                    filename = ogTitle;
                 }
-            } else if (StringUtils.isNotEmpty(header)) {
-                filename = header;
-            } else {
-                filename = ogTitle;
             }
         }
         if (filename == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            filename = this.getFID(link);
         }
         link.setFinalFileName(Encoding.htmlDecode(filename) + ".mp4");
         return AvailableStatus.TRUE;
@@ -131,9 +177,10 @@ public class YourFreePornUs extends PluginForHost {
                 logger.log(e);
             }
             if (br.containsHTML(">403 \\- Forbidden<")) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error", 5 * 60 * 1000l);
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 5 * 60 * 1000l);
             }
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            /* 2021-04-01: Random errors may appear such as 471 Unauthorized */
+            throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error");
         }
         dl.startDownload();
     }
