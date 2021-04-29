df5ace15896390312ef28fa21883c116ef93bbdb
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-04-29 16:07:29 +0000

    ThreadDump: -updated to include memory heap details SystemAPIImpl: -updated to support hardware,snap and docker infos LogAPIImpl: -moved ThreadDrump to getAvailableLogs, possible to create multiple thread dumps easily, for debugging
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44190 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: ed0014319634cbb7720c04f32550c7b5b5e63179

--- src/org/jdownloader/startup/commands/ThreadDump.java
+++ src/org/jdownloader/startup/commands/ThreadDump.java
@@ -3,6 +3,8 @@ package org.jdownloader.startup.commands;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+import org.appwork.utils.formatter.SizeFormatter;
+
 public class ThreadDump extends AbstractStartupCommand {
     public ThreadDump() {
         super("threaddump");
@@ -10,6 +12,19 @@ public class ThreadDump extends AbstractStartupCommand {
 
     @Override
     public void run(String command, String... parameters) {
+        try {
+            final java.lang.management.MemoryUsage memory = java.lang.management.ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+            if (memory != null) {
+                final StringBuilder sb = new StringBuilder();
+                sb.append("HeapUsed:" + SizeFormatter.formatBytes(memory.getUsed()));
+                sb.append("|HeapComitted:" + SizeFormatter.formatBytes(memory.getCommitted()));
+                sb.append("|HeapMax:" + SizeFormatter.formatBytes(memory.getMax()));
+                sb.append("\r\n");
+                logger.severe(sb.toString());
+            }
+        } catch (final Throwable e) {
+            logger.log(e);
+        }
         final Iterator<Entry<Thread, StackTraceElement[]>> it = Thread.getAllStackTraces().entrySet().iterator();
         while (it.hasNext()) {
             final Entry<Thread, StackTraceElement[]> next = it.next();
