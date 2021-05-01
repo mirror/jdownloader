e8e20814992b1106766f2f7b47633d56ea15f754
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-01 10:10:17 +0000

    Pornhub: -fixed correct webm and title -fixes thread 87339
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44201 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: eaf834f5072ec6138eea9b378ea402e34da05af1

--- src/jd/plugins/hoster/PornHubCom.java
+++ src/jd/plugins/hoster/PornHubCom.java
@@ -36,6 +36,16 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+import org.appwork.storage.JSonMapperException;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.downloader.hls.M3U8Playlist;
+import org.jdownloader.logging.LogController;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.config.ConfigContainer;
 import jd.config.ConfigEntry;
@@ -62,16 +72,6 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.PluginForHost;
 
-import org.appwork.storage.JSonMapperException;
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.StringUtils;
-import org.jdownloader.downloader.hls.HLSDownloader;
-import org.jdownloader.downloader.hls.M3U8Playlist;
-import org.jdownloader.logging.LogController;
-import org.jdownloader.plugins.components.hls.HlsContainer;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 public class PornHubCom extends PluginForHost {
     /* Connection stuff */
@@ -94,15 +94,15 @@ public class PornHubCom extends PluginForHost {
     /* Note: Video bitrates and resolutions are not exact, they can vary. */
     /* Quality, { videoCodec, videoBitrate, videoResolution, audioCodec, audioBitrate } */
     public static LinkedHashMap<String, String[]> formats                               = new LinkedHashMap<String, String[]>(new LinkedHashMap<String, String[]>() {
-        {
-            put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
-            put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
-            put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
-            put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
-            put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
-            put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
-        }
-    });
+                                                                                            {
+                                                                                                put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
+                                                                                                put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
+                                                                                                put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
+                                                                                                put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
+                                                                                                put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
+                                                                                                put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
+                                                                                            }
+                                                                                        });
     public static final String                    BEST_ONLY                             = "BEST_ONLY";
     public static final String                    BEST_SELECTION_ONLY                   = "BEST_SELECTION_ONLY";
     public static final String                    FAST_LINKCHECK                        = "FAST_LINKCHECK";
@@ -346,19 +346,19 @@ public class PornHubCom extends PluginForHost {
             br.setFollowRedirects(true);
             getPage(br, createPornhubGifLink(isPremiumFromURL, viewKey, null));
             checkAvailability(link, br);
-            String title = br.getRegex("class=\"gifTitle\">\\s*?<h1>([^<>\"]+)</h1>").getMatch(0);
+            String title = br.getRegex("data-gif-title\\s*=\\s*\"(.*?)\"").getMatch(0);
+            if (title == null) {
+                title = br.getRegex("<title\\s*>\\s*(.*?)\\s*(&#124;|\\|)").getMatch(0);
+            }
             if (title == null) {
                 html_filename = viewKey;
             } else {
-                html_filename = viewKey + "_" + title;
+                html_filename = title + "_" + viewKey;
             }
             if (!webm) {
                 // link or default is gif, check for gif
                 html_filename += ".gif";
-                dlUrl = br.getRegex("data\\-gif\\s*=\\s*\"(https?[^\"]+\\.gif)\"").getMatch(0);
-                if (dlUrl == null) {
-                    dlUrl = br.getRegex("fileGif\\s*=\\s*'(https?[^\"]+\\.gif)'").getMatch(0);
-                }
+                dlUrl = br.getRegex("data\\-gif\\s*=\\s*\"(https?[^\"]+" + Pattern.quote(viewKey) + "[^\\\"]*\\.gif)\"").getMatch(0);
                 if (dlUrl == null) {
                     logger.info("gif not found for:" + viewKey);
                 }
@@ -367,9 +367,9 @@ public class PornHubCom extends PluginForHost {
                 // gif -> don't fail but fallback to webm
                 webm = true;
                 html_filename += ".webm";
-                dlUrl = br.getRegex("data\\-webm\\s*=\\s*\"(https?[^\"]+\\.webm)\"").getMatch(0);
+                dlUrl = br.getRegex("data\\-webm\\s*=\\s*\"(https?[^\"]+" + Pattern.quote(viewKey) + "[^\\\"]*\\.webm)\"").getMatch(0);
                 if (dlUrl == null) {
-                    dlUrl = br.getRegex("fileWebm\\s*=\\s*'(https?[^\"]+\\.webm)'").getMatch(0);
+                    dlUrl = br.getRegex("fileWebm\\s*=\\s*'(https?[^']+" + Pattern.quote(viewKey) + "[^']*\\.webm)'").getMatch(0);
                 }
                 if (dlUrl == null) {
                     logger.info("webm not found for:" + viewKey);
