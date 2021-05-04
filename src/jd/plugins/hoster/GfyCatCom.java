048607c9478a2f9729cfc22cf811ea99e3c0ee98
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-04 12:07:36 +0000

    *Plugins: Fixes/Changes/Maintenance* - GfyCatCom: fixed redgifscom RE forum 87380
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44222 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/GfyCatCom.java
+++ src/jd/plugins/hoster/GfyCatCom.java
@@ -18,6 +18,17 @@ package jd.plugins.hoster;
 import java.io.IOException;
 import java.util.Map;
 
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.DebugMode;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.plugins.components.config.GfycatConfig;
+import org.jdownloader.plugins.components.config.GfycatConfig.PreferredFormat;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.PluginJsonConfig;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.http.Browser;
 import jd.http.URLConnectionAdapter;
@@ -29,15 +40,6 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.components.PluginJSonUtils;
-
-import org.appwork.utils.DebugMode;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.formatter.SizeFormatter;
-import org.jdownloader.plugins.components.config.GfycatConfig;
-import org.jdownloader.plugins.components.config.GfycatConfig.PreferredFormat;
-import org.jdownloader.plugins.config.PluginConfigInterface;
-import org.jdownloader.plugins.config.PluginJsonConfig;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "gfycat.com" }, urls = { "https?://(?:www\\.)?(?:gfycat\\.com(?:/ifr)?|gifdeliverynetwork\\.com(?:/ifr)?|redgifs\\.com/(?:watch|ifr))/([A-Za-z0-9]+)" })
 public class GfyCatCom extends PluginForHost {
@@ -97,6 +99,10 @@ public class GfyCatCom extends PluginForHost {
         return requestFileInformation(link, false);
     }
 
+    private static String redgifsAccessKey             = "";
+    private static String redgifsAccessToken           = "";
+    private static long   redgifsAccessTokenValidUntil = -1;
+
     private String[] getDownloadURL(final DownloadLink link, final Map<String, Object> video, final Map<String, Object> photo, final PreferredFormat format) throws Exception {
         // TODO: use JSON in complicatedJSON, it contains all available formats/qualities
         switch (format) {
@@ -143,111 +149,175 @@ public class GfyCatCom extends PluginForHost {
     }
 
     public AvailableStatus requestFileInformation(final DownloadLink link, final boolean isDownload) throws Exception {
+        if (!link.isNameSet()) {
+            link.setName(this.getFID(link));
+        }
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
         this.br.getHeaders().put("User-Agent", "JDownloader");
         br.setAllowedResponseCodes(new int[] { 500 });
-        br.getPage(link.getPluginPatternMatcher());
-        if (br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 500) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        }
-        if (br.getHost().equalsIgnoreCase("gifdeliverynetwork.com")) {
-            /* 2020-06-18: New and should not be needed! */
-            dllink = br.getRegex("\"(https?://[^<>\"]+\\.webm)\"").getMatch(0);
-            if (dllink == null) {
-                dllink = br.getRegex("\"(https?://[^<>\"]+\\.mp4)\"").getMatch(0);
-            }
-            if (dllink == null || dllink.contains(".webm")) {
-                link.setName(this.getFID(link) + ".webm");
-            } else {
-                link.setName(this.getFID(link) + ".mp4");
-            }
-        } else {
-            final String simpleJSON = br.getRegex("<script data-react-helmet\\s*=\\s*\"true\"\\s*type\\s*=\\s*\"application/ld\\+json\">\\s*(.*?)\\s*</script>").getMatch(0);
-            final String complicatedJSON = br.getRegex("___INITIAL_STATE__\\s*=\\s*(\\{.*?)\\s*</script").getMatch(0);
-            if (simpleJSON != null) {
-                final Map<String, Object> entries = JavaScriptEngineFactory.jsonToJavaMap(simpleJSON);
-                final String datePublished = (String) entries.get("datePublished");
-                final String description = (String) entries.get("description");
-                final Map<String, Object> photo = (Map<String, Object>) entries.get("image");
-                final Map<String, Object> video = (Map<String, Object>) entries.get("video");
-                if (!StringUtils.isEmpty(description) && link.getComment() == null) {
-                    link.setComment(description);
+        if (Browser.getHost(link.getPluginPatternMatcher()).equals("redgifs.com")) {
+            /* 2021-05-04: New API handling required for URLs of this domain */
+            synchronized (redgifsAccessKey) {
+                if (StringUtils.isEmpty(redgifsAccessKey)) {
+                    br.getPage(link.getPluginPatternMatcher());
+                    /* 2021-05-04: /assets/app.59be79d0c1811e38f695.js */
+                    final String jsurl = br.getRegex("<script src=\"(/assets/app\\.[a-f0-9]+\\.js)\">").getMatch(0);
+                    if (jsurl == null) {
+                        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                    }
+                    br.getPage(jsurl);
+                    redgifsAccessKey = br.getRegex("webloginAccessKey=\"([^\"]+)\"").getMatch(0);
+                    if (redgifsAccessKey == null) {
+                        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                    }
                 }
-                final String username = (String) entries.get("author");
-                String title = null;
-                if (DebugMode.TRUE_IN_IDE_ELSE_FALSE && !true) {
-                    /* Alternative ways to find title */
-                    if (complicatedJSON != null) {
-                        try {
-                            final Object rootO = JavaScriptEngineFactory.jsonToJavaObject(complicatedJSON);
-                            // final List<Object> ressourcelist = JSonStorage.restoreFromString(complicatedJSON, TypeRef.LIST);
-                            final Map<String, Object> allMedia = (Map<String, Object>) JavaScriptEngineFactory.walkJson(rootO, "{0}/cache/gifs");
-                            final Map<String, Object> thisMediaInfo = (Map<String, Object>) allMedia.get(this.getFID(link));
-                            title = (String) thisMediaInfo.get("title");
-                        } catch (final Throwable e) {
+            }
+            final Browser brapi = br.cloneBrowser();
+            synchronized (redgifsAccessToken) {
+                if (StringUtils.isEmpty(redgifsAccessToken) || redgifsAccessTokenValidUntil < System.currentTimeMillis()) {
+                    if (redgifsAccessToken == null) {
+                        logger.info("Creating token for the first time");
+                    } else {
+                        logger.info("Creating new token because the old one has expired");
+                    }
+                    brapi.postPageRaw("https://weblogin.redgifs.com/oauth/webtoken", "{\"access_key\":\"" + redgifsAccessKey + "\"}");
+                    final Map<String, Object> entries = JSonStorage.restoreFromString(brapi.toString(), TypeRef.HASHMAP);
+                    redgifsAccessToken = (String) entries.get("access_token");
+                    if (StringUtils.isEmpty(redgifsAccessToken)) {
+                        throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                    }
+                    redgifsAccessTokenValidUntil = System.currentTimeMillis() + ((Number) entries.get("expires_in")).longValue() * 1000l;
+                }
+            }
+            brapi.getHeaders().put("Referer", "https://redgifs.com/");
+            brapi.getHeaders().put("Authorization", "Bearer " + redgifsAccessToken);
+            brapi.getPage("https://api.redgifs.com/v1/gfycats/" + this.getFID(link));
+            if (brapi.getHttpConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            Map<String, Object> entries = JSonStorage.restoreFromString(brapi.toString(), TypeRef.HASHMAP);
+            entries = (Map<String, Object>) entries.get("gfyItem");
+            final Map<String, Object> sources = (Map<String, Object>) entries.get("content_urls");
+            final Map<String, Object> selectedSource;
+            switch (getPreferredFormat(link)) {
+            case WEBM:
+                selectedSource = (Map<String, Object>) sources.get("webm");
+                break;
+            case GIF:
+                selectedSource = (Map<String, Object>) sources.get("largeGif");
+                break;
+            case MP4: // MP4 == default
+            default:
+                selectedSource = (Map<String, Object>) sources.get("mp4");
+            }
+            final String username = (String) entries.get("userName");
+            link.setFinalFileName(username + " - " + this.getFID(link) + ".webm");
+            link.setVerifiedFileSize(((Number) selectedSource.get("size")).longValue());
+            this.dllink = (String) selectedSource.get("url");
+        } else {
+            br.getPage(link.getPluginPatternMatcher());
+            if (br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 500) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            if (br.getHost().equalsIgnoreCase("gifdeliverynetwork.com")) {
+                /* 2020-06-18: New and should not be needed! */
+                dllink = br.getRegex("\"(https?://[^<>\"]+\\.webm)\"").getMatch(0);
+                if (dllink == null) {
+                    dllink = br.getRegex("\"(https?://[^<>\"]+\\.mp4)\"").getMatch(0);
+                }
+                if (dllink == null || dllink.contains(".webm")) {
+                    link.setName(this.getFID(link) + ".webm");
+                } else {
+                    link.setName(this.getFID(link) + ".mp4");
+                }
+            } else {
+                final String simpleJSON = br.getRegex("<script data-react-helmet\\s*=\\s*\"true\"\\s*type\\s*=\\s*\"application/ld\\+json\">\\s*(.*?)\\s*</script>").getMatch(0);
+                final String complicatedJSON = br.getRegex("___INITIAL_STATE__\\s*=\\s*(\\{.*?)\\s*</script").getMatch(0);
+                if (simpleJSON != null) {
+                    final Map<String, Object> entries = JavaScriptEngineFactory.jsonToJavaMap(simpleJSON);
+                    final String datePublished = (String) entries.get("datePublished");
+                    final String description = (String) entries.get("description");
+                    final Map<String, Object> photo = (Map<String, Object>) entries.get("image");
+                    final Map<String, Object> video = (Map<String, Object>) entries.get("video");
+                    if (!StringUtils.isEmpty(description) && link.getComment() == null) {
+                        link.setComment(description);
+                    }
+                    final String username = (String) entries.get("author");
+                    String title = null;
+                    if (DebugMode.TRUE_IN_IDE_ELSE_FALSE && !true) {
+                        /* Alternative ways to find title */
+                        if (complicatedJSON != null) {
+                            try {
+                                final Object rootO = JavaScriptEngineFactory.jsonToJavaObject(complicatedJSON);
+                                // final List<Object> ressourcelist = JSonStorage.restoreFromString(complicatedJSON, TypeRef.LIST);
+                                final Map<String, Object> allMedia = (Map<String, Object>) JavaScriptEngineFactory.walkJson(rootO, "{0}/cache/gifs");
+                                final Map<String, Object> thisMediaInfo = (Map<String, Object>) allMedia.get(this.getFID(link));
+                                title = (String) thisMediaInfo.get("title");
+                            } catch (final Throwable e) {
+                            }
+                        }
+                        if (StringUtils.isEmpty(title)) {
+                            title = br.getRegex("<h1 class\\s*=\\s*\"title\">\\s*([^<>\"]+)\\s*</h1>").getMatch(0);
                         }
                     }
-                    if (StringUtils.isEmpty(title)) {
-                        title = br.getRegex("<h1 class\\s*=\\s*\"title\">\\s*([^<>\"]+)\\s*</h1>").getMatch(0);
+                    title = (String) entries.get("headline");
+                    if (!StringUtils.isEmpty(title)) {
+                        /* 2020-11-26: Remove stuff we don't want! */
+                        title = title.replaceFirst("(\\s*Porn\\s*GIF\\s*(by.+)?)", "");
                     }
-                }
-                title = (String) entries.get("headline");
-                if (!StringUtils.isEmpty(title)) {
-                    /* 2020-11-26: Remove stuff we don't want! */
-                    title = title.replaceFirst("(\\s*Porn\\s*GIF\\s*(by.+)?)", "");
-                }
-                /* 2021-03-09: Fallback - title can be "" (empty) [after title-correction]! */
-                title = this.getFID(link);
-                if (!StringUtils.isAllNotEmpty(datePublished, username, title)) {
-                    /* Most likely content is not downloadable e.g. gyfcat.com/upload */
-                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                }
-                String dateFormatted = new Regex(datePublished, "(\\d{4}-\\d{2}-\\d{2})").getMatch(0);
-                if (dateFormatted == null) {
-                    /* Fallback */
-                    dateFormatted = datePublished;
-                }
-                final String downloadURL[] = getDownloadURL(link, video, photo, getPreferredFormat(link));
-                dllink = downloadURL[1];
-                final String ext = downloadURL[2];
-                if (link.getFinalFileName() == null) {
-                    /*
-                     * 2020-11-26: Include fid AND title inside filenames because different URLs can have the same title and can be
-                     * published on the same date (very rare case).
-                     */
-                    String filename = dateFormatted + "_" + username;
-                    /* fid is used as fallback-title so in this case we don't want to have it twice in our filename! */
-                    if (!title.equals(this.getFID(link))) {
-                        filename += " - " + this.getFID(link);
+                    /* 2021-03-09: Fallback - title can be "" (empty) [after title-correction]! */
+                    title = this.getFID(link);
+                    if (!StringUtils.isAllNotEmpty(datePublished, username, title)) {
+                        /* Most likely content is not downloadable e.g. gyfcat.com/upload */
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
                     }
-                    filename += " - " + title + ext;
-                    link.setFinalFileName(filename);
+                    String dateFormatted = new Regex(datePublished, "(\\d{4}-\\d{2}-\\d{2})").getMatch(0);
+                    if (dateFormatted == null) {
+                        /* Fallback */
+                        dateFormatted = datePublished;
+                    }
+                    final String downloadURL[] = getDownloadURL(link, video, photo, getPreferredFormat(link));
+                    dllink = downloadURL[1];
+                    final String ext = downloadURL[2];
+                    if (link.getFinalFileName() == null) {
+                        /*
+                         * 2020-11-26: Include fid AND title inside filenames because different URLs can have the same title and can be
+                         * published on the same date (very rare case).
+                         */
+                        String filename = dateFormatted + "_" + username;
+                        /* fid is used as fallback-title so in this case we don't want to have it twice in our filename! */
+                        if (!title.equals(this.getFID(link))) {
+                            filename += " - " + this.getFID(link);
+                        }
+                        filename += " - " + title + ext;
+                        link.setFinalFileName(filename);
+                    }
+                } else {
+                    /* Old handling */
+                    // final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>)
+                    // JavaScriptEngineFactory.jsonToJavaMap(json);
+                    if (StringUtils.isEmpty(complicatedJSON) || br.getHttpConnection().getResponseCode() == 404) {
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    }
+                    final String username = PluginJSonUtils.getJsonValue(complicatedJSON, "author");
+                    final String filename = this.getFID(link);
+                    final String filesize = PluginJSonUtils.getJsonValue(complicatedJSON, "webmSize");
+                    if (StringUtils.isEmpty(username) || StringUtils.isEmpty(filename)) {
+                        /* Most likely content is not downloadable e.g. gyfcat.com/upload */
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    }
+                    link.setFinalFileName(username + " - " + filename + ".webm");
+                    if (!StringUtils.isEmpty(filesize)) {
+                        link.setDownloadSize(SizeFormatter.getSize(filesize));
+                    }
+                    dllink = PluginJSonUtils.getJsonValue(complicatedJSON, "webmUrl");
                 }
-            } else {
-                /* Old handling */
-                // final LinkedHashMap<String, Object> entries = (LinkedHashMap<String, Object>)
-                // JavaScriptEngineFactory.jsonToJavaMap(json);
-                if (StringUtils.isEmpty(complicatedJSON) || br.getHttpConnection().getResponseCode() == 404) {
-                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                }
-                final String username = PluginJSonUtils.getJsonValue(complicatedJSON, "author");
-                final String filename = this.getFID(link);
-                final String filesize = PluginJSonUtils.getJsonValue(complicatedJSON, "webmSize");
-                if (StringUtils.isEmpty(username) || StringUtils.isEmpty(filename)) {
-                    /* Most likely content is not downloadable e.g. gyfcat.com/upload */
-                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-                }
-                link.setFinalFileName(username + " - " + filename + ".webm");
-                if (!StringUtils.isEmpty(filesize)) {
-                    link.setDownloadSize(SizeFormatter.getSize(filesize));
-                }
-                dllink = PluginJSonUtils.getJsonValue(complicatedJSON, "webmUrl");
             }
-        }
-        if (!StringUtils.isEmpty(this.dllink) && !isDownload) {
-            if (!verifyDownloadURL(link, dllink)) {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            if (!StringUtils.isEmpty(this.dllink) && !isDownload) {
+                if (!verifyDownloadURL(link, dllink)) {
+                    throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                }
             }
         }
         return AvailableStatus.TRUE;
