573074400c845d982e0be1b62e5173e6f14391f9
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-11 14:14:16 +0000

    *Plugins: Fixes/Changes/Maintenance* - KernelVideoSharingComV2: added new default pattern - [YetiShare] fixed MOCH linkspremium.download which is using a modified YrtiShare script (or a plugin) for its MOCH features fixes #89792
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [KVS] yogapornnet closes #89791
    
    *Plugins: RIP*
    - MoviesFun fixes #89619
    - 0123moviestv.com
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44266 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/LinkspremiumDownload.java
+++ src/jd/plugins/hoster/LinkspremiumDownload.java
@@ -16,17 +16,18 @@
 package jd.plugins.hoster;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.appwork.utils.Regex;
 import org.appwork.utils.StringUtils;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.jdownloader.plugins.components.YetiShareCore;
 import org.jdownloader.plugins.controller.host.LazyHostPlugin.FEATURE;
 
 import jd.PluginWrapper;
-import jd.config.Property;
 import jd.http.Browser;
-import jd.http.Cookies;
-import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
 import jd.plugins.AccountInfo;
@@ -35,22 +36,14 @@ import jd.plugins.DownloadLink.AvailableStatus;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
-import jd.plugins.PluginForHost;
 import jd.plugins.components.MultiHosterManagement;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "linkspremium.download" }, urls = { "" })
-public class LinkspremiumDownload extends PluginForHost {
-    /* Connection limits */
-    private static final boolean         ACCOUNT_PREMIUM_RESUME    = false;
-    private static final int             ACCOUNT_PREMIUM_MAXCHUNKS = 0;
-    private static final boolean         USE_API                   = false;
-    private Account                      currentAcc                = null;
-    private DownloadLink                 currentLink               = null;
-    private static MultiHosterManagement mhm                       = new MultiHosterManagement("linkspremium.download");
-
+public class LinkspremiumDownload extends YetiShareCore {
     public LinkspremiumDownload(PluginWrapper wrapper) {
         super(wrapper);
-        this.enablePremium("http://linkspremium.download/");
+        // this.enablePremium("http://linkspremium.download/");
+        this.enablePremium(this.getPurchasePremiumURL());
     }
 
     @Override
@@ -58,230 +51,179 @@ public class LinkspremiumDownload extends PluginForHost {
         return "http://linkspremium.download/";
     }
 
-    private Browser prepBR(final Browser br) {
-        br.setCookiesExclusive(true);
-        br.getHeaders().put("User-Agent", "JDownloader");
-        br.setFollowRedirects(true);
-        return br;
+    @Override
+    public String[] siteSupportedNames() {
+        return new String[] { "linkspremium.download" };
     }
 
+    private static MultiHosterManagement mhm                = new MultiHosterManagement("linkspremium.download");
+    private static final String          PROPERTY_DIRECTURL = "linkspremiumdownloaddirectlink";
+
+    /**
+     * DEV NOTES YetiShare<br />
+     ****************************
+     * mods: See overridden functions<br />
+     * limit-info:<br />
+     * captchatype-info: 2021-05-11: null<br />
+     * other: Speciel special: YetiShare used as a multihost! <br />
+     */
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws PluginException {
         return AvailableStatus.UNCHECKABLE;
     }
 
-    private void setConstants(final Account acc, final DownloadLink dl) {
-        this.currentAcc = acc;
-        this.currentLink = dl;
-    }
-
     @Override
-    public boolean canHandle(DownloadLink downloadLink, Account account) throws Exception {
+    public boolean canHandle(final DownloadLink link, final Account account) throws Exception {
         if (account == null) {
             /* without account its not possible to download the link */
             return false;
+        } else {
+            return true;
         }
-        return true;
     }
 
     @Override
-    public void handleFree(DownloadLink downloadLink) throws Exception, PluginException {
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return true;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return true;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return true;
+        }
+    }
+
+    public int getMaxChunks(final Account account) {
+        if (account != null && account.getType() == AccountType.FREE) {
+            /* Free Account */
+            return 0;
+        } else if (account != null && account.getType() == AccountType.PREMIUM) {
+            /* Premium account */
+            return 0;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return 0;
+        }
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
         throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
     }
 
     @Override
-    public void handlePremium(DownloadLink link, Account account) throws Exception {
+    public void handlePremium(final DownloadLink link, final Account account) throws Exception {
         /* handle premium should never be called */
         throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
     }
 
     @Override
-    public FEATURE[] getFeatures() {
-        return new FEATURE[] { FEATURE.MULTIHOST };
+    protected AccountInfo fetchAccountInfoWebsite(final Account account) throws Exception {
+        final AccountInfo ai = super.fetchAccountInfoWebsite(account);
+        if (account.getType() == AccountType.FREE) {
+            ai.setTrafficLeft(0);
+        } else {
+            this.getPage("/filehostdl.html");
+            /* Try to get/set traffic information */
+            final Regex trafficInfo = br.getRegex("span class=\"badge badge-success\">(\\d+)\\s*/\\s*(\\d+(?:,\\d+)?) MB</span>");
+            final String trafficUsedToday = trafficInfo.getMatch(0);
+            if (trafficUsedToday != null) {
+                final String trafficMaxToday = trafficInfo.getMatch(1);
+                ai.setTrafficMax(SizeFormatter.getSize(trafficMaxToday.replace(",", "") + "MB"));
+                ai.setTrafficLeft(ai.getTrafficMax() - SizeFormatter.getSize(trafficUsedToday + "MB"));
+            }
+            /* Double-check */
+            final Regex linksleftInfo = br.getRegex("class=\"badge badge-success\">(\\d+(?:,\\d+)?)\\s*/\\s*(\\d+(?:,\\d+)?)\\s*Links</span>");
+            final String linksleftUsedToday = linksleftInfo.getMatch(0);
+            final String linksleftMaxToday = linksleftInfo.getMatch(1);
+            if (linksleftUsedToday != null) {
+                final int linksleftUsedTodayInt = Integer.parseInt(linksleftUsedToday.replace(",", ""));
+                final int linksleftMaxTodayInt = Integer.parseInt(linksleftMaxToday.replace(",", ""));
+                if (linksleftUsedTodayInt >= linksleftMaxTodayInt) {
+                    logger.info("Account is out of traffic because user has downloaded max. number of links per day already!");
+                    ai.setTrafficLeft(0);
+                }
+                ai.setStatus(account.getType() + ": Links left today: " + (linksleftMaxTodayInt - linksleftUsedTodayInt));
+            }
+            final String[] supportedHosts = br.getRegex("domain=([^\"]+)\"").getColumn(0);
+            ai.setMultiHostSupport(this, Arrays.asList(supportedHosts));
+        }
+        return ai;
     }
 
     @Override
     public void handleMultiHost(final DownloadLink link, final Account account) throws Exception {
-        this.br = prepBR(this.br);
-        setConstants(account, link);
-        mhm.runCheck(currentAcc, currentLink);
-        login(account, false);
-        final String dllink = getDllink(link);
-        if (StringUtils.isEmpty(dllink)) {
-            mhm.handleErrorGeneric(currentAcc, currentLink, "dllinknull", 2, 5 * 60 * 1000l);
-        }
-        handleDL(account, link, dllink);
-    }
-
-    private String getDllink(final DownloadLink link) throws IOException, PluginException {
-        String dllink = checkDirectLink(link, this.getHost() + "directlink");
-        if (dllink == null) {
-            if (USE_API) {
-                dllink = getDllinkAPI(link);
-            } else {
-                dllink = getDllinkWebsite(link);
+        mhm.runCheck(account, link);
+        if (!attemptStoredDownloadurlDownload(link, account)) {
+            br.setFollowRedirects(true);
+            this.loginWebsite(account, false);
+            this.getPage("https://" + this.getHost() + "/filehostdl.html");
+            final Form dlform = br.getFormbyProperty("id", "ajaxform");
+            if (dlform == null) {
+                this.checkErrorsLastResort(this.br, link, account);
             }
-        }
-        return dllink;
-    }
-
-    private String getDllinkAPI(final DownloadLink link) throws IOException, PluginException {
-        return null;
-    }
-
-    private String getDllinkWebsite(final DownloadLink link) throws IOException, PluginException {
-        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
-        br.postPage("/index.php?rand=0." + System.currentTimeMillis(), "gerar=" + Encoding.urlEncode(link.getDefaultPlugin().buildExternalDownloadURL(link, this)));
-        final String dllink = br.getRegex("title=\\'clique aqui para baixar\\' href=\\'(https?://[^<>\"\\']+)").getMatch(0);
-        return dllink;
-    }
-
-    private void handleDL(final Account account, final DownloadLink link, final String dllink) throws Exception {
-        link.setProperty(this.getHost() + "directlink", dllink);
-        try {
-            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, ACCOUNT_PREMIUM_RESUME, ACCOUNT_PREMIUM_MAXCHUNKS);
+            if (StringUtils.isEmpty(dlform.getAction())) {
+                dlform.setAction("/debrid_engine/gen_process_link.php");
+            }
+            dlform.put("urllist", Encoding.urlEncode(link.getDefaultPlugin().buildExternalDownloadURL(link, this)));
+            if (link.getDownloadPassword() != null) {
+                dlform.put("passe", Encoding.urlEncode(link.getDownloadPassword()));
+            }
+            final Browser brc = br.cloneBrowser();
+            brc.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+            this.submitForm(brc, dlform);
+            String dllink = brc.getRegex("ng-model=\"add_links_values\"[^>]*>(https?://[^<]+)").getMatch(0);
+            if (dllink == null) {
+                dllink = brc.getRegex("(https?://[^/]+/download/[^\">]+)\"").getMatch(0);
+            }
+            if (dllink == null) {
+                /* Use previous browser for errorchecking here . we don't want to run into logout failure accidently! */
+                this.checkErrorsLastResort(this.br, link, account);
+            }
+            dl = new jd.plugins.BrowserAdapter().openDownload(br, link, dllink, this.isResumeable(link, account), this.getMaxChunks(account));
             if (!this.looksLikeDownloadableContent(dl.getConnection())) {
-                br.followConnection(true);
-                updatestatuscode();
-                handleAPIErrors(this.br);
-                mhm.handleErrorGeneric(currentAcc, currentLink, "unknowndlerror", 2, 5 * 60 * 1000l);
+                try {
+                    br.followConnection(true);
+                } catch (final IOException e) {
+                    logger.log(e);
+                }
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Final downloadurl did not lead to downloadable content");
             }
-            this.dl.startDownload();
-        } catch (final Exception e) {
-            link.setProperty(this.getHost() + "directlink", Property.NULL);
-            throw e;
+            link.setProperty(PROPERTY_DIRECTURL, dllink);
         }
+        dl.startDownload();
     }
 
-    private String checkDirectLink(final DownloadLink link, final String property) {
-        String dllink = link.getStringProperty(property);
-        if (dllink != null) {
-            URLConnectionAdapter con = null;
-            try {
-                final Browser br2 = br.cloneBrowser();
-                br2.setFollowRedirects(true);
-                con = br2.openHeadConnection(dllink);
-                if (this.looksLikeDownloadableContent(con)) {
-                    return dllink;
-                }
-            } catch (final Exception e) {
-                logger.log(e);
-                return null;
-            } finally {
-                if (con != null) {
-                    con.disconnect();
-                }
-            }
+    private boolean attemptStoredDownloadurlDownload(final DownloadLink link, final Account account) throws Exception {
+        final String url = link.getStringProperty(PROPERTY_DIRECTURL);
+        if (StringUtils.isEmpty(url)) {
+            return false;
         }
-        return null;
+        try {
+            final Browser brc = br.cloneBrowser();
+            dl = new jd.plugins.BrowserAdapter().openDownload(brc, link, url, this.isResumeable(link, account), this.getMaxChunks(account));
+            if (this.looksLikeDownloadableContent(dl.getConnection())) {
+                return true;
+            } else {
+                brc.followConnection(true);
+                throw new IOException();
+            }
+        } catch (final Throwable e) {
+            logger.log(e);
+            try {
+                dl.getConnection().disconnect();
+            } catch (Throwable ignore) {
+            }
+            return false;
+        }
     }
 
     @Override
-    public AccountInfo fetchAccountInfo(final Account account) throws Exception {
-        setConstants(account, null);
-        this.br = prepBR(this.br);
-        final AccountInfo ai;
-        if (USE_API) {
-            ai = fetchAccountInfoAPI(account);
-        } else {
-            ai = fetchAccountInfoWebsite(account);
-        }
-        return ai;
-    }
-
-    public AccountInfo fetchAccountInfoWebsite(final Account account) throws Exception {
-        /*
-         * 2017-11-29: Lifetime premium not (yet) supported via website mode! But by the time we might need the website version again, they
-         * might have stopped premium lifetime sales already as that has never been a good idea for any (M)OCH.
-         */
-        final AccountInfo ai = new AccountInfo();
-        login(account, true);
-        if (br.getURL() == null || !br.getURL().contains("/gerador")) {
-            br.getPage("/gerador");
-        }
-        final boolean isPremium = true;
-        ArrayList<String> supportedHosts = new ArrayList<String>();
-        if (isPremium) {
-            account.setType(AccountType.PREMIUM);
-            final String[] hostlist = br.getRegex("/hosts/([A-Za-z0-9]+)\\.png'").getColumn(0);
-            if (hostlist != null) {
-                for (final String crippledHost : hostlist) {
-                    supportedHosts.add(crippledHost);
-                }
-            }
-            ai.setUnlimitedTraffic();
-        } else {
-            account.setType(AccountType.FREE);
-            ai.setTrafficLeft(0);
-        }
-        ai.setMultiHostSupport(this, supportedHosts);
-        return ai;
-    }
-
-    public AccountInfo fetchAccountInfoAPI(final Account account) throws Exception {
-        return null;
-    }
-
-    private void login(final Account account, final boolean force) throws Exception {
-        synchronized (account) {
-            /* Load cookies */
-            br.setCookiesExclusive(true);
-            this.br = prepBR(this.br);
-            if (USE_API) {
-                loginAPI(account, force);
-            } else {
-                loginWebsite(account, force);
-            }
-        }
-    }
-
-    private void loginWebsite(final Account account, final boolean force) throws Exception {
-        try {
-            final Cookies cookies = account.loadCookies("");
-            if (cookies != null) {
-                this.br.setCookies(this.getHost(), cookies);
-                /*
-                 * Even though login is forced first check if our cookies are still valid --> If not, force login!
-                 */
-                br.getPage("http://" + this.getHost() + "/gerador");
-                if (isLoggedinHTML()) {
-                    account.saveCookies(this.br.getCookies(this.getHost()), "");
-                    return;
-                }
-                /* Clear cookies to prevent unknown errors as we'll perform a full login below now. */
-                this.br.clearCookies(br.getHost());
-            }
-            br.getPage("http://" + this.getHost() + "/");
-            if (br.containsHTML("<title>IP bloqueado</title>")) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nIP blocked!", PluginException.VALUE_ID_PREMIUM_DISABLE);
-            }
-            // br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
-            // br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-            br.postPage("/login", "1=1&user=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()));
-            if (!isLoggedinHTML()) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-            }
-            account.saveCookies(this.br.getCookies(this.getHost()), "");
-        } catch (final PluginException e) {
-            account.clearCookies("");
-            throw e;
-        }
-    }
-
-    private boolean isLoggedinHTML() {
-        return br.containsHTML("/logout\"");
-    }
-
-    private void loginAPI(final Account account, final boolean force) throws Exception {
-    }
-
-    /** Keep this for possible future API implementation */
-    private void updatestatuscode() {
-    }
-
-    /** Keep this for possible future API implementation */
-    private void handleAPIErrors(final Browser br) throws PluginException {
+    public FEATURE[] getFeatures() {
+        return new FEATURE[] { FEATURE.MULTIHOST };
     }
 
     @Override
