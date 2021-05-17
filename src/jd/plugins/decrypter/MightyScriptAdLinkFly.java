5595844c04fd910d3fabab62623289de852bbd2d
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-17 12:10:07 +0000

    Changes RE: TXE-133-68638 *Plugins: Fixes/Changes/Maintenance* - MightyScriptAdLinkFly: refactored and moved nearly all supported hosts into: MightyScriptAdLinkFlyDefault - [MightyScript_AdLinkFly] uiiio: moved into separate plugin to be able to support special linktype
    *Plugins: New plugins or new websites that belong to existing scripts*
    - pasfoxcom
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44293 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MightyScriptAdLinkFly.java
+++ src/jd/plugins/decrypter/MightyScriptAdLinkFly.java
@@ -17,8 +17,6 @@ package jd.plugins.decrypter;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import org.appwork.utils.StringUtils;
@@ -50,150 +48,6 @@ import jd.plugins.components.SiteType.SiteTemplate;
  */
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class MightyScriptAdLinkFly extends antiDDoSForDecrypt {
-    private static final String[]     domains                    = { "arabtvlink.com", "medche.com", "go-urls.tk", "topklink.com", "shorts-link.com", "easy4earn.com", "linkat4all.com", "linkdrop.net", "shrtz.me", "ctkings.com", "linksad.net", "paylink.pro", "123link.pro", "donia2link.com", "cutpaid.com", "shortadz.org", "itiurl.co", "shortli.net", "cutearn.ca", "icutit.ca", "cut-one.com", "cll.press", "link-zero.com", "linktor.io", "cash4url.com", "cashat.net", "123short.com", "skip-url.me", "msms4.com", "empireshort.com", "loadurl.com", "geistlink.com", "cutt.us.com", "arabdollar.com", "shortenow.com", "kingurl.net", "best3link.com", "solo-link.com", "best5link.com", "lkky.co", "win4cut.com", "coinlink.co", "adlink.guru", "short.es", "tmearn.com", "ibly.co", "urle.co", "mitly.us", "zlshorte.net", "igram.im", "gram.im", "bit-url.com", "adbilty.me", "linclik.com", "oke.io", "vivads.net",
-            "pnd.tl", "met.bz", "urlcloud.us", "clik.pw", "z2i.com", "fant1asy.com",
-            /** 2021-04-22: Belongs to filehost "fastdrive.io" and is used to redirect to their final downloadurls. */
-            "roda.site", "securitystickers.info", "fencepedia.com",
-            /** 2021-03-29: tny.so -> URL-Shortener of doodstream.com [promoted 2021-03-27 via E-Mail campaign] */
-            "tny.so",
-            /** 2021-02-08: hoastie.com */
-            "hoastie.com",
-            /** 2021-01-19: viralhacks.net domains */
-            "viralhacks.net",
-            /** 2021-01-14: shortzzy.com domains */
-            "shortzzy.com", "shortzzy.link", "janusnotes.com", "shortzzy.in", "l.shortzzy.in",
-            /** 2020-04-20: shrinkhere.xyz domains */
-            "shrinkhere.xyz",
-            /** 2020-04-01: za.gl domains */
-            "za.gl", "zee.gl",
-            /** 2020-02-16: payskip.org domains */
-            "payskip.org",
-            /** 2020-02-10: adlinkcash.com domains */
-            "adlinkcash.com",
-            /** 2019-12-04: shortmony.com domains */
-            "shortmony.com", "shortmony.me", "gamez-ar.com",
-            /** samaa-pro.com domains */
-            "samaa-pro.com",
-            /** 2019-11-06: skip4all.com */
-            "skip4all.com",
-            /** 2019-10-30: glory-link.com domains */
-            "glory-link.com",
-            /** 2020-01-07: cashurl.win domains */
-            "cashurl.win", "todaynewspk.win",
-            /** 2019-10-30: exe.io domains */
-            "exe.io", "exe.app", "exee.io", "exey.io", "iddeas.xyz", "artiicle.xyz", "techbeast.xyz", "techofaqs.com", "caat.site", "2xs.io", "wealthh.xyz", "cu6.io", "mediumarticles.com", "bolssc.com", "mealip.com",
-            /** 2019-08-29: 4snip.pw domains, handles by FoursnipPw plugin */
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    /*
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     * "4snip.pw",
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     */
-            /** 2019-11-13: linkjust.com domains */
-            "linkjust.com", "thegreatfuture.com", "siha.xyz", "akltu.com", "rahlatt.com", "ekhtr.com",
-            /** 2020-01-21: encurta.net domains */
-            "encurta.net", "enrt.eu",
-            /** 2020-01-20: 7r6.com domains */
-            "7r6.com",
-            /** 2019-11-13: linkshorty */
-            "linkshorty.com", "americansvsarabs.com", "mat3sports.com", "shortyup.com",
-            /** 2019-11-13: ex-foary.com */
-            "ex-foary.com",
-            /** 2019-11-13: Zoom Link */
-            "zoom-link.com", "empir33.com",
-            /** 2019-08-24: shortzon.com domains */
-            "shortzon.com", "infothon.com",
-            /** 2019-08-24: ilinkshort.com domains */
-            "ilinkshort.com", "loopdiet.com", "xpshort.com",
-            /** 2019-07-24: 2click.us domains */
-            "2click.us", "2ck.cc", "earticale.xyz",
-            /** 2019-07-24: 2shorten.com domains */
-            "2shorten.com", "nashr.online", "almrf.com",
-            /** shrinkearn.com domains */
-            "shrinkearn.com", "wishes2.com", "shortearn.eu", "ckk.ai", "tii.ai",
-            /** shrinkme.io */
-            "shrinkme.io", "shrinke.me",
-            /** clk.sh domains */
-            "clk.sh", "oko.sh", "iir.ai",
-            /** bt4link.com domains */
-            "bt4link.com", "btc4link.com", "ur4link.com",
-            /** shorted4us.com domains */
-            "shorted4us.com", "thefreech.com", "sewrn.com",
-            /** asrar-alrb7.com domains */
-            "asrar-alrb7.com", "savircity.com",
-            /** cut1.ga domains */
-            "cut1.ga", "shortit.ca",
-            /** safelinku.com domains */
-            "safelinku.com", "idsly.bid", "idsly.net", "updatetribun.org",
-            /** cepmuzikindir.me domains - NOTE: safelinku.NET != safelinku.COM */
-            "cepmuzikindir.me", "safelinku.net", "miui.space",
-            /** uii.io domains */
-            "uii.io", "szs.pw",
-            /** short.pe domains */
-            "short.pe", "chrt.pw",
-            /** urlcero.us domains */
-            "urlcero.us", "urlcero.com",
-            /** clicksfly.com domains */
-            "clicksfly.com", "gifsis.com", "blogginggyanbox.com", "buyitonline.store", "clk.ink", "counsellingresult2016.in", "yourtechguider.com", "1921681254.tech", "iitjeemainguide.in", "yesmoviesapp.info", "newskart.xyz", "funnyquiz.blog", "fabsdeals.com", "utimetableresult.in", "geki.tech", "govtsmartjob.com", "clkfly.pw", "technodia.xyz", "ourtechnoew.xyz", "mutharammss.xyz", "viralcollect.info", "govtsmartjob.com",
-            /** eglink.info domains */
-            "eglink.info", "egyptiangy.net", "egypt-mix.com",
-            /** cut-urls.com domains */
-            "cut-urls.com", "curs.io", "cuon.io", "cuus.io", "cuto.io", "cu5.io",
-            /** wicr.me domains */
-            "wicr.me", "wi.cr",
-            /** cutwin.com domains */
-            "cutwin.com", "cutwin.us",
-            /** adshort.co domains */
-            "adshort.co", "adsrt.com", "adsrt.me", "adshort.me", "adshort.im",
-            /** hitfile short links */
-            "hil.to", "hitf.to", "hitf.cc", "hif.to" };
-    /** List of services for which waittime is skippable. */
-    private static final List<String> domains_waittime_skippable = Arrays.asList(new String[] {});
-    // /** List of services for which captcha is skippable or not required. */
-    /** TODO: Find a way to automatically detect this edge-case */
-    private static final List<String> domains_captcha_skippable  = Arrays.asList(new String[] { "safelinku.com", "idsly.bid", "idsly.net" });
-
-    /**
-     * returns the annotation pattern array
-     *
-     */
-    public static String[] getAnnotationUrls() {
-        // construct pattern
-        final String host = getHostsPattern();
-        /* [a-zA-Z0-9]{2,} will work for 99% but site shortit.ca is not compatible. */
-        /* 2019-04-25: Added special pattern for direct-redirects of clicksfly.com domains('/entrar/...') */
-        return new String[] { host + "/(entrar/[A-Za-z0-9\\-]+|[a-zA-Z0-9]{2,})" };
-    }
-
-    private static String getHostsPattern() {
-        final StringBuilder pattern = new StringBuilder();
-        for (final String name : domains) {
-            pattern.append((pattern.length() > 0 ? "|" : "") + Pattern.quote(name));
-        }
-        final String hosts = "https?://(?:www\\.)?" + "(?:" + pattern.toString() + ")";
-        return hosts;
-    }
-
-    @Override
-    public String[] siteSupportedNames() {
-        return domains;
-    }
-
-    private boolean supportsHost(final String host) {
-        for (final String domain : domains) {
-            if (domain.equals(host)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Returns the annotations names array
-     *
-     * @return
-     */
-    public static String[] getAnnotationNames() {
-        return new String[] { "cut-urls.com" };
-    }
-
     public enum CaptchaType {
         reCaptchaV2,
         reCaptchaV2_invisible,
@@ -212,22 +66,12 @@ public class MightyScriptAdLinkFly extends antiDDoSForDecrypt {
 
     private String appVars = null;
 
-    private String correctURL(final String input) {
-        final String output;
-        if (input.contains("linkdrop.net")) {
-            /* 2018-12-11: Their https is broken */
-            output = input.replace("https://", "http://");
-        } else if (input.contains("curs.io")) {
-            /*
-             * 2019-02-21: curs.io is a cut-urls.com domain which is down but the URLs can still be online so we need to replace it with one
-             * of their working domains.
-             */
-            output = input.replace("curs.io", "cuto.io");
-        } else {
-            /* Nothing to correct */
-            output = input;
-        }
-        return output;
+    protected String correctURL(final String url) {
+        return url;
+    }
+
+    protected boolean supportsHost(final String host) {
+        return host.equalsIgnoreCase(this.getHost());
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
@@ -493,16 +337,11 @@ public class MightyScriptAdLinkFly extends antiDDoSForDecrypt {
         return decryptedLinks;
     }
 
-    private boolean waittimeIsSkippable(final String source_host) {
-        if (StringUtils.isEmpty(source_host)) {
-            /* WTF */
-            return false;
-        }
-        if (domains_waittime_skippable.contains(source_host)) {
-            /* Waittime is skippable for this host */
-            return true;
-        }
-        /* Waittime is NOT skippable for all other hosts */
+    protected boolean waittimeIsSkippable(final String source_host) {
+        return false;
+    }
+
+    protected boolean captchaIsSkippable(final String source_host) {
         return false;
     }
 
@@ -543,7 +382,7 @@ public class MightyScriptAdLinkFly extends antiDDoSForDecrypt {
                 hasCaptcha = false;
             }
         }
-        if (hasCaptcha && domains_captcha_skippable.contains(source_host)) {
+        if (hasCaptcha && this.captchaIsSkippable(source_host)) {
             logger.info("Captcha should be required but current host does not require captcha");
             hasCaptcha = false;
         }
---
+++ src/jd/plugins/decrypter/UiiIo.java
@@ -0,0 +1,77 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.parser.UrlQuery;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class UiiIo extends MightyScriptAdLinkFly {
+    public UiiIo(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "uii.io", "szs.pw", "passgen.icu" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(full\\?api=[a-f0-9]+\\&url=[a-zA-Z0-9_/\\+\\=\\-%]+\\&type=1|[A-Za-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        if (param.getCryptedUrl().matches("https?://[^/]+/full\\?api=[a-f0-9]+\\&url=[a-zA-Z0-9_/\\+\\=\\-%]+\\&type=1")) {
+            final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+            /* Final URL is base64 encrypted in given URL -> No http request required */
+            final String urlBase64 = UrlQuery.parse(param.getCryptedUrl()).get("url");
+            decryptedLinks.add(this.createDownloadlink(Encoding.Base64Decode(urlBase64)));
+            return decryptedLinks;
+        } else {
+            /* Default handling */
+            return super.decryptIt(param, progress);
+        }
+    }
+}
