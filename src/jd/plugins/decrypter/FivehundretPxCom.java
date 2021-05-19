e21ad38f4686528e95d78ca35e63d62a048a6d0b
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-19 10:12:35 +0000

    FivehundretPxCom: -fixes #89406 -reviewed plugin, code cleanup, update filename/linkID handling
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44309 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4dfe3e13b071cbe6e845016465693f8fcc480e85

--- src/jd/plugins/decrypter/FivehundretPxCom.java
+++ src/jd/plugins/decrypter/FivehundretPxCom.java
@@ -1,16 +1,16 @@
 package jd.plugins.decrypter;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
+import java.util.Map.Entry;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
@@ -19,24 +19,123 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.encoding.URLEncode;
+import org.appwork.utils.net.URLHelper;
+
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "500px.com" }, urls = { "https?://(?:www\\.)?500px\\.com/p/([^/\\?]+)(/galleries/[^/]+)?" })
 public class FivehundretPxCom extends PluginForDecrypt {
     public FivehundretPxCom(PluginWrapper wrapper) {
         super(wrapper);
     }
 
+    private static Map<String, String> USER_ID_MAP  = new HashMap<String, String>();
+    private URL                        photoBaseURL = null;
+
+    protected String getUserName(final String userID) {
+        if (userID != null) {
+            synchronized (USER_ID_MAP) {
+                final Iterator<Entry<String, String>> it = USER_ID_MAP.entrySet().iterator();
+                while (it.hasNext()) {
+                    final Entry<String, String> next = it.next();
+                    if (userID.equals(next.getValue())) {
+                        return next.getKey();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    protected String getUserID(final String username) {
+        if (username != null) {
+            synchronized (USER_ID_MAP) {
+                final Iterator<Entry<String, String>> it = USER_ID_MAP.entrySet().iterator();
+                while (it.hasNext()) {
+                    final Entry<String, String> next = it.next();
+                    if (username.equalsIgnoreCase(next.getKey())) {
+                        return next.getValue();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    protected String getUserID(final Browser br, String username) throws Exception {
+        if (username == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        synchronized (USER_ID_MAP) {
+            String userID = getUserID(username);
+            if (userID == null) {
+                userID = "";
+                final Browser brc = br.cloneBrowser();
+                brc.getPage("https://api.500px.com/v1/users/search?term=" + URLEncode.encodeURIComponent(username));
+                final Map<String, Object> map = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
+                final List<Map<String, Object>> users = (List<Map<String, Object>>) map.get("users");
+                for (Map<String, Object> user : users) {
+                    final String realUserName = StringUtils.valueOfOrNull(user.get("username"));
+                    if (username.equalsIgnoreCase(realUserName)) {
+                        username = realUserName;
+                        userID = StringUtils.valueOfOrNull(user.get("id"));
+                        break;
+                    }
+                }
+                USER_ID_MAP.put(username, userID);
+            }
+            if (StringUtils.isEmpty(userID)) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            } else {
+                return userID;
+            }
+        }
+    }
+
+    @Override
+    public void init() {
+        Browser.setRequestIntervalLimitGlobal("https://api.500px.com", 250);
+    }
+
+    private DownloadLink parsePhoto(Map<String, Object> photo, final String userID, final String userName) throws Exception {
+        final Number photoID = (Number) photo.get("id");
+        if (photoID == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        String url = (String) photo.get("url");
+        if (StringUtils.isNotEmpty(url)) {
+            url = URLHelper.parseLocation(photoBaseURL, url).toString();
+        } else {
+            url = URLHelper.parseLocation(photoBaseURL, "/photo/" + photoID.toString()).toString();
+        }
+        final DownloadLink downloadLink = createDownloadlink(url);
+        final String photoName = (String) photo.get("name");
+        String fileName = userName;
+        if (StringUtils.isNotEmpty(photoName)) {
+            fileName = fileName + " - " + photoName;
+        }
+        fileName = fileName + " - " + photoID;
+        fileName = fileName + ".jpg";
+        downloadLink.setFinalFileName(fileName);
+        downloadLink.setProperty("userID", userID);
+        downloadLink.setProperty("photoID", photoID);
+        downloadLink.setAvailable(true);
+        return downloadLink;
+    }
+
     @Override
     public ArrayList<DownloadLink> decryptIt(CryptedLink parameter, ProgressController progress) throws Exception {
+        photoBaseURL = new URL("https://" + this.getHost());
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         br.setFollowRedirects(true);
-        final String username = new Regex(parameter.toString(), this.getSupportedLinks()).getMatch(0);
+        String userName = new Regex(parameter.toString(), this.getSupportedLinks()).getMatch(0);
+        final String userID = getUserID(br, userName);
+        userName = getUserName(userID);
         if (parameter.toString().matches(".+/galleries/.+")) {
             /* Crawl a gallery of a user */
-            if (true) {
-                /* TODO: 2020-09-09: Broken */
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            }
-            String curatorID = null;
             String galleryName = null;
             if (parameter.toString().matches(".+/featured")) {
                 galleryName = br.getRegex("<option selected value='\\d+'>\\s*(.*?)\\s*</option").getMatch(0);
@@ -48,55 +147,36 @@ public class FivehundretPxCom extends PluginForDecrypt {
             } else {
                 galleryName = new Regex(parameter.toString(), ".+/galleries/(.+)").getMatch(0);
             }
-            br.getPage("https://api.500px.com/v1/users/" + curatorID + "/galleries/" + galleryName + "?include_user=true&include_cover=1&cover_size=2048");
+            br.getPage("https://api.500px.com/v1/users/" + userID + "/galleries/" + galleryName + "?include_user=true&include_cover=1&cover_size=2048");
             final String galleryID = br.getRegex("\"id\"\\s*:\\s*(\\d+)").getMatch(0);
             if (galleryID == null) {
                 throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
             }
-            final String name = br.getRegex("\"name\"\\s*:\\s*\"(.*?)\"").getMatch(0);
+            galleryName = br.getRegex("\"name\"\\s*:\\s*\"(.*?)\"").getMatch(0);
             final FilePackage fp = FilePackage.getInstance();
             String fpName = "";
-            if (StringUtils.isNotEmpty(username)) {
-                fpName = username;
+            if (StringUtils.isNotEmpty(userName)) {
+                fpName = userName;
             } else {
                 fpName = galleryID;
             }
-            if (StringUtils.isNotEmpty(name)) {
+            if (StringUtils.isNotEmpty(galleryName)) {
                 if (fpName.length() > 0) {
-                    fpName = fpName + "_" + name;
+                    fpName = fpName + "_" + galleryName;
                 }
             }
             fp.setName(fpName);
             int page = 0;
             while (!isAbort()) {
-                br.getPage("https://api.500px.com/v1/users/" + curatorID + "/galleries/" + galleryID + "/items?rpp=50&image_size[]=1&image_size[]=2&image_size[]=32&image_size[]=31&image_size[]=33&image_size[]=34&image_size[]=35&image_size[]=36&image_size[]=2048&image_size[]=4&image_size[]=14&include_licensing=true&formats=jpeg,lytro&sort=position&sort_direction=asc&page=" + page++ + "&rpp=50");
+                br.getPage("https://api.500px.com/v1/users/" + userID + "/galleries/" + galleryID + "/items?rpp=50&image_size[]=1&image_size[]=2&image_size[]=32&image_size[]=31&image_size[]=33&image_size[]=34&image_size[]=35&image_size[]=36&image_size[]=2048&image_size[]=4&image_size[]=14&include_licensing=true&formats=jpeg,lytro&sort=position&sort_direction=asc&page=" + page++ + "&rpp=50");
                 final Map<String, Object> map = JSonStorage.restoreFromString(br.toString(), TypeRef.HASHMAP);
                 final List<Map<String, Object>> photos = (List<Map<String, Object>>) map.get("photos");
                 if (photos == null) {
                     throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
                 }
                 for (final Map<String, Object> photo : photos) {
-                    final Number id = (Number) photo.get("id");
-                    final String url = photo.get("url") != null ? (String) photo.get("url") : null;
-                    final DownloadLink downloadLink;
-                    if (StringUtils.isNotEmpty(url)) {
-                        downloadLink = createDownloadlink(br.getURL(url).toString());
-                    } else {
-                        downloadLink = createDownloadlink(br.getURL("/photo/" + id.intValue()).toString());
-                    }
-                    if (id != null) {
-                        downloadLink.setLinkID(getHost() + "://" + id);
-                    }
+                    final DownloadLink downloadLink = parsePhoto(photo, userID, userName);
                     fp.add(downloadLink);
-                    final String photoName = photo.get("name") != null ? (String) photo.get("name") : null;
-                    if (StringUtils.isNotEmpty(photoName)) {
-                        if (StringUtils.isNotEmpty(username)) {
-                            downloadLink.setName(username + " - " + photoName + ".jpg");
-                        } else {
-                            downloadLink.setName(photoName + ".jpg");
-                        }
-                    }
-                    downloadLink.setAvailable(true);
                     distribute(downloadLink);
                     ret.add(downloadLink);
                 }
@@ -108,15 +188,14 @@ public class FivehundretPxCom extends PluginForDecrypt {
         } else {
             /* Crawl all images of a user */
             final FilePackage fp = FilePackage.getInstance();
-            if (StringUtils.isNotEmpty(username)) {
-                fp.setName(username);
+            if (StringUtils.isNotEmpty(userName)) {
+                fp.setName(userName);
             }
             br.setAllowedResponseCodes(new int[] { 500 });
             int page = 0;
             while (!isAbort()) {
                 logger.info("Crawling page: " + page);
-                /* 2020-09-09: They're using a graphql API but we can keep using their "old" API! */
-                br.getPage("https://api.500px.com/v1/photos?feature=user&stream=photos&username=" + username + "&include_states=true&image_size[]=1&image_size[]=2&image_size[]=32&image_size[]=31&image_size[]=33&image_size[]=34&image_size[]=35&image_size[]=36&image_size[]=2048&image_size[]=4&image_size[]=14&include_licensing=true&page=" + page++ + "&rpp=50");
+                br.getPage("https://api.500px.com/v1/photos?feature=user&stream=photos&username=" + userName + "&include_states=true&image_size[]=1&image_size[]=2&image_size[]=32&image_size[]=31&image_size[]=33&image_size[]=34&image_size[]=35&image_size[]=36&image_size[]=2048&image_size[]=4&image_size[]=14&include_licensing=true&page=" + page++ + "&rpp=50");
                 if (br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 500) {
                     ret.add(createOfflinelink(parameter.toString()));
                     return ret;
@@ -127,30 +206,10 @@ public class FivehundretPxCom extends PluginForDecrypt {
                     throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
                 }
                 for (final Map<String, Object> photo : photos) {
-                    final Number id = (Number) photo.get("id");
-                    String url = photo.get("url") != null ? (String) photo.get("url") : null;
-                    if (StringUtils.isEmpty(url)) {
-                        url = "/photo/" + id.intValue();
-                    }
-                    if (url.startsWith("/")) {
-                        url = "https://" + this.getHost() + url;
-                    }
-                    final DownloadLink dl = createDownloadlink(url);
-                    if (id != null) {
-                        dl.setLinkID(getHost() + "://" + id);
-                    }
-                    fp.add(dl);
-                    final String photoName = photo.get("name") != null ? (String) photo.get("name") : null;
-                    if (StringUtils.isNotEmpty(photoName)) {
-                        if (StringUtils.isNotEmpty(username)) {
-                            dl.setName(username + " - " + photoName + ".jpg");
-                        } else {
-                            dl.setName(photoName + ".jpg");
-                        }
-                    }
-                    dl.setAvailable(true);
-                    distribute(dl);
-                    ret.add(dl);
+                    final DownloadLink downloadLink = parsePhoto(photo, userID, userName);
+                    fp.add(downloadLink);
+                    distribute(downloadLink);
+                    ret.add(downloadLink);
                 }
                 final Number total_pages = (Number) map.get("total_pages");
                 if (total_pages == null || page > total_pages.intValue()) {
