6c5a2b303fe3c8c390069b5e5c689770d820dd0c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-21 12:10:44 +0000

    *Plugins: Fixes/Changes/Maintenance* - HighWayCore: improved login, added apikey login refs #89787
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44329 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9fe426250b761431aa45a10aba4605602f3090ea

--- src/jd/plugins/hoster/SimplyPremiumCom2.java
+++ src/jd/plugins/hoster/SimplyPremiumCom2.java
@@ -18,9 +18,23 @@ package jd.plugins.hoster;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+import org.appwork.swing.MigPanel;
+import org.appwork.swing.components.ExtPasswordField;
+import org.appwork.uio.ConfirmDialogInterface;
+import org.appwork.uio.UIOManager;
+import org.appwork.utils.Application;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.os.CrossSystem;
+import org.appwork.utils.swing.dialog.ConfirmDialog;
+import org.jdownloader.gui.InputChangedCallbackInterface;
+import org.jdownloader.plugins.accounts.AccountBuilderInterface;
 import org.jdownloader.plugins.components.usenet.UsenetServer;
 
 import jd.PluginWrapper;
+import jd.gui.swing.components.linkbutton.JLink;
 import jd.plugins.Account;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
@@ -29,6 +43,8 @@ import jd.plugins.PluginException;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 1, names = { "simply-premium.com" }, urls = { "" })
 public class SimplyPremiumCom2 extends HighWayCore {
+    private static final String PROPERTY_ACCOUNT_API_MIGRATION_MESSAGE_DISPLAYED = "API_MIGRATION_MESSAGE_DISPLAYED";
+
     public SimplyPremiumCom2(PluginWrapper wrapper) {
         super(wrapper);
         this.enablePremium("https://www.simply-premium.com/vip");
@@ -52,6 +68,11 @@ public class SimplyPremiumCom2 extends HighWayCore {
     }
 
     @Override
+    protected boolean useApikeyLogin() {
+        return true;
+    }
+
+    @Override
     public void reset() {
     }
 
@@ -69,10 +90,189 @@ public class SimplyPremiumCom2 extends HighWayCore {
 
     @Override
     protected void exceptionAccountInvalid(final Account account) throws PluginException {
-        if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nUngültiger Benutzername/Passwort!\r\nDu bist dir sicher, dass dein eingegebener Benutzername und Passwort stimmen? Versuche folgendes:\r\n1. Falls dein Passwort Sonderzeichen enthält, ändere es (entferne diese) und versuche es erneut!\r\n2. Gib deine Zugangsdaten per Hand (ohne kopieren/einfügen) ein.", PluginException.VALUE_ID_PREMIUM_DISABLE);
+        if (account.hasProperty(PROPERTY_ACCOUNT_MAXCHUNKS) && !account.hasProperty(PROPERTY_ACCOUNT_API_MIGRATION_MESSAGE_DISPLAYED)) {
+            /**
+             * Show this message once for every user after migration to APIv2. </br>
+             * This uses property "usenetU" to determine if this account has ever been checked successfully before. </br>
+             * TODO: Remove this after 2021-09 (some time in 2021-10)
+             */
+            account.setProperty(PROPERTY_ACCOUNT_API_MIGRATION_MESSAGE_DISPLAYED, true);
+            showOneTimeLougoutAPIMigrationMessage();
         } else {
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nInvalid username/password!\r\nYou're sure that the username and password you entered are correct? Some hints:\r\n1. If your password contains special characters, change it (remove them) and try again!\r\n2. Type in your username/password by hand without copy & paste.", PluginException.VALUE_ID_PREMIUM_DISABLE);
+            showAPILoginInformation();
+        }
+        if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nAPI Key!\r\nDeinen API Key findest du hier: simply-premium.com/profile", PluginException.VALUE_ID_PREMIUM_DISABLE);
+        } else {
+            throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nInvalid API Key!\r\nYou can find your API Key here: simply-premium.com/profile", PluginException.VALUE_ID_PREMIUM_DISABLE);
+        }
+    }
+
+    private Thread showOneTimeLougoutAPIMigrationMessage() {
+        final Thread thread = new Thread() {
+            public void run() {
+                final String apiCredsURLWithoutProtocol = "simply-premium.com/profile";
+                try {
+                    String message = "";
+                    final String title;
+                    if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+                        title = "simply-premium.com - einmaliger Logout";
+                        message += "Hallo liebe(r) simply-premium NutzerIn\r\n";
+                        message += "Wegen technischer Änderungen wurdest du einmalig automatisch ausgeloggt.\r\n";
+                        message += "Ab sofort benötigst du für den simply-premium Login in JD nur noch deinen API Key.\r\n";
+                        message += "Dies dient der Sicherheit deines Accounts.\r\n";
+                        message += "Du findest deinen API Key hier: " + apiCredsURLWithoutProtocol;
+                    } else {
+                        title = "simply-premium.com - you've been logged out";
+                        message += "Hello dear simply-premium user\r\n";
+                        message += "Due to technical changes you have been logged out automatically once.\r\n";
+                        message += "From now on you will need your simply-premium API Key to login in JD.\r\n";
+                        message += "This step was taken to improve account security.\r\n";
+                        message += "Your can find your API Key here: " + apiCredsURLWithoutProtocol;
+                    }
+                    final ConfirmDialog dialog = new ConfirmDialog(UIOManager.LOGIC_COUNTDOWN, title, message);
+                    dialog.setTimeout(3 * 60 * 1000);
+                    if (CrossSystem.isOpenBrowserSupported() && !Application.isHeadless()) {
+                        CrossSystem.openURL("https://" + apiCredsURLWithoutProtocol);
+                    }
+                    final ConfirmDialogInterface ret = UIOManager.I().show(ConfirmDialogInterface.class, dialog);
+                    ret.throwCloseExceptions();
+                } catch (final Throwable e) {
+                    getLogger().log(e);
+                }
+            };
+        };
+        thread.setDaemon(true);
+        thread.start();
+        return thread;
+    }
+
+    private Thread showAPILoginInformation() {
+        final Thread thread = new Thread() {
+            public void run() {
+                final String apiCredsURLWithoutProtocol = "simply-premium.com/profile";
+                try {
+                    String message = "";
+                    final String title;
+                    if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+                        title = "simply-premium.com - Login";
+                        message += "Hallo liebe(r) simply-premium NutzerIn\r\n";
+                        message += "Um deinen simply-premium Account in JDownloader verwenden zu können, musst du folgende Schritte beachten:\r\n";
+                        message += "1. Öffne diesen Link im Browser falls das nicht automatisch passiert:\r\n\t'" + apiCredsURLWithoutProtocol + "'\t\r\n";
+                        message += "2. Logge dich mit dem dort aufgeführten API Key in JDownloader ein.\r\n";
+                        message += "Falls du myjdownloader/headless verwendest, gib diesen API Key in das Benutzername UND Passwort Feld ein.";
+                    } else {
+                        title = "simply-premium - Login";
+                        message += "Hello dear simply-premium user\r\n";
+                        message += "In order to use this service in JDownloader, you need to follow these steps:\r\n";
+                        message += "1. Open this URL in your browser if it is not opened automatically:\r\n\t'" + apiCredsURLWithoutProtocol + "'\t\r\n";
+                        message += "2. Enter your API Key to login in JDownloader.\r\n";
+                        message += "If you're using myjdownloader/headless, enter your API Key in the username AND password fields.";
+                    }
+                    final ConfirmDialog dialog = new ConfirmDialog(UIOManager.LOGIC_COUNTDOWN, title, message);
+                    dialog.setTimeout(2 * 60 * 1000);
+                    if (CrossSystem.isOpenBrowserSupported() && !Application.isHeadless()) {
+                        CrossSystem.openURL("https://" + apiCredsURLWithoutProtocol);
+                    }
+                    final ConfirmDialogInterface ret = UIOManager.I().show(ConfirmDialogInterface.class, dialog);
+                    ret.throwCloseExceptions();
+                } catch (final Throwable e) {
+                    getLogger().log(e);
+                }
+            };
+        };
+        thread.setDaemon(true);
+        thread.start();
+        return thread;
+    }
+
+    @Override
+    public AccountBuilderInterface getAccountFactory(final InputChangedCallbackInterface callback) {
+        return new SimplyPremiumAccountFactory(callback);
+    }
+
+    public static class SimplyPremiumAccountFactory extends MigPanel implements AccountBuilderInterface {
+        /**
+         *
+         */
+        private static final long serialVersionUID = 1L;
+        private final String      PINHELP          = "Enter your API Key";
+
+        private String getPassword() {
+            if (this.pass == null) {
+                return null;
+            }
+            if (EMPTYPW.equals(new String(this.pass.getPassword()))) {
+                return null;
+            }
+            return new String(this.pass.getPassword());
+        }
+
+        public boolean updateAccount(Account input, Account output) {
+            boolean changed = false;
+            if (!StringUtils.equals(input.getUser(), output.getUser())) {
+                output.setUser(input.getUser());
+                changed = true;
+            }
+            if (!StringUtils.equals(input.getPass(), output.getPass())) {
+                output.setPass(input.getPass());
+                changed = true;
+            }
+            return changed;
+        }
+
+        private final ExtPasswordField pass;
+        private static String          EMPTYPW = "                 ";
+
+        public SimplyPremiumAccountFactory(final InputChangedCallbackInterface callback) {
+            super("ins 0, wrap 2", "[][grow,fill]", "");
+            if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+                add(new JLabel("Hier findest du deinen API Key:"));
+            } else {
+                add(new JLabel("Click here to find your API Key:"));
+            }
+            add(new JLink("https://www.simply-premium.com/profile"));
+            add(new JLabel("API Key:"));
+            add(this.pass = new ExtPasswordField() {
+                @Override
+                public void onChanged() {
+                    callback.onChangedInput(this);
+                }
+            }, "");
+            if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
+                pass.setHelpText("Gib deinen API Key ein");
+            } else {
+                pass.setHelpText("Enter your API Key");
+            }
+        }
+
+        @Override
+        public JComponent getComponent() {
+            return this;
+        }
+
+        @Override
+        public void setAccount(Account defaultAccount) {
+            if (defaultAccount != null) {
+                // name.setText(defaultAccount.getUser());
+                pass.setText(defaultAccount.getPass());
+            }
+        }
+
+        @Override
+        public boolean validateInputs() {
+            // final String userName = getUsername();
+            // if (userName == null || !userName.trim().matches("^\\d{9}$")) {
+            // idLabel.setForeground(Color.RED);
+            // return false;
+            // }
+            // idLabel.setForeground(Color.BLACK);
+            return getPassword() != null;
+        }
+
+        @Override
+        public Account getAccount() {
+            return new Account(null, getPassword());
         }
     }
 }
