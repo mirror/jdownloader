c286b8eea6a4281a61ee0e424963fc2aab7731fc
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-25 17:19:16 +0000

    PluginForHost: -updated createAskToUsePremiumDialog, reduced timeout to 1 min
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44345 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/PluginForHost.java
+++ src/jd/plugins/PluginForHost.java
@@ -190,14 +190,14 @@ import org.jdownloader.updatev2.UpdateController;
 public abstract class PluginForHost extends Plugin {
     private static final String    COPY_MOVE_FILE = "CopyMoveFile";
     private static final Pattern[] PATTERNS       = new Pattern[] {
-        /**
-         * these patterns should split filename and fileextension (extension must include the
-         * point)
-         */
-        // multipart rar archives
-        Pattern.compile("(.*)(\\.pa?r?t?\\.?[0-9]+.*?\\.rar$)", Pattern.CASE_INSENSITIVE),
-        // normal files with extension
-        Pattern.compile("(.*)(\\..*?$)", Pattern.CASE_INSENSITIVE) };
+                                                  /**
+                                                   * these patterns should split filename and fileextension (extension must include the
+                                                   * point)
+                                                   */
+                                                  // multipart rar archives
+            Pattern.compile("(.*)(\\.pa?r?t?\\.?[0-9]+.*?\\.rar$)", Pattern.CASE_INSENSITIVE),
+            // normal files with extension
+            Pattern.compile("(.*)(\\..*?$)", Pattern.CASE_INSENSITIVE) };
     private LazyHostPlugin         lazyP          = null;
     /**
      * Is true if the user has answered a captcha challenge. Does not say anything whether or not the answer was correct.
@@ -1283,16 +1283,16 @@ public abstract class PluginForHost extends Plugin {
     public void handleMultiHost(DownloadLink downloadLink, Account account) throws Exception {
         /*
          * fetchAccountInfo must fill ai.setMultiHostSupport to signal all supported multiHosts
-         *
+         * 
          * please synchronized on accountinfo and the ArrayList<String> when you change something in the handleMultiHost function
-         *
+         * 
          * in fetchAccountInfo we don't have to synchronize because we create a new instance of AccountInfo and fill it
-         *
+         * 
          * if you need customizable maxDownloads, please use getMaxSimultanDownload to handle this you are in multihost when account host
          * does not equal link host!
-         *
-         *
-         *
+         * 
+         * 
+         * 
          * will update this doc about error handling
          */
         logger.severe("invalid call to handleMultiHost: " + downloadLink.getName() + ":" + downloadLink.getHost() + " to " + getHost() + ":" + this.getVersion() + " with " + account);
@@ -2375,12 +2375,14 @@ public abstract class PluginForHost extends Plugin {
     }
 
     protected AskToUsePremiumDialog createAskToUsePremiumDialog(final String domain) {
-        return new AskToUsePremiumDialog(domain, this) {
+        AskToUsePremiumDialog dialog = new AskToUsePremiumDialog(domain, this) {
             @Override
             public String getDontShowAgainKey() {
                 return "adsPremium_" + domain;
             }
         };
+        dialog.setTimeout(1 * 60 * 1000);
+        return dialog;
     }
 
     /**
