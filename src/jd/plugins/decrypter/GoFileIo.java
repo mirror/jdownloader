0ca9d6612fb99c44c30985cbd93483660752e93f
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-05-31 16:05:44 +0000

    *Plugins: Fixes/Changes/Maintenance* - atvat: prefer HD HLS over older handling RE forum 87234 - gofileio: fixed fixes #89809
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44383 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/GoFileIo.java
+++ src/jd/plugins/decrypter/GoFileIo.java
@@ -8,6 +8,7 @@ import org.appwork.net.protocol.http.HTTPConstants;
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
 import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
 import org.appwork.utils.net.HTTPHeader;
 import org.appwork.utils.parser.UrlQuery;
 
@@ -19,51 +20,34 @@ import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
-import jd.plugins.LinkStatus;
-import jd.plugins.PluginException;
+import jd.plugins.FilePackage;
 import jd.plugins.PluginForDecrypt;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "gofile.io" }, urls = { "https?://(?:www\\.)?gofile\\.io/(#download#|\\?c=|d/)[A-Za-z0-9]+$" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "gofile.io" }, urls = { "https?://(?:www\\.)?gofile\\.io/(?:#download#|\\?c=|d/)([A-Za-z0-9]+)$" })
 public class GoFileIo extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(final CryptedLink parameter, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        final String c = new Regex(parameter.getCryptedUrl(), "(#download#|\\?c=|d/)([A-Za-z0-9]+)").getMatch(1);
-        br.getPage(parameter.getCryptedUrl());
-        final GetRequest server = br.createGetRequest("https://apiv2.gofile.io/getServer?c=" + c);
-        server.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_ORIGIN, "https://gofile.io"));
-        Browser brc = br.cloneBrowser();
-        brc.getPage(server);
-        Map<String, Object> response = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
-        String serverHost = null;
-        if ("ok".equals(response.get("status"))) {
-            final Map<String, Object> data = (Map<String, Object>) response.get("data");
-            serverHost = (String) data.get("server");
-        } else if ("error".equals(response.get("status"))) {
-            return ret;
-        } else {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        if (serverHost == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
+        final String folderID = new Regex(parameter.getCryptedUrl(), this.getSupportedLinks()).getMatch(0);
         final UrlQuery query = new UrlQuery();
-        query.add("c", c);
+        query.add("folderId", folderID);
         String passCode = null;
         boolean passwordCorrect = true;
         boolean passwordRequired = false;
         int attempt = 0;
-        brc = br.cloneBrowser();
+        Map<String, Object> response = null;
+        final Browser brc = br.cloneBrowser();
         do {
             if (passwordRequired) {
                 passCode = getUserInput("Password?", parameter);
-                query.addAndReplace("p", JDHash.getSHA256(passCode));
+                query.addAndReplace("password", JDHash.getSHA256(passCode));
             }
-            final GetRequest post = br.createGetRequest("https://" + serverHost + ".gofile.io/getUpload?" + query.toString());
-            post.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_ORIGIN, "https://gofile.io"));
-            brc.getPage(post);
+            final GetRequest req = br.createGetRequest("https://api." + this.getHost() + "/getFolder?" + query.toString());
+            req.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_ORIGIN, "https://gofile.io"));
+            req.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_REFERER, "https://gofile.io"));
+            brc.getPage(req);
             response = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
-            if ("passwordRequired".equals(response.get("status")) || "passwordWrong".equals(response.get("status"))) {
+            if ("error-passwordRequired".equals(response.get("status")) || "error-passwordWrong".equals(response.get("status"))) {
                 passwordRequired = true;
                 passwordCorrect = false;
                 attempt += 1;
@@ -82,15 +66,33 @@ public class GoFileIo extends PluginForDecrypt {
         }
         if ("ok".equals(response.get("status"))) {
             final Map<String, Object> data = (Map<String, Object>) response.get("data");
-            final Map<String, Map<String, Object>> files = (Map<String, Map<String, Object>>) data.get("files");
+            final String folderName = (String) data.get("name");
+            final FilePackage fp = FilePackage.getInstance();
+            if (!StringUtils.isEmpty(folderName)) {
+                fp.setName(folderName);
+            } else {
+                /* Fallback */
+                fp.setName(folderID);
+            }
+            final Map<String, Map<String, Object>> files = (Map<String, Map<String, Object>>) data.get("contents");
             for (final Entry<String, Map<String, Object>> file : files.entrySet()) {
                 final String fileID = file.getKey();
-                final DownloadLink link = createDownloadlink("https://gofile.io/?c=" + c + "#file=" + fileID);
+                final DownloadLink link = createDownloadlink("https://gofile.io/?c=" + folderID + "#file=" + fileID);
                 final Map<String, Object> entry = file.getValue();
+                final String type = (String) entry.get("type");
+                if (!type.equals("file")) {
+                    logger.info("Unsupported type: " + type);
+                    continue;
+                }
+                final String description = (String) entry.get("description");
                 jd.plugins.hoster.GofileIo.parseFileInfo(link, entry);
                 link.setAvailable(true);
+                link._setFilePackage(fp);
                 if (passCode != null) {
                     link.setDownloadPassword(passCode);
+                }
+                if (!StringUtils.isEmpty(description)) {
+                    link.setComment(description);
                 }
                 ret.add(link);
             }
