9da3461eb9363199f9f407bdea13e1199d0716e8
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-06-11 12:05:39 +0000

    YetiShareCore: -updated isPremiumAccount, VIP Premium without expire date build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44460 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/plugins/components/YetiShareCore.java
+++ src/org/jdownloader/plugins/components/YetiShareCore.java
@@ -31,25 +31,6 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.StorageException;
-import org.appwork.storage.TypeRef;
-import org.appwork.uio.ConfirmDialogInterface;
-import org.appwork.uio.UIOManager;
-import org.appwork.utils.Application;
-import org.appwork.utils.DebugMode;
-import org.appwork.utils.Exceptions;
-import org.appwork.utils.Hash;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.Time;
-import org.appwork.utils.formatter.SizeFormatter;
-import org.appwork.utils.formatter.TimeFormatter;
-import org.appwork.utils.os.CrossSystem;
-import org.appwork.utils.parser.UrlQuery;
-import org.appwork.utils.swing.dialog.ConfirmDialog;
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 import jd.PluginWrapper;
 import jd.config.Property;
 import jd.controlling.AccountController;
@@ -75,6 +56,25 @@ import jd.plugins.PluginException;
 import jd.plugins.components.PluginJSonUtils;
 import jd.plugins.components.SiteType.SiteTemplate;
 import jd.plugins.components.UserAgents;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.StorageException;
+import org.appwork.storage.TypeRef;
+import org.appwork.uio.ConfirmDialogInterface;
+import org.appwork.uio.UIOManager;
+import org.appwork.utils.Application;
+import org.appwork.utils.DebugMode;
+import org.appwork.utils.Exceptions;
+import org.appwork.utils.Hash;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.Time;
+import org.appwork.utils.formatter.SizeFormatter;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.appwork.utils.os.CrossSystem;
+import org.appwork.utils.parser.UrlQuery;
+import org.appwork.utils.swing.dialog.ConfirmDialog;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperHostPluginRecaptchaV2;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
 public class YetiShareCore extends antiDDoSForHost {
@@ -282,8 +282,8 @@ public class YetiShareCore extends antiDDoSForHost {
 
     /**
      * @return true: Implies that website will show filename & filesize via website.tld/<fuid>~i <br />
-     *         Most YetiShare websites support this kind of linkcheck! </br>
-     *         false: Implies that website does NOT show filename & filesize via website.tld/<fuid>~i. <br />
+     *         Most YetiShare websites support this kind of linkcheck! </br> false: Implies that website does NOT show filename & filesize
+     *         via website.tld/<fuid>~i. <br />
      *         default: true
      */
     public boolean supports_availablecheck_over_info_page(DownloadLink link) {
@@ -320,9 +320,7 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * Enforces old, non-ajax login-method. </br>
-     * This is only rarely needed e.g. filemia.com </br>
-     * default = false
+     * Enforces old, non-ajax login-method. </br> This is only rarely needed e.g. filemia.com </br> default = false
      */
     @Deprecated
     protected boolean enforce_old_login_method() {
@@ -395,8 +393,8 @@ public class YetiShareCore extends antiDDoSForHost {
                 link.setDownloadSize(SizeFormatter.getSize(Encoding.htmlDecode(fileInfo[1].replace(",", ""))));
             }
             /**
-             * Additional offline check. Useful for websites which still provide filename & filesize for offline files. </br>
-             * This can only happen on special file information page!
+             * Additional offline check. Useful for websites which still provide filename & filesize for offline files. </br> This can only
+             * happen on special file information page!
              */
             if (br.containsHTML("(?i)>\\s*Status:</span>\\s*<span>\\s*(Deleted|Usunięto)\\s*</span>")) {
                 throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
@@ -617,8 +615,7 @@ public class YetiShareCore extends antiDDoSForHost {
                         break;
                     } else if (hasGoneThroughVerifiedLoginOnce) {
                         /**
-                         * Only try once! </br>
-                         * We HAVE to be logged in at this stage!
+                         * Only try once! </br> We HAVE to be logged in at this stage!
                          */
                         this.loggedInOrException(this.br, account);
                         break;
@@ -852,9 +849,8 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * API file operations usually requires us to have the internal ID of files. </br>
-     * Most of all times we don't have this but if a website is using the "new" YetiShare script version and files were added as part of a
-     * folder, we do have these internal fileIDs available!
+     * API file operations usually requires us to have the internal ID of files. </br> Most of all times we don't have this but if a website
+     * is using the "new" YetiShare script version and files were added as part of a folder, we do have these internal fileIDs available!
      */
     protected String getInternalFileID(final DownloadLink link, final Browser br) {
         String internalFileID = null;
@@ -1179,8 +1175,8 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * Checks for reasons to ignore given PluginExceptions. </br>
-     * Example: Certain errors thay may happen during availablecheck when user is not yet logged in but won't happen when user is logged in.
+     * Checks for reasons to ignore given PluginExceptions. </br> Example: Certain errors thay may happen during availablecheck when user is
+     * not yet logged in but won't happen when user is logged in.
      */
     @Deprecated
     protected void ignorePluginException(final PluginException exception, final Browser br, final DownloadLink link, final Account account) throws PluginException {
@@ -1214,8 +1210,7 @@ public class YetiShareCore extends antiDDoSForHost {
     protected static HashMap<String, String> errorMsgURLMap = new HashMap<String, String>();
 
     /**
-     * 2020-03-25: No plugin should ever have to override this. </br>
-     * Please create a ticket before changing this!
+     * 2020-03-25: No plugin should ever have to override this. </br> Please create a ticket before changing this!
      */
     private void checkErrorsLanguageIndependant(final Browser br, final DownloadLink link, final Account account) throws PluginException {
         final String errorMsgURL = this.getErrorMsgURL(br);
@@ -1263,7 +1258,7 @@ public class YetiShareCore extends antiDDoSForHost {
                 /* Very very rare case */
                 logger.info("This file can only be downloaded by the initial uploader");
                 throw new AccountRequiredException(errorMsgURL);
-            } /** Limit errorhandling */
+            }/** Limit errorhandling */
             else if (errorkey.equalsIgnoreCase(error_you_have_reached_the_download_limit)) {
                 throw new PluginException(LinkStatus.ERROR_IP_BLOCKED, errorMsgURL, default_waittime);
             } else if (errorkey.equalsIgnoreCase(error_you_have_reached_the_download_limit_this_file)) {
@@ -1405,8 +1400,7 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * @return true = file is offline, false = file is online </br>
-     *         Be sure to always call checkErrors before calling this!
+     * @return true = file is offline, false = file is online </br> Be sure to always call checkErrors before calling this!
      * @throws Exception
      */
     protected boolean isOfflineWebsite(final Browser br, final DownloadLink link) throws Exception {
@@ -1589,8 +1583,8 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * Access any YetiShare website via browser before and call this once to auto set flag for new YetiShare version! </br>
-     * New version = YetiShare 5.0 and above, see: https://yetishare.com/release_history.html
+     * Access any YetiShare website via browser before and call this once to auto set flag for new YetiShare version! </br> New version =
+     * YetiShare 5.0 and above, see: https://yetishare.com/release_history.html
      */
     protected void parseAndSetYetiShareVersion(final Browser br, final Account account) {
         if (br.containsHTML("(?i)https?://[^/]+/(account|register|account/login|account/logout)\"")) {
@@ -1607,8 +1601,7 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * @return true: Cookies were validated</br>
-     *         false: Cookies were not validated
+     * @return true: Cookies were validated</br> false: Cookies were not validated
      */
     public boolean loginWebsite(final Account account, boolean force) throws Exception {
         synchronized (account) {
@@ -1774,39 +1767,41 @@ public class YetiShareCore extends antiDDoSForHost {
 
     protected AccountInfo fetchAccountInfoWebsite(final Account account) throws Exception {
         final AccountInfo ai = new AccountInfo();
-        loginWebsite(account, true);
-        final AccountInfo apiAccInfo = fetchAccountInfoWebsiteAPI(this.br.cloneBrowser(), account);
-        if (apiAccInfo != null) {
-            logger.info("Found AccountInfo via API --> Prefer this over website AccountInfo");
-            return apiAccInfo;
-        }
-        getPage(this.getAccountNameSpaceUpgrade(account));
-        if (this.isPremiumAccount(account, this.br)) {
-            final String expireStr = regexExpireDate(this.br);
-            // if (expireStr != null) {
-            // final long expire_milliseconds = parseExpireTimeStamp(account, expireStr);
-            // final boolean isPremium = expire_milliseconds > System.currentTimeMillis();
-            // /* If the premium account is expired we'll simply accept it as a free account. */
-            // if (!isPremium) {
-            // /* Expired premium == FREE */
-            // this.setAccountLimitsByType(account, AccountType.FREE);
-            // // ai.setStatus("Registered (free) user");
-            // } else {
-            // ai.setValidUntil(expire_milliseconds, this.br);
-            // this.setAccountLimitsByType(account, AccountType.PREMIUM);
-            // // ai.setStatus("Premium account");
-            // }}
-            /*
-             * 2021-01-07: Allow premium accounts without expire-date: I've never seen such accounts but let's say we're unable to parse
-             * expire-date but we are sure it is a premium account -> That case is covered now
-             */
-            if (expireStr != null) {
-                final long expire_milliseconds = parseExpireTimeStamp(account, expireStr);
-                ai.setValidUntil(expire_milliseconds, this.br);
+        synchronized (account) {
+            loginWebsite(account, true);
+            final AccountInfo apiAccInfo = fetchAccountInfoWebsiteAPI(this.br.cloneBrowser(), account);
+            if (apiAccInfo != null) {
+                logger.info("Found AccountInfo via API --> Prefer this over website AccountInfo");
+                return apiAccInfo;
             }
-            this.setAccountLimitsByType(account, AccountType.PREMIUM);
-        } else {
-            this.setAccountLimitsByType(account, AccountType.FREE);
+            getPage(this.getAccountNameSpaceUpgrade(account));
+            if (this.isPremiumAccount(account, this.br)) {
+                final String expireStr = regexExpireDate(this.br);
+                // if (expireStr != null) {
+                // final long expire_milliseconds = parseExpireTimeStamp(account, expireStr);
+                // final boolean isPremium = expire_milliseconds > System.currentTimeMillis();
+                // /* If the premium account is expired we'll simply accept it as a free account. */
+                // if (!isPremium) {
+                // /* Expired premium == FREE */
+                // this.setAccountLimitsByType(account, AccountType.FREE);
+                // // ai.setStatus("Registered (free) user");
+                // } else {
+                // ai.setValidUntil(expire_milliseconds, this.br);
+                // this.setAccountLimitsByType(account, AccountType.PREMIUM);
+                // // ai.setStatus("Premium account");
+                // }}
+                /*
+                 * 2021-01-07: Allow premium accounts without expire-date: I've never seen such accounts but let's say we're unable to parse
+                 * expire-date but we are sure it is a premium account -> That case is covered now
+                 */
+                if (expireStr != null) {
+                    final long expire_milliseconds = parseExpireTimeStamp(account, expireStr);
+                    ai.setValidUntil(expire_milliseconds, this.br);
+                }
+                this.setAccountLimitsByType(account, AccountType.PREMIUM);
+            } else {
+                this.setAccountLimitsByType(account, AccountType.FREE);
+            }
         }
         ai.setUnlimitedTraffic();
         if (DebugMode.TRUE_IN_IDE_ELSE_FALSE) {
@@ -1826,19 +1821,21 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     protected boolean isPremiumAccount(final Account account, final Browser br) {
+        final String accountType = br.getRegex(">\\s*Account\\s*Type\\s*:?\\s*</[^>]*>\\s*<[^>]*>\\s*(.*?)\\s*<").getMatch(0);
+        final boolean isPremiumType = StringUtils.containsIgnoreCase(accountType, "Premium");
         final String expireStr = regexExpireDate(br);
         if (expireStr == null) {
-            return false;
+            return isPremiumType;
         } else {
             final long expire_milliseconds = parseExpireTimeStamp(account, expireStr);
-            return expire_milliseconds > System.currentTimeMillis();
+            return isPremiumType || expire_milliseconds > System.currentTimeMillis();
         }
     }
 
     protected String regexExpireDate(final Browser br) {
-        String expireStr = br.getRegex("Reverts To Free Account\\s*:\\s*</td>\\s*<td>\\s*(\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})").getMatch(0);
+        String expireStr = br.getRegex("Reverts\\s*To\\s*Free\\s*Account\\s*:\\s*</td>\\s*<td>\\s*(\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})").getMatch(0);
         if (expireStr == null) {
-            expireStr = br.getRegex("Reverts To Free Account\\s*:\\s*</span>\\s*<input[^>]*value\\s*=\\s*\"(\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})").getMatch(0);
+            expireStr = br.getRegex("Reverts\\s*To\\s*Free\\s*Account\\s*:\\s*</span>\\s*<input[^>]*value\\s*=\\s*\"(\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})").getMatch(0);
             if (expireStr == null) {
                 /* More wide RegEx to be more language independant (e.g. required for freefile.me) */
                 expireStr = br.getRegex(">\\s*(\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2})\\s*<").getMatch(0);
@@ -2168,8 +2165,7 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * According to: https://fhscript.com/api#account-info </br>
-     * and: https://fhscript.com/api#account-package </br>
+     * According to: https://fhscript.com/api#account-info </br> and: https://fhscript.com/api#account-package </br>
      */
     protected AccountInfo fetchAccountInfoAPI(final Browser br, final Account account, final String key1, final String key2) throws Exception {
         final AccountInfo ai = new AccountInfo();
@@ -2390,9 +2386,8 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * https://fhscript.com/api#file-download </br>
-     * This API call only works with self-uploaded files and/or whenever the internal fileID is given --> Most of all times it is of no use
-     * for us!
+     * https://fhscript.com/api#file-download </br> This API call only works with self-uploaded files and/or whenever the internal fileID is
+     * given --> Most of all times it is of no use for us!
      */
     protected void handleDownloadAPI(final DownloadLink link, final Account account, final String apikey1, final String apikey2) throws StorageException, Exception {
         final String directlinkproperty = getDownloadModeDirectlinkProperty(account);
@@ -2455,8 +2450,8 @@ public class YetiShareCore extends antiDDoSForHost {
     }
 
     /**
-     * Handles API errormessages. </br>
-     * We usually can't use this API for downloading thus all Exceptions will be account related (as of 2021-04-22)
+     * Handles API errormessages. </br> We usually can't use this API for downloading thus all Exceptions will be account related (as of
+     * 2021-04-22)
      */
     protected void checkErrorsAPI(final Browser br, final DownloadLink link, final Account account) throws PluginException {
         Map<String, Object> entries = null;
