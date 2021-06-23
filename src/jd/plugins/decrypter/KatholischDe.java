55d0aef05e76a1d16897e6052d020cf03d634f4f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-06-23 08:59:57 +0000

    KatholischDe: -added decrypter for this site
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44537 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/KatholischDe.java
@@ -0,0 +1,57 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.PluginForDecrypt;
+
+import org.appwork.utils.StringUtils;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "katholisch.de" }, urls = { "https?://www.katholisch.de/video/\\d+[a-z0-9\\-]+" })
+public class KatholischDe extends PluginForDecrypt {
+    public KatholischDe(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        br.getPage(param.getCryptedUrl());
+        br.followRedirect();
+        final String title = br.getRegex("<title>\\s*(.*?)\\s*(-\\s*katholisch\\.de)?\\s*</").getMatch(0);
+        final String m3u8 = br.getRegex("file\\s*:\\s*'(https?://[^']*\\.m3u8)'").getMatch(0);
+        if (StringUtils.isEmpty(title) || StringUtils.isEmpty(m3u8)) {
+            return new ArrayList<DownloadLink>();
+        } else {
+            final Browser brc = br.cloneBrowser();
+            brc.getPage(m3u8);
+            final ArrayList<DownloadLink> ret = GenericM3u8Decrypter.parseM3U8(this, m3u8, brc, null, null, null, title);
+            if (ret.size() > 1) {
+                FilePackage fp = FilePackage.getInstance();
+                fp.setName(title);
+                fp.addLinks(ret);
+            }
+            return ret;
+        }
+    }
+}
