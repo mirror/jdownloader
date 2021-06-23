39986a7f6040b88b88c777a5a5f3cfff491c691a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-06-23 12:39:30 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - docworkspacecom (drivewpscom) [cloud drive website of smartphone app "WPS Office"] closes #89830
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44540 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: e2277c5bba0bbe46b67f964d1a6fa2787ccc26fa

---
+++ src/jd/plugins/hoster/DocworkspaceCom.java
@@ -0,0 +1,229 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.AccountRequiredException;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class DocworkspaceCom extends PluginForHost {
+    public DocworkspaceCom(PluginWrapper wrapper) {
+        super(wrapper);
+        // this.enablePremium("");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://ru.docworkspace.com/";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "docworkspace.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://[a-z]{2}\\." + buildHostsPatternPart(domains) + "/d/([A-Za-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME               = true;
+    private static final int     FREE_MAXCHUNKS            = 1;
+    private static final int     FREE_MAXDOWNLOADS         = 20;
+    // private static final boolean ACCOUNT_FREE_RESUME = true;
+    // private static final int ACCOUNT_FREE_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_FREE_MAXDOWNLOADS = 20;
+    // private static final boolean ACCOUNT_PREMIUM_RESUME = true;
+    // private static final int ACCOUNT_PREMIUM_MAXCHUNKS = 0;
+    // private static final int ACCOUNT_PREMIUM_MAXDOWNLOADS = 20;
+    private static String        PROPERTY_ACCOUNT_REQUIRED = "account_required";
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        if (!link.isNameSet()) {
+            /* Fallback */
+            link.setName(this.getFID(link));
+        }
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String json = br.getRegex("window\\.__INITIAL_STATE__=(\\{.*?\\});\\(function").getMatch(0);
+        Map<String, Object> entries = JSonStorage.restoreFromString(json, TypeRef.HASHMAP);
+        entries = (Map<String, Object>) JavaScriptEngineFactory.walkJson(entries, "share/fileinfo");
+        final String result = (String) entries.get("result");
+        if (!result.equalsIgnoreCase("ok")) {
+            /* E.g. "lightLinkNotExist" */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final boolean accountRequired = ((Boolean) entries.get("needLogin")).booleanValue();
+        if (accountRequired) {
+            link.setProperty(PROPERTY_ACCOUNT_REQUIRED, true);
+        } else {
+            link.removeProperty(PROPERTY_ACCOUNT_REQUIRED);
+        }
+        String filename = (String) entries.get("doc_name");
+        if (filename == null) {
+            filename = br.getRegex("name=\"title\" content=\"([^\"]+)\"").getMatch(0);
+        }
+        if (filename != null) {
+            filename = Encoding.htmlDecode(filename).trim();
+            link.setName(filename);
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link);
+        if (link.hasProperty(PROPERTY_ACCOUNT_REQUIRED)) {
+            throw new AccountRequiredException();
+        }
+        doFree(link, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void doFree(final DownloadLink link, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        if (!attemptStoredDownloadurlDownload(link, directlinkproperty, resumable, maxchunks)) {
+            final Browser brc = br.cloneBrowser();
+            brc.getHeaders().put("Accept", "application/json, text/plain, */*");
+            brc.getPage("https://ru-drive.wps.com/api/v3/links/" + this.getFID(link) + "/download?isblocks=false");
+            Map<String, Object> entries = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
+            entries = (Map<String, Object>) entries.get("fileinfo");
+            /* 2021-06-23: Seems like these hashes do not reflect the original file hashes. */
+            // final String hashMD5 = (String) entries.get("md5");
+            // if (!StringUtils.isEmpty(hashMD5)) {
+            // link.setMD5Hash(hashMD5);
+            // }
+            // final String hashSHA1 = (String) entries.get("sha1");
+            // if (!StringUtils.isEmpty(hashSHA1)) {
+            // link.setSha1Hash(hashSHA1);
+            // }
+            String dllink = (String) entries.get("url");
+            if (StringUtils.isEmpty(dllink)) {
+                logger.warning("Failed to find final downloadurl");
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resumable, maxchunks);
+            if (!this.looksLikeDownloadableContent(dl.getConnection())) {
+                try {
+                    br.followConnection(true);
+                } catch (final IOException e) {
+                    logger.log(e);
+                }
+                if (dl.getConnection().getResponseCode() == 403) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 5 * 60 * 1000l);
+                } else if (dl.getConnection().getResponseCode() == 404) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 5 * 60 * 1000l);
+                }
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            link.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public boolean hasCaptcha(final DownloadLink link, final jd.plugins.Account acc) {
+        return false;
+    }
+
+    private boolean attemptStoredDownloadurlDownload(final DownloadLink link, final String directlinkproperty, final boolean resumable, final int maxchunks) throws Exception {
+        final String url = link.getStringProperty(directlinkproperty);
+        if (StringUtils.isEmpty(url)) {
+            return false;
+        }
+        try {
+            final Browser brc = br.cloneBrowser();
+            dl = new jd.plugins.BrowserAdapter().openDownload(brc, link, url, resumable, maxchunks);
+            if (this.looksLikeDownloadableContent(dl.getConnection())) {
+                return true;
+            } else {
+                brc.followConnection(true);
+                throw new IOException();
+            }
+        } catch (final Throwable e) {
+            logger.log(e);
+            try {
+                dl.getConnection().disconnect();
+            } catch (Throwable ignore) {
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
