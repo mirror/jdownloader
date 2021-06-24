8c17303b1710163b8aa43b1a98cd02890a1e189f
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-06-24 13:52:32 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44549 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 11312d21afc4773b832732911181f58b542ed2fb

--- src/org/jdownloader/api/polling/PollingAPI.java
+++ src/org/jdownloader/api/polling/PollingAPI.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.appwork.remoteapi.APIQuery;
 import org.appwork.remoteapi.RemoteAPIInterface;
+import org.appwork.remoteapi.RemoteAPIRequest;
 import org.appwork.remoteapi.annotations.APIParameterNames;
 import org.appwork.remoteapi.annotations.ApiNamespace;
 
 @ApiNamespace("polling")
 public interface PollingAPI extends RemoteAPIInterface {
     @APIParameterNames({ "queryParams" })
-    List<PollingResultAPIStorable> poll(APIQuery queryParams);
+    List<PollingResultAPIStorable> poll(RemoteAPIRequest request, APIQuery queryParams);
 }
