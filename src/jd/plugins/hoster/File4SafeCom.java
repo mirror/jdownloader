9f5f2fc9bbd90cad4f2e4b6e995299776357ee01
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-08-09 11:06:39 +0000

    *Plugins: Fixes/Changes/Maintenance* - [XFS] file4safecom: updated connection limits RE ticket KIX-991-58741
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44818 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3b04e4c170c87e3b062726118dd896fa1e960fce

--- src/jd/plugins/hoster/File4SafeCom.java
+++ src/jd/plugins/hoster/File4SafeCom.java
@@ -80,13 +80,13 @@ public class File4SafeCom extends XFileSharingProBasic {
         final AccountType type = account != null ? account.getType() : null;
         if (AccountType.FREE.equals(type)) {
             /* Free Account */
-            return 0;
+            return 1;
         } else if (AccountType.PREMIUM.equals(type) || AccountType.LIFETIME.equals(type)) {
             /* Premium account */
-            return 0;
+            return 1;
         } else {
             /* Free(anonymous) and unknown account type */
-            return 0;
+            return 1;
         }
     }
 
