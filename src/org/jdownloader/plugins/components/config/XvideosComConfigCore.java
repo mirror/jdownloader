812cbd53cd8c04bb74fb4e1ccd24d54d970852ae
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-08-17 13:51:47 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - added XvideosCore as new base for xvideos based websites and also in order to add support for xnxxgold more easily RE forum 88212 - xnxxgold RE forum 88212
    *Plugins: RIP*
    - xnxxcom: deleted plugin as content is hosted on xvideoscom, XnXxComCrawler has been added added a while ago so most of all users should not have old URLs in their downloadlist
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@44858 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 44ca159e8dbc33d83f86f61abe9e253bd76c597b

---
+++ src/org/jdownloader/plugins/components/config/XvideosComConfigCore.java
@@ -0,0 +1,106 @@
+package org.jdownloader.plugins.components.config;
+
+import org.appwork.storage.config.annotations.AboutConfig;
+import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DefaultEnumValue;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.appwork.storage.config.annotations.LabelInterface;
+import org.jdownloader.plugins.config.Order;
+import org.jdownloader.plugins.config.PluginConfigInterface;
+import org.jdownloader.plugins.config.TakeValueFromSubconfig;
+
+public interface XvideosComConfigCore extends PluginConfigInterface {
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("Enable fast linkcheck for host plugin? If enabled, filesize won't be displayed until download is started!")
+    @Order(15)
+    boolean isEnableFastLinkcheckForHostPlugin();
+
+    void setEnableFastLinkcheckForHostPlugin(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(false)
+    @TakeValueFromSubconfig("Prefer HLS")
+    @DescriptionForConfigEntry("Prefer HLS download?")
+    @Order(30)
+    boolean isPreferHLSDownload();
+
+    void setPreferHLSDownload(boolean b);
+
+    public static enum PreferredHLSQuality implements LabelInterface {
+        BEST {
+            @Override
+            public String getLabel() {
+                return "Best";
+            }
+        },
+        Q360P {
+            @Override
+            public String getLabel() {
+                return "360p";
+            }
+        },
+        Q480P {
+            @Override
+            public String getLabel() {
+                return "480p";
+            }
+        },
+        Q720P {
+            @Override
+            public String getLabel() {
+                return "720p";
+            }
+        },
+        Q1080P {
+            @Override
+            public String getLabel() {
+                return "1080p";
+            }
+        },
+        Q2160P {
+            @Override
+            public String getLabel() {
+                return "2160p (4k)";
+            }
+        };
+    }
+
+    public static enum PreferredHTTPQuality implements LabelInterface {
+        HIGH {
+            @Override
+            public String getLabel() {
+                return "High quality";
+            }
+        },
+        LOW {
+            @Override
+            public String getLabel() {
+                return "Low quality";
+            }
+        };
+    }
+
+    @AboutConfig
+    @DefaultEnumValue("BEST")
+    @DescriptionForConfigEntry("Select preferred HLS download quality. If your preferred HLS quality is not found, best quality will be downloaded instead.")
+    @Order(100)
+    PreferredHLSQuality getPreferredHLSQuality();
+
+    void setPreferredHLSQuality(PreferredHLSQuality quality);
+
+    @AboutConfig
+    @DefaultEnumValue("HIGH")
+    @DescriptionForConfigEntry("Select preferred HTTP download quality. If your preferred HTTP quality is not found, best quality will be downloaded instead.")
+    @Order(120)
+    PreferredHTTPQuality getPreferredHTTPQuality();
+
+    void setPreferredHTTPQuality(PreferredHTTPQuality quality);
+
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry("xvideos.com can 'shadow ban' users who download a lot. This will limit the max. available quality to 240p. This experimental setting will make JD try to detect this limit.")
+    @Order(140)
+    boolean isTryToRecognizeLimit();
+
+    void setTryToRecognizeLimit(boolean b);
+}
