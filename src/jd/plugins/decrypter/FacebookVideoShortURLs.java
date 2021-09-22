cb9bf3553c637408f5ce555fad9698ee1d864161
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-09-22 12:00:36 +0000

    *Plugins: Fixes/Changes/Maintenance* zdfmediathek: fixes #89908 - integrated phoenixde - deleted old phoenixde plugin - did not add custom settings for phoenixde content for now as it essentially is zdfde content
    *Plugins: New plugins or new websites that belong to existing scripts*
    - added simple crawler for facebook video shortURLs fb.watch
    
    *Plugins: RIP*
    - rlslog.net: RIP: rlslog.net/game-over/
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45027 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/FacebookVideoShortURLs.java
@@ -0,0 +1,50 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "fb.watch" }, urls = { "https?://(?:www\\.)?fb\\.watch/[A-Za-z0-9\\-_]+/?" })
+public class FacebookVideoShortURLs extends PluginForDecrypt {
+    public FacebookVideoShortURLs(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String parameter = param.toString().replaceFirst("http://", "https://");
+        br.setFollowRedirects(false);
+        br.getPage(parameter);
+        /* We expect a redirect to: https://www.facebook.com/watch/?v=<videoID> */
+        final String redirect = br.getRedirectLocation();
+        /* No redirect or redirect to self --> Probably offline content */
+        if (redirect == null || this.canHandle(redirect)) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else {
+            decryptedLinks.add(createDownloadlink(redirect));
+            return decryptedLinks;
+        }
+    }
+}
