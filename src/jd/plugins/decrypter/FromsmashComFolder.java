51828652e363971f8a789098f60895ea6271a244
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-09-28 10:31:50 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - fromsmash.com refs #83796
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45048 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f95fe31c61b379e53e341cbea0230fa35d42f030

---
+++ src/jd/plugins/decrypter/FromsmashComFolder.java
@@ -0,0 +1,161 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.appwork.net.protocol.http.HTTPConstants;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.Time;
+import org.appwork.utils.formatter.TimeFormatter;
+import org.appwork.utils.net.HTTPHeader;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.http.Browser;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.Plugin;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.hoster.FromsmashCom;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class FromsmashComFolder extends PluginForDecrypt {
+    public FromsmashComFolder(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "fromsmash.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([A-Za-z0-9\\-_]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    protected static AtomicReference<String> TOKEN                       = new AtomicReference<String>();
+    protected static AtomicLong              TOKEN_TIMESTAMP             = new AtomicLong(-1);
+    protected static AtomicLong              TOKEN_TIMESTAMP_VALID_UNTIL = new AtomicLong(-1);
+    protected final static long              TOKEN_EXPIRE                = 60 * 60 * 1000l;
+
+    public static String getToken(final Plugin plugin, final Browser br) throws IOException, PluginException {
+        synchronized (TOKEN) {
+            String token = TOKEN.get();
+            if (!StringUtils.isEmpty(token) && Time.systemIndependentCurrentJVMTimeMillis() < TOKEN_TIMESTAMP_VALID_UNTIL.get()) {
+                return token;
+            } else {
+                final Browser brc = br.cloneBrowser();
+                prepBR(brc);
+                brc.postPageRaw("https://iam.eu-central-1.fromsmash.co/account", "{}");
+                final Map<String, Object> response = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
+                final Map<String, Object> account = (Map<String, Object>) response.get("account");
+                final Map<String, Object> tokenMap = (Map<String, Object>) account.get("token");
+                token = (String) tokenMap.get("token");
+                /* 2021-09-28: Their tokens are valid for 24H. */
+                final String tokenExpireDate = (String) tokenMap.get("expiration");
+                final long tokenTimestamp = TimeFormatter.getMilliSeconds(tokenExpireDate, "yyyy-MM-dd'T'HH:mm:ss'.000Z'", Locale.ENGLISH);
+                if (StringUtils.isEmpty(token)) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                } else {
+                    TOKEN.set(token);
+                    TOKEN_TIMESTAMP.set(Time.systemIndependentCurrentJVMTimeMillis());
+                    TOKEN_TIMESTAMP_VALID_UNTIL.set(tokenTimestamp);
+                    return token;
+                }
+            }
+        }
+    }
+
+    public static Browser prepBR(final Browser br) {
+        br.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_ORIGIN, "https://fromsmash.com"));
+        br.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_REFERER, "https://fromsmash.com"));
+        /* 2021-09-28: Without these headers they will respond with XML. */
+        br.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_ACCEPT, "application/json, text/plain, */*"));
+        br.getHeaders().put(new HTTPHeader(HTTPConstants.HEADER_REQUEST_CONTENT_TYPE, "application/json"));
+        return br;
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final String folderID = new Regex(param.getCryptedUrl(), this.getSupportedLinks()).getMatch(0);
+        br.getHeaders().put("Authorization", "Bearer " + getToken(this, this.br));
+        prepBR(br);
+        br.getPage("https://transfer.eu-central-1.fromsmash.co/transfer/" + folderID + "/preview?version=07-2020");
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            /*
+             * E.g. {"code":404,"error":"Transfer <folderID> not found","requestId":"<someHash>","details":{"name":"Transfer","primary":
+             * "<folderID>"}}
+             */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        Map<String, Object> entries = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        final Map<String, Object> transfer = (Map<String, Object>) entries.get("transfer");
+        String fpName = (String) transfer.get("title");
+        if (StringUtils.isEmpty(fpName)) {
+            fpName = folderID;
+        }
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(fpName);
+        /* TODO: Add pagination if needed */
+        br.getPage("/transfer/" + folderID + "/files/preview?version=07-2020&limit=9");
+        entries = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        final List<Map<String, Object>> files = (List<Map<String, Object>>) entries.get("files");
+        for (final Map<String, Object> file : files) {
+            final DownloadLink link = this.createDownloadlink("https://" + this.getHost() + "/" + folderID + "#fileid=" + file.get("id").toString());
+            link.setFinalFileName(file.get("name").toString());
+            link.setVerifiedFileSize(((Number) file.get("size")).longValue());
+            link.setAvailable(true);
+            link.setProperty(FromsmashCom.PROPERTY_DIRECTURL, file.get("download").toString());
+            link._setFilePackage(fp);
+            decryptedLinks.add(link);
+        }
+        return decryptedLinks;
+    }
+}
---
+++ src/jd/plugins/hoster/FromsmashCom.java
@@ -0,0 +1,190 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.parser.UrlQuery;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.http.requests.PutRequest;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+import jd.plugins.decrypter.FromsmashComFolder;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class FromsmashCom extends PluginForHost {
+    public FromsmashCom(PluginWrapper wrapper) {
+        super(wrapper);
+        // this.enablePremium("");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://en.fromsmash.com/terms-policies";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "fromsmash.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([A-Za-z0-9\\-_]+)#fileid=([A-Za-z0-9\\-_]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME        = true;
+    private static final int     FREE_MAXCHUNKS     = 0;
+    private static final int     FREE_MAXDOWNLOADS  = 20;
+    public static final String   PROPERTY_DIRECTURL = "directurl";
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        if (link.getPluginPatternMatcher() != null) {
+            return this.getHost() + "://" + getFolderID(link) + "_" + getFileID(link);
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFolderID(final DownloadLink link) {
+        if (link.getPluginPatternMatcher() == null) {
+            return null;
+        } else {
+            return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+        }
+    }
+
+    private String getFileID(final DownloadLink link) {
+        if (link.getPluginPatternMatcher() == null) {
+            return null;
+        } else {
+            return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1);
+        }
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        final String directurl = link.getStringProperty(PROPERTY_DIRECTURL);
+        if (directurl != null) {
+            final String expireTimestampStr = UrlQuery.parse(directurl).get("Expires");
+            final long expireTimestamp = Long.parseLong(expireTimestampStr) * 1000;
+            if (expireTimestamp > System.currentTimeMillis()) {
+                /* Trust given timestamp */
+                return AvailableStatus.TRUE;
+            } else {
+                logger.info("Directurl needs to be refreshed");
+            }
+        }
+        logger.info("Obtaining fresh directurl");
+        final Browser brc = br.cloneBrowser();
+        final String token = FromsmashComFolder.getToken(this, brc);
+        brc.getHeaders().put("Authorization", "Bearer " + token);
+        FromsmashComFolder.prepBR(brc);
+        final PutRequest put = new PutRequest("https://transfer.eu-central-1.fromsmash.co/transfer/" + getFolderID(link) + "/urls?version=07-2020");
+        final String reqData = "{\"files\":[{\"id\":\"" + getFileID(link) + "\"}]}";
+        put.setPostBytes(reqData.getBytes());
+        brc.getPage(put);
+        if (brc.getHttpConnection().getResponseCode() == 404) {
+            /*
+             * E.g. {"code":404,"error":"Transfer <folderID> not found","requestId":"<someHash>","details":{"name":"Transfer","primary":
+             * "<folderID>"}}
+             */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final Map<String, Object> entries = JavaScriptEngineFactory.jsonToJavaMap(brc.toString());
+        /* Valid for 10 minutes, IP-independent! */
+        final String freshDirecturl = JavaScriptEngineFactory.walkJson(entries, "transfer/files/{0}/url").toString();
+        if (StringUtils.isEmpty(freshDirecturl)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        link.setProperty(PROPERTY_DIRECTURL, freshDirecturl);
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link);
+        final String dllink = link.getStringProperty(PROPERTY_DIRECTURL);
+        if (StringUtils.isEmpty(dllink)) {
+            /* This should never happen */
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, FREE_RESUME, FREE_MAXCHUNKS);
+        if (!this.looksLikeDownloadableContent(dl.getConnection())) {
+            try {
+                br.followConnection(true);
+            } catch (final IOException e) {
+                logger.log(e);
+            }
+            if (dl.getConnection().getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 5 * 60 * 1000l);
+            } else if (dl.getConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 5 * 60 * 1000l);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Final downloadurl did not lead to file");
+            }
+        }
+        dl.startDownload();
+    }
+
+    @Override
+    public boolean hasCaptcha(final DownloadLink link, final jd.plugins.Account acc) {
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
