484dda1bdd38a206ed46e68ea4d38b75ac835eb3
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-10-25 14:05:58 +0000

    GulfUpCom: -updated getMaxChunks -fixes thread 88887 XFileSharingProBasic: -updated handleDownload, set maxChunks to 1 when resume is disabled. I thought disabled resume will also disable chunks, CHECKME -refs thread 88887
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45173 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/GulfUpCom.java
+++ src/jd/plugins/hoster/GulfUpCom.java
@@ -18,13 +18,13 @@ package jd.plugins.hoster;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jdownloader.plugins.components.XFileSharingProBasic;
-
 import jd.PluginWrapper;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
 import jd.plugins.DownloadLink;
 import jd.plugins.HostPlugin;
+
+import org.jdownloader.plugins.components.XFileSharingProBasic;
 
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 public class GulfUpCom extends XFileSharingProBasic {
@@ -78,13 +78,13 @@ public class GulfUpCom extends XFileSharingProBasic {
     public int getMaxChunks(final Account account) {
         if (account != null && account.getType() == AccountType.FREE) {
             /* Free Account */
-            return 0;
+            return 1;
         } else if (account != null && account.getType() == AccountType.PREMIUM) {
             /* Premium account */
-            return 0;
+            return 1;
         } else {
             /* Free(anonymous) and unknown account type */
-            return 0;
+            return 1;
         }
     }
 
