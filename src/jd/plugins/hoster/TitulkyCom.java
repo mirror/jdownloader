7c4ee4153ad3caa0bd03900a7646ba3fc326dc93
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-11-16 17:27:11 +0000

    *General* - translation: slightly updated korean translation RE forum 89078
    *Plugins: New plugins or new websites that belong to existing scripts*
    - titulkycom closes #89970
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45217 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 21da4e045f68c48f21b730e6feb46f8fe7483da5

---
+++ src/jd/plugins/hoster/TitulkyCom.java
@@ -0,0 +1,227 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.AccountRequiredException;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class TitulkyCom extends PluginForHost {
+    public TitulkyCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.titulky.com/";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "titulky.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/idown\\.php\\?R=\\d+\\&titulky=(\\d+).*");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /* Connection stuff */
+    private static final boolean FREE_RESUME       = true;
+    private static final int     FREE_MAXCHUNKS    = 0;
+    private static final int     FREE_MAXDOWNLOADS = 20;
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    /** Use this in case we ever have a crawler for e.g.: https://www.titulky.com/SomeTitle-123456.htm */
+    public static final String CRAWLER_PRESET_TITLE = "crawler_preset_title";
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        if (link.hasProperty(CRAWLER_PRESET_TITLE)) {
+            link.setName(link.getStringProperty(CRAWLER_PRESET_TITLE) + ".srt");
+        } else if (!link.isNameSet()) {
+            /* Fallback */
+            link.setName(this.getFID(link) + ".srt");
+        }
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        br.getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        /* No filename/filesize given here */
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        handleDownload(link, FREE_RESUME, FREE_MAXCHUNKS, "free_directlink");
+    }
+
+    private void handleDownload(final DownloadLink link, final boolean resumable, final int maxchunks, final String directlinkproperty) throws Exception, PluginException {
+        if (!attemptStoredDownloadurlDownload(link, directlinkproperty)) {
+            requestFileInformation(link);
+            /* Captcha handling */
+            final Form dlform = br.getFormbyProperty("name", "downform");
+            final String captchaurl = getCaptchaURL(br);
+            if (dlform == null || captchaurl == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            final String code = this.getCaptchaCode(captchaurl, link);
+            dlform.put("downkod", Encoding.urlEncode(code));
+            br.submitForm(dlform);
+            /* Check for wrong captcha */
+            if (getCaptchaURL(br) != null) {
+                throw new PluginException(LinkStatus.ERROR_CAPTCHA);
+            } else if (br.containsHTML("(?i)Tyto titulky jsou úplně nové")) {
+                /* Check for premiumonly */
+                /* 2021-11-16: New items uploaded within the last 5 days can be downloaded by premium users only. */
+                throw new AccountRequiredException();
+            }
+            /*
+             * 2021-11-16: TODO: Add check for "daily downloadlimit reached": According to website, free users can download max. 25 items
+             * per day per IP.
+             */
+            /* Attempt download */
+            final String dllink = br.getRegex("(/idown\\.php\\?id=\\d+)").getMatch(0);
+            if (StringUtils.isEmpty(dllink)) {
+                logger.warning("Failed to find final downloadurl");
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            final String waitSecondsStr = br.getRegex("onload=\"CountDown\\((\\d+)\\)\"").getMatch(0);
+            if (waitSecondsStr != null) {
+                this.sleep(Long.parseLong(waitSecondsStr) * 1001l, link);
+            } else {
+                logger.warning("Failed to find pre-download-waittime");
+            }
+            dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, resumable, maxchunks);
+            if (!this.looksLikeDownloadableContent(dl.getConnection())) {
+                try {
+                    br.followConnection(true);
+                } catch (final IOException e) {
+                    logger.log(e);
+                }
+                if (dl.getConnection().getResponseCode() == 403) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 5 * 60 * 1000l);
+                } else if (dl.getConnection().getResponseCode() == 404) {
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 5 * 60 * 1000l);
+                } else {
+                    /*
+                     * 2021-11-16: Random errors like e.g. <h2>Odkaz ještě není funkční</h2>Pokračujte tudy na <a
+                     * href="https://www.titulky.com">titulky</a>
+                     */
+                    throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Unknown download error");
+                }
+            }
+            link.setProperty(directlinkproperty, dl.getConnection().getURL().toString());
+        }
+        dl.startDownload();
+    }
+
+    private String getCaptchaURL(final Browser br) {
+        return br.getRegex("(/captcha/captcha\\.php)").getMatch(0);
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        if (acc == null) {
+            /* no account, yes we can expect captcha */
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean attemptStoredDownloadurlDownload(final DownloadLink link, final String directlinkproperty) throws Exception {
+        final String url = link.getStringProperty(directlinkproperty);
+        if (StringUtils.isEmpty(url)) {
+            return false;
+        }
+        try {
+            final Browser brc = br.cloneBrowser();
+            dl = new jd.plugins.BrowserAdapter().openDownload(brc, link, url, FREE_RESUME, FREE_MAXCHUNKS);
+            if (this.looksLikeDownloadableContent(dl.getConnection())) {
+                return true;
+            } else {
+                brc.followConnection(true);
+                throw new IOException();
+            }
+        } catch (final Throwable e) {
+            logger.log(e);
+            try {
+                dl.getConnection().disconnect();
+            } catch (Throwable ignore) {
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
