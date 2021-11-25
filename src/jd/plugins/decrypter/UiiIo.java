d367d095156bca43b12269bba41124a57e1122ba
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-11-25 02:51:58 +0000

    *Plugins: Fixes/Changes/Maintenance* - [MightyScriptAdLinkFly] uiiio: added domain wordcountericu RE forum 89146 pornsocketcom: - added embed support - improved errorhandling - added support for sister-site outerporncom
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45232 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/UiiIo.java
+++ src/jd/plugins/decrypter/UiiIo.java
@@ -36,7 +36,7 @@ public class UiiIo extends MightyScriptAdLinkFly {
     public static List<String[]> getPluginDomains() {
         final List<String[]> ret = new ArrayList<String[]>();
         // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
-        ret.add(new String[] { "uii.io", "szs.pw", "passgen.icu" });
+        ret.add(new String[] { "uii.io", "szs.pw", "passgen.icu", "wordcounter.icu" });
         return ret;
     }
 
