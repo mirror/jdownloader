9981c9daa61f23dc7148beddde4090d37b87f4fb
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-12-01 02:51:24 +0000

    *Plugins: Fixes/Changes/Maintenance* - Try2LinkCom: fixed comment (this was added twice because mostafaz4 has added another version of the class written in lowercase ?? either way mostafaz4 your attempt is better than mine!) --> Fixed camelcase class conflict
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45253 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/Try2LinkCom.java
@@ -0,0 +1,54 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "try2link.com" }, urls = { "https?://(?:www\\.)?try2link\\.com/([A-Za-z0-9]+)" })
+public class Try2LinkCom extends MightyScriptAdLinkFly {
+    public Try2LinkCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    protected void handlePreCrawlProcess(final CryptedLink param, final ArrayList<DownloadLink> decryptedLinks) throws Exception {
+        param.setCryptedUrl(param.getCryptedUrl().replaceFirst("http://", "https://"));
+        br.setFollowRedirects(false);
+        /* Pre-set Referer to skip multiple ad pages e.g. try2link.com -> forex-gold.net -> try2link.com */
+        getPage(param.getCryptedUrl());
+        String location = br.getRequest().getLocation();
+        String timestampBase64 = new Regex(location, "d=(.*)").getMatch(0);
+        String timestamp = Encoding.Base64Decode(timestampBase64);
+        br.setFollowRedirects(true);
+        getPage(param.getCryptedUrl() + "/?d=" + timestamp);
+        if (this.regexAppVars(this.br) == null) {
+            logger.warning("Possible crawler failure...");
+        }
+        /* Now continue with parent class code (requires captcha + waittime) */
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        return super.decryptIt(param, progress);
+    }
+}
