50bc789a1d0c24a90df17c4405089f7b5ddc6e21
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-12-03 16:17:22 +0000

    *Plugins: Fixes/Changes/Maintenance* - KernelVideoSharingComV2: some refactoring and added API support: needs more work but is already working fine
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [KVS]: vxxxcom RE forum 89204
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45261 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/VxxxCom.java
@@ -0,0 +1,79 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.HostPlugin;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class VxxxCom extends KernelVideoSharingComV2 {
+    public VxxxCom(PluginWrapper wrapper) {
+        super(wrapper);
+        // this.enablePremium("");
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.test.com/help/privacy";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "vxxx.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?:video|embed)-\\d+");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    protected String getFUIDFromURL(final String url) {
+        if (url == null) {
+            return null;
+        } else {
+            return new Regex(url, "(\\d+)$").getMatch(0);
+        }
+    }
+
+    @Override
+    protected boolean useAPIAvailablecheck() {
+        return true;
+    }
+
+    @Override
+    protected boolean useAPIGetDllink() {
+        return true;
+    }
+}
