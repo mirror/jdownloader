207eb551494dc965f0aec51987eba04848823f85
mostafaz4 <mostafaz4@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-12-19 11:50:39 +0000

    [Add] support for arabplus2.co
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45284 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 852ac79e2375e9f34260f2c49797ec753321f414

---
+++ src/jd/plugins/decrypter/Arabplus2Co.java
@@ -0,0 +1,42 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "arabplus2.co" }, urls = { "https?://(?:www\\.)?arabplus2\\.co/([A-Za-z0-9]+)" })
+public class Arabplus2Co extends MightyScriptAdLinkFly {
+    public Arabplus2Co(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        br.setCookie(br.getHost(), "iwashere", "yes");
+        br.getPage(param.getCryptedUrl());
+        return super.decryptIt(param, progress);
+    }
+
+    @Override
+    protected String getSpecialReferer() {
+        return "http://www.figop.com/";
+    }
+}
