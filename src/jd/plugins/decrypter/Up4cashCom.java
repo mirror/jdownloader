1142a176a44b8f8767eeef026ffd138dad19e4b5
mostafaz4 <mostafaz4@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-12-24 18:16:56 +0000

    [Added] up4cash.com decrypter plugin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45296 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/decrypter/Up4cashCom.java
@@ -0,0 +1,41 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "up4cash.com" }, urls = { "https?://(?:www\\.)?up4cash\\.com/([A-Za-z0-9]+)" })
+public class Up4cashCom extends MightyScriptAdLinkFly {
+    public Up4cashCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        return super.decryptIt(param, progress);
+    }
+
+    @Override
+    protected String getSpecialReferer() {
+        /* Pre-set Referer to skip multiple ad pages e.g. bestcash2020.com -> e3raftech.online -> bestcash2020.com */
+        return "https://www.gamestation22.com/";
+    }
+}
