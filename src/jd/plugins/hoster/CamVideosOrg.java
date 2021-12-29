3abe78a60a0bd0c65c40653883c5131119a2d489
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2021-12-29 16:36:37 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45304 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: dcd683f4211d040938e8510cc0f4995babd2a10d

---
+++ src/jd/plugins/hoster/CamVideosOrg.java
@@ -0,0 +1,44 @@
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jd.PluginWrapper;
+import jd.plugins.HostPlugin;
+
+import org.appwork.utils.StringUtils;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class CamVideosOrg extends KernelVideoSharingComV2 {
+    public CamVideosOrg(final PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "camvideos.org" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return KernelVideoSharingComV2.buildAnnotationUrlsDefaultVideosPattern(getPluginDomains());
+    }
+
+    @Override
+    protected String generateContentURL(final String fuid, final String urlTitle) {
+        if (StringUtils.isEmpty(fuid) || StringUtils.isEmpty(urlTitle)) {
+            return null;
+        }
+        return "https://www." + this.getHost() + "/" + fuid + "/" + urlTitle + "/";
+    }
+}
