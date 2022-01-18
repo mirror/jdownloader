e38bf7aa2889d98b113124c2901dc5737b8aee07
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-01-18 12:35:47 +0000

    *Plugins: Fixes/Changes/Maintenance* - ardmediathek: added limited support for tagesschaude; existing codebase is very bad RE: me
    *Plugins: New plugins or new websites that belong to existing scripts*
    - gettrcom RE forum 89541
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45371 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/jd/plugins/hoster/GettrCom.java
@@ -0,0 +1,178 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class GettrCom extends PluginForHost {
+    public GettrCom(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://www.gettr.com/terms";
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "gettr.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/streaming/([a-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    /* Connection stuff */
+    private static final int    FREE_MAXDOWNLOADS  = 20;
+    private static final String PROPERTY_DIRECTURL = "directlink";
+    private static final String PROPERTY_TITLE     = "title";
+    private static final String PROPERTY_DATE      = "date";
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        if (!link.isNameSet()) {
+            /* Fallback */
+            link.setName(this.getFID(link) + ".mp4");
+        }
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        final Browser brc = this.br.cloneBrowser();
+        brc.setAllowedResponseCodes(400);
+        brc.getHeaders().put("Origin", "https://www." + this.getHost());
+        brc.getHeaders().put("Referer", "https://www." + this.getHost());
+        brc.getHeaders().put("ver", "2.6.0");
+        brc.getHeaders().put("x-app-auth", "{\"user\": null, \"token\": null}");
+        brc.postPage("https://api.gettr.com/u/live/join/" + this.getFID(link), "");
+        if (brc.getHttpConnection().getResponseCode() == 400) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (brc.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final Map<String, Object> root = JSonStorage.restoreFromString(brc.toString(), TypeRef.HASHMAP);
+        final Map<String, Object> result = (Map<String, Object>) root.get("result");
+        final Map<String, Object> videoInfo = (Map<String, Object>) result.get("postData");
+        final Map<String, Object> streamInfo = (Map<String, Object>) result.get("broadcast");
+        if (streamInfo.get("isRemoved") == Boolean.TRUE) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final long timestamp = ((Number) result.get("timestamp")).longValue();
+        final String dateFormatted = new SimpleDateFormat("yyyy-MM-dd").format(new Date(timestamp));
+        final String title = (String) videoInfo.get("ttl");
+        final String description = (String) videoInfo.get("dsc");
+        if (!StringUtils.isEmpty(description) && StringUtils.isEmpty(link.getComment())) {
+            link.setComment(description);
+        }
+        link.setFinalFileName(dateFormatted + "_" + title + ".mp4");
+        final String directurl = (String) streamInfo.get("url");
+        if (!StringUtils.isEmpty(directurl)) {
+            link.setProperty(PROPERTY_DIRECTURL, directurl);
+        }
+        link.setProperty(PROPERTY_DATE, dateFormatted);
+        link.setProperty(PROPERTY_TITLE, title);
+        if (streamInfo.get("isLive") == Boolean.TRUE) {
+            throw new PluginException(LinkStatus.ERROR_FATAL, "Livestreams are not supported");
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        handleDownload(link);
+    }
+
+    private void handleDownload(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link);
+        final String hlsMaster = link.getStringProperty(PROPERTY_DIRECTURL);
+        if (hlsMaster == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getPage(hlsMaster);
+        final HlsContainer hlsbest = HlsContainer.findBestVideoByBandwidth(HlsContainer.getHlsQualities(this.br));
+        checkFFmpeg(link, "Download a HLS Stream");
+        dl = new HLSDownloader(link, br, hlsbest.getDownloadurl());
+        dl.startDownload();
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return FREE_MAXDOWNLOADS;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
---
+++ src/org/jdownloader/plugins/components/config/TagesschauDeConfig.java
@@ -0,0 +1,8 @@
+package org.jdownloader.plugins.components.config;
+
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "tagesschau.de", type = Type.CRAWLER)
+public interface TagesschauDeConfig extends ArdConfigInterface {
+}
