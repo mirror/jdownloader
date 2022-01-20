708ce535188840899e8dbafd3b0c7b5e0726bc46
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-01-20 11:04:11 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45387 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/plugins/config/PluginJsonConfig.java
+++ src/org/jdownloader/plugins/config/PluginJsonConfig.java
@@ -33,7 +33,6 @@ import org.appwork.utils.Application;
 import org.appwork.utils.DebugMode;
 import org.appwork.utils.StringUtils;
 import org.appwork.utils.swing.dialog.Dialog;
-import org.jdownloader.logging.LogController;
 import org.jdownloader.plugins.controller.LazyPlugin;
 import org.jdownloader.plugins.controller.PluginClassLoader.PluginClassLoaderChild;
 import org.jdownloader.plugins.controller.crawler.CrawlerPluginController;
@@ -45,12 +44,12 @@ public class PluginJsonConfig {
     private static final WeakHashMap<ClassLoader, HashMap<String, WeakReference<ConfigInterface>>> CONFIG_CACHE  = new WeakHashMap<ClassLoader, HashMap<String, WeakReference<ConfigInterface>>>();
     private static final HashMap<String, JsonKeyValueStorage>                                      STORAGE_CACHE = new HashMap<String, JsonKeyValueStorage>();
     protected static final DelayedRunnable                                                         SAVEDELAYER   = new DelayedRunnable(5000, 30000) {
-        @Override
-        public void delayedrun() {
-            saveAll();
-            cleanup();
-        }
-    };
+                                                                                                                     @Override
+                                                                                                                     public void delayedrun() {
+                                                                                                                         saveAll();
+                                                                                                                         cleanup();
+                                                                                                                     }
+                                                                                                                 };
     private final static boolean                                                                   DEBUG         = false;
     static {
         final File pluginsFolder = Application.getResource("cfg/plugins/");
@@ -161,15 +160,6 @@ public class PluginJsonConfig {
                 }
             } else {
                 throw new WTFException("Bad Config Interface Definition. " + configInterface.getName() + ". @PluginHost(\"domain.de\") or    public Class<? extends UsenetConfigInterface> getConfigInterface() {... is missing");
-            }
-        }
-        if (DebugMode.TRUE_IN_IDE_ELSE_FALSE && lazyPlugin != null) {
-            final org.jdownloader.plugins.config.Type pluginType = lazyPlugin instanceof LazyCrawlerPlugin ? org.jdownloader.plugins.config.Type.CRAWLER : org.jdownloader.plugins.config.Type.HOSTER;
-            if (pluginType != type) {
-                LogController.CL(true).log(new Exception("Please check:" + configInterface + "|type missmatch:" + type + "!=" + pluginType));
-            }
-            if (!StringUtils.equals(host, lazyPlugin.getDisplayName())) {
-                LogController.CL(true).log(new Exception("Please check:" + configInterface + "|host missmatch:" + host + "!=" + lazyPlugin.getDisplayName()));
             }
         }
         String ID = JsonConfig.getStorageName(configInterface);
