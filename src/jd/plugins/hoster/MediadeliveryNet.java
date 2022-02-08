5e129f121e330a1b8c757627deb3e16ad2e28c7f
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-02-08 15:49:31 +0000

    *Plugins: Fixes/Changes/Maintenance* - DoubleJNetAu: applied cheap fix for domain change RE forum 89701
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [XFS] upelbarqnet RE forum 89713
    - mediadeliverynet RE forum 89314
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45445 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 12fc0bf735221e4c561eb01bdba2efacc08d2450

---
+++ src/jd/plugins/hoster/MediadeliveryNet.java
@@ -0,0 +1,149 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2017  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.plugins.components.antiDDoSForHost;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+
+import jd.PluginWrapper;
+import jd.parser.Regex;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class MediadeliveryNet extends antiDDoSForHost {
+    public MediadeliveryNet(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+    /* DEV NOTES */
+    // Tags: Porn plugin
+    // other: This plugin mostly handles embedded content from porn3dx.com.
+
+    /* Connection stuff */
+    private static final int   free_maxdownloads        = -1;
+    public static final String PROPERTY_AUTHOR          = "user";
+    public static final String PROPERTY_TITLE           = "title";
+    public static final String PROPERTY_POSITION        = "position";
+    public static final String PROPERTY_PORN3DX_POST_ID = "porn3dx_post_id";
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "mediadelivery.net" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://iframe\\." + buildHostsPatternPart(domains) + "/embed/(\\d+/[a-f0-9\\-]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://mediadelivery.net/";
+    }
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String linkid = getFID(link);
+        if (linkid != null) {
+            return this.getHost() + "://" + linkid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        setFilename(link);
+        if (!link.isNameSet()) {
+            /* Fallback */
+            link.setFinalFileName(this.getFID(link) + ".mp4");
+        }
+        this.setBrowserExclusive();
+        br.setFollowRedirects(true);
+        getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    public static void setFilename(final DownloadLink link) {
+        final String author = link.getStringProperty(PROPERTY_AUTHOR);
+        final String title = link.getStringProperty(PROPERTY_TITLE);
+        final int position = link.getIntegerProperty(PROPERTY_POSITION, -1);
+        final String porn3dxPostID = link.getStringProperty(PROPERTY_PORN3DX_POST_ID);
+        if (porn3dxPostID != null && author != null && title != null && position != -1) {
+            link.setFinalFileName(author + "_ " + porn3dxPostID + "_" + title + "_" + position + ".mp4");
+        }
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        final String hlsMaster = br.getRegex("\"(https?://[^\"]+playlist\\.m3u8)\"").getMatch(0);
+        if (StringUtils.isEmpty(hlsMaster)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.getPage(hlsMaster);
+        final HlsContainer hlsbest = HlsContainer.findBestVideoByBandwidth(HlsContainer.getHlsQualities(this.br));
+        checkFFmpeg(link, "Download a HLS Stream");
+        dl = new HLSDownloader(link, br, hlsbest.getDownloadurl());
+        dl.startDownload();
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return free_maxdownloads;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetPluginGlobals() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
