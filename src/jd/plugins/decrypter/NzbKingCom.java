c9c16e41f9856e2b43cfd50af69ec4676580cd05
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-02-20 23:57:56 +0000

    *Plugins: Fixes/Changes/Maintenance* - nzbkingcom: updated pattern and added offline errorhandling
    *Plugins: New plugins or new websites that belong to existing scripts*
    - nzbindexcom: added crawler which crawls up to 25 search results
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45470 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: ce1fb21fc93b14981887579f567b01936465aecd

--- src/jd/plugins/decrypter/NzbKingCom.java
+++ src/jd/plugins/decrypter/NzbKingCom.java
@@ -18,11 +18,13 @@ import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
 import jd.plugins.Plugin;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.components.NZBSAXHandler;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "nzbking.com" }, urls = { "https?://[\\w\\.]*nzbking.com/details(:|%3a)[0-9a-zA-Z]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "nzbking.com" }, urls = { "https?://(?:www\\.)?nzbking\\.com/details(?::|%3a)[a-f0-9]{24}" })
 public class NzbKingCom extends PluginForDecrypt {
     public NzbKingCom(PluginWrapper wrapper) {
         super(wrapper);
@@ -40,46 +42,52 @@ public class NzbKingCom extends PluginForDecrypt {
     @Override
     public ArrayList<DownloadLink> decryptIt(final CryptedLink param, final ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        br.setLoadLimit(Integer.MAX_VALUE);
         br.setFollowRedirects(true);
         URLConnectionAdapter con = null;
         File nzbFile = null;
         try {
             br.getPage(param.getCryptedUrl());
+            if (br.getHttpConnection().getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            } else if (br.containsHTML("<h1>Nothing found")) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
             final String title = br.getRegex("<div class='post-detail-subject'[^>]*>([^<]+)</div>").getMatch(0);
             final Form form = br.getFormbyAction("/nzb/");
             final Request request = br.createFormRequest(form);
             request.getHeaders().put("Accept-Encoding", "identity");
+            /* Prevent Exception for big NZB files. */
+            br.setLoadLimit(Integer.MAX_VALUE);
             con = br.openRequestConnection(request);
-            if (con.isOK()) {
-                ret.addAll(NZBSAXHandler.parseNZB(con.getInputStream()));
-                final String nzbFilename = Plugin.getFileNameFromHeader(con);
-                final Regex nzbCommonFilenameScheme = new Regex(nzbFilename, NZB.PATTERN_COMMON_FILENAME_SCHEME);
-                if (nzbFilename != null && nzbCommonFilenameScheme.matches()) {
-                    if (nzbCommonFilenameScheme.matches()) {
-                        /* Set extract-password and package name by information grabbed inside .nzb filename. */
-                        archiveInfo = new ArchiveInfo();
-                        archiveInfo.addExtractionPassword(nzbCommonFilenameScheme.getMatch(1));
-                        final FilePackage fp = FilePackage.getInstance();
-                        fp.setName(nzbCommonFilenameScheme.getMatch(0));
-                        fp.addLinks(ret);
-                    } else {
-                        final FilePackage fp = FilePackage.getInstance();
-                        fp.setName(nzbFilename);
-                        fp.addLinks(ret);
-                    }
-                } else if (title != null) {
+            if (!con.isOK()) {
+                br.followConnection();
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            ret.addAll(NZBSAXHandler.parseNZB(con.getInputStream()));
+            final String nzbFilename = Plugin.getFileNameFromHeader(con);
+            final Regex nzbCommonFilenameScheme = new Regex(nzbFilename, NZB.PATTERN_COMMON_FILENAME_SCHEME);
+            if (nzbFilename != null && nzbCommonFilenameScheme.matches()) {
+                if (nzbCommonFilenameScheme.matches()) {
+                    /* Set extract-password and package name by information grabbed inside .nzb filename. */
+                    final ArchiveInfo archiveInfo = new ArchiveInfo();
+                    archiveInfo.addExtractionPassword(nzbCommonFilenameScheme.getMatch(1));
                     final FilePackage fp = FilePackage.getInstance();
-                    fp.setName(Encoding.htmlDecode(title).trim());
+                    fp.setName(nzbCommonFilenameScheme.getMatch(0));
                     fp.addLinks(ret);
-                } else if (nzbFilename != null) {
-                    /* Last chance: Use filename as packagename. */
+                } else {
                     final FilePackage fp = FilePackage.getInstance();
                     fp.setName(nzbFilename);
                     fp.addLinks(ret);
                 }
-            } else {
-                br.followConnection();
+            } else if (title != null) {
+                final FilePackage fp = FilePackage.getInstance();
+                fp.setName(Encoding.htmlDecode(title).trim());
+                fp.addLinks(ret);
+            } else if (nzbFilename != null) {
+                /* Last chance: Use filename as packagename. */
+                final FilePackage fp = FilePackage.getInstance();
+                fp.setName(nzbFilename);
+                fp.addLinks(ret);
             }
         } finally {
             if (con != null) {
