e232b1865ffe2616e736207d35d7f9dd7c0695ec
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-02-21 17:07:09 +0000

    *General* - DownloadLink: added wrapper function to get/set value of property RELATIVE_DOWNLOAD_FOLDER_PATH
    *Plugins: Fixes/Changes/Maintenance*
    - GenericHTTPDirectoryIndexCrawler: set correct path even for single directlink items
    - HighWayMeFolder: override getCurrentDirectoryPath to set correct path for single items
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45476 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: a5cb37064f279f52756b303df8622f89ff39cec1

--- src/jd/plugins/DownloadLink.java
+++ src/jd/plugins/DownloadLink.java
@@ -30,19 +30,6 @@ import java.util.Locale;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.regex.Pattern;
 
-import jd.config.Property;
-import jd.controlling.downloadcontroller.DownloadLinkCandidate;
-import jd.controlling.downloadcontroller.DownloadWatchDog;
-import jd.controlling.downloadcontroller.HistoryEntry;
-import jd.controlling.downloadcontroller.SingleDownloadController;
-import jd.controlling.linkcrawler.CheckableLink;
-import jd.controlling.packagecontroller.AbstractNodeNotifier;
-import jd.controlling.packagecontroller.AbstractPackageChildrenNode;
-import jd.plugins.DownloadLinkDatabindingInterface.Key;
-import jd.plugins.download.DownloadInterface;
-import jd.plugins.download.HashInfo;
-import jd.plugins.download.HashInfo.TYPE;
-
 import org.appwork.exceptions.WTFException;
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
@@ -71,6 +58,19 @@ import org.jdownloader.plugins.SkipReason;
 import org.jdownloader.settings.GeneralSettings;
 import org.jdownloader.settings.staticreferences.CFG_GENERAL;
 
+import jd.config.Property;
+import jd.controlling.downloadcontroller.DownloadLinkCandidate;
+import jd.controlling.downloadcontroller.DownloadWatchDog;
+import jd.controlling.downloadcontroller.HistoryEntry;
+import jd.controlling.downloadcontroller.SingleDownloadController;
+import jd.controlling.linkcrawler.CheckableLink;
+import jd.controlling.packagecontroller.AbstractNodeNotifier;
+import jd.controlling.packagecontroller.AbstractPackageChildrenNode;
+import jd.plugins.DownloadLinkDatabindingInterface.Key;
+import jd.plugins.download.DownloadInterface;
+import jd.plugins.download.HashInfo;
+import jd.plugins.download.HashInfo.TYPE;
+
 /**
  * Hier werden alle notwendigen Informationen zu einem einzelnen Download festgehalten. Die Informationen werden dann in einer Tabelle
  * dargestellt
@@ -83,6 +83,7 @@ public class DownloadLink extends Property implements Serializable, AbstractPack
         FALSE(_GUI.T.linkgrabber_onlinestatus_offline()),
         UNCHECKABLE(_GUI.T.linkgrabber_onlinestatus_uncheckable()),
         TRUE(_GUI.T.linkgrabber_onlinestatus_online());
+
         private final String exp;
 
         private AvailableStatus(String exp) {
@@ -189,6 +190,16 @@ public class DownloadLink extends Property implements Serializable, AbstractPack
                 this.partOfAnArchive = archivePart;
             }
         }
+    }
+
+    /** Use this to set paths of online cloud services or http directories. */
+    public void setRelativeDownloadFolderPath(final String path) {
+        this.setProperty(RELATIVE_DOWNLOAD_FOLDER_PATH, path);
+    }
+
+    /** Use this to get paths of online cloud services or http directories. */
+    public String getRelativeDownloadFolderPath() {
+        return this.getStringProperty(RELATIVE_DOWNLOAD_FOLDER_PATH);
     }
 
     public long getJobID() {
@@ -965,7 +976,7 @@ public class DownloadLink extends Property implements Serializable, AbstractPack
     /*
      * Gibt zurueck ob Dieser Link schon auf verfuegbarkeit getestet wurde.+ Diese FUnktion fuehrt keinen!! Check durch. Sie prueft nur ob
      * schon geprueft worden ist. anschiessend kann mit isAvailable() die verfuegbarkeit ueberprueft werden
-     * 
+     *
      * @return Link wurde schon getestet (true) nicht getestet(false)
      */
     public boolean isAvailabilityStatusChecked() {
@@ -1580,8 +1591,9 @@ public class DownloadLink extends Property implements Serializable, AbstractPack
     }
 
     /**
-     * Returns whether or not a download password is required to download this item. 7 </br> In most of all cases, this cannot be known
-     * until downloads are started but in some instances this can be set e.g. during folder crawling already.
+     * Returns whether or not a download password is required to download this item. 7 </br>
+     * In most of all cases, this cannot be known until downloads are started but in some instances this can be set e.g. during folder
+     * crawling already.
      */
     public boolean isPasswordProtected() {
         return this.getBooleanProperty(PROPERTY_PASSWORD_PROTECTED, false);
