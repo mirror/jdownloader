70658c2d56cbcc49b1dc771842fec717539cbb26
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-04-01 13:44:49 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45764 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/controlling/proxy/ProxyController.java
+++ src/jd/controlling/proxy/ProxyController.java
@@ -113,11 +113,11 @@ import com.btr.proxy.util.Logger.LogLevel;
 //import com.btr.proxy.util.Logger.LogLevel;
 public class ProxyController implements ProxySelectorInterface {
     public static final URLStreamHandler SOCKETURLSTREAMHANDLER = new URLStreamHandler() {
-        @Override
-        protected URLConnection openConnection(URL u) throws IOException {
-            throw new IOException("not implemented");
-        }
-    };
+                                                                    @Override
+                                                                    protected URLConnection openConnection(URL u) throws IOException {
+                                                                        throw new IOException("not implemented");
+                                                                    }
+                                                                };
     private static final ProxyController INSTANCE               = new ProxyController();
 
     public static final ProxyController getInstance() {
@@ -129,21 +129,21 @@ public class ProxyController implements ProxySelectorInterface {
     private final InternetConnectionSettings                                config;
     private final LogSource                                                 logger;
     private final Queue                                                     QUEUE           = new Queue(getClass().getName()) {
-        @Override
-        public void killQueue() {
-            LogController.CL().log(new Throwable("YOU CANNOT KILL ME!"));
-            /*
-             * this queue can ' t be killed
-             */
-        }
-    };
+                                                                                                @Override
+                                                                                                public void killQueue() {
+                                                                                                    LogController.CL().log(new Throwable("YOU CANNOT KILL ME!"));
+                                                                                                    /*
+                                                                                                     * this queue can ' t be killed
+                                                                                                     */
+                                                                                                }
+                                                                                            };
     private final ConfigEventSender<Object>                                 customProxyListEventSender;
     private final EventSuppressor<ConfigEvent>                              eventSuppressor = new EventSuppressor<ConfigEvent>() {
-        @Override
-        public boolean suppressEvent(ConfigEvent eventType) {
-            return true;
-        }
-    };
+                                                                                                @Override
+                                                                                                public boolean suppressEvent(ConfigEvent eventType) {
+                                                                                                    return true;
+                                                                                                }
+                                                                                            };
 
     public Queue getQUEUE() {
         return QUEUE;
@@ -195,16 +195,16 @@ public class ProxyController implements ProxySelectorInterface {
         });
         getEventSender().addListener(new DefaultEventListener<ProxyEvent<AbstractProxySelectorImpl>>() {
             final DelayedRunnable asyncSaving = new DelayedRunnable(5000l, 60000l) {
-                @Override
-                public void delayedrun() {
-                    ProxyController.this.saveProxySettings();
-                }
+                                                  @Override
+                                                  public void delayedrun() {
+                                                      ProxyController.this.saveProxySettings();
+                                                  }
 
-                @Override
-                public String getID() {
-                    return "ProxyController";
-                }
-            };
+                                                  @Override
+                                                  public String getID() {
+                                                      return "ProxyController";
+                                                  }
+                                              };
 
             @Override
             public void onEvent(final ProxyEvent<AbstractProxySelectorImpl> event) {
@@ -249,8 +249,6 @@ public class ProxyController implements ProxySelectorInterface {
 
     private void updateProxyFilterList(final AbstractProxySelectorImpl proxy, PluginFinder pluginFinder) {
         final FilterList filter = proxy.getFilter();
-        // redgifs.com
-        // vielleicht auf accountinfo handling umschreiben
         if (filter != null && pluginFinder != null) {
             final String[] entries = filter.getEntries();
             final HashMap<String, String> mapping = new HashMap<String, String>();
