873528db55551c3dc281c80e527d55ada71f959b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-05-10 15:09:19 +0000

    *Plugins: Fixes/Changes/Maintenance* - AbstractPastebinCrawler: some more work on this refs #90043 - JustPasteIt: extends AbstractPastebinCrawler
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@45964 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/JustPasteIt.java
+++ src/jd/plugins/decrypter/JustPasteIt.java
@@ -1,5 +1,5 @@
 //    jDownloader - Downloadmanager
-//    Copyright (C) 2015  JD-Team support@jdownloader.org
+//    Copyright (C) 2009  JD-Team support@jdownloader.org
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
@@ -15,65 +15,104 @@
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.decrypter;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Set;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.formatter.TimeFormatter;
 
 import jd.PluginWrapper;
-import jd.controlling.ProgressController;
-import jd.parser.html.HTMLParser;
+import jd.http.Browser;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
-import jd.plugins.DownloadLink;
-import jd.plugins.PluginForDecrypt;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 
-import org.jdownloader.controlling.PasswordUtils;
-
-/**
- *
- * @version raz_Template-pastebin-201503051556
- * @author raztoki
- */
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "justpaste.it" }, urls = { "https?://(?:www\\.)?(?:justpaste\\.it/[A-Za-z0-9\\-_]+|jpst\\.it/[A-Za-z0-9]+)" })
-public class JustPasteIt extends PluginForDecrypt {
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = {}, urls = {})
+public class JustPasteIt extends AbstractPastebinCrawler {
     public JustPasteIt(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    /* DEV NOTES */
-    // Tags: pastebin
-    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        final String parameter = param.toString();
+    @Override
+    String getFID(final String url) {
+        return new Regex(url, this.getSupportedLinks()).getMatch(0);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "justpaste.it", "jpst.it" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([A-Za-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    protected String getPastebinText(final Browser br) {
+        return br.getRegex("<div[^>]+id=\"articleContent\"[^>]*>(.*?)</div>").getMatch(0);
+    }
+
+    @Override
+    protected void preProcess(final CryptedLink param) throws IOException, PluginException {
+        prepBR(br);
+        br.getPage(param.getCryptedUrl());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br.getHttpConnection().getResponseCode() == 451) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br.containsHTML("\"isCaptchaRequired\":true")) {
+            logger.warning("Captcha is not supported");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+    }
+
+    @Override
+    protected PastebinMetadata crawlMetadata(final CryptedLink param, final Browser br) {
+        final PastebinMetadata metadata = super.crawlMetadata(param, br);
+        final String json = br.getRegex("window\\.barOptions\\s*=\\s*(\\{.*?\\});").getMatch(0);
+        final String title = br.getRegex("class=\"articleFirstTitle\"[^>]*>([^<>\"]+)</h1>").getMatch(0);
+        if (title != null) {
+            metadata.setTitle(title);
+        }
+        try {
+            final Map<String, Object> entries = JSonStorage.restoreFromString(json, TypeRef.HASHMAP);
+            final String createdText = entries.get("createdText").toString();
+            metadata.setDate(new Date(TimeFormatter.getMilliSeconds(createdText.substring(0, createdText.lastIndexOf(":")), "yyyy-MM-dd'T'HH:mm:ss", Locale.ENGLISH)));
+        } catch (final Throwable e) {
+            logger.log(e);
+        }
+        return metadata;
+    }
+
+    public static Browser prepBR(final Browser br) {
         br.setLoadLimit(3 * br.getLoadLimit());
         br.setAllowedResponseCodes(451);
-        br.setFollowRedirects(true);
-        br.getPage(parameter);
-        /* Error handling */
-        if (br.containsHTML("Page Not Found") || br.containsHTML("<strong>\\s*Article unavailable\\s*</") || br.getHttpConnection() == null || br.getHttpConnection().getResponseCode() == 404 || br.getHttpConnection().getResponseCode() == 451) {
-            decryptedLinks.add(this.createOfflinelink(parameter));
-            return decryptedLinks;
-        }
-        final String plaintxt = br.getRegex("<div[^>]+id=\"articleContent\"[^>]*>(.*?)</div>").getMatch(0);
-        if (plaintxt == null) {
-            logger.info("Could not find 'plaintxt' : " + parameter);
-            return decryptedLinks;
-        }
-        final Set<String> pws = PasswordUtils.getPasswords(plaintxt);
-        final String[] links = HTMLParser.getHttpLinks(plaintxt, "");
-        if (links == null || links.length == 0) {
-            logger.info("Found no links[] from 'plaintxt' : " + parameter);
-            return decryptedLinks;
-        }
-        /* avoid recursion */
-        for (final String link : links) {
-            if (!this.canHandle(link)) {
-                final DownloadLink dl = createDownloadlink(link);
-                if (pws != null && pws.size() > 0) {
-                    dl.setSourcePluginPasswordList(new ArrayList<String>(pws));
-                }
-                decryptedLinks.add(dl);
-            }
-        }
-        return decryptedLinks;
+        return br;
     }
 }
