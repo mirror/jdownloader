54eb6f09a44ee195e0c539fc5800c22cdf7f074b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-06-07 15:20:08 +0000

    *Plugins: Fixes/Changes/Maintenance* - vk: Lowered minimum configurable wait for pagination from 1000ms to 500ms (default remains 1000ms) - ShrinkServiceIt: minor changes refs #90124
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46152 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/ShrinkServiceIt.java
+++ src/jd/plugins/decrypter/ShrinkServiceIt.java
@@ -16,15 +16,22 @@
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
+import java.util.Map;
 
+import org.appwork.utils.DebugMode;
 import org.appwork.utils.StringUtils;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.components.PluginJSonUtils;
 
@@ -34,14 +41,31 @@ public class ShrinkServiceIt extends PluginForDecrypt {
         super(wrapper);
     }
 
-    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+    private final String API_BASE = "https://www.shrink-service.it/v3/api";
+
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
         br.setFollowRedirects(true);
         br.getPage(param.getCryptedUrl());
         if (br.getHttpConnection().getResponseCode() == 404) {
-            decryptedLinks.add(this.createOfflinelink(param.getCryptedUrl()));
-            return decryptedLinks;
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br._getURL().getPath().equals("/HTTP404.html")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
+        /* 2022-06-07: Plugin broken */
+        if (!DebugMode.TRUE_IN_IDE_ELSE_FALSE) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String cookie_bypass_v1 = br.getCookie(br.getHost(), "cookie_bypass_v1", Cookies.NOTDELETEDPATTERN);
+        if (cookie_bypass_v1 == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        br.postPage(API_BASE + "/prototype/init", "req=init&uri=" + Encoding.urlEncode(br.getURL()) + "&cookie_bypass_v1=" + Encoding.urlEncode(cookie_bypass_v1));
+        final Map<String, Object> entries = JavaScriptEngineFactory.jsonToJavaMap(br.toString());
+        final Map<String, Object> settings = (Map<String, Object>) entries.get("settings");
+        final String rcKey = settings.get("rv2pk").toString();
+        final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br, rcKey).getToken();
+        br.postPage("/recaptcha.php", "v=v3&response=" + Encoding.urlEncode(recaptchaV2Response));
         String finallink = br.getRegex("<input type='hidden'[^<>\">]*?value='([^<>\"']*?)'>").getMatch(0);
         if (StringUtils.isEmpty(finallink)) {
             /* 2021-12-10: adshnk.com --> Skips captcha and waittime */
@@ -52,8 +76,7 @@ public class ShrinkServiceIt extends PluginForDecrypt {
             return null;
         } else if (finallink.equals("")) {
             /* Empty field --> Offline/invalid url */
-            decryptedLinks.add(this.createOfflinelink(param.getCryptedUrl()));
-            return decryptedLinks;
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         finallink = Encoding.htmlOnlyDecode(finallink);
         finallink = finallink.replace("&sol;", "/");
