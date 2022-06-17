1790cdaac184c28a2c882234102b28ad1581d8a6
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-06-17 17:20:40 +0000

    LogSendAction: -add links to help article and privacy policy -refs #85420 -refs #76001
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46243 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c948b4524a4857c440793cb299414033effa7b02

--- src/org/jdownloader/gui/mainmenu/action/LogSendAction.java
+++ src/org/jdownloader/gui/mainmenu/action/LogSendAction.java
@@ -1,25 +1,49 @@
 package org.jdownloader.gui.mainmenu.action;
 
 import java.awt.event.ActionEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
 
+import jd.gui.swing.components.linkbutton.JLink;
+import jd.gui.swing.jdgui.menu.actions.sendlogs.LogAction;
+
+import org.appwork.swing.MigPanel;
+import org.appwork.utils.logging2.sendlogs.LogFolder;
+import org.appwork.utils.logging2.sendlogs.SendLogDialog;
 import org.jdownloader.controlling.contextmenu.CustomizableAppAction;
 import org.jdownloader.gui.IconKey;
 import org.jdownloader.gui.translate._GUI;
-
-import jd.gui.swing.jdgui.menu.actions.sendlogs.LogAction;
+import org.jdownloader.images.AbstractIcon;
 
 public class LogSendAction extends CustomizableAppAction {
-
     public LogSendAction() {
         setName(_GUI.T.LogAction());
         setIconKey(IconKey.ICON_LOG);
         setTooltipText(_GUI.T.LogAction_tooltip());
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        new LogAction().actionPerformed(e);
+        new LogAction() {
+            @Override
+            protected SendLogDialog getSendLogDialog(List<LogFolder> folders) {
+                return new SendLogDialog(folders) {
+                    @Override
+                    protected MigPanel createBottomPanel() {
+                        final MigPanel ret = new MigPanel("ins 0", "[][][]20[grow,fill][]", "[]");
+                        try {
+                            final JLink help = new JLink(_GUI.T.action_help(), new AbstractIcon(IconKey.ICON_DIALOG_HELP, 16), new URL("https://support.jdownloader.org/Knowledgebase/Article/View/how-to-create-and-upload-session-logs"));
+                            final JLink privacy = new JLink(_GUI.T.jd_gui_swing_components_AboutDialog_privacy(), new AbstractIcon(IconKey.ICON_ABOUT, 16), new URL("https://my.jdownloader.org/legal/privacy.html#jdownloader"));
+                            ret.add(help, "gapleft 0");
+                            ret.add(privacy);
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        }
+                        return ret;
+                    }
+                };
+            }
+        }.actionPerformed(e);
     }
-
 }
