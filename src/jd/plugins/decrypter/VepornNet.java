77b10028a661fc5a7ab351c3bf1a55f70c5513a6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-06-21 15:35:58 +0000

    *Plugins: Fixes/Changes/Maintenance* - VidCloudIcu: fixed and renamed to asianembedio using Gogoplay4Com crawler RE forum 91013 - VepornNet: fixed and deleted offline domains RE forum 91012
    *Plugins: RIP*
    - vidcloud.icu: vidcloud.icu RE forum 91013
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46281 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/VepornNet.java
+++ src/jd/plugins/decrypter/VepornNet.java
@@ -16,6 +16,7 @@
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.jdownloader.plugins.components.antiDDoSForDecrypt;
 import org.jdownloader.plugins.controller.LazyPlugin;
@@ -25,15 +26,44 @@ import jd.controlling.ProgressController;
 import jd.http.Browser;
 import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
-import jd.plugins.DecrypterException;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "veporns.com" }, urls = { "https?://(?:www\\.)?(?:ve(?:-)?porns?\\.(?:net|com)|veporno\\.net)/video/[A-Za-z0-9\\-_]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 public class VepornNet extends antiDDoSForDecrypt {
     public VepornNet(PluginWrapper wrapper) {
         super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "veporno.net", "veporn.net", "veporns.com" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/video/([A-Za-z0-9\\-_]+)");
+        }
+        return ret.toArray(new String[0]);
     }
 
     @Override
@@ -42,7 +72,7 @@ public class VepornNet extends antiDDoSForDecrypt {
     }
 
     public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         final String parameter = param.toString();
         br.setFollowRedirects(true);
         getPage(parameter);
@@ -55,47 +85,54 @@ public class VepornNet extends antiDDoSForDecrypt {
                 }
             }
         }
-        if (br.getHttpConnection().getResponseCode() == 404 || !br.getURL().contains("video/") || br.containsHTML("URL=https?://www.ve(?:-)?porns?.(net|com)'")) {
-            decryptedLinks.add(this.createOfflinelink(parameter));
-            return decryptedLinks;
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (!this.canHandle(br.getURL())) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         String fpName = br.getRegex("(rhgd)").getMatch(0);
         final String[] links = br.getRegex("comment\\((\\d+)\\)").getColumn(0);
-        if (links == null || links.length == 0) {
-            throw new DecrypterException("Decrypter broken for link: " + parameter);
-        }
-        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        int counter = 1;
-        for (final String singleLink : links) {
-            if (this.isAbort()) {
-                return decryptedLinks;
-            }
-            final Browser br = this.br.cloneBrowser();
-            getPage(br, "/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
-            if (br.containsHTML(">Site is too crowded<")) {
-                for (int i = 1; i <= 3; i++) {
-                    sleep(i * 3 * 1001l, param);
-                    getPage(br, "/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
-                    if (!br.containsHTML(">Site is too crowded<")) {
-                        break;
+        if (links.length > 0) {
+            br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+            int counter = 1;
+            for (final String singleLink : links) {
+                if (this.isAbort()) {
+                    return ret;
+                }
+                final Browser br = this.br.cloneBrowser();
+                getPage(br, "/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
+                if (br.containsHTML(">Site is too crowded<")) {
+                    for (int i = 1; i <= 3; i++) {
+                        sleep(i * 3 * 1001l, param);
+                        getPage(br, "/ajax.php?page=video_play&thumb&theme=&video=&id=" + singleLink + "&server=" + counter);
+                        if (!br.containsHTML(">Site is too crowded<")) {
+                            break;
+                        }
                     }
                 }
-            }
-            String finallink = br.getRegex("iframe src='(https?[^<>']+)'").getMatch(0);
-            if (finallink == null) {
-                finallink = br.getRegex("iframe src=\"(https?[^<>\"]+)\"").getMatch(0);
+                String finallink = br.getRegex("iframe src='(https?[^<>']+)'").getMatch(0);
                 if (finallink == null) {
-                    continue;
+                    finallink = br.getRegex("iframe src=\"(https?[^<>\"]+)\"").getMatch(0);
+                    if (finallink == null) {
+                        continue;
+                    }
                 }
+                ret.add(createDownloadlink(finallink));
+                counter++;
             }
-            decryptedLinks.add(createDownloadlink(finallink));
-            counter++;
+        }
+        /* 2022-06-21: Most likely videos embedded on streamtape.com */
+        final String[] embedURLs = br.getRegex("<iframe src=\"(https?://[^\"]+)\"").getColumn(0);
+        if (embedURLs.length > 0) {
+            for (final String embedURL : embedURLs) {
+                ret.add(this.createDownloadlink(embedURL));
+            }
         }
         if (fpName != null) {
             final FilePackage fp = FilePackage.getInstance();
             fp.setName(Encoding.htmlDecode(fpName.trim()));
-            fp.addLinks(decryptedLinks);
+            fp.addLinks(ret);
         }
-        return decryptedLinks;
+        return ret;
     }
 }
