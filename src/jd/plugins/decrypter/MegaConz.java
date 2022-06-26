ede4ad86bff97d9808e1cba1040f2b31715cdf61
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-06-26 14:21:06 +0000

    MegaConz: -updated to use JSonParser and MinimalMemoryMap
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46327 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/MegaConz.java
+++ src/jd/plugins/decrypter/MegaConz.java
@@ -21,6 +21,24 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import org.appwork.exceptions.WTFException;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.SimpleMapper;
+import org.appwork.storage.TypeRef;
+import org.appwork.storage.simplejson.JSonFactory;
+import org.appwork.storage.simplejson.JSonObject;
+import org.appwork.storage.simplejson.JSonObjectHashMap;
+import org.appwork.storage.simplejson.JSonParser;
+import org.appwork.storage.simplejson.JSonValue;
+import org.appwork.storage.simplejson.MinimalMemoryMap;
+import org.appwork.storage.simplejson.mapper.JSonMapper;
+import org.appwork.utils.ByteArrayWrapper;
+import org.appwork.utils.IO;
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.config.MegaConzConfig;
+import org.jdownloader.plugins.config.PluginJsonConfig;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.controlling.linkcrawler.LinkCrawler;
@@ -34,23 +52,6 @@ import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
-
-import org.appwork.exceptions.WTFException;
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.SimpleMapper;
-import org.appwork.storage.TypeRef;
-import org.appwork.storage.simplejson.JSonFactory;
-import org.appwork.storage.simplejson.JSonObject;
-import org.appwork.storage.simplejson.JSonObjectHashMap;
-import org.appwork.storage.simplejson.JSonValue;
-import org.appwork.storage.simplejson.JsonByteArrayStringValue;
-import org.appwork.storage.simplejson.mapper.JSonMapper;
-import org.appwork.utils.ByteArrayWrapper;
-import org.appwork.utils.IO;
-import org.appwork.utils.StringUtils;
-import org.jdownloader.plugins.components.config.MegaConzConfig;
-import org.jdownloader.plugins.config.PluginJsonConfig;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "mega.co.nz" }, urls = { "(?:https?://(www\\.)?mega\\.(co\\.)?nz/[^/:]*#F|chrome://mega/content/secure\\.html#F|mega:/*#F)(!|%21)[a-zA-Z0-9]+(!|%21)[a-zA-Z0-9_,\\-%]{16,}((!|%21)[a-zA-Z0-9]+)?(\\?[a-zA-Z0-9]+)?" })
 public class MegaConz extends PluginForDecrypt {
@@ -177,9 +178,9 @@ public class MegaConz extends PluginForDecrypt {
                         }
                     } else {
                         con = br.openRequestConnection(br.createJSonPostRequest("https://g.api.mega.co.nz/cs?id=" + CS.incrementAndGet() + "&n=" + folderID
-                                /*
-                                 * + "&domain=meganz
-                                 */, "[{\"a\":\"f\",\"c\":\"1\",\"r\":\"1\",\"ca\":1}]"));// ca=1
+                        /*
+                         * + "&domain=meganz
+                         */, "[{\"a\":\"f\",\"c\":\"1\",\"r\":\"1\",\"ca\":1}]"));// ca=1
                         // ->
                         // !nocache,
                         // commands.cpp
@@ -196,56 +197,40 @@ public class MegaConz extends PluginForDecrypt {
                     }
                     final Object response;
                     try {
-                        final SimpleMapper mapper = new SimpleMapper() {
-                            @Override
-                            protected JSonFactory newJsonFactory(String jsonString) {
-                                return new JSonFactory(jsonString) {
-                                    @Override
-                                    protected JSonObject createJSonObject(JSonObject map) {
-                                        if (map == null) {
-                                            return new JSonObjectHashMap();
-                                        } else {
-                                            return new JSonObjectHashMap(map);
-                                        }
-                                    }
-                                };
-                            };
-
-                            @Override
-                            protected JSonMapper buildMapper() {
-                                return new JSonMapper() {
-                                    {
-                                        autoMapJsonObjectClass = HashMap.class;
-                                        autoMapJsonArrayclass = ArrayList.class;
-                                    }
-                                };
-                            }
-                        };
                         if (true) {
-                            JSonFactory factory = new JSonFactory(IO.BOM.read(IO.readStream(-1, con.getInputStream()), IO.BOM.UTF8.getCharSet())) {
+                            JSonParser factory = new JSonParser(IO.BOM.read(IO.readStream(-1, con.getInputStream()), IO.BOM.UTF8.getCharSet())) {
                                 @Override
-                                protected JSonObject createJSonObject(JSonObject map) {
-                                    if (map == null) {
-                                        return new JSonObjectHashMap();
-                                    } else {
-                                        return new JSonObjectHashMap(map);
-                                    }
-                                }
-
-                                @Override
-                                protected JSonValue createJSonValue(String value) {
-                                    if (value == null) {
-                                        return super.createJSonValue(value);
-                                    } else {
-                                        return new JsonByteArrayStringValue(value);
-                                    }
+                                protected Map<String, ? extends Object> createJSonObject() {
+                                    return new MinimalMemoryMap<Object>();
                                 }
                             };
                             response = factory.parse();
                             // allow GC of JSonFactory
                             factory = null;
                         } else {
+                            SimpleMapper mapper = new SimpleMapper() {
+                                @Override
+                                protected JSonFactory newJsonFactory(String jsonString) {
+                                    return new JSonFactory(jsonString) {
+                                        @Override
+                                        protected JSonObject createJSonObject() {
+                                            return new JSonObjectHashMap();
+                                        }
+                                    };
+                                };
+
+                                @Override
+                                protected JSonMapper buildMapper() {
+                                    return new JSonMapper() {
+                                        {
+                                            autoMapJsonObjectClass = HashMap.class;
+                                            autoMapJsonArrayclass = ArrayList.class;
+                                        }
+                                    };
+                                }
+                            };
                             response = mapper.inputStreamToObject(con.getInputStream(), TypeRef.OBJECT);
+                            mapper = null;
                         }
                     } finally {
                         con.disconnect();
