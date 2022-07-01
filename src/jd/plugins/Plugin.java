6a90aa91cd3327140b74ea6e939276fec21311f2
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-07-01 08:48:43 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46375 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/Plugin.java
+++ src/jd/plugins/Plugin.java
@@ -646,7 +646,7 @@ public abstract class Plugin implements ActionListener {
     }
 
     public <T> T restoreFromString(final String json, final TypeRef<T> typeRef) {
-        if (TypeRef.HASHMAP == typeRef || TypeRef.LIST == typeRef || TypeRef.OBJECT == typeRef) {
+        if (TypeRef.HASHMAP == typeRef || TypeRef.LIST == typeRef || TypeRef.OBJECT == typeRef || TypeRef.MAP == typeRef) {
             final JSonParser factory = new JSonParser(json) {
                 @Override
                 protected Map<String, ? extends Object> createJSonObject() {
