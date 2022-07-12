bed9fd552b6fa10f9020585080b64d6113568aef
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-07-12 11:36:14 +0000

    *Plugins: Fixes/Changes/Maintenance* - EvilangelCore: minor improvements
    *Plugins: New plugins or new websites that belong to existing scripts*
    Some evilangel based websites RE ticket ZJY-955-86648
    - filthykingscom
    - tabooheatcom
    - xempirecom
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46437 ebf7c1c2-ba36-0410-9fe8-c592906822b4

---
+++ src/org/jdownloader/plugins/components/config/EvilangelCoreConfigTabooheatCom.java
@@ -0,0 +1,8 @@
+package org.jdownloader.plugins.components.config;
+
+import org.jdownloader.plugins.config.PluginHost;
+import org.jdownloader.plugins.config.Type;
+
+@PluginHost(host = "tabooheat.com", type = Type.HOSTER)
+public interface EvilangelCoreConfigTabooheatCom extends EvilangelCoreConfig {
+}
