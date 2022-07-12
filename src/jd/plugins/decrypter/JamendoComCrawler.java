ef957958552b55cfeec74c98f86a7eaca3b059bc
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-07-12 15:26:10 +0000

    *Plugins: Fixes/Changes/Maintenance* jemandocom: refs #90075 - ensured full compatibility with old URLs - added artist crawler - added playlist crawler - added user-profile playlist crawler (unfinished) - updated errorhandling
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46441 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/decrypter/JamendoComCrawler.java
+++ src/jd/plugins/decrypter/JamendoComCrawler.java
@@ -23,15 +23,18 @@ import java.util.Map;
 
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
-import org.appwork.utils.DebugMode;
+import org.appwork.utils.parser.UrlQuery;
 import org.jdownloader.plugins.controller.LazyPlugin;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginDependencies;
 import jd.plugins.PluginException;
@@ -70,18 +73,20 @@ public class JamendoComCrawler extends PluginForDecrypt {
     public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
         final List<String> ret = new ArrayList<String>();
         for (final String[] domains : pluginDomains) {
-            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?:[a-z]{2}/)?(album/\\d+|artist/.+|list/a\\d+)");
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?:[a-z]{2}/)?(album/\\d+(?:/[a-z0-9\\-]+)?|artist/\\d+(?:/[a-z0-9\\-]+)?|list/a\\d+|playlist/\\d+(?:/[a-z0-9\\-]+)?|user/\\d+(?:/[a-z0-9\\-]+)?)");
         }
         return ret.toArray(new String[0]);
     }
 
-    private static final String                               TYPE_ALBUM      = "https?://[^/]+/(?:album/|list/a)(\\d+)";
-    private static final String                               TYPE_ARTIST     = "https?://[^/]+/artist/(\\d+)";
     private static LinkedHashMap<String, Map<String, Object>> USER_INFO_CACHE = new LinkedHashMap<String, Map<String, Object>>() {
                                                                                   protected boolean removeEldestEntry(Map.Entry<String, Map<String, Object>> eldest) {
-                                                                                      return size() > 200;
+                                                                                      return size() > 100;
                                                                                   };
                                                                               };
+    private static final String                               TYPE_ALBUM      = "https?://[^/]+/(?:[a-z]{2}/)?(?:album/|list/a)(\\d+).*?";
+    private static final String                               TYPE_ARTIST     = "https?://[^/]+/(?:[a-z]{2}/)?artist/(\\d+).*?";
+    private static final String                               TYPE_PLAYLIST   = "https?://[^/]+/(?:[a-z]{2}/)?playlist/(\\d+).*?";
+    private static final String                               TYPE_USER       = "https?://[^/]+/(?:[a-z]{2}/)?user/(\\d+).*?";
 
     @SuppressWarnings("deprecation")
     @Override
@@ -89,6 +94,10 @@ public class JamendoComCrawler extends PluginForDecrypt {
         JamendoCom.prepBR(br);
         if (param.getCryptedUrl().matches(TYPE_ALBUM)) {
             return crawlAlbum(param);
+        } else if (param.getCryptedUrl().matches(TYPE_PLAYLIST)) {
+            return this.crawlUserPlaylist(param);
+        } else if (param.getCryptedUrl().matches(TYPE_USER)) {
+            return this.crawlUserProfile(param);
         } else if (param.getCryptedUrl().matches(TYPE_ARTIST)) {
             return this.crawlArtist(param);
         } else {
@@ -103,54 +112,175 @@ public class JamendoComCrawler extends PluginForDecrypt {
             /* Developer mistake */
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        JamendoCom.queryAPI(br, "/albums?id%5B%5D=" + albumID);
+        queryAPI(br, "/albums?id%5B%5D=" + albumID);
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         final List<Object> ressourcelist = JSonStorage.restoreFromString(br.toString(), TypeRef.LIST);
+        if (ressourcelist.isEmpty()) {
+            /* Invalid albumID. */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
         final Map<String, Object> album = (Map<String, Object>) ressourcelist.get(0);
+        if (!JamendoCom.isAvailable(album)) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final Map<String, Object> artist = getArtistInfo(album.get("artistId").toString());
         final List<Map<String, Object>> tracks = (List<Map<String, Object>>) album.get("tracks");
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(artist.get("name") + " - " + album.get("name").toString());
+        fp.setComment((String) JavaScriptEngineFactory.walkJson(album, "description/en"));
         for (final Map<String, Object> track : tracks) {
             final DownloadLink link = createSongDownloadlink(track.get("id").toString());
-            link.setProperty(JamendoCom.PROPERTY_POSITION, track.get("position"));
+            link.setProperty(JamendoCom.PROPERTY_ARTIST, artist.get("name").toString());
+            link.setProperty(JamendoCom.PROPERTY_POSITION_ALBUM, track.get("position"));
+            link.setContainerUrl(param.getCryptedUrl());
+            link._setFilePackage(fp);
             ret.add(link);
         }
         return ret;
     }
 
-    public ArrayList<DownloadLink> crawlArtist(final CryptedLink param) throws Exception {
-        if (!DebugMode.TRUE_IN_IDE_ELSE_FALSE) {
+    public ArrayList<DownloadLink> crawlUserPlaylist(final CryptedLink param) throws Exception {
+        final String playlistID = new Regex(param.getCryptedUrl(), TYPE_PLAYLIST).getMatch(0);
+        if (playlistID == null) {
+            /* Developer mistake */
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
+        queryAPI(br, "/playlists?id%5B%5D=" + playlistID);
+        final List<Object> ressourcelist = JSonStorage.restoreFromString(br.toString(), TypeRef.LIST);
+        if (ressourcelist.isEmpty()) {
+            /* Invalid playlistID. */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final Map<String, Object> playlist = (Map<String, Object>) ressourcelist.get(0);
+        final Map<String, Object> user = getUserInfo(playlist.get("userId").toString());
+        return crawlProcessUserPlaylist(param, user, playlist);
+    }
+
+    public ArrayList<DownloadLink> crawlProcessUserPlaylist(final CryptedLink param, final Map<String, Object> user, final Map<String, Object> playlist) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final List<Map<String, Object>> tracks = (List<Map<String, Object>>) playlist.get("tracks");
+        if (tracks.isEmpty()) {
+            logger.info("Empty playlist");
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final FilePackage fp = FilePackage.getInstance();
+        fp.setName(user.get("dispname") + " - " + playlist.get("name").toString());
+        fp.setComment((String) playlist.get("description"));
+        boolean positionStartsAtZero = false;
+        for (final Map<String, Object> track : tracks) {
+            final DownloadLink link = createSongDownloadlink(track.get("id").toString());
+            link.setProperty(JamendoCom.PROPERTY_USER, user.get("dispname").toString());
+            final int position = ((Number) track.get("position")).intValue();
+            if (position == 0) {
+                positionStartsAtZero = true;
+            }
+            if (positionStartsAtZero) {
+                /* Fix position value */
+                link.setProperty(JamendoCom.PROPERTY_POSITION_PLAYLIST, position + 1);
+            } else {
+                link.setProperty(JamendoCom.PROPERTY_POSITION_PLAYLIST, position);
+            }
+            link.setContainerUrl(param.getCryptedUrl());
+            link._setFilePackage(fp);
+            ret.add(link);
+        }
+        return ret;
+    }
+
+    /** Crawls user profile --> All playlists of user */
+    public ArrayList<DownloadLink> crawlUserProfile(final CryptedLink param) throws Exception {
+        final String userID = new Regex(param.getCryptedUrl(), TYPE_USER).getMatch(0);
+        if (userID == null) {
+            /* Developer mistake */
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final Map<String, Object> user = this.getUserInfo(userID);
+        final int maxItemsPerPage = 16;
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        int page = 1;
+        do {
+            final UrlQuery query = new UrlQuery();
+            query.add("userId", userID);
+            query.add("order", "");
+            query.add("limit", Integer.toString(maxItemsPerPage));
+            queryAPI(br, "/playlists?" + query.toString());
+            final List<Map<String, Object>> playlists = (List<Map<String, Object>>) JSonStorage.restoreFromString(br.toString(), TypeRef.OBJECT);
+            for (final Map<String, Object> playlist : playlists) {
+                /* TODO: Update method to be able to set correct containerURL (URL to respective playlist!). */
+                ret.addAll(crawlProcessUserPlaylist(param, user, playlist));
+            }
+            logger.info("Crawled page " + page + " | Found results so far: " + ret.size());
+            if (playlists.size() < maxItemsPerPage) {
+                logger.info("Stopping because:");
+                break;
+            } else if (this.isAbort()) {
+                logger.info("Stopping because: Aborted by user");
+                break;
+            }
+            /* TODO: Add full pagination support and errorhandling */
+            page++;
+            break;
+        } while (true);
+        return ret;
+    }
+
+    public ArrayList<DownloadLink> crawlArtist(final CryptedLink param) throws Exception {
         final String artistID = new Regex(param.getCryptedUrl(), TYPE_ARTIST).getMatch(0);
         if (artistID == null) {
             /* Developer mistake */
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        JamendoCom.queryAPI(br, "/artists?id%5B%5D=" + artistID);
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         final Map<String, Object> artist = getArtistInfo(artistID);
-        final List<Map<String, Object>> tracks = (List<Map<String, Object>>) artist.get("tracks");
-        for (final Map<String, Object> track : tracks) {
+        /* Crawl all albums */
+        final List<Map<String, Object>> albums = (List<Map<String, Object>>) artist.get("albums");
+        for (final Map<String, Object> album : albums) {
+            ret.add(createAlbumDownloadlink(album.get("id").toString()));
+        }
+        final FilePackage fpSingles = FilePackage.getInstance();
+        fpSingles.setName(artist.get("name") + " - singles");
+        /* Crawl all singles */
+        final List<Map<String, Object>> singles = (List<Map<String, Object>>) artist.get("singles");
+        for (final Map<String, Object> track : singles) {
             final DownloadLink link = createSongDownloadlink(track.get("id").toString());
-            link.setProperty(JamendoCom.PROPERTY_POSITION, track.get("position"));
+            link.setProperty(JamendoCom.PROPERTY_ARTIST, artist.get("name").toString());
+            link._setFilePackage(fpSingles);
             ret.add(link);
         }
         return ret;
     }
 
     private Map<String, Object> getArtistInfo(final String artistID) throws IOException, PluginException {
-        if (artistID == null) {
+        final JamendoCom hostPlugin = (JamendoCom) this.getNewPluginForHostInstance(this.getHost());
+        return hostPlugin.getArtistInfo(artistID);
+    }
+
+    public Map<String, Object> getUserInfo(final String userID) throws IOException, PluginException {
+        if (userID == null) {
             /* Developer mistake */
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
-        if (USER_INFO_CACHE.containsKey(artistID)) {
-            return USER_INFO_CACHE.get(artistID);
+        if (USER_INFO_CACHE.containsKey(userID)) {
+            return USER_INFO_CACHE.get(userID);
         } else {
-            JamendoCom.queryAPI(br, "/artists?id%5B%5D=" + artistID);
-            final List<Object> ressourcelist = JSonStorage.restoreFromString(br.toString(), TypeRef.LIST);
-            final Map<String, Object> artist = (Map<String, Object>) ressourcelist.get(0);
-            USER_INFO_CACHE.put(artistID, artist);
-            return artist;
+            queryAPI(br, "/users?id%5B%5D=" + userID);
+            final List<Object> users = JSonStorage.restoreFromString(br.toString(), TypeRef.LIST);
+            if (users.isEmpty()) {
+                /* User not found */
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            final Map<String, Object> user = (Map<String, Object>) users.get(0);
+            USER_INFO_CACHE.put(userID, user);
+            return user;
         }
+    }
+
+    private void queryAPI(final Browser br, final String relativePath) throws IOException {
+        JamendoCom.queryAPI(br, relativePath);
+    }
+
+    private DownloadLink createAlbumDownloadlink(final String albumID) {
+        return this.createDownloadlink("https://www." + this.getHost() + "/album/" + albumID);
     }
 
     private DownloadLink createSongDownloadlink(final String songID) {
