556ffdcdb95c39541d22af9facc42df6b21cf4f6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-07-15 14:09:23 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - bigfilecc closes #89991
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46468 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d9d508eab4e885befc2abfc05b246a67017ced47

---
+++ src/jd/plugins/decrypter/BigfileCc.java
@@ -0,0 +1,91 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.Regex;
+import org.appwork.utils.formatter.SizeFormatter;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class BigfileCc extends PluginForDecrypt {
+    public BigfileCc(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "bigfile.cc" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/[a-z]{2}/([A-Za-z0-9]{3,})");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        br.getPage(param.getCryptedUrl());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br.containsHTML("/images/404\\.png")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String[] htmls = br.getRegex("class=\"urlfile\".*?</noindex></td></tr>").getColumn(-1);
+        for (final String html : htmls) {
+            final String fname = new Regex(html, "class=\"urlfile\">([^<]+)<").getMatch(0);
+            final String fsize = new Regex(html, "<td style=\"font-size:70%;\"[^>]*>(\\d+[^<]+)<").getMatch(0);
+            final String url = new Regex(html, "data-link=\"(https?://[^\"]*/d/[^\"]+)\".*?$").getMatch(0);
+            final DownloadLink link = this.createDownloadlink("directhttp://" + url);
+            link.setName(Encoding.htmlDecode(fname).trim());
+            if (fsize != null) {
+                link.setDownloadSize(SizeFormatter.getSize(fsize));
+            }
+            link.setAvailable(true);
+            ret.add(link);
+        }
+        return ret;
+    }
+}
