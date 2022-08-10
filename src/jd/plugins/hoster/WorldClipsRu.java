0e8ecda614888b6ede47160faf25c1a5ae7a93c6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-08-10 15:33:59 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - juba-get.com: started work on that refs #90187 - worldclipsru: improved login and updated connection limits RE ticket AWG-764-55213 RE forum 47272 user wanted me to check chunkload but that's impossible afaIk - PornoneComConfig: fixed bad hostername RE forum 91411
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46533 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/WorldClipsRu.java
+++ src/jd/plugins/hoster/WorldClipsRu.java
@@ -22,12 +22,14 @@ import java.util.Random;
 import org.appwork.utils.formatter.SizeFormatter;
 
 import jd.PluginWrapper;
+import jd.http.Browser;
 import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.Account;
 import jd.plugins.Account.AccountType;
 import jd.plugins.AccountInfo;
+import jd.plugins.AccountInvalidException;
 import jd.plugins.AccountRequiredException;
 import jd.plugins.DownloadLink;
 import jd.plugins.DownloadLink.AvailableStatus;
@@ -36,7 +38,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "worldclips.ru" }, urls = { "http://(www\\.)?(dev\\.)?worldclips\\.ru/clips/[^<>\"/]*?/[^<>\"/]+" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "worldclips.ru" }, urls = { "https?://(?:(www|dev)\\.)?worldclips\\.ru/clips/[^<>\"/]*?/[^<>\"/]+" })
 public class WorldClipsRu extends PluginForHost {
     public WorldClipsRu(PluginWrapper wrapper) {
         super(wrapper);
@@ -52,7 +54,7 @@ public class WorldClipsRu extends PluginForHost {
         link.setUrlDownload(link.getDownloadURL().replace("dev.worldclips.ru/", "worldclips.ru/"));
     }
 
-    private static final String TYPE_INVALID = "http://(www\\.)?worldclips\\.ru/clips/(top|new)/\\d+";
+    private static final String TYPE_INVALID = "https?://[^/]+/clips/(top|new)/\\d+";
 
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
@@ -60,6 +62,7 @@ public class WorldClipsRu extends PluginForHost {
         if (link.getDownloadURL().matches(TYPE_INVALID)) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
+        br.setFollowRedirects(true);
         br.getPage(link.getDownloadURL());
         if (br.containsHTML("<font size=\"8\">404</font><h2|> К сожалению, запрашиваемая страница не найдена<")) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
@@ -81,7 +84,7 @@ public class WorldClipsRu extends PluginForHost {
         filesize = filesize.replaceAll("(Б|б)", "");
         filesize = filesize + "b";
         try {
-            String charSet = br.getHttpConnection().getCharset();
+            final String charSet = br.getHttpConnection().getCharset();
             /* everything works with UTF-8, if you have different charset, then you have to use this than default one */
             filename = URLDecoder.decode(filename, charSet);
             link.setFinalFileName(filename.trim());
@@ -101,17 +104,29 @@ public class WorldClipsRu extends PluginForHost {
     private void login(final Account account, final boolean force) throws Exception {
         synchronized (account) {
             try {
-                // Load cookies
                 br.setCookiesExclusive(true);
+                br.setFollowRedirects(true);
                 final Cookies cookies = account.loadCookies("");
-                if (cookies != null && !force) {
+                if (cookies != null) {
                     this.br.setCookies(this.getHost(), cookies);
-                    return;
+                    if (!force) {
+                        /* Do not validate cookies */
+                        return;
+                    }
+                    br.getPage("http://" + this.getHost());
+                    if (this.isLoggedIN(br)) {
+                        logger.info("Cookie login successful");
+                        account.saveCookies(this.br.getCookies(this.getHost()), "");
+                        return;
+                    } else {
+                        logger.info("Cookie login failed");
+                        br.clearCookies(br.getHost());
+                    }
                 }
-                br.setFollowRedirects(false);
-                br.postPage("http://worldclips.ru/personal", "expire=on&auth=1&mail=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()));
-                if (br.getCookie(br.getHost(), "member_hash", Cookies.NOTDELETEDPATTERN) == null) {
-                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nInvalid username/password!\r\nUngültiger Benutzername oder ungültiges Passwort!", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                logger.info("Performing full login");
+                br.postPage("http://" + this.getHost() + "/personal", "expire=on&auth=1&mail=" + Encoding.urlEncode(account.getUser()) + "&pass=" + Encoding.urlEncode(account.getPass()));
+                if (!isLoggedIN(br)) {
+                    throw new AccountInvalidException();
                 }
                 account.saveCookies(this.br.getCookies(this.getHost()), "");
             } catch (final PluginException e) {
@@ -121,14 +136,22 @@ public class WorldClipsRu extends PluginForHost {
         }
     }
 
+    private boolean isLoggedIN(final Browser br) {
+        if (br.containsHTML("/personal/exit")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     @Override
     public AccountInfo fetchAccountInfo(final Account account) throws Exception {
         final AccountInfo ai = new AccountInfo();
         if (!account.getUser().matches(".+@.+\\..+")) {
             if ("de".equalsIgnoreCase(System.getProperty("user.language"))) {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nBitte gib deine E-Mail Adresse ins Benutzername Feld ein!", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                throw new AccountInvalidException("Bitte gib deine E-Mail Adresse ins Benutzername Feld ein!");
             } else {
-                throw new PluginException(LinkStatus.ERROR_PREMIUM, "\r\nPlease enter your e-mail address in the username field!", PluginException.VALUE_ID_PREMIUM_DISABLE);
+                throw new AccountInvalidException("Please enter your e-mail address in the username field!");
             }
         }
         login(account, true);
@@ -144,7 +167,7 @@ public class WorldClipsRu extends PluginForHost {
         br.setFollowRedirects(false);
         br.getPage(link.getDownloadURL());
         final String clipID = getClipID();
-        dl = jd.plugins.BrowserAdapter.openDownload(br, link, br.getURL(), "download_clip_id=" + clipID + "&x=" + new Random().nextInt(100) + "&y=" + new Random().nextInt(100), true, 0);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, br.getURL(), "download_clip_id=" + clipID + "&x=" + new Random().nextInt(100) + "&y=" + new Random().nextInt(100), true, 1);
         if (!this.looksLikeDownloadableContent(dl.getConnection())) {
             logger.warning("The final dllink seems not to be a file!");
             try {
@@ -166,6 +189,15 @@ public class WorldClipsRu extends PluginForHost {
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         return clipID;
+    }
+
+    public boolean canHandle(DownloadLink downloadLink, Account account) throws Exception {
+        /* Account required for downloading */
+        if (account != null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
