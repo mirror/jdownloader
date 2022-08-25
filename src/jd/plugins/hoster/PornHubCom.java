1a994f5b37a11224e766d169b73dc162f1a3ecbc
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-08-25 16:32:58 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46594 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 5f3d71ce191024a035bedc8fd29d46d5c541ee1c

--- src/jd/plugins/hoster/PornHubCom.java
+++ src/jd/plugins/hoster/PornHubCom.java
@@ -36,23 +36,6 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-import org.appwork.storage.JSonMapperException;
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.ReflectionUtils;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.net.httpconnection.HTTPConnection;
-import org.appwork.utils.net.httpconnection.HTTPConnectionImpl;
-import org.appwork.utils.net.httpconnection.SSLSocketStreamOptions;
-import org.appwork.utils.net.httpconnection.SSLSocketStreamOptionsModifier;
-import org.jdownloader.downloader.hls.HLSDownloader;
-import org.jdownloader.downloader.hls.M3U8Playlist;
-import org.jdownloader.gui.translate._GUI;
-import org.jdownloader.logging.LogController;
-import org.jdownloader.plugins.components.hls.HlsContainer;
-import org.jdownloader.plugins.controller.LazyPlugin;
-import org.jdownloader.scripting.JavaScriptEngineFactory;
-
 import jd.PluginWrapper;
 import jd.config.ConfigContainer;
 import jd.config.ConfigEntry;
@@ -83,6 +66,23 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 import jd.plugins.decrypter.PornHubComVideoCrawler;
 
+import org.appwork.storage.JSonMapperException;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.ReflectionUtils;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.net.httpconnection.HTTPConnection;
+import org.appwork.utils.net.httpconnection.HTTPConnectionImpl;
+import org.appwork.utils.net.httpconnection.SSLSocketStreamOptions;
+import org.appwork.utils.net.httpconnection.SSLSocketStreamOptionsModifier;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.downloader.hls.M3U8Playlist;
+import org.jdownloader.gui.translate._GUI;
+import org.jdownloader.logging.LogController;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+import org.jdownloader.plugins.controller.LazyPlugin;
+import org.jdownloader.scripting.JavaScriptEngineFactory;
+
 @HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 @PluginDependencies(dependencies = { PornHubComVideoCrawler.class })
 public class PornHubCom extends PluginForHost {
@@ -105,15 +105,15 @@ public class PornHubCom extends PluginForHost {
     /* Note: Video bitrates and resolutions are not exact, they can vary. */
     /* Quality, { videoCodec, videoBitrate, videoResolution, audioCodec, audioBitrate } */
     public static LinkedHashMap<String, String[]> formats                               = new LinkedHashMap<String, String[]>(new LinkedHashMap<String, String[]>() {
-                                                                                            {
-                                                                                                put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
-                                                                                                put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
-                                                                                                put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
-                                                                                                put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
-                                                                                                put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
-                                                                                                put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
-                                                                                            }
-                                                                                        });
+        {
+            put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
+            put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
+            put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
+            put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
+            put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
+            put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
+        }
+    });
     public static final String                    BEST_ONLY                             = "BEST_ONLY";
     public static final String                    BEST_SELECTION_ONLY                   = "BEST_SELECTION_ONLY";
     public static final String                    CRAWL_VIDEO_HLS                       = "CRAWL_VIDEO_HLS";
@@ -185,7 +185,7 @@ public class PornHubCom extends PluginForHost {
         this.setConfigElements();
     }
 
-    SSLSocketStreamOptionsModifier modifier = null;
+    Object modifier = null;
 
     @Override
     public void init() {
@@ -195,17 +195,21 @@ public class PornHubCom extends PluginForHost {
         for (String domain : domainsPremium) {
             Browser.setRequestIntervalLimitGlobal(domain, 333);
         }
-        final Thread currentThread = Thread.currentThread();
-        modifier = new SSLSocketStreamOptionsModifier() {
-            @Override
-            public SSLSocketStreamOptions modify(SSLSocketStreamOptions sslSocketStreamOptions, HTTPConnection httpConnection) {
-                if (Thread.currentThread() == currentThread) {
-                    sslSocketStreamOptions.getDisabledCipherSuites().clear();
+        try {
+            Class.forName("org.appwork.utils.net.httpconnection.SSLSocketStreamOptionsModifier");
+            final Thread currentThread = Thread.currentThread();
+            modifier = new SSLSocketStreamOptionsModifier() {
+                @Override
+                public SSLSocketStreamOptions modify(SSLSocketStreamOptions sslSocketStreamOptions, HTTPConnection httpConnection) {
+                    if (Thread.currentThread() == currentThread) {
+                        sslSocketStreamOptions.getDisabledCipherSuites().clear();
+                    }
+                    return sslSocketStreamOptions;
                 }
-                return sslSocketStreamOptions;
-            }
-        };
-        HTTPConnectionImpl.addSSLSocketStreamOptionsModifier(modifier);
+            };
+            HTTPConnectionImpl.addSSLSocketStreamOptionsModifier((SSLSocketStreamOptionsModifier) modifier);
+        } catch (Throwable e) {
+        }
     }
 
     @Override
@@ -213,8 +217,10 @@ public class PornHubCom extends PluginForHost {
         try {
             super.clean();
         } finally {
-            HTTPConnectionImpl.removeSSLSocketStreamOptionsModifier(modifier);
-            modifier = null;
+            if (modifier != null) {
+                HTTPConnectionImpl.removeSSLSocketStreamOptionsModifier((SSLSocketStreamOptionsModifier) modifier);
+                modifier = null;
+            }
         }
     }
 
@@ -1119,8 +1125,8 @@ public class PornHubCom extends PluginForHost {
                     if (premiumExpired && !isPremiumDomain(br.getHost())) {
                         /**
                          * Expired pornhub premium --> It should still be a valid free account --> We might need to access a special url
-                         * which redirects us to the pornhub free mainpage and sets the cookies. </br>
-                         * 2022-06-27: Old code but let's leave it in for now as we can't know if it is still needed.
+                         * which redirects us to the pornhub free mainpage and sets the cookies. </br> 2022-06-27: Old code but let's leave
+                         * it in for now as we can't know if it is still needed.
                          */
                         logger.info("Expired premium --> Free account (?)");
                         final String pornhubMainpageCookieRedirectUrl = br.getRegex("\\'pornhubLink\\'\\s*?:\\s*?(?:\"|\\')(https?://(?:www\\.)?pornhub\\.(?:com|org)/[^<>\"\\']+)(?:\"|\\')").getMatch(0);
@@ -1159,8 +1165,7 @@ public class PornHubCom extends PluginForHost {
     }
 
     /**
-     * Checks login and sets account-type. </br>
-     * Expects browser instance to be logged in already (cookies need to be there).
+     * Checks login and sets account-type. </br> Expects browser instance to be logged in already (cookies need to be there).
      *
      * @throws Exception
      */
