bc746a1a18a32b30e8d40857a2fd6a39e72305a8
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-09-21 15:57:00 +0000

    *Plugins: Fixes/Changes/Maintenance* FlimmitComCrawler: closes #90229 - Renamed crawler plugin class - crawl poster of series/movie if wanted by user (default = true) - crawl subtitle(s) of video items if wanted by user (default = true)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46762 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/plugins/components/config/FlimmitComConfig.java
+++ src/org/jdownloader/plugins/components/config/FlimmitComConfig.java
@@ -10,11 +10,57 @@ import org.jdownloader.plugins.config.Type;
 
 @PluginHost(host = "flimmit.com", type = Type.HOSTER)
 public interface FlimmitComConfig extends PluginConfigInterface {
+    final String                    text_PreferBest    = "Only add best quality?";
+    // final String text_CrawlThumbnail = "Crawl thumbnail?";
+    final String                    text_CrawlSubtitle = "Crawl subtitle?";
+    final String                    text_CrawlPoster   = "Crawl poster?";
+    public static final TRANSLATION TRANSLATION        = new TRANSLATION();
+
+    public static class TRANSLATION {
+        public String getPreferBest_label() {
+            return text_PreferBest;
+        }
+        // public String getCrawlThumbnail_label() {
+        // return text_CrawlThumbnail;
+        // }
+
+        public String getCrawlSubtitle_label() {
+            return text_CrawlSubtitle;
+        }
+
+        public String getCrawlPoster_label() {
+            return text_CrawlPoster;
+        }
+    }
+
     @AboutConfig
     @DefaultBooleanValue(false)
-    @DescriptionForConfigEntry("Only add best quality")
+    @DescriptionForConfigEntry(text_PreferBest)
     @Order(20)
     boolean isPreferBest();
 
     void setPreferBest(boolean b);
+    // @AboutConfig
+    // @DefaultBooleanValue(true)
+    // @DescriptionForConfigEntry(text_CrawlThumbnail)
+    // @Order(30)
+    // boolean isCrawlThumbnail();
+    //
+    // void setCrawlThumbnail(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(true)
+    @DescriptionForConfigEntry(text_CrawlSubtitle)
+    @Order(40)
+    boolean isCrawlSubtitle();
+
+    void setCrawlSubtitle(boolean b);
+
+    @AboutConfig
+    @DefaultBooleanValue(true)
+    @DescriptionForConfigEntry(text_CrawlPoster)
+    @Order(50)
+    boolean isCrawlPoster();
+
+    void setCrawlPoster(boolean b);
 }
