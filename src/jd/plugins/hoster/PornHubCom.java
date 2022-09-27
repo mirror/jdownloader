388a860da80dd24b3ce5358674f84e6e19c238ef
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-09-27 15:53:17 +0000

    DirectHTTP: -refactored isTrustHeadRequest/setTrustHeadRequest to getSessionValue/putSessionValue -updated requestFileInformation, cache/use last used Request.getAuthentication to SessionCache, avoid Request without Authentication -added handleRateLimit to handle 429 response code and auto set  Browser.setRequestIntervalLimitGlobal -updated prepareConnection to allow 429 response code for head requests
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46800 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/PornHubCom.java
+++ src/jd/plugins/hoster/PornHubCom.java
@@ -106,15 +106,15 @@ public class PornHubCom extends PluginForHost {
     /* Note: Video bitrates and resolutions are not exact, they can vary. */
     /* Quality, { videoCodec, videoBitrate, videoResolution, audioCodec, audioBitrate } */
     public static LinkedHashMap<String, String[]> formats                               = new LinkedHashMap<String, String[]>(new LinkedHashMap<String, String[]>() {
-                                                                                            {
-                                                                                                put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
-                                                                                                put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
-                                                                                                put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
-                                                                                                put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
-                                                                                                put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
-                                                                                                put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
-                                                                                            }
-                                                                                        });
+        {
+            put("240", new String[] { "AVC", "400", "420x240", "AAC LC", "54" });
+            put("480", new String[] { "AVC", "600", "850x480", "AAC LC", "54" });
+            put("720", new String[] { "AVC", "1500", "1280x720", "AAC LC", "54" });
+            put("1080", new String[] { "AVC", "4000", "1920x1080", "AAC LC", "96" });
+            put("1440", new String[] { "AVC", "6000", " 2560x1440", "AAC LC", "96" });
+            put("2160", new String[] { "AVC", "8000", "3840x2160", "AAC LC", "128" });
+        }
+    });
     public static final String                    BEST_ONLY                             = "BEST_ONLY";
     public static final String                    BEST_SELECTION_ONLY                   = "BEST_SELECTION_ONLY";
     public static final String                    CRAWL_VIDEO_HLS                       = "CRAWL_VIDEO_HLS";
@@ -590,6 +590,9 @@ public class PornHubCom extends PluginForHost {
                         URLConnectionAdapter con = null;
                         try {
                             final List<M3U8Playlist> m3u8list = hlsContainers.get(0).getM3U8(hlsCheck);
+                            if (m3u8list == null || m3u8list.size() == 0) {
+                                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                            }
                             final Browser segmentCheck = br.cloneBrowser();
                             segmentCheck.setFollowRedirects(true);
                             con = (Thread.currentThread() instanceof SingleDownloadController) ? segmentCheck.openGetConnection(m3u8list.get(0).getSegment(0).getUrl()) : null;
