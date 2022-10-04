cbc676922976e79f5fc31641232c8bd008ec93cb
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-04 13:44:58 +0000

    *Plugins: Fixes/Changes/Maintenance* - BookFiOrg: removed irritating fallback to default file-extension if none is found, fixed limit detection RE forum 91817 [XFS] sendcm: RE forum 90346 - fixed offline detection - fixed filename detection - enabled altAvailablecheck to find filesize - fixed premiumonly detection
    *Plugins: New plugins or new websites that belong to existing scripts*
    - [XFS] megauptocom closes #90246
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46829 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/jd/plugins/hoster/WorkuploadCom.java
+++ src/jd/plugins/hoster/WorkuploadCom.java
@@ -46,7 +46,7 @@ public class WorkuploadCom extends PluginForHost {
     /* Connection stuff */
     private static final boolean FREE_RESUME       = false;
     private static final int     FREE_MAXCHUNKS    = 1;
-    private static final int     FREE_MAXDOWNLOADS = 20;
+    private static final int     FREE_MAXDOWNLOADS = -1;
 
     @Override
     public String getLinkID(final DownloadLink link) {
