c9e2a418b4522e648c2045825c3f24c495536553
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-04 17:18:52 +0000

    BCSSLSocketStreamFactory: -updated getProtocolVersions, only modifiy if options is available(!=null)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46834 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/net/BCSSLSocketStreamFactory.java
+++ src/org/jdownloader/net/BCSSLSocketStreamFactory.java
@@ -215,17 +215,15 @@ public class BCSSLSocketStreamFactory implements SSLSocketStreamFactory {
         @Override
         public ProtocolVersion[] getProtocolVersions() {
             final ProtocolVersion[] ret = super.getProtocolVersions();
-            final boolean tls13Enabled = options != null && options.getCustomFactorySettings().contains(TLS13_ENABLED);
+            final Boolean tls13Enabled = options != null ? options.getCustomFactorySettings().contains(TLS13_ENABLED) : null;
             final List<ProtocolVersion> protocolVersions = new ArrayList<ProtocolVersion>(Arrays.asList(ret));
-            if (tls13Enabled) {
-                if (!protocolVersions.contains(ProtocolVersion.TLSv13)) {
-                    protocolVersions.add(0, ProtocolVersion.TLSv13);
-                }
-            } else {
+            if (Boolean.TRUE.equals(tls13Enabled) && !protocolVersions.contains(ProtocolVersion.TLSv13)) {
+                protocolVersions.add(0, ProtocolVersion.TLSv13);
+            } else if (Boolean.FALSE.equals(tls13Enabled)) {
                 protocolVersions.remove(ProtocolVersion.TLSv13);
             }
-            final boolean tls10_11Disabled = options != null && options.getCustomFactorySettings().contains(TLS10_11_DISABLED);
-            if (tls10_11Disabled) {
+            final Boolean tls10_11Disabled = options != null ? options.getCustomFactorySettings().contains(TLS10_11_DISABLED) : null;
+            if (Boolean.TRUE.equals(tls10_11Disabled)) {
                 protocolVersions.remove(ProtocolVersion.TLSv10);
                 protocolVersions.remove(ProtocolVersion.TLSv11);
             }
