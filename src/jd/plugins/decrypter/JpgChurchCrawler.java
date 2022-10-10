b7fe804aca81517afeb1e5ef331a51f1d104ad7c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-10 16:40:26 +0000

    *General* - Browser: HTMLSearch: added simple helper class for easier html parsing and added exemplaric usage in two plugin classes. Methods that can return a map with all results will follow if needed.
    *Plugins: New plugins or new websites that belong to existing scripts*
    - jpgchurch: added unfinished album/profile crawler refs #90252
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46871 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 7326db8eccacf00b8db163207415e945e6d9b9c3

---
+++ src/jd/plugins/decrypter/JpgChurchCrawler.java
@@ -0,0 +1,177 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.encoding.URLEncode;
+import org.appwork.utils.parser.UrlQuery;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.HTMLSearch;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginDependencies;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.hoster.JpgChurch;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+@PluginDependencies(dependencies = { JpgChurch.class })
+public class JpgChurchCrawler extends PluginForDecrypt {
+    public JpgChurchCrawler(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        return JpgChurch.getPluginDomains();
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?!img/).+");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        br.setFollowRedirects(true);
+        br.getPage(param.getCryptedUrl());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String ogURL = HTMLSearch.searchMetaTag("og:url", br.getRequest().getHtmlCode());
+        String seek = null;
+        if (ogURL != null) {
+            seek = UrlQuery.parse(Encoding.htmlDecode(ogURL)).get("seek");
+        }
+        br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
+        br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
+        final String token = br.getRegex("PF\\.obj.config.auth_token\\s*=\\s*\"([a-f0-9]+)\"").getMatch(0);
+        final String apiurl = br.getRegex("PF\\.obj.config.json_api\\s*=\\s*\"(https?://[^\"]+)\"").getMatch(0);
+        final String dataparamshidden = br.getRegex("data-params-hidden=\"([^\"]+)").getMatch(0);
+        int page = 1;
+        final UrlQuery query = dataparamshidden != null ? UrlQuery.parse(dataparamshidden) : new UrlQuery();
+        query.add("action", "list");
+        query.add("list", "images"); // contained in dataparamshidden
+        query.add("sort", "date_desc");
+        // query.add("page", "1"); // added later in do-while-loop
+        // query.add("userid", ""); // contained in dataparamshidden
+        // query.add("albumid", ""); // contained in dataparamshidden
+        // query.add("from", "user"); // contained in dataparamshidden
+        final String list = query.get("list");
+        if (list != null) {
+            query.add("params_hidden%5Blist%5D", list);
+        }
+        final String from = query.get("from");
+        query.add("params_hidden%5Buserid%5D", "");
+        if (from != null) {
+            query.add("params_hidden%5Bfrom%5D", from);
+        }
+        final String albumid = query.get("albumid");
+        if (albumid != null) {
+            query.add("params_hidden%5Balbumid%5D", albumid);
+        }
+        query.add("params_hidden%5Bparams_hidden%5D", "");
+        if (seek != null) {
+            query.add("seek", URLEncode.encodeURIComponent(seek));
+        }
+        if (token != null) {
+            query.add("auth_token", token);
+        }
+        final String siteTitle = HTMLSearch.searchMetaTag(new String[] { "og:title", "twitter:title" }, br.getRequest().getHtmlCode());
+        FilePackage fp = null;
+        if (siteTitle != null) {
+            fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(siteTitle).trim());
+        }
+        final HashSet<String> dupes = new HashSet<String>();
+        final int maxItemsPerPage = 42;
+        do {
+            final String[] htmls = br.getRegex("<div class=\"list-item [^\"]+\"(.*?)class=\"btn-lock fas fa-eye-slash\"[^>]*></div>").getColumn(0);
+            int numberofNewItems = 0;
+            for (final String html : htmls) {
+                final String url = new Regex(html, "<a href=\"(https?://[^\"]+)\" class=\"image-container --media\">").getMatch(0);
+                final String title = new Regex(html, "data-title=\"([^\"]+)\"").getMatch(0);
+                final String filesizeBytesStr = new Regex(html, "data-size=\"(\\d+)\"").getMatch(0);
+                if (url == null || title == null || filesizeBytesStr == null) {
+                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+                }
+                if (!dupes.add(url)) {
+                    continue;
+                }
+                numberofNewItems++;
+                final DownloadLink link = this.createDownloadlink(url);
+                link.setName(this.correctOrApplyFileNameExtension(Encoding.htmlDecode(title).trim(), ".jpg"));
+                link.setVerifiedFileSize(Long.parseLong(filesizeBytesStr));
+                link.setAvailable(true);
+                if (fp != null) {
+                    link._setFilePackage(fp);
+                }
+                distribute(link);
+                ret.add(link);
+            }
+            logger.info("Crawled page " + page + " | Found items so far: " + ret.size());
+            if (this.isAbort()) {
+                logger.info("Stopping because: Aborted by user");
+                break;
+            } else if (apiurl == null || token == null) {
+                /* This should never happen */
+                logger.info("Stopping because: Mandatory token/page params is/are missing");
+                break;
+            } else if (apiurl == null || token == null) {
+                logger.info("Stopping because: Mandatory token/page params is/are missing");
+                break;
+            } else if (numberofNewItems < maxItemsPerPage) {
+                logger.info("Stopping because: Current page contains less items than: " + maxItemsPerPage);
+                break;
+            } else {
+                /* TODO: Fix pagination */
+                page++;
+                query.addAndReplace("page", Integer.toString(page));
+                br.postPage(apiurl, query);
+                final Map<String, Object> entries = restoreFromString(br.getRequest().getHtmlCode(), TypeRef.HASHMAP);
+                br.getRequest().setHtmlCode(entries.get("html").toString());
+            }
+        } while (true);
+        return ret;
+    }
+}
