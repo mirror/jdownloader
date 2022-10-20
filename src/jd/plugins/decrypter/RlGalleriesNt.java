4e528521d65795767bb598fd2683bc8f3b98afd6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-20 14:44:01 +0000

    *Plugins: Fixes/Changes/Maintenance* - RlGalleriesNt: fixed, improved pagination and added superfast crawling via thumbnail-crawling RE forum 90722 ImageVenueCom: RE forum 90722 - added support for thumbnail urls -> original image-urls - do not change added URLs via correctDownloadLink anymore but always simply get the one we need via getContentURL - dropped old code
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46923 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 9fb5ed385e6300f1d0fcbee1c8227e780c5ad6fb

--- src/jd/plugins/decrypter/RlGalleriesNt.java
+++ src/jd/plugins/decrypter/RlGalleriesNt.java
@@ -18,12 +18,16 @@ package jd.plugins.decrypter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Random;
+import java.util.regex.Pattern;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.controller.LazyPlugin;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
 import jd.http.requests.GetRequest;
+import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
@@ -32,13 +36,9 @@ import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
-import jd.plugins.components.PluginJSonUtils;
 import jd.plugins.components.UserAgents;
 
-import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
-import org.jdownloader.plugins.controller.LazyPlugin;
-
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "urlgalleries.net" }, urls = { "https?://(?:[a-z0-9_\\-]+\\.)?urlgalleries\\.net/(porn-gallery-\\d+/.*|blog_gallery\\.php\\?id=\\d+.*|porn-picture-.+)|https?://go\\.urlgalleries\\.net/[a-z0-9]+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "urlgalleries.net" }, urls = { "https?://(?:[a-z0-9_\\-]+\\.)?urlgalleries\\.net/(porn-gallery-\\d+/.+|blog_gallery\\.php\\?id=\\d+.+)|https?://go\\.urlgalleries\\.net/[a-z0-9]+" })
 public class RlGalleriesNt extends PluginForDecrypt {
     private static String agent = null;
 
@@ -51,11 +51,9 @@ public class RlGalleriesNt extends PluginForDecrypt {
         return new LazyPlugin.FEATURE[] { LazyPlugin.FEATURE.XXX };
     }
 
-    private static final String TYPE_SINGLE_PICTURE = "https?://[^/]+/porn-picture-.+";
-
-    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        final String parameter = param.toString().replace("http://", "https://");
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final String addedurl = param.getCryptedUrl().replace("http://", "https://");
         br.setReadTimeout(3 * 60 * 1000);
         // br.setCookie(".urlgalleries.net", "popundr", "1");
         if (agent == null) {
@@ -65,11 +63,87 @@ public class RlGalleriesNt extends PluginForDecrypt {
         br.getHeaders().put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         br.getHeaders().put("Accept-Language", "en-gb, en;q=0.9");
         br.setFollowRedirects(true);
-        final String galleryID = new Regex(parameter, "(?:porn-gallery-|blog_gallery\\.php\\?id=)(\\d+)").getMatch(0);
-        if (galleryID == null || parameter.matches(TYPE_SINGLE_PICTURE)) {
-            /* 2020-03-19: Single link */
+        final String galleryID = new Regex(addedurl, "(?:porn-gallery-|blog_gallery\\.php\\?id=)(\\d+)").getMatch(0);
+        if (galleryID != null) {
+            /* Gallery */
+            logger.info("Crawling gallery");
+            final String domain = Browser.getHost(addedurl, true);
+            /* Display as many items as possible to avoid having to deal with pagination. */
+            // br.getPage(parameter);
+            br.getPage(new GetRequest(new URL(String.format("https://%s/porn-gallery-%s//&a=1000", domain, galleryID))));
+            if (isOffline(br)) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            }
+            String title = br.getRegex("border='0' /></a></div>(?:\\s*<h\\d+[^>]*>\\s*)?(.*?)(?:\\s*</h\\d+>\\s*)?</td></tr><tr>").getMatch(0);
+            if (title == null) {
+                title = br.getRegex("<title>([^<]*?)</title>").getMatch(0);
+            }
+            final FilePackage fp = FilePackage.getInstance();
+            if (title != null) {
+                title = Encoding.htmlDecode(title).trim();
+                title = title.replaceAll("(?i) - URLGalleries$", "");
+            }
+            if (!StringUtils.isEmpty(title)) {
+                fp.setName(title);
+            } else {
+                /* Fallback */
+                fp.setName(br._getURL().getPath());
+            }
+            int page = 1;
+            String nextpage = null;
+            final HashSet<String> dupes = new HashSet<String>();
+            do {
+                logger.info("Crawling page " + page + " of ??");
+                int numberofNewItems = 0;
+                final String[] redirecturls = br.getRegex("rel='nofollow noopener' href='(/[^/\\']+)' target='_blank'").getColumn(0);
+                /*
+                 * Check for imagevenue thumbnails which we can change to the original URLs without needing to crawl the individual
+                 * urlgalleries.net URLs -> Saves a lot of time
+                 */
+                final String[] thumbnailurls = br.getRegex("class='gallery' src='(https?://[^/]*\\.imagevenue\\.com/[^<>\"\\']+)'").getColumn(0);
+                for (final String thumbnailurl : thumbnailurls) {
+                    if (dupes.add(thumbnailurl)) {
+                        final DownloadLink link = this.createDownloadlink(thumbnailurl);
+                        link._setFilePackage(fp);
+                        ret.add(link);
+                        numberofNewItems++;
+                    }
+                }
+                if (redirecturls.length > thumbnailurls.length) {
+                    for (String redirecturl : redirecturls) {
+                        if (dupes.add(redirecturl)) {
+                            redirecturl = br.getURL(redirecturl).toString();
+                            final DownloadLink link = this.createDownloadlink(redirecturl);
+                            link._setFilePackage(fp);
+                            ret.add(link);
+                            numberofNewItems++;
+                        }
+                    }
+                } else {
+                    logger.info("Thumbnail crawling was successful");
+                }
+                nextpage = br.getRegex("(" + Pattern.quote(br._getURL().getPath()) + "\\?p=" + (page + 1) + ")").getMatch(0);
+                if (isAbort()) {
+                    logger.info("Stopping because: Aborted by user");
+                    break;
+                } else if (numberofNewItems == 0) {
+                    /* Fail-safe */
+                    logger.info("Stopping because: Failed to find any new item on this page");
+                    break;
+                } else if (nextpage == null) {
+                    logger.info("Stopping because: Reached last page?");
+                    break;
+                } else {
+                    br.getPage(nextpage);
+                    page++;
+                }
+            } while (true);
+            return ret;
+        } else {
+            /* Single image */
+            logger.info("Crawling single image");
             br.setFollowRedirects(false);
-            br.getPage(parameter);
+            br.getPage(addedurl);
             int counter = 0;
             String redirect = null;
             do {
@@ -80,9 +154,8 @@ public class RlGalleriesNt extends PluginForDecrypt {
                 }
                 br.getPage(redirect);
             } while (counter <= 5);
-            if (isOffline() || br.containsHTML("/not_found_adult\\.php") || (redirect != null && redirect.contains(this.getHost()))) {
-                decryptedLinks.add(this.createOfflinelink(parameter));
-                return decryptedLinks;
+            if (isOffline(br) || br.containsHTML("/not_found_adult\\.php") || (redirect != null && redirect.contains(this.getHost()))) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
             }
             final String finallink;
             if (redirect != null) {
@@ -93,126 +166,13 @@ public class RlGalleriesNt extends PluginForDecrypt {
             if (finallink == null) {
                 return null;
             }
-            decryptedLinks.add(this.createDownloadlink(finallink));
-            return decryptedLinks;
+            ret.add(this.createDownloadlink(finallink));
+            return ret;
         }
-        final String domain = Browser.getHost(parameter, true);
-        /* Display as many items as possible to avoid having to deal with pagination. */
-        // br.getPage(parameter);
-        br.getPage(new GetRequest(new URL(String.format("https://%s/porn-gallery-%s//&a=10000", domain, galleryID))));
-        if (isOffline()) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        }
-        String fpName = br.getRegex("border='0' /></a></div>(?:\\s*<h\\d+[^>]*>\\s*)?(.*?)(?:\\s*</h\\d+>\\s*)?</td></tr><tr>").getMatch(0);
-        if (fpName == null) {
-            fpName = br.getRegex("<title>([^<]*?)</title>").getMatch(0);
-        }
-        FilePackage fp = FilePackage.getInstance();
-        if (fpName != null) {
-            fp.setName(fpName.trim());
-        }
-        int page = 0;
-        String nextpage = null;
-        do {
-            page++;
-            logger.info("Crawling page " + page + " of ??");
-            if (nextpage != null) {
-                br.getPage(nextpage);
-            }
-            nextpage = br.getRegex("(/blog_gallery\\.php[^<>\"\\']+\\&p=" + (page + 1) + "[^<>\"\\']*)").getMatch(0);
-            String[][] items = br.getRegex("href='(/porn-picture[^']+)'[^<>]+><[^<>]+title=\"([^\"]+)\"").getMatches();
-            if (items == null || items.length == 0) {
-                logger.warning("Decrypter broken for link: " + parameter);
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-            }
-            int counter = 1;
-            /*
-             * 2019-05-15: Via browser it sometimes worked when using a private tab but so far I havent found a way to avoid their captcha.
-             */
-            final boolean tryToAvoidCaptcha = false;
-            // TODO
-            Browser brc;
-            if (tryToAvoidCaptcha) {
-                brc = new Browser();
-            } else {
-                brc = br.cloneBrowser();
-            }
-            brc.setFollowRedirects(false);
-            final HashSet<String> dups = new HashSet<String>();
-            boolean noNewItems = true;
-            for (final String item[] : items) {
-                String aLink = item[0];
-                if (!dups.add(aLink)) {
-                    continue;
-                }
-                noNewItems = false;
-                logger.info("Decrypting link " + counter + " of " + items.length);
-                sleep(new Random().nextInt(3) + 1000, param);
-                try {
-                    if (tryToAvoidCaptcha) {
-                        brc = new Browser();
-                        brc.getHeaders().put("User-Agent", UserAgents.stringUserAgent());
-                        brc.getPage(parameter);
-                    }
-                    brc.getPage("https://" + this.getHost() + aLink);
-                } catch (final Exception e) {
-                    logger.log(e);
-                    logger.info("Link timed out: " + aLink);
-                    counter++;
-                    continue;
-                }
-                String finallink = brc.getRedirectLocation();
-                if (brc.containsHTML("Suspicious activity detected, please confirm")) {
-                    final String rcKey = brc.getRegex("data\\-sitekey=\"([^<>\"]+)\"").getMatch(0);
-                    if (rcKey == null) {
-                        logger.warning("Captcha handling failed");
-                        break;
-                    }
-                    final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br, rcKey).getToken();
-                    // {\"data\":{\"response\":\"\"}}
-                    brc.postPageRaw("/unban.php", "{\"response\":\"" + recaptchaV2Response + "\"}");
-                    brc.getPage(aLink);
-                    final String error = PluginJSonUtils.getJson(brc, "error");
-                    if (error != null) {
-                        /* 2019-05-15: E.g. "{"error":"Captcha not ok"}" */
-                        logger.info("Captcha failure");
-                        throw new PluginException(LinkStatus.ERROR_CAPTCHA);
-                    }
-                    finallink = brc.getRedirectLocation();
-                }
-                if (finallink == null) {
-                    logger.warning("Decrypter broken for link: " + parameter);
-                    throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-                }
-                final DownloadLink dl = createDownloadlink(finallink);
-                // Give temp name so we have no same filenames
-                dl.setName(Integer.toString(new Random().nextInt(1000000000)));
-                if (fp.getName() != null) {
-                    fp.add(dl);
-                }
-                decryptedLinks.add(dl);
-                dl.setFinalFileName(item[1]);
-                /* 2020-03-20: Set all to available for faster crawling */
-                dl.setAvailable(true);
-                distribute(dl);
-                logger.info(finallink);
-                counter++;
-                if (isAbort()) {
-                    logger.info("Decryption process aborted by user, stopping...");
-                    break;
-                }
-            }
-            if (noNewItems) {
-                /* Fail-safe */
-                logger.info("Stopping because: Failed to find any new item on this page");
-                break;
-            }
-        } while (nextpage != null);
-        return decryptedLinks;
     }
 
-    private boolean isOffline() {
-        return br.getHttpConnection().getResponseCode() == 404 | br.containsHTML("<center>\\s*This blog has been closed down") | br.containsHTML("<title> - urlgalleries\\.net</title>|>ERROR - NO IMAGES AVAILABLE") || br.getURL().contains("/not_found_adult.php");
+    private boolean isOffline(final Browser br) {
+        return br.getHttpConnection().getResponseCode() == 404 | br.containsHTML("(?i)<center>\\s*This blog has been closed down") | br.containsHTML("<title> - urlgalleries\\.net</title>|>ERROR - NO IMAGES AVAILABLE") || br.getURL().contains("/not_found_adult.php");
     }
 
     /* NO OVERRIDE!! */
