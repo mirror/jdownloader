5af1a3faa306fa91e556db6f3954349d44cda74b
iskelderon <iskelderon@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-26 13:57:26 +0000

    propstoreauction.com -Added initial support
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46946 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 29d68abe8dbbaa6b001e3fd269aba120bfb5c64b

---
+++ src/jd/plugins/decrypter/PropStoreAuction.java
@@ -0,0 +1,58 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.antiDDoSForDecrypt;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "propstoreauction.com" }, urls = { "https?://(?:www\\\\.)?propstoreauction\\.com/lot-details/index/catalog/.+" })
+public class PropStoreAuction extends antiDDoSForDecrypt {
+    public PropStoreAuction(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
+        ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
+        ArrayList<String> links = new ArrayList<String>();
+        final String parameter = param.toString();
+        getPage(param.getCryptedUrl());
+        this.br.setFollowRedirects(true);
+        String fpName = br.getRegex("<title[^>]*>\\s*([^<]+)\\s*").getMatch(0);
+        Collections.addAll(links, br.getRegex("<div[^>]*class\\s*=\\s*\"carousel-item modal-trigger\"[^>]*style\\s*=\\s*\"background-image:url\\('\\s*([^']+)\\s*").getColumn(0));
+        for (String link : links) {
+            link = br.getURL(Encoding.htmlDecode(link)).toString().replaceAll("(\\-\\d+)(\\.\\w+)$", "$2");
+            decryptedLinks.add(createDownloadlink(link));
+        }
+        if (StringUtils.isNotEmpty(fpName)) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName));
+            fp.addLinks(decryptedLinks);
+            fp.addLinks(decryptedLinks);
+        }
+        return decryptedLinks;
+    }
+}
