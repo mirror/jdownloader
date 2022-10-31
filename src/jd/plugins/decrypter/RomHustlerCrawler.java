76206ae800be62224b736dd823e0b66661f30cab
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-10-31 14:16:17 +0000

    *Plugins: Fixes/Changes/Maintenance* - turbobitnet: added domain turbobiytnet RE forum 87080 - DoodstreamCom: added domain doodre RE forum 91564 - romhustler: fixed RE forum 92042 website looks quite similar to romulationorg (now) - SpankBangCom: added account support RE forum 92027
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@46976 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2f671a846568ff79e4c986469e41ce1efb7498ff

---
+++ src/jd/plugins/decrypter/RomHustlerCrawler.java
@@ -0,0 +1,106 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginForHost;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class RomHustlerCrawler extends PluginForDecrypt {
+    public RomHustlerCrawler(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "romhustler.org", "romhustler.net" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/rom/[^<>\"/]+/[^<>\"/]+(/[^<>\"/]+)?");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final String addedurl = param.getCryptedUrl();
+        final PluginForHost rhPlugin = this.getNewPluginForHostInstance(this.getHost());
+        ((jd.plugins.hoster.RomHustler) rhPlugin).prepBrowser(this.br);
+        br.getPage(addedurl);
+        if (this.br.getHttpConnection().getResponseCode() == 404 || br.containsHTML(">404 \\- Page got lost|>\\s*This is a ESA protected rom|>Administrators only")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String urlSlug = new Regex(addedurl, "/rom/(.+)").getMatch(0);
+        String fpName = br.getRegex("<h1 [^>]*?itemprop=\"name\"[^>]*?>([^<>\"]*?)</h1>").getMatch(0);
+        if (fpName == null) {
+            fpName = urlSlug;
+        }
+        final String[] results = br.getRegex("<a[^>]+(/roms/(?:file|download)/[^>]*/[A-Za-z0-9/\\+=%]*)\"[^>]+>").getColumn(0);
+        if (results == null || results.length == 0 || fpName == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        int counter = 1;
+        for (final String result : results) {
+            final String name = fpName + "_" + counter;
+            final DownloadLink dl = createDownloadlink(br.getURL(result).toString());
+            dl.setName(name);
+            dl.setProperty("decrypterLink", addedurl);
+            if (counter > 1) {
+                dl.setProperty("splitlink", true);
+            }
+            dl.setAvailable(true);
+            ret.add(dl);
+            counter++;
+        }
+        if (fpName != null) {
+            final FilePackage fp = FilePackage.getInstance();
+            fp.setName(Encoding.htmlDecode(fpName).trim());
+            fp.addLinks(ret);
+        }
+        return ret;
+    }
+}
--- src/jd/plugins/hoster/DoodstreamCom.java
+++ src/jd/plugins/hoster/DoodstreamCom.java
@@ -71,7 +71,7 @@ public class DoodstreamCom extends XFileSharingProBasic {
     public static List<String[]> getPluginDomains() {
         final List<String[]> ret = new ArrayList<String[]>();
         // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
-        ret.add(new String[] { "dood.so", "doodstream.com", "dood.to", "doodapi.com", "dood.watch", "dood.cx", "doodstream.co", "dood.la", "dood.ws", "dood.pm", "dood.sh", "dood.one", "dood.tech", "dood.wf" });
+        ret.add(new String[] { "dood.so", "doodstream.com", "dood.to", "doodapi.com", "dood.watch", "dood.cx", "doodstream.co", "dood.la", "dood.ws", "dood.pm", "dood.sh", "dood.one", "dood.tech", "dood.wf", "dood.re" });
         return ret;
     }
 
