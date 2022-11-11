79c488aa1a64d9822e1aaf7bcf2402e2b7b3ffde
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-11-11 12:20:36 +0000

    *Plugins: Fixes/Changes/Maintenance* - WiadomosciPlessPl: fixed RE forum 92113
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47030 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c6f279536d25ca6c2601ab5c03354cae79cc2e89

--- src/jd/plugins/decrypter/WiadomosciPlessPl.java
+++ src/jd/plugins/decrypter/WiadomosciPlessPl.java
@@ -13,65 +13,69 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
 import jd.plugins.FilePackage;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
+import jd.plugins.hoster.DirectHTTP;
 
 /**
  *
  * @author raztoki
  *
  */
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "wiadomosci.pless.pl" }, urls = { "http://(www\\.)?wiadomosci\\.pless\\.pl/galeria/\\d+/\\d+" }) 
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "wiadomosci.pless.pl" }, urls = { "https?://(\\w+\\.)?pless\\.pl/galeria/\\d+/\\d+" })
 public class WiadomosciPlessPl extends PluginForDecrypt {
-
     public WiadomosciPlessPl(PluginWrapper wrapper) {
         super(wrapper);
     }
 
-    public ArrayList<DownloadLink> decryptIt(CryptedLink param, ProgressController progress) throws Exception {
-        final ArrayList<DownloadLink> decryptedLinks = new ArrayList<DownloadLink>();
-        final String parameter = param.toString();
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        final String addedlink = param.getCryptedUrl();
+        br.setFollowRedirects(true);
         // creates HTTP GET to listening URLs
-        br.getPage(parameter + "/?p=lista");
+        br.getPage(addedlink + "/?p=lista");
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
         // look for a name for the package
         final String fpName = br.getRegex("<title>(.*?)</title>").getMatch(0);
         // now look for all the images we want..
-        final String[] links = br.getRegex("data-url=\"(https?://gal\\.pless\\.pl/ib/[a-f0-9]{32}/.*?)\"").getColumn(0);
+        final String[] links = br.getRegex("data-url=\"(https?://gal\\.pless\\.pl/[^\"]+\\.jpe?g)\"").getColumn(0);
         if (links == null || links.length == 0) {
-            logger.warning("Decrypter broken for link: " + parameter);
-            return null;
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         // this does task for each link found
         for (final String singleLink : links) {
             // create downloadlink for each image
-            final DownloadLink dl = createDownloadlink("directhttp://" + singleLink);
+            final DownloadLink dl = createDownloadlink(DirectHTTP.createURLForThisPlugin(singleLink));
             // set fast linkcheck otherwise it will take for ever!
             dl.setAvailable(true);
             // add downloadlink to array which returns
-            decryptedLinks.add(dl);
+            ret.add(dl);
         }
         // set file package name, this places all links into one package in JDownloader.
         if (fpName != null) {
             final FilePackage fp = FilePackage.getInstance();
-            fp.setName(fpName);
-            fp.addLinks(decryptedLinks);
+            fp.setName(Encoding.htmlDecode(fpName).trim());
+            fp.addLinks(ret);
         }
-        return decryptedLinks;
+        return ret;
     }
 
-    /* NO OVERRIDE!! */
+    @Override
     public boolean hasCaptcha(CryptedLink link, jd.plugins.Account acc) {
         return false;
     }
-
 }
