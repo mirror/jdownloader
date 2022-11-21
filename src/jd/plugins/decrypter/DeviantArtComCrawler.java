aa3bd8cb7c29b7b20bc642919247a1e43c7fe7fa
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-11-21 14:27:50 +0000

    *Plugins: Fixes/Changes/Maintenance* - DeviantArtComCrawler: some cleanup refs #87165 - premium.rpnet.biz: updated errormessage when user enters invalid logindata RE: admin
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47094 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: a78c09828db9be8aef2be556917e7e69996f836e

--- src/jd/plugins/decrypter/DeviantArtComCrawler.java
+++ src/jd/plugins/decrypter/DeviantArtComCrawler.java
@@ -24,7 +24,6 @@ import java.util.regex.Pattern;
 
 import org.appwork.storage.JSonStorage;
 import org.appwork.storage.TypeRef;
-import org.appwork.utils.StringUtils;
 import org.appwork.utils.parser.UrlQuery;
 import org.jdownloader.controlling.filter.CompiledFiletypeFilter;
 import org.jdownloader.plugins.components.config.DeviantArtComConfig;
@@ -46,13 +45,12 @@ import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
-import jd.plugins.components.PluginJSonUtils;
 import jd.plugins.hoster.DeviantArtCom;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "deviantart.com" }, urls = { "https?://[\\w\\.\\-]*?deviantart\\.com/(?!(?:[^/]+/)?art/|status/)[^<>\"]+" })
 public class DeviantArtComCrawler extends PluginForDecrypt {
     /**
-     * @author raztoki
+     * @author raztoki, pspzockerscene
      */
     public DeviantArtComCrawler(PluginWrapper wrapper) {
         super(wrapper);
@@ -79,22 +77,21 @@ public class DeviantArtComCrawler extends PluginForDecrypt {
     // I've created the plugin this way to allow users to grab as little or as
     // much, content as they wish. Hopefully this wont create any
     // issues.
-    private final String        PATTERN_USER                  = "^https?://[^/]+/([\\w\\-]+)$";
-    private final String        PATTERN_USER_FAVORITES        = "^https?://[^/]+/([\\w\\-]+)/favourites.*";
-    private final String        PATTERN_GALLERY               = "^https?://[^/]+/([\\w\\-]+)/gallery/(\\d+)/([\\w\\-]+)$";
-    private final String        PATTERN_COLLECTIONS           = "https?://[\\w\\.\\-]*?deviantart\\.com/.*?/collections(/.+)?";
-    private static final String PATTERN_CATPATH_ALL           = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath(=.+)?";
-    private static final String PATTERN_CATPATH_1             = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath(=(/|%2F([a-z0-9]+)?|[a-z0-9]+)(\\&offset=\\d+)?)?";
-    private static final String PATTERN_CATPATH_2             = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath=[a-z0-9]{1,}(\\&offset=\\d+)?";
-    private static final String PATTERN_JOURNAL               = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?journal.+";
-    private static final String PATTERN_JOURNAL2              = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?journal/[\\w\\-]+/?";
-    private static final String PATTERN_BLOG                  = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?blog/(\\?offset=\\d+)?";
+    private final String        PATTERN_USER           = "^https?://[^/]+/([\\w\\-]+)$";
+    private final String        PATTERN_USER_FAVORITES = "^https?://[^/]+/([\\w\\-]+)/favourites.*";
+    private final String        PATTERN_GALLERY        = "^https?://[^/]+/([\\w\\-]+)/gallery/(\\d+)/([\\w\\-]+)$";
+    private final String        PATTERN_COLLECTIONS    = "https?://[\\w\\.\\-]*?deviantart\\.com/.*?/collections(/.+)?";
+    private static final String PATTERN_CATPATH_ALL    = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath(=.+)?";
+    private static final String PATTERN_CATPATH_1      = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath(=(/|%2F([a-z0-9]+)?|[a-z0-9]+)(\\&offset=\\d+)?)?";
+    private static final String PATTERN_CATPATH_2      = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(gallery|favourites)/\\?catpath=[a-z0-9]{1,}(\\&offset=\\d+)?";
+    private static final String PATTERN_JOURNAL        = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?journal.+";
+    private static final String PATTERN_JOURNAL2       = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?journal/[\\w\\-]+/?";
+    private static final String PATTERN_BLOG           = "https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?blog/(\\?offset=\\d+)?";
     // private static final String TYPE_INVALID = "https?://[\\w\\.\\-]*?deviantart\\.com/stats/*?";
-    private String              parameter                     = null;
-    private final boolean       fastLinkCheck                 = true;
-    private boolean             forceHtmlDownload             = false;
-    private final boolean       crawlGivenOffsetsIndividually = false;
-    private long                decryptedUrlsNum              = 0;
+    private String              parameter              = null;
+    private final boolean       fastLinkCheck          = true;
+    private boolean             forceHtmlDownload      = false;
+    private long                decryptedUrlsNum       = 0;
 
     protected void distribute(final DownloadLink dl) {
         super.distribute(dl);
@@ -291,201 +288,6 @@ public class DeviantArtComCrawler extends PluginForDecrypt {
             }
             currentOffset += 5;
         } while (br.containsHTML("class=\"next\"><a class=\"away\" data\\-offset=\"\\d+\"") && !stop_after_first_run);
-    }
-
-    private void crawlStandard() throws DecrypterException, IOException, PluginException {
-        if (br.containsHTML("class=\"empty\\-state gallery\"|class=\"empty\\-state faves\"")) {
-            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-        }
-        /* Correct input links */
-        if (parameter.matches("https?://[^<>\"/]+\\.deviantart\\.com/(?:[^/]+/)?gallery/\\?\\d+")) {
-            final Regex paramregex = new Regex(parameter, "(https?://[^<>\"/]+\\.deviantart\\.com/(?:[^/]+/)?gallery/\\?)(\\d+)");
-            parameter = paramregex.getMatch(0) + "set=" + paramregex.getMatch(1);
-        }
-        /* only non /art/ requires packagename */
-        // find and set username
-        String username = getSiteUsername();
-        if (username == null && !parameter.contains("://www.")) {
-            username = new Regex(parameter, "https?://([^<>\"]*?)\\.deviantart\\.com/").getMatch(0);
-        }
-        if (username == null) {
-            logger.warning("Plugin broken for link: " + parameter);
-            throw new DecrypterException("Decrypter broken for link: " + parameter);
-        }
-        // find and set page type
-        String pagetype = "";
-        if (parameter.matches(PATTERN_CATPATH_2)) {
-            pagetype = new Regex(parameter, "deviantart\\.com/(?:[^/]+/)?gallery/\\?catpath=([a-z0-9]+)").getMatch(0);
-            /* First letter = capital letter */
-            pagetype = pagetype.substring(0, 1).toUpperCase() + pagetype.substring(1, pagetype.length());
-        } else if (parameter.contains("/favourites/")) {
-            pagetype = "Favourites";
-        } else if (parameter.contains("/gallery/")) {
-            pagetype = "Gallery";
-        } else {
-            pagetype = "Unknown";
-        }
-        // find and set pagename
-        String pagename = br.getRegex("class=\"folder\\-title\">([^<>\"]*?)</span>").getMatch(0);
-        if (pagename != null) {
-            pagename = Encoding.htmlDecode(pagename.trim());
-        }
-        // set packagename
-        String fpName = "";
-        if (pagename != null) {
-            fpName = username + " - " + pagetype + " - " + pagename;
-        } else {
-            fpName = username + " - " + pagetype;
-        }
-        int currentOffset = 0;
-        int maxOffset = 0;
-        int timesNoItems = 0;
-        final int times_No_Items_Max = 9;
-        final int offsetIncrease = 24;
-        int mp = 0;
-        int request_counter = 1;
-        boolean startOffsetGiven = false;
-        if (parameter.contains("offset=")) {
-            final int offsetLink = Integer.parseInt(new Regex(parameter, "offset=(\\d+)").getMatch(0));
-            currentOffset = offsetLink;
-            startOffsetGiven = true;
-        }
-        /* Debug */
-        // currentOffset = 130403;
-        // mp = 5435;
-        // counter = 2;
-        FilePackage fp = null;
-        if (fpName != null) {
-            fp = FilePackage.getInstance();
-            fp.setName(fpName);
-            fp.setAllowMerge(true);
-        }
-        final String csrf = PluginJSonUtils.getJsonValue(this.br, "csrf");
-        final String requestid = PluginJSonUtils.getJsonValue(this.br, "requestid");
-        final String galleryid = PluginJSonUtils.getJsonValue(this.br, "galleryId");
-        String catpath_var = new Regex(parameter, "catpath=([^\\&]+)").getMatch(0);
-        if (catpath_var != null) {
-            catpath_var = Encoding.urlEncode(catpath_var);
-        }
-        boolean use_ajax_requests = false;
-        boolean has_more = true;
-        String has_more_str = null;
-        if (csrf == null || csrf.equals("") || requestid == null || requestid.equals("") || galleryid == null || galleryid.equals("")) {
-            throw new DecrypterException("Plugin broken");
-        }
-        do {
-            if (this.isAbort()) {
-                logger.info("Decryption aborted by user: " + parameter);
-                return;
-            }
-            logger.info("Decrypting offset " + currentOffset + " of ?");
-            if (request_counter > 1) {
-                if (use_ajax_requests) {
-                    /*
-                     * 2017-02-20: Now they have an API. At the moment we do not yet parse the json as we really only need the URLs which
-                     * are inside the html which is inside the json :)
-                     */
-                    String postdata = "username=" + username + "&offset=" + currentOffset + "&limit=" + offsetIncrease + "&_csrf=" + csrf + "&dapiIid=" + requestid;
-                    if (catpath_var != null) {
-                        postdata += "&catpath=" + catpath_var;
-                    }
-                    this.br.getHeaders().put("Referer", this.parameter);
-                    this.br.getHeaders().put("Content-Type", "application/x-www-form-urlencoded");
-                    this.br.postPage(String.format("https://www.deviantart.com/dapi/v1/gallery/%s?iid=%s&mp=%s", galleryid, requestid, mp), postdata);
-                    has_more_str = PluginJSonUtils.getJsonValue(this.br, "has_more");
-                    if (has_more_str != null && has_more_str.matches("true|false")) {
-                        has_more = Boolean.parseBoolean(has_more_str);
-                    } else {
-                        has_more = false;
-                    }
-                    mp++;
-                } else {
-                    /* Old method */
-                    accessOffset(currentOffset);
-                }
-            }
-            try {
-                final boolean galleryEndReached = this.br.containsHTML("class=\"empty\\-state gallery\"");
-                if (galleryEndReached) {
-                    /* 2017-02-04 */
-                    logger.info("Seems like we have reached the end of the gallery");
-                    break;
-                }
-                String grab;
-                if (request_counter == 1 || !use_ajax_requests) {
-                    grab = br.getRegex("class=\"smbutton smbutton\\-green browse\\-search\\-button\"(.*?)class=\"rss\\-link\"").getMatch(0);
-                    if (grab == null) {
-                        grab = br.getRegex("class=\"folderview\\-art\"(.*?)class=\"rss\\-link\"").getMatch(0);
-                    }
-                    if (grab == null) {
-                        /* 2017-02-01: Favourites */
-                        grab = br.getRegex("value=\"Search Favourites\"(.*?)class=\"footer_copyright\"").getMatch(0);
-                    }
-                    if (grab == null) {
-                        /* 2017-02-02: Favourites */
-                        grab = br.getRegex("class=\"folderview\\-top\"(.*?)class=\"footer_copyright\"").getMatch(0);
-                    }
-                    if (grab == null) {
-                        /* 2017-02-02: Gallery */
-                        grab = br.getRegex("value=\"Search Gallery\"(.*?)data\\-gmiclass=\"CCommentThread\"").getMatch(0);
-                    }
-                } else {
-                    /* Unescape json */
-                    grab = this.br.toString().replace("\\", "");
-                }
-                final String[] links = new Regex(grab, "\"(https?://[\\w\\.\\-]*?deviantart\\.com/(?:[^/]+/)?(art|journal)/[\\w\\-]+)\"").getColumn(0);
-                if (links == null || links.length == 0) {
-                    /* "deviation in storage" links are no links - that are empty items so there is no reason to stop. */
-                    final String[] empty_links = br.getRegex("class=\"(instorage)\"").getColumn(0);
-                    if (empty_links != null && empty_links.length > 0) {
-                        logger.info("This offset only contains dummy links --> Continuing");
-                        continue;
-                    }
-                    if (timesNoItems <= times_No_Items_Max) {
-                        /* No items on this page but maybe later. */
-                        logger.info("Current offset contains no items: " + currentOffset);
-                        timesNoItems++;
-                        continue;
-                    } else {
-                        /* We went too far - we should already have all links (or this is a fatal error situation) --> Stop [fail safe] */
-                        logger.info("No items found on " + times_No_Items_Max + " pages in a row --> Stopping");
-                        break;
-                    }
-                } else {
-                    /* Reset no-items counter as we found items. */
-                    timesNoItems = 0;
-                }
-                for (final String artlink : links) {
-                    final String artlinkClass = new Regex(grab, "<span\\s*class\\s*=\\s*\"([^<>]*?)\"[^<>]*href\\s*=\\s*\"" + Pattern.quote(artlink) + "\"").getMatch(0);
-                    final DownloadLink fina = createDownloadlink(artlink);
-                    if (fastLinkCheck) {
-                        fina.setAvailable(true);
-                    }
-                    /* No reason to hide their single links */
-                    fina.setContentUrl(artlink);
-                    if (fp != null) {
-                        fina._setFilePackage(fp);
-                    }
-                    if (StringUtils.containsIgnoreCase(artlinkClass, "literature")) {
-                        fina.setName(fina.getName() + ".html");
-                        fina.setMimeHint(CompiledFiletypeFilter.DocumentExtensions.HTML);
-                    } else if (forceHtmlDownload) {
-                        fina.setMimeHint(CompiledFiletypeFilter.DocumentExtensions.HTML);
-                    } else {
-                        fina.setMimeHint(CompiledFiletypeFilter.ImageExtensions.JPG);
-                    }
-                    distribute(fina);
-                }
-            } finally {
-                currentOffset += offsetIncrease;
-                request_counter++;
-            }
-            if (startOffsetGiven && crawlGivenOffsetsIndividually) {
-                logger.info("Star-toffset given and crawled and user set plugin settings to stop after crawling individual offsets");
-                break;
-            }
-            /* Really make sure that we're not ending up in an infinite loop! */
-        } while (has_more || maxOffset > 0 && currentOffset >= maxOffset);
     }
 
     private ArrayList<DownloadLink> crawlProfileFavorites(final CryptedLink param) throws IOException, PluginException {
