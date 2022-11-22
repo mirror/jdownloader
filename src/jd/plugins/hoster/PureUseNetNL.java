497171685b0709363be7f38fcc4677335573208c
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-11-22 17:18:02 +0000

    *Plugins: Fixes/Changes/Maintenance* - pureusenetnl: fixed RE ticket RFA-622-42303
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47109 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3880e1faf609b104640faa229e09ea93eef5723c

--- src/jd/plugins/hoster/PureUseNetNL.java
+++ src/jd/plugins/hoster/PureUseNetNL.java
@@ -16,7 +16,9 @@ import jd.http.Cookies;
 import jd.nutils.encoding.Encoding;
 import jd.parser.html.Form;
 import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
 import jd.plugins.AccountInfo;
+import jd.plugins.AccountInvalidException;
 import jd.plugins.HostPlugin;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
@@ -27,6 +29,8 @@ public class PureUseNetNL extends UseNet {
         super(wrapper);
         this.enablePremium("https://www.pureusenet.nl/en/packages");
     }
+
+    private final String PROPERTY_USENET_USERNAME = "usenet_username";
 
     public static interface XPureUseNetNLAccountConfigInterface extends UsenetAccountConfigInterface {
     };
@@ -52,107 +56,146 @@ public class PureUseNetNL extends UseNet {
         final AccountInfo ai = new AccountInfo();
         br.setFollowRedirects(true);
         final Cookies cookies = account.loadCookies("");
-        try {
-            Form login = null;
-            if (cookies != null) {
-                br.setCookies(getHost(), cookies);
-                br.getPage("https://www." + this.getHost() + "/en/member");
-                login = br.getFormbyActionRegex(".*/login");
-                if (login != null && login.containsHTML("name=\"password\"")) {
-                    br.getCookies(getHost()).clear();
-                } else if (!containsSessionCookie(br)) {
-                    br.getCookies(getHost()).clear();
-                }
+        Form login = null;
+        if (cookies != null) {
+            logger.info("Checking login cookies...");
+            br.setCookies(getHost(), cookies);
+            br.getPage("https://www." + this.getHost() + "/en/member");
+            login = br.getFormbyActionRegex(".*/login");
+            if (login != null && login.containsHTML("name=\"password\"")) {
+                logger.info("Cookie login successful");
+                br.getCookies(getHost()).clear();
+            } else if (!containsSessionCookie(br)) {
+                logger.info("Cookie login failed");
+                br.getCookies(getHost()).clear();
             }
-            if (!containsSessionCookie(br)) {
-                logger.info("Performing full login");
-                account.clearCookies("");
-                final String userName = account.getUser();
-                if (userName == null || !userName.matches("^.+?@.+?\\.[^\\.]+")) {
-                    throw new PluginException(LinkStatus.ERROR_PREMIUM, "Please enter your e-mail/password for pureusenet.nl website!", PluginException.VALUE_ID_PREMIUM_DISABLE);
-                }
-                br.getPage("https://www." + this.getHost() + "/en/login");
-                login = br.getFormbyActionRegex(".*/login");
-                login.put("email", Encoding.urlEncode(userName));
-                login.put("password", Encoding.urlEncode(account.getPass()));
-                br.submitForm(login);
-                login = br.getFormbyActionRegex(".*/login");
-                if (login != null && login.containsHTML("name=\"password\"")) {
-                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-                } else if (!containsSessionCookie(br)) {
-                    throw new PluginException(LinkStatus.ERROR_PREMIUM, PluginException.VALUE_ID_PREMIUM_DISABLE);
-                }
+        }
+        if (!containsSessionCookie(br)) {
+            logger.info("Performing full login");
+            account.clearCookies("");
+            final String userName = account.getUser();
+            if (userName == null || !userName.matches("^.+?@.+?\\.[^\\.]+")) {
+                throw new AccountInvalidException("Please enter your e-mail address into the username field!");
             }
-            if (!StringUtils.containsIgnoreCase(br.getURL(), "/en/member")) {
-                br.getPage("/en/member");
+            br.getPage("https://www." + this.getHost() + "/en/login");
+            login = br.getFormbyActionRegex(".*/login");
+            login.put("email", Encoding.urlEncode(userName));
+            login.put("password", Encoding.urlEncode(account.getPass()));
+            br.submitForm(login);
+            login = br.getFormbyActionRegex(".*/login");
+            if (login != null && login.containsHTML("name=\"password\"")) {
+                throw new AccountInvalidException();
+            } else if (!containsSessionCookie(br)) {
+                throw new AccountInvalidException();
             }
-            account.saveCookies(br.getCookies(getHost()), "");
-            final String packageType = br.getRegex("<h3>Your package:\\s*<span class=\"font-primary.*?\">(.*?)<").getMatch(0);
-            final String connections = br.getRegex("<li><b>Connections:</b>\\s*(\\d+)\\s*<").getMatch(0);
-            if (packageType != null) {
-                ai.setStatus(packageType);
-                if (connections != null) {
-                    account.setMaxSimultanDownloads(Integer.parseInt(connections));
-                } else {
-                    if (packageType.contains("XS")) {
-                        // Pure XS = 4 Connections
-                        account.setMaxSimultanDownloads(4);
-                    } else if (packageType.contains("S")) {
-                        // Pure S = 4 Connections
-                        account.setMaxSimultanDownloads(4);
-                    } else if (packageType.contains("M")) {
-                        // Pure M = 8 Connections
-                        account.setMaxSimultanDownloads(8);
-                    } else if (packageType.contains("XXL")) {
-                        // Pure XXL = 12 Connections
-                        account.setMaxSimultanDownloads(12);
-                    } else if (packageType.contains("XL")) {
-                        // Pure XL = 12 Connections
-                        account.setMaxSimultanDownloads(12);
-                    } else if (packageType.contains("L")) {
-                        // Pure L = 8 Connections
-                        account.setMaxSimultanDownloads(8);
-                    } else {
-                        // 5 connections(fallback)
-                        account.setMaxSimultanDownloads(5);
-                    }
-                }
+        }
+        if (!StringUtils.containsIgnoreCase(br.getURL(), "/en/member")) {
+            br.getPage("/en/member");
+        }
+        account.saveCookies(br.getCookies(getHost()), "");
+        final String dedicatedUsenetUsername = br.getRegex(">\\s*([^<]+)\\s*</div>\\s*<a href=\"/[a-z]+/member/settings\"").getMatch(0);
+        if (dedicatedUsenetUsername == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        if (StringUtils.equalsIgnoreCase(dedicatedUsenetUsername, account.getUser())) {
+            /* dedicatedUsenetUsername == username of account -> No need to store it as a property. */
+            account.removeProperty(PROPERTY_USENET_USERNAME);
+        } else {
+            logger.info("Found dedicated Usenet username which differs from the username of this account: " + dedicatedUsenetUsername);
+            account.setProperty(PROPERTY_USENET_USERNAME, dedicatedUsenetUsername);
+        }
+        String packageType = br.getRegex("(?i)<h3>Your package:\\s*<span class=\"font-primary.*?\">(.*?)<").getMatch(0);
+        String connections = br.getRegex("<li><b>Connections:</b>\\s*(\\d+)\\s*<").getMatch(0);
+        if (connections == null) {
+            /* 2022-11-22 */
+            connections = br.getRegex("(?i)>\\s*Connections\\s*</h5>\\s*<div class=\"server-settings__value\">\\s*(\\d+)").getMatch(0);
+        }
+        final boolean isPremium = br.containsHTML("(?i)>\\s*Auto Renew Status\\s*:\\s*</h5>\\s*<div class=\"server-settings__value\">\\s*<b class=\"server-settings__value-b\">\\s*Active");
+        if (packageType == null && isPremium) {
+            packageType = "Auto Renew: Active";
+        }
+        if (packageType != null) {
+            if (connections != null) {
+                account.setMaxSimultanDownloads(Integer.parseInt(connections));
             } else {
-                ai.setStatus("Unknown packageType! Please contact JDownloader support at support@jdownloader.org");
-                if (connections != null) {
-                    account.setMaxSimultanDownloads(Integer.parseInt(connections));
+                if (StringUtils.containsIgnoreCase(packageType, "XS")) {
+                    // Pure XS = 4 Connections
+                    account.setMaxSimultanDownloads(4);
+                } else if (StringUtils.containsIgnoreCase(packageType, "S")) {
+                    // Pure S = 4 Connections
+                    account.setMaxSimultanDownloads(4);
+                } else if (StringUtils.containsIgnoreCase(packageType, "M")) {
+                    // Pure M = 8 Connections
+                    account.setMaxSimultanDownloads(8);
+                } else if (StringUtils.containsIgnoreCase(packageType, "XXL")) {
+                    // Pure XXL = 12 Connections
+                    account.setMaxSimultanDownloads(12);
+                } else if (StringUtils.containsIgnoreCase(packageType, "XL")) {
+                    // Pure XL = 12 Connections
+                    account.setMaxSimultanDownloads(12);
+                } else if (StringUtils.containsIgnoreCase(packageType, "L")) {
+                    // Pure L = 8 Connections
+                    account.setMaxSimultanDownloads(8);
                 } else {
                     // 5 connections(fallback)
                     account.setMaxSimultanDownloads(5);
                 }
             }
-            String expireDate = br.getRegex("<li><b>Expiration date:</b>\\s*(\\d{4}-\\d{2}-\\d{2})\\s*<").getMatch(0);
-            if (expireDate == null) {
-                /* 2020-01-21 */
-                expireDate = br.getRegex(">Next Billing:</b>\\s*(\\d{4}-\\d{2}-\\d{2})\\s*</li>").getMatch(0);
+            packageType += " | Connections: " + account.getMaxSimultanDownloads();
+            ai.setStatus(packageType);
+        } else {
+            ai.setStatus("Unknown packageType! Please contact JDownloader support at support@jdownloader.org");
+            if (connections != null) {
+                account.setMaxSimultanDownloads(Integer.parseInt(connections));
+            } else {
+                // 5 connections(fallback)
+                account.setMaxSimultanDownloads(5);
             }
-            if (expireDate == null) {
-                /* 2020-01-21 - wide open RegEx --> Last chance */
-                expireDate = br.getRegex("(\\d{4}-\\d{2}-\\d{2})").getMatch(0);
-            }
-            if (expireDate != null) {
-                final long date = TimeFormatter.getMilliSeconds(expireDate, "yyyy'-'MM'-'dd", null);
-                if (date > 0) {
-                    /* Add one extra day */
-                    ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
-                    ai.setProperty("multiHostSupport", Arrays.asList(new String[] { "usenet" }));
-                    account.setProperty(Account.PROPERTY_REFRESH_TIMEOUT, 2 * 60 * 60 * 1000l);
-                    return ai;
-                }
-            }
-            /* Free account or plugin failure */
-            throw new PluginException(LinkStatus.ERROR_PREMIUM, "Free accounts are not supported", PluginException.VALUE_ID_PREMIUM_DISABLE);
-        } catch (final PluginException e) {
-            if (e.getLinkStatus() == LinkStatus.ERROR_PREMIUM) {
-                account.clearCookies("");
-            }
-            throw e;
         }
+        String expireDate = br.getRegex("(?i)<li><b>Expiration date:</b>\\s*(\\d{4}-\\d{2}-\\d{2})\\s*<").getMatch(0);
+        if (expireDate == null) {
+            /* 2020-01-21 */
+            expireDate = br.getRegex(">Next Billing:</b>\\s*(\\d{4}-\\d{2}-\\d{2})\\s*</li>").getMatch(0);
+        }
+        if (expireDate == null) {
+            /* 2020-01-21 - wide open RegEx --> Last chance */
+            expireDate = br.getRegex("(\\d{4}-\\d{2}-\\d{2})").getMatch(0);
+        }
+        account.setProperty(Account.PROPERTY_REFRESH_TIMEOUT, 2 * 60 * 60 * 1000l);
+        ai.setProperty("multiHostSupport", Arrays.asList(new String[] { "usenet" }));
+        if (expireDate != null) {
+            /* Premium account with expiredate */
+            account.setType(AccountType.PREMIUM);
+            final long date = TimeFormatter.getMilliSeconds(expireDate, "yyyy'-'MM'-'dd", null);
+            if (date > 0) {
+                /* Add one extra day */
+                ai.setValidUntil(date + (24 * 60 * 60 * 1000l));
+                return ai;
+            }
+        }
+        if (isPremium) {
+            /* Auto Renew premium without known expire-date. */
+            account.setType(AccountType.PREMIUM);
+            return ai;
+        } else {
+            /* Free account or plugin failure */
+            throw new AccountInvalidException("Unsupported account type 'Free' or this plugin is broken");
+        }
+    }
+
+    @Override
+    protected String getUseNetUsername(final Account account) {
+        final String dedicatedUsenetUsername = account.getStringProperty(PROPERTY_USENET_USERNAME);
+        if (dedicatedUsenetUsername != null) {
+            return dedicatedUsenetUsername;
+        } else {
+            return account.getUser();
+        }
+    }
+
+    @Override
+    protected String getUseNetPassword(Account account) {
+        return account.getPass();
     }
 
     @Override
