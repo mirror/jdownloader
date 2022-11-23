2c63eabdd9ba15636d83c38c9c08826e28fdfd0d
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-11-23 17:40:23 +0000

    GoProCloudDecrypter: -fixed npe DownloadLink: -deprecate isGenericVariantSupport, new is hasGenericVariantSupport build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47125 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3b8b5c5f478f5c4b250007b63d1fd1f0f04ea5ab

--- src/jd/plugins/PluginForHost.java
+++ src/jd/plugins/PluginForHost.java
@@ -195,14 +195,14 @@ import org.jdownloader.updatev2.UpdateHandler;
 public abstract class PluginForHost extends Plugin {
     private static final String    COPY_MOVE_FILE = "CopyMoveFile";
     private static final Pattern[] PATTERNS       = new Pattern[] {
-        /**
-         * these patterns should split filename and fileextension (extension must include the
-         * point)
-         */
-        // multipart rar archives
-            Pattern.compile("(.*)(\\.pa?r?t?\\.?[0-9]+.*?\\.rar$)", Pattern.CASE_INSENSITIVE),
-            // normal files with extension
-            Pattern.compile("(.*)(\\..*?$)", Pattern.CASE_INSENSITIVE) };
+                                                  /**
+                                                   * these patterns should split filename and fileextension (extension must include the
+                                                   * point)
+                                                   */
+                                                  // multipart rar archives
+        Pattern.compile("(.*)(\\.pa?r?t?\\.?[0-9]+.*?\\.rar$)", Pattern.CASE_INSENSITIVE),
+        // normal files with extension
+        Pattern.compile("(.*)(\\..*?$)", Pattern.CASE_INSENSITIVE) };
     private LazyHostPlugin         lazyP          = null;
     /**
      * Is true if the user has answered a captcha challenge. Does not say anything whether or not the answer was correct.
@@ -1340,7 +1340,7 @@ public abstract class PluginForHost extends Plugin {
 
     public void postHandle(final DownloadLink downloadLink, final Account account, final PluginForHost pluginForHost) throws Exception {
         if (pluginForHost != null && downloadLink != null && StringUtils.equalsIgnoreCase(downloadLink.getHost(), pluginForHost.getHost())) {
-            if (downloadLink.isGenericVariantSupport() && downloadLink.hasVariantSupport()) {
+            if (downloadLink.hasGenericVariantSupport() && downloadLink.hasVariantSupport()) {
                 final GenericVariants var = downloadLink.getVariant(GenericVariants.class);
                 if (var != null) {
                     var.runPostDownload(this, downloadLink, account);
@@ -1351,7 +1351,7 @@ public abstract class PluginForHost extends Plugin {
 
     public void preHandle(final DownloadLink downloadLink, final Account account, final PluginForHost pluginForHost) throws Exception {
         if (pluginForHost != null && downloadLink != null && StringUtils.equalsIgnoreCase(downloadLink.getHost(), pluginForHost.getHost())) {
-            if (downloadLink.isGenericVariantSupport() && downloadLink.hasVariantSupport()) {
+            if (downloadLink.hasGenericVariantSupport() && downloadLink.hasVariantSupport()) {
                 final GenericVariants var = downloadLink.getVariant(GenericVariants.class);
                 if (var != null) {
                     var.runPreDownload(this, downloadLink, account);
@@ -1363,16 +1363,16 @@ public abstract class PluginForHost extends Plugin {
     public void handleMultiHost(DownloadLink downloadLink, Account account) throws Exception {
         /*
          * fetchAccountInfo must fill ai.setMultiHostSupport to signal all supported multiHosts
-         *
+         * 
          * please synchronized on accountinfo and the ArrayList<String> when you change something in the handleMultiHost function
-         *
+         * 
          * in fetchAccountInfo we don't have to synchronize because we create a new instance of AccountInfo and fill it
-         *
+         * 
          * if you need customizable maxDownloads, please use getMaxSimultanDownload to handle this you are in multihost when account host
          * does not equal link host!
-         *
-         *
-         *
+         * 
+         * 
+         * 
          * will update this doc about error handling
          */
         logger.severe("invalid call to handleMultiHost: " + downloadLink.getName() + ":" + downloadLink.getHost() + " to " + getHost() + ":" + this.getVersion() + " with " + account);
