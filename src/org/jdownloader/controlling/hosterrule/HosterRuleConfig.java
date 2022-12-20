c46f13bebb26c7dc3f966c2b5cb0ab9b32c920af
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2022-12-20 14:02:54 +0000

    *General* - HosterRuleConfig: added @DescriptionForConfigEntry
    *Plugins: Fixes/Changes/Maintenance*
    - SankakucomplexCom: put page number in filenames if one is given refs #90305
    - PornwildTo: added new domain pornwildsu RE forum 92412
    fastsharecz:
    - fixed detection of "Unlimited traffic" premium accounts RE ticket IFG-555-92960
    - fixed download handling for free accounts
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47217 ebf7c1c2-ba36-0410-9fe8-c592906822b4

--- src/org/jdownloader/controlling/hosterrule/HosterRuleConfig.java
+++ src/org/jdownloader/controlling/hosterrule/HosterRuleConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.appwork.storage.config.ConfigInterface;
 import org.appwork.storage.config.annotations.AboutConfig;
 import org.appwork.storage.config.annotations.DefaultJsonObject;
+import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
 
 public interface HosterRuleConfig extends ConfigInterface {
     @AboutConfig
     @DefaultJsonObject("[]")
+    @DescriptionForConfigEntry("Account Manager -> Usage Rules")
     public List<AccountRuleStorable> getRules();
 
     public void setRules(List<AccountRuleStorable> rules);
