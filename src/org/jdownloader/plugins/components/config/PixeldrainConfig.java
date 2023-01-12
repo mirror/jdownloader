0501e1032ff181f630acdda5496c8d1ba9cdb609
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-01-12 11:59:22 +0000

    *Plugins: Fixes/Changes/Maintenance* - pixeldrain.com: updated wording and settings GUI for setting regarding behavior/action when users' IP is speed limited to leave less room for misinterpretation refs #89975
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47271 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 39001d35359cebaea6ece3bdd0e5cf16fa4dc870

--- src/org/jdownloader/plugins/components/config/PixeldrainConfig.java
+++ src/org/jdownloader/plugins/components/config/PixeldrainConfig.java
@@ -1,8 +1,9 @@
 package org.jdownloader.plugins.components.config;
 
 import org.appwork.storage.config.annotations.AboutConfig;
-import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.storage.config.annotations.DefaultEnumValue;
 import org.appwork.storage.config.annotations.DescriptionForConfigEntry;
+import org.appwork.storage.config.annotations.LabelInterface;
 import org.jdownloader.plugins.config.Order;
 import org.jdownloader.plugins.config.PluginConfigInterface;
 import org.jdownloader.plugins.config.PluginHost;
@@ -10,20 +11,35 @@ import org.jdownloader.plugins.config.Type;
 
 @PluginHost(host = "pixeldrain.com", type = Type.HOSTER)
 public interface PixeldrainConfig extends PluginConfigInterface {
-    final String                                     text_ReconnectOnSpeedLimit = "Reconnect whenever speed limit is present (disable = allow to download with limited speeds)?";
-    public static final PixeldrainConfig.TRANSLATION TRANSLATION                = new TRANSLATION();
+    final String                                     text_ActionOnSpeedLimitReached = "Action to perform when downloads over your IP are speed limited:";
+    public static final PixeldrainConfig.TRANSLATION TRANSLATION                    = new TRANSLATION();
 
     public static class TRANSLATION {
-        public String getReconnectOnSpeedLimit_label() {
-            return text_ReconnectOnSpeedLimit;
+        public String getActionOnSpeedLimitReached_label() {
+            return text_ActionOnSpeedLimitReached;
         }
     }
 
-    @AboutConfig
-    @DefaultBooleanValue(true)
-    @DescriptionForConfigEntry(text_ReconnectOnSpeedLimit)
-    @Order(10)
-    boolean isReconnectOnSpeedLimit();
+    public static enum ActionOnSpeedLimitReached implements LabelInterface {
+        TRIGGER_RECONNECT_TO_CHANGE_IP {
+            @Override
+            public String getLabel() {
+                return "Trigger reconnect event to try to change IP";
+            }
+        },
+        ALLOW_SPEED_LIMITED_DOWNLOAD {
+            @Override
+            public String getLabel() {
+                return "Allow speed limited downloads";
+            }
+        };
+    }
 
-    void setReconnectOnSpeedLimit(boolean b);
+    @AboutConfig
+    @DefaultEnumValue("TRIGGER_RECONNECT_TO_CHANGE_IP")
+    @Order(10)
+    @DescriptionForConfigEntry(text_ActionOnSpeedLimitReached)
+    ActionOnSpeedLimitReached getActionOnSpeedLimitReached();
+
+    void setActionOnSpeedLimitReached(final ActionOnSpeedLimitReached speedlimitaction);
 }
