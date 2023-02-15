698500e7ecf17a2aa388f7441119bedcc4795f69
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-02-15 12:23:42 +0000

    *Plugins: Fixes/Changes/Maintenance* - TokyomotionNet: improved offline detection RE forum 88415 - thisavcom: fixed RE forum 92785 - dailyleechcom: minor changes
    *Plugins: RIP*
    - superhqporn.com RE forum 92784
    - throatfucktube.com RE forum 92789
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47349 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 96a518f3fedf4821f028b3b51c42c58a8b180f31

--- src/jd/plugins/hoster/ThisAVCom.java
+++ src/jd/plugins/hoster/ThisAVCom.java
@@ -15,6 +15,11 @@
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package jd.plugins.hoster;
 
+import java.io.IOException;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.controller.LazyPlugin;
+
 import jd.PluginWrapper;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -25,10 +30,15 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "thisav.com" }, urls = { "https?://(?:www\\.)?thisav\\.com/video/\\d+/" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "thisav.com" }, urls = { "https?://(?:www\\.)?thisav\\.com/video/(\\d+)/" })
 public class ThisAVCom extends PluginForHost {
     public ThisAVCom(PluginWrapper wrapper) {
         super(wrapper);
+    }
+
+    @Override
+    public LazyPlugin.FEATURE[] getFeatures() {
+        return new LazyPlugin.FEATURE[] { LazyPlugin.FEATURE.XXX };
     }
 
     private static final String  DEFAULT_EXTENSION = ".mp4";
@@ -44,63 +54,97 @@ public class ThisAVCom extends PluginForHost {
 
     @Override
     public String getLinkID(final DownloadLink link) {
-        final String linkid = new Regex(link.getPluginPatternMatcher(), "(\\d+)$").getMatch(0);
-        if (linkid != null) {
-            return linkid;
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
         } else {
             return super.getLinkID(link);
         }
     }
 
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        if (!link.isNameSet()) {
+            link.setName(this.getFID(link) + DEFAULT_EXTENSION);
+        }
         setBrowserExclusive();
         br.setFollowRedirects(true);
-        getPage(link.getPluginPatternMatcher());
+        br.getPage(link.getPluginPatternMatcher());
         if (br.getHttpConnection().getResponseCode() == 404 || br.getURL().contains("error404")) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
         String title = br.getRegex("<title>([^<>\"]*?) - ").getMatch(0);
-        if (title == null) {
-            title = getLinkID(link);
+        if (title != null) {
+            title = Encoding.htmlDecode(title.trim());
+            String filename = title;
+            if (!filename.endsWith(DEFAULT_EXTENSION)) {
+                filename += DEFAULT_EXTENSION;
+            }
+            filename = encodeUnicode(filename);
+            link.setFinalFileName(filename);
         }
-        if (title == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        title = Encoding.htmlDecode(title.trim());
-        String filename = title;
-        if (!filename.endsWith(DEFAULT_EXTENSION)) {
-            filename += DEFAULT_EXTENSION;
-        }
-        filename = encodeUnicode(filename);
-        link.setFinalFileName(filename);
         String mpdUrl = br.getRegex("<source src=\"([^\"]+)\"[^>]+label=('|\")HD\\2").getMatch(0);
         if (mpdUrl == null) {
             mpdUrl = br.getRegex("<source src=\"([^\"]+)\"[^>]+label=('|\")SD\\2").getMatch(0);
         }
         if (mpdUrl == null) {
             mpdUrl = br.getRegex("<source src=\"([^\"]+)\"[^>]+dash\\+xml").getMatch(0);
-            if (mpdUrl == null) {
-                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        if (mpdUrl == null) {
+            final String cryptedScripts[] = br.getRegex("p\\}\\((.*?)\\.split\\('\\|'\\)").getColumn(0);
+            if (cryptedScripts != null && cryptedScripts.length != 0) {
+                for (String crypted : cryptedScripts) {
+                    String decoded = null;
+                    try {
+                        final Regex params = new Regex(crypted, "'(.*?[^\\\\])',(\\d+),(\\d+),'(.*?)'");
+                        String p = params.getMatch(0).replaceAll("\\\\", "");
+                        int a = Integer.parseInt(params.getMatch(1));
+                        int c = Integer.parseInt(params.getMatch(2));
+                        String[] k = params.getMatch(3).split("\\|");
+                        while (c != 0) {
+                            c--;
+                            if (k[c].length() != 0) {
+                                p = p.replaceAll("\\b" + Integer.toString(c, a) + "\\b", k[c]);
+                            }
+                        }
+                        decoded = p;
+                        mpdUrl = new Regex(decoded, "source\\s*=\\s*'(http[^<>\"']+)'").getMatch(0);
+                    } catch (Exception e) {
+                        logger.log(e);
+                    }
+                }
             }
         }
-        getPage(mpdUrl);
-        link.setDownloadSize(getDashFileSize());
-        String baseUrl = br.getRegex("<BaseURL>([^<>]+)</").getMatch(0);
-        if (baseUrl == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (mpdUrl != null) {
+            br.getPage(mpdUrl);
+            link.setDownloadSize(getDashFileSize());
+            String baseUrl = br.getRegex("<BaseURL>([^<>]+)</").getMatch(0);
+            if (baseUrl == null) {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+            dllink = mpdUrl.replaceFirst("/([^/]+)$", "/" + baseUrl);
         }
-        dllink = mpdUrl.replaceFirst("/([^/]+)$", "/" + baseUrl);
         return AvailableStatus.TRUE;
     }
 
     @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception, PluginException {
-        requestFileInformation(downloadLink);
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, FREE_RESUME, FREE_MAXCHUNKS);
-        if (dl.getConnection().getContentType().contains("html")) {
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link);
+        if (StringUtils.isEmpty(this.dllink)) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, FREE_RESUME, FREE_MAXCHUNKS);
+        if (!this.looksLikeDownloadableContent(dl.getConnection())) {
             logger.warning("The final dllink seems not to be a file!");
-            br.followConnection();
+            try {
+                br.followConnection(true);
+            } catch (final IOException e) {
+                logger.log(e);
+            }
             throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
         }
         dl.startDownload();
@@ -113,10 +157,6 @@ public class ThisAVCom extends PluginForHost {
             result = Long.parseLong(mediaRange[mediaRange.length - 1]);
         }
         return result;
-    }
-
-    private void getPage(String page) throws Exception {
-        br.getPage(page);
     }
 
     @Override
