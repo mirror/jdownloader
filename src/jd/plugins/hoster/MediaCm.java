5038255ac6e6450768cc65b0cb85f7d9b79d116e
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-04-03 13:33:00 +0000

    *Plugins: New plugins or new websites that belong to existing scripts* - [XFS] mediacm similar to streamhidecom RE forum 93123
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47522 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 37b49fe87660c8fefbde30158b79bcd40c6f5fa6

---
+++ src/jd/plugins/hoster/MediaCm.java
@@ -0,0 +1,191 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2013  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.plugins.components.XFileSharingProBasic;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.Account;
+import jd.plugins.Account.AccountType;
+import jd.plugins.DownloadLink;
+import jd.plugins.HostPlugin;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class MediaCm extends XFileSharingProBasic {
+    public MediaCm(final PluginWrapper wrapper) {
+        super(wrapper);
+        this.enablePremium(super.getPurchasePremiumURL());
+    }
+
+    /**
+     * DEV NOTES XfileSharingProBasic Version SEE SUPER-CLASS<br />
+     * mods: See overridden functions<br />
+     * limit-info:<br />
+     * captchatype-info: 2023-04-03: reCaptchaV2 <br />
+     * other: Similar to: streamhide.com <br />
+     */
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "media.cm" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "(?::\\d+)?" + "/(?:d/)?([a-z0-9]{12})");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        final AccountType type = account != null ? account.getType() : null;
+        if (AccountType.FREE.equals(type)) {
+            /* Free Account */
+            return true;
+        } else if (AccountType.PREMIUM.equals(type) || AccountType.LIFETIME.equals(type)) {
+            /* Premium account */
+            return true;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return true;
+        }
+    }
+
+    @Override
+    public int getMaxChunks(final Account account) {
+        final AccountType type = account != null ? account.getType() : null;
+        if (AccountType.FREE.equals(type)) {
+            /* Free Account */
+            return 0;
+        } else if (AccountType.PREMIUM.equals(type) || AccountType.LIFETIME.equals(type)) {
+            /* Premium account */
+            return 0;
+        } else {
+            /* Free(anonymous) and unknown account type */
+            return 0;
+        }
+    }
+
+    @Override
+    protected String getDllinkViaOfficialVideoDownload(final Browser br, final DownloadLink link, final Account account, final boolean returnFilesize) throws Exception {
+        if (returnFilesize) {
+            logger.info("[FilesizeMode] Trying to find official video downloads");
+            return null;
+        } else {
+            logger.info("[DownloadMode] Trying to find official video downloads");
+        }
+        String continueURL = br.getRegex("(/d/[a-z0-9]{12}_o)\"").getMatch(0);
+        if (continueURL != null) {
+            logger.info("Looks like official video download [original] is possible:" + continueURL);
+            this.getPage(br, continueURL);
+        } else {
+            continueURL = br.getRegex("(/d/[a-z0-9]{12}_[a-z])\"").getMatch(0);
+            if (continueURL != null) {
+                logger.info("Looks like video download is possible:" + continueURL);
+                this.getPage(br, continueURL);
+            }
+        }
+        String dllink = null;
+        final Form download1 = br.getFormByInputFieldKeyValue("op", "download_orig");
+        if (download1 != null) {
+            this.handleCaptcha(link, br, download1);
+            this.submitForm(br, download1);
+            this.checkErrors(br, br.getRequest().getHtmlCode(), link, account, false);
+        }
+        dllink = this.getDllink(link, account, br, br.toString());
+        if (StringUtils.isEmpty(dllink)) {
+            dllink = br.getRegex("(?i)This download link will be available for your IP-address next[^<]*</div>\\s*<a href=\"(https?://[^\"]+)").getMatch(0);
+        }
+        if (StringUtils.isEmpty(dllink)) {
+            logger.warning("Failed to find dllink via official video download");
+        } else {
+            logger.info("Successfully found dllink via official video download");
+        }
+        return dllink;
+    }
+
+    @Override
+    public int getMaxSimultaneousFreeAnonymousDownloads() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultaneousFreeAccountDownloads() {
+        return -1;
+    }
+
+    @Override
+    public int getMaxSimultanPremiumDownloadNum() {
+        return -1;
+    }
+
+    @Override
+    protected boolean isVideohoster_enforce_video_filename() {
+        return true;
+    }
+
+    @Override
+    protected String getContentURL(final DownloadLink link) {
+        return "https://" + this.getHost() + "/d/" + this.getFUIDFromURL(link);
+    }
+
+    protected String getFUID(final String url, URL_TYPE type) {
+        if (url != null) {
+            return new Regex(url, this.getSupportedLinks()).getMatch(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    protected boolean supportsShortURLs() {
+        return false;
+    }
+
+    @Override
+    protected boolean supports_availablecheck_filename_abuse() {
+        return false;
+    }
+
+    @Override
+    public String[] scanInfo(final String html, final String[] fileInfo) {
+        super.scanInfo(html, fileInfo);
+        final String betterFilename = new Regex(html, "(?i)<Title>\\s*Download ([^<]+)</Title>").getMatch(0);
+        if (betterFilename != null) {
+            fileInfo[0] = betterFilename;
+        }
+        return fileInfo;
+    }
+}
