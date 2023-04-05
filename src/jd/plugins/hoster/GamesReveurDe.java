1dd50d7f2b77dd92664e2d566fc205843707ff3a
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-04-05 11:50:16 +0000

    *Plugins: Fixes/Changes/Maintenance* - GamesReveurDe: fixed and updated pattern RE forum 93141
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47544 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3fed787fb5384a1db4b03e206daf3a59420e08ed

--- src/jd/plugins/hoster/GamesReveurDe.java
+++ src/jd/plugins/hoster/GamesReveurDe.java
@@ -17,6 +17,8 @@ package jd.plugins.hoster;
 
 import java.io.IOException;
 
+import org.appwork.utils.formatter.SizeFormatter;
+
 import jd.PluginWrapper;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -27,9 +29,7 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-import org.appwork.utils.formatter.SizeFormatter;
-
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "games.reveur.de" }, urls = { "https?://(?:www\\.)?games\\.reveur\\.de/(?:en|de)/[a-z0-9\\-]+/addons/[a-z0-9\\-]+/[a-z0-9\\-]+/[a-z0-9\\-]+/\\d+/[A-Za-z0-9\\-]+\\.html" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "games.reveur.de" }, urls = { "https?://(?:www\\.)?games\\.reveur\\.de/(?:en|de)/[A-Za-z0-9\\-_/]+\\.html" })
 public class GamesReveurDe extends PluginForHost {
     public GamesReveurDe(PluginWrapper wrapper) {
         super(wrapper);
@@ -40,30 +40,46 @@ public class GamesReveurDe extends PluginForHost {
         return "http://games.reveur.de/en/impressum/";
     }
 
-    public void correctDownloadLink(final DownloadLink link) {
-        link.setUrlDownload(link.getDownloadURL().replace("/de/", "/en/"));
-    }
-
-    @SuppressWarnings("deprecation")
     @Override
     public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        final String dlid = new Regex(link.getPluginPatternMatcher(), "(?i)/single/(\\d+)/").getMatch(0);
+        if (dlid == null) {
+            /* Invalid link. */
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String assumedFileExtension = ".rar";
+        if (!link.isNameSet()) {
+            final String urlpart = new Regex(link.getPluginPatternMatcher(), "https?://[^/]+/(.+)\\.html").getMatch(0);
+            link.setName(urlpart + assumedFileExtension);
+        }
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
         br.getHeaders().put("Accept-Language", "de,en-us;q=0.7,en;q=0.3");
-        br.getPage(link.getDownloadURL());
-        if (br.getHttpConnection().getResponseCode() == 404 || new Regex(br.getURL(), "https?://games\\.reveur\\.de/?$").matches() || br.containsHTML(">\\s*Download not available!\\s*<")) {
+        br.getPage(link.getPluginPatternMatcher());
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (br.containsHTML(">\\s*Download not available!\\s*<")) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (!this.canHandle(br.getURL())) {
+            /* E.g. redirect to mainpage */
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        String filename = br.getRegex("<title>([^<>\"]*?) zum Download</title>").getMatch(0);
+        String filename = br.getRegex("<div class=\"clear\"></div>\\s*</div>\\s*<h2>([^<]+)</h2>").getMatch(0);
+        if (filename == null) {
+            filename = br.getRegex("<title>([^<>\"]*?) zum Download</title>").getMatch(0);
+        }
         if (filename == null) {
             filename = br.getRegex("<title>([^<>\"]*?)</title>").getMatch(0);
         }
-        String filesize = br.getRegex("Downloadgröße: ([^<>\"]*?)</td>").getMatch(0);
+        String filesize = br.getRegex("(?i)Downloadgröße: ([^<>\"]*?)</td>").getMatch(0);
         if (filesize == null) {
             filesize = br.getRegex("class=\"addon-info-right\">[\t\n\r ]+(\\d+ (?:kB|mB|gB))[\t\n\r ]+</div>").getMatch(0);
         }
         if (filename != null) {
-            link.setName(Encoding.htmlDecode(filename.trim()));
+            filename = Encoding.htmlDecode(filename).trim();
+            filename += "_" + dlid;
+            filename = this.correctOrApplyFileNameExtension(filename, assumedFileExtension);
+            link.setName(Encoding.htmlDecode(filename).trim());
         }
         if (filesize != null) {
             link.setDownloadSize(SizeFormatter.getSize(filesize));
@@ -74,7 +90,7 @@ public class GamesReveurDe extends PluginForHost {
     @Override
     public void handleFree(final DownloadLink link) throws Exception, PluginException {
         requestFileInformation(link);
-        final String dlid = new Regex(link.getDownloadURL(), "/single/(\\d+)/").getMatch(0);
+        final String dlid = new Regex(link.getPluginPatternMatcher(), "/single/(\\d+)/").getMatch(0);
         final String dltype = br.getRegex("dlType = \\'([^<>\"]*?)\\';").getMatch(0);
         final String gameid = br.getRegex("gameId = \\'(\\d+)\\';").getMatch(0);
         final String gameshort = br.getRegex("gameShort = \\'([^<>\"]*?)\\';").getMatch(0);
@@ -83,7 +99,7 @@ public class GamesReveurDe extends PluginForHost {
         }
         br.getHeaders().put("Accept", "application/json, text/javascript, */*; q=0.01");
         br.getHeaders().put("X-Requested-With", "XMLHttpRequest");
-        br.postPage("https://games.reveur.de/includes/ajax/getDlValues.php", "");
+        br.postPage("/includes/ajax/getDlValues.php", "");
         final String val1 = getJson("value1");
         final String val2 = getJson("value2");
         final String sign = getJson("arithmethicSign");
@@ -98,7 +114,7 @@ public class GamesReveurDe extends PluginForHost {
         } else {
             result = value1 - value2;
         }
-        br.postPage("https://games.reveur.de/includes/ajax/checkUserInput.php", "params={\"val1\":" + value1 + ",\"val2\":" + value2 + ",\"result\":\"" + result + "\"}");
+        br.postPage("/includes/ajax/checkUserInput.php", "params={\"val1\":" + value1 + ",\"val2\":" + value2 + ",\"result\":\"" + result + "\"}");
         final String ticket = getJson("ticket");
         if (ticket == null) {
             /* Happens sometimes ... */
