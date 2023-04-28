341f43507f28cb3d436ba739ccc50308d25bd971
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-04-28 16:51:58 +0000

    *Plugins: Fixes/Changes/Maintenance* - ulozto: minor changes
    *Plugins: New plugins*
    - [YetiShare] sharewithclick
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47720 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 57bf5dd2b2a0c0075e83cdfcd1e9a3d700ccd627

--- src/jd/plugins/decrypter/GenericYetiShareFolder.java
+++ src/jd/plugins/decrypter/GenericYetiShareFolder.java
@@ -81,6 +81,7 @@ public class GenericYetiShareFolder extends antiDDoSForDecrypt {
         ret.add(new String[] { "wrzucaj.pl" });
         ret.add(new String[] { "egofiles.net" });
         ret.add(new String[] { "rapidshare.io" });
+        ret.add(new String[] { "sharewith.click" });
         return ret;
     }
 
