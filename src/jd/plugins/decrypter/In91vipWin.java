94ecd2e61b2570b289dea50851844ef28fa310ee
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-05-05 13:35:11 +0000

    *Plugins: New plugins* - In91vipWin RE forum 93391
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47741 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 4794b043d874f93ae9afa45a3536b98c8a2a47de

---
+++ src/jd/plugins/decrypter/In91vipWin.java
@@ -0,0 +1,104 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.decrypter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+
+import jd.PluginWrapper;
+import jd.controlling.ProgressController;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.parser.html.Form;
+import jd.plugins.CryptedLink;
+import jd.plugins.DecrypterPlugin;
+import jd.plugins.DownloadLink;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForDecrypt;
+
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class In91vipWin extends PluginForDecrypt {
+    public In91vipWin(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "in91vip.win" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(?:go/)?([A-Za-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
+        final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
+        br.setFollowRedirects(true);
+        final String contentID = new Regex(param.getCryptedUrl(), this.getSupportedLinks()).getMatch(0);
+        br.getPage(param.getCryptedUrl());
+        if (!br.getURL().endsWith(contentID) && !param.getCryptedUrl().matches("(?i)https?://[^/]+/[A-Za-z0-9]+$")) {
+            /* Small workaround for e.g. for "/go/..." links which can't be accessed right away. */
+            br.getPage("/" + contentID);
+        }
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        } else if (!br.getURL().contains(contentID)) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final Form form1 = br.getFormbyActionRegex(".*/go/.*");
+        if (form1 == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br).getToken();
+        form1.put("g-recaptcha-response", Encoding.urlEncode(recaptchaV2Response));
+        br.submitForm(form1);
+        final Form form2 = br.getFormbyActionRegex(".*/ts/.*");
+        if (form2 == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        /* Skip 12 seconds of wait time here and send that form. */
+        br.setFollowRedirects(false);
+        br.submitForm(form2);
+        final String finallink = br.getRedirectLocation();
+        if (finallink == null) {
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        ret.add(createDownloadlink(finallink));
+        return ret;
+    }
+}
