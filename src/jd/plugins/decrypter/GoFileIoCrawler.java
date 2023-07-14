e78a722b555a89501b6cbbdff3e6f7bffe4dd796
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-07-14 16:58:07 +0000

    build>full
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@47995 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 92aa3fb1754703b67bc15c7c6eca8ef00ee9e0cc

--- src/jd/plugins/decrypter/GoFileIoCrawler.java
+++ src/jd/plugins/decrypter/GoFileIoCrawler.java
@@ -4,14 +4,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.appwork.net.protocol.http.HTTPConstants;
-import org.appwork.storage.JSonStorage;
-import org.appwork.storage.TypeRef;
-import org.appwork.utils.Regex;
-import org.appwork.utils.StringUtils;
-import org.appwork.utils.net.HTTPHeader;
-import org.appwork.utils.parser.UrlQuery;
-
 import jd.controlling.ProgressController;
 import jd.http.Browser;
 import jd.http.requests.GetRequest;
@@ -25,7 +17,16 @@ import jd.plugins.FilePackage;
 import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginForHost;
 import jd.plugins.hoster.GofileIo;
+
+import org.appwork.net.protocol.http.HTTPConstants;
+import org.appwork.storage.JSonStorage;
+import org.appwork.storage.TypeRef;
+import org.appwork.utils.Regex;
+import org.appwork.utils.StringUtils;
+import org.appwork.utils.net.HTTPHeader;
+import org.appwork.utils.parser.UrlQuery;
 
 @DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "gofile.io" }, urls = { "https?://(?:www\\.)?gofile\\.io/(?:#download#|\\?c=|d/)([A-Za-z0-9\\-]+)$" })
 public class GoFileIoCrawler extends PluginForDecrypt {
@@ -83,7 +84,7 @@ public class GoFileIoCrawler extends PluginForDecrypt {
             /* E.g. {"status":"error-notFound","data":{}} */
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        final GofileIo hosterplugin = (GofileIo) this.getNewPluginForHostInstance(this.getHost());
+        PluginForHost hosterplugin = null;
         final Map<String, Object> data = (Map<String, Object>) response.get("data");
         final String currentFolderName = (String) data.get("name");
         String path = this.getAdoptedCloudFolderStructure();
@@ -103,6 +104,9 @@ public class GoFileIoCrawler extends PluginForDecrypt {
             if (type.equals("file")) {
                 final String fileID = item.getKey();
                 final String url = "https://" + this.getHost() + "/?c=" + folderID + "#file=" + fileID;
+                if (hosterplugin == null) {
+                    hosterplugin = this.getNewPluginForHostInstance(this.getHost());
+                }
                 final DownloadLink file = new DownloadLink(hosterplugin, null, this.getHost(), url, true);
                 GofileIo.parseFileInfo(file, entry);
                 file.setAvailable(true);
