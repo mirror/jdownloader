122d7198bfa496dbb446094ee5cc0fbb3c5971b5
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2023-07-27 14:06:53 +0000

    *Plugins: Fixes/Changes/Maintenance* - Plugins: minor refactoring refs #83699
    *Plugins: RIP*
    - hqcollect.net and hqcollect.me RE forum 93972
    
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@48065 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: d97ce06b91fe911895114a1543ad9864e74d737f

--- src/jd/plugins/hoster/HoodAmateursCom.java
+++ src/jd/plugins/hoster/HoodAmateursCom.java
@@ -13,15 +13,12 @@
 //
 //You should have received a copy of the GNU General Public License
 //along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 package jd.plugins.hoster;
 
-import java.io.IOException;
+import org.jdownloader.plugins.controller.LazyPlugin;
 
 import jd.PluginWrapper;
-import jd.config.Property;
 import jd.http.Browser;
-import jd.http.Browser.BrowserException;
 import jd.http.URLConnectionAdapter;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
@@ -32,18 +29,21 @@ import jd.plugins.LinkStatus;
 import jd.plugins.PluginException;
 import jd.plugins.PluginForHost;
 
-@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hoodamateurs.com" }, urls = { "https?://(?:www\\.)?hoodamateurs\\.com/\\d+/[A-Za-z0-9\\-_]+/[a-z0-9\\-]+" })
+@HostPlugin(revision = "$Revision$", interfaceVersion = 2, names = { "hoodamateurs.com" }, urls = { "https?://(?:www\\.)?hoodamateurs\\.com/(\\d+)/([A-Za-z0-9\\-_]+)/([a-z0-9\\-]+)?" })
 public class HoodAmateursCom extends PluginForHost {
-
     public HoodAmateursCom(PluginWrapper wrapper) {
         super(wrapper);
+    }
+
+    @Override
+    public LazyPlugin.FEATURE[] getFeatures() {
+        return new LazyPlugin.FEATURE[] { LazyPlugin.FEATURE.XXX };
     }
 
     /* Connection stuff */
     private static final boolean free_resume       = true;
     private static final int     free_maxchunks    = 0;
     private static final int     free_maxdownloads = -1;
-
     private String               dllink            = null;
 
     @Override
@@ -51,92 +51,59 @@ public class HoodAmateursCom extends PluginForHost {
         return "http://www.hoodamateurs.com/static/terms/";
     }
 
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(0);
+    }
+
     @SuppressWarnings("deprecation")
     @Override
-    public AvailableStatus requestFileInformation(final DownloadLink downloadLink) throws IOException, PluginException {
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws Exception {
+        final String fid = this.getFID(link);
+        if (!link.isNameSet()) {
+            final String urlSlug = new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1);
+            if (urlSlug != null) {
+                link.setName(urlSlug.replace("-", " ").trim() + ".mp4");
+            } else {
+                link.setName(fid + ".mp4");
+            }
+        }
         dllink = null;
         this.setBrowserExclusive();
         br.setFollowRedirects(true);
-        br.getPage(downloadLink.getDownloadURL());
+        br.getPage(link.getDownloadURL());
         if (br.getHttpConnection().getResponseCode() == 404) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         }
-        String filename = br.getRegex("<h1 class=\"title\">([^<>]*?)</h1>").getMatch(0);
-        if (filename == null) {
-            filename = br.getRegex("property=\"og:title\" content=\"([^<>]*?)\"").getMatch(0);
+        String title = br.getRegex("<h1 class=\"title\">([^<>]*?)</h1>").getMatch(0);
+        if (title == null) {
+            title = br.getRegex("property=\"og:title\" content=\"([^<>]*?)\"").getMatch(0);
         }
-        if (filename == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        filename = Encoding.htmlDecode(filename);
-        filename = filename.trim();
-        filename = encodeUnicode(filename);
-        br.getPage("/misc/nuevo/config.php?id=" + new Regex(downloadLink.getDownloadURL(), "hoodamateurs\\.com/(\\d+)/").getMatch(0));
+        br.getPage("/misc/nuevo/config.php?id=" + fid);
         dllink = br.getRegex("<file>(?:<\\!\\[CDATA\\[)?(http://[^<>\"]*?)(?:\\]\\]>)?</file>").getMatch(0);
-        if (filename == null || dllink == null) {
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        if (title != null) {
+            title = Encoding.htmlDecode(title);
+            title = title.trim();
+            link.setFinalFileName(title + ".mp4");
         }
-        dllink = Encoding.htmlDecode(dllink);
-        final String ext = getFileNameExtensionFromString(dllink, ".mp4");
-        if (!filename.endsWith(ext)) {
-            filename += ext;
-        }
-        downloadLink.setFinalFileName(filename);
-        final Browser br2 = br.cloneBrowser();
-        // In case the link redirects to the finallink
-        br2.setFollowRedirects(true);
-        URLConnectionAdapter con = null;
-        try {
-            try {
-                con = br2.openGetConnection(dllink);
-            } catch (final BrowserException e) {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            }
-            if (!con.getContentType().contains("html")) {
-                downloadLink.setDownloadSize(con.getLongContentLength());
-            } else {
-                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
-            }
-            downloadLink.setProperty("directlink", dllink);
-            return AvailableStatus.TRUE;
-        } finally {
-            try {
-                con.disconnect();
-            } catch (final Throwable e) {
-            }
-        }
-    }
-
-    @Override
-    public void handleFree(final DownloadLink downloadLink) throws Exception {
-        requestFileInformation(downloadLink);
-        dl = jd.plugins.BrowserAdapter.openDownload(br, downloadLink, dllink, free_resume, free_maxchunks);
-        if (dl.getConnection().getContentType().contains("html")) {
-            if (dl.getConnection().getResponseCode() == 403) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
-            } else if (dl.getConnection().getResponseCode() == 404) {
-                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
-            }
-            br.followConnection();
-            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
-        }
-        dl.startDownload();
-    }
-
-    private String checkDirectLink(final DownloadLink downloadLink, final String property) {
-        String dllink = downloadLink.getStringProperty(property);
         if (dllink != null) {
+            final Browser br2 = br.cloneBrowser();
+            // In case the link redirects to the finallink
+            br2.setFollowRedirects(true);
             URLConnectionAdapter con = null;
             try {
-                final Browser br2 = br.cloneBrowser();
-                con = br2.openGetConnection(dllink);
-                if (con.getContentType().contains("html") || con.getLongContentLength() == -1) {
-                    downloadLink.setProperty(property, Property.NULL);
-                    dllink = null;
-                }
-            } catch (final Exception e) {
-                downloadLink.setProperty(property, Property.NULL);
-                dllink = null;
+                con = br2.openHeadConnection(dllink);
+                handleConnectionErrors(br2, con);
+                link.setVerifiedFileSize(con.getCompleteContentLength());
             } finally {
                 try {
                     con.disconnect();
@@ -144,7 +111,28 @@ public class HoodAmateursCom extends PluginForHost {
                 }
             }
         }
-        return dllink;
+        return AvailableStatus.TRUE;
+    }
+
+    private void handleConnectionErrors(final Browser br, final URLConnectionAdapter con) throws Exception {
+        if (!this.looksLikeDownloadableContent(con)) {
+            br.followConnection(true);
+            if (con.getResponseCode() == 403) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 403", 60 * 60 * 1000l);
+            } else if (con.getResponseCode() == 404) {
+                throw new PluginException(LinkStatus.ERROR_TEMPORARILY_UNAVAILABLE, "Server error 404", 60 * 60 * 1000l);
+            } else {
+                throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+            }
+        }
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception {
+        requestFileInformation(link);
+        dl = jd.plugins.BrowserAdapter.openDownload(br, link, dllink, free_resume, free_maxchunks);
+        handleConnectionErrors(br, dl.getConnection());
+        dl.startDownload();
     }
 
     @Override
