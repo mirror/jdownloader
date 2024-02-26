5057c096ca6a09f54cf5addcb4e69e942bc0a580
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-02-26 13:34:52 +0000

    - Fixed regression
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@48708 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: fd310709847b95d89c8d509b394b4e57bea21931

--- src/jd/plugins/decrypter/TwitterComCrawler.java
+++ src/jd/plugins/decrypter/TwitterComCrawler.java
@@ -1654,7 +1654,6 @@ public class TwitterComCrawler extends PluginForDecrypt {
             final List<Map<String, Object>> users = (List<Map<String, Object>>) responseO;
             user = users.get(0);
         } else {
-            br.setAllowedResponseCodes(400);
             final String queryID = this.getGraphqlQueryID("UserByScreenName");
             br.getPage("https://api.twitter.com/graphql/" + queryID + "/UserByScreenName?variables=%7B%22screen_name%22%3A%22" + PluginJSonUtils.escape(username)
                     + "%22%2C%22withSafetyModeUserFields%22%3Atrue%7D&features=%7B%22hidden_profile_likes_enabled%22%3Atrue%2C%22hidden_profile_subscriptions_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22subscriptions_verification_info_is_identity_verified_enabled%22%3Atrue%2C%22subscriptions_verification_info_verified_since_enabled%22%3Atrue%2C%22highlights_tweets_tab_ui_enabled%22%3Atrue%2C%22responsive_web_twitter_article_notes_tab_enabled%22%3Atrue%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%7D&fieldToggles=%7B%22withAuxiliaryUserLabels%22%3Afalse%7D");
