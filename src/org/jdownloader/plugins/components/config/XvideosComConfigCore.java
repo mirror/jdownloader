997e731b5e87aab653b4c21d2794429c6971d7d6
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-03-05 15:37:55 +0000

    *General* Older stuff from my 2nd workspace which is starting to get more annoying as we can't push CORE-updates and I can't change stuff that gets used in plugins as this would also prevent plugin updates: - XvideosComConfigCore: added new setting (not yet used anywhere due to pending CORE-updates)
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@48743 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 2fb7b99bb51572ec5b746f31647ff372ba1943a0

--- src/org/jdownloader/plugins/components/config/XvideosComConfigCore.java
+++ src/org/jdownloader/plugins/components/config/XvideosComConfigCore.java
@@ -12,10 +12,11 @@ import org.jdownloader.plugins.config.TakeValueFromSubconfig;
 public interface XvideosComConfigCore extends PluginConfigInterface {
     final String                    text_EnableFastLinkcheckForHostPlugin = "Enable fast linkcheck for host plugin? If enabled, filesize won't be displayed until download is started!";
     final String                    text_PreferHLSStreamDownload          = "Prefer HLS download?";
-    final String                    text_TryToRecognizeLimit              = "Try to recognize limit?";
     final String                    text_PreferredHLSQuality              = "Select preferred HLS download quality. If your preferred HLS quality is not found, best quality will be downloaded instead.";
     final String                    text_PreferredHTTPQuality             = "Select preferred HTTP download quality. If your preferred HTTP quality is not found, best quality will be downloaded instead.";
+    final String                    text_TryToRecognizeLimit              = "Try to recognize limit?";
     final String                    text_PreferredOfficialDownloadQuality = "Select preferred official download quality ('download' button). If your preferred quality is not found, best quality will be downloaded instead.";
+    final String                    text_PluginContentURLExposeDirecturls = "Display video direct-URLs in GUI as 'Custom URL' if available?";
     public static final TRANSLATION TRANSLATION                           = new TRANSLATION();
 
     public static class TRANSLATION {
@@ -41,6 +42,10 @@ public interface XvideosComConfigCore extends PluginConfigInterface {
 
         public String getTryToRecognizeLimit_label() {
             return text_TryToRecognizeLimit;
+        }
+
+        public String getPluginContentURLExposeDirecturls_label() {
+            return text_PluginContentURLExposeDirecturls;
         }
     }
 
@@ -161,9 +166,16 @@ public interface XvideosComConfigCore extends PluginConfigInterface {
     void setPreferredOfficialDownloadQuality(PreferredOfficialDownloadQuality quality);
 
     @DefaultBooleanValue(false)
-    @DescriptionForConfigEntry("xvideos.com can 'shadow ban' users who download a lot. This will limit the max. available quality to 240p. This experimental setting will make JD try to detect this limit.")
+    @DescriptionForConfigEntry("xvideos.com/xvideos.red/xnxx.cc can 'shadow ban' users who download a lot. This will limit the max. available quality to 240p. This experimental setting will make JD try to detect this limit.")
     @Order(140)
     boolean isTryToRecognizeLimit();
 
     void setTryToRecognizeLimit(boolean b);
+
+    @DefaultBooleanValue(false)
+    @DescriptionForConfigEntry(text_PluginContentURLExposeDirecturls)
+    @Order(150)
+    boolean isPluginContentURLExposeDirecturls();
+
+    void setPluginContentURLExposeDirecturls(boolean b);
 }
