a85b69266362b30340507a4d4af5e71b838fbeb6
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-04-04 13:38:20 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@48858 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: 416a8d502321869a5c945c81a9bc3e53c6a28c1a

--- src/jd/plugins/hoster/FastShareCz.java
+++ src/jd/plugins/hoster/FastShareCz.java
@@ -442,14 +442,11 @@ public class FastShareCz extends PluginForHost {
     }
 
     private void checkErrors(final Browser br, final DownloadLink link, final Account account) throws PluginException {
-        {
-            /* [Premium-] account related error messages */
-            if (br.containsHTML("(?i)máte dostatečný kredit pro stažení tohoto souboru")) {
-                throw new AccountUnavailableException("Traffic limit reached", 5 * 60 * 1000l);
-            }
-        }
         final String errortextMaxConcurrentDownloadsLimit = "Reached max concurrent downloads limit";
-        if (br.containsHTML("(?i)(>100% FREE slotů je plných|>Využijte PROFI nebo zkuste později)")) {
+        /* [Premium-] account related error messages */
+        if (br.containsHTML("(?i)máte dostatečný kredit pro stažení tohoto souboru")) {
+            throw new AccountUnavailableException("Traffic limit reached", 5 * 60 * 1000l);
+        } else if (br.containsHTML("(?i)(>100% FREE slotů je plných|>Využijte PROFI nebo zkuste později)")) {
             throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, "No free slots available", 10 * 60 * 1000l);
         } else if (br.containsHTML("Přes FREE můžete stahovat jen jeden soubor současně|Pres FREE muzete stahovat jen jeden soubor najednou")) {
             throw new PluginException(LinkStatus.ERROR_HOSTER_TEMPORARILY_UNAVAILABLE, errortextMaxConcurrentDownloadsLimit, 3 * 60 * 1000l);
