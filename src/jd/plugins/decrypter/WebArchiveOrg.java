ae5d822310c4ee39a3dd2ccde75b18e55013ae2b
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-04-17 11:02:45 +0000

    *Plugins: Fixes/Changes/Maintenance* - ArchiveOrgWaybackMachine: deleted this plugin as it is more or less a duplicate of WebArchiveOrg - WebArchiveOrg: added handling which was previously only included in ArchiveOrgWaybackMachine RE forum 95614
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@48925 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 15694f4c720a027b4f6b2bb7dd81559b1d16c1ad

--- src/jd/plugins/decrypter/WebArchiveOrg.java
+++ src/jd/plugins/decrypter/WebArchiveOrg.java
@@ -1,11 +1,14 @@
 package jd.plugins.decrypter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
 import jd.http.Browser;
+import jd.http.URLConnectionAdapter;
+import jd.http.requests.GetRequest;
 import jd.nutils.encoding.Encoding;
 import jd.parser.Regex;
 import jd.plugins.CryptedLink;
@@ -16,10 +19,11 @@ import jd.plugins.PluginException;
 import jd.plugins.PluginForDecrypt;
 import jd.plugins.hoster.DirectHTTP;
 
-@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = { "web.archive.org" }, urls = { "https?://web\\.archive\\.org/web/\\d+.+" })
+@DecrypterPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
 public class WebArchiveOrg extends PluginForDecrypt {
-    private static final Pattern PATTERN_DIRECT = Pattern.compile("https?://web\\.archive\\.org/web/\\d+(im|oe)_/(https?.+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTERN_DIRECT = Pattern.compile("https?://web\\.archive\\.org/web/(\\d+)(if|im|oe)_/(https?.+)", Pattern.CASE_INSENSITIVE);
     private static final Pattern PATTERN_OTHER  = Pattern.compile("https?://web\\.archive\\.org/web/(\\d+)/(https?.+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern PATTERN_FILE   = Pattern.compile("^https?://[^/]+/web/(\\d+)[^/]*/(.+)", Pattern.CASE_INSENSITIVE);
 
     public WebArchiveOrg(PluginWrapper wrapper) {
         super(wrapper);
@@ -32,28 +36,88 @@ public class WebArchiveOrg extends PluginForDecrypt {
         return br;
     }
 
+    public static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForDecrypt, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "web.archive.org" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        return buildAnnotationUrls(getPluginDomains());
+    }
+
+    public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : pluginDomains) {
+            ret.add("https?://" + buildHostsPatternPart(domains) + "/web/[0-9]+.+");
+        }
+        return ret.toArray(new String[0]);
+    }
+
     @Override
     public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
-        if (new Regex(param.getCryptedUrl(), PATTERN_DIRECT).patternFind()) {
+        final Regex regexDirect = new Regex(param.getCryptedUrl(), PATTERN_DIRECT);
+        final Regex regexFile = new Regex(param.getCryptedUrl(), PATTERN_FILE);
+        if (regexDirect.patternFind()) {
+            /* Sure that we got a direct-URL */
             ret.add(createDownloadlink(DirectHTTP.createURLForThisPlugin(param.getCryptedUrl())));
+        } else if (regexFile.patternFind()) {
+            /* Unsure if we got a direct-URL -> Check it */
+            final String fileID = regexFile.getMatch(0);
+            final String linkpart = regexFile.getMatch(1);
+            final String url = "https://web.archive.org/web/" + fileID + "if_" + "/" + linkpart;
+            /* First check if maybe the user has added a directURL. */
+            final GetRequest getRequest = br.createGetRequest(url);
+            final URLConnectionAdapter con = this.br.openRequestConnection(getRequest);
+            try {
+                if (this.looksLikeDownloadableContent(con)) {
+                    final DownloadLink direct = getCrawler().createDirectHTTPDownloadLink(getRequest, con);
+                    ret.add(direct);
+                } else {
+                    br.followConnection();
+                    if (br.getHttpConnection().getResponseCode() == 404) {
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    }
+                    /* E.g. embedded PDF */
+                    final String directurl = br.getRegex("<iframe id=\"playback\"[^>]*src=\"(https?://[^\"]+)").getMatch(0);
+                    if (directurl == null) {
+                        logger.info("URL is not supported or content is offline");
+                        throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+                    } else {
+                        ret.add(this.createDownloadlink(DirectHTTP.createURLForThisPlugin(directurl)));
+                    }
+                }
+            } finally {
+                con.disconnect();
+            }
         } else {
+            final String youtubeVideoID = TbCmV2.getVideoIDFromUrl(param.getCryptedUrl());
+            if (youtubeVideoID == null) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND, "We can't handle this type of URL");
+            }
+            /* Look for direct-URLs */
+            br.setFollowRedirects(false);
+            br.getPage(param.getCryptedUrl());
+            br.getPage("https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/" + Encoding.urlEncode(youtubeVideoID));
+            final String directurl = br.getRedirectLocation();
+            if (directurl == null) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND, "Failed to extract YT directurl");
+            }
+            ret.add(createDownloadlink(DirectHTTP.createURLForThisPlugin(directurl)));
             final String originalURL = new Regex(param.getCryptedUrl(), PATTERN_OTHER).getMatch(1);
             if (originalURL != null) {
                 ret.add(createDownloadlink(Encoding.htmlDecode(originalURL)));
-            }
-            final String youtubeVideoID = TbCmV2.getVideoIDFromUrl(param.getCryptedUrl());
-            specialyoutubehandling: if (youtubeVideoID != null) {
-                /* Look for direct-URLs */
-                br.setFollowRedirects(false);
-                br.getPage(param.getCryptedUrl());
-                br.getPage("https://web.archive.org/web/2oe_/http://wayback-fakeurl.archive.org/yt/" + Encoding.urlEncode(youtubeVideoID));
-                final String directurl = br.getRedirectLocation();
-                if (directurl == null) {
-                    logger.info("Failed to extract YT directurl");
-                    break specialyoutubehandling;
-                }
-                createDownloadlink(DirectHTTP.createURLForThisPlugin(directurl));
             }
         }
         if (ret.isEmpty()) {
