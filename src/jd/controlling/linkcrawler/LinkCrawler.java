6412242e8cbdc9903e82914b7e5f0b08d00ee33b
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-07-30 15:10:02 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49466 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: a6c58d53e8b40c8695fbfd0e3b652b5715743333

--- src/jd/controlling/linkcrawler/LinkCrawler.java
+++ src/jd/controlling/linkcrawler/LinkCrawler.java
@@ -33,6 +33,29 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import jd.controlling.linkcollector.LinkCollectingJob;
+import jd.controlling.linkcollector.LinkCollector.JobLinkCrawler;
+import jd.controlling.linkcrawler.LinkCrawlerConfig.DirectHTTPPermission;
+import jd.controlling.linkcrawler.LinkCrawlerRule.RULE;
+import jd.http.AuthenticationFactory;
+import jd.http.Browser;
+import jd.http.Request;
+import jd.http.URLConnectionAdapter;
+import jd.http.requests.PostRequest;
+import jd.nutils.SimpleFTP;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.parser.html.HTMLParser;
+import jd.parser.html.HTMLParser.HtmlParserCharSequence;
+import jd.parser.html.HTMLParser.HtmlParserResultSet;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginsC;
+
 import org.appwork.net.protocol.http.HTTPConstants;
 import org.appwork.scheduler.DelayedRunnable;
 import org.appwork.storage.config.JsonConfig;
@@ -68,29 +91,6 @@ import org.jdownloader.plugins.controller.crawler.LazyCrawlerPlugin;
 import org.jdownloader.plugins.controller.host.HostPluginController;
 import org.jdownloader.plugins.controller.host.LazyHostPlugin;
 import org.jdownloader.settings.GeneralSettings;
-
-import jd.controlling.linkcollector.LinkCollectingJob;
-import jd.controlling.linkcollector.LinkCollector.JobLinkCrawler;
-import jd.controlling.linkcrawler.LinkCrawlerConfig.DirectHTTPPermission;
-import jd.controlling.linkcrawler.LinkCrawlerRule.RULE;
-import jd.http.AuthenticationFactory;
-import jd.http.Browser;
-import jd.http.Request;
-import jd.http.URLConnectionAdapter;
-import jd.http.requests.PostRequest;
-import jd.nutils.SimpleFTP;
-import jd.nutils.encoding.Encoding;
-import jd.parser.html.Form;
-import jd.parser.html.HTMLParser;
-import jd.parser.html.HTMLParser.HtmlParserCharSequence;
-import jd.parser.html.HTMLParser.HtmlParserResultSet;
-import jd.plugins.CryptedLink;
-import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
-import jd.plugins.Plugin;
-import jd.plugins.PluginForDecrypt;
-import jd.plugins.PluginForHost;
-import jd.plugins.PluginsC;
 
 public class LinkCrawler {
     private static enum DISTRIBUTE {
@@ -1469,7 +1469,7 @@ public class LinkCrawler {
                     BrowserCrawledLink next = this.openCrawlDeeperConnectionV2(source, matchingRule, br, nextRequest);
                     if (next != null) {
                         forwardCrawledLinkInfos(source, next, lm, getAndClearSourceURLs(source), true);
-                        crawl(generation, Arrays.asList(new BrowserCrawledLink[] { next }));
+                        crawl(generation, Arrays.asList(new CrawledLink[] { next }));
                         return;
                     }
                     final CrawledLink deeperSource;
@@ -1566,7 +1566,7 @@ public class LinkCrawler {
                         if (finalPackageName != null) {
                             PackageInfo.setName(next, finalPackageName);
                         }
-                        crawl(generation, Arrays.asList(new BrowserCrawledLink[] { next }));
+                        crawl(generation, Arrays.asList(new CrawledLink[] { next }));
                         return;
                     }
                     /* Deep-decrypt (generic and via DEEPDECRYPT LinkCrawler rule). */
@@ -2833,8 +2833,9 @@ public class LinkCrawler {
                     cryptedLink.setCryptedUrl(match);
                 }
                 cryptedLink.setSourceLink(source.getSourceLink());
-                if (source.getMatchingRule() != null) {
-                    link.setMatchingRule(source.getMatchingRule());
+                final LinkCrawlerRule sourceMatchingRule = source.getMatchingRule();
+                if (sourceMatchingRule != null) {
+                    link.setMatchingRule(sourceMatchingRule);
                 }
             }
             results.add(link);
@@ -2992,8 +2993,9 @@ public class LinkCrawler {
                         if (possibleCryptedLink.getUrlLink() == null || StringUtils.equals(possibleCryptedLink.getUrlLink(), crawledLink.getURL())) {
                             // modify sourceLink because crawledLink arise from possibleCryptedLink(wplg.getDownloadLinks)
                             crawledLink.setSourceLink(possibleCryptedLink.getSourceLink());
-                            if (possibleCryptedLink.getMatchingRule() != null) {
-                                crawledLink.setMatchingRule(possibleCryptedLink.getMatchingRule());
+                            final LinkCrawlerRule matchingRule = possibleCryptedLink.getMatchingRule();
+                            if (matchingRule != null) {
+                                crawledLink.setMatchingRule(matchingRule);
                             }
                         }
                         distributeFinalCrawledLink(generation, crawledLink);
