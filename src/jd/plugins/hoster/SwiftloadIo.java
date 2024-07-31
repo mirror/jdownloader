23c1f71fdfc9b83a18374ddcfe2252a6a03ade6e
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-07-31 09:07:12 +0000

    *Plugins: New plugins* - Swiftloadio
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49473 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 0487cd32071ac448726a7f95503f433fd1798b9c

---
+++ src/jd/plugins/hoster/SwiftloadIo.java
@@ -0,0 +1,175 @@
+//jDownloader - Downloadmanager
+//Copyright (C) 2009  JD-Team support@jdownloader.org
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+package jd.plugins.hoster;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.appwork.utils.StringUtils;
+import org.jdownloader.downloader.hls.HLSDownloader;
+import org.jdownloader.plugins.components.hls.HlsContainer;
+
+import jd.PluginWrapper;
+import jd.http.Browser;
+import jd.nutils.encoding.Encoding;
+import jd.parser.Regex;
+import jd.plugins.Account;
+import jd.plugins.DownloadLink;
+import jd.plugins.DownloadLink.AvailableStatus;
+import jd.plugins.HostPlugin;
+import jd.plugins.LinkStatus;
+import jd.plugins.PluginException;
+import jd.plugins.PluginForHost;
+
+@HostPlugin(revision = "$Revision$", interfaceVersion = 3, names = {}, urls = {})
+public class SwiftloadIo extends PluginForHost {
+    public SwiftloadIo(PluginWrapper wrapper) {
+        super(wrapper);
+        // this.enablePremium("");
+    }
+
+    @Override
+    public Browser createNewBrowserInstance() {
+        final Browser br = super.createNewBrowserInstance();
+        br.setFollowRedirects(true);
+        return br;
+    }
+
+    @Override
+    public String getAGBLink() {
+        return "https://" + getHost();
+    }
+
+    private static List<String[]> getPluginDomains() {
+        final List<String[]> ret = new ArrayList<String[]>();
+        // each entry in List<String[]> will result in one PluginForHost, Plugin.getHost() will return String[0]->main domain
+        ret.add(new String[] { "swiftload.io" });
+        return ret;
+    }
+
+    public static String[] getAnnotationNames() {
+        return buildAnnotationNames(getPluginDomains());
+    }
+
+    @Override
+    public String[] siteSupportedNames() {
+        return buildSupportedNames(getPluginDomains());
+    }
+
+    public static String[] getAnnotationUrls() {
+        final List<String> ret = new ArrayList<String>();
+        for (final String[] domains : getPluginDomains()) {
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/(d|e)/([a-z0-9]+)");
+        }
+        return ret.toArray(new String[0]);
+    }
+
+    @Override
+    public String getLinkID(final DownloadLink link) {
+        final String fid = getFID(link);
+        if (fid != null) {
+            return this.getHost() + "://" + fid;
+        } else {
+            return super.getLinkID(link);
+        }
+    }
+
+    private String getFID(final DownloadLink link) {
+        return new Regex(link.getPluginPatternMatcher(), this.getSupportedLinks()).getMatch(1);
+    }
+
+    @Override
+    public boolean isResumeable(final DownloadLink link, final Account account) {
+        // HLS = not resumable
+        return false;
+    }
+
+    public int getMaxChunks(final DownloadLink link, final Account account) {
+        return 0;
+    }
+
+    @Override
+    public AvailableStatus requestFileInformation(final DownloadLink link) throws IOException, PluginException {
+        final String fid = this.getFID(link);
+        if (!link.isNameSet()) {
+            /* Fallback */
+            link.setName(fid + ".mp4");
+        }
+        this.setBrowserExclusive();
+        br.getPage("https://" + getHost() + "/d/" + fid);
+        if (br.getHttpConnection().getResponseCode() == 404) {
+            throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+        }
+        final String errorcode = br.getRegex("let error = '([^\\']+)'").getMatch(0);
+        if (errorcode != null) {
+            if (errorcode.equalsIgnoreCase("not_found")) {
+                throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
+            } else {
+                /* Unknown error */
+                throw new PluginException(LinkStatus.ERROR_FATAL, "Unknown error: " + errorcode);
+            }
+        }
+        String filename = br.getRegex("File Name:\\s*</div>\\s*<input[^>]*value=\"([^\"]+)").getMatch(0);
+        String filesizeBytesStr = br.getRegex("id=\"\"[^>]*value=\"(\\d+)").getMatch(0);
+        if (filename != null) {
+            filename = Encoding.htmlDecode(filename).trim();
+            link.setName(filename);
+        }
+        if (filesizeBytesStr != null) {
+            link.setDownloadSize(Long.parseLong(filesizeBytesStr));
+        }
+        return AvailableStatus.TRUE;
+    }
+
+    @Override
+    public void handleFree(final DownloadLink link) throws Exception, PluginException {
+        handleDownload(link);
+    }
+
+    private void handleDownload(final DownloadLink link) throws Exception, PluginException {
+        requestFileInformation(link);
+        checkFFmpeg(link, "Download a HLS Stream");
+        String dllink = br.getRegex("id=\"downloadNow\"[^>]*href=\"(https?://[^\"]+master\\.m3u8)\"").getMatch(0);
+        if (StringUtils.isEmpty(dllink)) {
+            logger.warning("Failed to find final downloadurl");
+            throw new PluginException(LinkStatus.ERROR_PLUGIN_DEFECT);
+        }
+        dllink = Encoding.htmlOnlyDecode(dllink);
+        br.getPage(dllink);
+        final HlsContainer best = HlsContainer.findBestVideoByBandwidth(HlsContainer.getHlsQualities(this.br));
+        dl = new HLSDownloader(link, br, best.getDownloadurl());
+        dl.startDownload();
+    }
+
+    @Override
+    public boolean hasCaptcha(DownloadLink link, jd.plugins.Account acc) {
+        return false;
+    }
+
+    @Override
+    public int getMaxSimultanFreeDownloadNum() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public void reset() {
+    }
+
+    @Override
+    public void resetDownloadlink(DownloadLink link) {
+    }
+}
