f084335047db673366706e6e0af24be3867f4b68
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-08-08 10:22:45 +0000

    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49541 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    Former-commit-id: b9f918900886380225962b567fe4e3373c71cf8e

--- src/org/jdownloader/updatev2/gui/DefaultFlatLAFExtension.java
+++ src/org/jdownloader/updatev2/gui/DefaultFlatLAFExtension.java
@@ -1,8 +1,20 @@
 package org.jdownloader.updatev2.gui;
 
+import org.appwork.storage.config.annotations.DefaultBooleanValue;
+import org.appwork.utils.os.CrossSystem;
 
 public class DefaultFlatLAFExtension extends DefaultEmptyLookAndFeelExtension {
     public DefaultFlatLAFExtension(final String laf) {
         super(laf);
+    }
+
+    @Override
+    @DefaultBooleanValue(true)
+    public boolean isWindowDecorationEnabled() {
+        if (CrossSystem.isWindows()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
