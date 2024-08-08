ced74ad8372b6facd0a569955168be269b3e1880
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-08-08 11:10:38 +0000

    CustomPanel: -fixed add, use Number instead of Integer as underlying Instance can also be Byte/Short
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49547 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 3147d97fc8c6cac92d4fd6900f9d0a3b9bf4b4b0

--- src/org/jdownloader/controlling/contextmenu/gui/CustomPanel.java
+++ src/org/jdownloader/controlling/contextmenu/gui/CustomPanel.java
@@ -234,15 +234,11 @@ public class CustomPanel extends MigPanel {
                 cb.setSelectedItem(values[index]);
                 add(new JLabel(getNameForCustomizer(gs)), "growx,spanx,wrap");
                 add(cb, "growx,spanx,wrap");
-            } else if (gs.getType() == String.class) {
-
-                String value = (String) actionData.fetchSetup(gs.getKey());
-
+            } else if (Clazz.isString(gs.getType())) {
+                String value = StringUtils.valueOfOrNull(actionData.fetchSetup(gs.getKey()));
                 if (value == null) {
                     value = (String) gs.get(actionClass);
-
                 }
-
                 final ExtTextField cb = new ExtTextField() {
 
                     @Override
@@ -253,21 +249,16 @@ public class CustomPanel extends MigPanel {
                         }
                         managerFrame.repaint();
                     }
-
                 };
 
                 cb.setText(value);
                 add(new JLabel(getNameForCustomizer(gs)), "growx,spanx,wrap");
                 add(cb, "growx,spanx,wrap,pushx,growx");
-            } else if (Clazz.isInteger(gs.getType())) {
-
-                Integer value = (Integer) actionData.fetchSetup(gs.getKey());
-
+            } else if (Clazz.isFixedPointNumber(gs.getType())) {
+                Number value = (Number) actionData.fetchSetup(gs.getKey());
                 if (value == null) {
-                    value = (Integer) gs.get(actionClass);
-
+                    value = (Number) gs.get(actionClass);
                 }
-
                 final ExtSpinner spinner = new ExtSpinner(new SpinnerNumberModel(value.intValue(), -1, 10000, 1));
                 spinner.addChangeListener(new ChangeListener() {
 
