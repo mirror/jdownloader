aa52863b5e6759ffb1d2de0b5606580615c8aa74
jiaz <jiaz@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-08-16 22:46:05 +0000

    GenericAutoContainer: -override canHandle and check for LinkCrawlerConfig.isAutoImportContainer there
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49601 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: e4ed4b0d2919f82c8b2d1e69690f5e63ab4bc4b7

--- src/jd/plugins/decrypter/GenericAutoContainer.java
+++ src/jd/plugins/decrypter/GenericAutoContainer.java
@@ -39,54 +39,54 @@ public class GenericAutoContainer extends PluginForDecrypt {
     }
 
     @Override
+    public boolean canHandle(String data) {
+        return super.canHandle(data) && JsonConfig.create(LinkCrawlerConfig.class).isAutoImportContainer();
+    }
+
+    @Override
     public ArrayList<DownloadLink> decryptIt(final CryptedLink param, ProgressController progress) throws Exception {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         final String url = param.getCryptedUrl();
-        if (!JsonConfig.create(LinkCrawlerConfig.class).isAutoImportContainer()) {
+        final List<LazyCrawlerPlugin> nextLazyCrawlerPlugins = findNextLazyCrawlerPlugins(url);
+        if (nextLazyCrawlerPlugins.size() > 0) {
             // forward to next supporting plugin
             ret.add(createDownloadlink(url));
-        } else {
-            final List<LazyCrawlerPlugin> nextLazyCrawlerPlugins = findNextLazyCrawlerPlugins(url);
-            if (nextLazyCrawlerPlugins.size() > 0) {
-                // forward to next supporting plugin
-                ret.add(createDownloadlink(url));
-                return ret;
-            }
-            final List<LazyHostPlugin> nextLazyHostPlugins = findNextLazyHostPlugins(url);
-            if (nextLazyHostPlugins.size() > 0) {
-                // forward to next supporting plugin
-                ret.add(createDownloadlink(url));
-                return ret;
-            }
-            final String type = new Regex(url, this.getSupportedLinks()).getMatch(0);
-            final URLConnectionAdapter con = br.openGetConnection(url);
-            File containerTemp = null;
-            try {
-                if (con.isOK()) {
-                    boolean seemsValidContainer = StringUtils.containsIgnoreCase(con.getContentType(), type);
-                    seemsValidContainer = seemsValidContainer | (con.isContentDisposition() && StringUtils.containsIgnoreCase(Plugin.getFileNameFromConnection(con), type)) || (con.getContentLength() > 100 && (con.getContentType() == null || !StringUtils.containsIgnoreCase(con.getContentType(), "text")));
-                    if (seemsValidContainer) {
-                        containerTemp = org.appwork.utils.Application.getResource("tmp/autocontainer/" + System.nanoTime() + "." + type);
-                        br.downloadConnection(containerTemp, con);
-                        if (containerTemp.exists() && containerTemp.length() > 100) {
-                            ret.addAll(loadContainerFile(containerTemp));
-                        }
+            return ret;
+        }
+        final List<LazyHostPlugin> nextLazyHostPlugins = findNextLazyHostPlugins(url);
+        if (nextLazyHostPlugins.size() > 0) {
+            // forward to next supporting plugin
+            ret.add(createDownloadlink(url));
+            return ret;
+        }
+        final String type = new Regex(url, this.getSupportedLinks()).getMatch(0);
+        final URLConnectionAdapter con = br.openGetConnection(url);
+        File containerTemp = null;
+        try {
+            if (con.isOK()) {
+                boolean seemsValidContainer = StringUtils.containsIgnoreCase(con.getContentType(), type);
+                seemsValidContainer = seemsValidContainer | (con.isContentDisposition() && StringUtils.containsIgnoreCase(Plugin.getFileNameFromConnection(con), type)) || (con.getContentLength() > 100 && (con.getContentType() == null || !StringUtils.containsIgnoreCase(con.getContentType(), "text")));
+                if (seemsValidContainer) {
+                    containerTemp = org.appwork.utils.Application.getResource("tmp/autocontainer/" + System.nanoTime() + "." + type);
+                    br.downloadConnection(containerTemp, con);
+                    if (containerTemp.exists() && containerTemp.length() > 100) {
+                        ret.addAll(loadContainerFile(containerTemp));
                     }
                 }
-            } catch (final IOException e) {
-                logger.log(e);
-                if (ret.size() == 0) {
-                    ret.add(createDownloadlink(url));
-                }
-            } finally {
-                if (containerTemp != null && containerTemp.exists()) {
-                    containerTemp.delete();
-                }
-                con.disconnect();
             }
-            if (containerTemp != null && ret.size() == 0) {
+        } catch (final IOException e) {
+            logger.log(e);
+            if (ret.size() == 0) {
                 ret.add(createDownloadlink(url));
             }
+        } finally {
+            if (containerTemp != null && containerTemp.exists()) {
+                containerTemp.delete();
+            }
+            con.disconnect();
+        }
+        if (containerTemp != null && ret.size() == 0) {
+            ret.add(createDownloadlink(url));
         }
         return ret;
     }
