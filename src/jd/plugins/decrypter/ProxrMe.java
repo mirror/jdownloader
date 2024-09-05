ee1874deee49af0f41afcff1b8e8f05218394844
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-09-05 14:01:32 +0000

    *Plugins: Fixes/Changes/Maintenance* - ProxrMe: added captcha support RE forum 90673
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49718 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: 900c57d6124c597314f8146e49d1a805c49720a5

--- src/jd/plugins/decrypter/ProxrMe.java
+++ src/jd/plugins/decrypter/ProxrMe.java
@@ -17,8 +17,12 @@ package jd.plugins.decrypter;
 
 import java.util.ArrayList;
 
+import org.appwork.utils.parser.UrlQuery;
+import org.jdownloader.captcha.v2.challenge.recaptcha.v2.CaptchaHelperCrawlerPluginRecaptchaV2;
+
 import jd.PluginWrapper;
 import jd.controlling.ProgressController;
+import jd.http.Browser;
 import jd.plugins.CryptedLink;
 import jd.plugins.DecrypterPlugin;
 import jd.plugins.DownloadLink;
@@ -36,6 +40,16 @@ public class ProxrMe extends PluginForDecrypt {
         br.setFollowRedirects(true);
         final String contenturl = param.getCryptedUrl().replaceFirst("(?i)http://", "https://");
         br.getPage(contenturl);
+        if (br.containsHTML("id=\"checkCaptcha\"")) {
+            final String recaptchaV2Response = new CaptchaHelperCrawlerPluginRecaptchaV2(this, br).getToken();
+            final Browser brc = br.cloneBrowser();
+            final UrlQuery query = new UrlQuery();
+            query.appendEncoded("response", recaptchaV2Response);
+            brc.postPage("/components/com_proxer/misc/captcha/recaptcha.php", query);
+            logger.info("Reload page after captcha");
+            br.getPage(contenturl);
+            /* Offline/404 can happen AFTER captcha! */
+        }
         if (br.getRequest().getHttpConnection().getResponseCode() == 404) {
             throw new PluginException(LinkStatus.ERROR_FILE_NOT_FOUND);
         } else if (br.containsHTML("/images/misc/404\\.png\"")) {
@@ -52,5 +66,11 @@ public class ProxrMe extends PluginForDecrypt {
         final ArrayList<DownloadLink> ret = new ArrayList<DownloadLink>();
         ret.add(createDownloadlink(finallink));
         return ret;
+    }
+
+    @Override
+    public int getMaxConcurrentProcessingInstances() {
+        /* 2024-09-05: Only 1 in order to avoid captchas */
+        return 1;
     }
 }
