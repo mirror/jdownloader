179b8dd19d94763bb70e95d8e2c6dae0e88df8f3
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-09-17 10:03:06 +0000

    *General* - LinkCrawler: added not yet used method which finds propertyPatterns results refs #89309
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49809 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: c364b92ca43238810918a4c76821da61c97e4eb0

--- src/jd/controlling/linkcrawler/LinkCrawler.java
+++ src/jd/controlling/linkcrawler/LinkCrawler.java
@@ -33,29 +33,6 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import jd.controlling.linkcollector.LinkCollectingJob;
-import jd.controlling.linkcollector.LinkCollector.JobLinkCrawler;
-import jd.controlling.linkcrawler.LinkCrawlerConfig.DirectHTTPPermission;
-import jd.controlling.linkcrawler.LinkCrawlerRule.RULE;
-import jd.http.AuthenticationFactory;
-import jd.http.Browser;
-import jd.http.Request;
-import jd.http.URLConnectionAdapter;
-import jd.http.requests.PostRequest;
-import jd.nutils.SimpleFTP;
-import jd.nutils.encoding.Encoding;
-import jd.parser.html.Form;
-import jd.parser.html.HTMLParser;
-import jd.parser.html.HTMLParser.HtmlParserCharSequence;
-import jd.parser.html.HTMLParser.HtmlParserResultSet;
-import jd.plugins.CryptedLink;
-import jd.plugins.DownloadLink;
-import jd.plugins.FilePackage;
-import jd.plugins.Plugin;
-import jd.plugins.PluginForDecrypt;
-import jd.plugins.PluginForHost;
-import jd.plugins.PluginsC;
-
 import org.appwork.net.protocol.http.HTTPConstants;
 import org.appwork.scheduler.DelayedRunnable;
 import org.appwork.storage.config.JsonConfig;
@@ -91,6 +68,29 @@ import org.jdownloader.plugins.controller.crawler.LazyCrawlerPlugin;
 import org.jdownloader.plugins.controller.host.HostPluginController;
 import org.jdownloader.plugins.controller.host.LazyHostPlugin;
 import org.jdownloader.settings.GeneralSettings;
+
+import jd.controlling.linkcollector.LinkCollectingJob;
+import jd.controlling.linkcollector.LinkCollector.JobLinkCrawler;
+import jd.controlling.linkcrawler.LinkCrawlerConfig.DirectHTTPPermission;
+import jd.controlling.linkcrawler.LinkCrawlerRule.RULE;
+import jd.http.AuthenticationFactory;
+import jd.http.Browser;
+import jd.http.Request;
+import jd.http.URLConnectionAdapter;
+import jd.http.requests.PostRequest;
+import jd.nutils.SimpleFTP;
+import jd.nutils.encoding.Encoding;
+import jd.parser.html.Form;
+import jd.parser.html.HTMLParser;
+import jd.parser.html.HTMLParser.HtmlParserCharSequence;
+import jd.parser.html.HTMLParser.HtmlParserResultSet;
+import jd.plugins.CryptedLink;
+import jd.plugins.DownloadLink;
+import jd.plugins.FilePackage;
+import jd.plugins.Plugin;
+import jd.plugins.PluginForDecrypt;
+import jd.plugins.PluginForHost;
+import jd.plugins.PluginsC;
 
 public class LinkCrawler {
     private static enum DISTRIBUTE {
@@ -1688,6 +1688,31 @@ public class LinkCrawler {
         } finally {
             checkFinishNotify(task);
         }
+    }
+
+    private Map<String, String> findPropertyPatternMatches(final Browser br, final LinkCrawlerRule rule) {
+        final Map<String, List<Pattern>> patternmap = rule._getPropertyPatterns();
+        if (patternmap == null) {
+            return null;
+        }
+        final Map<String, String> results = new HashMap<String, String>();
+        patternmapLoop: for (final Entry<String, List<Pattern>> entry : patternmap.entrySet()) {
+            final String key = entry.getKey();
+            for (final Pattern pattern : entry.getValue()) {
+                final Regex regex = new Regex(br, pattern);
+                if (!regex.patternFind()) {
+                    continue;
+                }
+                String match = new Regex(br, pattern).getMatch(0);
+                if (match == null) {
+                    match = regex.getMatch(-1);
+                }
+                results.put(key, match);
+                /* Take first match */
+                break;
+            }
+        }
+        return results;
     }
 
     /** Opens connection */
