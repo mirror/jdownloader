a9963a1356e107f0e33347988bb73f6f943e9f94
psp <psp@ebf7c1c2-ba36-0410-9fe8-c592906822b4>
2024-09-26 16:43:43 +0000

    *Plugins: Fixes/Changes/Maintenance* - NfileCc: updated pattern RE forum 96502
    git-svn-id: svn://svn.jdownloader.org/jdownloader/trunk@49877 ebf7c1c2-ba36-0410-9fe8-c592906822b4
    
    Former-commit-id: f55cf90ea750682d02c8924a2575a678b1d319b3

--- src/jd/plugins/decrypter/NfileCc.java
+++ src/jd/plugins/decrypter/NfileCc.java
@@ -56,7 +56,7 @@ public class NfileCc extends PluginForDecrypt {
     public static String[] buildAnnotationUrls(final List<String[]> pluginDomains) {
         final List<String> ret = new ArrayList<String>();
         for (final String[] domains : pluginDomains) {
-            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([A-Za-z0-9]+)/?$");
+            ret.add("https?://(?:www\\.)?" + buildHostsPatternPart(domains) + "/([A-Za-z0-9]+)(/[^/]*)?");
         }
         return ret.toArray(new String[0]);
     }
